a198b47c6bedf8aab8d3148c051e427b89d14702
epoger@google.com <epoger@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2011-06-23 14:34:29 +0000

    rename master_linux_cfg.py to master_builders_cfg.py, and associated cleanup
    git-svn-id: http://skia.googlecode.com/svn/buildbot@1683 2bbb7eff-a529-9590-31e7-b0007b416f81

--- master/master_builders_cfg.py
+++ master/master_builders_cfg.py
@@ -2,6 +2,8 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+# Sets up all the builders we want this buildbot master to run.
+
 import os
 
 from master import master_config
