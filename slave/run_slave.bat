0f71364781d5c4c04a26c69ff9cd2f4d36b5deeb
epoger@google.com <epoger@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2011-08-01 15:28:48 +0000

    Update windows buildbot slave to match other platforms
    git-svn-id: http://skia.googlecode.com/svn/buildbot@2007 2bbb7eff-a529-9590-31e7-b0007b416f81

--- slave/run_slave.bat
+++ slave/run_slave.bat
@@ -1,2 +1,11 @@
-set PYTHONPATH=..\third_party\chromium_buildbot\third_party\buildbot_7_12;..\third_party\chromium_buildbot\third_party\twisted_8_1;..\third_party\chromium_buildbot\site_config;..\site_config
+set THIRDPARTY=..\third_party
+set CHROMIUM_BUILDBOT=%THIRDPARTY%\chromium_buildbot
+set PUBLIC_CONFIG=%CHROMIUM_BUILDBOT%\site_config
+set PRIVATE_CONFIG=..\site_config
+
+set PYTHONPATH=%CHROMIUM_BUILDBOT%\third_party\buildbot_7_12;%CHROMIUM_BUILDBOT%\third_party\twisted_8_1;%PUBLIC_CONFIG%;%PRIVATE_CONFIG%
+
+xcopy /y %PRIVATE_CONFIG%\config_private.py %PUBLIC_CONFIG%
+xcopy /y %PRIVATE_CONFIG%\bot_password %PUBLIC_CONFIG%\.bot_password
+
 python run_slave.py --no_save -y buildbot.tac
