7dbb53f3eaebc5aed7808cd2a8dea8ebc458682b
epoger@google.com <epoger@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2011-08-03 20:41:12 +0000

    Clean up override of bot_password in private buildbot config Review URL: http://codereview.appspot.com/4849041
    git-svn-id: http://skia.googlecode.com/svn/buildbot@2037 2bbb7eff-a529-9590-31e7-b0007b416f81

--- slave/run_slave.bat
+++ slave/run_slave.bat
@@ -1,11 +1,8 @@
 set THIRDPARTY=..\third_party
 set CHROMIUM_BUILDBOT=%THIRDPARTY%\chromium_buildbot
-set PUBLIC_CONFIG=%CHROMIUM_BUILDBOT%\site_config
-set PRIVATE_CONFIG=..\site_config
+set PUBLICCONFIG_DIR=%CHROMIUM_BUILDBOT%\site_config
+set PRIVATECONFIG_DIR=..\site_config
 
-set PYTHONPATH=%CHROMIUM_BUILDBOT%\third_party\buildbot_7_12;%CHROMIUM_BUILDBOT%\third_party\twisted_8_1;%PUBLIC_CONFIG%;%PRIVATE_CONFIG%
-
-xcopy /y %PRIVATE_CONFIG%\config_private.py %PUBLIC_CONFIG%
-xcopy /y %PRIVATE_CONFIG%\bot_password %PUBLIC_CONFIG%\.bot_password
+set PYTHONPATH=%CHROMIUM_BUILDBOT%\third_party\buildbot_7_12;%CHROMIUM_BUILDBOT%\third_party\twisted_8_1;%PUBLICCONFIG_DIR%;%PRIVATECONFIG_DIR%
 
 python run_slave.py --no_save -y buildbot.tac
