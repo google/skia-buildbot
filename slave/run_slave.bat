8fc0bdcaf7b5dcb09c8604d174d7995a8ca0a0e0
epoger@google.com <epoger@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2012-03-29 18:38:28 +0000

    Make Skia buildbot slave scripts just thin wrappers around standard, DEPS-pulled Chromium buildbot slave scripts
    This will make it easier to incorporate changes to the Chromium buildbot scripts, while still having control over our own slave repository.
    Review URL: https://codereview.appspot.com/5962043
    
    git-svn-id: http://skia.googlecode.com/svn/buildbot@3544 2bbb7eff-a529-9590-31e7-b0007b416f81

--- slave/run_slave.bat
+++ slave/run_slave.bat
@@ -5,4 +5,5 @@ set PRIVATECONFIG_DIR=..\site_config
 
 set PYTHONPATH=%CHROMIUM_BUILDBOT%\third_party\buildbot_7_12;%CHROMIUM_BUILDBOT%\third_party\twisted_8_1;%PUBLICCONFIG_DIR%;%PRIVATECONFIG_DIR%
 
-python run_slave.py --no_save -y buildbot.tac
+echo fake bot_password to quiet run_slave.py >%PUBLICCONFIG_DIR%\.bot_password
+python %CHROMIUM_BUILDBOT%\slave\run_slave.py --no_save -y buildbot.tac
