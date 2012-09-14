de712c234ceb4d77382a6e40ed3b2213c72ce651
borenet@google.com <borenet@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2012-09-14 21:08:04 +0000

    Warning instead of failure on BenchPictures GPU
    Bots still can't handle the scaled-down size, so we'll disable them to get the
    bots green until https://code.google.com/p/skia/issues/detail?id=875 is fixed.
    Review URL: https://codereview.appspot.com/6503119
    
    git-svn-id: http://skia.googlecode.com/svn/buildbot@5559 2bbb7eff-a529-9590-31e7-b0007b416f81

--- slave/skia_slave_scripts/bench_pictures.py
+++ slave/skia_slave_scripts/bench_pictures.py
@@ -7,6 +7,7 @@
 
 from utils import misc
 from build_step import BuildStep
+from build_step import BuildStepWarning
 from run_bench import BuildArgs
 from run_bench import RunBench
 from run_bench import PreBench
@@ -30,8 +31,14 @@ class BenchPictures(RunBench):
   def _Run(self, args):
     self._DoBenchPictures('bitmap')
     gyp_defines = os.environ.get('GYP_DEFINES', '')
-    if 'skia_gpu=0' not in gyp_defines:
-      self._DoBenchPictures('gpu')
+    # TODO(borenet): bench_pictures in gpu mode crashes on Android and Windows.
+    # re-enable this when http://code.google.com/p/skia/issues/detail?id=875
+    # is fixed.
+    try:
+      if ('skia_gpu=0' not in gyp_defines):
+        self._DoBenchPictures('gpu')
+    except:
+      raise BuildStepWarning
 
 
 if '__main__' == __name__:
