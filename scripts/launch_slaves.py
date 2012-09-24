5348a407c06000a53ce6011ce09995e83699c7ec
borenet@google.com <borenet@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2012-09-24 21:05:56 +0000

    Fix launch_slaves script
    - Now properly mounts drives
    - Works on Windows, should work elsewhere too
    
    Unreviewed
    
    git-svn-id: http://skia.googlecode.com/svn/buildbot@5652 2bbb7eff-a529-9590-31e7-b0007b416f81

--- scripts/launch_slaves.py
+++ scripts/launch_slaves.py
@@ -1,31 +1,73 @@
 import optparse
 import os
-import pickle
-import shutil
+if os.name == 'nt':
+  import win32api
+  import string
 import subprocess
 import sys
 
 DEFAULT_SLAVENAME = 'production-slave'
-DEFAULT_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                            DEFAULT_SLAVENAME)
+SVN_URL = 'https://skia.googlecode.com/svn/buildbot'
+DRIVE_MAPPING=True
+
+if os.name == 'nt':
+  def GetFirstFreeDriveLetter():
+    all_possible = [c for c in string.uppercase]
+    in_use = win32api.GetLogicalDriveStrings()
+    free = list(set(all_possible) - set(in_use))
+    return free[0]
 
 def StartSlave(slavename):
   print 'Starting slave: %s' % slavename
-  slave_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)),
+  slave_dir = os.path.join(os.path.realpath(os.curdir),
                            slavename)
   if not os.path.isdir(slave_dir):
-    print 'creating directory: %s' % slave_dir
-    shutil.copytree(DEFAULT_DIR, slave_dir)
+    print 'Creating directory: %s' % slave_dir
+    os.makedirs(slave_dir)
+    os.chdir(slave_dir)
+    if os.name == 'nt':
+      gclient = 'gclient.bat'
+    else:
+      gclient = 'gclient'
+    proc = subprocess.Popen([gclient, 'config', SVN_URL])
+    if proc.wait() != 0:
+      raise Exception('Could not successfully configure gclient.')
+    print 'configured.'
+    proc = subprocess.Popen([gclient, 'sync'])
+    if proc.wait() != 0:
+      raise Exception('Sync failed.')
+
   try:
-    os.remove(os.path.join(slave_dir, 'buildbot', 'third_party', 'chromium_buildbot', 'slave', 'twistd.pid'))
+    os.remove(os.path.join(slave_dir, 'buildbot', 'third_party',
+                           'chromium_buildbot', 'slave', 'twistd.pid'))
   except:
     pass
-  cmd = 'setlocal &&'
-  cmd += 'net use O: \\\\localhost\%s&&O:&&cd %s&&' % (slave_dir.replace(':', '$'), os.path.join('buildbot', 'slave'))
-  if slavename != DEFAULT_SLAVENAME:
-    cmd += 'set TESTING_SLAVENAME=%s&&' % slavename
-  cmd += 'run_slave.bat'
-  cmd += '&& endlocal'
+
+  if os.name == 'nt':
+    # We run different commands for the Windows shell
+    if DRIVE_MAPPING:
+      drive_letter = GetFirstFreeDriveLetter()
+      print 'Mapping %c' % drive_letter
+      cmd = 'net use %c: \\\\localhost\%s' % (drive_letter,
+                                              slave_dir.replace(':', '$'))
+      print 'Running cmd: %s' % cmd
+      proc = subprocess.Popen(cmd)
+      if proc.wait() != 0:
+        raise Exception('Could not map %c' % drive_letter)
+      cmd = '%c:' % drive_letter
+      os.chdir(os.path.join('%c:' % drive_letter))
+      print os.path.realpath(os.curdir)
+    os.chdir(os.path.join('buildbot', 'slave'))
+    cmd = 'setlocal&&'
+    if slavename != DEFAULT_SLAVENAME:
+      cmd += 'set TESTING_SLAVENAME=%s&&' % slavename
+    cmd += 'run_slave.bat'
+    cmd += '&& endlocal'
+  else:
+    cmd = ''
+    if slavename != DEFAULT_SLAVENAME:
+      cmd += 'TESTING_SLAVENAME=%s ' % slavename
+    cmd += 'make -C %s restart' % os.path.join(slave_dir, 'buildbot', 'slave')
   print 'Running cmd: %s' % cmd
   subprocess.Popen(cmd, shell=True)
 
@@ -39,7 +81,7 @@ def LoadConfigFile(config_file):
   f.close()
   return slaves
 
-def main(argv):
+def main():
   """ Launch local build slave instances """
   option_parser = optparse.OptionParser()
   option_parser.add_option(
@@ -54,8 +96,9 @@ def main(argv):
   slaves = LoadConfigFile(options.config_file)
   if not slaves:
     slaves = [DEFAULT_SLAVENAME]
+  print 'In dir: %s' % os.path.realpath(os.curdir);
   for slavename in slaves:
     StartSlave(slavename)
 
 if '__main__' == __name__:
-  sys.exit(main(None))
+  sys.exit(main())
