59748578fe7ed6d0d7dd6f10ab9679b345010870
borenet@google.com <borenet@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2012-11-16 14:31:31 +0000

    Change build status behavior for skipped steps
    r6336 modified the behavior so that, when we skip steps on builds for
    rebaseline commits, the skipped steps inherit the result of the last time they
    ran. While this is correct in most cases, there is a race condition in which
    multiple slaves running the same builder produce incorrect results if the
    status changes just before a rebaseline commit so that the newly-successful or
    failing build is started before but finishes after the build for the rebaseline
    commit. The rebaseline build will inherit the wrong statuses.
    
    Instead, we abandon the notion of inheriting statuses and just mark the build
    as "some steps skipped" if all of the steps which ran succeeded but some were
    skipped. If some steps were skipped but one or more which ran failed, we still
    mark the build as a failure. This eliminates the confusion, but I'm not sure
    how it will affect breakage notifications since "some steps skipped" still
    counts as a success.
    Review URL: https://codereview.appspot.com/6843055
    
    git-svn-id: http://skia.googlecode.com/svn/buildbot@6456 2bbb7eff-a529-9590-31e7-b0007b416f81

--- master/skia_master_scripts/skia_build.py
+++ master/skia_master_scripts/skia_build.py
@@ -13,53 +13,20 @@ import types
 class SkiaBuild(Build):
   """ Skia-specific overrides for Build. """
 
-  def setupBuild(self, expectations):
-    Build.setupBuild(self, expectations)
-    # Set the initial build result to 'SKIPPED'. If any step succeeds or fails,
-    # this will be changed to reflect that. This setting causes builds in which
-    # all steps were skipped *and* all previous runs of those steps were either
-    # skipped or never run to be displayed as 'SKIPPED'.
-    self.result = SKIPPED
-
   def stepDone(self, step_result, step):
-    # This check is done in the superclass method in
-    # buildbot.process.build.Build, but since we may modify step_result, we have
-    # to do it here.
-    if type(step_result) == types.TupleType:
-      step_result, text = step_result
-    else:
-      text = None
-
-    # If this step was skipped, set its result to the result of the last run.
-    # Note that this only affects the success/failure status of the overall
-    # build; the individual step will still be shown as SKIPPED, since we don't
-    # modify step.result.
-    try:
-      if self.builder.builder_status.buildstepstatuses.has_key(step.name):
-        previous_result = self.builder.builder_status.buildstepstatuses[step.name]
-        if step_result == SKIPPED:
-          step_result = previous_result
-    except AttributeError:
-      self.builder.builder_status.buildstepstatuses = {}
-    if step_result != SKIPPED:
-      self.builder.builder_status.buildstepstatuses[step.name] = step_result
-
-    # Reset the result parameter to be used by the superclass method.
-    if text:
-      result_param = (step_result, text)
-    else:
-      result_param = step_result
-    build_result = self.result
-
     # Superclass method which sets the result of the build based on the result
     # of each step.
-    terminate = Build.stepDone(self, result_param, step)
+    terminate = Build.stepDone(self, step_result, step)
 
-    # Since we set the initial build status to SKIPPED instead of SUCCESS, and
-    # since SKIPPED is "worse" than SUCCESS according to
-    # buildbot.status.worst_status, it is possible that the superclass' stepDone
-    # method did not properly set the build status as SUCCESS.
-    if build_result == SKIPPED and step_result == SUCCESS:
-      self.result = SUCCESS
+    # step_result is a tuple if the step has any extra information associated
+    # with it.
+    if type(step_result) == types.TupleType:
+      step_result = step_result[0]
+
+    # We want the build result to show SKIPPED if all steps which ran succeeded
+    # but some steps were skipped. The superclass method just ignores the result
+    # of skipped steps.
+    if self.result == SUCCESS and step_result == SKIPPED:
+      self.result = SKIPPED
 
     return terminate
