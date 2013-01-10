bc216b75a0aef4da93c3c8cbd74854689ad3a974
borenet@google.com <borenet@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2013-01-10 21:11:30 +0000

    Modifications to launch_slaves.py
    - Read config information from checked-in slave_hosts.cfg file
    - Add "copies" section to allow copying files into buildslave checkouts
    Review URL: https://codereview.appspot.com/7026053
    
    git-svn-id: http://skia.googlecode.com/svn/buildbot@7124 2bbb7eff-a529-9590-31e7-b0007b416f81

--- scripts/launch_slaves.py
+++ scripts/launch_slaves.py
@@ -3,20 +3,27 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+
 """ Launch multiple buildbot slave instances on a single machine.  This script
 is intended to be run at boot time. """
 
-import optparse
+
+from contextlib import closing
+
 import os
 if os.name == 'nt':
   import win32api
   import string
+import shutil
+import socket
 import subprocess
 import sys
+import urllib2
 
-DEFAULT_SLAVENAME = 'production-slave'
+
 SVN_URL = 'https://skia.googlecode.com/svn/buildbot'
 DRIVE_MAPPING=True
+
 
 if os.name == 'nt':
   def GetFirstFreeDriveLetter():
@@ -26,18 +33,21 @@ if os.name == 'nt':
     free = list(set(all_possible) - set(in_use))
     return free[0]
 
-def StartSlave(slavename):
+
+def RunSlave(slavename, copies):
   """ Launch a single slave, checking out the buildbot tree if necessary.
 
-  slavename: string indicating the hostname of the build slave to launch
+  slavename: string indicating the hostname of the build slave to launch.
+  copies: dictionary with 'source' and 'destination' keys whose values are the
+      current location and destination location within the buildbot checkout of
+      files to be copied.
   """
   print 'Starting slave: %s' % slavename
   start_dir = os.path.realpath(os.curdir)
   slave_dir = os.path.join(start_dir, slavename)
-  if not os.path.isdir(slave_dir):
-    print 'Creating directory: %s' % slave_dir
-    os.makedirs(slave_dir)
-    os.chdir(slave_dir)
+
+  # Optionally map the slave directory to a drive letter.  This is helpful in
+  # avoiding path length limits on Windows.
   if os.name == 'nt' and DRIVE_MAPPING:
     drive_letter = GetFirstFreeDriveLetter()
     print 'Mapping %c' % drive_letter
@@ -47,14 +57,17 @@ def StartSlave(slavename):
     proc = subprocess.Popen(cmd)
     if proc.wait() != 0:
       raise Exception('Could not map %c' % drive_letter)
-    os.chdir(os.path.join('%c:' % drive_letter))
     # Because of weirdness in gclient, we can't run "gclient sync" in a drive
     # root.  So, we inject a minimal extra level.
-    subdir_path = 'b'
-    if not os.path.isdir(subdir_path):
-      os.mkdir(subdir_path)
-    os.chdir(subdir_path)
-    print os.path.realpath(os.curdir)
+    slave_dir = os.path.join('%c:' % drive_letter, 'b')
+
+  # Create the slave directory if needed
+  if not os.path.isdir(slave_dir):
+    print 'Creating directory: %s' % slave_dir
+    os.makedirs(slave_dir)
+
+  # Check out or update the buildbot scripts
+  os.chdir(slave_dir)
   if os.name == 'nt':
     gclient = 'gclient.bat'
   else:
@@ -65,59 +78,66 @@ def StartSlave(slavename):
   proc = subprocess.Popen([gclient, 'sync', '-j1'])
   if proc.wait() != 0:
     raise Exception('Sync failed.')
+  os.chdir(start_dir)
+
+  # Perform Copies
+  for copy in copies:
+    src = os.path.normpath(copy['source'])
+    dest = os.path.join(slave_dir, os.path.normpath(copy['destination']))
+    print 'Copying %s to %s' % (src, dest)
+    shutil.copy(src, dest)
 
   try:
     os.remove(os.path.join(slave_dir, 'buildbot', 'third_party',
                            'chromium_buildbot', 'slave', 'twistd.pid'))
   except:
     pass
-
+  os.chdir(os.path.join(slave_dir, 'buildbot', 'slave'))
   if os.name == 'nt':
     # We run different commands for the Windows shell
-    os.chdir(os.path.join('buildbot', 'slave'))
     cmd = 'setlocal&&'
-    if slavename != DEFAULT_SLAVENAME:
-      cmd += 'set TESTING_SLAVENAME=%s&&' % slavename
+    cmd += 'set TESTING_SLAVENAME=%s&&' % slavename
     cmd += 'run_slave.bat'
     cmd += '&& endlocal'
   else:
-    cmd = ''
-    if slavename != DEFAULT_SLAVENAME:
-      cmd += 'TESTING_SLAVENAME=%s ' % slavename
-    cmd += 'make -C %s restart' % os.path.join(slave_dir, 'buildbot', 'slave')
+    cmd = 'TESTING_SLAVENAME=%s ' % slavename
+    cmd += 'make restart'
   print 'Running cmd: %s' % cmd
   subprocess.Popen(cmd, shell=True)
   os.chdir(start_dir)
 
-def LoadConfigFile(config_file):
-  """ Loads build slave hostnames from the given config_file. """
-  f = open(config_file, 'r')
-  slaves = []
-  for line in f:
-    line = line.rstrip('\r\n')
-    if line != '':
-      slaves.append(line)
-  f.close()
-  return slaves
+
+def GetCfg(url):
+  """ Retrieve a config file from the SVN repository and return it.
+  
+  url: string; the url of the file to load.
+  """
+  with closing(urllib2.urlopen(url)) as f:
+    vars = {}
+    exec(f.read(), vars)
+    return vars
+
+
+def GetSlaveHostCfg():
+  """ Retrieve the latest slave_hosts.cfg file from the SVN repository and
+  return the slave host configuration for this machine.
+  """
+  cfg_vars = GetCfg(SVN_URL + '/site_config/slave_hosts.cfg')
+  return cfg_vars['GetSlaveHostConfig'](socket.gethostname())
+
 
 def main():
   """ Launch local build slave instances """
-  option_parser = optparse.OptionParser()
-  option_parser.add_option(
-      '--config_file',
-      help='file containing slavenames to run on this machine')
-  (options, args) = option_parser.parse_args()
-  if len(args) != 0:
-    raise Exception('bogus command-line argument; rerun with --help')
-  if not options.config_file:
-    raise Exception('missing command-line option %s; rerun with --help' %
-        '--config_file')
-  slaves = LoadConfigFile(options.config_file)
-  if not slaves:
-    slaves = [DEFAULT_SLAVENAME]
-  print 'In dir: %s' % os.path.realpath(os.curdir);
+
+  # Obtain configuration information about this build slave host machine.
+  slave_host = GetSlaveHostCfg()
+  slaves = slave_host['slaves']
+  copies = slave_host['copies']
+
+  # Launch the build slaves
   for slavename in slaves:
-    StartSlave(slavename)
+    RunSlave(slavename, copies)
+
 
 if '__main__' == __name__:
   sys.exit(main())
