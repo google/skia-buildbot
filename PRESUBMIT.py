bb3cccdf76ee43ed7800295f5a029443c7f08249
rmistry@google.com <rmistry@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2013-01-18 12:05:16 +0000

    Adding a presubmit script for buildbot Review URL: https://codereview.appspot.com/7086052
    git-svn-id: http://skia.googlecode.com/svn/buildbot@7269 2bbb7eff-a529-9590-31e7-b0007b416f81

---
+++ PRESUBMIT.py
@@ -0,0 +1,59 @@
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+
+"""Presubmit checks for the buildbot code."""
+
+
+def CheckChange(input_api, output_api):
+  """Presubmit checks for the change on upload or commit.
+
+  The presubmit checks have been handpicked from the list of canned checks
+  here:
+  http://src.chromium.org/viewvc/chrome/trunk/tools/depot_tools/presubmit_canned_checks.py
+
+  The following are the presubmit checks:
+  * Pylint is run if the change contains any .py files.
+  * Enforces max length for all lines is 80.
+  * Checks that the user didn't add TODO(name) without an owner.
+  * Checks that there is no stray whitespace at source lines end.
+  * Checks that there are no tab characters in any of the text files.
+  """
+  results = []
+
+  pylint_disabled_warnings = (
+      'F0401',  # Unable to import.
+      'E0611',  # No name in module.
+      'W0232',  # Class has no __init__ method.
+      'E1002',  # Use of super on an old style class.
+      'W0403',  # Relative import used.
+  )
+  # Run Pylint on only the modified python files. Unfortunately it still runs
+  # Pylint on the whole file instead of just the modified lines.
+  affected_python_files = []
+  for affected_file in input_api.AffectedSourceFiles(None):
+    affected_file_path = affected_file.LocalPath()
+    if affected_file_path.endswith('.py'):
+      affected_python_files.append(affected_file_path)
+  results += input_api.canned_checks.RunPylint(
+      input_api, output_api,
+      disabled_warnings=pylint_disabled_warnings,
+      white_list=affected_python_files)
+
+  results += input_api.canned_checks.CheckLongLines(input_api, output_api)
+  results += input_api.canned_checks.CheckChangeTodoHasOwner(
+      input_api, output_api)
+  results += input_api.canned_checks.CheckChangeHasNoStrayWhitespace(
+      input_api, output_api)
+  results += input_api.canned_checks.CheckChangeHasNoTabs(input_api, output_api)
+
+  return results
+
+
+def CheckChangeOnUpload(input_api, output_api):
+  return CheckChange(input_api, output_api)
+
+
+def CheckChangeOnCommit(input_api, output_api):
+  return CheckChange(input_api, output_api)
