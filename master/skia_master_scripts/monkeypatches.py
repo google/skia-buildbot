8dc18110345bd7a67f393b5a608f233b38af072a
borenet@google.com <borenet@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2013-01-24 13:57:50 +0000

    Verify that the requested builders are actually trybots Review URL: https://codereview.appspot.com/7194048
    git-svn-id: http://skia.googlecode.com/svn/buildbot@7360 2bbb7eff-a529-9590-31e7-b0007b416f81

--- master/skia_master_scripts/monkeypatches.py
+++ master/skia_master_scripts/monkeypatches.py
@@ -5,9 +5,11 @@
 
 """ Monkeypatches to override upstream code. """
 
+from master import try_job_base
 from master import try_job_svn
 from master.try_job_base import text_to_dict
 from twisted.internet import defer
+from twisted.python import log
 
 
 ################################################################################
@@ -18,7 +20,7 @@ from twisted.internet import defer
 @defer.deferredGenerator
 def SubmitTryJobChanges(self, changes):
   """ Override of SVNPoller.submit_changes:
-  http://src.chromium.org/viewvc/chrome/trunk/tools/build/scripts/master/try_job_svn.py?revision=119459&view=markup
+  http://src.chromium.org/viewvc/chrome/trunk/tools/build/scripts/master/try_job_svn.py?view=markup
 
   We modify it so that the patch file url is added to the build properties.
   This allows the slave to download the patch directly rather than receiving
@@ -46,3 +48,28 @@ def SubmitTryJobChanges(self, changes):
     self.parent.addChangeInner(chdict['files'], parsed, change.number)
 
 try_job_svn.SVNPoller.submit_changes = SubmitTryJobChanges
+
+
+def TryJobCreateBuildset(self, ssid, parsed_job):
+  """ Override of TryJobBase.create_buildset:
+  http://src.chromium.org/viewvc/chrome/trunk/tools/build/scripts/master/try_job_base.py?view=markup
+
+  We modify it to verify that the requested builders are in the builder pool for
+  this try scheduler. This prevents try requests from running on builders which
+  are not registered as trybots. This apparently isn't a problem for Chromium
+  since they use a separate try master.
+  """
+  log.msg('Creating try job(s) %s' % ssid)
+  result = None
+  for builder in parsed_job['bot']:
+    if builder in self.pools[self.name]:
+      result = self.addBuildsetForSourceStamp(ssid=ssid,
+          reason=parsed_job['name'],
+          external_idstring=parsed_job['name'],
+          builderNames=[builder],
+          properties=self.get_props(builder, parsed_job))
+    else:
+      log.msg('Rejecting try job for builder: %s' % builder)
+  return result
+
+try_job_base.TryJobBase.create_buildset = TryJobCreateBuildset
