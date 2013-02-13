f61a233fbfe0945c96f728da7e15884eac217faf
borenet@google.com <borenet@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2013-02-13 14:45:14 +0000

    Even more logging
    Unreviewed, will revert
    (SkipBuildbotRuns)
    
    git-svn-id: http://skia.googlecode.com/svn/buildbot@7716 2bbb7eff-a529-9590-31e7-b0007b416f81

--- slave/skia_slave_scripts/android_run_tests.py
+++ slave/skia_slave_scripts/android_run_tests.py
@@ -9,17 +9,19 @@ from android_build_step import AndroidBuildStep
 from build_step import BuildStep
 from utils import android_utils
 import sys
+import threading
 
 
 class AndroidRunTests(AndroidBuildStep):
   def _Run(self):
     android_utils.RunSkia(self._serial, ['tests'], stop_shell=self._has_root,
                           use_intent=(not self._has_root))
+    print 'AndroidRunTests process done...'
+    print 'Threads still running:\n%s' % threading.enumerate()
 
 
 if '__main__' == __name__:
   exitcode = BuildStep.RunBuildStep(AndroidRunTests)
   print 'AndroidRunTests finished with code %d' % exitcode
-  import threading
   print 'Threads still running:\n%s' % threading.enumerate()
   sys.exit(exitcode)
