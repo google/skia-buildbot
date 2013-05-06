7b1e642f0c2bb89493a5b8bfcb40b02be9d4b171
borenet@google.com <borenet@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2013-05-06 12:49:22 +0000

    Skip RenderPdfs until the crash is fixed
    Unreviewed
    (SkipBuildbotRuns)
    
    Review URL: https://codereview.chromium.org/14820009
    
    git-svn-id: http://skia.googlecode.com/svn/buildbot@9007 2bbb7eff-a529-9590-31e7-b0007b416f81

--- slave/skia_slave_scripts/render_pdfs.py
+++ slave/skia_slave_scripts/render_pdfs.py
@@ -7,13 +7,15 @@
 """ Run the Skia render_pdfs executable. """
 
 
-from build_step import BuildStep
+from build_step import BuildStep, BuildStepWarning
 import sys
 
 
 class RenderPdfs(BuildStep):
   def _Run(self):
-    self.RunFlavoredCmd('render_pdfs', [self._device_dirs.SKPDir()])
+    # Skip this step for now, since the new SKPs are causing it to crash.
+    raise BuildStepWarning('Skipping this step since it is crashing.')
+    #self.RunFlavoredCmd('render_pdfs', [self._device_dirs.SKPDir()])
 
 if '__main__' == __name__:
   sys.exit(BuildStep.RunBuildStep(RenderPdfs))
