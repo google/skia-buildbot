57b5024d39d24a6ced77cf4cca7d762ff1ac4298
rmistry@google.com <rmistry@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2013-06-11 19:34:15 +0000

    Using default user's key in vm_run_lua_on_slaves.sh.
    (SkipBuildbotRuns)
    
    Review URL: https://codereview.chromium.org/15675022
    
    git-svn-id: http://skia.googlecode.com/svn/buildbot@9513 2bbb7eff-a529-9590-31e7-b0007b416f81

--- compute_engine_scripts/telemetry/telemetry_master_scripts/vm_run_lua_on_slaves.sh
+++ compute_engine_scripts/telemetry/telemetry_master_scripts/vm_run_lua_on_slaves.sh
@@ -42,7 +42,7 @@ gclient sync
 for SLAVE_NUM in $(seq 1 $NUM_SLAVES); do
   CMD="bash vm_run_lua_on_skps.sh $SLAVE_NUM $LUA_SCRIPT_GS_LOCATION $RUN_ID"
   ssh -f -X -o UserKnownHostsFile=/dev/null -o CheckHostIP=no \
-    -o StrictHostKeyChecking=no -i /usr/local/google/home/rmistry/.ssh/google_compute_engine \
+    -o StrictHostKeyChecking=no -i /home/default/.ssh/google_compute_engine \
     -A -p 22 default@108.170.222.$SLAVE_NUM -- "source .bashrc; cd skia-repo/buildbot/compute_engine_scripts/telemetry/telemetry_slave_scripts; $CMD > /tmp/lua_output.$RUN_ID.txt 2>&1"
 done
 
