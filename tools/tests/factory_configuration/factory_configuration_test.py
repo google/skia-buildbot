e8d5771ed8228ec3507280c91b5a40d71774d120
borenet@google.com <borenet@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2013-06-12 18:35:12 +0000

    Add rebaselining capability to factory_configuration_test.
    (SkipBuildbotRuns)
    
    R=epoger@google.com, rmistry@google.com
    
    Review URL: https://codereview.chromium.org/16822002
    
    git-svn-id: http://skia.googlecode.com/svn/buildbot@9539 2bbb7eff-a529-9590-31e7-b0007b416f81

--- tools/tests/factory_configuration/factory_configuration_test.py
+++ tools/tests/factory_configuration/factory_configuration_test.py
@@ -6,11 +6,12 @@
 expectation. """
 
 
+from distutils import dir_util
 import os
 import sys
 
-buildbot_path = os.path.join(os.path.abspath(os.path.dirname(__file__)),
-                             os.pardir, os.pardir, os.pardir)
+my_path = os.path.abspath(os.path.dirname(__file__))
+buildbot_path = os.path.join(my_path, os.pardir, os.pardir, os.pardir)
 sys.path.append(os.path.join(buildbot_path, 'master'))
 sys.path.append(os.path.join(buildbot_path, 'site_config'))
 sys.path.append(os.path.join(buildbot_path, 'third_party', 'chromium_buildbot',
@@ -25,13 +26,13 @@ import config_private
 import master_builders_cfg
 
 
-def main():
+def RunTest(die_on_validation_failure=True):
   c = {}
   c['schedulers'] = []
   c['builders'] = []
 
   # Make sure that the configuration errors out if validation fails.
-  config_private.die_on_validation_failure = True
+  config_private.die_on_validation_failure = die_on_validation_failure
 
   # Pretend that the master is the production master, so that the tested
   # configuration is identical to that of the production master.
@@ -40,6 +41,17 @@ def main():
   # Run the configuration.
   master_builders_cfg.Update(config, config.Master.Skia, c)
 
+
+def main():
+  if '--rebaseline' in sys.argv:
+    print 'Generating new actuals.'
+    RunTest(die_on_validation_failure=False)
+    print 'Copying actual to expected.'
+    dir_util.copy_tree(os.path.join(my_path, 'actual'),
+                       os.path.join(my_path, 'expected'))
+  else:
+    print 'Validating factory configuration.'
+    RunTest()
 
 if '__main__' == __name__:
   sys.exit(main())
