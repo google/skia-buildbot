da5f5cc8e8c78222178829cc8cc3e0a64f5d06f8
rmistry@google.com <rmistry@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2013-06-13 11:18:38 +0000

    Make sure the AppEngine poller never crashes.
    (SkipBuildbotRuns)
    
    Review URL: https://codereview.chromium.org/16954005
    
    git-svn-id: http://skia.googlecode.com/svn/buildbot@9570 2bbb7eff-a529-9590-31e7-b0007b416f81

--- compute_engine_scripts/telemetry/telemetry_master_scripts/appengine_poller.py
+++ compute_engine_scripts/telemetry/telemetry_master_scripts/appengine_poller.py
@@ -25,38 +25,42 @@ ENCOUNTERED_KEYS = {}
 class Poller(object):
 
   def Poll(self):
-
     while True:
+      # TODO(rmistry): Put urls to open into a tuple.
+      try:
+        get_tasks_page = urllib.urlopen(
+            appengine_constants.SKIA_TELEMETRY_WEBAPP +
+            appengine_constants.GET_LUA_TASKS_SUBPATH)
+        pending_tasks = json.loads(
+            get_tasks_page.read().replace('\r\n', '\\r\\n'))
 
-      # Get all tasks in the queue
-      # TODO(rmistry): Make urls to open a tuple??
-      get_tasks_page = urllib.urlopen(
-          appengine_constants.SKIA_TELEMETRY_WEBAPP +
-          appengine_constants.GET_LUA_TASKS_SUBPATH)
-      pending_tasks = json.loads(
-          get_tasks_page.read().replace('\r\n', '\\r\\n'))
-      for key in sorted(pending_tasks.keys()):
-        task = pending_tasks[key]
-        task_key = task['key']
-        if task_key in ENCOUNTERED_KEYS:
-          '%s is already being processed' % task_key
-          continue
-        ENCOUNTERED_KEYS[task_key] = 1
-        # Create a run id.
-        run_id = '%s-%s' % (task['username'].split('@')[0], time.time())
-        lua_file = os.path.join(tempfile.gettempdir(), '%s.lua' % run_id)
-        f = open(lua_file, 'w')
-        f.write(task['lua_script'])
-        f.close()
-        # Now call the script!!!
-        log_file = os.path.join(tempfile.gettempdir(), '%s.output' % run_id)
-        cmd = 'bash vm_run_lua_on_slaves.sh %s %s %s %s' % (
-            lua_file, run_id, task['username'], task_key)
-        print 'Output will be available in %s' % log_file
-        subprocess.Popen(cmd.split(), stdout=open(log_file, 'w'), stderr=open(log_file, 'w'))
+        for key in sorted(pending_tasks.keys()):
+          task = pending_tasks[key]
+          task_key = task['key']
+          if task_key in ENCOUNTERED_KEYS:
+            '%s is already being processed' % task_key
+            continue
+          ENCOUNTERED_KEYS[task_key] = 1
+          # Create a run id.
+          run_id = '%s-%s' % (task['username'].split('@')[0], time.time())
+          lua_file = os.path.join(tempfile.gettempdir(), '%s.lua' % run_id)
+          f = open(lua_file, 'w')
+          f.write(task['lua_script'])
+          f.close()
+          # Now call the script!!!
+          log_file = os.path.join(tempfile.gettempdir(), '%s.output' % run_id)
+          cmd = 'bash vm_run_lua_on_slaves.sh %s %s %s %s' % (
+              lua_file, run_id, task['username'], task_key)
+          print 'Output will be available in %s' % log_file
+          subprocess.Popen(cmd.split(), stdout=open(log_file, 'w'),
+                           stderr=open(log_file, 'w'))
 
-      print 'Sleeping %s secs' % SLEEP_BETWEEN_POLLS_SECS
-      time.sleep(SLEEP_BETWEEN_POLLS_SECS)
+        print 'Sleeping %s secs' % SLEEP_BETWEEN_POLLS_SECS
+        time.sleep(SLEEP_BETWEEN_POLLS_SECS)
+      except Exception:
+        # The poller should never crash, output the exception and move on.
+        print traceback.format_exc()
+        continue
 
 
 if '__main__' == __name__:
