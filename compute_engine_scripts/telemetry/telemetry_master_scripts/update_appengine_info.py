f5d41020dcc4cd9dbac1cbe9a9c6bd174a530960
rmistry@google.com <rmistry@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2013-06-24 16:00:18 +0000

    Find number of archived webpages from Google Storage and update the WebApp.
    (SkipBuildbotRuns)
    
    Review URL: https://codereview.chromium.org/17582012
    
    git-svn-id: http://skia.googlecode.com/svn/buildbot@9741 2bbb7eff-a529-9590-31e7-b0007b416f81

--- compute_engine_scripts/telemetry/telemetry_master_scripts/update_appengine_info.py
+++ compute_engine_scripts/telemetry/telemetry_master_scripts/update_appengine_info.py
@@ -22,16 +22,31 @@ class UpdateInfo(object):
   def Update(self):
     # Find out when chrome was last built.
     chrome_last_built = int(os.path.getmtime(CHROME_BINARY_LOCATION))
+    print 'chrome_last_built: %s' % chrome_last_built
+
+    # Find number of GCE slaves and number of webpages per pageset.
     gce_slaves_cmd = [
         'bash',
         '-c',
-        'source ../vm_config.sh && echo $NUM_SLAVES,$NUM_WEBPAGES']
-
-    # Find number of GCE slaves and number of Webpages.
-    gce_slaves, num_webpages = subprocess.Popen(
+        'source ../vm_config.sh && echo $NUM_SLAVES,$MAX_WEBPAGES_PER_PAGESET']
+    gce_slaves, num_webpages_per_pageset = subprocess.Popen(
             gce_slaves_cmd,
             stdout=subprocess.PIPE,
             stderr=subprocess.STDOUT).communicate()[0].rstrip().split(',')
+    print 'gce_slaves: %s' % gce_slaves
+    print 'num_webpages_per_pageset: %s' % num_webpages_per_pageset
+
+    # Find the total number of archived webpages currently in Google Storage.
+    num_archives_cmd = [
+        'bash',
+        '-c',
+        'gsutil ls -l gs://chromium-skia-gm/telemetry/webpages_archive/*/*.json | wc -l']
+    num_archives = subprocess.Popen(
+        num_archives_cmd,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.STDOUT).communicate()[0].rstrip()
+    num_webpages = int(num_webpages_per_pageset) * int(num_archives)
+    print 'num_webpages: %s' % num_webpages
 
     # Find the total number of SKP files currently in Google Storage.
     num_skp_files_cmd = [
@@ -42,6 +57,7 @@ class UpdateInfo(object):
         num_skp_files_cmd,
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT).communicate()[0].rstrip()
+    print 'num_skp_files: %s' % num_skp_files
 
     # Get Chromium and Skia revisions.
     old_cwd = os.getcwd()
@@ -57,6 +73,8 @@ class UpdateInfo(object):
     skia_rev = match_obj.group(1)
     chromium_rev = match_obj.group(2)
     os.chdir(old_cwd)
+    print 'skia_rev: %s' % skia_rev
+    print 'chromium_rev: %s' % chromium_rev
 
     # Now communicate with the skia-telemetry webapp.
     update_info_url = '%s%s' % (
@@ -65,9 +83,10 @@ class UpdateInfo(object):
     password = open('appengine_password.txt').read().rstrip()
     update_info_post_data = (
         'chrome_last_built=%s&gce_slaves=%s&num_webpages=%s&'
-        'num_skp_files=%s&chromium_rev=%s&skia_rev=%s&password=%s' % (
+        'num_skp_files=%s&chromium_rev=%s&skia_rev=%s&password=%s&'
+        'num_webpages_per_pageset=%s' % (
             chrome_last_built, gce_slaves, num_webpages, num_skp_files,
-            chromium_rev, skia_rev, password))
+            chromium_rev, skia_rev, password, num_webpages_per_pageset))
     os.system('wget --post-data "%s" "%s" -O /dev/null' % (
         update_info_post_data, update_info_url))
 
