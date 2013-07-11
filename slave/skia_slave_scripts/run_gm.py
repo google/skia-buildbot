1757156e772f4bbbb019a0254f25794cc2318d93
epoger@google.com <epoger@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2013-07-11 17:20:23 +0000

    buildbots: upload ALL gm results, not just new ones BUG=https://code.google.com/p/skia/issues/detail?id=1410
    Review URL: https://codereview.chromium.org/18620007
    
    git-svn-id: http://skia.googlecode.com/svn/buildbot@10015 2bbb7eff-a529-9590-31e7-b0007b416f81

--- slave/skia_slave_scripts/run_gm.py
+++ slave/skia_slave_scripts/run_gm.py
@@ -22,10 +22,7 @@ class RunGM(BuildStep):
                               self._gm_image_subdir)
     cmd = ['--verbose',
            '--writeChecksumBasedFilenames',
-           # Don't bother writing out image files that match our expectations--
-           # we know that previous runs have already uploaded those!
-           '--mismatchPath', output_dir,
-           '--missingExpectationsPath', output_dir,
+           '--writePath', output_dir,
            '--writeJsonSummaryPath', os.path.join(output_dir,
                                                   JSON_SUMMARY_FILENAME),
            '--ignoreErrorTypes',
