d504902a61e732cc2180aaf8a89c67fecf3edadf
rmistry@google.com <rmistry@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2013-07-16 12:59:26 +0000

    Add vm_run_command_with_output_on_slaves.sh
    (SkipBuildbotRuns)
    
    Review URL: https://codereview.chromium.org/19356004
    
    git-svn-id: http://skia.googlecode.com/svn/buildbot@10100 2bbb7eff-a529-9590-31e7-b0007b416f81

--- compute_engine_scripts/telemetry/telemetry_master_scripts/vm_run_command_with_output_on_slaves.sh
+++ compute_engine_scripts/telemetry/telemetry_master_scripts/vm_run_command_with_output_on_slaves.sh
@@ -27,8 +27,13 @@ gclient sync
 
 echo "About to run $CMD on all slaves..."
 for SLAVE_NUM in $(seq 1 $NUM_SLAVES); do
-  ssh -f -X -o UserKnownHostsFile=/dev/null -o CheckHostIP=no \
+  cmd_output=`ssh -o UserKnownHostsFile=/dev/null -o CheckHostIP=no \
     -o StrictHostKeyChecking=no -i /home/default/.ssh/google_compute_engine \
-    -A -q -p 22 default@108.170.222.$SLAVE_NUM -- "$CMD"
+    -A -q -p 22 default@108.170.222.$SLAVE_NUM -- "$CMD"`
+  if [ "$cmd_output" ]; then
+    echo "===== skia-telemetry-worker$SLAVE_NUM output: ====="
+    echo $cmd_output
+    echo "============================================"
+  fi
 done
 
