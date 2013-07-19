74a7819f4109f9c1cab9c51ee076dac5680e7a19
rmistry@google.com <rmistry@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2013-07-19 18:51:06 +0000

    Master and slave side scripts to generate diff results for pdf viewer on the GCE cluster.
    (SkipBuildbotRuns)
    
    Review URL: https://codereview.chromium.org/19565003
    
    git-svn-id: http://skia.googlecode.com/svn/buildbot@10199 2bbb7eff-a529-9590-31e7-b0007b416f81

---
+++ compute_engine_scripts/telemetry/telemetry_master_scripts/vm_run_pdf_viewer_on_slaves.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+#
+# Starts the telemetry_slave_scripts/vm_run_pdf_viewer.sh script on all
+# slaves.
+#
+# The script should be run from the skia-telemetry-master GCE instance's
+# /home/default/skia-repo/buildbot/compute_engine_scripts/telemetry/telemetry_master_scripts
+# directory.
+#
+# Copyright 2013 Google Inc. All Rights Reserved.
+# Author: rmistry@google.com (Ravi Mistry)
+
+if [ $# -ne 2 ]; then
+  echo
+  echo "Usage: `basename $0` rmistry@google.com rmistry-2013-05-24.07-34-05"
+  echo
+  echo "The first argument is the email address of the requester."
+  echo "The second argument is a unique runid (typically requester + timestamp)."
+  echo
+  exit 1
+fi
+
+REQUESTER_EMAIL=$1
+RUN_ID=$2
+
+source ../vm_config.sh
+source vm_utils.sh
+
+# Update buildbot.
+gclient sync
+
+# Run vm_run_pdf_viewer.sh on all the slaves.
+for SLAVE_NUM in $(seq 1 $NUM_SLAVES); do
+  CMD="bash vm_run_pdf_viewer.sh $SLAVE_NUM $RUN_ID"
+  ssh -f -X -o UserKnownHostsFile=/dev/null -o CheckHostIP=no \
+    -o StrictHostKeyChecking=no -i /home/default/.ssh/google_compute_engine \
+    -A -p 22 default@108.170.222.$SLAVE_NUM -- "source .bashrc; cd skia-repo/buildbot/compute_engine_scripts/telemetry/telemetry_slave_scripts; svn update; $CMD > /tmp/pdfviewer-${RUN_ID}_output.txt 2>&1"
+done
+
+# Sleep for a minute to give the slaves some time to start processing.
+sleep 60
+
+# Check to see if the slaves are done with this pdfviewer request.
+SLAVES_STILL_PROCESSING=true
+while $SLAVES_STILL_PROCESSING ; do
+  SLAVES_STILL_PROCESSING=false
+  for SLAVE_NUM in $(seq 1 $NUM_SLAVES); do
+    RET=$( is_slave_currently_executing $SLAVE_NUM PDF_VIEWER.${RUN_ID} )
+    if $RET; then
+      echo "skia-telemetry-worker$SLAVE_NUM is still running PDF_VIEWER.${RUN_ID}"
+      echo "Sleeping for a minute and then retrying"
+      SLAVES_STILL_PROCESSING=true
+      sleep 60
+      break
+    else
+      echo "skia-telemetry-worker$SLAVE_NUM is done processing."
+    fi
+  done
+done
+
+# Copy over the outputs from all slaves and consolidate them into one file with
+# special handling for CSV files.
+LOGS_DIR=/home/default/storage/pdf_logs
+mkdir $LOGS_DIR
+mkdir $LOGS_DIR/$RUN_ID
+
+for SLAVE_NUM in $(seq 1 $NUM_SLAVES); do
+  gsutil cp gs://chromium-skia-gm/telemetry/pdfviewer/slave$SLAVE_NUM/outputs/${RUN_ID}.output \
+    $LOGS_DIR/$RUN_ID/$SLAVE_NUM.csv
+done
+
+python ../csv_merger.py --csv_dir=$LOGS_DIR/$RUN_ID --output_csv_name=result.csv
+
+# Copy the consolidated file into Google Storage.
+gsutil cp -a public-read $LOGS_DIR/$RUN_ID/result.csv \
+  gs://chromium-skia-gm/telemetry/pdfviewer/consolidated-outputs/$RUN_ID.output.txt
+OUTPUT_LINK=https://storage.cloud.google.com/chromium-skia-gm/telemetry/pdfviewer/consolidated-outputs/$RUN_ID.output.txt
+
+# Delete all tmp files.
+rm -rf $LOGS_DIR/*$RUN_ID*
+rm -rf /tmp/*$RUN_ID*
+
+# Email the requester.
+BOUNDARY=`date +%s|md5sum`
+BOUNDARY=${BOUNDARY:0:32}
+sendmail $REQUESTER_EMAIL <<EOF
+subject:Your PDF Viewer task has completed!
+to:$REQUESTER_EMAIL
+from:skia.buildbot@gmail.com
+Content-Type: multipart/mixed; boundary=\"$BOUNDARY\";
+
+This is a MIME-encapsulated message
+
+--$BOUNDARY
+Content-Type: text/html
+
+<html>
+  <head/>
+  <body>
+  The output of your script is available <a href='$OUTPUT_LINK'>here</a>.<br/>
+  Thanks!
+  </body>
+</html>
+
+--$BOUNDARY--
+
+EOF
+
---
+++ compute_engine_scripts/telemetry/telemetry_slave_scripts/vm_run_pdf_viewer.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+#
+# Generates diff results for pdf viewer into a CSV file and copies it to Google
+# Storage.
+#
+# The script should be run from the skia-telemetry-slave GCE instance's
+# /home/default/skia-repo/buildbot/compute_engine_scripts/telemetry/telemetry_slave_scripts
+# directory.
+#
+# Copyright 2013 Google Inc. All Rights Reserved.
+# Author: rmistry@google.com (Ravi Mistry)
+
+
+if [ $# -ne 2 ]; then
+  echo
+  echo "Usage: `basename $0` 1 rmistry2013-05-24.07-34-05"
+  echo
+  echo "The first argument is the slave_num of this telemetry slave."
+  echo "The second argument is the runid (typically requester + timestamp)."
+  echo
+  exit 1
+fi
+
+SLAVE_NUM=$1
+RUN_ID=$2
+
+source vm_utils.sh
+
+WORKER_FILE=PDF_VIEWER.$RUN_ID
+create_worker_file $WORKER_FILE
+
+# Sync trunk.
+cd /home/default/skia-repo/trunk
+/home/default/depot_tools/gclient sync
+
+# Build tools, pdfviewer and skpdiff.
+make tools BUILDTYPE=Release
+./gyp_skia gyp/pdfviewer.gyp
+make pdfviewer BUILDTYPE=Release
+./gyp_skia experimental/skpdiff/skpdiff.gyp
+make skpdiff BUILDTYPE=Release
+
+if [ -e /etc/boto.cfg ]; then
+  # Move boto.cfg since it may interfere with the ~/.boto file.
+  sudo mv /etc/boto.cfg /etc/boto.cfg.bak
+fi
+
+# Download the SKP files from Google Storage if the local TIMESTAMP is out of date.
+mkdir -p /home/default/storage/skps/
+are_timestamps_equal /home/default/storage/skps gs://chromium-skia-gm/telemetry/skps/slave$SLAVE_NUM
+if [ $? -eq 1 ]; then
+  gsutil cp gs://chromium-skia-gm/telemetry/skps/slave$SLAVE_NUM/* /home/default/storage/skps/
+fi
+
+# Create directories for outputs of this script.
+LOGS_DIR=/home/default/storage/pdf_logs/$RUN_ID
+mkdir -p $LOGS_DIR/expected
+mkdir -p $LOGS_DIR/pdf
+mkdir -p $LOGS_DIR/actual
+mkdir -p $LOGS_DIR/csv
+
+# Run render_pictures, render_pdfs, pdfviewer and skpdiff.
+out/Release/render_pictures -r /home/default/storage/skps/ -w $LOGS_DIR/expected/
+out/Release/render_pdfs /home/default/storage/skps/ -w $LOGS_DIR/pdf/
+out/Release/pdfviewer -r $LOGS_DIR/pdf/ -w $LOGS_DIR/actual/ -n
+out/Release/skpdiff -f $LOGS_DIR/expected/ $LOGS_DIR/actual/ --csv $LOGS_DIR/csv/result.csv
+
+# Copy the csv output to Google Storage.
+gsutil cp $LOGS_DIR/csv/result.csv gs://chromium-skia-gm/telemetry/pdfviewer/slave$SLAVE_NUM/outputs/${RUN_ID}.output
+
+# Clean up logs and the worker file.
+rm -rf /home/default/storage/pdf_logs/*${RUN_ID}*
+rm -rf /tmp/*${RUN_ID}*
+delete_worker_file $WORKER_FILE
