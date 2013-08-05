451d02bfa611f230be81faf06d48a4f0ef2379ec
edisonn@google.com <edisonn@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2013-08-05 17:43:19 +0000

    call chop_transparency before calling skpdiff for pdfviewer and render_pictures generated files. The reason is that some skps contains large areas of trasnparent content that need to be removed in order for skpdiff to generate a valid score for pdfviewer. (SkipBuildbotRuns)
    R=rmistry@google.com
    
    Review URL: https://codereview.chromium.org/22223003
    
    git-svn-id: http://skia.googlecode.com/svn/buildbot@10539 2bbb7eff-a529-9590-31e7-b0007b416f81

--- compute_engine_scripts/telemetry/telemetry_slave_scripts/vm_pdf_viewer_run_one_skp.sh
+++ compute_engine_scripts/telemetry/telemetry_slave_scripts/vm_pdf_viewer_run_one_skp.sh
@@ -7,4 +7,6 @@ SKP=`basename "$2" .skp`
 out/Release/render_pictures -r $FULL_PATH_SKP -w $LOGS_DIR/expected/ >> $LOGS_DIR/logs/$SKP.txt
 out/Release/render_pdfs $FULL_PATH_SKP -w $LOGS_DIR/pdf/ >> $LOGS_DIR/logs/$SKP.txt
 out/Release/pdfviewer -r $LOGS_DIR/pdf/$SKP.pdf -w $LOGS_DIR/actual/ -n >> $LOGS_DIR/logs/$SKP.txt
+out/Release/chop_transparency -r $LOGS_DIR/expected/$SKP.png
+out/Release/chop_transparency -r $LOGS_DIR/actual/$SKP.png
 out/Release/skpdiff -p $LOGS_DIR/expected/$SKP.png $LOGS_DIR/actual/$SKP.png --csv $LOGS_DIR/csv/$SKP.csv >> $LOGS_DIR/logs/$SKP.txt
