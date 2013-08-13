7e495caa4914fa88969d8873050fce1aeb4fff94
borenet@google.com <borenet@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2013-08-13 19:56:12 +0000

    Add -j1 to windows launch script
    (SkipBuildbotRuns)
    
    Review URL: https://codereview.chromium.org/23054003
    
    git-svn-id: http://skia.googlecode.com/svn/buildbot@10698 2bbb7eff-a529-9590-31e7-b0007b416f81

--- scripts/skiabot-slave-start-on-boot.bat
+++ scripts/skiabot-slave-start-on-boot.bat
@@ -1,5 +1,5 @@
 cd C:\Users\chrome-bot
 call gclient config https://skia.googlesource.com/buildbot.git
-call gclient sync --force
+call gclient sync --force -j1
 cd buildbot
 python scripts\launch_slaves.py
