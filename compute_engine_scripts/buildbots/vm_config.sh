5b82e75fe84aaa806cfe0964974d44d864d1126f
rmistry@google.com <rmistry@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2013-08-15 14:33:57 +0000

    The config used by all GCE buildbot scripts and a README detailing usage.
    This CL is one of 5 related GCE automation CLs- 
    https://codereview.chromium.org/22923007/ : Setup scripts for the GCE buildbot image and scripts for the master and the slaves. 
    https://codereview.chromium.org/23011008/ : Scripts to delete currently running GCE buildbot instances. 
    https://codereview.chromium.org/22971005/ : Scripts to create the GCE buildbot instances. 
    https://codereview.chromium.org/23007013/ : Script to copy the buildbot history from the old zone master to the new zone master. 
    https://codereview.chromium.org/23057003/ : The config used by all GCE buildbot scripts and a README detailing usage. 
    
    (SkipBuildbotRuns)
    
    R=borenet@google.com
    
    Review URL: https://codereview.chromium.org/23057003
    
    git-svn-id: http://skia.googlecode.com/svn/buildbot@10736 2bbb7eff-a529-9590-31e7-b0007b416f81

--- compute_engine_scripts/buildbots/vm_config.sh
+++ compute_engine_scripts/buildbots/vm_config.sh
@@ -1,15 +1,37 @@
+#!/bin/bash
+#
 # This file contains constants for the shell scripts which interact
 # with the Google Compute Engine instances.
+#
+# Copyright 2012 Google Inc. All Rights Reserved.
+# Author: rmistry@google.com (Ravi Mistry)
 
-GCUTIL=`which gcutil`
+# The base names of the VM instances. Actual names are VM_NAME_BASE-name-zone
+VM_NAME_BASE=${VM_NAME_BASE:="skia"}
 
-# The base names of the VM instances. Actual names are VM_NAME_BASE+name+zone
-VM_NAME_BASE=${VM_NAME_BASE:='skia'}
+# Master name, type and the IP address to use for each zone.
+VM_MASTER_NAME=${VM_MASTER_NAME:="master"}
+MASTER_MACHINE_TYPE="n1-standard-8-d"
+MASTER_IP_ADDRESS_a="173.255.115.253"
+MASTER_IP_ADDRESS_b="108.170.217.252"
 
-VM_MASTER_NAMES=${VM_MASTER_NAMES:="master"}
-VM_SLAVE_NAMES=${VM_SLAVE_NAMES:="housekeeping-slave compile1 compile2 compile3 compile4 compile5"}
-VM_NAMES="${VM_MASTER_NAMES} ${VM_SLAVE_NAMES}"
+# Slave names, type and their IP addresses.
+VM_SLAVE_NAMES=${VM_SLAVE_NAMES:="housekeeping-slave compile1 compile2 compile3 compile4 compile5 skia-android-canary"}
+SLAVES_MACHINE_TYPE="n1-standard-2-d"
+# The list of slave names must correspond to the list of their IP addresses.
+# If you need to add more slaves find available IP addresses by running:
+#   gcutil --project=google.com:skia-buildbots listaddresses
+SLAVE_IP_ADDRESSES="108.170.217.253 108.170.217.250 108.170.217.251 108.170.217.247 108.170.217.248 108.170.217.246 108.170.217.255"
 
+# The Skia buildbot GCE image name.
+SKIA_BUILDBOT_IMAGE_NAME="skia-buildbot-image"
+
+# The scope to use for image creation.
+SCOPES="https://www.googleapis.com/auth/devstorage.full_control"
+
+# Define required files for master and slaves.
+REQUIRED_FILES_FOR_MASTER=(~/.code_review_password \
+                           ~/.status_password)
 
 REQUIRED_FILES_FOR_SLAVES=(~/.autogen_svn_username \
                            ~/.autogen_svn_password \
@@ -17,27 +39,26 @@ REQUIRED_FILES_FOR_SLAVES=(~/.autogen_svn_username \
                            ~/.skia_svn_password \
                            ~/.boto)
 
-REQUIRED_FILES_FOR_MASTERS=(~/.code_review_password \
-                            ~/.status_password)
+# The directory where the scratch disk is mounted.
+SKIA_REPO_DIR="skia-repo"
 
 # The (Shared Fate) Zone is conceptually equivalent to a data center cell. VM
 # instances live in a zone.
 #
 # We flip the default one as required by PCRs in bigcluster. We are allowed
-# us-east1-a, us-east1-b and us-east1-c.
+# us-central1-a and us-central1-b.
 # A short tag to use as part of the VM instance name
 ZONE_TAG=${ZONE_TAG:=b}
 ZONE=us-central1-$ZONE_TAG
 
-# The size of the persistent disk (in GB) for all created VMs.
-DISK_SIZE=20
-
 # The Project ID is found in the Compute tab of the dev console.
 # https://code.google.com/apis/console/?pli=1#project:31977622648:overview
-PROJECT_ID='google.com:skia-buildbots'
+PROJECT_ID="google.com:skia-buildbots"
 
 # The user id which owns the server on the vm instance
 PROJECT_USER="default"
 
+# gcutil commands.
+GCUTIL=`which gcutil`
 GCOMPUTE_CMD="$GCUTIL --cluster=prod --project=$PROJECT_ID"
 GCOMPUTE_SSH_CMD="$GCOMPUTE_CMD ssh --ssh_user=$PROJECT_USER"
