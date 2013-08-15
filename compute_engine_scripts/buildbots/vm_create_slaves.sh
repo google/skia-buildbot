1a0f78d81d1a2e20fa3b5e4dca0c8fe8625007f2
rmistry@google.com <rmistry@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2013-08-15 14:33:12 +0000

    Scripts to create the GCE buildbot instances.
    This CL is one of 5 related GCE automation CLs- 
    https://codereview.chromium.org/22923007/ : Setup scripts for the GCE buildbot image and scripts for the master and the slaves. 
    https://codereview.chromium.org/23011008/ : Scripts to delete currently running GCE buildbot instances. 
    https://codereview.chromium.org/22971005/ : Scripts to create the GCE buildbot instances. 
    https://codereview.chromium.org/23007013/ : Script to copy the buildbot history from the old zone master to the new zone master. 
    https://codereview.chromium.org/23057003/ : The config used by all GCE buildbot scripts and a README detailing usage. 
    
    (SkipBuildbotRuns)
    
    R=borenet@google.com
    
    Review URL: https://codereview.chromium.org/22971005
    
    git-svn-id: http://skia.googlecode.com/svn/buildbot@10734 2bbb7eff-a529-9590-31e7-b0007b416f81

---
+++ compute_engine_scripts/buildbots/vm_create_slaves.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#
+# Create all the Skia buildbot slave VM instances.
+#
+# Copyright 2013 Google Inc. All Rights Reserved.
+# Author: rmistry@google.com (Ravi Mistry)
+
+source vm_config.sh
+
+COUNTER=0
+IP_ADDRESSES_ARR=($SLAVE_IP_ADDRESSES)
+for VM in $VM_SLAVE_NAMES; do
+  IP_ADDRESS=${IP_ADDRESSES_ARR[$COUNTER]}
+  COUNTER=$[$COUNTER +1]
+
+  $GCOMPUTE_CMD addinstance ${VM_NAME_BASE}-${VM}-${ZONE_TAG} \
+    --zone=$ZONE \
+    --external_ip_address=$IP_ADDRESS \
+    --service_account=default \
+    --service_account_scopes="$SCOPES" \
+    --network=default \
+    --machine_type=$SLAVES_MACHINE_TYPE \
+    --image=$SKIA_BUILDBOT_IMAGE_NAME \
+    --nopersistent_boot_disk
+done
+
+cat <<INP
+If you did not see tables print out above then the vm names may be running
+already. You will have to delete them to recreate them with different atttributes
+or move them to a different zone.
+
+Check ./vm_status.sh to wait until the status is RUNNING.
+
+When the vm is ready, run vm_setup_slaves.sh to setup all created slaves.
+INP
+exit 0
