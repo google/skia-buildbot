219c7269d61df7c1d7d0904a847503330cc8a87e
borenet@google.com <borenet@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2013-08-26 13:33:24 +0000

    Don't build debugger on 32-bit Linux
    R=rmistry@google.com
    
    Review URL: https://codereview.chromium.org/23020034
    
    git-svn-id: http://skia.googlecode.com/svn/buildbot@10914 2bbb7eff-a529-9590-31e7-b0007b416f81

--- master/skia_master_scripts/factory.py
+++ master/skia_master_scripts/factory.py
@@ -689,8 +689,10 @@ class SkiaFactory(BuildFactory):
       if not self._build_targets:
         self._build_targets = ['skia_lib', 'tests', 'gm', 'tools', 'bench']
         if (('Win7' in self._builder_name and 'x86_64' in self._builder_name) or
+            ('Ubuntu' in self._builder_name and 'x86-' in self._builder_name) or
             'Mac10.6' in self._builder_name):
-          # Don't compile the debugger in 64-bit Win7 or Mac 10.6.
+          # Don't compile the debugger in 64-bit Win7, Mac 10.6, or 32-bit Linux
+          # since the Qt SDK doesn't include libraries for those platforms.
           self._build_targets.append('most')
         else:
           self._build_targets.append('everything')
