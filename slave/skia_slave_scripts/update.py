40ed0770f52c670967ade6aab0cff17e21448a31
borenet@google.com <borenet@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2013-09-11 18:38:27 +0000

    Stop deleting checkouts on Update failure, increase Update timeout
    R=epoger@google.com
    
    Review URL: https://codereview.chromium.org/23477063
    
    git-svn-id: http://skia.googlecode.com/svn/buildbot@11205 2bbb7eff-a529-9590-31e7-b0007b416f81

--- slave/skia_slave_scripts/update.py
+++ slave/skia_slave_scripts/update.py
@@ -6,16 +6,14 @@
 """ Check out the Skia sources. """
 
 
-from utils import file_utils
 from utils import gclient_utils
 from build_step import BuildStep, BuildStepFailure
 import ast
-import os
 import sys
 
 
 class Update(BuildStep):
-  def __init__(self, timeout=6000, no_output_timeout=4800, attempts=5,
+  def __init__(self, timeout=10000, no_output_timeout=6000, attempts=5,
                **kwargs):
     super(Update, self).__init__(timeout=timeout,
                                  no_output_timeout=no_output_timeout,
@@ -46,32 +44,16 @@ class Update(BuildStep):
     gclient_utils.Config(spec=gclient_spec)
 
     # Run "gclient sync"
-    try:
-      if self._is_try:
-        # Clean our checkout to make sure we don't have a patch left over.
-        gclient_utils.Revert()
-      gclient_utils.Sync(
-          branches=[solution['name'] for solution in solution_dicts],
-          revision=self._revision,
-          verbose=True,
-          force=True,
-          delete_unversioned_trees=True)
-      got_revision = gclient_utils.GetCheckedOutHash()
-    except Exception:
-      # If the sync fails, clear the checkout and try again.
-      build_dir = os.path.abspath(os.curdir)
-      os.chdir(os.pardir)
-      file_utils.ClearDirectory(build_dir)
-      os.chdir(build_dir)
-      gclient_utils.Config(spec=gclient_spec)
-      gclient_utils.Sync(
-          branches=[solution['name'] for solution in solution_dicts],
-          revision=self._revision,
-          verbose=True,
-          force=True,
-          delete_unversioned_trees=True,
-          jobs=1)
-      got_revision = gclient_utils.GetCheckedOutHash()
+    if self._is_try:
+      # Clean our checkout to make sure we don't have a patch left over.
+      gclient_utils.Revert()
+    gclient_utils.Sync(
+        branches=[solution['name'] for solution in solution_dicts],
+        revision=self._revision,
+        verbose=True,
+        force=True,
+        delete_unversioned_trees=True)
+    got_revision = gclient_utils.GetCheckedOutHash()
 
     # If the revision we actually got differs from what was requested, raise an
     # exception.
