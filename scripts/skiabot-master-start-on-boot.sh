74527a218d2a8896e6fa3d3e14ecfe299bde2905
borenet@google.com <borenet@google.com@2bbb7eff-a529-9590-31e7-b0007b416f81>
2013-09-26 16:29:48 +0000

    Fix startup scripts
    PATH isn't set correctly when these are called, so running "gclient" fails.
    
    R=rmistry@google.com
    
    Review URL: https://codereview.chromium.org/24761002
    
    git-svn-id: http://skia.googlecode.com/svn/buildbot@11476 2bbb7eff-a529-9590-31e7-b0007b416f81

--- scripts/skiabot-master-start-on-boot.sh
+++ scripts/skiabot-master-start-on-boot.sh
@@ -1,11 +1,18 @@
+{
+while [ -z "$(df -v | grep sdb)" ]; do
+  echo "/dev/sdb not yet attached..."
+  sleep 1
+done
 if [ -z "$1" ]; then
   SKIA_REPO_DIR="skia-master"
 else
   SKIA_REPO_DIR="$1"
 fi
+export PATH="$PATH:/home/default/$SKIA_REPO_DIR/depot_tools"
 mkdir -p ~/$SKIA_REPO_DIR
 cd ~/$SKIA_REPO_DIR
 gclient config https://skia.googlesource.com/buildbot.git
 gclient sync --force
 cd buildbot
 python scripts/launch_master.py --loop
+} > ~/reboot.log 2>&1
--- scripts/skiabot-slave-start-on-boot.sh
+++ scripts/skiabot-slave-start-on-boot.sh
@@ -1,5 +1,8 @@
+{
 cd ~
+export PATH="$PATH:$(pwd)/depot_tools"
 gclient config https://skia.googlesource.com/buildbot.git
 gclient sync --force
 cd buildbot
 python scripts/launch_slaves.py
+} > ~/reboot.log 2>&1
