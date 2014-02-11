adcef10d0a0715c28d9695a7124e70946ec6565c
rmistry <rmistry@google.com>
2014-02-11 14:42:21 +0000

    Revert of Use the Skia checkout from third_party/skia and use the Patching step for trybot runs (https://codereview.chromium.org/158843003/)
    Reason for revert:
    Do not use the Skia checkout from third_party/skia
    
    Original issue's description:
    > Use the Skia checkout from third_party/skia and use the Patching step for trybot runs.
    >
    > BUG=skia:2155
    >
    > Committed: https://skia.googlesource.com/buildbot/+/5b04d06fe9b468f4f962ca914c7d52a97529c954
    
    R=borenet@google.com
    TBR=borenet@google.com
    NOTREECHECKS=true
    NOTRY=true
    BUG=skia:2155
    
    Author: rmistry@google.com
    
    Review URL: https://codereview.chromium.org/142543006

--- master/skia_master_scripts/recreate_skps_factory.py
+++ master/skia_master_scripts/recreate_skps_factory.py
@@ -5,6 +5,8 @@
 """BuildFactory for a builder which recaptures the buildbot SKP repo."""
 
 
+from config_private import SKIA_GIT_URL
+from master.factory import gclient_factory
 from skia_master_scripts import canary_factory
 
 import builder_name_schema
@@ -36,26 +38,40 @@ class RecreateSKPsFactory(canary_factory.CanaryFactory):
                       builder_name_schema.BUILDER_ROLE_HOUSEKEEPER)
 
     # Build Chromium LKGR + Skia ToT.
-    self.UpdateSteps()
+    self.UpdateScripts()
+    self.Update()
     self.Compile(retry_without_werr_on_failure=True)
 
     # Checkout and build Debug Skia tools.
+    skia_checkout = self.TargetPath.join(self._workdir, '..', '..', 'skia')
     # Pretend that the flavor is 'default' because we want to build
     # the Skia checkout and not the Chromium canary checkout.
     flavor_args = ['--flavor', 'default']
-    # This is the path to the slave scripts directory from self._workdir
     skia_slave_scripts_path = self.TargetPath.join(
-        '..', '..', '..', '..', '..', '..', 'slave',
+        '..', '..', '..', '..', '..', 'slave',
         'skia_slave_scripts')
+    gclient_solutions = [gclient_factory.GClientSolution(
+        svn_url=SKIA_GIT_URL, name='skia').GetSpec()]
+    self.AddSlaveScript(
+        script=self.TargetPath.join(skia_slave_scripts_path, 'update.py'),
+        description='SkiaUpdate',
+        args=['--gclient_solutions',
+              '"%s"' % gclient_solutions] + flavor_args,
+        timeout=None,
+        halt_on_failure=True,
+        get_props_from_stdout={'got_revision':'Skia updated to (\w+)'},
+        workdir=skia_checkout)
+    skia_slave_scripts_path = self.TargetPath.join(
+        '..', skia_slave_scripts_path)
     self.AddSlaveScript(
         script=self.TargetPath.join(
-            '..', '..', skia_slave_scripts_path, 'compile.py'),
+            skia_slave_scripts_path, 'compile.py'),
         description='SkiaBuildTools',
         args=['--configuration', 'Debug', '--target', 'tools', '--gyp_defines',
               'skia_arch_width=64'] + flavor_args,
         timeout=None,
         halt_on_failure=True,
-        workdir=self.TargetPath.join(self._workdir, self._path_to_skia))
+        workdir=self.TargetPath.join(skia_checkout, 'skia'))
 
     # Invoke the do_skps_capture buildstep.
     self.AddSlaveScript(
@@ -64,7 +80,7 @@ class RecreateSKPsFactory(canary_factory.CanaryFactory):
         description='RecreateSKPs',
         args=['--page_sets', 'all',
               '--skia_tools', self.TargetPath.join(
-                  self._path_to_skia, 'out', 'Debug'),
+                  '..', '..', 'skia', 'skia', 'out', 'Debug'),
               '--browser_executable', self.TargetPath.join(
                   'out', 'Debug', 'chrome')],
         timeout=None,
