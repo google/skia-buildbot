589bcef5db2f874d0f6f4095bdc749679ddd1113
borenet <borenet@google.com>
2014-03-06 10:07:42 -0800

    Trigger some Perf bots when RecreateSKPs finishes
    Warning: this hasn't been fully tested...
    
    BUG=skia:2225
    R=rmistry@google.com, epoger@google.com
    
    Author: borenet@google.com
    
    Review URL: https://codereview.chromium.org/184343009

--- master/master_android_builders_cfg.py
+++ master/master_android_builders_cfg.py
@@ -5,7 +5,7 @@
 # Sets up all the builders we want the FYI buildbot master to run.
 
 
-from master_builders_cfg import f_android, LINUX, S_PERCOMMIT
+from master_builders_cfg import f_android, LINUX, S_PERCOMMIT, S_COMMIT_OR_SKP
 
 import master_builders_cfg
 
@@ -23,27 +23,27 @@ def setup_test_and_perf_builders(helper, do_upload_results):
   #    Role,   OS,         Model,        GPU,           Arch,     Config,    Extra Config,   GYP_DEFS,  Factory,   Target, Scheduler,   Extra Args
   #
   builder_specs = [
-      ('Test', 'Android',  'NexusS',     'SGX540',      'Arm7',   'Debug',   None,           None,      f_android, LINUX,  S_PERCOMMIT, {'device': 'nexus_s'}),
-      ('Test', 'Android',  'NexusS',     'SGX540',      'Arm7',   'Release', None,           None,      f_android, LINUX,  S_PERCOMMIT, {'device': 'nexus_s'}),
-      ('Perf', 'Android',  'NexusS',     'SGX540',      'Arm7',   'Release', None,           None,      f_android, LINUX,  S_PERCOMMIT, {'device': 'nexus_s'}),
-      ('Test', 'Android',  'Nexus4',     'Adreno320',   'Arm7',   'Debug',   None,           None,      f_android, LINUX,  S_PERCOMMIT, {'device': 'nexus_4'}),
-      ('Test', 'Android',  'Nexus4',     'Adreno320',   'Arm7',   'Release', None,           None,      f_android, LINUX,  S_PERCOMMIT, {'device': 'nexus_4'}),
-      ('Perf', 'Android',  'Nexus4',     'Adreno320',   'Arm7',   'Release', None,           None,      f_android, LINUX,  S_PERCOMMIT, {'device': 'nexus_4'}),
-      ('Test', 'Android',  'Nexus7',     'Tegra3',      'Arm7',   'Debug',   None,           None,      f_android, LINUX,  S_PERCOMMIT, {'device': 'nexus_7'}),
-      ('Test', 'Android',  'Nexus7',     'Tegra3',      'Arm7',   'Release', None,           None,      f_android, LINUX,  S_PERCOMMIT, {'device': 'nexus_7'}),
-      ('Perf', 'Android',  'Nexus7',     'Tegra3',      'Arm7',   'Release', None,           None,      f_android, LINUX,  S_PERCOMMIT, {'device': 'nexus_7'}),
-      ('Test', 'Android',  'Nexus10',    'MaliT604',    'Arm7',   'Debug',   None,           None,      f_android, LINUX,  S_PERCOMMIT, {'device': 'nexus_10'}),
-      ('Test', 'Android',  'Nexus10',    'MaliT604',    'Arm7',   'Release', None,           None,      f_android, LINUX,  S_PERCOMMIT, {'device': 'nexus_10'}),
-      ('Perf', 'Android',  'Nexus10',    'MaliT604',    'Arm7',   'Release', None,           None,      f_android, LINUX,  S_PERCOMMIT, {'device': 'nexus_10'}),
-      ('Test', 'Android',  'GalaxyNexus','SGX540',      'Arm7',   'Debug',   None,           None,      f_android, LINUX,  S_PERCOMMIT, {'device': 'galaxy_nexus'}),
-      ('Test', 'Android',  'GalaxyNexus','SGX540',      'Arm7',   'Release', None,           None,      f_android, LINUX,  S_PERCOMMIT, {'device': 'galaxy_nexus'}),
-      ('Perf', 'Android',  'GalaxyNexus','SGX540',      'Arm7',   'Release', None,           None,      f_android, LINUX,  S_PERCOMMIT, {'device': 'galaxy_nexus'}),
-      ('Test', 'Android',  'Xoom',       'Tegra2',      'Arm7',   'Debug',   None,           None,      f_android, LINUX,  S_PERCOMMIT, {'device': 'xoom'}),
-      ('Test', 'Android',  'Xoom',       'Tegra2',      'Arm7',   'Release', None,           None,      f_android, LINUX,  S_PERCOMMIT, {'device': 'xoom'}),
-      ('Perf', 'Android',  'Xoom',       'Tegra2',      'Arm7',   'Release', None,           None,      f_android, LINUX,  S_PERCOMMIT, {'device': 'xoom'}),
-      ('Test', 'Android',  'IntelRhb',   'SGX544',      'x86',    'Debug',   None,           None,      f_android, LINUX,  S_PERCOMMIT, {'device': 'intel_rhb'}),
-      ('Test', 'Android',  'IntelRhb',   'SGX544',      'x86',    'Release', None,           None,      f_android, LINUX,  S_PERCOMMIT, {'device': 'intel_rhb'}),
-      ('Perf', 'Android',  'IntelRhb',   'SGX544',      'x86',    'Release', None,           None,      f_android, LINUX,  S_PERCOMMIT, {'device': 'intel_rhb'}),
+      ('Test', 'Android',  'NexusS',     'SGX540',      'Arm7',   'Debug',   None,           None,      f_android, LINUX,  S_PERCOMMIT,     {'device': 'nexus_s'}),
+      ('Test', 'Android',  'NexusS',     'SGX540',      'Arm7',   'Release', None,           None,      f_android, LINUX,  S_PERCOMMIT,     {'device': 'nexus_s'}),
+      ('Perf', 'Android',  'NexusS',     'SGX540',      'Arm7',   'Release', None,           None,      f_android, LINUX,  S_PERCOMMIT,     {'device': 'nexus_s'}),
+      ('Test', 'Android',  'Nexus4',     'Adreno320',   'Arm7',   'Debug',   None,           None,      f_android, LINUX,  S_PERCOMMIT,     {'device': 'nexus_4'}),
+      ('Test', 'Android',  'Nexus4',     'Adreno320',   'Arm7',   'Release', None,           None,      f_android, LINUX,  S_PERCOMMIT,     {'device': 'nexus_4'}),
+      ('Perf', 'Android',  'Nexus4',     'Adreno320',   'Arm7',   'Release', None,           None,      f_android, LINUX,  S_COMMIT_OR_SKP, {'device': 'nexus_4'}),
+      ('Test', 'Android',  'Nexus7',     'Tegra3',      'Arm7',   'Debug',   None,           None,      f_android, LINUX,  S_PERCOMMIT,     {'device': 'nexus_7'}),
+      ('Test', 'Android',  'Nexus7',     'Tegra3',      'Arm7',   'Release', None,           None,      f_android, LINUX,  S_PERCOMMIT,     {'device': 'nexus_7'}),
+      ('Perf', 'Android',  'Nexus7',     'Tegra3',      'Arm7',   'Release', None,           None,      f_android, LINUX,  S_COMMIT_OR_SKP, {'device': 'nexus_7'}),
+      ('Test', 'Android',  'Nexus10',    'MaliT604',    'Arm7',   'Debug',   None,           None,      f_android, LINUX,  S_PERCOMMIT,     {'device': 'nexus_10'}),
+      ('Test', 'Android',  'Nexus10',    'MaliT604',    'Arm7',   'Release', None,           None,      f_android, LINUX,  S_PERCOMMIT,     {'device': 'nexus_10'}),
+      ('Perf', 'Android',  'Nexus10',    'MaliT604',    'Arm7',   'Release', None,           None,      f_android, LINUX,  S_COMMIT_OR_SKP, {'device': 'nexus_10'}),
+      ('Test', 'Android',  'GalaxyNexus','SGX540',      'Arm7',   'Debug',   None,           None,      f_android, LINUX,  S_PERCOMMIT,     {'device': 'galaxy_nexus'}),
+      ('Test', 'Android',  'GalaxyNexus','SGX540',      'Arm7',   'Release', None,           None,      f_android, LINUX,  S_PERCOMMIT,     {'device': 'galaxy_nexus'}),
+      ('Perf', 'Android',  'GalaxyNexus','SGX540',      'Arm7',   'Release', None,           None,      f_android, LINUX,  S_PERCOMMIT,     {'device': 'galaxy_nexus'}),
+      ('Test', 'Android',  'Xoom',       'Tegra2',      'Arm7',   'Debug',   None,           None,      f_android, LINUX,  S_PERCOMMIT,     {'device': 'xoom'}),
+      ('Test', 'Android',  'Xoom',       'Tegra2',      'Arm7',   'Release', None,           None,      f_android, LINUX,  S_PERCOMMIT,     {'device': 'xoom'}),
+      ('Perf', 'Android',  'Xoom',       'Tegra2',      'Arm7',   'Release', None,           None,      f_android, LINUX,  S_PERCOMMIT,     {'device': 'xoom'}),
+      ('Test', 'Android',  'IntelRhb',   'SGX544',      'x86',    'Debug',   None,           None,      f_android, LINUX,  S_PERCOMMIT,     {'device': 'intel_rhb'}),
+      ('Test', 'Android',  'IntelRhb',   'SGX544',      'x86',    'Release', None,           None,      f_android, LINUX,  S_PERCOMMIT,     {'device': 'intel_rhb'}),
+      ('Perf', 'Android',  'IntelRhb',   'SGX544',      'x86',    'Release', None,           None,      f_android, LINUX,  S_PERCOMMIT,     {'device': 'intel_rhb'}),
   ]
 
   master_builders_cfg.setup_builders_from_config_list(
