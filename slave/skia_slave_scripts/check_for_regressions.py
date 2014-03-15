90acab8c54ebc628b563718bc707522ef8dd0b62
Ben Chen <bensong@google.com>
2014-03-15 15:48:03 -0400

    os.makedirs gives "File exists" Errno 17; so remove the dir first.
    BUG=skia:2225
    NOTRY=true
    TBR=borenet@google.com
    
    Review URL: https://codereview.chromium.org/201223002

--- slave/skia_slave_scripts/check_for_regressions.py
+++ slave/skia_slave_scripts/check_for_regressions.py
@@ -12,6 +12,7 @@ from utils import shell_utils
 
 import builder_name_schema
 import os
+import shutil
 import sys
 
 
@@ -41,6 +42,7 @@ class CheckForRegressions(BuildStep):
     # Writes the expectations from svn repo to the local file.
     path_to_bench_expectations = os.path.join(
         self._perf_range_input_dir, expectations_filename)
+    shutil.rmtree(self._perf_range_input_dir, ignore_errors=True)
     os.makedirs(self._perf_range_input_dir)
     with open(path_to_bench_expectations, 'w') as file_handle:
       file_handle.write(output)
