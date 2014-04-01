51cddef58f58bdd964d89fb409fc6d8db85a291b
Ravi Mistry <rmistry@google.com>
2014-04-01 08:27:55 -0400

    Add "smoothness" to the Cluster telemetry web app
    Live here: http://skia-tree-status.appspot.com/skia-telemetry/chromium_try
    
    BUG=358612
    
    Review URL: https://codereview.chromium.org/218403007

--- appengine_scripts/skia-tree-status/skia_telemetry.py
+++ appengine_scripts/skia-tree-status/skia_telemetry.py
@@ -38,6 +38,7 @@ PAGESET_TYPES = (
 # Constants for ChromiumTryPage.
 CHROMIUM_TRY_SUPPORTED_BENCHMARKS = (
     'rasterize_and_record_micro',
+    'smoothness',
     'loading_trace',
     'loading_profile'
 )
