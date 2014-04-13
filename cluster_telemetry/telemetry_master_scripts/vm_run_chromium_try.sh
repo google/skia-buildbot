db74061fa9e631c7e40760a682e66b36c89ad9d7
Ravi Mistry <rmistry@google.com>
2014-04-13 07:54:39 -0400

    Experiment with rebooting slaves before each run.

--- cluster_telemetry/telemetry_master_scripts/vm_run_chromium_try.sh
+++ cluster_telemetry/telemetry_master_scripts/vm_run_chromium_try.sh
@@ -143,9 +143,9 @@ if [ $ret_value -eq 0 ]; then
 
   # Below may no longer be needed after the move to bare-metal machines.
   # Reboot all slaves so that they start from a clean slate.
-  # bash vm_run_command_on_slaves.sh "sudo reboot"
+  bash vm_run_command_on_slaves.sh "sudo reboot"
   # Sleep for 4 mins for all slaves to come back up.
-  # sleep 240
+  sleep 240
 
   # Run telemetry on the slaves using the specified benchmark.
   TELEMETRY_BUILD_LOG=/tmp/try-telemetry-nopatch-$RUN_ID
@@ -158,9 +158,9 @@ if [ $ret_value -eq 0 ]; then
 
   # Below may no longer be needed after the move to bare-metal machines.
   # Reboot all slaves so that they start from a clean slate.
-  # bash vm_run_command_on_slaves.sh "sudo reboot"
+  bash vm_run_command_on_slaves.sh "sudo reboot"
   # Sleep for 4 mins for all slaves to come back up.
-  # sleep 240
+  sleep 240
 
   # Run telemetry using the patch build.
   TELEMETRY_BUILD_LOG=/tmp/try-telemetry-withpatch-$RUN_ID
