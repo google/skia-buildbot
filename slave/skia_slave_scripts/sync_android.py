090280bd5a33838f0972a6af11c5f8c17adfb226
scroggo <scroggo@google.com>
2014-04-21 06:28:34 -0700

    Get repo if needed on Android roll bot.
    In case we have to move to a new instance, we'll need to reinstall
    repo.
    
    BUG=skia:2409
    R=epoger@google.com, borenet@google.com
    
    Author: scroggo@google.com
    
    Review URL: https://codereview.chromium.org/237983003

--- slave/skia_slave_scripts/sync_android.py
+++ slave/skia_slave_scripts/sync_android.py
@@ -17,8 +17,8 @@ from utils import shell_utils
 ANDROID_CHECKOUT_PATH = os.path.join(os.pardir, 'android_repo')
 ANDROID_REPO_URL = ('https://googleplex-android.googlesource.com/a/platform/'
                     'manifest')
+REPO_URL = 'http://commondatastorage.googleapis.com/git-repo-downloads/repo'
 REPO = os.path.join(os.path.expanduser('~'), 'bin', 'repo')
-
 
 class SyncAndroid(BuildStep):
   """BuildStep which syncs the Android sources."""
@@ -30,6 +30,11 @@ class SyncAndroid(BuildStep):
       pass
     print 'cd %s' % ANDROID_CHECKOUT_PATH
     os.chdir(ANDROID_CHECKOUT_PATH)
+
+    if not os.path.exists(REPO):
+      # Download repo.
+      shell_utils.run(['curl', REPO_URL, '>', REPO])
+      shell_utils.run(['chmod', 'a+x', REPO])
 
     shell_utils.run([REPO, 'init', '-u', ANDROID_REPO_URL, '-g',
                      'all,-notdefault,-darwin', '-b', 'master-skia'])
