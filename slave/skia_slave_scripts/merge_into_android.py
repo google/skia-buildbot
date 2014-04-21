489fa99b98d7789d74f5d87aafb6d97c01964854
scroggo <scroggo@google.com>
2014-04-21 10:31:31 -0700

    Fixes to merge_into_android.py
    Address issues brought up by Eric in https://codereview.chromium.org/238053002/.
    - Use GetGlobalVariable to find url for skia repo.
    - Use misc.ChDir to change back to the original directory.
    - Use gclient_utils.GIT to call git.
    
    In addition:
    - Set git config user name and email locally rather than globally.
      - This is helpful for running a local slave.
    
    BUG=skia:2409
    R=borenet@google.com
    
    Author: scroggo@google.com
    
    Review URL: https://codereview.chromium.org/244753003

--- slave/skia_slave_scripts/merge_into_android.py
+++ slave/skia_slave_scripts/merge_into_android.py
@@ -11,11 +11,13 @@ import os
 import sys
 
 from build_step import BuildStep
+import skia_vars
 from sync_android import ANDROID_CHECKOUT_PATH, REPO
+from utils.gclient_utils import GIT
+from utils import misc
 from utils import shell_utils
 
-
-SKIA_REPO_URL = 'https://skia.googlesource.com/skia'
+SKIA_REPO_URL = skia_vars.GetGlobalVariable('skia_git_url')
 MASTER_SKIA_URL = ('https://googleplex-android-review.googlesource.com/'
                    'platform/external/skia')
 MASTER_SKIA_REFS = 'HEAD:refs/heads/master-skia'
@@ -35,7 +37,6 @@ PLATFORM_TOOLS_BIN = os.path.join('platform_tools', 'android', 'bin')
 # switch to git and there is no revision number? (Or will there be one? See
 # skbug.com/1639).
 GIT_SVN_ID = 'http://skia.googlecode.com/svn/trunk@'
-GIT = 'git'
 
 LOCAL_BRANCH_NAME = 'merge'
 
@@ -44,84 +45,76 @@ class MergeIntoAndroid(BuildStep):
   SkUserConfig.h"""
 
   def _Run(self):
-    # TODO: The code below this depends on the CWD being set to EXTERNAL_SKIA,
-    # but calling os.chdir() seems problematic. Will our calling code be upset
-    # by the fact that we changed the directory? What if a function we call
-    # changes the CWD? If shell_utils.run() took a cwd parameter, we could
-    # avoid the chdir.
-    print 'cd %s' % EXTERNAL_SKIA
-    os.chdir(EXTERNAL_SKIA)
+    with misc.ChDir(EXTERNAL_SKIA):
+      print 'cd %s' % EXTERNAL_SKIA
 
-    # Set up git config properly.
-    shell_utils.run([GIT, 'config', '--global', 'user.email',
-                     '"31977622648@project.gserviceaccount.com"'])
-    shell_utils.run([GIT, 'config', '--global', 'user.name',
-                     '"Skia_Android Canary Bot"'])
+      # Set up git config properly.
+      shell_utils.run([GIT, 'config', 'user.email',
+                       '"31977622648@project.gserviceaccount.com"'])
+      shell_utils.run([GIT, 'config', 'user.name',
+                       '"Skia_Android Canary Bot"'])
 
-    # Check to see whether there is an upstream yet.
-    if not UPSTREAM_REMOTE_NAME in shell_utils.run([GIT, 'remote', 'show']):
-      shell_utils.run([GIT, 'remote', 'add', UPSTREAM_REMOTE_NAME,
-                       SKIA_REPO_URL])
+      # Check to see whether there is an upstream yet.
+      if not UPSTREAM_REMOTE_NAME in shell_utils.run([GIT, 'remote', 'show']):
+        shell_utils.run([GIT, 'remote', 'add', UPSTREAM_REMOTE_NAME,
+                         SKIA_REPO_URL])
 
-    # Update the upstream remote.
-    shell_utils.run([GIT, 'fetch', UPSTREAM_REMOTE_NAME])
+      # Update the upstream remote.
+      shell_utils.run([GIT, 'fetch', UPSTREAM_REMOTE_NAME])
 
-    # Start the merge.
-    try:
-      shell_utils.run([GIT, 'merge', UPSTREAM_BRANCH_NAME, '--no-commit'])
-    except shell_utils.CommandFailedException:
-      # Merge conflict. There may be a more elegant solution, but for now, undo
-      # the merge, and allow (/make) a human to do it.
-      shell_utils.run([GIT, 'merge', '--abort'])
-      raise Exception('Merge failed. Fall back to manual human merge.')
+      # Start the merge.
+      try:
+        shell_utils.run([GIT, 'merge', UPSTREAM_BRANCH_NAME, '--no-commit'])
+      except shell_utils.CommandFailedException:
+        # Merge conflict. There may be a more elegant solution, but for now,
+        # undo the merge, and allow (/make) a human to do it.
+        shell_utils.run([GIT, 'merge', '--abort'])
+        raise Exception('Merge failed. Fall back to manual human merge.')
 
-    # FIXME (scroggo): If we put all of Skia into Android (see skbug.com/2416),
-    # it might make sense to do a commit now, and use a separate build step for
-    # generating Android.mk and SkUserConfig.h.
+      # Grab the upstream version of SkUserConfig, which will be used to
+      # generate Android's version.
+      shell_utils.run([GIT, 'checkout', UPSTREAM_BRANCH_NAME, '--',
+                       UPSTREAM_USER_CONFIG])
 
-    # Grab the upstream version of SkUserConfig, which will be used to
-    # generate Android's version.
-    shell_utils.run([GIT, 'checkout', UPSTREAM_BRANCH_NAME, '--',
-                     UPSTREAM_USER_CONFIG])
+      # We don't want to commit the upstream version, so remove it from the
+      # index.
+      shell_utils.run([GIT, 'reset', 'HEAD', UPSTREAM_USER_CONFIG])
 
-    # We don't want to commit the upstream version, so remove it from the index.
-    shell_utils.run([GIT, 'reset', 'HEAD', UPSTREAM_USER_CONFIG])
+      # Now generate Android.mk and SkUserConfig.h
+      sys.path.append(os.path.join(os.getcwd(), PLATFORM_TOOLS_BIN))
+      import gyp_to_android
+      gyp_to_android.main()
+      shell_utils.run([GIT, 'add', 'Android.mk'])
+      shell_utils.run([GIT, 'add', ANDROID_USER_CONFIG])
 
-    # Now generate Android.mk and SkUserConfig.h
-    sys.path.append(os.path.join(os.getcwd(), PLATFORM_TOOLS_BIN))
-    import gyp_to_android
-    gyp_to_android.main()
-    shell_utils.run([GIT, 'add', 'Android.mk'])
-    shell_utils.run([GIT, 'add', ANDROID_USER_CONFIG])
+      # Remove upstream user config, which is no longer needed.
+      shell_utils.run(['rm', UPSTREAM_USER_CONFIG])
 
-    # Remove upstream user config, which is no longer needed.
-    shell_utils.run(['rm', UPSTREAM_USER_CONFIG])
+      # Create a new branch.
+      shell_utils.run([REPO, 'start', LOCAL_BRANCH_NAME, '.'])
 
-    # Create a new branch.
-    shell_utils.run([REPO, 'start', LOCAL_BRANCH_NAME, '.'])
+      # Figure out the revision being merged, and use it to write the commit
+      # message.
+      commit_message = shell_utils.run([GIT, 'show', UPSTREAM_BRANCH_NAME])
+      rev_index = commit_message.find(GIT_SVN_ID) + len(GIT_SVN_ID)
+      rev_end = commit_message.find(' ', rev_index)
+      revision = commit_message[rev_index:rev_end]
 
-    # Figure out the revision being merged, and use it to write the commit
-    # message.
-    commit_message = shell_utils.run([GIT, 'show', UPSTREAM_BRANCH_NAME])
-    rev_index = commit_message.find(GIT_SVN_ID) + len(GIT_SVN_ID)
-    rev_end = commit_message.find(' ', rev_index)
-    revision = commit_message[rev_index:rev_end]
+      try:
+        shell_utils.run([GIT, 'commit', '-m', 'Merge Skia at r' + revision])
+      except shell_utils.CommandFailedException:
+        # It is possible that someone else already did the merge (for example,
+        # if they are testing a build slave). Clean up and exit.
+        shell_utils.run([REPO, 'abandon', LOCAL_BRANCH_NAME])
+        raise Exception('Nothing to merge; did someone already merge r%s?'
+                        % revision)
 
-    try:
-      shell_utils.run([GIT, 'commit', '-m', 'Merge Skia at r' + revision])
-    except shell_utils.CommandFailedException:
-      # It is possible that someone else already did the merge (for example, if
-      # they are testing a build slave). Clean up and exit.
-      shell_utils.run([REPO, 'abandon', LOCAL_BRANCH_NAME])
-      raise Exception('Nothing to merge; did someone already merge r%s?'
-                      % revision)
+      # Now push to master-skia branch
+      shell_utils.run([GIT, 'push', MASTER_SKIA_URL, MASTER_SKIA_REFS])
 
-    # Now push to master-skia branch
-    shell_utils.run([GIT, 'push', MASTER_SKIA_URL, MASTER_SKIA_REFS])
-
-    # Our branch is no longer needed. Remove it.
-    shell_utils.run([REPO, 'sync', '-j32', '.'])
-    shell_utils.run([REPO, 'prune', '.'])
+      # Our branch is no longer needed. Remove it.
+      shell_utils.run([REPO, 'sync', '-j32', '.'])
+      shell_utils.run([REPO, 'prune', '.'])
 
 
 if '__main__' == __name__:
