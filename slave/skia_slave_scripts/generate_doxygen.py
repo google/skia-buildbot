1e569ba61871b48d19c267dd97bb9dfe81c06de4
epoger <epoger@google.com>
2014-05-14 08:12:09 -0700

    upload doxygen docs to Google Storage instead of skia-autogen
    BUG=skia:2470
    NOTREECHECKS=True
    R=rmistry@google.com
    
    Author: epoger@google.com
    
    Review URL: https://codereview.chromium.org/284943002

--- slave/skia_slave_scripts/generate_doxygen.py
+++ slave/skia_slave_scripts/generate_doxygen.py
@@ -5,23 +5,52 @@
 
 """Generate Doxygen documentation."""
 
+import datetime
 import os
+import shutil
 import sys
 
 from build_step import BuildStep
 from utils import file_utils, shell_utils
 
-DOXYGEN_WORKING_DIR = os.path.join(os.pardir, os.pardir, 'doxygen-contents')
+DOXYFILE_BASENAME = 'Doxyfile'  # must match name of Doxyfile in skia root
+DOXYGEN_BINARY = 'doxygen'
+DOXYGEN_CONFIG_DIR = os.path.join(os.pardir, os.pardir, 'doxygen-config')
+DOXYGEN_WORKING_DIR = os.path.join(os.pardir, os.pardir, 'doxygen')
+
+IFRAME_FOOTER_TEMPLATE = """
+<html><body><address style="text-align: right;"><small>
+Generated at %s for skia
+by <a href="http://www.doxygen.org/index.html">doxygen</a>
+%s </small></address></body></html>
+"""
 
 
 class GenerateDoxygen(BuildStep):
   def _Run(self):
+    # Create empty dir and add static_footer.txt
     file_utils.create_clean_local_dir(DOXYGEN_WORKING_DIR)
+    static_footer_path = os.path.join(DOXYGEN_WORKING_DIR, 'static_footer.txt')
+    shutil.copyfile(os.path.join('tools', 'doxygen_footer.txt'),
+                    static_footer_path)
 
-    os.environ['DOXYGEN_TEMPDIR'] = DOXYGEN_WORKING_DIR
-    os.environ['DOXYGEN_COMMIT'] = 'false'
-    update_doxygen_path = os.path.join('tools', 'update-doxygen.sh')
-    shell_utils.run(update_doxygen_path)
+    # Make copy of doxygen config file, overriding any necessary configs,
+    # and run doxygen.
+    file_utils.create_clean_local_dir(DOXYGEN_CONFIG_DIR)
+    modified_doxyfile = os.path.join(DOXYGEN_CONFIG_DIR, DOXYFILE_BASENAME)
+    with open(DOXYFILE_BASENAME, 'r') as reader:
+      with open(modified_doxyfile, 'w') as writer:
+        shutil.copyfileobj(reader, writer)
+        writer.write('OUTPUT_DIRECTORY = %s\n' % DOXYGEN_WORKING_DIR)
+        writer.write('HTML_FOOTER = %s\n' % static_footer_path)
+    shell_utils.run([DOXYGEN_BINARY, modified_doxyfile])
+
+    # Create iframe_footer.html
+    with open(os.path.join(DOXYGEN_WORKING_DIR, 'iframe_footer.html'),
+              'w') as fh:
+      fh.write(IFRAME_FOOTER_TEMPLATE % (
+          datetime.datetime.now().isoformat(' '),
+          shell_utils.run([DOXYGEN_BINARY, '--version'])))
 
 
 if '__main__' == __name__:
--- slave/skia_slave_scripts/upload_doxygen.py
+++ slave/skia_slave_scripts/upload_doxygen.py
@@ -3,64 +3,33 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-"""Upload Doxygen documentation."""
+"""Uploads Doxygen documentation to Google Storage, where it can be browsed at
+http://chromium-skia-gm.commondatastorage.googleapis.com/doxygen/doxygen/html/index.html
+"""
 
-import os
+import posixpath
 import sys
 
 from build_step import BuildStep
-from config_private import AUTOGEN_SVN_BASEURL
-from utils import merge_into_svn
+from utils import gs_utils
 import generate_doxygen
+import skia_vars
 
+DOXYGEN_GSUTIL_PATH = posixpath.join(
+    skia_vars.GetGlobalVariable('googlestorage_bucket'), 'doxygen')
 
-# Class we can set attributes on, to emulate optparse-parsed options.
-# TODO(epoger): Delete this once we are uploading doxygen docs using some
-# mechanism other than SVN.
-class Options(object):
-  pass
+# Directives for HTTP caching of these files served out by Google Storage.
+# See http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html
+GS_CACHE_CONTROL_HEADER = 'Cache-Control:public,max-age=3600'
 
 
 class UploadDoxygen(BuildStep):
-
-  def _SVNUploadDir(self, src_dir, dest_subdir, step_name):
-    """Upload the entire contents of src_dir to the skia-autogen SVN repo.
-
-    This is mostly copy-pasted from _SVNUploadDir() in upload_gm_results.py.
-    TODO(epoger): Delete this and use some mechanism other than SVN.
-
-    Args:
-      src_dir: (string) directory to upload contents of
-      dest_subdir: (string) subdir on the skia-autogen repo to upload into
-      step_name: (string) name of the step that is performing this action
-    """
-    merge_basedir = os.path.join(os.pardir, os.pardir, 'doxygen-merge')
-    actual_svn_baseurl = '%s/%s' % (AUTOGEN_SVN_BASEURL, dest_subdir)
-    autogen_svn_username_file = self._args['autogen_svn_username_file']
-    autogen_svn_password_file = self._args['autogen_svn_password_file']
-
-    merge_options = Options()
-    # pylint: disable=W0201
-    merge_options.commit_message = '%s of r%s on %s' % (
-        step_name, self._got_revision, self._args['builder_name'])
-    # pylint: disable=W0201
-    merge_options.dest_svn_url = actual_svn_baseurl
-    # pylint: disable=W0201
-    merge_options.merge_dir_path = os.path.join(merge_basedir,
-                                                self._args['builder_name'])
-    # pylint: disable=W0201
-    merge_options.source_dir_path = src_dir
-    # pylint: disable=W0201
-    merge_options.svn_password_file = autogen_svn_password_file
-    # pylint: disable=W0201
-    merge_options.svn_username_file = autogen_svn_username_file
-    merge_into_svn.MergeIntoSvn(merge_options)
-
   def _Run(self):
-    self._SVNUploadDir(
+    gs_utils.copy_storage_directory(
         src_dir=generate_doxygen.DOXYGEN_WORKING_DIR,
-        dest_subdir='docs',
-        step_name=self.__class__.__name__)
+        dest_dir=DOXYGEN_GSUTIL_PATH,
+        gs_acl='public-read',
+        http_header_lines=[GS_CACHE_CONTROL_HEADER])
 
 
 if '__main__' == __name__:
