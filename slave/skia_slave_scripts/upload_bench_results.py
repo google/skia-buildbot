703dcbce30a4024744a53b011efa17e6ead1191d
kelvinly <kelvinly@google.com>
2014-05-23 14:49:13 -0700

    Add more data to JSON files before uploading
    BUG=skia:
    R=bensong@google.com, rmistry@google.com
    
    Author: kelvinly@google.com
    
    Review URL: https://codereview.chromium.org/298863015

--- slave/skia_slave_scripts/upload_bench_results.py
+++ slave/skia_slave_scripts/upload_bench_results.py
@@ -8,7 +8,11 @@
 from build_step import BuildStep, BuildStepWarning
 from utils import sync_bucket_subdir
 
+import json
+import os
+import os.path
 import posixpath
+import re
 import sys
 from datetime import datetime
 
@@ -28,12 +32,31 @@ class UploadBenchResults(BuildStep):
     dest_gsbase = (self._args.get('dest_gsbase') or
                    sync_bucket_subdir.DEFAULT_PERFDATA_GS_BASE)
 
+    # Upload the normal bench logs
     res_sync_data = sync_bucket_subdir.SyncBucketSubdir(
         directory=self._GetPerfDataDir(),
         dest_gsbase=dest_gsbase,
         subdir=self._GetBucketSubdir(),
         do_upload=True,
         do_download=False)
+
+    # Find and open JSON file to add in additional fields, then upload.
+    json_file_name = None
+    file_list = os.listdir(self._GetPerfDataDir())
+    for file_name in file_list:
+      if re.search('microbench_({})_[0-9]+\.json'.format(self._got_revision),
+             file_name):
+        json_file_name = os.path.join(self._GetPerfDataDir(), file_name)
+        break
+    if json_file_name:
+      json_file = open(json_file_name, 'r+')
+      json_data = json.load(json_file)
+      json_data['machine'] = self._builder_name
+      json_data['commitHash'] = self._got_revision
+      json_file.seek(0)
+      json.dump(json_data, json_file)
+      json_file.truncate()
+      json_file.close()
 
     now = datetime.utcnow()
     gs_json_path = '/'.join((str(now.year).zfill(4), str(now.month).zfill(2),
@@ -50,7 +73,7 @@ class UploadBenchResults(BuildStep):
         filenames_filter=
             'microbench_({})_[0-9]+\.json'.format(self._got_revision))
 
-    return res_sync_json or res_sync_data
+    return 0
 
   def _Run(self):
     if self._is_try:
