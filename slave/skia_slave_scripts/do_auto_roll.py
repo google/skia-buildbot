0a0e25e8cf1a7bcf27956dca5994b963bafbee2c
borenet <borenet@google.com>
2014-06-19 07:41:59 -0700

    AutoRoll: Update the arb_status.html file, display its contents on the buildbot page
    BUG=skia:
    R=rmistry@google.com
    
    Author: borenet@google.com
    
    Review URL: https://codereview.chromium.org/343793002

--- slave/skia_slave_scripts/do_auto_roll.py
+++ slave/skia_slave_scripts/do_auto_roll.py
@@ -28,13 +28,37 @@ HTML_CONTENT = '''
 <head>
 <meta http-equiv="Pragma" content="no-cache">
 <meta http-equiv="Expires" content="-1">
-<meta http-equiv="refresh" content="0; url=https://codereview.chromium.org/%s/" />
+<meta http-equiv="refresh" content="0; url=%s" />
 </head>
 </html>
 '''
-ISSUE_REGEXP = (
+ISSUE_URL_TEMPLATE = 'https://codereview.chromium.org/%(issue)s/'
+
+# TODO(borenet): Find a way to share these filenames (or their full GS URL) with
+# the webstatus which links to them.
+FILENAME_CURRENT_ATTEMPT = 'depsroll.html'
+FILENAME_ROLL_STATUS = 'arb_status.html'
+
+REGEXP_ISSUE_CREATED = (
     r'Issue created. URL: https://codereview.chromium.org/(?P<issue>\d+)')
-UPLOAD_FILENAME = 'depsroll.html'
+REGEXP_ROLL_ACTIVE = (
+    r'https://codereview.chromium.org/(?P<issue>\d+)/ is still active')
+REGEXP_ROLL_STOPPED = (
+    r'https://codereview.chromium.org/(?P<issue>\d+)/: Rollbot was stopped by')
+# This occurs when the ARB has "caught up" and has nothing new to roll, or when
+# a different roll (typically a manual roll) has already rolled past it.
+REGEXP_ROLL_TOO_OLD = r'Already at .+ refusing to roll backwards to .+'
+
+ROLL_STATUS_IN_PROGRESS = 'In progress - %s' % ISSUE_URL_TEMPLATE
+ROLL_STATUS_STOPPED = 'Stopped - %s' % ISSUE_URL_TEMPLATE
+ROLL_STATUS_IDLE = 'Idle'
+
+ROLL_STATUSES = [
+    (REGEXP_ISSUE_CREATED, ROLL_STATUS_IN_PROGRESS),
+    (REGEXP_ROLL_ACTIVE,   ROLL_STATUS_IN_PROGRESS),
+    (REGEXP_ROLL_STOPPED,  ROLL_STATUS_STOPPED),
+    (REGEXP_ROLL_TOO_OLD,  ROLL_STATUS_IDLE),
+]
 
 
 class AutoRoll(BuildStep):
@@ -50,19 +74,45 @@ class AutoRoll(BuildStep):
     chrome_path = os.path.join(os.pardir, 'src')
     # python auto_roll.py <project> <author> <path to chromium/src>
     cmd = ['python', auto_roll, 'skia', DEPS_ROLL_AUTHOR, chrome_path]
-    output = shell_utils.run(cmd)
 
-    match = re.search(ISSUE_REGEXP, output)
+    exception = None
+    try:
+      output = shell_utils.run(cmd)
+    except shell_utils.CommandFailedException as e:
+      output = e.output
+      exception = e
+
+    match = re.search(REGEXP_ISSUE_CREATED, output)
     if match:
       issue = match.group('issue')
       print 'Found issue #', issue
-      with open(UPLOAD_FILENAME, 'w') as f:
-        f.write(HTML_CONTENT % issue)
+      with open(FILENAME_CURRENT_ATTEMPT, 'w') as f:
+        f.write(HTML_CONTENT % (ISSUE_URL_TEMPLATE % {'issue': issue}))
       slave_utils.GSUtilCopyFile(
-          filename=UPLOAD_FILENAME,
+          filename=FILENAME_CURRENT_ATTEMPT,
           gs_base=skia_vars.GetGlobalVariable('googlestorage_bucket'),
           subdir=None,
           gs_acl='public-read')
+
+    roll_status = None
+    for regexp, status_msg in ROLL_STATUSES:
+      match = re.search(regexp, output)
+      if match:
+        roll_status = status_msg % match.groupdict()
+        break
+
+    if roll_status:
+      with open(FILENAME_ROLL_STATUS, 'w') as f:
+        f.write(roll_status)
+      slave_utils.GSUtilCopyFile(
+          filename=FILENAME_ROLL_STATUS,
+          gs_base=skia_vars.GetGlobalVariable('googlestorage_bucket'),
+          subdir=None,
+          gs_acl='public-read')
+
+    #pylint: disable=E0702
+    if exception:
+      raise exception
 
 
 if '__main__' == __name__:
