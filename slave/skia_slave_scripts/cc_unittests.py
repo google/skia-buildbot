840e63fc70f3e7f56e22d6b5fa2e96c0d3bdfa67
borenet <borenet@google.com>
2014-06-24 05:11:50 -0700

    Windows Chrome canary: skip SchedulerTest in cc_unittests
    BUG=skia:2688
    R=rmistry@google.com
    
    Author: borenet@google.com
    
    Review URL: https://codereview.chromium.org/334933006

--- slave/skia_slave_scripts/cc_unittests.py
+++ slave/skia_slave_scripts/cc_unittests.py
@@ -6,13 +6,19 @@
 """ Run the cc_unittests executable. """
 
 from build_step import BuildStep
+import os
 import sys
 
 
 class CCUnitTests(BuildStep):
 
   def _Run(self):
-    self._flavor_utils.RunFlavoredCmd('cc_unittests', [])
+    args = []
+    if os.name == 'nt':
+      # SchedulerTest is flaky, so disabling it for now. For details, see:
+      # https://code.google.com/p/chromium/issues/detail?id=380889
+      args.append('--gtest_filter=-SchedulerTest.*')
+    self._flavor_utils.RunFlavoredCmd('cc_unittests', args)
 
 
 if '__main__' == __name__:
