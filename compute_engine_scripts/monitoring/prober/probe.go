94caa6a4abc885f4ebe2779e157baa6d1e5f80bd
jcgregorio <jcgregorio@google.com>
2014-06-27 10:26:49 -0700

    Add the ability to have more than one config file.
    Add a bodytest parameter to the prober config that selects a function to run over the response body.
    
    Add a bodytest function that tests that buildslaves all report as connected.
    
    BUG=skia:2697
    
    I also had started on a Go program to read the global config and write the buildbots.json file, but then realized that the port number was stored separately from the IP address of the buildbot, and then I got sad, and tired. I can put up that code in a separate CL if someone want to take a crack as finishing it.
    
    R=borenet@google.com, mtklein@google.com
    
    Author: jcgregorio@google.com
    
    Review URL: https://codereview.chromium.org/345703004

--- compute_engine_scripts/monitoring/prober/probe.go
+++ compute_engine_scripts/monitoring/prober/probe.go
@@ -10,6 +10,7 @@ import (
 	"flag"
 	"fmt"
 	"github.com/rcrowley/go-metrics"
+	"io"
 	"log"
 	"net"
 	"net/http"
@@ -18,16 +19,28 @@ import (
 	"time"
 )
 
+import (
+	"github.com/golang/glog"
+)
+
 var (
-	config = flag.String("config", "probers.json", "Name of prober config file.")
+	config = flag.String("config", "probers.json,buildbots.json", "Comma separated names of prober config files.")
 	prefix = flag.String("prefix", "prober", "Prefix to add to all prober values sent to Carbon.")
 	carbon = flag.String("carbon", "localhost:2003", "Address of Carbon server and port.")
+
+	// bodyTesters is a mapping of names to functions that test response bodies.
+	bodyTesters = map[string]BodyTester{
+		"buildbotJSON": testBuildbotJSON,
+	}
 )
 
 const (
 	SAMPLE_PERIOD = time.Minute
 	TIMEOUT       = time.Duration(20 * time.Second)
 )
+
+// BodyTester tests the response body from a probe and returns true if it passes all tests.
+type BodyTester func(io.Reader) bool
 
 // Probe is a single endpoint we are probing.
 type Probe struct {
@@ -46,25 +59,39 @@ type Probe struct {
 	// The mimetype of the Body.
 	MimeType string `json:"mimetype"`
 
-	Success metrics.Counter
-	Failure metrics.Counter
-	Latency metrics.Timer
+	// The body testing function we should use.
+	BodyTestName string `json:"bodytest"`
+
+	bodyTest BodyTester
+	success  metrics.Counter
+	failure  metrics.Counter
+	latency  metrics.Timer
 }
 
 // Probes is all the probes that are to be run.
 type Probes map[string]*Probe
 
-func readConfigFile(filename string) (*Probes, error) {
-	file, err := os.Open(filename)
-	if err != nil {
-		return nil, fmt.Errorf("Failed to open config file: %s", err)
+func readConfigFiles(filenames string) (Probes, error) {
+	allProbes := Probes{}
+	for _, filename := range strings.Split(filenames, ",") {
+		file, err := os.Open(filename)
+		if err != nil {
+			return nil, fmt.Errorf("Failed to open config file: %s", err)
+		}
+		d := json.NewDecoder(file)
+		p := &Probes{}
+		if err := d.Decode(p); err != nil {
+			return nil, fmt.Errorf("Failed to decode JSON in config file: %s", err)
+		}
+		for k, v := range *p {
+			if f, ok := bodyTesters[v.BodyTestName]; ok {
+				v.bodyTest = f
+				log.Printf("Found a body test for %s", k)
+			}
+			allProbes[k] = v
+		}
 	}
-	d := json.NewDecoder(file)
-	p := &Probes{}
-	if err := d.Decode(p); err != nil {
-		return nil, fmt.Errorf("Failed to decode JSON in config file: %s", err)
-	}
-	return p, nil
+	return allProbes, nil
 }
 
 // In returns true if n is found in list.
@@ -80,6 +107,29 @@ func In(n int, list []int) bool {
 // dialTimeout is a dialer that sets a timeout.
 func dialTimeout(network, addr string) (net.Conn, error) {
 	return net.DialTimeout(network, addr, TIMEOUT)
+}
+
+// testBuildbotJSON tests that all of the slaves are reported as connected.
+func testBuildbotJSON(r io.Reader) bool {
+	type SlaveStatus struct {
+		Connected bool `json:"connected"`
+	}
+
+	type Slaves map[string]SlaveStatus
+
+	dec := json.NewDecoder(r)
+
+	slaves := make(Slaves)
+	if err := dec.Decode(&slaves); err != nil {
+		glog.Errorf("Failed to decode buildslave JSON: %s", err)
+		return false
+	}
+	allConnected := true
+	for k, v := range slaves {
+		glog.Infof("Disconnected buildslave: %s", k)
+		allConnected = allConnected && v.Connected
+	}
+	return allConnected
 }
 
 func main() {
@@ -102,16 +152,16 @@ func main() {
 	go metrics.Graphite(probeRegistry, SAMPLE_PERIOD, *prefix, addr)
 
 	// TODO(jcgregorio) Monitor config file and reload if it changes.
-	cfg, err := readConfigFile(*config)
+	cfg, err := readConfigFiles(*config)
 	if err != nil {
 		log.Fatalln("Failed to read config file: ", err)
 	}
 	log.Println("Successfully read config file.")
 	// Register counters for each probe.
-	for name, probe := range *cfg {
-		probe.Success = metrics.NewRegisteredCounter(name+".success", probeRegistry)
-		probe.Failure = metrics.NewRegisteredCounter(name+".failure", probeRegistry)
-		probe.Latency = metrics.NewRegisteredTimer(name+".latency", probeRegistry)
+	for name, probe := range cfg {
+		probe.success = metrics.NewRegisteredCounter(name+".success", probeRegistry)
+		probe.failure = metrics.NewRegisteredCounter(name+".failure", probeRegistry)
+		probe.latency = metrics.NewRegisteredTimer(name+".latency", probeRegistry)
 	}
 	var resp *http.Response
 	var begin time.Time
@@ -123,7 +173,7 @@ func main() {
 		},
 	}
 	for _ = range time.Tick(SAMPLE_PERIOD) {
-		for name, probe := range *cfg {
+		for name, probe := range cfg {
 			log.Println("Running probe: ", name)
 			begin = time.Now()
 			if probe.Method == "GET" {
@@ -134,15 +184,21 @@ func main() {
 				log.Println("Error: unknown method: ", probe.Method)
 				continue
 			}
-			resp.Body.Close()
+			bodyTestResults := true
+			if probe.bodyTest != nil {
+				bodyTestResults = probe.bodyTest(resp.Body)
+			}
+			if resp.Body != nil {
+				resp.Body.Close()
+			}
 			d := time.Since(begin)
 			// TODO(jcgregorio) Save the last N responses and present them in a web UI.
-			if err == nil && In(resp.StatusCode, probe.Expected) {
-				probe.Success.Inc(1)
+			if err == nil && In(resp.StatusCode, probe.Expected) && bodyTestResults {
+				probe.success.Inc(1)
 			} else {
-				probe.Failure.Inc(1)
+				probe.failure.Inc(1)
 			}
-			probe.Latency.Update(d)
+			probe.latency.Update(d)
 		}
 	}
 }
