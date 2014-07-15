4edf272426bd6a42f5a39470f76f064582d3ac07
mtklein <mtklein@chromium.org>
2014-07-15 07:36:38 -0700

    Fix up DM.
    NOTREECHECKS=true
    
    BUG=skia:
    R=borenet@google.com, mtklein@google.com
    
    Author: mtklein@chromium.org
    
    Review URL: https://codereview.chromium.org/398483002

--- slave/skia_slave_scripts/run_dm.py
+++ slave/skia_slave_scripts/run_dm.py
@@ -8,14 +8,40 @@
 from build_step import BuildStep
 import sys
 
-
 class RunDM(BuildStep):
+  def _AnyMatch(self, *args):
+    return any(arg in self._builder_name for arg in args)
+
+  def _AllMatch(self, *args):
+    return all(arg in self._builder_name for arg in args)
+
   def _Run(self):
     args = [
       '--verbose',
       '--resourcePath', self._device_dirs.ResourceDir(),
     ]
-    self._flavor_utils.RunFlavoredCmd('dm', args)
+
+    run_dm = True
+    match  = []
+
+    # Disable failing tests.
+    if self._AnyMatch('ChromeOS', 'DirectWrite'):
+      match.append('~bitmapscroll')
+
+    if self._AnyMatch('Win7'):
+      match.append('~blurroundrect')
+
+    # Disable crashing tests.
+    if self._AllMatch('10.6', 'Debug'):
+      # Not sure what's failing exactly, so disable DM entirely.
+      run_dm = False
+
+    if match:
+      args.append('--match')
+      args.extend(match)
+
+    if run_dm:
+      self._flavor_utils.RunFlavoredCmd('dm', args)
 
 if '__main__' == __name__:
   sys.exit(BuildStep.RunBuildStep(RunDM))
