c584a98982b1fd09cf8fb097e5c4c860021f57d5
bensong <bensong@google.com>
2014-08-04 13:36:45 -0700

    fix path typo; change perf to skiaperf.
    BUG=skia:
    R=jcgregorio@google.com, kelvinly@google.com
    
    Author: bensong@google.com
    
    Review URL: https://codereview.chromium.org/436333002

--- perf/setup/perf_setup.sh
+++ perf/setup/perf_setup.sh
@@ -12,8 +12,8 @@ sudo install $PARAMS --mode=766 continue_install /home/perf/
 sudo install $PARAMS --mode=666 -T sys/_bash_aliases /home/perf/.bash_aliases
 sudo su perf -c /home/perf/continue_install
 
-sudo cp sys/perf_init /etc/init.d/perf
-sudo chmod 744 /etc/init.d/perf
+sudo cp sys/perf_init /etc/init.d/skiaperf
+sudo chmod 744 /etc/init.d/skiaperf
 sudo cp sys/tilebuilder_init /etc/init.d/tilebuilder
 sudo chmod 744 /etc/init.d/tilebuilder
 sudo cp sys/ingest_init /etc/init.d/ingest
@@ -25,7 +25,7 @@ sudo cp sys/perf_squid /etc/squid3/squid.conf
 # Confirm that monit is happy.
 sudo monit -t
 sudo monit reload
-sudo /etc/init.d/perf restart
+sudo /etc/init.d/skiaperf restart
 sudo /etc/init.d/tilebuilder restart
 sudo /etc/init.d/ingest restart
 sudo /etc/init.d/logserver restart
