af9477b51150cb4879563a96a2528febba3c85ed
mtklein <mtklein@chromium.org>
2014-08-06 06:46:13 -0700

    Can we do something like this to squeltch known RenderPdfs failures?
    BUG=skia:2743
    R=borenet@google.com, halcanary@google.com, mtklein@google.com
    
    Author: mtklein@chromium.org
    
    Review URL: https://codereview.chromium.org/442953002

--- slave/skia_slave_scripts/render_pdfs.py
+++ slave/skia_slave_scripts/render_pdfs.py
@@ -7,14 +7,25 @@
 """ Run the Skia render_pdfs executable. """
 
 
-from build_step import BuildStep
+from build_step import BuildStep, BuildStepWarning
+from py.utils.shell_utils import CommandFailedException
 import sys
 
 
 class RenderPdfs(BuildStep):
   def _Run(self):
-    self._flavor_utils.RunFlavoredCmd('render_pdfs',
-                                      [self._device_dirs.SKPDir()])
+    try:
+      self._flavor_utils.RunFlavoredCmd('render_pdfs',
+                                        [self._device_dirs.SKPDir()])
+    except CommandFailedException, e:
+      if ('Nexus4' in self._builder_name or
+          'NexusS' in self._builder_name or
+          'Xoom'   in self._builder_name):
+        raise BuildStepWarning('skia:2743 RenderPdfs is known to fail on %s: %s'
+                               % (self._builder_name, str(e)))
+      else:
+        raise e
+
 
 if '__main__' == __name__:
   sys.exit(BuildStep.RunBuildStep(RenderPdfs))
