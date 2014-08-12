d77b54b44fbf2150875a47d349e2cb8604ad13db
jcgregorio <jcgregorio@google.com>
2014-08-12 04:04:21 -0700

    Human friendly tick marks.
    Go side implementation, not plumbed through yet to the UI.
    
    BUG=skia:
    R=mtklein@google.com
    
    Author: jcgregorio@google.com
    
    Review URL: https://codereview.chromium.org/459693002

---
+++ perf/go/human/human.go
@@ -0,0 +1,121 @@
+// Package human provides human friendly display formats.
+package human
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/golang/glog"
+)
+
+const MIN_TICKS = 2
+
+// TimeOp is a function that given a time will return a string that would be a
+// good label for that time.
+type TimeOp func(time.Time) string
+
+// choices is the list of hour increments and associated TimeOps for those
+// durations, sorted from largest to smallest hour duration.
+var choices = []struct {
+	Duration time.Duration
+	Op       TimeOp
+}{
+	{24 * 7 * time.Hour, func(t time.Time) string { return t.Format("Jan") }}, // Month
+	{48 * time.Hour, func(t time.Time) string { return t.Format("2") }},       // Day of month
+	{24 * time.Hour, func(t time.Time) string { return t.Format("Mon") }},     // Weekdays
+	{2 * time.Hour, func(t time.Time) string { return t.Format("3pm") }},      // Hours
+}
+
+// opFromHours takes a number of hours and from that returns a function that
+// will produce good labels for tick marks in that time range. For example, if
+// the time range is small enough then the ticks will be marked with the
+// weekday, e.g.  "Sun", if the time range is much larger the ticks may be
+// marked with the month, e.g. "Jul".
+func opFromHours(duration time.Duration) (TimeOp, error) {
+	// Move down the list of choices from the largest granularity to the finest.
+	// The first one that would generate more than MIN_TICKS for the given number
+	// of hours is chosen and that TimeOp is returned.
+	var Op TimeOp = nil
+	for _, c := range choices {
+		if duration > c.Duration {
+			Op = c.Op
+			break
+		}
+	}
+	if Op == nil {
+		return nil, fmt.Errorf("Couldn't calculate TimeOp")
+	}
+	return Op, nil
+}
+
+// Tick represents a single tick mark.
+type Tick struct {
+	X     float64
+	Value string
+}
+
+// ToFlot converts a slice of Ticks into something that will serialize into
+// JSON that Flot consumes, which is an array of 2 element arrays. The 2 element
+// arrays contain an x offset and then a label as a string. For example:
+//
+//   [ [ 0.5, "Saturday" ], [ 1.5, "Sunday" ] ]
+//
+func ToFlot(ticks []*Tick) []interface{} {
+	ret := []interface{}{}
+	for _, t := range ticks {
+		ret = append(ret, []interface{}{t.X, t.Value})
+	}
+	return ret
+}
+
+// TickMarks produces human readable tickmarks to span the given timestamps.
+//
+// The array of timestamps are presumed to be in ascending order.
+// If 'in' is nil then the "Local" time zone is used.
+//
+// The choice of how to label the tick marks is based on the full time
+// range of the timestamps.
+func TickMarks(timestamps []int64, in *time.Location) []*Tick {
+	loc := in
+	if loc == nil {
+		var err error
+		loc, err = time.LoadLocation("Local")
+		if err != nil {
+			loc = time.UTC
+		}
+	}
+	ret := []*Tick{}
+	if len(timestamps) < 2 {
+		return ret
+	}
+
+	begin := time.Unix(timestamps[0], 0).In(loc)
+	end := time.Unix(timestamps[len(timestamps)-1], 0).In(loc)
+	duration := end.Sub(begin)
+	op, err := opFromHours(duration)
+	if err != nil {
+		glog.Errorf("Failed to calculate tickmarks for: %s %s: %s", begin, end, err)
+		return ret
+	}
+	last := op(begin)
+	for i, t := range timestamps {
+		if tickValue := op(time.Unix(t, 0).In(loc)); last != tickValue {
+			last = tickValue
+			ret = append(ret, &Tick{X: float64(i) - 0.5, Value: tickValue})
+		}
+	}
+
+	return ret
+}
+
+// FlotTickMarks returns a struct that will serialize into JSON that Flot
+// expects for a value for tick marks.
+//
+// If an error occurs the tick list will be empty.
+func FlotTickMarks(ts []int64) []interface{} {
+	loc, err := time.LoadLocation("America/New_York")
+	if err != nil {
+		return []interface{}{}
+	}
+	return ToFlot(TickMarks(ts, loc))
+}
---
+++ perf/go/human/human_test.go
@@ -0,0 +1,123 @@
+package human
+
+import (
+	"encoding/json"
+	"testing"
+	"time"
+)
+
+func testTickMarks(t *testing.T, ts []int64, expected []*Tick) {
+	ticks := TickMarks(ts, time.UTC)
+	if got, want := len(ticks), len(expected); got != want {
+		t.Fatalf("Wrong length: Got %v Want %v", got, want)
+	}
+	for i, e := range expected {
+		if got, want := ticks[i].X, e.X; got != want {
+			t.Errorf("X is wrong: Got %v Want %v", got, want)
+		}
+		if got, want := ticks[i].Value, e.Value; got != want {
+			t.Errorf("Value is wrong: Got %v Want %v", got, want)
+		}
+	}
+}
+
+func TestTickMarks(t *testing.T) {
+	// Test Months.
+	ts := []int64{
+		time.Date(2014, 6, 1, 0, 0, 0, 0, time.UTC).Unix(),
+		time.Date(2014, 7, 1, 0, 0, 0, 0, time.UTC).Unix(),
+		time.Date(2014, 7, 2, 0, 0, 0, 0, time.UTC).Unix(),
+		time.Date(2014, 9, 1, 0, 0, 0, 0, time.UTC).Unix(),
+	}
+	expected := []*Tick{
+		{
+			X:     0.5,
+			Value: "Jul",
+		},
+		{
+			X:     2.5,
+			Value: "Sep",
+		},
+	}
+	testTickMarks(t, ts, expected)
+
+	// Test Days.
+	ts = []int64{
+		time.Date(2014, 8, 1, 0, 0, 0, 0, time.UTC).Unix(),
+		time.Date(2014, 8, 3, 0, 0, 0, 0, time.UTC).Unix(),
+		time.Date(2014, 8, 5, 0, 0, 0, 0, time.UTC).Unix(),
+	}
+	expected = []*Tick{
+		{
+			X:     0.5,
+			Value: "3",
+		},
+		{
+			X:     1.5,
+			Value: "5",
+		},
+	}
+	testTickMarks(t, ts, expected)
+
+	// Test Hours.
+	ts = []int64{
+		time.Date(2014, 8, 1, 1, 0, 0, 0, time.UTC).Unix(),
+		time.Date(2014, 8, 1, 2, 0, 0, 0, time.UTC).Unix(),
+		time.Date(2014, 8, 1, 3, 30, 0, 0, time.UTC).Unix(),
+		time.Date(2014, 8, 1, 13, 30, 0, 0, time.UTC).Unix(),
+	}
+	expected = []*Tick{
+		{
+			X:     0.5,
+			Value: "2am",
+		},
+		{
+			X:     1.5,
+			Value: "3am",
+		},
+		{
+			X:     2.5,
+			Value: "1pm",
+		},
+	}
+	testTickMarks(t, ts, expected)
+
+	// Test Weekdays.
+	ts = []int64{
+		time.Date(2014, 8, 1, 1, 0, 0, 0, time.UTC).Unix(),
+		time.Date(2014, 8, 2, 1, 0, 0, 0, time.UTC).Unix(),
+		time.Date(2014, 8, 3, 1, 0, 0, 0, time.UTC).Unix(),
+	}
+	expected = []*Tick{
+		{
+			X:     0.5,
+			Value: "Sat",
+		},
+		{
+			X:     1.5,
+			Value: "Sun",
+		},
+	}
+	testTickMarks(t, ts, expected)
+
+	// Test ToFlot.
+	b, err := json.MarshalIndent(ToFlot(TickMarks(ts, time.UTC)), "", "  ")
+	if err != nil {
+		t.Fatalf("Failed to encode: %s", err)
+	}
+	got := string(b)
+	want := `[
+  [
+    0.5,
+    "Sat"
+  ],
+  [
+    1.5,
+    "Sun"
+  ]
+]`
+
+	if got != want {
+		t.Errorf(": Got %v Want %v", got, want)
+	}
+}
