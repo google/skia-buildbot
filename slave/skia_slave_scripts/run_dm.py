abb160493398e98bcb7acd0776061cd5280ee61e
Mike Klein <mtklein@gmail.com>
2014-08-29 21:38:28 -0400

    okay, last try to get this right..
    BUG=skia:
    
    Review URL: https://codereview.chromium.org/524493003

--- slave/skia_slave_scripts/run_dm.py
+++ slave/skia_slave_scripts/run_dm.py
@@ -41,13 +41,12 @@ class RunDM(BuildStep):
       match.append('~WritePixels')  # skia:1699
 
     if self._AnyMatch('GalaxyNexus'):
-      args.append('~fontcache')  # skia:2900
+      match.append('~fontcache')  # skia:2900
 
     # Though their GPUs are interesting, these don't test anything on
     # the CPU that other ARMv7+NEON bots don't test faster (N5).
     if self._AnyMatch('GalaxyNexus', 'Nexus10', 'Nexus7'):
       args.append('--nocpu')
-
 
     if match:
       args.append('--match')
