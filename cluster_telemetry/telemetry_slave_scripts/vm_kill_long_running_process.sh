c174de70f06705cb637f78ab4418b2bc39adea7b
Ravi Mistry <rmistry@google.com>
2014-09-18 10:04:07 -0400

    Fix for vm_kill_long_running_process.sh

--- cluster_telemetry/telemetry_slave_scripts/vm_kill_long_running_process.sh
+++ cluster_telemetry/telemetry_slave_scripts/vm_kill_long_running_process.sh
@@ -1,5 +1,5 @@
 ps -eo uid,pid,lstart,comm | tail -n+2 | grep timeout |
-     while read PROC_UID PROC_PID PROC_LSTART PROC_CMD; do
+     while read PROC_UID PROC_PID _ _ _ PROC_LSTART PROC_CMD; do
          SECONDS=$[$(date +%s) - $(date -d"$PROC_LSTART" +%s)]
          if [ $SECONDS -gt 2700 ]; then
              echo $PROC_PID
