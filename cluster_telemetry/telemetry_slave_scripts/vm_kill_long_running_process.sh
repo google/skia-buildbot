70dda0d831dd1ae505388fa8f60c328d7a6f6f1c
Ravi Mistry <rmistry@google.com>
2014-10-06 07:17:18 -0400

    Handle negative values in vm_kill_long_running_process.sh

--- cluster_telemetry/telemetry_slave_scripts/vm_kill_long_running_process.sh
+++ cluster_telemetry/telemetry_slave_scripts/vm_kill_long_running_process.sh
@@ -1,7 +1,7 @@
 ps -eo uid,pid,lstart,comm | tail -n+2 | grep timeout |
      while read PROC_UID PROC_PID _ _ _ PROC_LSTART PROC_CMD; do
          SECONDS=$[$(date +%s) - $(date -d"$PROC_LSTART" +%s)]
-         if [ $SECONDS -gt 2700 ]; then
+         if [ ${SECONDS#-} -gt 2700 ]; then
              echo $PROC_PID
          fi
       done |
