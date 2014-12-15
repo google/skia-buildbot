4fb92057f2c4f1793f2f71889d8d1342c826ed64
jcgregorio <jcgregorio@google.com>
2014-12-15 06:32:59 -0800

    Move skia-monitoring logserver over to being managed by Skia Push.
    BUG=skia:
    
    Review URL: https://codereview.chromium.org/800103004

--- compute_engine_scripts/monitoring/setup.sh
+++ compute_engine_scripts/monitoring/setup.sh
@@ -38,14 +38,18 @@ fi
 export PATH=$PATH:$(pwd)/${NODE_VERSION}/bin
 
 # Build applications
-go get -u skia.googlesource.com/buildbot.git/perf/go/logserver \
-  skia.googlesource.com/buildbot.git/monitoring/go/grains \
+go get -u skia.googlesource.com/buildbot.git/monitoring/go/grains \
   skia.googlesource.com/buildbot.git/monitoring/go/prober \
   skia.googlesource.com/buildbot.git/monitoring/go/alertserver
 
 # Install InfluxDB.
 wget http://s3.amazonaws.com/influxdb/influxdb_latest_amd64.deb
 sudo dpkg -i influxdb_latest_amd64.deb
+
+# Install pull.
+# Temporary step to bootstrap monitoring using Skia Push.
+gsutil cp gs://skia-push/debs/pull/pull:jcgregorio@jcgregorio.cnc.corp.google.com:2014-12-15T14:12:52Z:6152bc3bcdaa54989c957809e77bed282c35676b.deb pull.deb
+sudo dpkg -i pull.deb
 
 # Install Grafana.
 cd
@@ -93,7 +97,6 @@ mkdir -p /mnt/pd0/alertserver_scratch
 # Now that the default installs are in place, overwrite the installs with our
 # custom config files.
 cd ~/buildbot/compute_engine_scripts/monitoring/
-sudo install $ROOT_PARAMS $EXE_FILE mlogserver_init /etc/init.d/logserver
 sudo install $PARAMS $CONFIG_FILE influxdb-config.toml /opt/influxdb/shared/config.toml
 sudo install $PARAMS $CONFIG_FILE bashrc /home/default/.bashrc
 sudo install $PARAMS $CONFIG_FILE grafana-config.js /home/default/grafana/config.js
@@ -109,7 +112,6 @@ sudo monit -t
 sudo monit reload
 
 sudo /etc/init.d/influxdb restart
-sudo /etc/init.d/logserver restart
 sudo /etc/init.d/grains restart
 sudo /etc/init.d/prober restart
 sudo /etc/init.d/collectd restart
