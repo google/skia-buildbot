439f4e9c7769cc0220a85e2f7957fc294b40b0c1
jcgregorio <jcgregorio@google.com>
2014-12-22 07:38:20 -0800

    Add certpoller.
    An application to keep certs up to date with their source of truth, which is stored in GCE project level metadata.
    
    BUG=skia:
    
    Review URL: https://codereview.chromium.org/817303002

---
+++ nginx/go/certpoller/main.go
@@ -0,0 +1,146 @@
+// certpoller polls the current set of nginx SSL certs for this machine in Google Compute
+// Project level metadata and updates the local copies if they change.
+//
+// Presumes that the project metadata keys map to file names in the following way:
+//
+//  skiamonitor-com-key    ->   /etc/nginx/ssl/skiamonitor_com.key
+//
+// Run by passing in all the metadata keys on the command line:
+//
+//    $ certpoller skiamonitor-com-key skiaalerts-com-key
+//
+package main
+
+import (
+	"bytes"
+	"crypto/md5"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os/exec"
+	"strings"
+	"time"
+
+	"github.com/golang/glog"
+	"skia.googlesource.com/buildbot.git/go/common"
+	"skia.googlesource.com/buildbot.git/go/util"
+)
+
+var (
+	graphiteServer = flag.String("graphite_server", "skiamonitor.com:2003", "Where is Graphite metrics ingestion server running.")
+)
+
+// cert contains information about one SSL certificate file.
+type cert struct {
+	metadata string // The name of the cert in GCE project level metadata.
+	file     string // The local filename of the cert.
+	etag     string // The etag of the cert when last retrieved from GCE project level metadata.
+}
+
+// fileFromMetadata turns a metadata key name into a local filename.
+//
+// For example:
+//    skiamonitor-com-key    ->   /etc/nginx/ssl/skiamonitor_com.key
+func fileFromMetadata(metadata string) string {
+	return "/etc/nginx/ssl/" + strings.Replace(strings.Replace(metadata, "-", "_", 1), "-", ".", 1)
+}
+
+// md5File returns the md5File hash of the given file.
+func md5File(filename string) string {
+	cmd := exec.Command("sudo", "md5sum", filename)
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		// We are OK to Fatal here because md5s will only be checked on startup.
+		glog.Fatalf("Failed to calculate md5sum for %s: %s", filename, err)
+	}
+	return strings.Split(out.String(), " ")[0]
+}
+
+// get retrieves the metadata file if it's changed and writes it to the correct location.
+func get(client *http.Client, cert *cert) error {
+	// We aren't using the metadata package here because we need to set/get etags.
+	r, err := http.NewRequest("GET", "http://metadata/computeMetadata/v1/project/attributes/"+cert.metadata, nil)
+	if err != nil {
+		return fmt.Errorf("Failed to create request for metadata: %s", err)
+	}
+	r.Header.Set("Metadata-Flavor", "Google")
+	if cert.etag != "" {
+		r.Header.Set("If-None-Match", cert.etag)
+	}
+	resp, err := client.Do(r)
+	if err != nil {
+		return fmt.Errorf("Failed to retrieve metadata for %s: %s", cert.metadata, err)
+	}
+	if resp.StatusCode != 200 {
+		if cert.etag != "" && resp.StatusCode == 304 {
+			// The etag is set and matches what we've already seen, so the file is
+			// unchanged. Note that this can't happen the first time get() is called
+			// for each file as etag won't be set, so we'll fall through to the code
+			// below in that case.
+			glog.Infof("etag unchanged for %s: %s", cert.file, cert.etag)
+			return nil
+		} else {
+			return fmt.Errorf("Unexpected status response: %d: %s", resp.StatusCode, resp.Status)
+		}
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return fmt.Errorf("Failed to read metadata for %s: %s", cert.metadata, err)
+	}
+	glog.Infof("Read body for %s: len %d", cert.file, len(body))
+
+	// Store the etag to be used for the next time through get().
+	cert.etag = resp.Header.Get("ETag")
+
+	newMD5Hash := fmt.Sprintf("%x", md5.Sum(body))
+	if cert.etag != "" || newMD5Hash != md5File(cert.file) {
+		// Write out the file to a temp file then sudo mv it over to the right location.
+		f, err := ioutil.TempFile("", "certpoller")
+		if err != nil {
+			glog.Errorf("Failed to create tmp cert file for %s: %s", cert.metadata, err)
+		}
+		n, err := f.Write(body)
+		if err != nil || n != len(body) {
+			return fmt.Errorf("Failed to write cert len(body)=%d, n=%d: %s", len(body), n, err)
+		}
+		tmpName := f.Name()
+		f.Close()
+		cmd := exec.Command("sudo", "mv", tmpName, cert.file)
+		err = cmd.Run()
+		if err != nil {
+			return fmt.Errorf("Failed to mv certfile into place for %s: %s", cert.metadata, err)
+		}
+	}
+	return nil
+}
+
+func main() {
+	common.InitWithMetrics("certpoller", *graphiteServer)
+	client := util.NewTimeoutClient()
+	certs := []*cert{}
+	// Populate certs based on cmd-line args.
+	for _, metadata := range flag.Args() {
+		c := &cert{
+			metadata: metadata,
+			file:     fileFromMetadata(metadata),
+			etag:     "",
+		}
+		err := get(client, c)
+		if err != nil {
+			glog.Fatalf("Failed to retrieve the cert %s: %s", c, err)
+		}
+		certs = append(certs, c)
+	}
+
+	for _ = range time.Tick(30 * time.Minute) {
+		for _, c := range certs {
+			if err := get(client, c); err != nil {
+				glog.Errorf("Failed to update cert %s: %s", c.metadata, err)
+			}
+		}
+	}
+}
