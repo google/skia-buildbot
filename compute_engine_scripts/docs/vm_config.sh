1a1a6e15fe0564463f967c1dadf00fb5c5dea9d1
jcgregorio <jcgregorio@google.com>
2015-01-02 09:33:07 -0800

    Standing up the skiadocs server.
    BUG=skia:
    TBR=rmistry
    
    Review URL: https://codereview.chromium.org/804773005

---
+++ compute_engine_scripts/docs/vm_config.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# This file contains constants for the shell scripts which interact
+# with the skia-docs Google Compute Engine instance.
+#
+# Copyright 2014 Google Inc. All Rights Reserved.
+
+# Sets all constants in compute_engine_cfg.py as env variables.
+$(python ../compute_engine_cfg.py)
+if [ $? != "0" ]; then
+  echo "Failed to read compute_engine_cfg.py!"
+  exit 1
+fi
+
+# The base names of the VM instances. Actual names are VM_NAME_BASE-name
+VM_NAME_BASE=${VM_NAME_BASE:="skia"}
+
+# The name of instance where skia docs is running on.
+INSTANCE_NAME=${VM_NAME_BASE}-docs
+
+DOCS_IP_ADDRESS=104.154.112.101
+
---
+++ compute_engine_scripts/docs/vm_create_instance.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Creates the compute instance for skiadocs.
+#
+set -x
+
+source vm_config.sh
+
+# Create a boot disk from the pushable base snapshot.
+gcloud compute --project $PROJECT_ID disks create $INSTANCE_NAME \
+  --zone $ZONE \
+  --source-snapshot skia-pushable-base \
+  --type "pd-standard"
+
+gcloud compute --project $PROJECT_ID instances create $INSTANCE_NAME \
+  --zone $ZONE \
+  --machine-type n1-standard-1 \
+  --network "default" \
+  --maintenance-policy "MIGRATE" \
+  --scopes $SCOPES \
+  --tags "http-server" "https-server" \
+  --disk "name="$INSTANCE_NAME "device-name="$INSTANCE_NAME "mode=rw" "boot=yes" "auto-delete=yes" \
+  --metadata-from-file "startup-script=startup-script.sh" \
+  --address $DOCS_IP_ADDRESS
