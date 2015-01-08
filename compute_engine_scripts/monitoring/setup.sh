2c7a63565a6920ecca67d58b7f3d1cdef5b2e453
jcgregorio <jcgregorio@google.com>
2015-01-08 10:46:23 -0800

    Everything except nginx is now managed by push on skia-monitoring
    BUG=skia:
    
    Review URL: https://codereview.chromium.org/841093002

--- compute_engine_scripts/monitoring/setup.sh
+++ compute_engine_scripts/monitoring/setup.sh
@@ -13,43 +13,10 @@ EXE_FILE="--mode=755"
 CONFIG_FILE="--mode=666"
 MONIT_CONFIG_FILE="--mode=600"
 
-# Install Go
-if [ -d go ]; then
-  echo Go already installed.
-else
-  wget https://storage.googleapis.com/golang/go1.3.3.linux-amd64.tar.gz
-  tar -xzf go1.3.3.linux-amd64.tar.gz
-fi
-
-mkdir=$HOME/golib
-export GOROOT=$HOME/go
-export GOPATH=$HOME/golib
-export PATH=$PATH:$GOROOT/bin
-
-# Install Node
-NODE_VERSION="node-v0.10.33-linux-x64"
-if [ -d ${NODE_VERSION} ]; then
-  echo Node already installed.
-else
-  wget http://nodejs.org/dist/v0.10.33/${NODE_VERSION}.tar.gz
-  tar xzf ${NODE_VERSION}.tar.gz
-fi
-
-export PATH=$PATH:$(pwd)/${NODE_VERSION}/bin
-
 # Install pull.
 # Temporary step to bootstrap monitoring using Skia Push.
 gsutil cp gs://skia-push/debs/pull/pull:jcgregorio@jcgregorio.cnc.corp.google.com:2014-12-15T14:12:52Z:6152bc3bcdaa54989c957809e77bed282c35676b.deb pull.deb
 sudo dpkg -i pull.deb
-
-# Install Grafana.
-cd
-sudo rm -rf grafana
-GRAFANA=grafana-1.8.1
-wget http://grafanarel.s3.amazonaws.com/${GRAFANA}.tar.gz
-tar -xzf ${GRAFANA}.tar.gz
-rm ${GRAFANA}.tar.gz
-mv $GRAFANA grafana
 
 # Add the nginx configuration files.
 cd ~/buildbot/compute_engine_scripts/monitoring/
