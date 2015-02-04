53fd1adf0b3515c5e2f881d8add39fd38bead4c8
mtklein <mtklein@chromium.org>
2015-02-04 12:26:06 -0800

    export DISPLAY=:0 on bots
    The environment seems to have lost this when upgrading to 14.04.
    Did we always just run the slave scripts from a GUI terminal?
    
    This won't hurt Macs or non-GPU programs.
    
    Before update:
    http://build.chromium.org/p/client.skia/builders/Perf-Ubuntu12-ShuttleA-GTX660-x86-Release-Trybot/builds/4/steps/nanobench/logs/stdio
    
    Busted:
    http://build.chromium.org/p/client.skia/builders/Perf-Ubuntu12-ShuttleA-GTX660-x86-Release-Trybot/builds/5/steps/nanobench/logs/stdio
    
    Fixed:
    http://build.chromium.org/p/client.skia/builders/Perf-Ubuntu12-ShuttleA-GTX660-x86-Release-Trybot/builds/7/steps/nanobench/logs/stdio
    
    TBR=
    
    BUG=skia:
    
    Review URL: https://codereview.chromium.org/867623004

--- scripts/skiabot-slave-start-on-boot.sh
+++ scripts/skiabot-slave-start-on-boot.sh
@@ -5,6 +5,7 @@ fi
 mkdir -p $SKIA_REPO_DIR
 cd $SKIA_REPO_DIR
 export PATH="$PATH:$HOME/depot_tools"
+export DISPLAY=:0
 echo "SKIA_REPO_DIR: $SKIA_REPO_DIR"
 echo "PATH: $PATH"
 gclient config --spec='solutions = [{ "name": "buildbot","url": "https://skia.googlesource.com/buildbot.git","deps_file": "DEPS","managed": True,"custom_deps": {},"safesync_url": "",},{ "name": "src","url": "https://chromium.googlesource.com/chromium/src.git","deps_file": ".DEPS.git","managed": True,"custom_deps": {},"safesync_url": "",},]'
