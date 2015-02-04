890e1ba92b55516d2ef9510da60cc4742744a68a
Greg Humphreys <humper@gmail.com>
2015-02-04 15:06:17 -0500

    even more fiddle migration fixes
    BUG=skia:
    
    Review URL: https://codereview.chromium.org/899953003

--- webtry/setup/setup_jail.sh
+++ webtry/setup/setup_jail.sh
@@ -4,10 +4,11 @@ apt-get upgrade
 apt-get install -y g++ libfreetype6 libfreetype6-dev libpng12-0 libpng12-dev \
 libglu1-mesa-dev mesa-common-dev freeglut3-dev libgif-dev libfontconfig \
 libfontconfig-dev git python wget libpoppler-cpp-dev libosmesa6-dev \
-ttf-mscorefonts-installer
+ttf-mscorefonts-installer git
 
 mkdir /skia_build
 chmod 777 /skia_build
 
 mkdir /skia_build/bin
 mkdir /skia_build/fiddle_main
+mkdir /skia_build/scripts
