4177ef385f876e1ab11e1865f3371e99e27ff444
jcgregorio <jcgregorio@google.com>
2015-02-05 12:02:00 -0800

    Add a plain text image file format.
    This is the first CL in a series of CLs to first tighten up the testing on our image diffing algorithm and then to change over the diffing algorithm to one that shows the magnitude of the difference in pixels.
    
    BUG=skia:
    
    Review URL: https://codereview.chromium.org/897293002

---
+++ golden/go/image/text/text.go
@@ -0,0 +1,141 @@
+// text is an image plain text file format encoder and decoder.
+//
+// A super simple format of the form:
+//
+// ! SKTEXTSIMPLE
+// width height
+// 0x000000ff 0xffffffff ...
+// 0xddddddff 0xffffff88 ...
+// ...
+//
+// Where the pixel values are encoded as 0xRRGGBBAA.
+package text
+
+import (
+	"bufio"
+	"fmt"
+	"image"
+	"image/color"
+	"io"
+	"strconv"
+	"strings"
+)
+
+const skTextHeader = "! SKTEXTSIMPLE\n"
+
+// dim returns the dimensions of the image.
+func dim(reader *bufio.Reader) (int, int, error) {
+	line, err := reader.ReadString('\n')
+	if err != nil {
+		return 0, 0, fmt.Errorf("Failed to read header from SKTEXT file: %s", err)
+	}
+	if line != skTextHeader {
+		return 0, 0, fmt.Errorf("Not a valid SKTEXT file: %q %q", line, skTextHeader)
+	}
+	line, err = reader.ReadString('\n')
+	if err != nil && err != io.EOF {
+		return 0, 0, fmt.Errorf("Failed to read dimenstions from SKTEXT file: %s", err)
+	}
+	width := 0
+	height := 0
+	n, err := fmt.Sscanf(line, "%d %d", &width, &height)
+	if err != nil {
+		return 0, 0, fmt.Errorf("Not a valid SKTEXT file: %s", err)
+	}
+	if n != 2 {
+		return 0, 0, fmt.Errorf("Not a valid SKTEXT file, couldn't find width and height.")
+	}
+	return width, height, nil
+}
+
+// Decode reads an SKTEXT image from r and returns it as an image.Image.
+// The type of Image returned will always be NRGBA.
+func Decode(r io.Reader) (image.Image, error) {
+	reader := bufio.NewReader(r)
+	width, height, err := dim(reader)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to decode SKTEXT config: %s", err)
+	}
+	ret := image.NewNRGBA(image.Rect(0, 0, width, height))
+	lineNum := 0
+	for {
+		if lineNum > height {
+			return nil, fmt.Errorf("Too many y values: %d > %d", lineNum, height)
+		}
+		line, err := reader.ReadString('\n')
+		if err != nil && err != io.EOF {
+			break
+		}
+		hexline := strings.Split(line, " ")
+		if len(hexline) > width && len(hexline[0]) > 0 {
+			return nil, fmt.Errorf("Too many x values: %d > %d", len(hexline), width)
+		}
+		for i, h := range hexline {
+			h = strings.TrimSpace(h)
+			if h != "" {
+				if !strings.HasPrefix(h, "0x") || len(h) != 10 {
+					return nil, fmt.Errorf("Invalid pixel format, must be 0xRRGGBBAA, got %q", h)
+				}
+				rgba, err := strconv.ParseUint(strings.TrimSpace(h), 0, 32)
+				if err != nil {
+					return nil, err
+				}
+				offset := lineNum*ret.Stride + i*4
+				ret.Pix[offset+0] = uint8((rgba >> 24) & 0xff)
+				ret.Pix[offset+1] = uint8((rgba >> 16) & 0xff)
+				ret.Pix[offset+2] = uint8((rgba >> 8) & 0xff)
+				ret.Pix[offset+3] = uint8((rgba >> 0) & 0xff)
+			}
+		}
+		lineNum += 1
+		if err != nil {
+			break
+		}
+	}
+	if err == nil || err == io.EOF {
+		return ret, nil
+	}
+	return nil, fmt.Errorf("Failed reading SKTEXT file contents: %s", err)
+}
+
+// DecodeConfig returns the color model and dimensions of SKTEXT image without
+// decoding the entire image.
+func DecodeConfig(r io.Reader) (image.Config, error) {
+	reader := bufio.NewReader(r)
+	width, height, err := dim(reader)
+	if err != nil {
+		return image.Config{}, fmt.Errorf("Failed to Decode SKTEXT file: %s", err)
+	}
+
+	return image.Config{
+		ColorModel: color.NRGBAModel,
+		Width:      width,
+		Height:     height,
+	}, nil
+}
+
+// Encode encoded the image in SKTEXT format.
+func Encode(w io.Writer, m *image.NRGBA) error {
+	fmt.Fprintf(w, "%s%d %d\n", skTextHeader, m.Rect.Dx(), m.Rect.Dy())
+	height := m.Bounds().Dy()
+	for i := 0; i < len(m.Pix); i += 4 {
+		_, err := fmt.Fprintf(w, "0x%02x%02x%02x%02x", m.Pix[i+0], m.Pix[i+1], m.Pix[i+2], m.Pix[i+3])
+		if err != nil {
+			return err
+		}
+		// Add whitespace.
+		if (i > 0 || m.Stride == 4) && (i+4)%m.Stride == 0 {
+			// Don't add a trailing \n to the very last line.
+			if (i+4)/m.Stride < height {
+				fmt.Fprintln(w)
+			}
+		} else {
+			fmt.Fprint(w, " ")
+		}
+	}
+	return nil
+}
+
+func init() {
+	image.RegisterFormat("sktext", skTextHeader, Decode, DecodeConfig)
+}
---
+++ golden/go/image/text/text_test.go
@@ -0,0 +1,142 @@
+package text
+
+import (
+	"bytes"
+	"image"
+	"testing"
+)
+
+const IMAGE = `! SKTEXTSIMPLE
+2 2
+0x112233ff 0xffffffff
+0xddeeff00 0xffffff88`
+
+func TestDecoder(t *testing.T) {
+	buf := bytes.NewBufferString(IMAGE)
+	img, err := Decode(buf)
+	if err != nil {
+		t.Fatalf("Failed to decode a valid image: %s", err)
+	}
+
+	if got, want := img.Bounds().Dx(), 2; got != want {
+		t.Errorf("Wrong x dim: Got %v Want %v", got, want)
+	}
+	if got, want := img.Bounds().Dy(), 2; got != want {
+		t.Errorf("Wrong y dim: Got %v Want %v", got, want)
+	}
+	nrgba := img.(*image.NRGBA)
+
+	testCases := []struct {
+		x, y       int
+		r, g, b, a uint8
+	}{
+		{
+			x: 0, y: 0,
+			r: 0x11, g: 0x22, b: 0x33, a: 0xff,
+		},
+		{
+			x: 1, y: 0,
+			r: 0xff, g: 0xff, b: 0xff, a: 0xff,
+		},
+		{
+			x: 0, y: 1,
+			r: 0xdd, g: 0xee, b: 0xff, a: 0x00,
+		},
+		{
+			x: 1, y: 1,
+			r: 0xff, g: 0xff, b: 0xff, a: 0x88,
+		},
+	}
+	for _, tc := range testCases {
+		c := nrgba.NRGBAAt(tc.x, tc.y)
+		if got, want := c.R, uint8(tc.r); got != want {
+			t.Errorf("Wrong r channel value: Got %x Want %x", got, want)
+		}
+		if got, want := c.G, uint8(tc.g); got != want {
+			t.Errorf("Wrong g channel value: Got %x Want %x", got, want)
+		}
+		if got, want := c.B, uint8(tc.b); got != want {
+			t.Errorf("Wrong b channel value: Got %x Want %x", got, want)
+		}
+		if got, want := c.A, uint8(tc.a); got != want {
+			t.Errorf("Wrong a channel value: Got %x Want %x", got, want)
+		}
+	}
+}
+
+const ZERO_IMAGE = `! SKTEXTSIMPLE
+0 0
+`
+
+func TestDecoderDegenerate(t *testing.T) {
+	buf := bytes.NewBufferString(ZERO_IMAGE)
+	img, err := Decode(buf)
+	if err != nil {
+		t.Fatalf("Failed to decode a valid image: %s", err)
+	}
+	if got, want := img.Bounds().Dx(), 0; got != want {
+		t.Errorf("Wrong x dim: Got %v Want %v", got, want)
+	}
+	if got, want := img.Bounds().Dy(), 0; got != want {
+		t.Errorf("Wrong y dim: Got %v Want %v", got, want)
+	}
+}
+
+const BAD_IMAGE_1 = ``
+
+const BAD_IMAGE_2 = `! SKTEXTBAD
+0 0`
+
+const BAD_IMAGE_3 = `! SKTEXTSIMPLE
+1 1
+0x112233ff 0xffffffff
+0xddeeff00 0xffffff88`
+
+const BAD_IMAGE_4 = `! SKTEXTSIMPLE
+2 2
+0x11       0xffffffff
+0xddeeff00 0xffffff88`
+
+const BAD_IMAGE_5 = `! SKTEXTSIMPLE
+2 2
+  112233ff 0xffffffff
+0xddeeff00 0xffffff88`
+
+func TestDecoderFails(t *testing.T) {
+	for _, tc := range []string{BAD_IMAGE_1, BAD_IMAGE_2, BAD_IMAGE_3, BAD_IMAGE_4, BAD_IMAGE_5} {
+		buf := bytes.NewBufferString(tc)
+		_, err := Decode(buf)
+		if err == nil {
+			t.Fatalf("Decodede an invalid image: %s", tc)
+		}
+	}
+}
+
+const NON_SQUARE_IMAGE = `! SKTEXTSIMPLE
+2 3
+0x112233ff 0xffffffff
+0xddeeff00 0xffffff88
+0x001100ff 0x11001188`
+
+const NON_SQUARE_IMAGE_2 = `! SKTEXTSIMPLE
+1 3
+0x112233ff
+0xddeeff00
+0x001100ff`
+
+func TestRoundTrip(t *testing.T) {
+	for _, tc := range []string{ZERO_IMAGE, IMAGE, NON_SQUARE_IMAGE, NON_SQUARE_IMAGE_2} {
+		buf := bytes.NewBufferString(tc)
+		img, err := Decode(buf)
+		if err != nil {
+			t.Fatalf("Failed to decode a valid image: %s", tc)
+		}
+		wbuf := &bytes.Buffer{}
+		if err := Encode(wbuf, img.(*image.NRGBA)); err != nil {
+			t.Fatalf("Decodede an encode a valid image: %s", tc)
+		}
+		if got, want := wbuf.String(), tc; got != want {
+			t.Errorf("Roundtrip mismatch: Got %q Want %q", got, want)
+		}
+	}
+}
