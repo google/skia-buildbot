02f5da04b6082462adf07d2c29dbb36d88197047
rmistry <rmistry@google.com>
2015-03-03 04:53:12 -0800

    Make TEMPLATE_DIRS a tuple to make newer version of django happy
    BUG=skia:3465
    
    Review URL: https://codereview.chromium.org/968223003

--- ct/py/csv-django-settings.py
+++ ct/py/csv-django-settings.py
@@ -6,7 +6,7 @@
 import os
 
 
-TEMPLATE_DIRS = os.path.join(os.path.dirname(os.path.realpath(__file__)),
-                             'html-templates')
+TEMPLATE_DIRS = (os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                              'html-templates'),)
 
 SECRET_KEY = '*lk^6@0l0(iulgar$j)fbvfy&^(^u+qk3j73d18@&+ur^xuTxY'
