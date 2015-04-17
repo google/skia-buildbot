6664931cef9ce6a5026fb3a484ad28b01be49617
halcanary <halcanary@google.com>
2015-04-17 12:58:27 -0700

    go/pdf: add build_pdfium script
    Land this before http://crrev.com/1088373002
    
    BUG=skia:3732
    
    Review URL: https://codereview.chromium.org/1097543002

---
+++ go/pdf/build_pdfium.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+if [ -z "$1" ] || [ -z "$2" ] ; then
+    echo 'usage: build_pdfium.sh TARGET_DIRECTORY PDFIUM_BUILD_DIRECTORY' >&2
+    exit 1
+fi
+
+set -x -e
+
+REPO="https://pdfium.googlesource.com/pdfium"
+COMMIT="c7a3f3c0e2652130cf62f0a937dd21f15d19882f"
+
+TARGET_DIRECTORY="$1"
+PDFIUM_BUILD_DIRECTORY="$2"
+
+mkdir -p "$PDFIUM_BUILD_DIRECTORY"
+cd "$PDFIUM_BUILD_DIRECTORY"
+
+if ! [ -d pdfium ]; then
+    git clone "$REPO"
+fi
+
+cd pdfium
+
+if ! [ -f ".gclient" ] ; then
+    gclient config --name . --unmanaged "$REPO"
+fi
+
+if [ "$(git diff --shortstat)" ] ; then
+    git stash save
+fi
+
+if [ "$(git rev-parse HEAD)" != "$COMMIT" ]; then
+    git fetch
+    git checkout "$COMMIT"
+fi
+
+if [ "$(git hash-object DEPS)" != "$(git config sync-deps.last)" ] ; then
+    gclient sync
+    git config sync-deps.last "$(git hash-object DEPS)"
+fi
+
+GYP_GENERATORS=ninja build/gyp_pdfium
+
+ninja -C out/Release pdfium_test
+
+mkdir -p "$TARGET_DIRECTORY"
+
+cp -a out/Release/pdfium_test "${TARGET_DIRECTORY}/pdfium_test"
