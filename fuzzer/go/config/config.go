8d2f90ed813fa7aca245b626b898067d96206b82
jcgregorio <jcgregorio@google.com>
2015-04-28 09:39:51 -0700

    Revert of Part 1 of fuzzer land; skeleton for selecting which fuzz to run and (patchset #5 id:80001 of https://codereview.chromium.org/1084723002/)
    Reason for revert:
    Failed go error check:
    
    http://build.chromium.org/p/client.skia.fyi/builders/Infra-PerCommit-Trybot/builds/428/steps/run_unittests/logs/stdio
    
    Original issue's description:
    > Part 1 of fuzzer land; skeleton for selecting which fuzz to run and
    > building CPP template in GO instead of script
    >
    > BUG=skia:
    > R=jcgregorio@google.com
    > TBR=jcgregorio
    >
    > Committed: https://skia.googlesource.com/buildbot/+/1c2a9098380542794bcba3feffc624397e0cdc78
    
    TBR=humper@google.com
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=skia:
    
    Review URL: https://codereview.chromium.org/1108383002

--- fuzzer/go/config/config.go
+++ fuzzer/go/config/config.go
@@ -1,29 +1,11 @@
 package config
 
-type Common struct {
-	ResourcePath string
-}
-
 type FrontEnd struct {
 	Port           string
+	ResourcePath   string
 	GraphiteServer string
 }
 
-type Fuzzer struct {
-	CachePath     string
-	Indentation   int
-	SkiaSourceDir string
-}
-
-type Generator struct {
-	Weight      int
-	Debug       bool
-	ExtraParams map[string]string
-}
-
-var Config struct {
-	Common     Common
-	FrontEnd   FrontEnd
-	Fuzzer     Fuzzer
-	Generators map[string]Generator
+var Fuzzer struct {
+	FrontEnd FrontEnd
 }
