b1f6b1b55c05224b6730e91f6e5f669710324bdd
jcgregorio <jcgregorio@google.com>
2015-05-15 09:57:50 -0700

    sksysmon: Now return a real list of Units from systemd.
    Screenshot running on a Ubuntu 15.04 server in GCE:
    
      https://screenshot.googleplex.com/HW8vWvQ2p5D.png
    
    BUG=skia:
    
    Review URL: https://codereview.chromium.org/1142643003

--- sksysmon/go/sksysmon/main.go
+++ sksysmon/go/sksysmon/main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"html/template"
 	"net/http"
 	"path/filepath"
 	"runtime"
+	"strings"
 
+	"github.com/coreos/go-systemd/dbus"
 	"github.com/gorilla/mux"
 	"github.com/skia-dev/glog"
 	"go.skia.org/infra/go/common"
@@ -16,6 +19,9 @@ import (
 var (
 	// indexTemplate is the main index.html page we serve.
 	indexTemplate *template.Template = nil
+
+	// dbc is the dbus connection we use to talk to systemd.
+	dbc *dbus.Conn
 )
 
 // flags
@@ -38,6 +44,56 @@ func loadResouces() {
 	))
 }
 
+func Init() {
+	var err error
+	dbc, err = dbus.New()
+	if err != nil {
+		glog.Fatalf("Failed to initialize dbus: %s", err)
+	}
+	loadResouces()
+}
+
+// serviceOnly returns only units that are services.
+func serviceOnly(units []dbus.UnitStatus) []dbus.UnitStatus {
+	ret := []dbus.UnitStatus{}
+	for _, u := range units {
+		if strings.HasSuffix(u.Name, ".service") {
+			ret = append(ret, u)
+		}
+	}
+	return ret
+}
+
+// listHandler returns the list of units.
+func listHandler(w http.ResponseWriter, r *http.Request) {
+	glog.Infof("List Handler: %q\n", r.URL.Path)
+	units, err := dbc.ListUnits()
+	if err != nil {
+		if *local {
+			// If running locally the above will fail because we aren't on systemd
+			// yet, so return some dummy data.
+			units = []dbus.UnitStatus{
+				dbus.UnitStatus{
+					Name:     "test.service",
+					SubState: "running",
+				},
+				dbus.UnitStatus{
+					Name:     "something.service",
+					SubState: "halted",
+				},
+			}
+		} else {
+			util.ReportError(w, r, err, "Failed to list units.")
+			return
+		}
+	}
+	units = serviceOnly(units)
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(units); err != nil {
+		util.ReportError(w, r, err, "Failed to encode response.")
+	}
+}
+
 // mainHandler handles the GET of the main page.
 func mainHandler(w http.ResponseWriter, r *http.Request) {
 	glog.Infof("Main Handler: %q\n", r.URL.Path)
@@ -54,11 +110,12 @@ func mainHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	common.InitWithMetrics("sksysmon", graphiteServer)
-	loadResouces()
+	Init()
 
 	r := mux.NewRouter()
 	r.PathPrefix("/res/").HandlerFunc(util.MakeResourceHandler(*resourcesDir))
 	r.HandleFunc("/", mainHandler).Methods("GET")
+	r.HandleFunc("/_/list", listHandler).Methods("GET")
 	http.Handle("/", util.LoggingGzipRequestResponse(r))
 	glog.Infoln("Ready to serve.")
 	glog.Fatal(http.ListenAndServe(*port, nil))
