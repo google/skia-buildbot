6de682af760ec5ff514bc419c379820b85416e21
Ravi Mistry <rmistry@google.com>
2015-07-14 07:11:07 -0400

    Capture CT archives from 11500-12999

--- ct/go/worker_scripts/capture_archives/main.go
+++ ct/go/worker_scripts/capture_archives/main.go
@@ -85,7 +85,7 @@ func main() {
 	}
 	// TODO(rmistry): Remove this hack once the 1M webpage archives have been captured.
 	glog.Infof("The length of fileInfos is: %s", len(fileInfos))
-	fileInfos = fileInfos[10000:11500]
+	fileInfos = fileInfos[11500:13000]
 	glog.Infof("The fileInfos are: %s", fileInfos)
 	for _, fileInfo := range fileInfos {
 		pagesetBaseName := filepath.Base(fileInfo.Name())
