ac6c1292955c888d35f6c01cf97d3b6ff4029424
rmistry <rmistry@google.com>
2015-08-15 11:08:22 -0700

    Revert of Add a library for running external commands, providing timeouts and test injection. (patchset #2 id:40001 of https://codereview.chromium.org/1290913004/ )
    Reason for revert:
    CT Chromium Perf tasks are failing with:
    
    E0815 11:07:22.649344   13091 exec.go:193] Command exited with invalid argument: python /b/skia-repo/go/src/go.skia.org/infra/ct/py/sync_skia_in_chrome.py --destination=/b/storage/chromium_builds/linux_base --fetch_target=chromium --chrome_revision=6ae54795f6d077e31f4fb42540ead46f4141f2bb --skia_revision=fb28cd2b13d76c324570341e1155aaadc1b532b6
    
    Original issue's description:
    > Add a library for running external commands, providing timeouts and test injection.
    >
    > BUG=skia:4106
    >
    > Committed: https://skia.googlesource.com/buildbot/+/b65fde6749a3e060938cd280c7e281e29516edc1
    
    TBR=jcgregorio@google.com,benjaminwagner@google.com
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    BUG=skia:4106
    
    Review URL: https://codereview.chromium.org/1295043002

--- go/util/command.go
+++ go/util/command.go
@@ -1,12 +1,31 @@
 package util
 
 import (
-	"go.skia.org/infra/go/exec"
+	"os/exec"
+	"strings"
+
+	"github.com/skia-dev/glog"
 )
 
 // DoCmd executes the given command line string; the command being
 // run is expected to not care what its current working directory is.
-// Returns the stdout and stderr.
+// Returns the stdout and stderr.  If there is an error, the
+// returned error will be of type ExitError, which the caller
+// can use to find out more about what happened.
 func DoCmd(commandLine string) (string, error) {
-	return exec.RunSimple(commandLine)
+	glog.Infof("Command: %q\n", commandLine)
+	programAndArgs := strings.SplitN(commandLine, " ", 2)
+	program := programAndArgs[0]
+	args := []string{}
+	if len(programAndArgs) > 1 {
+		args = strings.Split(programAndArgs[1], " ")
+	}
+	cmd := exec.Command(program, args...)
+	message, err := cmd.CombinedOutput()
+	glog.Infof("StdOut + StdErr: %s\n", string(message))
+	if err != nil {
+		glog.Errorf("Exit status: %s\n", err)
+		return string(message), err
+	}
+	return string(message), nil
 }
