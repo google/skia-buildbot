720010ecee583b088e6a11f0b0f212f5f9cfa79e
borenet <borenet@google.com>
2015-08-25 05:18:05 -0700

    Add GCE instance for datahopper
    BUG=skia:4242
    
    Review URL: https://codereview.chromium.org/1309273002

---
+++ compute_engine_scripts/datahopper/README.md
@@ -0,0 +1,8 @@
+Skia Datahopper
+===============
+
+Instance creation and deletion scripts for Datahopper.
+
+Note that there aren't any setup or push scripts as all of that is handled by
+Skia Push. See ../../push/DESIGN.md.
+
---
+++ compute_engine_scripts/datahopper/vm_create_instance.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+#
+# Creates the compute instance for skia-datahopper
+#
+set -x
+
+source vm_config.sh
+
+MACHINE_TYPE=n1-highmem-16
+SOURCE_SNAPSHOT=skia-systemd-pushable-base
+SCOPES='https://www.googleapis.com/auth/devstorage.full_control https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile'
+IP_ADDRESS=104.154.112.116
+
+# Create a boot disk from the pushable base snapshot.
+gcloud compute --project $PROJECT_ID disks create $INSTANCE_NAME \
+  --zone $ZONE \
+  --source-snapshot $SOURCE_SNAPSHOT \
+  --type "pd-standard"
+
+# Create a large data disk.
+gcloud compute --project $PROJECT_ID disks create $INSTANCE_NAME"-data" \
+  --size "50" \
+  --zone $ZONE \
+  --type "pd-standard"
+
+# Create the instance with the two disks attached.
+gcloud compute --project $PROJECT_ID instances create $INSTANCE_NAME \
+  --zone $ZONE \
+  --machine-type $MACHINE_TYPE \
+  --network "default" \
+  --maintenance-policy "MIGRATE" \
+  --scopes $SCOPES \
+  --tags "http-server" "https-server" \
+  --metadata-from-file "startup-script=startup-script.sh" \
+  --disk name=${INSTANCE_NAME}      device-name=${INSTANCE_NAME}      "mode=rw" "boot=yes" "auto-delete=yes" \
+  --disk name=${INSTANCE_NAME}-data device-name=${INSTANCE_NAME}-data "mode=rw" "boot=no" \
+  --address=$IP_ADDRESS
+
+WAIT_TIME_AFTER_CREATION_SECS=600
+echo
+echo "===== Wait $WAIT_TIME_AFTER_CREATION_SECS secs for instance to" \
+  "complete startup script. ====="
+echo
+sleep $WAIT_TIME_AFTER_CREATION_SECS
+
+# The instance believes it is skia-systemd-snapshot-maker until it is rebooted.
+echo
+echo "===== Rebooting the instance ======"
+# Using "shutdown -r +1" rather than "reboot" so that the connection isn't
+# terminated immediately, which causes a non-zero exit code.
+gcloud compute --project $PROJECT_ID ssh $PROJECT_USER@$INSTANCE_NAME \
+  --zone $ZONE \
+  --command "sudo shutdown -r +1" \
+  || echo "Reboot failed; please reboot the instance manually."
