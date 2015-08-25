284d65805384788288cfc25efd4acdba6965a5b9
stephana <stephana@google.com>
2015-08-25 11:48:20 -0700

    Remove startup script from pushable-systemd-snapshot
    BUG=skia:
    
    Review URL: https://codereview.chromium.org/1317663002

--- compute_engine_scripts/pushable-systemd-snapshot/vm_create_instance.sh
+++ compute_engine_scripts/pushable-systemd-snapshot/vm_create_instance.sh
@@ -12,9 +12,20 @@ gcloud compute --project $PROJECT_ID instances create $INSTANCE_NAME \
   --network "default" \
   --maintenance-policy "MIGRATE" \
   --scopes $SCOPES \
-  --tags "http-server" "https-server" \
-  --metadata-from-file "startup-script=startup-script.sh" \
+  --tags "http-server,https-server" \
   --image $IMAGE_TYPE \
   --boot-disk-type "pd-standard" \
   --boot-disk-device-name $INSTANCE_NAME \
   --address=$IP_ADDRESS
+
+# Wait until the instance is up.
+until nc -w 1 -z $IP_ADDRESS 22; do
+    echo "Waiting for VM to come up."
+    sleep 2
+done
+
+gcloud compute copy-files ./setup-script.sh default@${INSTANCE_NAME}:setup-script.sh \
+  --zone $ZONE
+
+gcloud compute ssh default@${INSTANCE_NAME} --zone $ZONE \
+  --command "sudo bash setup-script.sh"
