96679e09bfa09055f3f0116d40c4ebeb2e883a7f
borenet <borenet@google.com>
2015-08-26 08:03:56 -0700

    Fix skia-monitoring-staging IP address
    Whoops.
    
    TBR=jcgregorio
    BUG=skia:4242
    
    Review URL: https://codereview.chromium.org/1313383002

--- compute_engine_scripts/monitoring-staging/vm_create_instance.sh
+++ compute_engine_scripts/monitoring-staging/vm_create_instance.sh
@@ -9,7 +9,7 @@ source vm_config.sh
 MACHINE_TYPE=n1-highmem-16
 SOURCE_SNAPSHOT=skia-systemd-pushable-base
 SCOPES='https://www.googleapis.com/auth/devstorage.full_control https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile'
-IP_ADDRESS=104.154.112.116
+IP_ADDRESS=104.154.112.117
 
 # Create a boot disk from the pushable base snapshot.
 gcloud compute --project $PROJECT_ID disks create $INSTANCE_NAME \
