f7ce9bf8b766f0e964049a4c35092856b90ee2c6
borenet <borenet@google.com>
2015-08-26 07:58:20 -0700

    Add skia-monitoring-staging GCE instance, setup scripts
    BUG=skia:4242
    
    Review URL: https://codereview.chromium.org/1313233004

---
+++ compute_engine_scripts/monitoring-staging/startup-script.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+sudo mkdir -p /mnt/pd0
+sudo /usr/share/google/safe_format_and_mount -m "mkfs.ext4 -F" /dev/disk/by-id/google-skia-monitoring-staging-data /mnt/pd0
+sudo chmod 777 /mnt/pd0
