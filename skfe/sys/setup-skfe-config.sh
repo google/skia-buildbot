2e59c846513655ac1f81d37c8838eb03594ca383
stephana <stephana@google.com>
2015-09-08 13:19:11 -0700

    This also removes 'sudo' from skfe-config-setp.sh script as suggested in
    https://codereview.chromium.org/1308253003/
    
    BUG=skia:
    TBR=jcgregorio
    
    Review URL: https://codereview.chromium.org/1302213004

--- skfe/sys/setup-skfe-config.sh
+++ skfe/sys/setup-skfe-config.sh
@@ -5,8 +5,7 @@ set -x
 
 # Remove the default host and install the certs then restart nginx.
 # Note, if one of the next two lines fail, nginx will continue to run with the
-# previous configuation, which makes it fault silent but available. 
-sudo rm -f /etc/nginx/sites-enabled/default
-sudo /usr/local/bin/certpoller --log_dir=/var/log/logserver \
-     skia-org-pem skia-org-key
-sudo systemctl restart nginx
+# previous configuation, which makes it fault silent but available.
+rm -f /etc/nginx/sites-enabled/default
+/usr/local/bin/certpoller --log_dir=/var/log/logserver skia-org-pem skia-org-key
+systemctl restart nginx
