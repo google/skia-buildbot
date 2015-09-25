ba0d9e4d652ccecdcb080e7300955f9aa32a026a
borenet <borenet@google.com>
2015-09-25 11:19:58 -0700

    Fixes for skia-monitoring
    BUG=skia:4242
    
    Review URL: https://codereview.chromium.org/1360943006

--- compute_engine_scripts/monitoring/startup-script.sh
+++ compute_engine_scripts/monitoring/startup-script.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
-apt-get update
-apt-get upgrade -y
-apt-get install -y git
+sudo apt-get --assume-yes install --fix-broken
+sudo apt-get update
+sudo apt-get install -y git
 sudo mkdir -p /mnt/pd0
 sudo /usr/share/google/safe_format_and_mount -m "mkfs.ext4 -F" /dev/disk/by-id/google-skia-monitoring-data /mnt/pd0
 sudo chmod 777 /mnt/pd0
