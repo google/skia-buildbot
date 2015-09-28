ae8c08c47c0b363786de2060bba9712cf6290a45
borenet <borenet@google.com>
2015-09-28 07:57:10 -0700

    Datahopper: replace use of go-metrics histogram with sliding time window
    BUG=skia:4268
    
    Review URL: https://codereview.chromium.org/1365713002

--- go/buildbot/db.go
+++ go/buildbot/db.go
@@ -8,7 +8,6 @@ import (
 	"time"
 
 	"github.com/jmoiron/sqlx"
-	go_metrics "github.com/rcrowley/go-metrics"
 
 	"go.skia.org/infra/go/database"
 	"go.skia.org/infra/go/metrics"
@@ -720,7 +719,7 @@ func (b *Build) replaceIntoDBTx(tx *sqlx.Tx) (rv error) {
 			// Measure the time between build start and first DB insertion.
 			if b.Id == 0 {
 				t := int64(time.Now().Sub(util.UnixFloatToTime(b.Started)))
-				go_metrics.GetOrRegisterHistogram("buildbot.startToIngestLatency", go_metrics.DefaultRegistry, go_metrics.NewUniformSample(100)).Update(t)
+				metrics.GetOrRegisterSlidingWindow("buildbot.startToIngestLatency", metrics.DEFAULT_WINDOW).Update(t)
 			}
 			// Update the build with any new IDs.
 			b.Id = buildID
--- go/metrics/metrics.go
+++ go/metrics/metrics.go
@@ -2,10 +2,15 @@
 package metrics
 
 import (
+	"fmt"
+	"sync"
 	"time"
 
 	"github.com/rcrowley/go-metrics"
 )
+
+// DEFAULT_WINDOW is the default window size for SlidingWindow.
+const DEFAULT_WINDOW = time.Minute
 
 // Liveness keeps a time-since-last-successful-update metric.
 //
@@ -39,6 +44,96 @@ func NewLiveness(name string) *Liveness {
 	return l
 }
 
+var registry = map[string]*SlidingWindow{}
+var registryMutex = sync.Mutex{}
+
+// SlidingWindow stores data points over a time period and reports statistics on
+// the data to go-metrics every minute.
+type SlidingWindow struct {
+	data   []int64
+	mutex  sync.Mutex
+	name   string
+	period time.Duration
+	times  []time.Time
+}
+
+// GetOrRegisterSlidingWindow returns the SlidingWindow with the given name,
+// creating it if necessary.
+func GetOrRegisterSlidingWindow(name string, period time.Duration) *SlidingWindow {
+	registryMutex.Lock()
+	defer registryMutex.Unlock()
+	if _, ok := registry[name]; !ok {
+		r := SlidingWindow{
+			data:   []int64{},
+			mutex:  sync.Mutex{},
+			name:   name,
+			period: period,
+			times:  []time.Time{},
+		}
+		go func() {
+			for _ = range time.Tick(time.Minute) {
+				r.report()
+			}
+		}()
+		registry[name] = &r
+	}
+	return registry[name]
+}
+
+// Update inserts a data point.
+func (r *SlidingWindow) Update(v int64) {
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
+	r.data = append(r.data, v)
+	r.times = append(r.times, time.Now())
+}
+
+// report sends statistics to go-metrics.
+func (r *SlidingWindow) report() {
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
+
+	limit := time.Now().Add(-r.period)
+
+	// Evict old data points.
+	idx := 0
+	for _, t := range r.times {
+		if t.After(limit) {
+			break
+		}
+		idx++
+	}
+	r.data = r.data[idx:]
+	r.times = r.times[idx:]
+	if len(r.data) == 0 {
+		return
+	}
+
+	// Calculate stats.
+	max := metrics.SampleMax(r.data)
+	mean := metrics.SampleMean(r.data)
+	min := metrics.SampleMin(r.data)
+	percentiles := []float64{0.5, 0.75, 0.95, 0.99}
+	percentileVals := metrics.SamplePercentiles(r.data, percentiles)
+	stddev := metrics.SampleStdDev(r.data)
+	sum := metrics.SampleSum(r.data)
+	variance := metrics.SampleVariance(r.data)
+
+	// Log stats into Gauges.
+	reg := metrics.DefaultRegistry
+	metrics.GetOrRegisterGauge(r.name+".max", reg).Update(max)
+	metrics.GetOrRegisterGaugeFloat64(r.name+".mean", reg).Update(mean)
+	metrics.GetOrRegisterGauge(r.name+".min", reg).Update(min)
+	for i, p := range percentiles {
+		metric := r.name + "." + fmt.Sprintf("%0.2f", p)[2:]
+		metrics.GetOrRegisterGaugeFloat64(metric, reg).Update(percentileVals[i])
+	}
+	metrics.GetOrRegisterGaugeFloat64(r.name+".std-dev", reg).Update(stddev)
+	metrics.GetOrRegisterGauge(r.name+".sum", reg).Update(sum)
+	metrics.GetOrRegisterGaugeFloat64(r.name+".variance", reg).Update(variance)
+	metrics.GetOrRegisterGauge(r.name+".count", reg).Update(int64(len(r.data)))
+}
+
 // Timer is a timer which reports its result to go-metrics.
 //
 // The standard way to use Timer is at the top of the func you
@@ -60,5 +155,5 @@ func NewTimer(metric string) *Timer {
 
 func (t Timer) Stop() {
 	v := int64(time.Now().Sub(t.Begin))
-	metrics.GetOrRegisterHistogram(t.Metric, metrics.DefaultRegistry, metrics.NewUniformSample(100)).Update(v)
+	GetOrRegisterSlidingWindow(t.Metric, DEFAULT_WINDOW).Update(v)
 }
