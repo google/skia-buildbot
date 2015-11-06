a0445ab954fdbaad3a84936ed756e84389e6f234
borenet <borenet@google.com>
2015-11-06 07:03:33 -0800

    Add depot_tools setup to autoroll instance startup script
    BUG=skia:4455
    
    Review URL: https://codereview.chromium.org/1430023007

--- compute_engine_scripts/autoroll/startup-script.sh
+++ compute_engine_scripts/autoroll/startup-script.sh
@@ -2,3 +2,17 @@
 sudo mkdir -p /mnt/pd0
 sudo /usr/share/google/safe_format_and_mount -m "mkfs.ext4 -F" /dev/disk/by-id/google-skia-autoroll-data /mnt/pd0
 sudo chmod 777 /mnt/pd0
+
+pushd /home/default
+# Install depot_tools if needed.
+if [[ ! -d depot_tools ]]; then
+  sudo -u default git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+fi
+
+# Add depot_tools to PATH if needed.
+if [ -z "$(which gclient)" ]; then
+  sudo -u default echo '# Add depot_tools to PATH"' >> .bashrc
+  sudo -u default echo 'export PATH="/home/default/depot_tools:$PATH"' >> .bashrc
+fi
+
+popd
