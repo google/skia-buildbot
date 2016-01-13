5e030871220a0821955cb4c45f537e5038571d41
borenet <borenet@google.com>
2016-01-13 11:37:07 -0800

    Fix systemd timer and script for buildbot-db-backup
    BUG=skia:4643
    
    Review URL: https://codereview.chromium.org/1588603002

--- datahopper/buildbot_db_backup.sh
+++ datahopper/buildbot_db_backup.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-WORKDIR=/tmp/buildbot_db_backup
+set -x
+{
+WORKDIR=/mnt/pd0/buildbot_db_backup
 mkdir -p $WORKDIR
 pushd $WORKDIR
-curl http://localhost:8001/backup > buildbot.db
-gsutil cp buildbot.db gs://skia-buildbots/db_backup/$(date +%Y/%m/%d)/buildbot.db
+curl http://localhost:8001/backup > buildbot.db && \
+sudo gsutil cp buildbot.db gs://skia-buildbots/db_backup/$(date +%Y/%m/%d)/buildbot.db
 popd
 rm -rf $WORKDIR
+} > /var/log/logserver/buildbot-db-backup.$(hostname).$(whoami).log.INFO.$(date +%Y%m%d-%H%M%S.%N) 2>&1
