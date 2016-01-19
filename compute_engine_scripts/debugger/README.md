be8d02ed1205d63fae9850a08c20dc5e9f4c9845
jcgregorio <jcgregorio@google.com>
2016-01-19 13:09:01 -0800

    skdebugger: Create instance for serving debugger Polymer files.
    BUG=skia:
    
    Review URL: https://codereview.chromium.org/1605143002

---
+++ compute_engine_scripts/debugger/README.md
@@ -0,0 +1,9 @@
+Skia Debugger
+=============
+
+Instance creation and deletion scripts for Skia Debugger, the content
+server for the SKP debugger.
+
+Note that there aren't any setup or push scripts as all of that is handled by
+Skia Push. See ../../push/DESIGN.md.
+
---
+++ compute_engine_scripts/debugger/vm_create_instance.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+#
+# Creates the compute instance for skia-tracedb.
+#
+set -x
+
+source vm_config.sh
+
+DEBUGGER_MACHINE_TYPE=n1-standard-1
+DEBUGGER_SOURCE_SNAPSHOT=skia-systemd-pushable-base
+DEBUGGER_SCOPES='https://www.googleapis.com/auth/devstorage.full_control'
+DEBUGGER_IP_ADDRESS=104.154.112.116
+
+# Create a boot disk from the pushable base snapshot.
+gcloud compute --project $PROJECT_ID disks create $INSTANCE_NAME \
+  --zone $ZONE \
+  --source-snapshot $DEBUGGER_SOURCE_SNAPSHOT \
+  --type "pd-standard"
+
+# Create the instance with the two disks attached.
+gcloud compute --project $PROJECT_ID instances create $INSTANCE_NAME \
+  --zone $ZONE \
+  --machine-type $DEBUGGER_MACHINE_TYPE \
+  --network "default" \
+  --maintenance-policy "MIGRATE" \
+  --scopes $DEBUGGER_SCOPES \
+  --tags "http-server" "https-server" \
+  --disk name=${INSTANCE_NAME}      device-name=${INSTANCE_NAME}      "mode=rw" "boot=yes" "auto-delete=yes" \
+  --address=$DEBUGGER_IP_ADDRESS
+
+# The instance believes it is skia-systemd-snapshot-maker until it is rebooted.
+echo
+echo "===== Rebooting the instance ======"
+# Using "shutdown -r +1" rather than "reboot" so that the connection isn't
+# terminated immediately, which causes a non-zero exit code.
+gcloud compute --project $PROJECT_ID ssh $PROJECT_USER@$INSTANCE_NAME \
+  --zone $ZONE \
+  --command "sudo shutdown -r +1" \
+  || echo "Reboot failed; please reboot the instance manually."
