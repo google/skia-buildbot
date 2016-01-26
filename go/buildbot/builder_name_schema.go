822dfb5da953dc4865f6872ff380aaf916ce169d
borenet <borenet@google.com>
2016-01-26 04:28:57 -0800

    Add builder_name_schema parsing to buildbot package
    BUG=skia:
    
    Review URL: https://codereview.chromium.org/1633743002

---
+++ go/buildbot/builder_name_schema.go
@@ -0,0 +1,75 @@
+package buildbot
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/skia-dev/glog"
+
+	"go.skia.org/infra/go/gitiles"
+)
+
+// This file must be kept in sync with:
+// https://chromium.googlesource.com/chromium/tools/build/+/master/scripts/common/skia/builder_name_schema.json
+
+var (
+	schema *builderNameSchema
+)
+
+type builderNameSchema struct {
+	Schema           map[string][]string `json:"builder_name_schema"`
+	BuilderNameSep   string              `json:"builder_name_sep"`
+	TrybotNameSuffix string              `json:"trybot_name_suffix"`
+}
+
+func init() {
+	buf := bytes.NewBuffer(nil)
+	r := gitiles.NewRepo("https://chromium.googlesource.com/chromium/tools/build")
+	if err := r.ReadFile("scripts/common/skia/builder_name_schema.json", buf); err != nil {
+		glog.Fatal(err)
+	}
+	res := new(builderNameSchema)
+	if err := json.NewDecoder(buf).Decode(res); err != nil {
+		glog.Fatal(err)
+	}
+	schema = res
+}
+
+func ParseBuilderName(name string) (map[string]string, error) {
+	split := strings.Split(name, schema.BuilderNameSep)
+	if len(split) < 2 {
+		return nil, fmt.Errorf("Invalid builder name: %q", name)
+	}
+	role := split[0]
+	split = split[1:]
+	keys, ok := schema.Schema[role]
+	if !ok {
+		return nil, fmt.Errorf("Invalid builder name; %q is not a valid role.", role)
+	}
+	isTrybot := false
+	if split[len(split)-1] == schema.TrybotNameSuffix {
+		isTrybot = true
+		split = split[:len(split)-1]
+	}
+	extraConfig := ""
+	if len(split) == len(keys)+1 {
+		extraConfig = split[len(split)-1]
+		split = split[:len(split)-1]
+	}
+	if len(split) != len(keys) {
+		return nil, fmt.Errorf("Invalid builder name: %q has incorrect number of parts.", name)
+	}
+	rv := make(map[string]string, len(keys)+2)
+	rv["role"] = role
+	rv["is_trybot"] = strconv.FormatBool(isTrybot)
+	if extraConfig != "" {
+		rv["extra_config"] = extraConfig
+	}
+	for i, k := range keys {
+		rv[k] = split[i]
+	}
+	return rv, nil
+}
---
+++ go/buildbot/builder_name_schema_test.go
@@ -0,0 +1,57 @@
+package buildbot
+
+import (
+	"testing"
+
+	assert "github.com/stretchr/testify/require"
+)
+
+func TestBuilderNameSchema(t *testing.T) {
+	tc := map[string]map[string]string{
+		"Build-Ubuntu-GCC-x86-Release": {
+			"role":          "Build",
+			"os":            "Ubuntu",
+			"compiler":      "GCC",
+			"target_arch":   "x86",
+			"configuration": "Release",
+			"is_trybot":     "false",
+		},
+		"Build-Ubuntu-GCC-x86-Release-Trybot": {
+			"role":          "Build",
+			"os":            "Ubuntu",
+			"compiler":      "GCC",
+			"target_arch":   "x86",
+			"configuration": "Release",
+			"is_trybot":     "true",
+		},
+		"Build-Ubuntu-GCC-x86-Debug-Android": {
+			"role":          "Build",
+			"os":            "Ubuntu",
+			"compiler":      "GCC",
+			"target_arch":   "x86",
+			"configuration": "Debug",
+			"is_trybot":     "false",
+			"extra_config":  "Android",
+		},
+		"Test-Ubuntu-GCC-GCE-CPU-AVX2-x86_64-Debug-CT_DM_1m_SKPs": {
+			"role":             "Test",
+			"os":               "Ubuntu",
+			"compiler":         "GCC",
+			"model":            "GCE",
+			"cpu_or_gpu":       "CPU",
+			"cpu_or_gpu_value": "AVX2",
+			"arch":             "x86_64",
+			"configuration":    "Debug",
+			"extra_config":     "CT_DM_1m_SKPs",
+			"is_trybot":        "false",
+		},
+	}
+	for builderName, params := range tc {
+		res, err := ParseBuilderName(builderName)
+		if params == nil {
+			assert.NotNil(t, err)
+		} else {
+			assert.Equal(t, params, res)
+		}
+	}
+}
