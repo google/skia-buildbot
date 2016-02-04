c07bcca5bbe27e1eac319b8dcfceac7141ba2cdc
borenet <borenet@google.com>
2016-02-04 09:28:56 -0800

    Add metrics2/doc.go
    BUG=skia:
    
    Review URL: https://codereview.chromium.org/1661173004

---
+++ go/metrics2/docs.go
@@ -0,0 +1,41 @@
+package metrics2
+
+/*
+Metrics2 Package
+================
+
+Concepts
+--------
+
+The major difference between the old metrics format and the new is that, from the app’s point of view, the old metrics were specified using a single metric name.  In the new format, metrics are specified using a measurement name and a map[string]string of tags.  Expect all instances of metrics types to require both a measurement name and tags map instead of just the metric name.  When using common.InitWithMetrics2, runtime stats including uptime are automatically recorded, and the “host” and “app” tags are automatically set for all measurements.  In many cases, it may be unnecessary to provide more tags, so you can just pass nil.
+
+Metrics Helpers
+---------------
+The metrics2 package provides a number of helper structs and functions which should be your primary mode of interacting with metrics.
+
+### Typed Metrics
+
+Metrics2 provides a few typed metrics helpers which behave similarly to go_metrics.GetOrRegisterGauge: once registered, the metric contains a single value which is pushed into InfluxDB at regular intervals.  The value can be changed by calling Update() on the Metric.  The metrics are stored in a sort of registry so that you don’t need to keep the object around and can just do: metrics2.GetInt64Metric(metric, tags).Update(value).
+
+### Counter
+
+Counter in the metrics2 package behaves similarly to those from go-metrics except that there is no GetOrRegisterCounter equivalent.  Instead, you should call metrics2.NewCounter(name, tags), hold on to the returned struct instance and call Inc(), Dec(), etc, on it as desired.  Note that Counter requires a name and not a measurement, because the measurement is always “counter”, and the provided name is inserted as a tag.
+
+### Liveness
+
+Liveness in metrics2 behaves similarly to the old metrics liveness, except that you provide a name and tags.  Call metrics2.NewLiveness() to start the liveness timer, and call Reset() on the instance to reset it to zero.  Note that Liveness requires a name and not a measurement, because the measurement is always “liveness”, and the provided name is inserted as a tag.
+
+### Timer
+
+Timer in metrics2 behaves similarly to the old metrics timer, except that you provide a name and tags instead of a metric.  Call metrics2.NewTimer(name, tags) to start the timer and call Stop() on the instance to measure the duration and push a data point into InfluxDB.  Timer does not behave like a Gauge, in that it does not push values at regular intervals.  Instead, it only pushes a value when Stop() is called.  Be aware of this when creating alerts based on timers, since data points will not be evenly spaced and may not exist for a time period.  Timer requires a name and not a measurement, because the measurement is always “timer” and the provided name is inserted as a tag.
+
+### FuncTimer
+
+FuncTimer is a special Timer designed specifically for timing the duration of functions.  It does not accept any parameters because it automatically fills in the function name and package name in the tags.  Just do defer metrics2.FuncTimer().Stop() at the beginning of the function.
+
+Raw Data Insertion
+------------------
+
+In addition to the above helpers, you can also insert data directly, without the use of a helper.  There is a single function, RawAddInt64PointAtTime, which may be called to add a single point with a given measurement name, tag set, and timestamp.  This is useful for event-based measurements, or when none of the above helpers are appropriate.  You should avoid using this unless you absolutely need it, since it does not play by the same rules as the other metrics types.
+
+*/
