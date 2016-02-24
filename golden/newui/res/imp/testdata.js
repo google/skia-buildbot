7aa09fb5d8129680fc5c72295b93f88ab49c5225
stephana <stephana@google.com>
2016-02-24 10:51:27 -0800

    Convert gold elements to Polymer 1.0
    This converts these tags to Polymer 1.0:
      - blame-sk
      - dot-legend-sk
      - paramset-sk
      - triage-sk  (renamed from tri-sk)
    
    BUG=skia:4154
    
    Review URL: https://codereview.chromium.org/1722003003

---
+++ golden/newui/res/imp/testdata.js
@@ -0,0 +1,275 @@
+"use strict"
+
+// Declares the testdata object that is a container for JS
+// testdata used by the *-demo.html pages. This avoids 
+// cluttering the demo pages and allows to use testdata 
+// across. 
+
+
+var testdata = testdata || {}; 
+
+// Commits.
+testdata.commits = [
+  {
+    "commit_time": 1455800679,
+    "hash": "adab5a2a4b2ae4d7fa832093fe81a640e848746c",
+    "author": "joshualitt (joshualitt@chromium.org)"
+  },
+  {
+    "commit_time": 1455803133,
+    "hash": "6b14c6b329fde5402ce7458d8261e80944279d06",
+    "author": "caryclark (caryclark@google.com)"
+  },
+  {
+    "commit_time": 1455803965,
+    "hash": "ef0fed3bf06de231b66be5f5279468afd01e6f75",
+    "author": "scroggo (scroggo@google.com)"
+  },
+  {
+    "commit_time": 1455804805,
+    "hash": "a11053891225b7c08466eef37068d8ff2dada168",
+    "author": "caryclark (caryclark@google.com)"
+  },
+  {
+    "commit_time": 1455804991,
+    "hash": "a7b16858d51b5d9b7981087cadb1ff764632bf14",
+    "author": "caryclark (caryclark@google.com)"
+  },
+  {
+    "commit_time": 1455805659,
+    "hash": "5bd98a244bb1ab1b3cad945e2fa1ce3dfd62d8cf",
+    "author": "kjlubick (kjlubick@google.com)"
+  },
+  {
+    "commit_time": 1455806455,
+    "hash": "c969bfced3849a73f6c015367beed5b46142f333",
+    "author": "bsalomon (bsalomon@google.com)"
+  },
+  {
+    "commit_time": 1455807493,
+    "hash": "e1fce93f36d7b73df9942135dc5a342b629e6b3a",
+    "author": "mtklein (mtklein@chromium.org)"
+  },
+  {
+    "commit_time": 1455811548,
+    "hash": "d9fa758292462c4200231f49286aa97750f5e689",
+    "author": "borenet (borenet@google.com)"
+  },
+  {
+    "commit_time": 1455812193,
+    "hash": "4528c867c4ba65fdf5cfd6de35ea4c36a16c73f6",
+    "author": "reed (reed@google.com)"
+  },
+  {
+    "commit_time": 1455814084,
+    "hash": "3b087f4010327b304242aaf27ef3689150dfc226",
+    "author": "robertphillips (robertphillips@google.com)"
+  },
+  {
+    "commit_time": 1455814413,
+    "hash": "a7e9f05119174848c6be5028568f46eb5e85398e",
+    "author": "bungeman (bungeman@google.com)"
+  },
+  {
+    "commit_time": 1455814801,
+    "hash": "dcd1fcc6e9891e1427c8ce4500edac45a8687fbd",
+    "author": "caryclark (caryclark@google.com)"
+  },
+  {
+    "commit_time": 1455818471,
+    "hash": "1526e0a5f5d975b34878ed46c2e5fc1671fdac76",
+    "author": "bsalomon (bsalomon@google.com)"
+  },
+  {
+    "commit_time": 1455819194,
+    "hash": "3fe4df6d8bdaa2bd2d2ca9ad988907e9d2e8a116",
+    "author": "borenet (borenet@google.com)"
+  },
+  {
+    "commit_time": 1455819754,
+    "hash": "3ca1e1ae4fe710265056eadb086fc6ccc2caa57e",
+    "author": "ethannicholas (ethannicholas@google.com)"
+  },
+  {
+    "commit_time": 1455821450,
+    "hash": "3afae70a48cd9f9c0f57dc7db3f31aea0bb158f1",
+    "author": "ethannicholas (ethannicholas@google.com)"
+  },
+  {
+    "commit_time": 1455822073,
+    "hash": "bc1cebbd2b25cbc43b2ab76809d959b5185b29e2",
+    "author": "bungeman (bungeman@google.com)"
+  },
+  {
+    "commit_time": 1455824520,
+    "hash": "517558ad19ff6b1eeecd9ba00d20a9f72866979f",
+    "author": "mtklein (mtklein@chromium.org)"
+  },
+  {
+    "commit_time": 1455825198,
+    "hash": "d689bf874826d38c6ef0d8b802b74d61fab5ec2f",
+    "author": "bungeman (bungeman@google.com)"
+  },
+  {
+    "commit_time": 1455827828,
+    "hash": "8528541dd7f09f5bd76f3f1ce5f45d08ac7347c7",
+    "author": "cdalton (cdalton@nvidia.com)"
+  },
+  {
+    "commit_time": 1455827954,
+    "hash": "dd9ffea9ce051a49dbc6544e6aa3cb68fe987f47",
+    "author": "reed (reed@google.com)"
+  },
+  {
+    "commit_time": 1455831940,
+    "hash": "c85d9fbc0adacc9acb9faa3fe107652f0d76760a",
+    "author": "ethannicholas (ethannicholas@google.com)"
+  },
+  {
+    "commit_time": 1455832502,
+    "hash": "ed545042fca0e1d385561d956156e0b66532d161",
+    "author": "herb (herb@google.com)"
+  },
+  {
+    "commit_time": 1455835937,
+    "hash": "4b25cf0bc0ee91fdb9351bbdd284f23c0d9919b1",
+    "author": "reed (reed@google.com)"
+  },
+  {
+    "commit_time": 1455844307,
+    "hash": "b187dd3fb34a2368a31f1672c917adf068e8a6d3",
+    "author": "reed (reed@google.com)"
+  },
+  {
+    "commit_time": 1455882473,
+    "hash": "4ed187a0103e86c9c1cc413d4ef67826f40ff138",
+    "author": "rmistry (rmistry@google.com)"
+  },
+  {
+    "commit_time": 1455892387,
+    "hash": "77b798ce8e8616e8cb0e82a22ad9eb4f69eb271f",
+    "author": "scroggo (scroggo@chromium.org)"
+  },
+  {
+    "commit_time": 1455892522,
+    "hash": "e507ff0460f4f878214b9454fb5b9ab8d64d8063",
+    "author": "jvanverth (jvanverth@google.com)"
+  },
+  {
+    "commit_time": 1455896434,
+    "hash": "449f7fcf7088bb67b86729bd4675a72c7580c443",
+    "author": "caryclark (caryclark@google.com)"
+  },
+  {
+    "commit_time": 1455898780,
+    "hash": "df7bb471e5455dece2784a970d9ae50d3ab0ca75",
+    "author": "robertphillips (robertphillips@google.com)"
+  },
+  {
+    "commit_time": 1455900059,
+    "hash": "85fca851e621d18db9b0cb5dc47878a77310ef15",
+    "author": "ethannicholas (ethannicholas@google.com)"
+  },
+  {
+    "commit_time": 1455900206,
+    "hash": "d106c2c64012688f8d1439543d942f8f66a142e0",
+    "author": "jvanverth (jvanverth@google.com)"
+  },
+  {
+    "commit_time": 1455901730,
+    "hash": "09d06850fabc883d6e80faae6b5d3129026d1e8b",
+    "author": "mtklein (mtklein@chromium.org)"
+  },
+  {
+    "commit_time": 1455903624,
+    "hash": "be8c19e8d3deac9b9585c44b9a423912dd00a75a",
+    "author": "mtklein (mtklein@chromium.org)"
+  },
+  {
+    "commit_time": 1455907273,
+    "hash": "cbc5bccebec983cdd7bea4f10373ec3af115def3",
+    "author": "robertphillips (robertphillips@google.com)"
+  },
+  {
+    "commit_time": 1455909283,
+    "hash": "740668d9071b8b767ec4ca344cb31fc0773ac2d5",
+    "author": "msarett (msarett@google.com)"
+  },
+  {
+    "commit_time": 1455911037,
+    "hash": "27e88d0dab8d9fe66b572004972b01adc62a8063",
+    "author": "mtklein (mtklein@chromium.org)"
+  },
+  {
+    "commit_time": 1455912521,
+    "hash": "950fb1ca24cca9803eb29dc6aa5472b0f506a67f",
+    "author": "scroggo (scroggo@google.com)"
+  },
+  {
+    "commit_time": 1455917933,
+    "hash": "c5b1228141bd26d4573552a00ebeb0b85a4768df",
+    "author": "reed (reed@google.com)"
+  },
+  {
+    "commit_time": 1455918363,
+    "hash": "3b32bc51f4759d02aca6cd327fcad39f7132a1d1",
+    "author": "reed (reed@google.com)"
+  },
+  {
+    "commit_time": 1455919983,
+    "hash": "8e0ef294d0fc310432308dc09b8a48a53f7aabc1",
+    "author": "joshualitt (joshualitt@chromium.org)"
+  },
+  {
+    "commit_time": 1455920790,
+    "hash": "365bbff200a58a12a38631e53639c12a878b1fea",
+    "author": "bsalomon (bsalomon@google.com)"
+  },
+  {
+    "commit_time": 1455920834,
+    "hash": "cbf897802b41d61dc35d6db0552d8d9604945fdc",
+    "author": "mtklein (mtklein@chromium.org)"
+  },
+  {
+    "commit_time": 1455921587,
+    "hash": "3eb4895ea1205a5a2dd1457bf67a1497a29ebc81",
+    "author": "herb (herb@google.com)"
+  },
+  {
+    "commit_time": 1455924620,
+    "hash": "4534562f7600e036fd7561df1fe04dc5e58a58fc",
+    "author": "benjaminwagner (benjaminwagner@google.com)"
+  },
+  {
+    "commit_time": 1455929652,
+    "hash": "f5d4746ad73ef5eabc927d3d988bb9ee97c77921",
+    "author": "herb (herb@google.com)"
+  },
+  {
+    "commit_time": 1456006707,
+    "hash": "00bea4ad310c4ec4dd95809b47ce3fbfa8fd0e1e",
+    "author": "reed (reed@google.com)"
+  },
+  {
+    "commit_time": 1456022938,
+    "hash": "d97f07dc43ed07536921ec4e87a8785ae2da8e6a",
+    "author": "reed (reed@google.com)"
+  },
+  {
+    "commit_time": 1456044232,
+    "hash": "df56af4b4ac979a18ea1507bffe2e80f9faab47c",
+    "author": "update-skps (update-skps@skia.org)"
+  }
+];
+
+// Blame distributions. 
+testdata.oldBlameDist = {
+  "freq": [ 43, 44, 45, 46, 47,  48 ],
+  "old": true
+};
+
+// Blame distribution. 
+testdata.newBlameDist = {
+  "freq": [ 43, 44, 45, 46, 47,  48 ],
+  "old": false
+};
