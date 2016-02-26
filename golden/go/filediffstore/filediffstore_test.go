303bba346336fd2d29e636b48fdba58631f9feb2
mtklein <mtklein@google.com>
2016-02-26 09:35:11 -0800

    Revert of Update diff.go to work on 16-bit channels. (patchset #9 id:160001 of https://codereview.chromium.org/1724113002/ )
    Reason for revert:
    new plan, gonna stick to 8-bit pngs for now
    
    Original issue's description:
    > Update diff.go to work on 16-bit channels.
    >
    > All the math is now upgraded to 16-bit channels,
    > so diffs that used to range from [0,255] now range from [0, 65535],
    > or 4x that.  You can see this in diff_test.go.
    >
    > I've also changed some of the test values in diff_test.go to be opaque,
    > to make them more resilient to color format changes.  I was seeing, e.g.
    > {1,0,0,0} converted into {0,0,0,0}, which is correct but perhaps surprising; switching to {1,0,0,255} makes the 1 matter.
    >
    > The old NRGBA / NRGBA fast path is now 3 fast paths:
    >    - diff_32_32: basically the old fast path
    >    - diff_64_64: same idea for NRGBA64
    >    - diff_32_64: diff between the two
    >
    > diff_32_32 should stop mattering first, then diff_32_64.
    > Long term we'll just be using diff_64_64.
    >
    > I've noted several places in my head where we can make all these faster using C.
    >
    > BUG=skia:
    >
    > Committed: https://skia.googlesource.com/buildbot/+/38be2c5a82dc78c3720cf03485766cd8f9044266
    
    TBR=stephana@google.com,mtklein@chromium.org
    # Not skipping CQ checks because original CL landed more than 1 days ago.
    BUG=skia:
    
    Review URL: https://codereview.chromium.org/1737203003

--- golden/go/filediffstore/filediffstore_test.go
+++ golden/go/filediffstore/filediffstore_test.go
@@ -70,7 +70,7 @@ func getTestFileDiffStore(t *testing.T, storageBaseDir string, cleanBaseDir bool
 		NumDiffPixels:     2233,
 		PixelDiffPercent:  0.8932,
 		PixelDiffFilePath: diffpath1_2,
-		MaxRGBADiffs:      []int{0, 0, 257, 0},
+		MaxRGBADiffs:      []int{0, 0, 1, 0},
 		DimDiffer:         false,
 	}
 	relExpectedDiffMetrics1_2 = &diff.DiffMetrics{}
@@ -83,7 +83,7 @@ func getTestFileDiffStore(t *testing.T, storageBaseDir string, cleanBaseDir bool
 		NumDiffPixels:     250000,
 		PixelDiffPercent:  100,
 		PixelDiffFilePath: diffpath1_3,
-		MaxRGBADiffs:      []int{65535, 65535, 65535, 0},
+		MaxRGBADiffs:      []int{255, 255, 255, 0},
 		DimDiffer:         true,
 	}
 
