0b6d903065d551ddc93bd492d1c669097000c65b
jcgregorio <jcgregorio@google.com>
2016-04-13 10:24:54 -0700

    fiddle: block and tackle UI.
    BUG=skia:
    R=rmistry
    
    Review URL: https://codereview.chromium.org/1886793002

--- fiddle/go/fiddle/main.go
+++ fiddle/go/fiddle/main.go
@@ -3,13 +3,17 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"html/template"
 	"net/http"
+	"path/filepath"
 
 	"github.com/gorilla/mux"
 	"github.com/skia-dev/glog"
 	"go.skia.org/infra/go/common"
 	"go.skia.org/infra/go/httputils"
 	"go.skia.org/infra/go/influxdb"
+	"go.skia.org/infra/go/login"
 )
 
 // flags
@@ -24,21 +28,59 @@ var (
 	influxDatabase = flag.String("influxdb_database", influxdb.DEFAULT_DATABASE, "The InfluxDB database.")
 )
 
-// mainHandler handles the GET of the main page.
-func mainHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "GET" {
+var (
+	templates *template.Template
+)
+
+func loadTemplates() {
+	templates = template.Must(template.New("").ParseFiles(
+		filepath.Join(*resourcesDir, "templates/index.html"),
+		// Sub templates used by other templates.
+		filepath.Join(*resourcesDir, "templates/header.html"),
+	))
+}
+
+func templateHandler(name string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/html")
-		if _, err := w.Write([]byte("Hello world!")); err != nil {
-			glog.Errorf("Failed to write: %s", err)
+		if *local {
+			loadTemplates()
 		}
+		if err := templates.ExecuteTemplate(w, name, struct{}{}); err != nil {
+			glog.Errorln("Failed to expand template:", err)
+		}
+	}
+}
+
+func makeResourceHandler() func(http.ResponseWriter, *http.Request) {
+	fileServer := http.FileServer(http.Dir(*resourcesDir))
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Cache-Control", "max-age=300")
+		fileServer.ServeHTTP(w, r)
 	}
 }
 
 func main() {
 	defer common.LogPanic()
-	common.InitWithMetrics2("push", influxHost, influxUser, influxPassword, influxDatabase, local)
+	if *local {
+		common.Init()
+	} else {
+		common.InitWithMetrics2("push", influxHost, influxUser, influxPassword, influxDatabase, local)
+	}
+	var redirectURL = fmt.Sprintf("http://localhost%s/oauth2callback/", *port)
+	if !*local {
+		redirectURL = "https://fiddle.skia.org/oauth2callback/"
+	}
+	if err := login.InitFromMetadataOrJSON(redirectURL, login.DEFAULT_SCOPE, login.DEFAULT_DOMAIN_WHITELIST); err != nil {
+		glog.Fatalf("Failed to initialize the login system: %s", err)
+	}
 	r := mux.NewRouter()
-	r.HandleFunc("/", mainHandler)
+	r.PathPrefix("/res/").HandlerFunc(makeResourceHandler())
+	r.HandleFunc("/", templateHandler("index.html"))
+	r.HandleFunc("/oauth2callback/", login.OAuth2CallbackHandler)
+	r.HandleFunc("/logout/", login.LogoutHandler)
+	r.HandleFunc("/loginstatus/", login.StatusHandler)
+
 	http.Handle("/", httputils.LoggingGzipRequestResponse(r))
 	glog.Infoln("Ready to serve.")
 	glog.Fatal(http.ListenAndServe(*port, nil))
