53b5d3c74aa9ea1a5e0a4bf4a1a96a9b3f75cbd8
stephana <stephana@google.com>
2016-04-18 14:59:26 -0700

    Generalize Search Page
    Adds the search-controls-sk element that is shared between the list-page-sk and search-page-sk elements. It encapsulates the search API that the backend exposes.
    
    https://screenshot.googleplex.com/rZPVCe5tsMy.png
    https://screenshot.googleplex.com/SObawjQhGYg.png
    
    BUG=skia:4154
    
    Review URL: https://codereview.chromium.org/1897863004

--- golden/newui/res/js/gold.js
+++ golden/newui/res/js/gold.js
@@ -11,6 +11,16 @@ var gold = gold || {};
 
 (function(){
 
+  // Default values for the search controls. 
+  gold.defaultSearchState = {
+     query:   "source_type=gm",
+     head:    true,
+     include: false,
+     pos: false,
+     neg: false, 
+     unt: true 
+  };
+
   // Returns the query string to pass to the diff page or to the diff endpoint. 
   // Input is the name of the test and the two digests to compare. 
   gold.diffQuery = function(test, left, right) {
@@ -21,6 +31,25 @@ var gold = gold || {};
   // diff endpoint. 
   gold.detailQuery = function(test, digest) {
     return '?test=' + test + '&digest=' + digest;
+  };
+
+  // stateFromQuery returns a state object based on the query portion of the URL. 
+  gold.stateFromQuery = function(defaultState) {
+    var delta = sk.query.toObject(window.location.search.slice(1), defaultState);
+    return sk.object.applyDelta(delta, defaultState);
   }; 
 
+  // queryFromState returns a query string from the the given state object.
+  gold.queryFromState = function(srcState) {
+    var ret = sk.query.fromObject(srcState); 
+    if (ret === '') {
+      return ''; 
+    }
+    return '?' + ret; 
+  }; 
+
+  // setQuery sets the current query on the URL without reloading. 
+  gold.setUrlQuery = function(q) {
+    history.pushState(null, "", window.location.origin + window.location.pathname + q);
+  };
 })();
