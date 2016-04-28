20429fcc5b105b5590203121e5e7726567b78fe5
kjlubick <kjlubick@google.com>
2016-04-28 13:03:33 -0700

    Add real adb to swarming bot
    BUG=skia:
    
    Review-Url: https://codereview.chromium.org/1927783003

--- skolo/raspberry-pi/setup-swarming.sh
+++ skolo/raspberry-pi/setup-swarming.sh
@@ -10,6 +10,7 @@ ln -s /usr/share/zoneinfo/US/Eastern /etc/localtime
 #If chrome-bot is already a member, this won't hurt
 for i in $(groups pi | cut -d " " -f 4-); do echo $i; adduser chrome-bot $i; done
 gpasswd -a chrome-bot plugdev
+gpasswd -a chrome-bot adb
 
 # Swarming requires a .boto file
 touch /home/chrome-bot/.boto
@@ -17,7 +18,7 @@ chown chrome-bot:chrome-bot /home/chrome-bot/.boto
 
 # This took a very long time for me.  Maybe just a fluke
 apt-get update
-apt-get install libusb-1.0-0-dev libssl-dev openssl time build-essential swig python-m2crypto ntpdate python-pip git
+apt-get install libusb-1.0-0-dev libssl-dev openssl time build-essential swig python-m2crypto ntpdate python-pip git android-tools-adb
 
 # Now to setup python-adb in /opt/adb
 cd /opt
