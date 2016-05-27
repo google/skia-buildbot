fe9c4ea21ac30acb2a2fb6d1bf685f7fe716e85a
jcgregorio <jcgregorio@google.com>
2016-05-27 08:07:18 -0700

    debugger: Attach a 1TB drive and install all the tools we need to run the debugger in a container.
    Also fixes the install.sh for fiddle.
    
    BUG=skia:
    R=rmistry
    
    Review-Url: https://codereview.chromium.org/2018693003

--- compute_engine_scripts/debugger/install.sh
+++ compute_engine_scripts/debugger/install.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 set -x
 
-/tmp/format_and_mount.sh fiddle
+/tmp/format_and_mount.sh debugger
 
 # The same set of packages need to be installed both on the instance and within the container.
 PACKAGES="systemd-container git debootstrap build-essential libosmesa-dev libfreetype6-dev libfontconfig-dev libpng12-dev libgif-dev libqt4-dev mesa-common-dev"
@@ -11,14 +11,14 @@ sudo apt-get --assume-yes install ${PACKAGES}
 
 # Install depot_tools
 cd /mnt/pd0
-mkdir fiddle
-mkdir fiddle/out
+mkdir debugger
+mkdir debugger/out
 git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
 echo -e "\nexport PATH=/mnt/pd0/depot_tools:\$PATH" >> ~/.bashrc
 
 # Build the containter
 CONTAINER=/mnt/pd0/container
-sudo debootstrap --arch=amd64 wily --include=${PACKAGES// /,} /mnt/pd0/container
+sudo debootstrap --arch=amd64 wily /mnt/pd0/container
 
 sudo rm $CONTAINER/etc/machine-id
 sudo rm $CONTAINER/etc/resolv.conf
--- compute_engine_scripts/debugger/vm_create_instance.sh
+++ compute_engine_scripts/debugger/vm_create_instance.sh
@@ -17,6 +17,15 @@ gcloud compute --project $PROJECT_ID disks create $INSTANCE_NAME \
   --source-snapshot $DEBUGGER_SOURCE_SNAPSHOT \
   --type "pd-standard"
 
+set +e
+# The cmd may fail if the disk already exists, which is fine.
+# Create a large data disk.
+gcloud compute --project $PROJECT_ID disks create $INSTANCE_NAME"-data" \
+  --size "1000" \
+  --zone $ZONE \
+  --type "pd-standard"
+set -e
+
 # Create the instance with the two disks attached.
 gcloud compute --project $PROJECT_ID instances create $INSTANCE_NAME \
   --zone $ZONE \
@@ -24,9 +33,10 @@ gcloud compute --project $PROJECT_ID instances create $INSTANCE_NAME \
   --network "default" \
   --maintenance-policy "MIGRATE" \
   --scopes $DEBUGGER_SCOPES \
-  --tags "http-server" "https-server" \
+  --tags "http-server,https-server" \
   --metadata "owner_primary=jcgregorio" \
-  --disk name=${INSTANCE_NAME}      device-name=${INSTANCE_NAME}      "mode=rw" "boot=yes" "auto-delete=yes" \
+  --disk "name=${INSTANCE_NAME},device-name=${INSTANCE_NAME},mode=rw,boot=yes,auto-delete=yes" \
+  --disk "name=${INSTANCE_NAME}-data,device-name=${INSTANCE_NAME}-data,mode=rw,boot=no" \
   --address=$DEBUGGER_IP_ADDRESS
 
 # The instance believes it is skia-systemd-snapshot-maker until it is rebooted.
@@ -38,3 +48,21 @@ gcloud compute --project $PROJECT_ID ssh $PROJECT_USER@$INSTANCE_NAME \
   --zone $ZONE \
   --command "sudo shutdown -r +1" \
   || echo "Reboot failed; please reboot the instance manually."
+
+# Wait for shutdown.
+sleep 120
+
+# Wait until the instance is up.
+until nc -w 1 -z $DEBUGGER_IP_ADDRESS 22; do
+    echo "Waiting for VM to come up."
+    sleep 2
+done
+
+gcloud compute copy-files ../common/format_and_mount.sh $PROJECT_USER@$INSTANCE_NAME:/tmp/format_and_mount.sh --zone $ZONE
+gcloud compute copy-files ../common/safe_format_and_mount $PROJECT_USER@$INSTANCE_NAME:/tmp/safe_format_and_mount --zone $ZONE
+gcloud compute copy-files install.sh $PROJECT_USER@$INSTANCE_NAME:/tmp/install.sh --zone $ZONE
+gcloud compute --project $PROJECT_ID ssh $PROJECT_USER@$INSTANCE_NAME \
+  --zone $ZONE \
+  --command "/tmp/install.sh" \
+  || echo "Installation failure."
+
