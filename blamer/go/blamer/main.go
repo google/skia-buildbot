1d7c36c934919a330c92215900350d9a21b2cce3
jcgregorio <jcgregorio@google.com>
2016-07-20 12:45:04 -0700

    blamer: A tool for figuring out which recent commit changed a file or class.
    For example:
    
      $ blamer --num 20 --match "SkDevice"
      E0720 15:31:03.036186   29490 common.go:37] Initializing logserver for log level ERROR.
      commit ea70c4bb22394c8dcc29a369d3422a2b8f3b3e80
      Author: robertphillips <robertphillips@google.com>
      Date:   Wed Jul 20 08:54:31 2016 -0700
    
          Remove SkDevice::accessRenderTarget virtual
          GOLD_TRYBOT_URL= https://gold.skia.org/search?issue=2167723002
    
          Review-Url: https://codereview.chromium.org/2167723002
    
      commit e51c356ae4e074b9c286c50a4efce11205f7463c
      Author: reed <reed@google.com>
      Date:   Tue Jul 19 14:33:20 2016 -0700
    
          pre-land special methods on device
    
          BUG=skia:
          GOLD_TRYBOT_URL= https://gold.skia.org/search?issue=2161233002
    
          Review-Url: https://codereview.chromium.org/2161233002
    
    BUG=skia:
    R=kjlubick
    
    Review-Url: https://codereview.chromium.org/2166063002

---
+++ blamer/go/blamer/main.go
@@ -0,0 +1,64 @@
+// blamer searches through git history and does text searches on the full patch
+// text and the commit message. If any matches are found then the commit
+// message is displayed.
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/skia-dev/glog"
+	"go.skia.org/infra/go/common"
+	"go.skia.org/infra/go/exec"
+)
+
+// flags
+var (
+	num     = flag.Int("num", 10, "The last N commits to search through.")
+	match   = flag.String("match", "SkEdgeClipper", "The case-sensitive text to search for.")
+	verbose = flag.Bool("verbose", true, "Show the commit message for each match, otherwise just display the git hash.")
+)
+
+func init() {
+	flag.Usage = func() {
+		fmt.Printf("Usage: %s <flags>\n\n", os.Args[0])
+		fmt.Printf(`%s searches through git history and does text searches on the full patch
+text and the commit message. If any matches are found then the commit
+message is displayed. Must be run within the git repo.
+
+`, os.Args[0])
+		flag.PrintDefaults()
+	}
+}
+func main() {
+	common.Init()
+	match_found := false
+	for i := 0; i < *num; i++ {
+		res, err := exec.RunSimple(fmt.Sprintf("git show HEAD~%d..HEAD~%d", i+1, i))
+		if err != nil {
+			glog.Fatalf("Failed to get the git info: %s", err)
+		}
+		// Each response begins with "commit 3f61d....ffb2\n", so use
+		// that to slice out the git hash.
+		parts := strings.SplitN(res, "\n", 2)
+		firstLine := strings.Split(parts[0], " ")
+		githash := firstLine[1]
+		if strings.Index(res, *match) >= 0 {
+			match_found = true
+			if *verbose {
+				msg, err := exec.RunSimple(fmt.Sprintf("git show --no-patch %s", githash))
+				if err != nil {
+					glog.Fatalf("Failed to get commit details: %s", err)
+				}
+				fmt.Printf("%s\n\n", msg)
+			} else {
+				fmt.Printf("%s\n", githash)
+			}
+		}
+	}
+	if !match_found {
+		fmt.Println("No matches found.")
+	}
+}
