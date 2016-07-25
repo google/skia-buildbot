e9ef5fb36d9b76adaadeb2ba2f03bdb89461df4b
rmistry <rmistry@google.com>
2016-07-25 07:14:25 -0700

    Install Python 2.7.11 on CT bots
    BUG=skia:5562
    
    Review-Url: https://codereview.chromium.org/2182483002

--- ct/tools/setup_ct_machine.sh
+++ ct/tools/setup_ct_machine.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 #
 # Setup the files and checkouts on a cluster telemetry machine.
-# WARNING: This script is out-of-date for non-swarming CT bots.
 #
+
+set -e
 
 # Install required packages.
 sudo apt-get update;
 sudo apt-get -y install python-django libgif-dev lua5.2 && \
     sudo easy_install -U pip && sudo pip install setuptools \
     --no-use-wheel --upgrade && sudo pip install -U crcmod
+
+# Install Python 2.7.11. See skbug.com/5562 for context.
+sudo apt-get -y install autotools-dev blt-dev bzip2 dpkg-dev g++-multilib \
+    gcc-multilib libbluetooth-dev libbz2-dev libexpat1-dev libffi-dev libffi6 \
+    libffi6-dbg libgdbm-dev libgpm2 libncursesw5-dev libreadline-dev \
+    libsqlite3-dev libssl-dev libtinfo-dev mime-support net-tools netbase \
+    python-crypto python-mox3 python-pil python-ply quilt tk-dev zlib1g-dev
+wget https://www.python.org/ftp/python/2.7.11/Python-2.7.11.tgz
+tar xfz Python-2.7.11.tgz
+cd Python-2.7.11/
+./configure --prefix /usr/local/lib/python2.7.11 --enable-ipv6
+make
+sudo make install
 
 # Checkout depot_tools
 cd /b/
