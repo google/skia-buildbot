7ee5b81af992956bfea156440a9a868fed8910c9
rmistry <rmistry@google.com>
2016-07-29 05:17:16 -0700

    Add two more packages to the CT setup script
    BUG=chromium:620393
    TBR=benjaminwagner
    
    Review-Url: https://codereview.chromium.org/2193193002

--- ct/tools/setup_ct_machine.sh
+++ ct/tools/setup_ct_machine.sh
@@ -16,7 +16,8 @@ sudo apt-get -y install autotools-dev blt-dev bzip2 dpkg-dev g++-multilib \
     gcc-multilib libbluetooth-dev libbz2-dev libexpat1-dev libffi-dev libffi6 \
     libffi6-dbg libgdbm-dev libgpm2 libncursesw5-dev libreadline-dev \
     libsqlite3-dev libssl-dev libtinfo-dev mime-support net-tools netbase \
-    python-crypto python-mox3 python-pil python-ply quilt tk-dev zlib1g-dev
+    python-crypto python-mox3 python-pil python-ply quilt tk-dev zlib1g-dev \
+    mesa-utils android-tools-adb
 wget https://www.python.org/ftp/python/2.7.11/Python-2.7.11.tgz
 tar xfz Python-2.7.11.tgz
 cd Python-2.7.11/
