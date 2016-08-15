b3e65910018762c87e786a3b7bd37a601cec24d0
rmistry <rmistry@google.com>
2016-08-15 13:11:22 -0700

    Chrome extension for Skia developers.
    Chrome extension that displays the Skia pencil icon with the state of the
    Skia tree (green, yellow, red) and how long it has been in that state.
    Highlights:
    * When the tree changes state a Chrome notification with the tree status
      message is displayed.
    * Mousing over the extension will display the tree status message.
    * Clicking on the extension will open a popup with more information.
    * The popup displays: trooper/sheriff/wrangler, perf/gold alerts, roll
      status, infra alerts count.
    * The status of the last build for all Skia masters is also displayed. Note: Does not get this data from status.skia.org, if this extension is useful for others we can look into adding something into status page.
    
    Screenshot: https://screenshot.googleplex.com/ajSu50KTF8f.png
    
    BUG=skia:5622
    
    Review-Url: https://codereview.chromium.org/2228513002

---
+++ chromeextensions/skia-dev/background.js
@@ -0,0 +1,94 @@
+// Copyright (c) 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+(function() {
+
+  function updateBadgeOnErrorStatus() {
+    chrome.browserAction.setBadgeText({text:'?'});
+    chrome.browserAction.setBadgeBackgroundColor({color:[0,0,255,255]});
+  }
+
+  var lastNotification = null;
+  function notifyStatusChange(treeState, status) {
+    if (lastNotification)
+      lastNotification.close();
+
+    lastNotification = new Notification('Skia Tree is ' + treeState, {
+      icon: chrome.extension.getURL('pencil128_' + treeState + '.png'),
+      body: status
+    });
+  }
+
+  function updateTimeBadge(lastChangeTime) {
+    chrome.browserAction.setBadgeText(
+      {text: sk.human.diffDate(lastChangeTime)});
+  }
+
+  // The type parameter should be 'open', 'closed', or 'caution'.
+  function getLastStatusTime(callback, type) {
+    sk.get('http://skia-tree-status.appspot.com/allstatus?limit=20&format=json').then(function(text) {
+      var entries = JSON.parse(text);
+      
+      for (var i = 0; i < entries.length; i++) {
+        if (entries[i].general_state == type) {
+          callback(new Date(entries[i].date + ' UTC'));
+          return;
+        }
+      }
+    }).catch(updateBadgeOnErrorStatus);
+  }
+
+  var lastState;
+  var lastChangeTime;
+  function updateStatus(status) {
+    var badgeState = {
+      open: {color: 'green', defaultText: '\u2022'},
+      closed: {color: 'red', defaultText: '\u00D7'},
+      caution: {color: '#CDCD00', defaultText: '!'}
+    };
+
+    chrome.browserAction.setTitle({title:status});
+    var treeState = (/open/i).exec(status) ? 'open' :
+        (/caution/i).exec(status) ? 'caution' : 'closed';
+
+    if (lastState && lastState != treeState) {
+      notifyStatusChange(treeState, status);
+    }
+
+    chrome.browserAction.setBadgeBackgroundColor(
+        {color: badgeState[treeState].color});
+
+    if (lastChangeTime === undefined) {
+      chrome.browserAction.setBadgeText(
+          {text: badgeState[treeState].defaultText});
+      lastState = treeState;
+      getLastStatusTime(function(time) {
+        lastChangeTime = time;
+        updateTimeBadge(lastChangeTime);
+      }, treeState);
+    } else {
+      if (treeState != lastState) {
+        lastState = treeState;
+        // The change event will occur 1/2 the polling frequency before we
+        // are aware of it, on average.
+        lastChangeTime = Date.now() - 10000 / 2;
+      }
+      updateTimeBadge(lastChangeTime);
+    }
+  }
+
+  function requestStatus() {
+    sk.get('http://skia-tree-status.appspot.com/current?format=raw')
+        .then(updateStatus)
+        .catch(updateBadgeOnErrorStatus);
+    setTimeout(requestStatus, 10000);
+  }
+
+  function main() {
+    requestStatus();
+  }
+
+  main();
+
+})();
---
+++ chromeextensions/skia-dev/popup.js
@@ -0,0 +1,150 @@
+// Copyright (c) 2016 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+(function(){
+
+  function linkify(wrapMe, link) {
+    return '<a href="' + link + '" target="_blank">' + wrapMe + '</a>'
+  }
+
+  function setTreeStatus() {
+    var statusURL = 'http://skia-tree-status.appspot.com/allstatus?limit=1&format=json';
+    sk.get(statusURL).then(function(resp) {
+      var entry = JSON.parse(resp)[0];
+      var d = new Date(entry.date + ' UTC');
+      var delta = sk.human.diffDate(d);
+      var messageLink = linkify(
+          entry.message, 'http://skia-tree-status.appspot.com/');
+      var statusSummary = '<b>' + messageLink + '</b> [' +
+                          entry.username.split('@')[0] + ' ' + delta + ' ago]';
+      document.getElementById('tree-status').innerHTML = 'Tree status: ' + statusSummary;
+    }).catch(function() {
+      document.getElementById('errors').innerHTML += 'Error connecting to skia-tree-status</br>';
+    });
+  }
+
+  function setTrooperSheriffWrangler() {
+    var urls = ['http://skia-tree-status.appspot.com/current-trooper',
+                'http://skia-tree-status.appspot.com/current-sheriff',
+                'http://skia-tree-status.appspot.com/current-gpu-sheriff']
+    urls.forEach(function(url) {
+      sk.get(url).then(function(resp) {
+        var tokens = url.split('/');
+        var idName = tokens[tokens.length - 1];
+        var username = JSON.parse(resp).username.split('@')[0];
+        var usernameLink = linkify(username, 'http://who/' + username);
+        document.getElementById(idName).innerHTML = usernameLink;
+      }).catch(function() {
+        document.getElementById('errors').innerHTML += 'Error connecting to ' + url + '</br>';
+      });
+    });
+  }
+
+  function setPerfAlerts() {
+    var perfURL = 'https://status.skia.org/json/perfAlerts';
+    sk.get(perfURL).then(function(resp) {
+      var numAlerts = JSON.parse(resp).alerts;
+      document.getElementById('perf-alerts').innerHTML =
+        linkify(numAlerts, 'https://perf.skia.org/alerts/');
+    }).catch(function() {
+      document.getElementById('errors').innerHTML += 'Error connecting to perfAlerts</br>';
+    });
+  }
+
+  function setGoldAlerts() {
+    var goldURL = 'https://status.skia.org/json/goldStatus';
+    sk.get(goldURL).then(function(resp) {
+      var json = JSON.parse(resp);
+      document.getElementById('gm-alerts').innerHTML =
+          linkify(json.gm, 'https://gold.skia.org/list?query=source_type%3Dgm');
+      document.getElementById('image-alerts').innerHTML =
+          linkify(json.image, 'https://gold.skia.org/list?query=source_type%3Dimage');
+    }).catch(function() {
+      document.getElementById('errors').innerHTML += 'Error connecting to goldStatus</br>';
+    });
+  }
+
+  function setRollStatus() {
+    var rollURL = 'https://autoroll.skia.org/json/status';
+    sk.get(rollURL).then(function(resp) {
+      var json = JSON.parse(resp);
+      var currentStatus = json.currentRoll ? json.currentRoll.result : 'up to date';
+      document.getElementById('current-roll').innerHTML =
+          linkify(currentStatus, 'https://autoroll.skia.org/');
+      document.getElementById('last-roll').innerHTML =
+          linkify(json.lastRoll.result, 'https://autoroll.skia.org/');
+    }).catch(function() {
+      document.getElementById('errors').innerHTML += 'Error connecting to autoroller</br>';
+    });
+  }
+
+  function setInfraAlerts() {
+    var alertsURL = 'https://alerts.skia.org/json/alerts/';
+    sk.get(alertsURL).then(function(resp) {
+      var alerts = JSON.parse(resp);
+      var activeInfraAlerts = 0;
+      for (var i = 0; i < alerts.length; i++) {
+        if (alerts[i]['category'] == 'infra' && alerts[i]['snoozedUntil'] == 0) {
+          activeInfraAlerts++;
+        }
+      }
+      document.getElementById('infra-alerts').innerHTML =
+        linkify(activeInfraAlerts, 'http://alerts.skia.org/infra');
+    }).catch(function() {
+      document.getElementById('errors').innerHTML += 'Error connecting to alertserver</br>';
+    });
+  }
+
+  function addMasterStatusRow(master) {
+    var table = document.getElementById('status-table');
+
+    var baseURL = 'http://build.chromium.org/p/' + master;
+    var consoleURL = baseURL + '/console';
+    var statusURL = baseURL + '/horizontal_one_box_per_builder';
+
+    var row = table.insertRow(-1);
+    row.className = 'trunk-status-row ' + master;
+    var label = row.insertCell(-1);
+    label.className = 'status-label';
+    var labelAnchor = document.createElement('a');
+    labelAnchor.href = consoleURL;
+    labelAnchor.target = '_blank';
+    labelAnchor.id = 'link_' + master;
+    labelAnchor.textContent = master;
+    label.appendChild(labelAnchor);
+
+    var status = row.insertCell(-1);
+    status.className = 'trunk-status-cell';
+    var statusIframe = document.createElement('iframe');
+    statusIframe.scrolling = 'no';
+    statusIframe.src = statusURL;
+    status.appendChild(statusIframe);
+  }
+
+  function addMasterStatusRows() {
+    var masters = [
+      'client.skia',
+      'client.skia.android',
+      'client.skia.compile',
+      'client.skia.fyi'
+    ];
+
+    masters.forEach(function(master) {
+      addMasterStatusRow(master);
+    });
+  }
+
+  function main() {
+    setTreeStatus();
+    setTrooperSheriffWrangler();
+    setPerfAlerts();
+    setGoldAlerts();
+    setRollStatus();
+    setInfraAlerts();
+    addMasterStatusRows();
+  }
+
+  main();
+
+})();
