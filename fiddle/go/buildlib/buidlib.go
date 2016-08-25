7d455b01ba31a219a6944b311a75b81f97956384
jcgregorio <jcgregorio@google.com>
2016-08-25 09:28:49 -0700

    fiddle: The default build of out/Release/fiddle should run before we accept a git revision as good.
    This will keep fiddle.skia.org up and running in the face of these problems.
    
    BUG=skia:
    
    Review-Url: https://codereview.chromium.org/2281593002

--- fiddle/go/buildlib/buidlib.go
+++ fiddle/go/buildlib/buidlib.go
@@ -2,9 +2,11 @@ package buildlib
 
 import (
 	"fmt"
+	"path/filepath"
 
 	"github.com/skia-dev/glog"
 	"go.skia.org/infra/go/buildskia"
+	"go.skia.org/infra/go/exec"
 )
 
 // BuildLib, given a directory that Skia is checked out into, builds libskia.a
@@ -19,5 +21,18 @@ func BuildLib(checkout, depotTools string) error {
 	if msg, err := buildskia.GNNinjaBuild(checkout, depotTools, "Release", "fiddle", true); err != nil {
 		return fmt.Errorf("Failed ninja build of fiddle: %q %s", msg, err)
 	}
+
+	glog.Info("Running the default fiddle")
+	runFiddleCmd := &exec.Command{
+		Name:      filepath.Join(checkout, "skia", "out", "Release", "fiddle"),
+		Dir:       filepath.Join(checkout, "skia"),
+		LogStderr: true,
+		LogStdout: true,
+	}
+
+	if err := exec.Run(runFiddleCmd); err != nil {
+		return fmt.Errorf("Failed to run the default fiddle: %s", err)
+	}
+
 	return nil
 }
