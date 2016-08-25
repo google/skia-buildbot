2ba09168da297b7b63ebaed10e3023c9dc6e8c76
jcgregorio <jcgregorio@google.com>
2016-08-25 05:38:41 -0700

    perf: Add dataframe, the new Tile replacement for Perf.
    BUG=skia:
    
    Review-Url: https://codereview.chromium.org/2275043002

---
+++ perf/go/dataframe/dataframe.go
@@ -0,0 +1,110 @@
+// Package dataframe provides DataFrame which is a TraceSet with a calculated
+// ParamSet and associated commit info.
+package dataframe
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/skia-dev/glog"
+	"go.skia.org/infra/go/paramtools"
+	"go.skia.org/infra/go/query"
+	"go.skia.org/infra/go/vcsinfo"
+	"go.skia.org/infra/perf/go/ptracestore"
+)
+
+const (
+	// DEFAULT_NUM_COMMITS is the number of commits in the DataFrame returned
+	// from New().
+	DEFAULT_NUM_COMMITS = 50
+)
+
+// ColumnHeader describes each column in a DataFrame.
+type ColumnHeader struct {
+	Source    string    `json:"source"`
+	ID        string    `json:"id"`
+	Desc      string    `json:"desc"`
+	Timestamp time.Time `json:"timestamp"`
+}
+
+// DataFrame stores Perf measurements in a table where each row is a Trace
+// indexed by a structured key (see go/query), and each column is described by
+// a ColumnHeader, which could be a commit or a trybot patch level.
+//
+// The name DataFrame was gratuitously borrowed from R.
+type DataFrame struct {
+	TraceSet ptracestore.TraceSet
+	Header   []*ColumnHeader
+	ParamSet paramtools.ParamSet
+}
+
+// rangeImpl returns the slices of ColumnHeader and ptracestore.CommitID that
+// are needed by DataFrame and ptracestore.PTraceStore, respectively. The
+// slices are populated from the given vcsinfo.IndexCommits.
+func rangeImpl(resp []*vcsinfo.IndexCommit) ([]*ColumnHeader, []*ptracestore.CommitID) {
+	headers := []*ColumnHeader{}
+	commits := []*ptracestore.CommitID{}
+	for _, r := range resp {
+		commits = append(commits, &ptracestore.CommitID{
+			Offset: r.Index,
+			Source: "master",
+		})
+		headers = append(headers, &ColumnHeader{
+			Source:    "master",
+			ID:        fmt.Sprintf("%d", r.Index),
+			Timestamp: r.Timestamp,
+		})
+	}
+	return headers, commits
+}
+
+// lastN returns the slices of ColumnHeader and ptracestore.CommitID that are
+// needed by DataFrame and ptracestore.PTraceStore, respectively. The slices
+// are for the last 50 commits in the repo.
+func lastN(vcs vcsinfo.VCS) ([]*ColumnHeader, []*ptracestore.CommitID) {
+	if err := vcs.Update(true, false); err != nil {
+		glog.Errorf("Failed to update repo: %s", err)
+	}
+	return rangeImpl(vcs.LastNIndex(DEFAULT_NUM_COMMITS))
+}
+
+// getRange returns the slices of ColumnHeader and ptracestore.CommitID that are
+// needed by DataFrame and ptracestore.PTraceStore, respectively. The slices
+// are for the commits that fall in the given time range [begin, end).
+func getRange(vcs vcsinfo.VCS, begin, end time.Time) ([]*ColumnHeader, []*ptracestore.CommitID) {
+	if err := vcs.Update(true, false); err != nil {
+		glog.Errorf("Failed to update repo: %s", err)
+	}
+	return rangeImpl(vcs.Range(begin, end))
+}
+
+func _new(colHeaders []*ColumnHeader, commitIDs []*ptracestore.CommitID, q query.Query, store ptracestore.PTraceStore) (*DataFrame, error) {
+	traceSet, err := store.Match(commitIDs, q)
+	if err != nil {
+		return nil, fmt.Errorf("DataFrame failed to query for all traces: %s", err)
+	}
+	paramSet := paramtools.ParamSet{}
+	for key, _ := range traceSet {
+		paramSet.AddParamsFromKey(key)
+	}
+	return &DataFrame{
+		TraceSet: traceSet,
+		Header:   colHeaders,
+		ParamSet: paramSet,
+	}, nil
+}
+
+// New returns a populated DataFrame of the last 50 commits given the 'vcs' and
+// 'store', or a non-nil error if there was a failure retrieving the traces.
+func New(vcs vcsinfo.VCS, store ptracestore.PTraceStore) (*DataFrame, error) {
+	colHeaders, commitIDs := lastN(vcs)
+	return _new(colHeaders, commitIDs, query.Query{}, store)
+}
+
+// NewFromQueryAndRange returns a populated DataFrame of the traces that match
+// the given time range [begin, end) and the passed in query, or a non-nil
+// error if the traces can't be retrieved.
+func NewFromQueryAndRange(vcs vcsinfo.VCS, store ptracestore.PTraceStore, begin, end time.Time, q query.Query) (*DataFrame, error) {
+	colHeaders, commitIDs := getRange(vcs, begin, end)
+	return _new(colHeaders, commitIDs, q, store)
+}
---
+++ perf/go/dataframe/dataframe_test.go
@@ -0,0 +1,184 @@
+package dataframe
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+
+	"go.skia.org/infra/go/query"
+	"go.skia.org/infra/go/testutils"
+	"go.skia.org/infra/go/util"
+	"go.skia.org/infra/go/vcsinfo"
+	"go.skia.org/infra/perf/go/ptracestore"
+)
+
+type mockVcs struct {
+	commits    []*vcsinfo.IndexCommit
+	updateFail bool
+}
+
+func (m *mockVcs) From(start time.Time) []string                     { return nil }
+func (m *mockVcs) Range(begin, end time.Time) []*vcsinfo.IndexCommit { return nil }
+func (m *mockVcs) Details(hash string, includeBranchInfo bool) (*vcsinfo.LongCommit, error) {
+	return nil, nil
+}
+
+func (m *mockVcs) Update(pull, allBranches bool) error {
+	if m.updateFail {
+		return fmt.Errorf("Failed to update.")
+	}
+	return nil
+}
+
+func (m *mockVcs) LastNIndex(N int) []*vcsinfo.IndexCommit {
+	return m.commits
+}
+
+type mockPTraceStore struct {
+	traceSet  ptracestore.TraceSet
+	matchFail bool
+}
+
+func (m mockPTraceStore) Add(commitID *ptracestore.CommitID, values map[string]float32, sourceFile string) error {
+	return nil
+}
+
+func (m mockPTraceStore) Details(commitID *ptracestore.CommitID, traceID string) (string, float32, error) {
+	return "", 0, nil
+}
+
+func (m mockPTraceStore) Match(commitIDs []*ptracestore.CommitID, q query.Query) (ptracestore.TraceSet, error) {
+	if m.matchFail {
+		return nil, fmt.Errorf("Failed to retrieve traces.")
+	}
+	return m.traceSet, nil
+}
+
+var (
+	ts0 = time.Unix(1406721642, 0).UTC()
+	ts1 = time.Unix(1406721715, 0).UTC()
+
+	commits = []*vcsinfo.IndexCommit{
+		&vcsinfo.IndexCommit{
+			Hash:      "7a669cfa3f4cd3482a4fd03989f75efcc7595f7f",
+			Index:     0,
+			Timestamp: ts0,
+		},
+		&vcsinfo.IndexCommit{
+			Hash:      "8652a6df7dc8a7e6addee49f6ed3c2308e36bd18",
+			Index:     1,
+			Timestamp: ts1,
+		},
+	}
+
+	store = mockPTraceStore{
+		traceSet: ptracestore.TraceSet{
+			",arch=x86,config=565,":  ptracestore.Trace{1.2, 2.1},
+			",arch=x86,config=8888,": ptracestore.Trace{1.3, 3.1},
+			",arch=x86,config=gpu,":  ptracestore.Trace{1.4, 4.1},
+		},
+	}
+)
+
+func TestRangeImpl(t *testing.T) {
+
+	expected_headers := []*ColumnHeader{
+		&ColumnHeader{
+			Source:    "master",
+			ID:        "0",
+			Desc:      "",
+			Timestamp: ts0,
+		},
+		&ColumnHeader{
+			Source:    "master",
+			ID:        "1",
+			Desc:      "",
+			Timestamp: ts1,
+		},
+	}
+	expected_pcommits := []*ptracestore.CommitID{
+		&ptracestore.CommitID{
+			Offset: 0,
+			Source: "master",
+		},
+		&ptracestore.CommitID{
+			Offset: 1,
+			Source: "master",
+		},
+	}
+
+	headers, pcommits := rangeImpl(commits)
+	assert.Equal(t, 2, len(headers))
+	assert.Equal(t, 2, len(pcommits))
+	testutils.AssertDeepEqual(t, expected_headers, headers)
+	testutils.AssertDeepEqual(t, expected_pcommits, pcommits)
+
+	headers, pcommits = rangeImpl([]*vcsinfo.IndexCommit{})
+	assert.Equal(t, 0, len(headers))
+	assert.Equal(t, 0, len(pcommits))
+}
+
+func TestNew(t *testing.T) {
+	colHeaders := []*ColumnHeader{
+		&ColumnHeader{
+			Source:    "master",
+			ID:        "0",
+			Desc:      "",
+			Timestamp: ts0,
+		},
+		&ColumnHeader{
+			Source:    "master",
+			ID:        "1",
+			Desc:      "",
+			Timestamp: ts1,
+		},
+	}
+	pcommits := []*ptracestore.CommitID{
+		&ptracestore.CommitID{
+			Offset: 0,
+			Source: "master",
+		},
+		&ptracestore.CommitID{
+			Offset: 1,
+			Source: "master",
+		},
+	}
+	store.matchFail = false
+
+	d, err := _new(colHeaders, pcommits, query.Query{}, store)
+	assert.NoError(t, err)
+	assert.Equal(t, 3, len(d.TraceSet))
+	assert.True(t, util.SSliceEqual(d.ParamSet["arch"], []string{"x86"}))
+	assert.True(t, util.SSliceEqual(d.ParamSet["config"], []string{"8888", "565", "gpu"}))
+}
+
+func TestVCS(t *testing.T) {
+	vcs := &mockVcs{
+		commits: commits,
+	}
+	store.matchFail = false
+
+	d, err := New(vcs, store)
+	assert.NoError(t, err)
+	assert.Equal(t, 3, len(d.TraceSet))
+
+	d, err = NewFromQueryAndRange(vcs, store, ts0, ts1.Add(time.Second), query.Query{})
+	assert.NoError(t, err)
+	assert.Equal(t, 3, len(d.TraceSet))
+
+	// Test error conditions, i.e. that we log only and don't return an error.
+	vcs.updateFail = true
+	_, err = New(vcs, store)
+	assert.NoError(t, err)
+	_, err = NewFromQueryAndRange(vcs, store, ts0, ts1.Add(time.Second), query.Query{})
+	assert.NoError(t, err)
+
+	store.matchFail = true
+	// Test error conditions if the store fails.
+	_, err = New(vcs, store)
+	assert.Error(t, err)
+	_, err = NewFromQueryAndRange(vcs, store, ts0, ts1.Add(time.Second), query.Query{})
+	assert.Error(t, err)
+}
