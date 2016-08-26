f921b2b68b61450cf026f06705de67ed2ee53b3c
borenet <borenet@chromium.org>
2016-08-26 11:24:04 -0700

    Add Trie implementation
    Uses sets of strings as keys
    
    BUG=skia:5626
    
    Review-Url: https://codereview.chromium.org/2281033002

---
+++ go/trie/trie.go
@@ -0,0 +1,150 @@
+package trie
+
+import (
+	"fmt"
+	"sort"
+
+	"go.skia.org/infra/go/util"
+)
+
+// Trie is a struct used for efficient searching on sets of strings.
+type Trie struct {
+	root *trieNode
+}
+
+// New returns a Trie instance.
+func New() *Trie {
+	return &Trie{
+		root: newTrieNode(),
+	}
+}
+
+func sorted(s []string) []string {
+	cpy := make([]string, len(s))
+	copy(cpy, s)
+	sort.Strings(cpy)
+	return cpy
+}
+
+// Insert inserts the data into the trie with the given string keys.
+func (t *Trie) Insert(strs []string, data interface{}) {
+	t.root.Insert(sorted(strs), data)
+}
+
+// Delete removes the data from the trie.
+func (t *Trie) Delete(strs []string, data interface{}) {
+	t.root.Delete(sorted(strs), data)
+}
+
+// Search returns all inserted data which exactly matches the given string keys.
+func (t *Trie) Search(strs []string) []interface{} {
+	return t.root.Search(sorted(strs))
+}
+
+type searchContext struct {
+	count int
+	data  [][]interface{}
+}
+
+// SearchSubset returns all inserted data which matches a subset of the given
+// string keys.
+func (t *Trie) SearchSubset(strs []string) []interface{} {
+	keys := make(map[string]bool, len(strs))
+	for _, k := range strs {
+		keys[k] = true
+	}
+	ctx := searchContext{
+		count: 0,
+		data:  [][]interface{}{},
+	}
+	t.root.SearchSubset(keys, &ctx)
+	rv := make([]interface{}, ctx.count)
+	idx := 0
+	for _, d := range ctx.data {
+		copy(rv[idx:idx+len(d)], d)
+		idx += len(d)
+	}
+	return rv
+}
+
+// String returns a string representation of the Trie.
+func (t *Trie) String() string {
+	return fmt.Sprintf("Trie(%s)", t.root.String(0))
+}
+
+type trieNode struct {
+	children map[string]*trieNode
+	data     []interface{}
+}
+
+func newTrieNode() *trieNode {
+	return &trieNode{
+		children: map[string]*trieNode{},
+		data:     []interface{}{},
+	}
+}
+
+func (n *trieNode) Insert(strs []string, data interface{}) {
+	if len(strs) == 0 {
+		n.data = append(n.data, data)
+	} else {
+		child, ok := n.children[strs[0]]
+		if !ok {
+			child = newTrieNode()
+			n.children[strs[0]] = child
+		}
+		child.Insert(strs[1:], data)
+	}
+}
+
+func (n *trieNode) Delete(strs []string, data interface{}) {
+	if len(strs) == 0 {
+		idx := -1
+		for i, v := range n.data {
+			if v == data {
+				idx = i
+			}
+		}
+		if idx == -1 {
+			return
+		}
+		n.data = append(n.data[:idx], n.data[idx+1:]...)
+	} else if child, ok := n.children[strs[0]]; ok {
+		child.Delete(strs[1:], data)
+	}
+}
+
+func (n *trieNode) Search(strs []string) []interface{} {
+	if len(strs) == 0 {
+		return n.data
+	} else {
+		if child, ok := n.children[strs[0]]; ok {
+			return child.Search(strs[1:])
+		} else {
+			return []interface{}{}
+		}
+	}
+}
+
+func (n *trieNode) SearchSubset(strs map[string]bool, ctx *searchContext) {
+	ctx.count += len(n.data)
+	ctx.data = append(ctx.data, n.data)
+	for k, c := range n.children {
+		if strs[k] {
+			c.SearchSubset(strs, ctx)
+		}
+	}
+}
+
+func (n *trieNode) String(indent int) string {
+	rv := fmt.Sprintf("Node(%v, {", n.data)
+	if len(n.children) == 0 {
+		return rv + "})"
+	}
+	rv += "\n"
+	for k, c := range n.children {
+		rv += fmt.Sprintf("%s\"%s\": %s,\n", util.RepeatJoin("  ", "", indent+1), k, c.String(indent+1))
+	}
+	rv += fmt.Sprintf("%s})", util.RepeatJoin("  ", "", indent))
+	return rv
+}
---
+++ go/trie/trie_test.go
@@ -0,0 +1,68 @@
+package trie
+
+import (
+	"sort"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"go.skia.org/infra/go/testutils"
+)
+
+func TestTrie(t *testing.T) {
+	trie := New()
+	trie.Insert([]string{"a", "b", "c"}, "1")
+	trie.Insert([]string{"a", "b"}, "2")
+	trie.Insert([]string{"c", "b", "a"}, "3")
+	trie.Insert([]string{}, "4")
+	trie.Insert([]string{"c", "d"}, "5")
+	searchSubset := func(search []string) []string {
+		got := trie.SearchSubset(search)
+		rv := make([]string, 0, len(got))
+		for _, s := range got {
+			rv = append(rv, s.(string))
+		}
+		sort.Strings(rv)
+		return rv
+	}
+	searchExact := func(search []string) []string {
+		got := trie.Search(search)
+		rv := make([]string, 0, len(got))
+		for _, s := range got {
+			rv = append(rv, s.(string))
+		}
+		sort.Strings(rv)
+		return rv
+	}
+	testutils.AssertDeepEqual(t, []string{"4"}, searchSubset([]string{}))
+	testutils.AssertDeepEqual(t, []string{"4"}, searchExact([]string{}))
+	testutils.AssertDeepEqual(t, []string{"4"}, searchSubset([]string{"a"}))
+	testutils.AssertDeepEqual(t, []string{}, searchExact([]string{"a"}))
+	testutils.AssertDeepEqual(t, []string{"2", "4"}, searchSubset([]string{"a", "b"}))
+	testutils.AssertDeepEqual(t, []string{"2"}, searchExact([]string{"a", "b"}))
+	testutils.AssertDeepEqual(t, []string{"1", "2", "3", "4"}, searchSubset([]string{"a", "b", "c"}))
+	testutils.AssertDeepEqual(t, []string{"1", "3"}, searchExact([]string{"a", "b", "c"}))
+	testutils.AssertDeepEqual(t, []string{"1", "2", "3", "4", "5"}, searchSubset([]string{"d", "b", "a", "c"}))
+	testutils.AssertDeepEqual(t, []string{}, searchExact([]string{"d", "b", "a", "c"}))
+
+	trie.Delete([]string{"c", "b", "a"}, "3")
+
+	testutils.AssertDeepEqual(t, []string{"1", "2", "4"}, searchSubset([]string{"a", "b", "c"}))
+	testutils.AssertDeepEqual(t, []string{"1", "2", "4", "5"}, searchSubset([]string{"d", "b", "a", "c"}))
+}
+
+func TestString(t *testing.T) {
+	trie := New()
+	assert.Equal(t, "Trie(Node([], {}))", trie.String())
+	trie.Insert([]string{"a"}, "1")
+	assert.Equal(t, `Trie(Node([], {
+  "a": Node([1], {}),
+}))`, trie.String())
+	trie.Insert([]string{"b", "c"}, "2")
+	assert.Equal(t, `Trie(Node([], {
+  "a": Node([1], {}),
+  "b": Node([], {
+    "c": Node([2], {}),
+  }),
+}))`, trie.String())
+}
