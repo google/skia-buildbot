e387b77491249616e6865b94e677ecd14da24192
Ravi Mistry <rmistry@google.com>
2016-09-09 11:09:26 -0400

    Gerrit library checkpoint
    BUG=skia:5725
    
    Change-Id: I891f15cd98afa2fbbf52c69e1bb3ac958b21cb08

---
+++ go/gerrit/gerrit_test.go
@@ -0,0 +1,55 @@
+package gerrit
+
+import (
+	"testing"
+	"time"
+
+	assert "github.com/stretchr/testify/require"
+	"go.skia.org/infra/go/testutils"
+)
+
+// Basic test to make sure we can retrieve issues from Gerrit.
+func TestGerritSearch(t *testing.T) {
+	testutils.SkipIfShort(t)
+
+	api := NewGerrit("https://skia-review.googlesource.com", nil)
+	t_delta := time.Now().Add(-10 * 24 * time.Hour)
+	issues, err := api.Search(1, api.SearchModifiedAfter(t_delta))
+	assert.NoError(t, err)
+	assert.True(t, len(issues) > 0)
+
+	//for _, issue := range issues {
+	//	assert.True(t, issue.Modified.After(t_delta))
+	//	details, err := api.GetIssueProperties(issue.Issue, false)
+	//	assert.NoError(t, err)
+	//	assert.True(t, details.Modified.After(t_delta))
+	//	assert.True(t, len(details.Patchsets) > 0)
+	//}
+
+	//keys, err := api.SearchKeys(5, SearchModifiedAfter(time.Now().Add(-time.Hour)))
+	//assert.NoError(t, err)
+	//assert.Equal(t, 5, len(keys))
+}
+
+// Basic test to make sure we can retrieve issues from Rietveld.
+func TestRietveld(t *testing.T) {
+	testutils.SkipIfShort(t)
+
+	api := New("https://codereview.chromium.org", nil)
+	t_delta := time.Now().Add(-10 * 24 * time.Hour)
+	issues, err := api.Search(1, SearchModifiedAfter(t_delta))
+	assert.NoError(t, err)
+	assert.True(t, len(issues) > 0)
+
+	for _, issue := range issues {
+		assert.True(t, issue.Modified.After(t_delta))
+		details, err := api.GetIssueProperties(issue.Issue, false)
+		assert.NoError(t, err)
+		assert.True(t, details.Modified.After(t_delta))
+		assert.True(t, len(details.Patchsets) > 0)
+	}
+
+	keys, err := api.SearchKeys(5, SearchModifiedAfter(time.Now().Add(-time.Hour)))
+	assert.NoError(t, err)
+	assert.Equal(t, 5, len(keys))
+}
