50fdaccaa70d91f05cec9b87fd3bd6d819897d9e
Joe Gregorio <jcgregorio@google.com>
2016-09-19 16:16:40 -0400

    look up info from a commit id.
    BUG=skia:
    
    Change-Id: I277a017ff206a1f9a19fb28cce1a1e0e3a0eca5e

--- go/gitinfo/gitinfo.go
+++ go/gitinfo/gitinfo.go
@@ -280,6 +280,16 @@ func (g *GitInfo) IndexOf(hash string) (int, error) {
 	return n, nil
 }
 
+func (g *GitInfo) ByIndex(N int) (*vcsinfo.LongCommit, error) {
+	g.mutex.Lock()
+	numHashes := len(g.hashes)
+	g.mutex.Unlock()
+	if N < 0 || N >= numHashes {
+		return nil, fmt.Errorf("Hash index not found: %d", N)
+	}
+	return g.Details(g.hashes[N], false)
+}
+
 // LastN returns the last N commits.
 func (g *GitInfo) LastN(N int) []string {
 	g.mutex.Lock()
