079c64d8e229135de1d2523de4d2ddcb1b1e148e
Ravi Mistry <rmistry@google.com>
2016-09-19 12:22:02 -0400

    Go Gerrit API library
    BUG=skia:5702
    
    Change-Id: I891f15cd98afa2fbbf52c69e1bb3ac958b21cb08

--- go/buildbucket/buildbucket.go
+++ go/buildbucket/buildbucket.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"time"
 
 	"go.skia.org/infra/go/util"
@@ -264,7 +265,12 @@ func (c *Client) Search(url string) ([]*Build, error) {
 }
 
 // GetTrybotsForCL retrieves trybot results for the given CL.
-func (c *Client) GetTrybotsForCL(issueID int64, patchsetID int64) ([]*Build, error) {
-	url := fmt.Sprintf("%s/search?tag=buildset%%3Apatch%%2Frietveld%%2Fcodereview.chromium.org%%2F%d%%2F%d", apiUrl, issueID, patchsetID)
+func (c *Client) GetTrybotsForCL(issueID int64, patchsetID int64, patchStorage, crUrl string) ([]*Build, error) {
+	u, err := url.Parse(crUrl)
+	if err != nil {
+		return nil, err
+	}
+	host := u.Host
+	url := fmt.Sprintf("%s/search?tag=buildset%%3Apatch%%2F%s%%2F%s%%2F%d%%2F%d", apiUrl, patchStorage, host, issueID, patchsetID)
 	return c.Search(url)
 }
