4b55c3e53ba71e65601c84c7d9c8003b258e70a8
Joe Gregorio <jcgregorio@google.com>
2016-09-21 10:40:52 -0400

    Add scripts to extract perf json files from isolates and upload them.
    BUG=skia:
    
    Change-Id: Ia290f5cf5280a00225b114e4171e874afb09339e

---
+++ perf/scripts/upload_extracted_json_files.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Uploads all the JSON files under ./downloads to the Skia Perf bucket for
+# ingestion.
+
+gsutil \
+  -m cp -r \
+  downloads/ \
+  gs://skia-perf/nano-json-v1/$(date --utc --date +1hour +%Y/%m/%d/%H)
