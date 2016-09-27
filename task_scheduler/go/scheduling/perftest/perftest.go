a9ad1119532a8d4dc57a05e55da68472a10faec3
Ben Wagner <benjaminwagner@google.com>
2016-09-27 13:59:41 -0400

    Add expiration to JobCache.
    Expiration is based on revision timestamp so that jobs can be created after their dependent tasks but we will not have unexpired jobs that depend on expired tasks.
    
    BUG=skia:5723
    
    Change-Id: Id7e4ef29add423b4a2f6fe7bdee0166a6218313b

--- task_scheduler/go/scheduling/perftest/perftest.go
+++ task_scheduler/go/scheduling/perftest/perftest.go
@@ -113,13 +113,6 @@ func main() {
 			glog.Fatal(err)
 		}
 	}()
-	d, err := local_db.NewDB("testdb", path.Join(workdir, "tasks.db"))
-	assertNoError(err)
-	tCache, err := db.NewTaskCache(d, time.Hour)
-	assertNoError(err)
-	jCache, err := db.NewJobCache(d, time.Hour)
-	assertNoError(err)
-
 	repoName := "skia.git"
 	repoDir := path.Join(workdir, repoName)
 	assertNoError(os.Mkdir(path.Join(workdir, repoName), os.ModePerm))
@@ -261,6 +254,13 @@ func main() {
 	commits, err := repo.RevList(head)
 	assertNoError(err)
 	assertDeepEqual([]string{head}, commits)
+
+	d, err := local_db.NewDB("testdb", path.Join(workdir, "tasks.db"))
+	assertNoError(err)
+	tCache, err := db.NewTaskCache(d, time.Hour)
+	assertNoError(err)
+	jCache, err := db.NewJobCache(d, time.Hour, db.RepoMapGetRevisionTimestamp(repos))
+	assertNoError(err)
 
 	isolateClient, err := isolate.NewClient(workdir)
 	assertNoError(err)
