251f515e620ae749de314211e6b9be2c0d4679b0
Stephan Altmueller <stephana@google.com>
2016-09-29 11:46:36 -0400

    Fix pixel values output and add link to image info in Gold
    - Output the correct channel values for the chosen pixel
    - Add a link to https://imageinfo.skia.org/ for each digest
    
    BUG=skia:
    
    Change-Id: I527cde7b6a9febbba325d6f74690012551c86391

--- golden/frontend/res/js/gold.js
+++ golden/frontend/res/js/gold.js
@@ -67,6 +67,11 @@ var gold = gold || {};
     return '/img/images/' + digest + '.png'
   };
 
+  gold.imageInfoHref = function(digest) {
+    var imgUrl = window.location.protocol + "//" + window.location.hostname + gold.imgHref(digest);
+    return "https://imageinfo.skia.org/info?" + sk.query.fromObject({url: imgUrl});
+  },
+
   // Return the URL for the diff image between the two given digests.
   gold.diffImgHref = function(d1, d2) {
     if (!d1 || !d2) {
