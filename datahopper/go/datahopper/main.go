46188f4547e52d14902927ea8c9bf3d951e6fd88
Ravi Mistry <rmistry@google.com>
2016-10-03 14:52:20 -0400

    Status + Gerrit checkpoint
    BUG=skia:
    
    Change-Id: Ia010d092fde947c15e21c6b85ef4bf0fedba12d0

--- datahopper/go/datahopper/main.go
+++ datahopper/go/datahopper/main.go
@@ -6,26 +6,26 @@ package main
 
 import (
 	"flag"
-	"fmt"
+	//"fmt"
 	"path"
 	"regexp"
 	"strconv"
-	"strings"
+	//"strings"
 	"time"
 
-	"cloud.google.com/go/storage"
+	//"cloud.google.com/go/storage"
 	"github.com/skia-dev/glog"
-	"go.skia.org/infra/go/auth"
+	//"go.skia.org/infra/go/auth"
 	"go.skia.org/infra/go/buildbot"
 	"go.skia.org/infra/go/common"
 	"go.skia.org/infra/go/gitinfo"
-	"go.skia.org/infra/go/gs"
+	//"go.skia.org/infra/go/gs"
 	"go.skia.org/infra/go/influxdb"
 	"go.skia.org/infra/go/metrics2"
-	"go.skia.org/infra/go/swarming"
+	//"go.skia.org/infra/go/swarming"
 	"go.skia.org/infra/go/util"
-	"golang.org/x/net/context"
-	"google.golang.org/api/option"
+	//"golang.org/x/net/context"
+	//"google.golang.org/api/option"
 )
 
 const (
@@ -83,17 +83,17 @@ func main() {
 	common.InitWithMetrics2("datahopper", influxHost, influxUser, influxPassword, influxDatabase, local)
 
 	// Authenticated HTTP client.
-	oauthCacheFile := path.Join(*workdir, "google_storage_token.data")
-	httpClient, err := auth.NewClient(*local, oauthCacheFile, swarming.AUTH_SCOPE)
-	if err != nil {
-		glog.Fatal(err)
-	}
+	//oauthCacheFile := path.Join(*workdir, "google_storage_token.data")
+	//httpClient, err := auth.NewClient(*local, oauthCacheFile, swarming.AUTH_SCOPE)
+	//if err != nil {
+	//	glog.Fatal(err)
+	//}
 
 	// Swarming API client.
-	swarm, err := swarming.NewApiClient(httpClient)
-	if err != nil {
-		glog.Fatal(err)
-	}
+	//swarm, err := swarming.NewApiClient(httpClient)
+	//if err != nil {
+	//	glog.Fatal(err)
+	//}
 
 	// Shared repo objects.
 	skiaRepo, err := gitinfo.CloneOrUpdate(SKIA_REPO, path.Join(*workdir, "datahopper_skia"), true)
@@ -264,200 +264,200 @@ func main() {
 		}
 	}()
 
-	// Swarming bots.
-	go func() {
-		oldMetrics := []*metrics2.Int64Metric{}
-		for _ = range time.Tick(2 * time.Minute) {
-			glog.Info("Loading Skia Swarming bot data.")
-			skiaBots, err := swarm.ListSkiaBots()
-			if err != nil {
-				glog.Error(err)
-				continue
-			}
-			glog.Info("Loading SkiaTriggers Swarming bot data.")
-			skiaTriggers, err := swarm.ListSkiaTriggerBots()
-			if err != nil {
-				glog.Error(err)
-				continue
-			}
-			glog.Info("Loading CT Swarming bot data.")
-			ctBots, err := swarm.ListCTBots()
-			if err != nil {
-				glog.Error(err)
-				continue
-			}
-			bots := append(skiaBots, skiaTriggers...)
-			bots = append(bots, ctBots...)
-			glog.Infof("DEBUG: Loaded Skia and CT Swarming bot data")
+	//// Swarming bots.
+	//go func() {
+	//	oldMetrics := []*metrics2.Int64Metric{}
+	//	for _ = range time.Tick(2 * time.Minute) {
+	//		glog.Info("Loading Skia Swarming bot data.")
+	//		skiaBots, err := swarm.ListSkiaBots()
+	//		if err != nil {
+	//			glog.Error(err)
+	//			continue
+	//		}
+	//		glog.Info("Loading SkiaTriggers Swarming bot data.")
+	//		skiaTriggers, err := swarm.ListSkiaTriggerBots()
+	//		if err != nil {
+	//			glog.Error(err)
+	//			continue
+	//		}
+	//		glog.Info("Loading CT Swarming bot data.")
+	//		ctBots, err := swarm.ListCTBots()
+	//		if err != nil {
+	//			glog.Error(err)
+	//			continue
+	//		}
+	//		bots := append(skiaBots, skiaTriggers...)
+	//		bots = append(bots, ctBots...)
+	//		glog.Infof("DEBUG: Loaded Skia and CT Swarming bot data")
 
-			// Delete old metrics, replace with new ones. This fixes the case where
-			// bots are removed but their metrics hang around, or where dimensions
-			// change resulting in duplicate metrics with the same bot ID.
-			failedDelete := []*metrics2.Int64Metric{}
-			for _, m := range oldMetrics {
-				glog.Infof("DEBUG: Deleting metric %v", m)
-				if err := m.Delete(); err != nil {
-					glog.Warningf("Failed to delete metric: %s", err)
-					failedDelete = append(failedDelete, m)
-				}
-			}
-			oldMetrics = append([]*metrics2.Int64Metric{}, failedDelete...)
+	//		// Delete old metrics, replace with new ones. This fixes the case where
+	//		// bots are removed but their metrics hang around, or where dimensions
+	//		// change resulting in duplicate metrics with the same bot ID.
+	//		failedDelete := []*metrics2.Int64Metric{}
+	//		for _, m := range oldMetrics {
+	//			glog.Infof("DEBUG: Deleting metric %v", m)
+	//			if err := m.Delete(); err != nil {
+	//				glog.Warningf("Failed to delete metric: %s", err)
+	//				failedDelete = append(failedDelete, m)
+	//			}
+	//		}
+	//		oldMetrics = append([]*metrics2.Int64Metric{}, failedDelete...)
 
-			now := time.Now()
-			for _, bot := range bots {
-				glog.Infof("DEBUG: Processing Swarming bot %s", bot.BotId)
-				glog.Flush()
-				last, err := time.Parse("2006-01-02T15:04:05", bot.LastSeenTs)
-				if err != nil {
-					glog.Error(err)
-					glog.Flush()
-					continue
-				}
+	//		now := time.Now()
+	//		for _, bot := range bots {
+	//			glog.Infof("DEBUG: Processing Swarming bot %s", bot.BotId)
+	//			glog.Flush()
+	//			last, err := time.Parse("2006-01-02T15:04:05", bot.LastSeenTs)
+	//			if err != nil {
+	//				glog.Error(err)
+	//				glog.Flush()
+	//				continue
+	//			}
 
-				tags := map[string]string{
-					"bot": bot.BotId,
-				}
-				for _, d := range bot.Dimensions {
-					tags[fmt.Sprintf("dimension-%s", d.Key)] = strings.Join(d.Value, ",")
-				}
+	//			tags := map[string]string{
+	//				"bot": bot.BotId,
+	//			}
+	//			for _, d := range bot.Dimensions {
+	//				tags[fmt.Sprintf("dimension-%s", d.Key)] = strings.Join(d.Value, ",")
+	//			}
 
-				glog.Infof("DEBUG: Updating %s for %s", MEASUREMENT_SWARM_BOTS_LAST_SEEN, tags)
-				glog.Flush()
+	//			glog.Infof("DEBUG: Updating %s for %s", MEASUREMENT_SWARM_BOTS_LAST_SEEN, tags)
+	//			glog.Flush()
 
-				// Bot last seen <duration> ago.
-				m1 := metrics2.GetInt64Metric(MEASUREMENT_SWARM_BOTS_LAST_SEEN, tags)
-				m1.Update(int64(now.Sub(last)))
-				oldMetrics = append(oldMetrics, m1)
+	//			// Bot last seen <duration> ago.
+	//			m1 := metrics2.GetInt64Metric(MEASUREMENT_SWARM_BOTS_LAST_SEEN, tags)
+	//			m1.Update(int64(now.Sub(last)))
+	//			oldMetrics = append(oldMetrics, m1)
 
-				glog.Infof("DEBUG: Updating %s for %s", MEASUREMENT_SWARM_BOTS_QUARANTINED, tags)
-				glog.Flush()
+	//			glog.Infof("DEBUG: Updating %s for %s", MEASUREMENT_SWARM_BOTS_QUARANTINED, tags)
+	//			glog.Flush()
 
-				// Bot quarantined status.
-				quarantined := int64(0)
-				if bot.Quarantined {
-					quarantined = int64(1)
-				}
-				m2 := metrics2.GetInt64Metric(MEASUREMENT_SWARM_BOTS_QUARANTINED, tags)
-				m2.Update(quarantined)
-				oldMetrics = append(oldMetrics, m2)
-			}
-			glog.Infof("DEBUG: oldMetrics after loop %s", oldMetrics)
-			glog.Flush()
+	//			// Bot quarantined status.
+	//			quarantined := int64(0)
+	//			if bot.Quarantined {
+	//				quarantined = int64(1)
+	//			}
+	//			m2 := metrics2.GetInt64Metric(MEASUREMENT_SWARM_BOTS_QUARANTINED, tags)
+	//			m2.Update(quarantined)
+	//			oldMetrics = append(oldMetrics, m2)
+	//		}
+	//		glog.Infof("DEBUG: oldMetrics after loop %s", oldMetrics)
+	//		glog.Flush()
 
-		}
-	}()
+	//	}
+	//}()
 
-	// Swarming tasks.
-	go func() {
-		// Initial query: load data from the past 2 hours.
-		lastLoad := time.Now().Add(-2 * time.Hour)
+	//// Swarming tasks.
+	//go func() {
+	//	// Initial query: load data from the past 2 hours.
+	//	lastLoad := time.Now().Add(-2 * time.Hour)
 
-		revisitTasks := map[string]bool{}
+	//	revisitTasks := map[string]bool{}
 
-		for _ = range time.Tick(2 * time.Minute) {
-			glog.Infof("Loading Swarming task data.")
-			glog.Flush()
-			now := time.Now()
-			tasks, err := swarm.ListSkiaTasks(lastLoad, now)
-			if err != nil {
-				glog.Error(err)
-				glog.Flush()
-				continue
-			}
-			glog.Infof("Revisiting %d tasks.", len(revisitTasks))
-			glog.Flush()
-			for id, _ := range revisitTasks {
-				task, err := swarm.GetTaskMetadata(id)
-				if err != nil {
-					glog.Error(err)
-					glog.Flush()
-					continue
-				}
-				tasks = append(tasks, task)
-			}
-			revisitTasks = map[string]bool{}
-			lastLoad = now
-			for _, task := range tasks {
-				if task.TaskResult.State == "COMPLETED" {
-					if task.TaskResult.DedupedFrom != "" {
-						continue
-					}
+	//	for _ = range time.Tick(2 * time.Minute) {
+	//		glog.Infof("Loading Swarming task data.")
+	//		glog.Flush()
+	//		now := time.Now()
+	//		tasks, err := swarm.ListSkiaTasks(lastLoad, now)
+	//		if err != nil {
+	//			glog.Error(err)
+	//			glog.Flush()
+	//			continue
+	//		}
+	//		glog.Infof("Revisiting %d tasks.", len(revisitTasks))
+	//		glog.Flush()
+	//		for id, _ := range revisitTasks {
+	//			task, err := swarm.GetTaskMetadata(id)
+	//			if err != nil {
+	//				glog.Error(err)
+	//				glog.Flush()
+	//				continue
+	//			}
+	//			tasks = append(tasks, task)
+	//		}
+	//		revisitTasks = map[string]bool{}
+	//		lastLoad = now
+	//		for _, task := range tasks {
+	//			if task.TaskResult.State == "COMPLETED" {
+	//				if task.TaskResult.DedupedFrom != "" {
+	//					continue
+	//				}
 
-					// Get the created time for the task. We'll use that as the
-					// timestamp for all data points related to it.
-					createdTime, err := swarming.Created(task)
-					if err != nil {
-						glog.Errorf("Failed to parse Swarming task created timestamp: %s", err)
-						glog.Flush()
-						continue
-					}
+	//				// Get the created time for the task. We'll use that as the
+	//				// timestamp for all data points related to it.
+	//				createdTime, err := swarming.Created(task)
+	//				if err != nil {
+	//					glog.Errorf("Failed to parse Swarming task created timestamp: %s", err)
+	//					glog.Flush()
+	//					continue
+	//				}
 
-					// Find the tags for the task, including ID, name, dimensions,
-					// and components of the builder name.
-					name, err := swarming.GetTagValue(task.TaskResult, "name")
-					if err != nil || name == "" {
-						glog.Errorf("Failed to find name for Swarming task: %v", task)
-						continue
-					}
-					builderName, err := swarming.GetTagValue(task.TaskResult, "buildername")
-					if err != nil || builderName == "" {
-						glog.Errorf("Failed to find buildername for Swarming task: %v", task)
-						continue
-					}
-					builderTags, err := buildbot.ParseBuilderName(builderName)
-					if err != nil {
-						glog.Errorf("Failed to parse builder name for Swarming task: %s", err)
-						continue
-					}
+	//				// Find the tags for the task, including ID, name, dimensions,
+	//				// and components of the builder name.
+	//				name, err := swarming.GetTagValue(task.TaskResult, "name")
+	//				if err != nil || name == "" {
+	//					glog.Errorf("Failed to find name for Swarming task: %v", task)
+	//					continue
+	//				}
+	//				builderName, err := swarming.GetTagValue(task.TaskResult, "buildername")
+	//				if err != nil || builderName == "" {
+	//					glog.Errorf("Failed to find buildername for Swarming task: %v", task)
+	//					continue
+	//				}
+	//				builderTags, err := buildbot.ParseBuilderName(builderName)
+	//				if err != nil {
+	//					glog.Errorf("Failed to parse builder name for Swarming task: %s", err)
+	//					continue
+	//				}
 
-					tags := map[string]string{
-						"bot-id":    task.TaskResult.BotId,
-						"task-id":   task.TaskId,
-						"task-name": name,
-					}
-					for _, d := range task.Request.Properties.Dimensions {
-						tags[fmt.Sprintf("dimension-%s", d.Key)] = d.Value
-					}
-					for k, v := range builderTags {
-						tags[k] = v
-					}
+	//				tags := map[string]string{
+	//					"bot-id":    task.TaskResult.BotId,
+	//					"task-id":   task.TaskId,
+	//					"task-name": name,
+	//				}
+	//				for _, d := range task.Request.Properties.Dimensions {
+	//					tags[fmt.Sprintf("dimension-%s", d.Key)] = d.Value
+	//				}
+	//				for k, v := range builderTags {
+	//					tags[k] = v
+	//				}
 
-					// Task duration in milliseconds.
-					metrics2.RawAddInt64PointAtTime(MEASUREMENT_SWARM_TASKS_DURATION, tags, int64(task.TaskResult.Duration*float64(1000.0)), createdTime)
+	//				// Task duration in milliseconds.
+	//				metrics2.RawAddInt64PointAtTime(MEASUREMENT_SWARM_TASKS_DURATION, tags, int64(task.TaskResult.Duration*float64(1000.0)), createdTime)
 
-					glog.Infof("DEBUG: task %s task.TaskResult.PerformanceStats %v", tags, task.TaskResult.PerformanceStats)
-					glog.Flush()
+	//				glog.Infof("DEBUG: task %s task.TaskResult.PerformanceStats %v", tags, task.TaskResult.PerformanceStats)
+	//				glog.Flush()
 
-					// TODO(benjaminwagner): Does this nil-check eliminate the error "invalid memory address or nil pointer dereference"?
-					if task.TaskResult.PerformanceStats != nil {
-						// Overhead stats, in milliseconds.
-						metrics2.RawAddInt64PointAtTime(MEASUREMENT_SWARM_TASKS_OVERHEAD_BOT, tags, int64(task.TaskResult.PerformanceStats.BotOverhead*float64(1000.0)), createdTime)
-						if task.TaskResult.PerformanceStats.IsolatedDownload != nil {
-							metrics2.RawAddInt64PointAtTime(MEASUREMENT_SWARM_TASKS_OVERHEAD_DOWNLOAD, tags, int64(task.TaskResult.PerformanceStats.IsolatedDownload.Duration*float64(1000.0)), createdTime)
-						} else {
-							glog.Errorf("Swarming task is missing its IsolatedDownload section: %v", task.TaskResult)
-						}
-						if task.TaskResult.PerformanceStats.IsolatedUpload != nil {
-							metrics2.RawAddInt64PointAtTime(MEASUREMENT_SWARM_TASKS_OVERHEAD_UPLOAD, tags, int64(task.TaskResult.PerformanceStats.IsolatedUpload.Duration*float64(1000.0)), createdTime)
-						} else {
-							glog.Errorf("Swarming task is missing its IsolatedUpload section: %v", task.TaskResult)
-						}
-					}
+	//				// TODO(benjaminwagner): Does this nil-check eliminate the error "invalid memory address or nil pointer dereference"?
+	//				if task.TaskResult.PerformanceStats != nil {
+	//					// Overhead stats, in milliseconds.
+	//					metrics2.RawAddInt64PointAtTime(MEASUREMENT_SWARM_TASKS_OVERHEAD_BOT, tags, int64(task.TaskResult.PerformanceStats.BotOverhead*float64(1000.0)), createdTime)
+	//					if task.TaskResult.PerformanceStats.IsolatedDownload != nil {
+	//						metrics2.RawAddInt64PointAtTime(MEASUREMENT_SWARM_TASKS_OVERHEAD_DOWNLOAD, tags, int64(task.TaskResult.PerformanceStats.IsolatedDownload.Duration*float64(1000.0)), createdTime)
+	//					} else {
+	//						glog.Errorf("Swarming task is missing its IsolatedDownload section: %v", task.TaskResult)
+	//					}
+	//					if task.TaskResult.PerformanceStats.IsolatedUpload != nil {
+	//						metrics2.RawAddInt64PointAtTime(MEASUREMENT_SWARM_TASKS_OVERHEAD_UPLOAD, tags, int64(task.TaskResult.PerformanceStats.IsolatedUpload.Duration*float64(1000.0)), createdTime)
+	//					} else {
+	//						glog.Errorf("Swarming task is missing its IsolatedUpload section: %v", task.TaskResult)
+	//					}
+	//				}
 
-					// Pending time in milliseconds.
-					startTime, err := swarming.Started(task)
-					if err != nil {
-						glog.Errorf("Failed to parse Swarming task started timestamp: %s", err)
-						continue
-					}
-					pendingMs := int64(startTime.Sub(createdTime).Seconds() * float64(1000.0))
-					metrics2.RawAddInt64PointAtTime(MEASUREMENT_SWARM_TASKS_PENDING_TIME, tags, pendingMs, createdTime)
-				} else {
-					revisitTasks[task.TaskId] = true
-				}
-			}
-		}
-	}()
+	//				// Pending time in milliseconds.
+	//				startTime, err := swarming.Started(task)
+	//				if err != nil {
+	//					glog.Errorf("Failed to parse Swarming task started timestamp: %s", err)
+	//					continue
+	//				}
+	//				pendingMs := int64(startTime.Sub(createdTime).Seconds() * float64(1000.0))
+	//				metrics2.RawAddInt64PointAtTime(MEASUREMENT_SWARM_TASKS_PENDING_TIME, tags, pendingMs, createdTime)
+	//			} else {
+	//				revisitTasks[task.TaskId] = true
+	//			}
+	//		}
+	//	}
+	//}()
 
 	// Number of commits in the repo.
 	go func() {
@@ -469,44 +469,44 @@ func main() {
 		}
 	}()
 
-	// Time since last successful backup.
-	go func() {
-		lv := metrics2.NewLiveness("last-buildbot-db-backup", nil)
-		authClient, err := auth.NewDefaultJWTServiceAccountClient(auth.SCOPE_READ_ONLY)
-		if err != nil {
-			glog.Fatal(err)
-		}
-		gsClient, err := storage.NewClient(context.Background(), option.WithHTTPClient(authClient))
-		if err != nil {
-			glog.Fatal(err)
-		}
-		setLastBackupTime := func() error {
-			last := time.Time{}
-			if err := gs.AllFilesInDir(gsClient, "skia-buildbots", "db_backup", func(item *storage.ObjectAttrs) {
-				if item.Updated.After(last) {
-					last = item.Updated
-				}
-			}); err != nil {
-				return err
-			}
-			lv.ManualReset(last)
-			glog.Infof("Last DB backup was %s.", last)
-			return nil
-		}
-		if err := setLastBackupTime(); err != nil {
-			glog.Fatal(err)
-		}
-		for _ = range time.Tick(10 * time.Minute) {
-			if err := setLastBackupTime(); err != nil {
-				glog.Errorf("Failed to get last DB backup time: %s", err)
-			}
-		}
-	}()
+	//// Time since last successful backup.
+	//go func() {
+	//	lv := metrics2.NewLiveness("last-buildbot-db-backup", nil)
+	//	authClient, err := auth.NewDefaultJWTServiceAccountClient(auth.SCOPE_READ_ONLY)
+	//	if err != nil {
+	//		glog.Fatal(err)
+	//	}
+	//	gsClient, err := storage.NewClient(context.Background(), option.WithHTTPClient(authClient))
+	//	if err != nil {
+	//		glog.Fatal(err)
+	//	}
+	//	setLastBackupTime := func() error {
+	//		last := time.Time{}
+	//		if err := gs.AllFilesInDir(gsClient, "skia-buildbots", "db_backup", func(item *storage.ObjectAttrs) {
+	//			if item.Updated.After(last) {
+	//				last = item.Updated
+	//			}
+	//		}); err != nil {
+	//			return err
+	//		}
+	//		lv.ManualReset(last)
+	//		glog.Infof("Last DB backup was %s.", last)
+	//		return nil
+	//	}
+	//	if err := setLastBackupTime(); err != nil {
+	//		glog.Fatal(err)
+	//	}
+	//	for _ = range time.Tick(10 * time.Minute) {
+	//		if err := setLastBackupTime(); err != nil {
+	//			glog.Errorf("Failed to get last DB backup time: %s", err)
+	//		}
+	//	}
+	//}()
 
-	// Run a backup server.
-	go func() {
-		glog.Fatal(buildbot.RunBackupServer(db, *httpPort))
-	}()
+	//// Run a backup server.
+	//go func() {
+	//	glog.Fatal(buildbot.RunBackupServer(db, *httpPort))
+	//}()
 
 	// Wait while the above goroutines generate data.
 	select {}
