06259b8d583fa1ffa051d89718a0a9be0dd695fb
Stephan Altmueller <stephana@google.com>
2016-10-05 16:42:19 -0400

    Move test from gerrit to buildbucket
    Fixes a bug that causes the JSON parsing to fail when 
    when the timestamp is enclosed in quotes. 
    Might have been a change in the output format of 
    buildbucket results. 
    
    BUG=skia:
    
    Change-Id: I8947eaf8065c5955431a82a02570ad14fd141979

---
+++ go/buildbucket/buildbucket_test.go
@@ -0,0 +1,16 @@
+package buildbucket
+
+import (
+	"testing"
+
+	assert "github.com/stretchr/testify/require"
+
+	"go.skia.org/infra/go/httputils"
+)
+
+func TestGetTrybotsForCL(t *testing.T) {
+	client := NewClient(httputils.NewTimeoutClient())
+	tries, err := client.GetTrybotsForCL(2347, 7, "gerrit", "https://skia-review.googlesource.com")
+	assert.NoError(t, err)
+	assert.Equal(t, 1, len(tries))
+}
