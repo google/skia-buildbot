441a86380c86de6b4a7808e026c8a7701c33d541
Joe Gregorio <jcgregorio@google.com>
2016-10-05 12:23:23 -0400

    perf: Add skp markers
    BUG=skia:
    
    Change-Id: Ibc9c4693aaf2028291c388899f70bd0f0c01759d

--- perf/go/skiaperf/main.go
+++ perf/go/skiaperf/main.go
@@ -12,6 +12,7 @@ import (
 	"path/filepath"
 	"regexp"
 	"runtime"
+	"sort"
 	"strconv"
 	"strings"
 	"time"
@@ -1178,6 +1179,65 @@ type SearchRequest struct {
 type SearchResponse struct {
 	DataFrame *dataframe.DataFrame `json:"dataframe"`
 	Ticks     []interface{}        `json:"ticks"`
+	Skps      []int                `json:"skps"`
+}
+
+type Int64Slice []int64
+
+func (p Int64Slice) Len() int           { return len(p) }
+func (p Int64Slice) Less(i, j int) bool { return p[i] < p[j] }
+func (p Int64Slice) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+
+func getSkps(headers []*dataframe.ColumnHeader) ([]int, error) {
+	ci, err := git.ByIndex(int(headers[0].Offset))
+	if err != nil {
+		return nil, fmt.Errorf("Could not find commit for index %d: %s", headers[0].Offset, err)
+	}
+	begin := ci.Hash
+	ci, err = git.ByIndex(int(headers[len(headers)-1].Offset))
+	if err != nil {
+		return nil, fmt.Errorf("Could not find commit for index %d: %s", headers[len(headers)-1].Offset, err)
+	}
+	end := ci.Hash
+	log, err := git.LogFine(begin, end, "--format=format:%ct", "infra/bots/assets/skp/VERSION")
+	if err != nil {
+		return nil, fmt.Errorf("Could not get skp log for %s...%s: %s", begin, end, err)
+	}
+	ts := []int64{}
+	for _, s := range strings.Split(log, "\n") {
+		i, err := strconv.Atoi(s)
+		if err != nil {
+			continue
+		}
+		ts = append(ts, int64(i))
+	}
+	sort.Sort(Int64Slice(ts))
+
+	// Now flag all the columns where the skp changes.
+	ret := []int{}
+	for i, h := range headers {
+		if len(ts) == 0 {
+			break
+		}
+		if h.Timestamp >= ts[0] {
+			ret = append(ret, i)
+			ts = ts[1:]
+			if len(ts) == 0 {
+				break
+			}
+			// Coalesce all skp updates for a col into a single index.
+			for h.Timestamp >= ts[0] {
+				ts = ts[1:]
+				if len(ts) == 0 {
+					break
+				}
+			}
+		}
+	}
+	// Always add in the last index so we draw a band if there's an odd
+	// number of skp updates.
+	ret = append(ret, len(headers)-1)
+	return ret, nil
 }
 
 // searchHandler takes the POST'd query and returns a serialized DataFrame of
@@ -1206,6 +1266,10 @@ func searchHandler(w http.ResponseWriter, r *http.Request) {
 	begin := git.LastNIndex(sr.LastN)[0].Timestamp
 	end := time.Now()
 	df, err := dataframe.NewFromQueryAndRange(git, ptracestore.Default, begin, end, q)
+	if err != nil {
+		httputils.ReportError(w, r, err, "Failed to load data.")
+		return
+	}
 
 	// Calculate the human ticks based on the column headers.
 	ts := []int64{}
@@ -1214,9 +1278,17 @@ func searchHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	ticks := human.FlotTickMarks(ts)
 
+	// Determine where SKP changes occurred.
+	skps, err := getSkps(df.Header)
+	if err != nil {
+		httputils.ReportError(w, r, err, "Failed to load skps.")
+		return
+	}
+
 	resp := &SearchResponse{
 		DataFrame: df,
 		Ticks:     ticks,
+		Skps:      skps,
 	}
 
 	// TODO(jcgregorio) Limit the results if there are too many?
