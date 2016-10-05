27bef08490833066cd4da83b093bff6fc28d041a
Eric Boren <borenet@google.com>
2016-10-05 15:08:39 -0400

    [task scheduler] Add depot_tools checkout to startup script
    BUG=skia:5723
    
    Change-Id: Ib4cca64ac9e9671657f7dd4eee631cb0a2edfc6a

--- compute_engine_scripts/task_scheduler/startup-script.sh
+++ compute_engine_scripts/task_scheduler/startup-script.sh
@@ -1,1 +1,12 @@
 #! /bin/bash
+
+# Install depot_tools if needed.
+if [[ ! -d /mnt/pd0/depot_tools ]]; then
+  sudo -u default git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git /mnt/pd0/depot_tools
+fi
+
+# Add depot_tools to PATH if needed.
+if [ -z "$(which gclient)" ]; then
+  sudo -u default echo '# Add depot_tools to PATH"' >> /home/default/.bashrc
+  sudo -u default echo 'export PATH="/mnt/pd0/depot_tools:$PATH"' >> /home/default/.bashrc
+fi
