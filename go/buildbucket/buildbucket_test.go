12b0a6013c552885212178e5eab35e85dc742428
Stephan Altmueller <stephana@google.com>
2016-10-10 21:48:03 -0400

    Add Gerrit support to Gold
    BUG=skia:
    
    Change-Id: I0b86a81c3429fe47bc63b50cb57fe015389bb7fd

--- go/buildbucket/buildbucket_test.go
+++ go/buildbucket/buildbucket_test.go
@@ -1,8 +1,10 @@
 package buildbucket
 
 import (
+	"fmt"
 	"testing"
 
+	"github.com/davecgh/go-spew/spew"
 	assert "github.com/stretchr/testify/require"
 
 	"go.skia.org/infra/go/httputils"
@@ -16,4 +18,6 @@ func TestGetTrybotsForCL(t *testing.T) {
 	tries, err := client.GetTrybotsForCL(2347, 7, "gerrit", "https://skia-review.googlesource.com")
 	assert.NoError(t, err)
 	assert.Equal(t, 1, len(tries))
+
+	fmt.Printf("XXX: %s", spew.Sprint(tries))
 }
