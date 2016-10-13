41ff8f840e990f2e73d5558e3f05864ea4bfbbcf
Stephan Altmueller <stephana@google.com>
2016-10-13 17:35:55 -0400

    Gerrit integration in Gold
    Trybots triggered by Gerrit will now show up in Gold. The only difference is the UI will not show for Gerrit issues whether they have been ingested or not.
    That will depend on making it possible to connect Job information in Gold, 
    Buildbucket and the ingester. 
    
    BUG=skia:5785
    
    Change-Id: Idd513d8265bded87d240df17bd9b1014c4877498
    Reviewed-on: https://skia-review.googlesource.com/3300
    Commit-Queue: Stephan Altmueller <stephana@google.com>
    Reviewed-by: Ravi Mistry <rmistry@google.com>

--- golden/go/trybot/trybot.go
+++ golden/go/trybot/trybot.go
@@ -12,6 +12,7 @@ import (
 
 	cache "github.com/patrickmn/go-cache"
 	"github.com/skia-dev/glog"
+	"go.skia.org/infra/go/buildbucket"
 	"go.skia.org/infra/go/gerrit"
 	"go.skia.org/infra/go/rietveld"
 	"go.skia.org/infra/go/tiling"
@@ -72,24 +73,22 @@ type Tryjob struct {
 
 // TrybotResults manages everything related to aggregating information about trybot results.
 type TrybotResults struct {
-	tileBuilder    tracedb.BranchTileBuilder
-	reviewURL      string
-	rietveldAPI    *rietveld.Rietveld
-	gerritAPI      *gerrit.Gerrit
-	ingestionStore *goldingestion.IngestionStore
-	timeFrame      time.Duration
-	patchsetCache  *cache.Cache
+	tileBuilder           tracedb.BranchTileBuilder
+	rietveldAPI           *rietveld.Rietveld
+	gerritAPI             *gerrit.Gerrit
+	ingestionStore        *goldingestion.IngestionStore
+	timeFrame             time.Duration
+	rietveldPatchsetCache *cache.Cache
 }
 
 func NewTrybotResults(tileBuilder tracedb.BranchTileBuilder, rietveldAPI *rietveld.Rietveld, gerritAPI *gerrit.Gerrit, ingestionStore *goldingestion.IngestionStore) *TrybotResults {
 	ret := &TrybotResults{
-		tileBuilder:    tileBuilder,
-		reviewURL:      rietveldAPI.Url(0),
-		rietveldAPI:    rietveldAPI,
-		gerritAPI:      gerritAPI,
-		ingestionStore: ingestionStore,
-		timeFrame:      TIME_FRAME,
-		patchsetCache:  cache.New(PATCHSET_CACHE_EXPIRATION, PATCHSET_CACHE_CLEANUP_INTERVAL),
+		tileBuilder:           tileBuilder,
+		rietveldAPI:           rietveldAPI,
+		gerritAPI:             gerritAPI,
+		ingestionStore:        ingestionStore,
+		timeFrame:             TIME_FRAME,
+		rietveldPatchsetCache: cache.New(PATCHSET_CACHE_EXPIRATION, PATCHSET_CACHE_CLEANUP_INTERVAL),
 	}
 	return ret
 }
@@ -101,16 +100,24 @@ func (t *TrybotResults) ListTrybotIssues(offset, size int) ([]*Issue, int, error
 	end := time.Now()
 	begin := end.Add(-t.timeFrame)
 
-	commits, issueIDs, err := t.getCommits(begin, end, t.reviewURL)
+	// Get all issues from Rietveld.
+	commits, issueIDs, err := t.getCommits(begin, end, t.rietveldAPI.Url(0), false)
 	if err != nil {
 		return nil, 0, err
 	}
+	issuesList := t.getIssuesFromCommits(commits, issueIDs, false)
 
-	issuesList := t.getIssuesFromCommits(commits, issueIDs)
+	// Get all issues from Gerrit.
+	commits, issueIDs, err = t.getCommits(begin, end, t.gerritAPI.Url(0), true)
+	if err != nil {
+		return nil, 0, err
+	}
+	issuesList = append(issuesList, t.getIssuesFromCommits(commits, issueIDs, true)...)
 	if len(issuesList) == 0 {
 		return []*Issue{}, 0, nil
 	}
 
+	sort.Sort(IssuesSlice(issuesList))
 	maxIdx := util.MaxInt(0, len(issuesList)-1)
 	offset = util.MinInt(util.MaxInt(offset, 0), maxIdx)
 	size = util.MaxInt(size, 0)
@@ -130,8 +137,8 @@ func (t *TrybotResults) GetIssue(issueID string, targetPatchsets []string) (*Iss
 
 	end := time.Now()
 	begin := end.Add(-t.timeFrame)
-	prefix := t.getPrefix(numIssueID)
-	commits, issueIDs, err := t.getCommits(begin, end, prefix)
+	prefix, isGerrit := t.getPrefix(numIssueID)
+	commits, issueIDs, err := t.getCommits(begin, end, prefix, isGerrit)
 
 	if err != nil {
 		return nil, nil, err
@@ -141,14 +148,20 @@ func (t *TrybotResults) GetIssue(issueID string, targetPatchsets []string) (*Iss
 		return nil, nil, nil
 	}
 
-	issue := t.getIssuesFromCommits(commits, issueIDs)[0]
+	issue := t.getIssuesFromCommits(commits, issueIDs, isGerrit)[0]
 	tile, err := t.tileBuilder.CachedTileFromCommits(tracedb.ShortFromLong(commits))
 	if err != nil {
 		return nil, nil, fmt.Errorf("Error retrieving tile: %s", err)
 	}
 
+	// get the patchsets we want and make sure they are probable.
+	targetPatchsets, err = t.getTargetPatchsets(issue, targetPatchsets)
+	if err != nil {
+		return nil, nil, err
+	}
+
 	// Retrieve all the patchset results for this commit.
-	patchsetDetails, targetPatchsets, err := t.getPatchsetDetails(issue, targetPatchsets)
+	patchsetDetails, err := t.getPatchsetDetails(issue, isGerrit)
 	if err != nil {
 		return nil, nil, err
 	}
@@ -161,99 +174,40 @@ func (t *TrybotResults) GetIssue(issueID string, targetPatchsets []string) (*Iss
 	}, tile, nil
 }
 
-func (t *TrybotResults) getPrefix(issueID int64) string {
-	// TODO(stephana): Make this general to work for gerrit and rietveld.
-	return t.rietveldAPI.Url(issueID)
+// getPrefix returns the URL prefix for the given issue ID and whether it's
+// a Gerrit issue or not.
+func (t *TrybotResults) getPrefix(issueID int64) (string, bool) {
+	// This uses a heuristic to distinguish between Gerrit and Rietveld issues.
+	// This is a hack and will be obsolete once Rietveld support is removed.
+	if issueID < 1000000 {
+		return t.gerritAPI.Url(issueID), true
+	}
+	return t.rietveldAPI.Url(issueID), false
 }
 
-func (t *TrybotResults) getPatchsetDetails(issue *Issue, targetPatchsets []string) (map[int64]*PatchsetDetail, []string, error) {
-	// Get the target patchsets.
-	var int64PidMap map[int64]bool = nil
+func (t *TrybotResults) getPatchsetDetails(issue *Issue, isGerrit bool) (map[int64]*PatchsetDetail, error) {
+	intIssueID, err := strconv.ParseInt(issue.ID, 10, 64)
+	if err != nil {
+		return nil, fmt.Errorf("Unable to parse issue id %s. Got error: %s", issue.ID, err)
+	}
 
-	// if no patchset was given, use the last one.
-	if len(targetPatchsets) == 0 {
-		pset := issue.Patchsets[len(issue.Patchsets)-1]
-		targetPatchsets = []string{strconv.Itoa(int(pset))}
-		int64PidMap = map[int64]bool{pset: true}
-	} else {
-		int64PidMap = make(map[int64]bool, len(targetPatchsets))
-		for _, k := range targetPatchsets {
-			convKey, err := strconv.ParseInt(k, 10, 64)
-			if err != nil {
-				return nil, nil, fmt.Errorf("Invalid patchset id (%s): %s", k, err)
-			}
-			int64PidMap[convKey] = true
-		}
+	// Select the extraction method.
+	extractPatchsetDetails := t.extractRietveldPatchsetDetails
+	if isGerrit {
+		extractPatchsetDetails = t.extractGerritPatchsetDetails
 	}
 
 	ret := make(map[int64]*PatchsetDetail, len(issue.Patchsets))
 	var wg sync.WaitGroup
 	var mutex sync.Mutex
-
-	intIssueID, err := strconv.ParseInt(issue.ID, 10, 64)
-	if err != nil {
-		return nil, nil, fmt.Errorf("Unable to parse issue id %s. Got error: %s", issue.ID, err)
-	}
-
 	for _, pid := range issue.Patchsets {
 		wg.Add(1)
 		go func(pid int64) {
 			defer wg.Done()
-			crPatchSet, err := t.getCachedPatchset(intIssueID, pid, int64PidMap)
+			pSet, err := extractPatchsetDetails(intIssueID, pid)
 			if err != nil {
 				glog.Errorf("Error retrieving patchset %d: %s", pid, err)
 				return
-			}
-
-			nTryjobs := len(crPatchSet.TryjobResults)
-			tryjobs := make([]*Tryjob, 0, nTryjobs)
-			var tjIngested int64 = 0
-			for _, tj := range crPatchSet.TryjobResults {
-				// Filter out tryjobs we want to ignore. This includes compile bots, since we'll never
-				// ingest any results from them. We count the bot as ingested.
-				if filterTryjob(tj) {
-					tjIngested++
-					continue
-				}
-
-				var status string
-				checkIngested := false
-				switch tj.Result {
-				// scheduled but not yet started.
-				case 6:
-					status = TRYJOB_SCHEDULED
-				// currently running.
-				case -1:
-					status = TRYJOB_RUNNING
-					checkIngested = true
-					// Finished.
-				case 0:
-					status = TRYJOB_COMPLETE
-					checkIngested = true
-				// failed.
-				case 2:
-					status = TRYJOB_FAILED
-				default:
-					status = TRYJOB_FAILED
-				}
-
-				// Check if the job has been ingested if the job has at least been started.
-				if checkIngested && t.ingestionStore.IsIngested(config.CONSTRUCTOR_GOLD, tj.Master, tj.Builder, tj.BuildNumber) {
-					status = TRYJOB_INGESTED
-					tjIngested++
-				}
-
-				tryjobs = append(tryjobs, &Tryjob{
-					Builder: tj.Builder,
-					Status:  status,
-				})
-			}
-
-			pSet := &PatchsetDetail{
-				ID:       pid,
-				Tryjobs:  tryjobs,
-				JobDone:  tjIngested,
-				JobTotal: int64(nTryjobs),
 			}
 
 			mutex.Lock()
@@ -262,14 +216,72 @@ func (t *TrybotResults) getPatchsetDetails(issue *Issue, targetPatchsets []strin
 		}(pid)
 	}
 	wg.Wait()
-	return ret, targetPatchsets, nil
+	return ret, nil
 }
 
-func (t *TrybotResults) getCachedPatchset(intIssueID, pid int64, targetPatchsets map[int64]bool) (*rietveld.Patchset, error) {
+func (t *TrybotResults) extractRietveldPatchsetDetails(issueID, patchsetID int64) (*PatchsetDetail, error) {
+	crPatchSet, err := t.getCachedRietveldPatchset(issueID, patchsetID)
+	if err != nil {
+		return nil, err
+	}
+
+	nTryjobs := len(crPatchSet.TryjobResults)
+	tryjobs := make([]*Tryjob, 0, nTryjobs)
+	var tjIngested int64 = 0
+	for _, tj := range crPatchSet.TryjobResults {
+		// Filter out tryjobs we want to ignore. This includes compile bots, since we'll never
+		// ingest any results from them. We count the bot as ingested.
+		if filterTryjob(tj.Builder) {
+			tjIngested++
+			continue
+		}
+
+		var status string
+		checkIngested := false
+		switch tj.Result {
+		// scheduled but not yet started.
+		case 6:
+			status = TRYJOB_SCHEDULED
+		// currently running.
+		case -1:
+			status = TRYJOB_RUNNING
+			checkIngested = true
+			// Finished.
+		case 0:
+			status = TRYJOB_COMPLETE
+			checkIngested = true
+		// failed.
+		case 2:
+			status = TRYJOB_FAILED
+		default:
+			status = TRYJOB_FAILED
+		}
+
+		// Check if the job has been ingested if the job has at least been started.
+		if checkIngested && t.ingestionStore.IsIngested(config.CONSTRUCTOR_GOLD, tj.Master, tj.Builder, tj.BuildNumber) {
+			status = TRYJOB_INGESTED
+			tjIngested++
+		}
+
+		tryjobs = append(tryjobs, &Tryjob{
+			Builder: tj.Builder,
+			Status:  status,
+		})
+	}
+
+	return &PatchsetDetail{
+		ID:       patchsetID,
+		Tryjobs:  tryjobs,
+		JobDone:  tjIngested,
+		JobTotal: int64(nTryjobs),
+	}, nil
+}
+
+func (t *TrybotResults) getCachedRietveldPatchset(intIssueID, pid int64) (*rietveld.Patchset, error) {
 	key := strconv.FormatInt(intIssueID, 10) + ":" + strconv.FormatInt(pid, 10)
 
 	// Check for the key.
-	if val, ok := t.patchsetCache.Get(key); ok {
+	if val, ok := t.rietveldPatchsetCache.Get(key); ok {
 		return val.(*rietveld.Patchset), nil
 	}
 
@@ -278,25 +290,110 @@ func (t *TrybotResults) getCachedPatchset(intIssueID, pid int64, targetPatchsets
 		return nil, err
 	}
 
-	t.patchsetCache.Set(key, val, 0)
+	t.rietveldPatchsetCache.Set(key, val, 0)
 	return val, nil
 }
 
-// filterTryjob returns true if the given tryjob should be ignored.
-func filterTryjob(tj *rietveld.TryjobResult) bool {
-	return !strings.HasPrefix(tj.Builder, "Test")
+func (t *TrybotResults) extractGerritPatchsetDetails(issueID, patchsetID int64) (*PatchsetDetail, error) {
+	builds, err := t.gerritAPI.GetTrybotResults(issueID, patchsetID)
+	if err != nil {
+		return nil, err
+	}
+
+	nTryjobs := len(builds)
+	tryjobs := make([]*Tryjob, 0, nTryjobs)
+	var tjIngested int64 = 0
+	for _, build := range builds {
+		params := build.Parameters
+		if (params == nil) || (params.BuilderName == "") || (params.Properties.Master == "") {
+			glog.Errorf("Unable to find builder name or master for a build: %s", build.Id)
+			continue
+		}
+		builderName := build.Parameters.BuilderName
+
+		// Filter out tryjobs we want to ignore. This includes compile bots, since we'll never
+		// ingest any results from them. We count the bot as ingested.
+		if filterTryjob(build.Parameters.BuilderName) {
+			tjIngested++
+			continue
+		}
+
+		status := TRYJOB_FAILED
+		switch build.Status {
+		// scheduled but not yet started.
+		case buildbucket.STATUS_SCHEDULED:
+			status = TRYJOB_SCHEDULED
+		// currently running.
+		case buildbucket.STATUS_STARTED:
+			status = TRYJOB_RUNNING
+
+		case buildbucket.STATUS_COMPLETED:
+			if build.Result == buildbucket.RESULT_SUCCESS {
+				status = TRYJOB_COMPLETE
+			}
+		}
+
+		// TODO(stephana): Set the status based on whether the requested tryjobs
+		// have already been ingested or not.
+		// This depends on buildbucket or task scheduler having an API retrieve
+		// the results of a tryjob. We further needs a unique identifier for each
+		// tryjob which will be an argument for the call to IsIngested  i.e.:
+		// if checkIngested && t.ingestionStore.IsIngested(config.CONSTRUCTOR_GOLD, tj.Master, tj.Builder, tj.BuildNumber) {
+		// 	status = TRYJOB_INGESTED
+		// 	tjIngested++
+		// }
+
+		tryjobs = append(tryjobs, &Tryjob{
+			Builder: builderName,
+			Status:  status,
+		})
+	}
+
+	return &PatchsetDetail{
+		ID:       patchsetID,
+		Tryjobs:  tryjobs,
+		JobDone:  tjIngested,
+		JobTotal: int64(nTryjobs),
+	}, nil
 }
 
-func (t *TrybotResults) getCommits(startTime, endTime time.Time, prefix string) ([]*tracedb.CommitIDLong, map[string]bool, error) {
+func (t *TrybotResults) getTargetPatchsets(issue *Issue, targetPatchsets []string) ([]string, error) {
+	// if no patchset was given, use the last one.
+	if len(targetPatchsets) == 0 {
+		pset := issue.Patchsets[len(issue.Patchsets)-1]
+		targetPatchsets = []string{strconv.Itoa(int(pset))}
+	} else {
+		for _, k := range targetPatchsets {
+			_, err := strconv.ParseInt(k, 10, 64)
+			if err != nil {
+				return nil, fmt.Errorf("Invalid patchset id (%s): %s", k, err)
+			}
+		}
+	}
+	return targetPatchsets, nil
+}
+
+// filterTryjob returns true if the given tryjob should be ignored.
+func filterTryjob(builder string) bool {
+	return !strings.HasPrefix(builder, "Test")
+}
+
+// getCommits retrieves the commits within the given time range and prefix.
+// isGerrit is a convenience flag indicating whether the Gerrit api should be queried.
+func (t *TrybotResults) getCommits(startTime, endTime time.Time, prefix string, isGerrit bool) ([]*tracedb.CommitIDLong, map[string]bool, error) {
 	commits, err := t.tileBuilder.ListLong(startTime, endTime, prefix)
 	if err != nil {
 		return nil, nil, fmt.Errorf("Error retrieving commits in the range %s - %s. Got error: %s", startTime, endTime, err)
 	}
 
-	commits, issueIDs, newBegin := t.uniqueIssues(commits)
+	commits, issueIDs, newBegin := t.uniqueIssues(commits, isGerrit)
 
 	// Retrieve any commitIDs we have not retrieved for the issues of interest.
-	earlierCommits, err := t.tileBuilder.ListLong(newBegin, startTime, t.reviewURL)
+	reviewURL := t.rietveldAPI.Url(0)
+	if isGerrit {
+		reviewURL = t.gerritAPI.Url(0)
+	}
+	earlierCommits, err := t.tileBuilder.ListLong(newBegin, startTime, reviewURL)
 	if err != nil {
 		return nil, nil, fmt.Errorf("Error retrieving commits in the range %s - %s. Got error: %s", newBegin, startTime, err)
 	}
@@ -316,8 +413,7 @@ func (t *TrybotResults) getCommits(startTime, endTime time.Time, prefix string) 
 // getIssuesFromCommits returns instances of Issue based on the provided commits and the set
 // of issue ids. It is assumed that issueIDs only contains issues that have Rietveld details
 // attached to them. Any commit that is not in issueIDs will be ommitted.
-func (t *TrybotResults) getIssuesFromCommits(commits []*tracedb.CommitIDLong, issueIDs map[string]bool) []*Issue {
-	codeReviewURL := strings.TrimSuffix(t.reviewURL, "/")
+func (t *TrybotResults) getIssuesFromCommits(commits []*tracedb.CommitIDLong, issueIDs map[string]bool, isGerrit bool) []*Issue {
 	issueMap := make(map[string]*Issue, len(issueIDs))
 	for _, cid := range commits {
 		iid, _ := goldingestion.ExtractIssueInfo(cid.CommitID, t.rietveldAPI, t.gerritAPI)
@@ -327,16 +423,28 @@ func (t *TrybotResults) getIssuesFromCommits(commits []*tracedb.CommitIDLong, is
 			continue
 		}
 
+		// if the iid is not numeric it is wrong.
+		numIID, err := strconv.ParseInt(iid, 10, 64)
+		if err != nil {
+			glog.Errorf("Unable to parse issue id %s. Got error: %s", iid, err)
+			continue
+		}
+
+		getURL := t.rietveldAPI.Url
+		if isGerrit {
+			getURL = t.gerritAPI.Url
+		}
+
 		issue, ok := issueMap[iid]
 		if !ok {
-			details := cid.Details.(*rietveld.Issue)
+			patchsets, modified := getIssuePatchSetsAndModified(cid, isGerrit)
 			issue = &Issue{
 				ID:        iid,
 				Subject:   cid.Desc,
 				Owner:     cid.Author,
-				Updated:   details.Modified.Unix(),
-				URL:       fmt.Sprintf("%s/%s", codeReviewURL, iid),
-				Patchsets: details.Patchsets,
+				Updated:   modified,
+				URL:       getURL(numIID),
+				Patchsets: patchsets,
 			}
 			issueMap[iid] = issue
 		}
@@ -347,14 +455,13 @@ func (t *TrybotResults) getIssuesFromCommits(commits []*tracedb.CommitIDLong, is
 		ret = append(ret, issue)
 	}
 
-	sort.Sort(IssuesSlice(ret))
 	return ret
 }
 
 // uniqueIssues returns the set of all issues contained in the given list of commit ids. If an issue does not
 // have Rietveld information associated with it (i.e. the Details file is nil) it will be ommitted from the
 // returned list of commit ids and the set of commit issue ids.
-func (t *TrybotResults) uniqueIssues(commitIDs []*tracedb.CommitIDLong) ([]*tracedb.CommitIDLong, map[string]bool, time.Time) {
+func (t *TrybotResults) uniqueIssues(commitIDs []*tracedb.CommitIDLong, isGerrit bool) ([]*tracedb.CommitIDLong, map[string]bool, time.Time) {
 	minTime := time.Now()
 	issueIDs := map[string]bool{}
 	ret := make([]*tracedb.CommitIDLong, 0, len(commitIDs))
@@ -366,12 +473,32 @@ func (t *TrybotResults) uniqueIssues(commitIDs []*tracedb.CommitIDLong) ([]*trac
 		ret = append(ret, cid)
 		iid, _ := goldingestion.ExtractIssueInfo(cid.CommitID, t.rietveldAPI, t.gerritAPI)
 		issueIDs[iid] = true
-		rIssue := cid.Details.(*rietveld.Issue)
-		if minTime.After(rIssue.Created) {
-			minTime = rIssue.Created
+		createTime := getIssueCreateTime(cid, isGerrit)
+		if minTime.After(createTime) {
+			minTime = createTime
 		}
 	}
 	return ret, issueIDs, minTime
+}
+
+// getIssueCreateTime extracts the create time from the details of the details
+// field of the given CommitIDLong.
+func getIssueCreateTime(cid *tracedb.CommitIDLong, isGerrit bool) time.Time {
+	if isGerrit {
+		return cid.Details.(*gerrit.ChangeInfo).Created
+	}
+	return cid.Details.(*rietveld.Issue).Created
+}
+
+// getIssuePatchSetsAndModified returns the patchsets for the given issue and
+// the timestamp when it was last modified.
+func getIssuePatchSetsAndModified(cid *tracedb.CommitIDLong, isGerrit bool) ([]int64, int64) {
+	if isGerrit {
+		changeInfo := cid.Details.(*gerrit.ChangeInfo)
+		return changeInfo.GetPatchsetIDs(), changeInfo.Updated.Unix()
+	}
+	issue := cid.Details.(*rietveld.Issue)
+	return issue.Patchsets, issue.Modified.Unix()
 }
 
 type IssuesSlice []*Issue
