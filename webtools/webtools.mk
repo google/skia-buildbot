be4a15b8592c322ffe0fbfe3d27d9ac419ffee24
Kevin Lubick <kjlubick@google.com>
2016-10-13 15:52:29 -0400

    Fix missing architecture in fuzzer-fe
    Also fix CSS mixins by not minifying CSS
    
    
    BUG=skia:
    
    Change-Id: Iff5a70256705bb76afc4f8311ca584f65dc2b210
    Reviewed-on: https://skia-review.googlesource.com/3344
    Commit-Queue: Kevin Lubick <kjlubick@google.com>
    Reviewed-by: Joe Gregorio <jcgregorio@google.com>

--- webtools/webtools.mk
+++ webtools/webtools.mk
@@ -71,7 +71,7 @@ debug_elements_html:
 
 res/vul/elements.html: res/imp/*.html elements.html ./node_modules/.bin/vulcanize
 	-mkdir res/vul
-	./node_modules/.bin/vulcanize --inline-css --inline-scripts --strip-comments --abspath=./ elements.html | ./node_modules/.bin/html-minifier -o res/vul/elements.html --minify-css --minify-js --remove-comments --collapse-whitespace --conservative-collapse
+	./node_modules/.bin/vulcanize --inline-css --inline-scripts --strip-comments --abspath=./ elements.html | ./node_modules/.bin/html-minifier -o res/vul/elements.html --minify-js --remove-comments --collapse-whitespace --conservative-collapse
 
 #### clean_webtools ####
 
