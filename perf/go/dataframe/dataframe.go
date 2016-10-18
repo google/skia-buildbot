dd6f536b0f2aa87cc1da2a42e857e6c0526b4964
Joe Gregorio <jcgregorio@google.com>
2016-10-18 08:12:42 -0400

    perf: Display the number of commits skipped if displaying a subsample.
    Screenshot:
      https://screenshot.googleplex.com/GxDadW8FpKr.png
    
    BUG=skia:
    
    Change-Id: Ic5e316967074c868b093fd79840852e104b623e1

--- perf/go/dataframe/dataframe.go
+++ perf/go/dataframe/dataframe.go
@@ -34,17 +34,25 @@ type ColumnHeader struct {
 // indexed by a structured key (see go/query), and each column is described by
 // a ColumnHeader, which could be a commit or a trybot patch level.
 //
+// Skip is the number of commits skipped to bring the DataFrame down
+// to less than MAX_SAMPLE_SIZE commits. If Skip is zero then no
+// commits were skipped.
+//
 // The name DataFrame was gratuitously borrowed from R.
 type DataFrame struct {
 	TraceSet ptracestore.TraceSet `json:"traceset"`
 	Header   []*ColumnHeader      `json:"header"`
 	ParamSet paramtools.ParamSet  `json:"paramset"`
+	Skip     int                  `json:"skip"`
 }
 
 // rangeImpl returns the slices of ColumnHeader and cid.CommitID that
 // are needed by DataFrame and ptracestore.PTraceStore, respectively. The
 // slices are populated from the given vcsinfo.IndexCommits.
-func rangeImpl(resp []*vcsinfo.IndexCommit) ([]*ColumnHeader, []*cid.CommitID) {
+//
+// The value for 'skip', the number of commits skipped, is passed through to
+// the return values.
+func rangeImpl(resp []*vcsinfo.IndexCommit, skip int) ([]*ColumnHeader, []*cid.CommitID, int) {
 	headers := []*ColumnHeader{}
 	commits := []*cid.CommitID{}
 	for _, r := range resp {
@@ -58,24 +66,29 @@ func rangeImpl(resp []*vcsinfo.IndexCommit) ([]*ColumnHeader, []*cid.CommitID) {
 			Timestamp: r.Timestamp.Unix(),
 		})
 	}
-	return headers, commits
+	return headers, commits, skip
 }
 
 // lastN returns the slices of ColumnHeader and cid.CommitID that are
 // needed by DataFrame and ptracestore.PTraceStore, respectively. The slices
 // are for the last 50 commits in the repo.
-func lastN(vcs vcsinfo.VCS) ([]*ColumnHeader, []*cid.CommitID) {
-	return rangeImpl(vcs.LastNIndex(DEFAULT_NUM_COMMITS))
+//
+// Returns 0 for 'skip', the number of commits skipped.
+func lastN(vcs vcsinfo.VCS) ([]*ColumnHeader, []*cid.CommitID, int) {
+	return rangeImpl(vcs.LastNIndex(DEFAULT_NUM_COMMITS), 0)
 }
 
 // getRange returns the slices of ColumnHeader and cid.CommitID that are
 // needed by DataFrame and ptracestore.PTraceStore, respectively. The slices
 // are for the commits that fall in the given time range [begin, end).
-func getRange(vcs vcsinfo.VCS, begin, end time.Time) ([]*ColumnHeader, []*cid.CommitID) {
-	return rangeImpl(DownSample(vcs.Range(begin, end), MAX_SAMPLE_SIZE))
+//
+// The value for 'skip', the number of commits skipped, is also returned.
+func getRange(vcs vcsinfo.VCS, begin, end time.Time) ([]*ColumnHeader, []*cid.CommitID, int) {
+	commits, skip := DownSample(vcs.Range(begin, end), MAX_SAMPLE_SIZE)
+	return rangeImpl(commits, skip)
 }
 
-func _new(colHeaders []*ColumnHeader, commitIDs []*cid.CommitID, q *query.Query, store ptracestore.PTraceStore, progress ptracestore.Progress) (*DataFrame, error) {
+func _new(colHeaders []*ColumnHeader, commitIDs []*cid.CommitID, q *query.Query, store ptracestore.PTraceStore, progress ptracestore.Progress, skip int) (*DataFrame, error) {
 	defer timer.New("_new time").Stop()
 	traceSet, err := store.Match(commitIDs, q, progress)
 	if err != nil {
@@ -92,14 +105,15 @@ func _new(colHeaders []*ColumnHeader, commitIDs []*cid.CommitID, q *query.Query,
 		TraceSet: traceSet,
 		Header:   colHeaders,
 		ParamSet: paramSet,
+		Skip:     skip,
 	}, nil
 }
 
 // New returns a populated DataFrame of the last 50 commits given the 'vcs' and
 // 'store', or a non-nil error if there was a failure retrieving the traces.
 func New(vcs vcsinfo.VCS, store ptracestore.PTraceStore, progress ptracestore.Progress) (*DataFrame, error) {
-	colHeaders, commitIDs := lastN(vcs)
-	return _new(colHeaders, commitIDs, &query.Query{}, store, progress)
+	colHeaders, commitIDs, skip := lastN(vcs)
+	return _new(colHeaders, commitIDs, &query.Query{}, store, progress, skip)
 }
 
 // NewFromQueryAndRange returns a populated DataFrame of the traces that match
@@ -108,8 +122,8 @@ func New(vcs vcsinfo.VCS, store ptracestore.PTraceStore, progress ptracestore.Pr
 // periodically as the query is processed.
 func NewFromQueryAndRange(vcs vcsinfo.VCS, store ptracestore.PTraceStore, begin, end time.Time, q *query.Query, progress ptracestore.Progress) (*DataFrame, error) {
 	defer timer.New("NewFromQueryAndRange time").Stop()
-	colHeaders, commitIDs := getRange(vcs, begin, end)
-	return _new(colHeaders, commitIDs, q, store, progress)
+	colHeaders, commitIDs, skip := getRange(vcs, begin, end)
+	return _new(colHeaders, commitIDs, q, store, progress, skip)
 }
 
 // NewEmpty returns a new empty DataFrame.
@@ -124,10 +138,11 @@ func NewEmpty() *DataFrame {
 // NewHeaderOnly returns a DataFrame with a populated Header, with no traces.
 // The 'progress' callback is called periodically as the query is processed.
 func NewHeaderOnly(vcs vcsinfo.VCS, begin, end time.Time) *DataFrame {
-	colHeaders, _ := getRange(vcs, begin, end)
+	colHeaders, _, skip := getRange(vcs, begin, end)
 	return &DataFrame{
 		TraceSet: ptracestore.TraceSet{},
 		Header:   colHeaders,
 		ParamSet: paramtools.ParamSet{},
+		Skip:     skip,
 	}
 }
--- perf/go/dataframe/downsample_test.go
+++ perf/go/dataframe/downsample_test.go
@@ -14,29 +14,37 @@ func TestDownSample(t *testing.T) {
 		sample = append(sample, &vcsinfo.IndexCommit{Index: i})
 	}
 
-	ss := DownSample(sample, 5)
+	ss, n := DownSample(sample, 5)
 	assert.Equal(t, 5, len(ss))
+	assert.Equal(t, 0, n)
 
-	ss = DownSample(sample, 6)
+	ss, n = DownSample(sample, 6)
 	assert.Equal(t, 5, len(ss))
+	assert.Equal(t, 0, n)
 
-	ss = DownSample(sample, 3)
+	ss, n = DownSample(sample, 3)
 	assert.Equal(t, 3, len(ss))
+	assert.Equal(t, 1, n)
 	assert.Equal(t, 2, ss[1].Index)
 	assert.Equal(t, 4, ss[2].Index)
 
-	ss = DownSample(sample, 1)
+	ss, n = DownSample(sample, 1)
+	assert.Equal(t, 4, n)
 	assert.Equal(t, 1, len(ss))
 
-	ss = DownSample(sample[:1], 1)
+	ss, n = DownSample(sample[:1], 1)
+	assert.Equal(t, 0, n)
 	assert.Equal(t, 1, len(ss))
 
-	ss = DownSample([]*vcsinfo.IndexCommit{}, 1)
+	ss, n = DownSample([]*vcsinfo.IndexCommit{}, 1)
+	assert.Equal(t, 0, n)
 	assert.Equal(t, 0, len(ss))
 
-	ss = DownSample(sample, 0)
+	ss, n = DownSample(sample, 0)
+	assert.Equal(t, 0, n)
 	assert.Equal(t, 5, len(ss))
 
-	ss = DownSample(sample, 4)
+	ss, n = DownSample(sample, 4)
+	assert.Equal(t, 1, n)
 	assert.Equal(t, 3, len(ss))
 }
