09cdf2ac54bdf104847aa43872c12c1cd50e503b
Joe Gregorio <jcgregorio@google.com>
2016-10-18 14:59:41 -0400

    perf: ctrace2, implements kmeans interfaces.
    BUG=skia:
    
    Change-Id: I5f7517a475eacfdf642b086cac865f863f80f2f9
    Reviewed-on: https://skia-review.googlesource.com/3598
    Commit-Queue: Joe Gregorio <jcgregorio@google.com>
    Reviewed-by: Ben Wagner <benjaminwagner@google.com>

---
+++ perf/go/ctrace2/ctrace.go
@@ -0,0 +1,70 @@
+// ctrace makes Traces into ClusterableTraces which can then be used in kmeans.
+package ctrace2
+
+import (
+	"fmt"
+	"math"
+
+	"go.skia.org/infra/go/vec32"
+	"go.skia.org/infra/perf/go/kmeans"
+)
+
+// ClusterableTrace contains Trace data and implements kmeans.Clusterable and kmeans.Centroid.
+type ClusterableTrace struct {
+	Key    string
+	Values []float32
+}
+
+// See kmeans.Centroid.
+func (t *ClusterableTrace) Distance(c kmeans.Clusterable) float64 {
+	// Data always has the same length, and NewFullTrace keeps that guarantee.
+	o := c.(*ClusterableTrace)
+	sum := float32(0.0)
+	for i, x := range t.Values {
+		sum += (x - o.Values[i]) * (x - o.Values[i])
+	}
+	return math.Sqrt(float64(sum))
+}
+
+// See kmeans.Centroid.
+func (t *ClusterableTrace) AsClusterable() kmeans.Clusterable {
+	return t
+}
+
+func (t *ClusterableTrace) String() string {
+	return fmt.Sprintf("%s %#v", t.Key, t.Values[:2])
+}
+
+// NewFullTrace takes data you would find in a Trace and returns a
+// ClusterableTrace usable for kmeans clustering.
+func NewFullTrace(key string, values []float32, minStdDev float32) *ClusterableTrace {
+	norm := make([]float32, len(values))
+	copy(norm, values)
+	vec32.Fill(norm)
+	vec32.Norm(norm, minStdDev)
+
+	return &ClusterableTrace{
+		Key:    key,
+		Values: norm,
+	}
+}
+
+// CalculateCentroid implements kmeans.CalculateCentroid.
+func CalculateCentroid(members []kmeans.Clusterable) kmeans.Centroid {
+	first := members[0].(*ClusterableTrace)
+	mean := make([]float32, len(first.Values))
+	for _, m := range members {
+		ft := m.(*ClusterableTrace)
+		for i, x := range ft.Values {
+			mean[i] += x
+		}
+	}
+	numMembers := float32(len(members))
+	for i, _ := range mean {
+		mean[i] = mean[i] / numMembers
+	}
+	return &ClusterableTrace{
+		Key:    "I'm a centroid!",
+		Values: mean,
+	}
+}
