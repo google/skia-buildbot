8f137484c7f93afc06f2fd25a050f94c3407694c
Eric Boren <borenet@google.com>
2016-10-19 10:38:04 -0400

    Add Gerrit auth scope to skia-task-scheduler GCE instance
    BUG=skia:5723, chromium:656686
    
    Change-Id: I9f0f1089c88914c6aa9bcd8f2c6a65ae3999e43f
    Reviewed-on: https://skia-review.googlesource.com/3634
    Reviewed-by: Ben Wagner <benjaminwagner@google.com>
    Commit-Queue: Eric Boren <borenet@google.com>

--- compute_engine_scripts/task_scheduler/vm_create_instance.sh
+++ compute_engine_scripts/task_scheduler/vm_create_instance.sh
@@ -19,7 +19,7 @@ done
 
 MACHINE_TYPE=n1-highmem-16
 SOURCE_SNAPSHOT=skia-systemd-pushable-base
-SCOPES='https://www.googleapis.com/auth/devstorage.full_control https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile'
+SCOPES='https://www.googleapis.com/auth/devstorage.full_control https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/gerritcodereview'
 IP_ADDRESS=104.154.112.128
 
 # Create a boot disk from the pushable base snapshot.
