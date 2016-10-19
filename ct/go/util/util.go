029a062bf1596944f92bee1275572d60a98cda05
Ravi Mistry <rmistry@google.com>
2016-10-19 07:40:57 -0400

    [CT] Add task name as a tag to make direct log links work
    BUG=skia:5868
    
    Change-Id: Id84c3765a532ccdbc372d5949abc28f112f58b42
    Reviewed-on: https://skia-review.googlesource.com/3625
    Reviewed-by: Ben Wagner <benjaminwagner@google.com>
    Reviewed-by: Kevin Lubick <kjlubick@google.com>
    Commit-Queue: Ravi Mistry <rmistry@google.com>

--- ct/go/util/util.go
+++ ct/go/util/util.go
@@ -415,7 +415,7 @@ func TriggerSwarmingTask(pagesetType, taskPrefix, isolateName, runID string, har
 		return fmt.Errorf("len(genJSONs) was %d and len(tasksToHashes) was %d", len(genJSONs), len(tasksToHashes))
 	}
 	// Trigger swarming using the isolate hashes.
-	tasks, err := s.TriggerSwarmingTasks(tasksToHashes, dimensions, map[string]string{"runid": runID}, priority, 7*24*time.Hour, hardTimeout, ioTimeout, false)
+	tasks, err := s.TriggerSwarmingTasks(tasksToHashes, dimensions, map[string]string{"runid": runID}, priority, 7*24*time.Hour, hardTimeout, ioTimeout, false, true)
 	if err != nil {
 		return fmt.Errorf("Could not trigger swarming task: %s", err)
 	}
@@ -431,7 +431,7 @@ func TriggerSwarmingTask(pagesetType, taskPrefix, isolateName, runID string, har
 
 	if len(failedTasksToHashes) > 0 {
 		glog.Info("Retrying tasks that failed...")
-		retryTasks, err := s.TriggerSwarmingTasks(failedTasksToHashes, dimensions, map[string]string{"runid": runID}, priority, 7*24*time.Hour, hardTimeout, ioTimeout, false)
+		retryTasks, err := s.TriggerSwarmingTasks(failedTasksToHashes, dimensions, map[string]string{"runid": runID}, priority, 7*24*time.Hour, hardTimeout, ioTimeout, false, true)
 		if err != nil {
 			return fmt.Errorf("Could not trigger swarming task: %s", err)
 		}
@@ -727,7 +727,7 @@ func TriggerBuildRepoSwarmingTask(taskName, runID, repo, targetPlatform string, 
 	} else {
 		dimensions = GCE_LINUX_BUILDER_DIMENSIONS
 	}
-	tasks, err := s.TriggerSwarmingTasks(tasksToHashes, dimensions, map[string]string{"runid": runID}, swarming.RECOMMENDED_PRIORITY, 2*24*time.Hour, hardTimeout, ioTimeout, false)
+	tasks, err := s.TriggerSwarmingTasks(tasksToHashes, dimensions, map[string]string{"runid": runID}, swarming.RECOMMENDED_PRIORITY, 2*24*time.Hour, hardTimeout, ioTimeout, false, true)
 	if err != nil {
 		return nil, fmt.Errorf("Could not trigger swarming task: %s", err)
 	}
