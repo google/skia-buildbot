2ce2cda8994977702618c9894eee36a1abb5cc14
Joe Gregorio <jcgregorio@google.com>
2016-10-26 13:42:28 -0400

    Clustering master CL.
    BUG=skia:
    
    Change-Id: Ib316ab3ac84d5c810ef7d1fd15c21e5988a039fc

--- perf/go/clustering2/clustering.go
+++ perf/go/clustering2/clustering.go
@@ -116,7 +116,7 @@ func chooseK(observations []kmeans.Clusterable, k int) []kmeans.Centroid {
 	popN := len(observations)
 	centroids := make([]kmeans.Centroid, k)
 	for i := 0; i < k; i++ {
-		centroids[i] = observations[rand.Intn(popN)].(*ctrace2.ClusterableTrace).Dup("I'm a centroid")
+		centroids[i] = observations[rand.Intn(popN)].(*ctrace2.ClusterableTrace).Dup(ctrace2.CENTROID_KEY)
 	}
 	return centroids
 }
@@ -141,7 +141,11 @@ func getParamSummaries(cluster []kmeans.Clusterable) map[string][]ValueWeight {
 	clusterSize := float64(len(cluster))
 	// First figure out what parameters and values appear in the cluster.
 	for _, o := range cluster {
-		params, err := query.ParseKey(o.(*ctrace2.ClusterableTrace).Key)
+		key := o.(*ctrace2.ClusterableTrace).Key
+		if key == ctrace2.CENTROID_KEY {
+			continue
+		}
+		params, err := query.ParseKey(key)
 		if err != nil {
 			glog.Errorf("Invalid key found in Cluster: %s", err)
 			continue
