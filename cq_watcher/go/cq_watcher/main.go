b39de8df754e98bec2cbfb3c7b1dfde25d2a1836
Ravi Mistry <rmistry@google.com>
2016-10-27 10:17:53 -0400

    Add CQ Watcher service to report CQ metrics of Gerrit changes
    BUG=skia:5898
    
    Change-Id: Id1cce9db687d980507dcdc8587d2b4ac98cd39ff
    Reviewed-on: https://skia-review.googlesource.com/3971
    Commit-Queue: Ravi Mistry <rmistry@google.com>
    Reviewed-by: Joe Gregorio <jcgregorio@google.com>
    Reviewed-by: Eric Boren <borenet@google.com>

---
+++ cq_watcher/go/cq_watcher/main.go
@@ -0,0 +1,137 @@
+// The CQ Watcher monitors the Skia CQ for Gerrit CLs. It pumps the results of
+// the monitoring into InfluxDB.
+package main
+
+import (
+	"flag"
+	"fmt"
+	"time"
+
+	"github.com/skia-dev/glog"
+
+	"go.skia.org/infra/go/common"
+	"go.skia.org/infra/go/cq"
+	"go.skia.org/infra/go/gerrit"
+	"go.skia.org/infra/go/httputils"
+	"go.skia.org/infra/go/influxdb"
+	"go.skia.org/infra/go/metrics2"
+)
+
+var (
+	testing = flag.Bool("testing", false, "Set to true for local testing.")
+
+	influxHost     = flag.String("influxdb_host", influxdb.DEFAULT_HOST, "The InfluxDB hostname.")
+	influxUser     = flag.String("influxdb_name", influxdb.DEFAULT_USER, "The InfluxDB username.")
+	influxPassword = flag.String("influxdb_password", influxdb.DEFAULT_PASSWORD, "The InfluxDB password.")
+	influxDatabase = flag.String("influxdb_database", influxdb.DEFAULT_DATABASE, "The InfluxDB database.")
+)
+
+const (
+	// How often the two pollers should poll Gerrit.
+	IN_FLIGHT_POLL_TIME    = 30 * time.Second
+	AFTER_COMMIT_POLL_TIME = 5 * time.Minute
+
+	// How often to refresh the trybots received from cq.cfg.
+	REFRESH_CQ_TRYBOTS_TIME = time.Hour
+
+	MAX_CLS_PER_POLL = 100
+	METRIC_NAME      = "cq_watcher"
+)
+
+// monitorStatsForInFlightCLs queries Gerrit for all CLs currently in dry run
+// or waiting for commit. The following metrics are then reported for the
+// matching CLs:
+// * Number of CLs waiting for the CQ (both for dry runs and commits).
+// * How long CQ trybots have been running for.
+// * How many CQ trybots have been triggered.
+func monitorStatsForInFlightCLs(cqClient *cq.Client, gerritClient *gerrit.Gerrit) {
+	liveness := metrics2.NewLiveness(fmt.Sprintf("%s.%s", METRIC_NAME, cq.INFLIGHT_METRIC_NAME))
+	cqMetric := metrics2.GetInt64Metric(fmt.Sprintf("%s.%s.%s", METRIC_NAME, cq.INFLIGHT_METRIC_NAME, cq.INFLIGHT_WAITING_IN_CQ))
+	for _ = range time.Tick(time.Duration(IN_FLIGHT_POLL_TIME)) {
+		dryRunChanges, err := gerritClient.Search(MAX_CLS_PER_POLL, gerrit.SearchStatus("open"), gerrit.SearchProject("skia"), gerrit.SearchLabel(gerrit.COMMITQUEUE_LABEL, "1"))
+		if err != nil {
+			glog.Errorf("Error searching for open changes with dry run in Gerrit: %s", err)
+			continue
+		}
+		cqChanges, err := gerritClient.Search(MAX_CLS_PER_POLL, gerrit.SearchStatus("open"), gerrit.SearchProject("skia"), gerrit.SearchLabel(gerrit.COMMITQUEUE_LABEL, "2"))
+		if err != nil {
+			glog.Errorf("Error searching for open changes waiting for CQ in Gerrit: %s", err)
+			continue
+		}
+
+		// Combine dryrun and CQ changes into one slice.
+		changes := dryRunChanges
+		changes = append(changes, cqChanges...)
+		cqMetric.Update(int64(len(changes)))
+
+		for _, change := range changes {
+			if err := cqClient.ReportCQStats(change.Issue); err != nil {
+				glog.Errorf("Could not get CQ stats for %d: %s", change.Issue, err)
+				continue
+			}
+		}
+		liveness.Reset()
+	}
+}
+
+// monitorStatsForLandedCLs queries Gerrit for all CLs that have been merged in
+// the last AFTER_COMMIT_POLL_TIME seconds. The following metrics are then
+// reported for the matching CLs:
+// * The total time the CL spent waiting for CQ trybots to complete.
+// * The time each CQ trybot took to complete.
+func monitorStatsForLandedCLs(cqClient *cq.Client, gerritClient *gerrit.Gerrit) {
+	liveness := metrics2.NewLiveness(fmt.Sprintf("%s.%s", METRIC_NAME, cq.LANDED_METRIC_NAME))
+	previousPollChanges := []*gerrit.ChangeInfo{}
+	for _ = range time.Tick(time.Duration(AFTER_COMMIT_POLL_TIME)) {
+		// Add a short (2 min) buffer to overlap with the last poll to make sure
+		// we do not lose any edge cases.
+		t_delta := time.Now().Add(-AFTER_COMMIT_POLL_TIME).Add(-2 * time.Minute)
+		changes, err := gerritClient.Search(MAX_CLS_PER_POLL, gerrit.SearchModifiedAfter(t_delta), gerrit.SearchStatus("merged"), gerrit.SearchProject("skia"))
+		if err != nil {
+			glog.Errorf("Error searching for merged changes in Gerrit: %s", err)
+			continue
+		}
+		for _, change := range changes {
+			if gerrit.ContainsAny(change.Issue, previousPollChanges) {
+				continue
+			}
+			if err := cqClient.ReportCQStats(change.Issue); err != nil {
+				glog.Errorf("Could not get CQ stats for %d: %s", change.Issue, err)
+				continue
+			}
+		}
+		previousPollChanges = changes
+		liveness.Reset()
+	}
+}
+
+func refreshCQTryBots(cqClient *cq.Client) {
+	for _ = range time.Tick(time.Duration(REFRESH_CQ_TRYBOTS_TIME)) {
+		if err := cqClient.RefreshCQTryBots(); err != nil {
+			glog.Errorf("Error refresing CQ trybots: %s", err)
+		}
+	}
+}
+
+func main() {
+	defer common.LogPanic()
+	common.InitWithMetrics2(METRIC_NAME, influxHost, influxUser, influxPassword, influxDatabase, testing)
+
+	httpClient := httputils.NewTimeoutClient()
+	gerritClient, err := gerrit.NewGerrit(gerrit.GERRIT_SKIA_URL, "", httpClient)
+	if err != nil {
+		glog.Fatalf("Failed to create Gerrit client: %s", err)
+	}
+	cqClient, err := cq.NewClient(gerritClient, cq.GetSkiaCQTryBots, METRIC_NAME)
+	if err != nil {
+		glog.Fatalf("Failed to create CQ client: %s", err)
+	}
+
+	// Periodically refresh slice of trybots to make sure any changes to cq.cfg
+	// are picked up without needing to restart the app.
+	go refreshCQTryBots(cqClient)
+	// Monitor in-flight CLs.
+	go monitorStatsForInFlightCLs(cqClient, gerritClient)
+	// Monitor landed CLs.
+	monitorStatsForLandedCLs(cqClient, gerritClient)
+}
