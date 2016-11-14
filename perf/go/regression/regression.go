2534eeac53585f555d5d96d83c01697219715c8a
Joe Gregorio <jcgregorio@google.com>
2016-11-14 13:29:38 -0500

    perf: Add module for tracking regression.
    Next CL will add to/from MySQL.
    
    BUG=skia:
    
    Change-Id: Iff41e65e3356634ff0af649d6aaaf804c512902b
    Reviewed-on: https://skia-review.googlesource.com/4765
    Commit-Queue: Joe Gregorio <jcgregorio@google.com>
    Reviewed-by: Ben Wagner <benjaminwagner@google.com>

---
+++ perf/go/regression/regression.go
@@ -0,0 +1,155 @@
+// Package regression provides for tracking Perf regressions.
+package regression
+
+import (
+	"encoding/json"
+	"errors"
+	"sync"
+
+	"go.skia.org/infra/perf/go/clustering2"
+	"go.skia.org/infra/perf/go/dataframe"
+)
+
+var ErrNoClusterFound = errors.New("No Cluster.")
+
+// Status is used in TriageStatus.
+type Status string
+
+// Status constants.
+const (
+	NONE   Status = ""       // There is no regression.
+	NEW    Status = "New"    // There is an untriaged regression.
+	BUG    Status = "Bug"    // This regression is a bug.
+	IGNORE Status = "Ignore" // The regression is ignorable, e.g. noise or SKP update.
+	NSF    Status = "Nsf"    // Insufficient data to do clustering.
+)
+
+// Regressions is a map[query]Regression and one Regressions is stored for each
+// CommitID if any regressions are found.
+type Regressions struct {
+	ByQuery map[string]*Regression `json:"by_query"`
+	mutex   sync.Mutex
+}
+
+type TriageStatus struct {
+	Status  Status `json:"status"`
+	Message string `json:"message"`
+}
+
+// Regression tracks the status of the Low and High regression clusters, if they
+// exist for a given CommitID and query.
+//
+// Note that Low and High can be nil if no regression has been found in that
+// direction.
+type Regression struct {
+	Low        *clustering2.ClusterSummary `json:"low"`   // Can be nil.
+	High       *clustering2.ClusterSummary `json:"high"`  // Can be nil.
+	Frame      *dataframe.FrameResponse    `json:"frame"` // Describes the Low and High ClusterSummary's.
+	LowStatus  TriageStatus                `json:"low_status"`
+	HighStatus TriageStatus                `json:"high_status"`
+}
+
+func newRegression() *Regression {
+	return &Regression{
+		LowStatus: TriageStatus{
+			Status: NONE,
+		},
+		HighStatus: TriageStatus{
+			Status: NONE,
+		},
+	}
+}
+
+func New() *Regressions {
+	return &Regressions{
+		ByQuery: map[string]*Regression{},
+	}
+}
+
+// SetLow sets the cluster for a low regression.
+func (r *Regressions) SetLow(query string, df *dataframe.FrameResponse, low *clustering2.ClusterSummary) {
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
+	reg, ok := r.ByQuery[query]
+	if !ok {
+		reg = newRegression()
+		r.ByQuery[query] = reg
+	}
+	if reg.Frame == nil {
+		reg.Frame = df
+	}
+	// TODO(jcgregorio) Add checks so that we only overwrite a cluster if the new
+	// cluster is 'better', for some definition of 'better'.
+	reg.Low = low
+	if reg.LowStatus.Status == NONE {
+		reg.LowStatus.Status = NEW
+	}
+}
+
+// SetHigh sets the cluster for a high regression.
+func (r *Regressions) SetHigh(query string, df *dataframe.FrameResponse, high *clustering2.ClusterSummary) {
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
+	reg, ok := r.ByQuery[query]
+	if !ok {
+		reg = newRegression()
+		r.ByQuery[query] = reg
+	}
+	if reg.Frame == nil {
+		reg.Frame = df
+	}
+	// TODO(jcgregorio) Add checks so that we only overwrite a cluster if the new
+	// cluster is 'better', for some definition of 'better'.
+	reg.High = high
+	if reg.HighStatus.Status == NONE {
+		reg.HighStatus.Status = NEW
+	}
+}
+
+// TriageLow sets the triage status for the low cluster.
+func (r *Regressions) TriageLow(query string, tr TriageStatus) error {
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
+	reg, ok := r.ByQuery[query]
+	if !ok {
+		return ErrNoClusterFound
+	}
+	if reg.Low == nil {
+		return ErrNoClusterFound
+	}
+	reg.LowStatus = tr
+	return nil
+}
+
+// TriageHigh sets the triage status for the high cluster.
+func (r *Regressions) TriageHigh(query string, tr TriageStatus) error {
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
+	reg, ok := r.ByQuery[query]
+	if !ok {
+		return ErrNoClusterFound
+	}
+	if reg.High == nil {
+		return ErrNoClusterFound
+	}
+	reg.HighStatus = tr
+	return nil
+}
+
+// Triaged returns true if all clusters are triaged.
+func (r *Regressions) Triaged() bool {
+	ret := true
+	for _, reg := range r.ByQuery {
+		ret = ret && (reg.HighStatus.Status != NEW)
+		ret = ret && (reg.LowStatus.Status != NEW)
+	}
+	return ret
+}
+
+// JSON returns the Regressions serialized as JSON. Use this instead of
+// serializing Regression directly as it holds the mutex while serializing.
+func (r *Regressions) JSON() ([]byte, error) {
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
+	return json.Marshal(r)
+}
---
+++ perf/go/regression/regression_test.go
@@ -0,0 +1,60 @@
+package regression
+
+import (
+	"testing"
+
+	"go.skia.org/infra/go/testutils"
+	"go.skia.org/infra/perf/go/clustering2"
+	"go.skia.org/infra/perf/go/dataframe"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRegressions(t *testing.T) {
+	testutils.SmallTest(t)
+	r := New()
+	assert.True(t, r.Triaged(), "With no clusters, it should have Triaged() == true.")
+
+	df := &dataframe.FrameResponse{}
+	cl := &clustering2.ClusterSummary{}
+	r.SetLow("source_type=skp", df, cl)
+	assert.False(t, r.Triaged(), "Should not be Triaged.")
+
+	// Triage the low cluster.
+	err := r.TriageLow("source_type=skp", TriageStatus{
+		Status:  IGNORE,
+		Message: "SKP Update",
+	})
+	assert.NoError(t, err)
+	assert.True(t, r.Triaged())
+
+	// Trying to triage a high cluster that doesn't exists.
+	err = r.TriageHigh("source_type=skp", TriageStatus{
+		Status: BUG,
+	})
+	assert.Equal(t, err, ErrNoClusterFound)
+	assert.True(t, r.Triaged())
+
+	// Set a high cluster.
+	r.SetHigh("source_type=skp", df, cl)
+	assert.False(t, r.Triaged())
+
+	// And triage the high cluster.
+	err = r.TriageHigh("source_type=skp", TriageStatus{
+		Status:  BUG,
+		Message: "See bug #foo.",
+	})
+	assert.NoError(t, err)
+	assert.True(t, r.Triaged())
+
+	// Trying to triage an unknown query.
+	err = r.TriageHigh("uknownquery", TriageStatus{
+		Status: BUG,
+	})
+	assert.Equal(t, err, ErrNoClusterFound)
+
+	// Try serializing to JSON.
+	b, err := r.JSON()
+	assert.NoError(t, err)
+	assert.Equal(t, "{\"by_query\":{\"source_type=skp\":{\"low\":{\"centroid\":null,\"keys\":null,\"param_summaries\":null,\"step_fit\":null,\"step_point\":null,\"num\":0},\"high\":{\"centroid\":null,\"keys\":null,\"param_summaries\":null,\"step_fit\":null,\"step_point\":null,\"num\":0},\"frame\":{\"dataframe\":null,\"ticks\":null,\"skps\":null,\"msg\":\"\"},\"low_status\":{\"status\":\"Ignore\",\"message\":\"SKP Update\"},\"high_status\":{\"status\":\"Bug\",\"message\":\"See bug #foo.\"}}}}", string(b))
+}
