b96be83d4da94d22587009b097a45eec000656dd
Eric Boren <borenet@google.com>
2016-11-18 10:23:11 -0500

    [task scheduler] BusyBots need to consider tasks as well as bots
    BUG=skia:5984
    
    Change-Id: I0e0f3b864b6689f00f1c133e0930d509cc711806

--- task_scheduler/go/scheduling/busy_bots.go
+++ task_scheduler/go/scheduling/busy_bots.go
@@ -8,22 +8,22 @@ import (
 
 // busyBots is a struct used for marking a bot as busy while it runs a Task.
 type busyBots struct {
-	bots map[string]bool
+	bots map[string]string
 	mtx  sync.Mutex
 }
 
 // newBusyBots returns a busyBots instance.
 func newBusyBots() *busyBots {
 	return &busyBots{
-		bots: map[string]bool{},
+		bots: map[string]string{},
 	}
 }
 
 // Reserve marks a bot as busy.
-func (b *busyBots) Reserve(bot string) {
+func (b *busyBots) Reserve(bot, task string) {
 	b.mtx.Lock()
 	defer b.mtx.Unlock()
-	b.bots[bot] = true
+	b.bots[bot] = task
 }
 
 // Filter returns a copy of the given slice of bots with the busy bots removed.
@@ -32,7 +32,7 @@ func (b *busyBots) Filter(bots []*swarming_api.SwarmingRpcsBotInfo) []*swarming_
 	defer b.mtx.Unlock()
 	rv := make([]*swarming_api.SwarmingRpcsBotInfo, 0, len(bots))
 	for _, bot := range bots {
-		if !b.bots[bot.BotId] {
+		if _, ok := b.bots[bot.BotId]; !ok {
 			rv = append(rv, bot)
 		}
 	}
@@ -43,12 +43,15 @@ func (b *busyBots) Filter(bots []*swarming_api.SwarmingRpcsBotInfo) []*swarming_
 func (b *busyBots) Busy(bot string) bool {
 	b.mtx.Lock()
 	defer b.mtx.Unlock()
-	return b.bots[bot]
+	_, ok := b.bots[bot]
+	return ok
 }
 
 // Release marks the bot as not busy.
-func (b *busyBots) Release(bot string) {
+func (b *busyBots) Release(bot, task string) {
 	b.mtx.Lock()
 	defer b.mtx.Unlock()
-	delete(b.bots, bot)
+	if b.bots[bot] == task {
+		delete(b.bots, bot)
+	}
 }
