09846b59fe8b686b60f99005a8f1b7d1ec5c9d44
Ben Wagner <benjaminwagner@google.com>
2016-11-22 13:20:45 -0500

    [task scheduler] Logging to diagnose idle bots.
    BUG=skia:
    
    Change-Id: Ia238aaf704251a32b148f6edcfe8244bf3ebd577
    Reviewed-on: https://skia-review.googlesource.com/5123
    Reviewed-by: Kevin Lubick <kjlubick@google.com>
    Commit-Queue: Ben Wagner <benjaminwagner@google.com>

--- task_scheduler/go/scheduling/busy_bots.go
+++ task_scheduler/go/scheduling/busy_bots.go
@@ -4,6 +4,7 @@ import (
 	"sync"
 
 	swarming_api "github.com/luci/luci-go/common/api/swarming/swarming/v1"
+	"github.com/skia-dev/glog"
 )
 
 // busyBots is a struct used for marking a bot as busy while it runs a Task.
@@ -31,11 +32,22 @@ func (b *busyBots) Filter(bots []*swarming_api.SwarmingRpcsBotInfo) []*swarming_
 	b.mtx.Lock()
 	defer b.mtx.Unlock()
 	rv := make([]*swarming_api.SwarmingRpcsBotInfo, 0, len(bots))
+	// TODO(benjaminwagner): Remove debug logging.
+	androidbots := map[string]string{}
 	for _, bot := range bots {
-		if _, ok := b.bots[bot.BotId]; !ok {
+		task, ok := b.bots[bot.BotId]
+		if ok {
+			if hasDim(bot, "device_type", "*") {
+				androidbots[bot.BotId] = "busy " + task
+			}
+		} else {
+			if hasDim(bot, "device_type", "*") {
+				androidbots[bot.BotId] = "available"
+			}
 			rv = append(rv, bot)
 		}
 	}
+	glog.Infof("DEBUG: android bots: %s", androidbots)
 	return rv
 }
 
