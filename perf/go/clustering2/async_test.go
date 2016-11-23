56991902a90cd11a636691c3e2ceca07027c763b
Joe Gregorio <jcgregorio@google.com>
2016-11-23 11:14:08 -0500

    Don't do clustering on traces with <50% data.
    Continuous clustering is up and running, but there are too many
    false positives because for some bots (Win) there is insufficient
    data. See:
      https://screenshot.googleplex.com/9j6Vr9xY8pD.png
    
    For example, this alerting cluster is a set of traces with
    only two points per trace:
      https://perf.skia.org/e/?keys=5889&begin=1479839155&end=1479847743
    
    For now we don't cluster on traces with insufficient data. Going
    forward we need to add formulas we can use to find out which
    bots are not producing enough data so we can add more bots.
    
    BUG=skia:
    
    Change-Id: Id2db1db132f6d6a3d533962b92f69698a8443bea

---
+++ perf/go/clustering2/async_test.go
@@ -0,0 +1,59 @@
+package clustering2
+
+import (
+	"testing"
+
+	"go.skia.org/infra/go/testutils"
+	"go.skia.org/infra/go/vec32"
+	"go.skia.org/infra/perf/go/ptracestore"
+)
+
+const (
+	e = vec32.MISSING_DATA_SENTINEL
+)
+
+func TestTooMuchMissingData(t *testing.T) {
+	testutils.SmallTest(t)
+	testCases := []struct {
+		value    ptracestore.Trace
+		expected bool
+		message  string
+	}{
+		{
+			value:    ptracestore.Trace{e, e, 1, 1, 1},
+			expected: true,
+			message:  "missing one side",
+		},
+		{
+			value:    ptracestore.Trace{1, e, 1, 1, 1},
+			expected: false,
+			message:  "exactly 50%",
+		},
+		{
+			value:    ptracestore.Trace{e, e, 1, 1},
+			expected: true,
+			message:  "missing one side - even",
+		},
+		{
+			value:    ptracestore.Trace{e, 1, 1, 1},
+			expected: false,
+			message:  "exactly 50% - even",
+		},
+		{
+			value:    ptracestore.Trace{e, 1, 1},
+			expected: true,
+			message:  "Radius = 1",
+		},
+		{
+			value:    ptracestore.Trace{1},
+			expected: false,
+			message:  "len(tr) < 3",
+		},
+	}
+
+	for _, tc := range testCases {
+		if got, want := tooMuchMissingData(tc.value), tc.expected; got != want {
+			t.Errorf("Failed case Got %v Want %v: %s", got, want, tc.message)
+		}
+	}
+}
