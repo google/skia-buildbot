ba37ec3ea49f077fd02172813c66cc988cec3cc9
Stephan Altmueller <stephana@google.com>
2016-11-28 13:21:07 -0500

    Factor out indexing of a bolt database
    Factors out indexing from issuestore into the boltutil package. 
    The Store type allows to store instances of any type that implement 
    a simple interface and supports indexing secondary keys. 
    
    BUG=skia:
    
    Change-Id: I42916f58c33071656cb0b7f6c4ac2aa14969e755

---
+++ go/boltutil/store_test.go
@@ -0,0 +1,74 @@
+package boltutil
+
+import (
+	"path"
+	"testing"
+
+	"github.com/boltdb/bolt"
+	assert "github.com/stretchr/testify/require"
+
+	"go.skia.org/infra/go/fileutil"
+	"go.skia.org/infra/go/testutils"
+	"go.skia.org/infra/go/util"
+)
+
+const (
+	TEST_DATA_DIR   = "./testdata"
+	TEST_INDEX_NAME = "testindex"
+)
+
+type exampleRec struct {
+	ID  string
+	Val string
+}
+
+func newExample(id, val string) *exampleRec {
+	return &exampleRec{
+		ID:  id,
+		Val: val,
+	}
+}
+
+func (e *exampleRec) Key() string {
+	return e.ID
+}
+
+func (e *exampleRec) IndexValues() map[string][]string {
+	return map[string][]string{TEST_INDEX_NAME: []string{e.Val}}
+}
+
+func TestKeyConflicts(t *testing.T) {
+	testutils.MediumTest(t)
+	const N_ISSUES = 20
+
+	// Add a number of issues
+	baseDir, err := fileutil.EnsureDirExists(TEST_DATA_DIR)
+	assert.NoError(t, err)
+	defer testutils.RemoveAll(t, TEST_DATA_DIR)
+
+	db, err := bolt.Open(path.Join(baseDir, "test.db"), 0600, nil)
+	assert.NoError(t, err)
+	defer db.Close()
+
+	ib, err := NewIndexedBucket(&Config{
+		DB:      db,
+		Name:    "testbucket",
+		Indices: []string{TEST_INDEX_NAME},
+		Codec:   util.JSONCodec(&exampleRec{}),
+	})
+	assert.NoError(t, err)
+
+	assert.NoError(t, ib.Insert([]Record{newExample("a", "aaa")}))
+	assert.NoError(t, ib.Insert([]Record{newExample("aa", "aa")}))
+	assert.NoError(t, ib.Insert([]Record{newExample("aaa", "a")}))
+
+	found, err := ib.ReadIndex(TEST_INDEX_NAME, []string{"a", "aa", "aaa"})
+	assert.NoError(t, err)
+	assert.Equal(t, map[string][]string{"a": {"aaa"}, "aa": {"aa"}, "aaa": {"a"}}, found)
+
+	// issueStore, err := New(TEST_DATA_DIR)
+	// assert.NoError(t, err)
+	// Set up the store
+
+	// Add a few instances
+}
