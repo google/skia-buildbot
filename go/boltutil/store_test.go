dbaf0613341a9ecb7bedf6fb14aba8fb438e107e
Stephan Altmueller <stephana@google.com>
2016-11-28 22:02:37 -0500

    Add addition and removal of indices to IndexedBucket
    BUG=skia:
    
    Change-Id: I76e737d28ebcb2a77d35471f2095665bbb3d71fe

--- go/boltutil/store_test.go
+++ go/boltutil/store_test.go
@@ -13,58 +13,163 @@ import (
 )
 
 const (
-	TEST_DATA_DIR   = "./testdata"
-	TEST_INDEX_NAME = "testindex"
+	TEST_DATA_DIR    = "./testdata"
+	TEST_INDEX_ONE   = "testindex-1"
+	TEST_INDEX_TWO   = "testindex-2"
+	TEST_BUCKET_NAME = "testbucket"
 )
+
+func newIndexedBucket(t *testing.T, indices []string) (*IndexedBucket, string, string) {
+	baseDir, err := ioutil.TempDir(".", TEST_DATA_DIR)
+	assert.NoError(t, err)
+
+	dbFileName := path.Join(baseDir, "test.db")
+	db, err := bolt.Open(dbFileName, 0600, nil)
+	assert.NoError(t, err)
+
+	ib, err := NewIndexedBucket(&Config{
+		DB:      db,
+		Name:    TEST_BUCKET_NAME,
+		Indices: indices,
+		Codec:   util.JSONCodec(&exampleRec{}),
+	})
+	assert.NoError(t, err)
+	return ib, baseDir, dbFileName
+}
 
 func TestKeyConflicts(t *testing.T) {
 	testutils.MediumTest(t)
 
-	// Add a number of issues
-	baseDir, err := ioutil.TempDir("", "temp-boltutil")
-	assert.NoError(t, err)
-	defer testutils.RemoveAll(t, TEST_DATA_DIR)
+	testIndices := []string{TEST_INDEX_ONE}
+	ib, baseDir, _ := newIndexedBucket(t, testIndices)
+	defer testutils.RemoveAll(t, baseDir)
+	defer ib.DB.Close()
 
-	db, err := bolt.Open(path.Join(baseDir, "test.db"), 0600, nil)
-	assert.NoError(t, err)
-	defer db.Close()
+	currentTestIndices = util.CopyStringSlice(testIndices)
+	assert.NoError(t, ib.Insert([]Record{
+		newExample("a", "aaa", ""),
+		newExample("aa", "aa", ""),
+		newExample("aaa", "a", ""),
+	}))
 
-	ib, err := NewIndexedBucket(&Config{
-		DB:      db,
-		Name:    "testbucket",
-		Indices: []string{TEST_INDEX_NAME},
-		Codec:   util.JSONCodec(&exampleRec{}),
-	})
-	assert.NoError(t, err)
-
-	assert.NoError(t, ib.Insert([]Record{newExample("a", "aaa")}))
-	assert.NoError(t, ib.Insert([]Record{newExample("aa", "aa")}))
-	assert.NoError(t, ib.Insert([]Record{newExample("aaa", "a")}))
-
-	found, err := ib.ReadIndex(TEST_INDEX_NAME, []string{"a", "aa", "aaa"})
+	found, err := ib.ReadIndex(TEST_INDEX_ONE, []string{"a", "aa", "aaa"})
 	assert.NoError(t, err)
 	assert.Equal(t, map[string][]string{"a": {"aaa"}, "aa": {"aa"}, "aaa": {"a"}}, found)
 
 	assert.NoError(t, ib.Delete([]string{"a", "aa", "aaa"}))
 
-	assert.NoError(t, ib.Insert([]Record{newExample("a", "aaa")}))
-	assert.NoError(t, ib.Insert([]Record{newExample("ab", "aa")}))
-	assert.NoError(t, ib.Insert([]Record{newExample("aac", "a")}))
+	assert.NoError(t, ib.Insert([]Record{
+		newExample("a", "aaa", ""),
+		newExample("ab", "aa", ""),
+		newExample("aac", "a", ""),
+	}))
 
-	found, err = ib.ReadIndex(TEST_INDEX_NAME, []string{"a", "aa", "aaa"})
+	found, err = ib.ReadIndex(TEST_INDEX_ONE, []string{"a", "aa", "aaa"})
 	assert.NoError(t, err)
 	assert.Equal(t, map[string][]string{"a": {"aac"}, "aa": {"ab"}, "aaa": {"a"}}, found)
 }
 
-type exampleRec struct {
-	ID  string
-	Val string
+func TestIndexedBucket(t *testing.T) {
+	ib, baseDir, dbFileName := newIndexedBucket(t, []string{TEST_INDEX_ONE})
+	defer testutils.RemoveAll(t, baseDir)
+
+	currentTestIndices = []string{TEST_INDEX_ONE}
+	inputRecs := []Record{
+		newExample("id_01", "val_01", "val_11"),
+		newExample("id_02", "val_01", "val_11"),
+		newExample("id_03", "val_02", "val_12"),
+		newExample("id_04", "val_02", "val_12"),
+		newExample("id_05", "val_03", "val_12"),
+	}
+	assert.NoError(t, ib.Insert(inputRecs))
+
+	// Read from the first index.
+	found, err := ib.ReadIndex(TEST_INDEX_ONE, []string{"val_01", "val_02", "val_03"})
+	assert.NoError(t, err)
+	assert.Equal(t, map[string][]string{
+		"val_01": {"id_01", "id_02"},
+		"val_02": {"id_03", "id_04"},
+		"val_03": {"id_05"}}, found)
+
+	// CRUD
+	foundAll, total, err := ib.List(0, -1)
+	assert.NoError(t, err)
+	assert.Equal(t, len(inputRecs), total)
+	assert.Equal(t, len(inputRecs), len(foundAll))
+	for i, rec := range inputRecs {
+		assert.Equal(t, rec, foundAll[i])
+	}
+
+	assert.NoError(t, ib.Delete([]string{"id_03"}))
+	foundRec, err := ib.Read([]string{"id_03", "id_01"})
+	assert.NoError(t, err)
+	assert.Equal(t, []Record{nil, inputRecs[0]}, foundRec)
+
+	found, err = ib.ReadIndex(TEST_INDEX_ONE, []string{"val_01", "val_02", "val_03"})
+	assert.NoError(t, err)
+	compReadIndex(t, map[string][]string{
+		"val_01": {"id_01", "id_02"},
+		"val_02": {"id_04"},
+		"val_03": {"id_05"}}, found)
+
+	assert.NoError(t, ib.Update(inputRecs[:1], func(tx *bolt.Tx, recs []Record) error {
+		rec := recs[0].(*exampleRec)
+		rec.Val_1 = rec.Val_1 + "--1"
+		return nil
+	}))
+
+	found, err = ib.ReadIndex(TEST_INDEX_ONE, []string{"val_01--1", "val_01"})
+	assert.NoError(t, err)
+	assert.Equal(t, map[string][]string{
+		"val_01--1": {"id_01"},
+		"val_01":    {"id_02"}}, found)
+
+	// And an index to that database.
+	assert.NoError(t, ib.DB.Close())
+
+	// Make IndexValues return for both indices.
+	currentTestIndices = []string{TEST_INDEX_ONE, TEST_INDEX_TWO}
+
+	db, err := bolt.Open(dbFileName, 0600, nil)
+	assert.NoError(t, err)
+
+	ib, err = NewIndexedBucket(&Config{
+		DB:      db,
+		Name:    TEST_BUCKET_NAME,
+		Indices: util.CopyStringSlice(currentTestIndices),
+		Codec:   util.JSONCodec(&exampleRec{}),
+	})
+
+	found, err = ib.ReadIndex(TEST_INDEX_TWO, []string{"val_11", "val_12"})
+	assert.NoError(t, err)
+	compReadIndex(t, map[string][]string{
+		"val_11": {"id_01", "id_02"},
+		"val_12": {"id_04", "id_05"},
+	}, found)
 }
 
-func newExample(id, val string) *exampleRec {
+func compReadIndex(t *testing.T, exp map[string][]string, actual map[string][]string) {
+	assert.Equal(t, len(exp), len(actual))
+	for key, vals := range exp {
+		assert.NotNil(t, actual[key])
+		assert.Equal(t, util.NewStringSet(vals), util.NewStringSet(actual[key]))
+	}
+}
+
+var currentTestIndices []string
+
+// exampleRec is a test implementation of Record.
+type exampleRec struct {
+	ID    string
+	Val_1 string
+	Val_2 string
+}
+
+func newExample(id, val_1, val_2 string) *exampleRec {
 	return &exampleRec{
-		ID:  id,
-		Val: val,
+		ID:    id,
+		Val_1: val_1,
+		Val_2: val_2,
 	}
 }
 
@@ -73,5 +178,14 @@ func (e *exampleRec) Key() string {
 }
 
 func (e *exampleRec) IndexValues() map[string][]string {
-	return map[string][]string{TEST_INDEX_NAME: []string{e.Val}}
+	ret := map[string][]string{}
+	for _, idx := range currentTestIndices {
+		switch idx {
+		case TEST_INDEX_ONE:
+			ret[idx] = []string{e.Val_1}
+		case TEST_INDEX_TWO:
+			ret[idx] = []string{e.Val_2}
+		}
+	}
+	return ret
 }
