5806e85985cfe328cb2c0e9b30bceb9a4f617f53
Ben Wagner <benjaminwagner@google.com>
2016-11-29 13:41:37 -0500

    [task scheduler] Add gs_jobs_viewer to download backed-up Jobs from GS.
    Fix a bug in downloadGOB -- GS serves us decompressed data.
    
    BUG=skia:5729
    
    Change-Id: Ide2f6c0b0ff1332dec7fda0a550310d4eb8953fa
    Reviewed-on: https://skia-review.googlesource.com/5068
    Commit-Queue: Ben Wagner <benjaminwagner@google.com>
    Reviewed-by: Eric Boren <borenet@google.com>

---
+++ task_scheduler/go/db/recovery/gs_jobs_viewer/main.go
@@ -0,0 +1,79 @@
+// Read Job GOBs from GS and write as JSON.
+//
+// Example:
+//   gs_jobs_viewer
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"os"
+	"sort"
+	"time"
+
+	"cloud.google.com/go/storage"
+	"golang.org/x/net/context"
+	"google.golang.org/api/option"
+
+	"github.com/skia-dev/glog"
+	"go.skia.org/infra/go/auth"
+	"go.skia.org/infra/go/common"
+	"go.skia.org/infra/task_scheduler/go/db"
+	"go.skia.org/infra/task_scheduler/go/db/recovery"
+)
+
+var (
+	gsBucket = flag.String("bucket", "skia-task-scheduler", "GS bucket to read.")
+	period   = flag.Duration("period", 24*time.Hour, "Duration of time range to read.")
+)
+
+func main() {
+	defer common.LogPanic()
+
+	// Global init.
+	common.Init()
+
+	// Authenticated HTTP client.
+	httpClient, err := auth.NewClient(true, "", auth.SCOPE_READ_ONLY)
+	if err != nil {
+		glog.Fatal(err)
+	}
+
+	ctx := context.Background()
+	gsClient, err := storage.NewClient(ctx, option.WithHTTPClient(httpClient))
+	if err != nil {
+		glog.Fatal(err)
+	}
+
+	begin := time.Now().UTC().Add(-*period)
+
+	glog.Infof("Reading jobs since %s...", begin)
+	jobsMap, err := recovery.RetrieveJobs(ctx, begin, gsClient, *gsBucket)
+	if err != nil {
+		glog.Fatal(err)
+	}
+
+	jobs := make([]*db.Job, 0, len(jobsMap))
+	for _, job := range jobsMap {
+		jobs = append(jobs, job)
+	}
+	sort.Sort(db.JobSlice(jobs))
+
+	v := struct {
+		Begin time.Time
+		Jobs  []*db.Job
+	}{
+		Begin: begin,
+		Jobs:  jobs,
+	}
+
+	enc, err := json.MarshalIndent(v, "", "  ")
+	if err != nil {
+		glog.Fatal(err)
+	}
+	if _, err := os.Stdout.Write(enc); err != nil {
+		glog.Fatal(err)
+	}
+	fmt.Fprintln(os.Stdout)
+}
