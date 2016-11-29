d695a4e98833ba115a41847b212b1f3f64e4724c
Ben Wagner <benjaminwagner@google.com>
2016-11-29 13:50:56 -0500

    [task scheduler] Add ts_local_db_viewer.
    BUG=skia:
    
    Change-Id: I4f7f80e1c804465bdfb5c0363bc4291369ad0aec
    Reviewed-on: https://skia-review.googlesource.com/4837
    Commit-Queue: Ben Wagner <benjaminwagner@google.com>
    Reviewed-by: Eric Boren <borenet@google.com>

---
+++ task_scheduler/go/db/local_db/ts_local_db_viewer/main.go
@@ -0,0 +1,79 @@
+// Read data from a local task scheduler DB file and output as JSON.
+//
+// Example:
+//   dbviewer --db /path/to/task_scheduler.bdb
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/skia-dev/glog"
+	"go.skia.org/infra/go/common"
+	"go.skia.org/infra/task_scheduler/go/db"
+	"go.skia.org/infra/task_scheduler/go/db/local_db"
+)
+
+var (
+	dbfile   = flag.String("db", local_db.DB_FILENAME, "DB to read.")
+	period   = flag.Duration("period", 24*time.Hour, "Duration of time range to read.")
+	beginStr = flag.String("begin", "", "Beginning of time range to read; default (now - period). Format is "+time.RFC3339+".")
+)
+
+func main() {
+	defer common.LogPanic()
+
+	// Global init.
+	common.Init()
+
+	d, err := local_db.NewDB(local_db.DB_NAME, *dbfile)
+	if err != nil {
+		glog.Fatal(err)
+	}
+
+	begin := time.Now().UTC().Add(-*period)
+	if *beginStr != "" {
+		parsed, err := time.Parse(time.RFC3339, *beginStr)
+		if err != nil {
+			glog.Fatal(err)
+		}
+		begin = parsed.UTC()
+	}
+	end := begin.Add(*period)
+
+	glog.Infof("Reading tasks from %s to %s...", begin, end)
+	tasks, err := d.GetTasksFromDateRange(begin, end)
+	if err != nil {
+		glog.Fatal(err)
+	}
+
+	glog.Infof("Reading jobs from %s to %s...", begin, end)
+	jobs, err := d.GetJobsFromDateRange(begin, end)
+	if err != nil {
+		glog.Fatal(err)
+	}
+
+	v := struct {
+		Begin time.Time
+		End   time.Time
+		Tasks []*db.Task
+		Jobs  []*db.Job
+	}{
+		Begin: begin,
+		End:   end,
+		Tasks: tasks,
+		Jobs:  jobs,
+	}
+
+	enc, err := json.MarshalIndent(v, "", "  ")
+	if err != nil {
+		glog.Fatal(err)
+	}
+	if _, err := os.Stdout.Write(enc); err != nil {
+		glog.Fatal(err)
+	}
+	fmt.Println()
+}
