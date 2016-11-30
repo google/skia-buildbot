4ffafcf7fc5fada4e859f768065a0ba22e57bbef
Ben Wagner <benjaminwagner@google.com>
2016-11-30 19:23:45 +0000

    Revert "[task scheduler] Logging to diagnose idle bots."
    This reverts commit 09846b59fe8b686b60f99005a8f1b7d1ec5c9d44.
    
    Reason for revert: Issue hasn't occurred recently.
    
    Original change's description:
    > [task scheduler] Logging to diagnose idle bots.
    > 
    > BUG=skia:
    > 
    > Change-Id: Ia238aaf704251a32b148f6edcfe8244bf3ebd577
    > Reviewed-on: https://skia-review.googlesource.com/5123
    > Reviewed-by: Kevin Lubick <kjlubick@google.com>
    > Commit-Queue: Ben Wagner <benjaminwagner@google.com>
    > 
    
    TBR=benjaminwagner@google.com,kjlubick@google.com,borenet@google.com,reviews@skia.org
    NOPRESUBMIT=true
    NOTREECHECKS=true
    NOTRY=true
    
    Change-Id: Ide584f7a8845e3185bab6a0cdc7ca7adb8704863

--- task_scheduler/go/scheduling/busy_bots.go
+++ task_scheduler/go/scheduling/busy_bots.go
@@ -4,7 +4,6 @@ import (
 	"sync"
 
 	swarming_api "github.com/luci/luci-go/common/api/swarming/swarming/v1"
-	"github.com/skia-dev/glog"
 )
 
 // busyBots is a struct used for marking a bot as busy while it runs a Task.
@@ -32,22 +31,11 @@ func (b *busyBots) Filter(bots []*swarming_api.SwarmingRpcsBotInfo) []*swarming_
 	b.mtx.Lock()
 	defer b.mtx.Unlock()
 	rv := make([]*swarming_api.SwarmingRpcsBotInfo, 0, len(bots))
-	// TODO(benjaminwagner): Remove debug logging.
-	androidbots := map[string]string{}
 	for _, bot := range bots {
-		task, ok := b.bots[bot.BotId]
-		if ok {
-			if hasDim(bot, "device_type", "*") {
-				androidbots[bot.BotId] = "busy " + task
-			}
-		} else {
-			if hasDim(bot, "device_type", "*") {
-				androidbots[bot.BotId] = "available"
-			}
+		if _, ok := b.bots[bot.BotId]; !ok {
 			rv = append(rv, bot)
 		}
 	}
-	glog.Infof("DEBUG: android bots: %s", androidbots)
 	return rv
 }
 
