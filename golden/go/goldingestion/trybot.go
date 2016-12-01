b395ed40d4e90845971cd237b0fcbba59f6e5f5f
Stephan Altmueller <stephana@google.com>
2016-12-01 12:31:36 -0500

    Fix deadlock in gold trybot ingestion
    - Fixes a bug that caused a deadlock as soon as an error occurred during gold trybot ingestion. 
    - Breaks down ingestion into time chunks that are processed in reverse chronological order. 
    
    BUG=skia:
    
    Change-Id: I1c5f4073a2497233554fe6e4445fdaf6cb65cc71

--- golden/go/goldingestion/trybot.go
+++ golden/go/goldingestion/trybot.go
@@ -82,19 +82,26 @@ func (g *goldTrybotProcessor) getCommitID(commit *vcsinfo.LongCommit, dmResults 
 	var ok bool
 	var err error
 	var cacheId = fmt.Sprintf("%d:%d", dmResults.Issue, dmResults.Patchset)
-	g.cacheMutex.Lock()
-	if ts, ok = g.cache[cacheId]; !ok {
-		if ts, err = g.getCreatedTimeStamp(dmResults); err != nil {
-			return nil, err
-		}
 
-		// p.cache is a very crude LRU cache.
-		if len(g.cache) > TIMESTAMP_LRU_CACHE_SIZE {
-			g.cache = map[string]time.Time{}
+	err = func() error {
+		g.cacheMutex.Lock()
+		defer g.cacheMutex.Unlock()
+		if ts, ok = g.cache[cacheId]; !ok {
+			if ts, err = g.getCreatedTimeStamp(dmResults); err != nil {
+				return err
+			}
+
+			// p.cache is a very crude LRU cache.
+			if len(g.cache) > TIMESTAMP_LRU_CACHE_SIZE {
+				g.cache = map[string]time.Time{}
+			}
+			g.cache[cacheId] = ts
 		}
-		g.cache[cacheId] = ts
+		return nil
+	}()
+	if err != nil {
+		return nil, err
 	}
-	g.cacheMutex.Unlock()
 
 	// Get the source (url) from the issue.
 	var source string
