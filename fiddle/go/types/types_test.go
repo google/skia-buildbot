8476d7bfb4bf0bbb0296ad5d4c96d26f6a9c10e3
Joe Gregorio <jcgregorio@google.com>
2016-12-02 10:30:38 -0500

    fiddle: Fixes DrawOptions which now takes 2 more options.
    BUG=skia:
    
    Change-Id: I9db0572f5f5e8c74a0d0d1583146655898e0114a

--- fiddle/go/types/types_test.go
+++ fiddle/go/types/types_test.go
@@ -27,4 +27,24 @@ func TestOptions(t *testing.T) {
 	hash, err := o.ComputeHash(code)
 	assert.NoError(t, err)
 	assert.Equal(t, "cbb8dee39e9f1576cd97c2d504db8eee", hash)
+
+	code = `void draw(SkCanvas* canvas) {
+    SkPaint p;
+    p.setColor(SK_ColorRED);
+    p.setAntiAlias(true);
+    p.setStyle(SkPaint::kStroke_Style);
+    p.setStrokeWidth(10);
+
+    canvas->drawLine(20, 20, 100, 100, p);
+}`
+	o = Options{
+		Width:  256,
+		Height: 256,
+		Source: 0,
+		SRGB:   true,
+		F16:    true,
+	}
+	hash, err = o.ComputeHash(code)
+	assert.NoError(t, err)
+	assert.Equal(t, "fddc0a319e575c79a97ff535b455dc5d", hash)
 }
