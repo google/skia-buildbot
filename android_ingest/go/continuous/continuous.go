4c303ea4a33accbbd7c6a87f10424410062148ec
Joe Gregorio <jcgregorio@google.com>
2016-12-12 15:40:47 -0500

    android_ingest: Add continous building of the repo.
    BUG=skia:
    
    Change-Id: I8bd908e3820af98a3c0dff59e603bda7b72fc694

---
+++ android_ingest/go/continuous/continuous.go
@@ -0,0 +1,67 @@
+// Package continuous provides ...
+package continuous
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/skia-dev/glog"
+
+	"go.skia.org/infra/android_ingest/go/buildapi"
+	"go.skia.org/infra/android_ingest/go/poprepo"
+	"go.skia.org/infra/go/metrics2"
+)
+
+type Continuous struct {
+	api    *buildapi.API
+	repo   *poprepo.PopRepo
+	branch string
+}
+
+func New(branch, repoUrl, workdir string, client *http.Client) (*Continuous, error) {
+	repo, err := poprepo.NewPopRepo(repoUrl, workdir)
+	if err != nil {
+		return nil, err
+	}
+	api, err := buildapi.NewAPI(client)
+	if err != nil {
+		return nil, err
+	}
+	return &Continuous{
+		api:    api,
+		repo:   repo,
+		branch: branch,
+	}, nil
+}
+
+func (c *Continuous) Last() (int64, int64, error) {
+	return c.repo.GetLast()
+}
+
+func (c *Continuous) Start() {
+	go func() {
+		t := metrics2.NewTimer("repobuilder")
+		for _ = range time.Tick(time.Minute) {
+			t.Start()
+			buildid, _, err := c.repo.GetLast()
+			if err != nil {
+				glog.Errorf("Failed to get last buildid: %s", err)
+				continue
+			}
+			builds, err := c.api.List(c.branch, buildid)
+			if err != nil {
+				glog.Errorf("Failed to get buildids from api: %s", err)
+				continue
+			}
+			for _, b := range builds {
+				if err := c.repo.Add(b.BuildId, b.TS); err != nil {
+					glog.Errorf("Failed to add new buildid to repo: %s", err)
+					// break since we don't want to add anymore buildids until this one
+					// lands successfully.
+					break
+				}
+			}
+			t.Stop()
+		}
+	}()
+}
