2f4f3565df69687de7c534b9f0e40716e20293da
Joe Gregorio <jcgregorio@google.com>
2016-12-12 13:54:32 -0500

    android_ingest: Add continous building of the repo.
    BUG=skia:
    
    Change-Id: I8bd908e3820af98a3c0dff59e603bda7b72fc694

---
+++ android_ingest/go/continuous/continuous.go
@@ -0,0 +1,39 @@
+// Package continuous provides ...
+package continuous
+
+import (
+	"net/http"
+	"time"
+
+	"go.skia.org/infra/android_ingest/go/buildapi"
+	"go.skia.org/infra/android_ingest/go/poprepo"
+)
+
+type Continuous struct {
+	api    *buildapi.API
+	repo   *poprepo.PopRepo
+	branch string
+}
+
+func New(branch, repoUrl, workdir string, client *http.Client) (*Continuous, error) {
+	repo, err := poprepo.NewPopRepo(repoUrl, workdir)
+	if err != nil {
+		return nil, err
+	}
+	api, err := buildapi.NewAPI(client)
+	if err != nil {
+		return nil, err
+	}
+	return &Continuous{
+		api:    api,
+		repo:   repo,
+		branch: branch,
+	}, nil
+}
+
+func (c *Continuous) Start() {
+	go func() {
+		for _ = range time.Tick(time.Minute) {
+		}
+	}()
+}
