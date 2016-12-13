fb98e965018f469e93e4222c28b7acf9d4fbbb23
Joe Gregorio <jcgregorio@google.com>
2016-12-13 16:37:31 -0500

    androidingest: Add a cache of buildids and the git hash they map to.
    Involves a slight refactoring, instead of passing *repoUrl, *workRoot to continuous, we 
    create a git.Checkout and use that to construct both continuous.Process and lookup.Cache.
    
    BUG=skia:
    
    Change-Id: I98b00253625f1c920c120551446f94d5a102972d

---
+++ android_ingest/go/lookup/lookup.go
@@ -0,0 +1,104 @@
+// Package lookup provides ...
+package lookup
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"sync"
+
+	"github.com/skia-dev/glog"
+
+	"go.skia.org/infra/go/git"
+)
+
+// Cache keeps a cache of recent, at least four weeks, worth of buildids and
+// their associated git hashes. Since this is only used for ingesting test
+// results this window is fine since we can presume the tests successfully
+// finish running in under a month.
+type Cache struct {
+	mutex sync.Mutex
+
+	// hashes maps buildids to git hashes. We don't get fancy and use an lru
+	// cache because the amount of data we store is so small. Git hashes are 40
+	// chars and int64's are 8, so if we assume 100 commits to the repo per day,
+	// and the application ran without restart for three years straight then this
+	// data structure would grow to 48*100*365*3 bytes, which is ~5MB.
+	hashes map[int64]string
+}
+
+// New returns a newly populated *Cache with buildids for the last 2 weeks.
+//
+// The 'checkout' is only used during the construction of *Cache.
+func New(checkout *git.Checkout) (*Cache, error) {
+	// Runs
+	//
+	//   git log master --format=oneline --since="4 weeks ago"
+	//
+	// to prepopulate hashes.
+	log, err := checkout.Git("log", "master", "--format=oneline", "--since=\"2 weeks ago\"")
+	if err != nil {
+		return nil, fmt.Errorf("Failed to prime cache from checkout: %s", err)
+	}
+	c := &Cache{
+		hashes: map[int64]string{},
+	}
+	if err := c.parseLog(log); err != nil {
+		return nil, fmt.Errorf("Failed to parse git log from checkout: %s", err)
+	}
+	return c, nil
+}
+
+func (c *Cache) parseLog(log string) error {
+	// The oneline log format looks like
+	//
+	//   6dab50c23b3927daf7487b4a6f105fc74aff5fa7 https://android-ingest.skia.org/r/3553310
+	//   3133350e05eb07629d681c3bb61a91a51e2ff2ef https://android-ingest.skia.org/r/3553227
+	//
+	// if you include the commit message that poprepo adds.
+	lines := strings.Split(log, "\n")
+	for _, line := range lines {
+		if line == "" {
+			continue
+		}
+		// Split at the space between hash and url.
+		parts := strings.Split(line, " ")
+		if len(parts) != 2 {
+			return fmt.Errorf("Found invalid line: %q", line)
+		}
+		hash := parts[0]
+		url := parts[1]
+		// Split the URL on the slashes.
+		urlParts := strings.Split(url, "/")
+		if len(urlParts) != 5 {
+			return fmt.Errorf("Found invalid url: %q", url)
+		}
+		buildid, err := strconv.ParseInt(urlParts[4], 10, 64)
+		if err != nil {
+			return fmt.Errorf("Found invalid buildid: %q", urlParts[4])
+		}
+		c.hashes[buildid] = hash
+	}
+	glog.Infof("Prepopulated lookup.Cache with %d buildids.", len(c.hashes))
+	return nil
+}
+
+// Lookup returns the git hash for the given buildid.
+func (c *Cache) Lookup(buildid int64) (string, error) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	if hash, ok := c.hashes[buildid]; !ok {
+		return "", fmt.Errorf("BuildId not found in cache: %d", buildid)
+	} else {
+		return hash, nil
+	}
+}
+
+// Add a new buildid, githash to the cache.
+func (c *Cache) Add(buildid int64, hash string) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+
+	c.hashes[buildid] = hash
+}
--- android_ingest/go/poprepo/poprepo.go
+++ android_ingest/go/poprepo/poprepo.go
@@ -26,8 +26,8 @@ const (
 //
 // It supports adding and reading BuildIDs from a git repo.
 type PopRepoI interface {
-	// GetLast returns the last committed buildid and its timestamp.
-	GetLast() (int64, int64, error)
+	// GetLast returns the last committed buildid, its timestamp, and git hash.
+	GetLast() (int64, int64, string, error)
 
 	// Add a new buildid to the repo.
 	Add(buildid, ts int64) error
@@ -37,57 +37,52 @@ type PopRepoI interface {
 type PopRepo struct {
 	checkout *git.Checkout
 	workdir  string
-	repo     string
 	local    bool
 }
 
-// NewPopRepo returns a *PopRepo that writes and reads BuildIds
-// into the git repo from repoUrl. The workdir is where the repo
-// is to be checked out.
+// NewPopRepo returns a *PopRepo that writes and reads BuildIds into the
+// 'checkout'.
 //
 // If not 'local' then the HOME environment variable is set for running on the
 // server.
-func NewPopRepo(repoUrl, workdir string, local bool) (*PopRepo, error) {
-	checkout, err := git.NewCheckout(repoUrl, workdir)
-	if err != nil {
-		return nil, fmt.Errorf("Unable to create the checkout of %q at %q: %s", repoUrl, workdir, err)
-	}
-	if err := checkout.Update(); err != nil {
-		return nil, fmt.Errorf("Unable to update the checkout of %q at %q: %s", repoUrl, workdir, err)
-	}
+func NewPopRepo(checkout *git.Checkout, local bool) *PopRepo {
 	return &PopRepo{
 		checkout: checkout,
 		workdir:  checkout.Dir(),
-		repo:     repoUrl,
 		local:    local,
-	}, nil
+	}
 }
 
-// GetLast returns the last buildid and the timestamp of when that buildid
-// was added.
-func (p *PopRepo) GetLast() (int64, int64, error) {
-	fullpath := filepath.Join(p.workdir, BUILDID_FILENAME)
+// GetLast returns the last buildid, the timestamp of when that buildid was
+// added, and the git hash.
+func (p *PopRepo) GetLast() (int64, int64, string, error) {
+	fullpath := filepath.Join(p.checkout.Dir(), BUILDID_FILENAME)
 	b, err := ioutil.ReadFile(fullpath)
 	if err != nil {
-		return 0, 0, fmt.Errorf("Unable to read file %q: %s", fullpath, err)
+		return 0, 0, "", fmt.Errorf("Unable to read file %q: %s", fullpath, err)
 	}
 	s := strings.TrimSpace(string(b))
 	parts := strings.Split(s, " ")
 	if len(parts) != 2 {
-		return 0, 0, fmt.Errorf("Unable to find just buildid and timestamp in: %q", s)
+		return 0, 0, "", fmt.Errorf("Unable to find just buildid and timestamp in: %q", s)
 	}
 	ts, err := strconv.ParseInt(parts[1], 10, 64)
 	if err != nil {
-		return 0, 0, fmt.Errorf("Timestamp is invalid in %q: %s", s, err)
+		return 0, 0, "", fmt.Errorf("Timestamp is invalid in %q: %s", s, err)
 	}
 	buildid, err := strconv.ParseInt(parts[0], 10, 64)
 	if err != nil {
-		return 0, 0, fmt.Errorf("BuildID is invalid in %q: %s", s, err)
+		return 0, 0, "", fmt.Errorf("BuildID is invalid in %q: %s", s, err)
 	}
-	return buildid, ts, nil
+	hash, err := p.checkout.RevParse("HEAD")
+	if err != nil {
+		return 0, 0, "", fmt.Errorf("Unable to retrieve git hash: %s", err)
+	}
+	return buildid, ts, hash, nil
 }
 
 // Add a new buildid and its assocatied Unix timestamp to the repo.
+//
 func (p *PopRepo) Add(buildid int64, ts int64) error {
 	rollback := false
 	defer func() {
@@ -95,7 +90,7 @@ func (p *PopRepo) Add(buildid int64, ts int64) error {
 			return
 		}
 		if err := p.checkout.Update(); err != nil {
-			glog.Errorf("While rolling back failed Add(): Unable to update the checkout of %q at %q: %s", p.repo, p.workdir, err)
+			glog.Errorf("While rolling back failed Add(): Unable to update the checkout at %q: %s", p.checkout.Dir(), err)
 		}
 	}()
 
@@ -105,7 +100,7 @@ func (p *PopRepo) Add(buildid int64, ts int64) error {
 		Name:           "git",
 		Args:           []string{"commit", "-m", fmt.Sprintf("https://android-ingest.skia.org/r/%d", buildid), fmt.Sprintf("--date=%d", ts)},
 		Env:            []string{fmt.Sprintf("GIT_COMMITTER_DATE=%d", ts)},
-		Dir:            p.workdir,
+		Dir:            p.checkout.Dir(),
 		CombinedOutput: &output,
 	}
 	if !p.local {
@@ -113,14 +108,14 @@ func (p *PopRepo) Add(buildid int64, ts int64) error {
 	}
 
 	// Also needs to confirm that the buildids are ascending, which means they should be ints.
-	lastBuildID, _, err := p.GetLast()
+	lastBuildID, _, _, err := p.GetLast()
 	if err != nil {
 		return fmt.Errorf("Couldn't get last buildid: %s", err)
 	}
 	if buildid <= lastBuildID {
 		return fmt.Errorf("Error: buildid=%d <= lastBuildID=%d, buildid added in wrong order.", buildid, lastBuildID)
 	}
-	if err := ioutil.WriteFile(filepath.Join(p.workdir, BUILDID_FILENAME), []byte(fmt.Sprintf("%d %d", buildid, ts)), 0644); err != nil {
+	if err := ioutil.WriteFile(filepath.Join(p.checkout.Dir(), BUILDID_FILENAME), []byte(fmt.Sprintf("%d %d", buildid, ts)), 0644); err != nil {
 		rollback = true
 		return fmt.Errorf("Failed to write updated buildid: %s", err)
 	}
@@ -132,6 +127,7 @@ func (p *PopRepo) Add(buildid int64, ts int64) error {
 		rollback = true
 		return fmt.Errorf("Failed to commit updated file %q: %s", output.String(), err)
 	}
+	fmt.Printf("git commit: %q", output.String())
 	if msg, err := p.checkout.Git("push", "origin", "master"); err != nil {
 		rollback = true
 		return fmt.Errorf("Failed to push updated checkout %q: %s", msg, err)
