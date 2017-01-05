5145bca5ff974fc475c66b6c33728b1945de1681
Joe Gregorio <jcgregorio@google.com>
2017-01-05 15:17:57 -0500

    webhook email proxy wip
    BUG=skia:
    
    Change-Id: I69c45179a0bef3bd35183dd7082ac20c7cfc2a3d

---
+++ prometheus/go/alertmanager/alertmanager.go
@@ -0,0 +1,64 @@
+package alertmanager
+
+import (
+	"bytes"
+	"fmt"
+	"html/template"
+	"time"
+)
+
+const (
+	alert_email = `<b>Alert</b>: {{range .GroupLabels}}{{.}}{{end}} <br><br>
+
+<table border="0" cellspacing="5" cellpadding="5">
+	<tr>
+	  <th>Name</th>
+		<th>Severity</th>
+		<th>Status</th></th>
+		<th>Description</th>
+  </tr>
+	{{range .Alerts}}
+		<tr>
+		  <td>{{.Labels.alertname}}</td>
+			<td>{{.Labels.severity}}</td>
+			<td>{{.Status}}</td>
+			<td>{{.Annotations.description}}</td>
+		</tr>
+	{{end}}
+</table>
+`
+)
+
+var (
+	emailTemplate = template.Must(template.New("alert_email").Parse(alert_email))
+)
+
+type AlertManagerRequest struct {
+	Receiver string  `json:"receiver"`
+	Status   string  `json:"status"`
+	Alerts   []Alert `json:"alerts"`
+
+	GroupLabels       map[string]string `json:"groupLabels"`
+	CommonLabels      map[string]string `json:"commonLabels"`
+	CommonAnnotations map[string]string `json:"commonAnnotations"`
+
+	ExternalURL string `json:"externalURL"`
+}
+
+// Alert holds one alert for notification templates.
+type Alert struct {
+	Status       string            `json:"status"`
+	Labels       map[string]string `json:"labels"`
+	Annotations  map[string]string `json:"annotations"`
+	StartsAt     time.Time         `json:"startsAt"`
+	EndsAt       time.Time         `json:"endsAt"`
+	GeneratorURL string            `json:"generatorURL"`
+}
+
+func EmailBody(a AlertManagerRequest) (string, error) {
+	var b bytes.Buffer
+	if err := emailTemplate.Execute(&b, a); err != nil {
+		return "", fmt.Errorf("Failed to template alert: %s", err)
+	}
+	return b.String(), nil
+}
