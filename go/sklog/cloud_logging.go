bdb9cf82b18d04908ebaf3022ebbe083ae1a6d03
Ben Wagner <benjaminwagner@google.com>
2017-01-05 10:32:56 -0500

    Add context with timeout to cloud logging.
    BUG=skia:6093
    
    Change-Id: I1e5a4d8ddc8037e238934fb761aafa7862bce532

--- go/sklog/cloud_logging.go
+++ go/sklog/cloud_logging.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/davecgh/go-spew/spew"
 	"github.com/skia-dev/glog"
+	"golang.org/x/net/context"
 	logging "google.golang.org/api/logging/v2beta1"
 )
 
@@ -218,7 +219,9 @@ func (c *logsClient) BatchCloudLog(reportName string, payloads ...*LogPayload) {
 		if len(entries) > 1 {
 			glog.Infof("Sending log entry batch of %d", len(entries))
 		}
-		if resp, err := c.service.Entries.Write(&request).Do(); err != nil {
+		ctx, cancel := context.WithTimeout(context.Background(), 100*time.Millisecond)
+		defer cancel()
+		if resp, err := c.service.Entries.Write(&request).Context(ctx).Do(); err != nil {
 			// We can't use httputil.DumpResponse, because that doesn't accept *logging.WriteLogEntriesResponse
 			glog.Errorf("Problem writing logs \nLogPayloads:\n%v\nLogEntries:\n%v\nResponse:\n%v:\n%s", spew.Sdump(payloads), spew.Sdump(entries), spew.Sdump(resp), err)
 		} else if resp.HTTPStatusCode != http.StatusOK {
