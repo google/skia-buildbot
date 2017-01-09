27393253b223f552ffca12efcb7fb5ba014916ef
Joe Gregorio <jcgregorio@google.com>
2017-01-09 15:19:42 -0500

    login: Move cookie to .skia.org.
    BUG=skia:
    
    Change-Id: Iad9c5b63046ea3247bbbaea1dfaa084ea11bae83

--- go/login/login_test.go
+++ go/login/login_test.go
@@ -28,6 +28,7 @@ func TestLoginURL(t *testing.T) {
 	}
 	url := LoginURL(w, r)
 	assert.Contains(t, w.HeaderMap.Get("Set-Cookie"), SESSION_COOKIE_NAME, "Session cookie should be set.")
+	assert.Contains(t, url, "approval_prompt=auto", "Not forced into prompt.")
 	cookie := &http.Cookie{
 		Name:  SESSION_COOKIE_NAME,
 		Value: "some-random-state",
@@ -44,7 +45,7 @@ func TestLoggedInAs(t *testing.T) {
 	testutils.SmallTest(t)
 	once.Do(loginInit)
 
-	r, err := http.NewRequest("GET", "http://example.com/", nil)
+	r, err := http.NewRequest("GET", "http://www.skia.org/", nil)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -52,15 +53,35 @@ func TestLoggedInAs(t *testing.T) {
 	assert.Equal(t, LoggedInAs(r), "", "No skid cookie means not logged in.")
 
 	s := Session{
-		Email:     "fred@example.com",
+		Email:     "fred@chromium.org",
 		ID:        "12345",
 		AuthScope: DEFAULT_SCOPE[0],
 		Token:     nil,
 	}
-	cookie, err := CookieFor(&s)
-	if err != nil {
-		t.Fatal(err)
-	}
+	cookie, err := CookieFor(&s, r)
+	assert.NoError(t, err)
+	assert.Equal(t, "skia.org", cookie.Domain)
 	r.AddCookie(cookie)
-	assert.Equal(t, LoggedInAs(r), "fred@example.com", "Correctly get logged in email.")
+	assert.Equal(t, LoggedInAs(r), "fred@chromium.org", "Correctly get logged in email.")
+	w := httptest.NewRecorder()
+	url := LoginURL(w, r)
+	assert.Contains(t, url, "approval_prompt=auto", "Not forced into prompt.")
+
+	delete(activeUserDomainWhiteList, "chromium.org")
+	assert.Equal(t, LoggedInAs(r), "", "Not in the domain whitelist.")
+	url = LoginURL(w, r)
+	assert.Contains(t, url, "approval_prompt=force", "Force into prompt.")
+
+	activeUserEmailWhiteList["fred@chromium.org"] = true
+	assert.Equal(t, LoggedInAs(r), "fred@chromium.org", "Found in the email whitelist.")
+}
+
+func TestDomainFromHost(t *testing.T) {
+	testutils.SmallTest(t)
+	assert.Equal(t, "localhost", domainFromHost("localhost:10110"))
+	assert.Equal(t, "localhost", domainFromHost("localhost"))
+	assert.Equal(t, "skia.org", domainFromHost("skia.org"))
+	assert.Equal(t, "skia.org", domainFromHost("perf.skia.org"))
+	assert.Equal(t, "skia.org", domainFromHost("perf.skia.org:443"))
+	assert.Equal(t, "skia.org", domainFromHost("example.com:443"))
 }
