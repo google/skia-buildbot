fc0614ba9740b46bdd16708fe27fea2d07b177d9
Ben Wagner <benjaminwagner@google.com>
2017-01-16 21:24:54 -0500

    [task scheduler] Add fields Fake and Properties to Task.
    BUG=skia:
    
    Change-Id: Ib40c38d4f05e8131f19e3eeecac561e097445aee

--- task_scheduler/go/db/task.go
+++ task_scheduler/go/db/task.go
@@ -118,6 +118,10 @@ type Task struct {
 	// of the associated Swarming task.
 	DbModified time.Time
 
+	// Fake indicates this task represents a roll or other process that occurs
+	// per-revision, not a Swarming task.
+	Fake bool
+
 	// Finished is the time the task stopped running or expired from the queue, or
 	//  zero if the task is pending or running.
 	Finished time.Time
@@ -135,6 +139,11 @@ type Task struct {
 
 	// ParentTaskIds are IDs of tasks which satisfied this task's dependencies.
 	ParentTaskIds []string
+
+	// Properties contains key-value pairs from external sources. Key can be any
+	// string, but preferably a JavaScript identifier. Value can be any UTF-8
+	// string; please use base64 encoding for binary data.
+	Properties map[string]string
 
 	// RetryOf is the ID of the task which this task is a retry of, if any.
 	RetryOf string
@@ -360,10 +369,12 @@ func (t *Task) Copy() *Task {
 		Commits:        commits,
 		Created:        t.Created,
 		DbModified:     t.DbModified,
+		Fake:           t.Fake,
 		Finished:       t.Finished,
 		Id:             t.Id,
 		IsolatedOutput: t.IsolatedOutput,
 		ParentTaskIds:  parentTaskIds,
+		Properties:     util.CopyStringMap(t.Properties),
 		RetryOf:        t.RetryOf,
 		Started:        t.Started,
 		Status:         t.Status,
