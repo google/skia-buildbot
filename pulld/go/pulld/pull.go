b0e15d2a54aa5ee868261adad1de2746ca9f6a62
Joe Gregorio <jcgregorio@google.com>
2017-01-20 11:15:58 -0500

    Change the mechanism that pulld and push use to announce changes.
    The mechanism used to be a request to /pullpullpull on the pulld server, but since we are
    locking down external IP address this won't work anymore when using 'pushcli'. Switch to
    using a change in instance level metadata to trigger pulld to go look for new packages to
    install.
    
    See:
      https://cloud.google.com/compute/docs/storing-retrieving-metadata#waitforchange
    
    BUG=skia:
    
    Change-Id: I62f4fa6e4ce926a2e2a5ccab8332d10264bdc862

--- pulld/go/pulld/pull.go
+++ pulld/go/pulld/pull.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"os"
 	"strings"
@@ -17,7 +16,7 @@ import (
 )
 
 var (
-	httpTriggerCh = make(chan bool, 1)
+	metadataTriggerCh = make(chan bool, 1)
 
 	store *storage.Service
 )
@@ -98,11 +97,26 @@ func containsPulld(packages []string) bool {
 	return false
 }
 
-// pullHandler triggers a pull when /pullpullpull is requested.
-func pullHandler(w http.ResponseWriter, r *http.Request) {
-	httpTriggerCh <- true
-	w.Header().Set("Content-Type", "text/plain")
-	fmt.Fprintf(w, "Pull triggered.")
+// metadataWait waits for the instance level metadata 'pushrev' to change, at
+// which point the server should check for new versions of apps to install.
+func metadataWait() {
+	for {
+		req, err := http.NewRequest("GET", "http://metadata.google.internal/computeMetadata/v1/instance/attributes/pushrev?wait_for_change=true", nil)
+		if err != nil {
+			sklog.Errorf("Failed to create request: %s", err)
+			continue
+		}
+		req.Header.Set("Metadata-Flavor", "Google")
+		// We use the default client which should never timeout.
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil || resp.StatusCode != 200 {
+			sklog.Errorf("wait_for_change failed: %s %v", err, *resp)
+			time.Sleep(time.Minute)
+			continue
+		}
+		metadataTriggerCh <- true
+		sklog.Infof("Pull triggered via metadata.")
+	}
 }
 
 func pullInit(serviceAccountPath string) {
@@ -123,13 +137,15 @@ func pullInit(serviceAccountPath string) {
 		sklog.Fatalf("Failed to create storage service client: %s", err)
 	}
 
+	go metadataWait()
+
 	step(client, store, hostname)
-	timeCh := time.Tick(time.Second * 60)
+	timeCh := time.Tick(5 * time.Minute)
 	go func() {
 		for {
 			select {
 			case <-timeCh:
-			case <-httpTriggerCh:
+			case <-metadataTriggerCh:
 			}
 			step(client, store, hostname)
 		}
