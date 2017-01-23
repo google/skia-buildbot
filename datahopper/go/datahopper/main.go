282367a3d1a90719a6649fcdb5796af20de0991e
Joe Gregorio <jcgregorio@google.com>
2017-01-23 15:02:34 -0500

    Datahopper: Stop reporting metrics for individual tasks.
    BUG=skia:
    
    Change-Id: I553b6d16d7ab9d0366f62fd660568ab9214923da

--- datahopper/go/datahopper/main.go
+++ datahopper/go/datahopper/main.go
@@ -6,7 +6,6 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"os"
 	"path"
 	"path/filepath"
@@ -29,27 +28,25 @@ import (
 )
 
 const (
-	MEASUREMENT_SWARM_BOTS_LAST_SEEN          = "swarming.bots.last-seen"
-	MEASUREMENT_SWARM_BOTS_QUARANTINED        = "swarming.bots.quarantined"
-	MEASUREMENT_SWARM_TASKS_DURATION          = "swarming.tasks.duration"
-	MEASUREMENT_SWARM_TASKS_OVERHEAD_BOT      = "swarming.tasks.overhead.bot"
-	MEASUREMENT_SWARM_TASKS_OVERHEAD_DOWNLOAD = "swarming.tasks.overhead.download"
-	MEASUREMENT_SWARM_TASKS_OVERHEAD_UPLOAD   = "swarming.tasks.overhead.upload"
-	MEASUREMENT_SWARM_TASKS_PENDING_TIME      = "swarming.tasks.pending-time"
+	MEASUREMENT_SWARM_BOTS_LAST_SEEN   = "swarming.bots.last-seen"
+	MEASUREMENT_SWARM_BOTS_QUARANTINED = "swarming.bots.quarantined"
+	MEASUREMENT_SWARM_TASKS_RUNNING    = "swarming.tasks.running"
 )
 
 // flags
 var (
-	workdir  = flag.String("workdir", ".", "Working directory used by data processors.")
-	local    = flag.Bool("local", false, "Running locally if true. As opposed to in production.")
-	grpcPort = flag.String("grpc_port", ":8000", "Port on which to run the buildbot data gRPC server.")
-	httpPort = flag.String("http_port", ":8001", "Port on which to run the HTTP server.")
-
-	influxHost     = flag.String("influxdb_host", influxdb.DEFAULT_HOST, "The InfluxDB hostname.")
-	influxUser     = flag.String("influxdb_name", influxdb.DEFAULT_USER, "The InfluxDB username.")
-	influxPassword = flag.String("influxdb_password", influxdb.DEFAULT_PASSWORD, "The InfluxDB password.")
+	grpcPort       = flag.String("grpc_port", ":8000", "Port on which to run the buildbot data gRPC server.")
+	httpPort       = flag.String("http_port", ":8001", "Port on which to run the HTTP server.")
 	influxDatabase = flag.String("influxdb_database", influxdb.DEFAULT_DATABASE, "The InfluxDB database.")
+	influxHost     = flag.String("influxdb_host", influxdb.DEFAULT_HOST, "The InfluxDB hostname.")
+	influxPassword = flag.String("influxdb_password", influxdb.DEFAULT_PASSWORD, "The InfluxDB password.")
+	influxUser     = flag.String("influxdb_name", influxdb.DEFAULT_USER, "The InfluxDB username.")
+	local          = flag.Bool("local", false, "Running locally if true. As opposed to in production.")
+	promPort       = flag.String("prom_port", ":20000", "Metrics service address (e.g., ':10110')")
+	workdir        = flag.String("workdir", ".", "Working directory used by data processors.")
+)
 
+var (
 	// Regexp matching non-alphanumeric characters.
 	re = regexp.MustCompile("[^A-Za-z0-9]+")
 
@@ -68,9 +65,12 @@ func fixName(s string) string {
 
 func main() {
 	defer common.LogPanic()
-
-	// Global init to initialize glog and parse arguments.
-	common.InitWithMetrics2("datahopper", influxHost, influxUser, influxPassword, influxDatabase, local)
+	common.InitWithMust(
+		"datahopper",
+		common.InfluxOpt(influxHost, influxUser, influxPassword, influxDatabase, local),
+		common.PrometheusOpt(promPort),
+		common.CloudLoggingOpt(),
+	)
 
 	// Absolutify the workdir.
 	w, err := filepath.Abs(*workdir)
@@ -155,10 +155,13 @@ func main() {
 				}
 
 				tags := map[string]string{
-					"bot": bot.BotId,
+					"bot":  bot.BotId,
+					"pool": "",
 				}
 				for _, d := range bot.Dimensions {
-					tags[fmt.Sprintf("dimension-%s", d.Key)] = strings.Join(d.Value, ",")
+					if d.Key == "pool" {
+						tags["pool"] = strings.Join(d.Value, ",")
+					}
 				}
 
 				// Bot last seen <duration> ago.
@@ -182,9 +185,6 @@ func main() {
 	go func() {
 		// Initial query: load data from the past 2 hours.
 		lastLoad := time.Now().Add(-2 * time.Hour)
-
-		revisitTasks := map[string]bool{}
-
 		for _ = range time.Tick(2 * time.Minute) {
 			now := time.Now()
 			tasks, err := swarm.ListSkiaTasks(lastLoad, now)
@@ -192,114 +192,18 @@ func main() {
 				sklog.Error(err)
 				continue
 			}
-			for id, _ := range revisitTasks {
-				task, err := swarm.GetTaskMetadata(id)
-				if err != nil {
-					sklog.Error(err)
-					continue
-				}
-				tasks = append(tasks, task)
-			}
-			revisitTasks = map[string]bool{}
 			lastLoad = now
-			for _, task := range tasks {
-				if task.TaskResult.State == "COMPLETED" {
-					if task.TaskResult.DedupedFrom != "" {
-						continue
-					}
 
-					// Get the created time for the task. We'll use that as the
-					// timestamp for all data points related to it.
-					createdTime, err := swarming.Created(task)
-					if err != nil {
-						sklog.Errorf("Failed to parse Swarming task created timestamp: %s", err)
-						continue
-					}
+			// Count the number of tasks in each state.
+			counts := map[string]int64{}
+			for _, t := range tasks {
+				counts[t.TaskResult.State] += 1
+			}
 
-					// Find the tags for the task, including ID, name, dimensions,
-					// and components of the builder name.
-					var builderName string
-					var builderTags map[string]string
-					var name string
-					user, err := swarming.GetTagValue(task.TaskResult, "user")
-					if err != nil || user == "" {
-						// This is an old-style task.
-						name, err = swarming.GetTagValue(task.TaskResult, "name")
-						if err != nil || name == "" {
-							sklog.Errorf("Failed to find name for Swarming task: %v", task)
-							continue
-						}
-						builderName, err = swarming.GetTagValue(task.TaskResult, "buildername")
-						if err != nil || builderName == "" {
-							sklog.Errorf("Failed to find buildername for Swarming task: %v", task)
-							continue
-						}
-						builderTags, err = buildbot.ParseBuilderName(builderName)
-						if err != nil {
-							sklog.Errorf("Failed to parse builder name for Swarming task: %s", err)
-							continue
-						}
-					} else if user == "skia-task-scheduler" {
-						// This is a new-style task.
-						builderName, err = swarming.GetTagValue(task.TaskResult, "sk_name")
-						if err != nil || builderName == "" {
-							sklog.Errorf("Failed to find sk_name for Swarming task: %v", task)
-							continue
-						}
-						name = builderName
-						if strings.HasPrefix(name, "Upload") {
-							// These bots are "special".
-							builderTags = map[string]string{}
-						} else {
-							builderTags, err = buildbot.ParseBuilderName(builderName)
-							if err != nil {
-								sklog.Errorf("Failed to parse builder name for Swarming task: %s", err)
-								continue
-							}
-						}
-					}
-
-					tags := map[string]string{
-						"bot-id":    task.TaskResult.BotId,
-						"task-id":   task.TaskId,
-						"task-name": name,
-					}
-					for _, d := range task.Request.Properties.Dimensions {
-						tags[fmt.Sprintf("dimension-%s", d.Key)] = d.Value
-					}
-					for k, v := range builderTags {
-						tags[k] = v
-					}
-
-					// Task duration in milliseconds.
-					metrics2.RawAddInt64PointAtTime(MEASUREMENT_SWARM_TASKS_DURATION, tags, int64(task.TaskResult.Duration*float64(1000.0)), createdTime)
-
-					if task.TaskResult.PerformanceStats != nil {
-						// Overhead stats, in milliseconds.
-						metrics2.RawAddInt64PointAtTime(MEASUREMENT_SWARM_TASKS_OVERHEAD_BOT, tags, int64(task.TaskResult.PerformanceStats.BotOverhead*float64(1000.0)), createdTime)
-						if task.TaskResult.PerformanceStats.IsolatedDownload != nil {
-							metrics2.RawAddInt64PointAtTime(MEASUREMENT_SWARM_TASKS_OVERHEAD_DOWNLOAD, tags, int64(task.TaskResult.PerformanceStats.IsolatedDownload.Duration*float64(1000.0)), createdTime)
-						} else {
-							sklog.Errorf("Swarming task is missing its IsolatedDownload section: %v", task.TaskResult)
-						}
-						if task.TaskResult.PerformanceStats.IsolatedUpload != nil {
-							metrics2.RawAddInt64PointAtTime(MEASUREMENT_SWARM_TASKS_OVERHEAD_UPLOAD, tags, int64(task.TaskResult.PerformanceStats.IsolatedUpload.Duration*float64(1000.0)), createdTime)
-						} else {
-							sklog.Errorf("Swarming task is missing its IsolatedUpload section: %v", task.TaskResult)
-						}
-					}
-
-					// Pending time in milliseconds.
-					startTime, err := swarming.Started(task)
-					if err != nil {
-						sklog.Errorf("Failed to parse Swarming task started timestamp: %s", err)
-						continue
-					}
-					pendingMs := int64(startTime.Sub(createdTime).Seconds() * float64(1000.0))
-					metrics2.RawAddInt64PointAtTime(MEASUREMENT_SWARM_TASKS_PENDING_TIME, tags, pendingMs, createdTime)
-				} else {
-					revisitTasks[task.TaskId] = true
-				}
+			// Report the number of running tasks as a metric.
+			for state, count := range counts {
+				skiaGauge := metrics2.GetInt64Metric(MEASUREMENT_SWARM_TASKS_RUNNING, map[string]string{"pool": "skia", "state": state})
+				skiaGauge.Update(count)
 			}
 		}
 	}()
