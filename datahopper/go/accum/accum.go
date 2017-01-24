d9ec6194497ebd0a01f8108e74e4da2bf8152c6f
Joe Gregorio <jcgregorio@google.com>
2017-01-24 15:00:45 -0500

    works
    BUG=skia:
    
    Change-Id: Ia8d95ec95140373a65cfe660b01567d6fe3b6313

---
+++ datahopper/go/accum/accum.go
@@ -0,0 +1,87 @@
+package accum
+
+import (
+	"sort"
+	"strings"
+
+	"go.skia.org/infra/go/metrics2"
+	"go.skia.org/infra/go/sklog"
+)
+
+type Reporter func(metric string, tags map[string]string, value int64)
+
+func DefaultReporter(metric string, tags map[string]string, value int64) {
+	metrics2.GetInt64Metric(metric, tags).Update(value)
+}
+
+type metricAccum struct {
+	duration    int64
+	numDuration int64
+	tags        map[string]string
+}
+
+// Accum accumulates measurements for the given set of tags
+// and measurement name.
+//
+type Accum struct {
+	//       map[<meas>]map[<keys>]
+	values   map[string]map[string]*metricAccum
+	reporter Reporter
+}
+
+func New(reporter Reporter) *Accum {
+	return &Accum{
+		values:   map[string]map[string]*metricAccum{},
+		reporter: reporter,
+	}
+}
+
+func keyFromTags(tags map[string]string) string {
+	keys := make([]string, 0, len(tags))
+	for key, _ := range tags {
+		keys = append(keys, key)
+	}
+	sort.Strings(keys)
+
+	ret := []string{}
+	for _, key := range keys {
+		ret = append(ret, key, tags[key])
+	}
+	return strings.Join(ret, " ")
+}
+
+func (a *Accum) Add(metric string, tags map[string]string, duration int64) {
+	// Remove 'task-id' which means each swarming task has it's own trace.
+	if _, ok := tags["task-id"]; ok {
+		// Report each value to logging to aid in debugging bad bots.
+		sklog.Infof("Task: %s %s Value: %d", metric, keyFromTags(tags), duration)
+		delete(tags, "task-id")
+	}
+	byMetric, ok := a.values[metric]
+	if !ok {
+		a.values[metric] = map[string]*metricAccum{}
+		byMetric = a.values[metric]
+	}
+	key := keyFromTags(tags)
+	byTags, ok := byMetric[key]
+	if !ok {
+		byMetric[key] = &metricAccum{
+			tags: tags,
+		}
+		byTags = byMetric[key]
+	}
+	byTags.duration += duration
+	byTags.numDuration += 1
+}
+
+func (a *Accum) Report() {
+	for metric, byTags := range a.values {
+		for _, accum := range byTags {
+			accum.tags["type"] = "duration"
+			a.reporter(metric, accum.tags, accum.duration)
+			accum.tags["type"] = "num"
+			a.reporter(metric, accum.tags, accum.numDuration)
+		}
+	}
+	a.values = map[string]map[string]*metricAccum{}
+}
