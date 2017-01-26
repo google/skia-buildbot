1ccbbf152cad5d873aee01fd0a3199ea4d3e0a34
Ravi Mistry <rmistry@google.com>
2017-01-26 08:29:52 -0500

    Fixing ct-master creation and deployment scripts
    BUG=skia:
    
    Change-Id: Iec169f07af658eacd831ec1ab3e31bde11fcff2d

--- compute_engine_scripts/ct_master/install.sh
+++ compute_engine_scripts/ct_master/install.sh
@@ -14,16 +14,10 @@ source ~/.bashrc
 # Install necessary packages.
 sudo apt-get update
 sudo apt-get --assume-yes upgrade
-sudo apt-get --assume-yes install golang-go
+sudo apt-get --assume-yes install golang-go python-django python-setuptools lua5.2
+sudo easy_install -U pip
+sudo pip install -U crcmod
 
 # Checkout the Skia infra repo.
 cd /b/skia-repo
-go get -u -t go.skia.org/infra/...
-
-# Start the CT poller.
-cd /b/skia-repo/go/src/go.skia.org/infra/ct/
-make all
-nohup poller --log_dir=/b/storage/glog \
-  --influxdb_host=skia-monitoring:10117 \
-  --influxdb_name=root \
-  --influxdb_database=skmetrics &
+GOPATH=/b/skia-repo/go/ go get -u -t go.skia.org/infra/...
