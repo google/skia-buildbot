33c8ff50e3d41be057849c48c55621c259d0697c
Ben Wagner <benjaminwagner@google.com>
2017-01-26 21:33:46 -0500

    [status] Fix links for fake tasks.
    BUG=skia:6167
    
    Change-Id: Ie5d72388755acd87585a241599b3848a359437a4
    Reviewed-on: https://skia-review.googlesource.com/7637
    Reviewed-by: Eric Boren <borenet@google.com>
    Commit-Queue: Ben Wagner <benjaminwagner@google.com>

--- status/go/franken/cache.go
+++ status/go/franken/cache.go
@@ -309,14 +309,24 @@ func (c *BTCache) taskToBuild(task *db.Task, loggedIn bool) *buildbot.Build {
 
 	buildSlave := DEFAULT_BUILD_SLAVE
 
-	taskUrlFmt := NOAUTH_TASK_URL_FMT
+	taskURL := fmt.Sprintf(NOAUTH_TASK_URL_FMT, task.SwarmingTaskId)
 	if loggedIn {
-		taskUrlFmt = TASK_URL_FMT
+		taskURL = fmt.Sprintf(TASK_URL_FMT, task.SwarmingTaskId)
+	}
+	if url, ok := task.Properties["url"]; ok {
+		taskURL = url
+	}
+	tasklistURL := fmt.Sprintf(TASKLIST_URL_FMT, db.SWARMING_TAG_NAME, task.Name)
+	if task.Fake() {
+		// TODO(benjaminwagner): How to generalize?
+		tasklistURL = fmt.Sprintf("https://internal.skia.org/builders/%s", task.Name)
 	}
 	properties := [][]interface{}{
-		{"taskURL", fmt.Sprintf(taskUrlFmt, task.SwarmingTaskId), PROPERTY_SOURCE},
-		{"taskRetryURL", fmt.Sprintf(TASK_TRIGGER_URL_FMT, task.Name, task.Revision), PROPERTY_SOURCE},
-		{"taskSpecTasklistURL", fmt.Sprintf(TASKLIST_URL_FMT, db.SWARMING_TAG_NAME, task.Name), PROPERTY_SOURCE},
+		{"taskURL", taskURL, PROPERTY_SOURCE},
+		{"taskSpecTasklistURL", tasklistURL, PROPERTY_SOURCE},
+	}
+	if !task.Fake() {
+		properties = append(properties, []interface{}{"taskRetryURL", fmt.Sprintf(TASK_TRIGGER_URL_FMT, task.Name, task.Revision), PROPERTY_SOURCE})
 	}
 	if task.SwarmingBotId != "" {
 		buildSlave = task.SwarmingBotId
