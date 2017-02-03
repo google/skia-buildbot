7a25b11e7034a768612930b7e3b3881563a9f204
Stephan Altmueller <stephana@google.com>
2017-02-03 10:53:36 -0500

    Add basic validation whenever a digest is given
    This adds basic validation for any request that sends 
    digests. It checks whether the given strings match 
    the digest format on a string level.
    This will greatly reduce the number of false entries in
    the failure log. 
    
    BUG=skia:4961
    
    Change-Id: I10bc9be5722c1b6e47de1a22d56476eb26b13c40

---
+++ golden/go/validation/validation_test.go
@@ -0,0 +1,20 @@
+package validation
+
+import (
+	"testing"
+
+	"go.skia.org/infra/go/testutils"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIsValidDigest(t *testing.T) {
+	testutils.SmallTest(t)
+
+	assert.False(t, IsValidDigest(""))
+	assert.True(t, IsValidDigest("766923700b970e4e7ecf9508b8455e0d"))
+	assert.True(t, IsValidDigest("766923700b970e4e7ecf9508b8455e0d"))
+	assert.False(t, IsValidDigest("766923700b970e4e7ecf9508b8455e0x"))
+	assert.False(t, IsValidDigest("766923700b970e4e7ECf9508b8455e0x"))
+	assert.False(t, IsValidDigest("766923700b970e4e7ecf08b8455e0f"))
+}
