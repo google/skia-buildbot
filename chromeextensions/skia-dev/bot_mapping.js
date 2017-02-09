80db83503ad8185277fb8c444c130b296a6b9110
Kevin Lubick <kjlubick@google.com>
2017-02-09 11:24:47 -0500

    Merge skia-swarming extension into skia-dev
    BUG=skia:
    
    Change-Id: Ie4869a842ea9d6fd4f0a43ed58fe960ace156ee2
    Reviewed-on: https://skia-review.googlesource.com/8253
    Reviewed-by: Ravi Mistry <rmistry@google.com>
    Commit-Queue: Kevin Lubick <kjlubick@google.com>

--- chromeextensions/skia-dev/bot_mapping.js
+++ chromeextensions/skia-dev/bot_mapping.js
@@ -7,26 +7,12 @@ var botMapping = {
     "skia-rpi-0(49|5[0-9]|6[0-4])": "Skolo Shelf 1a",
     "skia-rpi-0(6[5-9]|7[0-6]|80)": "Skolo Shelf 1b",
 
-    "skiabot-win-001": "Skolo Office A",
-    "win8-hd7770-001": "Skolo Office B",
-    "win8-hd7770-000": "Skolo Office C",
-    "skiabot-shuttle-ubuntu12-gtx660-001": "Skolo Office D",
-    "skiabot-shuttle-ubuntu12-gtx550ti-001": "Skolo Office E",
-    "skiabot-shuttle-ubuntu12-gtx660-002": "Skolo Office F",
+    "skia-e-linux-00[1-4]": "Skolo Door Bottom Shelf",
+    "skia-e-linux-020": "Skolo Door C",
+    "skia-e-linux-021": "Skolo Door E",
+    "skia-e-linux-022": "Skolo Door G",
 
-    "skiabot-mac-10_10-ios": "Skolo Office 2",
-    "win-i75557u-000": "Skolo Office 5",
-    "skiabot-macmini-10_8-002": "Skolo Office 6",
-    "win-i75557u-001": "Skolo Office 7",
-    "skiabot-macmini-10_8-001": "Skolo Office 8",
-
-    "win8-4790k-001": "Skolo Door 1",
-    "win8-gtx960-002": "Skolo Door 2",
-    "win8-4790k-002": "Skolo Door 3",
-    "win-ihd530-000": "Skolo Door 4",
-    "win8-4790k-003": "Skolo Door 7",
-    "win-gtx960-003": "Skolo Door A",
-    "win-gtx960-004": "Skolo Door B",
+    "skia-e-win.+": "Skia Office Shelf",
 
     "skia-(vm|ct-vm).*": "GCE",
     "ct-vm.*": "GCE",
@@ -40,13 +26,13 @@ var botMapping = {
 
     "skia.*": "<a href='https://goto.google.com/skolo-maintenance'>go/skolo-maintenance</a>",
 
-    "win.+": "<a href='https://goto.google.com/skolo-maintenance'>go/skolo-maintenance</a>",
+    "skia-e-win.+": "<a href='https://goto.google.com/skolo-maintenance'>go/skolo-maintenance</a>",
 
     ".+(m3|m5|a3)": "<a href='https://bugs.chromium.org/p/chromium/issues/entry?summary=[Device%20Restart]%20for%20_id_&description=Please%20Reboot%20_id_&cc=rmistry@google.com&components=Infra%3ELabs&labels=Pri-2,Infra-Troopers,Restrict-View-Google'> File a bug</a>",
   },
   useJumphost: {
     "skia-rpi-.+": true,
-    "skiabot-.+ubuntu.+": true,
+    "skia-e-linux.+": true,
   },
   golo: {
     ".+(m3|m5)": true,
