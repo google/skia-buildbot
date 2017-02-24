85a486ccd79fb7e2d6d75a4eaf9f4a96e49adab4
Stephan Altmueller <stephana@google.com>
2017-02-24 14:45:58 -0500

    Remove influx from gold, ingestion and tracedb
    BUG=skia:
    
    Change-Id: I7e779c8a3a51e96858a5d1c2f8cc3d3e30bced13

--- tracedb/go/traceserver/main.go
+++ tracedb/go/traceserver/main.go
@@ -12,7 +12,6 @@ import (
 	"syscall"
 
 	"go.skia.org/infra/go/common"
-	"go.skia.org/infra/go/influxdb"
 	"go.skia.org/infra/go/sharedb"
 	"go.skia.org/infra/go/sklog"
 	"go.skia.org/infra/go/trace/service"
@@ -21,21 +20,16 @@ import (
 
 // flags
 var (
-	cpuprofile     = flag.String("cpuprofile", "", "Write cpu profile to file.")
-	db_file        = flag.String("db_file", "", "The name of the BoltDB file that will store the traces.")
-	influxDatabase = flag.String("influxdb_database", influxdb.DEFAULT_DATABASE, "The InfluxDB database.")
-	influxHost     = flag.String("influxdb_host", influxdb.DEFAULT_HOST, "The InfluxDB hostname.")
-	influxPassword = flag.String("influxdb_password", influxdb.DEFAULT_PASSWORD, "The InfluxDB password.")
-	influxUser     = flag.String("influxdb_name", influxdb.DEFAULT_USER, "The InfluxDB username.")
-	local          = flag.Bool("local", false, "Running locally if true. As opposed to in production.")
-	port           = flag.String("port", ":9090", "The port to serve the gRPC endpoint on.")
-	promPort       = flag.String("prom_port", ":20000", "Metrics service address (e.g., ':10110')")
-	sharedbDir     = flag.String("sharedb_dir", "", "Directory used by shareDB. If empty shareDB service will not enabled.")
+	cpuprofile = flag.String("cpuprofile", "", "Write cpu profile to file.")
+	db_file    = flag.String("db_file", "", "The name of the BoltDB file that will store the traces.")
+	local      = flag.Bool("local", false, "Running locally if true. As opposed to in production.")
+	port       = flag.String("port", ":9090", "The port to serve the gRPC endpoint on.")
+	promPort   = flag.String("prom_port", ":20000", "Metrics service address (e.g., ':10110')")
+	sharedbDir = flag.String("sharedb_dir", "", "Directory used by shareDB. If empty shareDB service will not enabled.")
 )
 
 func main() {
 	common.InitWithMust(filepath.Base(os.Args[0]),
-		common.InfluxOpt(influxHost, influxUser, influxPassword, influxDatabase, local),
 		common.PrometheusOpt(promPort),
 		common.CloudLoggingOpt(),
 	)
