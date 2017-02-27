21d3c901ae753044beb5a57f1341d79d36365497
Eric Boren <borenet@google.com>
2017-02-27 10:59:34 -0500

    Test fix karma
    BUG=skia:
    
    Change-Id: Ie6e5b8bf62d404f68c35e4f7b1abbebd5f5f97e3

--- infra/bots/recipes/swarm_infra.py
+++ infra/bots/recipes/swarm_infra.py
@@ -176,8 +176,11 @@ def RunSteps(api):
     cmd.append('--medium')
   else:
     cmd.append('--small')
-  with api.step.context({'cwd': infra_dir}):
-    api.step('run_unittests', cmd, env=env)
+  with api.step.context({'cwd': infra_dir.join('res', 'js')}):
+    api.step('npm', ['npm', 'install'], env=env)
+    api.step('test', ['xvfb-run', '--auto-servernum', '--server-args', '"-screen 0 1280x1024x24"',
+                      'node_modules/karma/bin/karma', '--no-single-run', 'start', 'karma.conf.js'], env=env)
+    #api.step('run_unittests', cmd, env=env)
 
 
 def GenTests(api):
