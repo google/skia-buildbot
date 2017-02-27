fb263493b0c5650ef33df6deca29cf8cf16dc818
Kevin Lubick <kjlubick@google.com>
2017-02-27 16:13:48 -0500

    Add 3rd fuzzer-be for (slow) canvas fuzzers
    Also update clang to 3.8 and add the aforementioned fuzzers.
    
    Requires https://skia-review.googlesource.com/c/8418/ to have landed.
    
    BUG=skia:
    
    Change-Id: Iec11d6648b6434daefb62caef9c00dc4ed026cce

--- compute_engine_scripts/fuzzer/install.sh
+++ compute_engine_scripts/fuzzer/install.sh
@@ -2,13 +2,13 @@
 # Install clang/llvm
 wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key|sudo apt-key add -
 sudo apt-get update
-sudo apt-get install clang-3.6 lldb-3.6 make build-essential libfontconfig1-dev libfreetype6-dev libgif-dev libpng12-dev libqt4-dev ninja-build python-dev python-imaging libosmesa-dev -y
-# Afl-fuzz can't find the clang-3.6 aliases, so make the standard /usr/bin/clang
-sudo ln /usr/bin/clang-3.6 /usr/bin/clang
-sudo ln /usr/bin/clang++-3.6 /usr/bin/clang++
-sudo ln /usr/bin/llvm-config-3.6 /usr/bin/llvm-config
+sudo apt-get install clang-3.8 lldb-3.8 make build-essential libfontconfig1-dev libfreetype6-dev libgif-dev libpng12-dev libqt4-dev ninja-build python-dev python-imaging libosmesa-dev -y
+# Afl-fuzz can't find the clang-3.8 aliases, so make the standard /usr/bin/clang
+sudo ln /usr/bin/clang-3.8 /usr/bin/clang
+sudo ln /usr/bin/clang++-3.8 /usr/bin/clang++
+sudo ln /usr/bin/llvm-config-3.8 /usr/bin/llvm-config
 # Make symbolizer easier to find
-sudo ln /usr/bin/llvm-symbolizer-3.6 /usr/bin/llvm-symbolizer
+sudo ln /usr/bin/llvm-symbolizer-3.8 /usr/bin/llvm-symbolizer
 
 # Mount data disk
 sudo mkdir -p /mnt/ssd0
