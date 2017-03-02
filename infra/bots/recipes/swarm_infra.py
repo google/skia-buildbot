690fe1a7e4ecbffe5c67088ace5a7ba42e3203bc
Eric Boren <borenet@google.com>
2017-03-02 15:39:42 -0500

    Test fix karma
    BUG=skia:
    
    Change-Id: Ie6e5b8bf62d404f68c35e4f7b1abbebd5f5f97e3

--- infra/bots/recipes/swarm_infra.py
+++ infra/bots/recipes/swarm_infra.py
@@ -166,8 +166,10 @@ def RunSteps(api):
   karma_port = '9876'
   if m and len(m.groups()) > 0:
     karma_port = '15%s' % m.groups()[0]
-  env['KARMA_PORT'] = karma_port
-  env['DEPOT_TOOLS'] = api.path['depot_tools']
+  env['SUDO_COMMAND'] = None
+  env['SUDO_GID'] = None
+  env['SUDO_UID'] = None
+  env['SUDO_USER'] = None
 
   cmd = ['go', 'run', './run_unittests.go', '--alsologtostderr']
   if 'Large' in api.properties['buildername']:
@@ -176,7 +178,7 @@ def RunSteps(api):
     cmd.append('--medium')
   else:
     cmd.append('--small')
-  with api.step.context({'cwd': infra_dir}):
+  with api.step.context({'cwd': infra_dir.join('res', 'js')}):
     api.step('run_unittests', cmd, env=env)
 
 
