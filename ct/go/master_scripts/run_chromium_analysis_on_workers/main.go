da68eb61de2c5dc68b7d2676fd3a4105fbcde99a
Ravi Mistry <rmistry@google.com>
2017-03-03 15:05:57 -0500

    [CT] Use --pageset-repeats while calculating num of total required swarming tasks
    BUG=skia:6322
    
    Change-Id: Iafc7d7868af018db843e723cead1aa4f0c89ca9b

--- ct/go/master_scripts/run_chromium_analysis_on_workers/main.go
+++ ct/go/master_scripts/run_chromium_analysis_on_workers/main.go
@@ -202,7 +202,7 @@ func main() {
 		sklog.Errorf("Error encountered when calculating number of pages: %s", err)
 		return
 	}
-	if err := util.TriggerSwarmingTask(*pagesetType, "chromium_analysis", util.CHROMIUM_ANALYSIS_ISOLATE, *runID, 12*time.Hour, 1*time.Hour, util.USER_TASKS_PRIORITY, MAX_PAGES_PER_SWARMING_BOT, numPages, isolateExtraArgs, workerDimensions); err != nil {
+	if err := util.TriggerSwarmingTask(*pagesetType, "chromium_analysis", util.CHROMIUM_ANALYSIS_ISOLATE, *runID, 12*time.Hour, 1*time.Hour, util.USER_TASKS_PRIORITY, MAX_PAGES_PER_SWARMING_BOT, numPages, isolateExtraArgs, workerDimensions, util.GetRepeatValue(*benchmarkExtraArgs, 1)); err != nil {
 		sklog.Errorf("Error encountered when swarming tasks: %s", err)
 		return
 	}
