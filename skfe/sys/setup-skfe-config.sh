1933fcdf32aff9cababcbeaa26286e97046a4c66
Joe Gregorio <jcgregorio@google.com>
2017-03-07 11:13:44 -0500

    Remove last vestiges of local logging.
    BUG=skia:
    
    Change-Id: I221b68738a9d1470e3f04e39a590830540400600

--- skfe/sys/setup-skfe-config.sh
+++ skfe/sys/setup-skfe-config.sh
@@ -7,5 +7,5 @@ set -x
 # Note, if one of the next two lines fail, nginx will continue to run with the
 # previous configuation, which makes it fault silent but available.
 rm -f /etc/nginx/sites-enabled/default
-/usr/local/bin/certpoller --log_dir=/var/log/logserver skia-org-pem skia-org-key dh-params-pem
+/usr/local/bin/certpoller --logtostderr skia-org-pem skia-org-key dh-params-pem
 systemctl restart nginx
