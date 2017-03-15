5c1212f01219b690dd6950d2d4a851cf4a9fe8e9
Eric Boren <borenet@google.com>
2017-03-15 11:18:22 -0400

    Add events package for event-based metrics
    BUG=skia:
    
    Change-Id: I0a8dc4ce4703b50eceec2dd119afa343e100dc15

---
+++ go/metrics2/events/db.go
@@ -0,0 +1,107 @@
+package events
+
+import (
+	"bytes"
+	"encoding/gob"
+	"fmt"
+	"time"
+
+	"go.skia.org/infra/go/util"
+
+	"github.com/boltdb/bolt"
+)
+
+// EventDB is an interface used for storing Events in a BoltDB.
+type EventDB interface {
+	Append(string, []byte) error
+	Close() error
+	Insert(*Event) error
+	Range(string, time.Time, time.Time) ([]*Event, error)
+}
+
+// eventDB is a struct used for storing Events in a BoltDB.
+type eventDB struct {
+	db *bolt.DB
+}
+
+// NewEventDB returns an EventDB instance.
+func NewEventDB(filename string) (EventDB, error) {
+	db, err := bolt.Open(filename, 0600, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	rv := &eventDB{
+		db: db,
+	}
+
+	return rv, nil
+}
+
+// Close cleans up the eventDB.
+func (m *eventDB) Close() error {
+	return m.db.Close()
+}
+
+// Insert inserts the given Event into DB.
+func (m *eventDB) Insert(e *Event) error {
+	if util.TimeIsZero(e.Timestamp) {
+		return fmt.Errorf("Cannot insert an event without a timestamp.")
+	}
+	if e.Stream == "" {
+		return fmt.Errorf("Cannot insert an event without a stream.")
+	}
+
+	k, err := encodeKey(e.Timestamp)
+	if err != nil {
+		return err
+	}
+	var buf bytes.Buffer
+	if err := gob.NewEncoder(&buf).Encode(e); err != nil {
+		return err
+	}
+	return m.db.Update(func(tx *bolt.Tx) error {
+		b, err := tx.CreateBucketIfNotExists([]byte(e.Stream))
+		if err != nil {
+			return err
+		}
+		return b.Put(k, buf.Bytes())
+	})
+}
+
+// Append inserts the given data into the given stream at the current time.
+func (m *eventDB) Append(stream string, data []byte) error {
+	return m.Insert(&Event{
+		Stream:    stream,
+		Timestamp: time.Now(),
+		Data:      data,
+	})
+}
+
+// Range returns all Events in the given range from the given stream.
+func (m *eventDB) Range(stream string, start, end time.Time) ([]*Event, error) {
+	min, err := encodeKey(start)
+	if err != nil {
+		return nil, err
+	}
+	max, err := encodeKey(end)
+	if err != nil {
+		return nil, err
+	}
+
+	rv := []*Event{}
+	if err := m.db.View(func(tx *bolt.Tx) error {
+		c := tx.Bucket([]byte(stream)).Cursor()
+		for k, v := c.Seek(min); k != nil && bytes.Compare(k, max) <= 0; k, v = c.Next() {
+			e := new(Event)
+			if err := gob.NewDecoder(bytes.NewBuffer(v)).Decode(e); err != nil {
+				return err
+			}
+			rv = append(rv, e)
+		}
+		return nil
+	}); err != nil {
+		return nil, err
+	}
+	return rv, nil
+}
---
+++ go/metrics2/events/events.go
@@ -0,0 +1,187 @@
+package events
+
+/*
+	This package provides facilities for event-based metrics.
+
+	The metrics2 package deals with gauges; it doesn't have natural support for
+	events, ie. individual samples with timestamps. This package provides that
+	support by allowing the user to insert individual data points and provides
+	functions for aggregating data points into a gauge format which can then be
+	used as a normal metric.
+*/
+
+import (
+	"fmt"
+	"sync"
+	"time"
+
+	"golang.org/x/net/context"
+
+	"go.skia.org/infra/go/metrics2"
+	"go.skia.org/infra/go/sklog"
+	"go.skia.org/infra/go/util"
+)
+
+const (
+	MEASUREMENT_NAME = "event-metrics"
+
+	timestampFormat = "20060102T150405.000000000Z"
+)
+
+func encodeKey(ts time.Time) ([]byte, error) {
+	if ts.UnixNano() < 0 {
+		return nil, fmt.Errorf("Time is invalid: %s", ts)
+	}
+	return []byte(ts.UTC().Format(timestampFormat)), nil
+}
+
+func decodeKey(b []byte) (time.Time, error) {
+	return time.Parse(timestampFormat, string(b))
+}
+
+// AggregateFn is a function which reduces a number of Events into a single
+// data point.
+type AggregateFn func([]*Event) (float64, error)
+
+// Event contains some metadata plus whatever data the user chooses.
+type Event struct {
+	Stream    string
+	Timestamp time.Time
+	Data      []byte
+}
+
+// metric is a set of information used to derive metrics.
+type metric struct {
+	measurement string
+	tags        map[string]string
+	stream      string
+	period      time.Duration
+	agg         AggregateFn
+}
+
+// EventMetrics is a struct used for creating aggregate metrics on steams of
+// events.
+type EventMetrics struct {
+	db      EventDB
+	metrics []*metric
+	mtx     sync.Mutex
+}
+
+// NewEventMetrics returns an EventMetrics instance.
+func NewEventMetrics(db EventDB) (*EventMetrics, error) {
+	return &EventMetrics{
+		db:      db,
+		metrics: []*metric{},
+		mtx:     sync.Mutex{},
+	}, nil
+}
+
+// Start initiates the EventMetrics goroutines.
+func (m *EventMetrics) Start(ctx context.Context) {
+	lv := metrics2.NewLiveness("last-successful-event-metrics-update")
+	go util.RepeatCtx(time.Minute, ctx, func() {
+		if err := m.updateMetrics(time.Now()); err != nil {
+			sklog.Errorf("Failed to update event metrics: %s", err)
+		} else {
+			lv.Reset()
+		}
+	})
+}
+
+// Close cleans up the EventMetrics.
+func (m *EventMetrics) Close() error {
+	return m.db.Close()
+}
+
+// AggregateMetric sets the given aggregation function on the event stream and
+// adds a gauge for it. For example, to compute the sum of all int64 events over
+// a 24-hour period:
+//
+//      s.AggregateMetric("my-stream", myTags, 24*time.Hour, func(ev []Event) (float64, error) {
+//              sum := int64(0)
+//              for _, e := range ev {
+//                      sum += decodeInt64(e)
+//              }
+//              return float64(sum), nil
+//      })
+//
+func (m *EventMetrics) AggregateMetric(stream string, tags map[string]string, period time.Duration, agg AggregateFn) error {
+	mx := &metric{
+		measurement: MEASUREMENT_NAME,
+		tags: map[string]string{
+			"period": fmt.Sprintf("%s", period),
+			"stream": stream,
+		},
+		stream: stream,
+		period: period,
+		agg:    agg,
+	}
+	for k, v := range tags {
+		if _, ok := mx.tags[k]; ok {
+			return fmt.Errorf("Tag %q is reserved.", k)
+		}
+		mx.tags[k] = v
+	}
+	m.mtx.Lock()
+	defer m.mtx.Unlock()
+	m.metrics = append(m.metrics, mx)
+	return nil
+}
+
+// updateMetric updates the value for a single metric.
+func (m *EventMetrics) updateMetric(now time.Time, mx *metric) error {
+	ev, err := m.db.Range(mx.stream, now.Add(-mx.period), now)
+	if err != nil {
+		return err
+	}
+	v, err := mx.agg(ev)
+	if err != nil {
+		return err
+	}
+	metrics2.GetFloat64Metric(mx.measurement, mx.tags).Update(v)
+	return nil
+}
+
+// updateMetrics recalculates values for all metrics using the given value for
+// the current time.
+func (m *EventMetrics) updateMetrics(now time.Time) error {
+	m.mtx.Lock()
+	defer m.mtx.Unlock()
+	errs := []error{}
+	for _, mx := range m.metrics {
+		if err := m.updateMetric(now, mx); err != nil {
+			errs = append(errs, err)
+			continue
+		}
+	}
+	if len(errs) > 0 {
+		return fmt.Errorf("UpdateMetrics errors: %v", errs)
+	}
+	return nil
+}
+
+// UpdateMetrics recalculates values for all metrics.
+func (m *EventMetrics) UpdateMetrics() error {
+	return m.updateMetrics(time.Now())
+}
+
+// LogMetrics logs the current values for all metrics.
+func (m *EventMetrics) LogMetrics() {
+	m.mtx.Lock()
+	defer m.mtx.Unlock()
+	sklog.Infof("Current event metrics values:")
+	for _, mx := range m.metrics {
+		v := metrics2.GetFloat64Metric(mx.measurement, mx.tags).Get()
+		sklog.Infof("  %s %v: %f", mx.measurement, mx.tags, v)
+	}
+}
+
+// GetEventStream returns an EventStream instance.
+func (m *EventMetrics) GetEventStream(name string) *EventStream {
+	m.mtx.Lock()
+	defer m.mtx.Unlock()
+	return &EventStream{
+		name: name,
+		m:    m,
+	}
+}
