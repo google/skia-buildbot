9722eccbdcc5447a106cbab068a6e646edf17162
Eric Boren <borenet@google.com>
2017-03-15 11:18:22 -0400

    Add events package for event-based metrics
    BUG=skia:
    
    Change-Id: I0a8dc4ce4703b50eceec2dd119afa343e100dc15
    Reviewed-on: https://skia-review.googlesource.com/9542
    Reviewed-by: Stephan Altmueller <stephana@google.com>
    Commit-Queue: Eric Boren <borenet@google.com>

---
+++ go/metrics2/events/events_test.go
@@ -0,0 +1,47 @@
+package events
+
+import (
+	"io/ioutil"
+	"path"
+	"testing"
+	"time"
+
+	"go.skia.org/infra/go/testutils"
+
+	assert "github.com/stretchr/testify/require"
+)
+
+func TestAggregateMetric(t *testing.T) {
+	testutils.SmallTest(t)
+
+	tmp, err := ioutil.TempDir("", "")
+	assert.NoError(t, err)
+	defer testutils.RemoveAll(t, tmp)
+
+	db, err := NewEventDB(path.Join(tmp, "events.bdb"))
+	assert.NoError(t, err)
+	m, err := NewEventMetrics(db)
+	assert.NoError(t, err)
+
+	s := "my-events"
+	now := time.Now()
+	k1 := now.Add(-3 * time.Second)
+	v1 := 0.05
+	assert.NoError(t, m.db.Insert(&Event{
+		Stream:    s,
+		Timestamp: k1,
+		Data:      encodeEvent(v1),
+	}))
+
+	period := 20 * time.Minute
+	called := false
+	assert.NoError(t, m.AggregateMetric(s, nil, period, func(vs []*Event) (float64, error) {
+		called = true
+		assert.Equal(t, 1, len(vs))
+		assert.Equal(t, v1, decodeEvent(vs[0].Data))
+		return 0.0, nil
+	}))
+	assert.False(t, called)
+	assert.NoError(t, m.updateMetrics(now))
+	assert.True(t, called)
+}
