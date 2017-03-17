6a149fe0e193aeb3755be1e6541cefe8b4a8608a
Ravi Mistry <rmistry@google.com>
2017-03-17 15:13:54 -0400

    WIP II
    BUG=skia:
    
    Change-Id: Ie9479ba71186a55476fbe2ab7be313ef7f576af3

--- autoroll/go/repo_manager/repo_manager.go
+++ autoroll/go/repo_manager/repo_manager.go
@@ -15,6 +15,7 @@ import (
 	"go.skia.org/infra/go/sklog"
 
 	"go.skia.org/infra/go/autoroll"
+	"go.skia.org/infra/go/common"
 	"go.skia.org/infra/go/exec"
 	"go.skia.org/infra/go/git/gitinfo"
 	"go.skia.org/infra/go/util"
@@ -30,6 +31,10 @@ const (
 	ROLL_STRATEGY_SINGLE = "single"
 
 	TMPL_CQ_INCLUDE_TRYBOTS = "CQ_INCLUDE_TRYBOTS=%s"
+
+	SERVICE_ACCOUNT = "31977622648@project.gserviceaccount.com"
+
+	UPSTREAM_REMOTE_NAME = "remote"
 )
 
 var (
@@ -38,6 +43,8 @@ var (
 	NewRepoManager func(string, string, string, time.Duration, string) (RepoManager, error) = NewDefaultRepoManager
 
 	DEPOT_TOOLS_AUTH_USER_REGEX = regexp.MustCompile(fmt.Sprintf("Logged in to %s as ([\\w-]+).", autoroll.RIETVELD_URL))
+
+	IGNORE_MERGE_CONFLICT_FILES = []string{"include/config/SkUserConfig.h", "fuzz/FuzzCanvas.cpp", "src/gpu/GrTextureAdjuster.h"}
 )
 
 // issueJson is the structure of "git cl issue --json"
@@ -57,22 +64,44 @@ type RepoManager interface {
 	User() string
 }
 
+type AndroidRepoManager struct {
+	RepoManager
+}
+
 // repoManager is a struct used by AutoRoller for managing checkouts.
 type repoManager struct {
-	depot_tools string
-	gclient     string
+	depot_tools             string
+	gclient                 string
+	infoMtx                 sync.RWMutex
+	lastRollRev             string
+	repoMtx                 sync.RWMutex
+	rollDep                 string
+	childDir                string
+	childHead               string
+	childPath               string
+	childRepo               *gitinfo.GitInfo
+	parentDir               string
+	parentRepo              string
+	user                    string
+	workdir                 string
+	repoToolPath            string
+	gitCookieAuthDaemonPath string
+}
+
+type androidRepoManager struct {
 	infoMtx     sync.RWMutex
 	lastRollRev string
 	repoMtx     sync.RWMutex
-	rollDep     string
 	childDir    string
 	childHead   string
 	childPath   string
 	childRepo   *gitinfo.GitInfo
-	parentDir   string
-	parentRepo  string
-	user        string
-	workdir     string
+	//parentDir           string
+	//parentRepo          string
+	user                string
+	workdir             string
+	repoTool            string
+	gitCookieAuthDaemon string
 }
 
 // getEnv returns the environment used for most commands.
@@ -99,6 +128,350 @@ func getDepotToolsUser(depotTools string) (string, error) {
 		return "", fmt.Errorf("Unable to parse the output of depot-tools-auth.")
 	}
 	return m[1], nil
+}
+
+// rmistry
+func NewAndroidRepoManager(workdir, childPath string, frequency time.Duration, repoTool, gitCookieAuthDaemon string) (RepoManager, error) {
+	fmt.Println("I AM HERE!!")
+
+	wd := path.Join(workdir, "android_repo")
+	//parentBase := strings.TrimSuffix(path.Base(parentRepo), ".git")
+	//parentDir := path.Join(wd, parentBase)
+
+	r := &androidRepoManager{
+		childDir:            path.Join(wd, childPath),
+		childPath:           childPath,
+		childRepo:           nil, // This will be filled in on the first update.
+		gitCookieAuthDaemon: gitCookieAuthDaemon,
+		repoTool:            repoTool,
+		user:                SERVICE_ACCOUNT,
+		workdir:             wd,
+	}
+
+	// Put the git config --global color.ui true in the startup script?? and the other git config stuff??
+
+	if err := r.update(); err != nil {
+		return nil, err
+	}
+	go func() {
+		for _ = range time.Tick(frequency) {
+			// Authenticate before trying to update repo.
+			if _, err := exec.RunCwd(gitCookieAuthDaemon); err != nil {
+				util.LogErr(err)
+			}
+			// Update repo.
+			util.LogErr(r.update())
+		}
+	}()
+	return r, nil
+}
+
+// update syncs code in the relevant repositories.
+func (r *androidRepoManager) update() error {
+	// Sync the projects.
+	r.repoMtx.Lock()
+	defer r.repoMtx.Unlock()
+
+	// Create the working directory if needed.
+	if _, err := os.Stat(r.workdir); err != nil {
+		if err := os.MkdirAll(r.workdir, 0755); err != nil {
+			return err
+		}
+	}
+
+	// TODO(rmistry): Get URL from metadata
+	fmt.Println("Skipping the init and sync cmds!")
+	//fmt.Println("ABOUT TO RUN THE INIT AND SYNC CMDS")
+	//if _, err := exec.RunCwd(r.workdir, r.repoTool, "init", "-u", "https://googleplex-android.googlesource.com/a/platform/manifest", "-g", "all,-notdefault,-darwin", "-b", "master"); err != nil {
+	//	return err
+	//}
+	//if _, err := exec.RunCwd(r.workdir, r.repoTool, "sync", "-j32"); err != nil {
+	//	return err
+	//}
+
+	// Create the child GitInfo if needed.
+	if r.childRepo == nil {
+		childRepo, err := gitinfo.NewGitInfo(r.childDir, false, false /* allBranches what should this be?*/)
+		if err != nil {
+			return err
+		}
+		r.childRepo = childRepo
+	}
+
+	// Fix the review to be "https://googleplex-android.googlesource.com/"
+	// instead of "sso://googleplex-android which does not work outside prod.
+	// TODO(rmistry): Get this from metadata as well.
+	if _, err := exec.RunCwd(r.childRepo.Dir(), "git", "config", "remote.goog.review", "https://googleplex-android.googlesource.com/"); err != nil {
+		return err
+	}
+
+	// Check to see whether there is an upstream yet.
+	remoteOutput, err := exec.RunCwd(r.childRepo.Dir(), "git", "remote", "show")
+	if err != nil {
+		return err
+	}
+	if !strings.Contains(remoteOutput, UPSTREAM_REMOTE_NAME) {
+		if _, err := exec.RunCwd(r.childRepo.Dir(), "git", "remote", "add", UPSTREAM_REMOTE_NAME, common.REPO_SKIA); err != nil {
+			return err
+		}
+
+	}
+
+	// Get the last roll revision.
+	lastRollRev, err := r.getLastRollRev()
+	if err != nil {
+		return err
+	}
+
+	// Record child HEAD
+	childHead, err := r.getChildRepoHead()
+	if err != nil {
+		return err
+	}
+	r.infoMtx.Lock()
+	defer r.infoMtx.Unlock()
+	r.lastRollRev = lastRollRev
+	r.childHead = childHead
+
+	fmt.Println("lastRollRev is:")
+	fmt.Println(lastRollRev)
+	fmt.Println("Child head is:")
+	fmt.Println(childHead)
+	return nil
+}
+
+// ForceUpdate forces the repoManager to update.
+func (r *androidRepoManager) ForceUpdate() error {
+	return r.update()
+}
+
+// getChildRepoHead returns the commit hash of the latest commit in the child repo.
+func (r *androidRepoManager) getChildRepoHead() (string, error) {
+	output, err := exec.RunCwd(r.childRepo.Dir(), "git", "ls-remote", UPSTREAM_REMOTE_NAME, "refs/heads/master", "-1")
+	if err != nil {
+		return "", err
+	}
+	tokens := strings.Split(output, "\t")
+	return tokens[0], nil
+}
+
+// getLastRollRev returns the commit hash of the last-completed DEPS roll.
+func (r *androidRepoManager) getLastRollRev() (string, error) {
+	output, err := exec.RunCwd(r.childRepo.Dir(), "git", "log", "--pretty=format:%ae %H")
+	if err != nil {
+		return "", err
+	}
+	commitLines := strings.Split(output, "\n")
+	indexWithMergeCommit := 0
+	for i, commitLine := range commitLines {
+		tokens := strings.Split(commitLine, " ")
+		if tokens[0] == "31977622648@project.gserviceaccount.com" {
+			indexWithMergeCommit = i
+			break
+		}
+	}
+	// The commit immediately before the merge commit will have a commit hash
+	// that corresponds to the target repo.
+	tokens := strings.Split(commitLines[indexWithMergeCommit+1], " ")
+	return tokens[1], nil
+}
+
+// FullChildHash returns the full hash of the given short hash or ref in the
+// child repo.
+func (r *androidRepoManager) FullChildHash(shortHash string) (string, error) {
+	r.repoMtx.RLock()
+	defer r.repoMtx.RUnlock()
+	return r.childRepo.FullHash(shortHash)
+}
+
+// LastRollRev returns the last-rolled child commit.
+func (r *androidRepoManager) LastRollRev() string {
+	r.infoMtx.RLock()
+	defer r.infoMtx.RUnlock()
+	return r.lastRollRev
+}
+
+// RolledPast determines whether DEPS has rolled past the given commit.
+func (r *androidRepoManager) RolledPast(hash string) (bool, error) {
+	r.repoMtx.RLock()
+	defer r.repoMtx.RUnlock()
+	return git.GitDir(r.childDir).IsAncestor(hash, r.lastRollRev)
+}
+
+// ChildHead returns the current child origin/master branch head.
+func (r *androidRepoManager) ChildHead() string {
+	r.infoMtx.RLock()
+	defer r.infoMtx.RUnlock()
+	return r.childHead
+}
+
+// CreateNewRoll creates and uploads a new Android roll to the given commit.
+// Returns the change number of the uploaded roll.
+func (r *androidRepoManager) CreateNewRoll(strategy string, emails []string, cqExtraTrybots string, dryRun, gerrit bool) (int64, error) {
+	r.repoMtx.Lock()
+	defer r.repoMtx.Unlock()
+
+	//// Clean the checkout, get onto a fresh branch.
+	//if err := r.cleanParent(); err != nil {
+	//	return 0, err
+	//}
+	//if _, err := exec.RunCwd(r.workdir, "git", "checkout", "-b", DEPS_ROLL_BRANCH, "-t", "origin/master", "-f"); err != nil {
+	//	return 0, err
+	//}
+
+	//// Defer some more cleanup.
+	//defer func() {
+	//	util.LogErr(r.cleanParent())
+	//}()
+
+	// Update the upstream remote.
+	if _, err := exec.RunCwd(r.childDir, "git", "fetch", UPSTREAM_REMOTE_NAME); err != nil {
+		return 0, err
+	}
+
+	// Create the roll CL.
+
+	// Determine what commit we're rolling to.
+	cr := r.childRepo
+	commits, err := cr.RevList(fmt.Sprintf("%s..%s", r.lastRollRev, r.childHead))
+	if err != nil {
+		return 0, fmt.Errorf("Failed to list revisions: %s", err)
+	}
+	rollTo := r.childHead
+	if strategy == ROLL_STRATEGY_SINGLE {
+		rollTo = commits[len(commits)-1]
+		commits = commits[len(commits)-1:]
+	}
+
+	// rmistry: LEFT IT HERE!
+	// Need to now do the merge.
+	// Then the GN steps.
+	// Then create commit msg.
+
+	// TODO(rmistry): TEMP TEMP TEMP
+	rollTo = "0c984a0af30989fe20b1f8af18867983a88c48b6" // 2 changes conflict here!
+
+	// Start the merge.
+
+	// This section is tricky. Will have to be tested well.
+	if _, err := exec.RunCwd(r.childDir, "git", "merge", rollTo, "--no-commit"); err != nil {
+		// Check to see if this was a merge conflict with IGNORE_MERGE_CONFLICT_FILES.
+		conflictsOutput, conflictsErr := exec.RunCwd(r.childDir, "git", "diff", "--name-only")
+		fmt.Println(conflictsOutput)
+		fmt.Println(conflictsErr)
+		if conflictsErr != nil || conflictsOutput == "" {
+			util.LogErr(conflictsErr)
+			return 0, fmt.Errorf("Failed to roll to %s. Needs human investigation: %s", rollTo, err)
+		}
+		for _, conflict := range strings.Split(conflictsOutput, "\n") {
+			if conflict == "" {
+				continue
+			}
+			ignoreConflict := false
+			for _, ignore := range IGNORE_MERGE_CONFLICT_FILES {
+				if conflict == ignore {
+					ignoreConflict = true
+					break
+				}
+			}
+			fmt.Println("000000000000000")
+			fmt.Println(conflict)
+			fmt.Println(ignoreConflict)
+			if !ignoreConflict {
+				return 0, fmt.Errorf("Failed to roll to %s. Conflicts in %s: %s", rollTo, conflictsOutput, err)
+			}
+		}
+	}
+
+	fmt.Println("Done with the merge. Now what????")
+	return 0, nil
+
+	// Run roll-dep.
+	args := []string{r.childPath, "--roll-to", rollTo}
+	//if len(bugs) > 0 {
+	//	args = append(args, "--bug", strings.Join(bugs, ","))
+	//}
+	sklog.Infof("Running command: roll-dep %s", strings.Join(args, " "))
+	if _, err := exec.RunCommand(&exec.Command{
+		Dir:  r.workdir,
+		Env:  getEnv("r.depot_tools"),
+		Name: "r.rollDep",
+		Args: args,
+	}); err != nil {
+		return 0, err
+	}
+	// Build the commit message, starting with the message provided by roll-dep.
+	commitMsg, err := exec.RunCwd(r.workdir, "git", "log", "-n1", "--format=%B", "HEAD")
+	if err != nil {
+		return 0, err
+	}
+	commitMsg += `
+Documentation for the AutoRoller is here:
+https://skia.googlesource.com/buildbot/+/master/autoroll/README.md
+
+If the roll is causing failures, see:
+http://www.chromium.org/developers/tree-sheriffs/sheriff-details-chromium#TOC-Failures-due-to-DEPS-rolls
+
+`
+	if cqExtraTrybots != "" {
+		commitMsg += "\n" + fmt.Sprintf(TMPL_CQ_INCLUDE_TRYBOTS, cqExtraTrybots)
+	}
+	uploadCmd := &exec.Command{
+		Dir:  r.workdir,
+		Env:  getEnv("r.depot_tools"),
+		Name: "git",
+		Args: []string{"cl", "upload", "--bypass-hooks", "-f"},
+	}
+	if dryRun {
+		uploadCmd.Args = append(uploadCmd.Args, "--cq-dry-run")
+	} else {
+		uploadCmd.Args = append(uploadCmd.Args, "--use-commit-queue")
+	}
+	if gerrit {
+		uploadCmd.Args = append(uploadCmd.Args, "--gerrit")
+	}
+	tbr := "\nTBR="
+	if emails != nil && len(emails) > 0 {
+		emailStr := strings.Join(emails, ",")
+		tbr += emailStr
+		uploadCmd.Args = append(uploadCmd.Args, "--send-mail", "--cc", emailStr)
+	}
+	commitMsg += tbr
+	uploadCmd.Args = append(uploadCmd.Args, "-m", commitMsg)
+
+	// Upload the CL.
+	if _, err := exec.RunCommand(uploadCmd); err != nil {
+		return 0, err
+	}
+
+	// Obtain the issue number.
+	tmp, err := ioutil.TempDir("", "")
+	if err != nil {
+		return 0, err
+	}
+	defer util.RemoveAll(tmp)
+	jsonFile := path.Join(tmp, "issue.json")
+	if _, err := exec.RunCommand(&exec.Command{
+		Dir:  r.workdir,
+		Env:  getEnv("r.depot_tools"),
+		Name: "git",
+		Args: []string{"cl", "issue", fmt.Sprintf("--json=%s", jsonFile)},
+	}); err != nil {
+		return 0, err
+	}
+	f, err := os.Open(jsonFile)
+	if err != nil {
+		return 0, err
+	}
+	var issue issueJson
+	if err := json.NewDecoder(f).Decode(&issue); err != nil {
+		return 0, err
+	}
+	return issue.Issue, nil
+}
+
+func (r *androidRepoManager) User() string {
+	return r.user
 }
 
 // NewDefaultRepoManager returns a RepoManager instance which operates in the given
