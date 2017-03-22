1c0adf7fb6f3feed2656df2664aaf799da406b46
Eric Boren <borenet@google.com>
2017-03-22 15:17:10 -0400

    [datahopper] Add "time to X% test coverage" metrics
    BUG=skia:
    
    Change-Id: Iee8376bdbf0ee70ec646cc77f2101508306a66bd

--- datahopper/go/datahopper/main.go
+++ datahopper/go/datahopper/main.go
@@ -14,6 +14,7 @@ import (
 
 	"cloud.google.com/go/storage"
 	"go.skia.org/infra/datahopper/go/accum"
+	"go.skia.org/infra/datahopper/go/time_to_bot_coverage"
 	"go.skia.org/infra/go/auth"
 	"go.skia.org/infra/go/buildbot"
 	"go.skia.org/infra/go/common"
@@ -348,6 +349,11 @@ func main() {
 
 	// Jobs metrics.
 	if err := StartJobMetrics(*taskSchedulerDbUrl, context.Background()); err != nil {
+		sklog.Fatal(err)
+	}
+
+	// Generate "time to X% bot coverage" metrics.
+	if err := time_to_bot_coverage.Start(*taskSchedulerDbUrl, *workdir, db, context.Background()); err != nil {
 		sklog.Fatal(err)
 	}
 
