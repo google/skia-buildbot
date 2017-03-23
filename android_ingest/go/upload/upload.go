ba5d9781dda41ace7dbd8f19636c00ba2658e8b0
Joe Gregorio <jcgregorio@google.com>
2017-03-23 09:37:32 -0400

    [android-ingest] Use md5 hash instead of timestamps to differentiate files.
    BUG=skia:
    
    Change-Id: I2bfe2b5062f99e7bcd2b7662e4cf97e61a8038e8

--- android_ingest/go/upload/upload.go
+++ android_ingest/go/upload/upload.go
@@ -1,6 +1,7 @@
 package upload
 
 import (
+	"crypto/md5"
 	"fmt"
 	"path/filepath"
 	"strings"
@@ -14,13 +15,15 @@ import (
 //
 // gcsPath will be the root of the path.
 // now is the time which will be encoded in the path.
-func ObjectPath(benchData *ingestcommon.BenchData, gcsPath string, now time.Time) string {
+// b is the source bytes of the incoming file.
+func ObjectPath(benchData *ingestcommon.BenchData, gcsPath string, now time.Time, b []byte) string {
+	hash := fmt.Sprintf("%x", md5.Sum(b))
 	keyparts := []string{}
 	if benchData.Key != nil {
 		for k, v := range benchData.Key {
 			keyparts = append(keyparts, k, v)
 		}
 	}
-	filename := fmt.Sprintf("%s_%s_%d.json", benchData.Hash, strings.Join(keyparts, "_"), now.UnixNano())
+	filename := fmt.Sprintf("%s_%s_%s.json", benchData.Hash, strings.Join(keyparts, "_"), hash)
 	return filepath.Join(gcsPath, now.Format("2006/01/02/15/"), filename)
 }
