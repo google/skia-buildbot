b8e3a147579f97f365bebfce16e2296064558c8d
Eric Boren <borenet@google.com>
2017-03-24 19:28:22 -0400

    WIP
    BUG=skia:
    
    Change-Id: I1b52add197eba547010d65b0d6d4b2ad078e40b3

---
+++ status/go/find_breaks/find_breaks.go
@@ -0,0 +1,234 @@
+package find_breaks
+
+import (
+	"crypto/md5"
+	"sort"
+
+	"go.skia.org/infra/go/sklog"
+)
+
+// set is a struct used for slicing subsets of a larger slice while supporting
+// set intersection. Union is not defined, since it is not possible to union
+// two arbitrary sets while maintaining a contiguous range.
+type set struct {
+	start int
+	end   int
+}
+
+func newSet(start, end int) set {
+	return set{
+		start: start,
+		end:   end,
+	}
+}
+
+func (s set) Intersect(other set) set {
+	rv := newSet(s.start, s.end)
+	if other.start > rv.start {
+		rv.start = other.start
+	}
+	if other.end < rv.end {
+		rv.end = other.end
+	}
+	if rv.start >= rv.end {
+		rv.start = -1
+		rv.end = -1
+	}
+	return rv
+}
+
+func (s set) Len() int {
+	return s.end - s.start
+}
+
+func (s set) Empty() bool {
+	return s.Len() == 0
+}
+
+func (s set) Copy() set {
+	return newSet(s.start, s.end)
+}
+
+// failure is a struct which represents a failure of a given task spec,
+// potentially merged from several failed tasks.
+type failure struct {
+	// id is provided by the creator as a handle for the failure.
+	id string
+
+	// brokeIn is the set of commits which may have caused the failure.
+	// Should be empty if the failure might have been caused before the
+	// current commit window.
+	brokeIn set
+
+	// failing is the set of commits affected by the failure. Must be a
+	// superset of brokeIn. Should not extend outside the current commit
+	// window.
+	failing set
+
+	// fixedIn is the set of commits which may have fixed the failure. May
+	// be empty if the failure has not yet been fixed.
+	fixedIn set
+}
+
+// failureGroup represents a group of failures which are likely to be related.
+type failureGroup struct {
+	// ids are the ids of all of the failures in the group.
+	ids []string
+
+	// brokeIn is the set of commits which may have caused the failures in
+	// the group. May be empty if the failure could have been caused before
+	// the current ocmmit window.
+	brokeIn set
+
+	// failing is the set of commits which are affected by the failures in
+	// the group.
+	failing set
+
+	// fixedIn is the set of commits which may have fixed the failures in
+	// the group. May be empty if the failures have not yet been fixed.
+	fixedIn set
+}
+
+// merge combines two failures into a third failure or returns nil if the two
+// failures cannot be merged.
+func merge(f1, f2 *failure) *failure {
+	rv := &failure{}
+
+	// In order for two failures to be part of the same set:
+
+	// 1. The set of commits which fail must overlap.
+	if f1.failing.Intersect(f2.failing).Empty() {
+		sklog.Errorf("f1.failing and f2.failing do not intersect")
+		return nil
+	}
+
+	// 2. The intersection of the sets of commits which caused the failures
+	//    must be non-empty. If the break might have occurred before the
+	//    current commit window, the brokeIn set is empty; in this case, the
+	//    brokeIn sets of both failures must be empty in order to match.
+	rv.brokeIn = f1.brokeIn.Intersect(f2.brokeIn)
+	if rv.brokeIn.Empty() {
+		if !f1.brokeIn.Empty() || !f2.brokeIn.Empty() {
+			sklog.Errorf("f1.brokeIn and f2.brokeIn do not intersect")
+			return nil
+		}
+	}
+
+	// 3. If both failures have been fixed, the intersection of the sets of
+	//    commits which fixed the failures must be non-empty.
+	rv.fixedIn = f1.fixedIn.Intersect(f2.fixedIn)
+	rv.failing = set{
+		start: rv.brokeIn.start,
+		end:   rv.fixedIn.start,
+	}
+	if !f1.fixedIn.Empty() && !f2.fixedIn.Empty() {
+		if rv.fixedIn.Empty() {
+			sklog.Errorf("f1.fixedIn and f2.fixedIn do not intersect")
+			return nil
+		}
+		// 3a. If both the brokeIn and fixedIn sets match, we have
+		//     enough information to stop here.
+		return rv
+	}
+
+	// 4. The fixedIn set of one failure cannot be wholly contained within
+	//    the failing set of the other.
+	if f1.fixedIn.Intersect(f2.failing).Len() == f1.fixedIn.Len() {
+		return nil
+	}
+	if f2.fixedIn.Intersect(f1.failing).Len() == f2.fixedIn.Len() {
+		return nil
+	}
+	return rv
+}
+
+// findFailureGroups finds groups of related failures by attempting to merge
+// each failure with every other failure.
+// TODO(borenet): This algorithm is both inefficient (n^2) and possibly
+// incorrect, since the results may depend on the ordering of the inputs.
+// Test heavily to ensure correctness and try to come up with a more efficient
+// algorithm.
+func findFailureGroups(failures []*failure) ([]*failureGroup, error) {
+	groups := map[string]*failureGroup{}
+	for _, f1 := range failures {
+		ids := []string{f1.id}
+		merged := &failure{
+			brokeIn: f1.brokeIn.Copy(),
+			failing: f1.failing.Copy(),
+			fixedIn: f1.fixedIn.Copy(),
+		}
+		for _, f2 := range failures {
+			if f2.id == f1.id {
+				continue
+			}
+			m := merge(merged, f2)
+			if m == nil {
+				continue
+			}
+			merged = m
+			ids = append(ids, f2.id)
+		}
+		sort.Strings(ids)
+		h := md5.New()
+		for _, id := range ids {
+			if _, err := h.Write([]byte(id)); err != nil {
+				return nil, err
+			}
+		}
+		fg := &failureGroup{
+			ids:     ids,
+			brokeIn: merged.brokeIn,
+			failing: merged.failing,
+			fixedIn: merged.fixedIn,
+		}
+		groups[string(h.Sum(nil))] = fg
+	}
+
+	rv := make([]*failureGroup, 0, len(groups))
+	for _, fg := range groups {
+		rv = append(rv, fg)
+	}
+	return rv, nil
+}
+
+func main() {
+	// Find groups of failing tasks which constitute breakages.
+
+	// Start by merging tasks into contiguous sets of successes and failures
+	// within a column (task spec). Note that we're assuming all failures
+	// within a column have the same cause. A more intelligent system would
+	// be able to distinguish *what* failed and only merge the failures if
+	// they matched.
+
+	// Then merge columns into breakages. For each column:
+	// - Find the set of commits which may have caused the break.
+	// - Find the set of commits which may have fixed the break.
+	//
+	// Two columns may be merged if:
+	// - The intersection of their break sets is non-empty.
+	// - The intersection of their fix sets is non-empty.
+	// - The intersection of the break set of one column and the fix set of
+	//   the other is empty.
+	//
+	// However, in merging columns we may reduce the size of the break and
+	// fix sets. We want this behavior in some sense, because the goal is to
+	// narrow down the blame to a single commit. But we need to make sure
+	// that we don't include a failure which then causes us to exclude
+	// others. For example:
+	//
+	//           col1  col2  col3
+	//            S     S     S
+	//            F     S     F
+	//            S     F     F
+	//            S     S     S
+	//
+	// In this case, we can merge col1 with col3 or col2 with col3 but not
+	// both. We need to make sure that the algorithm gives us both sets:
+	// [col1, col3] and [col2, col3].
+	//
+	// Edge cases. A failure may extend up to the most recent task, or back
+	// to before our time window, such that we can't actually determine what
+	// the break or fix set should be. We can handle this by making the
+	// break or fix set empty (or contain a single special value) and let it
+	// behave as a wildcard which matches anything.
+}
---
+++ status/go/find_breaks/find_breaks_test.go
@@ -0,0 +1,207 @@
+package find_breaks
+
+import (
+	"fmt"
+	"testing"
+
+	assert "github.com/stretchr/testify/require"
+	"go.skia.org/infra/go/sklog"
+	"go.skia.org/infra/go/testutils"
+	"go.skia.org/infra/go/util"
+)
+
+var (
+	commits = []string{"a", "b", "c", "d", "e", "f", "g"}
+)
+
+func assertSubset(t *testing.T, v, set set) {
+	assert.Equal(t, v.Len(), v.Intersect(set).Len())
+}
+
+func assertValid(t *testing.T, f *failure) {
+	assert.Equal(t, 0, f.brokeIn.Intersect(f.fixedIn).Len())
+	// assert(brokeIn < fixedIn)
+	assertSubset(t, f.brokeIn, f.failing)
+}
+
+func TestMerge(t *testing.T) {
+	f1 := &failure{
+		id:      "f1",
+		brokeIn: newSet(0, 1),
+		failing: newSet(0, 1),
+		fixedIn: newSet(1, 2),
+	}
+	f2 := &failure{
+		id:      "f2",
+		brokeIn: newSet(0, 1),
+		failing: newSet(0, 1),
+		fixedIn: newSet(1, 2),
+	}
+	f3 := &failure{
+		id:      "f3",
+		brokeIn: newSet(1, 2),
+		failing: newSet(1, 2),
+		fixedIn: newSet(2, 3),
+	}
+	f4 := &failure{
+		id:      "f4",
+		brokeIn: newSet(0, 2),
+		failing: newSet(0, 2),
+		fixedIn: newSet(2, 3),
+	}
+	f5 := &failure{
+		id:      "f5",
+		brokeIn: newSet(1, 2),
+		failing: newSet(1, 2),
+		fixedIn: newSet(3, 4),
+	}
+	f6 := &failure{
+		id:      "f6",
+		brokeIn: newSet(0, 2),
+		failing: newSet(0, 2),
+		fixedIn: newSet(2, 4),
+	}
+	f7 := &failure{
+		id:      "f7",
+		brokeIn: newSet(0, 1),
+		failing: newSet(0, 1),
+		fixedIn: newSet(1, 4),
+	}
+	f8 := &failure{
+		id:      "f8",
+		brokeIn: newSet(2, 3),
+		failing: newSet(2, 3),
+		fixedIn: newSet(3, 4),
+	}
+	f9 := &failure{
+		id:      "f9",
+		brokeIn: newSet(0, 2),
+		failing: newSet(0, 2),
+		fixedIn: newSet(-1, -1),
+	}
+
+	type testCase struct {
+		f1     *failure
+		f2     *failure
+		expect *failure
+	}
+	tc := []testCase{
+		// 0. Very simple equality.
+		testCase{
+			f1: f1,
+			f2: f2,
+			expect: &failure{
+				brokeIn: newSet(0, 1),
+				failing: newSet(0, 1),
+				fixedIn: newSet(1, 2),
+			},
+		},
+		// 1. Not equal.
+		testCase{
+			f1:     f1,
+			f2:     f3,
+			expect: nil,
+		},
+		// 2. Overlap, fixes don't match.
+		testCase{
+			f1:     f4,
+			f2:     f5,
+			expect: nil,
+		},
+		// 3. Overlap, fixes match.
+		testCase{
+			f1: f6,
+			f2: f5,
+			expect: &failure{
+				brokeIn: newSet(1, 2),
+				failing: newSet(1, 3),
+				fixedIn: newSet(3, 4),
+			},
+		},
+		// 4. Totally disjoint.
+		testCase{
+			f1:     f7,
+			f2:     f8,
+			expect: nil,
+		},
+		// 5. The fix set of one failure cannot be wholly contained
+		//    within the failed set of the other.
+		testCase{
+			f1:     f1,
+			f2:     f9,
+			expect: nil,
+		},
+	}
+	for idx, tc := range tc {
+		sklog.Errorf("%d", idx)
+		assertValid(t, tc.f1)
+		assertValid(t, tc.f2)
+		m := merge(tc.f1, tc.f2)
+		testutils.AssertDeepEqual(t, tc.expect, m)
+		if m != nil {
+			assertValid(t, m)
+		}
+	}
+}
+
+func TestFindFailureGroups(t *testing.T) {
+	f1 := &failure{
+		id:      "1",
+		brokeIn: newSet(0, 1),
+		failing: newSet(0, 1),
+		fixedIn: newSet(1, 2),
+	}
+	f2 := &failure{
+		id:      "2",
+		brokeIn: newSet(1, 2),
+		failing: newSet(1, 2),
+		fixedIn: newSet(2, 3),
+	}
+	f3 := &failure{
+		id:      "3",
+		brokeIn: newSet(0, 2),
+		failing: newSet(0, 2),
+		fixedIn: newSet(2, 5),
+	}
+	f4 := &failure{
+		id:      "4",
+		brokeIn: newSet(1, 2),
+		failing: newSet(1, 2),
+		fixedIn: newSet(3, 5),
+	}
+	f5 := &failure{
+		id:      "5",
+		brokeIn: newSet(2, 4),
+		failing: newSet(2, 4),
+		fixedIn: newSet(4, 5),
+	}
+
+	count := 1
+	check := func(input []*failure, expect [][]string) {
+		sklog.Errorf("%d", count)
+		count++
+		got, err := findFailureGroups(input)
+		assert.NoError(t, err)
+		sklog.Errorf("got %d failure groups", len(got))
+		remaining := make([][]string, len(expect))
+		copy(remaining, expect)
+		for _, fg := range got {
+			sklog.Errorf("%v", fg)
+			found := false
+			for i, e := range remaining {
+				if util.SSliceEqual(e, fg.ids) {
+					remaining = append(remaining[:i], remaining[i+1:]...)
+					found = true
+					break
+				}
+			}
+			assert.True(t, found, fmt.Sprintf("failure group: %v", fg))
+		}
+		assert.Equal(t, 0, len(remaining))
+	}
+
+	check([]*failure{f1, f2}, [][]string{[]string{f1.id}, []string{f2.id}})
+	check([]*failure{f2, f3}, [][]string{[]string{f2.id, f3.id}})
+	check([]*failure{f3, f4, f5}, [][]string{[]string{f3.id, f4.id}, []string{f4.id, f5.id}})
+	check([]*failure{f1, f2, f3, f4, f5}, [][]string{[]string{f1.id, f3.id}, []string{f2.id, f3.id}, []string{f3.id, f4.id}, []string{f4.id, f5.id}})
+}
