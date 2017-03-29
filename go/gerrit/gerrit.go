ad4ba097c304beba24c39f01aeba75af34363664
Eric Boren <borenet@google.com>
2017-03-29 11:06:38 -0400

    [autoroll] Fixes for internal autoroller
    Bug: skia:
    Change-Id: I550aede7ca02a1d8b1268aefdd04dc2314263efc

--- go/gerrit/gerrit.go
+++ go/gerrit/gerrit.go
@@ -260,7 +260,7 @@ func (g *Gerrit) GetPatch(issue int64, revision string) (string, error) {
 		return "", fmt.Errorf("Failed to GET %s: %s", url, err)
 	}
 	if resp.StatusCode == 404 {
-		return "", fmt.Errorf("Not a valid Issue %s", url)
+		return "", fmt.Errorf("Issue not found: %s", url)
 	}
 	if resp.StatusCode >= 400 {
 		return "", fmt.Errorf("Error retrieving %s: %d %s", url, resp.StatusCode, resp.Status)
@@ -343,7 +343,7 @@ func (g *Gerrit) get(suburl string, rv interface{}) error {
 		return fmt.Errorf("Failed to GET %s: %s", g.url+suburl, err)
 	}
 	if resp.StatusCode == 404 {
-		return fmt.Errorf("Not a valid Issue %s", g.url+suburl)
+		return fmt.Errorf("Issue not found: %s", g.url+suburl)
 	}
 	if resp.StatusCode >= 400 {
 		return fmt.Errorf("Error retrieving %s: %d %s", g.url+suburl, resp.StatusCode, resp.Status)
