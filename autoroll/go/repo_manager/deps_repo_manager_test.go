150f8469627a14dc01ed799b0e18b8a6e095ab00
Ravi Mistry <rmistry@google.com>
2017-03-30 09:25:24 -0400

    Create Android Autoroller
    This CL:
    * Converts repo_manager.go into a generic interface with helper methods.
    * Creates deps_repo_manager.go which is just code copied out of the old repo_manager.go
    * Creates a new android_repo_manager.go which contains simplified steps from https://chrome-internal.googlesource.com/chrome/tools/build_limited/scripts/slave/+/master/skia/merge_into_android.py
    * Private URLs are retrieved from metadata.
    * Uses the new features added in go/gerrit.
    
    BUG=skia:5538
    BUG=skia:6438
    
    Change-Id: Ie9479ba71186a55476fbe2ab7be313ef7f576af3

--- autoroll/go/repo_manager/deps_repo_manager_test.go
+++ autoroll/go/repo_manager/deps_repo_manager_test.go
@@ -2,18 +2,17 @@ package repo_manager
 
 import (
 	"fmt"
-	"io/ioutil"
-	"path"
-	"strings"
-	"testing"
-	"time"
-
 	assert "github.com/stretchr/testify/require"
 	"go.skia.org/infra/go/autoroll"
 	"go.skia.org/infra/go/exec"
 	"go.skia.org/infra/go/git"
 	git_testutils "go.skia.org/infra/go/git/testutils"
 	"go.skia.org/infra/go/testutils"
+	"io/ioutil"
+	"path"
+	"strings"
+	"testing"
+	"time"
 )
 
 const (
@@ -33,7 +32,6 @@ var (
 func setup(t *testing.T) (string, *git_testutils.GitBuilder, []string, *git_testutils.GitBuilder, func()) {
 	wd, err := ioutil.TempDir("", "")
 	assert.NoError(t, err)
-
 	// Create child and parent repos.
 	child := git_testutils.GitInit(t)
 	f := "somefile.txt"
@@ -41,18 +39,15 @@ func setup(t *testing.T) (string, *git_testutils.GitBuilder, []string, *git_test
 	for i := 0; i < numChildCommits; i++ {
 		childCommits = append(childCommits, child.CommitGen(f))
 	}
-
 	parent := git_testutils.GitInit(t)
 	parent.Add("DEPS", fmt.Sprintf(`deps = {
   "%s": "%s@%s",
 }`, childPath, child.RepoUrl(), childCommits[0]))
 	parent.Commit()
-
 	mockRun := exec.CommandCollector{}
 	mockRun.SetDelegateRun(func(cmd *exec.Command) error {
 		if strings.Contains(cmd.Name, "depot-tools-auth") {
 			auth := fmt.Sprintf(`Logged in to %s as %s.
-
 To login with a different email run:
   depot-tools-auth login https://codereview.chromium.org
 To logout and purge the authentication token run:
@@ -78,41 +73,34 @@ To logout and purge the authentication token run:
 		return exec.DefaultRun(cmd)
 	})
 	exec.SetRunForTesting(mockRun.Run)
-
 	cleanup := func() {
 		exec.SetRunForTesting(exec.DefaultRun)
 		testutils.RemoveAll(t, wd)
 		child.Cleanup()
 		parent.Cleanup()
 	}
-
 	return wd, child, childCommits, parent, cleanup
 }
 
 // TestRepoManager tests all aspects of the RepoManager except for CreateNewRoll.
 func TestRepoManager(t *testing.T) {
 	testutils.LargeTest(t)
-
 	wd, child, childCommits, parent, cleanup := setup(t)
 	defer cleanup()
-
-	rm, err := NewDefaultRepoManager(wd, parent.RepoUrl(), childPath, 24*time.Hour, depotTools)
+	rm, err := NewDEPSRepoManager(wd, parent.RepoUrl(), childPath, 24*time.Hour, depotTools, nil)
 	assert.NoError(t, err)
 	assert.Equal(t, childCommits[0], rm.LastRollRev())
 	assert.Equal(t, childCommits[len(childCommits)-1], rm.ChildHead())
-
 	// Test FullChildHash.
 	for _, c := range childCommits {
 		h, err := rm.FullChildHash(c[:12])
 		assert.NoError(t, err)
 		assert.Equal(t, c, h)
 	}
-
 	// Test update.
 	lastCommit := child.CommitGen("abc.txt")
 	assert.NoError(t, rm.ForceUpdate())
 	assert.Equal(t, lastCommit, rm.ChildHead())
-
 	// RolledPast.
 	rp, err := rm.RolledPast(childCommits[0])
 	assert.NoError(t, err)
@@ -122,25 +110,20 @@ func TestRepoManager(t *testing.T) {
 		assert.NoError(t, err)
 		assert.False(t, rp)
 	}
-
 	// User, name only.
 	assert.Equal(t, strings.Split(mockUser, "@")[0], rm.User())
 }
-
 func testCreateNewRoll(t *testing.T, strategy string, expectIdx int) {
 	testutils.LargeTest(t)
-
 	wd, child, childCommits, parent, cleanup := setup(t)
 	defer cleanup()
-
-	rm, err := NewDefaultRepoManager(wd, parent.RepoUrl(), childPath, 24*time.Hour, depotTools)
+	rm, err := NewDEPSRepoManager(wd, parent.RepoUrl(), childPath, 24*time.Hour, depotTools, nil)
 	assert.NoError(t, err)
-
 	// Create a roll, assert that it's at tip of tree.
 	issue, err := rm.CreateNewRoll(strategy, emails, cqExtraTrybots, false, true)
 	assert.NoError(t, err)
 	assert.Equal(t, issueNum, issue)
-	msg, err := ioutil.ReadFile(path.Join(rm.(*repoManager).parentDir, ".git", "COMMIT_EDITMSG"))
+	msg, err := ioutil.ReadFile(path.Join(rm.parentDir, ".git", "COMMIT_EDITMSG"))
 	assert.NoError(t, err)
 	from, to, err := autoroll.RollRev(strings.Split(string(msg), "\n")[0], func(h string) (string, error) {
 		return git.GitDir(child.Dir()).RevParse(h)
