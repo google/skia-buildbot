6d2fff92a6b8ad5a007bc6a4f46c37a65a4ab5a8
Eric Boren <borenet@google.com>
2017-04-19 14:36:53 -0400

    Fix builder_name_schema test
    Bug: skia:
    Change-Id: Ic2ae06dd5788f7837f5d48ccfaefc81c0f27e123
    Reviewed-on: https://skia-review.googlesource.com/13866
    Reviewed-by: Eric Boren <borenet@google.com>
    Commit-Queue: Eric Boren <borenet@google.com>

--- go/buildbot/builder_name_schema_test.go
+++ go/buildbot/builder_name_schema_test.go
@@ -17,15 +17,6 @@ func TestBuilderNameSchema(t *testing.T) {
 			"compiler":      "GCC",
 			"target_arch":   "x86",
 			"configuration": "Release",
-			"is_trybot":     "false",
-		},
-		"Build-Ubuntu-GCC-x86-Release-Trybot": {
-			"role":          "Build",
-			"os":            "Ubuntu",
-			"compiler":      "GCC",
-			"target_arch":   "x86",
-			"configuration": "Release",
-			"is_trybot":     "true",
 		},
 		"Build-Ubuntu-GCC-x86-Debug-Android": {
 			"role":          "Build",
@@ -33,7 +24,6 @@ func TestBuilderNameSchema(t *testing.T) {
 			"compiler":      "GCC",
 			"target_arch":   "x86",
 			"configuration": "Debug",
-			"is_trybot":     "false",
 			"extra_config":  "Android",
 		},
 		"Test-Ubuntu-GCC-GCE-CPU-AVX2-x86_64-Debug-CT_DM_1m_SKPs": {
@@ -46,7 +36,6 @@ func TestBuilderNameSchema(t *testing.T) {
 			"arch":             "x86_64",
 			"configuration":    "Debug",
 			"extra_config":     "CT_DM_1m_SKPs",
-			"is_trybot":        "false",
 		},
 	}
 	for builderName, params := range tc {
