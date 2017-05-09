2cce92190166d889238a3cc94b0bddecb91262ac
Stephan Altmueller <stephana@google.com>
2017-05-09 18:04:49 -0400

    Remote Powercycle Devices Via Servos
    - Adds a simple Arduino program that powercycles a devices 
      by turning a servo a set amount of time. 
    - Adds serial_relay that sends reset requests to the 
      Arduino board via USB/serial 
    - Extends powercycle to configure devices that can be 
      rebooted via the Aduino board and triggers 
      serial_relay over SSH. 
    
    This implements a proof of concept. Once proven in practice 
    we will simplify the hardware setup potentially with an 
    Arduino Yun. 
    
    Bug: skia:
    Change-Id: I49ad27e49facf1c0625a468da3044b20aa89c97e

---
+++ skolo/go/serial_relay/main.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"io"
+	"strconv"
+	"sync"
+	"time"
+
+	"github.com/huin/goserial"
+
+	"go.skia.org/infra/go/common"
+	"go.skia.org/infra/go/sklog"
+	"go.skia.org/infra/go/util"
+)
+
+// Command is an alias for a string command.
+type Command string
+
+const (
+	// command understood by the Arduino to calibrate a servo.
+	CMD_CALIBRATE Command = "calibrate"
+
+	// command understood by the Arduino to reset a specific port.
+	CMD_RESET Command = "reset"
+
+	// duration how long the arduino pushes the power button.
+	RESET_DELAY = 12 * time.Second
+
+	// serial-over-USB device.
+	SERIAL_DEVICE = "/dev/ttyACM0"
+
+	// baud rate expected to by the Arduino board.
+	BAUD_RATE = 9600
+)
+
+type ArduinoClient struct {
+	conf  *goserial.Config
+	rwc   io.ReadWriteCloser
+	buf   []byte
+	mutex sync.RWMutex
+}
+
+func NewArduinoClient(devName string, baud int) (*ArduinoClient, error) {
+	ret := &ArduinoClient{
+		conf: &goserial.Config{Name: devName, Baud: baud},
+	}
+	if err := ret.reopen(); err != nil {
+		return nil, err
+	}
+	return ret, nil
+}
+
+func (a *ArduinoClient) reopen() error {
+	if a.rwc != nil {
+		util.Close(a.rwc)
+	}
+	var err error
+	if a.rwc, err = goserial.OpenPort(a.conf); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (a *ArduinoClient) retry(nTimes int, fn func() error) error {
+	var err error
+	for i := 0; i < nTimes; i++ {
+		if err = fn(); (err == nil) || (err != io.EOF) {
+			return err
+		}
+
+		if err = a.reopen(); err != nil {
+			return err
+		}
+	}
+	return err
+}
+
+func (a *ArduinoClient) Send(cmd Command, port int) error {
+	a.mutex.Lock()
+	err := a.retry(3, func() error {
+		_, err := a.rwc.Write([]byte(fmt.Sprintf("%s %d", cmd, port)))
+		return err
+	})
+	a.mutex.Unlock()
+
+	if err != nil {
+		return err
+	}
+	time.Sleep(RESET_DELAY)
+	return err
+}
+
+func (a *ArduinoClient) Close() error {
+	return a.rwc.Close()
+}
+
+func main() {
+	common.Init()
+	ports := flag.Args()
+
+	client, err := NewArduinoClient(SERIAL_DEVICE, BAUD_RATE)
+	if err != nil {
+		sklog.Fatalf("Error: %s", err)
+	}
+	defer client.Close()
+
+	if err := client.Send(CMD_CALIBRATE, 1); err != nil {
+		sklog.Fatalf("Error callibrating: %s", err)
+	}
+
+	for _, portStr := range ports {
+		port, err := strconv.ParseInt(portStr, 10, 64)
+		if err != nil {
+			sklog.Fatalf("Wrong port %s. Needs to be an integer. ", portStr)
+		}
+
+		if err := client.Send(CMD_RESET, int(port)); err != nil {
+			sklog.Fatalf("Error writing: %s", err)
+		}
+	}
+}
