ab3b47a1daeb5a2f477cc6495b443a77a0e94f34
Joe Gregorio <jcgregorio@google.com>
2017-05-11 10:08:12 -0400

    [fiddlecli] Speed up by parallelizing the requests.
    BUG=skia:
    
    Change-Id: I5330f7d665d247aef1f1efc053ef8db204f497c3

--- fiddle/go/fiddlecli/main.go
+++ fiddle/go/fiddlecli/main.go
@@ -11,6 +11,9 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"sync"
+
+	"golang.org/x/sync/errgroup"
 
 	"go.skia.org/infra/fiddle/go/types"
 	"go.skia.org/infra/go/httputils"
@@ -21,9 +24,16 @@ var (
 	domain = flag.String("domain", "https://fiddle.skia.org", "Where to send the JSON request.")
 	input  = flag.String("input", "", "The name of the file to read the JSON from.")
 	output = flag.String("output", "", "The name of the file to write the JSON results to.")
+	procs  = flag.Int("procs", 4, "The number of parallel requests to make to the fiddle server.")
 	quiet  = flag.Bool("quiet", false, "Run without a progress bar.")
 	force  = flag.Bool("force", false, "Force a compile and run for each fiddle, don't take the fast path.")
 )
+
+// chanRequest is sent to each worker in the pool.
+type chanRequest struct {
+	id  string
+	req *types.FiddleContext
+}
 
 func main() {
 	// Check flags.
@@ -47,40 +57,67 @@ func main() {
 		log.Fatalf("%s does not contain valid JSON: %s", *input, err)
 	}
 
-	// Loop over each entry and run it.
-	c := httputils.NewTimeoutClient()
-	response := types.BulkResponse{}
-	for id, req := range requests {
-		if !*quiet {
-			fmt.Print(".")
-		}
-		if *force {
-			req.Fast = false
-		}
-		// POST to fiddle.
-		b, err = json.Marshal(req)
-		if err != nil {
-			log.Fatalf("Failed to encode an individual request: %s", err)
-		}
-		resp, err := c.Post(*domain+"/_/run", "application/json", bytes.NewReader(b))
-		if err != nil {
-			log.Fatalf("Failed to make request: %s", err)
-		}
+	g := errgroup.Group{}
+	requestsCh := make(chan chanRequest, len(requests))
 
-		// Decode response and add to all responses.
-		runResults := types.RunResults{}
-		if err := json.NewDecoder(resp.Body).Decode(&runResults); err != nil {
-			log.Fatalf("Failed to read response: %s", err)
-		}
-		if resp.StatusCode != 200 {
-			log.Fatalf("Request failed with %d: %s", resp.StatusCode, string(b))
-		}
-		response[id] = &runResults
+	// mutex protects response.
+	mutex := sync.Mutex{}
+	response := types.BulkResponse{}
+
+	// Spin up workers.
+	for i := 0; i < *procs; i++ {
+		g.Go(func() error {
+			c := httputils.NewTimeoutClient()
+			for req := range requestsCh {
+				if !*quiet {
+					fmt.Print(".")
+				}
+				if *force {
+					req.req.Fast = false
+				}
+				// POST to fiddle.
+				b, err = json.Marshal(req.req)
+				if err != nil {
+					return fmt.Errorf("Failed to encode an individual request: %s", err)
+				}
+				resp, err := c.Post(*domain+"/_/run", "application/json", bytes.NewReader(b))
+				if err != nil {
+					return fmt.Errorf("Failed to make request: %s", err)
+				}
+
+				// Decode response and add to all responses.
+				runResults := types.RunResults{}
+				if err := json.NewDecoder(resp.Body).Decode(&runResults); err != nil {
+					return fmt.Errorf("Failed to read response: %s", err)
+				}
+				if resp.StatusCode != 200 {
+					return fmt.Errorf("Request failed with %d: %s", resp.StatusCode, string(b))
+				}
+				mutex.Lock()
+				response[req.id] = &runResults
+				mutex.Unlock()
+			}
+			return nil
+		})
 	}
+
+	// Loop over each entry and queue them up for the workers.
+	for id, req := range requests {
+		requestsCh <- chanRequest{
+			id:  id,
+			req: req,
+		}
+	}
+	close(requestsCh)
+
+	// Wait for the workers to finish.
+	if err := g.Wait(); err != nil {
+		log.Fatalf("Failed to complete all requests: %s", err)
+	}
+
 	if !*quiet {
 		fmt.Print("\n")
 	}
-
 	b, err = json.MarshalIndent(response, "", "  ")
 	if err != nil {
 		log.Fatalf("Failed to encode response file: %s", err)
