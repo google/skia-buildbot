f1259c7817232cf382c95365371eb052b9240059
Kevin Lubick <kjlubick@google.com>
2017-05-23 11:33:42 -0400

    Add simple go client for promalerts
    Add stub api client and test
    
    
    Add stub for alerts client
    
    Bug: skia:
    Change-Id: If28eca29d1989f9e7e0acfc10dc52886edd7c9b4

---
+++ go/promalertsclient/client.go
@@ -0,0 +1,68 @@
+package promalertsclient
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"go.skia.org/infra/go/util"
+
+	"github.com/prometheus/alertmanager/dispatch"
+)
+
+const (
+	API_BASE_PATH_PATTERN = "https://%s/api/v1/alerts"
+	AUTH_SCOPE            = "https://www.googleapis.com/auth/userinfo.email"
+)
+
+type ApiClient interface {
+	GetAlerts(group string) ([]dispatch.APIAlert, error)
+}
+
+type apiclient struct {
+	hc       HTTPClient
+	basePath string
+}
+
+type HTTPClient interface {
+	Get(url string) (*http.Response, error)
+}
+
+func New(hc HTTPClient, server string) ApiClient {
+	path := fmt.Sprintf(API_BASE_PATH_PATTERN, server)
+	return &apiclient{
+		hc:       hc,
+		basePath: path,
+	}
+}
+
+type alertsResponse struct {
+	Status string              `json:"status"`
+	Data   []dispatch.APIAlert `json:"data"`
+}
+
+func (a *apiclient) GetAlerts(group string) ([]dispatch.APIAlert, error) {
+	r, err := a.hc.Get(a.basePath)
+	if err != nil {
+		return nil, err
+	}
+	if r.StatusCode != 200 {
+		return nil, fmt.Errorf("HTTP error %s", r.Status)
+	}
+	defer util.Close(r.Body)
+	d := json.NewDecoder(r.Body)
+	var alerts alertsResponse
+	if err := d.Decode(&alerts); err != nil {
+		return nil, fmt.Errorf("Could not parse JSON: %s", err)
+	}
+
+	retVal := []dispatch.APIAlert{}
+	for _, a := range alerts.Data {
+		alertName := string(a.Labels["alertname"])
+		if alertName == group {
+			retVal = append(retVal, a)
+		}
+	}
+
+	return retVal, nil
+}
