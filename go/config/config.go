b3fd641db6ef1c5ebb6e483bbe733ae2e48b1478
Ben Wagner <benjaminwagner@google.com>
2017-05-26 14:45:25 -0400

    Add Skolo Win unattend templates and gen-unattend script.
    Change-Id: Ic6d8ad72eb988297baf6fbb7ad1a8d009ed1ddd9

--- go/config/config.go
+++ go/config/config.go
@@ -1,7 +1,11 @@
 package config
 
 import (
+	"io/ioutil"
 	"time"
+
+	"github.com/flynn/json5"
+	"go.skia.org/infra/go/sklog"
 )
 
 // tomlDuration is a simple struct wrapper to allow us to parse strings as durations
@@ -24,4 +28,14 @@ type Common struct {
 	TileDir               string // Path where tiles will be placed.
 	OAuthCacheFile        string // Path to the file where to cache cache the oauth credentials.
 	OAuthClientSecretFile string // Path to the file with the oauth client secret.
+}
+
+// MustParseConfigFile reads path as JSON5 into out. If an error occurs, logs a fatal error
+// referencing the given flagName.
+func MustParseConfigFile(path, flagName string, out interface{}) {
+	if data, err := ioutil.ReadFile(path); err != nil {
+		sklog.Fatalf("Unable to read %s file %q: %s", flagName, path, err)
+	} else if err := json5.Unmarshal(data, out); err != nil {
+		sklog.Fatalf("Unable to parse %s file %q: %s", flagName, path, err)
+	}
 }
