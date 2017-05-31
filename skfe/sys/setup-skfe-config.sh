1824c6b192683cd4830c9c1fb3988561c838247e
Stephan Altmueller <stephana@google.com>
2017-05-31 16:16:54 -0400

    Fix skfe to be installable
    NOTRY=True
    TBR=kjlubick
    Bug: skia:
    Change-Id: I2748d0ea56df061cb5cc8ef1cfa21d10923ea52a

--- skfe/sys/setup-skfe-config.sh
+++ skfe/sys/setup-skfe-config.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
-set -e
 set -x
 
 # Remove the default host and install the certs then restart nginx.
@@ -8,4 +7,5 @@ set -x
 # previous configuation, which makes it fault silent but available.
 rm -f /etc/nginx/sites-enabled/default
 /usr/local/bin/certpoller --logtostderr skia-org-pem skia-org-key dh-params-pem
+systemctl daemon-reload
 systemctl restart nginx
