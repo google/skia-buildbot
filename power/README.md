da8e5aef3be6a5e46f06eb36096cff4ee046a6f4
Kevin Lubick <kjlubick@google.com>
2017-06-02 11:07:52 -0400

    [power] Finish up power-controller server code
    Also fix some minor UI changes.
    
    Bug: skia:
    Change-Id: I5dc4c67de30abbaf230f2a6649dda6acf1b019ca

--- power/README.md
+++ power/README.md
@@ -5,3 +5,15 @@ The purpose of this module is to expose a web UI for powercycling various machin
 the Skia fleet.
 
 https://docs.google.com/document/d/15sb7RN_S3ctw06xQoNG7c3Owu-DS2jPPDaWAdQIkPLw/edit#
+
+
+Local Testing
+-------------
+
+The easiest way to test locally is to set up an SSH tunnel to skia-prom
+
+    gcloud compute ssh --zone us-central1-c --project google.com:skia-buildbots skia-prom -- -v -L 8001:skia-prom:8001
+
+Then, start the server,
+
+    go run ./go/power-controller/main.go --logtostderr --alerts_endpoint localhost:8001 --local --powercycle_config ../skolo/sys/powercycle.yaml
--- power/go/power-controller/main.go
+++ power/go/power-controller/main.go
@@ -1,17 +1,27 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
+	"fmt"
 	"html/template"
 	"net/http"
+	"path"
 	"path/filepath"
+	"time"
 
 	"github.com/gorilla/mux"
 
+	"go.skia.org/infra/go/auth"
 	"go.skia.org/infra/go/common"
 	"go.skia.org/infra/go/httputils"
 	"go.skia.org/infra/go/login"
+	"go.skia.org/infra/go/promalertsclient"
+	"go.skia.org/infra/go/skiaversion"
 	"go.skia.org/infra/go/sklog"
+	skswarming "go.skia.org/infra/go/swarming"
+	"go.skia.org/infra/power/go/decider"
+	"go.skia.org/infra/power/go/gatherer"
 )
 
 const (
@@ -22,15 +32,25 @@ const (
 var (
 	// indexTemplate is the main index.html page we serve.
 	indexTemplate *template.Template = nil
+
+	downBots gatherer.Gatherer = nil
 )
 
 var (
 	// web server params
-	host         = flag.String("host", "localhost", "HTTP service host")
-	port         = flag.String("port", ":8001", "HTTP service port (e.g., ':8002')")
-	local        = flag.Bool("local", false, "Running locally if true. As opposed to in production.")
-	resourcesDir = flag.String("resources_dir", "", "The directory to find templates, JS, and CSS files. If blank the current directory will be used.")
-	promPort     = flag.String("prom_port", ":20000", "Metrics service address (e.g., ':10110')")
+	host           = flag.String("host", "localhost", "HTTP service host")
+	port           = flag.String("port", ":8080", "HTTP service port (e.g., ':8002')")
+	local          = flag.Bool("local", false, "Running locally if true. As opposed to in production.")
+	resourcesDir   = flag.String("resources_dir", "", "The directory to find templates, JS, and CSS files. If blank the current directory will be used.")
+	promPort       = flag.String("prom_port", ":20000", "Metrics service address (e.g., ':10110')")
+	alertsEndpoint = flag.String("alerts_endpoint", "skia-prom:8001", "The Prometheus GCE name and port")
+
+	// OAUTH params
+	authWhiteList = flag.String("auth_whitelist", login.DEFAULT_DOMAIN_WHITELIST, "White space separated list of domains and email addresses that are allowed to login.")
+	redirectURL   = flag.String("redirect_url", "https://power.skia.org/oauth2callback/", "OAuth2 redirect url. Only used when local=false.")
+
+	powercycleConfig = flag.String("powercycle_config", "/etc/powercycle.yaml", "YAML file with powercycle bot/device configuration. Same as used for powercycle.")
+	updatePeriod     = flag.Duration("update_period", time.Minute, "How often to update the list of down bots.")
 )
 
 func main() {
@@ -39,12 +59,41 @@ func main() {
 	common.InitWithMust(
 		"power-controller",
 		common.PrometheusOpt(promPort),
-		//common.CloudLoggingOpt(),
+		common.CloudLoggingOpt(),
 	)
 
 	loadTemplates()
 
-	runServer()
+	authedClient, err := setupAuthentication()
+	if err != nil {
+		sklog.Fatalf("Could not setup authentication: %s", err)
+	}
+
+	es, err := skswarming.NewApiClient(authedClient, "chromium-swarm.appspot.com")
+	if err != nil {
+		sklog.Fatalln(err)
+	}
+	is, err := skswarming.NewApiClient(authedClient, "chrome-swarming.appspot.com")
+	if err != nil {
+		sklog.Fatalln(err)
+	}
+	ac := promalertsclient.New(&http.Client{}, *alertsEndpoint)
+	d, err := decider.New(*powercycleConfig)
+	if err != nil {
+		sklog.Fatalln(err)
+	}
+
+	downBots = gatherer.New(es, is, ac, d)
+
+	go func() {
+		downBots.Update()
+		for {
+			<-time.Tick(*updatePeriod)
+			downBots.Update()
+		}
+	}()
+
+	runServer() // runs forever
 }
 
 func loadTemplates() {
@@ -67,6 +116,41 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+type downBotsResponse struct {
+	List []gatherer.DownBot `json:"list"`
+}
+
+func downBotsHandler(w http.ResponseWriter, r *http.Request) {
+	if downBots == nil {
+		http.Error(w, "The power-controller isn't finished booting up.  Try again later", http.StatusServiceUnavailable)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+
+	response := downBotsResponse{List: downBots.CachedDownBots()}
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		sklog.Errorf("Failed to write or encode output: %s", err)
+		return
+	}
+}
+
+func setupAuthentication() (*http.Client, error) {
+	oauthCacheFile := path.Join(*resourcesDir, "google_storage_token.data")
+	authedClient, err := auth.NewClient(*local, oauthCacheFile, skswarming.AUTH_SCOPE)
+	if err != nil {
+		return nil, fmt.Errorf("Could not set up autheticated HTTP client: %s", err)
+	}
+
+	useRedirectURL := fmt.Sprintf("http://localhost%s/oauth2callback/", *port)
+	if !*local {
+		useRedirectURL = *redirectURL
+	}
+	if err := login.Init(useRedirectURL, *authWhiteList); err != nil {
+		return nil, fmt.Errorf("Problem setting up server OAuth: %s", err)
+	}
+	return authedClient, nil
+}
+
 func runServer() {
 	serverURL := "https://" + *host
 	if *local {
@@ -78,6 +162,10 @@ func runServer() {
 
 	r.HandleFunc(OAUTH2_CALLBACK_PATH, login.OAuth2CallbackHandler)
 	r.HandleFunc("/", indexHandler)
+	r.HandleFunc("/loginstatus/", login.StatusHandler)
+	r.HandleFunc("/logout/", login.LogoutHandler)
+	r.HandleFunc("/json/version", skiaversion.JsonHandler)
+	r.HandleFunc("/down_bots", downBotsHandler)
 
 	rootHandler := httputils.LoggingGzipRequestResponse(r)
 
