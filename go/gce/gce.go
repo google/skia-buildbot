fa796c7bad9e58d94aefa3be9634cec21fd03817
Eric Boren <borenet@google.com>
2017-06-08 10:45:12 -0400

    Port docs VM creation scripts to Go
    Bug: skia:6612
    Change-Id: Iec7301445b8e65bfe852f5fe3c248a5aafad93a7

--- go/gce/gce.go
+++ go/gce/gce.go
@@ -35,6 +35,7 @@ const (
 	MACHINE_TYPE_HIGHMEM_32  = "n1-highmem-32"
 	MACHINE_TYPE_STANDARD_1  = "n1-standard-1"
 	MACHINE_TYPE_STANDARD_2  = "n1-standard-2"
+	MACHINE_TYPE_STANDARD_4  = "n1-standard-4"
 	MACHINE_TYPE_STANDARD_8  = "n1-standard-8"
 	MACHINE_TYPE_STANDARD_16 = "n1-standard-16"
 	MACHINE_TYPE_STANDARD_32 = "n1-standard-32"
