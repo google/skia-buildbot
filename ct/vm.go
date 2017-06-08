9bf0ccb0e047d3c634e866c90ee9edb3fc9b8896
Eric Boren <borenet@google.com>
2017-06-08 10:42:35 -0400

    Add ct_master setup to ct/vm.go
    Bug: skia:6612
    Change-Id: I490ec49a555bc684b283b0d1026d2f28a7bd4955

--- ct/vm.go
+++ ct/vm.go
@@ -1,26 +1,45 @@
 package main
 
 import (
+	"path"
+	"runtime"
+
+	"go.skia.org/infra/go/auth"
 	"go.skia.org/infra/go/gce"
 	"go.skia.org/infra/go/gce/server"
 )
 
 func CtBase(name, ipAddress string) *gce.Instance {
 	vm := server.Server20170518(name)
-	vm.DataDisk = nil
 	vm.ExternalIpAddress = ipAddress
-	vm.MachineType = gce.MACHINE_TYPE_STANDARD_2
 	vm.Metadata["owner_primary"] = "rmistry"
 	vm.Metadata["owner_secondary"] = "benjaminwagner"
 	return vm
 }
 
-func Prod() *gce.Instance {
-	return CtBase("skia-ctfe", "104.154.112.110")
+func CTFE() *gce.Instance {
+	vm := CtBase("skia-ctfe", "104.154.112.110")
+	vm.DataDisk = nil
+	vm.MachineType = gce.MACHINE_TYPE_STANDARD_2
+	return vm
+}
+
+func CtMaster() *gce.Instance {
+	vm := CtBase("skia-ct-master", "104.154.112.17")
+	vm.DataDisk.SizeGb = 500
+	vm.DataDisk.Type = gce.DISK_TYPE_PERSISTENT_STANDARD
+	vm.MachineType = gce.MACHINE_TYPE_HIGHMEM_16
+	vm.Scopes = append(vm.Scopes, auth.SCOPE_GERRIT)
+
+	_, filename, _, _ := runtime.Caller(0)
+	dir := path.Dir(filename)
+	vm.SetupScript = path.Join(dir, "setup-script-master.sh")
+	return vm
 }
 
 func main() {
 	server.Main(map[string]*gce.Instance{
-		"prod": Prod(),
+		"ctfe":   CTFE(),
+		"master": CtMaster(),
 	})
 }
