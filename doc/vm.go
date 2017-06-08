d9d28a10cee9b4f2c14d0531ac245e52dff6db80
Eric Boren <borenet@google.com>
2017-06-08 10:32:26 -0400

    Port docs VM creation scripts to Go
    Bug: skia:6612
    Change-Id: Iec7301445b8e65bfe852f5fe3c248a5aafad93a7

---
+++ doc/vm.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+
+	"go.skia.org/infra/go/gce"
+	"go.skia.org/infra/go/gce/server"
+)
+
+func DocBase(name, ipAddress string) *gce.Instance {
+	vm := server.Server20170518(name) // TODO(borenet): Needs git configs.
+	vm.DataDisk.Name = fmt.Sprintf("%s-data", name)
+	vm.DataDisk.SizeGb = 1000
+	vm.DataDisk.Type = gce.DISK_TYPE_PERSISTENT_STANDARD
+	vm.ExternalIpAddress = ipAddress
+	vm.MachineType = gce.MACHINE_TYPE_STANDARD_4
+	vm.Metadata["owner_primary"] = "jcgregorio"
+	return vm
+}
+
+func Prod() *gce.Instance {
+	return DocBase("skia-docs", "104.154.112.101")
+}
+
+func main() {
+	server.Main(map[string]*gce.Instance{
+		"prod": Prod(),
+	})
+}
