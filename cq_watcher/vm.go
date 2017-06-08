8fcbea2a1d71430749fe7fb3dd407dcba8503d3f
Eric Boren <borenet@google.com>
2017-06-08 10:27:04 -0400

    Port cq_watcher VM creation scripts to Go
    Bug: skia:6612
    Change-Id: If0089a2226417d58f7808b7ffee184b3662d5e7a
    Reviewed-on: https://skia-review.googlesource.com/19043
    Commit-Queue: Eric Boren <borenet@google.com>
    Reviewed-by: Ravi Mistry <rmistry@google.com>

---
+++ cq_watcher/vm.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"go.skia.org/infra/go/gce"
+	"go.skia.org/infra/go/gce/server"
+)
+
+func CqWatcherBase(name, ipAddress string) *gce.Instance {
+	vm := server.Server20170518(name)
+	vm.DataDisk = nil
+	vm.ExternalIpAddress = ipAddress
+	vm.MachineType = gce.MACHINE_TYPE_HIGHMEM_2
+	vm.Metadata["owner_primary"] = "rmistry"
+	vm.Metadata["owner_secondary"] = "borenet"
+	return vm
+}
+
+func Prod() *gce.Instance {
+	return CqWatcherBase("skia-cq-watcher", "104.154.112.115")
+}
+
+func main() {
+	server.Main(map[string]*gce.Instance{
+		"prod": Prod(),
+	})
+}
