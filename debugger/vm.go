013e30d2dbede28378ea18ec0fc27ac1f466c454
Eric Boren <borenet@google.com>
2017-06-08 12:38:21 -0400

    Port debugger VM creation scripts to Go
    Bug: skia:6612
    Change-Id: I5c1d45e5dcd220e6d128a2b7147585ebbaa2c1ef

---
+++ debugger/vm.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"path"
+	"runtime"
+
+	"go.skia.org/infra/go/gce"
+	"go.skia.org/infra/go/gce/server"
+)
+
+func DebuggerBase(name, ipAddress string) *gce.Instance {
+	vm := server.Server20170518(name) // TODO(borenet): Needs git configs.
+	vm.DataDisk.Name = fmt.Sprintf("%s-data", name)
+	vm.DataDisk.SizeGb = 1000
+	vm.DataDisk.Type = gce.DISK_TYPE_PERSISTENT_STANDARD
+	vm.ExternalIpAddress = ipAddress
+	vm.MachineType = gce.MACHINE_TYPE_HIGHMEM_32
+	vm.Metadata["owner_primary"] = "jcgregorio"
+
+	_, filename, _, _ := runtime.Caller(0)
+	dir := path.Dir(filename)
+	vm.SetupScript = path.Join(dir, "setup-script.sh")
+	return vm
+}
+
+func Prod() *gce.Instance {
+	return DebuggerBase("skia-debugger", "104.154.112.116")
+}
+
+func main() {
+	server.Main(map[string]*gce.Instance{
+		"prod": Prod(),
+	})
+}
