cedb4e552ebdd022c2be324defa0c426d319eb52
Eric Boren <borenet@google.com>
2017-06-12 11:48:43 -0400

    GCE setup scripts can now run "apt-get update"
    Bug: skia:6612
    Change-Id: I71b5e01acc9f3319dbba40c22202aa3e3fd9452c

--- fiddle/vm.go
+++ fiddle/vm.go
@@ -2,14 +2,11 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"path"
 	"runtime"
 
 	"go.skia.org/infra/go/gce"
 	"go.skia.org/infra/go/gce/server"
-	"go.skia.org/infra/go/metadata"
-	"go.skia.org/infra/go/sklog"
 )
 
 const (
@@ -27,16 +24,9 @@ func FiddleBase(name string) *gce.Instance {
 	vm.Metadata["owner_primary"] = "jcgregorio"
 	vm.ServiceAccount = "service-account-json@skia-buildbots.google.com.iam.gserviceaccount.com"
 
-	// The setup script has to be run in an interactive terminal. Make sure
-	// it ends up on the machine and we'll ask the user to run it.
 	_, filename, _, _ := runtime.Caller(0)
 	dir := path.Dir(filename)
-	setupContents, err := ioutil.ReadFile(path.Join(dir, "setup-script.sh"))
-	if err != nil {
-		sklog.Fatal(err)
-	}
-	vm.Metadata["setup-script"] = string(setupContents)
-	vm.MetadataDownloads[SETUP_SCRIPT] = fmt.Sprintf(metadata.METADATA_URL, "instance", "setup-script")
+	vm.SetupScript = path.Join(dir, "setup-script.sh")
 	return vm
 }
 
@@ -48,5 +38,4 @@ func main() {
 	server.Main(gce.ZONE_GPU, map[string]*gce.Instance{
 		"prod": Prod(),
 	})
-	sklog.Warningf("Instance created successfully. Please log in and run:\n$ %s", SETUP_SCRIPT)
 }
