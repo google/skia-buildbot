545ab0b5cd0c4b9508cc9c1166d9d688409ac710
Eric Boren <borenet@google.com>
2017-06-12 12:27:47 -0400

    Port SKFE VM creation scripts to Go
    Bug: skia:6612
    Change-Id: I5ed7da94d955140a0161aa77cce4299f45fed811

---
+++ skfe/vm.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+
+	"go.skia.org/infra/go/gce"
+	"go.skia.org/infra/go/gce/server"
+)
+
+func SKFEBase(name, ipAddress string) *gce.Instance {
+	vm := server.Server20170518(name)
+	vm.DataDisk = nil
+	vm.ExternalIpAddress = ipAddress
+	vm.MachineType = gce.MACHINE_TYPE_STANDARD_4
+	vm.Metadata["owner_primary"] = "stephana"
+	vm.Metadata["owner_secondary"] = "jcgregorio"
+	return vm
+}
+
+func Prod(num int, ip string) *gce.Instance {
+	return SKFEBase(fmt.Sprintf("skia-skfe-%d", num), ip)
+}
+
+func main() {
+	server.Main(gce.ZONE_DEFAULT, map[string]*gce.Instance{
+		"prod-1": Prod(1, "104.154.112.11"),
+		"prod-2": Prod(2, "104.154.112.103"),
+	})
+}
