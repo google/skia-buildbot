20beb2be141a4f5bb65d6f74b98b81dcfcdc4151
Eric Boren <borenet@google.com>
2017-06-12 11:48:43 -0400

    GCE setup scripts can now run "apt-get update"
    Bug: skia:6612
    Change-Id: I71b5e01acc9f3319dbba40c22202aa3e3fd9452c
    Reviewed-on: https://skia-review.googlesource.com/19487
    Reviewed-by: Kevin Lubick <kjlubick@google.com>
    Commit-Queue: Eric Boren <borenet@google.com>

--- debugger/setup-script.sh
+++ debugger/setup-script.sh
@@ -2,8 +2,6 @@
 set -e
 set -x
 
-/tmp/format_and_mount.sh skia-debugger
-
 # The same set of packages need to be installed both on the instance and within the container.
 PACKAGES="systemd-container git debootstrap build-essential libosmesa-dev libfreetype6-dev libfontconfig-dev libpng12-dev libgif-dev libqt4-dev mesa-common-dev"
 sudo apt-get update
