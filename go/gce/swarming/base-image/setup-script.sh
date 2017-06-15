dea1cb8d2c73ca18f201124f41ad7339f0c56c6b
Eric Boren <borenet@google.com>
2017-06-15 13:37:21 -0400

    Port Swarming disk image creation script to Go
    Bug: skia:6612
    Change-Id: I5f5e4364bda5c558f66545edb37b5abe256bfac5

---
+++ go/gce/swarming/base-image/setup-script.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+#
+# Setup the Swarming base image on Skia GCE instance.
+#
+# This script is used on a temporary GCE instance. Just run it on a fresh
+# instance and then capture a snapshot of the disk. Any image
+# started with this snapshot as its image should be immediately setup to
+# run as a Swarming bot.
+set -x -e
+export TERM=xterm
+
+echo 'Dpkg::Progress-Fancy "0";' | sudo tee /etc/apt/apt.conf.d/99progressbar
+
+sudo DEBIAN_FRONTEND=noninteractive apt -o quiet=2 --assume-yes -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" update
+sudo DEBIAN_FRONTEND=noninteractive apt -o quiet=2 --assume-yes -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
+
+# Move to testing.
+cat <<EOF | sudo tee /etc/apt/sources.list
+deb http://deb.debian.org/debian/ testing main contrib non-free
+deb-src http://deb.debian.org/debian/ testing main contrib non-free
+deb http://security.debian.org/ testing/updates main contrib non-free
+deb-src http://security.debian.org/ testing/updates main contrib non-free
+EOF
+
+# Yes, we need to run it this many times.
+for i in {1..4}
+do
+  sudo DEBIAN_FRONTEND=noninteractive apt -o quiet=2 --assume-yes -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" update
+  sudo DEBIAN_FRONTEND=noninteractive apt -o quiet=2 --assume-yes -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" full-upgrade
+  sudo DEBIAN_FRONTEND=noninteractive apt -o quiet=2 --assume-yes -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" autoremove
+done
+
+# Now install the apps that we guarantee to appear.
+sudo DEBIAN_FRONTEND=noninteractive apt -o quiet=2 --assume-yes -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install git collectd unattended-upgrades subversion make python-dev libfreetype6-dev xvfb python-twisted-core libpng-dev zlib1g-dev fontconfig libfontconfig-dev libglu-dev poppler-utils netpbm vim gyp g++ gdb unzip linux-tools libgif-dev python-imaging libosmesa-dev linux-tools-3.16
+sudo DEBIAN_FRONTEND=noninteractive apt -o quiet=2 --assume-yes -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" install gcc python-dev python-setuptools
+sudo easy_install -U pip
+sudo pip install setuptools --no-binary :all: --upgrade
+sudo pip install -U crcmod
+
+echo 'PATH="/home/default/depot_tools:/usr/local/sbin:/usr/sbin:/sbin:$PATH"' >> ~/.bashrc
+echo 'alias ll="ls -al"' >> ~/.bashrc
+
+sudo rm -rf /etc/boto.cfg
+
+sudo apt --assume-yes install --fix-broken
+
+# Setup collectd.
+cat <<EOF > collectd.conf
+FQDNLookup false
+Interval 60
+
+LoadPlugin "logfile"
+<Plugin "logfile">
+  LogLevel "info"
+  File "/var/log/collectd.log"
+  Timestamp true
+</Plugin>
+
+LoadPlugin syslog
+
+<Plugin syslog>
+        LogLevel info
+</Plugin>
+
+LoadPlugin battery
+LoadPlugin cpu
+LoadPlugin df
+LoadPlugin disk
+LoadPlugin entropy
+LoadPlugin interface
+LoadPlugin irq
+LoadPlugin load
+LoadPlugin memory
+LoadPlugin processes
+LoadPlugin swap
+LoadPlugin users
+LoadPlugin write_graphite
+
+<Plugin write_graphite>
+        <Carbon>
+                Host "skia-monitoring"
+                Port "2003"
+                Prefix "collectd."
+                StoreRates false
+                AlwaysAppendDS false
+                EscapeCharacter "_"
+                Protocol "tcp"
+        </Carbon>
+</Plugin>
+EOF
+sudo install -D --verbose --backup=none --group=root --owner=root --mode=600 collectd.conf /etc/collectd/collectd.conf
+sudo /etc/init.d/collectd restart
+
+cat <<EOF | sudo tee /etc/apt/apt.conf.d/20auto-upgrades
+APT::Periodic::Update-Package-Lists "1";
+APT::Periodic::Unattended-Upgrade "1";
+EOF
+
+cat <<EOF | sudo tee /etc/apt/apt.conf.d/50unattended-upgrades
+Unattended-Upgrade::Origins-Pattern {
+      "${distro_id}:${distro_codename}-security";
+};
+Unattended-Upgrade::Remove-Unused-Dependencies "true";
+Unattended-Upgrade::Automatic-Reboot "true";
+EOF
+
+# Check the output to confirm that the config is working.
+sudo unattended-upgrades -d
+
