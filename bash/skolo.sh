83fb7d26b1af3df146cfc4f7dae54934dc63ad78
Joe Gregorio <jcgregorio@google.com>
2017-06-26 11:21:54 -0400

    [bash] Add skolo.sh to be sourced for jumphost addresses.
    Bug: skia:
    Change-Id: I742bb6535fd4c8c22036d4ad929b5fbefc51002f

---
+++ bash/skolo.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# Utilities for accessing the skolo.
+# Should be sourced from $HOME/.bashrc
+
+alias skolo_win='ssh chrome-bot@100.115.95.132'
+alias skolo_rpi='ssh chrome-bot@100.115.95.143'
+alias skolo_linux='ssh chrome-bot@100.115.95.130'
