e44fc44e824dec7afd713b247a83fd9b848edbbc
Joe Gregorio <jcgregorio@google.com>
2017-06-29 15:17:13 -0400

    [perf] Start adding stuff for configurable alerts.
    Bug: skia:6801
    Change-Id: If58e6b040c307d04cba058ab5a88bea0b0e18be2

---
+++ perf/go/alerts/alertconfigstate_test.go
@@ -0,0 +1,34 @@
+package alerts
+
+import (
+	"encoding/json"
+	"testing"
+
+	"go.skia.org/infra/go/testutils"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type TestStruct struct {
+	State ConfigState
+}
+
+func TestJSON(t *testing.T) {
+	testutils.SmallTest(t)
+	ts := TestStruct{
+		State: DELETED,
+	}
+	b, err := json.Marshal(ts)
+	assert.NoError(t, err)
+	assert.Equal(t, "{\"State\":\"DELETED\"}", string(b))
+
+	target := &TestStruct{}
+	err = json.Unmarshal(b, target)
+	assert.NoError(t, err)
+	assert.Equal(t, DELETED, target.State)
+
+	target = &TestStruct{}
+	err = json.Unmarshal([]byte("{\"State\":\"NOT A VALID VALUE\"}"), target)
+	assert.NoError(t, err)
+	assert.Equal(t, ACTIVE, target.State)
+}
