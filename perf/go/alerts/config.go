a9c10d6a35b84616d4e8eda4a31b802d2f422e85
Joe Gregorio <jcgregorio@google.com>
2017-06-29 15:17:13 -0400

    [perf] Start adding stuff for configurable alerts.
    Bug: skia:6801
    Change-Id: If58e6b040c307d04cba058ab5a88bea0b0e18be2
    Reviewed-on: https://skia-review.googlesource.com/21102
    Reviewed-by: Joe Gregorio <jcgregorio@google.com>

---
+++ perf/go/alerts/config.go
@@ -0,0 +1,27 @@
+package alerts
+
+import "go.skia.org/infra/perf/go/clustering2"
+
+const (
+	INVALID_ID = -1
+)
+
+// Config represents the configuration for one alert.
+type Config struct {
+	ID             int                     `json:"id"`
+	Query          string                  `json:"query"`            // The query to perform on the trace store to select the traces to alert on.
+	Alert          string                  `json:"alert"`            // Email address or id of a chat room to send alerts to.
+	Interesting    float32                 `json:"interesting"`      // The regression interestingness threshhold.
+	BugURITemplate string                  `json:"bug_uri_template"` // URI Template used for reporting bugs. Format TBD.
+	Algo           clustering2.ClusterAlgo `json:"algo"`             // Which clustering algorithm to use.
+	State          ConfigState             `json:"state"`            // The state of the config.
+}
+
+// NewConfig creates a new Config properly initialized.
+func NewConfig() *Config {
+	return &Config{
+		ID:    INVALID_ID,
+		Algo:  clustering2.KMEANS_ALGO,
+		State: ACTIVE,
+	}
+}
