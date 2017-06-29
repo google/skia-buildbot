eb667ce58734701cfe723240b849518abc3bdb8c
Joe Gregorio <jcgregorio@google.com>
2017-06-29 11:27:19 -0400

    [perf] Start adding stuff for configurable alerts.
    Bug: skia:6801
    Change-Id: If58e6b040c307d04cba058ab5a88bea0b0e18be2

---
+++ perf/go/alerts/db_test.go
@@ -0,0 +1,45 @@
+package alerts
+
+import (
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"go.skia.org/infra/go/testutils"
+	"go.skia.org/infra/perf/go/db"
+	sqlmock "gopkg.in/DATA-DOG/go-sqlmock.v1"
+)
+
+func TestStore(t *testing.T) {
+	testutils.SmallTest(t)
+
+	mdb, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatalf("an error '%s' was not expected when opening a stub database connection", err)
+	}
+
+	a := NewStore()
+	cfg := NewConfig()
+	cfg.Query = "source_type=svg"
+	body, err := json.Marshal(cfg)
+	assert.NoError(t, err)
+
+	//	rows := sqlmock.NewRows([]string{"id", "state", "body"})
+
+	// Set expectations.
+	mock.ExpectBegin()
+	mock.ExpectExec("INSERT INTO alerts").WithArgs(cfg.State, body).WillReturnResult(sqlmock.NewResult(1, 1))
+	mock.ExpectCommit()
+
+	// Put mock db into place.
+	db.DB = mdb
+
+	err = a.Save(cfg)
+	assert.NoError(t, err)
+
+	// Make sure that all expectations were met.
+	if err := mock.ExpectationsWereMet(); err != nil {
+		t.Errorf("there were unfulfilled expections: %s", err)
+	}
+}
