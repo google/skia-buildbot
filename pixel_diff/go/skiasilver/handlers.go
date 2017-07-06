5b0d7f8d8000ee36bc61c16c73ee372ef0d9f6b8
Lawrence Choi <lchoi@google.com>
2017-07-06 11:08:47 -0700

    New image diffing server for Cluster Telemetry.
    Reuses components from Gold but will function as a standalone server.
    
    Bug: skia:6779
    Change-Id: I47248b1996b590027e37fa5f12154f608b4712cb

---
+++ pixel_diff/go/skiasilver/handlers.go
@@ -0,0 +1,213 @@
+package main
+
+import (
+	"sort"
+	"strconv"
+	"strings"
+	"os"
+	"encoding/csv"
+	"net/http"
+	"go.skia.org/infra/golden/go/diff"
+	"go.skia.org/infra/go/sklog"
+)
+
+const (
+	// DEFAULT_PAGE_SIZE is the default page size used for pagination.
+	DEFAULT_PAGE_SIZE = 20
+
+	// MAX_PAGE_SIZE is the maximum page size used for pagination.
+	MAX_PAGE_SIZE = 100
+)
+
+// jsonJobHandler returns the current list of CT Pixel Diff jobs.
+func jsonJobsHandler(w http.ResponseWriter, r *http.Request) {
+	jobs := make([]string, 0)
+	jobs = append(jobs, "rmistry-20170623165155")
+	jobs = append(jobs, "rmistry-20170623184523")
+	sendJsonResponse(w, map[string][]string{"jobs": jobs})
+}
+
+// jsonDiffHandler instantiates the map linking job strings to lists of diff
+// results, and instantiates and fills in the list of diff results for a given job
+func jsonDiffHandler(w http.ResponseWriter, r *http.Request) {
+	if (diffResultsMap == nil) {
+		diffResultsMap = make(map[string][]*DiffResult)
+	}
+
+	job := r.FormValue("job")
+	if (diffResultsMap[job] == nil) {
+		csvfile, err := os.Open("/usr/local/google/home/lchoi/Downloads/top1m.csv")
+		if err != nil {
+			sklog.Error(err)
+			return
+		}
+		defer csvfile.Close()
+
+		reader := csv.NewReader(csvfile)
+		rawcsvdata, err := reader.ReadAll()
+		if err != nil {
+			sklog.Error(err)
+			return
+		}
+
+		for i := 0; i < 1000; i++ {
+			siteUrl := rawcsvdata[i][1]
+			images := &ImageResult{
+				Left: job + "--nopatch--http___www_" + strings.Replace(siteUrl, ".", "_", -1),
+				Right: job + "--withpatch--http___www_" + strings.Replace(siteUrl, ".", "_", -1),
+			}
+			diff, err := diffStore.Get(diff.PRIORITY_NOW, images.Left, []string{images.Right})
+			if err != nil {
+				sklog.Errorf("Failed to calculate diffs: %s", err)
+				return
+			}
+			rank, err := strconv.Atoi(rawcsvdata[i][0])
+			if err != nil {
+				sklog.Errorf("Failed to parse web page rank: %s", err)
+			}
+			diffResult := &DiffResult {
+				Url: siteUrl,
+				Images: images,
+				Diff: diff[images.Right],
+				Rank: rank,
+			}
+			diffResultsMap[job] = append(diffResultsMap[job], diffResult)
+		}
+	}
+}
+
+// DiffResult encapsulates the results of a diff request.
+type DiffResult struct {
+	Url       string `json:"url"`
+	Images    *ImageResult  `json:"images"`
+	Diff      *diff.DiffMetrics  `json:"diffmetrics"`
+	Rank      int  `json:"rank"`
+}
+
+// ImageResult encapsulates nopatch and withpatch images.
+type ImageResult struct {
+	Left   string  `json:"left"`
+	Right  string  `json:"right"`
+}
+
+// jsonLoadHandler parses a start index, end index, and job from the query and
+// uses them to return diff results in the specified range for the specified run.
+func jsonLoadHandler(w http.ResponseWriter, r *http.Request) {
+	job := r.FormValue("job")
+	if diffResultsMap[job] != nil {
+		startIdx, err := strconv.Atoi(r.FormValue("startIdx"))
+		if err != nil {
+			sklog.Errorf("Failed to parse start index: %s", err)
+		}
+		endIdx, err := strconv.Atoi(r.FormValue("endIdx"))
+		if err != nil {
+			sklog.Errorf("Failed to parse end index: %s", err)
+		}
+		sendJsonResponse(w, map[string]interface{}{"data": diffResultsMap[job][startIdx:endIdx]})
+	}
+}
+
+// jsonSortHandler sorts the list of diff results using the specified sort value
+// and job
+func jsonSortHandler(w http.ResponseWriter, r *http.Request) {
+	job := r.FormValue("job")
+	diffResults := diffResultsMap[job]
+	sortVal, err := strconv.Atoi(r.FormValue("sortVal"))
+	if err != nil {
+		sklog.Errorf("Failed to parse sort value: %s", err)
+	}
+	switch sortVal {
+	case 0:
+		sort.Slice(diffResults, func(i, j int) bool {
+			if diffResults[i].Diff == nil || diffResults[j].Diff == nil {
+				return true
+			}
+			return diffResults[i].Diff.NumDiffPixels > diffResults[j].Diff.NumDiffPixels
+		})
+	case 1:
+		sort.Slice(diffResults, func(i, j int) bool {
+			if diffResults[i].Diff == nil || diffResults[j].Diff == nil {
+				return true
+			}
+			return diffResults[i].Diff.NumDiffPixels < diffResults[j].Diff.NumDiffPixels
+		})
+	case 2:
+		sort.Slice(diffResults, func(i, j int) bool {
+			if diffResults[i].Diff == nil || diffResults[j].Diff == nil {
+				return true
+			}
+			return diffResults[i].Diff.PixelDiffPercent > diffResults[j].Diff.PixelDiffPercent
+		})
+	case 3:
+		sort.Slice(diffResults, func(i, j int) bool {
+			if diffResults[i].Diff == nil || diffResults[j].Diff == nil {
+				return true
+			}
+			return diffResults[i].Diff.PixelDiffPercent < diffResults[j].Diff.PixelDiffPercent
+		})
+	case 4:
+		sort.Slice(diffResults, func(i, j int) bool {
+			if diffResults[i].Diff == nil || diffResults[j].Diff == nil {
+				return true
+			}
+			return diffResults[i].Diff.MaxRGBADiffs[0] > diffResults[j].Diff.MaxRGBADiffs[0]
+		})
+	case 5:
+		sort.Slice(diffResults, func(i, j int) bool {
+			if diffResults[i].Diff == nil || diffResults[j].Diff == nil {
+				return true
+			}
+			return diffResults[i].Diff.MaxRGBADiffs[0] < diffResults[j].Diff.MaxRGBADiffs[0]
+		})
+	case 6:
+		sort.Slice(diffResults, func(i, j int) bool {
+			if diffResults[i].Diff == nil || diffResults[j].Diff == nil {
+				return true
+			}
+			return diffResults[i].Diff.MaxRGBADiffs[1] > diffResults[j].Diff.MaxRGBADiffs[1]
+		})
+	case 7:
+		sort.Slice(diffResults, func(i, j int) bool {
+			if diffResults[i].Diff == nil || diffResults[j].Diff == nil {
+				return true
+			}
+			return diffResults[i].Diff.MaxRGBADiffs[1] < diffResults[j].Diff.MaxRGBADiffs[1]
+		})
+	case 8:
+		sort.Slice(diffResults, func(i, j int) bool {
+			if diffResults[i].Diff == nil || diffResults[j].Diff == nil {
+				return true
+			}
+			return diffResults[i].Diff.MaxRGBADiffs[2] > diffResults[j].Diff.MaxRGBADiffs[2]
+		})
+	case 9:
+		sort.Slice(diffResults, func(i, j int) bool {
+			if diffResults[i].Diff == nil || diffResults[j].Diff == nil {
+				return true
+			}
+			return diffResults[i].Diff.MaxRGBADiffs[2] < diffResults[j].Diff.MaxRGBADiffs[2]
+		})
+	case 10:
+		sort.Slice(diffResults, func(i, j int) bool {
+			return diffResults[i].Rank < diffResults[j].Rank
+		})
+	case 11:
+		sort.Slice(diffResults, func(i, j int) bool {
+			return diffResults[i].Rank > diffResults[j].Rank
+		})
+	}
+}
+
+// jsonStatusHandler returns the current status of with respect to HEAD.
+func jsonStatusHandler(w http.ResponseWriter, r *http.Request) {
+	sendJsonResponse(w, statusWatcher.GetStatus())
+}
+
+// makeResourceHandler creates a static file handler that sets a caching policy.
+func makeResourceHandler(resourceDir string) func(http.ResponseWriter, *http.Request) {
+	fileServer := http.FileServer(http.Dir(resourceDir))
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Cache-Control", "max-age=300")
+		fileServer.ServeHTTP(w, r)
+	}
+}
