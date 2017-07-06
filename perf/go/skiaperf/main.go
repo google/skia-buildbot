5c57b90409d425475095b5497483f3f9188ec4b1
Joe Gregorio <jcgregorio@google.com>
2017-07-06 16:37:51 -0400

    [perf] Add page for editing alert configs.
    Screenshot:
      https://screenshot.googleplex.com/YxiYdTuoHmv.png
    
    Bug: skia:6801
    Change-Id: I8297b4457ba0e9f5ca58ad83c7612116bf2b567a

--- perf/go/skiaperf/main.go
+++ perf/go/skiaperf/main.go
@@ -36,6 +36,7 @@ import (
 	"go.skia.org/infra/go/sharedconfig"
 	"go.skia.org/infra/go/util"
 	"go.skia.org/infra/perf/go/activitylog"
+	"go.skia.org/infra/perf/go/alerts"
 	"go.skia.org/infra/perf/go/cid"
 	"go.skia.org/infra/perf/go/clustering2"
 	"go.skia.org/infra/perf/go/config"
@@ -96,6 +97,8 @@ var (
 	continuous *regression.Continuous
 
 	storageClient *storage.Client
+
+	alertStore *alerts.Store
 )
 
 func loadTemplates() {
@@ -103,6 +106,7 @@ func loadTemplates() {
 		filepath.Join(*resourcesDir, "templates/newindex.html"),
 		filepath.Join(*resourcesDir, "templates/clusters2.html"),
 		filepath.Join(*resourcesDir, "templates/triage.html"),
+		filepath.Join(*resourcesDir, "templates/alerts.html"),
 		filepath.Join(*resourcesDir, "templates/help.html"),
 		filepath.Join(*resourcesDir, "templates/activitylog.html"),
 
@@ -172,6 +176,8 @@ func Init() {
 	initIngestion()
 	rietveldAPI := rietveld.New(rietveld.RIETVELD_SKIA_URL, httputils.NewTimeoutClient())
 	cidl = cid.New(git, rietveldAPI, *gitRepoURL)
+
+	alertStore = alerts.NewStore()
 
 	frameRequests = dataframe.NewRunningFrameRequests(git)
 	clusterRequests = clustering2.NewRunningClusterRequests(git, cidl, float32(*interesting))
@@ -973,6 +979,59 @@ func shiftHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func alertListHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	show := mux.Vars(r)["show"]
+	resp, err := alertStore.List(show == "true")
+	if err != nil {
+		httputils.ReportError(w, r, err, "Failed to retrieve alert configs.")
+	}
+	if err := json.NewEncoder(w).Encode(resp); err != nil {
+		sklog.Errorf("Failed to write JSON response: %s", err)
+	}
+}
+
+func alertNewHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(alerts.NewConfig()); err != nil {
+		sklog.Errorf("Failed to write JSON response: %s", err)
+	}
+}
+
+func alertUpdateHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	if login.LoggedInAs(r) == "" {
+		httputils.ReportError(w, r, fmt.Errorf("Not logged in."), "You must be logged in to edit alerts.")
+		return
+	}
+
+	cfg := &alerts.Config{}
+	if err := json.NewDecoder(r.Body).Decode(cfg); err != nil {
+		httputils.ReportError(w, r, err, "Failed to decode JSON.")
+		return
+	}
+	if err := alertStore.Save(cfg); err != nil {
+		httputils.ReportError(w, r, err, "Failed to save alerts.Config.")
+	}
+}
+
+func alertDeleteHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	if login.LoggedInAs(r) == "" {
+		httputils.ReportError(w, r, fmt.Errorf("Not logged in."), "You must be logged in to delete alerts.")
+		return
+	}
+
+	sid := mux.Vars(r)["id"]
+	id, err := strconv.ParseInt(sid, 10, 64)
+	if err != nil {
+		httputils.ReportError(w, r, err, "Failed to parse alert id.")
+	}
+	if err := alertStore.Delete(int(id)); err != nil {
+		httputils.ReportError(w, r, err, "Failed to delete the alerts.Config.")
+	}
+}
+
 func makeResourceHandler() func(http.ResponseWriter, *http.Request) {
 	fileServer := http.FileServer(http.Dir(*resourcesDir))
 	return func(w http.ResponseWriter, r *http.Request) {
@@ -1069,6 +1128,7 @@ func main() {
 	router.HandleFunc("/e/", templateHandler("newindex.html"))
 	router.HandleFunc("/c/", templateHandler("clusters2.html"))
 	router.HandleFunc("/t/", templateHandler("triage.html"))
+	router.HandleFunc("/a/", templateHandler("alerts.html"))
 	router.HandleFunc("/g/{dest:[ect]}/{hash:[a-zA-Z0-9]+}", gotoHandler)
 	router.HandleFunc("/help/", helpHandler)
 	router.PathPrefix("/activitylog/").HandlerFunc(activityHandler)
@@ -1092,6 +1152,10 @@ func main() {
 	router.HandleFunc("/_/alerts/", alertsHandler)
 	router.HandleFunc("/_/details/", detailsHandler).Methods("POST")
 	router.HandleFunc("/_/shift/", shiftHandler).Methods("POST")
+	router.HandleFunc("/_/alert/list/{show}", alertListHandler).Methods("GET")
+	router.HandleFunc("/_/alert/new", alertNewHandler).Methods("GET")
+	router.HandleFunc("/_/alert/update", alertUpdateHandler).Methods("POST")
+	router.HandleFunc("/_/alert/delete/{id:[0-9]+}", alertDeleteHandler).Methods("POST")
 
 	var h http.Handler = router
 	if *internalOnly {
