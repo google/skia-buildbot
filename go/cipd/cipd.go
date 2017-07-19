65fa6930c0258321ec0a288eeb864b5effe2fb82
Eric Boren <borenet@google.com>
2017-07-19 11:18:59 -0400

    Add go/cipd package as a small wrapper around the CIPD client
    This will help our servers obtain CIPD packages where necessary.
    
    Bug: skia:
    Change-Id: I1aea429ba34a466f8ee59af2c1a06c946829d6c1

---
+++ go/cipd/cipd.go
@@ -0,0 +1,73 @@
+package cipd
+
+/*
+	Utilities for working with CIPD.
+*/
+
+//go:generate go run gen_versions.go
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/luci/luci-go/cipd/client/cipd"
+	"github.com/luci/luci-go/cipd/client/cipd/common"
+	"go.skia.org/infra/go/auth"
+)
+
+const (
+	// CIPD server to use for obtaining packages.
+	SERVICE_URL = "https://chrome-infra-packages.appspot.com"
+)
+
+var (
+	// CIPD package for the Go installation.
+	PkgGo = &Package{
+		Dest:    "go",
+		Name:    "skia/bots/go",
+		Version: fmt.Sprintf("version:%s", PKG_VERSIONS_FROM_ASSETS["go"]),
+	}
+)
+
+// Package describes a CIPD package.
+type Package struct {
+	// Relative path within the root dir to install the package.
+	Dest string
+
+	// Name of the package.
+	Name string
+
+	// Version of the package. See the CIPD docs for valid version strings:
+	// https://godoc.org/github.com/luci/luci-go/cipd/client/cipd/common#ValidateInstanceVersion
+	Version string
+}
+
+// Run "cipd ensure" to get the correct packages in the given location.
+func Ensure(local bool, rootdir string, packages ...*Package) error {
+	httpClient, err := auth.NewDefaultClient(local, auth.SCOPE_USERINFO_EMAIL, auth.SCOPE_PLUS_ME)
+	if err != nil {
+		return fmt.Errorf("Failed to create authenticated HTTP client: %s", err)
+	}
+	c, err := cipd.NewClient(cipd.ClientOptions{
+		ServiceURL:          SERVICE_URL,
+		Root:                rootdir,
+		AuthenticatedClient: httpClient,
+	})
+	if err != nil {
+		return fmt.Errorf("Failed to create CIPD client: %s", err)
+	}
+
+	ctx := context.Background()
+	pkgs := common.PinSliceBySubdir{}
+	for _, pkg := range packages {
+		pin, err := c.ResolveVersion(ctx, pkg.Name, pkg.Version)
+		if err != nil {
+			return fmt.Errorf("Failed to resolve package version %q @ %q: %s", pkg.Name, pkg.Version, err)
+		}
+		pkgs[pkg.Dest] = common.PinSlice{pin}
+	}
+	if _, err := c.EnsurePackages(context.Background(), pkgs, false); err != nil {
+		return fmt.Errorf("Failed to ensure packages: %s", err)
+	}
+	return nil
+}
