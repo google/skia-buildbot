12b3e9288ff6b9a0bcf22b152eb57345c8fbae8f
Joe Gregorio <jcgregorio@google.com>
2017-07-25 17:08:16 -0400

    [perf] Support sparse data when clustering.
    Bug: skia:
    Change-Id: I31f53e99470a81faddc52da1c5a6b7963b9fe9c9

--- perf/go/clustering2/async.go
+++ perf/go/clustering2/async.go
@@ -82,6 +82,7 @@ type ClusterRequest struct {
 	TZ          string      `json:"tz"`
 	Algo        ClusterAlgo `json:"algo"`
 	Interesting float32     `json:"interesting"`
+	Sparse      bool        `json:"sparse"`
 }
 
 func (c *ClusterRequest) Id() string {
@@ -107,10 +108,11 @@ type ClusterRequestProcess struct {
 	lastUpdate time.Time        // The last time this process was updated.
 	state      ProcessState     // The current state of the process.
 	message    string           // Describes the current state of the process.
+	cids       []*cid.CommitID  // The cids to run the clustering over. Calculated in calcCids.
 }
 
 func newProcess(req *ClusterRequest, git *gitinfo.GitInfo, cidl *cid.CommitIDLookup) *ClusterRequestProcess {
-	ret := &ClusterRequestProcess{
+	return &ClusterRequestProcess{
 		request:    req,
 		git:        git,
 		cidl:       cidl,
@@ -118,6 +120,10 @@ func newProcess(req *ClusterRequest, git *gitinfo.GitInfo, cidl *cid.CommitIDLoo
 		state:      PROCESS_RUNNING,
 		message:    "Running",
 	}
+}
+
+func newRunningProcess(req *ClusterRequest, git *gitinfo.GitInfo, cidl *cid.CommitIDLookup) *ClusterRequestProcess {
+	ret := newProcess(req, git, cidl)
 	go ret.Run()
 	return ret
 }
@@ -188,7 +194,7 @@ func (fr *RunningClusterRequests) Add(req *ClusterRequest) string {
 		}
 	}
 	if _, ok := fr.inProcess[id]; !ok {
-		fr.inProcess[id] = newProcess(req, fr.git, fr.cidl)
+		fr.inProcess[id] = newRunningProcess(req, fr.git, fr.cidl)
 	}
 	return id
 }
@@ -284,25 +290,44 @@ func tooMuchMissingData(tr ptracestore.Trace) bool {
 	return missing(tr[:n]) || missing(tr[len(tr)-n:])
 }
 
+func (p *ClusterRequestProcess) calcCids() {
+	p.mutex.Lock()
+	defer p.mutex.Unlock()
+
+	p.cids = []*cid.CommitID{}
+	if p.request.Sparse {
+		// Search for points in blocks of 100 points count(query).
+		// The count() could either be done via calc, but that would require
+		// running a FrameRequest.
+
+		// Start by checking center point as a quick exit strategy.
+		lastCommit := p.git.LastNIndex(1)
+		lastIndex := last[0].Index
+		// Then check from center forward in time, as another quick exit strategy.
+		// Finally check backward in time.
+	} else {
+		if p.request.Radius <= 0 {
+			p.request.Radius = 1
+		}
+		if p.request.Radius > MAX_RADIUS {
+			p.request.Radius = MAX_RADIUS
+		}
+		for i := p.request.Offset - p.request.Radius; i <= p.request.Offset+p.request.Radius; i++ {
+			p.cids = append(p.cids, &cid.CommitID{
+				Source: p.request.Source,
+				Offset: i,
+			})
+		}
+	}
+}
+
 // Run does the work in a ClusterRequestProcess. It does not return until all the
 // work is done or the request failed. Should be run as a Go routine.
 func (p *ClusterRequestProcess) Run() {
 	if p.request.Algo == "" {
 		p.request.Algo = KMEANS_ALGO
 	}
-	cids := []*cid.CommitID{}
-	if p.request.Radius <= 0 {
-		p.request.Radius = 1
-	}
-	if p.request.Radius > MAX_RADIUS {
-		p.request.Radius = MAX_RADIUS
-	}
-	for i := p.request.Offset - p.request.Radius; i <= p.request.Offset+p.request.Radius; i++ {
-		cids = append(cids, &cid.CommitID{
-			Source: p.request.Source,
-			Offset: i,
-		})
-	}
+	p.calcCids()
 	parsedQuery, err := url.ParseQuery(p.request.Query)
 	if err != nil {
 		p.reportError(err, "Invalid URL query.")
@@ -313,7 +338,7 @@ func (p *ClusterRequestProcess) Run() {
 		p.reportError(err, "Invalid Query.")
 		return
 	}
-	df, err := dataframe.NewFromCommitIDsAndQuery(cids, p.cidl, ptracestore.Default, q, p.progress)
+	df, err := dataframe.NewFromCommitIDsAndQuery(p.cids, p.cidl, ptracestore.Default, q, p.progress)
 	if err != nil {
 		p.reportError(err, "Invalid range of commits.")
 		return
