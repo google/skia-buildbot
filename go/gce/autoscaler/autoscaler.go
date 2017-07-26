6f560a49ceec1a465e30bdbbe3ae916bb8ce4ca6
Ravi Mistry <rmistry@google.com>
2017-07-26 13:26:13 -0400

    Add gce/autoscaller.go
    Initial checkin of basic functions to start/stop all matching instances
    
    Bug: skia:6648
    Change-Id: I9272ee2fa6da7439324b1433f50450e2b460b8c4
    Reviewed-on: https://skia-review.googlesource.com/26781
    Reviewed-by: Eric Boren <borenet@google.com>
    Commit-Queue: Ravi Mistry <rmistry@google.com>

---
+++ go/gce/autoscaler/autoscaler.go
@@ -0,0 +1,99 @@
+package autoscaler
+
+import (
+	"path/filepath"
+	"sort"
+	"sync"
+
+	"go.skia.org/infra/go/gce"
+	"go.skia.org/infra/go/util"
+)
+
+// Autoscaler is a struct used for autoscaling instances in GCE.
+type Autoscaler struct {
+	g         *gce.GCloud
+	workdir   string
+	instances []*gce.Instance
+}
+
+// NewAutoscaler returns an Autoscaler instance.
+func NewAutoscaler(zone, workdir string, minInstanceNum, maxInstanceNum int, getInstance func(int) *gce.Instance) (*Autoscaler, error) {
+	// Get the absolute workdir.
+	wdAbs, err := filepath.Abs(workdir)
+	if err != nil {
+		return nil, err
+	}
+
+	// Create the GCloud object.
+	g, err := gce.NewGCloud(zone, wdAbs)
+	if err != nil {
+		return nil, err
+	}
+	// Create slice of instances.
+	instances := []*gce.Instance{}
+	for num := minInstanceNum; num <= maxInstanceNum; num++ {
+		instances = append(instances, getInstance(num))
+	}
+	return &Autoscaler{
+		g:         g,
+		workdir:   workdir,
+		instances: instances,
+	}, nil
+}
+
+// GetRunningInstances returns a slice of all running instance names.
+func (a *Autoscaler) GetRunningInstances() ([]string, error) {
+	runningInstances := []string{}
+	// Mutex to control access to above slice.
+	var m sync.Mutex
+	// Perform the requested operation.
+	group := util.NewNamedErrGroup()
+	for _, instance := range a.instances {
+		name := instance.Name // https://golang.org/doc/faq#closures_and_goroutines
+		group.Go(name, func() error {
+			if a.g.IsInstanceRunning(name) {
+				m.Lock()
+				runningInstances = append(runningInstances, name)
+				m.Unlock()
+			}
+			return nil
+		})
+	}
+	if err := group.Wait(); err != nil {
+		return nil, err
+	}
+	sort.Strings(runningInstances)
+	return runningInstances, nil
+}
+
+// StopAllInstances stops all instances.
+func (a *Autoscaler) StopAllInstances() error {
+	// Perform the requested operation.
+	group := util.NewNamedErrGroup()
+	for _, instance := range a.instances {
+		instance := instance // https://golang.org/doc/faq#closures_and_goroutines
+		group.Go(instance.Name, func() error {
+			return a.g.Stop(instance)
+		})
+	}
+	if err := group.Wait(); err != nil {
+		return err
+	}
+	return nil
+}
+
+// StartAllInstances starts all instances.
+func (a *Autoscaler) StartAllInstances() error {
+	// Perform the requested operation.
+	group := util.NewNamedErrGroup()
+	for _, instance := range a.instances {
+		instance := instance // https://golang.org/doc/faq#closures_and_goroutines
+		group.Go(instance.Name, func() error {
+			return a.g.Start(instance)
+		})
+	}
+	if err := group.Wait(); err != nil {
+		return err
+	}
+	return nil
+}
