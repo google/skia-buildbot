10543868cb0b05acda36689a6d7434118e46708d
Eric Boren <borenet@google.com>
2017-07-27 14:55:16 -0400

    [autoroll] Fix some bugs, add tests for RollCLImpl
    Bug: skia:
    Change-Id: I3c7dadc83a62df2c8358626c6d85258fd13b429e

---
+++ autoroll/go/autorollerv2/roll_test.go
@@ -0,0 +1,365 @@
+package autorollerv2
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"path"
+	"testing"
+	"time"
+
+	assert "github.com/stretchr/testify/require"
+	"go.skia.org/infra/autoroll/go/recent_rolls"
+	repo_manager_testutils "go.skia.org/infra/autoroll/go/repo_manager/testutils"
+	"go.skia.org/infra/go/autoroll"
+	"go.skia.org/infra/go/buildbucket"
+	"go.skia.org/infra/go/gerrit"
+	gerrit_testutils "go.skia.org/infra/go/gerrit/testutils"
+	"go.skia.org/infra/go/jsonutils"
+	"go.skia.org/infra/go/mockhttpclient"
+	"go.skia.org/infra/go/testutils"
+)
+
+func TestGerritRoll(t *testing.T) {
+	testutils.LargeTest(t)
+
+	tmp, err := ioutil.TempDir("", "")
+	assert.NoError(t, err)
+	defer testutils.RemoveAll(t, tmp)
+
+	g := gerrit_testutils.NewGerrit(t, tmp, false)
+	rm := repo_manager_testutils.NewRepoManager(t, false)
+
+	recent, err := recent_rolls.NewRecentRolls(path.Join(tmp, "recent.bdb"))
+	assert.NoError(t, err)
+
+	// Upload and retrieve the roll.
+	from := "abcde12345abcde12345abcde12345abcde12345"
+	to := "fghij67890fghij67890fghij67890fghij67890"
+	rm.MockFullChildHash(from[:12], from)
+	rm.MockFullChildHash(to[:12], to)
+	roll := rm.RollerWillUpload(123, from, to, false)
+	g.MockGetIssueProperties(roll)
+	g.MockGetTrybotResults(roll, nil)
+	gr, err := newGerritRoll(g.Gerrit, rm, recent, 123)
+	assert.NoError(t, err)
+	assert.False(t, gr.IsFinished())
+	assert.False(t, gr.IsSuccess())
+	g.AssertEmpty()
+	assert.Equal(t, to, gr.RollingTo())
+
+	// Insert into DB.
+	current := recent.CurrentRoll()
+	assert.Nil(t, current)
+	assert.NoError(t, gr.InsertIntoDB())
+	current = recent.CurrentRoll()
+	assert.NotNil(t, current)
+	assert.Equal(t, current.Issue, roll.Issue)
+	g.AssertEmpty()
+
+	// Add a comment.
+	msg := "Here's a comment"
+	g.MockAddComment(roll, msg)
+	assert.NoError(t, gr.AddComment(msg))
+	g.AssertEmpty()
+
+	// Set dry run.
+	g.MockSetDryRun(roll, "Mode was changed to dry run")
+	g.MockGetIssueProperties(roll)
+	g.MockGetTrybotResults(roll, nil)
+	assert.NoError(t, gr.SwitchToDryRun())
+	g.AssertEmpty()
+
+	// Set normal.
+	g.MockSetCQ(roll, "Mode was changed to normal")
+	g.MockGetIssueProperties(roll)
+	g.MockGetTrybotResults(roll, nil)
+	assert.NoError(t, gr.SwitchToNormal())
+	g.AssertEmpty()
+
+	// Update.
+	roll.Status = gerrit.CHANGE_STATUS_MERGED
+	g.MockGetIssueProperties(roll)
+	g.MockGetTrybotResults(roll, nil)
+	assert.NoError(t, gr.Update())
+	assert.True(t, gr.IsFinished())
+	assert.True(t, gr.IsSuccess())
+	assert.Nil(t, recent.CurrentRoll())
+
+	// Upload and retrieve another roll, dry run this time.
+	roll = rm.RollerWillUpload(124, from, to, true)
+	g.MockGetIssueProperties(roll)
+	tryjob := &buildbucket.Build{
+		Created:        jsonutils.Time(time.Now().UTC().Round(time.Millisecond)),
+		Status:         autoroll.TRYBOT_STATUS_STARTED,
+		ParametersJson: "{\"builder_name\":\"fake-builder\",\"properties\":{\"category\":\"cq\"}}",
+	}
+	g.MockGetTrybotResults(roll, []*buildbucket.Build{tryjob})
+	gr, err = newGerritRoll(g.Gerrit, rm, recent, 124)
+	assert.NoError(t, err)
+	assert.False(t, gr.IsDryRunFinished())
+	assert.False(t, gr.IsDryRunSuccess())
+	g.AssertEmpty()
+	assert.Equal(t, to, gr.RollingTo())
+
+	// Insert into DB.
+	current = recent.CurrentRoll()
+	assert.Nil(t, current)
+	assert.NoError(t, gr.InsertIntoDB())
+	current = recent.CurrentRoll()
+	assert.NotNil(t, current)
+	assert.Equal(t, current.Issue, roll.Issue)
+	g.AssertEmpty()
+
+	// Success.
+	tryjob.Status = autoroll.TRYBOT_STATUS_COMPLETED
+	tryjob.Result = autoroll.TRYBOT_RESULT_SUCCESS
+	roll.Labels[gerrit.COMMITQUEUE_LABEL] = &gerrit.LabelEntry{
+		All: []*gerrit.LabelDetail{
+			&gerrit.LabelDetail{
+				Value: gerrit.COMMITQUEUE_LABEL_NONE,
+			},
+		},
+	}
+	g.MockGetIssueProperties(roll)
+	g.MockGetTrybotResults(roll, []*buildbucket.Build{tryjob})
+	assert.NoError(t, gr.Update())
+	assert.True(t, gr.IsDryRunFinished())
+	assert.True(t, gr.IsDryRunSuccess())
+	g.AssertEmpty()
+
+	// Close for cleanup.
+	roll.Status = gerrit.CHANGE_STATUS_ABANDONED
+	g.MockGetIssueProperties(roll)
+	g.MockGetTrybotResults(roll, []*buildbucket.Build{tryjob})
+	assert.NoError(t, gr.Update())
+
+	// Verify that all of the mutation functions handle a conflict (eg.
+	// someone closed the CL) gracefully.
+
+	// 1. SwitchToDryRun.
+	roll = rm.RollerWillUpload(125, from, to, false)
+	g.MockGetIssueProperties(roll)
+	g.MockGetTrybotResults(roll, nil)
+	gr, err = newGerritRoll(g.Gerrit, rm, recent, 125)
+	assert.NoError(t, err)
+	assert.NoError(t, gr.InsertIntoDB())
+	url, reqBytes := g.MakePostRequest(roll, "Mode was changed to dry run", map[string]int{
+		gerrit.COMMITQUEUE_LABEL: gerrit.COMMITQUEUE_LABEL_DRY_RUN,
+	})
+	g.Mock.MockOnce(url, mockhttpclient.MockPostError("application/json", reqBytes, "CONFLICT", http.StatusConflict))
+	roll.Status = gerrit.CHANGE_STATUS_ABANDONED
+	g.MockGetIssueProperties(roll)
+	g.MockGetTrybotResults(roll, nil)
+	assert.NoError(t, gr.SwitchToDryRun())
+	g.AssertEmpty()
+
+	// 2. SwitchToNormal
+	roll = rm.RollerWillUpload(126, from, to, false)
+	g.MockGetIssueProperties(roll)
+	g.MockGetTrybotResults(roll, nil)
+	gr, err = newGerritRoll(g.Gerrit, rm, recent, 126)
+	assert.NoError(t, err)
+	assert.NoError(t, gr.InsertIntoDB())
+	url, reqBytes = g.MakePostRequest(roll, "Mode was changed to normal", map[string]int{
+		gerrit.COMMITQUEUE_LABEL: gerrit.COMMITQUEUE_LABEL_SUBMIT,
+	})
+	g.Mock.MockOnce(url, mockhttpclient.MockPostError("application/json", reqBytes, "CONFLICT", http.StatusConflict))
+	roll.Status = gerrit.CHANGE_STATUS_ABANDONED
+	g.MockGetIssueProperties(roll)
+	g.MockGetTrybotResults(roll, nil)
+	assert.NoError(t, gr.SwitchToNormal())
+	g.AssertEmpty()
+
+	// 3. Close.
+	roll = rm.RollerWillUpload(127, from, to, false)
+	g.MockGetIssueProperties(roll)
+	g.MockGetTrybotResults(roll, nil)
+	gr, err = newGerritRoll(g.Gerrit, rm, recent, 127)
+	assert.NoError(t, err)
+	assert.NoError(t, gr.InsertIntoDB())
+	url = fmt.Sprintf("%s/a/changes/%d/abandon", gerrit_testutils.FAKE_GERRIT_URL, roll.Issue)
+	req := testutils.MarshalJSON(t, &struct {
+		Message string `json:"message"`
+	}{
+		Message: "close it!",
+	})
+	g.Mock.MockOnce(url, mockhttpclient.MockPostError("application/json", []byte(req), "CONFLICT", http.StatusConflict))
+	roll.Status = gerrit.CHANGE_STATUS_ABANDONED
+	g.MockGetIssueProperties(roll)
+	g.MockGetTrybotResults(roll, nil)
+	assert.NoError(t, gr.Close(autoroll.ROLL_RESULT_FAILURE, "close it!"))
+	g.AssertEmpty()
+}
+
+func TestGerritAndroidRoll(t *testing.T) {
+	testutils.LargeTest(t)
+
+	tmp, err := ioutil.TempDir("", "")
+	assert.NoError(t, err)
+	defer testutils.RemoveAll(t, tmp)
+
+	g := gerrit_testutils.NewGerrit(t, tmp, true)
+	rm := repo_manager_testutils.NewRepoManager(t, true)
+
+	recent, err := recent_rolls.NewRecentRolls(path.Join(tmp, "recent.bdb"))
+	assert.NoError(t, err)
+
+	// Upload and retrieve the roll.
+	from := "abcde12345abcde12345abcde12345abcde12345"
+	to := "fghij67890fghij67890fghij67890fghij67890"
+	rm.MockFullChildHash(from[:12], from)
+	rm.MockFullChildHash(to[:12], to)
+	roll := rm.RollerWillUpload(123, from, to, false)
+	g.MockGetIssueProperties(roll)
+	g.MockGetTrybotResults(roll, nil)
+	gr, err := newGerritAndroidRoll(g.Gerrit, rm, recent, 123)
+	assert.NoError(t, err)
+	assert.False(t, gr.IsFinished())
+	assert.False(t, gr.IsSuccess())
+	g.AssertEmpty()
+	assert.Equal(t, to, gr.RollingTo())
+
+	// Insert into DB.
+	current := recent.CurrentRoll()
+	assert.Nil(t, current)
+	assert.NoError(t, gr.InsertIntoDB())
+	current = recent.CurrentRoll()
+	assert.NotNil(t, current)
+	assert.Equal(t, current.Issue, roll.Issue)
+	g.AssertEmpty()
+
+	// Add a comment.
+	msg := "Here's a comment"
+	g.MockAddComment(roll, msg)
+	assert.NoError(t, gr.AddComment(msg))
+	g.AssertEmpty()
+
+	// Set dry run.
+	g.MockSetDryRun(roll, "Mode was changed to dry run")
+	g.MockGetIssueProperties(roll)
+	g.MockGetTrybotResults(roll, nil)
+	assert.NoError(t, gr.SwitchToDryRun())
+	g.AssertEmpty()
+
+	// Set normal.
+	g.MockSetCQ(roll, "Mode was changed to normal")
+	g.MockGetIssueProperties(roll)
+	g.MockGetTrybotResults(roll, nil)
+	assert.NoError(t, gr.SwitchToNormal())
+	g.AssertEmpty()
+
+	// Update.
+	roll.Status = gerrit.CHANGE_STATUS_MERGED
+	g.MockGetIssueProperties(roll)
+	g.MockGetTrybotResults(roll, nil)
+	assert.NoError(t, gr.Update())
+	assert.True(t, gr.IsFinished())
+	assert.True(t, gr.IsSuccess())
+	assert.Nil(t, recent.CurrentRoll())
+
+	// Upload and retrieve another roll, dry run this time.
+	roll = rm.RollerWillUpload(124, from, to, true)
+	g.MockGetIssueProperties(roll)
+	tryjob := &buildbucket.Build{
+		Created:        jsonutils.Time(time.Now().UTC().Round(time.Millisecond)),
+		Status:         autoroll.TRYBOT_STATUS_STARTED,
+		ParametersJson: "{\"builder_name\":\"fake-builder\",\"properties\":{\"category\":\"cq\"}}",
+	}
+	g.MockGetTrybotResults(roll, []*buildbucket.Build{tryjob})
+	gr, err = newGerritAndroidRoll(g.Gerrit, rm, recent, 124)
+	assert.NoError(t, err)
+	assert.False(t, gr.IsDryRunFinished())
+	assert.False(t, gr.IsDryRunSuccess())
+	g.AssertEmpty()
+	assert.Equal(t, to, gr.RollingTo())
+
+	// Insert into DB.
+	current = recent.CurrentRoll()
+	assert.Nil(t, current)
+	assert.NoError(t, gr.InsertIntoDB())
+	current = recent.CurrentRoll()
+	assert.NotNil(t, current)
+	assert.Equal(t, current.Issue, roll.Issue)
+	g.AssertEmpty()
+
+	// Success.
+	tryjob.Status = autoroll.TRYBOT_STATUS_COMPLETED
+	tryjob.Result = autoroll.TRYBOT_RESULT_SUCCESS
+	roll.Labels[gerrit.PRESUBMIT_VERIFIED_LABEL] = &gerrit.LabelEntry{
+		All: []*gerrit.LabelDetail{
+			&gerrit.LabelDetail{
+				Value: gerrit.PRESUBMIT_VERIFIED_LABEL_ACCEPTED,
+			},
+		},
+	}
+	g.MockGetIssueProperties(roll)
+	g.MockGetTrybotResults(roll, []*buildbucket.Build{tryjob})
+	assert.NoError(t, gr.Update())
+	assert.True(t, gr.IsDryRunFinished())
+	assert.True(t, gr.IsDryRunSuccess())
+	g.AssertEmpty()
+
+	// Close for cleanup.
+	roll.Status = gerrit.CHANGE_STATUS_ABANDONED
+	g.MockGetIssueProperties(roll)
+	g.MockGetTrybotResults(roll, []*buildbucket.Build{tryjob})
+	assert.NoError(t, gr.Update())
+
+	// Verify that all of the mutation functions handle a conflict (eg.
+	// someone closed the CL) gracefully.
+
+	// 1. SwitchToDryRun.
+	roll = rm.RollerWillUpload(125, from, to, true)
+	g.MockGetIssueProperties(roll)
+	g.MockGetTrybotResults(roll, nil)
+	gr, err = newGerritAndroidRoll(g.Gerrit, rm, recent, 125)
+	assert.NoError(t, err)
+	assert.NoError(t, gr.InsertIntoDB())
+	url, reqBytes := g.MakePostRequest(roll, "Mode was changed to dry run", map[string]int{
+		gerrit.AUTOSUBMIT_LABEL: gerrit.AUTOSUBMIT_LABEL_NONE,
+	})
+	g.Mock.MockOnce(url, mockhttpclient.MockPostError("application/json", reqBytes, "CONFLICT", http.StatusConflict))
+	roll.Status = gerrit.CHANGE_STATUS_ABANDONED
+	g.MockGetIssueProperties(roll)
+	g.MockGetTrybotResults(roll, nil)
+	assert.NoError(t, gr.SwitchToDryRun())
+	g.AssertEmpty()
+
+	// 2. SwitchToNormal
+	roll = rm.RollerWillUpload(126, from, to, true)
+	g.MockGetIssueProperties(roll)
+	g.MockGetTrybotResults(roll, nil)
+	gr, err = newGerritAndroidRoll(g.Gerrit, rm, recent, 126)
+	assert.NoError(t, err)
+	assert.NoError(t, gr.InsertIntoDB())
+	url, reqBytes = g.MakePostRequest(roll, "Mode was changed to normal", map[string]int{
+		gerrit.AUTOSUBMIT_LABEL: gerrit.AUTOSUBMIT_LABEL_SUBMIT,
+	})
+	g.Mock.MockOnce(url, mockhttpclient.MockPostError("application/json", reqBytes, "CONFLICT", http.StatusConflict))
+	roll.Status = gerrit.CHANGE_STATUS_ABANDONED
+	g.MockGetIssueProperties(roll)
+	g.MockGetTrybotResults(roll, nil)
+	assert.NoError(t, gr.SwitchToNormal())
+	g.AssertEmpty()
+
+	// 3. Close.
+	roll = rm.RollerWillUpload(127, from, to, true)
+	g.MockGetIssueProperties(roll)
+	g.MockGetTrybotResults(roll, nil)
+	gr, err = newGerritAndroidRoll(g.Gerrit, rm, recent, 127)
+	assert.NoError(t, err)
+	assert.NoError(t, gr.InsertIntoDB())
+	url = fmt.Sprintf("%s/a/changes/%d/abandon", gerrit_testutils.FAKE_GERRIT_URL, roll.Issue)
+	req := testutils.MarshalJSON(t, &struct {
+		Message string `json:"message"`
+	}{
+		Message: "close it!",
+	})
+	g.Mock.MockOnce(url, mockhttpclient.MockPostError("application/json", []byte(req), "CONFLICT", http.StatusConflict))
+	roll.Status = gerrit.CHANGE_STATUS_ABANDONED
+	g.MockGetIssueProperties(roll)
+	g.MockGetTrybotResults(roll, nil)
+	assert.NoError(t, gr.Close(autoroll.ROLL_RESULT_FAILURE, "close it!"))
+	g.AssertEmpty()
+}
