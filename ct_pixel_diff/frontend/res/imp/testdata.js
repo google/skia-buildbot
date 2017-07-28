0051f4ad849ede4612e22acb583a0270f6037de8
Lawrence Choi <lchoi@google.com>
2017-07-28 14:02:41 -0700

    Polymer elements for diff results
    Contains the diff-details-sk and results-page-sk custom Polymer elements, along
    with demo pages, test data for the demos, and build files. diff-details-sk is
    used to display the information of a single ResultRec instance: URL, rank, the
    nopatch image, withpatch image, diff image, and all the diff metrics values.
    results-page-sk is a dom-repeat wrapper around a list of diff-details-sk
    elements, along with an iron-scroll-threshold to incrementally load more diff
    results when the user scrolls to the bottom of the page and a dropdown menu
    to select a sort parameter.
    
    Bug: skia:6779
    Change-Id: I540ef47e99216acb1bc3ecc4892e1fd4470afec4
    Reviewed-on: https://skia-review.googlesource.com/27862
    Commit-Queue: Lawrence Choi <lchoi@google.com>
    Reviewed-by: Stephan Altmueller <stephana@google.com>
    Reviewed-by: Walter Korman <wkorman@google.com>

--- ct_pixel_diff/frontend/res/imp/testdata.js
+++ ct_pixel_diff/frontend/res/imp/testdata.js
@@ -44,3 +44,95 @@ testdata.runs = [
   "lchoi-20170727123456",
   "rmistry-201717202555",
 ]
+
+// Results is a serialized list of ResultRecs.
+testdata.results = [
+  {
+    "URL": "http://www.google.com",
+    "Rank": 1,
+    "NoPatchImg": "lchoi-20170727123456/nopatch/1/http___www_google_com",
+    "WithPatchImg": "lchoi-20170727123456/withpatch/1/http___www_google_com",
+    "DiffMetrics": {
+      "numDiffPixels": 1000,
+      "pixelDiffPercent": 50.2,
+      "maxRGBADiffs": [0, 128, 255, 0]
+    }
+  },
+  {
+    "URL": "http://www.youtube.com",
+    "Rank": 2,
+    "NoPatchImg": "lchoi-20170727123456/nopatch/2/http___www_youtube_com",
+    "WithPatchImg": "lchoi-20170727123456/withpatch/2/http___www_youtube_com",
+    "DiffMetrics": {
+      "numDiffPixels": 2500,
+      "pixelDiffPercent": 25.7,
+      "maxRGBADiffs": [7, 14, 21, 0]
+    }
+  },
+  {
+    "URL": "http://www.google.com",
+    "Rank": 1,
+    "NoPatchImg": "lchoi-20170727123456/nopatch/1/http___www_google_com",
+    "WithPatchImg": "lchoi-20170727123456/withpatch/1/http___www_google_com",
+    "DiffMetrics": {
+      "numDiffPixels": 1000,
+      "pixelDiffPercent": 50.2,
+      "maxRGBADiffs": [0, 128, 255, 0]
+    }
+  },
+  {
+    "URL": "http://www.youtube.com",
+    "Rank": 2,
+    "NoPatchImg": "lchoi-20170727123456/nopatch/2/http___www_youtube_com",
+    "WithPatchImg": "lchoi-20170727123456/withpatch/2/http___www_youtube_com",
+    "DiffMetrics": {
+      "numDiffPixels": 2500,
+      "pixelDiffPercent": 25.7,
+      "maxRGBADiffs": [7, 14, 21, 0]
+    }
+  },
+  {
+    "URL": "http://www.google.com",
+    "Rank": 1,
+    "NoPatchImg": "lchoi-20170727123456/nopatch/1/http___www_google_com",
+    "WithPatchImg": "lchoi-20170727123456/withpatch/1/http___www_google_com",
+    "DiffMetrics": {
+      "numDiffPixels": 1000,
+      "pixelDiffPercent": 50.2,
+      "maxRGBADiffs": [0, 128, 255, 0]
+    }
+  },
+  {
+    "URL": "http://www.youtube.com",
+    "Rank": 2,
+    "NoPatchImg": "lchoi-20170727123456/nopatch/2/http___www_youtube_com",
+    "WithPatchImg": "lchoi-20170727123456/withpatch/2/http___www_youtube_com",
+    "DiffMetrics": {
+      "numDiffPixels": 2500,
+      "pixelDiffPercent": 25.7,
+      "maxRGBADiffs": [7, 14, 21, 0]
+    }
+  },
+  {
+    "URL": "http://www.google.com",
+    "Rank": 1,
+    "NoPatchImg": "lchoi-20170727123456/nopatch/1/http___www_google_com",
+    "WithPatchImg": "lchoi-20170727123456/withpatch/1/http___www_google_com",
+    "DiffMetrics": {
+      "numDiffPixels": 1000,
+      "pixelDiffPercent": 50.2,
+      "maxRGBADiffs": [0, 128, 255, 0]
+    }
+  },
+  {
+    "URL": "http://www.youtube.com",
+    "Rank": 2,
+    "NoPatchImg": "lchoi-20170727123456/nopatch/2/http___www_youtube_com",
+    "WithPatchImg": "lchoi-20170727123456/withpatch/2/http___www_youtube_com",
+    "DiffMetrics": {
+      "numDiffPixels": 2500,
+      "pixelDiffPercent": 25.7,
+      "maxRGBADiffs": [7, 14, 21, 0]
+    }
+  }
+]
