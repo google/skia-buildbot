57771597a03ddec9c5c4c22b4b3546b0f54f54e6
Lawrence Choi <lchoi@google.com>
2017-07-28 13:00:43 -0700

    Polymer elements for viewing runs
    Contains the run-details-sk and runs-page-sk custom Polymer elements, along with
    demo pages, test data for the demos, and build files. run-details-sk is used
    to display the runID of a single CT Pixel Diff run. runs-page-sk is a dom-repeat
    wrapper around a list of run-details-sk elements.
    
    Bug: skia:6779
    Change-Id: I7939d191baed76dd5fe4d232a7e68213316dfae9
    Reviewed-on: https://skia-review.googlesource.com/27880
    Reviewed-by: Stephan Altmueller <stephana@google.com>
    Reviewed-by: Walter Korman <wkorman@google.com>
    Commit-Queue: Lawrence Choi <lchoi@google.com>

---
+++ ct_pixel_diff/frontend/res/imp/testdata.js
@@ -0,0 +1,46 @@
+"use strict"
+
+// Declares the testdata object that is a container for JS
+// testdata used by the *-demo.html pages.
+
+var testdata = testdata || {};
+
+// Runs is a list of CT Pixel Diff runIDs.
+testdata.runs = [
+  "lchoi-20170727123456",
+  "rmistry-201717202555",
+  "lchoi-20170727123456",
+  "rmistry-201717202555",
+  "lchoi-20170727123456",
+  "rmistry-201717202555",
+  "lchoi-20170727123456",
+  "rmistry-201717202555",
+  "lchoi-20170727123456",
+  "rmistry-201717202555",
+  "lchoi-20170727123456",
+  "rmistry-201717202555",
+  "lchoi-20170727123456",
+  "rmistry-201717202555",
+  "lchoi-20170727123456",
+  "rmistry-201717202555",
+  "lchoi-20170727123456",
+  "rmistry-201717202555",
+  "lchoi-20170727123456",
+  "rmistry-201717202555",
+  "lchoi-20170727123456",
+  "rmistry-201717202555",
+  "lchoi-20170727123456",
+  "rmistry-201717202555",
+  "lchoi-20170727123456",
+  "rmistry-201717202555",
+  "lchoi-20170727123456",
+  "rmistry-201717202555",
+  "lchoi-20170727123456",
+  "rmistry-201717202555",
+  "lchoi-20170727123456",
+  "rmistry-201717202555",
+  "lchoi-20170727123456",
+  "rmistry-201717202555",
+  "lchoi-20170727123456",
+  "rmistry-201717202555",
+]
