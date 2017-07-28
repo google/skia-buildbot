73c842a34bf9f6b08696dcfdfc26884ce1130336
Ravi Mistry <rmistry@google.com>
2017-07-28 11:10:36 -0400

    [CT] Use CTAutoscaler from the Poller
    Bug: skia:6648
    Change-Id: I55a1b1e128f2119ec5ac7694702359e99979c14c

--- ct/go/poller/main.go
+++ ct/go/poller/main.go
@@ -20,6 +20,7 @@ import (
 
 	"go.skia.org/infra/go/sklog"
 
+	"go.skia.org/infra/ct/go/ct_autoscaler"
 	"go.skia.org/infra/ct/go/ctfe/admin_tasks"
 	"go.skia.org/infra/ct/go/ctfe/capture_skps"
 	"go.skia.org/infra/ct/go/ctfe/chromium_analysis"
@@ -404,7 +405,7 @@ func updateWebappTaskSetFailed(task Task) error {
 // go routine. The function returns without waiting for the task to finish and the
 // WaitGroup of the goroutine is returned to the caller. The caller can then call
 // wg.Wait() if they would like to wait for the task to finish.
-func pollAndExecOnce() *sync.WaitGroup {
+func pollAndExecOnce(autoscaler ct_autoscaler.ICTAutoscaler) *sync.WaitGroup {
 	pending, err := frontend.GetOldestPendingTaskV2()
 	var wg sync.WaitGroup
 	if err != nil {
@@ -416,32 +417,39 @@ func pollAndExecOnce() *sync.WaitGroup {
 		return &wg
 	}
 
-	taskName, id := task.GetTaskName(), task.GetCommonCols().Id
+	taskId := fmt.Sprintf("%s.%d", task.GetTaskName(), task.GetCommonCols().Id)
 	tasksMtx.Lock()
-	_, exists := pickedUpTasks[fmt.Sprintf("%s.%d", taskName, id)]
+	_, exists := pickedUpTasks[taskId]
 	tasksMtx.Unlock()
 	if exists {
 		return &wg
 	}
 	tasksMtx.Lock()
-	pickedUpTasks[fmt.Sprintf("%s.%d", taskName, id)] = "1"
+	pickedUpTasks[taskId] = "1"
 	tasksMtx.Unlock()
-
-	sklog.Infof("Preparing to execute task %s %d", taskName, id)
-	if err = updateAndBuild(); err != nil {
-		sklog.Error(err)
+	if err := autoscaler.RegisterGCETask(taskId); err != nil {
+		sklog.Errorf("Error when registering GCE task in CT autoscaler: %s", err)
 		return &wg
 	}
-	sklog.Infof("Executing task %s %d", taskName, id)
+
+	sklog.Infof("Preparing to execute task %s", taskId)
+	if err = updateAndBuild(); err != nil {
+		sklog.Error(err)
+		if err := autoscaler.UnregisterGCETask(taskId); err != nil {
+			sklog.Errorf("Error when unregistering GCE task in CT autoscaler: %s", err)
+		}
+		return &wg
+	}
+	sklog.Infof("Executing task %s", taskId)
 	// Increment the WaitGroup counter.
 	wg.Add(1)
 	go func() {
 		// Decrement the counter when the goroutine completes.
 		defer wg.Done()
 		if err = task.Execute(); err == nil {
-			sklog.Infof("Completed task %s %d", taskName, id)
+			sklog.Infof("Completed task %s", taskId)
 		} else {
-			sklog.Errorf("Task %s %d failed: %v", taskName, id, err)
+			sklog.Errorf("Task %s failed: %s", taskId, err)
 			if !*dryRun {
 				if err := updateWebappTaskSetFailed(task); err != nil {
 					sklog.Error(err)
@@ -449,8 +457,11 @@ func pollAndExecOnce() *sync.WaitGroup {
 			}
 		}
 		tasksMtx.Lock()
-		delete(pickedUpTasks, fmt.Sprintf("%s.%d", taskName, id))
+		delete(pickedUpTasks, taskId)
 		tasksMtx.Unlock()
+		if err := autoscaler.UnregisterGCETask(taskId); err != nil {
+			sklog.Errorf("Error when unregistering GCE task in CT autoscaler: %s", err)
+		}
 	}()
 	// Return the WaitGroup to allow some callers to call wg.Wait()
 	return &wg
@@ -467,13 +478,17 @@ func main() {
 		})
 	}
 
+	autoscaler, err := ct_autoscaler.NewCTAutoscaler()
+	if err != nil {
+		sklog.Fatalf("Could not instantiate the CT autoscaler: %s", err)
+	}
 	healthyGauge := metrics2.GetInt64Metric("healthy")
 
 	// Run immediately, since pollTick will not fire until after pollInterval.
-	pollAndExecOnce()
+	pollAndExecOnce(autoscaler)
 	for range time.Tick(*pollInterval) {
 		healthyGauge.Update(1)
-		pollAndExecOnce()
+		pollAndExecOnce(autoscaler)
 		// Sleeping for a second to avoid the small probability of ending up
 		// with 2 tasks with the same runID. For context see
 		// https://skia-review.googlesource.com/c/26941/8/ct/go/poller/main.go#96
