d872c52db5e7ae544cc7718f3c70f8a39626d5f8
Ben Wagner <benjaminwagner@google.com>
2017-08-05 19:15:28 -0400

    Update Swarming VM setup script to install gcc-4.8.
    Bug: skia:6921
    Change-Id: If3bfd58f91d37287ab1be5ac7d5c74dac902ceaf

--- go/gce/swarming/setup-script-linux.sh
+++ go/gce/swarming/setup-script-linux.sh
@@ -4,21 +4,27 @@ set -e
 
 # Install packages.
 
-sudo apt-get -y install build-essential mercurial libosmesa-dev libexpat1-dev clang llvm poppler-utils netpbm gcc-multilib g++-multilib openjdk-8-jdk-headless libxi-dev python-django libc++-dev libc++abi-dev gperf bison usbutils
+sudo apt-get --assume-yes install build-essential mercurial libosmesa-dev libexpat1-dev clang llvm \
+  poppler-utils netpbm gcc-multilib g++-multilib openjdk-8-jdk-headless libxi-dev python-django \
+  libc++-dev libc++abi-dev gperf bison usbutils lsb-release
 
-# TODO(borenet,rmistry): apt-get update is failing due to mismatch between
-# cached apt packages files.
-sudo rm -rf /var/lib/apt/lists/*
-
-# Catapult requires a lsb-release file even if it's empty.
-# TODO(rmistry): Remove this after https://github.com/catapult-project/catapult/issues/3705
-# is resolved.
-sudo touch /etc/lsb-release
+# gcc-4.8 is only available in jessie. gcc-4.8 is required to compile for Ubuntu-14.04.
+sudo cat <<EOF >> /etc/apt/sources.list
+deb http://deb.debian.org/debian/ jessie main
+deb-src http://deb.debian.org/debian/ jessie main
+deb http://security.debian.org/ jessie/updates main
+deb-src http://security.debian.org/ jessie/updates main
+deb http://deb.debian.org/debian/ jessie-updates main
+deb-src http://deb.debian.org/debian/ jessie-updates main
+EOF
+sudo apt-get update
+sudo apt-get --assume-yes install gcc-4.8 g++-4.8
 
 # Obtain and symlink i386 libs.
 sudo dpkg --add-architecture i386
 sudo apt-get update
-sudo apt-get -y install libfreetype6:i386 libfontconfig1:i386 libgl1-mesa-glx:i386 libglu1-mesa:i386 libx11-6:i386 libxext-dev:i386
+sudo apt-get --assume-yes install libfreetype6:i386 libfontconfig1:i386 libgl1-mesa-glx:i386 \
+  libglu1-mesa:i386 libx11-6:i386 libxext-dev:i386
 sudo ln -sfn /usr/lib/i386-linux-gnu/libfreetype.so.6 /usr/lib/i386-linux-gnu/libfreetype.so
 sudo ln -sfn /usr/lib/i386-linux-gnu/libfontconfig.so.1 /usr/lib/i386-linux-gnu/libfontconfig.so
 sudo ln -sfn /usr/lib/i386-linux-gnu/libGLU.so.1 /usr/lib/i386-linux-gnu/libGLU.so
@@ -28,7 +34,7 @@ sudo ln -sfn /usr/lib/i386-linux-gnu/libX11.so.6.3.0 /usr/lib/i386-linux-gnu/lib
 # MySQL setup.
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password tmp_pass'
 sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password tmp_pass'
-sudo apt-get -y install mysql-client mysql-server
+sudo apt-get --assume-yes install mysql-client mysql-server
 sudo mysql -uroot -ptmp_pass -e "SET PASSWORD = PASSWORD('');"
 cat <<EOF | sudo tee --append /etc/mysql/my.cnf
 
@@ -40,7 +46,7 @@ EOF
 
 # NodeJS / NPM.
 curl -sL https://deb.nodesource.com/setup_6.x | sudo bash -
-sudo apt-get -y install nodejs
+sudo apt-get --assume-yes install nodejs
 sudo npm install -g npm@3.10.9
 sudo npm install -g bower@1.6.5
 sudo npm install -g polylint@2.10.4
@@ -55,7 +61,8 @@ sudo sh -c "echo '* - nofile 500000' >> /etc/security/limits.conf"
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 mkdir -p ~/.config/google-chrome
 touch ~/.config/google-chrome/First\\ Run
-sudo dpkg -i google-chrome-stable_current_amd64.deb || sudo apt-get -f -y install
+sudo dpkg -i google-chrome-stable_current_amd64.deb || \
+  sudo apt-get --fix-broken --assume-yes install
 rm google-chrome-stable_current_amd64.deb
 
 # Fix depot_tools.
@@ -72,7 +79,9 @@ ls | grep --invert-match 'lost+found' | \
 popd
 
 # Get access token from metadata.
-TOKEN=`curl "http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token" -H "Metadata-Flavor: Google" | python -c "import sys, json; print json.load(sys.stdin)['access_token']"`
+TOKEN_URL="http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token"
+TOKEN="$(curl "${TOKEN_URL}" -H "Metadata-Flavor: Google" | \
+  python -c "import sys, json; print json.load(sys.stdin)['access_token']")"
 # Bootstrap Swarming.
 sudo ln -s /mnt/pd0 /b
 mkdir -p /b/s
@@ -81,7 +90,8 @@ if [[ $(hostname) == *"-i-"* ]]; then
   SWARMING=https://chrome-swarming.appspot.com
 fi
 HOSTNAME=`hostname`
-curl ${SWARMING}/bot_code?bot_id=$HOSTNAME -H "Authorization":"Bearer $TOKEN" -o /b/s/swarming_bot.zip
+curl ${SWARMING}/bot_code?bot_id=$HOSTNAME -H "Authorization":"Bearer $TOKEN" \
+  -o /b/s/swarming_bot.zip
 ln -sfn /b/s /b/swarm_slave
 
 cat <<EOF | sudo tee /etc/systemd/system/swarming_bot.service
