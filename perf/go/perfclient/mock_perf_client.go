19802d29c8a446db2b0faf64e68d9f67548931c3
Kevin Lubick <kjlubick@google.com>
2017-08-11 11:24:45 -0400

    Add perf client
    Bug: skia:6712
    Change-Id: I521ff19adbcd13404a9b98292d38bf8132bccd1c

---
+++ perf/go/perfclient/mock_perf_client.go
@@ -0,0 +1,26 @@
+package perfclient
+
+import (
+	"time"
+
+	"github.com/stretchr/testify/mock"
+	"go.skia.org/infra/perf/go/ingestcommon"
+)
+
+type MockPerfClient struct {
+	mock.Mock
+}
+
+// NewMockPerfClient returns a pointer to a newly created struct.  We return the pointer because we want to
+// make sure the methods on mock.Mock stay accessible, e.g. m.On()
+func NewMockPerfClient() *MockPerfClient {
+	return &MockPerfClient{}
+}
+
+func (m *MockPerfClient) PushToPerf(now time.Time, folderName, filePrefix string, data ingestcommon.BenchData) error {
+	args := m.Called(now, folderName, filePrefix, data)
+	return args.Error(0)
+}
+
+// Ensure MockPerfClient fulfills ClientInterface
+var _ ClientInterface = (*MockPerfClient)(nil)
