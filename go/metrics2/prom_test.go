dcf1e5494a1e9364c8b9e8831366841c10106686
Eric Boren <borenet@google.com>
2017-08-14 13:42:38 -0400

    [metrics] Add delete implementation for prometheus metrics
    Bug: skia:6729
    Change-Id: I5ed0bafc8f5499b6238e607d9bb3baffee6d0243

--- go/metrics2/prom_test.go
+++ go/metrics2/prom_test.go
@@ -1,13 +1,19 @@
 package metrics2
 
 import (
+	"io/ioutil"
+	"net/http/httptest"
+	"strconv"
+	"strings"
 	"testing"
 
 	"go.skia.org/infra/go/testutils"
+	"go.skia.org/infra/go/util"
 
-	"github.com/stretchr/testify/assert"
+	assert "github.com/stretchr/testify/require"
 
 	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func TestClean(t *testing.T) {
@@ -20,79 +26,136 @@ func getPromClient() *promClient {
 	return newPromClient()
 }
 
+func get(t *testing.T, metric string) string {
+	req := httptest.NewRequest("GET", "/metrics", nil)
+	rw := httptest.NewRecorder()
+	promhttp.HandlerFor(prometheus.DefaultRegisterer.(*prometheus.Registry), promhttp.HandlerOpts{
+		ErrorLog:           nil,
+		ErrorHandling:      promhttp.PanicOnError,
+		DisableCompression: true,
+	}).ServeHTTP(rw, req)
+	resp := rw.Result()
+	defer util.Close(resp.Body)
+	b, err := ioutil.ReadAll(resp.Body)
+	assert.NoError(t, err)
+	for _, s := range strings.Split(string(b), "\n") {
+		if strings.HasPrefix(s, metric) {
+			return strings.Split(s, " ")[1]
+		}
+	}
+	return ""
+}
+
 func TestInt64(t *testing.T) {
 	testutils.SmallTest(t)
 	c := getPromClient()
+	check := func(m Int64Metric, metric string, expect int64) {
+		actual, err := strconv.ParseInt(get(t, metric), 10, 64)
+		assert.NoError(t, err)
+		assert.Equal(t, expect, actual)
+		assert.Equal(t, m.Get(), expect)
+	}
 	g := c.GetInt64Metric("a.b", map[string]string{"some_key": "some-value"})
 	assert.NotNil(t, g)
 	assert.NotNil(t, c.int64GaugeVecs["a_b [some_key]"])
 	assert.NotNil(t, c.int64Gauges["a_b-some_key-some-value"])
 	assert.Nil(t, c.int64GaugeVecs["a.b"])
+	check(g, "a_b{some_key=\"some-value\"}", 0)
 
 	g.Update(3)
-	assert.Equal(t, int64(3), g.Get())
+	check(g, "a_b{some_key=\"some-value\"}", 3)
 
 	g2 := c.GetInt64Metric("a.b", map[string]string{"some_key": "some-new-value"})
 	assert.NotNil(t, g2)
 	g2.Update(4)
 
-	assert.Equal(t, int64(3), g.Get())
-	assert.Equal(t, int64(4), g2.Get())
+	check(g, "a_b{some_key=\"some-value\"}", 3)
+	check(g2, "a_b{some_key=\"some-new-value\"}", 4)
 
 	g2 = c.GetInt64Metric("a.b", map[string]string{"some_key": "some-new-value"})
-	assert.Equal(t, int64(4), g2.Get())
+	check(g2, "a_b{some_key=\"some-new-value\"}", 4)
 
 	// Metric with two tags.
 	g = c.GetInt64Metric("metric_name", map[string]string{"a": "2", "b": "1"})
 	assert.NotNil(t, g)
 	assert.NotNil(t, c.int64GaugeVecs["metric_name [a b]"])
 	assert.NotNil(t, c.int64Gauges["metric_name-a-2-b-1"])
+	check(g, "metric_name{a=\"2\",b=\"1\"}", 0)
+
+	// Test delete.
+	assert.NoError(t, g.Delete())
+	assert.Equal(t, "", get(t, "a_c{some_key=\"some-new-value\"}"))
 }
 
 func TestFloat64(t *testing.T) {
 	testutils.SmallTest(t)
 	c := getPromClient()
+	check := func(m Float64Metric, metric string, expect float64) {
+		actual, err := strconv.ParseFloat(get(t, metric), 64)
+		assert.NoError(t, err)
+		assert.Equal(t, expect, actual)
+		assert.Equal(t, m.Get(), expect)
+	}
 	g := c.GetFloat64Metric("a.c", map[string]string{"some_key": "some-value"})
 	assert.NotNil(t, g)
 	assert.NotNil(t, c.float64GaugeVecs["a_c [some_key]"])
 	assert.NotNil(t, c.float64Gauges["a_c-some_key-some-value"])
 	assert.Nil(t, c.float64GaugeVecs["a.c"])
+	check(g, "a_c{some_key=\"some-value\"}", 0.0)
 
 	g.Update(3)
-	assert.Equal(t, float64(3), g.Get())
+	check(g, "a_c{some_key=\"some-value\"}", 3.0)
 
 	g2 := c.GetFloat64Metric("a.c", map[string]string{"some_key": "some-new-value"})
 	assert.NotNil(t, g2)
 	g2.Update(4)
 
-	assert.Equal(t, float64(3), g.Get())
-	assert.Equal(t, float64(4), g2.Get())
+	check(g, "a_c{some_key=\"some-value\"}", 3.0)
+	check(g2, "a_c{some_key=\"some-new-value\"}", 4.0)
 
 	g2 = c.GetFloat64Metric("a.c", map[string]string{"some_key": "some-new-value"})
-	assert.Equal(t, float64(4), g2.Get())
+	check(g2, "a_c{some_key=\"some-new-value\"}", 4.0)
 
 	// Metric with two tags.
 	g = c.GetFloat64Metric("float_metric_name", map[string]string{"a": "2", "b": "1"})
 	assert.NotNil(t, g)
 	assert.NotNil(t, c.float64GaugeVecs["float_metric_name [a b]"])
 	assert.NotNil(t, c.float64Gauges["float_metric_name-a-2-b-1"])
+	check(g, "float_metric_name{a=\"2\",b=\"1\"}", 0.0)
+
+	// Test delete.
+	assert.NoError(t, g.Delete())
+	assert.Equal(t, "", get(t, "float_metric_name{a=\"2\",b=\"1\"}"))
 }
 
 func TestCounter(t *testing.T) {
 	testutils.SmallTest(t)
 	c := getPromClient()
+	check := func(m Counter, metric string, expect int64) {
+		actual, err := strconv.ParseInt(get(t, metric), 10, 64)
+		assert.NoError(t, err)
+		assert.Equal(t, expect, actual)
+		assert.Equal(t, m.Get(), expect)
+	}
 	g := c.GetCounter("c", map[string]string{"some_key": "some-value"})
 	assert.NotNil(t, g)
 
 	g.Inc(3)
 	g = c.GetCounter("c", map[string]string{"some_key": "some-value"})
+	check(g, "c{some_key=\"some-value\"}", 3)
 	assert.Equal(t, int64(3), g.Get())
 
 	g.Dec(2)
+	check(g, "c{some_key=\"some-value\"}", 1)
 	assert.Equal(t, int64(1), g.Get())
 
 	g.Reset()
+	check(g, "c{some_key=\"some-value\"}", 0)
 	assert.Equal(t, int64(0), g.Get())
+
+	// Test delete.
+	assert.NoError(t, g.Delete())
+	assert.Equal(t, "", get(t, "c{some_key=\"some-value\"}"))
 }
 
 func TestPanicOn(t *testing.T) {
