236cad4d28eca2941a5076d17d7a2fa316f2440a
Ravi Mistry <rmistry@google.com>
2017-08-23 13:58:14 -0400

    Use ephemeral IPs in perf and android_ingest
    Bug: skia:6745
    Change-Id: I68de3ae04741e623a51f4d3c78e70ca72eb941ea

--- perf/vm.go
+++ perf/vm.go
@@ -7,12 +7,11 @@ import (
 	"go.skia.org/infra/go/gce/server"
 )
 
-func PerfBase(name, ipAddress string) *gce.Instance {
+func PerfBase(name string) *gce.Instance {
 	vm := server.AddGitConfigs(server.Server20170613(name), "skia-perf")
 	vm.DataDisk.Name = fmt.Sprintf("%s-ssd-data", name)
 	vm.DataDisk.SizeGb = 1000
 	vm.DataDisk.Type = gce.DISK_TYPE_PERSISTENT_SSD
-	vm.ExternalIpAddress = ipAddress
 	vm.MachineType = gce.MACHINE_TYPE_STANDARD_32
 	vm.Metadata["owner_primary"] = "jcgregorio"
 	vm.Metadata["owner_secondary"] = "stephana"
@@ -20,15 +19,15 @@ func PerfBase(name, ipAddress string) *gce.Instance {
 }
 
 func Prod() *gce.Instance {
-	return PerfBase("skia-perf", "104.154.112.108")
+	return PerfBase("skia-perf")
 }
 
 func AndroidMaster() *gce.Instance {
-	return PerfBase("skia-android-master-perf", "104.154.112.139")
+	return PerfBase("skia-android-master-perf")
 }
 
 func Android() *gce.Instance {
-	return PerfBase("skia-android-perf", "104.154.112.137")
+	return PerfBase("skia-android-perf")
 }
 
 func main() {
