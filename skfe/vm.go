2d3f6c42f01c5a61e4895054edb0692d33a66734
Ravi Mistry <rmistry@google.com>
2017-08-28 13:57:07 -0400

    Use ephemeral IPs for skfe-{1,2}
    Bug: skia:6745
    Change-Id: Iabd18d033845da02650f97653d2cb8b95330a2d8

--- skfe/vm.go
+++ skfe/vm.go
@@ -7,23 +7,22 @@ import (
 	"go.skia.org/infra/go/gce/server"
 )
 
-func SKFEBase(name, ipAddress string) *gce.Instance {
+func SKFEBase(name string) *gce.Instance {
 	vm := server.Server20170613(name)
 	vm.DataDisk = nil
-	vm.ExternalIpAddress = ipAddress
 	vm.MachineType = gce.MACHINE_TYPE_STANDARD_4
 	vm.Metadata["owner_primary"] = "stephana"
 	vm.Metadata["owner_secondary"] = "jcgregorio"
 	return vm
 }
 
-func Prod(num int, ip string) *gce.Instance {
-	return SKFEBase(fmt.Sprintf("skia-skfe-%d", num), ip)
+func Prod(num int) *gce.Instance {
+	return SKFEBase(fmt.Sprintf("skia-skfe-%d", num))
 }
 
 func main() {
 	server.Main(gce.ZONE_DEFAULT, map[string]*gce.Instance{
-		"prod-1": Prod(1, "104.154.112.11"),
-		"prod-2": Prod(2, "104.154.112.103"),
+		"prod-1": Prod(1),
+		"prod-2": Prod(2),
 	})
 }
