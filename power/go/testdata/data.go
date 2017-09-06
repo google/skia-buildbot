4466753ca53b9fdfda0b5b6a6dd7183ed0f2af72
Kevin Lubick <kjlubick@google.com>
2017-09-06 09:33:59 -0400

    Fix powercycle for booting devices
    Booting devices was a state added in a recent py-adb update.
    
    Bug: skia:
    Change-Id: Ib10c274747a4f0f715829d87f796a4633075efb6

--- power/go/testdata/data.go
+++ power/go/testdata/data.go
@@ -12,8 +12,9 @@ import (
 // The JSON in these files was recieved from calls to
 // https://chromium-swarm.appspot.com/_ah/api/swarming/v1/bot/[bot]/get
 // when bots were displaying interesting behavior
-var DEAD_BOT = "dead_bot.json"
+var BOOTING_DEVICE = "booting_device.json"
 var DEAD_AND_QUARANTINED = "dead_and_quarantined.json"
+var DEAD_BOT = "dead_bot.json"
 var DELETED_BOT = "deleted_bot.json"
 var MISSING_DEVICE = "missing_device.json"
 var TOO_HOT = "too_hot.json"
