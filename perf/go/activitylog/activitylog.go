b829f1edf9b47894aa2bb2ed3631c3c3ac586cf0
Joe Gregorio <jcgregorio@google.com>
2017-09-11 09:23:39 -0400

    [perf] Move activitylog optionally over to Cloud Datastore.
    Bug: skia:
    Change-Id: I3a5c078ad67a21f0d05f689d5babaec7091d9363
    Reviewed-on: https://skia-review.googlesource.com/44720
    Reviewed-by: Joe Gregorio <jcgregorio@google.com>
    Commit-Queue: Joe Gregorio <jcgregorio@google.com>

--- perf/go/activitylog/activitylog.go
+++ perf/go/activitylog/activitylog.go
@@ -2,19 +2,33 @@
 package activitylog
 
 import (
+	"context"
 	"fmt"
 	"time"
 
 	"go.skia.org/infra/go/sklog"
+	"google.golang.org/api/iterator"
 
 	"go.skia.org/infra/go/util"
 	"go.skia.org/infra/perf/go/db"
+	"go.skia.org/infra/perf/go/ds"
 )
+
+var (
+	useCloudDatastore bool
+)
+
+// Init initializes activitylog.
+//
+// useDS - Is true if activities should be store in Google Cloud Datastore, otherwise they're stored in Cloud SQL.
+func Init(useDS bool) {
+	useCloudDatastore = useDS
+}
 
 // Activity stores information on one user action activity. This corresponds to
 // one record in the activity database table. See DESIGN.md for details.
 type Activity struct {
-	ID     int
+	ID     int64
 	TS     int64
 	UserID string
 	Action string
@@ -30,46 +44,75 @@ func (a *Activity) Date() string {
 // Input is in types.Activity format, but ID and TS are ignored. Instead, always
 // use autoincrement ID and the current timestamp for the new record.
 func Write(r *Activity) error {
-	sklog.Infof("Write activity: %s\n", r)
-	if r.UserID == "" || r.Action == "" {
-		return fmt.Errorf("Activity UserID and Action cannot be empty: %v\n", r)
+	if useCloudDatastore {
+		r.TS = time.Now().Unix()
+		key := ds.NewKey(ds.ACTIVITY)
+		if _, err := ds.DS.Put(context.Background(), key, r); err != nil {
+			return fmt.Errorf("Failed to store activity: %s", err)
+		}
+		return nil
+	} else {
+		// TODO(jcgregorio) Delete this code after the migration away from MySQL.
+		sklog.Infof("Write activity: %s\n", r)
+		if r.UserID == "" || r.Action == "" {
+			return fmt.Errorf("Activity UserID and Action cannot be empty: %v\n", r)
+		}
+		_, err := db.DB.Exec(
+			"INSERT INTO activitylog (timestamp, userid, action, url) VALUES (?, ?, ?, ?)",
+			time.Now().Unix(), r.UserID, r.Action, r.URL)
+		if err != nil {
+			return fmt.Errorf("Failed to write to database: %s", err)
+		}
+		return nil
 	}
-	_, err := db.DB.Exec(
-		"INSERT INTO activitylog (timestamp, userid, action, url) VALUES (?, ?, ?, ?)",
-		time.Now().Unix(), r.UserID, r.Action, r.URL)
-	if err != nil {
-		return fmt.Errorf("Failed to write to database: %s", err)
-	}
-	return nil
 }
 
 // GetRecent returns the most recent n activity records in Activity struct format.
 func GetRecent(n int) ([]*Activity, error) {
-	ret := []*Activity{}
-	rows, err := db.DB.Query("SELECT id, timestamp, userid, action, url FROM activitylog ORDER BY id DESC LIMIT ?", n)
-	if err != nil {
-		return nil, fmt.Errorf("Failed to read from database: %s", err)
-	}
-	defer util.Close(rows)
-	sklog.Infoln("Processing activity rows.")
-	for rows.Next() {
-		var id int
-		var timestamp int64
-		var userid string
-		var action string
-		var url string
-		if err := rows.Scan(&id, &timestamp, &userid, &action, &url); err != nil {
-			return nil, fmt.Errorf("Failed to read row from database: %s", err)
+	if useCloudDatastore {
+		ret := []*Activity{}
+		q := ds.NewQuery(ds.ACTIVITY).EventualConsistency().Limit(n).Order("-TS")
+		it := ds.DS.Run(context.Background(), q)
+		for {
+			a := &Activity{}
+			k, err := it.Next(a)
+			if err == iterator.Done {
+				break
+			} else if err != nil {
+				return nil, fmt.Errorf("Failed retrieving activity list: %s", err)
+			}
+			a.ID = k.ID
+			ret = append(ret, a)
 		}
-		r := &Activity{
-			ID:     id,
-			TS:     timestamp,
-			UserID: userid,
-			Action: action,
-			URL:    url,
+		return ret, nil
+	} else {
+		// TODO(jcgregorio) Delete this code after the migration away from MySQL.
+		ret := []*Activity{}
+		rows, err := db.DB.Query("SELECT id, timestamp, userid, action, url FROM activitylog ORDER BY id DESC LIMIT ?", n)
+		if err != nil {
+			return nil, fmt.Errorf("Failed to read from database: %s", err)
 		}
-		ret = append(ret, r)
-	}
+		defer util.Close(rows)
+		sklog.Infoln("Processing activity rows.")
+		for rows.Next() {
+			var id int
+			var timestamp int64
+			var userid string
+			var action string
+			var url string
+			if err := rows.Scan(&id, &timestamp, &userid, &action, &url); err != nil {
+				return nil, fmt.Errorf("Failed to read row from database: %s", err)
+			}
+			r := &Activity{
+				ID:     int64(id),
+				TS:     timestamp,
+				UserID: userid,
+				Action: action,
+				URL:    url,
+			}
+			ret = append(ret, r)
+		}
 
-	return ret, nil
+		return ret, nil
+	}
 }
