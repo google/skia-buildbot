f972089950a0d405c7643aa05d8742f19dcd000d
Joe Gregorio <jcgregorio@google.com>
2017-09-12 16:25:23 -0400

    [perf] Add tests against the cloud datastore emulator.
    Note that these won't run on the bots yet, but they won't fail because
    on failing to find the right environment variable it will t.Skip() the
    tests.
    
    Bug: skia:
    Change-Id: I62da80db576bbd8ecdc1fa9b1b2310575f7cd6cf
    Reviewed-on: https://skia-review.googlesource.com/45881
    Commit-Queue: Joe Gregorio <jcgregorio@google.com>
    Reviewed-by: Ben Wagner <benjaminwagner@google.com>

---
+++ perf/go/regression/db_datastore_test.go
@@ -0,0 +1,99 @@
+package regression
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"go.skia.org/infra/go/testutils"
+	"go.skia.org/infra/perf/go/cid"
+	"go.skia.org/infra/perf/go/clustering2"
+	"go.skia.org/infra/perf/go/dataframe"
+	"go.skia.org/infra/perf/go/ds"
+	"go.skia.org/infra/perf/go/dstestutil"
+)
+
+// TestDS test storing regressions in the datastore.
+func TestDS(t *testing.T) {
+	testutils.MediumTest(t)
+
+	cleanup := dstestutil.InitDatastore(t, ds.REGRESSION)
+
+	Init(true)
+	defer Init(false)
+	defer cleanup()
+
+	st := NewStore()
+
+	r := New()
+	c := &cid.CommitDetail{
+		CommitID: cid.CommitID{
+			Source: "master",
+			Offset: 1,
+		},
+		Timestamp: 1479235651,
+	}
+
+	// Test Regressions.
+	df := &dataframe.FrameResponse{}
+	cl := &clustering2.ClusterSummary{}
+	r.SetLow("foo", df, cl)
+	_, ok := r.ByAlertID["foo"]
+	assert.True(t, ok)
+	assert.False(t, r.Triaged())
+
+	// Test store.
+
+	// Create a new regression.
+	isNew, err := st.SetLow(c, "foo", df, cl)
+	assert.True(t, isNew)
+	assert.NoError(t, err)
+
+	// Overwrite a regression.
+	isNew, err = st.SetLow(c, "foo", df, cl)
+	assert.False(t, isNew)
+	assert.NoError(t, err)
+
+	// Confirm new regression is present.
+	ranges, err := st.Range(0, 0, UNTRIAGED_SUBSET)
+	assert.NoError(t, err)
+	assert.Len(t, ranges, 1)
+
+	count, err := st.Untriaged()
+	assert.Equal(t, count, 1)
+
+	// Triage existing regression.
+	tr := TriageStatus{
+		Status:  POSITIVE,
+		Message: "bad",
+	}
+	err = st.TriageLow(c, "foo", tr)
+	assert.NoError(t, err)
+
+	// Confirm regression is triaged.
+	ranges, err = st.Range(0, 0, UNTRIAGED_SUBSET)
+	assert.NoError(t, err)
+	assert.Len(t, ranges, 0)
+
+	count, err = st.Untriaged()
+	assert.Equal(t, count, 0)
+
+	now := time.Unix(c.Timestamp, 0)
+	begin := now.Add(-time.Hour).Unix()
+	end := now.Add(time.Hour).Unix()
+
+	ranges, err = st.Range(begin, end, ALL_SUBSET)
+	assert.NoError(t, err)
+	assert.Len(t, ranges, 1)
+
+	// Try triaging a regression that doesn't exist.
+	err = st.TriageHigh(c, "bar", tr)
+	assert.Error(t, err)
+
+	ranges, err = st.Range(begin, end, ALL_SUBSET)
+	assert.NoError(t, err)
+	assert.Len(t, ranges, 1)
+
+	count, err = st.Untriaged()
+	assert.Equal(t, count, 0)
+}
