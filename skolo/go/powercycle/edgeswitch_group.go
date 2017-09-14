644c44705cf128f3e919b3b6c970fd8a22e0d15b
Kevin Lubick <kjlubick@google.com>
2017-09-14 10:17:44 -0400

    [skolo] Add censustaker, which maps bots and edgeswitch ports
    Right now, it just prints out a list, but in a future CL, it will
    update the powercycle config.
    
    Bug: skia:
    Change-Id: Ic2d065846e84bfe014c39a8992538f0ea26e4367

---
+++ skolo/go/powercycle/edgeswitch_group.go
@@ -0,0 +1,161 @@
+package powercycle
+
+import (
+	"fmt"
+	"sort"
+	"strconv"
+	"strings"
+	"time"
+
+	"go.skia.org/infra/go/sklog"
+	"go.skia.org/infra/go/util"
+)
+
+const (
+	// Number of seconds to wait between turning a port on and off again.
+	EDGE_SWITCH_DELAY = 5
+)
+
+// EdgeSwitchConfig contains configuration options for a single EdgeSwitch.
+// Note: We assume the device is on a trusted network.
+type EdgeSwitchConfig struct {
+	Address    string         `json:"address"` // IP address and port of the device, i.e. 192.168.1.33:22
+	DevPortMap map[string]int `json:"ports"`   // Mapping between device name and port on the power strip.
+}
+
+// EdgeSwitchDevGroup implements the DeviceGroup interface.
+type EdgeSwitchDevGroup struct {
+	conf       *EdgeSwitchConfig
+	portDevMap map[int]string
+	devIDs     []string
+	client     *EdgeSwitchClient
+}
+
+// NewEdgeSwitchDevGroup connects to the EdgeSwitch identified by the given
+// configuration and returns a new instance of EdgeSwitchDevGroup.
+func NewEdgeSwitchDevGroup(conf *EdgeSwitchConfig, connect bool) (*EdgeSwitchDevGroup, error) {
+	ret := &EdgeSwitchDevGroup{
+		conf:   conf,
+		client: NewEdgeSwitchClient(conf.Address),
+	}
+
+	if connect {
+		if err := Ping(ret.client); err != nil {
+			return nil, err
+		}
+	}
+
+	// Build the dev-port mappings. Ensure each device and port occur only once.
+	devIDSet := make(util.StringSet, len(conf.DevPortMap))
+	ret.portDevMap = make(map[int]string, len(conf.DevPortMap))
+	for id, port := range conf.DevPortMap {
+		if devIDSet[id] {
+			return nil, fmt.Errorf("Device '%s' occurs more than once.", id)
+		}
+		if _, ok := ret.portDevMap[port]; ok {
+			return nil, fmt.Errorf("Port '%d' specified more than once.", port)
+		}
+		devIDSet[id] = true
+		ret.portDevMap[port] = id
+	}
+	ret.devIDs = devIDSet.Keys()
+	sort.Strings(ret.devIDs)
+
+	return ret, nil
+}
+
+// DeviceIDs, see the DeviceGroup interface.
+func (e *EdgeSwitchDevGroup) DeviceIDs() []string {
+	return e.devIDs
+}
+
+// PowerCycle, see the DeviceGroup interface.
+func (e *EdgeSwitchDevGroup) PowerCycle(devID string, delayOverride time.Duration) error {
+	delay := EDGE_SWITCH_DELAY * time.Second
+	if delayOverride > 0 {
+		delay = delayOverride
+	}
+
+	port, ok := e.conf.DevPortMap[devID]
+	if !ok {
+		return fmt.Errorf("Invalid port: %d", port)
+	}
+
+	// Turn the given port off, wait and then on again.
+	if err := TurnOffPort(e.client, port); err != nil {
+		return err
+	}
+
+	time.Sleep(delay)
+
+	if err := TurnOnPort(e.client, port); err != nil {
+		return err
+	}
+	return nil
+}
+
+// PowerUsage, see the DeviceGroup interface.
+func (e *EdgeSwitchDevGroup) PowerUsage() (*GroupPowerUsage, error) {
+	outputLines, err := e.client.ExecCmds([]string{
+		"show poe status all",
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	ret := &GroupPowerUsage{
+		TS: time.Now(),
+	}
+	ret.Stats = make(map[string]*PowerStat, len(outputLines))
+	// only consider lines like:
+	// Intf      Detection      Class   Consumed(W) Voltage(V) Current(mA) Temperature(C)
+	// 0/6       Good           Class3         1.93      52.82       36.62             45
+	for _, oneLine := range outputLines {
+		fields := strings.Fields(oneLine)
+		if (len(fields) < 7) || (len(fields[0]) < 3) || (fields[0][1] != '/') {
+			continue
+		}
+
+		stat := &PowerStat{}
+		var err error = nil
+		last := len(fields)
+		stat.Ampere = parseFloat(&err, fields[last-2])
+		stat.Volt = parseFloat(&err, fields[last-3])
+		stat.Watt = parseFloat(&err, fields[last-4])
+		port := parseInt(&err, fields[0][2:])
+
+		if err != nil {
+			sklog.Errorf("Error: %s", err)
+			continue
+		}
+
+		devID, ok := e.portDevMap[port]
+
+		if !ok {
+			continue
+		}
+
+		sklog.Infof("Found port %d and dev '%s'", port, devID)
+		ret.Stats[devID] = stat
+	}
+
+	return ret, nil
+}
+
+func parseFloat(err *error, strVal string) float32 {
+	if *err != nil {
+		return 0
+	}
+	var ret float64
+	ret, *err = strconv.ParseFloat(strVal, 32)
+	return float32(ret)
+}
+
+func parseInt(err *error, strVal string) int {
+	if *err != nil {
+		return 0
+	}
+	var ret int64
+	ret, *err = strconv.ParseInt(strVal, 10, 32)
+	return int(ret)
+}
