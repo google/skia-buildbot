b08fb7a47ab2d5a20a766a9236beee5868d6eca6
Stephan Altmueller <stephana@google.com>
2017-09-15 10:08:01 -0400

    WIP: gold cts
    Bug: skia:
    Change-Id: I362030ab99058eb4ec763fa8deb6c5f0867be3ac

---
+++ golden/go/tsuite/build.go
@@ -0,0 +1,11 @@
+package tsuite
+
+import (
+	"go.skia.org/infra/go/tiling"
+	"go.skia.org/infra/golden/go/storage"
+)
+
+func ExtractKnowledge(tile *tiling.Tile, storages *storage.Storage) error {
+
+	return nil
+}
