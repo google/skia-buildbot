dfa264661d438c6eb57d1ccc368afd82052ef38e
Ravi Mistry <rmistry@google.com>
2017-09-20 08:53:00 -0400

    Create utility for zipping and un-zipping
    Will be used by CT to zip/unzip Chromium builds
    
    Bug: skia:7043
    Change-Id: I8834920414eceb06266c27ed25df173dc229d3c2

---
+++ go/util/zip.go
@@ -0,0 +1,96 @@
+package util
+
+import (
+	"archive/zip"
+	"io"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+// UnZip unzips the file specified in src into the 'dest' directory.
+func UnZip(src, dest string) error {
+	r, err := zip.OpenReader(src)
+	if err != nil {
+		return err
+	}
+	defer Close(r)
+
+	for _, f := range r.File {
+		rc, err := f.Open()
+		if err != nil {
+			return err
+		}
+		defer Close(rc)
+
+		path := filepath.Join(dest, f.Name)
+		if f.FileInfo().IsDir() {
+			if err := os.MkdirAll(path, f.Mode()); err != nil {
+				return err
+			}
+		} else {
+			f, err := os.OpenFile(path, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+			if err != nil {
+				return err
+			}
+			defer Close(f)
+
+			_, err = io.Copy(f, rc)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+// ZipIt zips the specified directory into the target file.
+func ZipIt(source, target string) error {
+	zipfile, err := os.Create(target)
+	if err != nil {
+		return err
+	}
+	defer zipfile.Close()
+
+	archive := zip.NewWriter(zipfile)
+	defer archive.Close()
+
+	err = filepath.Walk(source, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		header, err := zip.FileInfoHeader(info)
+		if err != nil {
+			return err
+		}
+
+		header.Name = filepath.Join(".", strings.TrimPrefix(path, source))
+
+		if info.IsDir() {
+			header.Name += "/"
+		} else {
+			header.Method = zip.Deflate
+		}
+
+		writer, err := archive.CreateHeader(header)
+		if err != nil {
+			return err
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+
+		file, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+		defer file.Close()
+		_, err = io.Copy(writer, file)
+		return err
+	})
+
+	return err
+}
---
+++ go/util/zip_test.go
@@ -0,0 +1,64 @@
+package util
+
+import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"go.skia.org/infra/go/testutils"
+
+	assert "github.com/stretchr/testify/require"
+)
+
+func createFile(dir, prefix, content string, t *testing.T) *os.File {
+	f, err := ioutil.TempFile(dir, prefix)
+	assert.Nil(t, err)
+	_, err = f.WriteString(content)
+	assert.Nil(t, err)
+	return f
+}
+
+func assertFileExists(dir, path, content string, t *testing.T) {
+	c, err := ioutil.ReadFile(filepath.Join(dir, filepath.Base(path)))
+	assert.Nil(t, err)
+	assert.Equal(t, content, string(c))
+}
+
+func TestZipE2E(t *testing.T) {
+	testutils.MediumTest(t)
+
+	// Create a directory in temp.
+	targetDir, err := ioutil.TempDir("", "zip_test")
+	assert.Nil(t, err)
+	defer RemoveAll(targetDir)
+
+	// Populate the target dir.
+	// Create two files in target dir.
+	f1 := createFile(targetDir, "temp1", "testing1", t)
+	f2 := createFile(targetDir, "temp2", "testing2", t)
+	// Create subdir.
+	subDir, err := ioutil.TempDir(targetDir, "zip_test")
+	assert.Nil(t, err)
+	// Create one file in subdir.
+	f3 := createFile(subDir, "temp3", "testing3", t)
+
+	// Zip the directory.
+	outputDir, err := ioutil.TempDir("", "zip_location")
+	defer RemoveAll(outputDir)
+	zipPath := filepath.Join(outputDir, "test.zip")
+	err = ZipIt(targetDir, zipPath)
+	assert.Nil(t, err)
+	// Assert that zip was created
+	_, err = os.Stat(zipPath)
+	assert.Nil(t, err)
+	assert.False(t, os.IsNotExist(err))
+
+	// Test UnZipping.
+	err = UnZip(zipPath, outputDir)
+	assert.Nil(t, err)
+	// Assert that the 3 zipped files are in the right locations.
+	assertFileExists(outputDir, f1.Name(), "testing1", t)
+	assertFileExists(outputDir, f2.Name(), "testing2", t)
+	assertFileExists(filepath.Join(outputDir, filepath.Base(subDir)), f3.Name(), "testing3", t)
+}
