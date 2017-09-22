e9ecd135c688554e61c9206df5f6388ab54e46ca
Stephan Altmueller <stephana@google.com>
2017-09-22 16:58:59 -0400

    Resolve SHA for deps rolled commits
    Bug: skia:
    Change-Id: I84f04c4ce6ddee2d432770d127ebf2b1f7978f59

--- go/git/gitinfo/gitinfo_test.go
+++ go/git/gitinfo/gitinfo_test.go
@@ -310,7 +310,7 @@ Author: Joe Gregorio <jcgregorio@google.com>
 Date:   Wed Jul 30 08:00:42 2014 -0400
 
     First "checkin"
-    
+
     With quotes.
 
 README.txt
@@ -625,4 +625,21 @@ func TestSetBranch(t *testing.T) {
 	commits := r.LastN(10)
 	assert.Equal(t, 3, len(commits))
 	assert.Equal(t, "3f5a807d432ac232a952bbf223bc6952e4b49b2c", commits[2])
+}
+
+func TestExtractDEPS(t *testing.T) {
+	tr := util.NewTempRepo()
+	defer tr.Cleanup()
+
+	r, err := NewGitInfo(filepath.Join(tr.Dir, "testrepo"), false, false)
+	assert.NoError(t, err)
+
+	// Check for a valid git hash that has a DEPS file.
+	assert.Equal(t, "f4b9bf7d9e688f1afedcf4a960a31582ddb56f4a", ExtractCommitFromDEPS(r, "4ede5bf7c9cc9eccbea0e7c088e47ab8b70aa9a8", DefaultSkiaDEPSRegexp))
+
+	// Check an invalid git hash.
+	assert.Equal(t, "", ExtractCommitFromDEPS(r, "invalid-sha-example", DefaultSkiaDEPSRegexp))
+
+	// Check a valid git hash that has no DEPS file.
+	assert.Equal(t, "", ExtractCommitFromDEPS(r, "8652a6df7dc8a7e6addee49f6ed3c2308e36bd18", DefaultSkiaDEPSRegexp))
 }
