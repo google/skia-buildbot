1e8e1a056dbd8f6ac319b2f423190f6f9dff1d99
Eric Boren <borenet@google.com>
2017-09-25 13:45:17 -0400

    [status] Switch UI to use "task" terms; fix demos; cleanup
    Bug: skia:
    Change-Id: Icc2d627f66541c67aefdf1791836d40113eede2d

--- status/res/js/status_utils.js
+++ status/res/js/status_utils.js
@@ -2,21 +2,10 @@
  * status_utils.js contains shared utilities used by Skia Status.
  */
 
-var BUILDBOT_PENDING = 0;
-
-var BUILDBOT_RESULT_SUCCESS = 0;
-var BUILDBOT_RESULT_WARNINGS = 1;
-var BUILDBOT_RESULT_FAILURE = 2;
-var BUILDBOT_RESULT_SKIPPED = 3;
-var BUILDBOT_RESULT_EXCEPTION = 4;
-var BUILDBOT_RESULT_CANCELLED = 5;
-
-// Note: The unobfuscate-status extension relies on the existence of the below
-//       class names.
-var CLASS_BUILD_SINGLE = "build_single";
-var CLASS_BUILD_TOP = "build_top";
-var CLASS_BUILD_MIDDLE = "build_middle";
-var CLASS_BUILD_BOTTOM = "build_bottom";
+var CLASS_TASK_SINGLE = "task_single";
+var CLASS_TASK_TOP = "task_top";
+var CLASS_TASK_MIDDLE = "task_middle";
+var CLASS_TASK_BOTTOM = "task_bottom";
 var CLASS_DASHED_TOP = "dashed_top";
 var CLASS_DASHED_BOTTOM = "dashed_bottom";
 
