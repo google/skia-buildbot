834d67ae97f8a1280bca5f3fadf960709ea72dea
Kevin Lubick <kjlubick@google.com>
2017-09-28 14:35:09 -0400

    Update Fuzzer to run on Debian
    I wanted to update afl-fuzz, then I learned our disk 
    image had updated, so I had to shave this yak.
    
    Apparently the name of the local SSD disk changed
    when we use the API as opposed to the gcloud tool, thus
    the change from /mnt/ssd0 to /mnt/pd0.  Also, it appears
    that the disk is mounted and formatted, so we don't
    need to do that anymore.
    
    Bug: skia:
    Change-Id: I5c6980ac768e8baeeb7df5504ef4105b6280c025
    Reviewed-on: https://skia-review.googlesource.com/52960
    Reviewed-by: Kevin Lubick <kjlubick@google.com>
    Commit-Queue: Kevin Lubick <kjlubick@google.com>

--- fuzzer/setup-script.sh
+++ fuzzer/setup-script.sh
@@ -5,7 +5,7 @@ set -e
 # Install clang/llvm
 wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key|sudo apt-key add -
 sudo apt-get update
-sudo apt-get install clang-3.8 lldb-3.8 make build-essential libfontconfig1-dev libfreetype6-dev libgif-dev libpng12-dev libqt4-dev ninja-build python-dev python-imaging libosmesa-dev -y
+sudo apt-get install clang-3.8 lldb-3.8 make build-essential libfontconfig1-dev libfreetype6-dev libgif-dev libqt4-dev ninja-build python-dev python-imaging libosmesa6-dev -y
 # Afl-fuzz can't find the clang-3.8 aliases, so make the standard /usr/bin/clang
 sudo ln /usr/bin/clang-3.8 /usr/bin/clang
 sudo ln /usr/bin/clang++-3.8 /usr/bin/clang++
@@ -13,11 +13,5 @@ sudo ln /usr/bin/llvm-config-3.8 /usr/bin/llvm-config
 # Make symbolizer easier to find
 sudo ln /usr/bin/llvm-symbolizer-3.8 /usr/bin/llvm-symbolizer
 
-# Mount data disk
-sudo mkdir -p /mnt/ssd0
-sudo mkfs.ext4 -F /dev/disk/by-id/google-local-ssd-0
-sudo mount -o discard,defaults /dev/disk/by-id/google-local-ssd-0 /mnt/ssd0
-sudo chmod 777 /mnt/ssd0
-
-# Add mounting instructions to fstab so it remounts on reboot.
-echo '/dev/disk/by-id/google-local-ssd-0 /mnt/ssd0 ext4 discard,defaults 1 1' | sudo tee -a /etc/fstab
+# Set up data disk
+sudo chmod 777 /mnt/pd0
--- fuzzer/startup-script.sh
+++ fuzzer/startup-script.sh
@@ -3,25 +3,25 @@
 set -e
 set -x
 
-AFL_VERSION="2.35b"
+AFL_VERSION="2.51b"
 # We need clang set as our c++ builder to build afl-clang
 export CC=/usr/bin/clang CXX=/usr/bin/clang++
 
 # Download and install afl-fuzz
-sudo rm -rf /mnt/ssd0/afl
-sudo mkdir /mnt/ssd0/afl
-sudo chmod 777 /mnt/ssd0/afl
+sudo rm -rf /mnt/pd0/afl
+sudo mkdir /mnt/pd0/afl
+sudo chmod 777 /mnt/pd0/afl
 wget 'https://storage.googleapis.com/skia-fuzzer/afl-mirror/afl-'$AFL_VERSION'.tgz' -O /tmp/afl.tgz
-tar -C /mnt/ssd0/afl/ -zxf /tmp/afl.tgz --strip=1 "afl-"$AFL_VERSION
-cd /mnt/ssd0/afl/
+tar -C /mnt/pd0/afl/ -zxf /tmp/afl.tgz --strip=1 "afl-"$AFL_VERSION
+cd /mnt/pd0/afl/
 make
 # build afl-clang-fast
-cd /mnt/ssd0/afl/llvm_mode/
+cd /mnt/pd0/afl/llvm_mode/
 make
 
-# Download and install depot_tools to /mnt/ssd0/depot_tools
-git clone 'https://chromium.googlesource.com/chromium/tools/depot_tools.git' /mnt/ssd0/depot_tools
-sudo chmod 777 /mnt/ssd0/depot_tools
+# Download and install depot_tools to /mnt/pd0/depot_tools
+git clone 'https://chromium.googlesource.com/chromium/tools/depot_tools.git' /mnt/pd0/depot_tools
+sudo chmod 777 /mnt/pd0/depot_tools
 
 # Fix afl-fuzz's requirement on core
 sudo sh -c "echo 'core' >/proc/sys/kernel/core_pattern"
