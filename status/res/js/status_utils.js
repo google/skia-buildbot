6e608796bd2aa8403be139b4542a6a3a93d3dabd
Eric Boren <borenet@google.com>
2017-10-11 12:39:34 -0400

    [status] Revert all incremental loading stuff
    Revert "[status] Make UI load data incrementally"
    
    This reverts commit cd8325e2426ed5fa8987da64aa949eb22266d666.
    
    Revert "[status] Switch UI to use "task" terms; fix demos; cleanup"
    
    This reverts commit 195ece6c8797da4fa42cf9a3ec66184a28baa92a.
    
    Revert "[status] Reset the incremental cache every 24 hours"
    
    This reverts commit fc58b499c58ec1eddaabc967a77fb17f50a02088.
    
    Revert "[status] Incremental cache: Fix race"
    
    This reverts commit b39ee937a50d73d46e8c5e9c041c99757d834ed9.
    
    Revert "[status] Incremental cache: handle errors after tasks.Update"
    
    This reverts commit 8c774d7cce1892af811093b5a6fe006dc3bd2299.
    
    Revert "Remove datahopper_internal and all buildbot-related stuff"
    
    This reverts commit 08ff7181ed17ae89823234ac73a6a22394df2839.
    
    Bug: skia:
    Change-Id: Ife50540ad183a00bf6fe1b64b2097b99d9683ebd

--- status/res/js/status_utils.js
+++ status/res/js/status_utils.js
@@ -2,10 +2,21 @@
  * status_utils.js contains shared utilities used by Skia Status.
  */
 
-var CLASS_TASK_SINGLE = "task_single";
-var CLASS_TASK_TOP = "task_top";
-var CLASS_TASK_MIDDLE = "task_middle";
-var CLASS_TASK_BOTTOM = "task_bottom";
+var BUILDBOT_PENDING = 0;
+
+var BUILDBOT_RESULT_SUCCESS = 0;
+var BUILDBOT_RESULT_WARNINGS = 1;
+var BUILDBOT_RESULT_FAILURE = 2;
+var BUILDBOT_RESULT_SKIPPED = 3;
+var BUILDBOT_RESULT_EXCEPTION = 4;
+var BUILDBOT_RESULT_CANCELLED = 5;
+
+// Note: The unobfuscate-status extension relies on the existence of the below
+//       class names.
+var CLASS_BUILD_SINGLE = "build_single";
+var CLASS_BUILD_TOP = "build_top";
+var CLASS_BUILD_MIDDLE = "build_middle";
+var CLASS_BUILD_BOTTOM = "build_bottom";
 var CLASS_DASHED_TOP = "dashed_top";
 var CLASS_DASHED_BOTTOM = "dashed_bottom";
 
@@ -13,5 +24,29 @@ this.status_utils = this.status_utils || function() {
   "use strict";
 
   var status_utils = {};
+
+
+  // Global variables.
+  status_utils.buildbotUrlPrefixInternal = "https://uberchromegw.corp.google.com/i";
+  status_utils.buildbotUrlPrefixExternal = "http://build.chromium.org/p";
+
+  /*
+   * getBuildbotUrlPrefix determines the buildbot URL prefix for the given build.
+   */
+  status_utils.getBuildbotUrlPrefix = function(build, internalView) {
+    var buildbotUrlPrefix = status_utils.buildbotUrlPrefixExternal + build.master;
+    for (var i = 0; i < build.properties.length; i++) {
+      if (build.properties[i][0] == "buildbotURL") {
+        buildbotUrlPrefix = build.properties[i][1];
+        break;
+      }
+    }
+    if (internalView) {
+      buildbotUrlPrefix = buildbotUrlPrefix.replace(status_utils.buildbotUrlPrefixExternal,
+                                                    status_utils.buildbotUrlPrefixInternal);
+    }
+    return buildbotUrlPrefix;
+  };
+
   return status_utils;
 }();
