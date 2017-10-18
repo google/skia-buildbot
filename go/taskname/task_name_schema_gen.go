b719848cfdce4198ce9944dab5d7ff963604a26c
Kevin Lubick <kjlubick@google.com>
2017-10-18 08:24:38 -0400

    Fix package.json files to appease bots
    Bug: skia:
    Change-Id: Ie8cb8b0038a844e402d2e17984c9c4f64acdc778

--- go/taskname/task_name_schema_gen.go
+++ go/taskname/task_name_schema_gen.go
@@ -4,6 +4,7 @@ package taskname
 
 var SCHEMA_FROM_GIT = map[string][]string{
 	"Build":       {"os", "compiler", "target_arch", "configuration"},
+	"Calmbench":   {"os"},
 	"Canary":      {"project", "os", "compiler", "target_arch", "configuration"},
 	"Housekeeper": {"frequency"},
 	"Infra":       {"frequency"},
