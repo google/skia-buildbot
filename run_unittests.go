ee3f13b31ac24010af0acd16d66f4c8274b9ca0b
Kevin Lubick <kjlubick@google.com>
2017-10-19 16:03:02 -0400

    Add -race to all tree tests
    Bug: skia:
    Change-Id: I1c1128076046820670007990b34d950a6857142a

--- run_unittests.go
+++ run_unittests.go
@@ -197,7 +197,7 @@ func polylintTests() []*test {
 
 // goTest returns a test which runs `go test` in the given cwd.
 func goTest(cwd string, testType string, args ...string) *test {
-	cmd := []string{"go", "test", "-v", "./go/...", "-parallel", "1"}
+	cmd := []string{"go", "test", "-v", "-race", "./go/...", "-parallel", "1"}
 	cmd = append(cmd, args...)
 	t := cmdTest(cmd, cwd, fmt.Sprintf("go tests (%s) in %s", testType, cwd), testType)
 
