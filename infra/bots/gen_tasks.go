5aff7bf4ea6832f8bbebd5156361182dcd07bb3b
Eric Boren <borenet@google.com>
2017-10-19 11:14:26 -0400

    [recipes] Use kitchen rather than recipes.py
    Bug: skia:
    Change-Id: Iffafb913633108a92f33bc40e3406f4b4a1f224c

--- infra/bots/gen_tasks.go
+++ infra/bots/gen_tasks.go
@@ -9,9 +9,11 @@ package main
 */
 
 import (
+	"encoding/json"
 	"fmt"
 	"strings"
 
+	"go.skia.org/infra/go/sklog"
 	"go.skia.org/infra/task_scheduler/go/specs"
 )
 
@@ -32,7 +34,69 @@ var (
 		"Infra-PerCommit-Medium",
 		"Infra-PerCommit-Large",
 	}
+
+	// TODO(borenet): Roll this version automatically!
+	CIPD_PKGS_KITCHEN = []*specs.CipdPackage{
+		&specs.CipdPackage{
+			Name:    "infra/tools/luci/kitchen/${platform}",
+			Path:    "kitchen",
+			Version: "git_revision:178957e5b67f42bd0ec25c609febedb66dc5d544",
+		},
+		&specs.CipdPackage{
+			Name:    "infra/git/${platform}",
+			Path:    "cipd_bin_packages",
+			Version: "version:2.14.1.chromium10",
+		},
+		&specs.CipdPackage{
+			Name:    "infra/python/cpython/${platform}",
+			Path:    "cipd_bin_packages",
+			Version: "version:2.7.14.chromium14",
+		},
+		&specs.CipdPackage{
+			Name:    "infra/tools/git/${platform}",
+			Path:    "cipd_bin_packages",
+			Version: "git_revision:fa7a52f4741f5e04bba0dfccc9b8456dc572c60b",
+		},
+		&specs.CipdPackage{
+			Name:    "infra/tools/luci/git-credential-luci/${platform}",
+			Path:    "cipd_bin_packages",
+			Version: "git_revision:fa7a52f4741f5e04bba0dfccc9b8456dc572c60b",
+		},
+		&specs.CipdPackage{
+			Name:    "infra/tools/luci/vpython/${platform}",
+			Path:    "cipd_bin_packages",
+			Version: "git_revision:25b0564a204da2bfd6346f26a59b9efb8cfc2212",
+		},
+		&specs.CipdPackage{
+			Name:    "infra/tools/authutil/${platform}",
+			Path:    "cipd_bin_packages",
+			Version: "git_revision:9c63809842a277ce10a86afd51b61c639a665d11",
+		},
+		&specs.CipdPackage{
+			Name:    "infra/tools/buildbucket/${platform}",
+			Path:    "cipd_bin_packages",
+			Version: "git_revision:43acdc268b04a3eae197db17d81accbdfe928d94",
+		},
+		&specs.CipdPackage{
+			Name:    "infra/tools/cloudtail/${platform}",
+			Path:    "cipd_bin_packages",
+			Version: "git_revision:43acdc268b04a3eae197db17d81accbdfe928d94",
+		},
+	}
 )
+
+// Apply the default CIPD packages.
+func cipd(pkgs []*specs.CipdPackage) []*specs.CipdPackage {
+	return append(CIPD_PKGS_KITCHEN, pkgs...)
+}
+
+func props(p map[string]string) string {
+	j, err := json.Marshal(p)
+	if err != nil {
+		sklog.Fatal(err)
+	}
+	return strings.Replace(string(j), "\\u003c", "<", -1)
+}
 
 // infra generates an infra test Task. Returns the name of the last Task in the
 // generated chain of Tasks, which the Job should add as a dependency.
@@ -41,8 +105,21 @@ func infra(b *specs.TasksCfgBuilder, name string) string {
 	if strings.Contains(name, "Large") {
 		pkgs = append(pkgs, b.MustGetCipdPackageFromAsset("protoc"))
 	}
+	propsJson := props(map[string]string{
+		"repository":    specs.PLACEHOLDER_REPO,
+		"buildername":   name,
+		"mastername":    "fake-master",
+		"buildnumber":   "2",
+		"slavename":     "fake-buildslave",
+		"nobuildbot":    "True",
+		"swarm_out_dir": specs.PLACEHOLDER_ISOLATED_OUTDIR,
+		"patch_storage": specs.PLACEHOLDER_PATCH_STORAGE,
+		"patch_issue":   specs.PLACEHOLDER_ISSUE,
+		"patch_set":     specs.PLACEHOLDER_PATCHSET,
+	})
+	sklog.Infof("Serialized: %q", string(propsJson))
 	b.MustAddTask(name, &specs.TaskSpec{
-		CipdPackages: pkgs,
+		CipdPackages: cipd(pkgs),
 		Dimensions: []string{
 			"pool:Skia",
 			fmt.Sprintf("os:%s", DEFAULT_OS_LINUX),
@@ -50,18 +127,8 @@ func infra(b *specs.TasksCfgBuilder, name string) string {
 			"cpu:x86-64-Haswell_GCE",
 		},
 		ExtraArgs: []string{
-			"--workdir", "../../..", "swarm_infra",
-			fmt.Sprintf("repository=%s", specs.PLACEHOLDER_REPO),
-			fmt.Sprintf("buildername=%s", name),
-			"mastername=fake-master",
-			"buildnumber=2",
-			"slavename=fake-buildslave",
-			"nobuildbot=True",
-			fmt.Sprintf("swarm_out_dir=%s", specs.PLACEHOLDER_ISOLATED_OUTDIR),
-			fmt.Sprintf("revision=%s", specs.PLACEHOLDER_REVISION),
-			fmt.Sprintf("patch_storage=%s", specs.PLACEHOLDER_PATCH_STORAGE),
-			fmt.Sprintf("patch_issue=%s", specs.PLACEHOLDER_ISSUE),
-			fmt.Sprintf("patch_set=%s", specs.PLACEHOLDER_PATCHSET),
+			"-recipe", "swarm_infra",
+			"-properties", string(propsJson),
 		},
 		Isolate:  "swarm_recipe.isolate",
 		Priority: 0.8,
