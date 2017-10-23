ea4c38fb13bcde3fc7aa2322b875d0f3bb05fadc
Eric Boren <borenet@google.com>
2017-10-23 09:06:23 -0400

    [recipes] Use kitchen rather than recipes.py
    Bug: skia:7050
    Change-Id: Iffafb913633108a92f33bc40e3406f4b4a1f224c

---
+++ infra/bots/bundle_recipes.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -x -e
+
+# We start in infra/bots
+cd ../..
+ls ../cipd_bin_packages
+ls -alh ../cipd_bin_packages/bin
+../cipd_bin_packages/bin/git --version
+INFRA_GIT_WRAPPER_TRACE=1
+PATH=../cipd_bin_packages:$PATH
+which git
+git --version
+git init
+git add .
+git commit -m "Commit Recipes"
+pwd
+ls
+python infra/bots/recipes.py bundle --destination ${1}/recipe_bundle
