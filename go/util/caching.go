069023d79be87867ea17b28cf65cd86977c9a7e6
Stephan Altmueller <stephana@google.com>
2017-10-25 10:56:10 -0400

    Event driven cache updates expectations store
    Bug: skia:
    Change-Id: Iebeea753ea6b6130573cfb0d78d2dd4a2d2c94fc

--- go/util/caching.go
+++ go/util/caching.go
@@ -109,7 +109,7 @@ func (m *MemLRUCache) Keys() []interface{} {
 
 type jsonCodec struct {
 	targetType reflect.Type
-	isSlice    bool
+	stripPtr   bool
 }
 
 // JSONCodec implements the LRUCodec interface by serializing and
@@ -121,10 +121,10 @@ func JSONCodec(instance interface{}) LRUCodec {
 	if targetType.Kind() == reflect.Ptr {
 		targetType = targetType.Elem()
 	}
-	isSlice := targetType.Kind() == reflect.Slice
+	kind := targetType.Kind()
 	return &jsonCodec{
 		targetType: targetType,
-		isSlice:    isSlice,
+		stripPtr:   (kind == reflect.Slice) || (kind == reflect.Map),
 	}
 }
 
@@ -135,13 +135,14 @@ func (j *jsonCodec) Encode(data interface{}) ([]byte, error) {
 
 // See LRUCodec interface.
 func (j *jsonCodec) Decode(byteData []byte) (interface{}, error) {
+	// Get a pointer to a new instance, because that's what Unmarshal needs.
 	ret := reflect.New(j.targetType).Interface()
 	err := json.Unmarshal(byteData, ret)
 	if err != nil {
 		return nil, err
-	} else if j.isSlice {
+	} else if j.stripPtr {
+		// Strip the pointer for slices and maps.
 		return reflect.ValueOf(ret).Elem().Interface(), nil
 	}
-
 	return ret, nil
 }
