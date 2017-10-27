e33993c4fac7dc9a02fb5ac38f589c05f8666c40
Stephan Altmueller <stephana@google.com>
2017-10-27 17:33:48 -0400

    WIP: skeleton of new trybot infrastructure
    Bug: skia:
    Change-Id: I0f24be61127d9849799410b52ecc0f606c32590e

---
+++ golden/go/bbtrybot/bbtypes.go
@@ -0,0 +1,12 @@
+package bbtrybot
+
+import (
+	"go.skia.org/infra/go/buildbucket"
+	"go.skia.org/infra/golden/go/trybot"
+)
+
+type Issue = trybot.Issue
+type IssueDetails = trybot.IssueDetails
+type PatchSet = trybot.PatchsetDetail
+type Parameters = buildbucket.Parameters
+type Properties = buildbucket.Properties
