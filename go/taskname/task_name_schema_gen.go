39b9821772bf8af0363d249bae10b27e5352b723
Eric Boren <borenet@google.com>
2017-10-30 13:46:10 -0400

    Fix go generate
    Bug: skia:
    Change-Id: I1ede86867d7e8b982053d7210f213b048fabe4b3

--- go/taskname/task_name_schema_gen.go
+++ go/taskname/task_name_schema_gen.go
@@ -4,7 +4,7 @@ package taskname
 
 var SCHEMA_FROM_GIT = map[string][]string{
 	"Build":       {"os", "compiler", "target_arch", "configuration"},
-	"Calmbench":   {"os", "compiler", "target_arch", "configuration"},
+	"Calmbench":   {"os", "compiler", "model", "cpu_or_gpu", "cpu_or_gpu_value", "arch", "configuration"},
 	"Canary":      {"project", "os", "compiler", "target_arch", "configuration"},
 	"Housekeeper": {"frequency"},
 	"Infra":       {"frequency"},
