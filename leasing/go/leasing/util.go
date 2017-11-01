75a41f6fdd95ed35651e1369a7d67028ec9b7b4d
Ravi Mistry <rmistry@google.com>
2017-11-01 10:18:36 -0400

    Checkpoint
    Bug: skia:
    Change-Id: Ie4a8447ef8da8558c5e52fd8fab7a9283ef5bb25

---
+++ leasing/go/leasing/util.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+const (
+	SKIA_TROOPER_URL = "http://skia-tree-status.appspot.com/current-trooper"
+)
+
+func GetTrooperEmail(httpClient *http.Client) (string, error) {
+	// TODO(rmistry): Read from http://skia-tree-status.appspot.com/current-trooper
+
+	resp, err := httpClient.Get(SKIA_TROOPER_URL)
+	if err != nil {
+		return "", fmt.Errorf("Error when hitting %s: %s", SKIA_TROOPER_URL, err)
+	}
+	trooper := struct {
+		Username string
+	}{}
+	if err := json.NewDecoder(resp.Body).Decode(&trooper); err != nil {
+		return "", fmt.Errorf("Could not get trooper data: %s", err)
+	}
+	fmt.Println("HERE HERE HERE")
+	fmt.Println("X" + trooper.Username + "X")
+
+	// USE trooper.Username here!
+	return "rmistry@google.com", nil
+}
+
+func GetSwarmingTaskLink(server, taskId string) string {
+	return fmt.Sprintf("https://%s/task?id=%s", server, taskId)
+}
