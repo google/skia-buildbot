118d4e161db8de1cd0d37f084ac4d9c98ad0c8db
Joe Gregorio <jcgregorio@google.com>
2017-11-06 15:18:34 -0500

    Move StartTraceDBTestServer out of go/ingestion/mocks.go since it isn't a mock.
    Bug: skia:
    Change-Id: Id1559f6b40e458b6fbaa78acdf7d8c36d35cec21
    Reviewed-on: https://skia-review.googlesource.com/67810
    Commit-Queue: Joe Gregorio <jcgregorio@google.com>
    Reviewed-by: Joe Gregorio <jcgregorio@google.com>

--- go/ingestion/mocks.go
+++ go/ingestion/mocks.go
@@ -2,16 +2,10 @@ package ingestion
 
 import (
 	"fmt"
-	"net"
 	"sort"
 	"time"
 
-	"github.com/stretchr/testify/assert"
-	"google.golang.org/grpc"
-
 	"go.skia.org/infra/go/depot_tools"
-	"go.skia.org/infra/go/sharedb"
-	"go.skia.org/infra/go/trace/service"
 	"go.skia.org/infra/go/vcsinfo"
 )
 
@@ -78,32 +72,4 @@ func (m MockVCSImpl) ResolveCommit(commitHash string) (string, error) {
 func (m MockVCSImpl) SetSecondaryRepo(secVCS vcsinfo.VCS, extractor depot_tools.DEPSExtractor) {
 	m.secondaryVCS = secVCS
 	m.secondaryExtractor = extractor
-}
-
-// StartTestTraceDBServer starts up a traceDB server for testing. It stores its
-// data at the given path and returns the address at which the server is
-// listening as the second return value.
-// Upon completion the calling test should call the Stop() function of the
-// returned server object.
-func StartTraceDBTestServer(t assert.TestingT, traceDBFileName, shareDBDir string) (*grpc.Server, string) {
-	traceDBServer, err := traceservice.NewTraceServiceServer(traceDBFileName)
-	assert.NoError(t, err)
-
-	lis, err := net.Listen("tcp", "localhost:0")
-	assert.NoError(t, err)
-
-	server := grpc.NewServer()
-	traceservice.RegisterTraceServiceServer(server, traceDBServer)
-
-	if shareDBDir != "" {
-		sharedb.RegisterShareDBServer(server, sharedb.NewServer(shareDBDir))
-	}
-
-	go func() {
-		// We ignore the error, because calling the Stop() function always causes
-		// an error and we are primarily interested in using this to test other code.
-		_ = server.Serve(lis)
-	}()
-
-	return server, lis.Addr().String()
 }
