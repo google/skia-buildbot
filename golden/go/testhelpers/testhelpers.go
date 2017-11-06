1e66ebd99cd798359961ddf9adf4ec45ee343c91
Joe Gregorio <jcgregorio@google.com>
2017-11-06 15:18:34 -0500

    Move StartTraceDBTestServer out of go/ingestion/mocks.go since it isn't a mock.
    Bug: skia:
    Change-Id: Id1559f6b40e458b6fbaa78acdf7d8c36d35cec21

---
+++ golden/go/testhelpers/testhelpers.go
@@ -0,0 +1,39 @@
+package testhelpers
+
+import (
+	"net"
+
+	"github.com/stretchr/testify/assert"
+	"go.skia.org/infra/go/sharedb"
+	"google.golang.org/grpc"
+
+	"go.skia.org/infra/go/trace/service"
+)
+
+// StartTestTraceDBServer starts up a traceDB server for testing. It stores its
+// data at the given path and returns the address at which the server is
+// listening as the second return value.
+// Upon completion the calling test should call the Stop() function of the
+// returned server object.
+func StartTraceDBTestServer(t assert.TestingT, traceDBFileName, shareDBDir string) (*grpc.Server, string) {
+	traceDBServer, err := traceservice.NewTraceServiceServer(traceDBFileName)
+	assert.NoError(t, err)
+
+	lis, err := net.Listen("tcp", "localhost:0")
+	assert.NoError(t, err)
+
+	server := grpc.NewServer()
+	traceservice.RegisterTraceServiceServer(server, traceDBServer)
+
+	if shareDBDir != "" {
+		sharedb.RegisterShareDBServer(server, sharedb.NewServer(shareDBDir))
+	}
+
+	go func() {
+		// We ignore the error, because calling the Stop() function always causes
+		// an error and we are primarily interested in using this to test other code.
+		_ = server.Serve(lis)
+	}()
+
+	return server, lis.Addr().String()
+}
