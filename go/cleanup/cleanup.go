3a03ff5bfae24f0fe08b8112c57d3ecd10761173
Eric Boren <borenet@google.com>
2017-11-09 12:48:10 -0500

    Add registry of handlers for SIGINT and SIGTERM in common
    Make cleanup package use these handlers.
    
    Bug: skia:7257
    Change-Id: I846ef1ee781664475474f2392baa9d6eca139fc2

--- go/cleanup/cleanup.go
+++ go/cleanup/cleanup.go
@@ -3,6 +3,7 @@ package cleanup
 import (
 	"context"
 	"sync"
+	"syscall"
 	"time"
 
 	"go.skia.org/infra/go/sklog"
@@ -12,20 +13,24 @@ import (
 var (
 	cancel context.CancelFunc
 	ctx    context.Context
+	once   sync.Once
 	wg     sync.WaitGroup
 )
 
 // Initialize the package.
 func init() {
-	resetContext()
+	intHandler = newHandler(syscall.SIGINT, syscall.SIGTERM)
+	reset()
+	OnInterrupt(Cleanup)
 }
 
-// Reset the context. This is in a non-init function for testing purposes.
-func resetContext() {
+// Reset the package. This is in a non-init function for testing purposes.
+func reset() {
 	// The below should be unnecessary but makes "go vet" happy.
 	newContext, newCancel := context.WithCancel(context.Background())
 	ctx = newContext
 	cancel = newCancel
+	once = sync.Once{}
 }
 
 // Repeat runs the tick function immediately and on the given timer. When
@@ -44,10 +49,16 @@ func Repeat(tickFrequency time.Duration, tick, cleanup func()) {
 }
 
 // Cleanup cancels all tick functions registered via Repeat(), then waits for
-// them to fully stop running and for their cleanup functions to run.
+// them to fully stop running and for their cleanup functions to run. Cleanup()
+// runs automatically when SIGINT or SIGTERM is received. If your program runs
+// interactively or is expected to exit normally under other circumstances, you
+// should make sure Cleanup() is called at the end of main(). common.Defer() is
+// the canonical way to do this.
 func Cleanup() {
-	sklog.Warningf("Shutdown request received")
-	cancel()
-	wg.Wait()
-	sklog.Warningf("Finished clean shutdown procedure.")
+	once.Do(func() {
+		sklog.Warningf("Running clean shutdown procedures.")
+		cancel()
+		wg.Wait()
+		sklog.Warningf("Finished clean shutdown procedures.")
+	})
 }
