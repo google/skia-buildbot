2b026fe2a0049797c2e2e9cbcf3862e6e4ec7a35
Ben Wagner <benjaminwagner@google.com>
2017-11-15 14:52:55 -0500

    [status/capacity] Merge stats where bots overlap.
    Currently on https://status.skia.org/capacity, we have multiple rows
    where we ideally should have only one. E.g.:
     - OS: Debian-9.2, GPU: none, CPU: x86-64-Haswell_GCE
     - OS: Debian-9.2, , CPU: x86-64-Haswell_GCE
    
    The result is that we are splitting "Required Bots" among two rows, but
    including all bots in "Actual Bot Count" for both rows. This leads to
    the "Percent of Optimistic Estimate" being about twice as high as it
    should be.
    
    Another example is:
     - OS: Windows-10-15063, , CPU: x86-64-i7-5557U
     - OS: Windows-10-15063, GPU: Intel Skylake Iris 540/550
    
    Both of these tasks are running on NUC6i5SYK.
    
    In this CL, I am merging BotConfig where a bot appears in both
    BotConfigs. This has some problems, as mentioned in code comments, but
    I think in general it will improve the usefulness of the Capacity Stats.
    
    Also split up CapacityClient.QueryAll and add some TODOs.
    
    Change-Id: Ib4f24d34cf89ea36ed7c03767a6ce6661c7faa16

--- status/go/capacity/capacity.go
+++ status/go/capacity/capacity.go
@@ -24,9 +24,11 @@ type CapacityClient struct {
 	tasks db.TaskCache
 	repos repograph.Map
 	// The cached measurements
+	// TODO(benjaminwagner): Needs a mutex?
 	lastMeasurements map[string]BotConfig
 }
 
+// Caller is responsible for periodically updating the arguments.
 func New(tcc *specs.TaskCfgCache, tasks db.TaskCache, repos repograph.Map) *CapacityClient {
 	return &CapacityClient{tcc: tcc, tasks: tasks, repos: repos}
 }
@@ -49,16 +51,15 @@ type BotConfig struct {
 	TaskAverageDurations []TaskDuration  `json:"tasks"`
 }
 
-// QueryAll updates the capacity metrics.
-func (c *CapacityClient) QueryAll() error {
-	sklog.Infoln("Recounting Capacity Stats")
-
+// getTaskDurations fetches Tasks from the TaskCache and generates a taskData for each completed
+// Swarming Task, grouped by TaskSpec name.
+func (c *CapacityClient) getTaskDurations() (map[string][]taskData, error) {
 	// Fetch last 72 hours worth of tasks that TaskScheduler created.
 	now := time.Now()
 	before := now.Add(-72 * time.Hour)
 	tasks, err := c.tasks.GetTasksFromDateRange(before, now)
 	if err != nil {
-		return fmt.Errorf("Could not fetch tasks between %s and %s: %s", before, now, err)
+		return nil, fmt.Errorf("Could not fetch tasks between %s and %s: %s", before, now, err)
 	}
 	sklog.Infof("Found %d tasks in last 72 hours", len(tasks))
 
@@ -74,6 +75,7 @@ func (c *CapacityClient) QueryAll() error {
 			continue
 		}
 		duration := task.Finished.Sub(task.Started)
+		// TODO(benjaminwagner): We're assuming here that Task names are unique across repos.
 		durations[task.Name] = append(durations[task.Name], taskData{
 			Duration: duration,
 			BotId:    task.SwarmingBotId,
@@ -81,7 +83,37 @@ func (c *CapacityClient) QueryAll() error {
 	}
 
 	sklog.Infof("From %d tasks, we saw %d unique task names", len(tasks), len(durations))
+	return durations, nil
+}
 
+// getCQTaskSpecs returns the TaskSpec names of all Jobs on the CQ.
+// TODO(benjaminwagner): return a util.StringSet{}
+func (c *CapacityClient) getCQTaskSpecs() ([]string, error) {
+	cqTasks, err := cq.GetSkiaCQTryBots()
+	if err != nil {
+		sklog.Warningf("Could not get Skia CQ bots.  Continuing anyway.  %s", err)
+		cqTasks = []string{}
+	}
+	infraCQTasks, err := cq.GetSkiaInfraCQTryBots()
+	if err != nil {
+		sklog.Warningf("Could not get Skia CQ bots.  Continuing anyway.  %s", err)
+		infraCQTasks = []string{}
+	}
+	cqTasks = append(cqTasks, infraCQTasks...)
+	// TODO(benjaminwagner): This is a list of Job names, not Task names.
+	return cqTasks, nil
+}
+
+// botConfigKey creates a string key from a list of dimensions. dims will be sorted.
+func botConfigKey(dims []string) string {
+	sort.Strings(dims)
+	return strings.Join(dims, "|")
+}
+
+// computeBotConfigs groups TaskSpecs by identical dimensions and returns a BotConfig for each
+// dimension set. Arguments are getTaskDurations() and getCQTaskSpecs(). The returned map is keyed
+// by botConfigKey(BotConfig.Dimensions).
+func (c *CapacityClient) computeBotConfigs(durations map[string][]taskData, cqTasks []string) (map[string]BotConfig, error) {
 	// The db.Task structs don't have their dimensions, so we pull those off of the master
 	// branches of all the repos. If the dimensions were updated recently, this may lead
 	// to some inaccuracies. In practice, this probably won't happen because updates
@@ -94,18 +126,6 @@ func (c *CapacityClient) QueryAll() error {
 			Revision: master.Hash,
 		})
 	}
-
-	cqTasks, err := cq.GetSkiaCQTryBots()
-	if err != nil {
-		sklog.Warningf("Could not get Skia CQ bots.  Continuing anyway.  %s", err)
-		cqTasks = []string{}
-	}
-	infraCQTasks, err := cq.GetSkiaInfraCQTryBots()
-	if err != nil {
-		sklog.Warningf("Could not get Skia CQ bots.  Continuing anyway.  %s", err)
-		infraCQTasks = []string{}
-	}
-	cqTasks = append(cqTasks, infraCQTasks...)
 
 	sklog.Infof("About to look up those tasks in %+v", tips)
 
@@ -130,8 +150,7 @@ func (c *CapacityClient) QueryAll() error {
 			continue
 		}
 		dims := taskSpec.Dimensions
-		sort.Strings(dims)
-		key := strings.Join(dims, "|")
+		key := botConfigKey(dims)
 		config, ok := botConfigs[key]
 		if !ok {
 			config = BotConfig{
@@ -157,6 +176,86 @@ func (c *CapacityClient) QueryAll() error {
 		})
 		botConfigs[key] = config
 	}
+	return botConfigs, nil
+}
+
+// mergeBotConfigs replaces overlapping BotConfigs in the given map by a combined BotConfig.
+func mergeBotConfigs(botConfigs map[string]BotConfig) {
+	botIdToConfigs := map[string][]string{}
+	configsToMerge := util.NewStringSet()
+	for key, config := range botConfigs {
+		for botId, _ := range config.Bots {
+			if !util.In(key, botIdToConfigs[botId]) {
+				botIdToConfigs[botId] = append(botIdToConfigs[botId], key)
+			}
+			if len(botIdToConfigs[botId]) > 1 {
+				configsToMerge[key] = true
+			}
+		}
+	}
+	for key, _ := range configsToMerge {
+		if _, ok := botConfigs[key]; !ok {
+			// Already merged.
+			continue
+		}
+		groupKeys := util.NewStringSet()
+		bots := util.NewStringSet()
+		var gather func(string)
+		gather = func(key string) {
+			if _, ok := groupKeys[key]; ok {
+				return
+			}
+			groupKeys[key] = true
+			config := botConfigs[key]
+			for botId, _ := range config.Bots {
+				bots[botId] = true
+				for _, other := range botIdToConfigs[botId] {
+					gather(other)
+				}
+			}
+		}
+		gather(key)
+		dimSet := util.NewStringSet()
+		durs := []TaskDuration{}
+		for key, _ := range groupKeys {
+			config := botConfigs[key]
+			dimSet.AddLists(config.Dimensions)
+			durs = append(durs, config.TaskAverageDurations...)
+			delete(botConfigs, key)
+		}
+		dims := dimSet.Keys()
+		newKey := botConfigKey(dims)
+		botConfigs[newKey] = BotConfig{
+			Dimensions:           dims,
+			Bots:                 bots,
+			TaskAverageDurations: durs,
+		}
+	}
+}
+
+// QueryAll updates the capacity metrics.
+func (c *CapacityClient) QueryAll() error {
+	sklog.Infoln("Recounting Capacity Stats")
+
+	durations, err := c.getTaskDurations()
+	if err != nil {
+		return err
+	}
+
+	cqTasks, err := c.getCQTaskSpecs()
+	if err != nil {
+		return err
+	}
+
+	botConfigs, err := c.computeBotConfigs(durations, cqTasks)
+	if err != nil {
+		return err
+	}
+
+	// Merge BotConfigs with overlapping bots. (This could be the wrong thing to do if a bot's
+	// dimensions change, e.g. installing a GPU in a CPU bot or recreating a GCE bot with a different
+	// CPU type. I expect that to be a rare case.)
+	mergeBotConfigs(botConfigs)
 
 	c.lastMeasurements = botConfigs
 	return err
