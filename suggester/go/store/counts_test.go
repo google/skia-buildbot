cb10d516526d1cae0545bcc479a00126041e26ff
Joe Gregorio <jcgregorio@google.com>
2017-12-11 07:57:56 -0500

    suggester wip
    Bug: skia:
    Change-Id: I647f34b07600bbe091d44a6fbfe0a6422266dab1

---
+++ suggester/go/store/counts_test.go
@@ -0,0 +1,38 @@
+package store
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"go.skia.org/infra/go/testutils"
+	"go.skia.org/infra/suggester/go/ds"
+	"go.skia.org/infra/suggester/go/dstestutil"
+)
+
+func TestCountsReadWrite(t *testing.T) {
+	testutils.MediumTest(t)
+	cleanup := dstestutil.InitDatastore(t, ds.FILE_COUNT)
+
+	defer cleanup()
+
+	totals := map[string]map[string]int{
+		"bench/ColorCodecBench.cpp": map[string]int{
+			"Test-Chromecast-GCC-Chorizo-CPU-Cortex_A7-arm-Release-All":             1,
+			"Test-Chromecast-GCC-Chorizo-GPU-Cortex_A7-arm-Release-All":             1,
+			"Test-Debian9-Clang-GCE-CPU-AVX2-x86_64-Debug-All-MSAN_FAAA":            1,
+			"Test-Debian9-Clang-GCE-CPU-AVX2-x86_64-Debug-All-MSAN_FDAA":            1,
+			"Test-Debian9-Clang-GCE-CPU-AVX2-x86_64-Debug-All-MSAN_FSAA":            1,
+			"Test-Win10-Clang-NUC5i7RYH-GPU-IntelIris6100-x86_64-Debug-All-ANGLE":   1,
+			"Test-Win10-Clang-NUC5i7RYH-GPU-IntelIris6100-x86_64-Release-All-ANGLE": 1,
+			"Test-Win10-Clang-NUCD34010WYKH-GPU-IntelHD4400-x86_64-Debug-All-ANGLE": 1,
+		},
+	}
+
+	err := WriteTotals(totals)
+	assert.NoError(t, err)
+	time.Sleep(2)
+	readTotals, err := ReadTotals()
+	assert.NoError(t, err)
+	assert.Len(t, readTotals, 1)
+}
