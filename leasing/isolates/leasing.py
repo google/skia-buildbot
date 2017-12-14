8511512057be2c8498c802819a473b1ca62f462e
Ravi Mistry <rmistry@google.com>
2017-12-14 13:04:39 -0500

    [Leasing Server] Bring up debugger on Android devices and provide instructions to connect to it
    Highlists:
    * Displays a new dropdown in the UI asking if Debugger should be setup for an Android lease.
    * Lease start email includes instructions on how to remotely connect to the debugger running on the device.
    * Add gsutil as a CIPD package to leasing tasks.
    * Downloads skiaserve binary via the work done in https://skia-review.googlesource.com/c/skia/+/83981
    * Automatically copy over skiaserve to device and bring it up.
    * Setup adb port forwarding from host to device.
    * After task is done cleanup copied over artifacts from the device and remove
      adb port forwarding.
    
    
    Bug: skia:7399
    Change-Id: I5da7d307a092629da15fba63d119613f2f1a3c58

--- leasing/isolates/leasing.py
+++ leasing/isolates/leasing.py
@@ -8,7 +8,10 @@
 import argparse
 import json
 import os
+import socket
+import subprocess
 import sys
+import tempfile
 import time
 import urllib2
 
@@ -28,6 +31,9 @@ def main():
                       help='The command users can use to run the debug task.')
   parser.add_argument('-r', '--command-relative-dir', required=True, type=str,
                       help='The directory the command should be run in.')
+  parser.add_argument('-g', '--skiaserve-gs-path', required=False, type=str,
+                      default=None,
+                      help='GS location of skiaserve binary to use.')
   args = parser.parse_args()
 
   if args.debug_command:
@@ -40,6 +46,35 @@ def main():
     print 'Dir to run command in: '
     print os.path.join(os.getcwd(), args.command_relative_dir)
     print
+
+  if args.skiaserve_gs_path:
+    hostname = socket.gethostname()
+    gsutil_binary = os.path.join('cipd_bin_packages', 'gsutil')
+    skiaserve_device_location = '/data/tmp-debugger/skiaserve'
+    # Make sure we are running as adb root.
+    subprocess.check_call(['adb', 'root'])
+    # Setup directory structure on the device.
+    print 'Creating %s on device' % os.path.dirname(skiaserve_device_location)
+    subprocess.check_call(['adb', 'shell', 'mkdir', '-p',
+                           os.path.dirname(skiaserve_device_location)])
+    # Copy skiaserver binary from Google storage to the device.
+    _, tmpFile = tempfile.mkstemp()
+    try:
+      print 'Copying %s locally to %s' % (args.skiaserve_gs_path, tmpFile)
+      subprocess.check_call(
+          [gsutil_binary, 'cp', args.skiaserve_gs_path, tmpFile])
+      print 'Copying %s to device %s' % (tmpFile, skiaserve_device_location)
+      subprocess.check_call(['adb', 'push', tmpFile, skiaserve_device_location])
+    finally:
+      os.remove(tmpFile)
+    # Turn on the debugger and setup adb port forwarding from host to device.
+    print 'Bringing up the debugger'
+    subprocess.check_call(['adb', 'shell', 'chmod', '+x', skiaserve_device_location])
+    proc = subprocess.Popen(["adb", "shell", skiaserve_device_location])
+    print 'Running adb forward tcp:8888 tcp:8888'
+    subprocess.check_call(['adb', 'forward', 'tcp:8888', 'tcp:8888'])
+    print
+
   print
   print ('Please cleanup after you are done debugging or when you get the '
          '15 min warning email!')
@@ -56,6 +91,17 @@ def main():
     time.sleep(POLLING_WAIT_TIME_SECS)
 
   print 'The lease time has expired.'
+
+  # Cleanup everything that was setup for the debugger.
+  if args.skiaserve_gs_path:
+    # Stop the debugger.
+    proc.terminate()
+    # Cleanup the tmp dir.
+    subprocess.check_call(['adb', 'shell', 'rm', '-r',
+                           os.path.dirname(skiaserve_device_location)])
+    # Remove adb port forwarding.
+    subprocess.check_call(['adb', 'forward', '--remove', 'tcp:8888'])
+
   print 'Failing the task so that swarming reboots the bot.'
   # Fail the task so that swarming reboots the bot. This will force all SSH'ed
   # users to disconnect.
