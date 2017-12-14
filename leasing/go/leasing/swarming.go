16b652ebbdc20ae3874c323b8ee1c6efe4fae6c7
Ravi Mistry <rmistry@google.com>
2017-12-14 13:04:39 -0500

    [Leasing Server] Bring up debugger on Android devices and provide instructions to connect to it
    Highlists:
    * Displays a new dropdown in the UI asking if Debugger should be setup.
    * In lease start email detail instructions on how to remotely connect to the
      debugger running on the device.
    * Add gsutil as a CIPD package to leasing tasks
    * Downloads skiaserve binary via the work done in https://skia-review.googlesource.com/c/skia/+/83981
    * Automatically copy over skiaserve to device and bring it up.
    * Setup adb port forwarding from host to device.
    * After task is done cleanup copied over artifacts from the device and remove
      adb port forwarding.
    
    
    Bug: skia:7399
    Change-Id: I5da7d307a092629da15fba63d119613f2f1a3c58

--- leasing/go/leasing/swarming.go
+++ leasing/go/leasing/swarming.go
@@ -60,6 +60,12 @@ var (
 	}
 
 	isolateServerPath string
+
+	gsutilPackage = &swarming_api.SwarmingRpcsCipdPackage{
+		PackageName: "infra/gsutil",
+		Path:        "cipd_bin_packages",
+		Version:     "version:4.28",
+	}
 )
 
 func SwarmingInit() error {
@@ -227,7 +233,7 @@ func GetSwarmingTaskMetadata(pool, taskId string) (*swarming_api.SwarmingRpcsTas
 	return swarmingClient.GetTaskMetadata(taskId)
 }
 
-func TriggerSwarmingTask(pool, requester, datastoreId, osType, deviceType, botId, serverURLstring, isolateHash string, isolateDetails *IsolateDetails) (string, error) {
+func TriggerSwarmingTask(pool, requester, datastoreId, osType, deviceType, arch, botId, serverURLstring, isolateHash string, isolateDetails *IsolateDetails, setupDebugger bool) (string, error) {
 	dimsMap := map[string]string{
 		"pool": pool,
 	}
@@ -256,11 +262,30 @@ func TriggerSwarmingTask(pool, requester, datastoreId, osType, deviceType, botId
 		"--debug-command", strings.Join(isolateDetails.Command, " "),
 		"--command-relative-dir", isolateDetails.RelativeCwd,
 	}
+	if setupDebugger {
+		skiaserveGSPath, err := GetSkiaServeGSPath(arch)
+		if err != nil {
+			return "", fmt.Errorf("Could not find skiaserve for %s: s", arch, err)
+		}
+		extraArgs = append(extraArgs, "--skiaserve-gs-path", skiaserveGSPath)
+
+		// Add GsUtil CIPD package to isolate input. It will be used to download
+		// the skiaserve binary from Google Storage.
+		if isolateDetails.CipdInput == nil {
+			isolateDetails.CipdInput = &swarming_api.SwarmingRpcsCipdInput{}
+		}
+		if isolateDetails.CipdInput.Packages == nil {
+			isolateDetails.CipdInput.Packages = []*swarming_api.SwarmingRpcsCipdPackage{gsutilPackage}
+		} else {
+			isolateDetails.CipdInput.Packages = append(isolateDetails.CipdInput.Packages, gsutilPackage)
+		}
+	}
 	isolateServer := GetSwarmingInstance(pool).IsolateServer
 	expirationSecs := int64(swarming.RECOMMENDED_EXPIRATION.Seconds())
 	executionTimeoutSecs := int64(SWARMING_HARD_TIMEOUT.Seconds())
 	ioTimeoutSecs := int64(SWARMING_HARD_TIMEOUT.Seconds())
 	taskName := fmt.Sprintf("Leased by %s using leasing.skia.org", requester)
+
 	taskRequest := &swarming_api.SwarmingRpcsNewTaskRequest{
 		ExpirationSecs: expirationSecs,
 		Name:           taskName,
