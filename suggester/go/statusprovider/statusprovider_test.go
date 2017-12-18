0c835c3952be31d359f51034ec0bbb0fe1621be9
Joe Gregorio <jcgregorio@google.com>
2017-12-18 11:10:52 -0500

    suggester wip
    Bug: skia:
    Change-Id: I647f34b07600bbe091d44a6fbfe0a6422266dab1

---
+++ suggester/go/statusprovider/statusprovider_test.go
@@ -0,0 +1,56 @@
+package statusprovider
+
+import (
+	"bytes"
+	"encoding/json"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+
+	"go.skia.org/infra/task_scheduler/go/db"
+)
+
+func TestJSONToFlaky(t *testing.T) {
+	now := time.Now()
+	aMinuteEarlier := now.Add(-1 * time.Minute)
+	comments := []*db.RepoComments{
+		&db.RepoComments{
+			TaskSpecComments: map[string][]*db.TaskSpecComment{
+				"Bot-1": []*db.TaskSpecComment{
+					&db.TaskSpecComment{
+						Timestamp: now,
+						Flaky:     true,
+					},
+					&db.TaskSpecComment{
+						Timestamp: now.Add(time.Minute),
+						Flaky:     false,
+					},
+					&db.TaskSpecComment{
+						Timestamp: aMinuteEarlier,
+						Flaky:     true,
+					},
+				},
+			},
+		},
+	}
+	var b bytes.Buffer
+	err := json.NewEncoder(&b).Encode(comments)
+	assert.NoError(t, err)
+
+	flakes, err := jsonToFlaky(&b)
+	assert.NoError(t, err)
+	assert.Len(t, flakes, 1)
+	assert.True(t, aMinuteEarlier.Equal(flakes["Bot-1"]))
+}
+
+func TestJSONEmptyToFlaky(t *testing.T) {
+	flakes, err := jsonToFlaky(bytes.NewBufferString("[]"))
+	assert.NoError(t, err)
+	assert.Len(t, flakes, 0)
+}
+
+func TestJSONInvalidToFlaky(t *testing.T) {
+	_, err := jsonToFlaky(bytes.NewBufferString(""))
+	assert.Error(t, err)
+}
