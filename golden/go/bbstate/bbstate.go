a8de8331511872e41482d912a2606ca060d8904a
Stephan Altmueller <stephana@google.com>
2018-01-15 21:12:10 -0500

    wip: bugfixes tryjob support
    Bug: skia:
    Change-Id: I7d545abdc7737bc6b234a90c51d4b1f71707b117

--- golden/go/bbstate/bbstate.go
+++ golden/go/bbstate/bbstate.go
@@ -158,6 +158,7 @@ func (b *BuildBucketState) syncTryjob(buildBucketID int64) (*tryjobstore.Tryjob,
 		return nil, fmt.Errorf("Unable to retrieve build %d. Got %s", buildBucketID, buildResp.Error.Message)
 	}
 
+	sklog.Infof("sync 1: %s", spew.Sdump(buildResp.Build))
 	return b.processBuild(buildResp.Build)
 }
 
@@ -262,7 +263,7 @@ func (b *BuildBucketState) syncGerritIssue(issueID, patchsetID int64, issue *try
 	// If 'issue' is nil, we need to see if we can find it in Gerrit.
 	if issue == nil {
 		var err error
-		issue, err = b.fetchGerritIssue(issueID)
+		issue, err = b.updateGerritIssue(issueID, issue)
 		if err != nil {
 			// We didn't find the issue in Gerrit.
 			if err == gerrit.ErrNotFound {
@@ -273,7 +274,8 @@ func (b *BuildBucketState) syncGerritIssue(issueID, patchsetID int64, issue *try
 	} else {
 		// Check if the issue is up to date.
 		if !issue.HasPatchset(patchsetID) {
-			if err := b.updateGerritIssue(issueID, issue); err != nil {
+			var err error
+			if issue, err = b.updateGerritIssue(issueID, issue); err != nil {
 				return nil, fmt.Errorf("Error updating issue %d: %s", issueID, err)
 			}
 		}
@@ -288,27 +290,21 @@ func (b *BuildBucketState) syncGerritIssue(issueID, patchsetID int64, issue *try
 	return issue, nil
 }
 
-// fetchGerritIssue retrieves the given issue from Gerrit.
-func (b *BuildBucketState) fetchGerritIssue(issueID int64) (*tryjobstore.IssueDetails, error) {
+// updateGerritIssue fetches issue detaisl from Gerrit and merges them into the
+// internal representation of the Gerrit issues. If issue is nil a new instance
+// will be allocated and returned.
+func (b *BuildBucketState) updateGerritIssue(issueID int64, issue *tryjobstore.IssueDetails) (*tryjobstore.IssueDetails, error) {
 	changeInfo, err := b.gerritAPI.GetIssueProperties(issueID)
 	if err != nil {
 		return nil, err
 	}
 
-	ret := &tryjobstore.IssueDetails{Issue: &tryjobstore.Issue{}}
-	b.setIssueDetails(issueID, changeInfo, ret)
-	return ret, nil
-}
-
-// updateGerritIssue fetches issue detaisl from Gerrit and merges them into the
-// internal representation of the Gerrit issues.
-func (b *BuildBucketState) updateGerritIssue(issueID int64, issue *tryjobstore.IssueDetails) error {
-	changeInfo, err := b.gerritAPI.GetIssueProperties(issueID)
-	if err != nil {
-		return err
+	if issue == nil {
+		issue = &tryjobstore.IssueDetails{Issue: &tryjobstore.Issue{}}
 	}
+
 	b.setIssueDetails(issueID, changeInfo, issue)
-	return nil
+	return issue, nil
 }
 
 // setIssueDetails set the properties of the given IssueDetails from the values
@@ -320,7 +316,13 @@ func (b *BuildBucketState) setIssueDetails(issueID int64, changeInfo *gerrit.Cha
 	issue.Issue.Updated = changeInfo.Updated
 	issue.Issue.URL = b.gerritAPI.Url(issueID)
 	issue.Status = changeInfo.Status
-	issue.UpdatePatchsets(extractPatchsetDetails(changeInfo))
+
+	// extract the PatchsetDetail.
+	psDetails := make([]*tryjobstore.PatchsetDetail, 0, len(changeInfo.Patchsets))
+	for _, revision := range changeInfo.Patchsets {
+		psDetails = append(psDetails, &tryjobstore.PatchsetDetail{ID: revision.Number})
+	}
+	issue.UpdatePatchsets(psDetails)
 }
 
 // pollBuildBucket queries the BuildBucket instance from (now - timeWindow) to now.
@@ -364,16 +366,6 @@ func (b *BuildBucketState) startBuildPoller(buildsCh chan<- *bb_api.ApiCommonBui
 	return nil
 }
 
-// extractPatchsetDetails produces instances of PatchsetDetail from the change
-// info retrieved from Gerrit.
-func extractPatchsetDetails(changeInfo *gerrit.ChangeInfo) []*tryjobstore.PatchsetDetail {
-	ret := make([]*tryjobstore.PatchsetDetail, 0, len(changeInfo.Patchsets))
-	for _, revision := range changeInfo.Patchsets {
-		ret = append(ret, &tryjobstore.PatchsetDetail{ID: revision.Number})
-	}
-	return ret
-}
-
 // extractPatchsetRegex is used to extract the patchset ID from BuildBucket builds.
 var extractPatchsetRegex = regexp.MustCompile(`^refs\/changes\/[0-9]*\/[0-9]*\/(?P<patchset>.+)$`)
 
@@ -393,6 +385,11 @@ func getTryjobInfo(build *bb_api.ApiCommonBuildMessage, params *tryjobstore.Para
 	}
 
 	issueID := int64(params.Properties.GerritIssue)
+
+	// Make sure the relevant ids are correct.
+	if (issueID <= 0) || (patchsetID <= 0) {
+		return nil, sklog.FmtErrorf("Invalid issue id (%d) or patchset id (%d).", issueID, patchsetID)
+	}
 
 	// Translate the two result fields into one for tryjobs.
 	var status tryjobstore.TryjobStatus = tryjobstore.TRYJOB_UNKNOWN
