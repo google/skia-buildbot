c89eef99d9bd09d80d241c8f8abcb7accfbeb616
Ravi Mistry <rmistry@google.com>
2018-01-22 14:53:03 -0500

    Android Compile Server
    Bug: skia:7469
    Change-Id: I40f3b81e80c604896999b70accc2dee2cb710338

---
+++ go/android_skia_checkout/android_skia_checkout.go
@@ -0,0 +1,46 @@
+// android_skia_checkout contains util methods for interacting with
+// the Skia repository within Android.
+package android_skia_checkout
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"path"
+
+	"go.skia.org/infra/go/exec"
+)
+
+var (
+	// Files within the Skia checkout.
+	SkUserConfigManualRelPath = path.Join("include", "config", "SkUserConfigManual.h")
+	SkUserConfigRelPath       = path.Join("include", "config", "SkUserConfig.h")
+	AndroidBpRelPath          = path.Join("Android.bp")
+)
+
+const (
+	// The remote pointing to googleplex repo that is automatically present
+	// in the Skia repository within Android.
+	BUILT_IN_REMOTE = "goog"
+)
+
+func RunGnToBp(ctx context.Context, skiaCheckout string) error {
+	if _, syncErr := exec.RunCwd(ctx, skiaCheckout, "./bin/sync"); syncErr != nil {
+		// Sync may return errors, but this is ok.
+	}
+	if _, fetchGNErr := exec.RunCwd(ctx, skiaCheckout, "./bin/fetch-gn"); fetchGNErr != nil {
+		return fmt.Errorf("Failed to install GN: %s", fetchGNErr)
+	}
+
+	// Generate and add files created by gn/gn_to_bp.py
+	gnEnv := []string{fmt.Sprintf("PATH=%s/:%s", path.Join(skiaCheckout, "bin"), os.Getenv("PATH"))}
+	if _, gnToBpErr := exec.RunCommand(ctx, &exec.Command{
+		Env:  gnEnv,
+		Dir:  skiaCheckout,
+		Name: "python",
+		Args: []string{"-c", "from gn import gn_to_bp"},
+	}); gnToBpErr != nil {
+		return fmt.Errorf("Failed to run gn_to_bp: %s", gnToBpErr)
+	}
+	return nil
+}
