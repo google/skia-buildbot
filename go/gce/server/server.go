60c811958c4f0e0a0d396afd25de7a49eed57695
Eric Boren <borenet@google.com>
2018-01-29 14:09:00 -0500

    [autoroll] Add a contact for each roller
    In most cases, I'd prefer to have more than one.
    
    Bug: skia:
    Change-Id: I9fa760a9f13f1fe5c9e9980f8785691ef7cb2e35

--- go/gce/server/server.go
+++ go/gce/server/server.go
@@ -24,6 +24,7 @@ var (
 	delete         = flag.Bool("delete", false, "Delete the instance. Either --create or --delete is required.")
 	deleteDataDisk = flag.Bool("delete-data-disk", false, "Delete the data disk. Only valid with --delete")
 	ignoreExists   = flag.Bool("ignore-exists", false, "Do not fail out when creating a resource which already exists or deleting a resource which does not exist.")
+	listContacts   = flag.Bool("list-contacts", false, "List contacts for each instance and exit.")
 	workdir        = flag.String("workdir", ".", "Working directory.")
 )
 
@@ -65,6 +66,16 @@ func Server20170928(name string) *gce.Instance {
 func Main(zone string, instances map[string]*gce.Instance) {
 	common.Init()
 	defer common.LogPanic()
+
+	if *listContacts {
+		for instance, vm := range instances {
+			fmt.Println(instance + ":")
+			for _, contact := range vm.Contacts {
+				fmt.Println("\t" + contact)
+			}
+		}
+		return
+	}
 
 	vm, ok := instances[*instance]
 	if !ok {
