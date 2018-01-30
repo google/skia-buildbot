2aa4a4396814d98fb68339634951ec604b75e5d4
Eric Boren <borenet@google.com>
2018-01-30 08:22:17 -0500

    [autoroll] Don't run git-cookie-authdaemon in the Android repo manager
    Bug: skia:
    Change-Id: I77a817b536d64b050ccd79fe214137338c2ed701

--- autoroll/setup-script-android.sh
+++ autoroll/setup-script-android.sh
@@ -9,16 +9,10 @@ if [[ ! -e bin/repo ]]; then
   sudo -u default chmod a+x bin/repo
 fi
 
-# Install gcompute-tools if needed.
-if [[ ! -d gcompute-tools ]]; then
-  sudo -u default git clone https://gerrit.googlesource.com/gcompute-tools
-fi
-
 # Add repo and gcompute-tools to PATH if needed.
 if [ -z "$(which repo)" ]; then
   sudo -u default echo '# Add Android tools to PATH"' >> .bashrc
   sudo -u default echo 'export PATH="/home/default/bin:$PATH"' >> .bashrc
-  sudo -u default echo 'export PATH="/home/default/gcompute-tools:$PATH"' >> .bashrc
 fi
 
 # Set git configs required for the repo tool to not prompt.
