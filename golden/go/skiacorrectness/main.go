7ad7adfb9fadf61c384fe27700c0b170d38e18bf
Stephan Altmueller <stephana@google.com>
2018-01-30 14:34:05 -0500

    [gold] Disable tryjob queries when whitelist present
    Bug: skia:
    Change-Id: I145ff85812669941994854acb80e5a28a220340e
    Reviewed-on: https://skia-review.googlesource.com/101842
    Reviewed-by: Kevin Lubick <kjlubick@google.com>
    Commit-Queue: Stephan Altmueller <stephana@google.com>

--- golden/go/skiacorrectness/main.go
+++ golden/go/skiacorrectness/main.go
@@ -96,6 +96,11 @@ const (
 	OAUTH2_CALLBACK_PATH = "/oauth2callback/"
 )
 
+var (
+	// disableIssueQueries controls whether this instance can query tryjob results.
+	disableIssueQueries = false
+)
+
 func main() {
 	defer common.LogPanic()
 	var err error
@@ -310,11 +315,12 @@ func main() {
 		GStorageClient:    gsClient,
 	}
 
-	// Load the whitelist if there is one.
+	// Load the whitelist if there is one and disable querying for issues.
 	if *pubWhiteList != "" {
 		if err := storages.LoadWhiteList(*pubWhiteList); err != nil {
 			sklog.Fatalf("Empty or invalid white list file. A non-empty white list must be provided if force_login=false.")
 		}
+		disableIssueQueries = true
 	}
 
 	// Check if this is public instance. If so make sure there is a white list.
