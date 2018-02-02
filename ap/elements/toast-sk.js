14b2bb6a0b70fea62760a75056db7893db969c49
Joe Gregorio <joe@bitworking.org>
2018-02-02 14:55:22 -0500

    [ap] Initial commit of experimental custom elements.
    Screenshot:
      https://screenshot.googleplex.com/f1UE79qfxEk.png
    
    Bug: skia:
    Change-Id: I95af9cf3ac4feacb321a610cab03ae33c90ce32a

---
+++ ap/elements/toast-sk.js
@@ -0,0 +1,36 @@
+import { upgradeProperty } from './upgrade-property.js'
+
+window.customElements.define('toast-sk', class extends HTMLElement {
+  constructor() {
+    super();
+    this._timer = null;
+  }
+
+  connectedCallback() {
+    if (!this.hasAttribute('duration')) {
+      this.duration = 5000;
+    }
+    upgradeProperty(this, 'duration');
+  }
+
+  get duration() { return +this.getAttribute('duration'); }
+  set duration(val) { this.setAttribute('duration', val); }
+
+  show() {
+    this.setAttribute('shown', '');
+    if (this.duration > 0 && !this._timer) {
+      this._timer = window.setTimeout(() => {
+        this._timer = null;
+        this.hide();
+      }, this.duration);
+    }
+  }
+
+  hide() {
+    this.removeAttribute('shown');
+    if (this._timer) {
+      window.clearTimeout(this._timer);
+      this._timer = null;
+    }
+  }
+});
