6057b09692afac53217847f690d156cb8a31057c
Joe Gregorio <joe@bitworking.org>
2018-02-05 09:54:03 -0500

    [ap] Fix how imports are done.
    Instead of using the -webpack.js extension, we put each element in its own directory
    and we use an index.js file in that directory, which webpack looks for, so:
    
      import 'ap/elements/spinner_sk-webpack.js'
    
    becomes:
    
      import 'skia-elements/spinner-sk'
    
    Also, webpack looks for a .js extension on files, so this:
    
      import { $ } from 'ap/elements/core.js'
    
    changes to:
    
      import { $ } from 'skia-elements/code'
    
    Bug: skia:
    Change-Id: Ic1e6b7c8765b16676eccdcf17cfe10b35d59d955

--- ap/elements/radio-sk.js
+++
@@ -1,7 +0,0 @@
-import { CheckboxElement } from './checkbox-sk.js';
-
-class RadioElement extends CheckboxElement {
-  static get _role() { return 'radio'; }
-}
-
-window.customElements.define('radio-sk', RadioElement);
