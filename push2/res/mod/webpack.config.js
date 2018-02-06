2b703382091440edef1eff01786bebfcc47799f9
Joe Gregorio <joe@bitworking.org>
2018-02-06 11:08:06 -0500

    Consolidate much of the webpack config into a single file.
    Bug: skia:
    Change-Id: I4feb7fd8fe2b8e66d99a216f424381269f6823d6

--- push2/res/mod/webpack.config.js
+++ push2/res/mod/webpack.config.js
@@ -1,57 +1,14 @@
-const path = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-  entry: {
-    push_selection_sk_demo: './push-selection-sk/push-selection-sk-demo.js',
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name]-bundle.js?[chunkhash]',
-    publicPath: '/',
-    library: 'Demo',
-  },
-  resolve: {
-    alias: {
-      'skia-elements': path.resolve(__dirname, '..', '..', '..', 'ap', 'skia-elements'),
-      'common': path.resolve(__dirname, '..', '..', '..', 'res', 'mod'),
-    },
-    modules: [path.resolve(__dirname), "node_modules"],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          use: [
-            { loader:'css-loader', },
-          ],
-        })
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader:'html-loader',
-            options: {
-              name: '[name].[ext]',
-            },
-          }
-        ],
-      },
-    ]
-  },
-  plugins: [
-    new ExtractTextPlugin({
-      filename: '[name]-bundle.css?[contenthash]',
-    }),
-    new HtmlWebpackPlugin({
-      filename: 'push-selection-sk-demo.html',
-      template: './push-selection-sk/push-selection-sk-demo.html',
-      chunks: ['push_selection_sk_demo'],
-    }),
-    new CleanWebpackPlugin(['dist']),
-  ],
-}
+var common = require('../../../res/mod/webpack.common.js');
+
+common.entry.push_selection_sk_demo = './push-selection-sk/push-selection-sk-demo.js';
+common.plugins.push(
+  new HtmlWebpackPlugin({
+    filename: 'push-selection-sk-demo.html',
+    template: './push-selection-sk/push-selection-sk-demo.html',
+    chunks: ['push_selection_sk_demo'],
+  }),
+);
+
+module.exports = common;
