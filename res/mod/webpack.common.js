a8b5e65394879ba84e1028456e1770ee70bcb38a
Joe Gregorio <joe@bitworking.org>
2018-02-08 09:15:01 -0500

    Add a production target to webpack.common.js.
    Bug: skia:
    Change-Id: Ia1ba188043ad13356807795b3a9ae6b23e7c4df6

--- res/mod/webpack.common.js
+++ res/mod/webpack.common.js
@@ -10,6 +10,31 @@
 //       let common = commonBuilder(__dirname);
 //       module.exports = common;
 //
+//    If you are just building demo pages for elements:
+//
+//       const { commonBuilder } = require('./webpack.common.js');
+//       const { demoFinder } = require('./webpack.demo-finder.js')
+//
+//       module.exports = demoFinder(__dirname, commonBuilder(__dirname));
+//
+//    For an application you need to add the entry points and associate them
+//    with HTML files:
+//
+//        const { commonBuilder } = require('../res/mod/webpack.common.js');
+//        const HtmlWebpackPlugin = require('html-webpack-plugin');
+//
+//        let common = commonBuilder(__dirname);
+//        common.entry.index = './pages/index.js'
+//        common.plugins.push(
+//            new HtmlWebpackPlugin({
+//              filename: 'index.html',
+//              template: './pages/index.html',
+//              chunks: ['index'],
+//            })
+//        );
+//
+//        module.exports = common
+//
 // This config adds aliases for the 'skia-elements' and 'common' libraries,
 // so they will always be available at those prefixes. See 'demo/demo.js'.
 //
@@ -19,24 +44,32 @@
 //
 //    @import '~common/colors'
 //
-//
 // You do not need to add any of the plugins or loaders used here to your
 // local package.json, on the other hand, if you add new loaders or plugins
 // in your local project then you should 'yarn add' them to your local
 // package.json.
 //
+// This config understands NODE_ENV and can build production versions
+// of assets by setting the environment variable. An example Makefile:
+//
+//     build:
+//      	npx webpack
+//
+//     release:
+//      	NODE_ENV=production npx webpack
+//
+//
 // NB - Remember to 'yarn add' any plugins or loaders added to this file in
 //   this directory so they are available to all users of webpack.common.js.
 //
-// TODO - Add a production option to this config to produce minimized js, css, etc.
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const MinifyPlugin = require("babel-minify-webpack-plugin");
 
 module.exports.commonBuilder = function(dirname) {
-  return {
+  let common = {
     entry: {
       // Users of webpack.common must fill in the entry point(s).
     },
@@ -114,5 +147,13 @@ module.exports.commonBuilder = function(dirname) {
       // need to make sure they installed them in their project via yarn.
     ],
   };
+  if (process.env.NODE_ENV == 'production') {
+    common.plugins.push(
+      new MinifyPlugin({}, {
+        comments: false,
+      })
+    )
+  }
+  return common;
 
 };
