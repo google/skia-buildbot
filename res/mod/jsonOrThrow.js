d2b23db7888a0099eb928a188744fc71f59753ec
Joe Gregorio <joe@bitworking.org>
2018-02-08 14:31:03 -0500

    Break out jsonOrThrow helper function into its own module.
    Bug: skia:
    Change-Id: Idbdef99fced75c823e76be22e16ecc050419b148

---
+++ res/mod/jsonOrThrow.js
@@ -0,0 +1,11 @@
+// Helper function when making fetch() request.
+//
+//    fetch('/_/list').then(jsonOrThrow).then(json => {
+//      // Do something with the parsed json here.
+//    }).catch(errorMessage);
+export const jsonOrThrow = (resp) => {
+  if (resp.ok) {
+    return resp.json();
+  }
+  throw 'Bad network response.';
+}
