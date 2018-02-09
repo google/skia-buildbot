ed7f9df750a3a4cdf8959c3cea127db9576c48c8
Joe Gregorio <joe@bitworking.org>
2018-02-09 16:59:19 -0500

    Add karma testing for some modules.
    Bug: skia:
    Change-Id: Ia0304cfb6bc745d2e30a7cb02a1c6812f8506525
    Reviewed-on: https://skia-review.googlesource.com/106240
    Commit-Queue: Joe Gregorio <jcgregorio@google.com>
    Reviewed-by: Kevin Lubick <kjlubick@google.com>

--- res/mod/webpack.config.js
+++ res/mod/webpack.config.js
@@ -1,4 +1,8 @@
 const { commonBuilder } = require('./webpack.common.js');
 const { demoFinder } = require('./webpack.demo-finder.js')
+const { glob } = require('glob');
 
-module.exports = demoFinder(__dirname, commonBuilder(__dirname));
+let common = demoFinder(__dirname, commonBuilder(__dirname));
+common.entry.tests = glob.sync('./tests/*.js');
+
+module.exports = common;
