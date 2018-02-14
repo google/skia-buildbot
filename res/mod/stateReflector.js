5405c474da313aba1aa23562fe1776ccb4ef9c5c
Joe Gregorio <joe@bitworking.org>
2018-02-14 10:50:34 -0500

    [pushd] Reflect search string to query.
    Bug: skia:
    Change-Id: I38f25ed2056f5136b5e3fd8b7a2f0013dd767f65

---
+++ res/mod/stateReflector.js
@@ -0,0 +1,54 @@
+import * as query from './query.js'
+import * as object from './object.js'
+import { DomReady } from './dom.js'
+
+// Track the state of an object and reflect it to and from the URL.
+//
+//   stateHolder - An object with a property <name> where the state to be reflected
+//        into the URL is stored. We need the level of indirection because
+//        JS doesn't have pointers.
+//
+//        The state must be on Object and all the values in the Object
+//        must be Number, String, Boolean, Object, or Array of String.
+//        Doesn't handle NaN, null, or undefined.
+//
+//   cb   - A callback of the form function() that is called when state has been
+//        changed by a change in the URL.
+//
+//   name - The name of the property in stateHolder that holds the state.
+//
+// Returns
+//        A function to call when state has changed and needs to be reflected
+//        to the URL.
+export function stateReflector(stateHolder, name, cb) {
+  // The default state of the stateHolder. Used to calculate diffs to state.
+  let defaultState = object.deepCopy(stateHolder[name]);
+
+  // The last state of the stateHolder. Used to determine if the stateHolder[name] has changed recently.
+  let lastState = object.deepCopy(stateHolder[name]);
+
+  // stateFromURL should be called when the URL has changed, it updates
+  // the stateHolder[name] and triggers the callback.
+  const stateFromURL = () => {
+    let delta = query.toObject(window.location.search.slice(1), defaultState);
+    stateHolder[name] = object.applyDelta(delta, defaultState);
+    lastState = object.deepCopy(stateHolder[name]);
+    cb();
+  }
+
+  // When we are loaded we should update the state from the URL.
+  DomReady.then(stateFromURL);
+
+  // Every popstate event should also update the state.
+  window.addEventListener('popstate', stateFromURL);
+
+  // Return a function to call when the state has changed to force reflection into the URL.
+  return () => {
+    if (Object.keys(object.getDelta(lastState, stateHolder[name])).length > 0) {
+      lastState = object.deepCopy(stateHolder[name]);
+      let q = query.fromObject(object.getDelta(stateHolder[name], defaultState));
+      history.pushState(null, "", window.location.origin + window.location.pathname + "?" +  q);
+    }
+  };
+}
+
