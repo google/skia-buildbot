42a2849c76d918b2eff172cc03b9c1b6bdff1e0f
Joe Gregorio <joe@bitworking.org>
2018-02-14 14:47:34 -0500

    Add documentation for skia-elements.
    Bug: skia:
    Change-Id: I2486b1839703dc2315a6703ccc14901c7e15f985

--- ap/skia-elements/collapse-sk/collapse-sk.js
+++ ap/skia-elements/collapse-sk/collapse-sk.js
@@ -1,5 +1,20 @@
 import { upgradeProperty } from '../dom'
 
+// The <collapse-sk> custom element declaration.
+//
+//  Attributes:
+//    closed - A boolean attribute that, if present, causes the element to
+//             collapse, i.e., transition to display: none.
+//
+//  Properties:
+//    closed - Mirrors the 'closed' attribute.
+//
+//  Events:
+//    None
+//
+//  Methods:
+//    None
+//
 window.customElements.define('collapse-sk', class extends HTMLElement {
   connectedCallback() {
     upgradeProperty(this, 'closed');
