e74284c7e7db73451fff0de5166ee90c18212478
Joe Gregorio <joe@bitworking.org>
2018-02-15 09:00:32 -0500

    nav-links-sk should close when ESC is pressed or the focus moves elsewhere.
    Bug: skia:
    Change-Id: I953767a17464f98b990f2db702016cc923c0eb41
    Reviewed-on: https://skia-review.googlesource.com/107680
    Reviewed-by: Kevin Lubick <kjlubick@google.com>
    Commit-Queue: Joe Gregorio <jcgregorio@google.com>

--- ap/skia-elements/nav-sk/nav-sk.js
+++ ap/skia-elements/nav-sk/nav-sk.js
@@ -2,23 +2,35 @@ import '../icon-sk';
 import '../buttons';
 import { upgradeProperty } from '../dom'
 
-const navButtonSk = document.createElement('template');
-navButtonSk.innerHTML = `<button><icon-menu-sk></icon-menu-sk></button>`;
-
-// TODO(jcgregorio) Add support for 'ESC' key and clicking outside
-// the element to close the nav-links-sk.
-
 // The <nav-button-sk> custom element declaration.
 //
 // Allows for the creation of a pop-up menu. The actual menu is contained
 // in a sibling <nav-links-sk> element. For example:
 //
 //    <nav-button-sk></nav-button-sk>
-//    <nav-links-sk closed>
+//    <nav-links-sk shown>
 //      <a href="">Main</a>
 //      <a href="">Triage</a>
 //      <a href="">Alerts</a>
-//    </nav-links-sk closed>
+//    </nav-links-sk>
+//
+//  <nav-button-sk> is just a convenience that contains the hamburger menu
+//  and toggles the shown property of a sibling 'nav-links-sk'. Other types
+//  of popup menus can be created using buttons and icons directly. For
+//  example:
+//
+//    <button onclick="this.nextElementSibling.shown = true;">
+//      <icon-create-sk></icon-create-sk>
+//    </button>
+//    <nav-links-sk>
+//      <a href="">New A</a>
+//      <a href="">New B</a>
+//      <a href="">New C</a>
+//    </nav-links-sk>
+//
+//
+//  The children of 'nav-links-sk' does not have to be links, it could
+//  be other elements, such as buttons.
 //
 //  Attributes:
 //    None
@@ -35,8 +47,7 @@ navButtonSk.innerHTML = `<button><icon-menu-sk></icon-menu-sk></button>`;
 window.customElements.define('nav-button-sk', class extends HTMLElement {
   connectedCallback() {
     this.addEventListener('click', this);
-    let icon = navButtonSk.content.cloneNode(true);
-    this.appendChild(icon);
+    this.innerHTML = `<button><icon-menu-sk></icon-menu-sk></button>`;
   }
 
   disconnectedCallback() {
@@ -45,7 +56,10 @@ window.customElements.define('nav-button-sk', class extends HTMLElement {
 
   handleEvent(e) {
     if (this.nextElementSibling.tagName === "NAV-LINKS-SK") {
-      this.nextElementSibling.closed = !this.nextElementSibling.closed;
+      this.nextElementSibling.shown = !this.nextElementSibling.shown;
+      if (this.nextElementSibling.shown) {
+        this.nextElementSibling.firstElementChild.focus();
+      }
     }
   }
 });
@@ -54,30 +68,67 @@ window.customElements.define('nav-button-sk', class extends HTMLElement {
 //
 // See the documentation above for nav-button-sk.
 //
+// The nav-links-sk will closed if the user presses ESC, or if focus moves off
+// of <nav-links-sk> or any of its children.
+//
 //  Attributes:
-//    closed - A boolean attribute controlling if the list of links
+//    shown - A boolean attribute controlling if the list of links
 //             is displayed or not.
 //
 //  Properties:
-//    closed - Mirrors the 'closed' attribute.
-//
-//  Events:
-//    None
+//    shown - Mirrors the 'shown' attribute.
 //
 //  Methods:
 //    None
 //
+//  Events:
+//    closed - This event is generated when nav-links-sk is closed.
+//
 window.customElements.define('nav-links-sk', class extends HTMLElement {
-  connectedCallback() {
-    upgradeProperty(this, 'closed');
+  static get observedAttributes() {
+    return ['shown'];
   }
 
-  get closed() { return this.hasAttribute('closed'); }
-  set closed(val) {
+  connectedCallback() {
+    upgradeProperty(this, 'shown');
+  }
+
+  get shown() { return this.hasAttribute('shown'); }
+  set shown(val) {
     if (val) {
-      this.setAttribute('closed', '');
+      this.setAttribute('shown', '');
     } else {
-      this.removeAttribute('closed');
+      this.removeAttribute('shown');
+    }
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (newValue !== null) {
+      window.addEventListener('keydown', this);
+      window.addEventListener('focusin', this);
+    } else {
+      window.removeEventListener('keydown', this);
+      window.removeEventListener('focusin', this);
+      this.dispatchEvent(new CustomEvent('closed', { bubbles: true }));
+    }
+  }
+
+  handleEvent(e) {
+    if (e.type === 'keydown') {
+        if (e.key === "Escape") {
+          e.preventDefault();
+          this.shown = false;
+        }
+    } else {
+      // If focus is not on 'this' or its children then close.
+      let ele = e.target;
+      while (ele !== this && ele !== null) {
+        ele = ele.parentElement;
+      }
+      if (!ele) {
+        this.shown = false;
+      }
     }
   }
 });
+
