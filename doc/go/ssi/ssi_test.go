c1a51afb9f854b4faefd84a36b2de35b1c04964f
Stephan Altmueller <stephana@google.com>
2018-03-05 08:18:54 -0500

    Add simple mechanism to add custom server side tags to docserver
    - adds a the ability to add custom server side tags to docserver
    - implements a tag to show the listing of a GCS folder
    
    
    Bug: skia:
    Change-Id: Ib758d75f02e11135f57f8b535bb9da345d3265f4

---
+++ doc/go/ssi/ssi_test.go
@@ -0,0 +1,87 @@
+package ssi
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"strings"
+	"testing"
+
+	"cloud.google.com/go/storage"
+	"github.com/alecthomas/template"
+	assert "github.com/stretchr/testify/require"
+	"go.skia.org/infra/go/auth"
+	"go.skia.org/infra/go/testutils"
+	"google.golang.org/api/option"
+)
+
+func TestProcessSSI(t *testing.T) {
+	testutils.SmallTest(t)
+
+	// Inject our test function below.
+	processFNs["testfn"] = getTestProcessFn(t, "testfn")
+	processFNs["errfn"] = func(map[string]string) ([]byte, error) {
+		return nil, fmt.Errorf("Error!")
+	}
+
+	checkSSI(t, "<p><ssi:testfn k1=v2></p>", "<p>testfn:k1:v2:</p>")
+	checkSSI(t, "<p><ssi:testfn k1=v2 k2=v2></p>", "<p>testfn:k1:v2:k2:v2:</p>")
+	checkSSI(t, "<p><ssi:testfn></p>", "<p>testfn:</p>")
+
+	_, err := ProcessSSI([]byte("<p><ssi:invalidfn k1=v1></p>"))
+	assert.Error(t, err)
+	_, err = ProcessSSI([]byte("<p><ssi:testfn =v1></p>"))
+	assert.Error(t, err)
+	_, err = ProcessSSI([]byte("<p><ssi:errfn k1=v1></p>"))
+	assert.Error(t, err)
+}
+
+const (
+	// Alternate row template to make testing easier.
+	testRowTmpl = "{{.created}}:{{.url}}:{{.name}}:{{.date}}:{{.commit}}:{{.commit_url}}:{{.commit_message}}\n"
+
+	// Expected output.
+	expOutput = `2018-03-05T12:44:24Z:https://storage.cloud.google.com/skia-infra-testdata/skqp-testing/skqp-universal-006-debug.apk:skqp-universal-006-debug.apk: 2018-03-05T12:32:58Z:95a7b76a44edd2f25423a4d395df553b80fe06d7:https://example.com/+/95a7b76a44edd2f25423a4d395df553b80fe06d7:Add new feature to dm
+2018-03-05T12:44:17Z:https://storage.cloud.google.com/skia-infra-testdata/skqp-testing/skqp-universal-005-debug.apk:skqp-universal-005-debug.apk: 2018-03-05T12:32:58Z:95a7b76a44edd2f25423a4d395df553b80fe06d7:https://example.com/+/95a7b76a44edd2f25423a4d395df553b80fe06d7:Add new feature to dm
+2018-03-05T12:44:09Z:https://storage.cloud.google.com/skia-infra-testdata/skqp-testing/skqp-universal-004-debug.apk:skqp-universal-004-debug.apk: 2018-03-05T12:32:58Z:95a7b76a44edd2f25423a4d395df553b80fe06d7:https://example.com/+/95a7b76a44edd2f25423a4d395df553b80fe06d7:Add new feature to dm
+2018-03-05T12:44:01Z:https://storage.cloud.google.com/skia-infra-testdata/skqp-testing/skqp-universal-003-debug.apk:skqp-universal-003-debug.apk: 2018-03-05T12:32:58Z:95a7b76a44edd2f25423a4d395df553b80fe06d7:https://example.com/+/95a7b76a44edd2f25423a4d395df553b80fe06d7:Add new feature to dm
+2018-03-05T12:32:53Z:https://storage.cloud.google.com/skia-infra-testdata/skqp-testing/skqp-universal-002-debug.apk:skqp-universal-002-debug.apk: 2018-03-05T12:32:58Z:95a7b76a44edd2f25423a4d395df553b80fe06d7:https://example.com/+/95a7b76a44edd2f25423a4d395df553b80fe06d7:Add new feature to dm`
+)
+
+func TestGCEListing(t *testing.T) {
+	testutils.MediumTest(t)
+
+	// Swap out the templates to make testing easier.
+	listGCSTagSnippet = "%s"
+	gceListingRowTmpl = template.Must(template.New("row").Parse(testRowTmpl))
+
+	tokenSrc, err := auth.NewJWTServiceAccountTokenSource("", "", storage.ScopeFullControl)
+	assert.NoError(t, err)
+
+	client, err := storage.NewClient(context.Background(), option.WithTokenSource(tokenSrc))
+	assert.NoError(t, err)
+
+	Init("https://example.com", client)
+	doc, err := ProcessSSI([]byte("<ssi:listgce path=skia-infra-testdata/skqp-testing>"))
+	assert.NoError(t, err)
+	assert.NotNil(t, doc)
+	assert.Equal(t, expOutput, strings.TrimSpace(string(doc)))
+}
+
+func checkSSI(t *testing.T, tag, expDoc string) {
+	doc, err := ProcessSSI([]byte(tag))
+	assert.NoError(t, err)
+	assert.Equal(t, expDoc, string(doc))
+}
+
+func getTestProcessFn(t *testing.T, tagName string) ssiProcessFn {
+	return func(params map[string]string) ([]byte, error) {
+		var buf bytes.Buffer
+		buf.Write([]byte(tagName + ":"))
+		for k, v := range params {
+			_, err := buf.Write([]byte(fmt.Sprintf("%s:%s:", k, v)))
+			assert.NoError(t, err)
+		}
+		return buf.Bytes(), nil
+	}
+}
