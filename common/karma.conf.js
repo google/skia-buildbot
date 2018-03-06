05cbc5351aeda536a0469e01f4879d12f6e1668f
Joe Gregorio <joe@bitworking.org>
2018-03-05 20:08:42 -0500

    Got tests working again, adding in element tests.
    Bug: skia:
    Change-Id: Ib8bc1e053a3763f8658559505946813a425729af

---
+++ common/karma.conf.js
@@ -0,0 +1,81 @@
+// Copyright (c) 2014 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file.
+//
+// Use of this source code is governed by a BSD-style
+//  license that can be found in the LICENSE file.
+//
+// Karma configuration
+//
+var webpackConfig = require('./webpack.config.js');
+webpackConfig.entry = null;
+
+module.exports = function(config) {
+  config.set({
+
+    // base path, that will be used to resolve files and exclude
+    basePath: '',
+
+
+    // frameworks to use
+    frameworks: ['mocha', 'chai', 'sinon'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+      'tests/*.js',
+    ],
+
+    preprocessors: {
+      // add webpack as preprocessor
+      'tests/*.js': [ 'webpack' ],
+    },
+
+    // list of files to exclude
+    exclude: [
+    ],
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+    reporters: ['dots'],
+
+
+    // Get the port from KARMA_PORT if it is set.
+    port: parseInt(process.env.KARMA_PORT || "9876"),
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: false,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR ||
+    // config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: true,
+
+
+    // Start these browsers; we only care about Chrome for infra projects.
+    browsers: ['Chrome'],
+
+
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
+
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    //
+    // This can be over-ridden by command-line flag when running Karma. I.e.:
+    //
+    //    ./node_modules/karma/bin/karma --no-single-run start
+    //
+    singleRun: true,
+
+    webpack: webpackConfig,
+  });
+};
---
+++ common/tests/select-sk.js
@@ -0,0 +1,21 @@
+import '../modules/select-sk'
+
+let container = document.createElement("div");
+document.body.appendChild(container);
+
+afterEach(function() {
+  container.innerHTML = "";
+});
+
+describe('Test select-sk custom element.',
+  function() {
+    it('Test defaults', function() {
+      return window.customElements.whenDefined('select-sk').then(() => {
+        let s = document.createElement('select-sk');
+        container.appendChild(s);
+        assert.equal(-1, s._selection);
+        container.removeChild(s);
+      })
+    });
+  }
+);
