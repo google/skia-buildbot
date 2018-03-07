8023f855a4450048c17729fefd8e77dd89389aaa
Joe Gregorio <joe@bitworking.org>
2018-03-07 12:10:36 -0500

    docs kinda work
    Bug: skia:
    Change-Id: I3bf9f56469d95618a30110eddbaa48e3c93987df

---
+++ common/plugins/element.js
@@ -0,0 +1,42 @@
+exports.defineTags = function(dictionary) {
+  dictionary.defineTag('evt', {
+    canHaveName: true,
+    onTagged: function(doclet, tag) {
+      if (!doclet.webevents) {
+        doclet.webevents= [];
+      }
+
+      doclet.webevents.push(
+        {
+          name: tag.value.name,
+          description: tag.value.description || '',
+        }
+      );
+    }
+  });
+}
+
+const eventRows = (events) => events.map( (e) => `
+  <tr>
+    <td>${e.name}</td>
+    <td><pre>${e.description}</pre></td>
+  </tr>
+`);
+
+exports.handlers = {
+  newDoclet: function(e) {
+    if (e.doclet.webevents) {
+      const table = `<h5>Events</h5>
+        <table class=params>
+          <thead>
+            <th>Name</th>
+            <th>Description</th>
+          </thead>
+          ${eventRows(e.doclet.webevents).join('')}
+        </table>
+      `;
+      e.doclet.description = `${e.doclet.description || ''}
+      ${table}`;
+    }
+  }
+}
