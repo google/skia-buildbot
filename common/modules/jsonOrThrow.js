fa4fa16c81e514c70b1f30bc08a96c66893bf079
Joe Gregorio <joe@bitworking.org>
2018-03-08 12:50:40 -0500

    [common] Clean up some docs.
    Bug: skia:
    Change-Id: Id83f169951c0828aef2271089e82a56b89227924

--- common/modules/jsonOrThrow.js
+++ common/modules/jsonOrThrow.js
@@ -9,8 +9,10 @@
  *    fetch('/_/list').then(jsonOrThrow).then(json => {
  *      // Do something with the parsed json here.
  *    }).catch(errorMessage);
+ *
+ * @returns {Promise}
  */
-export const jsonOrThrow = (resp) => {
+export function jsonOrThrow(resp) {
   if (resp.ok) {
     return resp.json();
   }
