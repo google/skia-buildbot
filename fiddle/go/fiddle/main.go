7d3b3d53400a398f6bf9a1d451e561c3e029c474
Joe Gregorio <joe@bitworking.org>
2018-03-14 11:43:40 -0400

    Pass along compiler warnings.
    Bug: skia:
    Change-Id: Icd971bd49bf8ecd0825b451e63ba1626b6e488ff

--- fiddle/go/fiddle/main.go
+++ fiddle/go/fiddle/main.go
@@ -420,11 +420,15 @@ func run(ctx context.Context, user string, req *types.FiddleContext) (*types.Run
 	}
 	// Take the compiler output and strip off all the implementation dependant information
 	// and format it to be retured in types.RunResults.
-	if res.Compile.Errors != "" {
+	if res.Compile.Output != "" {
 		lines := strings.Split(res.Compile.Output, "\n")
 		for _, line := range lines {
 			match := parseCompilerOutput.FindAllStringSubmatch(line, -1)
 			if match == nil || len(match[0]) < 5 {
+				// Skip the ninja generated lines.
+				if strings.HasPrefix(line, "ninja:") || strings.HasPrefix(line, "[") {
+					continue
+				}
 				resp.CompileErrors = append(resp.CompileErrors, types.CompileError{
 					Text: line,
 					Line: 0,
