439804f31a544e5a51d220af4b5dc634397073e8
Joe Gregorio <joe@bitworking.org>
2018-03-14 16:50:16 -0400

    Capture lottie screenshots from the web player.
    Bug: skia:
    Change-Id: I48356b3eaa7df4ca44c98aa7e519a4a119a257cb

---
+++ lottiecap/drive.js
@@ -0,0 +1,20 @@
+const puppeteer = require('puppeteer');
+
+function resolveAfter2Seconds() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve();
+    }, 2000);
+  });
+}
+
+(async() => {
+const browser = await puppeteer.launch();
+const page = await browser.newPage();
+await page.goto('http://localhost:8080/driver.html', {waitUntil: 'networkidle2'});
+await resolveAfter2Seconds();
+await page.screenshot({path: 'page.png'});
+
+
+await browser.close();
+})();
