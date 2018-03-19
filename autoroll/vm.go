650be80879aa2b0dcd6058d06ab433e47f384a91
Ravi Mistry <rmistry@google.com>
2018-03-19 12:39:43 -0400

    Checkpoint
    Bug: skia:
    Change-Id: I2b7d3e29fefbfe874489ba22cb243468d371b076

--- autoroll/vm.go
+++ autoroll/vm.go
@@ -124,6 +124,21 @@ func PDFium() *gce.Instance {
 	return vm
 }
 
+func Flutter() *gce.Instance {
+	vm := AutoRollBase("flutter-autoroll", "" /* Use ephemeral IP */)
+	vm.Contacts = []string{
+		"rmistry@google.com",
+		"brianosman@google.com",
+	}
+	vm.ServiceAccount = "skia-flutter-autoroll@skia-buildbots.google.com.iam.gserviceaccount.com"
+
+	// TODO(rmistry): Will need some sort of setup
+	//_, filename, _, _ := runtime.Caller(0)
+	//dir := path.Dir(filename)
+	//vm.SetupScript = path.Join(dir, "setup-script-github.sh")
+	return vm
+}
+
 func Fuchsia() *gce.Instance {
 	vm := AutoRollBase("fuchsia-autoroll", "" /* Use ephemeral IP */)
 	vm.Contacts = []string{
@@ -225,6 +240,7 @@ func main() {
 		"catapult":              Catapult(),
 		"chromite-chromium":     Chromite_Chromium(),
 		"depot-tools-chromium":  DepotTools_Chromium(),
+		"flutter":               Flutter(),
 		"fuchsia":               Fuchsia(),
 		"google3":               Google3(),
 		"ios-internal-chromium": IosInternal_Chromium(),
