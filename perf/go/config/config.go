09b0866be3cad6e17a67fcec513bb4f813a502ef
Yuqian Li <liyuqian@google.com>
2018-03-21 14:50:40 -0400

    Increase MAX_SAMPLE_TRACES_PER_CLUSTER to 200
    It seems that some "compiler noise" could well persist
    for 50 commits. Hence let's increase this from 50 to 200
    to further prune those pesky "compiler noise".
    
    Bug: skia:
    Change-Id: Id1c25c0bc84a629b0c506f8bdc8758de7a46fe6b

--- perf/go/config/config.go
+++ perf/go/config/config.go
@@ -3,7 +3,7 @@ package config
 const (
 	// MAX_SAMPLE_TRACES_PER_CLUSTER  is the maximum number of traces stored in a
 	// ClusterSummary.
-	MAX_SAMPLE_TRACES_PER_CLUSTER = 50
+	MAX_SAMPLE_TRACES_PER_CLUSTER = 200
 
 	// MIN_STDDEV is the smallest standard deviation we will normalize, smaller
 	// than this and we presume it's a standard deviation of zero.
