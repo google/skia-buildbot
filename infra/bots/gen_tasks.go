d8d8b137ec2accc4c9829c0df8734b886ceae8e4
Joe Gregorio <joe@bitworking.org>
2018-03-26 10:14:59 -0400

    try to use node
    Bug: skia:
    Change-Id: I731b2719c8e86ab94bacf1b46c9837ea67b52596

--- infra/bots/gen_tasks.go
+++ infra/bots/gen_tasks.go
@@ -94,6 +94,14 @@ var (
 		},
 	}
 
+	CIPD_PKGS_NODE = []*specs.CipdPackage{
+		&specs.CipdPackage{
+			Name:    "skia/bots/node",
+			Path:    "cipd_bin_packages",
+			Version: "0",
+		},
+	}
+
 	LOGDOG_ANNOTATION_URL = fmt.Sprintf("logdog://logs.chromium.org/%s/%s/+/annotations", PROJECT, specs.PLACEHOLDER_TASK_ID)
 )
 
@@ -157,7 +165,7 @@ func bundleRecipes(b *specs.TasksCfgBuilder) string {
 func infra(b *specs.TasksCfgBuilder, name string) string {
 	bundle := bundleRecipes(b)
 
-	pkgs := append([]*specs.CipdPackage{b.MustGetCipdPackageFromAsset("go")}, CIPD_PKGS_GSUTIL...)
+	pkgs := append([]*specs.CipdPackage{b.MustGetCipdPackageFromAsset("go"), b.MustGetCipdPackageFromAsset("node")}, CIPD_PKGS_GSUTIL...)
 	if strings.Contains(name, "Large") {
 		pkgs = append(pkgs, b.MustGetCipdPackageFromAsset("protoc"))
 	}
