42f970e9db7524d19ca6d6459252e27b836972ba
Joe Gregorio <joe@bitworking.org>
2018-03-26 11:09:12 -0400

    Keep alias's up to date with ip list.
    Bug: skia:
    Change-Id: I33c3e2ad363325684e774d997eb7737094a8bec4
    Reviewed-on: https://skia-review.googlesource.com/116460
    Reviewed-by: Eric Boren <borenet@google.com>
    Commit-Queue: Joe Gregorio <jcgregorio@google.com>

--- skolo/bash/skolo.sh
+++ skolo/bash/skolo.sh
@@ -3,6 +3,9 @@
 # Utilities for accessing the skolo.
 # Should be sourced from $HOME/.bashrc
 
-alias skolo_win='ssh chrome-bot@100.115.95.132'
-alias skolo_rpi='ssh chrome-bot@100.115.95.143'
+alias skolo_internal='ssh chrome-bot@100.115.95.131'
 alias skolo_linux='ssh chrome-bot@100.115.95.130'
+alias skolo_rpi='ssh chrome-bot@100.115.95.143'
+alias skolo_win='ssh chrome-bot@100.115.95.132'
+alias skolo_win2='ssh chrome-bot@100.115.95.133'
+alias skolo_win3='ssh chrome-bot@100.115.95.134'
