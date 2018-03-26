e051c864c114c6e21c5f87394ea74ec1e59f3f64
Eric Boren <borenet@google.com>
2018-03-26 15:16:06 -0400

    [autoroll] More sophisticated notifications
    Send emails or chat message, with threading and filtering customizable
    per-roller.
    
    Bug: skia:7701
    Change-Id: Ic9ca83f05c322d8df8b1d83ea7a08087cb914c52

---
+++ autoroll/go/notifier/manager.go
@@ -0,0 +1,68 @@
+package notifier
+
+import (
+	"golang.org/x/sync/errgroup"
+)
+
+// Message represents a message to be sent through one or more Notifiers.
+type Message struct {
+	// Required. Subject line of the message. This is ignored in favor of
+	// the default in single-thread mode.
+	Subject string
+	// Required. Body of the message.
+	Body string
+	// Severity of the message. May cause the message not to be sent,
+	// depending on filter settings.
+	Severity Severity
+}
+
+// filteredThreadedNotifier groups a Notifier with a Filter and an optional
+// static subject line for all messages to this Notifier.
+type filteredThreadedNotifier struct {
+	notifier            Notifier
+	filter              Filter
+	singleThreadSubject string
+}
+
+// Manager is a struct used for sending notification through zero or more
+// Notifiers.
+type Manager struct {
+	notifiers []*filteredThreadedNotifier
+}
+
+// Send a notification.
+func (m *Manager) Send(msg *Message) error {
+	var group errgroup.Group
+	for _, n := range m.notifiers {
+		n := n
+		group.Go(func() error {
+			if n.filter.ShouldSend(msg.Severity) {
+				subject := msg.Subject
+				if n.singleThreadSubject != "" {
+					subject = n.singleThreadSubject
+				}
+				return n.notifier.Send(subject, msg)
+			}
+			return nil
+		})
+	}
+	return group.Wait()
+}
+
+// Return a Manager instance.
+func NewManager() (*Manager, error) {
+	return &Manager{
+		notifiers: []*filteredThreadedNotifier{},
+	}, nil
+}
+
+// Add a new Notifier, which filters according to the given Filter. If
+// singleThreadSubject is provided, that will be used as the subject for all
+// Messages, ignoring their Subject field.
+func (m *Manager) Add(n Notifier, f Filter, singleThreadSubject string) {
+	m.notifiers = append(m.notifiers, &filteredThreadedNotifier{
+		notifier:            n,
+		filter:              f,
+		singleThreadSubject: singleThreadSubject,
+	})
+}
---
+++ autoroll/go/notifier/notifier.go
@@ -0,0 +1,62 @@
+package notifier
+
+import (
+	"go.skia.org/infra/go/chatbot"
+	"go.skia.org/infra/go/email"
+)
+
+const (
+	EMAIL_FROM_ADDRESS = "autoroller@skia.org"
+)
+
+// Notifier is an interface used for sending notifications from an AutoRoller.
+type Notifier interface {
+	// Send the given message.
+	Send(string, *Message) error
+}
+
+// emailNotifier is a Notifier implementation which sends email to interested
+// parties.
+type emailNotifier struct {
+	from   string
+	gmail  *email.GMail
+	markup string
+	to     []string
+}
+
+// See documentation for Notifier interface.
+func (n *emailNotifier) Send(subject string, msg *Message) error {
+	if n.gmail == nil {
+		return nil
+	}
+	return n.gmail.SendWithMarkup(n.from, n.to, subject, msg.Body, n.markup)
+}
+
+// EmailNotifier returns a Notifier which sends email to interested parties.
+// Sends the same ViewAction markup with each message.
+func EmailNotifier(emails []string, emailer *email.GMail, markup string) (Notifier, error) {
+	return &emailNotifier{
+		from:   EMAIL_FROM_ADDRESS,
+		gmail:  emailer,
+		markup: markup,
+		to:     emails,
+	}, nil
+}
+
+// chatNotifier is a Notifier implementation which sends chat messages.
+type chatNotifier struct {
+	room string
+}
+
+// See documentation for Notifier interface.
+func (n *chatNotifier) Send(thread string, msg *Message) error {
+	// TODO(borenet): How to thread?
+	return chatbot.Send(msg.Body, n.room)
+}
+
+// ChatNotifier returns a Notifier which sends email to interested parties.
+func ChatNotifier(room string) (Notifier, error) {
+	return &chatNotifier{
+		room: room,
+	}, nil
+}
