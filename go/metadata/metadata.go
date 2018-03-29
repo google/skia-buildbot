18e208ee1344c2156be0c1542f76253f3c3c6cbb
Joe Gregorio <joe@bitworking.org>
2018-03-29 10:28:53 -0400

    [auth] Have a consistent idea of an instance service account.
    Part of forklift is to move to using specific service accounts on
    an instance and not the default service account. Which means apps
    should have a simple way to get the instance level service account,
    even when running in skolo.
    
    Today a server can call:
    
       auth.NewDefaultClient(*local, scopes...)
    
    and it will work locally and on GCE, but not in skolo. Since we
    now have an access token provider in skolo we can get this to
    work everywhere.
    
    Note that this CL also adds:
    
       auth.NewDefaultTokenSource(*local, scopes...)
    
    Since this is needed for APIs that use gRPC.
    
    This CL also adds ProjectId() which is used in this CL and will
    also be used in the revised push/pulld.
    
    Bug: skia:
    Change-Id: Ia56b26a9101346de6cccbb9626b982e0a4d05457

--- go/metadata/metadata.go
+++ go/metadata/metadata.go
@@ -79,11 +79,16 @@ const (
 	// to the metadata server.
 	HEADER_MD_FLAVOR_KEY = "Metadata-Flavor"
 	HEADER_MD_FLAVOR_VAL = "Google"
+
+	SKOLO_PROJECT_ID = "skolo"
 )
 
 var (
 	// Metadata path for a default service account token.
 	TOKEN_PATH = fmt.Sprintf(METADATA_PATH_PREFIX_TMPL, LEVEL_INSTANCE) + "/service-accounts/default/token"
+
+	// PROJECT_ID_PATH is the metadata location of the project id.
+	PROJECT_ID_PATH = fmt.Sprintf(METADATA_PATH_PREFIX_TMPL, LEVEL_PROJECT) + "/project-id"
 
 	// Full metadata URL for a default service account token.
 	TOKEN_URL = METADATA_URL_PREFIX + TOKEN_PATH
@@ -151,6 +156,16 @@ func ProjectGetWithDefault(name, defaultValue string) string {
 		sklog.Warningf("Unable to obtain %q from metadata server: %v", name, err)
 		return defaultValue
 	}
+}
+
+// ProjectId returns the project id as reported by the metadata
+// server, or "skolo" if no metadata server is present.
+func ProjectId() (string, error) {
+	id, err := getUrl(fmt.Sprintf(METADATA_URL_PREFIX, PROJECT_ID_PATH))
+	if err == nil && id == "" {
+		id = SKOLO_PROJECT_ID
+	}
+	return id, err
 }
 
 // MustGet is Get() that panics on error.
