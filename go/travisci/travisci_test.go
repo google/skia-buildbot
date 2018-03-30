18c79e7f45d904abc008817f61fdc929a6b84803
Ravi Mistry <rmistry@google.com>
2018-03-30 13:12:26 -0400

    Checkpoint
    Bug: skia:
    Change-Id: I2b7d3e29fefbfe874489ba22cb243468d371b076

---
+++ go/travisci/travisci_test.go
@@ -0,0 +1,61 @@
+package travisci
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	assert "github.com/stretchr/testify/require"
+	"go.skia.org/infra/go/testutils"
+)
+
+func TestGetPullRequestBuilds(t *testing.T) {
+	testutils.SmallTest(t)
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		fmt.Fprintln(w, `
+{
+  "@type": "builds",
+  "builds": [
+    {
+	  "@type": "build",
+	  "id": 358526331,
+	  "pull_request_number": 4868,
+	  "duration": 1157,
+	  "state": "failed"
+	},
+	{
+	  "@type": "build",
+	  "id": 987654321,
+	  "pull_request_number": 4868,
+	  "duration": 1542,
+	  "state": "passed"
+	},
+	{
+	  "@type": "build",
+	  "id": 123456789,
+	  "pull_request_number": 1111,
+	  "duration": 234,
+	  "state": "passed"
+	}
+  ]
+}
+`)
+	}))
+	defer ts.Close()
+
+	travisClient, err := NewTravisCI(context.Background(), "kryptonians", "krypton", "")
+	travisClient.apiURL = ts.URL
+	assert.NoError(t, err)
+	builds, err := travisClient.GetPullRequestBuilds(4868, "superman")
+	assert.NoError(t, err)
+	assert.Equal(t, 2, len(builds))
+	assert.Equal(t, 358526331, builds[0].Id)
+	assert.Equal(t, 1157, builds[0].Duration)
+	assert.Equal(t, "failed", builds[0].State)
+	assert.Equal(t, 987654321, builds[1].Id)
+	assert.Equal(t, 1542, builds[1].Duration)
+	assert.Equal(t, "passed", builds[1].State)
+}
