8e98ec597ca52af1145944e44bbfcfbc2ad66cbe
Ravi Mistry <rmistry@google.com>
2018-03-30 12:58:49 -0400

    Checkpoint
    Bug: skia:
    Change-Id: I2b7d3e29fefbfe874489ba22cb243468d371b076

--- autoroll/go/repo_manager/pre_upload_steps.go
+++ autoroll/go/repo_manager/pre_upload_steps.go
@@ -9,9 +9,11 @@ import (
 	"fmt"
 	"os"
 	"path"
+	"path/filepath"
 
 	"go.skia.org/infra/go/exec"
 	"go.skia.org/infra/go/go_install"
+	"go.skia.org/infra/go/sklog"
 )
 
 // PreUploadStep is a function to be run after the roll is performed but before
@@ -22,7 +24,8 @@ type PreUploadStep func(context.Context, string) error
 // Return the PreUploadStep with the given name.
 func GetPreUploadStep(s string) (PreUploadStep, error) {
 	rv, ok := map[string]PreUploadStep{
-		"TrainInfra": TrainInfra,
+		"TrainInfra":            TrainInfra,
+		"FlutterLicenseScripts": FlutterLicenseScripts,
 	}[s]
 	if !ok {
 		return nil, fmt.Errorf("No such pre-upload step: %s", s)
@@ -75,5 +78,67 @@ func TrainInfra(ctx context.Context, parentRepoDir string) error {
 	if _, err := exec.RunCwd(ctx, parentRepoDir, "git", "commit", "-a", "--amend", "--no-edit"); err != nil {
 		return err
 	}
+	return nil
+}
+
+// Run the flutter license scripts as described in
+// https://bugs.chromium.org/p/skia/issues/detail?id=7730#c6 and in
+// https://github.com/flutter/engine/blob/master/tools/licenses/README.md
+func FlutterLicenseScripts(ctx context.Context, parentRepoDir string) error {
+	fmt.Println("WHAT IS parentRepoDir in pre upload steps!")
+	fmt.Println(parentRepoDir)
+	binariesPath := filepath.Join(parentRepoDir, "..", "third_party", "dart", "tools", "sdks", "linux", "dart-sdk", "bin")
+
+	// Step1: Run pub get.
+	licenseToolsDir := filepath.Join(parentRepoDir, "tools", "licenses")
+	pubBinary := filepath.Join(binariesPath, "pub")
+	if _, err := exec.RunCwd(ctx, licenseToolsDir, pubBinary, "get"); err != nil {
+		return fmt.Errorf("Error when running pub get: %s", err)
+	}
+
+	// Step2: Clean out out/licenses
+	licensesOutDir := filepath.Join(parentRepoDir, "out", "licenses")
+	if err := os.RemoveAll(licensesOutDir); err != nil {
+		return fmt.Errorf("Error when cleaning out/licenses: %s", err)
+	}
+
+	// Step3: Run dart license script.
+	dartBinary := filepath.Join(binariesPath, "dart")
+	licensesGoldenDir := filepath.Join(parentRepoDir, "travis", "licenses_golden")
+	if _, err := exec.RunCwd(ctx, licenseToolsDir, dartBinary, "lib/main.dart", "--src", "../../..", "--out", licensesOutDir, "--golden", licensesGoldenDir); err != nil {
+		return fmt.Errorf("Error when running dart license script: %s", err)
+	}
+
+	licensesThirdPartyFileName := "licenses_third_party"
+	// Step4: Check to see if licenses_third_party was created in the out dir.
+	if _, err := os.Stat(filepath.Join(licensesOutDir, licensesThirdPartyFileName)); err == nil {
+		// Step5: Copy from out dir to goldens dir.
+		if _, err := exec.RunCwd(ctx, licenseToolsDir, "cp", filepath.Join(licensesOutDir, licensesThirdPartyFileName), filepath.Join(licensesGoldenDir, licensesThirdPartyFileName)); err != nil {
+			return fmt.Errorf("Error when copying licenses_third_party from out to golden dir: %s", err)
+		}
+		// Step6: Capture diff of licenses_golden/licenses_third_party.
+		licensesDiffOutput, err := exec.RunCwd(ctx, licenseToolsDir, "git", "diff", filepath.Join(licensesGoldenDir, licensesThirdPartyFileName))
+		if err != nil {
+			return fmt.Errorf("Error when seeing diff of golden licenses_third_party: %s", err)
+		}
+		sklog.Infof("The licenses diff output is:\n%s", licensesDiffOutput)
+		// TODO(rmistry): Run Step7 after detecting unexpected changes in licensesDiffOutput.
+		//// Step7: Update sky_engine/LICENSE
+		//if _, err := exec.RunCwd(ctx, licenseToolsDir, dartBinary, "lib/main.dart", "--release", "--src", "../../..", "--out", licensesOutDir, ">", "../../sky/packages/sky_engine/LICENSE"); err != nil {
+		//	return fmt.Errorf("Error when running dart license script: %s", err)
+		//}
+
+	}
+
+	// Step8: Revert any change to pubspec.lock. This should be a temporary step.
+	if _, err := exec.RunCwd(ctx, licenseToolsDir, "git", "checkout", "--", "pubspec.lock"); err != nil {
+		return err
+	}
+
+	// Step8: Commit all changed files.
+	if _, err := exec.RunCwd(ctx, parentRepoDir, "git", "commit", "-a", "--amend", "--no-edit"); err != nil {
+		return err
+	}
+
 	return nil
 }
