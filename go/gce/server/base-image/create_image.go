acc950621d071649974c5c0f18724dbde9cff580
Joe Gregorio <joe@bitworking.org>
2018-04-02 10:04:41 -0400

    [snapshot] Move to using a managed base image.
    Remove all unattended upgrades from the startup script since the image
    does that already.
    
    Bug: skia:
    Change-Id: I2419ebc6454f45087e4270324332bb9b13cf4aca

--- go/gce/server/base-image/create_image.go
+++ go/gce/server/base-image/create_image.go
@@ -33,7 +33,7 @@ func BaseConfig() *gce.Instance {
 	vm := &gce.Instance{
 		BootDisk: &gce.Disk{
 			Name:        INSTANCE_NAME,
-			SourceImage: "projects/debian-cloud/global/images/debian-9-stretch-v20170829",
+			SourceImage: "projects/eip-images/global/images/debian-9-drawfork-v20180327",
 			Type:        gce.DISK_TYPE_PERSISTENT_STANDARD,
 		},
 		MachineType: gce.MACHINE_TYPE_STANDARD_4,
