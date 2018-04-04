5fefe0aab564c7978f6731a26b5e796fbf0f856a
Stephan Altmueller <stephana@google.com>
2018-04-04 13:09:32 -0400

    [gold] Add support of different instances to tracing
    Bug: skia:
    Change-Id: Ifeb058ac31dcc8c74c55de16aaf3c34c8a20f9a9

--- go/sktrace/sktrace.go
+++ go/sktrace/sktrace.go
@@ -10,30 +10,38 @@ import (
 	"golang.org/x/oauth2"
 	"google.golang.org/api/option"
 
-	"go.skia.org/infra/go/common"
 	"go.skia.org/infra/go/sklog"
 )
 
 // TODO(stephana): Re-add Jaeger as a local option when the Go package is stable
 // again.
 
-// Init initializes traceing support.
-func Init(serviceName string, tokenSrc oauth2.TokenSource) error {
+// TraceClient wraps the information relevant for tracing of the current host.
+// It currently assumes that the node runs one application. This might change in
+// the future as the trace api (see https://opencensus.io) evolves.
+type TraceClient struct {
+	serviceName string
+}
+
+// NewTraceClient returns a new trace client instance.
+func NewTraceClient(projectID, serviceName string, tokenSrc oauth2.TokenSource) (*TraceClient, error) {
 	var exporter trace.Exporter
 	var err error
 
 	sdOptions := stackdriver.Options{
-		ProjectID:     common.PROJECT_ID,
+		ProjectID:     projectID,
 		ClientOptions: []option.ClientOption{option.WithTokenSource(tokenSrc)},
 	}
 	if exporter, err = stackdriver.NewExporter(sdOptions); err != nil {
-		return sklog.FmtErrorf("Error creating stackdriver exporter: %s", err)
+		return nil, sklog.FmtErrorf("Error creating stackdriver exporter: %s", err)
 	}
 	sklog.Infof("StackDriver trace exporter created.")
 
 	trace.RegisterExporter(exporter)
 	trace.ApplyConfig(trace.Config{DefaultSampler: trace.AlwaysSample()})
-	return nil
+	return &TraceClient{
+		serviceName: serviceName,
+	}, nil
 }
 
 // Trace wraps a given pattern and handler function and sets up tracing for it.
@@ -44,10 +52,12 @@ func Init(serviceName string, tokenSrc oauth2.TokenSource) error {
 // Inside the 'handler' function they can retrieve the contex via r.Context()
 // where 'r' is the http.Request object and pass it around for more detailed
 // tracing.
-func Trace(pattern string, handler func(http.ResponseWriter, *http.Request)) (string, func(http.ResponseWriter, *http.Request)) {
+func (t *TraceClient) Trace(pattern string, handler func(http.ResponseWriter, *http.Request)) (string, func(http.ResponseWriter, *http.Request)) {
+	spanURL := t.serviceName + pattern
 	return pattern, func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
-		ctx, span := trace.StartSpan(ctx, pattern)
+		ctx, span := trace.StartSpan(ctx, spanURL)
+		span.AddAttributes(trace.StringAttribute("service", t.serviceName))
 		defer span.End()
 
 		// If the context was created in the Context() call we need to make sure
