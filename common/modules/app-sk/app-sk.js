7a7584a0cdcc25f662cad0cc149213d30b7a92a3
Kevin Lubick <kjlubick@google.com>
2018-04-06 14:05:10 -0400

    [ap] Add basic app-sk
    This doesn't do much fancy at the moment, other than provide some
    stock CSS.
    
    Future work:
     - port version-sk and stick in in the side bar.
     - deduplicate login-sk CSS rules (and maybe rethink how we wnat to do CSS variables)
    
    
    Bug: skia:
    Change-Id: I08ddcd6cbf630fe60d395063591bbff0020fd23e

---
+++ common/modules/app-sk/app-sk.js
@@ -0,0 +1,17 @@
+/**
+ * @module common/app-sk
+ * @description TODO(kjlubick)
+ *
+ * <p>
+ * The <login-sk> custom element. Uses the Login promise to display the
+ * current login status and provides login/logout links. Reports errors via
+ * {@linkcode module:common/errorMessage}.
+ * </p>
+ */
+
+
+window.customElements.define('app-sk', class extends HTMLElement {
+  connectedCallback() {
+    // TODO
+  }
+});
