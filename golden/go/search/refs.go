48ef526510a1715358070b8146cca014108c8583
Stephan Altmueller <stephana@google.com>
2018-04-09 12:22:44 -0400

    wip: rhs query
    Bug: skia:
    Change-Id: I329e36403102c3090cdcc7b1287094c78679673f

--- golden/go/search/refs.go
+++ golden/go/search/refs.go
@@ -1,8 +1,10 @@
 package search
 
 import (
+	"context"
 	"math"
 
+	"go.opencensus.io/trace"
 	"go.skia.org/infra/go/paramtools"
 	"go.skia.org/infra/go/sklog"
 	"go.skia.org/infra/go/util"
@@ -48,15 +50,18 @@ func NewRefDiffer(exp ExpSlice, diffStore diff.DiffStore, idx *indexer.SearchInd
 // metric. 'match' is the list of parameters that need to match between
 // the digests that are compared, i.e. this allows to restrict comparison
 // of gamma correct images to other digests that are also gamma correct.
-func (r *RefDiffer) GetRefDiffs(metric string, match []string, test, digest string, params paramtools.ParamSet, includeIgnores bool) (string, map[string]*SRDiffDigest) {
+func (r *RefDiffer) GetRefDiffs(ctx context.Context, metric string, match []string, test, digest string, params paramtools.ParamSet, rhsQuery paramtools.ParamSet, includeIgnores bool) (string, map[string]*SRDiffDigest) {
+	ctx, span := trace.StartSpan(ctx, "refdiffer/GetRefDiffs")
+	defer span.End()
+
 	unavailableDigests := r.diffStore.UnavailableDigests()
 	if _, ok := unavailableDigests[digest]; ok {
 		return "", nil
 	}
 
 	paramsByDigest := r.idx.GetParamsetSummaryByTest(false)[test]
-	posDigests := r.getDigestsWithLabel(test, match, params, paramsByDigest, unavailableDigests, types.POSITIVE)
-	negDigests := r.getDigestsWithLabel(test, match, params, paramsByDigest, unavailableDigests, types.NEGATIVE)
+	posDigests := r.getDigestsWithLabel(test, match, params, paramsByDigest, unavailableDigests, rhsQuery, types.POSITIVE)
+	negDigests := r.getDigestsWithLabel(test, match, params, paramsByDigest, unavailableDigests, rhsQuery, types.NEGATIVE)
 
 	ret := make(map[string]*SRDiffDigest, 3)
 	ret[REF_CLOSEST_POSTIVE] = r.getClosestDiff(metric, digest, posDigests)
@@ -89,10 +94,17 @@ func (r *RefDiffer) GetRefDiffs(metric string, match []string, test, digest stri
 // getDigestsWithLabel return all digests within the given test that
 // have the given label assigned to them and where the parameters
 // listed in 'match' match.
-func (r *RefDiffer) getDigestsWithLabel(test string, match []string, params paramtools.ParamSet, paramsByDigest map[string]paramtools.ParamSet, unavailable map[string]*diff.DigestFailure, targetLabel types.Label) []string {
+func (r *RefDiffer) getDigestsWithLabel(test string, match []string, params paramtools.ParamSet, paramsByDigest map[string]paramtools.ParamSet, unavailable map[string]*diff.DigestFailure, rhsQuery paramtools.ParamSet, targetLabel types.Label) []string {
 	ret := []string{}
 	for d, digestParams := range paramsByDigest {
-		if _, ok := unavailable[d]; !ok && (r.exp.Classification(test, d) == targetLabel) && paramSetsMatch(match, params, digestParams) {
+		// Accept all digests that are: available, in the set of allowed digests
+		//                              match the target label and where the required
+		//                              parameter fields match.
+		_, ok := unavailable[d]
+		if !ok &&
+			((rhsQuery == nil) || rhsQuery.Matches(digestParams)) &&
+			(r.exp.Classification(test, d) == targetLabel) &&
+			paramSetsMatch(match, params, digestParams) {
 			ret = append(ret, d)
 		}
 	}
