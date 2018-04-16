ede9dbbcfd2951a63cde702487272b9238444809
Joe Gregorio <joe@bitworking.org>
2018-04-16 14:32:39 -0400

    Add in pub/sub capabilities.
    Bug: skia:
    Change-Id: I5cf392628dd2f1c8dfe7671097f048b82284f104

--- prom/go/iap_proxy/main.go
+++ prom/go/iap_proxy/main.go
@@ -12,15 +12,17 @@ import (
 	"path/filepath"
 
 	"go.skia.org/infra/go/common"
-	"go.skia.org/infra/go/login"
+	"go.skia.org/infra/go/iap"
 	"go.skia.org/infra/go/sklog"
 )
 
 var (
-	local      = flag.Bool("local", false, "Running locally if true. As opposed to in production.")
-	port       = flag.String("port", ":8000", "HTTP service address (e.g., ':8000')")
-	promPort   = flag.String("prom_port", ":10110", "Metrics service address (e.g., ':10110')")
-	targetPort = flag.String("target_port", ":10116", "The port we are proxying to.")
+	local            = flag.Bool("local", false, "Running locally if true. As opposed to in production.")
+	port             = flag.String("port", ":8000", "HTTP service address (e.g., ':8000')")
+	promPort         = flag.String("prom_port", ":10110", "Metrics service address (e.g., ':10110')")
+	targetPort       = flag.String("target_port", ":10116", "The port we are proxying to.")
+	projectNumber    = flag.String("project_number", "", "The number id of the GCE project.")
+	backendServiceId = flag.String("backend_service_id", "", "The id of the GCE backend service.")
 )
 
 type Proxy struct {
@@ -34,14 +36,7 @@ func NewProxy(target *url.URL) *Proxy {
 }
 
 func (p Proxy) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	// Check if it's a GET request from the whitelisted IPs for "/", and if so
-	// then return a 200 with no body.
-	// From 130.211.0.0/22 and 35.191.0.0/16
-	sklog.Infof("Requesting: %s", r.RequestURI)
-	if login.LoggedInAs(r) == "" {
-		http.Redirect(w, r, login.LoginURL(w, r), http.StatusSeeOther)
-		return
-	}
+	sklog.Infof("Requesting: %s As: %s", r.RequestURI, r.Header.Get("x-user-email"))
 	p.reverseProxy.ServeHTTP(w, r)
 }
 
@@ -50,16 +45,20 @@ func main() {
 	common.InitWithMust(
 		filepath.Base(os.Args[0]),
 		common.PrometheusOpt(promPort),
-		common.CloudLoggingOpt(),
 	)
-	login.SimpleInitMust(*port, *local)
-
 	target, err := url.Parse(fmt.Sprintf("http://localhost%s", *targetPort))
 	if err != nil {
 		sklog.Fatalf("Unable to parse target URL %q: %s", targetPort, err)
 	}
 
-	http.Handle("/", NewProxy(target))
-	http.HandleFunc("/oauth2callback/", login.OAuth2CallbackHandler)
+	var h http.Handler = NewProxy(target)
+	if !*local {
+		var err error
+		h, err = iap.New([]string{}, *projectNumber, *backendServiceId, h)
+		if err != nil {
+			sklog.Fatalf("Failed to init iap: %s", err)
+		}
+	}
+	http.Handle("/", h)
 	sklog.Fatal(http.ListenAndServe(*port, nil))
 }
