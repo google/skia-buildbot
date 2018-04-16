a5a485850ec20e18415ece38f5fdc53724d3c1e0
Joe Gregorio <joe@bitworking.org>
2018-04-16 12:50:38 -0400

    Add in pub/sub capabilities.
    Bug: skia:
    Change-Id: I5cf392628dd2f1c8dfe7671097f048b82284f104

---
+++ go/iap/iap.go
@@ -0,0 +1,184 @@
+package iap
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net"
+	"net/http"
+	"sync"
+
+	jwt "github.com/dgrijalva/jwt-go"
+	"go.skia.org/infra/go/httputils"
+	"go.skia.org/infra/go/util"
+)
+
+type IdentityState int
+
+var (
+	errInvalid  = errors.New("Invalid User")
+	errNotFound = errors.New("Not Found")
+)
+
+const (
+	INVALID = "~~invalid~~"
+
+	IAP_PUBLIC_KEY_URL = "https://www.gstatic.com/iap/verify/public_key"
+)
+
+type iapHandler struct {
+	jwtToEmail          map[string]string
+	healthCheckIPRanges []*net.IPNet
+	aud                 string
+	keys                map[string]string
+	client              *http.Client
+	handler             http.Handler
+	mutex               sync.Mutex
+}
+
+// TODO All this code and we don't even support setting a list of users or domains to restrict the access to.
+
+func New(whitelist []string, projectNumber, backendServiceId string, h http.Handler) (http.Handler, error) {
+	// The GCP HTTPS Load Balancer, as configured by GKE is as dump as a stump
+	// and requires that the path '/' always return 200 for healthcheck requests,
+	// so we have to look for requests coming from the range of IP addresses
+	// reserved for the health checker (which is just copied manually from
+	// documentation) and respond differently to those requests.
+	_, healthCheck1, err := net.ParseCIDR("130.211.0.0/22")
+	if err != nil {
+		return nil, err
+	}
+	_, healthCheck2, err := net.ParseCIDR("35.191.0.0/16")
+	if err != nil {
+		return nil, err
+	}
+	return &iapHandler{
+		jwtToEmail:          map[string]string{},
+		healthCheckIPRanges: []*net.IPNet{healthCheck1, healthCheck2},
+		aud:                 fmt.Sprintf("/projects/%s/global/backendServices/%s", projectNumber, backendServiceId),
+		keys:                map[string]string{},
+		client:              httputils.NewTimeoutClient(),
+		handler:             h,
+	}, nil
+}
+
+func (i *iapHandler) IPinRange(remoteAddr string) error {
+	ip, _, err := net.SplitHostPort(remoteAddr)
+	if err != nil {
+		return fmt.Errorf("Invalid IP:port format for %q: %s", remoteAddr, err)
+	}
+
+	userIP := net.ParseIP(ip)
+	if userIP == nil {
+		return fmt.Errorf("Invalid IP format for %q: %s", remoteAddr, err)
+	}
+	for _, ipRange := range i.healthCheckIPRanges {
+		if ipRange.Contains(userIP) {
+			return nil
+		}
+	}
+	return fmt.Errorf("IP not in acceptable range")
+}
+
+func (i *iapHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	// Only this func is allowed to set x-user-email.
+	r.Header.Set("x-user-email", "")
+	jwtAssertion := r.Header.Get("x-goog-iap-jwt-assertion")
+	if r.URL.Path == "/" && jwtAssertion == "" {
+		if err := i.IPinRange(r.RemoteAddr); err != nil {
+			httputils.ReportError(w, r, fmt.Errorf("Healthcheck failed: %s", err), "Failed health check.")
+			return
+		} else {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+	}
+	if jwtAssertion == "" {
+		httputils.ReportError(w, r, fmt.Errorf("Missing jwt assertion for non-'/' path."), "Identity Aware Proxy has been disabled, all access is denied.")
+		return
+	}
+	email, err := i.getEmail(jwtAssertion)
+	if err == errInvalid {
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
+		return
+	} else if err == errNotFound {
+		// Validate jwtAsssertion
+		parser := &jwt.Parser{
+			ValidMethods: []string{"ES256"},
+		}
+		token, err := parser.Parse(jwtAssertion, func(token *jwt.Token) (interface{}, error) {
+			mapClaims, ok := token.Claims.(jwt.MapClaims)
+			if !ok {
+				return nil, fmt.Errorf("Doesn't contain MapClaims.")
+			}
+			if mapClaims.Valid() != nil {
+				return nil, fmt.Errorf("Invalid claims.")
+			}
+			if mapClaims["exp"].(string) == "" || mapClaims["iat"].(string) == "" {
+				return nil, fmt.Errorf("Missing exp or iat claim.")
+			}
+			if !mapClaims.VerifyAudience(i.aud, true) {
+				return nil, fmt.Errorf("Wrong audience.")
+			}
+			if !mapClaims.VerifyIssuer("https://cloud.google.com/iap", true) {
+				return nil, fmt.Errorf("Wrong issuer.")
+			}
+			if mapClaims["alg"].(string) != "ES256" {
+				return nil, fmt.Errorf("Wrong alg.")
+			}
+			return i.findKey(mapClaims["kid"].(string))
+		})
+		if err != nil {
+			httputils.ReportError(w, r, fmt.Errorf("Failed to validate JWT: %s", err), "Failed to validate JWT.")
+			return
+		}
+		email = token.Claims.(jwt.MapClaims)["email"].(string)
+		i.setEmail(jwtAssertion, email)
+	}
+	// TODO Maybe set this in the request Context?
+	r.Header.Set("x-user-email", email)
+	i.handler.ServeHTTP(w, r)
+}
+
+func (i *iapHandler) setEmail(jwtAssertion, email string) {
+	i.mutex.Lock()
+	defer i.mutex.Unlock()
+	i.jwtToEmail[jwtAssertion] = email
+}
+
+func (i *iapHandler) getEmail(jwtAssertion string) (string, error) {
+	i.mutex.Lock()
+	defer i.mutex.Unlock()
+	email, ok := i.jwtToEmail[jwtAssertion]
+	if ok && email != INVALID {
+		return email, nil
+	}
+	if !ok {
+		return "", errNotFound
+	} else {
+		return "", errInvalid
+	}
+}
+
+func (i *iapHandler) findKey(kid string) (string, error) {
+	i.mutex.Lock()
+	defer i.mutex.Unlock()
+	key, ok := i.keys[kid]
+	if ok {
+		return key, nil
+	}
+	resp, err := i.client.Get(IAP_PUBLIC_KEY_URL)
+	if err != nil || resp.StatusCode != 200 {
+		return "", fmt.Errorf("Could not retrieve iap public_key")
+	}
+	defer util.Close(resp.Body)
+	keys := map[string]string{}
+	if err := json.NewDecoder(resp.Body).Decode(&keys); err != nil {
+		return "", fmt.Errorf("Could not decode keys: %s", err)
+	}
+	i.keys = keys
+	if key, ok = i.keys[kid]; ok {
+		return key, nil
+	}
+	return "", fmt.Errorf("No key with id %q found.", kid)
+}
---
+++ go/iap/iap_test.go
@@ -0,0 +1,134 @@
+package iap
+
+import (
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"go.skia.org/infra/go/mockhttpclient"
+)
+
+func TestBasics(t *testing.T) {
+	h, err := New([]string{}, "145247227042", "k8s-be-32071--e03569f20c11b77d", nil)
+	assert.NoError(t, err)
+	ih := h.(*iapHandler)
+	assert.Equal(t, "/projects/145247227042/global/backendServices/k8s-be-32071--e03569f20c11b77d", ih.aud)
+}
+
+func TestIPinRange(t *testing.T) {
+	h, err := New([]string{}, "145247227042", "k8s-be-32071--e03569f20c11b77d", nil)
+	assert.NoError(t, err)
+	ih := h.(*iapHandler)
+	assert.NoError(t, ih.IPinRange("130.211.1.1:80"))
+	assert.NoError(t, ih.IPinRange("35.191.1.1:8080"))
+	assert.Error(t, ih.IPinRange("130.211.1.1"))
+	assert.Error(t, ih.IPinRange("10.1.1.1:80"))
+}
+
+func TestFindKey(t *testing.T) {
+	h, err := New([]string{}, "145247227042", "k8s-be-32071--e03569f20c11b77d", nil)
+	assert.NoError(t, err)
+	ih := h.(*iapHandler)
+
+	m := mockhttpclient.NewURLMock()
+	m.MockOnce(IAP_PUBLIC_KEY_URL, mockhttpclient.MockGetDialogue([]byte(`{"foo": "bar"}`)))
+	m.MockOnce(IAP_PUBLIC_KEY_URL, mockhttpclient.MockGetDialogue([]byte(`{"foo": "bar"}`)))
+	m.MockOnce(IAP_PUBLIC_KEY_URL, mockhttpclient.MockGetDialogue([]byte(`{"foo": "bar", "baz": "quxx"}`)))
+	ih.client = m.Client()
+
+	// Make request on empty keys and force an http request.
+	key, err := ih.findKey("foo")
+	assert.NoError(t, err)
+	assert.Equal(t, "bar", key)
+	assert.Len(t, ih.keys, 1)
+
+	// Make request on key that is in cache, so http request.
+	key, err = ih.findKey("foo")
+	assert.NoError(t, err)
+	assert.Equal(t, "bar", key)
+	assert.Len(t, ih.keys, 1)
+
+	// Make request for missing key and force an http request, and the key isn't there.
+	_, err = ih.findKey("baz")
+	assert.Error(t, err)
+	assert.Len(t, ih.keys, 1)
+
+	// Make request for missing key and force an http request, and the key is now present.
+	key, err = ih.findKey("baz")
+	assert.NoError(t, err)
+	assert.Equal(t, "quxx", key)
+	assert.Len(t, ih.keys, 2)
+}
+
+func TestEmail(t *testing.T) {
+	h, err := New([]string{}, "145247227042", "k8s-be-32071--e03569f20c11b77d", nil)
+	assert.NoError(t, err)
+	ih := h.(*iapHandler)
+
+	email, err := ih.getEmail("a.b.c")
+	assert.Equal(t, errNotFound, err)
+
+	ih.setEmail("a.b.c", "fred@example.org")
+
+	email, err = ih.getEmail("a.b.c")
+	assert.NoError(t, err)
+	assert.Equal(t, "fred@example.org", email)
+
+	ih.setEmail("x.y.z", INVALID)
+	email, err = ih.getEmail("x.y.z")
+	assert.Equal(t, errInvalid, err)
+}
+
+func TestServe(t *testing.T) {
+	h, err := New([]string{}, "145247227042", "k8s-be-32071--e03569f20c11b77d", nil)
+	assert.NoError(t, err)
+	ih := h.(*iapHandler)
+
+	ih.setEmail("a.b.c", "fred@example.org")
+
+	// Healthcheck from wrong address.
+	r := httptest.NewRequest("GET", "http://example.com/", nil)
+	r.RemoteAddr = "10.0.0.1:8080"
+	w := httptest.NewRecorder()
+	ih.ServeHTTP(w, r)
+	resp := w.Result()
+	assert.Equal(t, 500, resp.StatusCode)
+
+	// Good healtcheck.
+	r.RemoteAddr = "130.211.1.1:8080"
+	w = httptest.NewRecorder()
+	ih.ServeHTTP(w, r)
+	resp = w.Result()
+	assert.Equal(t, 200, resp.StatusCode)
+
+	// Add a user that has already been validated.
+	ih.handler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "<html><body>Hello World!</body></html>")
+	})
+	ih.setEmail("a.b.c", "fred@example.com")
+	r = httptest.NewRequest("GET", "http://example.com/foo", nil)
+	r.Header.Set("x-goog-iap-jwt-assertion", "a.b.c")
+	w = httptest.NewRecorder()
+	ih.ServeHTTP(w, r)
+	resp = w.Result()
+	body, _ := ioutil.ReadAll(resp.Body)
+	assert.Equal(t, "<html><body>Hello World!</body></html>", string(body))
+	assert.Equal(t, 200, resp.StatusCode)
+	assert.Equal(t, "fred@example.com", r.Header.Get("x-user-email"))
+
+	// Add a user that has already been found to be invalid.
+	ih.handler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		io.WriteString(w, "<html><body>Hello World!</body></html>")
+	})
+	ih.setEmail("x.y.z", INVALID)
+	r = httptest.NewRequest("GET", "http://example.com/foo", nil)
+	r.Header.Set("x-goog-iap-jwt-assertion", "x.y.z")
+	w = httptest.NewRecorder()
+	ih.ServeHTTP(w, r)
+	resp = w.Result()
+	assert.Equal(t, 401, resp.StatusCode)
+	assert.Equal(t, "", r.Header.Get("x-user-email"))
+}
