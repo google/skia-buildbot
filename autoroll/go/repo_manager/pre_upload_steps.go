d08d4c7c2bc3d96b79a0f461746dfba9cac9ecc4
Ravi Mistry <rmistry@google.com>
2018-04-17 09:58:05 -0400

    Add new FlutterLicenseScripts pre-upload step
    Bug: skia:7730
    Change-Id: I2868a32354435373106a69c4e8d4474c892ad6b5

--- autoroll/go/repo_manager/pre_upload_steps.go
+++ autoroll/go/repo_manager/pre_upload_steps.go
@@ -9,9 +9,12 @@ import (
 	"fmt"
 	"os"
 	"path"
+	"path/filepath"
 
 	"go.skia.org/infra/go/exec"
+	"go.skia.org/infra/go/git"
 	"go.skia.org/infra/go/go_install"
+	"go.skia.org/infra/go/sklog"
 )
 
 // PreUploadStep is a function to be run after the roll is performed but before
@@ -22,7 +25,8 @@ type PreUploadStep func(context.Context, string) error
 // Return the PreUploadStep with the given name.
 func GetPreUploadStep(s string) (PreUploadStep, error) {
 	rv, ok := map[string]PreUploadStep{
-		"TrainInfra": TrainInfra,
+		"TrainInfra":            TrainInfra,
+		"FlutterLicenseScripts": FlutterLicenseScripts,
 	}[s]
 	if !ok {
 		return nil, fmt.Errorf("No such pre-upload step: %s", s)
@@ -75,5 +79,86 @@ func TrainInfra(ctx context.Context, parentRepoDir string) error {
 	if _, err := exec.RunCwd(ctx, parentRepoDir, "git", "commit", "-a", "--amend", "--no-edit"); err != nil {
 		return err
 	}
+	return nil
+}
+
+// Run the flutter license scripts as described in
+// https://bugs.chromium.org/p/skia/issues/detail?id=7730#c6 and in
+// https://github.com/flutter/engine/blob/master/tools/licenses/README.md
+func FlutterLicenseScripts(ctx context.Context, parentRepoDir string) error {
+	sklog.Info("Running flutter license scripts.")
+	binariesPath := filepath.Join(parentRepoDir, "..", "third_party", "dart", "tools", "sdks", "linux", "dart-sdk", "bin")
+
+	// Step1: Run pub get.
+	licenseToolsDir := filepath.Join(parentRepoDir, "tools", "licenses")
+	pubBinary := filepath.Join(binariesPath, "pub")
+	sklog.Info("Running put get.")
+	if _, err := exec.RunCwd(ctx, licenseToolsDir, pubBinary, "get"); err != nil {
+		return fmt.Errorf("Error when running pub get: %s", err)
+	}
+
+	// Step2: Clean out out/licenses. Things fail without this step.
+	licensesOutDir := filepath.Join(parentRepoDir, "..", "out", "licenses")
+	if err := os.RemoveAll(licensesOutDir); err != nil {
+		return fmt.Errorf("Error when cleaning out/licenses: %s", err)
+	}
+
+	// Step3: Run dart license script to create new licenses.
+	dartBinary := filepath.Join(binariesPath, "dart")
+	licensesGoldenDir := filepath.Join(parentRepoDir, "travis", "licenses_golden")
+	licenseCmd := []string{dartBinary, "lib/main.dart", "--src", "../../..", "--out", licensesOutDir, "--golden", licensesGoldenDir}
+	sklog.Infof("Running %s", licenseCmd)
+	if _, err := exec.RunCwd(ctx, licenseToolsDir, licenseCmd...); err != nil {
+		return fmt.Errorf("Error when running dart license script: %s", err)
+	}
+
+	licensesThirdPartyFileName := "licenses_third_party"
+	// Step4: Check to see if licenses_third_party was created in the out dir.
+	//        It will be created if the third_party hash changes.
+	if _, err := os.Stat(filepath.Join(licensesOutDir, licensesThirdPartyFileName)); err == nil {
+		sklog.Infof("Found %s", licensesThirdPartyFileName)
+
+		// Step5: Copy from out dir to goldens dir. This is required for updating
+		//        the release file in sky_engine/LICENSE.
+		if _, err := exec.RunCwd(ctx, licenseToolsDir, "cp", filepath.Join(licensesOutDir, licensesThirdPartyFileName), filepath.Join(licensesGoldenDir, licensesThirdPartyFileName)); err != nil {
+			return fmt.Errorf("Error when copying licenses_third_party from out to golden dir: %s", err)
+		}
+		// Step6: Capture diff of licenses_golden/licenses_third_party.
+		licensesDiffOutput, err := git.GitDir(licenseToolsDir).Git(ctx, "diff", "--no-ext-diff", filepath.Join(licensesGoldenDir, licensesThirdPartyFileName))
+		if err != nil {
+			return fmt.Errorf("Error when seeing diff of golden licenses_third_party: %s", err)
+		}
+		sklog.Infof("The licenses diff output is:\n%s", licensesDiffOutput)
+
+		// Step7: Update sky_engine/LICENSE. It should always be run
+		// according to https://github.com/flutter/engine/pull/4959#issuecomment-380222322
+		updateLicenseCmd := []string{dartBinary, "lib/main.dart", "--release", "--src", "../../..", "--out", licensesOutDir}
+		sklog.Infof("Running %s", updateLicenseCmd)
+		releasesLicensePath := filepath.Join(parentRepoDir, "sky", "packages", "sky_engine", "LICENSE")
+		outFile, err := os.Create(releasesLicensePath)
+		if err != nil {
+			return fmt.Errorf("Could not open %s: %s", releasesLicensePath, err)
+		}
+		if err := exec.Run(ctx, &exec.Command{
+			Dir:    licenseToolsDir,
+			Name:   dartBinary,
+			Args:   []string{"lib/main.dart", "--release", "--src", "../../..", "--out", licensesOutDir},
+			Stdout: outFile,
+		}); err != nil {
+			return fmt.Errorf("Error when running dart license script: %s", err)
+		}
+	}
+
+	// Step8: Revert any change to pubspec.lock. This should be a temporary step
+	// as described in https://bugs.chromium.org/p/skia/issues/detail?id=7730#c9
+	if _, err := git.GitDir(licenseToolsDir).Git(ctx, "checkout", "--", "pubspec.lock"); err != nil {
+		return err
+	}
+
+	// Step9: Commit all changed files.
+	if _, err := git.GitDir(parentRepoDir).Git(ctx, "commit", "-a", "--amend", "--no-edit"); err != nil {
+		return err
+	}
+
 	return nil
 }
