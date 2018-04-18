1f47b6f1bc4ca82625dce397efe2b8b5508dda28
Ravi Mistry <rmistry@google.com>
2018-04-18 11:01:02 +0000

    Revert "[Android compile bot] Revert building external/skia"
    This reverts commit 9c9e06ddf4edd46bf1b4d7f229a62ff8a83e94af.
    
    Reason for revert: Land this to see how fast things are before https://skia-review.googlesource.com/c/skia/+/121865 lands.
    
    Original change's description:
    > [Android compile bot] Revert building external/skia
    > 
    > Bug: skia:7815
    > Change-Id: I9a0665c1357a3d934c0fae90afff43964670e8b0
    > Reviewed-on: https://skia-review.googlesource.com/121782
    > Commit-Queue: Ravi Mistry <rmistry@google.com>
    > Commit-Queue: Eric Boren <borenet@google.com>
    > Auto-Submit: Ravi Mistry <rmistry@google.com>
    > Reviewed-by: Eric Boren <borenet@google.com>
    
    TBR=borenet@google.com,rmistry@google.com
    
    Change-Id: I5f7ea61a9ab809f96c101951a4354e90219c9afd
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: skia:7815

--- android_compile/compile.sh
+++ android_compile/compile.sh
@@ -37,6 +37,6 @@ mmma_cmd="mmma -j32 frameworks/base/core/jni"
 log_step "Running $mmma_cmd"
 eval $mmma_cmd
 
-# mmm_skia_cmd="mmm -j32 external/skia"
-# log_step "Running $mmm_skia_cmd"
-# eval $mmm_skia_cmd
+mmm_skia_cmd="mmm -j32 external/skia"
+log_step "Running $mmm_skia_cmd"
+eval $mmm_skia_cmd
