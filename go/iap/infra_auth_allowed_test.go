003d4dac4416a56410dda5ff9711f06cceb1ebec
Joe Gregorio <joe@bitworking.org>
2018-04-19 11:54:20 -0400

    [iap] Add chrome infra auth support.
    Bug: skia:
    Change-Id: I248ce5e8d64a9041159b85739f0d191d5da96c09
    Reviewed-on: https://skia-review.googlesource.com/122380
    Commit-Queue: Joe Gregorio <jcgregorio@google.com>
    Reviewed-by: Ben Wagner <benjaminwagner@google.com>

---
+++ go/iap/infra_auth_allowed_test.go
@@ -0,0 +1,53 @@
+package iap
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"go.skia.org/infra/go/mockhttpclient"
+	"go.skia.org/infra/go/testutils"
+)
+
+func TestInfraConvert(t *testing.T) {
+	testutils.SmallTest(t)
+	infra := []string{
+		"user:*@google.com",
+		"user:test@example.com",
+		"anonymous:anonymous",
+		"bot:foo",
+		"service:bar",
+		"user:",
+		"user:last@example.org",
+	}
+
+	expected := []string{
+		"google.com",
+		"test@example.com",
+		"last@example.org",
+	}
+
+	assert.Equal(t, expected, infraAuthToAllowFromList(infra))
+	assert.Equal(t, []string{}, infraAuthToAllowFromList([]string{}))
+}
+
+const JSON = `{
+  "group": {
+    "members": [
+      "user:test@example.org",
+      "user:*@chromium.org"
+    ]
+  }
+}`
+
+func TestWithClientMock(t *testing.T) {
+	testutils.SmallTest(t)
+	m := mockhttpclient.NewURLMock()
+	m.Mock(fmt.Sprintf(GROUP_URL_TEMPLATE, "test"), mockhttpclient.MockGetDialogue([]byte(JSON)))
+	i, err := NewAllowedFromChromeInfraAuth(m.Client(), "test")
+	assert.NoError(t, err)
+	assert.True(t, i.Member("foo@chromium.org"))
+	assert.True(t, i.Member("test@example.org"))
+	assert.False(t, i.Member("example.org"))
+	assert.False(t, i.Member("bar@example.org"))
+}
