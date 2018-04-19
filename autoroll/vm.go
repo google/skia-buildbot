6be7c75f5103461da3b7d07c840f56560fda1a6f
Ravi Mistry <rmistry@google.com>
2018-04-19 13:58:11 -0400

    Release files for Skia->Flutter roller
    Bug: skia:7730
    Change-Id: I7d432fb788499dd7e0af44c4a1942965f74cc8f4

--- autoroll/vm.go
+++ autoroll/vm.go
@@ -124,6 +124,16 @@ func PDFium() *gce.Instance {
 	return vm
 }
 
+func Flutter() *gce.Instance {
+	vm := AutoRollBase("skia-flutter-autoroll", "" /* Use ephemeral IP */)
+	vm.Contacts = []string{
+		"rmistry@google.com",
+		"brianosman@google.com",
+	}
+	vm.ServiceAccount = "skia-flutter-autoroll@skia-buildbots.google.com.iam.gserviceaccount.com"
+	return vm
+}
+
 func Fuchsia() *gce.Instance {
 	vm := AutoRollBase("fuchsia-autoroll", "" /* Use ephemeral IP */)
 	vm.Contacts = []string{
@@ -235,6 +245,7 @@ func main() {
 		"catapult":              Catapult(),
 		"chromite-chromium":     Chromite_Chromium(),
 		"depot-tools-chromium":  DepotTools_Chromium(),
+		"skia-flutter":          Flutter(),
 		"fuchsia":               Fuchsia(),
 		"fuchsia-sdk-chromium":  FuchsiaSDK_Chromium(),
 		"google3":               Google3(),
