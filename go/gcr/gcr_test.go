03101352d71736a6fdfd6345782b95695043e88a
Joe Gregorio <joe@bitworking.org>
2018-04-23 18:28:24 -0400

    [gcr] New module for interacting the Google Cloud Registry.
    Bug: skia:
    Change-Id: I07a9adfce16acc436a93e3b32b916f7c8d7e4728

---
+++ go/gcr/gcr_test.go
@@ -0,0 +1,50 @@
+package gcr
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"go.skia.org/infra/go/mockhttpclient"
+	"go.skia.org/infra/go/testutils"
+)
+
+func TestTags(t *testing.T) {
+	testutils.SmallTest(t)
+	url := fmt.Sprintf("https://%s/v2/skia-public/docserver/tags/list", SERVER)
+	m := mockhttpclient.NewURLMock()
+	m.Mock(url, mockhttpclient.MockGetDialogue([]byte(`{"tags": ["foo", "bar"]}`)))
+	c := &Client{
+		client:    m.Client(),
+		projectId: "skia-public",
+		imageName: "docserver",
+	}
+	tags, err := c.Tags()
+	assert.NoError(t, err)
+	assert.Equal(t, []string{"foo", "bar"}, tags)
+
+	c.imageName = "unknown"
+	tags, err = c.Tags()
+	assert.Error(t, err)
+}
+
+func TestGcrTokenSource(t *testing.T) {
+	testutils.SmallTest(t)
+	m := mockhttpclient.NewURLMock()
+	url := fmt.Sprintf("https://%s/v2/token?scope=repository:skia-public/docserver:pull", SERVER)
+	m.Mock(url, mockhttpclient.MockGetDialogue([]byte(`{"token": "foo", "expires_in": 3600}`)))
+
+	ts := &gcrTokenSource{
+		client:    m.Client(),
+		projectId: "skia-public",
+		imageName: "docserver",
+	}
+	token, err := ts.Token()
+	assert.NoError(t, err)
+	assert.Equal(t, "foo", token.AccessToken)
+	assert.Equal(t, "Bearer", token.TokenType)
+
+	ts.imageName = "unknown"
+	token, err = ts.Token()
+	assert.Error(t, err)
+}
