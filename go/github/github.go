b9070c229900c5d3e06d93b70ae07aa79b7251c1
Ravi Mistry <rmistry@google.com>
2018-04-24 13:48:05 -0400

    [Autoroller] Specify merge method when merging pull request
    Bug: skia:7730
    Change-Id: I53950745f3cb07ef19c3e2e99ff7d70c9b1a3695

--- go/github/github.go
+++ go/github/github.go
@@ -129,8 +129,11 @@ func (g *GitHub) CreatePullRequest(title, baseBranch, headBranch, body string) (
 
 // See https://developer.github.com/v3/pulls/#merge-a-pull-request-merge-button
 // for the API documentation.
-func (g *GitHub) MergePullRequest(pullRequestNum int, msg string) error {
-	_, resp, err := g.client.PullRequests.Merge(g.ctx, g.RepoOwner, g.RepoName, pullRequestNum, msg, &github.PullRequestOptions{})
+func (g *GitHub) MergePullRequest(pullRequestNum int, msg, mergeMethod string) error {
+	options := &github.PullRequestOptions{
+		MergeMethod: mergeMethod,
+	}
+	_, resp, err := g.client.PullRequests.Merge(g.ctx, g.RepoOwner, g.RepoName, pullRequestNum, msg, options)
 	if err != nil {
 		return fmt.Errorf("Failed doing pullrequests.merge: %s", err)
 	}
