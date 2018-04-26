a3f67ade2db413cacfd7b52eca1fe71c19653b6a
Eric Boren <borenet@google.com>
2018-04-26 10:11:26 -0400

    [recipes] Use named caches for git and vpython
    Bug: skia:
    Change-Id: Iaa4d6b2555659d40ab2f85d458ece5fc0588bdaa

--- infra/bots/gen_tasks.go
+++ infra/bots/gen_tasks.go
@@ -180,6 +180,12 @@ func kitchenTask(name, recipe, isolate, serviceAccount string, dimensions []stri
 		outputs = []string{outputDir}
 	}
 	return &specs.TaskSpec{
+		Caches: []*specs.Cache{
+			&specs.Cache{
+				Name: "vpython",
+				Path: "cache/vpython",
+			},
+		},
 		CipdPackages: cipd,
 		Command: []string{
 			"./kitchen${EXECUTABLE_SUFFIX}", "cook",
@@ -266,6 +272,16 @@ func presubmit(b *specs.TasksCfgBuilder, name string) string {
 	}
 	replaceArg("-repository", "https://chromium.googlesource.com/chromium/tools/build")
 	replaceArg("-revision", "HEAD")
+	task.Caches = append(task.Caches, []*specs.Cache{
+		&specs.Cache{
+			Name: "git",
+			Path: "cache/git",
+		},
+		&specs.Cache{
+			Name: "git_cache",
+			Path: "cache/git_cache",
+		},
+	}...)
 	task.CipdPackages = append(task.CipdPackages, CIPD_PKGS_GIT...)
 	task.Dependencies = []string{} // No bundled recipes for this one.
 	b.MustAddTask(name, task)
