13d3e6bf10eace5bad15d18162f88b920774c6f1
Eric Boren <borenet@google.com>
2018-05-04 11:08:17 -0400

    [skolo] Make metadata server share the service account token mapping
    Bug: skia:7873
    Change-Id: I514788199614c831c8879d4e9a71c755772f2f97

--- skolo/go/metadata_server/main.go
+++ skolo/go/metadata_server/main.go
@@ -5,7 +5,7 @@ import (
 	"flag"
 	"fmt"
 	"net/http"
-	"strings"
+	"os"
 
 	"github.com/gorilla/mux"
 	"go.skia.org/infra/go/common"
@@ -13,16 +13,25 @@ import (
 	"go.skia.org/infra/go/metadata"
 	"go.skia.org/infra/go/skiaversion"
 	"go.skia.org/infra/go/sklog"
+	"go.skia.org/infra/skolo/go/service_accounts"
+)
+
+const (
+	// APP_NAME is the name of this app.
+	APP_NAME = "metadata_server"
 )
 
 var (
-	// APP_NAME is the name of this app.
-	APP_NAME = "metadata_server"
-
 	// Flags.
-	port       = flag.String("port", ":8000", "HTTP service port for the web server (e.g., ':8000')")
-	promPort   = flag.String("prom_port", ":20000", "Metrics service address (e.g., ':10110')")
-	tokenFiles = common.NewMultiStringFlag("token_file", nil, "Mapping of IP address to token file, eg. \"127.0.0.1:/path/to/token.json\"")
+	port     = flag.String("port", ":8000", "HTTP service port for the web server (e.g., ':8000')")
+	promPort = flag.String("prom_port", ":20000", "Metrics service address (e.g., ':10110')")
+
+	serviceAccountIpMapping = map[*service_accounts.ServiceAccount][]string{
+		service_accounts.ChromeSwarming: []string{"*"},
+		service_accounts.ChromiumSwarm:  []string{"*"},
+		service_accounts.Jumphost:       []string{"self"},
+		service_accounts.RpiMaster:      []string{"192.168.1.98", "192.168.1.99"},
+	}
 )
 
 type instanceMetadataMap map[string]map[string]string
@@ -64,27 +73,34 @@ func main() {
 		},
 	}
 
-	if *tokenFiles == nil {
-		sklog.Fatal("At least one --token_file must be specified.")
+	hostname, err := os.Hostname()
+	if err != nil {
+		sklog.Fatal(err)
 	}
-	tokens := make(map[string]*metadata.ServiceAccountToken, len(*tokenFiles))
-	tokenIpMapping := make(map[string]string, len(*tokenFiles))
-	for _, f := range *tokenFiles {
-		split := strings.Split(f, ":")
-		if len(split) != 2 {
-			sklog.Fatalf("Invalid value for --token_file: %s", f)
-		}
-		ipAddr := split[0]
-		tokenFile := split[1]
+	serviceAccounts, ok := service_accounts.JumphostServiceAccountMapping[hostname]
+	if !ok {
+		sklog.Fatalf("Hostname not in jumphost mapping: %s", hostname)
+	}
 
-		tokenIpMapping[ipAddr] = tokenFile
-		if _, ok := tokens[tokenFile]; !ok {
-			tok, err := metadata.NewServiceAccountToken(tokenFile)
-			if err != nil {
-				sklog.Fatal(err)
+	tokens := make(map[string]*metadata.ServiceAccountToken, len(serviceAccounts))
+	tokenIpMapping := make(map[string]string, len(serviceAccounts))
+	for _, acct := range serviceAccounts {
+		ipAddrs, ok := serviceAccountIpMapping[acct]
+		if !ok {
+			sklog.Fatalf("Service account has no IP address mapping: %s", acct.Email)
+		}
+		tokenFile := fmt.Sprintf("/var/local/token_%s.json", acct.Nickname)
+
+		for _, ipAddr := range ipAddrs {
+			tokenIpMapping[ipAddr] = tokenFile
+			if _, ok := tokens[tokenFile]; !ok {
+				tok, err := metadata.NewServiceAccountToken(tokenFile)
+				if err != nil {
+					sklog.Fatal(err)
+				}
+				go tok.UpdateLoop(context.Background())
+				tokens[tokenFile] = tok
 			}
-			go tok.UpdateLoop(context.Background())
-			tokens[tokenFile] = tok
 		}
 	}
 	tokenMapping := make(map[string]*metadata.ServiceAccountToken, len(tokenIpMapping))
