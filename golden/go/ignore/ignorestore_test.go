a3b7714e7d1869355db93ec98ba9c2a6a54348d5
Stephan Altmueller <stephana@google.com>
2018-05-04 13:32:57 -0400

    [gold] Add Cloud Datastore based storage implementations
    This lays the basis for replacing MySQL with Cloud Datastore
    as the storage backend for Gold.
    
    - refactors existing storage packages (expstorage, ignore)
    - adds cloud store implementations that pass all tests for
      existing storage backend
    - adds helper types in 'ds' package to allow consistent listing
      of collections (as opposed to eventually consistent listings)
    
    Bug: skia:
    Change-Id: Ib7273ac6d17e225d703ea1ab4e763cebe044ea65

--- golden/go/ignore/ignorestore_test.go
+++ golden/go/ignore/ignorestore_test.go
@@ -5,15 +5,49 @@ import (
 	"testing"
 	"time"
 
-	"go.skia.org/infra/go/testutils"
-
 	assert "github.com/stretchr/testify/require"
+
+	"go.skia.org/infra/go/database/testutil"
+	"go.skia.org/infra/go/ds"
+	ds_testutil "go.skia.org/infra/go/ds/testutil"
+	"go.skia.org/infra/go/testutils"
+	"go.skia.org/infra/golden/go/db"
 )
 
 func TestTestMemIgnoreStore(t *testing.T) {
 	testutils.SmallTest(t)
 	memStore := NewMemIgnoreStore()
 	testIgnoreStore(t, memStore)
+}
+
+func TestSQLIgnoreStore(t *testing.T) {
+	testutils.LargeTest(t)
+	// Set up the database. This also locks the db until this test is finished
+	// causing similar tests to wait.
+	migrationSteps := db.MigrationSteps()
+	mysqlDB := testutil.SetupMySQLTestDatabase(t, migrationSteps)
+	defer mysqlDB.Close(t)
+
+	vdb, err := testutil.LocalTestDatabaseConfig(migrationSteps).NewVersionedDB()
+	assert.NoError(t, err)
+	defer testutils.AssertCloses(t, vdb)
+
+	store := NewSQLIgnoreStore(vdb, nil, nil)
+	testIgnoreStore(t, store)
+}
+
+func TestCloudIgnoreStore(t *testing.T) {
+	testutils.LargeTest(t)
+
+	// Run to the locally running emulator.
+	cleanup := ds_testutil.InitDatastore(t,
+		ds.IGNORE_RULE,
+		ds.RECENT_KEYS)
+	defer cleanup()
+
+	store, err := NewCloudIgnoreStore(ds.DS, nil, nil)
+	assert.NoError(t, err)
+	testIgnoreStore(t, store)
 }
 
 func testIgnoreStore(t *testing.T, store IgnoreStore) {
