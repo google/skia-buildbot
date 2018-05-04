c3584769f528f4f54af5158e190562014f06275f
Joe Gregorio <joe@bitworking.org>
2018-05-04 16:23:39 -0400

    [pushk] Add option to push all updatable images.
    Bug: skia:
    Change-Id: I896eddb3e1ca97d461003f8ea27177b0d3cf08ae
    Reviewed-on: https://skia-review.googlesource.com/126122
    Commit-Queue: Joe Gregorio <jcgregorio@google.com>
    Reviewed-by: Joe Gregorio <jcgregorio@google.com>

--- kube/go/pushk/main.go
+++ kube/go/pushk/main.go
@@ -4,6 +4,7 @@
 // given image. Prints the kubectl command to run to apply the changes,
 // or actually applied them if --apply is supplied.
 //
+// pushk
 // pushk docserver
 // pushk --rollback docserver
 // pushk --project=skia-public docserver
@@ -33,16 +34,26 @@ import (
 
 func init() {
 	flag.Usage = func() {
-		fmt.Printf("Usage: pushk <flags> [image]\n\n")
+		fmt.Printf("Usage: pushk <flags> [zero or more image names]\n\n")
 		fmt.Printf(`pushk pushes a new version of an app.
 
 Actually just modifies kubernetes yaml files with the correct tag for the given
 image. Prints the kubectl command to run to apply the changes, or actually
 applies them if --apply is supplied.
 
+If no image names are supplied then pushk looks through all the yaml files for
+appropriate images (ones that match the SERVER and project) and tries to push a
+new image for each of them.
+
 Examples:
+  # Push the latest version of all images from the given container repository.
+  pushk
+
   # Push the latest version of docserver.
   pushk docserver
+
+  # Push the latest version of docserver and iap_proxy
+  pushk docserver iap_proxy
 
   # Rollback docserver.
   pushk --rollback docserver
@@ -86,6 +97,28 @@ func filter(tags []string) ([]string, error) {
 	return validTags, nil
 }
 
+// findAllImageNames searches for all the images that comes from the given
+// project container registry across all the yaml files listed in filenames.
+func findAllImageNames(filenames []string, server, project string) []string {
+	// allImageRegex has the following groups returned on match:
+	// 0 - the entire line
+	// 1 - the image name
+	allImageRegex := regexp.MustCompile(fmt.Sprintf(`(?m)^\s+image:\s+%s/%s/([^:]+):\S+\s*$`, server, project))
+	filenameSet := util.StringSet{}
+	for _, filename := range filenames {
+		b, err := ioutil.ReadFile(filename)
+		if err != nil {
+			sklog.Errorf("Failed to read %q (skipping): %s", filename, err)
+			continue
+		}
+		matches := allImageRegex.FindAllStringSubmatch(string(b), -1)
+		for _, m := range matches {
+			filenameSet[m[1]] = true
+		}
+	}
+	return filenameSet.Keys()
+}
+
 func main() {
 	common.Init()
 	if *apply {
@@ -94,81 +127,88 @@ func main() {
 		_ = flag.Lookup("logtostderr").Value.Set("true")
 	}
 
-	tokenSource := auth.NewGCloudTokenSource(*project)
-	imageName := flag.Arg(0)
-	if imageName == "" {
-		flag.Usage()
-		os.Exit(1)
-	}
-
-	// imageRegex has the following groups returned on match:
-	// 0 - the entire line
-	// 1 - the prefix, i.e. image:, with correct spacing.
-	// 2 - full image name
-	// 3 - just the tag
-	//
-	// We pull out the 'prefix' so we can use it when
-	// we rewrite the image: line so the indent level is
-	// unchanged.
-	imageRegex := regexp.MustCompile(fmt.Sprintf(`^(\s+image:\s+)(%s/%s/%s:(\S+))\s*$`, gcr.SERVER, *project, imageName))
-
 	// Get all the yaml files.
 	filenames, err := filepath.Glob(filepath.Join(*kubeDir, *project, "*.yaml"))
 	if err != nil {
 		sklog.Fatal(err)
 	}
 
-	// Get all the tags for the selected image.
-	tags, err := gcr.NewClient(tokenSource, *project, imageName).Tags()
-	if err != nil {
-		sklog.Fatal(err)
-	}
-
-	// Filter the tags
-	tags, err = filter(tags)
-	if err != nil {
-		sklog.Fatal(err)
-	}
-
-	// Pick the target tag we want to move to.
-	tag := tags[len(tags)-1]
-	if *rollback {
-		if len(tags) < 2 {
-			sklog.Fatal(fmt.Errorf("No version to rollback to."))
+	tokenSource := auth.NewGCloudTokenSource(*project)
+	imageNames := flag.Args()
+	if len(imageNames) == 0 {
+		imageNames = findAllImageNames(filenames, gcr.SERVER, *project)
+		if len(imageNames) == 0 {
+			fmt.Printf("Failed to find any images that match kubernetes directory: %q and project: %q.", *kubeDir, *project)
+			flag.Usage()
+			os.Exit(1)
 		}
-		tag = tags[len(tags)-2]
 	}
+	sklog.Infof("Pushing the following images: %q", imageNames)
 
-	// The full docker image name and tag of the image we want to deploy.
-	image := fmt.Sprintf("%s/%s/%s:%s", gcr.SERVER, *project, imageName, tag)
-
-	// Loop over all the yaml files and update tags for the given imageName.
 	changed := util.StringSet{}
-	for _, filename := range filenames {
-		b, err := ioutil.ReadFile(filename)
+	for _, imageName := range imageNames {
+		// imageRegex has the following groups returned on match:
+		// 0 - the entire line
+		// 1 - the prefix, i.e. image:, with correct spacing.
+		// 2 - full image name
+		// 3 - just the tag
+		//
+		// We pull out the 'prefix' so we can use it when
+		// we rewrite the image: line so the indent level is
+		// unchanged.
+		imageRegex := regexp.MustCompile(fmt.Sprintf(`^(\s+image:\s+)(%s/%s/%s:(\S+))\s*$`, gcr.SERVER, *project, imageName))
+
+		// Get all the tags for the selected image.
+		tags, err := gcr.NewClient(tokenSource, *project, imageName).Tags()
 		if err != nil {
-			sklog.Errorf("Failed to read %q (skipping): %s", filename, err)
-			continue
+			sklog.Fatal(err)
 		}
-		lines := strings.Split(string(b), "\n")
-		for i, line := range lines {
-			matches := imageRegex.FindStringSubmatch(line)
-			if len(matches) != 4 {
+
+		// Filter the tags
+		tags, err = filter(tags)
+		if err != nil {
+			sklog.Fatal(err)
+		}
+
+		// Pick the target tag we want to move to.
+		tag := tags[len(tags)-1]
+		if *rollback {
+			if len(tags) < 2 {
+				sklog.Fatal(fmt.Errorf("No version to rollback to."))
+			}
+			tag = tags[len(tags)-2]
+		}
+
+		// The full docker image name and tag of the image we want to deploy.
+		image := fmt.Sprintf("%s/%s/%s:%s", gcr.SERVER, *project, imageName, tag)
+
+		// Loop over all the yaml files and update tags for the given imageName.
+		for _, filename := range filenames {
+			b, err := ioutil.ReadFile(filename)
+			if err != nil {
+				sklog.Errorf("Failed to read %q (skipping): %s", filename, err)
 				continue
 			}
-			if matches[3] != tag {
-				changed[filename] = true
-				// Replace with the old 'prefix' and our new image.
-				lines[i] = matches[1] + image
+			lines := strings.Split(string(b), "\n")
+			for i, line := range lines {
+				matches := imageRegex.FindStringSubmatch(line)
+				if len(matches) != 4 {
+					continue
+				}
+				if matches[3] != tag {
+					changed[filename] = true
+					// Replace with the old 'prefix' and our new image.
+					lines[i] = matches[1] + image
+				}
 			}
-		}
-		if changed[filename] {
-			err := util.WithWriteFile(filename, func(w io.Writer) error {
-				_, err := w.Write([]byte(strings.Join(lines, "\n")))
-				return err
-			})
-			if err != nil {
-				sklog.Fatalf("Failed to write update config file %q: %s", filename, err)
+			if changed[filename] {
+				err := util.WithWriteFile(filename, func(w io.Writer) error {
+					_, err := w.Write([]byte(strings.Join(lines, "\n")))
+					return err
+				})
+				if err != nil {
+					sklog.Fatalf("Failed to write update config file %q: %s", filename, err)
+				}
 			}
 		}
 	}
