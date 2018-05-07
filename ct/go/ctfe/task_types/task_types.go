2dee150b24f9ef42aa417c745678848c0a3f16cc
Ravi Mistry <rmistry@google.com>
2018-05-07 16:36:41 -0400

    [CTFE] Add metrics analysis to list of recognized tasks
    Bug: skia:7872
    Change-Id: I3441f52c4f5c388462dcee8b43cf1e6c93cf3eae
    Reviewed-on: https://skia-review.googlesource.com/126623
    Reviewed-by: Ravi Mistry <rmistry@google.com>
    Commit-Queue: Ravi Mistry <rmistry@google.com>

--- ct/go/ctfe/task_types/task_types.go
+++ ct/go/ctfe/task_types/task_types.go
@@ -11,6 +11,7 @@ import (
 	"go.skia.org/infra/ct/go/ctfe/chromium_builds"
 	"go.skia.org/infra/ct/go/ctfe/chromium_perf"
 	"go.skia.org/infra/ct/go/ctfe/lua_scripts"
+	"go.skia.org/infra/ct/go/ctfe/metrics_analysis"
 	"go.skia.org/infra/ct/go/ctfe/pixel_diff"
 	"go.skia.org/infra/ct/go/ctfe/task_common"
 )
@@ -18,13 +19,14 @@ import (
 // Slice of all tasks supported by CTFE.
 func Prototypes() []task_common.Task {
 	return []task_common.Task{
-		&chromium_analysis.DBTask{},
-		&chromium_perf.DBTask{},
-		&pixel_diff.DBTask{},
-		&capture_skps.DBTask{},
-		&lua_scripts.DBTask{},
-		&chromium_builds.DBTask{},
 		&admin_tasks.RecreatePageSetsDBTask{},
 		&admin_tasks.RecreateWebpageArchivesDBTask{},
+		&capture_skps.DBTask{},
+		&chromium_analysis.DBTask{},
+		&chromium_builds.DBTask{},
+		&chromium_perf.DBTask{},
+		&lua_scripts.DBTask{},
+		&metrics_analysis.DBTask{},
+		&pixel_diff.DBTask{},
 	}
 }
