8dce4f89dfeac30f6defac043618a81428bd8ba6
Stephan Altmueller <stephana@google.com>
2018-05-07 12:53:48 -0400

    wip: add cloud datastore emulator for testing
    Bug: skia:
    Change-Id: I51f07faeddf428ccad975aed430ff15aa25a0a31

--- go/cipd/asset_versions_gen.go
+++ go/cipd/asset_versions_gen.go
@@ -3,7 +3,8 @@
 package cipd
 
 var PKG_VERSIONS_FROM_ASSETS = map[string]string{
-	"go":     "4",
-	"node":   "0",
-	"protoc": "0",
+	"gcloud_linux": "10",
+	"go":           "4",
+	"node":         "0",
+	"protoc":       "0",
 }
