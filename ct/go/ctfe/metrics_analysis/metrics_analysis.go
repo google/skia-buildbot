e76c3187249c2ee17b4f3657c007ec3440490184
Ravi Mistry <rmistry@google.com>
2018-05-07 12:37:22 -0400

    [CT] Add metrics analysis master script and CTFE handlers and types
    Bug: skia:7872
    Change-Id: I271609873a9ee4401f9412431ff8739a965e5a8f
    Reviewed-on: https://skia-review.googlesource.com/125060
    Commit-Queue: Ravi Mistry <rmistry@google.com>
    Reviewed-by: Ben Wagner <benjaminwagner@google.com>

---
+++ ct/go/ctfe/metrics_analysis/metrics_analysis.go
@@ -0,0 +1,232 @@
+/*
+	Handlers and types specific to Metrics analysis tasks.
+*/
+
+package metrics_analysis
+
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+	"path/filepath"
+	"strconv"
+	"text/template"
+
+	"github.com/gorilla/mux"
+
+	"go.skia.org/infra/ct/go/ctfe/task_common"
+	ctfeutil "go.skia.org/infra/ct/go/ctfe/util"
+	"go.skia.org/infra/ct/go/db"
+	ctutil "go.skia.org/infra/ct/go/util"
+	"go.skia.org/infra/go/httputils"
+)
+
+var (
+	addTaskTemplate     *template.Template = nil
+	runsHistoryTemplate *template.Template = nil
+
+	httpClient = httputils.NewTimeoutClient()
+)
+
+func ReloadTemplates(resourcesDir string) {
+	addTaskTemplate = template.Must(template.ParseFiles(
+		filepath.Join(resourcesDir, "templates/metrics_analysis.html"),
+		filepath.Join(resourcesDir, "templates/header.html"),
+		filepath.Join(resourcesDir, "templates/titlebar.html"),
+	))
+	runsHistoryTemplate = template.Must(template.ParseFiles(
+		filepath.Join(resourcesDir, "templates/metrics_analysis_runs_history.html"),
+		filepath.Join(resourcesDir, "templates/header.html"),
+		filepath.Join(resourcesDir, "templates/titlebar.html"),
+	))
+}
+
+type DBTask struct {
+	task_common.CommonCols
+
+	MetricName         string         `db:"metric_name"`
+	CustomTraces       string         `db:"custom_traces"`
+	AnalysisTaskId     string         `db:"analysis_task_id"`
+	AnalysisOutputLink string         `db:"analysis_output_link"`
+	BenchmarkArgs      string         `db:"benchmark_args"`
+	Description        string         `db:"description"`
+	ChromiumPatch      string         `db:"chromium_patch"`
+	CatapultPatch      string         `db:"catapult_patch"`
+	RawOutput          sql.NullString `db:"raw_output"`
+}
+
+func (task DBTask) GetTaskName() string {
+	return "MetricsAnalysis"
+}
+
+func (dbTask DBTask) GetPopulatedAddTaskVars() task_common.AddTaskVars {
+	taskVars := &AddTaskVars{}
+	taskVars.Username = dbTask.Username
+	taskVars.TsAdded = ctutil.GetCurrentTs()
+	taskVars.RepeatAfterDays = strconv.FormatInt(dbTask.RepeatAfterDays, 10)
+	taskVars.MetricName = dbTask.MetricName
+	taskVars.CustomTraces = dbTask.CustomTraces
+	taskVars.AnalysisTaskId = dbTask.AnalysisTaskId
+	taskVars.AnalysisOutputLink = dbTask.AnalysisOutputLink
+	taskVars.BenchmarkArgs = dbTask.BenchmarkArgs
+	taskVars.Description = dbTask.Description
+	taskVars.ChromiumPatch = dbTask.ChromiumPatch
+	taskVars.CatapultPatch = dbTask.CatapultPatch
+	return taskVars
+}
+
+func (task DBTask) GetResultsLink() string {
+	if task.RawOutput.Valid {
+		return task.RawOutput.String
+	} else {
+		return ""
+	}
+}
+
+func (task DBTask) GetUpdateTaskVars() task_common.UpdateTaskVars {
+	return &UpdateVars{}
+}
+
+func (task DBTask) RunsOnGCEWorkers() bool {
+	return true
+}
+
+func (task DBTask) TableName() string {
+	return db.TABLE_METRICS_ANALYSIS_TASKS
+}
+
+func (task DBTask) Select(query string, args ...interface{}) (interface{}, error) {
+	result := []DBTask{}
+	err := db.DB.Select(&result, query, args...)
+	return result, err
+}
+
+func addTaskView(w http.ResponseWriter, r *http.Request) {
+	ctfeutil.ExecuteSimpleTemplate(addTaskTemplate, w, r)
+}
+
+type AddTaskVars struct {
+	task_common.AddTaskCommonVars
+
+	MetricName         string `json:"metric_name"`
+	CustomTraces       string `json:"custom_traces"`
+	AnalysisTaskId     string `json:"analysis_task_id"`
+	AnalysisOutputLink string `json:"analysis_output_link"`
+	BenchmarkArgs      string `json:"benchmark_args"`
+	Description        string `json:"desc"`
+	ChromiumPatch      string `json:"chromium_patch"`
+	CatapultPatch      string `json:"catapult_patch"`
+}
+
+func (task *AddTaskVars) GetInsertQueryAndBinds() (string, []interface{}, error) {
+	if task.MetricName == "" {
+		return "", nil, fmt.Errorf("Must specify metric name")
+	}
+	if task.CustomTraces == "" && task.AnalysisTaskId == "" {
+		return "", nil, fmt.Errorf("Must specify one of custom traces or analysis task id")
+	}
+	if task.Description == "" {
+		return "", nil, fmt.Errorf("Must specify description")
+	}
+	if err := ctfeutil.CheckLengths([]ctfeutil.LengthCheck{
+		{Name: "metric_name", Value: task.MetricName, Limit: 255},
+		{Name: "benchmark_args", Value: task.BenchmarkArgs, Limit: 255},
+		{Name: "desc", Value: task.Description, Limit: 255},
+		{Name: "custom_traces", Value: task.CustomTraces, Limit: db.LONG_TEXT_MAX_LENGTH},
+		{Name: "chromium_patch", Value: task.ChromiumPatch, Limit: db.LONG_TEXT_MAX_LENGTH},
+		{Name: "catapult_patch", Value: task.CatapultPatch, Limit: db.LONG_TEXT_MAX_LENGTH},
+	}); err != nil {
+		return "", nil, err
+	}
+	if task.AnalysisTaskId != "" {
+		// Get analysis output link from analysis task id.
+		outputLinks := []string{}
+		query := fmt.Sprintf("SELECT raw_output FROM %s WHERE id = ?", db.TABLE_CHROMIUM_ANALYSIS_TASKS)
+		if err := db.DB.Select(&outputLinks, query, task.AnalysisTaskId); err != nil || len(outputLinks) < 1 {
+			return "", nil, fmt.Errorf("Unable to validate analysis task id parameter %v", task.AnalysisTaskId)
+		}
+		if len(outputLinks) != 1 {
+			return "", nil, fmt.Errorf("Unable to find requested analysis task id.")
+		}
+		task.AnalysisOutputLink = outputLinks[0]
+	}
+
+	return fmt.Sprintf("INSERT INTO %s (username,metric_name,custom_traces,analysis_task_id,analysis_output_link,benchmark_args,description,chromium_patch,catapult_patch,ts_added,repeat_after_days) VALUES (?,?,?,?,?,?,?,?,?,?,?);",
+			db.TABLE_METRICS_ANALYSIS_TASKS),
+		[]interface{}{
+			task.Username,
+			task.MetricName,
+			task.CustomTraces,
+			task.AnalysisTaskId,
+			task.AnalysisOutputLink,
+			task.BenchmarkArgs,
+			task.Description,
+			task.ChromiumPatch,
+			task.CatapultPatch,
+			task.TsAdded,
+			task.RepeatAfterDays,
+		},
+		nil
+}
+
+func addTaskHandler(w http.ResponseWriter, r *http.Request) {
+	task_common.AddTaskHandler(w, r, &AddTaskVars{})
+}
+
+func getTasksHandler(w http.ResponseWriter, r *http.Request) {
+	task_common.GetTasksHandler(&DBTask{}, w, r)
+}
+
+type UpdateVars struct {
+	task_common.UpdateTaskCommonVars
+
+	RawOutput sql.NullString
+}
+
+func (vars *UpdateVars) UriPath() string {
+	return ctfeutil.UPDATE_METRICS_ANALYSIS_TASK_POST_URI
+}
+
+func (task *UpdateVars) GetUpdateExtraClausesAndBinds() ([]string, []interface{}, error) {
+	if err := ctfeutil.CheckLengths([]ctfeutil.LengthCheck{
+		{Name: "RawOutput", Value: task.RawOutput.String, Limit: 255},
+	}); err != nil {
+		return nil, nil, err
+	}
+	clauses := []string{}
+	args := []interface{}{}
+	if task.RawOutput.Valid {
+		clauses = append(clauses, "raw_output = ?")
+		args = append(args, task.RawOutput.String)
+	}
+	return clauses, args, nil
+}
+
+func updateTaskHandler(w http.ResponseWriter, r *http.Request) {
+	task_common.UpdateTaskHandler(&UpdateVars{}, db.TABLE_CHROMIUM_ANALYSIS_TASKS, w, r)
+}
+
+func deleteTaskHandler(w http.ResponseWriter, r *http.Request) {
+	task_common.DeleteTaskHandler(&DBTask{}, w, r)
+}
+
+func redoTaskHandler(w http.ResponseWriter, r *http.Request) {
+	task_common.RedoTaskHandler(&DBTask{}, w, r)
+}
+
+func runsHistoryView(w http.ResponseWriter, r *http.Request) {
+	ctfeutil.ExecuteSimpleTemplate(runsHistoryTemplate, w, r)
+}
+
+func AddHandlers(r *mux.Router) {
+	ctfeutil.AddForceLoginHandler(r, "/"+ctfeutil.METRICS_ANALYSIS_URI, "GET", addTaskView)
+	ctfeutil.AddForceLoginHandler(r, "/"+ctfeutil.METRICS_ANALYSIS_RUNS_URI, "GET", runsHistoryView)
+
+	ctfeutil.AddForceLoginHandler(r, "/"+ctfeutil.ADD_METRICS_ANALYSIS_TASK_POST_URI, "POST", addTaskHandler)
+	ctfeutil.AddForceLoginHandler(r, "/"+ctfeutil.GET_METRICS_ANALYSIS_TASKS_POST_URI, "POST", getTasksHandler)
+	ctfeutil.AddForceLoginHandler(r, "/"+ctfeutil.DELETE_METRICS_ANALYSIS_TASK_POST_URI, "POST", deleteTaskHandler)
+	ctfeutil.AddForceLoginHandler(r, "/"+ctfeutil.REDO_METRICS_ANALYSIS_TASK_POST_URI, "POST", redoTaskHandler)
+
+	// Do not add force login handler for update methods. They use webhooks for authentication.
+	r.HandleFunc("/"+ctfeutil.UPDATE_METRICS_ANALYSIS_TASK_POST_URI, updateTaskHandler).Methods("POST")
+}
---
+++ ct/go/master_scripts/metrics_analysis_on_workers/main.go
@@ -0,0 +1,263 @@
+// metrics_analysis_on_workers is an application that runs the
+// analysis_metrics_ct benchmark on all CT workers and uploads results to Google
+// Storage. The requester is emailed when the task is started and also after
+// completion.
+//
+// Can be tested locally with:
+// $ go run go/master_scripts/metrics_analysis_on_workers/main.go --run_id=rmistry-test1 --benchmark_extra_args="--output-format=csv" --logtostderr=true --description=testing --metric_name=loadingMetric --analysis_output_link="https://ct.skia.org/results/cluster-telemetry/tasks/benchmark_runs/rmistry-20180502115012/consolidated_outputs/rmistry-20180502115012.output"
+//
+package main
+
+import (
+	"context"
+	"database/sql"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"go.skia.org/infra/ct/go/ctfe/metrics_analysis"
+	"go.skia.org/infra/ct/go/frontend"
+	"go.skia.org/infra/ct/go/master_scripts/master_common"
+	"go.skia.org/infra/ct/go/util"
+	"go.skia.org/infra/go/common"
+	"go.skia.org/infra/go/email"
+	"go.skia.org/infra/go/fileutil"
+	"go.skia.org/infra/go/sklog"
+	skutil "go.skia.org/infra/go/util"
+)
+
+const (
+	// TODO(rmistry): What is the sweet spot here?
+	MAX_PAGES_PER_SWARMING_BOT = 200
+)
+
+var (
+	emails             = flag.String("emails", "", "The comma separated email addresses to notify when the task is picked up and completes.")
+	description        = flag.String("description", "", "The description of the run as entered by the requester.")
+	gaeTaskID          = flag.Int64("gae_task_id", -1, "The key of the CT task in CTFE. The task will be updated when it is started and also when it completes.")
+	benchmarkExtraArgs = flag.String("benchmark_extra_args", "", "The extra arguments that are passed to the analysis_metrics_ct benchmark.")
+	runID              = flag.String("run_id", "", "The unique run id (typically requester + timestamp).")
+
+	metricName         = flag.String("metric_name", "", "The metric to parse the traces with. Eg: loadingMetric")
+	analysisOutputLink = flag.String("analysis_output_link", "", "Cloud trace links will be gathered from this specified CT analysis run Id. If not specified, trace links will be read from ${TMPDIR}/<run_id>.traces.csv")
+
+	taskCompletedSuccessfully = false
+
+	chromiumPatchLink = ""
+	catapultPatchLink = ""
+	tracesLink        = ""
+	outputLink        = ""
+)
+
+func sendEmail(recipients []string, gs *util.GcsUtil) {
+	// Send completion email.
+	emailSubject := fmt.Sprintf("Metrics analysis cluster telemetry task has completed (%s)", *runID)
+	failureHtml := ""
+	viewActionMarkup := ""
+	var err error
+
+	if taskCompletedSuccessfully {
+		if viewActionMarkup, err = email.GetViewActionMarkup(outputLink, "View Results", "Direct link to the CSV results"); err != nil {
+			sklog.Errorf("Failed to get view action markup: %s", err)
+			return
+		}
+	} else {
+		emailSubject += " with failures"
+		failureHtml = util.GetFailureEmailHtml(*runID)
+		if viewActionMarkup, err = email.GetViewActionMarkup(fmt.Sprintf(util.SWARMING_RUN_ID_ALL_TASKS_LINK_TEMPLATE, *runID), "View Failure", "Direct link to the swarming logs"); err != nil {
+			sklog.Errorf("Failed to get view action markup: %s", err)
+			return
+		}
+	}
+
+	bodyTemplate := `
+	The metrics analysis task has completed. %s.<br/>
+	Run description: %s<br/>
+	%s
+	The CSV output is <a href='%s'>here</a>.%s<br/>
+	The patch(es) you specified are here:
+	<a href='%s'>chromium</a>/<a href='%s'>catapult</a>
+	<br/>
+	Traces used for this run are <a href='%s'>here</a>.
+	<br/><br/>
+	You can schedule more runs <a href='%s'>here</a>.
+	<br/><br/>
+	Thanks!
+	`
+	emailBody := fmt.Sprintf(bodyTemplate, util.GetSwarmingLogsLink(*runID), *description, failureHtml, outputLink, chromiumPatchLink, catapultPatchLink, tracesLink, frontend.MetricsAnalysisTasksWebapp)
+	if err := util.SendEmailWithMarkup(recipients, emailSubject, emailBody, viewActionMarkup); err != nil {
+		sklog.Errorf("Error while sending email: %s", err)
+		return
+	}
+}
+
+func updateWebappTask() {
+	vars := metrics_analysis.UpdateVars{}
+	vars.Id = *gaeTaskID
+	vars.SetCompleted(taskCompletedSuccessfully)
+	vars.RawOutput = sql.NullString{String: outputLink, Valid: true}
+	skutil.LogErr(frontend.UpdateWebappTaskV2(&vars))
+}
+
+func main() {
+	defer common.LogPanic()
+	master_common.Init("run_metrics_analysis")
+
+	ctx := context.Background()
+
+	// Send start email.
+	emailsArr := util.ParseEmails(*emails)
+	emailsArr = append(emailsArr, util.CtAdmins...)
+	if len(emailsArr) == 0 {
+		sklog.Error("At least one email address must be specified")
+		return
+	}
+	// Instantiate GcsUtil object.
+	gs, err := util.NewGcsUtil(nil)
+	if err != nil {
+		sklog.Errorf("Could not instantiate gsutil object: %s", err)
+		return
+	}
+
+	skutil.LogErr(frontend.UpdateWebappTaskSetStarted(&metrics_analysis.UpdateVars{}, *gaeTaskID, *runID))
+	skutil.LogErr(util.SendTaskStartEmail(emailsArr, "Metrics analysis", *runID, *description))
+	// Ensure webapp is updated and email is sent even if task fails.
+	defer updateWebappTask()
+	defer sendEmail(emailsArr, gs)
+	// Cleanup dirs after run completes.
+	defer skutil.RemoveAll(filepath.Join(util.StorageDir, util.BenchmarkRunsDir, *runID))
+	// Finish with glog flush and how long the task took.
+	defer util.TimeTrack(time.Now(), "Running metrics analysis task on workers")
+	defer sklog.Flush()
+
+	if *runID == "" {
+		sklog.Error("Must specify --run_id")
+		return
+	}
+	if *metricName == "" {
+		sklog.Error("Must specify --metric_name")
+		return
+	}
+
+	tracesFileName := *runID + ".traces.csv"
+	tracesFilePath := filepath.Join(os.TempDir(), tracesFileName)
+	if *analysisOutputLink != "" {
+		if err := extractTracesFromAnalysisRun(tracesFilePath, gs); err != nil {
+			sklog.Errorf("Error when extracting traces from run %s to %s: %s", *analysisOutputLink, tracesFilePath, err)
+			return
+		}
+	}
+	// Figure out how many traces we are dealing with.
+	traces, err := ioutil.ReadFile(tracesFilePath)
+	if err != nil {
+		sklog.Errorf("Could not read %s: %s", tracesFilePath, err)
+		return
+	}
+	numTraces := len(strings.Split(string(traces), ","))
+	if numTraces == 0 {
+		sklog.Errorf("Could not find any traces in %s", tracesFileName)
+		return
+	}
+
+	// Copy the patches and traces to Google Storage.
+	remoteOutputDir := filepath.Join(util.BenchmarkRunsDir, *runID)
+	chromiumPatchName := *runID + ".chromium.patch"
+	catapultPatchName := *runID + ".catapult.patch"
+	for _, patchName := range []string{chromiumPatchName, catapultPatchName, tracesFileName} {
+		if err := gs.UploadFile(patchName, os.TempDir(), remoteOutputDir); err != nil {
+			sklog.Errorf("Could not upload %s to %s: %s", patchName, remoteOutputDir, err)
+			return
+		}
+	}
+	chromiumPatchLink = util.GCS_HTTP_LINK + filepath.Join(util.GCSBucketName, remoteOutputDir, chromiumPatchName)
+	catapultPatchLink = util.GCS_HTTP_LINK + filepath.Join(util.GCSBucketName, remoteOutputDir, catapultPatchName)
+	tracesLink = util.GCS_HTTP_LINK + filepath.Join(util.GCSBucketName, remoteOutputDir, tracesFileName)
+
+	// Find which chromium hash the workers should use.
+	chromiumHash, err := util.GetChromiumHash(ctx)
+	if err != nil {
+		sklog.Error("Could not find the latest chromium hash")
+		return
+	}
+
+	// Archive, trigger and collect swarming tasks.
+	isolateExtraArgs := map[string]string{
+		"RUN_ID":         *runID,
+		"BENCHMARK_ARGS": *benchmarkExtraArgs,
+		"METRIC_NAME":    *metricName,
+		"CHROMIUM_HASH":  chromiumHash,
+	}
+	numSlaves, err := util.TriggerSwarmingTask(ctx, "" /* pagesetType */, "metrics_analysis", util.METRICS_ANALYSIS_ISOLATE, *runID, 12*time.Hour, 1*time.Hour, util.USER_TASKS_PRIORITY, MAX_PAGES_PER_SWARMING_BOT, numTraces, isolateExtraArgs, true /* runOnGCE */, util.GetRepeatValue(*benchmarkExtraArgs, 1))
+	if err != nil {
+		sklog.Errorf("Error encountered when swarming tasks: %s", err)
+		return
+	}
+
+	// If "--output-format=csv" is specified then merge all CSV files and upload.
+	noOutputSlaves := []string{}
+	pathToPyFiles := util.GetPathToPyFiles(false)
+	if strings.Contains(*benchmarkExtraArgs, "--output-format=csv") {
+		if noOutputSlaves, err = util.MergeUploadCSVFiles(ctx, *runID, pathToPyFiles, gs, numTraces, MAX_PAGES_PER_SWARMING_BOT, true /* handleStrings */, util.GetRepeatValue(*benchmarkExtraArgs, 1)); err != nil {
+			sklog.Errorf("Unable to merge and upload CSV files for %s: %s", *runID, err)
+		}
+	}
+	// If the number of noOutputSlaves is the same as the total number of triggered slaves then consider the run failed.
+	if len(noOutputSlaves) == numSlaves {
+		sklog.Errorf("All %d slaves produced no output", numSlaves)
+		return
+	}
+
+	// Construct the output link.
+	outputLink = util.GCS_HTTP_LINK + filepath.Join(util.GCSBucketName, util.BenchmarkRunsDir, *runID, "consolidated_outputs", *runID+".output")
+
+	// Display the no output slaves.
+	for _, noOutputSlave := range noOutputSlaves {
+		directLink := fmt.Sprintf(util.SWARMING_RUN_ID_TASK_LINK_PREFIX_TEMPLATE, *runID, "metrics_analysis_"+noOutputSlave)
+		fmt.Printf("Missing output from %s\n", directLink)
+	}
+
+	taskCompletedSuccessfully = true
+}
+
+// extractTracesFromAnalysisRuns gathers all traceURLs from the specified analysis
+// run and writes to the specified outputPath.
+func extractTracesFromAnalysisRun(outputPath string, gs *util.GcsUtil) error {
+	// Construct path to the google storage locations and download it locally.
+	remoteCsvPath := strings.Split(*analysisOutputLink, util.GCSBucketName+"/")[1]
+	localDownloadPath := filepath.Join(util.StorageDir, util.BenchmarkRunsDir, *runID, "downloads")
+	localCsvPath := filepath.Join(localDownloadPath, *runID+".csv")
+	if err := fileutil.EnsureDirPathExists(localCsvPath); err != nil {
+		return fmt.Errorf("Could not create %s: %s", localDownloadPath, err)
+	}
+	defer skutil.RemoveAll(localDownloadPath)
+	if err := gs.DownloadRemoteFile(remoteCsvPath, localCsvPath); err != nil {
+		return fmt.Errorf("Error downloading %s to %s: %s", remoteCsvPath, localCsvPath, err)
+	}
+
+	headers, values, err := util.GetRowsFromCSV(localCsvPath)
+	if err != nil {
+		return fmt.Errorf("Could not read %s: %s. Analysis output link: %s", localCsvPath, err, *analysisOutputLink)
+	}
+	// Gather trace URLs from the CSV.
+	traceURLs := []string{}
+	for i := range headers {
+		if headers[i] == "traceUrls" {
+			for j := range values {
+				if values[j][i] != "" {
+					traceURLs = append(traceURLs, values[j][i])
+				}
+			}
+		}
+	}
+	if len(traceURLs) == 0 {
+		return fmt.Errorf("There were no traceURLs found for the analysis output link: %s", *analysisOutputLink)
+	}
+	if err := ioutil.WriteFile(outputPath, []byte(strings.Join(traceURLs, ",")), 0644); err != nil {
+		return fmt.Errorf("Could not write traceURLs to %s: %s", outputPath, err)
+	}
+	return nil
+}
