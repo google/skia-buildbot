92e4293a1dd66f87aa3332d1772e82dd54ccb5a7
Joe Gregorio <jcgregorio@google.com>
2018-05-14 14:30:39 -0400

    Start breaking out webhook proxy as its own service.
    * Switch alertmanager over to using notifier.
    * Generalize create-sah.sh to ramdisk.sh and move to /bash/.
    
    Should take further steps to generalize so other apps can use notifier.
    
    Bug: skia:
    Change-Id: I22d94e74fc78c7dea9299051cb79ccad80cb8df5
    Reviewed-on: https://skia-review.googlesource.com/128000
    Commit-Queue: Joe Gregorio <jcgregorio@google.com>
    Reviewed-by: Ben Wagner <benjaminwagner@google.com>
    Auto-Submit: Joe Gregorio <jcgregorio@google.com>

--- kube/create-skia-doc-sa.sh
+++ kube/create-skia-doc-sa.sh
@@ -6,10 +6,12 @@
 
 set -e -x
 source ./config.sh
-source ./create-sa.sh
+source ../bash/ramdisk.sh
 
 # New service account we will create.
 SA_NAME=skia-docs
+
+cd /tmp/ramdisk
 
 gcloud iam service-accounts create "${SA_NAME}" --display-name="Read-only access to GCS for skia docs server side includes."
 
@@ -17,3 +19,4 @@ gcloud beta iam service-accounts keys create ${SA_NAME}.json --iam-account="${SA
 
 kubectl create secret generic "${SA_NAME}" --from-file=key.json=${SA_NAME}.json
 
+cd -
