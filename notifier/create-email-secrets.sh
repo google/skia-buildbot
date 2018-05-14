d7f17b2995359991c5ac4eca5a74737216f151cb
Joe Gregorio <joe@bitworking.org>
2018-05-14 09:13:47 -0400

    notifier_wip
    Bug: skia:
    Change-Id: I22d94e74fc78c7dea9299051cb79ccad80cb8df5

--- notifier/create-email-secrets.sh
+++ notifier/create-email-secrets.sh
@@ -1,16 +1,17 @@
-# Create the secrets needed for alertmanager-webhooks to send email.
+# Create the secrets needed for notifier to send email.
 set -e
 
 # Enable the gmail API for your project and create a client secret for this applicaiton.
 # Then download the client_secret.json file.
-echo "Download client_secret.json for alertmanager-webhooks."
+echo "Download client_secret.json for notifier to the email_secrets directory."
 read -r -p "Press enter to continue..." key
 
 # Then run 'three_legged_flow' in this directory and when prompted authorize as alertserver@skia.org to create the client_token.json file.
 go install ./go/three_legged_flow
+cd email_secrets
 three_legged_flow --scopes=https://mail.google.com/
-kubectl create secret generic alertmanager-webhook-client-secret --from-file=client_secret.json=client_secret.json --dry-run -o yaml | kubectl apply -f -
-kubectl create secret generic alertmanager-webhook-client-token --from-file=client_token.json=client_token.json --dry-run -o yaml | kubectl apply -f -
+cd ..
+kubectl create secret generic notifier-email-secrets --from-file=email_secrets=.. --dry-run -o yaml | kubectl apply -f -
 
 # Finally, remove the token file since it contains a refresh token.
 rm client_token.json
