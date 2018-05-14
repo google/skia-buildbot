65d83e15abad436041c6b26c83db3ae354175373
Stephan Altmüller <stephana@google.com>
2018-05-14 15:12:08 -0400

    wip: debug race bot
    Bug: skia:
    Change-Id: Ia4a276a3180553dcb13f2248522a16f71e964097

--- infra/bots/gen_tasks.go
+++ infra/bots/gen_tasks.go
@@ -231,7 +231,17 @@ func kitchenTask(name, recipe, isolate, serviceAccount string, dimensions []stri
 // infra generates an infra test Task. Returns the name of the last Task in the
 // generated chain of Tasks, which the Job should add as a dependency.
 func infra(b *specs.TasksCfgBuilder, name string) string {
-	task := kitchenTask(name, "swarm_infra", "infrabots.isolate", SERVICE_ACCOUNT_COMPILE, linuxGceDimensions(), nil, OUTPUT_NONE)
+	dimensions := linuxGceDimensions()
+	if strings.Contains(name, "Race") {
+		for idx, dim := range dimensions {
+			if strings.HasPrefix(dim, "machine_type") {
+				dimensions[idx] = "machine_type:n1-highcpu-64"
+				break
+			}
+		}
+	}
+
+	task := kitchenTask(name, "swarm_infra", "infrabots.isolate", SERVICE_ACCOUNT_COMPILE, dimensions, nil, OUTPUT_NONE)
 	task.CipdPackages = append(task.CipdPackages, CIPD_PKGS_GIT...)
 	task.CipdPackages = append(task.CipdPackages, b.MustGetCipdPackageFromAsset("go"))
 	task.CipdPackages = append(task.CipdPackages, b.MustGetCipdPackageFromAsset("node"))
@@ -242,6 +252,7 @@ func infra(b *specs.TasksCfgBuilder, name string) string {
 	}
 	task.MaxAttempts = 2
 	if strings.Contains(name, "Race") {
+		task.CipdPackages = append(task.CipdPackages, b.MustGetCipdPackageFromAsset("gcloud_linux"))
 		task.MaxAttempts = 1
 		task.IoTimeout = 40 * time.Minute
 	}
