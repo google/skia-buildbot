90b864b1ea7ed1211990c69e17c4c1a6c6727bf9
Joe Gregorio <jcgregorio@google.com>
2018-05-16 15:48:55 -0400

    Port over the hosted debugger to skia-public.
    Bug: skia:
    Change-Id: If97274215a77a836f3b05752c695c71c2b2f25a4

--- debugger/go/runner/runner_test.go
+++ debugger/go/runner/runner_test.go
@@ -9,10 +9,6 @@ import (
 	"go.skia.org/infra/go/testutils"
 )
 
-func mockGetCurrentHash() string {
-	return "aabbccdd"
-}
-
 // execString is the command line that would have been run through exec.
 var execString string
 
@@ -26,20 +22,9 @@ func TestRunContainer(t *testing.T) {
 	testutils.SmallTest(t)
 	// Now test local runs, first set up exec for testing.
 	ctx := exec.NewContext(context.Background(), testRun)
-	runner := New("/mnt/pd0/debugger", "/mnt/pd0/container", mockGetCurrentHash, false)
+	runner := New()
 	err := runner.Start(ctx, 20003)
 	assert.NoError(t, err)
 	assert.NotNil(t, runner)
-	assert.Equal(t, "sudo systemd-nspawn -D /mnt/pd0/container --read-only --machine debug20003 --bind-ro /mnt/pd0/debugger xargs --arg-file=/dev/null /mnt/pd0/debugger/versions/aabbccdd/skia/out/Release/skiaserve --port 20003 --hosted", execString)
-}
-
-func TestRunLocal(t *testing.T) {
-	testutils.SmallTest(t)
-	// Now test local runs, first set up exec for testing.
-	ctx := exec.NewContext(context.Background(), testRun)
-	runner := New("/mnt/pd0/debugger", "/mnt/pd0/container", mockGetCurrentHash, true)
-	err := runner.Start(ctx, 20003)
-	assert.NoError(t, err)
-	assert.NotNil(t, runner)
-	assert.Equal(t, "/mnt/pd0/debugger/versions/aabbccdd/skia/out/Release/skiaserve --port 20003 --source  --hosted", execString)
+	assert.Equal(t, "/bin/skiaserve --port 20003 --source  --hosted", execString)
 }
