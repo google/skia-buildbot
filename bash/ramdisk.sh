b4bc76b9a5a517c0a8353b7aff6554b537150491
Joe Gregorio <jcgregorio@google.com>
2018-05-18 16:43:57 -0400

    [notifier] Make editing the chat config a single flow.
    Bug: skia:
    Change-Id: I764f831032695970c89cb100282119a932d949e5
    Reviewed-on: https://skia-review.googlesource.com/129242
    Commit-Queue: Joe Gregorio <jcgregorio@google.com>
    Reviewed-by: Ben Wagner <benjaminwagner@google.com>
    Auto-Submit: Joe Gregorio <jcgregorio@google.com>

--- bash/ramdisk.sh
+++ bash/ramdisk.sh
@@ -1,14 +1,14 @@
 # Utilities for creating a service account key and securely moving into a
 # Kubernetes secret.
 
-set -x -e
+set -e
 
 mkdir /tmp/ramdisk
 echo "sudo is needed to create the ramdisk."
 sudo mount  -t tmpfs -o size=10m tmpfs /tmp/ramdisk
 
 function finish {
-  sleep 1
+  sleep 2
   sudo umount /tmp/ramdisk
   rmdir /tmp/ramdisk
 }
--- notifier/get-chat-config.sh
+++
@@ -1,7 +0,0 @@
-#/bin/bash
-
-# Downloads the secret chat_config.txt file for editing, i.e. to add, remove,
-# or update new chatroom targets.
-#
-# Once you are done editing the file, upload it with 'put-chat-config.sh'.
-kubectl get secret notifier-chat-config -o json | jq -r '.data["chat_config.txt"]' | base64 -d > chat_config.txt
