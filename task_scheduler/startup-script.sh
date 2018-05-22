2d500c5248a0909ca66086aa121dc90c559f3c1f
Ben Wagner <benjaminwagner@google.com>
2018-05-22 16:54:52 -0400

    [task scheduler] Set git config in startup script.
    Change-Id: I6fd5c7ea8c7e9d6130c5de92f4f783b26357b3b8

--- task_scheduler/startup-script.sh
+++ task_scheduler/startup-script.sh
@@ -2,3 +2,10 @@
 
 # Create tmp dir if needed.
 sudo -u default mkdir -p /mnt/pd0/tmp
+
+# These must be set, otherwise bot_update will temporarily set them and then
+# unset them after it has finished, which fails because we run bot_update
+# concurrently. See also
+# https://chromium-review.googlesource.com/c/chromium/tools/depot_tools/+/1036309
+sudo -u default git config --global user.email chrome-bot@chromium.org
+sudo -u default git config --global user.name chrome-bot
