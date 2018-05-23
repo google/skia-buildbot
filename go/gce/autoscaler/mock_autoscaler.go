cadcd31e060e2ecea2803b3e1262087474e99e9b
Ravi Mistry <rmistry@google.com>
2018-05-23 14:17:42 -0400

    Add GetNamesOfManagedInstances to autoscaler.go
    Bug: skia:7900
    Change-Id: Ia7e05a98e9c9331ea95ecd9454c9427ba1dbffa2

--- go/gce/autoscaler/mock_autoscaler.go
+++ go/gce/autoscaler/mock_autoscaler.go
@@ -1,12 +1,20 @@
 package autoscaler
 
+var (
+	TestInstances = []string{"ct-gce-001", "ct-gce-002"}
+)
+
 type MockAutoscaler struct {
 	StopAllInstancesTimesCalled  int
 	StartAllInstancesTimesCalled int
 }
 
 func (m *MockAutoscaler) GetRunningInstances() ([]string, error) {
-	return []string{"ct-gce-001", "ct-gce-002"}, nil
+	return TestInstances, nil
+}
+
+func (m *MockAutoscaler) GetNamesOfManagedInstances() []string {
+	return TestInstances
 }
 
 func (m *MockAutoscaler) StopAllInstances() error {
