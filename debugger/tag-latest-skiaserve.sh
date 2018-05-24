502c90b8b6084ec1f37469d477527f988f92ac75
Joe Gregorio <jcgregorio@google.com>
2018-05-24 09:13:58 -0400

    Bring all Dockerfiles into alignment.
    Put apps in /usr/local/bin.
    The push: Makefile target always runs with --apply.
    Always use COPY if possible.
    Always use --no-cache on apk install.
    
    Bug: skia:
    Change-Id: If137ee10e91afd8c23d89c7e3a3147d1e4749726

--- debugger/tag-latest-skiaserve.sh
+++ debugger/tag-latest-skiaserve.sh
@@ -4,4 +4,4 @@
 set -x -e
 
 DIGEST=`gcloud container images list-tags gcr.io/skia-public/skiaserve --limit=1 --format=json | jq -r '.[0].digest'`
-gcloud container images add-tag gcr.io/skia-public/skiaserve@${DIGEST} gcr.io/skia-public/skiaserve:latest
+gcloud container images add-tag gcr.io/skia-public/skiaserve@${DIGEST} gcr.io/skia-public/skiaserve:prod
