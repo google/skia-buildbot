246e9675503847d4eba6f53837c69a182834ff89
Joe Gregorio <jcgregorio@google.com>
2018-05-26 21:26:40 -0400

    [jsdoc] Fix missing renames from common to common-sk.
    Bug: skia:
    Change-Id: I13fcd4ef4436f9f6a7e1d5c09d9cb697b32cd11d
    Reviewed-on: https://skia-review.googlesource.com/130327
    Reviewed-by: Joe Gregorio <jcgregorio@google.com>

--- jsdoc/jsdoc.config.js
+++ jsdoc/jsdoc.config.js
@@ -1,3 +1,3 @@
-let config = require('../common/jsdoc.common.js')
+let config = require('../common-sk/jsdoc.common.js')
 config.source.include = ['../ap/elements-sk', '../common-sk/modules', './jsdoc/README.md'];
 module.exports = config;
