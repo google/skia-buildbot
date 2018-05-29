57dc1a2792de85eab006e0b5c6959d28711edc17
Ravi Mistry <rmistry@google.com>
2018-05-29 11:29:26 -0400

    CT Datastore experiment
    Bug: skia:
    Change-Id: Ie07b752a1aa8c978018a7cdf9ed3e881cb5cab01

--- ct/go/ctfe/metrics_analysis/metrics_analysis.go
+++ ct/go/ctfe/metrics_analysis/metrics_analysis.go
@@ -12,12 +12,14 @@ import (
 	"strconv"
 	"text/template"
 
+	"cloud.google.com/go/datastore"
 	"github.com/gorilla/mux"
 
 	"go.skia.org/infra/ct/go/ctfe/task_common"
 	ctfeutil "go.skia.org/infra/ct/go/ctfe/util"
 	"go.skia.org/infra/ct/go/db"
 	ctutil "go.skia.org/infra/ct/go/util"
+	"go.skia.org/infra/go/ds"
 	"go.skia.org/infra/go/httputils"
 )
 
@@ -95,10 +97,15 @@ func (task DBTask) TableName() string {
 	return db.TABLE_METRICS_ANALYSIS_TASKS
 }
 
-func (task DBTask) Select(query string, args ...interface{}) (interface{}, error) {
-	result := []DBTask{}
-	err := db.DB.Select(&result, query, args...)
-	return result, err
+func (task DBTask) GetDatastoreKind() ds.Kind {
+	return ds.METRICS_ANALYSIS_TASKS
+}
+
+func (task DBTask) Select(it *datastore.Iterator) (interface{}, error) {
+	return nil, nil
+	//result := []DBTask{}
+	//err := db.DB.Select(&result, query, args...)
+	//return result, err
 }
 
 func addTaskView(w http.ResponseWriter, r *http.Request) {
@@ -116,6 +123,10 @@ type AddTaskVars struct {
 	Description        string `json:"desc"`
 	ChromiumPatch      string `json:"chromium_patch"`
 	CatapultPatch      string `json:"catapult_patch"`
+}
+
+func (task *AddTaskVars) GetPopulatedDatastoreTask() (task_common.Task, error) {
+	return nil, nil
 }
 
 func (task *AddTaskVars) GetInsertQueryAndBinds() (string, []interface{}, error) {
