e06c830bc832bb14c4bc325fd0dd37256c3b6d93
smut <smut@google.com>
2018-06-06 14:37:57 -0700

    [Isolate] Update clients to hit /_ah/api instead of /api
    The webapp2 adapter is now serving at /_ah/api, the old Endpoints v1 path.
    
    https://chromium.googlesource.com/infra/infra revision:
    d8ec646a9503426a5018cf5bc251abe4a842357f
    
    https://chromium.googlesource.com/infra/luci/luci-go revision:
    928030a644473add1ca1bd044d13f48fee569c1a
    
    ISOLATE_EXE_SHA1:
    https://ci.chromium.org/buildbot/chromium.infra/infra-continuous-win-64/12934
    
    Bug: 805543
    Change-Id: I352ab8b0dd76d699b2698a80424e69a14b21f334

--- go/isolate/isolate.go
+++ go/isolate/isolate.go
@@ -23,7 +23,7 @@ import (
 
 const (
 	DEFAULT_NAMESPACE          = "default-gzip"
-	ISOLATE_EXE_SHA1           = "b1c3c39fe8fe084bd2ec4ed0f03037751a3a8650"
+	ISOLATE_EXE_SHA1           = "af227603890ea1d8c082b5caf15e46a6bf060a2e"
 	ISOLATESERVER_EXE_SHA1     = "23dfc04c65f2ddf2f8dc8dce0fcdcf57ed6b98a3"
 	ISOLATE_SERVER_URL         = "https://isolateserver.appspot.com"
 	ISOLATE_SERVER_URL_FAKE    = "fake"
