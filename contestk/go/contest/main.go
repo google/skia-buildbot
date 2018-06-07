e8fa980f9c9b941f3940ecd5d136c8d8a98b325f
Kevin Lubick <kjlubick@google.com>
2018-06-07 12:46:03 -0400

    Remove support for chrome tab, fix ie=egde typo
    Bug: skia:
    Change-Id: Ie125d2ad921a924407a98476fcebf2da5eee48c7
    Reviewed-on: https://skia-review.googlesource.com/132921
    Reviewed-by: Joe Gregorio <jcgregorio@google.com>
    Commit-Queue: Kevin Lubick <kjlubick@google.com>

--- contestk/go/contest/main.go
+++ contestk/go/contest/main.go
@@ -26,7 +26,7 @@ const (
 <head>
     <title>Skia Drawing Contest</title>
     <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=egde,chrome=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<style>
 		  body {
