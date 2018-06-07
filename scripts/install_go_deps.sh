cff5f9cca010b3401f5dcc877a2bb09e0c455a3c
Stephan Altmueller <stephana@google.com>
2018-06-07 09:40:41 -0400

    Add script to install Go dependencies
    The new script is also called from recipes
    
    Bug: skia:
    Change-Id: I39f09bde811090a973a032add4825aac863a5f42
    Reviewed-on: https://skia-review.googlesource.com/129860
    Reviewed-by: Eric Boren <borenet@google.com>
    Commit-Queue: Stephan Altmueller <stephana@google.com>

---
+++ scripts/install_go_deps.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Install the necessary dependencies
+set -e -x
+
+go get -u github.com/kisielk/errcheck \
+          golang.org/x/tools/cmd/goimports \
+          go.chromium.org/luci/client/cmd/isolate
+
+go get -u github.com/golang/protobuf/protoc-gen-go \
+          golang.org/x/tools/cmd/stringer
+
