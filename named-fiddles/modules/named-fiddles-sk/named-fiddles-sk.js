e2ae477e6d4b194e03c661585ba784c3d3be5e19
Joe Gregorio <jcgregorio@google.com>
2018-06-14 08:19:02 -0400

    [named-fiddles] A separate protected app to manage named fiddles.
    Bug: skia:
    Change-Id: Ifcf8df1f58b4195b4e88975ea445f97bdae02970

---
+++ named-fiddles/modules/named-fiddles-sk/named-fiddles-sk.js
@@ -0,0 +1,62 @@
+/**
+ * @module /named-fiddles-sk
+ * @description <h2><code>named-fiddles-sk</code></h2>
+ *
+ * @evt
+ *
+ * @attr
+ *
+ * @example
+ */
+import { html, render } from 'lit-html/lib/lit-extended'
+import { repeat } from 'lit-html/lib/repeat';
+import '../named-fiddle-sk'
+import 'elements-sk/buttons'
+import 'common-sk/modules/error-toast-sk'
+import { jsonOrThrow } from 'common-sk/modules/jsonOrThrow'
+import { errorMessage } from 'common-sk/modules/errorMessage'
+
+const template = (ele) => html`
+<header>
+  <h1>Named Fiddles</h1>
+</header>
+<main>
+  <section>
+   ${repeat(ele._named_fiddles, (i) => i.name, (i, index) => html`<named-fiddle-sk state=${i}></named-fiddle-sk>`)}
+  </section>
+  <button class=fab>+</button>
+</main>
+<footer>
+  <error-toast-sk></error-toast-sk>
+<footer>
+`;
+
+window.customElements.define('named-fiddles-sk', class extends HTMLElement {
+  constructor() {
+    super();
+    this._named_fiddles = [];
+  }
+
+  connectedCallback() {
+    fetch('/_/named').then(jsonOrThrow).then((json) => {
+      this._named_fiddles = json;
+      this._render();
+    }).catch(errorMessage);
+    this._render();
+    this.addEventListener('named-delete', (e) => this._delete(e));
+    this.addEventListener('named-edit', (e) => this._edit(e));
+  }
+
+  _delete(e) {
+    console.log(e.detail);
+  }
+
+  _edit(e) {
+    console.log(e.detail);
+  }
+
+  _render() {
+    render(template(this), this);
+  }
+
+});
