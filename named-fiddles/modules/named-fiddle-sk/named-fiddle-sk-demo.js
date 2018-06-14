17b19af8dee46361ac7e26df1323a5d4a1126d73
Joe Gregorio <jcgregorio@google.com>
2018-06-14 09:49:24 -0400

    [named-fiddles] A separate protected app to manage named fiddles.
    Bug: skia:
    Change-Id: Ifcf8df1f58b4195b4e88975ea445f97bdae02970

---
+++ named-fiddles/modules/named-fiddle-sk/named-fiddle-sk-demo.js
@@ -0,0 +1,23 @@
+import './index.js'
+import { $$ } from 'common-sk/modules/dom'
+
+(function () {
+  $$('#ok').state = {
+    name: 'Octopus_Generator',
+    hash: 'ad161cfe21bb38bcec264bbacecbe93a',
+    status:  'OK',
+  };
+  $$('#failed').state = {
+    name: 'Octopus_Generator_Animated',
+    hash: 'f8c5a15bb959b455eb98d680f0a848c8',
+    status:  'Failed',
+  };
+
+  function display(e) {
+    $$('#events').textContent = `Event: ${e.type}\nDetail: ${JSON.stringify(e.detail, null, '  ')}`;
+  }
+
+  document.addEventListener('named-edit', display);
+  document.addEventListener('named-delete', display);
+
+})();
