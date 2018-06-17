cde5820e6bc897831d3d1ed821c5f5fa0f15522b
Ben Wagner <benjaminwagner@google.com>
2018-06-17 16:12:01 -0400

    Reland "Add machine_type dimension for Infra tasks."
    This reverts commit a9aa18d7685d410ea272110d24ff11b85bbbe6df.
    
    Reason for revert: Relanding
    
    Original change's description:
    > Revert "Add machine_type dimension for Infra tasks."
    >
    > This reverts commit 4b0e5158bd490eee0919c5c56a975ae9d7f0b264.
    >
    > Reason for revert: IO bandwidth is too small for this many cores.
    >
    > Original change's description:
    > > Add machine_type dimension for Infra tasks.
    > >
    > > This is a companion to
    > > https://skia-review.googlesource.com/c/skia/+/124340
    > >
    > > Bug: skia:7415
    > > Change-Id: Id6dc74bdd680a4f9f06caecb88c2374bf5a1b5b6
    > > Reviewed-on: https://skia-review.googlesource.com/124348
    > > Commit-Queue: Eric Boren <borenet@google.com>
    > > Auto-Submit: Ben Wagner <benjaminwagner@google.com>
    > > Reviewed-by: Eric Boren <borenet@google.com>
    >
    > TBR=borenet@google.com,benjaminwagner@google.com
    >
    > Change-Id: If3d7ee8e89d7da6b97cd2d74f5bd551f7d61ff3b
    > No-Presubmit: true
    > No-Tree-Checks: true
    > No-Try: true
    > Bug: skia:7415
    > Reviewed-on: https://skia-review.googlesource.com/125780
    > Reviewed-by: Ben Wagner <benjaminwagner@google.com>
    > Commit-Queue: Ben Wagner <benjaminwagner@google.com>
    
    Change-Id: I49821850728d83cdcbe06c43830f3f661340be32
    Bug: skia:7415

--- infra/bots/gen_tasks.go
+++ infra/bots/gen_tasks.go
@@ -24,6 +24,13 @@ const (
 	DEFAULT_OS       = DEFAULT_OS_LINUX
 	DEFAULT_OS_LINUX = "Debian-9.4"
 
+	// Small is a 2-core machine.
+	MACHINE_TYPE_SMALL = "n1-highmem-2"
+	// Medium is a 16-core machine
+	MACHINE_TYPE_MEDIUM = "n1-standard-16"
+	// Large is a 64-core machine.
+	MACHINE_TYPE_LARGE = "n1-highcpu-64"
+
 	// Swarming output dirs.
 	OUTPUT_NONE = "output_ignored" // This will result in outputs not being isolated.
 
@@ -106,13 +113,13 @@ var (
 )
 
 // Dimensions for Linux GCE instances.
-func linuxGceDimensions() []string {
+func linuxGceDimensions(machineType string) []string {
 	return []string{
 		"pool:Skia",
 		fmt.Sprintf("os:%s", DEFAULT_OS_LINUX),
 		"gpu:none",
 		"cpu:x86-64-Haswell_GCE",
-		"machine_type:n1-standard-16",
+		fmt.Sprintf("machine_type:%s", machineType),
 	}
 }
 
@@ -151,7 +158,7 @@ func bundleRecipes(b *specs.TasksCfgBuilder) string {
 		Command: []string{
 			"/bin/bash", "buildbot/infra/bots/bundle_recipes.sh", specs.PLACEHOLDER_ISOLATED_OUTDIR,
 		},
-		Dimensions: linuxGceDimensions(),
+		Dimensions: linuxGceDimensions(MACHINE_TYPE_SMALL),
 		EnvPrefixes: map[string][]string{
 			"PATH": []string{"cipd_bin_packages", "cipd_bin_packages/bin"},
 		},
@@ -233,7 +240,12 @@ func kitchenTask(name, recipe, isolate, serviceAccount string, dimensions []stri
 // infra generates an infra test Task. Returns the name of the last Task in the
 // generated chain of Tasks, which the Job should add as a dependency.
 func infra(b *specs.TasksCfgBuilder, name string) string {
-	task := kitchenTask(name, "swarm_infra", "infrabots.isolate", SERVICE_ACCOUNT_COMPILE, linuxGceDimensions(), nil, OUTPUT_NONE)
+	machineType := MACHINE_TYPE_MEDIUM
+	if strings.Contains(name, "Large") {
+		// Using MACHINE_TYPE_LARGE for Large tests saves ~2 minutes.
+		machineType = MACHINE_TYPE_LARGE
+	}
+	task := kitchenTask(name, "swarm_infra", "infrabots.isolate", SERVICE_ACCOUNT_COMPILE, linuxGceDimensions(machineType), nil, OUTPUT_NONE)
 	task.CipdPackages = append(task.CipdPackages, CIPD_PKGS_GIT...)
 	task.CipdPackages = append(task.CipdPackages, b.MustGetCipdPackageFromAsset("go"))
 	task.CipdPackages = append(task.CipdPackages, b.MustGetCipdPackageFromAsset("node"))
@@ -267,7 +279,7 @@ func presubmit(b *specs.TasksCfgBuilder, name string) string {
 		"reason":           "CQ",
 		"repo_name":        "skia_buildbot",
 	}
-	task := kitchenTask(name, "run_presubmit", "empty.isolate", SERVICE_ACCOUNT_COMPILE, linuxGceDimensions(), extraProps, OUTPUT_NONE)
+	task := kitchenTask(name, "run_presubmit", "empty.isolate", SERVICE_ACCOUNT_COMPILE, linuxGceDimensions(MACHINE_TYPE_MEDIUM), extraProps, OUTPUT_NONE)
 
 	replaceArg := func(key, value string) {
 		found := false
