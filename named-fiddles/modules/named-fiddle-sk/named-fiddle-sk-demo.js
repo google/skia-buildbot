1c53cb81f16c8bee887fb820c00df3a3e9d53549
Joe Gregorio <jcgregorio@google.com>
2018-06-20 15:47:15 -0400

    [named-fiddls] Show owner of named fiddle.
    Bug: skia:
    Change-Id: I145bd235b92b39957eaa4d2e9cb455eb8cc248fc

--- named-fiddles/modules/named-fiddle-sk/named-fiddle-sk-demo.js
+++ named-fiddles/modules/named-fiddle-sk/named-fiddle-sk-demo.js
@@ -6,11 +6,13 @@ import { $$ } from 'common-sk/modules/dom'
     name: 'Octopus_Generator',
     hash: 'ad161cfe21bb38bcec264bbacecbe93a',
     status:  'OK',
+    user: 'fred@example.com',
   };
   $$('#failed').state = {
     name: 'Octopus_Generator_Animated',
     hash: 'f8c5a15bb959b455eb98d680f0a848c8',
     status:  'Failed',
+    user: 'barney@example.org',
   };
 
   function display(e) {
