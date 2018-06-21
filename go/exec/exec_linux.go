814d6cd692338220ee1339df5d219f40e2dcd018
Joe Gregorio <jcgregorio@google.com>
2018-06-21 09:43:01 -0400

    [fiddlecli] exec.go that builds on windows.
    Bug: skia:
    Change-Id: Ic75f7de33f6ed49768f5ec1136f2a68e3f86e740

---
+++ go/exec/exec_linux.go
@@ -0,0 +1,20 @@
+package exec
+
+import (
+	"context"
+	"syscall"
+)
+
+// NoInterruptContext returns a context.Context instance which launches
+// subprocesses in a difference process group so that they are not killed when
+// this process is killed.
+func NoInterruptContext(ctx context.Context) context.Context {
+	parent := getCtx(ctx)
+	runFn := func(c *Command) error {
+		c.SysProcAttr = &syscall.SysProcAttr{
+			Setpgid: true,
+		}
+		return parent.runFn(c)
+	}
+	return NewContext(ctx, runFn)
+}
