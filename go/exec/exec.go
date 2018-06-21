0f84eda2b465d43f40d383ae72119005d271f405
Joe Gregorio <jcgregorio@google.com>
2018-06-21 09:56:12 -0400

    [fiddlecli] exec.go that builds on windows.
    Bug: skia:
    Change-Id: Ic75f7de33f6ed49768f5ec1136f2a68e3f86e740
    Reviewed-on: https://skia-review.googlesource.com/136582
    Reviewed-by: Eric Boren <borenet@google.com>
    Commit-Queue: Joe Gregorio <jcgregorio@google.com>

--- go/exec/exec.go
+++ go/exec/exec.go
@@ -295,20 +295,6 @@ func NewContext(ctx context.Context, runFn func(*Command) error) context.Context
 	return context.WithValue(ctx, contextKey, newCtx)
 }
 
-// NoInterruptContext returns a context.Context instance which launches
-// subprocesses in a difference process group so that they are not killed when
-// this process is killed.
-func NoInterruptContext(ctx context.Context) context.Context {
-	parent := getCtx(ctx)
-	runFn := func(c *Command) error {
-		c.SysProcAttr = &syscall.SysProcAttr{
-			Setpgid: true,
-		}
-		return parent.runFn(c)
-	}
-	return NewContext(ctx, runFn)
-}
-
 // getCtx retrieves the Context associated with the context.Context.
 func getCtx(ctx context.Context) *execContext {
 	if v := ctx.Value(contextKey); v != nil {
