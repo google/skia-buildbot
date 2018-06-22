32e5f436f7a11714c06fbcf05d9eb14e1b7f20b9
Joe Gregorio <jcgregorio@google.com>
2018-06-22 14:55:41 -0400

    Continuously deploy fiddlers as they get built.
    Bug: skia:
    Change-Id: I0056d440ff2d22b5ee9895177f9c572d38c33f39

---
+++ kube/create-continuous-deploy-sa.sh
@@ -0,0 +1,21 @@
+#/bin/bash
+
+# Create the service account that has access to the skia-public-config repo
+# and export a key for it into the kubernetes cluster as a secret.
+
+set -e -x
+source ./config.sh
+source ../bash/ramdisk.sh
+
+# New service account we will create.
+SA_NAME=continuous-deploy
+
+cd /tmp/ramdisk
+
+gcloud iam service-accounts create "${SA_NAME}" --display-name="Read-write access to the skia-public-config repo."
+
+gcloud beta iam service-accounts keys create ${SA_NAME}.json --iam-account="${SA_NAME}@${PROJECT_ID}.iam.gserviceaccount.com"
+
+kubectl create secret generic "${SA_NAME}" --from-file=key.json=${SA_NAME}.json
+
+cd -
