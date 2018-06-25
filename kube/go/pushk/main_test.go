f8534bde2c111b55c781d99fbc5d8ad9d1ad7963
Joe Gregorio <jcgregorio@google.com>
2018-06-25 13:44:56 -0400

    [pushk] Add ability to push a specific tag of an image.
    Bug: skia:
    Change-Id: Ic7be1dc91d211ed5ac658ea7b86efdc9196cb304

--- kube/go/pushk/main_test.go
+++ kube/go/pushk/main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"sort"
 	"testing"
 
@@ -96,5 +97,57 @@ func TestFilter(t *testing.T) {
 		wantError := tc.hasError
 		assert.Equal(t, wantError, gotError, "hasError: "+tc.message)
 		assert.Equal(t, want, got, tc.message)
+	}
+}
+
+func TestImageFromCmdLineImage(t *testing.T) {
+	testutils.SmallTest(t)
+	testCases := []struct {
+		value         string
+		provider      tagProvider
+		expected      string
+		expectedError bool
+		message       string
+	}{
+		{
+			value:         "gcr.io/skia-public/fiddle:2018-...",
+			provider:      nil,
+			expected:      "gcr.io/skia-public/fiddle:2018-...",
+			expectedError: false,
+			message:       "already full name",
+		},
+		{
+			value: "fiddle",
+			provider: func(name string) ([]string, error) {
+				return []string{
+					"2018-04-20T21_14_00Z-jcgregorio-f40851bf4611a844bb63b289e91cddc6eba886ae-dirty",
+					"foo",
+					"2018-04-20T21_21_48Z-jcgregorio-f40851bf4611a844bb63b289e91cddc6eba886ae-dirty",
+				}, nil
+			},
+			expectedError: false,
+			expected:      "gcr.io/skia-public/fiddle:2018-04-20T21_21_48Z-jcgregorio-f40851bf4611a844bb63b289e91cddc6eba886ae-dirty",
+			message:       "tag provider",
+		},
+		{
+			value: "fiddle",
+			provider: func(name string) ([]string, error) {
+				return []string{}, fmt.Errorf("Failed to find any tags.")
+			},
+			expected:      "",
+			expectedError: true,
+			message:       "tag provider",
+		},
+	}
+
+	for _, tc := range testCases {
+		got, err := imageFromCmdLineImage(tc.value, tc.provider)
+		if (err != nil) != tc.expectedError {
+			t.Errorf("Error condition unexpected: %s, Error expected: %v, %s", err, tc.expectedError, tc.message)
+
+		}
+		if want := tc.expected; got != want {
+			t.Errorf("Failed case Got %v Want %v: %s", got, want, tc.message)
+		}
 	}
 }
