79d5e465c896656605857a27d99b076a2f6482f8
Joe Gregorio <jcgregorio@google.com>
2018-06-25 16:56:53 -0400

    [predict] Turn down.
    Bug: skia:
    Change-Id: Iaabd48e56e6092bf5fed29590e9d188c44fc0538
    Reviewed-on: https://skia-review.googlesource.com/137423
    Reviewed-by: Eric Boren <borenet@google.com>
    Commit-Queue: Joe Gregorio <jcgregorio@google.com>

--- predict/task_scheduler_tunnel.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Sets up an SSH port forwarding from localhost:8008 to skia-task-scheduler:8008,
-# which is where the task scheduler remote db endpoint is running.
-gcloud compute ssh default@skia-task-scheduler --zone=us-central1-c --ssh-flag="-L 8008:localhost:8008"
