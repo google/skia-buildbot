87a0e3d0d6de1863af0c342ba903e0bc0a82bf5c
Joe Gregorio <jcgregorio@google.com>
2018-06-26 16:15:10 -0400

    [android_ingest] Add a transaction log of all incoming POST requests.
    Bug: skia: 8111
    Change-Id: If37665f342663a068412385b8d5ff54bdf59245b

--- android_ingest/go/upload/upload.go
+++ android_ingest/go/upload/upload.go
@@ -27,3 +27,15 @@ func ObjectPath(benchData *ingestcommon.BenchData, gcsPath string, now time.Time
 	filename := fmt.Sprintf("%s_%s_%s.json", benchData.Hash, strings.Join(keyparts, "_"), hash)
 	return filepath.Join(gcsPath, now.Format("2006/01/02/15/"), filename)
 }
+
+// LogPath returns the Google Cloud Storage path where the raw POST data
+// should be stored.
+//
+// gcsPath will be the root of the path.
+// now is the time which will be encoded in the path.
+// b is the source bytes of the incoming file.
+func LogPath(gcsPath string, now time.Time, b []byte) string {
+	hash := fmt.Sprintf("%x", md5.Sum(b))
+	filename := fmt.Sprintf("%s.json", hash)
+	return filepath.Join(gcsPath, now.Format("2006/01/02/15/"), filename)
+}
