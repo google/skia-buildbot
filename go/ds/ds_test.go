ea36ce19e49bded1b4893f55195588740b1073b9
Stephan Altmueller <stephana@google.com>
2018-06-27 13:03:24 -0400

    Adds a generic tool to write maintenance functions for datastore
    - Adds a simple command line tool that allows to connect commands
      from the command line to functions. These are intended to make
      sweeping changes across a namespace.
    
    - Adds the 'delete' and 'touch' function
    
    Bug: skia:
    Change-Id: Iadf74e807f78592e3878b44b5f7461ba27cb181e
    Reviewed-on: https://skia-review.googlesource.com/135577
    Reviewed-by: Ben Wagner <benjaminwagner@google.com>
    Reviewed-by: Kevin Lubick <kjlubick@google.com>
    Commit-Queue: Stephan Altmueller <stephana@google.com>

--- go/ds/ds_test.go
+++ go/ds/ds_test.go
@@ -16,6 +16,7 @@ import (
 const TEST_KIND = Kind("DS_TEST_KIND")
 
 type testEntity struct {
+	Key      *datastore.Key `datastore:"__key__"`
 	Random   int64
 	Sortable int64
 }
@@ -28,7 +29,10 @@ func TestDeleteAll(t *testing.T) {
 
 	nEntries := 1200
 	maxID := int64(nEntries * 10)
-	addRandEntities(t, client, nEntries, maxID)
+
+	// Ignore the cleanup-call returned by addRandEntities since we are
+	// calling DeleteAll in this function anyway.
+	_, _ = addRandEntities(t, client, nEntries, maxID)
 	_, err := DeleteAll(client, TEST_KIND, true)
 	assert.NoError(t, err)
 
@@ -37,9 +41,40 @@ func TestDeleteAll(t *testing.T) {
 	assert.Equal(t, 0, count)
 }
 
-func addRandEntities(t *testing.T, client *datastore.Client, nEntries int, maxID int64) {
+func TestIterKeys(t *testing.T) {
+	testutils.LargeTest(t)
+
+	nEntries := 1200
+	maxID := int64(nEntries / 2)
+	assert.NoError(t, InitForTesting("test-project", "test-namespace"))
+	client := DS
+	exp, cleanup := addRandEntities(t, client, nEntries, maxID)
+	defer cleanup()
+
+	// Iterate over the type and collect the instances
+	iterCh, err := IterKeys(client, TEST_KIND, 10)
+	assert.NoError(t, err)
+	ctx := context.TODO()
+	var found []*testEntity
+
+	for item := range iterCh {
+		assert.NoError(t, item.Err)
+		keySlice := item.Keys
+		target := make([]*testEntity, len(keySlice))
+		assert.NoError(t, client.GetMulti(ctx, keySlice, target))
+		found = append(found, target...)
+	}
+	assert.Equal(t, exp, found)
+}
+
+func addRandEntities(t *testing.T, client *datastore.Client, nEntries int, maxID int64) ([]*testEntity, func()) {
 	_, err := DeleteAll(client, TEST_KIND, true)
 	assert.NoError(t, err)
+
+	cleanup := func() {
+		_, err := DeleteAll(client, TEST_KIND, true)
+		assert.NoError(t, err)
+	}
 
 	// Create a test type and fill it with random values
 	exp := make([]*testEntity, 0, nEntries)
@@ -49,18 +84,15 @@ func addRandEntities(t *testing.T, client *datastore.Client, nEntries int, maxID
 			Random:   rand.Int63(),
 			Sortable: (rand.Int63() % maxID) + 1,
 		}
-		exp = append(exp, newEntry)
-		_, err := client.Put(ctx, NewKey(TEST_KIND), newEntry)
+		newEntry.Key, err = client.Put(ctx, NewKey(TEST_KIND), newEntry)
 		assert.NoError(t, err)
+		exp = append(exp, newEntry)
 	}
 
-	sort.Slice(exp, func(i, j int) bool {
-		return ((exp[i].Sortable < exp[j].Sortable) ||
-			(exp[i].Sortable == exp[j].Sortable) && (exp[i].Random < exp[j].Random))
-	})
-
+	sort.Slice(exp, func(i, j int) bool { return exp[i].Key.ID < exp[j].Key.ID })
 	wait(t, client, TEST_KIND, nEntries)
 	assert.Equal(t, nEntries, len(exp))
+	return exp, cleanup
 }
 
 func wait(t *testing.T, client *datastore.Client, kind Kind, expectedCount int) {
