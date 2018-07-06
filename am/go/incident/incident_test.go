e366850b854947be51005d0c44ff696934faa893
Joe Gregorio <jcgregorio@google.com>
2018-07-06 10:04:08 -0400

    wip initial sketch of alert-manager.
    Bug: skia:
    Change-Id: I8140eeb2bad6912b8c4993f0bb1f6784eedeb9e5

---
+++ am/go/incident/incident_test.go
@@ -0,0 +1,77 @@
+package incident
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"go.skia.org/infra/go/alerts"
+	"go.skia.org/infra/go/ds"
+	"go.skia.org/infra/go/ds/testutil"
+	"go.skia.org/infra/go/testutils"
+)
+
+func TestAlertArrival(t *testing.T) {
+	testutils.LargeTest(t)
+
+	cleanup := testutil.InitDatastore(t, ds.INCIDENT_AM, ds.INCIDENT_ACTIVE_PARENT_AM)
+	defer cleanup()
+
+	st := NewStore(ds.DS, []string{"ignore"})
+
+	m := map[string]string{
+		"ignore":    "pod_123",
+		alerts.TYPE: alerts.TYPE_ALERTS,
+		"alertname": "BotUnemployed",
+		"bot":       "skia-rpi-064",
+		"category":  "infra",
+	}
+	a, err := st.AlertArrival(m)
+	assert.NoError(t, err)
+	assert.Equal(t, true, a.Active)
+	assert.NotEqual(t, "", a.Key)
+
+	// A second alert arrival with the same config doesn't create
+	// a new Incident.
+	a, err = st.AlertArrival(m)
+	assert.NoError(t, err)
+	assert.Equal(t, true, a.Active)
+	assert.NotEqual(t, "", a.Key)
+
+	all, err := st.GetAll()
+	assert.NoError(t, err)
+	assert.Equal(t, 1, len(all))
+
+	// But an alert arrival with a different config does create
+	// a new incident.
+	m["alertname"] = "BotMissing"
+	m[ID] = ""
+	b, err := st.AlertArrival(m)
+	assert.NoError(t, err)
+	assert.Equal(t, true, b.Active)
+	assert.NotEqual(t, "", b.Key)
+	assert.NotEqual(t, a.ID, b.ID)
+
+	// Add a note.
+	bb, err := st.AddNote(b.Key, Note{
+		Text:   "Stuff happened.",
+		Author: "fred@example.com",
+		TS:     time.Now().Unix(),
+	})
+	assert.NoError(t, err)
+	assert.Equal(t, "Stuff happened.", bb.Notes[0].Text)
+	assert.Equal(t, b.Key, bb.Key)
+
+	// Fail to add note, bad key.
+	_, err = st.AddNote("badkey", Note{})
+	assert.Error(t, err)
+
+	// Delete note, bad index.
+	_, err = st.DeleteNote(b.Key, 1)
+	assert.Error(t, err)
+
+	// Delete note.
+	b, err = st.DeleteNote(b.Key, 0)
+	assert.NoError(t, err)
+	assert.Equal(t, 0, len(b.Notes))
+}
