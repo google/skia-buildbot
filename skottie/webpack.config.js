8d38b630e5957d16ef3070c96b0cf36761f816d6
Joe Gregorio <jcgregorio@google.com>
2018-07-10 09:45:52 -0400

    A better fix for infra-sk
    Bug: skia:
    Change-Id: I86e10059221ddfeff3f458f5995f41081531432b

--- skottie/webpack.config.js
+++ skottie/webpack.config.js
@@ -14,5 +14,6 @@ module.exports = (env, argv) => {
   config.resolve = config.resolve || {};
   config.resolve.alias = config.resolve.alias || {};
   config.resolve.alias['infra-sk'] = resolve(__dirname, '../infra-sk/');
+  config.resolve.modules = [resolve(__dirname, 'node_modules'), 'node_modules'];
   return config;
 }
