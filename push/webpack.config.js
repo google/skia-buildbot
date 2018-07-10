c16d27ea2537c9ccc7cb631d2a78fddc69638b17
Joe Gregorio <jcgregorio@google.com>
2018-07-10 09:40:27 -0400

    A better fix for infra-sk
    Bug: skia:
    Change-Id: I86e10059221ddfeff3f458f5995f41081531432b

--- push/webpack.config.js
+++ push/webpack.config.js
@@ -3,5 +3,9 @@ const { resolve } = require('path')
 
 module.exports = (env, argv) => {
   let config = commonBuilder(env, argv, __dirname);
+  config.resolve = config.resolve || {};
+  config.resolve.alias = config.resolve.alias || {};
+  config.resolve.alias['infra-sk'] = resolve(__dirname, '../infra-sk/');
+  config.resolve.modules = [resolve(__dirname, 'node_modules'), 'node_modules'];
   return config
 }
