1bbdb0c636501a3447a011cba94bab3fb54f4524
Joe Gregorio <jcgregorio@google.com>
2018-07-10 07:13:32 -0400

    Fix infra-sk dependency
    Bug: skia:
    Change-Id: I3572b87b9ec8347be7a5dcf3a5c3bfbb644df42d

--- infra-sk/modules/app-sk/app-sk-demo.js
+++ infra-sk/modules/app-sk/app-sk-demo.js
@@ -2,6 +2,6 @@ import 'elements-sk/icon/create-icon-sk'
 
 import '../login-sk'
 import './index.js'
-import 'common-sk/modules/error-toast-sk'
+import 'elements-sk/error-toast-sk'
 
 import './app-sk_demo.scss'
