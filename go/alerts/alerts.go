848f551a30e802665b260de375f58394aa41dea4
Joe Gregorio <jcgregorio@google.com>
2018-07-12 16:13:07 -0400

    wip initial sketch of alert-manager.
    Bug: skia:
    Change-Id: I8140eeb2bad6912b8c4993f0bb1f6784eedeb9e5

--- go/alerts/alerts.go
+++ go/alerts/alerts.go
@@ -3,6 +3,18 @@ package alerts
 const (
 	// TOPIC is the PubSub topic for alert messages.
 	TOPIC = "promtheus-alerts"
+
+	// Well known keys and values for Metric.
+	TYPE         = "__name__" // The two valid values are ALERTS and HEALTHZ.
+	TYPE_ALERTS  = "ALERTS"
+	TYPE_HEALTHZ = "HEALTHZ"
+
+	STATE          = "__state__"
+	STATE_ACTIVE   = "active"
+	STATE_RESOLVED = "resolved"
+
+	// Where the alert came from, e.g. 'skia-public' or 'skolo'.
+	LOCATION = "location"
 )
 
 // Result contains all the key-value pairs for each alert, for example:
