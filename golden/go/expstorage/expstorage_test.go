2e710dce856ce37abbb780c6242244de187ca744
Stephan Altmüller <stephana@google.com>
2018-07-18 16:41:22 +0200

    wip: cleanup tryjobstore
    Bug: skia:
    Change-Id: I48ad10bbd036cce057a793168adda9eb4ba4e48f

--- golden/go/expstorage/expstorage_test.go
+++ golden/go/expstorage/expstorage_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"math/rand"
 	"sort"
+	"strconv"
 	"testing"
 
 	assert "github.com/stretchr/testify/require"
@@ -148,6 +149,11 @@ func testExpectationStore(t *testing.T, store ExpectationsStore, eventBus eventb
 	initialLogRecs, initialLogTotal, err := store.QueryLog(0, 100, true)
 	assert.NoError(t, err)
 	initialLogRecsLen := len(initialLogRecs)
+
+	// Request expectations and make sure they are emtpy.
+	emptyExp, err := store.Get()
+	assert.NoError(t, err)
+	assert.Equal(t, 0, len(emptyExp.Tests))
 
 	// If we have an event bus then keep gathering events.
 	callbackCh := make(chan []string, 3)
@@ -301,11 +307,11 @@ func testExpectationStore(t *testing.T, store ExpectationsStore, eventBus eventb
 	assert.Equal(t, 0, len(logEntries))
 
 	// Undo the latest version and make sure the corresponding record is correct.
-	changes, err := store.UndoChange(int64(lastRec.ID), "user-1")
+	changes, err := store.UndoChange(parseID(t, lastRec.ID), "user-1")
 	assert.NoError(t, err)
 	checkLogEntry(t, store, changes)
 
-	changes, err = store.UndoChange(int64(secondToLastRec.ID), "user-1")
+	changes, err = store.UndoChange(parseID(t, secondToLastRec.ID), "user-1")
 	assert.NoError(t, err)
 	checkLogEntry(t, store, changes)
 
@@ -331,7 +337,7 @@ func testExpectationStore(t *testing.T, store ExpectationsStore, eventBus eventb
 	logEntries, _, err = store.QueryLog(0, 1, false)
 	assert.NoError(t, err)
 	assert.Equal(t, 1, len(logEntries))
-	_, err = store.UndoChange(int64(logEntries[0].ID), "user-1")
+	_, err = store.UndoChange(parseID(t, logEntries[0].ID), "user-1")
 	assert.NotNil(t, err)
 
 	// Make sure getExpectationsAt works correctly.
@@ -349,6 +355,12 @@ func testExpectationStore(t *testing.T, store ExpectationsStore, eventBus eventb
 		checkExpectationsAt(t, sqlStore, secondAdd, "second")
 		checkExpectationsAt(t, sqlStore, secondUndo, "third")
 	}
+}
+
+func parseID(t *testing.T, idStr string) int64 {
+	ret, err := strconv.ParseInt(idStr, 10, 64)
+	assert.NoError(t, err)
+	return ret
 }
 
 func checkExpectationsAt(t *testing.T, sqlStore *SQLExpectationsStore, changeInfo *TriageLogEntry, name string) {
