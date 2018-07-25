59df7cc701900e17cff6b8a56d822e7fd6d534ca
Joe Gregorio <jcgregorio@google.com>
2018-07-25 00:23:01 -0400

    wip initial sketch of alert-manager.
    Bug: skia:
    Change-Id: I8140eeb2bad6912b8c4993f0bb1f6784eedeb9e5

---
+++ am/modules/paramset/paramset_test.js
@@ -0,0 +1,42 @@
+import { ParamSet } from './index.js'
+
+describe('ParamSet',
+  function() {
+    function testParamSet() {
+      let ps = new ParamSet();
+      // The empty paramset matches everything.
+      assert.isTrue(ps.match({}));
+      assert.isTrue(ps.match({"foo": "2", "bar": "a"}));
+
+      let p = {
+        "foo": "1",
+        "bar": "a",
+      };
+      ps.add(p);
+      assert.isTrue(ps.match(p));
+      assert.isFalse(ps.match({}));
+
+      ps.add({
+        "foo": "2",
+        "bar": "b",
+      });
+
+      ps.add({
+        "foo": "1",
+        "bar": "b",
+      });
+
+      assert.isTrue(ps.match( {"foo": "2", "bar": "a"}));
+      assert.isTrue(ps.match( {"foo": "2", "bar": "a", "baz": "other"}));
+      assert.isFalse(ps.match({            "bar": "a"}));
+      assert.isFalse(ps.match({"foo": "2"            }));
+      assert.isFalse(ps.match({                      }));
+      assert.isFalse(ps.match({"foo": "3", "bar": "a"}));
+      assert.isFalse(ps.match({"foo": "2", "bar": "c"}));
+    }
+
+    it('should be able get match against params', function() {
+      testParamSet();
+    });
+  }
+);
