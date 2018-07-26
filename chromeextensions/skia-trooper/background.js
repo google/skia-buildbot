72da809713fa98a42fe1adfcc30012a4d9211589
Ravi Mistry <rmistry@google.com>
2018-07-26 08:07:01 -0400

    Extensions Checkpoint
    Bug: skia:
    Change-Id: Ida1c66bdde97d15781fae1d339a5ea4f86517a7b

--- chromeextensions/skia-trooper/background.js
+++ chromeextensions/skia-trooper/background.js
@@ -2,53 +2,52 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-function updateIcon() {
-  var xmlHttp = new XMLHttpRequest();
-  xmlHttp.open("GET", "https://promalerts.skia.org/api/v1/alerts/groups", true);
-  xmlHttp.timeout = 5000;
-  xmlHttp.onload = function(e) {
-    if (xmlHttp.readyState === XMLHttpRequest.DONE && xmlHttp.status === 200) {
-      var numActiveInfraAlerts = 0;
+function updateIcon(data, appendToAlerts) {
+  let numActiveInfraAlerts = 0;
+  chrome.browserAction.getBadgeText({}, badgeText => {
+    if (appendToAlerts) {
+      numActiveInfraAlerts = Number(badgeText);
+    }
 
-      var resp = JSON.parse(xmlHttp.responseText);
-      var alertGroups = resp.data;
-      alertGroups.forEach(function(alertGroup) {
-        if (!alertGroup.blocks) {
-          return;
-        }
-        alertGroup.blocks.forEach(function(block) {
-          block.alerts.forEach(function(al) {
-            if (al.labels.category != "infra") {
-              return;
-            }
-            if (al.status.state === "suppressed") {
-              return;
-            }
-            numActiveInfraAlerts++;
-          });
+    const alertGroups = JSON.parse(data).data;
+    alertGroups.forEach(alertGroup => {
+     if (!alertGroup.blocks) {
+        return;
+      }
+      alertGroup.blocks.forEach(function(block) {
+        block.alerts.forEach(function(al) {
+          if (al.labels.category != "infra") {
+            return;
+          }
+          if (al.status.state === "suppressed") {
+            return;
+          }
+          numActiveInfraAlerts++;
         });
       });
-      chrome.browserAction.setIcon({path:"bell.png"});
-      chrome.browserAction.setTitle({title:"Alerts for Skia Troopers"});
-      chrome.browserAction.setBadgeText({text: String(numActiveInfraAlerts)});
-      if (numActiveInfraAlerts > 0) {
-        chrome.browserAction.setBadgeBackgroundColor({color: "red"});
-      } else {
-        chrome.browserAction.setBadgeBackgroundColor({color: "green"});
-      }
+    });
+    chrome.browserAction.setIcon({path:"bell.png"});
+    chrome.browserAction.setTitle({title:"Alerts for Skia Troopers"});
+    chrome.browserAction.setBadgeText({text: String(numActiveInfraAlerts)});
+    if (numActiveInfraAlerts > 0) {
+      chrome.browserAction.setBadgeBackgroundColor({color: "red"});
     } else {
-      console.error("Error talking to alertserver.");
+      chrome.browserAction.setBadgeBackgroundColor({color: "green"});
     }
-  }
-  xmlHttp.onreadystatechange = function() {
-    if (xmlHttp.readyState == XMLHttpRequest.DONE) {
-      setTimeout(updateIcon, 10000);
-    }
-  }
-  xmlHttp.onerror = function(e) {
-    console.error("Error talking to alertserver.");
-  }
-  xmlHttp.send(null);
+  });
 }
 
-updateIcon();
+function requestData() {
+  sk.get("https://promalerts.skia.org/api/v1/alerts/groups")
+    .then(function(data) {
+      updateIcon(data, false);
+      sk.get("https://alerts2.skia.org/api/v1/alerts/groups")
+        .then(data => updateIcon(data, true));
+    }).catch(e => {
+      console.error("Error talking to alertserver");
+      console.error(e);
+    });
+  setTimeout(requestData, 10000);
+}
+
+requestData();
--- chromeextensions/skia-trooper/popup.js
+++ chromeextensions/skia-trooper/popup.js
@@ -41,6 +41,7 @@
     return `<a href='${bugTemplate}' target='_blank' rel='noopener' class=auto-bug><svg id='file-bug'><path d='M20 8h-2.81c-.45-.78-1.07-1.45-1.82-1.96L17 4.41 15.59 3l-2.17 2.17C12.96 5.06 12.49 5 12 5c-.49 0-.96.06-1.41.17L8.41 3 7 4.41l1.62 1.63C7.88 6.55 7.26 7.22 6.81 8H4v2h2.09c-.05.33-.09.66-.09 1v1H4v2h2v1c0 .34.04.67.09 1H4v2h2.81c1.04 1.79 2.97 3 5.19 3s4.15-1.21 5.19-3H20v-2h-2.09c.05-.33.09-.66.09-1v-1h2v-2h-2v-1c0-.34-.04-.67-.09-1H20V8zm-6 8h-4v-2h4v2zm0-4h-4v-2h4v2z'><title>File a Chrome Infra bug</title></path></svg></a>`
   }
 
+  // TODO(rmistry): rmistry - take in link to alert server here.
   function handleActionEvent(alertId, labels) {
     var commentText = document.getElementById("comment_input" + alertId).value;
     var duration = parseInt(document.querySelector('input[name="duration' + alertId + '"]:checked').value);
@@ -109,6 +110,8 @@
           var numActiveAlerts = 0;
           // Put alertGroups alphabetically for easier scanning.
           alertGroups.sort(function(a,b) {
+            console.log("Any chance you have link to alert serveR???");
+            console.log(a);
             return a.labels.alertname.localeCompare(b.labels.alertname);
           });
 
@@ -171,7 +174,7 @@
               });
             });
           });
-          document.getElementById("silenced-alerts").innerHTML = numSilencedAlerts;
+          document.getElementById("silenced-alerts1").innerHTML = numSilencedAlerts;
 
           if (!numActiveAlerts) {
             table.insertRow(-1).insertCell(-1).innerHTML = "<br/>No alerts are active."
