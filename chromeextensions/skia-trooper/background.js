df49ca57f50948be10bd6e4b9b3a332292cab6d7
Ravi Mistry <rmistry@google.com>
2018-07-26 09:38:09 -0400

    Extensions Checkpoint
    Bug: skia:
    Change-Id: Ida1c66bdde97d15781fae1d339a5ea4f86517a7b

--- chromeextensions/skia-trooper/background.js
+++ chromeextensions/skia-trooper/background.js
@@ -2,53 +2,55 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-function updateIcon() {
-  var xmlHttp = new XMLHttpRequest();
-  xmlHttp.open("GET", "https://promalerts.skia.org/api/v1/alerts/groups", true);
-  xmlHttp.timeout = 5000;
-  xmlHttp.onload = function(e) {
-    if (xmlHttp.readyState === XMLHttpRequest.DONE && xmlHttp.status === 200) {
-      var numActiveInfraAlerts = 0;
-
-      var resp = JSON.parse(xmlHttp.responseText);
-      var alertGroups = resp.data;
-      alertGroups.forEach(function(alertGroup) {
-        if (!alertGroup.blocks) {
+function getActiveInfraAlerts(data) {
+  let numActiveInfraAlerts = 0;
+  const alertGroups = JSON.parse(data).data;
+  alertGroups.forEach(alertGroup => {
+    if (!alertGroup.blocks) {
+      return;
+    }
+    alertGroup.blocks.forEach(block => {
+      block.alerts.forEach(al => {
+        if (al.labels.category != "infra") {
           return;
         }
-        alertGroup.blocks.forEach(function(block) {
-          block.alerts.forEach(function(al) {
-            if (al.labels.category != "infra") {
-              return;
-            }
-            if (al.status.state === "suppressed") {
-              return;
-            }
-            numActiveInfraAlerts++;
-          });
-        });
+        if (al.status.state === "suppressed") {
+          return;
+        }
+        numActiveInfraAlerts++;
       });
-      chrome.browserAction.setIcon({path:"bell.png"});
-      chrome.browserAction.setTitle({title:"Alerts for Skia Troopers"});
-      chrome.browserAction.setBadgeText({text: String(numActiveInfraAlerts)});
-      if (numActiveInfraAlerts > 0) {
-        chrome.browserAction.setBadgeBackgroundColor({color: "red"});
-      } else {
-        chrome.browserAction.setBadgeBackgroundColor({color: "green"});
-      }
-    } else {
-      console.error("Error talking to alertserver.");
-    }
-  }
-  xmlHttp.onreadystatechange = function() {
-    if (xmlHttp.readyState == XMLHttpRequest.DONE) {
-      setTimeout(updateIcon, 10000);
-    }
-  }
-  xmlHttp.onerror = function(e) {
-    console.error("Error talking to alertserver.");
-  }
-  xmlHttp.send(null);
+    });
+  });
+  return numActiveInfraAlerts;
 }
 
-updateIcon();
+function updateIcon(numActiveInfraAlerts) {
+  chrome.browserAction.setIcon({path:"bell.png"});
+  chrome.browserAction.setTitle({title:"Alerts for Skia Troopers"});
+  chrome.browserAction.setBadgeText({text: String(numActiveInfraAlerts)});
+  if (numActiveInfraAlerts > 0) {
+    chrome.browserAction.setBadgeBackgroundColor({color: "red"});
+  } else {
+    chrome.browserAction.setBadgeBackgroundColor({color: "green"});
+  }
+}
+
+function logAlertServerErrors(e) {
+  console.error("Error talking to alertserver");
+  console.error(e);
+}
+
+function requestData() {
+  sk.get("https://promalerts.skia.org/api/v1/alerts/groups")
+    .then(data => {
+      const numAlerts1 = getActiveInfraAlerts(data);
+      sk.get("https://alerts2.skia.org/api/v1/alerts/groups")
+        .then(data => {
+          const numAlerts2 = getActiveInfraAlerts(data);
+          updateIcon(numAlerts1 + numAlerts2);
+        }).catch(e => logAlertServerErrors(e));
+    }).catch(e => logAlertServerErrors(e));
+  setTimeout(requestData, 10000);
+}
+
+requestData();
