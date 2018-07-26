4af2ec855a81de47f8b50e620c8b1069d871dda4
Joe Gregorio <jcgregorio@google.com>
2018-07-26 13:26:36 -0400

    wip initial sketch of alert-manager.
    Bug: skia:
    Change-Id: I8140eeb2bad6912b8c4993f0bb1f6784eedeb9e5

---
+++ am/modules/silence-sk/silence-sk.js
@@ -0,0 +1,176 @@
+/**
+ * @module /silence-sk
+ * @description <h2><code>silence-sk</code></h2>
+ *
+ * @evt
+ *
+ * @attr
+ *
+ * @example
+ */
+import { html, render } from 'lit-html/lib/lit-extended'
+import { unsafeHTML } from 'lit-html/lib/unsafe-html'
+import { $$ } from 'common-sk/modules/dom'
+import { diffDate } from 'common-sk/modules/human'
+import 'elements-sk/icon/delete-icon-sk'
+import * as paramset from '../paramset'
+
+const linkRe = /(http[s]?:\/\/[^\s]*)/gm;
+
+function linkify(s) {
+  console.log(s.replace(linkRe, '<a href="$&">$&</a>'));
+  return s.replace(linkRe, '<a href="$&">$&</a>');
+}
+
+function notes(ele) {
+  if (!ele._state.notes) {
+    return [];
+  }
+  return ele._state.notes.map((note, index) => html`<section class=note>
+  <p>${linkify(note.text)}</p>
+  <div class=meta>
+    <span class=author>${note.author}</span>
+    <span class=date>${diffDate(note.ts*1000)}</span>
+    <delete-icon-sk title='Delete comment.' on-click=${(e) => ele._deleteNote(e, index)}></delete-icon-sk>
+  </div>
+</section>`);
+}
+
+function table(ele, o) {
+  let keys = Object.keys(o);
+  keys.sort();
+  return keys.filter(k => !k.startsWith('__')).map((k) =>
+    html`<tr><td><delete-icon-sk title='Delete rule.' on-click=${(e) => ele._deleteRule(e, k)}></delete-icon-sk></td><th>${k}</th><td>${o[k].join(', ')}</td></tr>`);
+}
+
+function addNote(ele) {
+  if (ele._state.key) {
+    return html`
+    <textarea rows=2 cols=80></textarea>
+    <button on-click=${(e) => ele._addNote(e)}>Submit</button>
+  `
+  } else {
+    return ``
+  }
+}
+
+function displaySilence(silence) {
+  let ret = [];
+  for (let key in silence.param_set) {
+    if (key.startsWith('__')) {
+      continue
+    }
+    ret.push(`${key} - ${silence.param_set[key].join(', ')}`);
+  }
+  let s = ret.join(' ');
+  if (s.length > 33) {
+    s = s.slice(0, 30) + '...';
+  }
+  return s;
+}
+
+function abbr(ele) {
+  let s = ele.params['abbr'];
+  if (s) {
+    return ` - ${s}`;
+  } else {
+    return ``
+  }
+}
+
+function matches(ele) {
+  if (!ele._incidents) {
+    return ``
+  }
+  return ele._incidents.filter(
+    incident => paramset.match(ele._state.param_set, incident.params)
+  ).map(incident => html`<h2> ${incident.params.alertname} ${abbr(incident)}</h2>`);
+
+}
+
+const template = (ele) => html`
+  <section class=actions>
+    <button on-click=${e => ele._save()}>Save</button>
+    <button on-click=${e => ele._archive()}>Archive</button>
+  </section>
+  <table class=info>
+    <tr><th>User:</th><td>${ele._state.user}</td></th>
+    <tr><th>Duration:</th><td><input value=${ele._state.duration}></input></td></th>
+  </table>
+  <table class=params>
+    ${table(ele, ele._state.param_set)}
+  </table>
+  <section class=notes>
+    ${notes(ele)}
+  </section>
+  <section class=addNote>
+    ${addNote(ele)}
+  </section>
+  <section class=matches>
+    ${matches(ele)}
+  </section>
+`;
+
+window.customElements.define('silence-sk', class extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  /** @prop state {Object} A Silence. */
+  get state() { return this._state }
+  set state(val) {
+    this._state = val;
+    this._render();
+  }
+
+  /** @prop incidents {string} The current active incidents. */
+  get incidents() { return this._incidents }
+  set incidents(val) {
+    this._incidents = val;
+    this._render();
+  }
+
+  _save(e) {
+    let detail = {
+      silence: this._state,
+    };
+    this.dispatchEvent(new CustomEvent('save-silence', { detail: detail, bubbles: true }));
+  }
+
+  _archive(e) {
+    let detail = {
+      silence: this._state,
+    };
+    this.dispatchEvent(new CustomEvent('archive-silence', { detail: detail, bubbles: true }));
+  }
+
+  _deleteRule(e, key) {
+    let detail = {
+      key: key,
+    };
+    this.dispatchEvent(new CustomEvent('delete-silence-param', { detail: detail, bubbles: true }));
+  }
+
+  _addNote(e) {
+    let textarea = $$('textarea', this);
+    let detail = {
+      key: this._state.key,
+      text: textarea.value,
+    };
+    this.dispatchEvent(new CustomEvent('add-silence-note', { detail: detail, bubbles: true }));
+    textarea.value = '';
+  }
+
+  _deleteNote(e, index) {
+    let detail = {
+      key: this._state.key,
+      index: index,
+    };
+    this.dispatchEvent(new CustomEvent('del-silence-note', { detail: detail, bubbles: true }));
+  }
+
+  _render() {
+    render(template(this), this);
+  }
+
+});
