a2adc99f9bc82512629ebf9b4ba358f8c139f164
Ravi Mistry <rmistry@google.com>
2018-08-01 13:15:01 -0400

    Port CQ Watcher over to skia-public
    Bug: skia:8215
    Change-Id: I5e23f440555b23d17c1d11dc45c8d78feef1b03c
    Reviewed-on: https://skia-review.googlesource.com/136783
    Reviewed-by: Joe Gregorio <jcgregorio@google.com>
    Commit-Queue: Ravi Mistry <rmistry@google.com>

---
+++ kube/create-cq-watcher-sa.sh
@@ -0,0 +1,21 @@
+#/bin/bash
+
+# Creates the service account used by CQ Watcher, and export a key for
+# it into the kubernetes cluster as a secret.
+
+set -e -x
+source ./config.sh
+source ../bash/ramdisk.sh
+
+# New service account we will create.
+SA_NAME="skia-cq-watcher"
+
+cd /tmp/ramdisk
+
+gcloud --project=${PROJECT_ID} iam service-accounts create "${SA_NAME}" --display-name="Service account for CQ Watcher"
+
+gcloud beta iam service-accounts keys create ${SA_NAME}.json --iam-account="${SA_NAME}@${PROJECT_ID}.iam.gserviceaccount.com"
+
+kubectl create secret generic "${SA_NAME}" --from-file=key.json=${SA_NAME}.json
+
+cd -
