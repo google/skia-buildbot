f0ac568b92acb5afc455b3a77b7df3590424bf9d
Joe Gregorio <jcgregorio@google.com>
2018-08-03 14:59:59 -0400

    wip initial sketch of alert-manager.
    Bug: skia:
    Change-Id: I8140eeb2bad6912b8c4993f0bb1f6784eedeb9e5

--- go/alerts/alerts.go
+++ go/alerts/alerts.go
@@ -3,34 +3,16 @@ package alerts
 const (
 	// TOPIC is the PubSub topic for alert messages.
 	TOPIC = "promtheus-alerts"
+
+	// Well known keys and values for Metric.
+	TYPE         = "__name__" // The two valid values are ALERTS and HEALTHZ.
+	TYPE_ALERTS  = "ALERTS"
+	TYPE_HEALTHZ = "HEALTHZ"
+
+	STATE          = "__state__"
+	STATE_ACTIVE   = "active"
+	STATE_RESOLVED = "resolved"
+
+	// Where the alert came from, e.g. 'skia-public' or 'skolo'.
+	LOCATION = "location"
 )
-
-// Result contains all the key-value pairs for each alert, for example:
-//
-// "metric":{
-// 		"__name__":"ALERTS",
-// 		"alertname":"BotUnemployed",
-// 		"alertstate":"firing",
-// 		"bot":"skia-rpi-064",
-// 		"category":"infra",
-// 		"instance":"skia-datahopper2:20000",
-// 		"job":"datahopper",
-// 		"pool":"Skia",
-// 		"severity":"critical",
-// 		"swarming":"chromium-swarm.appspot.com"
-// },
-type Result struct {
-	Metric map[string]string `json:"metric"`
-}
-
-type Data struct {
-	ResultType string   `json:"result_type"`
-	Results    []Result `json:"results"`
-}
-
-// QueryResponse is the top level struct returned from querying
-// the Prometheus v1 REST API.
-type QueryResponse struct {
-	Status string `json:"status"`
-	Data   Data   `json:"data"`
-}
