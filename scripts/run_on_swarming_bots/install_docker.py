06aaf21b5d91a68de7c02aafc0fdee8531ba0c21
Eric Boren <borenet@google.com>
2018-08-03 15:37:24 -0400

    Sacrifice to appease the Pylint
    Bug: skia:
    Change-Id: I21a3b25824c31081afca1a0e18b4597537c6f975
    Reviewed-on: https://skia-review.googlesource.com/145370
    Commit-Queue: Eric Boren <borenet@google.com>
    Reviewed-by: Kevin Lubick <kjlubick@google.com>

--- scripts/run_on_swarming_bots/install_docker.py
+++ scripts/run_on_swarming_bots/install_docker.py
@@ -6,16 +6,20 @@
 # found in the LICENSE file.
 
 
-"""Install Docker on a Swarming bot, then "fail", causing a reboot. Aborts on error."""
+"""
+Install Docker on a Swarming bot then "fail", causing a reboot. Aborts on error.
+"""
 
 
 import os
 import subprocess
 import sys
 
-# This is a linux amd64 version of a credential helper recommended for use on the cloud.
-# See https://cloud.google.com/container-registry/docs/advanced-authentication
-AUTH_HELPER_URL = 'https://storage.googleapis.com/skia-backups/docker-credential-gcr/1.5.0/docker-credential-gcr'
+# This is a linux amd64 version of a credential helper recommended for use on
+# the cloud. See
+# https://cloud.google.com/container-registry/docs/advanced-authentication
+AUTH_HELPER_URL = ('https://storage.googleapis.com/skia-backups/'
+                   'docker-credential-gcr/1.5.0/docker-credential-gcr')
 
 # Copied from
 # https://skia.googlesource.com/buildbot/+/d864d83d992f2968cf4d229cebf2d3104ee11ebf/go/gce/swarming/base-image/setup-script.sh#20
@@ -25,34 +29,44 @@ base_cmd = ['sudo', 'DEBIAN_FRONTEND=noninteractive', 'apt',
             '-o', 'Dpkg::Options::=--force-confold']
 # Install pre-reqs
 subprocess.check_call(base_cmd + ['update'])
-subprocess.check_call(base_cmd + ['install', 'apt-transport-https', 'ca-certificates', 'curl', 'gnupg2', 'software-properties-common'])
+subprocess.check_call(base_cmd + [
+    'install', 'apt-transport-https', 'ca-certificates', 'curl', 'gnupg2',
+    'software-properties-common'])
 # Set up docker repo
-os.system("curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -")
-subprocess.check_call(['sudo', 'add-apt-repository', 'deb [arch=amd64] https://download.docker.com/linux/debian stretch stable'])
+os.system(
+    'curl -fsSL https://download.docker.com/linux/debian/gpg | '
+    'sudo apt-key add -')
+subprocess.check_call([
+    'sudo', 'add-apt-repository',
+    'deb [arch=amd64] https://download.docker.com/linux/debian stretch stable'])
 # install docker
 subprocess.check_call(base_cmd + ['update'])
 subprocess.check_call(base_cmd + ['install', 'docker-ce'])
 # Clear cache, which frees up 1G in many cases
 subprocess.check_call(['sudo', 'apt-get', 'clean'])
 
-# Stop the daemon and tell it to put the docker images in /mnt/pd0/docker, which has a bigger disk.
+# Stop the daemon and tell it to put the docker images in /mnt/pd0/docker, which
+# has a bigger disk.
 subprocess.check_call(['sudo', 'mkdir', '-p', '/mnt/pd0/docker'])
 subprocess.check_call(['sudo', 'systemctl', 'stop', 'docker'])
-os.system("sudo sh -c \"echo '{\\\"graph\\\":\\\"/mnt/pd0/docker\\\"}' >> /etc/docker/daemon.json\"")
+os.system("sudo sh -c \"echo '{\\\"graph\\\":\\\"/mnt/pd0/docker\\\"}' >>"
+          " /etc/docker/daemon.json\"")
 subprocess.check_call(['sudo', 'systemctl', 'start', 'docker'])
 
 # Test it out
 subprocess.check_call(['sudo', 'docker', 'run', 'hello-world'])
 
 # Add chrome-bot to docker group
-# The following is necessary, but it appears installing docker now creates the group.
-# Leaving this around mainly for documentation.
+# The following is necessary, but it appears installing docker now creates the
+# group. Leaving this around mainly for documentation.
 #subprocess.check_call(['sudo', 'groupadd', 'docker'])
 subprocess.check_call(['sudo', 'usermod', '-aG', 'docker', 'chrome-bot'])
 
-# Download the Docker authenticator helper, which will authenticate our calls to gcr.io properly
+# Download the Docker authenticator helper, which will authenticate our calls to
+# gcr.io properly
 subprocess.check_call(['rm', '-f', '/home/chrome-bot/.docker/config.json'])
-subprocess.check_call(['wget', '-O', '/tmp/docker-credential-gcr', AUTH_HELPER_URL])
+subprocess.check_call([
+    'wget', '-O', '/tmp/docker-credential-gcr', AUTH_HELPER_URL])
 subprocess.check_call(['chmod', '+x', '/tmp/docker-credential-gcr'])
 subprocess.check_call(['sudo', 'cp', '/tmp/docker-credential-gcr', '/usr/bin'])
 subprocess.check_call(['docker-credential-gcr', 'configure-docker'])
