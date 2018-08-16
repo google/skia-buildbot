f2ee793a09d902a43d3cfb2264f2722cf2f270a0
Stephan Altmueller <stephana@google.com>
2018-08-16 13:41:15 -0400

    Add SSL probing to proberk
    Bug: skia:8189
    Change-Id: Iab4030f224cd93ee3b805ff99056177bfa0b5b2e

---
+++ proberk/go/proberk/main_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"testing"
+
+	assert "github.com/stretchr/testify/require"
+
+	"go.skia.org/infra/go/testutils"
+	"go.skia.org/infra/proberk/go/types"
+)
+
+func TestProbeSSL(t *testing.T) {
+	testutils.LargeTest(t)
+	probes := &types.Probe{
+		URLs: []string{
+			"https://skia.org",
+			"https://skia.org:443",
+			"https://35.201.76.220",
+		},
+		Method: "SSL",
+	}
+
+	// Verify the Certs are valid. This implies they are valid for 10 days.
+	for _, url := range probes.URLs {
+		assert.NoError(t, probeSSL(probes, url))
+	}
+
+	// Verify failure by expecting certs to be valid for 20 years.
+	probes.Expected = []int{7300}
+	for _, url := range probes.URLs {
+		assert.Error(t, probeSSL(probes, url))
+	}
+}
