d74f544f66ffe22b47abf65d21cbb30562c5f8f0
Eric Boren <borenet@google.com>
2018-08-20 09:31:54 -0400

    [autoroll] Add backup sheriff to config
    Bug: skia:8258
    Change-Id: Ia05e5534becbf74dc46baca5f2bfd32829b1325a

--- autoroll/go/roller/sheriff.go
+++ autoroll/go/roller/sheriff.go
@@ -9,16 +9,27 @@ import (
 	"strings"
 
 	"go.skia.org/infra/go/httputils"
+	"go.skia.org/infra/go/metrics2"
+	"go.skia.org/infra/go/sklog"
 	"go.skia.org/infra/go/util"
 )
 
 // Update the current sheriff list.
-func getSheriff(parentName, childName string, sheriffSources []string) ([]string, error) {
+func getSheriff(parentName, childName, metricsName string, sheriffSources, backupSheriffs []string) ([]string, error) {
+	tags := map[string]string{
+		"roller": metricsName,
+	}
+	m := metrics2.GetInt64Metric("autoroll_get_sheriff_success", tags)
 	allEmails := []string{}
 	for _, s := range sheriffSources {
 		emails, err := getSheriffHelper(s)
 		if err != nil {
-			return nil, err
+			sklog.Errorf("Failed to retrieve sheriff(s): %s", err)
+			m.Update(0)
+			if len(backupSheriffs) == 0 {
+				return nil, fmt.Errorf("Failed to retrieve sheriffs and no backup sheriffs supplied!  %s", err)
+			}
+			return backupSheriffs, nil
 		}
 		// TODO(borenet): Do we need this any more?
 		if strings.Contains(parentName, "Chromium") && childName != "WebRTC" && childName != "Perfetto" {
@@ -28,6 +39,7 @@ func getSheriff(parentName, childName string, sheriffSources []string) ([]string
 		}
 		allEmails = append(allEmails, emails...)
 	}
+	m.Update(1)
 	return allEmails, nil
 }
 
