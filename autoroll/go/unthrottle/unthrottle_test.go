37071d2a781ea5e67623a01214dd5b7a948af47e
Eric Boren <borenet@google.com>
2018-08-21 10:27:04 -0400

    Fix build
    Comments 1
    
    
    Add frontend back to all hosts
    
    
    Add support for unthrottling
    
    
    Add comment
    
    
    Backtrack, only internal roller
    
    
    Add frontend to push config
    
    
    Release and service files
    
    
    Fix some tests
    
    
    WIP
    
    
    WIP
    
    
    Ignore res
    
    
    Split frontend and backend
    
    
    Continue separating front end and back end
    
    
    WIP separating frontend and backend
    
    Bug: skia:
    Change-Id: I716df0de3bc5a026c7f334d981624dab5373ad0d

---
+++ autoroll/go/unthrottle/unthrottle_test.go
@@ -0,0 +1,36 @@
+package unthrottle
+
+import (
+	"context"
+	"testing"
+
+	assert "github.com/stretchr/testify/require"
+	"go.skia.org/infra/go/ds"
+	"go.skia.org/infra/go/ds/testutil"
+	"go.skia.org/infra/go/testutils"
+)
+
+func TestUnthrottle(t *testing.T) {
+	testutils.LargeTest(t)
+
+	r := "fake-roller"
+	ctx := context.Background()
+	testutil.InitDatastore(t, ds.KIND_AUTOROLL_UNTHROTTLE)
+
+	check := func(expect bool) {
+		actual, err := Get(ctx, r)
+		assert.NoError(t, err)
+		assert.Equal(t, expect, actual)
+	}
+
+	// No entry exists; ensure that we return false and no error.
+	check(false)
+
+	// Unthrottle the roller.
+	assert.NoError(t, Unthrottle(ctx, r))
+	check(true)
+
+	// Reset.
+	assert.NoError(t, Reset(ctx, r))
+	check(false)
+}
