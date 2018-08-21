391b8ffdf60e9ad37593abcf181bc61c6732bfb6
Stephan Altmueller <stephana@google.com>
2018-08-21 14:04:25 -0400

    wip: storage events
    Bug: skia:
    Change-Id: I3e79ffd4b87f9f71f416a55637af27cce49882e4

--- cmd/gevent_viewer/main.go
+++ cmd/gevent_viewer/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"strings"
 
+	"cloud.google.com/go/storage"
 	"github.com/davecgh/go-spew/spew"
+
 	"go.skia.org/infra/go/common"
 	"go.skia.org/infra/go/gevent"
 	"go.skia.org/infra/go/sklog"
@@ -17,6 +20,7 @@ import (
 var (
 	topic          = flag.String("topic", "testing-gold-stage-eventbus", "Google Cloud PubSub topic of the eventbus.")
 	subscriberName = flag.String("subscriber", "local-wien", "ID of the pubsub subscriber.")
+	storageBucket  = flag.String("bucket", "wien", "ID of the pubsub subscriber.")
 	projectID      = flag.String("project_id", common.PROJECT_ID, "Project ID of the Cloud project where the PubSub topic lives.")
 	channels       = flag.String("channels", expstorage.EV_EXPSTORAGE_CHANGED, "Comma separated list of event channels.")
 )
@@ -28,9 +32,9 @@ func main() {
 		sklog.Fatalf("project_id, topic, subscriber and channels flags must all be set.")
 	}
 
-	eventBus, err := gevent.New(common.PROJECT_ID, *topic, *subscriberName)
+	eventBus, err := gevent.New(*projectID, *topic, *subscriberName)
 	if err != nil {
-		sklog.Fatalf("Error createing event bus: %s", err)
+		sklog.Fatalf("Error creating event bus: %s", err)
 	}
 
 	allChannels := strings.Split(*channels, ",")
@@ -41,6 +45,20 @@ func main() {
 				fmt.Println(spew.Sdump(evt))
 			})
 		}(oneChannel)
+	}
+
+	if *storageBucket != "" {
+		storageClient, err := storage.NewClient(context.TODO())
+		if err != nil {
+			sklog.Fatalf("Unable to create storage client: %s", err)
+		}
+		gEventBus := eventBus.(*gevent.DistEventBus)
+		gEventBus.RegisterStorageEvents(*storageBucket, storageClient)
+		sklog.Infof("Registered storage events")
+		gEventBus.SubscribeAsync(gEventBus.StorageEventType(*storageBucket), func(evt interface{}) {
+			fmt.Printf("Received Message for bucket %s: \n %s\n", *storageBucket, spew.Sdump(evt))
+		})
+
 	}
 
 	// Wait forever as messages come in.
