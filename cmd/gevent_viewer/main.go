f9884bb583ec6451dad9e2ffdd5b7640d6dc6166
Stephan Altmueller <stephana@google.com>
2018-08-24 17:27:42 -0400

    wip: storage events
    Bug: skia:
    Change-Id: I3e79ffd4b87f9f71f416a55637af27cce49882e4

--- cmd/gevent_viewer/main.go
+++ cmd/gevent_viewer/main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
+	"os"
+	"regexp"
 	"strings"
 
+	"google.golang.org/api/option"
+
+	"cloud.google.com/go/storage"
 	"github.com/davecgh/go-spew/spew"
+
+	"go.skia.org/infra/go/auth"
 	"go.skia.org/infra/go/common"
 	"go.skia.org/infra/go/gevent"
 	"go.skia.org/infra/go/sklog"
@@ -15,10 +23,13 @@ import (
 
 // Command line flags.
 var (
-	topic          = flag.String("topic", "testing-gold-stage-eventbus", "Google Cloud PubSub topic of the eventbus.")
-	subscriberName = flag.String("subscriber", "local-wien", "ID of the pubsub subscriber.")
-	projectID      = flag.String("project_id", common.PROJECT_ID, "Project ID of the Cloud project where the PubSub topic lives.")
-	channels       = flag.String("channels", expstorage.EV_EXPSTORAGE_CHANGED, "Comma separated list of event channels.")
+	topic           = flag.String("topic", "testing-gold-stage-eventbus", "Google Cloud PubSub topic of the eventbus.")
+	subscriberName  = flag.String("subscriber", "local-wien", "ID of the pubsub subscriber.")
+	storageBucket   = flag.String("bucket", "", "ID of the pubsub subscriber.")
+	objectPrefix    = flag.String("object_prefix", "", "Prefix of the storage path that should be watched.")
+	objectRegExpStr = flag.String("object_regex", "", "Regex that must be matched by the object id")
+	projectID       = flag.String("project_id", common.PROJECT_ID, "Project ID of the Cloud project where the PubSub topic lives.")
+	channels        = flag.String("channels", expstorage.EV_EXPSTORAGE_CHANGED, "Comma separated list of event channels.")
 )
 
 func main() {
@@ -28,9 +39,9 @@ func main() {
 		sklog.Fatalf("project_id, topic, subscriber and channels flags must all be set.")
 	}
 
-	eventBus, err := gevent.New(common.PROJECT_ID, *topic, *subscriberName)
+	eventBus, err := gevent.New(*projectID, *topic, *subscriberName)
 	if err != nil {
-		sklog.Fatalf("Error createing event bus: %s", err)
+		sklog.Fatalf("Error creating event bus: %s", err)
 	}
 
 	allChannels := strings.Split(*channels, ",")
@@ -41,6 +52,39 @@ func main() {
 				fmt.Println(spew.Sdump(evt))
 			})
 		}(oneChannel)
+	}
+
+	if *storageBucket != "" {
+		// Get the token source from the same service account. Needed to access cloud pubsub and datastore.
+		serviceAccountFile := os.Getenv("GOOGLE_APPLICATION_CREDENTIALS")
+		sklog.Infof("Svc account file: %s", serviceAccountFile)
+		tokenSource, err := auth.NewJWTServiceAccountTokenSource("", serviceAccountFile, storage.ScopeFullControl)
+		if err != nil {
+			sklog.Fatalf("Failed to authenticate service account to get token source: %s", err)
+		}
+
+		opts := []option.ClientOption{option.WithTokenSource(tokenSource)}
+		// option.WithCredentialsFile()}
+		storageClient, err := storage.NewClient(context.TODO(), opts...)
+		if err != nil {
+			sklog.Fatalf("Unable to create storage client: %s", err)
+		}
+		gEventBus := eventBus.(*gevent.DistEventBus)
+
+		var objRegEx *regexp.Regexp
+		if *objectRegExpStr != "" {
+			objRegEx = regexp.MustCompile(*objectRegExpStr)
+		}
+
+		eventType, err := gEventBus.RegisterStorageEvents(*storageBucket, *objectPrefix, objRegEx, storageClient)
+		if err != nil {
+			sklog.Fatalf("Error: %s", err)
+		}
+
+		sklog.Infof("Registered storage events. Eventtype: %s", eventType)
+		gEventBus.SubscribeAsync(eventType, func(evt interface{}) {
+			sklog.Infof("Received Message for bucket %s: \n %s\n", *storageBucket, spew.Sdump(evt))
+		})
 	}
 
 	// Wait forever as messages come in.
