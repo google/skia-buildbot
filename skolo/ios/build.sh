d6f063f5c802aa8fb4386a2d72489f04a5becf2f
Stephan Altmueller <stephana@chromium.org>
2018-09-04 13:27:18 -0400

    [skolo] Update iOS package for iOS 11
    Changes the scripts to build a version of libimobiledevice for
    use with iOS 11.
    
    No-Presubmit: true
    No-Try: true
    Bug: skia:
    Change-Id: Ifec350c021927fc6b5caf037e81113b8caa10d27
    Reviewed-on: https://skia-review.googlesource.com/147043
    Commit-Queue: Stephan Altmueller <stephana@google.com>
    Reviewed-by: Ben Wagner <benjaminwagner@google.com>

--- skolo/ios/build.sh
+++ skolo/ios/build.sh
@@ -23,9 +23,19 @@ export PKG_CONFIG_PATH="${PREFIX}/lib/pkgconfig"
 # Build and install in order of dependencies.
 cd libplist && ./autogen.sh --prefix=$PREFIX --without-cython && make && make install && cd ..
 cd libusbmuxd && ./autogen.sh --prefix=$PREFIX && make && make install && cd ..
-cd libimobiledevice && ./autogen.sh --prefix=$PREFIX && make && make install && cd ..
+cd libimobiledevice && ./autogen.sh --prefix=$PREFIX --without-cython && make && make install && cd ..
 cd ifuse && ./autogen.sh --prefix=$PREFIX && make && make install && cd ..
-cd ideviceinstaller && ./autogen.sh --prefix=$PREFIX && make && make install && cd ..
+
+# Patch a specific commit of ideviceinstaller so it can be compile on RPi Stretch
+# The reason might be 32-bit vs 64-bit architectures or the fact that RPi Stretch 
+# uses gcc 6 by default while Debian uses gcc 7. 
+cd ideviceinstaller
+git checkout f7988de8279051f3d2d7973b8d7f2116aa5d9317
+git am ../patches/ideviceinstaller.patch
+./autogen.sh --prefix=$PREFIX
+make
+make install 
+cd ..
 
 cd usbmuxd
 ./autogen.sh --prefix=$PREFIX \
--- skolo/ios/install-dependencies.sh
+++ skolo/ios/install-dependencies.sh
@@ -2,4 +2,6 @@
 
 sudo apt-get -y install build-essential autoconf checkinstall \
                 libtool libzip-dev fuse libusb-1.0-0-dev \
-                libfuse-dev libxml2-dev
+                libfuse-dev libxml2-dev \
+                libssl-dev
+
