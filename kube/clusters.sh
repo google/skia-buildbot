c03dbf9231fe1a1fbbb7050510999449c43d04ab
Joe Gregorio <jcgregorio@google.com>
2018-09-12 16:24:51 -0400

    Add script to launch the port-forwarding to skia-corp's Prometheus instance.
    Bug: skia:
    Change-Id: Iba8b61d9aced5cdea08544a5fbf0202ef3d1cfc6

--- kube/clusters.sh
+++ kube/clusters.sh
@@ -3,11 +3,11 @@
 set -e
 
 function __skia_public() {
-  gcloud config set project skia-public
-  gcloud container clusters get-credentials skia-public --zone us-central1-a --project skia-public
+  gcloud config set project skia-public > /dev/null 2>&1
+  gcloud container clusters get-credentials skia-public --zone us-central1-a --project skia-public > /dev/null 2>&1
 }
 
 function __skia_corp() {
-  gcloud config set project google.com:skia-corp
-  gcloud container clusters get-credentials skia-corp --zone us-central1-a --project google.com:skia-corp
+  gcloud config set project google.com:skia-corp > /dev/null 2>&1
+  gcloud container clusters get-credentials skia-corp --zone us-central1-a --project google.com:skia-corp > /dev/null 2>&1
 }
