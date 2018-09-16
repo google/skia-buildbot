92bcaea5591e68041c260d0cf945e4234c408682
Stephan Altmüller <stephana@google.com>
2018-09-15 22:12:00 -0400

    wip:switch all ingestion to be event driven
    Bug: skia:
    Change-Id: I5a2cea6bd8aeb5ce187a0a7662d891bdba90b386

--- go/ingestion/helpers_test.go
+++ go/ingestion/helpers_test.go
@@ -17,17 +17,6 @@ const (
 
 	// TEST_GCS_DIR is the directory from where to fetch GCS test data.
 	TEST_GCS_DIR = "ingest-testdata/dm-json-v1"
-
-	// TEST_DATA_DIR  is the directory with data used for local ingest.
-	TEST_DATA_DIR = "./testdata/local-ingest-test"
-
-	// TEST_DATA_STORAGE_PATH is the folder in the test data bucket.
-	// See go/testutils for details.
-	TEST_DATA_STORAGE_PATH = "ingest-testdata/local-new-ingestion.tar.gz"
-
-	// TEST_DATA_FILE_INDEX contains the list of files contained in the locally
-	// ingested file and the Google storage bucket.
-	TEST_DATA_INDEX_FILE = "./testdata/local_ingest_files.txt"
 )
 
 var (
@@ -45,66 +34,10 @@ func TestGoogleStorageSource(t *testing.T) {
 	testSource(t, src)
 }
 
-func TestFileSystemResultFileLocations(t *testing.T) {
-	testutils.LargeTest(t)
-
-	err := gcs.DownloadTestDataArchive(t, gcs.TEST_DATA_BUCKET, TEST_DATA_STORAGE_PATH, TEST_DATA_DIR)
-	assert.NoError(t, err)
-	defer testutils.RemoveAll(t, TEST_DATA_DIR)
-
-	src, err := NewFileSystemSource("test-fs-source", TEST_DATA_DIR)
-	assert.NoError(t, err)
-	testSource(t, src)
-}
-
-func TestCompareSources(t *testing.T) {
-	testutils.LargeTest(t)
-
-	gsSource, err := NewGoogleStorageSource("gs-test-src", gcs.TEST_DATA_BUCKET, TEST_GCS_DIR, http.DefaultClient, nil)
-	assert.NoError(t, err)
-
-	err = gcs.DownloadTestDataArchive(t, gcs.TEST_DATA_BUCKET, TEST_DATA_STORAGE_PATH, TEST_DATA_DIR)
-	assert.NoError(t, err)
-	defer testutils.RemoveAll(t, TEST_DATA_DIR)
-
-	fsSource, err := NewFileSystemSource("test-fs-source", TEST_DATA_DIR)
-	assert.NoError(t, err)
-
-	gsResults, err := gsSource.Poll(START_TIME, END_TIME)
-	assert.NoError(t, err)
-
-	fsResults, err := fsSource.Poll(START_TIME, END_TIME)
-	assert.NoError(t, err)
-
-	assert.Equal(t, len(gsResults), len(fsResults))
-	sort.Sort(rflSlice(gsResults))
-	sort.Sort(rflSlice(fsResults))
-
-	// Only compare a subset of all files to limit the runtime of this test.
-	gsResults = gsResults[:10]
-	fsResults = fsResults[:10]
-
-	for idx, result := range gsResults {
-		// Check if the MD5 from GS and the file system match.
-		assert.Equal(t, result.MD5(), fsResults[idx].MD5())
-
-		// Check if the content is the same for both sources.
-		file, err := result.Open()
-		assert.NoError(t, err)
-		c1, err := ioutil.ReadAll(file)
-		assert.NoError(t, err)
-		file, err = fsResults[idx].Open()
-		c2, err := ioutil.ReadAll(file)
-		assert.NoError(t, err)
-		assert.Equal(t, string(c1), string(c2))
-	}
-}
-
 func testSource(t *testing.T, src Source) {
 	testFilePaths := readTestFileNames(t)
 
-	resultFileLocations, err := src.Poll(START_TIME, END_TIME)
-	assert.NoError(t, err)
+	resultFileLocations := drainPollChannel(src.Poll(START_TIME, END_TIME))
 
 	assert.Equal(t, len(testFilePaths), len(resultFileLocations))
 	sort.Sort(rflSlice(resultFileLocations))
@@ -114,8 +47,7 @@ func testSource(t *testing.T, src Source) {
 	}
 
 	// Make sure the narrow and wide time range produce the same result.
-	allResultFileLocations, err := src.Poll(BEGINNING_OF_TIME, END_OF_TIME)
-	assert.NoError(t, err)
+	allResultFileLocations := drainPollChannel(src.Poll(BEGINNING_OF_TIME, END_OF_TIME))
 	sort.Sort(rflSlice(allResultFileLocations))
 
 	assert.Equal(t, len(resultFileLocations), len(allResultFileLocations))
@@ -139,3 +71,11 @@ type rflSlice []ResultFileLocation
 func (r rflSlice) Len() int           { return len(r) }
 func (r rflSlice) Less(i, j int) bool { return r[i].Name() < r[j].Name() }
 func (r rflSlice) Swap(i, j int)      { r[i], r[j] = r[j], r[i] }
+
+func drainPollChannel(ch <-chan ResultFileLocation) []ResultFileLocation {
+	ret := []ResultFileLocation{}
+	for rf := range ch {
+		ret = append(ret, rf)
+	}
+	return ret
+}
