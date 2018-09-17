137b72c6ff4479794629443d5b1ca944feca0e83
Stephan Altmüller <stephana@google.com>
2018-09-17 08:10:50 -0400

    Fix go generate output
    - Temporarily disables 'goimports' until the output of go grpc generated code
      complies with it again.
    
    TBR=borenet
    
    Bug: skia:
    Change-Id: I6b56f26376a7dba5c3ddf70548aa5569b3df237a
    Reviewed-on: https://skia-review.googlesource.com/154722
    Commit-Queue: Eric Boren <borenet@google.com>
    Reviewed-by: Eric Boren <borenet@google.com>
    Reviewed-by: Stephan Altmueller <stephana@google.com>
    Auto-Submit: Stephan Altmueller <stephana@google.com>

--- go/trace/service/traceservice.pb.go
+++ go/trace/service/traceservice.pb.go
@@ -3,9 +3,11 @@
 
 package traceservice
 
-import proto "github.com/golang/protobuf/proto"
-import fmt "fmt"
-import math "math"
+import (
+	fmt "fmt"
+	proto "github.com/golang/protobuf/proto"
+	math "math"
+)
 
 import (
 	context "golang.org/x/net/context"
