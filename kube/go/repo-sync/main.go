87c1e39e13cbd9205ce24db17585aef46d5450b3
Joe Gregorio <jcgregorio@google.com>
2018-09-18 14:24:00 -0400

    repo-sync: works locally.
    Bug: skia:
    Change-Id: I9ec320d58dd14f8ca635f277b45f2941aa3aafe9

---
+++ kube/go/repo-sync/main.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+
+	"go.skia.org/infra/go/common"
+	"go.skia.org/infra/go/exec"
+	"go.skia.org/infra/go/sklog"
+)
+
+// flags
+var (
+	period      = flag.Duration("period", 15*time.Minute, "How frequently to sync the repos.")
+	source      = flag.String("source", "https://skia.googlesource.com/skia", "URL of the source repo.")
+	destination = flag.String("destination", "skia", "Name of the GCP source repo to sync to.")
+	project     = flag.String("project", "skia-public", "The GCE project name of the destination repo.")
+	workDir     = flag.String("work_dir", "/tmp", "Directory to place the checkout.")
+	promPort    = flag.String("prom_port", ":20000", "Metrics service address (e.g., ':10110')")
+)
+
+func main() {
+	common.InitWithMust(
+		"repo-sync",
+		common.PrometheusOpt(promPort),
+	)
+	ctx := context.Background()
+	repoDir := filepath.Join(*workDir, *destination)
+	if _, err := os.Stat(repoDir); os.IsNotExist(err) {
+		sklog.Info("Cloning repo.")
+		// gcloud source repos clone skia
+		output := bytes.Buffer{}
+		err := exec.Run(ctx, &exec.Command{
+			Name:           "gcloud",
+			Args:           []string{"source", "repos", "clone", *destination},
+			Dir:            *workDir,
+			CombinedOutput: &output,
+			Timeout:        10 * time.Minute,
+		})
+		if err != nil {
+			sklog.Errorf("gcloud output: %q", output.String())
+			sklog.Fatalf("Failed to initially clone the target repo %q: %s", *destination, err)
+		}
+	} else {
+		sklog.Info("Skipping clone, repo directory already exists.")
+	}
+
+	if err := os.Chdir(repoDir); err != nil {
+		sklog.Fatalf("Failed to change cwd to the repo location: %s", err)
+	}
+
+	for _ = range time.Tick(*period) {
+		// git pull "some external repo"
+		sklog.Info("pull")
+		cmd := fmt.Sprintf("git pull %s master", *source)
+		sklog.Infof("%q", cmd)
+		out, err := exec.RunSimple(ctx, cmd)
+		if err != nil {
+			sklog.Errorf("git output: %q", out)
+			sklog.Errorf("Failed to pull: %s", err)
+			continue
+		}
+		sklog.Info("pulled: %q", out)
+		// git push
+		sklog.Info("push")
+		out, err = exec.RunSimple(ctx, "git push")
+		if err != nil {
+			sklog.Errorf("git output: %q", out)
+			sklog.Errorf("Failed to push: %s", err)
+		}
+		sklog.Info("pushed: %q", out)
+	}
+}
