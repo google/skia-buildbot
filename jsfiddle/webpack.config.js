b4053f0af78ec39af4a7711ba1eac591fc944e3e
Kevin Lubick <kjlubick@google.com>
2018-09-18 16:53:07 -0400

    [jsfiddle] Initial HTML and bare-bones server
    The goal for JS Fiddle is to show live demos and code
    of our JS offerings, like PathKit and CanvasKit.
    
    Bug: skia:
    Change-Id: I3d6d6d220ca5f8e0c607f0600a8faeb1c7043caf

---
+++ jsfiddle/webpack.config.js
@@ -0,0 +1,18 @@
+const commonBuilder = require('pulito');
+const { resolve } = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+module.exports = (env, argv) => {
+  let config = commonBuilder(env, argv, __dirname);
+  // Make all CSS/JS files appear at the /res location.
+  config.output.publicPath='/res/';
+  config.plugins.push(
+    new CopyWebpackPlugin([
+        { from: 'node_modules/pathkit-wasm/bin/pathkit.wasm' }
+    ])
+  );
+  config.node = {
+    fs: 'empty'
+  };
+  return config
+}
