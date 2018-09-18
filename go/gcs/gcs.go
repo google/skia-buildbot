bd79c96d048431bcbca0d4398d1c4e4ebd5df0c4
Stephan Altmueller <stephana@google.com>
2018-09-18 14:42:55 -0400

    wip:switch all ingestion to be event driven
    Bug: skia:
    Change-Id: I5a2cea6bd8aeb5ce187a0a7662d891bdba90b386

--- go/gcs/gcs.go
+++ go/gcs/gcs.go
@@ -103,6 +103,29 @@ func GetLatestGCSDirs(startTS int64, endTS int64, bsSubdir string) []string {
 	return results
 }
 
+// GetHourlyGCSDirs generates paths with the given prefix for all that paths of the
+// form 'prefix/<year>/<month>/<day>/<hour>'. All path segments are padded with zeros
+// where necessary, e.g. prefix/2018/01/02/09. All hours are from 0-23. This is
+// intended to enumerate the directories that should be polled when ingesting data.
+func GetHourlyGCSDirs(startTS int64, endTS int64, prefixDir string) []string {
+	if endTS <= startTS {
+		return []string{}
+	}
+
+	// The result will be roughly the number of hourse in the range.
+	ret := make([]string, 0, (endTS-startTS)/3600)
+	currTime := time.Unix(startTS, 0).UTC()
+	endTime := time.Unix(endTS, 0).UTC().Add(time.Hour)
+
+	for currTime.Before(endTime) {
+		year, month, day := currTime.Date()
+		hour := currTime.Hour()
+		ret = append(ret, fmt.Sprintf("%s/%04d/%02d/%02d/%02d", prefixDir, year, month, day, hour))
+		currTime = currTime.Add(time.Hour)
+	}
+	return ret
+}
+
 // RequestForStorageURL returns an http.Request for a given Cloud Storage URL.
 // This is workaround of a known issue: embedded slashes in URLs require use of
 // URL.Opaque property
