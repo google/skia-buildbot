2ffa74a1b31f84808d0763879706aded9b7261cf
Kevin Lubick <kjlubick@google.com>
2018-09-19 14:07:46 -0400

    [jsfiddle] Add storage and retrieval to GCS
    Bug: skia:
    Change-Id: I824c60021308b175e37c68a1ba5fb0e7d149bdb5

--- jsfiddle/go/jsfiddle_server/main.go
+++ jsfiddle/go/jsfiddle_server/main.go
@@ -1,8 +1,11 @@
 package main
 
+// The webserver for jsfiddle.skia.org. It serves up the web page
+
 import (
 	"encoding/json"
 	"flag"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"path/filepath"
@@ -13,6 +16,7 @@ import (
 	"go.skia.org/infra/go/httputils"
 	"go.skia.org/infra/go/sklog"
 	"go.skia.org/infra/go/util"
+	"go.skia.org/infra/jsfiddle/go/store"
 )
 
 var (
@@ -22,7 +26,13 @@ var (
 	resourcesDir = flag.String("resources_dir", "./dist", "The directory to find templates, JS, and CSS files. If blank the current directory will be used.")
 )
 
+const MAX_FIDDLE_SIZE = 10 * 1024 * 1024 // 10KB ought to be enough for anyone.
+
 var pathkitPage []byte
+
+var knownTypes = []string{"pathkit", "canvaskit"}
+
+var fiddleStore *store.Store
 
 func healthzHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
@@ -40,24 +50,58 @@ func pathkitHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-type codeResponse struct {
+type fiddleContext struct {
 	Code string `json:"code"`
+	Type string `json:"type,omitempty"`
+}
+
+type saveResponse struct {
+	NewURL string `json:"new_url"`
 }
 
 func codeHandler(w http.ResponseWriter, r *http.Request) {
 	qp := r.URL.Query()
-	if util.In("pathkit", qp["type"]) {
-		if util.In("demo", qp["hash"]) {
-			cr := codeResponse{Code: pathkitDemoCode}
-			w.Header().Set("Content-Type", "application/json")
-			if err := json.NewEncoder(w).Encode(cr); err != nil {
-				httputils.ReportError(w, r, err, "Failed to JSON Encode response.")
-			}
-			return
-		}
-		// TODO(kjlubick): actually look up the code from GCS
+	fiddleType := ""
+	if xt, ok := qp["type"]; ok {
+		fiddleType = xt[0]
 	}
-	http.Error(w, "Not found", http.StatusBadRequest)
+	if !util.In(fiddleType, knownTypes) {
+		sklog.Warningf("Unknown type requested %s", qp["type"])
+		http.Error(w, "Invalid Type", http.StatusBadRequest)
+		return
+	}
+
+	hash := ""
+	if xh, ok := qp["hash"]; ok {
+		hash = xh[0]
+	}
+	if hash == "" {
+		sklog.Warningf("Blank hash requested %s", qp["hash"])
+		http.Error(w, "Hash is required, cannot be blank.", http.StatusBadRequest)
+		return
+	}
+
+	if "demo" == hash {
+		cr := fiddleContext{}
+		if "pathkit" == fiddleType {
+			cr.Code = pathkitDemoCode
+		}
+		w.Header().Set("Content-Type", "application/json")
+		if err := json.NewEncoder(w).Encode(cr); err != nil {
+			httputils.ReportError(w, r, err, "Failed to JSON Encode response.")
+		}
+		return
+	}
+	code, err := fiddleStore.GetCode(hash, fiddleType)
+	if err != nil {
+		http.Error(w, "Not found", http.StatusBadRequest)
+		return
+	}
+	cr := fiddleContext{Code: code}
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(cr); err != nil {
+		httputils.ReportError(w, r, err, "Failed to JSON Encode response.")
+	}
 }
 
 func makeResourceHandler() func(http.ResponseWriter, *http.Request) {
@@ -84,13 +128,49 @@ func loadPages() {
 }
 
 func saveHandler(w http.ResponseWriter, r *http.Request) {
-	// TODO(kjlubick)
-	http.Error(w, "Not implemented", 500)
+	req := fiddleContext{}
+	dec := json.NewDecoder(r.Body)
+	defer util.Close(r.Body)
+	if err := dec.Decode(&req); err != nil {
+		httputils.ReportError(w, r, err, "Failed to decode request.")
+		return
+	}
+	if !util.In(req.Type, knownTypes) {
+		http.Error(w, "Invalid type", http.StatusBadRequest)
+		return
+	}
+	if len(req.Code) > MAX_FIDDLE_SIZE {
+		http.Error(w, fmt.Sprintf("Fiddle Too Big, max size is %d bytes", MAX_FIDDLE_SIZE), http.StatusBadRequest)
+		return
+	}
+
+	hash, err := fiddleStore.PutCode(req.Code, req.Type)
+	if err != nil {
+		httputils.ReportError(w, r, err, "Failed to save fiddle.")
+	}
+	sr := saveResponse{NewURL: fmt.Sprintf("/%s/%s", req.Type, hash)}
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(sr); err != nil {
+		httputils.ReportError(w, r, err, "Failed to JSON Encode response.")
+	}
 }
 
 func mainHandler(w http.ResponseWriter, r *http.Request) {
 	// TODO(kjlubick) have a nicer landing page, maybe one that shows canvaskit and pathkit.
 	http.Redirect(w, r, "/pathkit", http.StatusFound)
+}
+
+// cspHandler is an HTTP handler function which adds CSP (Content-Security-Policy)
+// headers to this request
+func cspHandler(h func(http.ResponseWriter, *http.Request)) func(http.ResponseWriter, *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// recommended by https://content-security-policy.com/
+		// "This policy allows images, scripts, AJAX, and CSS from the same origin, and does
+		// not allow any other resources to load (eg object, frame, media, etc).
+		// It is a good starting point for many sites."
+		w.Header().Add("Access-Control-Allow-Origin", "default-src 'none'; script-src 'self'; connect-src 'self'; img-src 'self'; style-src 'self';")
+		h(w, r)
+	}
 }
 
 func main() {
@@ -100,13 +180,19 @@ func main() {
 		common.MetricsLoggingOpt(),
 	)
 	loadPages()
+	var err error
+	fiddleStore, err = store.New(*local)
+	if err != nil {
+		sklog.Fatalf("Failed to connect to store: %s", err)
+	}
 
 	r := mux.NewRouter()
-	r.PathPrefix("/res/").HandlerFunc(makeResourceHandler())
-	r.HandleFunc("/pathkit", pathkitHandler)
-	r.HandleFunc("/", mainHandler)
-	r.HandleFunc("/_/save", saveHandler)
-	r.HandleFunc("/_/code", codeHandler)
+	r.PathPrefix("/res/").HandlerFunc(makeResourceHandler()).Methods("GET")
+	r.HandleFunc("/pathkit", cspHandler(pathkitHandler)).Methods("GET")
+	r.HandleFunc("/pathkit/{id:[@0-9a-zA-Z_]+}", cspHandler(pathkitHandler)).Methods("GET")
+	r.HandleFunc("/", mainHandler).Methods("GET")
+	r.HandleFunc("/_/save", saveHandler).Methods("PUT")
+	r.HandleFunc("/_/code", codeHandler).Methods("GET")
 
 	h := httputils.LoggingGzipRequestResponse(r)
 	h = httputils.HealthzAndHTTPS(h)
