757bef78072ea6c84c33e0b94cab223c6f1b7d3f
Stephan Altmueller <stephana@google.com>
2018-09-20 15:13:55 -0400

    Make all ingestion event driven
    This makes the ingestion framework event driven. Any new file that is uploaded
    is ingested immediately. There is a background process that polls the source and
    generates events if a file was missed.
    
    - Changes ingestion to only ingest files triggered by events
    - Adds background process to poll the data and generate events for files not ingested.
    - Extends the EventBus interface to generate synthetic storage events (used by polling
      and testing)
    
    Bug: skia:
    Change-Id: I5a2cea6bd8aeb5ce187a0a7662d891bdba90b386

--- go/gcs/gcs_test.go
+++ go/gcs/gcs_test.go
@@ -8,48 +8,11 @@ import (
 	"os"
 	"path"
 	"testing"
-	"time"
 
 	"cloud.google.com/go/storage"
 	assert "github.com/stretchr/testify/require"
 	"go.skia.org/infra/go/testutils"
 )
-
-// compareStringSlices compares two string slices, and returns true iff the
-// contents and sequence of the two slices are identical.
-func compareStringSlices(a, b []string) bool {
-	if len(a) != len(b) {
-		return false
-	}
-	for i := 0; i < len(a); i++ {
-		if a[i] != b[i] {
-			return false
-		}
-	}
-	return true
-}
-
-func TestGetLatestGCSDirs(t *testing.T) {
-	testutils.SmallTest(t)
-	startTS := time.Date(1970, time.November, 29, 13, 45, 20, 67, time.UTC).Unix()
-	endTS := time.Date(1972, time.February, 2, 3, 45, 20, 67, time.UTC).Unix()
-	results := GetLatestGCSDirs(startTS, endTS, "prefix")
-	expected := []string{
-		"prefix/1970/11/29",
-		"prefix/1970/11/30",
-		"prefix/1970/12",
-		"prefix/1971",
-		"prefix/1972/01",
-		"prefix/1972/02/01",
-		"prefix/1972/02/02/00",
-		"prefix/1972/02/02/01",
-		"prefix/1972/02/02/02",
-		"prefix/1972/02/02/03",
-	}
-	if !compareStringSlices(results, expected) {
-		t.Errorf("GetLatestGCSDirs unexpected results! Got:\n%s\nWant:\n%s", results, expected)
-	}
-}
 
 func TestDownloadHelper(t *testing.T) {
 	testutils.MediumTest(t)
