d539d3366b929db4624078b28f1f54cfd2a3e73a
Joe Gregorio <jcgregorio@google.com>
2018-09-21 16:46:41 -0400

    Start on BigTable Perf tracedb (latest).
    Bug: skia:
    Change-Id: Iecaa698e6a9cba1797de1affc2f466058752ac73

---
+++ perf/go/types/types.go
@@ -0,0 +1,14 @@
+package types
+
+import "go.skia.org/infra/go/vec32"
+
+// Trace is just a slice of float32s.
+type Trace []float32
+
+// NewTrace returns a Trace of length 'traceLen' initialized to vec32.MISSING_DATA_SENTINEL.
+func NewTrace(traceLen int) Trace {
+	return Trace(vec32.New(traceLen))
+}
+
+// TraceSet is a set of Trace's, keyed by trace id.
+type TraceSet map[string]Trace
