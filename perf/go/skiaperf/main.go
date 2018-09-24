a9ad5a149840a3861f4d42d0c15146becab3bd2a
Joe Gregorio <jcgregorio@google.com>
2018-09-24 15:59:11 -0400

    Start on BigTable Perf tracedb (latest).
    Bug: skia:
    Change-Id: Iecaa698e6a9cba1797de1affc2f466058752ac73

--- perf/go/skiaperf/main.go
+++ perf/go/skiaperf/main.go
@@ -21,6 +21,7 @@ import (
 	"strings"
 	"time"
 
+	"cloud.google.com/go/bigtable"
 	storage "cloud.google.com/go/storage"
 	"github.com/gorilla/mux"
 	"go.skia.org/infra/go/ds"
@@ -37,18 +38,18 @@ import (
 	"go.skia.org/infra/go/common"
 	"go.skia.org/infra/go/git/gitinfo"
 	"go.skia.org/infra/go/httputils"
-	"go.skia.org/infra/go/ingestion"
 	"go.skia.org/infra/go/login"
-	"go.skia.org/infra/go/sharedconfig"
 	"go.skia.org/infra/go/util"
 	"go.skia.org/infra/perf/go/activitylog"
 	"go.skia.org/infra/perf/go/alertfilter"
 	"go.skia.org/infra/perf/go/alerts"
+	"go.skia.org/infra/perf/go/btts"
 	"go.skia.org/infra/perf/go/bug"
 	"go.skia.org/infra/perf/go/cid"
 	"go.skia.org/infra/perf/go/clustering2"
 	"go.skia.org/infra/perf/go/config"
 	"go.skia.org/infra/perf/go/dataframe"
+	"go.skia.org/infra/perf/go/dfbuilder"
 	"go.skia.org/infra/perf/go/notify"
 	_ "go.skia.org/infra/perf/go/ptraceingest"
 	"go.skia.org/infra/perf/go/ptracestore"
@@ -104,6 +105,7 @@ var (
 	resourcesDir          = flag.String("resources_dir", "", "The directory to find templates, JS, and CSS files. If blank the current directory will be used.")
 	stepUpOnly            = flag.Bool("step_up_only", false, "Only regressions that look like a step up will be reported.")
 	subdomain             = flag.String("subdomain", "perf", "The public subdomain of the server, i.e. 'perf' for perf.skia.org.")
+	useBigTable           = flag.Bool("use_big_table", false, "If true then use BigTable as the trace store.")
 )
 
 var (
@@ -126,6 +128,8 @@ var (
 	configProvider regression.ConfigProvider
 
 	notifier *notify.Notifier
+
+	traceStore *btts.BigTableTraceStore
 )
 
 func loadTemplates() {
@@ -208,8 +212,19 @@ func Init() {
 	if !*local && !util.In(*namespace, []string{ds.PERF_NS, ds.PERF_ANDROID_NS, ds.PERF_ANDROID_MASTER_NS}) {
 		sklog.Fatal("When running in prod the datastore namespace must be a known value.")
 	}
-	if err := ds.Init(*projectName, *namespace); err != nil {
+
+	ts, err := auth.NewDefaultTokenSource(*local, bigtable.Scope, storage.ScopeReadOnly)
+	if err != nil {
+		sklog.Fatalf("Failed to get TokenSource: %s", err)
+	}
+
+	if err := ds.InitWithOpt(*projectName, *namespace, option.WithTokenSource(ts)); err != nil {
 		sklog.Fatalf("Failed to init Cloud Datastore: %s", err)
+	}
+
+	storageClient, err = storage.NewClient(ctx, option.WithTokenSource(ts))
+	if err != nil {
+		sklog.Fatalf("Failed to authenicate to cloud storage: %s", err)
 	}
 
 	clusterAlgo, err := clustering2.ToClusterAlgo(*algo)
@@ -218,21 +233,27 @@ func Init() {
 	}
 
 	loadTemplates()
-
 	git, err = gitinfo.CloneOrUpdate(ctx, *gitRepoURL, *gitRepoDir, false)
 	if err != nil {
 		sklog.Fatal(err)
 	}
-	ptracestore.Init(*ptraceStoreDir)
 
-	dfBuilder := dataframe.NewDataFrameBuilderFromPTraceStore(git, ptracestore.Default)
+	var dfBuilder dataframe.DataFrameBuilder
+	if *useBigTable {
+		traceStore, err = btts.NewBigTableTraceStoreFromConfig(ctx, config.PERF_INGESTION_CONFIGS["nano"], ts, false)
+		if err != nil {
+			sklog.Fatalf("Failed to open trace store: %s", err)
+		}
+		dfBuilder = dfbuilder.NewDataFrameBuilderFromBTTS(git, traceStore)
+	} else {
+		dfBuilder = dataframe.NewDataFrameBuilderFromPTraceStore(git, ptracestore.Default)
+	}
 
 	freshDataFrame, err = dataframe.NewRefresher(ctx, git, dfBuilder, time.Minute, *dataFrameSize)
 	if err != nil {
 		sklog.Fatalf("Failed to build the dataframe Refresher: %s", err)
 	}
 
-	initIngestion(ctx)
 	cidl = cid.New(ctx, git, *gitRepoURL)
 
 	alerts.DefaultSparse = *defaultSparse
@@ -1011,11 +1032,32 @@ func detailsHandler(w http.ResponseWriter, r *http.Request) {
 		httputils.ReportError(w, r, err, "Failed to decode JSON.")
 		return
 	}
-	name, _, err := ptracestore.Default.Details(&dr.CID, dr.TraceID)
+	index := int32(dr.CID.Offset)
+	tileKey := traceStore.TileKey(index)
+
+	ops, err := traceStore.GetOrderedParamSet(tileKey)
+	if err != nil {
+		httputils.ReportError(w, r, err, "Failed to find details")
+		return
+	}
+	p, err := query.ParseKey(dr.TraceID)
+	if err != nil {
+		httputils.ReportError(w, r, err, "Invalid trace id")
+		return
+	}
+	encodedKey, err := ops.EncodeParamsAsString(p)
+	if err != nil {
+		httputils.ReportError(w, r, err, "Failed to encode key")
+		return
+	}
+	name, err := traceStore.GetSource(index, encodedKey)
 	if err != nil {
 		httputils.ReportError(w, r, err, "Failed to load details")
 		return
 	}
+	bucket := config.PERF_INGESTION_CONFIGS["nano"].Bucket
+	name = "gs://" + bucket + "/" + name
+	sklog.Infof("Full URL to source: %q", name)
 	u, err := url.Parse(name)
 	if err != nil {
 		httputils.ReportError(w, r, err, "Failed to parse source file location.")
@@ -1027,11 +1069,11 @@ func detailsHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	sklog.Infof("Host: %q Path: %q", u.Host, u.Path)
 	reader, err := storageClient.Bucket(u.Host).Object(u.Path[1:]).NewReader(context.Background())
-	defer util.Close(reader)
 	if err != nil {
 		httputils.ReportError(w, r, err, "Failed to get reader for source file location")
 		return
 	}
+	defer util.Close(reader)
 	if includeResults {
 		if _, err := io.Copy(w, reader); err != nil {
 			sklog.Errorf("Failed to copy JSON file to HTTP stream: %s", err)
@@ -1247,35 +1289,6 @@ func makeResourceHandler() func(http.ResponseWriter, *http.Request) {
 	}
 }
 
-func initIngestion(ctx context.Context) {
-	// Initialize oauth client and start the ingesters.
-	client, err := auth.NewDefaultJWTServiceAccountClient(storage.ScopeReadWrite)
-	if err != nil {
-		sklog.Fatalf("Failed to auth: %s", err)
-	}
-
-	storageClient, err = storage.NewClient(context.Background(), option.WithHTTPClient(client))
-	if err != nil {
-		sklog.Fatalf("Failed to create a Google Storage API client: %s", err)
-	}
-
-	// Start the ingesters.
-	config, err := sharedconfig.ConfigFromJson5File(*configFilename)
-	if err != nil {
-		sklog.Fatalf("Unable to read config file %s. Got error: %s", *configFilename, err)
-	}
-
-	ingesters, err := ingestion.IngestersFromConfig(ctx, config, client, nil, nil)
-	if err != nil {
-		sklog.Fatalf("Unable to instantiate ingesters: %s", err)
-	}
-	for _, oneIngester := range ingesters {
-		if err := oneIngester.Start(ctx); err != nil {
-			sklog.Fatalf("Unable to start ingester: %s", err)
-		}
-	}
-}
-
 func oldMainHandler(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/e/", http.StatusMovedPermanently)
 }
@@ -1310,11 +1323,12 @@ func main() {
 	common.InitWithMust(
 		"skiaperf",
 		common.PrometheusOpt(promPort),
-		common.CloudLoggingOpt(),
 	)
 
 	Init()
-	login.SimpleInitMust(*port, *local)
+	if !*local {
+		login.SimpleInitMust(*port, *local)
+	}
 
 	// Resources are served directly.
 	router := mux.NewRouter()
