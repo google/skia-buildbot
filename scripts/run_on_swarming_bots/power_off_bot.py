7d60eb844978234991bb87f2eb414988b5596e26
Ben Wagner <benjaminwagner@google.com>
2018-09-26 19:45:44 -0400

    Fix rpi-master networking config.
    Change-Id: I33359afa38c7a27872a5aebed021ea489980594a

--- scripts/run_on_swarming_bots/power_off_bot.py
+++ scripts/run_on_swarming_bots/power_off_bot.py
@@ -6,14 +6,22 @@
 # found in the LICENSE file.
 
 
-"""Poweroff a Swarming bot."""
+"""Poweroff a Swarming bot.
+
+Note that this only works on Linux/Mac if the Swarming user is able to run
+'sudo shutdown -h now'."""
 
 
 import os
+import time
 import sys
 
 
 if sys.platform == 'win32':
     os.system('shutdown -s')
 else:
-    os.system('sudo shutdown now')
+    os.system('sudo shutdown -h now')
+
+time.sleep(60)
+
+assert false, "Did not shutdown within 60 seconds."
