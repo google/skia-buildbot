e6f65809cbcd6f093f00df7928903c8f04ed2857
Eric Boren <borenet@google.com>
2018-09-28 07:25:55 -0400

    [autoroll] Update AFDO roller commit message to indicate Linux+Android
    Bug: chromium:889742
    Change-Id: I404f4b449960051f2508091ccecebd468ed10d83

--- autoroll/go/repo_manager/chromium_afdo_repo_manager.go
+++ autoroll/go/repo_manager/chromium_afdo_repo_manager.go
@@ -25,6 +25,9 @@ This CL may cause a small binary size increase, roughly proportional
 to how long it's been since our last AFDO profile roll. For larger
 increases (around or exceeding 100KB), please file a bug against
 gbiv@chromium.org. Additional context: https://crbug.com/805539
+
+Please note that, despite rolling to chrome/android, this profile is
+used for both Linux and Android.
 ` + COMMIT_MSG_FOOTER_TMPL
 )
 
