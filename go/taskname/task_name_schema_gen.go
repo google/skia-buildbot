c65c02fecef9f5dcde559ecf8aac4e347226c0c0
Kevin Lubick <kjlubick@google.com>
2018-10-08 14:45:04 -0400

    Regenerate taskname schema
    Changed in https://skia-review.googlesource.com/c/skia/+/160220
    
    Bug: skia:
    Change-Id: I2890c7c7b407f8199dc86c0deecbffb7e124f472
    Reviewed-on: https://skia-review.googlesource.com/c/160520
    Reviewed-by: Eric Boren <borenet@google.com>

--- go/taskname/task_name_schema_gen.go
+++ go/taskname/task_name_schema_gen.go
@@ -4,12 +4,13 @@ package taskname
 
 var SCHEMA_FROM_GIT = map[string]*Schema{
 	"Build":       {Keys: []string{"os", "compiler", "target_arch", "configuration"}, OptionalKeys: []string{"extra_config"}, RecurseRoles: []string(nil)},
+	"BuildStats":  {Keys: []string{"os", "compiler", "target_arch", "configuration"}, OptionalKeys: []string{"extra_config"}, RecurseRoles: []string(nil)},
 	"Calmbench":   {Keys: []string{"os", "compiler", "model", "cpu_or_gpu", "cpu_or_gpu_value", "arch", "configuration", "test_filter"}, OptionalKeys: []string{"extra_config"}, RecurseRoles: []string(nil)},
 	"Housekeeper": {Keys: []string{"frequency"}, OptionalKeys: []string{"extra_config"}, RecurseRoles: []string(nil)},
 	"Infra":       {Keys: []string{"frequency"}, OptionalKeys: []string{"extra_config"}, RecurseRoles: []string(nil)},
 	"Perf":        {Keys: []string{"os", "compiler", "model", "cpu_or_gpu", "cpu_or_gpu_value", "arch", "configuration", "test_filter"}, OptionalKeys: []string{"extra_config"}, RecurseRoles: []string(nil)},
 	"Test":        {Keys: []string{"os", "compiler", "model", "cpu_or_gpu", "cpu_or_gpu_value", "arch", "configuration", "test_filter"}, OptionalKeys: []string{"extra_config"}, RecurseRoles: []string(nil)},
-	"Upload":      {Keys: []string(nil), OptionalKeys: []string(nil), RecurseRoles: []string{"Build", "Calmbench", "Perf", "Test"}},
+	"Upload":      {Keys: []string(nil), OptionalKeys: []string(nil), RecurseRoles: []string{"Build", "BuildStats", "Calmbench", "Perf", "Test"}},
 }
 
 var SEPARATOR_FROM_GIT = "-"
