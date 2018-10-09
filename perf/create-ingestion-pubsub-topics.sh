f65a4143457887a06f686d099d8874f6565e86f9
Kevin Lubick <kjlubick@google.com>
2018-10-09 08:25:46 -0400

    Add buildstats to perf ingestion
    Bug: skia:
    Change-Id: I047c639da939fc14738cae8185746cb0381152cc
    Reviewed-on: https://skia-review.googlesource.com/c/160740
    Reviewed-by: Joe Gregorio <jcgregorio@google.com>

--- perf/create-ingestion-pubsub-topics.sh
+++ perf/create-ingestion-pubsub-topics.sh
@@ -9,4 +9,7 @@ source ../kube/config.sh
 TOPIC=perf-ingestion-skia
 
 gcloud pubsub topics create ${TOPIC}
+gsutil notification create -f json -e OBJECT_FINALIZE -t projects/${PROJECT_ID}/topics/${TOPIC} -p buildstats-json-v1  gs://skia-perf
+gsutil notification create -f json -e OBJECT_FINALIZE -t projects/${PROJECT_ID}/topics/${TOPIC} -p ct/10k  gs://skia-perf
 gsutil notification create -f json -e OBJECT_FINALIZE -t projects/${PROJECT_ID}/topics/${TOPIC} -p nano-json-v1  gs://skia-perf
+gsutil notification create -f json -e OBJECT_FINALIZE -t projects/${PROJECT_ID}/topics/${TOPIC} -p task-duration  gs://skia-perf
