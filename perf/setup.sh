ac0da206654f3ca9a9c69c6e3617cab62afadcff
Joe Gregorio <jcgregorio@google.com>
2018-10-09 16:13:55 -0400

    Perf - Start standing up Android instance of Perf on kubernetes.
    Starting with perf-ingest.
    
    Also cleans out the Subscription member of the config which is unused.
    
    Bug: skia:
    Change-Id: I5178939c77888ef2255020fdca97d92b30f47269

--- perf/setup.sh
+++ perf/setup.sh
@@ -4,3 +4,4 @@
 
 # Skia instance.
 cbt --instance perf-bt createtable skia families=V:maxversions=1,S:maxversions=1,D:maxversions=1,H:maxversions=1
+cbt --instance perf-bt createtable android families=V:maxversions=1,S:maxversions=1,D:maxversions=1,H:maxversions=1
