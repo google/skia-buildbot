cd9bc4c3822cf25bc01094f2b831b2fd12b8382a
Eric Boren <borenet@google.com>
2018-10-10 08:57:25 -0400

    [infra] Playing at a replacement for recipes
    Bug: skia:
    Change-Id: I17119e1a54aac8d0e4dc4e41ad72e1ff9bdbcae1

---
+++ go/task_driver/tools/pubsub_listener/pubsub_listener.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"os"
+	"time"
+
+	"cloud.google.com/go/pubsub"
+	"go.skia.org/infra/go/common"
+	"go.skia.org/infra/go/sklog"
+	"go.skia.org/infra/go/task_driver"
+)
+
+var (
+	project = flag.String("project_id", "", "GCE Project ID")
+)
+
+func main() {
+	common.Init()
+	if *project == "" {
+		sklog.Fatal("--project_id is required.")
+	}
+	ctx := context.Background()
+	client, err := pubsub.NewClient(ctx, *project)
+	if err != nil {
+		sklog.Fatal(err)
+	}
+	topic := client.Topic(task_driver.PUBSUB_TOPIC)
+	if exists, err := topic.Exists(ctx); err != nil {
+		sklog.Fatal(err)
+	} else if !exists {
+		topic, err = client.CreateTopic(ctx, task_driver.PUBSUB_TOPIC)
+		if err != nil {
+			sklog.Fatal(err)
+		}
+	}
+	hostname, err := os.Hostname()
+	if err != nil {
+		sklog.Fatal(err)
+	}
+	subName := fmt.Sprintf("pubsub_listener_%s", hostname)
+	sub := client.Subscription(subName)
+	if exists, err := sub.Exists(ctx); err != nil {
+		sklog.Fatal(err)
+	} else if !exists {
+		subName := fmt.Sprintf("pubsub_listener_%s", hostname)
+		sub, err = client.CreateSubscription(ctx, subName, pubsub.SubscriptionConfig{
+			Topic:       topic,
+			AckDeadline: 10 * time.Second,
+		})
+		if err != nil {
+			sklog.Fatal(err)
+		}
+	}
+	for {
+		sklog.Infof("Waiting for messages.")
+		if err := sub.Receive(ctx, func(ctx context.Context, msg *pubsub.Message) {
+			var m task_driver.Message
+			if err := json.Unmarshal(msg.Data, &m); err != nil {
+				sklog.Fatal(err)
+			}
+			b, err := json.MarshalIndent(m, "", "  ")
+			if err != nil {
+				sklog.Fatal(err)
+			}
+			msg.Ack()
+			sklog.Infof("Received message: %s", string(b))
+		}); err != nil {
+			sklog.Fatal(err)
+		}
+	}
+}
