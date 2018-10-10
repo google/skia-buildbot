a6e16bcdebe3963dc8c5158a9be926743e8aaabe
Eric Boren <borenet@google.com>
2018-10-10 13:51:03 -0400

    [infra] Playing at a replacement for recipes
    Bug: skia:
    Change-Id: I17119e1a54aac8d0e4dc4e41ad72e1ff9bdbcae1

---
+++ go/task_driver/lib/checkout/checkout.go
@@ -0,0 +1,186 @@
+package checkout
+
+/*
+   Canned steps used for checking out code in task drivers.
+*/
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"go.skia.org/infra/go/exec"
+	"go.skia.org/infra/go/git"
+	"go.skia.org/infra/go/sklog"
+	"go.skia.org/infra/go/task_driver"
+	"go.skia.org/infra/go/task_driver/lib/os_steps"
+	"go.skia.org/infra/task_scheduler/go/db"
+)
+
+// ValidateCheckout returns true if the git checkout in the given destination
+// dir is in a reasonable state. Assumes that the dest dir exists.
+func ValidateCheckout(s *task_driver.Step, dest string, rs db.RepoState) (bool, error) {
+	if _, err := os_steps.Stat(s, filepath.Join(dest, ".git")); err == nil {
+		gd := git.GitDir(dest)
+
+		// Run "git status" and log the result, in case it's needed for
+		// forensics.
+		output, err := gd.Git(s.Ctx(), "status")
+		if err != nil {
+			// This is the first git command we've run in this
+			// checkout. It could fail for a number of reasons,
+			// including the checkout not actually being a checkout.
+			if strings.Contains(err.Error(), "not a git repository") {
+				sklog.Info("Dest dir is not a git repository.")
+				return false, nil
+			} else {
+				return false, err
+			}
+		}
+		sklog.Infof("Output of 'git status':\n%s", output)
+
+		// We have a git checkout, but it might not be the right one.
+		// Ensure that "origin" is set to the correct URL.
+		output, err = gd.Git(s.Ctx(), "remote", "-v")
+		if err != nil {
+			return false, err
+		}
+		// Strip out any empty lines.
+		lines := strings.Split(strings.TrimSpace(output), "\n")
+		remotes := make([]string, 0, len(lines))
+		for _, line := range lines {
+			if line != "" {
+				remotes = append(remotes, line)
+			}
+		}
+		// If there's no remote, this is not the checkout we're
+		// looking for.
+		if len(remotes) == 0 {
+			sklog.Infof("Repository has no remotes.")
+			return false, nil
+		} else {
+			// TODO(borenet): It's possible that someone
+			// (eg. bot_update) changed the remote URL to
+			// point to a local cache. It would be very
+			// wasteful to delete the checkout on every run.
+			// Should we try to change the remote URL in
+			// this case?
+
+			// Verify that origin is set to the correct URL.
+			for _, remote := range remotes {
+				fields := strings.Fields(remote)
+				if len(fields) != 3 {
+					return false, fmt.Errorf("Got unexpected output from 'git remote -v':\n%s", output)
+				}
+				if fields[0] == "origin" && fields[1] != rs.Repo {
+					sklog.Infof("Repository has remote 'origin' set to incorrect URL:\n%s", output)
+					return false, nil
+				}
+			}
+
+			// If we're still okay at this point, perform
+			// some sanity checks on the checkout.
+			if _, err := gd.Git(s.Ctx(), "rev-parse", "HEAD"); err != nil {
+				if strings.Contains(err.Error(), "ambiguous argument 'HEAD'") {
+					// Something strange is going on; take no chances.
+					sklog.Infof("Unable to obtain current HEAD: %s", err)
+					return false, nil
+				} else {
+					return false, err
+				}
+			}
+		}
+	} else if os.IsNotExist(err) {
+		// If the dest dir is present but has no .git dir,
+		// assume that it's in an unusable state and delete it.
+		sklog.Infof("No .git dir found in %s", filepath.Join(dest, ".git"))
+		return false, nil
+	} else {
+		return false, err
+	}
+	return true, nil
+}
+
+// EnsureGitCheckout obtains a clean git checkout of the given repo, at the
+// given commit, in the given destination dir.
+func EnsureGitCheckout(s *task_driver.Step, dest string, rs db.RepoState) (co *git.Checkout, err error) {
+	s = s.Step().Infra().Name("Ensure Git Checkout").Start()
+	defer s.Done(&err)
+
+	if !rs.Valid() {
+		return nil, fmt.Errorf("Invalid RepoState: %+v", rs)
+	}
+
+	// Is the dest dir present?
+	if _, err := os_steps.Stat(s, dest); err == nil {
+		// If the dest dir is present but not in a reasonable state,
+		// delete it.
+		okay, err := ValidateCheckout(s, dest, rs)
+		if err != nil {
+			return nil, err
+		}
+		if !okay {
+			sklog.Infof("Removing incompatible checkout in %s", dest)
+			if err := os_steps.RemoveAll(s, dest); err != nil {
+				return nil, err
+			}
+		}
+	}
+
+	// If the dest dir is not present, clone the repo into it.
+	if _, err := os_steps.Stat(s, dest); err != nil {
+		if os.IsNotExist(err) {
+			sklog.Infof("Cloning %s into %s", rs.Repo, dest)
+			if err := os_steps.MkdirAll(s, filepath.Dir(dest)); err != nil {
+				return nil, err
+			}
+			if _, err := exec.RunCwd(s.Ctx(), filepath.Dir(dest), "git", "clone", rs.Repo, dest); err != nil {
+				return nil, err
+			}
+		} else {
+			return nil, err
+		}
+	}
+	// Create the checkout object.
+	co = &git.Checkout{GitDir: git.GitDir(dest)}
+
+	// Now we know we have a git checkout of the correct repo in the dest
+	// dir, but it could be in any state. co.Update() will forcibly clean
+	// the checkout and update it to match upstream master.
+	sklog.Infof("Updating git checkout")
+	if err := co.Update(s.Ctx()); err != nil {
+		return nil, err
+	}
+
+	// Apply a patch, or reset to the requested commit.
+	if rs.IsTryJob() {
+		ref := rs.GetPatchRef()
+		sklog.Infof("Applying patch ref: %s", ref)
+		if err := co.FetchRefFromRepo(s.Ctx(), rs.Repo, ref); err != nil {
+			return nil, err
+		}
+		if _, err := co.Git(s.Ctx(), "reset", "--hard", "FETCH_HEAD"); err != nil {
+			return nil, err
+		}
+		if _, err := co.Git(s.Ctx(), "rebase", rs.Revision); err != nil {
+			return nil, err
+		}
+	} else {
+		sklog.Infof("Resetting to %s", rs.Revision)
+		if _, err := co.Git(s.Ctx(), "reset", "--hard", rs.Revision); err != nil {
+			return nil, err
+		}
+	}
+	return co, nil
+}
+
+// EnsureGitCheckoutWithDEPS obtains a clean git checkout of the given repo,
+// at the given commit, in the given workdir, and syncs the DEPS as well. The
+// checkout itself will be a subdirectory of the workdir.
+func EnsureGitCheckoutWithDEPS(s *task_driver.Step, workdir string, rs db.RepoState) (co *git.Checkout, err error) {
+	s = s.Step().Infra().Name("Ensure Git Checkout (with DEPS)").Start()
+	defer s.Done(&err)
+	// TODO(borenet): Implement this code using gclient or bot_update.
+	return nil, fmt.Errorf("NOT IMPLEMENTED")
+}
---
+++ go/task_driver/receiver.go
@@ -0,0 +1,296 @@
+package task_driver
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+
+	"cloud.google.com/go/logging"
+	"cloud.google.com/go/pubsub"
+	"github.com/golang/glog"
+	"go.skia.org/infra/go/sklog"
+	"go.skia.org/infra/go/util"
+)
+
+// Receiver is an interface used to implement arbitrary receivers of step
+// metadata, as steps are run.
+type Receiver interface {
+	// Handle the given message.
+	HandleMessage(*Message) error
+	LogStream(stepId string, logId string, severity string) (io.Writer, error)
+}
+
+// DebugReceiver just dumps the messages straight to the log (stdout/stderr, not
+// to Cloud Logging).
+type DebugReceiver struct{}
+
+// See documentation for Receiver interface.
+func (r *DebugReceiver) HandleMessage(m *Message) error {
+	switch m.Type {
+	case MSG_TYPE_STEP_STARTED:
+		glog.Infof("STEP_STARTED: %s", m.StepId)
+	case MSG_TYPE_STEP_FINISHED:
+		glog.Infof("STEP_FINISHED: %s", m.StepId)
+	case MSG_TYPE_STEP_DATA:
+		b, err := json.MarshalIndent(m.Data, "", " ")
+		if err != nil {
+			return err
+		}
+		glog.Infof("STEP_DATA: %s: %s", m.StepId, string(b))
+	default:
+		return fmt.Errorf("Invalid message type %s", m.Type)
+	}
+	return nil
+}
+
+// See documentation for Receiver interface.
+func (r *DebugReceiver) LogStream(stepId, logId, severity string) (io.Writer, error) {
+	sev := logging.ParseSeverity(severity)
+	if sev >= logging.Warning {
+		return os.Stderr, nil
+	}
+	return os.Stdout, nil
+}
+
+// StepReport is a struct used to collect information about a given step.
+type StepReport struct {
+	*StepResult
+	*StepProperties
+	Data  []interface{} `json:"data,omitempty"`
+	logs  map[string]*bytes.Buffer
+	Steps []*StepReport `json:"steps,omitempty"`
+}
+
+// ReportReceiver collects all messages and generates a report when requested.
+type ReportReceiver struct {
+	root   *StepReport
+	output string
+}
+
+// newReportReceiver returns a ReportReceiver instance.
+func newReportReceiver(output string) *ReportReceiver {
+	return &ReportReceiver{
+		output: output,
+	}
+}
+
+// Recurse through all steps, running the given function. If the function
+// returns false, recursion stops.
+func (s *StepReport) Recurse(fn func(*StepReport) bool) bool {
+	if keepGoing := fn(s); !keepGoing {
+		return false
+	}
+	for _, sub := range s.Steps {
+		if keepGoing := sub.Recurse(fn); !keepGoing {
+			return false
+		}
+	}
+	return true
+}
+
+// Find the step with the given ID in our list. This helps in case messages
+// arrive out of order.
+func (s *StepReport) findStep(id string) (*StepReport, error) {
+	var rv *StepReport
+	s.Recurse(func(s *StepReport) bool {
+		if s.Id == id {
+			rv = s
+			return false
+		}
+		return true
+	})
+	if rv != nil {
+		return rv, nil
+	}
+	return nil, fmt.Errorf("Unknown step ID %q", id)
+}
+
+// Find the step with the given ID in our list. This helps in case messages
+// arrive out of order.
+func (r *ReportReceiver) findStep(id string) (*StepReport, error) {
+	if r.root == nil {
+		return nil, fmt.Errorf("No steps!")
+	}
+	return r.root.findStep(id)
+}
+
+// See documentation for Receiver interface.
+func (r *ReportReceiver) HandleMessage(m *Message) error {
+	switch m.Type {
+	case MSG_TYPE_STEP_STARTED:
+		s := &StepReport{
+			StepProperties: m.Step,
+			logs:           map[string]*bytes.Buffer{},
+		}
+		if m.Step.Id == STEP_ID_ROOT {
+			r.root = s
+		} else {
+			parent, err := r.findStep(m.Step.Parent)
+			if err != nil {
+				return err
+			}
+			parent.Steps = append(parent.Steps, s)
+		}
+	case MSG_TYPE_STEP_FINISHED:
+		s, err := r.findStep(m.StepId)
+		if err != nil {
+			return err
+		}
+		s.StepResult = m.Result
+	case MSG_TYPE_STEP_DATA:
+		s, err := r.findStep(m.StepId)
+		if err != nil {
+			return err
+		}
+		s.Data = append(s.Data, m.Data)
+	}
+	return nil
+}
+
+// Write the report in JSON format to the given Writer.
+func (r *ReportReceiver) Report() error {
+	var w io.Writer
+	if r.output == "" {
+		return nil
+	} else if r.output == "-" {
+		w = os.Stdout
+	} else {
+		f, err := os.Create(r.output)
+		if err != nil {
+			return err
+		}
+		defer util.Close(f)
+		w = f
+	}
+
+	// Visit each step, attaching the final log contents to each logData
+	// instance.
+	r.root.Recurse(func(s *StepReport) bool {
+		for _, data := range s.Data {
+			d, ok := data.(*logData)
+			if ok {
+				if logBuf, ok := s.logs[d.Id]; ok {
+					d.Log = logBuf.String()
+				}
+			}
+		}
+		return true
+	})
+
+	// Dump JSON to the given Writer.
+	b, err := json.MarshalIndent(r.root, "", "  ")
+	if err != nil {
+		return err
+	}
+	_, err = w.Write(b)
+	return err
+}
+
+// See documentation for Receiver interface.
+func (r *ReportReceiver) LogStream(stepId, logId, severity string) (io.Writer, error) {
+	buf := bytes.NewBuffer([]byte{})
+	step, err := r.findStep(stepId)
+	if err != nil {
+		return nil, err
+	}
+	if _, ok := step.logs[logId]; ok {
+		return nil, fmt.Errorf("Step %s already has a log with ID %s", stepId, logId)
+	}
+	step.logs[logId] = buf
+	return buf, nil
+}
+
+// CloudLoggingReceiver is a Receiver which sends step metadata and logs to
+// Cloud Logging.
+type CloudLoggingReceiver struct {
+	// logger is a handle to the Logger used for the entire test run.
+	logger *logging.Logger
+}
+
+// Return a new CloudLoggingReceiver. This initializes Cloud Logging for the
+// entire test run.
+func NewCloudLoggingReceiver(logger *logging.Logger) (*CloudLoggingReceiver, error) {
+	return &CloudLoggingReceiver{
+		logger: logger,
+	}, nil
+}
+
+// See documentation for Receiver interface.
+func (r *CloudLoggingReceiver) HandleMessage(m *Message) error {
+	// TODO(borenet): When should we LogSync, or Flush? If the program
+	// crashes or is killed, we'll want to have already flushed the logs.
+	r.logger.Log(logging.Entry{
+		Payload:  m,
+		Severity: logging.ParseSeverity(sklog.DEBUG),
+	})
+	return nil
+}
+
+// cloudLogsWriter is an io.Writer which writes to Cloud Logging.
+type cloudLogsWriter struct {
+	logger   *logging.Logger
+	labels   map[string]string
+	severity logging.Severity
+}
+
+// See documentation for io.Writer.
+func (w *cloudLogsWriter) Write(b []byte) (int, error) {
+	// TODO(borenet): Should we buffer until we see a newline?
+	// TODO(borenet): When should we LogSync, or Flush? If the program
+	// crashes or is killed, we'll want to have already flushed the logs.
+	w.logger.Log(logging.Entry{
+		Labels:   w.labels,
+		Payload:  string(b),
+		Severity: w.severity,
+	})
+	return len(b), nil
+}
+
+// See documentation for Receiver interface.
+func (r *CloudLoggingReceiver) LogStream(stepId, logId, severity string) (io.Writer, error) {
+	return &cloudLogsWriter{
+		logger: r.logger,
+		labels: map[string]string{
+			"logId":  logId,
+			"stepId": stepId,
+		},
+		severity: logging.ParseSeverity(severity),
+	}, nil
+}
+
+// PubSubReceiver is a Receiver which sends step metadata through Cloud Pubsub.
+// It does not send logs.
+type PubSubReceiver struct {
+	topic *pubsub.Topic
+}
+
+// NewPubSubReceiver returns a PubSubReceiver instance.
+func NewPubSubReceiver(topic *pubsub.Topic) (Receiver, error) {
+	return &PubSubReceiver{
+		topic: topic,
+	}, nil
+}
+
+// See documentation for Receiver interface.
+func (r *PubSubReceiver) HandleMessage(m *Message) error {
+	b, err := json.Marshal(m)
+	if err != nil {
+		return fmt.Errorf("Failed to encode message: %s", err)
+	}
+	// Send the message synchronously.
+	ctx := context.Background()
+	_, err = r.topic.Publish(ctx, &pubsub.Message{
+		Data: b,
+	}).Get(ctx)
+	return err
+}
+
+// See documentation for Receiver interface.
+func (r *PubSubReceiver) LogStream(_, _, _ string) (io.Writer, error) {
+	// PubSubReceiver does not send logs.
+	return ioutil.Discard, nil
+}
