385279edaa8b7b45c7c26c52b0bb4970aee11258
Eric Boren <borenet@google.com>
2018-10-15 10:16:48 -0400

    [task driver] Rewrite in terms of context.Context
    Bug: skia:8459
    Change-Id: I376ac12455573c412821f5a17aca19df3d7e0120

---
+++ task_driver/examples/panic/panic.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"flag"
+
+	"go.skia.org/infra/task_driver/go/td"
+)
+
+/*
+   Task Driver panic example.
+
+   Run like this:
+
+   $ go run ./panic.go --logtostderr --project_id=skia-swarming-bots --task_name=basic_example -o - --local
+*/
+
+var (
+	// Required flags for all TaskDrivers.
+	projectId = flag.String("project_id", "", "ID of the Google Cloud project.")
+	taskId    = flag.String("task_id", "", "ID of this task.")
+	taskName  = flag.String("task_name", "", "Name of the task.")
+	output    = flag.String("o", "", "If provided, dump a JSON blob of step data to the given file. Prints to stdout if '-' is given.")
+	local     = flag.Bool("local", false, "True if running locally (as opposed to in production)")
+)
+
+func main() {
+	ctx := td.StartRun(projectId, taskId, taskName, output, local)
+	defer td.EndRun(ctx)
+
+	panic("this is a panic")
+}
