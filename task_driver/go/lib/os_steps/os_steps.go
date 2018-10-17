ed15c45f0450529b6fa51459c3c8425c436620c0
Eric Boren <borenet@google.com>
2018-10-17 09:50:15 -0400

    [task driver] Initial framework implementation
    Bug: skia:8459
    Change-Id: I376ac12455573c412821f5a17aca19df3d7e0120

---
+++ task_driver/go/lib/os_steps/os_steps.go
@@ -0,0 +1,37 @@
+package os_steps
+
+/*
+	Canned steps to be used for performing OS and filesystem interactions in task drivers.
+*/
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"go.skia.org/infra/task_driver/go/td"
+)
+
+// Stat is a wrapper for os.Stat.
+func Stat(ctx context.Context, path string) (os.FileInfo, error) {
+	var rv os.FileInfo
+	return rv, td.Do(ctx, td.Props(fmt.Sprintf("Stat %s", path)).Infra(), func(context.Context) error {
+		fi, err := os.Stat(path)
+		rv = fi
+		return err
+	})
+}
+
+// MkdirAll is a wrapper for os.MkdirAll.
+func MkdirAll(ctx context.Context, path string) (err error) {
+	return td.Do(ctx, td.Props(fmt.Sprintf("MkdirAll %s", path)).Infra(), func(context.Context) error {
+		return os.MkdirAll(path, os.ModePerm)
+	})
+}
+
+// RemoveAll is a wrapper for os.RemoveAll.
+func RemoveAll(ctx context.Context, path string) (err error) {
+	return td.Do(ctx, td.Props(fmt.Sprintf("RemoveAll %s", path)).Infra(), func(context.Context) error {
+		return os.RemoveAll(path)
+	})
+}
