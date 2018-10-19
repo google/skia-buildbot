1890c9e8920f1f3f0cb7587e8e8fc14f555ff9bc
Joe Gregorio <jcgregorio@google.com>
2018-10-19 10:06:44 -0400

    Perf - Add Alert display name to email subject.
    Bug: skia:8482
    Change-Id: Id64eb5ef43cf82cbd01c85a90574c8ea5f425db4

--- perf/go/notify/nofity_test.go
+++ perf/go/notify/nofity_test.go
@@ -29,12 +29,13 @@ func TestExampleSend(t *testing.T) {
 	e := &emailMock{}
 	n := New(e, "perf")
 	alert := &alerts.Config{
-		Alert: "someone@example.org, someother@example.com ",
+		Alert:       "someone@example.org, someother@example.com ",
+		DisplayName: "MyAlert",
 	}
 	err := n.ExampleSend(alert)
 	assert.NoError(t, err)
 	assert.Equal(t, []string{"someone@example.org", "someother@example.com"}, e.to)
 	assert.Equal(t, FROM_ADDRESS, e.from)
-	assert.Equal(t, "Regression found for \"Re-enable opList dependency tracking\"", e.subject)
+	assert.Equal(t, "MyAlert - Regression found for \"Re-enable opList dependency tracking\"", e.subject)
 	assert.Equal(t, "<b>Alert</b><br><br>\n<p>\n\tA Perf Regression has been found at:\n</p>\n<p style=\"padding: 1em;\">\n\t<a href=\"https://perf.skia.org/g/t/d261e1075a93677442fdf7fe72aba7e583863664\">https://perf.skia.org/g/t/d261e1075a93677442fdf7fe72aba7e583863664</a>\n</p>\n<p>\n  For:\n</p>\n<p style=\"padding: 1em;\">\n  <a href=\"https://skia.googlesource.com/skia/&#43;/d261e1075a93677442fdf7fe72aba7e583863664\">https://skia.googlesource.com/skia/&#43;/d261e1075a93677442fdf7fe72aba7e583863664</a>\n</p>\n<p>\n\tWith 10 matching traces.\n</p>", e.body)
 }
