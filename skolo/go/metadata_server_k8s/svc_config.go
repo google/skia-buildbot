70207a07bc4b91b5fe18308481e1292b0a6561fb
Stephan Altmueller <stephana@google.com>
2018-10-23 16:41:48 -0400

    wip: adapt metda_dataserver to kubernetes
    Bug: skia:
    Change-Id: I01ea61f0816b5949d5b8b1a4642490396a4b3561

---
+++ skolo/go/metadata_server_k8s/svc_config.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"go.skia.org/infra/go/sklog"
+)
+
+type ServiceAccountConf struct {
+	Project string   `json:"project"`
+	Email   string   `json:"email"`
+	KeyFile string   `json:"keyFile"`
+	Clients []string `json:"clients"`
+}
+
+func readConfigFile(confFile string) ([]*ServiceAccountConf, error) {
+	if confFile == "" {
+		return nil, sklog.FmtErrorf("Must provide a config file.")
+	}
+
+	return nil, nil
+}
