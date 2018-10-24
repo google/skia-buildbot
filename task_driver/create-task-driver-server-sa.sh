567afc18ac52f922ea453cccb0af7a900b8e38b9
Eric Boren <borenet@google.com>
2018-10-24 14:58:00 -0400

    [task driver] Add release files for task-driver-server
    Bug: skia:8459
    Change-Id: Ic751dbbaa5872b2f7bb0c3c69c158b61fd95fe52

---
+++ task_driver/create-task-driver-server-sa.sh
@@ -0,0 +1,22 @@
+#/bin/bash
+
+# Creates the service account used by the Task Driver server, and export a key
+# for it into the kubernetes cluster as a secret.
+
+set -e -x
+source ../kube/config.sh
+source ../bash/ramdisk.sh
+
+# New service account we will create.
+SA_NAME="task-driver"
+SA_EMAIL="${SA_NAME}@${PROJECT_SUBDOMAIN}.iam.gserviceaccount.com"
+
+cd /tmp/ramdisk
+
+gcloud --project=${PROJECT_ID} iam service-accounts create "${SA_NAME}" --display-name="Service account for Task Driver"
+gcloud projects add-iam-policy-binding skia-swarming-bots --member serviceAccount:${SA_EMAIL} --role roles/pubsub.admin --role roles/logging.admin
+gcloud beta iam service-accounts keys create ${SA_NAME}.json --iam-account="${SA_EMAIL}"
+
+kubectl create secret generic "${SA_NAME}" --from-file=key.json=${SA_NAME}.json
+
+cd -
