e994675ed0d2739c3fd3181df89caf3651e8ca1f
Kevin Lubick <kjlubick@google.com>
2018-10-25 10:53:06 -0400

    [power] Move to k8s
    Next step - remove (now broken) dependency on prom-alerts
    
    
    Bug: skia:
    Change-Id: I0dfde994e606d895cdeb2f7fced5f9f97e0162f0
    Reviewed-on: https://skia-review.googlesource.com/c/164940
    Reviewed-by: Joe Gregorio <jcgregorio@google.com>
    Commit-Queue: Kevin Lubick <kjlubick@google.com>

--- power/create-power-controller-sa.sh
+++ power/create-power-controller-sa.sh
@@ -1,20 +1,21 @@
 #/bin/bash
 
-# Creates the service account used by the Leasing server, and export a key for
+# Creates the service account used by the power-controller server, and export a key for
 # it into the kubernetes cluster as a secret.
 # Afterwards, the steps on https://bugs.chromium.org/p/skia/issues/detail?id=8248#c5 are
-# needed to hook the service account up to swarming.
+# needed to hook the service account up to swarming (don't need task-scheduling
+# permissions, just viewing ones).
 
 set -e -x
 source ../kube/config.sh
 source ../bash/ramdisk.sh
 
 # New service account we will create.
-SA_NAME="skia-leasing"
+SA_NAME="skia-power-controller"
 
 cd /tmp/ramdisk
 
-gcloud --project=${PROJECT_ID} iam service-accounts create "${SA_NAME}" --display-name="Service account for Leasing Server"
+gcloud --project=${PROJECT_ID} iam service-accounts create "${SA_NAME}" --display-name="Service account for Skia Power Controller"
 
 gcloud beta iam service-accounts keys create ${SA_NAME}.json --iam-account="${SA_NAME}@${PROJECT_SUBDOMAIN}.iam.gserviceaccount.com"
 
