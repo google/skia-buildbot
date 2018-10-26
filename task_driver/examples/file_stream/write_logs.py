4a403ba6444fc8e492469f42319597e5de4bf5cb
Eric Boren <borenet@google.com>
2018-10-26 13:53:52 -0400

    [task driver] Add FileStream support
    Continually reads from a file and streams its contents as logs. I expect
    to use this for DM's verbose log, although I'm not sure how it will work
    on Windows, and we may be limited to copying the log file from
    Android/iOS/ChromeOS devices and "streaming" it after the fact.
    
    Bug: skia:8459
    Change-Id: Idf103a01a3d4956aa107bce45ee2d404f565f714

---
+++ task_driver/examples/file_stream/write_logs.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import sys
+import time
+
+with open(sys.argv[1], 'w') as f:
+  print 'Writing verbose logs to %s' % sys.argv[1]
+  for i in range(10):
+    f.write('%d\n' % i)
+    f.flush()
+    time.sleep(1)
