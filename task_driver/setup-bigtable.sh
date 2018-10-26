d7f66728dbb006a8ef92ae7337e71f2dc725f03c
Eric Boren <borenet@google.com>
2018-10-26 16:01:40 -0400

    [task driver] Add "scribe" app to retrieve logs for a task driver
    Bug: skia:
    Change-Id: Ie90d2987e663d53662b062938f24a71e4a2a7c16

--- task_driver/setup-bigtable.sh
+++ task_driver/setup-bigtable.sh
@@ -4,9 +4,15 @@ set -e -x
 
 PROJECT="${PROJECT:-skia-public}"
 INSTANCE="${INSTANCE:-task-driver}"
-TABLE="${TABLE:-task-driver-runs}"
-COLUMN_FAMILY="${COLUMN_FAMILY:-MSGS}"
 
 go get -u cloud.google.com/go/bigtable/cmd/cbt
+
+TABLE="task-driver-runs"
+COLUMN_FAMILY="MSGS"
+cbt --project=${PROJECT} --instance=${INSTANCE} createtable ${TABLE}
+cbt --project=${PROJECT} --instance=${INSTANCE} createfamily ${TABLE} ${COLUMN_FAMILY}
+
+TABLE="task-driver-logs"
+COLUMN_FAMILY="LOGS"
 cbt --project=${PROJECT} --instance=${INSTANCE} createtable ${TABLE}
 cbt --project=${PROJECT} --instance=${INSTANCE} createfamily ${TABLE} ${COLUMN_FAMILY}
