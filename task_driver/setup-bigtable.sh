ee61a024b26de68f7f2733729942548b75d762bd
Eric Boren <borenet@google.com>
2018-10-29 11:09:29 -0400

    [task driver] Add log entries to BigTable, task-driver-server collects and serves logs
    Bug: skia:
    Change-Id: Ie90d2987e663d53662b062938f24a71e4a2a7c16

--- task_driver/setup-bigtable.sh
+++ task_driver/setup-bigtable.sh
@@ -3,10 +3,28 @@
 set -e -x
 
 PROJECT="${PROJECT:-skia-public}"
-INSTANCE="${INSTANCE:-task-driver}"
-TABLE="${TABLE:-task-driver-runs}"
-COLUMN_FAMILY="${COLUMN_FAMILY:-MSGS}"
+BIGTABLE_INSTANCE="${BIGTABLE_INSTANCE:-task-driver}"
+PUBSUB_TOPIC="${PUBSUB_TOPIC:-task-driver-logs}"
+LOG_NAME="${LOG_NAME:-task-driver}"
 
+# Set up BigTable tables and column families.
 go get -u cloud.google.com/go/bigtable/cmd/cbt
-cbt --project=${PROJECT} --instance=${INSTANCE} createtable ${TABLE}
-cbt --project=${PROJECT} --instance=${INSTANCE} createfamily ${TABLE} ${COLUMN_FAMILY}
+
+BIGTABLE_TABLE="task-driver-runs"
+BIGTABLE_COLUMN_FAMILY="MSGS"
+cbt --project=${PROJECT} --instance=${BIGTABLE_INSTANCE} createtable ${BIGTABLE_TABLE}
+cbt --project=${PROJECT} --instance=${BIGTABLE_INSTANCE} createfamily ${BIGTABLE_TABLE} ${BIGTABLE_COLUMN_FAMILY}
+
+BIGTABLE_TABLE="task-driver-logs"
+BIGTABLE_COLUMN_FAMILY="LOGS"
+cbt --project=${PROJECT} --instance=${BIGTABLE_INSTANCE} createtable ${BIGTABLE_TABLE}
+cbt --project=${PROJECT} --instance=${BIGTABLE_INSTANCE} createfamily ${BIGTABLE_TABLE} ${BIGTABLE_COLUMN_FAMILY}
+
+# Set up logs export to pubsub.
+gcloud --project=${PROJECT} logging sinks create task-driver-logs-to-pubsub \
+    pubsub.googleapis.com/projects/${PROJECT}/topics/${PUBSUB_TOPIC} \
+    --log-filter="logName=\"projects/${PROJECT}/logs/${LOG_NAME}\""
+
+PUBSUB_SERVICE_ACCOUNT=$(gcloud logging --project=${PROJECT} sinks describe task-driver-logs-to-pubsub | grep writerIdentity | sed -e 's/writerIdentity: serviceAccount://g')
+
+gcloud projects add-iam-policy-binding ${PROJECT} --member serviceAccount:${PUBSUB_SERVICE_ACCOUNT} --role roles/pubsub.publisher
