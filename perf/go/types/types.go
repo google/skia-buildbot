9666f0407029e7328a7b62624694314453a8a210
Joe Gregorio <jcgregorio@google.com>
2018-11-05 15:29:57 -0500

    Perf - Start moving clustering over to LastN.
    Bug: skia:
    Change-Id: Icca70dca439b02aed61d73b7e0019a90244ea305

--- perf/go/types/types.go
+++ perf/go/types/types.go
@@ -1,6 +1,10 @@
 package types
 
-import "go.skia.org/infra/go/vec32"
+import (
+	"fmt"
+
+	"go.skia.org/infra/go/vec32"
+)
 
 // Trace is just a slice of float32s.
 type Trace []float32
@@ -15,3 +19,28 @@ type TraceSet map[string]Trace
 
 // Progress is a func that is called to update the progress on a computation.
 type Progress func(step, totalSteps int)
+
+type ClusterAlgo string
+
+// ClusterAlgo constants.
+//
+// Update algo-select-sk if this enum is changed.
+const (
+	KMEANS_ALGO  ClusterAlgo = "kmeans"  // Cluster traces using k-means clustering on their shapes.
+	STEPFIT_ALGO ClusterAlgo = "stepfit" // Look at each trace individually and determing if it steps up or down.
+	TAIL_ALGO    ClusterAlgo = "tail"    // Whether a trace has a jumping tail (a step in the end)
+)
+
+var (
+	AllClusterAlgos = []ClusterAlgo{KMEANS_ALGO, STEPFIT_ALGO, TAIL_ALGO}
+)
+
+func ToClusterAlgo(s string) (ClusterAlgo, error) {
+	ret := ClusterAlgo(s)
+	for _, c := range AllClusterAlgos {
+		if c == ret {
+			return ret, nil
+		}
+	}
+	return ret, fmt.Errorf("%q is not a valid ClusterAlgo, must be a value in %v", s, AllClusterAlgos)
+}
