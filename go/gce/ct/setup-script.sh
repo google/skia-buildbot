d98d4590446aa60baf7737046397251fd519a2d7
Ravi Mistry <rmistry@google.com>
2018-11-05 08:41:37 -0500

    [CT] numpy and pandas are now required packages on CT swarming bots
    Bug: skia:8524
    Change-Id: I9d2846c5b371ae196e5d1dc57869b9ed37d6cada
    Reviewed-on: https://skia-review.googlesource.com/c/168220
    Reviewed-by: Ravi Mistry <rmistry@google.com>
    Commit-Queue: Ravi Mistry <rmistry@google.com>

--- go/gce/ct/setup-script.sh
+++ go/gce/ct/setup-script.sh
@@ -50,7 +50,7 @@ make
 sudo make install
 # Install psutil in Python 2.7.11. See skbug.com/7293 for context.
 sudo /usr/local/lib/python2.7.11/bin/python -m ensurepip --upgrade
-sudo /usr/local/lib/python2.7.11/bin/pip install psutil httplib2 mock
+sudo /usr/local/lib/python2.7.11/bin/pip install psutil httplib2 mock numpy pandas
 # Remove /usr/bin/ninja it interferences with depot_tools sometimes.
 sudo rm /usr/bin/ninja
 
