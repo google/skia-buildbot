7b982c8fe07eaf1dc17eb6b164a64284d2543481
Joe Gregorio <jcgregorio@google.com>
2018-11-05 21:31:59 -0500

    Perf - Start moving clustering over to LastN.
    Bug: skia:
    Change-Id: Icca70dca439b02aed61d73b7e0019a90244ea305

--- perf/go/regression/stepfit_test.go
+++ perf/go/regression/stepfit_test.go
@@ -1,4 +1,4 @@
-package clustering2
+package regression
 
 import (
 	"math/rand"
@@ -6,39 +6,19 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/assert"
+	"go.skia.org/infra/go/ds"
+	"go.skia.org/infra/go/ds/testutil"
 	"go.skia.org/infra/go/paramtools"
 	"go.skia.org/infra/go/testutils"
-	"go.skia.org/infra/perf/go/ctrace2"
 	"go.skia.org/infra/perf/go/dataframe"
-	"go.skia.org/infra/perf/go/kmeans"
 	"go.skia.org/infra/perf/go/types"
 )
 
-func TestParamSummaries(t *testing.T) {
-	testutils.SmallTest(t)
-	obs := []kmeans.Clusterable{
-		ctrace2.NewFullTrace(",arch=x86,config=8888,", []float32{1, 2}, 0.001),
-		ctrace2.NewFullTrace(",arch=x86,config=565,", []float32{2, 3}, 0.001),
-		ctrace2.NewFullTrace(",arch=x86,config=565,", []float32{3, 2}, 0.001),
-	}
-	expected := map[string][]ValueWeight{
-		"arch": {
-			{"x86", 26},
-		},
-		"config": {
-			{"565", 21},
-			{"8888", 16},
-		},
-	}
-	assert.Equal(t, expected, getParamSummaries(obs))
-
-	obs = []kmeans.Clusterable{}
-	expected = map[string][]ValueWeight{}
-	assert.Equal(t, expected, getParamSummaries(obs))
-}
-
-func TestCalcCusterSummaries(t *testing.T) {
+func TestStepFit(t *testing.T) {
 	testutils.LargeTest(t)
+
+	cleanup := testutil.InitDatastore(t, ds.REGRESSION)
+	defer cleanup()
 
 	rand.Seed(1)
 	now := time.Now()
@@ -82,24 +62,11 @@ func TestCalcCusterSummaries(t *testing.T) {
 	for key := range df.TraceSet {
 		df.ParamSet.AddParamsFromKey(key)
 	}
-	sum, err := CalculateClusterSummaries(df, 4, 0.01, nil, 50)
+
+	sum, err := StepFit(df, 4, 0.01, nil, 50)
 	assert.NoError(t, err)
 	assert.NotNil(t, sum)
-	assert.Equal(t, 2, len(sum.Clusters))
+	assert.Equal(t, 1, len(sum.Clusters))
 	assert.Equal(t, df.Header[2], sum.Clusters[0].StepPoint)
 	assert.Equal(t, 2, len(sum.Clusters[0].Keys))
-	assert.Equal(t, 2, len(sum.Clusters[1].Keys))
-}
-
-func TestCalcCusterSummariesDegenerate(t *testing.T) {
-	testutils.SmallTest(t)
-	rand.Seed(1)
-	df := &dataframe.DataFrame{
-		TraceSet: types.TraceSet{},
-		Header:   []*dataframe.ColumnHeader{},
-		ParamSet: paramtools.ParamSet{},
-		Skip:     0,
-	}
-	_, err := CalculateClusterSummaries(df, 4, 0.01, nil, 50)
-	assert.Error(t, err)
 }
