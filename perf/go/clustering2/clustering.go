fc60cda411c0cb632c183c5ed0ffbb5e6dfee028
Joe Gregorio <jcgregorio@google.com>
2018-11-05 21:46:55 -0500

    Perf - Re-org the regression and clustering code.
    Bug: skia:
    Change-Id: Icca70dca439b02aed61d73b7e0019a90244ea305

--- perf/go/clustering2/clustering.go
+++ perf/go/clustering2/clustering.go
@@ -8,12 +8,10 @@ import (
 
 	"go.skia.org/infra/go/query"
 	"go.skia.org/infra/go/sklog"
-	"go.skia.org/infra/go/vec32"
 	"go.skia.org/infra/perf/go/config"
 	"go.skia.org/infra/perf/go/ctrace2"
 	"go.skia.org/infra/perf/go/dataframe"
 	"go.skia.org/infra/perf/go/kmeans"
-	"go.skia.org/infra/perf/go/shortcut2"
 	"go.skia.org/infra/perf/go/stepfit"
 )
 
@@ -67,8 +65,8 @@ type ClusterSummary struct {
 	Num int `json:"num"`
 }
 
-// newClusterSummary returns a new ClusterSummary.
-func newClusterSummary() *ClusterSummary {
+// NewClusterSummary returns a new ClusterSummary.
+func NewClusterSummary() *ClusterSummary {
 	return &ClusterSummary{
 		Keys:           []string{},
 		ParamSummaries: map[string][]ValueWeight{},
@@ -118,16 +116,16 @@ func getParamSummaries(cluster []kmeans.Clusterable) map[string][]ValueWeight {
 		}
 		keys = append(keys, key)
 	}
-	return getParamSummariesForKeys(keys)
+	return GetParamSummariesForKeys(keys)
 }
 
-// getParamSummariesForKeys summarizes all the parameters for all observations in a
+// GetParamSummariesForKeys summarizes all the parameters for all observations in a
 // cluster.
 //
 // The return value is an array of []ValueWeight's, one []ValueWeight per
 // parameter. The members of each []ValueWeight are sorted by the Weight, with
 // higher Weight's first.
-func getParamSummariesForKeys(keys []string) map[string][]ValueWeight {
+func GetParamSummariesForKeys(keys []string) map[string][]ValueWeight {
 	// For each cluster member increment each parameters count.
 	//        map[key]   map[value] count
 	counts := map[string]map[string]int{}
@@ -204,7 +202,7 @@ func getClusterSummaries(observations []kmeans.Clusterable, centroids []kmeans.C
 			numSampleKeys = config.MAX_SAMPLE_TRACES_PER_CLUSTER
 		}
 		stepFit := stepfit.GetStepFitAtMid(centroids[i].(*ctrace2.ClusterableTrace).Values, interesting)
-		summary := newClusterSummary()
+		summary := NewClusterSummary()
 		summary.ParamSummaries = getParamSummaries(cluster)
 		summary.StepFit = stepFit
 		summary.StepPoint = header[stepFit.TurningPoint]
@@ -236,154 +234,33 @@ func getClusterSummaries(observations []kmeans.Clusterable, centroids []kmeans.C
 
 type Progress func(totalError float64)
 
-// Please see go/calmbench-trace-tail for how we filter the tail
-func FilterTail(trace []float32, quantile float64, multiplier float64, slack float64) float32 {
-	if len(trace) == 0 {
-		return 0
-	}
-	tail := trace[len(trace)-1]
-	if tail == vec32.MISSING_DATA_SENTINEL {
-		return 0
-	}
-
-	sortedTrace := make([]float64, 0, len(trace))
-	for i := 0; i < len(trace)-1; i++ {
-		if trace[i] != vec32.MISSING_DATA_SENTINEL {
-			sortedTrace = append(sortedTrace, float64(trace[i]))
-		}
-	}
-	sort.Float64s(sortedTrace)
-
-	n := len(sortedTrace)
-	p := int(math.Floor(float64(n-1) * quantile))
-	lowerBound := math.Min(0, sortedTrace[p])
-	upperBound := math.Max(0, sortedTrace[n-1-p])
-
-	tail64 := float64(tail)
-	if tail64 > upperBound*multiplier+slack || tail64 < lowerBound*multiplier-slack {
-		return tail
-	} else {
-		return 0
-	}
-}
-
-// shortcutFromKeys stores a new shortcut for each cluster based on its Keys.
-func shortcutFromKeys(summary *ClusterSummaries) error {
-	var err error
-	for _, cs := range summary.Clusters {
-		if cs.Shortcut, err = shortcut2.InsertShortcut(&shortcut2.Shortcut{Keys: cs.Keys}); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
 // CalculateClusterSummaries runs k-means clustering over the trace shapes.
-func CalculateClusterSummaries(df *dataframe.DataFrame, k int, stddevThreshold float32, progress Progress, interesting float32, algo ClusterAlgo) (*ClusterSummaries, error) {
-	if algo == KMEANS_ALGO {
-		// Convert the DataFrame to a slice of kmeans.Clusterable.
-		observations := make([]kmeans.Clusterable, 0, len(df.TraceSet))
-		for key, trace := range df.TraceSet {
-			observations = append(observations, ctrace2.NewFullTrace(key, trace, stddevThreshold))
-		}
-		if len(observations) == 0 {
-			return nil, fmt.Errorf("Zero traces in the DataFrame.")
-		}
-
-		// Create K starting centroids.
-		centroids := chooseK(observations, k)
-		lastTotalError := 0.0
-		for i := 0; i < MAX_KMEANS_ITERATIONS; i++ {
-			centroids = kmeans.Do(observations, centroids, ctrace2.CalculateCentroid)
-			totalError := kmeans.TotalError(observations, centroids)
-			if progress != nil {
-				progress(totalError)
-			}
-			if math.Abs(totalError-lastTotalError) < KMEAN_EPSILON {
-				break
-			}
-			lastTotalError = totalError
-		}
-		clusterSummaries := getClusterSummaries(observations, centroids, df.Header, interesting)
-		clusterSummaries.K = k
-		clusterSummaries.StdDevThreshold = stddevThreshold
-		if err := shortcutFromKeys(clusterSummaries); err != nil {
-			return nil, fmt.Errorf("Failed to write shortcut for keys: %s", err)
-		}
-		return clusterSummaries, nil
-	} else if algo == STEPFIT_ALGO || algo == TAIL_ALGO {
-
-		low := newClusterSummary()
-		high := newClusterSummary()
-		// Normalize each trace and then run through stepfit. If interesting then
-		// add to appropriate cluster.
-		count := 0
-		for key, trace := range df.TraceSet {
-			count++
-			if count%10000 == 0 {
-				sklog.Infof("stepfit count: %d", count)
-			}
-			t := vec32.Dup(trace)
-			vec32.Norm(t, stddevThreshold)
-			sf := stepfit.GetStepFitAtMid(t, interesting)
-
-			isLow := sf.Status == stepfit.LOW
-			isHigh := sf.Status == stepfit.HIGH
-			if algo == TAIL_ALGO {
-				quantile := 1 / float64(interesting)
-				slack := float64(k) * 0.01
-				const MULTIPLIER = 2 // TODO(liyuqian): Make this configurable
-
-				tail := FilterTail(trace, quantile, MULTIPLIER, slack)
-				isLow = tail < 0
-				isHigh = tail > 0
-				sf.TurningPoint = len(trace) - 1
-			}
-
-			// If stepfit is at the middle and if it is a step up or down.
-			if isLow {
-				if low.StepFit.Status == "" {
-					low.StepFit = sf
-					low.StepFit.Status = stepfit.LOW // for TAIL_ALGO
-					low.StepPoint = df.Header[sf.TurningPoint]
-					low.Centroid = vec32.Dup(trace)
-				}
-				low.Num++
-				if low.Num < config.MAX_SAMPLE_TRACES_PER_CLUSTER {
-					low.Keys = append(low.Keys, key)
-				}
-			} else if isHigh {
-				if high.StepFit.Status == "" {
-					high.StepFit = sf
-					high.StepFit.Status = stepfit.HIGH // for TAIL_ALGO
-					high.StepPoint = df.Header[sf.TurningPoint]
-					high.Centroid = vec32.Dup(trace)
-				}
-				high.Num++
-				if high.Num < config.MAX_SAMPLE_TRACES_PER_CLUSTER {
-					high.Keys = append(high.Keys, key)
-				}
-			}
-		}
-		sklog.Infof("Found LOW: %d HIGH: %d", low.Num, high.Num)
-		ret := &ClusterSummaries{
-			Clusters:        []*ClusterSummary{},
-			K:               k,
-			StdDevThreshold: stddevThreshold,
-		}
-		if low.Num > 0 {
-			low.ParamSummaries = getParamSummariesForKeys(low.Keys)
-			ret.Clusters = append(ret.Clusters, low)
-		}
-		if high.Num > 0 {
-			high.ParamSummaries = getParamSummariesForKeys(high.Keys)
-			ret.Clusters = append(ret.Clusters, high)
-		}
-		if err := shortcutFromKeys(ret); err != nil {
-			return nil, fmt.Errorf("Failed to write shortcut for keys: %s", err)
-		}
-		return ret, nil
-	} else {
-		return nil, fmt.Errorf("Unknown clustering algorithm: %s", algo)
+func CalculateClusterSummaries(df *dataframe.DataFrame, k int, stddevThreshold float32, progress Progress, interesting float32) (*ClusterSummaries, error) {
+	// Convert the DataFrame to a slice of kmeans.Clusterable.
+	observations := make([]kmeans.Clusterable, 0, len(df.TraceSet))
+	for key, trace := range df.TraceSet {
+		observations = append(observations, ctrace2.NewFullTrace(key, trace, stddevThreshold))
 	}
+	if len(observations) == 0 {
+		return nil, fmt.Errorf("Zero traces in the DataFrame.")
+	}
+
+	// Create K starting centroids.
+	centroids := chooseK(observations, k)
+	lastTotalError := 0.0
+	for i := 0; i < MAX_KMEANS_ITERATIONS; i++ {
+		centroids = kmeans.Do(observations, centroids, ctrace2.CalculateCentroid)
+		totalError := kmeans.TotalError(observations, centroids)
+		if progress != nil {
+			progress(totalError)
+		}
+		if math.Abs(totalError-lastTotalError) < KMEAN_EPSILON {
+			break
+		}
+		lastTotalError = totalError
+	}
+	clusterSummaries := getClusterSummaries(observations, centroids, df.Header, interesting)
+	clusterSummaries.K = k
+	clusterSummaries.StdDevThreshold = stddevThreshold
+	return clusterSummaries, nil
 }
