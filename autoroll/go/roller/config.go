fbd4dfc7d580d7405b12eac62aadc407754f1306
Yuqian Li <liyuqian@google.com>
2018-11-09 14:14:46 -0800

    Fix typo githubMergeMethod -> githubMergeMethodURL
    Bug: skia:
    Change-Id: I127cdcf72022c9d66f382afb10ee99fefc0d7256
    Reviewed-on: https://skia-review.googlesource.com/c/170295
    Auto-Submit: Yuqian Li <liyuqian@google.com>
    Commit-Queue: Ravi Mistry <rmistry@google.com>
    Reviewed-by: Ravi Mistry <rmistry@google.com>

--- autoroll/go/roller/config.go
+++ autoroll/go/roller/config.go
@@ -160,7 +160,7 @@ type AutoRollerConfig struct {
 	GithubRepoName       string   `json:"githubRepoName,omitempty"`
 	GithubChecksNum      int      `json:"githubChecksNum,omitempty"`
 	GithubChecksWaitFor  []string `json:"githubChecksWaitFor,omitempty"`
-	GithubMergeMethodURL string   `json:"githubMergeMethod,omitempty"`
+	GithubMergeMethodURL string   `json:"githubMergeMethodURL,omitempty"`
 
 	// RepoManager configs. Exactly one must be provided.
 	AFDORepoManager           *repo_manager.AFDORepoManagerConfig           `json:"afdoRepoManager,omitempty"`
