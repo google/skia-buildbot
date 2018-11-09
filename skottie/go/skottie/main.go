f3af691a3e5b2f986f75e0dbe0cd27428ef62be5
Kevin Lubick <kjlubick@google.com>
2018-11-09 14:30:56 -0500

    [skottie] Make frontend use CanvasKit
    Auto push will be broken until a change in
    Skia's cloudbuild.yaml can be landed. It will likely
    be similar behavior to jsfiddle.
    
    Bug: skia:
    Change-Id: I1a44c8d0acbb62bade64210a539a4b8e806fd331
    Reviewed-on: https://skia-review.googlesource.com/c/169700
    Reviewed-by: Joe Gregorio <jcgregorio@google.com>

--- skottie/go/skottie/main.go
+++ skottie/go/skottie/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bytes"
 	"context"
 	"crypto/md5"
 	"encoding/json"
@@ -10,9 +9,7 @@ import (
 	"fmt"
 	"html/template"
 	"io"
-	"io/ioutil"
 	"net/http"
-	"os"
 	"path/filepath"
 	"runtime"
 	"strings"
@@ -22,7 +19,6 @@ import (
 	"go.skia.org/infra/go/allowed"
 	"go.skia.org/infra/go/auth"
 	"go.skia.org/infra/go/common"
-	"go.skia.org/infra/go/exec"
 	"go.skia.org/infra/go/httputils"
 	"go.skia.org/infra/go/login"
 	"go.skia.org/infra/go/sklog"
@@ -43,8 +39,8 @@ var (
 	port         = flag.String("port", ":8000", "HTTP service address (e.g., ':8000')")
 	promPort     = flag.String("prom_port", ":20000", "Metrics service address (e.g., ':10110')")
 	resourcesDir = flag.String("resources_dir", "", "The directory to find templates, JS, and CSS files. If blank the current directory will be used.")
-	skottieTool  = flag.String("skottie_tool", "", "Absolute path to the skottie_tool executable.")
-	versionFile  = flag.String("version_file", "/etc/skia-prod/VERSION", "The full path of the Skia VERSION file.")
+	skottieTool  = flag.String("skottie_tool", "", "[deprecated/unused]Absolute path to the skottie_tool executable.")
+	versionFile  = flag.String("version_file", "[deprecated/unused]/etc/skia-prod/VERSION", "The full path of the Skia VERSION file.")
 )
 
 var (
@@ -55,7 +51,6 @@ var (
 type Server struct {
 	bucket    *storage.BucketHandle
 	templates *template.Template
-	version   string
 }
 
 func New() (*Server, error) {
@@ -74,12 +69,6 @@ func New() (*Server, error) {
 		return nil, fmt.Errorf("Problem creating storage client: %s", err)
 	}
 
-	b, err := ioutil.ReadFile(*versionFile)
-	if err != nil {
-		return nil, fmt.Errorf("Failed to read Skia version: %s", err)
-	}
-	version := strings.TrimSpace(string(b))
-
 	if *lockedDown {
 		allow := allowed.NewAllowedFromList([]string{"google.com"})
 		login.InitWithAllow(*port, *local, nil, nil, allow)
@@ -91,8 +80,7 @@ func New() (*Server, error) {
 	}
 
 	srv := &Server{
-		bucket:  storageClient.Bucket(bucket),
-		version: version,
+		bucket: storageClient.Bucket(bucket),
 	}
 	srv.loadTemplates()
 	return srv, nil
@@ -104,41 +92,12 @@ func (srv *Server) loadTemplates() {
 	))
 }
 
-// createWebm runs ffmpeg over the images in the given dir.
-func createWebm(ctx context.Context, dir string, fps float32) error {
-	// ffmpeg -r $FPS -pattern_type glob -i '*.png' -c:v libvpx-vp9 -lossless 1 lottie.webm
-	name := "ffmpeg"
-	args := []string{
-		"-r", fmt.Sprintf("%f", fps),
-		"-pattern_type", "glob", "-i", "*.png",
-		"-c:v", "libvpx-vp9",
-		"-lossless", "1",
-		"lottie.webm",
-	}
-	output := &bytes.Buffer{}
-	runCmd := &exec.Command{
-		Name:      name,
-		Args:      args,
-		Dir:       dir,
-		LogStderr: true,
-		Stdout:    output,
-	}
-	if err := exec.Run(ctx, runCmd); err != nil {
-		return fmt.Errorf("ffmpeg failed %#v: %s", *runCmd, err)
-	}
-
-	return nil
-}
-
 func (srv *Server) mainHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	if *local {
 		srv.loadTemplates()
 	}
-	context := map[string]string{
-		"Version": srv.version,
-	}
-	if err := srv.templates.ExecuteTemplate(w, "index.html", context); err != nil {
+	if err := srv.templates.ExecuteTemplate(w, "index.html", nil); err != nil {
 		sklog.Errorf("Failed to expand template: %s", err)
 	}
 }
@@ -154,21 +113,6 @@ func (srv *Server) jsonHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	if _, err = io.Copy(w, reader); err != nil {
 		httputils.ReportError(w, r, err, "Failed to write JSON file.")
-		return
-	}
-}
-
-func (srv *Server) webmHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "video/webm")
-	hash := mux.Vars(r)["hash"]
-	path := strings.Join([]string{hash, "lottie.webm"}, "/")
-	reader, err := srv.bucket.Object(path).NewReader(r.Context())
-	if err != nil {
-		httputils.ReportError(w, r, err, "Can't load file from GCS")
-		return
-	}
-	if _, err = io.Copy(w, reader); err != nil {
-		httputils.ReportError(w, r, err, "Failed to write webm file.")
 		return
 	}
 }
@@ -214,40 +158,9 @@ func (srv *Server) uploadHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	source, err := ioutil.TempDir("", "source")
-	if err != nil {
-		httputils.ReportError(w, r, err, "Can't create temp space.")
-		return
-	}
-	defer util.RemoveAll(source)
-	dest, err := ioutil.TempDir("", "dest")
-	if err != nil {
-		httputils.ReportError(w, r, err, "Can't create temp space.")
-		return
-	}
-	defer util.RemoveAll(dest)
-	sourceFullPath := filepath.Join(source, "lottie.json")
-	destFullPath := filepath.Join(dest, "lottie.webm")
-
 	b, err := json.Marshal(req.Lottie)
 	if err != nil {
 		httputils.ReportError(w, r, err, "Can't re-encode lottie file.")
-		return
-	}
-	if err := ioutil.WriteFile(sourceFullPath, b, 0644); err != nil {
-		httputils.ReportError(w, r, err, "Can't write file.")
-		return
-	}
-	// Run through skottie_tool.
-	toolResults, err := exec.RunSimple(ctx, fmt.Sprintf("%s --input %s --writePath %s --width %d --height %d --fps %f", *skottieTool, sourceFullPath, dest, req.Width, req.Height, req.FPS))
-	if err != nil {
-		sklog.Warningf("Failed running: %q", toolResults)
-		httputils.ReportError(w, r, err, "Failed running skottie_tool.")
-		return
-	}
-	// Run results of that through ffmpeg to create webm.
-	if err := createWebm(ctx, dest, req.FPS); err != nil {
-		httputils.ReportError(w, r, err, "Failed building webm.")
 		return
 	}
 
@@ -284,22 +197,6 @@ func (srv *Server) uploadHandler(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	// Write webm file.
-	path = strings.Join([]string{hash, "lottie.webm"}, "/")
-	wr = srv.bucket.Object(path).NewWriter(ctx)
-	defer util.Close(wr)
-	wr.ObjectAttrs.ContentEncoding = "video/webm"
-	f, err := os.Open(destFullPath)
-	if err != nil {
-		httputils.ReportError(w, r, err, "Failed opening webm.")
-		return
-	}
-	_, err = io.Copy(wr, f)
-	if err != nil {
-		httputils.ReportError(w, r, err, "Failed writing webm to GCS.")
-		return
-	}
-
 	resp := UploadResponse{
 		Hash: hash,
 	}
@@ -320,10 +217,6 @@ func main() {
 		sklog.Fatalf("Can't be run as both --locked_down and --local.")
 	}
 
-	if *skottieTool == "" {
-		sklog.Fatal("The --skottie_tool flag is required.")
-	}
-
 	srv, err := New()
 	if err != nil {
 		sklog.Fatalf("Failed to start: %s", err)
@@ -332,7 +225,6 @@ func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/{hash:[0-9A-Za-z]*}", srv.mainHandler)
 
-	r.HandleFunc("/_/i/{hash:[0-9A-Za-z]+}", srv.webmHandler)
 	r.HandleFunc("/_/j/{hash:[0-9A-Za-z]+}", srv.jsonHandler)
 	r.HandleFunc("/_/upload", srv.uploadHandler)
 
---
+++ skottie/modules/skottie-sk/skottie-sk-demo.js
@@ -0,0 +1,25 @@
+import './index.js'
+import './skottie-sk-demo.css'
+
+import { gear } from './test_gear.js'
+const fetchMock = require('fetch-mock');
+
+let state = {
+  filename: 'gear.json',
+  lottie: gear,
+  width: 200,
+  height: 200,
+  fps: 30,
+}
+fetchMock.get('glob:/_/j/*', {
+  status: 200,
+  body: JSON.stringify(state),
+  headers: {'Content-Type':'application/json'},
+});
+fetchMock.post('glob:/_/upload', {
+  status: 200,
+  body: JSON.stringify({
+    hash: 'MOCK_UPLOADED'
+  }),
+  headers: {'Content-Type':'application/json'},
+});
