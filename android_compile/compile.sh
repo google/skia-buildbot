db0677de06495aad91205ca3bdb56dfd82415c6c
Ravi Mistry <rmistry@google.com>
2018-11-20 13:35:35 -0500

    Make compile.sh configurable
    Bug: skia:
    Change-Id: I787c3ec171952cbb21084306093ee1915b7654f3

--- android_compile/compile.sh
+++ android_compile/compile.sh
@@ -17,38 +17,46 @@ function log_step() {
 }
 
 
-if [ -z "$1" ]
+if [[ -z "$1" || -z "$2" || -z "$3" ]]
   then
-    echo "Missing Android checkout directory"
+    echo "Three arguments are required:"
+    echo "1. Android checkout directory."
+    echo "2. Lunch target."
+    echo "3. A comma separated list of targets to build via mmma."
+    echo
+    echo "Example usage:"
+    echo "bash compile.sh /mnt/pd0/checkouts/checkout_1 cf_x86_phone-eng frameworks/base/core/jni,external/skia"
     exit 1
 fi
 checkout=$1
-cd $checkout
+lunch_target=$2
+mmma_targets=$3
+# TODO(rmistrty): Uncomment the below.
+# cd $checkout
 
 # Set ccache env variables.
 export CCACHE_DIR=/mnt/pd0/ccache
 export USE_CCACHE=1
 export CCACHE_EXEC=/usr/bin/ccache
-ccache -M 500G
+# ccache -M 500G
 
 source_cmd="source ./build/envsetup.sh"
 log_step "Running $source_cmd"
-eval $source_cmd
+# eval $source_cmd
 
-lunch_cmd="lunch cf_x86_phone-eng"
+lunch_cmd="lunch $lunch_target"
 log_step "Running $lunch_cmd"
-eval $lunch_cmd
+# eval $lunch_cmd
 
 log_step "ccache stats before compilations"
-ccache -s
+# ccache -s
 
-mmma_cmd="time mmma -j50 frameworks/base/core/jni"
-log_step "Running $mmma_cmd"
-eval $mmma_cmd
-
-mmma_skia_cmd="time mmma -j50 external/skia"
-log_step "Running $mmma_skia_cmd"
-eval $mmma_skia_cmd
+IFS=',' read -ra mmma_targets_arr <<< "$mmma_targets"
+for i in "${mmma_targets_arr[@]}"; do
+  mmma_cmd="time mmma -j50 $i"
+  log_step "Running $mmma_cmd"
+  # eval $mmma_cmd
+done
 
 log_step "ccache stats after compilations"
-ccache -s
+# ccache -s
