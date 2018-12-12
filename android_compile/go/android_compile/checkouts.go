b31a9b6f9629797de75150f24000561b3350e368
Ravi Mistry <rmistry@google.com>
2018-12-12 13:21:17 -0500

    [Android compile server] Do not recreate mirror right after we already created it
    Bug: skia:8053
    Change-Id: I1a32b0e1509f0466d5f85b1b854829745a1226b4

--- android_compile/go/android_compile/checkouts.go
+++ android_compile/go/android_compile/checkouts.go
@@ -244,6 +244,7 @@ func updateCheckout(ctx context.Context, checkoutPath string, isMirror bool) err
 		defer checkoutsMutex.RUnlock()
 	}
 
+	firstSyncOfMirror := isMirror && recreateMirror
 	if isMirror && recreateMirror {
 		sklog.Info("Recreating the mirror.")
 		util.RemoveAll(checkoutPath)
@@ -274,7 +275,15 @@ func updateCheckout(ctx context.Context, checkoutPath string, isMirror bool) err
 		defer func() {
 			duration := timerMetric.Stop()
 			if isMirror {
-				recreateMirror = duration > MAX_MIRROR_SYNC_TIME_BEFORE_RECREATION
+				if firstSyncOfMirror {
+					// We did the first sync of a recreated mirror. The first sync takes longer than usual
+					// thus we do not need to check if duration exceeded MAX_MIRROR_SYNC_TIME_BEFORE_RECREATION.
+					// Initialize both recreateMirror and firstSyncOfMirror to false.
+					recreateMirror = false
+					firstSyncOfMirror = false
+				} else {
+					recreateMirror = duration > MAX_MIRROR_SYNC_TIME_BEFORE_RECREATION
+				}
 				if recreateMirror {
 					sklog.Warningf("Mirror sync time %s was greater than %s", duration, MAX_MIRROR_SYNC_TIME_BEFORE_RECREATION)
 					sklog.Info("Will recreate mirror before next sync.")
