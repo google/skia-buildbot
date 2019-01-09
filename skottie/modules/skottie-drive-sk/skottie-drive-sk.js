145b1433e548782930672f91881d0abee9a1c82b
Joe Gregorio <jcgregorio@google.com>
2019-01-09 11:22:51 -0500

    Drive plugin for Skottie Preview - wip
    Bug: skia:
    Change-Id: Idfce66ff61a4413c183c5b7083dd517e9e004a27

---
+++ skottie/modules/skottie-drive-sk/skottie-drive-sk.js
@@ -0,0 +1,60 @@
+/**
+ * @module skottie-drive-sk
+ * @description <h2><code>skottie-drive-sk</code></h2>
+ *
+ * @evt
+ *
+ * @attr
+ *
+ * @example
+ */
+import '../skottie-player-sk'
+
+let gapiLoaded = new Promise((resolve, reject) => {
+  document.addEventListener('gapi-loaded', () => {
+    resolve();
+  })
+});
+
+window.customElements.define('skottie-drive-sk', class extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this._render();
+    gapiLoaded.then(() => {
+       gapi.load('client:auth2', this.initClient);
+    });
+  }
+
+  initClient() {
+    gapi.client.init({
+      apiKey: "",
+      clientId: "145247227042-fetft5vnkf582o817e1t553cm3tjvobl.apps.googleusercontent.com",
+      discoveryDocs: ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"],
+      scopes: ['https://www.googleapis.com/auth/drive.readonly', 'https://www.googleapis.com/auth/drive.install'],
+    }).then(function () {
+      let isSignedIn = gapi.auth2.getAuthInstance().isSignedIn;
+      // Listen for sign-in state changes.
+      isSignedIn.listen(this.updateSigninStatus);
+      // Handle the initial sign-in state.
+      this.updateSigninStatus(isSignedIn.get());
+    });
+  }
+
+  /**
+   *  Called when the signed in status changes, to update the UI
+   *  appropriately. After a sign-in, the API is called.
+   */
+  updateSigninStatus(isSignedIn) {
+  }
+
+  disconnectedCallback() {
+  }
+
+  _render() {
+    this.innerHTML = `<skottie-player-sk></skottie-player-sk>`;
+  }
+
+});
