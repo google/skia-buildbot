2714af1a31f3ab273562ded0397014014f681dde
Eric Boren <borenet@google.com>
2019-01-10 10:15:12 -0500

    [autoroll] If a file has been set to empty, delete it
    Bug: skia:
    Change-Id: I595e25d776af7cf99481ccf2420d28c2cff487eb

--- autoroll/go/repo_manager/no_checkout_repo_manager.go
+++ autoroll/go/repo_manager/no_checkout_repo_manager.go
@@ -96,8 +96,14 @@ func (rm *noCheckoutRepoManager) CreateNewRoll(ctx context.Context, from, to str
 	// Create the change.
 	ci, err := gerrit.CreateAndEditChange(rm.g, rm.gerritConfig.Project, rm.parentBranch, commitMsg, rm.baseCommit, func(g gerrit.GerritInterface, ci *gerrit.ChangeInfo) error {
 		for file, contents := range rm.nextRollChanges {
-			if err := g.EditFile(ci, file, contents); err != nil {
-				return fmt.Errorf("Failed to edit %s file: %s", file, err)
+			if contents == "" {
+				if err := g.DeleteFile(ci, file); err != nil {
+					return fmt.Errorf("Failed to delete %s file: %s", file, err)
+				}
+			} else {
+				if err := g.EditFile(ci, file, contents); err != nil {
+					return fmt.Errorf("Failed to edit %s file: %s", file, err)
+				}
 			}
 		}
 		return nil
