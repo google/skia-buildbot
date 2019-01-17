18bb7074053e69e48ea99407b2e672c8ff9d2a74
skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
2019-01-17 06:18:24 +0000

    Roll go_deps 126..127
    The AutoRoll server is located here: https://autoroll.skia.org/r/skia-go-deps-infra-autoroll
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+/master/autoroll/README.md
    
    If the roll is causing failures, please contact the current sheriff, who should
    be CC'd on the roll, and stop the roller if necessary.
    
    TBR=borenet@google.com
    
    Change-Id: Ibf0f548732218106520e57db6066893143737e02

--- go/cipd/asset_versions_gen.go
+++ go/cipd/asset_versions_gen.go
@@ -5,7 +5,7 @@ package cipd
 var PKG_VERSIONS_FROM_ASSETS = map[string]string{
 	"gcloud_linux": "13",
 	"go":           "6",
-	"go_deps":      "126",
+	"go_deps":      "127",
 	"node":         "0",
 	"protoc":       "0",
 }
