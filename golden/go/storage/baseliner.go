0033c20f1ec0472751a8519f0a86bc989e75582f
Stephan Altmueller <stephana@google.com>
2019-01-18 13:50:43 -0500

    [gold] Fix backend to handle baselines correctly and faster
    - Adds asynchronous startup where setting up the storage infrastructure
      is done in a separate go-routine so we reach a ready state quickly
      for Kubernetes.
    - Rename ready endpoint from "/ready" to "/healthz"
    - Continuously calculate a per commit expectation, cache it and
      write it to GCS.
    - Faster serving of expectations for issues and commits.
    - Extends CommitableBaseline by including simple stats about available
      digests in preparation of export function to be added to goldctl.
    
    Bug: skia:8293
    Change-Id: I9ddcf20d1338919a7235d7d9c5e0044243a79261

--- golden/go/storage/baseliner.go
+++ golden/go/storage/baseliner.go
@@ -1,16 +1,28 @@
 package storage
 
 import (
-	"github.com/davecgh/go-spew/spew"
+	"context"
+	"sync"
+
+	lru "github.com/hashicorp/golang-lru"
 	"go.skia.org/infra/go/skerr"
 	"go.skia.org/infra/go/sklog"
 	"go.skia.org/infra/go/tiling"
+	"go.skia.org/infra/go/util"
+	"go.skia.org/infra/go/vcsinfo"
 	"go.skia.org/infra/golden/go/baseline"
 	"go.skia.org/infra/golden/go/expstorage"
 	"go.skia.org/infra/golden/go/tally"
 	"go.skia.org/infra/golden/go/tryjobstore"
-	"go.skia.org/infra/golden/go/types"
 	"golang.org/x/sync/errgroup"
+)
+
+// TODO(stephana): Tune issueCacheSize by either finding a good value that works across instance
+// or one that can be tuned.
+
+const (
+	// issueCacheSize is the size of the baselines cache for issue.
+	issueCacheSize = 10000
 )
 
 // TODO(stephana): Baseliner needs to merged into the baseline package and
@@ -26,16 +38,31 @@ type Baseliner struct {
 	expectationsStore    expstorage.ExpectationsStore
 	issueExpStoreFactory expstorage.IssueExpStoreFactory
 	tryjobStore          tryjobstore.TryjobStore
+	vcs                  vcsinfo.VCS
+
+	mutex                sync.RWMutex
+	lastWrittenBaselines map[string]string
+	baselineCache        map[string]*baseline.CommitableBaseLine
+	currentTile          *tiling.Tile
+	issueBaselineCache   *lru.Cache
 }
 
 // NewBaseliner creates a new instance of Baseliner.
-func NewBaseliner(gStorageClient *GStorageClient, expectationsStore expstorage.ExpectationsStore, issueExpStoreFactory expstorage.IssueExpStoreFactory, tryjobStore tryjobstore.TryjobStore) *Baseliner {
+func NewBaseliner(gStorageClient *GStorageClient, expectationsStore expstorage.ExpectationsStore, issueExpStoreFactory expstorage.IssueExpStoreFactory, tryjobStore tryjobstore.TryjobStore, vcs vcsinfo.VCS) (*Baseliner, error) {
+	cache, err := lru.New(issueCacheSize)
+	if err != nil {
+		return nil, skerr.Fmt("Error allocating cache: %s", err)
+	}
+
 	return &Baseliner{
 		gStorageClient:       gStorageClient,
 		expectationsStore:    expectationsStore,
 		issueExpStoreFactory: issueExpStoreFactory,
 		tryjobStore:          tryjobStore,
-	}
+		vcs:                  vcs,
+		issueBaselineCache:   cache,
+		lastWrittenBaselines: map[string]string{},
+	}, nil
 }
 
 // CanWriteBaseline returns true if this instance was configured to write baseline files.
@@ -49,31 +76,63 @@ func (b *Baseliner) PushMasterBaselines(tile *tiling.Tile) error {
 		return skerr.Fmt("Trying to write baseline while GCS path is not configured.")
 	}
 
-	_, baseLine, err := b.getMasterBaseline(tile)
+	perCommitBaselines, err := b.calcMasterBaselines(tile)
 	if err != nil {
-		return skerr.Fmt("Error retrieving master baseline: %s", err)
+		return skerr.Fmt("Error getting master baseline: %s", err)
 	}
 
-	if baseLine == nil {
-		sklog.Infof("No baseline available.")
-		return nil
+	// Get the current list of files that have been written.
+	b.mutex.Lock()
+	lastWritten := b.lastWrittenBaselines
+	b.mutex.Unlock()
+
+	// Write the ones to disk that have not been written
+	writeBaselines := make(util.StringSet, len(perCommitBaselines))
+	for commit, bLine := range perCommitBaselines {
+		md5Sum, ok := lastWritten[commit]
+		if ok && md5Sum == bLine.MD5 {
+			continue
+		}
+		writeBaselines[commit] = true
 	}
 
-	// Write the baseline to GCS.
-	outputPath, err := b.gStorageClient.WriteBaseLine(baseLine)
-	if err != nil {
-		return skerr.Fmt("Error writing baseline to GCS: %s", err)
+	written := make(map[string]string, len(writeBaselines))
+	var wMutex sync.Mutex
+	var egroup errgroup.Group
+
+	for commit, bLine := range perCommitBaselines {
+		// If we have written this baseline before, we mark it as written and process the next one.
+		if md5Sum, ok := lastWritten[commit]; ok && md5Sum == bLine.MD5 {
+			written[commit] = bLine.MD5
+			continue
+		}
+
+		func(commit string, bLine *baseline.CommitableBaseLine) {
+			egroup.Go(func() error {
+				// Write the baseline to GCS.
+				_, err := b.gStorageClient.WriteBaseLine(bLine)
+				if err != nil {
+					return skerr.Fmt("Error writing baseline to GCS: %s", err)
+				}
+				wMutex.Lock()
+				written[commit] = bLine.MD5
+				wMutex.Unlock()
+				return nil
+			})
+		}(commit, bLine)
 	}
-	sklog.Infof("Baseline for master written to %s.", outputPath)
+
+	// Swap out the baseline cache and the list of last written files.
+	b.mutex.Lock()
+	b.currentTile = tile
+	b.baselineCache = perCommitBaselines
+	b.lastWrittenBaselines = written
+	b.mutex.Unlock()
 	return nil
 }
 
 // PushIssueBaseline writes the baseline for a Gerrit issue to GCS.
 func (b *Baseliner) PushIssueBaseline(issueID int64, tile *tiling.Tile, tallies *tally.Tallies) error {
-	if !b.CanWriteBaseline() {
-		return skerr.Fmt("Trying to write baseline while GCS path is not configured.")
-	}
-
 	issueExpStore := b.issueExpStoreFactory(issueID)
 	exp, err := issueExpStore.Get()
 	if err != nil {
@@ -85,7 +144,17 @@ func (b *Baseliner) PushIssueBaseline(issueID int64, tile *tiling.Tile, tallies 
 		return skerr.Fmt("Unable to get TryjobResults")
 	}
 	talliesByTest := tallies.ByTest()
-	baseLine := baseline.GetBaselineForIssue(issueID, tryjobs, tryjobResults, exp, tile.Commits, talliesByTest)
+	baseLine, err := baseline.GetBaselineForIssue(issueID, tryjobs, tryjobResults, exp, tile.Commits, talliesByTest)
+	if err != nil {
+		return skerr.Fmt("Error error calculating issue baseline: %s", err)
+	}
+
+	// Add it to the cache.
+	_ = b.issueBaselineCache.Add(issueID, baseLine)
+
+	if !b.CanWriteBaseline() {
+		return skerr.Fmt("Trying to write baseline while GCS path is not configured.")
+	}
 
 	// Write the baseline to GCS.
 	outputPath, err := b.gStorageClient.WriteBaseLine(baseLine)
@@ -100,22 +169,41 @@ func (b *Baseliner) PushIssueBaseline(issueID int64, tile *tiling.Tile, tallies 
 // loading the master baseline and the issue baseline from GCS and combining
 // them. If either of them doesn't exist an empty baseline is assumed.
 func (b *Baseliner) FetchBaseline(commitHash string, issueID int64, patchsetID int64) (*baseline.CommitableBaseLine, error) {
+	isIssue := issueID > 0
+
+	if isIssue {
+		commitHash = ""
+	}
+
 	var masterBaseline *baseline.CommitableBaseLine
 	var issueBaseline *baseline.CommitableBaseLine
-
 	var egroup errgroup.Group
+
+	// Retrieve the baseline on master.
 	egroup.Go(func() error {
 		var err error
-		masterBaseline, err = b.gStorageClient.ReadBaseline(commitHash, 0)
-		sklog.Infof("Master: %s    %s", commitHash, spew.Sdump(masterBaseline))
+		masterBaseline, err = b.getMasterExpectations(commitHash)
 		return err
 	})
 
-	if issueID > 0 {
+	if isIssue {
 		egroup.Go(func() error {
+			val, ok := b.issueBaselineCache.Get(issueID)
+			if ok {
+				issueBaseline = val.(*baseline.CommitableBaseLine)
+				return nil
+			}
+
 			var err error
-			issueBaseline, err = b.gStorageClient.ReadBaseline(commitHash, issueID)
-			sklog.Infof("issue %s   %d: %s", commitHash, issueID, spew.Sdump(issueBaseline))
+			issueBaseline, err = b.gStorageClient.ReadBaseline("", issueID)
+			if err != nil {
+				return err
+			}
+			// If no baseline was found. We place an empty one.
+			if issueBaseline == nil {
+				issueBaseline = baseline.EmptyBaseline(nil, nil)
+			}
+
 			return err
 		})
 	}
@@ -124,18 +212,62 @@ func (b *Baseliner) FetchBaseline(commitHash string, issueID int64, patchsetID i
 		return nil, err
 	}
 
-	if issueBaseline != nil {
+	if isIssue {
 		masterBaseline.Baseline.Update(issueBaseline.Baseline)
 	}
 	return masterBaseline, nil
 }
 
-// getMasterBaseline retrieves the master baseline based on the given tile.
-func (b *Baseliner) getMasterBaseline(tile *tiling.Tile) (types.Expectations, *baseline.CommitableBaseLine, error) {
+// calcMasterBaselines retrieves the master baseline based on the given tile.
+func (b *Baseliner) calcMasterBaselines(tile *tiling.Tile) (map[string]*baseline.CommitableBaseLine, error) {
 	exps, err := b.expectationsStore.Get()
 	if err != nil {
-		return nil, nil, skerr.Fmt("Unable to retrieve expectations: %s", err)
+		return nil, skerr.Fmt("Unable to retrieve expectations: %s", err)
+	}
+	return baseline.GetBaselinesPerCommit(exps, tile)
+}
+
+func (b *Baseliner) getMasterExpectations(commitHash string) (*baseline.CommitableBaseLine, error) {
+	b.mutex.RLock()
+	cache := b.baselineCache
+	tile := b.currentTile
+	b.mutex.RUnlock()
+
+	if commitHash == "" {
+		// If we have no tile yet, we cannot get the HEAD of it.
+		if tile == nil {
+			return baseline.EmptyBaseline(nil, nil), nil
+		}
+		commitHash = tile.Commits[tile.LastCommitIndex()].Hash
 	}
 
-	return exps, baseline.GetBaselineForMaster(exps, tile), nil
+	if bLine, ok := cache[commitHash]; ok {
+		return bLine, nil
+	}
+
+	// We did not find it in the cache so lets load it from GCS.
+	ret, err := b.gStorageClient.ReadBaseline(commitHash, 0)
+	if err != nil {
+		return nil, err
+	}
+
+	// Look up the commit to see if it's valid.
+	if ret == nil {
+		details, err := b.vcs.Details(context.TODO(), commitHash, false)
+		if err != nil {
+			return nil, skerr.Fmt("Unknown commit: %s", commitHash)
+		}
+		commit := fromLongCommit(details)
+		ret = baseline.EmptyBaseline(commit, commit)
+	}
+	return ret, nil
+}
+
+// fromLongCommit converts a *vcsinfo.LongCommit to a *tiling.Commit
+func fromLongCommit(lc *vcsinfo.LongCommit) *tiling.Commit {
+	return &tiling.Commit{
+		CommitTime: lc.Timestamp.Unix(),
+		Hash:       lc.Hash,
+		Author:     lc.Author,
+	}
 }
