8b0dc753c3f9a2d8031e09ee4858b2c1e9943510
Stephan Altmueller <stephana@google.com>
2019-01-24 15:29:33 -0500

    [goldctl] Make LUCI token source an auth option
    TBR=benjaminwagner
    Bug: skia:
    Change-Id: I7ddc5646e5ef59e6751a93a2d12d218b7f641108

--- gold-client/cmd/goldctl/cmd_auth.go
+++ gold-client/cmd/goldctl/cmd_auth.go
@@ -1,15 +1,26 @@
 package main
 
 import (
+	"fmt"
+
 	gstorage "cloud.google.com/go/storage"
 	"github.com/spf13/cobra"
 	"go.skia.org/infra/go/auth"
 	"go.skia.org/infra/gold-client/go/goldclient"
+	"golang.org/x/oauth2"
+)
+
+const (
+	// Define the flag names to be consistent.
+	fstrServiceAccount = "service-account"
+	fstrLUCI           = "luci"
+	fstrWorkDir        = "workdir"
 )
 
 // authEnv provides the environment for the auth command.
 type authEnv struct {
 	flagServiceAccount string
+	flagUseLUCIContext bool
 	flagWorkDir        string
 }
 
@@ -22,18 +33,29 @@ func getAuthCmd() *cobra.Command {
 		Long: `
 Authenticate against GCP and the Gold instance.
 Currently only service accounts are supported. `,
-		Run: env.runAuthCmd,
+		PreRunE: env.validateFlags,
+		Run:     env.runAuthCmd,
 	}
 
-	// add the --service-account flag and make it required
-	cmd.Flags().StringVarP(&env.flagServiceAccount, "service-account", "", "", "Service account file to be used to authenticate against GCP and Gold")
-	_ = cmd.MarkFlagRequired("service-account")
+	// add the service-account flag.
+	cmd.Flags().StringVarP(&env.flagServiceAccount, fstrServiceAccount, "", "", "Service account file to be used to authenticate against GCP and Gold")
 
-	// add the --work-dir flag and make it required
-	cmd.Flags().StringVarP(&env.flagWorkDir, "workdir", "", "", "Temporary work directory")
-	_ = cmd.MarkFlagRequired("work-dir")
+	// add the luci flag to use the LUCI_CONTEXT for authentication.
+	cmd.Flags().BoolVarP(&env.flagUseLUCIContext, fstrLUCI, "", false, "Use the LUCI context to retrieve an oauth token.")
+
+	// add the workdir flag and make it required
+	cmd.Flags().StringVarP(&env.flagWorkDir, fstrWorkDir, "", "", "Temporary work directory")
+	_ = cmd.MarkFlagRequired(fstrWorkDir)
 
 	return cmd
+}
+
+// validateFlags validates across individual flags.
+func (a *authEnv) validateFlags(cmd *cobra.Command, args []string) error {
+	if a.flagServiceAccount == "" && !a.flagUseLUCIContext {
+		return fmt.Errorf("ERROR: Either the %q or %q flag must be set to choose an auth token source.", fstrServiceAccount, fstrLUCI)
+	}
+	return nil
 }
 
 // runAuthCommand
@@ -46,10 +68,14 @@ func (a *authEnv) runAuthCmd(cmd *cobra.Command, args []string) {
 	goldClient, err := goldclient.NewCloudClient(config, nil)
 	ifErrLogExit(cmd, err)
 
-	tokenSrc, err := auth.NewJWTServiceAccountTokenSource("#bogus", a.flagServiceAccount, gstorage.ScopeFullControl)
+	var tokenSrc oauth2.TokenSource
+	if a.flagUseLUCIContext {
+		tokenSrc, err = auth.NewLUCIContextTokenSource(gstorage.ScopeFullControl)
+	} else {
+		tokenSrc, err = auth.NewJWTServiceAccountTokenSource("#bogus", a.flagServiceAccount, gstorage.ScopeFullControl)
+	}
 	ifErrLogExit(cmd, err)
 
 	err = goldClient.ServiceAccount(tokenSrc)
 	ifErrLogExit(cmd, err)
-
 }
