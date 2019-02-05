e731c91e22842ff76e92fd1d1aa7ba4d9c199937
Joe Gregorio <jcgregorio@google.com>
2019-02-05 07:13:15 -0500

    Perf - Fix for slow dfbuilder wip.
    Bug: skia:
    Change-Id: I685bf89034bd78eb245f68377a8d74e9cb97d5db

---
+++ perf/go/tracesetbuilder/tracesetbuilder.go
@@ -0,0 +1,143 @@
+package tracesetbuilder
+
+import (
+	"crypto/md5"
+	"encoding/binary"
+	"sync"
+
+	"go.skia.org/infra/go/paramtools"
+	"go.skia.org/infra/go/query"
+	"go.skia.org/infra/go/sklog"
+	"go.skia.org/infra/perf/go/types"
+)
+
+const NUM_WORKERS = 64
+
+type request struct {
+	key      string // In the first stage this is the encoded key, in the second stage it is the decoded key.
+	params   paramtools.Params
+	trace    []float32
+	ops      paramtools.OrderedParamSet
+	traceMap map[int32]int32
+}
+
+type mergeWorker struct {
+	ch       chan *request
+	wg       *sync.WaitGroup
+	traceSet types.TraceSet
+	paramSet paramtools.ParamSet
+}
+
+func newMergeWorker(wg *sync.WaitGroup, size int) *mergeWorker {
+	m := &mergeWorker{
+		ch:       make(chan *request),
+		wg:       wg,
+		traceSet: types.TraceSet{},
+		paramSet: paramtools.ParamSet{},
+	}
+	go func() {
+		for req := range m.ch {
+			// Inlay the trace into traceSet and paramSet.
+			trace, ok := m.traceSet[req.key]
+			if !ok {
+				trace = types.NewTrace(size)
+			}
+			for srcIndex, dstIndex := range req.traceMap {
+				trace[dstIndex] = req.trace[srcIndex]
+			}
+			m.traceSet[req.key] = trace
+			m.paramSet.AddParams(req.params)
+			m.wg.Done()
+		}
+	}()
+
+	return m
+}
+
+func (m *mergeWorker) Process(req *request) {
+	m.ch <- req
+}
+
+func (m *mergeWorker) Close() {
+	close(m.ch)
+}
+
+type TraceSetBuilder struct {
+	wg           *sync.WaitGroup
+	hashStageCh  chan *request
+	mergeWorkers []*mergeWorker // There are NUM_WORKERS of these.
+}
+
+// size == len(indices)
+func New(size int) *TraceSetBuilder {
+	t := &TraceSetBuilder{
+		wg:           &sync.WaitGroup{},
+		hashStageCh:  make(chan *request),
+		mergeWorkers: []*mergeWorker{},
+	}
+	// Build a pool of 64 merge workers.
+
+	for i := 0; i < NUM_WORKERS; i++ {
+		t.mergeWorkers = append(t.mergeWorkers, newMergeWorker(t.wg, size))
+	}
+
+	// Build a pool of 64 hash workers.
+	for i := 0; i < NUM_WORKERS; i++ {
+		go func() {
+			var err error
+			for req := range t.hashStageCh {
+				req.params, err = req.ops.DecodeParamsFromString(req.key)
+				if err != nil {
+					// It is possible we matched a trace that appeared after we grabbed the OPS,
+					// so just ignore it.
+					t.wg.Done()
+					continue
+				}
+				req.key, err = query.MakeKey(req.params)
+				if err != nil {
+					sklog.Errorf("Failed to make a key from %#v: %s", req.params, err)
+					t.wg.Done()
+					continue
+				}
+				// Calculate which merge worker should handle this request.
+				index := binary.BigEndian.Uint64(md5.New().Sum([]byte(req.key))) % NUM_WORKERS
+				t.mergeWorkers[index].Process(req)
+			}
+		}()
+	}
+	return t
+}
+
+// traces are key'd by encoded key and the traces are just a singe tile length.
+func (t *TraceSetBuilder) Add(ops paramtools.OrderedParamSet, traceMap map[int32]int32, traces map[string][]float32) {
+	t.wg.Add(len(traces))
+	for encodedKey, trace := range traces {
+		req := &request{
+			key:      encodedKey,
+			trace:    trace,
+			ops:      ops,
+			traceMap: traceMap,
+		}
+		t.hashStageCh <- req
+	}
+}
+
+// Don't call Build until Add() has been called for every tile to be added.
+func (t *TraceSetBuilder) Build() (types.TraceSet, paramtools.ParamSet) {
+	t.wg.Wait()
+	// Shut down the first stage.
+	close(t.hashStageCh)
+
+	traceSet := types.TraceSet{}
+	paramSet := paramtools.ParamSet{}
+	// Now merge all the traceSets and paramSets.
+	for _, mw := range t.mergeWorkers {
+		for k, v := range mw.traceSet {
+			traceSet[k] = v
+		}
+		paramSet.AddParamSet(mw.paramSet)
+		// Shut down the second stage.
+		mw.Close()
+	}
+	return traceSet, paramSet
+}
