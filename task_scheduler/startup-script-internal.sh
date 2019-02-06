e2fa81f64b427e315d5c00b64b42f3f1bdfc8511
Ben Wagner <benjaminwagner@google.com>
2019-02-06 14:37:30 -0500

    [task scheduler] Set git config in startup script.
    Change-Id: I6fd5c7ea8c7e9d6130c5de92f4f783b26357b3b8

---
+++ task_scheduler/startup-script-internal.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+# Create tmp dir if needed.
+sudo -u default mkdir -p /mnt/pd0/tmp
+
+# These must be set, otherwise bot_update will temporarily set them and then
+# unset them after it has finished, which fails because we run bot_update
+# concurrently. See also
+# https://chromium-review.googlesource.com/c/chromium/tools/depot_tools/+/1036309
+sudo -u default git config --global user.email \
+  task-scheduler-internal@skia-buildbots.google.com.iam.gserviceaccount.com
+sudo -u default git config --global user.name skia-task-scheduler-internal
