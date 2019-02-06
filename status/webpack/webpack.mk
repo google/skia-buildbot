1162777b2dd27cb22e76bd6b1c9544b09e1b251f
Ben Wagner <benjaminwagner@google.com>
2019-02-06 10:42:14 -0500

    [status] Fix webpack Makefile.
    Change-Id: I686ef3f30e2991297367a57db388cda9f491d388

--- status/webpack/webpack.mk
+++ status/webpack/webpack.mk
@@ -1,6 +1,9 @@
 THIS_FILE := $(abspath $(lastword $(MAKEFILE_LIST)))
 WEBPACK_DIR := $(dir $(THIS_FILE))
 
+$(WEBPACK_DIR)/package-lock.json: $(WEBPACK_DIR)/package.json
+	cd $(WEBPACK_DIR) && npm install
+
 .PHONY: webpack
-webpack:
+webpack: $(WEBPACK_DIR)/package-lock.json
 	cd $(WEBPACK_DIR) && npx webpack --mode=development
