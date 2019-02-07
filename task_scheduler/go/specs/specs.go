6956256081d36d4a6093ead31721c021703b693d
Eric Boren <borenet@google.com>
2019-02-07 15:19:03 -0500

    [task scheduler] Add func timers for temp git repos, incl. bot_update
    Bug: skia:
    Change-Id: I4585c5487a3e3175601b9a01eeb2f1008e7e4a49

--- task_scheduler/go/specs/specs.go
+++ task_scheduler/go/specs/specs.go
@@ -1058,6 +1058,8 @@ func (c *TaskCfgCache) TempGitRepo(ctx context.Context, rs types.RepoState, botU
 // tempGitRepo creates a git repository in a temporary directory, gets it into
 // the given RepoState, and returns its location.
 func tempGitRepo(ctx context.Context, repo *git.Repo, rs types.RepoState) (rv *git.TempCheckout, rvErr error) {
+	defer metrics2.FuncTimer().Stop()
+
 	if rs.IsTryJob() {
 		return nil, fmt.Errorf("specs.tempGitRepo does not apply patches, and should not be called for try jobs.")
 	}
@@ -1084,6 +1086,8 @@ func tempGitRepo(ctx context.Context, repo *git.Repo, rs types.RepoState) (rv *g
 // tempGitRepoBotUpdate creates a git repository in a temporary directory, gets it into
 // the given RepoState, and returns its location.
 func tempGitRepoBotUpdate(ctx context.Context, rs types.RepoState, depotToolsDir, gitCacheDir, tmp string) (*git.TempCheckout, error) {
+	defer metrics2.FuncTimer().Stop()
+
 	// Run bot_update to obtain a checkout of the repo and its DEPS.
 	botUpdatePath := path.Join(depotToolsDir, "recipes", "recipe_modules", "bot_update", "resources", "bot_update.py")
 	projectName := strings.TrimSuffix(path.Base(rs.Repo), ".git")
