fe208e4d98b77a53e24fd54e59e7ad0026b9c749
Ravi Mistry <rmistry@google.com>
2019-02-08 15:09:11 -0500

    [CT] App to copy Datastore entities between projects
    Change-Id: If4cbe9468a9fcc4d32d6d78dc1411ed9ab826efb

---
+++ ct/go/copy_datastore/main.go
@@ -0,0 +1,128 @@
+// Copies CT data from one cloud project to another.
+package main
+
+import (
+	"context"
+	"flag"
+	"time"
+
+	"cloud.google.com/go/datastore"
+
+	"go.skia.org/infra/ct/go/ctfe/task_common"
+	"go.skia.org/infra/ct/go/ctfe/task_types"
+	"go.skia.org/infra/ct/go/util"
+	"go.skia.org/infra/go/auth"
+	"go.skia.org/infra/go/common"
+	"go.skia.org/infra/go/ds"
+	"go.skia.org/infra/go/sklog"
+	"google.golang.org/api/iterator"
+	"google.golang.org/api/option"
+)
+
+var (
+	srcProject = flag.String("src_project", "google.com:skia-buildbots", "The source project.")
+	dstProject = flag.String("dst_project", "skia-public", "The destination project.")
+	namespace  = flag.String("namespace", "cluster-telemetry-testing", "The Cloud Datastore namespace, such as 'cluster-telemetry'.")
+)
+
+func migrateData(ctx context.Context, srcClient, dstClient *datastore.Client, task task_common.Task) {
+	// Delete everything from the datastore first.
+	if _, err := DeleteKind(ctx, dstClient, task.GetDatastoreKind()); err != nil {
+		sklog.Fatal(err)
+	}
+	// Migrate data.
+	q := ds.NewQuery(task.GetDatastoreKind())
+	for t := srcClient.Run(ctx, q); ; {
+		key, err := t.Next(task)
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			sklog.Fatal(err)
+		}
+		_, err = dstClient.Put(ctx, key, task)
+		sklog.Infof("%s: %s", task.GetDatastoreKind(), key)
+		if err != nil {
+			sklog.Fatal(err)
+		}
+	}
+}
+
+func main() {
+	common.Init()
+
+	// Construct clients.
+	ts, err := auth.NewDefaultTokenSource(true, datastore.ScopeDatastore)
+	if err != nil {
+		sklog.Fatal(err)
+	}
+	if err := ds.InitWithOpt(*srcProject, *namespace, option.WithTokenSource(ts)); err != nil {
+		sklog.Fatal(err)
+	}
+	srcClient := ds.DS
+	if err := ds.InitWithOpt(*dstProject, *namespace, option.WithTokenSource(ts)); err != nil {
+		sklog.Fatal(err)
+	}
+	dstClient := ds.DS
+
+	ctx := context.Background()
+
+	defer util.TimeTrack(time.Now(), "Database migration")
+	for _, task := range task_types.Prototypes() {
+		migrateData(ctx, srcClient, dstClient, task)
+	}
+	// Also migrate the ClusterTelemetryIDs KIND.
+	ctIDsTask := &task_common.ClusterTelemetryIDs{}
+	// Delete everything from the datastore first.
+	if _, err := DeleteKind(ctx, dstClient, ds.CLUSTER_TELEMETRY_IDS); err != nil {
+		sklog.Fatal(err)
+	}
+	// Migrate data.
+	q := ds.NewQuery(ds.CLUSTER_TELEMETRY_IDS)
+	for t := srcClient.Run(ctx, q); ; {
+		key, err := t.Next(ctIDsTask)
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			sklog.Fatal(err)
+		}
+		_, err = dstClient.Put(ctx, key, ctIDsTask)
+		sklog.Infof("%s: %s", ds.CLUSTER_TELEMETRY_IDS, key)
+		if err != nil {
+			sklog.Fatal(err)
+		}
+	}
+	sklog.Infoln("Database migration finished.")
+}
+
+func DeleteKind(ctx context.Context, dstClient *datastore.Client, datastoreKind ds.Kind) (int, error) {
+	var i int
+	var lastSeenKey *datastore.Key
+	q := ds.NewQuery(datastoreKind).Limit(500).KeysOnly().Order("__key__")
+	timeout := time.After(time.Second * 60)
+	for {
+		select {
+		case <-timeout:
+			{
+				return i, nil
+			}
+		default:
+			{
+				keys, err := dstClient.GetAll(ctx, q, nil)
+				if err != nil || len(keys) == 0 {
+					return i, err
+				} else {
+					lastSeenKey = keys[len(keys)-1]
+					i = i + len(keys)
+					if err := dstClient.DeleteMulti(ctx, keys); err != nil {
+						return i, err
+					}
+					sklog.Infof("Deleted %s. Entries deleted: %d", datastoreKind, i)
+				}
+				q = ds.NewQuery(datastoreKind).Limit(500).KeysOnly().Order("__key__").Filter("__key__ >", lastSeenKey)
+			}
+		}
+	}
+	return i, nil
+}
