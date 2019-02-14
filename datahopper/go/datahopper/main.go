ebfe198537fc390cb24168c4942ce97c8f6a1895
Ben Wagner <benjaminwagner@google.com>
2019-02-14 15:50:56 -0500

    [datahopper] Add metrics for the latest Firestore backup
    Before adding alerts on these metrics, I will add a periodic backup job.
    
    Change-Id: I2534fa6eb3f21881aa6cd2ed1987682cbfaee9c9

--- datahopper/go/datahopper/main.go
+++ datahopper/go/datahopper/main.go
@@ -8,34 +8,11 @@ import (
 	"context"
 	"flag"
 	"fmt"
-	"os"
-	"path"
-	"path/filepath"
 	"regexp"
-	"time"
 
-	"cloud.google.com/go/bigtable"
-	"cloud.google.com/go/storage"
-	"go.skia.org/infra/datahopper/go/bot_metrics"
-	"go.skia.org/infra/datahopper/go/swarming_metrics"
-	"go.skia.org/infra/go/auth"
 	"go.skia.org/infra/go/common"
-	"go.skia.org/infra/go/depot_tools"
-	"go.skia.org/infra/go/gcs"
-	"go.skia.org/infra/go/git/repograph"
-	"go.skia.org/infra/go/httputils"
-	"go.skia.org/infra/go/metrics2"
 	"go.skia.org/infra/go/sklog"
-	"go.skia.org/infra/go/swarming"
-	"go.skia.org/infra/go/taskname"
-	"go.skia.org/infra/go/util"
-	"go.skia.org/infra/perf/go/perfclient"
-	"go.skia.org/infra/task_scheduler/go/db"
-	"go.skia.org/infra/task_scheduler/go/db/firestore"
 	"go.skia.org/infra/task_scheduler/go/db/pubsub"
-	"go.skia.org/infra/task_scheduler/go/db/remote_db"
-	"go.skia.org/infra/task_scheduler/go/specs"
-	"google.golang.org/api/option"
 )
 
 // flags
@@ -74,160 +51,164 @@ func main() {
 		common.CloudLoggingOpt(),
 	)
 	ctx := context.Background()
+	/*
+		// Absolutify the workdir.
+		w, err := filepath.Abs(*workdir)
+		if err != nil {
+			sklog.Fatal(w)
+		}
+		sklog.Infof("Workdir is %s", w)
 
-	// Absolutify the workdir.
-	w, err := filepath.Abs(*workdir)
-	if err != nil {
-		sklog.Fatal(w)
-	}
-	sklog.Infof("Workdir is %s", w)
+		// Authenticated HTTP client.
+		oauthCacheFile := path.Join(w, "google_storage_token.data")
+		legacyTs, err := auth.NewLegacyTokenSource(*local, oauthCacheFile, "", swarming.AUTH_SCOPE)
+		if err != nil {
+			sklog.Fatal(err)
+		}
+		httpClient := httputils.DefaultClientConfig().WithTokenSource(legacyTs).With2xxOnly().Client()
 
-	// Authenticated HTTP client.
-	oauthCacheFile := path.Join(w, "google_storage_token.data")
-	legacyTs, err := auth.NewLegacyTokenSource(*local, oauthCacheFile, "", swarming.AUTH_SCOPE)
-	if err != nil {
-		sklog.Fatal(err)
-	}
-	httpClient := httputils.DefaultClientConfig().WithTokenSource(legacyTs).With2xxOnly().Client()
+		// Swarming API client.
+		swarm, err := swarming.NewApiClient(httpClient, swarming.SWARMING_SERVER)
+		if err != nil {
+			sklog.Fatal(err)
+		}
+		swarmInternal, err := swarming.NewApiClient(httpClient, swarming.SWARMING_SERVER_PRIVATE)
+		if err != nil {
+			sklog.Fatal(err)
+		}
 
-	// Swarming API client.
-	swarm, err := swarming.NewApiClient(httpClient, swarming.SWARMING_SERVER)
-	if err != nil {
-		sklog.Fatal(err)
-	}
-	swarmInternal, err := swarming.NewApiClient(httpClient, swarming.SWARMING_SERVER_PRIVATE)
-	if err != nil {
-		sklog.Fatal(err)
-	}
+		jwtTs, err := auth.NewDefaultJWTServiceAccountTokenSource(auth.SCOPE_READ_WRITE)
+		if err != nil {
+			sklog.Fatal(err)
+		}
+		authClient := httputils.DefaultClientConfig().WithTokenSource(jwtTs).With2xxOnly().Client()
 
-	jwtTs, err := auth.NewDefaultJWTServiceAccountTokenSource(auth.SCOPE_READ_WRITE)
-	if err != nil {
-		sklog.Fatal(err)
-	}
-	authClient := httputils.DefaultClientConfig().WithTokenSource(jwtTs).With2xxOnly().Client()
+		gsClient, err := storage.NewClient(ctx, option.WithHTTPClient(authClient))
+		if err != nil {
+			sklog.Fatal(err)
+		}
+		storageClient := gcs.NewGCSClient(gsClient, *perfBucket)
+		pc := perfclient.New(*perfPrefix, storageClient)
 
-	gsClient, err := storage.NewClient(ctx, option.WithHTTPClient(authClient))
-	if err != nil {
-		sklog.Fatal(err)
-	}
-	storageClient := gcs.NewGCSClient(gsClient, *perfBucket)
-	pc := perfclient.New(*perfPrefix, storageClient)
+		tnp := taskname.DefaultTaskNameParser()
 
-	tnp := taskname.DefaultTaskNameParser()
-
-	// Shared repo objects.
-	reposDir := path.Join(w, "repos")
-	if err := os.MkdirAll(reposDir, os.ModePerm); err != nil {
-		sklog.Fatal(err)
-	}
-	// TODO(borenet): We should include all (public and private) repos.
-	repos, err := repograph.NewMap(ctx, []string{common.REPO_SKIA, common.REPO_SKIA_INFRA, common.REPO_LOTTIE_CI}, reposDir)
-	if err != nil {
-		sklog.Fatal(err)
-	}
-	if err := repos.Update(ctx); err != nil {
-		sklog.Fatal(err)
-	}
-	lvRepos := metrics2.NewLiveness("datahopper_repo_update")
-	go util.RepeatCtx(time.Minute, ctx, func() {
+		// Shared repo objects.
+		reposDir := path.Join(w, "repos")
+		if err := os.MkdirAll(reposDir, os.ModePerm); err != nil {
+			sklog.Fatal(err)
+		}
+		// TODO(borenet): We should include all (public and private) repos.
+		repos, err := repograph.NewMap(ctx, []string{common.REPO_SKIA, common.REPO_SKIA_INFRA, common.REPO_LOTTIE_CI}, reposDir)
+		if err != nil {
+			sklog.Fatal(err)
+		}
 		if err := repos.Update(ctx); err != nil {
-			sklog.Errorf("Failed to update repos: %s", err)
+			sklog.Fatal(err)
+		}
+		lvRepos := metrics2.NewLiveness("datahopper_repo_update")
+		go util.RepeatCtx(time.Minute, ctx, func() {
+			if err := repos.Update(ctx); err != nil {
+				sklog.Errorf("Failed to update repos: %s", err)
+			} else {
+				lvRepos.Reset()
+			}
+		})
+
+		// TaskCfgCache.
+		if *recipesCfgFile == "" {
+			*recipesCfgFile = path.Join(*workdir, "recipes.cfg")
+		}
+		depotTools, err := depot_tools.Sync(ctx, w, *recipesCfgFile)
+		if err != nil {
+			sklog.Fatalf("Failed to sync depot_tools: %s", err)
+		}
+		newTs, err := auth.NewDefaultTokenSource(*local, auth.SCOPE_USERINFO_EMAIL, pubsub.AUTH_SCOPE, bigtable.Scope)
+		if err != nil {
+			sklog.Fatal(err)
+		}
+		tcc, err := specs.NewTaskCfgCache(ctx, repos, depotTools, path.Join(w, "taskCfgCache"), 1, *btProject, *btInstance, newTs)
+		if err != nil {
+			sklog.Fatalf("Failed to create TaskCfgCache: %s", err)
+		}
+		go util.RepeatCtx(30*time.Minute, ctx, func() {
+			if err := tcc.Cleanup(OVERDUE_JOB_METRICS_PERIOD); err != nil {
+				sklog.Errorf("Failed to cleanup TaskCfgCache: %s", err)
+			}
+		})
+
+		// Data generation goroutines.
+
+		// Swarming bots.
+		swarmingClients := map[string]swarming.ApiClient{
+			swarming.SWARMING_SERVER:         swarm,
+			swarming.SWARMING_SERVER_PRIVATE: swarmInternal,
+		}
+		swarmingPools := map[string][]string{
+			swarming.SWARMING_SERVER:         swarming.POOLS_PUBLIC,
+			swarming.SWARMING_SERVER_PRIVATE: swarming.POOLS_PRIVATE,
+		}
+		swarming_metrics.StartSwarmingBotMetrics(swarmingClients, swarmingPools, metrics2.GetDefaultClient())
+
+		// Swarming tasks.
+		if err := swarming_metrics.StartSwarmingTaskMetrics(w, swarm, ctx, pc, tnp); err != nil {
+			sklog.Fatal(err)
+		}
+
+		// Number of commits in the repo.
+		go func() {
+			skiaGauge := metrics2.GetInt64Metric("repo_commits", map[string]string{"repo": "skia"})
+			infraGauge := metrics2.GetInt64Metric("repo_commits", map[string]string{"repo": "infra"})
+			for range time.Tick(5 * time.Minute) {
+				nSkia, err := repos[common.REPO_SKIA].Repo().NumCommits(ctx)
+				if err != nil {
+					sklog.Errorf("Failed to get number of commits for Skia: %s", err)
+				} else {
+					skiaGauge.Update(nSkia)
+				}
+				nInfra, err := repos[common.REPO_SKIA_INFRA].Repo().NumCommits(ctx)
+				if err != nil {
+					sklog.Errorf("Failed to get number of commits for Infra: %s", err)
+				} else {
+					infraGauge.Update(nInfra)
+				}
+			}
+		}()
+
+		// Tasks metrics.
+		// TODO(borenet): We should include metrics from all three (prod,
+		// internal, staging) instances.
+		var d db.RemoteDB
+		if *firestoreInstance != "" {
+			label := "datahopper"
+			mod, err := pubsub.NewModifiedData(*pubsubTopicSet, label, newTs)
+			if err != nil {
+				sklog.Fatal(err)
+			}
+			d, err = firestore.NewDB(ctx, firestore.FIRESTORE_PROJECT, *firestoreInstance, newTs, mod)
+			if err != nil {
+				sklog.Fatalf("Failed to create Firestore DB client: %s", err)
+			}
 		} else {
-			lvRepos.Reset()
-		}
-	})
-
-	// TaskCfgCache.
-	if *recipesCfgFile == "" {
-		*recipesCfgFile = path.Join(*workdir, "recipes.cfg")
-	}
-	depotTools, err := depot_tools.Sync(ctx, w, *recipesCfgFile)
-	if err != nil {
-		sklog.Fatalf("Failed to sync depot_tools: %s", err)
-	}
-	newTs, err := auth.NewDefaultTokenSource(*local, auth.SCOPE_USERINFO_EMAIL, pubsub.AUTH_SCOPE, bigtable.Scope)
-	if err != nil {
-		sklog.Fatal(err)
-	}
-	tcc, err := specs.NewTaskCfgCache(ctx, repos, depotTools, path.Join(w, "taskCfgCache"), 1, *btProject, *btInstance, newTs)
-	if err != nil {
-		sklog.Fatalf("Failed to create TaskCfgCache: %s", err)
-	}
-	go util.RepeatCtx(30*time.Minute, ctx, func() {
-		if err := tcc.Cleanup(OVERDUE_JOB_METRICS_PERIOD); err != nil {
-			sklog.Errorf("Failed to cleanup TaskCfgCache: %s", err)
-		}
-	})
-
-	// Data generation goroutines.
-
-	// Swarming bots.
-	swarmingClients := map[string]swarming.ApiClient{
-		swarming.SWARMING_SERVER:         swarm,
-		swarming.SWARMING_SERVER_PRIVATE: swarmInternal,
-	}
-	swarmingPools := map[string][]string{
-		swarming.SWARMING_SERVER:         swarming.POOLS_PUBLIC,
-		swarming.SWARMING_SERVER_PRIVATE: swarming.POOLS_PRIVATE,
-	}
-	swarming_metrics.StartSwarmingBotMetrics(swarmingClients, swarmingPools, metrics2.GetDefaultClient())
-
-	// Swarming tasks.
-	if err := swarming_metrics.StartSwarmingTaskMetrics(w, swarm, ctx, pc, tnp); err != nil {
-		sklog.Fatal(err)
-	}
-
-	// Number of commits in the repo.
-	go func() {
-		skiaGauge := metrics2.GetInt64Metric("repo_commits", map[string]string{"repo": "skia"})
-		infraGauge := metrics2.GetInt64Metric("repo_commits", map[string]string{"repo": "infra"})
-		for range time.Tick(5 * time.Minute) {
-			nSkia, err := repos[common.REPO_SKIA].Repo().NumCommits(ctx)
+			d, err = remote_db.NewClient(*taskSchedulerDbUrl, *pubsubTopicSet, "datahopper", newTs)
 			if err != nil {
-				sklog.Errorf("Failed to get number of commits for Skia: %s", err)
-			} else {
-				skiaGauge.Update(nSkia)
-			}
-			nInfra, err := repos[common.REPO_SKIA_INFRA].Repo().NumCommits(ctx)
-			if err != nil {
-				sklog.Errorf("Failed to get number of commits for Infra: %s", err)
-			} else {
-				infraGauge.Update(nInfra)
+				sklog.Fatal(err)
 			}
 		}
-	}()
-
-	// Tasks metrics.
-	// TODO(borenet): We should include metrics from all three (prod,
-	// internal, staging) instances.
-	var d db.RemoteDB
-	if *firestoreInstance != "" {
-		label := "datahopper"
-		mod, err := pubsub.NewModifiedData(*pubsubTopicSet, label, newTs)
-		if err != nil {
+		if err := StartTaskMetrics(ctx, d); err != nil {
 			sklog.Fatal(err)
 		}
-		d, err = firestore.NewDB(ctx, firestore.FIRESTORE_PROJECT, *firestoreInstance, newTs, mod)
-		if err != nil {
-			sklog.Fatalf("Failed to create Firestore DB client: %s", err)
-		}
-	} else {
-		d, err = remote_db.NewClient(*taskSchedulerDbUrl, *pubsubTopicSet, "datahopper", newTs)
-		if err != nil {
+
+		// Jobs metrics.
+		if err := StartJobMetrics(ctx, d, repos, tcc); err != nil {
 			sklog.Fatal(err)
 		}
-	}
-	if err := StartTaskMetrics(ctx, d); err != nil {
-		sklog.Fatal(err)
-	}
 
-	// Jobs metrics.
-	if err := StartJobMetrics(ctx, d, repos, tcc); err != nil {
-		sklog.Fatal(err)
-	}
-
-	// Generate "time to X% bot coverage" metrics.
-	if err := bot_metrics.Start(ctx, d, repos, tcc, *workdir); err != nil {
+		// Generate "time to X% bot coverage" metrics.
+		if err := bot_metrics.Start(ctx, d, repos, tcc, *workdir); err != nil {
+			sklog.Fatal(err)
+		}
+	*/
+	if err := StartFirestoreBackupMetrics(ctx); err != nil {
 		sklog.Fatal(err)
 	}
 
