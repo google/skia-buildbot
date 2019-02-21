7bc6b3fed2c0541059c60f6f7155ad6fa7d75d04
Eric Boren <borenet@google.com>
2019-02-21 12:23:32 -0500

    [task scheduler] Cache isolates with BigTable backing store
    Includes a large refactor of TaskCfgCache which breaks out the syncing
    work into its own package and makes TaskCfgCache rely on a separate
    cacher entity to insert entries into the cache.
    
    This results in our only needing to sync to a given RepoState once,
    rather than doing so every time we trigger a task at that RepoState.
    
    Bug: skia:
    Change-Id: I646457a2efd039fd1c2985f6a2d8829d69303c60

---
+++ task_scheduler/go/cacher/cacher.go
@@ -0,0 +1,130 @@
+package cacher
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path"
+
+	"go.skia.org/infra/go/git"
+	"go.skia.org/infra/go/isolate"
+	"go.skia.org/infra/task_scheduler/go/isolate_cache"
+	"go.skia.org/infra/task_scheduler/go/specs"
+	"go.skia.org/infra/task_scheduler/go/syncer"
+	"go.skia.org/infra/task_scheduler/go/types"
+)
+
+// Cacher is a struct which handles insertion of data for RepoStates into
+// various caches used by Task Scheduler. It ensures that we only sync to a
+// given RepoState once (barring transient errors).
+type Cacher struct {
+	isolate      *isolate.Client
+	isolateCache *isolate_cache.Cache
+	s            *syncer.Syncer
+	tcc          *specs.TaskCfgCache
+}
+
+// New creates a Cacher instance.
+func New(s *syncer.Syncer, tcc *specs.TaskCfgCache, isolateClient *isolate.Client, isolateCache *isolate_cache.Cache) *Cacher {
+	return &Cacher{
+		isolate:      isolateClient,
+		isolateCache: isolateCache,
+		s:            s,
+		tcc:          tcc,
+	}
+}
+
+// GetOrCacheRepoState returns the cached value(s) for the given RepoState,
+// performing the sync to obtain and insert the value(s) into the cache(s) if
+// necessary.
+func (c *Cacher) GetOrCacheRepoState(ctx context.Context, rs types.RepoState) (*specs.TasksCfg, error) {
+	ltgr := c.s.LazyTempGitRepo(rs, true)
+	defer ltgr.Done()
+
+	// Obtain the TasksCfg.
+	cv, err := c.tcc.SetIfUnset(ctx, rs, func(ctx context.Context) (*specs.CachedValue, error) {
+		var tasksCfg *specs.TasksCfg
+		err := ltgr.Do(ctx, func(co *git.TempCheckout) error {
+			cfg, err := specs.ReadTasksCfg(co.Dir())
+			if err != nil {
+				return err
+			}
+			tasksCfg = cfg
+			return nil
+		})
+		if err != nil && !specs.ErrorIsPermanent(err) {
+			return nil, err
+		}
+		return &specs.CachedValue{
+			RepoState: rs,
+			Cfg:       tasksCfg,
+			Err:       err,
+		}, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	// Obtain the IsolatedFiles.
+	if err := c.isolateCache.SetIfUnset(ctx, rs, func(ctx context.Context) (*isolate_cache.CachedValue, error) {
+		isolatedFiles := map[string]*isolate.IsolatedFile{}
+		err := ltgr.Do(ctx, func(co *git.TempCheckout) error {
+			// Isolates may need a .gclient file at the root of the checkout.
+			if err := ioutil.WriteFile(path.Join(co.Dir(), "..", ".gclient"), []byte("dummy"), os.ModePerm); err != nil {
+				return err
+			}
+
+			// Isolate all of the task specs and update their IsolateHashes.
+			// TODO(borenet): Batcharchive should do a decent job, but it'd
+			// be better to de-duplicate the isolate.Tasks here to save some
+			// work.
+			isolateFileNames := []string{}
+			done := map[string]bool{}
+			isolateTasks := []*isolate.Task{}
+			for _, taskSpec := range cv.Cfg.Tasks {
+				if done[taskSpec.Isolate] {
+					continue
+				}
+				t := &isolate.Task{
+					BaseDir:     co.Dir(),
+					Blacklist:   isolate.DEFAULT_BLACKLIST,
+					IsolateFile: path.Join(co.Dir(), "infra", "bots", taskSpec.Isolate),
+					OsType:      "linux", // Unused by our isolates.
+				}
+				isolateFileNames = append(isolateFileNames, taskSpec.Isolate)
+				isolateTasks = append(isolateTasks, t)
+				done[taskSpec.Isolate] = true
+			}
+			// Now, isolate all of the tasks.
+			if len(isolateTasks) > 0 {
+				_, isolatedFilesSlice, err := c.isolate.IsolateTasks(ctx, isolateTasks)
+				if err != nil {
+					return err
+				}
+				if len(isolatedFilesSlice) != len(isolateTasks) {
+					return fmt.Errorf("IsolateTasks returned incorrect number of isolated files (%d but wanted %d)", len(isolatedFiles), len(isolateTasks))
+				}
+				for idx, name := range isolateFileNames {
+					isolatedFiles[name] = isolatedFilesSlice[idx]
+				}
+			}
+			return nil
+		})
+		if err != nil && !specs.ErrorIsPermanent(err) {
+			return nil, err
+		}
+		errString := ""
+		if err != nil {
+			errString = err.Error()
+		}
+		return &isolate_cache.CachedValue{
+			Isolated: isolatedFiles,
+			Error:    errString,
+		}, nil
+	}); err != nil {
+		return nil, err
+	}
+
+	return cv.Cfg, nil
+}
