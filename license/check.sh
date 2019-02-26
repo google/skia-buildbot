23448f4b92f8b593c65a463b05952b33880ff8e1
Joe Gregorio <jcgregorio@google.com>
2019-02-26 15:09:46 -0500

    Start tracking all deps - for license issues.
    Bug: skia:
    Change-Id: I283b499abde349847196777fb1018f08b5f849a4

---
+++ license/check.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+DEPS=`go list -f '{{join .Deps "\n"}}' ../... | sort | uniq | grep "[a-zA-Z0-9]\+\." | \
+  egrep -v \(cloud.google.com\|go.chromium.org\|go.chromium.org\|golang.org\|google.golang.org\|go.opencensus.io\|go.skia.org\|k8s.io\)`
+
+if [ "$1" = "regenerate" ]; then
+  echo "$DEPS" > all_deps.txt
+elif [ "$1" = "" ]; then
+  diff -s <(echo "$DEPS") all_deps.txt
+else
+  echo "Run with either no arguments to check the list of dependent packages, or with an argument of 'regenerate' to regenerate the list of dependent packages."
+  exit 1
+fi
+
