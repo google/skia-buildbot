c20e6aad8ce2687b30abb50c638c8c8c5b0747f9
Joe Gregorio <jcgregorio@google.com>
2019-03-05 15:04:01 -0500

    perf - Move skia perf over to the production BigTable instance.
    Bug: skia:
    Change-Id: I24d41b3dcba574622977f81459d0a87f5c9fb87f
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/197863
    Auto-Submit: Joe Gregorio <jcgregorio@google.com>
    Reviewed-by: Eric Boren <borenet@google.com>
    Commit-Queue: Eric Boren <borenet@google.com>

--- perf/setup.sh
+++ perf/setup.sh
@@ -3,6 +3,6 @@
 # Creates the tables for Perf in BigTable.
 
 # Skia instance.
-cbt --instance perf-bt createtable skia families=V:maxversions=1,S:maxversions=1,D:maxversions=1,H:maxversions=1
+cbt --instance production createtable perf-skia families=V:maxversions=1,S:maxversions=1,D:maxversions=1,H:maxversions=1
 cbt --instance perf-bt createtable android families=V:maxversions=1,S:maxversions=1,D:maxversions=1,H:maxversions=1
 cbt --instance perf-bt createtable ct families=V:maxversions=1,S:maxversions=1,D:maxversions=1,H:maxversions=1
