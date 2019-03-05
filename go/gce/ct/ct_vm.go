80cea7941ca180629281eccf18193e604d87a500
Ravi Mistry <rmistry@google.com>
2019-03-05 09:16:36 -0500

    Experiments
    Change-Id: Iec27f4b81259a8884a1a74fa58219ea138d8aab2
    Bug: skia:

--- go/gce/ct/ct_vm.go
+++ go/gce/ct/ct_vm.go
@@ -5,25 +5,36 @@ package main
 */
 
 import (
+	"bytes"
 	"context"
 	"flag"
+	"fmt"
+	"io/ioutil"
+	"path"
+	"path/filepath"
+	"runtime"
 
 	"go.skia.org/infra/go/common"
+	"go.skia.org/infra/go/exec"
 	"go.skia.org/infra/go/gce"
 	"go.skia.org/infra/go/gce/ct/instance_types"
+	skia_instance_types "go.skia.org/infra/go/gce/swarming/instance_types"
 	"go.skia.org/infra/go/sklog"
 	"go.skia.org/infra/go/util"
 )
 
 var (
 	// Flags.
-	instances      = flag.String("instances", "", "Which instances to create/delete, eg. \"2,3-10,22\"")
-	androidBuilder = flag.Bool("android-builder", false, "Whether or not this is an android builder instance.")
-	linuxBuilder   = flag.Bool("linux-builder", false, "Whether or not this is a linux builder instance.")
-	create         = flag.Bool("create", false, "Create the instance. Either --create or --delete is required.")
-	delete         = flag.Bool("delete", false, "Delete the instance. Either --create or --delete is required.")
-	deleteDataDisk = flag.Bool("delete-data-disk", false, "Delete the data disk. Only valid with --delete")
-	ignoreExists   = flag.Bool("ignore-exists", false, "Do not fail out when creating a resource which already exists or deleting a resource which does not exist.")
+	instances       = flag.String("instances", "", "Which instances to create/delete, eg. \"2,3-10,22\"")
+	windowsInstance = flag.Bool("windows", false, "Whether the created instances should be windows.")
+	androidBuilder  = flag.Bool("android-builder", false, "Whether or not this is an android builder instance.")
+	linuxBuilder    = flag.Bool("linux-builder", false, "Whether or not this is a linux builder instance.")
+	windowsBuilder  = flag.Bool("windows-builder", false, "Whether or not this is a windows builder instance.")
+	create          = flag.Bool("create", false, "Create the instance. Either --create or --delete is required.")
+	delete          = flag.Bool("delete", false, "Delete the instance. Either --create or --delete is required.")
+	deleteDataDisk  = flag.Bool("delete-data-disk", false, "Delete the data disk. Only valid with --delete")
+	ignoreExists    = flag.Bool("ignore-exists", false, "Do not fail out when creating a resource which already exists or deleting a resource which does not exist.")
+	workdir         = flag.String("workdir", ".", "Working directory.")
 )
 
 func main() {
@@ -36,6 +47,20 @@ func main() {
 
 	if *androidBuilder && *linuxBuilder {
 		sklog.Fatal("Cannot specify both --android-builder and --linux-builder.")
+	} else if *androidBuilder && *windowsBuilder {
+		sklog.Fatal("Cannot specify both --android-builder and --windows-builder.")
+	} else if *linuxBuilder && *windowsBuilder {
+		sklog.Fatal("Cannot specify both --linux-builder and --windows-builder.")
+	}
+
+	if *windowsBuilder && !*windowsInstance {
+		sklog.Fatal("--windows must be specified if --windows-builder is specified.")
+	}
+
+	// Get the absolute workdir.
+	wdAbs, err := filepath.Abs(*workdir)
+	if err != nil {
+		sklog.Fatal(err)
 	}
 
 	instanceNums, err := util.ParseIntSet(*instances)
@@ -69,6 +94,35 @@ func main() {
 			vm = instance_types.CTAndroidBuilderInstance(num)
 		} else if *linuxBuilder {
 			vm = instance_types.CTLinuxBuilderInstance(num)
+		} else if *windowsInstance {
+			_, filename, _, _ := runtime.Caller(0)
+			checkoutRoot := path.Dir(path.Dir(path.Dir(path.Dir(filename))))
+			setupScript, startupScript, chromebotScript, err := skia_instance_types.GetWindowsScripts(ctx, checkoutRoot, wdAbs)
+			if err != nil {
+				sklog.Fatal(err)
+			}
+
+			// Set .netrc contents in the setup script.
+			// TODO(rmistry): This should NOT be required. Investigate how to give the
+			// service account all required permissions.
+			netrcContents, err := exec.RunCwd(ctx, ".", "gsutil", "cat", instance_types.GS_URL_NETRC)
+			if err != nil {
+				sklog.Fatal(err)
+			}
+			input, err := ioutil.ReadFile(setupScript)
+			if err != nil {
+				sklog.Fatal(err)
+			}
+			output := bytes.Replace(input, []byte("INSERTFILE(/tmp/.netrc)"), []byte(netrcContents), -1)
+			if err = ioutil.WriteFile(setupScript, output, 0666); err != nil {
+				sklog.Fatal(err)
+			}
+
+			if *windowsBuilder {
+				vm = instance_types.CTWindowsBuilderInstance(num, setupScript, startupScript, chromebotScript)
+			} else {
+				vm = instance_types.CTWindowsInstance(num, setupScript, startupScript, chromebotScript)
+			}
 		} else {
 			vm = instance_types.CTInstance(num)
 		}
