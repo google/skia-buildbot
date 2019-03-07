9936da1686a1152033068798d1beee7f6950bd36
Eric Boren <borenet@google.com>
2019-03-07 15:29:29 -0500

    Roll recipes to problematic commit, add logging for tests
    Bug: skia:
    Change-Id: Ia6549e922fa295d66dbe82b2cef87c2984f530a9

--- autoroll/go/repo_manager/repo_manager.go
+++ autoroll/go/repo_manager/repo_manager.go
@@ -412,6 +412,7 @@ func (r *depotToolsRepoManager) createAndSyncParentWithRemote(ctx context.Contex
 	}
 
 	if _, err := os.Stat(path.Join(r.parentDir, ".git")); err == nil {
+		sklog.Errorf("parent repo exists.")
 		if err := r.cleanParentWithRemote(ctx, remote); err != nil {
 			return err
 		}
@@ -422,6 +423,8 @@ func (r *depotToolsRepoManager) createAndSyncParentWithRemote(ctx context.Contex
 		if _, err := exec.RunCwd(ctx, r.parentDir, "git", "reset", "--hard", fmt.Sprintf("%s/%s", remote, r.parentBranch)); err != nil {
 			return err
 		}
+	} else {
+		sklog.Errorf("parent repo does not exist.")
 	}
 
 	args := []string{r.gclient, "config"}
