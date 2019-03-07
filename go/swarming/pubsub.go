fc8dde025486da2f68cfd7e2a8963d57a66dbd4d
Eric Boren <borenet@google.com>
2019-03-07 07:12:54 -0500

    [task scheduler] Switch to pull for Swarming pubsub messages
    This will simplify the move to k8s and will clean up the HTTP server
    response code metrics.
    
    Bug: skia:8636
    Change-Id: I856c9779b082a1eaa826b22e869da310eddb5bc1

--- go/swarming/pubsub.go
+++ go/swarming/pubsub.go
@@ -4,26 +4,23 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"net/http"
-	"strings"
 	"time"
 
 	"cloud.google.com/go/pubsub"
-	"github.com/gorilla/mux"
 	"go.skia.org/infra/go/common"
-	"go.skia.org/infra/go/httputils"
+	"go.skia.org/infra/go/sklog"
 )
 
 const (
 	PUBSUB_FULLY_QUALIFIED_TOPIC_TMPL    = "projects/%s/topics/%s"
 	PUBSUB_TOPIC_SWARMING_TASKS          = "swarming-tasks"
 	PUBSUB_TOPIC_SWARMING_TASKS_INTERNAL = "swarming-tasks-internal"
-	PUSH_URL_SWARMING_TASKS              = "pubsub/swarming-tasks"
 )
 
-// InitPubSub ensures that the pub/sub topics and subscriptions needed by the
-// TaskScheduler exist.
-func InitPubSub(serverUrl, topicName, subscriberName string) error {
+// InitPubSub ensures that the pub/sub topic and subscription exist and starts
+// receiving messages, calling the given callback function for each one. The
+// callback returns a bool indicating whether or not to ACK the message.
+func InitPubSub(topicName, subscriberName string, callback func(*PubSubTaskMessage) bool) error {
 	ctx := context.Background()
 
 	// Create a client.
@@ -54,28 +51,30 @@ func InitPubSub(serverUrl, topicName, subscriberName string) error {
 		return err
 	}
 	if !exists {
-		endpoint := serverUrl
-		if !strings.HasSuffix(endpoint, "/") {
-			endpoint += "/"
-		}
-		endpoint += PUSH_URL_SWARMING_TASKS
 		if _, err := client.CreateSubscription(ctx, subName, pubsub.SubscriptionConfig{
 			Topic:       topic,
 			AckDeadline: 3 * time.Minute,
-			PushConfig: pubsub.PushConfig{
-				Endpoint: endpoint,
-			},
 		}); err != nil {
 			return err
 		}
 	}
+	go func() {
+		if err := sub.Receive(ctx, func(ctx context.Context, m *pubsub.Message) {
+			var taskMsg PubSubTaskMessage
+			if err := json.Unmarshal(m.Data, &taskMsg); err != nil {
+				sklog.Errorf("Failed to decode pubsub message body: %s", err)
+				m.Ack() // We'll never be able to handle this message.
+			}
+			if callback(&taskMsg) {
+				m.Ack()
+			} else {
+				m.Nack()
+			}
+		}); err != nil {
+			sklog.Fatalf("Failed to receive pubsub messages: %s", err)
+		}
+	}()
 	return nil
-}
-
-// PubSubRequest is the format of pub/sub HTTP request body.
-type PubSubRequest struct {
-	Message      pubsub.Message `json:"message"`
-	Subscription string         `json:"subscription"`
 }
 
 // PubSubTaskMessage is a message received from Swarming via pub/sub about a
@@ -83,34 +82,4 @@ type PubSubRequest struct {
 type PubSubTaskMessage struct {
 	SwarmingTaskId string `json:"task_id"`
 	UserData       string `json:"userdata"`
-}
-
-// PubSubHandler is an interface used for handling pub/sub messages.
-type PubSubHandler interface {
-	HandleSwarmingPubSub(*PubSubTaskMessage) bool
-}
-
-// RegisterPubSubServer adds handler to r that handle pub/sub push
-// notifications.
-func RegisterPubSubServer(h PubSubHandler, r *mux.Router) {
-	r.HandleFunc("/"+PUSH_URL_SWARMING_TASKS, func(w http.ResponseWriter, r *http.Request) {
-		var req PubSubRequest
-		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-			httputils.ReportError(w, r, err, "Failed to decode request body.")
-			return
-		}
-
-		var t PubSubTaskMessage
-		if err := json.Unmarshal(req.Message.Data, &t); err != nil {
-			httputils.ReportError(w, r, err, "Failed to decode PubSubTaskMessage.")
-			return
-		}
-
-		ack := h.HandleSwarmingPubSub(&t)
-		if ack {
-			w.WriteHeader(http.StatusOK)
-		} else {
-			w.WriteHeader(http.StatusServiceUnavailable)
-		}
-	}).Methods(http.MethodPost)
 }
