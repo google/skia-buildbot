a69b4f6e4bb999cb36d144377c39431af1e390dd
Joe Gregorio <jcgregorio@google.com>
2019-03-08 13:25:07 -0500

    Launch https://api.skia.org server for doxygen docs.
    Bug: skia:
    Change-Id: Ic77007039837ee8ecae5a7bc2cf684caefb06fa2
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/199247
    Reviewed-by: Ravi Mistry <rmistry@google.com>
    Commit-Queue: Joe Gregorio <jcgregorio@google.com>

---
+++ api/README.md
@@ -0,0 +1,7 @@
+api.skia.org
+============
+
+This application serves the Doxygen documentation for the Skia library.
+
+You must set the SKIA environment variable to point at a local
+checkout of the Skia library to build a release.
---
+++ api/go/api/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"flag"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"go.skia.org/infra/go/common"
+	"go.skia.org/infra/go/httputils"
+	"go.skia.org/infra/go/sklog"
+)
+
+// flags
+var (
+	local        = flag.Bool("local", false, "Running locally if true. As opposed to in production.")
+	port         = flag.String("port", ":8000", "HTTP service address (e.g., ':8000')")
+	promPort     = flag.String("prom_port", ":20000", "Metrics service address (e.g., ':10110')")
+	resourcesDir = flag.String("resources_dir", "", "The directory to find templates, JS, and CSS files. If blank the current directory will be used.")
+)
+
+func main() {
+	common.InitWithMust(
+		"api",
+		common.PrometheusOpt(promPort),
+		common.MetricsLoggingOpt(),
+	)
+
+	r := mux.NewRouter()
+	r.PathPrefix("/").Handler(http.HandlerFunc(httputils.MakeResourceHandler(*resourcesDir)))
+
+	h := httputils.LoggingGzipRequestResponse(r)
+	if !*local {
+		h = httputils.HealthzAndHTTPS(h)
+	}
+
+	http.Handle("/", h)
+	sklog.Infoln("Ready to serve.")
+	sklog.Fatal(http.ListenAndServe(*port, nil))
+}
