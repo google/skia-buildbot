e5366af55b9506bd7f09d5dd36047e1a39e4f5c0
Eric Boren <borenet@google.com>
2019-03-14 13:40:31 -0400

    [task scheduler] Fixes for Kubernetes
    Bug: skia:8636
    Change-Id: I45440c3170ed168b185fdd111c07625fcff0a772
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/201200
    Commit-Queue: Eric Boren <borenet@google.com>
    Reviewed-by: Ben Wagner <benjaminwagner@google.com>

--- task_scheduler/create-task-scheduler-internal-sa.sh
+++ task_scheduler/create-task-scheduler-internal-sa.sh
@@ -9,10 +9,15 @@ source ../bash/ramdisk.sh
 
 # New service account we will create.
 SA_NAME="task-scheduler-internal"
+SA_EMAIL="${SA_NAME}@${PROJECT_SUBDOMAIN}.iam.gserviceaccount.com"
 
 cd /tmp/ramdisk
 
 gcloud --project=${PROJECT_ID} iam service-accounts create "${SA_NAME}" --display-name="Service account for Skia Task Scheduler Internal"
+gcloud projects add-iam-policy-binding google.com:skia-buildbots --member serviceAccount:${SA_EMAIL} --role roles/pubsub.admin
+gcloud projects add-iam-policy-binding skia-firestore --member serviceAccount:${SA_EMAIL} --role roles/datastore.user
+gcloud projects add-iam-policy-binding skia-corp --member serviceAccount:${SA_EMAIL} --role roles/pubsub.admin
+gcloud projects add-iam-policy-binding skia-corp --member serviceAccount:${SA_EMAIL} --role roles/bigtable.user
 
 gcloud beta iam service-accounts keys create ${SA_NAME}.json --iam-account="${SA_NAME}@${PROJECT_SUBDOMAIN}.iam.gserviceaccount.com"
 
