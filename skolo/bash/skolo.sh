bad314ac334006789988bda8105134bfb5ed511f
Joe Gregorio <jcgregorio@google.com>
2019-03-14 12:38:07 -0400

    skolo - Add alias for rpi rack 2.
    Bug: skia:
    Change-Id: I70e9e17773ca580c54caae811dc3027b7b813ecc

--- skolo/bash/skolo.sh
+++ skolo/bash/skolo.sh
@@ -6,6 +6,7 @@
 alias skolo_internal='ssh chrome-bot@100.115.95.131'
 alias skolo_linux='ssh chrome-bot@100.115.95.130'
 alias skolo_rpi='ssh chrome-bot@100.115.95.143'
+alias skolo_rpi2='ssh chrome-bot@100.115.95.145'
 alias skolo_win='ssh chrome-bot@100.115.95.132'
 alias skolo_win2='ssh chrome-bot@100.115.95.133'
 alias skolo_win3='ssh chrome-bot@100.115.95.134'
