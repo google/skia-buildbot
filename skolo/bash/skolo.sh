fd505a008adb0192bd088b1ed0140f963d44a5eb
Joe Gregorio <jcgregorio@google.com>
2019-03-15 11:21:04 -0400

    skolo - Add more alias helpers.
    Bug: skia:
    Change-Id: I16c23d77507193abfe2d6bd98f590a095aa7b910
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/201373
    Reviewed-by: Kevin Lubick <kjlubick@google.com>
    Commit-Queue: Joe Gregorio <jcgregorio@google.com>

--- skolo/bash/skolo.sh
+++ skolo/bash/skolo.sh
@@ -10,3 +10,12 @@ alias skolo_rpi2='ssh chrome-bot@100.115.95.135'
 alias skolo_win='ssh chrome-bot@100.115.95.132'
 alias skolo_win2='ssh chrome-bot@100.115.95.133'
 alias skolo_win3='ssh chrome-bot@100.115.95.134'
+
+# Sets up port-forwarding to the Router.
+alias skolo_internal_router='google-chrome https://localhost:8888; ssh -L 8888:192.168.1.1:443 chrome-bot@100.115.95.131'
+alias skolo_linux_router='google-chrome https://localhost:8888; ssh -L 8888:192.168.1.1:443 chrome-bot@100.115.95.130'
+alias skolo_rpi_router='google-chrome https://localhost:8888; ssh -L 8888:192.168.1.1:443 chrome-bot@100.115.95.143'
+alias skolo_rpi2_router='google-chrome https://localhost:8888; ssh -L 8888:192.168.1.1:443 chrome-bot@100.115.95.135'
+alias skolo_win_router='google-chrome https://localhost:8888; ssh -L 8888:192.168.1.1:443 chrome-bot@100.115.95.132'
+alias skolo_win2_router='google-chrome https://localhost:8888; ssh -L 8888:192.168.1.1:443 chrome-bot@100.115.95.133'
+alias skolo_win3_router='google-chrome https://localhost:8888; ssh -L 8888:192.168.1.1:443 chrome-bot@100.115.95.134'
