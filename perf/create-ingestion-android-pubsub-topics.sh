7236a95fb9cbdc075b82cba6a00132171941494d
Joe Gregorio <jcgregorio@google.com>
2019-03-16 10:04:35 -0400

    perf - Migrate android to the production bigtable instance.
    Bug: skia:
    Change-Id: I71f2e0ebaee0cfb48cccf1ba016309c9be6c92c2

--- perf/create-ingestion-android-pubsub-topics.sh
+++ perf/create-ingestion-android-pubsub-topics.sh
@@ -6,7 +6,7 @@ set -e -x
 
 source ../kube/config.sh
 
-TOPIC=perf-ingestion-android
+TOPIC=perf-ingestion-android-production
 
 gcloud pubsub topics create ${TOPIC} || true
 gsutil notification create -f json -e OBJECT_FINALIZE -t projects/${PROJECT_ID}/topics/${TOPIC} -p android-master-ingest gs://skia-perf
