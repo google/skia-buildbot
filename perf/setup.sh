b71a6d481b3dbb3ab8ce18f9bf29df9613b2d222
Joe Gregorio <jcgregorio@google.com>
2019-03-25 11:08:48 -0400

    Perf - Move cluster telemetry to the production bigtable instance.
    Ingestion into the new instance is complete:
      Old: https://screenshot.googleplex.com/hgjymeNY21G
      New: https://screenshot.googleplex.com/Wu3TJfkPqx3
    
    Bug: skia:
    Change-Id: I153cab29d5518a4b5229b8e4185c69ccbb3dde64
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/203384
    Reviewed-by: Ravi Mistry <rmistry@google.com>
    Commit-Queue: Joe Gregorio <jcgregorio@google.com>

--- perf/setup.sh
+++ perf/setup.sh
@@ -5,4 +5,4 @@
 # Skia instance.
 cbt --instance production createtable perf-skia families=V:maxversions=1,S:maxversions=1,D:maxversions=1,H:maxversions=1
 cbt --instance production createtable perf-android families=V:maxversions=1,S:maxversions=1,D:maxversions=1,H:maxversions=1
-cbt --instance perf-bt createtable ct families=V:maxversions=1,S:maxversions=1,D:maxversions=1,H:maxversions=1
+cbt --instance production createtable perf-ct families=V:maxversions=1,S:maxversions=1,D:maxversions=1,H:maxversions=1
