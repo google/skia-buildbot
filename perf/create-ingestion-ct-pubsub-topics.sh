0308759521c9908c353a5bb269ffd50fc2d98f94
Joe Gregorio <jcgregorio@google.com>
2019-03-25 11:08:48 -0400

    Perf - Move cluster telemetry to the production bigtable instance.
    Bug: skia:
    Change-Id: I153cab29d5518a4b5229b8e4185c69ccbb3dde64

--- perf/create-ingestion-ct-pubsub-topics.sh
+++ perf/create-ingestion-ct-pubsub-topics.sh
@@ -6,7 +6,7 @@ set -e -x
 
 source ../kube/config.sh
 
-TOPIC=perf-ingestion-ct
+TOPIC=perf-ingestion-ct-production
 
 gcloud pubsub topics create ${TOPIC} || true
 gsutil notification create -f json -e OBJECT_FINALIZE -t projects/${PROJECT_ID}/topics/${TOPIC} -p ingest gs://cluster-telemetry-perf
