84b3b80708cb36ce31d1c323595fcf17b2d906f6
Ravi Mistry <rmistry@google.com>
2019-03-27 12:25:27 -0400

    [Autoroller] Create new Bug line for each Android bug
    Bug: skia:8920
    Change-Id: Ifdcb4b707c73f9b4093e4b7130fb4d579bc0c423
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/204143
    Commit-Queue: Ravi Mistry <rmistry@google.com>
    Reviewed-by: Eric Boren <borenet@google.com>

--- autoroll/go/repo_manager/android_repo_manager.go
+++ autoroll/go/repo_manager/android_repo_manager.go
@@ -8,6 +8,7 @@ import (
 	"os/user"
 	"path"
 	"regexp"
+	"sort"
 	"strings"
 
 	"go.skia.org/infra/autoroll/go/codereview"
@@ -384,13 +385,17 @@ Exempt-From-Owner-Approval: The autoroll bot does not require owner approval.
 		}
 
 	}
-	// Create a single bug line and append it to the commit message.
+	// Create a separate bug line for each bug and append it to the commit
+	// message. See skbug.com/8920.
 	if len(bugMap) > 0 {
 		bugs := []string{}
 		for b := range bugMap {
 			bugs = append(bugs, b)
 		}
-		commitMsg += fmt.Sprintf("\nBug: %s", strings.Join(bugs, ", "))
+		sort.Strings(bugs)
+		for _, b := range bugs {
+			commitMsg += fmt.Sprintf("\nBug: %s", b)
+		}
 	}
 
 	if r.parentBranch != "master" {
