6472090c920a0c664171976f0f7c0b64930903f6
Ravi Mistry <rmistry@google.com>
2019-04-03 14:46:23 -0400

    Experiment
    Change-Id: I5d4111e4133ff378e6cccd8c19bcad86256c888e

---
+++ alerts_checker/go/alerts_checker/main.go
@@ -0,0 +1,62 @@
+// sheriff_emails is an application that emails the next sheriff every week.
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+	"regexp"
+
+	"go.skia.org/infra/go/common"
+	"go.skia.org/infra/go/git"
+	"go.skia.org/infra/go/sklog"
+)
+
+const ()
+
+var (
+	// Flags.
+	k8sYamlRepo = flag.String("k8_yaml_repo", "https://skia.googlesource.com/skia-public-config", "The repository where K8s yaml files are stored (eg: https://skia.googlesource.com/skia-public-config)")
+	local       = flag.Bool("local", false, "Running locally if true. As opposed to in production.")
+	workdir     = flag.String("workdir", ".", "Directory to use for scratch work.")
+)
+
+func main() {
+	common.Init()
+	defer sklog.Flush()
+	ctx := context.Background()
+
+	// Just check out what you need to here in skia-public-config or skia-corp-config specify via a flag.
+	g, err := git.NewCheckout(ctx, *k8sYamlRepo, *workdir)
+	if err != nil {
+		sklog.Fatal(err)
+	}
+
+	if err := g.Update(ctx); err != nil {
+		sklog.Fatal(err)
+	}
+
+	files, err := ioutil.ReadDir(g.Dir())
+	if err != nil {
+		sklog.Fatal(err)
+	}
+	re := regexp.MustCompile(`image: .*-dirty`)
+	for _, f := range files {
+		if filepath.Ext(f.Name()) != ".yaml" {
+			// Only interested in yaml configs.
+			continue
+		}
+		b, err := ioutil.ReadFile(filepath.Join(g.Dir(), f.Name()))
+		if err != nil {
+			sklog.Fatal(err)
+		}
+		if re.Match(b) {
+			// Do your stuff here!
+			fmt.Println(f.Name())
+		}
+
+		//fmt.Println(f.Name())
+	}
+}
