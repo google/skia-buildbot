cd10db6e5fcec84cfbd834e2b216ac91d6756296
Eric Boren <borenet@google.com>
2019-04-03 14:36:31 -0400

    Rename license dir -> licenses
    This prevents conflict on case-insensitive systems.
    
    Change-Id: Ia90b572f184f84ec66afe7a5681207868b990e2f
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/205882
    Reviewed-by: Eric Boren <borenet@google.com>
    Commit-Queue: Eric Boren <borenet@google.com>

--- run_unittests.go
+++ run_unittests.go
@@ -442,7 +442,7 @@ func main() {
 	tests = append(tests, cmdTest([]string{"make", "testci"}, "common-sk", "common-sk elements", testutils.MEDIUM_TEST))
 	tests = append(tests, cmdTest([]string{"make", "testci"}, "named-fiddles", "named-fiddles elements", testutils.MEDIUM_TEST))
 	tests = append(tests, cmdTest([]string{"make", "test"}, "push", "push elements", testutils.MEDIUM_TEST))
-	tests = append(tests, cmdTest([]string{"make"}, "license", "check go package licenses", testutils.MEDIUM_TEST))
+	tests = append(tests, cmdTest([]string{"make"}, "licenses", "check go package licenses", testutils.MEDIUM_TEST))
 
 	if !*race {
 		// put this behind a flag because polylintTests trys to build the polymer files
