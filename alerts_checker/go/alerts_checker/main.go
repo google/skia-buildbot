750b7a4c32fe3778b2f54d164a88cfe98b022141
Ravi Mistry <rmistry@google.com>
2019-04-03 13:51:41 -0400

    Experiment
    Change-Id: I5d4111e4133ff378e6cccd8c19bcad86256c888e

---
+++ alerts_checker/go/alerts_checker/main.go
@@ -0,0 +1,18 @@
+// sheriff_emails is an application that emails the next sheriff every week.
+package main
+
+import (
+	"go.skia.org/infra/go/common"
+	"go.skia.org/infra/go/sklog"
+)
+
+const ()
+
+var ()
+
+func main() {
+	common.Init()
+
+	defer sklog.Flush()
+
+}
