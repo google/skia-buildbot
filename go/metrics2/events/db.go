0b9f1fac767faf260494701f3d0046faefbbaeb3
Eric Boren <borenet@google.com>
2019-04-08 14:31:40 -0400

    [event metrics] Remove BoltDB impl
    There's really no reason to use this over BigTable, so I'm removing it
    to prevent accidental use.
    
    Change-Id: If3eb472e4f6b4c7fe5845073953dbf3d52f2f142
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/206704
    Reviewed-by: Ben Wagner aka dogben <benjaminwagner@google.com>
    Commit-Queue: Eric Boren <borenet@google.com>

--- go/metrics2/events/db.go
+++ go/metrics2/events/db.go
@@ -1,13 +1,7 @@
 package events
 
 import (
-	"bytes"
-	"encoding/gob"
-	"fmt"
 	"time"
-
-	"github.com/boltdb/bolt"
-	"go.skia.org/infra/go/util"
 )
 
 // EventDB is an interface used for storing Events in a BoltDB.
@@ -22,95 +16,4 @@ type EventDB interface {
 	// Range returns all Events in the given range from the given stream.
 	// The beginning of the range is inclusive, while the end is exclusive.
 	Range(string, time.Time, time.Time) ([]*Event, error)
-}
-
-// eventDB is a struct used for storing Events in a BoltDB.
-type eventDB struct {
-	db *bolt.DB
-}
-
-// NewEventDB returns an EventDB instance which is backed by a local Bolt DB.
-func NewEventDB(filename string) (EventDB, error) {
-	db, err := bolt.Open(filename, 0600, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	rv := &eventDB{
-		db: db,
-	}
-
-	return rv, nil
-}
-
-// See documentation for EventDB interface.
-func (m *eventDB) Close() error {
-	return m.db.Close()
-}
-
-// See documentation for EventDB interface.
-func (m *eventDB) Insert(e *Event) error {
-	if util.TimeIsZero(e.Timestamp) {
-		return fmt.Errorf("Cannot insert an event without a timestamp.")
-	}
-	if e.Stream == "" {
-		return fmt.Errorf("Cannot insert an event without a stream.")
-	}
-
-	k, err := encodeKey(e.Timestamp)
-	if err != nil {
-		return err
-	}
-	var buf bytes.Buffer
-	if err := gob.NewEncoder(&buf).Encode(e); err != nil {
-		return err
-	}
-	return m.db.Update(func(tx *bolt.Tx) error {
-		b, err := tx.CreateBucketIfNotExists([]byte(e.Stream))
-		if err != nil {
-			return err
-		}
-		return b.Put(k, buf.Bytes())
-	})
-}
-
-// See documentation for EventDB interface.
-func (m *eventDB) Append(stream string, data []byte) error {
-	return m.Insert(&Event{
-		Stream:    stream,
-		Timestamp: time.Now(),
-		Data:      data,
-	})
-}
-
-// See documentation for EventDB interface.
-func (m *eventDB) Range(stream string, start, end time.Time) ([]*Event, error) {
-	min, err := encodeKey(start)
-	if err != nil {
-		return nil, err
-	}
-	max, err := encodeKey(end)
-	if err != nil {
-		return nil, err
-	}
-
-	rv := []*Event{}
-	if err := m.db.View(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte(stream))
-		if b == nil {
-			return nil
-		}
-		c := b.Cursor()
-		for k, v := c.Seek(min); k != nil && bytes.Compare(k, max) <= 0; k, v = c.Next() {
-			e := new(Event)
-			if err := gob.NewDecoder(bytes.NewBuffer(v)).Decode(e); err != nil {
-				return err
-			}
-			rv = append(rv, e)
-		}
-		return nil
-	}); err != nil {
-		return nil, err
-	}
-	return rv, nil
 }
