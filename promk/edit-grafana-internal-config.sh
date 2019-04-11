180d573046fcc63fce02f793b8878333fc0479bb
Eric Boren <borenet@google.com>
2019-04-11 12:28:36 -0400

    Add scripts for internal grafana
    Change-Id: I3abfcb77a5c186a7ace046a8633712f7ae0a16d6
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/207603
    Commit-Queue: Eric Boren <borenet@google.com>
    Reviewed-by: Ben Wagner aka dogben <benjaminwagner@google.com>

--- promk/edit-grafana-internal-config.sh
+++ promk/edit-grafana-internal-config.sh
@@ -3,8 +3,8 @@
 # Downloads the secret grafana.ini file for editing.
 #
 # Once you are done editing the file, the config will be uploaded.
-SECRET_NAME=grafana-ini
-source ../kube/config.sh
+SECRET_NAME=grafana-internal-ini
+source ../kube/corp-config.sh
 source ../bash/ramdisk.sh
 cd /tmp/ramdisk
 kubectl get secret ${SECRET_NAME} -o json | jq -r '.data["grafana.ini"]' | base64 -d > grafana.ini
@@ -15,4 +15,4 @@ echo ""
 read -r -p "When you are done editing press enter to upload the updated grafana.ini file..." key
 kubectl create secret generic ${SECRET_NAME} --from-file=grafana.ini --dry-run -o yaml | kubectl apply -f -
 cd -
-echo "Now restart the grafana-0 pod so it picks up the config change."
+echo "Now restart the grafana-internal-0 pod so it picks up the config change."
