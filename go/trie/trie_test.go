d69dc65d42cf419a92de2a1abdb79fe9aa2d7add
Ben Wagner <benjaminwagner@google.com>
2019-04-15 15:44:55 -0400

    [task scheduler] Add debugging of busy_bots
    Change-Id: I39ec88dcb901110d9b516479c09ea42b0a90f7f9
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/208280
    Commit-Queue: Eric Boren <borenet@google.com>
    Auto-Submit: Ben Wagner aka dogben <benjaminwagner@google.com>
    Reviewed-by: Eric Boren <borenet@google.com>

--- go/trie/trie_test.go
+++ go/trie/trie_test.go
@@ -17,6 +17,7 @@ func TestTrie(t *testing.T) {
 	trie.Insert([]string{"c", "b", "a"}, "3")
 	trie.Insert([]string{}, "4")
 	trie.Insert([]string{"c", "d"}, "5")
+	assert.Equal(t, 5, trie.Len())
 	searchSubset := func(search []string) []string {
 		got := trie.SearchSubset(search)
 		rv := make([]string, 0, len(got))
@@ -47,6 +48,7 @@ func TestTrie(t *testing.T) {
 	deepequal.AssertDeepEqual(t, []string{}, searchExact([]string{"d", "b", "a", "c"}))
 
 	trie.Delete([]string{"c", "b", "a"}, "3")
+	assert.Equal(t, 4, trie.Len())
 
 	deepequal.AssertDeepEqual(t, []string{"1", "2", "4"}, searchSubset([]string{"a", "b", "c"}))
 	deepequal.AssertDeepEqual(t, []string{"1", "2", "4", "5"}, searchSubset([]string{"d", "b", "a", "c"}))
@@ -67,4 +69,17 @@ func TestString(t *testing.T) {
     "c": Node([2], {}),
   }),
 }))`, trie.String())
+}
+
+func TestLen(t *testing.T) {
+	testutils.SmallTest(t)
+	trie := New()
+	assert.Equal(t, 0, trie.Len())
+	trie.Insert([]string{"a"}, "1")
+	assert.Equal(t, 1, trie.Len())
+	trie.Insert([]string{"b", "c"}, "2")
+	assert.Equal(t, 2, trie.Len())
+	trie.Delete([]string{"a"}, "1")
+	trie.Delete([]string{"b", "c"}, "2")
+	assert.Equal(t, 0, trie.Len())
 }
