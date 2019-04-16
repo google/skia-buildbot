723ba844518a58c4dd323215f543d97ce2e6c422
Stephan Altmüller <stephana@google.com>
2019-04-16 13:52:31 -0400

    Expose number of concurrent operations as a flag in dstool
    This allows to tune the level of concurrency when manipulating
    many instances at once and receiving conflicts.
    
    Bug: skia:
    Change-Id: I37449d3d9402d9755ad7453153202050a0db6ab3

--- cmd/dstool/main.go
+++ cmd/dstool/main.go
@@ -25,8 +25,9 @@ import (
 
 // Command line flags.
 var (
-	dsNamespace = flag.String("ds_namespace", "", "Cloud datastore namespace to be used by this instance.")
-	projectID   = flag.String("project_id", common.PROJECT_ID, "GCP project ID.")
+	concurrently = flag.Int("concurrent", 500, "Concurrently processed entities. Lower if error occur.")
+	dsNamespace  = flag.String("ds_namespace", "", "Cloud datastore namespace to be used by this instance.")
+	projectID    = flag.String("project_id", common.PROJECT_ID, "GCP project ID.")
 )
 
 // opsEntry defines the parameters and the function for an operation
@@ -99,7 +100,7 @@ func deleteEntities(client *datastore.Client, params ...string) {
 			sklog.Errorf("Error deleting slice: %s", err)
 		}
 	}
-	processConcurrently(client, procFn, entityName, 500)
+	processConcurrently(client, procFn, entityName, *concurrently)
 }
 
 // touchEntities implements the touch command.
