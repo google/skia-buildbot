2b117af46dcb7e15d9dd7ae5732c44b138cb62f7
Eric Boren <borenet@google.com>
2019-04-25 10:28:13 -0400

    Support running find_go_import_paths without --start_pkg
    ie. find all import paths to the given package from go.skia.org/infra/...
    
    Change-Id: I02660d40a2531d5f07b474d58667d478dddfc2f0

--- scripts/find_go_import_paths/find_go_import_paths.go
+++ scripts/find_go_import_paths/find_go_import_paths.go
@@ -12,22 +12,39 @@ import (
 )
 
 var (
-	startPkg = flag.String("start_pkg", "", "Start with this package.")
-	findPkg  = flag.String("find_pkg", "", "Find importers of this package.")
+	startPkg = flag.String("start_pkg", "", "Optional; find import paths from this package.")
+	findPkg  = flag.String("find_pkg", "", "Required; find importers of this package.")
 )
 
 func main() {
 	common.Init()
-	// Pre-load data for all packages in this repo.
-	if _, err := imports.LoadAllPackageData(context.Background()); err != nil {
-		sklog.Fatal(err)
+
+	if *findPkg == "" {
+		sklog.Fatal("--find_pkg is required.")
 	}
-	paths, err := imports.FindImportPaths(context.Background(), *startPkg, *findPkg)
+
+	// Pre-load data for all packages in this repo.
+	allPkgs, err := imports.LoadAllPackageData(context.Background())
 	if err != nil {
 		sklog.Fatal(err)
 	}
-	for _, path := range paths {
-		str := strings.Join(path, " <- ")
-		fmt.Println(str)
+	if *startPkg != "" {
+		allPkgs = map[string]*imports.Package{
+			*startPkg: allPkgs[*startPkg],
+		}
+	}
+	allPaths := map[string]bool{}
+	for name := range allPkgs {
+		paths, err := imports.FindImportPaths(context.Background(), name, *findPkg)
+		if err != nil {
+			sklog.Fatal(err)
+		}
+		for _, path := range paths {
+			str := strings.Join(path, " <- ")
+			allPaths[str] = true
+		}
+	}
+	for path := range allPaths {
+		fmt.Println(path)
 	}
 }
