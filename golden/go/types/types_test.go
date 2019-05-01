0a9982f427e53b9cc96b2f82541d68cf943f8cb4
Kevin Lubick <kjlubick@google.com>
2019-05-01 10:56:55 -0400

    [gold] Refactor baseliner and add tests
    Refactor baseliner to its own package
    
    Remove old endpoints from baseline server, as the fix has been deployed
    for several months now.
    
    Change-Id: I142b69357f24ce66c13e78df5c8c10bd469a7958

--- golden/go/types/types_test.go
+++ golden/go/types/types_test.go
@@ -13,13 +13,13 @@ func TestGoldenTrace(t *testing.T) {
 	// Test NewGoldenTrace.
 	g := NewGoldenTraceN(N)
 	if got, want := g.Len(), N; got != want {
-		t.Errorf("Wrong Values Size: Got %v Want %v", got, want)
+		t.Errorf("Wrong Digests Size: Got %v Want %v", got, want)
 	}
-	if got, want := len(g.Params_), 0; got != want {
-		t.Errorf("Wrong Params_ initial size: Got %v Want %v", got, want)
+	if got, want := len(g.Keys), 0; got != want {
+		t.Errorf("Wrong Keys initial size: Got %v Want %v", got, want)
 	}
 
-	g.Values[0] = "a digest"
+	g.Digests[0] = "a digest"
 
 	if got, want := g.IsMissing(1), true; got != want {
 		t.Errorf("All values should start as missing: Got %v Want %v", got, want)
@@ -31,40 +31,40 @@ func TestGoldenTrace(t *testing.T) {
 	// Test Merge.
 	M := 7
 	gm := NewGoldenTraceN(M)
-	gm.Values[1] = "another digest"
+	gm.Digests[1] = "another digest"
 	g2 := g.Merge(gm)
 	if got, want := g2.Len(), N+M; got != want {
 		t.Errorf("Merge length wrong: Got %v Want %v", got, want)
 	}
-	if got, want := g2.(*GoldenTrace).Values[0], "a digest"; got != want {
+	if got, want := g2.(*GoldenTrace).Digests[0], "a digest"; got != want {
 		t.Errorf("Digest not copied correctly: Got %v Want %v", got, want)
 	}
-	if got, want := g2.(*GoldenTrace).Values[6], "another digest"; got != want {
+	if got, want := g2.(*GoldenTrace).Digests[6], "another digest"; got != want {
 		t.Errorf("Digest not copied correctly: Got %v Want %v", got, want)
 	}
 
 	// Test Grow.
 	g = NewGoldenTraceN(N)
-	g.Values[0] = "foo"
+	g.Digests[0] = "foo"
 	g.Grow(2*N, tiling.FILL_BEFORE)
-	if got, want := g.Values[N], "foo"; got != want {
+	if got, want := g.Digests[N], "foo"; got != want {
 		t.Errorf("Grow didn't FILL_BEFORE correctly: Got %v Want %v", got, want)
 	}
 
 	g = NewGoldenTraceN(N)
-	g.Values[0] = "foo"
+	g.Digests[0] = "foo"
 	g.Grow(2*N, tiling.FILL_AFTER)
-	if got, want := g.Values[0], "foo"; got != want {
+	if got, want := g.Digests[0], "foo"; got != want {
 		t.Errorf("Grow didn't FILL_AFTER correctly: Got %v Want %v", got, want)
 	}
 
 	// Test Trim
 	g = NewGoldenTraceN(N)
-	g.Values[1] = "foo"
+	g.Digests[1] = "foo"
 	if err := g.Trim(1, 3); err != nil {
 		t.Fatalf("Trim Failed: %s", err)
 	}
-	if got, want := g.Values[0], "foo"; got != want {
+	if got, want := g.Digests[0], "foo"; got != want {
 		t.Errorf("Trim didn't copy correctly: Got %v Want %v", got, want)
 	}
 	if got, want := g.Len(), 2; got != want {
@@ -111,7 +111,7 @@ func TestSetAt(t *testing.T) {
 		}
 	}
 	for i, tc := range testCases {
-		if got, want := tr.Values[i], tc.want; got != want {
+		if got, want := tr.Digests[i], tc.want; got != want {
 			t.Errorf("SetAt(%d, %#v)failed: Got %s Want %s", i, []byte(tc.want), got, want)
 		}
 	}
