88e0c238d5d270d3700e3bb79bd9081a311306fa
Kevin Lubick <kjlubick@google.com>
2019-05-03 09:12:27 -0400

    Add test for detecting testify imports
    This is a follow up on the quest to remove our run-time, executable's
    dependencies on testify.
    
    Change-Id: Idd7a10b7b18d6c2225b1a8e24662603c75cda847
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/211822
    Commit-Queue: Kevin Lubick <kjlubick@google.com>
    Reviewed-by: Ben Wagner aka dogben <benjaminwagner@google.com>

--- go/imports/imports_test.go
+++ go/imports/imports_test.go
@@ -7,6 +7,7 @@ import (
 
 	assert "github.com/stretchr/testify/require"
 	"go.skia.org/infra/go/testutils"
+	"go.skia.org/infra/go/util"
 )
 
 func TestNoPackageImportsTesting(t *testing.T) {
@@ -16,4 +17,45 @@ func TestNoPackageImportsTesting(t *testing.T) {
 	importers, err := FindImporters(context.Background(), "testing")
 	assert.NoError(t, err)
 	assert.Emptyf(t, importers, "No non-test packages should import \"testing\" but the following do:\n%s", strings.Join(importers, "\n"))
+}
+
+// Any package named testutil(s) and the packages in the
+// following list are allowed to import testify
+var testifyExceptions = []string{
+	"go.skia.org/infra/go/deepequal",
+	"go.skia.org/infra/go/mockhttpclient",
+	"go.skia.org/infra/golden/go/mocks",
+}
+
+var unwantedTestifyPackages = []string{
+	"github.com/stretchr/testify/require",
+	"github.com/stretchr/testify/assert",
+}
+
+// TODO(kjlubick): this list should be empty.
+// items should not be added to this list, only removed.
+var legacyTestifyPackages = []string{
+	"go.skia.org/infra/task_driver/go/db",
+	"go.skia.org/infra/task_driver/go/td",
+	"go.skia.org/infra/task_scheduler/go/db",
+	"go.skia.org/infra/task_scheduler/go/isolate_cache",
+	"go.skia.org/infra/task_scheduler/go/tryjobs",
+}
+
+func TestNoPackageImportsTestify(t *testing.T) {
+	testutils.LargeTest(t)
+
+	for _, p := range unwantedTestifyPackages {
+
+		// Assert that no package has this import
+		importers, err := FindImporters(context.Background(), p)
+		assert.NoError(t, err)
+		badImports := []string{}
+		for _, i := range importers {
+			if !strings.HasSuffix(i, "testutil") && !strings.HasSuffix(i, "testutils") && !util.In(i, testifyExceptions) && !util.In(i, legacyTestifyPackages) {
+				badImports = append(badImports, i)
+			}
+		}
+		assert.Emptyf(t, badImports, "No non-test or non-testutil packages should import %q but the following do:\n%s", p, strings.Join(badImports, "\n"))
+	}
 }
