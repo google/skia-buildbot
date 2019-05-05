0637549d777f2657c2bd7ac20c94be839182ad00
Kevin Lubick <kjlubick@google.com>
2019-05-05 12:18:50 -0400

    [gold] Refactor tally -> DigestCounter
    In the first PS, I change the tests to be more clear.
    In the second PS, I rename everything around tally because
    the name tally wasn't super clear to me. I also wanted
    to introduce an interface TallyCounter instead of just the
    bare struct.
    
    Bug: skia:
    Change-Id: I148df53cc9e2161bf67bc3188853a9bbb969d333

---
+++ golden/go/digest_counter/digest_counter_test.go
@@ -0,0 +1,101 @@
+package digest_counter
+
+import (
+	"net/url"
+	"testing"
+
+	assert "github.com/stretchr/testify/require"
+	"go.skia.org/infra/go/testutils"
+	"go.skia.org/infra/go/tiling"
+	"go.skia.org/infra/go/util"
+	"go.skia.org/infra/golden/go/types"
+)
+
+func TestDigestCountCalculate(t *testing.T) {
+	testutils.SmallTest(t)
+	tile := makeTestTile()
+
+	dc := New()
+	dc.Calculate(tile)
+
+	assert.Equal(t, map[string]DigestCount{
+		"foo:x86": {
+			"aaa": 2,
+			"bbb": 1,
+		},
+		"foo:x86_64": {
+			"aaa": 1,
+			"ccc": 1,
+		},
+	}, dc.ByTrace())
+
+	assert.Equal(t, map[string]DigestCount{
+		"foo": {
+			"aaa": 3,
+			"bbb": 1,
+			"ccc": 1,
+		},
+	}, dc.ByTest())
+
+	assert.Equal(t, map[string]util.StringSet{
+		"foo": {
+			"aaa": true,
+		},
+	}, dc.MaxDigestsByTest())
+}
+
+func TestDigestCountByQuery(t *testing.T) {
+	testutils.SmallTest(t)
+	tile := makeTestTile()
+
+	dc := New()
+	dc.Calculate(tile)
+
+	bq := dc.ByQuery(tile, url.Values{
+		types.CORPUS_FIELD: []string{"gm"},
+	})
+
+	assert.Equal(t, DigestCount{
+		"aaa": 2,
+		"bbb": 1,
+	}, bq)
+
+	bq = dc.ByQuery(tile, url.Values{
+		types.CORPUS_FIELD: []string{"image"},
+	})
+
+	assert.Equal(t, DigestCount{
+		"aaa": 1,
+		"ccc": 1,
+	}, bq)
+
+	bq = dc.ByQuery(tile, url.Values{
+		types.PRIMARY_KEY_FIELD: []string{"foo"},
+	})
+
+	assert.Equal(t, DigestCount{
+		"aaa": 3,
+		"bbb": 1,
+		"ccc": 1,
+	}, bq)
+}
+
+func makeTestTile() *tiling.Tile {
+	// Create a tile to test against.
+	tile := tiling.NewTile()
+	trace1 := types.NewGoldenTrace()
+	trace1.Digests[0] = "aaa"
+	trace1.Digests[1] = "aaa"
+	trace1.Digests[2] = "bbb"
+	trace1.Keys[types.PRIMARY_KEY_FIELD] = "foo"
+	trace1.Keys[types.CORPUS_FIELD] = "gm"
+	tile.Traces["foo:x86"] = trace1
+
+	trace2 := types.NewGoldenTrace()
+	trace2.Digests[0] = "ccc"
+	trace2.Digests[1] = "aaa"
+	trace2.Keys[types.PRIMARY_KEY_FIELD] = "foo"
+	trace2.Keys[types.CORPUS_FIELD] = "image"
+	tile.Traces["foo:x86_64"] = trace2
+	return tile
+}
