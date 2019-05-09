e70d8d2b4af926489aae7d36d7b75ae0e90237bd
Kevin Lubick <kjlubick@google.com>
2019-05-09 15:00:46 -0400

    [gold] Update kubernetes generation files
    Note to the reviewer, these scripts/templates
    are used to generate files like this:
    https://skia.googlesource.com/skia-public-config/+/refs/heads/master/gold-chrome-gpu-skiacorrectness.yaml
    
    The ones in prod have jumped ahead of this generation
    code, so this aligns them all.
    
    Hopefully, this lets me make flutter-skiacorrectness
    not a stateful set anymore (and fuchsia too).
    
    Change-Id: Ib668e42b0cad68189d49294aab0d46d9e30b4b9e

--- golden/k8s-gen-config.sh
+++ golden/k8s-gen-config.sh
@@ -25,6 +25,7 @@ TRACE_SERVER_CONF="${DEPLOY_CONF}-traceserver.yaml"
 INGESTION_SERVER_CONF="${DEPLOY_CONF}-ingestion.yaml"
 CORRECTNESS_CONF="${DEPLOY_CONF}-skiacorrectness.yaml"
 BASELINE_SERVER_CONF="${DEPLOY_CONF}-baselineserver.yaml"
+DIFF_SERVER_CONF="${DEPLOY_CONF}-diffserver.yaml"
 
 mkdir -p $CONF_OUT_DIR
 rm -f $CONF_OUT_DIR/*
@@ -73,6 +74,13 @@ kube-conf-gen -c "${TMPL_DIR}/gold-common.json5" \
               -parse_conf=false -quote -strict \
               -o "${BASELINE_SERVER_CONF}"
 
+kube-conf-gen -c "${TMPL_DIR}/gold-common.json5" \
+              -c "${INSTANCE_DIR}/${INSTANCE_ID}-instance.json5" \
+              -extra "INSTANCE_ID:${INSTANCE_ID}" \
+              -t "${TMPL_DIR}/gold-diffserver-template.yaml" \
+              -parse_conf=false -quote -strict \
+              -o "${DIFF_SERVER_CONF}"
+
 set +x
 
 # Push the config map to kubernetes
@@ -83,13 +91,16 @@ echo "kubectl create configmap $CONF_MAP --from-file=$INGEST_CONF"
 # Push the trace server and show pods so we can see if it landed correctly.
 echo "kubectl apply -f ${TRACE_SERVER_CONF} && kubectl get pods -w -l app=gold-$INSTANCE_ID-traceserver"
 
-# Push the trace server and show pods so we can see if it landed correctly.
+# Push the ingestion and show pods so we can see if it landed correctly.
 echo "kubectl apply -f ${INGESTION_SERVER_CONF} && kubectl get pods -w -l app=gold-$INSTANCE_ID-ingestion"
 
-# Push the trace server and show pods so we can see if it landed correctly.
+# Push the diff server and show pods so we can see if it landed correctly.
+echo "kubectl apply -f ${DIFF_SERVER_CONF} && kubectl get pods -w -l app=gold-$INSTANCE_ID-diffserver"
+
+# Push the main server and show pods so we can see if it landed correctly.
 echo "kubectl apply -f ${CORRECTNESS_CONF} && kubectl get pods -w -l app=gold-$INSTANCE_ID-skiacorrectness"
 
-# Push the trace server and show pods so we can see if it landed correctly.
+# Push the baseline server and show pods so we can see if it landed correctly.
 echo "kubectl apply -f ${BASELINE_SERVER_CONF} && kubectl get pods -w -l app=gold-$INSTANCE_ID-baselineserver"
 
 # Push the trace server and show pods so we can see if it landed correctly.
