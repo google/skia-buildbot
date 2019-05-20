2c45c5a1605cc4ec76a31eb214a30e6b8363f5d3
Kevin Lubick <kjlubick@google.com>
2019-05-20 12:57:22 -0400

    Implements a basic trace store based on BigTable
    How to review this CL:
    
      1. Read bt_tracestore/BIGTABLE.md to get an overview of how the
         data is stored in BT.
      2. Browse through the consts and types in bt_tracestore/types.go
      3. Read TestBTTraceStorePutGet to get a sense of how this code will be used.
      4. Read the implementations for Put and GetTile, as that's the core pathway.
      5. Look at everything else.
    
    Initially uploaded as https://skia-review.googlesource.com/c/buildbot/+/213381
    but taken over by me.
    
    Bug: skia:9096
    Change-Id: Ida1279f315abc5c21fcca42563e77b16342cc4c4

---
+++ golden/go/tracestore/bt_tracestore/BIGTABLE.md
@@ -0,0 +1,194 @@
+Storing Gold traces in BigTable
+===============================
+
+This implementation is based on the original btts.go made for Perf.
+See perf/BIGTABLE.md for an overview of that schema.
+
+Data we want to store
+---------------------
+
+We need to store traces, which are arranged in a table.
+Traces have commits as their columns, a tiling.traceID as their rows, and
+a digest as their cells. These traceIDs are derived from the key:value pairs
+associated with the digest, e.g. {"os":"Android", "gpu": "NVidia1080"}
+
+Gold has an idea of a tile, which is a collection of N of these trace columns (i.e. commits).
+
+
+Mapping the data to BigTable
+----------------------------
+
+BigTable (BT) stores its data as one big table. Concretely, we have
+
+```
+     | col_1  | col_2  | col_3  | ...
+==================================
+row_1| [data] | [data] | [data] | ...
+row_2| [data] | [data] | [data] | ...
+...
+```
+
+For performance, BT provides [Column Families](https://cloud.google.com/bigtable/docs/schema-design#column_families_and_column_qualifiers)
+which group columns together so you can query some cells from a row belonging
+to that family w/o getting everything. As an analogy, if the whole table is a Sheets
+workbook, a column family can be thought of as a single spreadsheet in that workbook (at least,
+for our purposes).
+
+BT uses the term "Column Qualifier", which essentially just means "column name", because they
+can be arbitrary strings.
+
+It's tempting to just put the commit hashes as columns, the trace ids as the row keys and store the
+commits in the cells, but this isn't ideal for 5 performance reasons:
+
+  1. traceIDs get really long with lots of params - BT has a limit of 4kb per row key.
+  2. digests are long strings that are repeated a lot - if we stored them as int64s, it would save
+     a lot of data per request (32 bytes per cell -> 8 bytes per cell).
+  3. BT has a soft limit of 100Mb of data per row, and when it does a fetch, it fetches all columns
+     in a row. We only really want to fetch the last few hundred commits (the last tile or two), so
+     we need to wrap the data for a trace across many rows.
+  4. We want to split our rows up further into shards, so we can execute multiple queries at once
+     (one query per shard).
+
+To address these performance issues, we need to store our traces and some auxiliary data.
+
+  1. An OrderedParamSet that can convert tiling.TraceId (long string) <->
+     EncodedTraceId (short string).
+  2. A DigestMap that can convert types.Digest (string) <-> DigestID (int64).
+  3. A monotonically increasing counter to derive more DigestIDs.
+
+These 3 "tables" along with the traces will be stored using 4 Column Families and can
+logically thought of being stored as independent "tables" or "spreadsheets" even
+though they are all stored in the "one big table".
+
+Row naming scheme
+-----------------
+There is a row naming scheme (for all 4 tables) as follows:
+
+    [shard]:[namespace]:[type]:[tile]:[key]
+
+Where shard and key are optional (can be "").
+namespace is constant for all tracestore data: "ts". Reminder that there's one table per Gold
+instance, so if we store more data to BT (e.g. expectations, tryjobs, etc) we need to have such
+a namespace.
+tile is a 0 padded 32 bit number, which for tile 0 (the oldest commits), would
+manifest as "2147483646". (tile 0 => key 2^32-1 => 2147483646)
+
+BigTable can easily fetch rows starting with a given prefix, so this naming schema
+is set up to request things of a type for one or more tiles, with optional sharding.
+
+Note that sharding is a thing we have enabled by our choice of row names, not something
+given out for free by BigTable.
+
+Gold (and perf) shards its data based on the based on the key (essentially key % 32)
+This makes the workload be spread more evenly, even when fetching only one tile.
+The shards come first in the naming convention to try to spread the rows across multiple
+tablets for better performance (rows on BT are stored sorted by row key).
+
+Storing the OrderedParamSet for traceIDs
+----------------------------------------
+As mentioned above, traceIDs are derived from the paramtools.Params map of key-value pairs.
+We compress these maps using a paramstool.OrderedParamSet which concretely look like:
+
+    ,0=1,4=2,3=1,
+
+To do this compression/decompression, we need to store the OrderedParamSet (OPS).
+Conceptually, this is stored like:
+```
+           |   OPS   |    H   |
+==============================
+ops_tile0 | [bytes] | [hash] |
+ops_tile1 | [bytes] | [hash] |
+...
+```
+
+The bytes stored in under the "OPS" column are just a gob encoded OrderedParamSet and
+the hash stored under the "H" column is the hash of the OPS, used to query the row when updating.
+
+As mentioned before, ops_tile0 is a conceptual simplification of the actual
+row name. Given the row naming schema, we define "type" for OPS to be "o" and
+let "shard" and "key" both be "".
+Thus, the actual row for tile 0 would be
+
+    :ts:o:2147483646:
+
+
+Storing the Digest Map
+----------------------
+We need to store a (potentially large) map of types.Digest (string) <-> DigestID (int64)
+with there being one map per tile. Conceptually, this is stored like:
+```
+              | [digest1] | [digest2] | [digest3] | ...
+====================================================
+map_tile0_000 |  [blank]  |  [int64]  |  [blank]  |
+map_tile0_001 |  [int64]  |  [blank]  |  [blank]  |
+...
+map_tile0_ffe |  [blank]  |  [blank]  |  [blank]  |
+map_tile0_fff |  [blank]  |  [blank]  |  [int64]  |
+map_tile1_000 |  [blank]  |  [int64]  |  [blank]  |
+...
+```
+
+Basically, we take a digest, chop off the first three characters, use those as
+the "key" in the row so we can make sure our rows don't exceed the maximum size.
+Given the soft row limit of 100Mb, a digest column + a cell is at most 40 bytes,
+which means a soft limit of 2.5 million digests per row. Splitting on 3 hex
+characters means we have 4096-way splitting, so a soft limit of 10 billion digests.
+
+Given that we define "type" for the digest map to be "d", an
+example row for tile 0 and digest "92eb5ffee6ae2fec3ad71c777531578f"
+(assume this key is tied to shard 19) would be:
+
+    19:ts:d:2147483646:92e
+
+The ids of digests out start at 0 (for MISSING_DIGEST aka "") and increase monotonically.
+We manage these ids ourselves because using the autogenerated ids can lead to issues when
+migrating data from one table to another.
+
+Storing the Digest ID counter
+-----------------------------
+We assign newly seen digests an id that is simply an increasing int64. To store
+this int64 in BT, we essentially have a single cell dedicated to this:
+
+```
+               |    idc   |
+====================================================
+digest_counter |  [int64] |
+```
+Even though the digest maps are tile-by-tile, we have one global id counter
+to make sure that if we merge two tiles together, we don't have any id conflicts.
+
+There's only one row for the counter, which is:
+
+    :ts:i:0000000000:
+
+Storing the traces
+------------------
+
+With all the auxiliary data set, we can look at how the traces themselves are stored.
+Going with the default tile size of 256, the data would be like:
+
+```
+             | offset0 | offset1 | ... | offset255
+====================================================
+tile0_trace0 | [dig_1] | [dig_1] | ... | [dig_2] |
+tile0_trace1 | [dig_1] | [dig_2] | ... | [dig_2] |
+...
+tile0_traceN | [dig_8] | [blank] | ... | [dig_6] |
+tile1_trace0 | [blank] | [dig_2] | ... | [blank] |
+...
+```
+
+The columns are the offset into a tile of a commit. For example, the third commit in a repo would
+end up in tile 0, offset 3. The 1000th commit with a tile of 256 would be in tile 3 (1000 / 256),
+offset 232 (1000 % 256). This has the effect of wrapping a given trace across many tiles.
+The row keys are based off the encoded trace ids (which correspond to a ParamSet).
+The cells are the int64 ids of the digest that were drawn according to that ParamSet.
+
+The rows follow the standard naming scheme, using "t" as "type", and making use of the shards
+(32 by default). The value for "key" is the encoded ParamSet (and from this key a shard is derived).
+An example row for encoded ParamSet ",0=1,1=3,3=0," on tile 0 (assume shard calculates
+to be 7) would be:
+
+    07:ts:t:2147483646:,0=1,1=3,3=0,
+
+Blank cells will be read as id 0, which is hard-coded to belong to MISSING_DIGEST ("").
---
+++ golden/go/tracestore/bt_tracestore/types.go
@@ -0,0 +1,273 @@
+package bt_tracestore
+
+import (
+	"crypto/md5"
+	"fmt"
+	"time"
+
+	"cloud.google.com/go/bigtable"
+	"go.skia.org/infra/go/paramtools"
+	"go.skia.org/infra/go/skerr"
+	"go.skia.org/infra/golden/go/types"
+)
+
+// Constants adapted from btts.go
+// See BIGTABLE.md for an overview of how the data is stored in BT.
+const (
+	// Namespace for this package's data. Due to the fact that there is one table per instance,
+	// This makes sure we don't have collisions between traces and something else
+	// in BT (i.e. git info)
+	traceStoreNameSpace = "ts"
+
+	// Column Families.
+	// https://cloud.google.com/bigtable/docs/schema-design#column_families_and_column_qualifiers
+	digestMapFamily = "D" // Store a mapping of Digest->DigestID
+	idCounterFamily = "I" // Keeps a monotonically increasing number to generate DigestIDs
+	opsFamily       = "O" // ops short for Ordered Param Set
+	traceFamily     = "T" // Holds "0"..."tilesize-1" columns with a DigestID at each cell
+
+	// The columns (and rows) in the digest map family are derived from the digest, which are md5
+	// hashes. The row will be the first two characters of the hash and the column will
+	// be the remaining characters (see b.rowAndColNameFromDigest)
+	// All entries will have the following prefix
+	digestMapFamilyPrefix = digestMapFamily + ":"
+
+	// Columns in the ID counter family. There is only one row and one column.
+	idCounterColumn = "idc"
+
+	// Columns in the OrderedParamSet column family.
+	opsHashColumn   = "H"
+	opsOpsColumn    = "OPS"
+	hashFullColName = opsFamily + ":" + opsHashColumn
+	opsFullColName  = opsFamily + ":" + opsOpsColumn
+
+	// The columns in the trace family are "0", "1", "2"..."N" where N is
+	// the BT tile size (default below). These values correspond to the commitOffset,
+	// where 0 is the first (most recent) commit in the tile and N is the last (oldest)
+	// commit in the tile.
+	// They will all have the following prefix.
+	traceFamilyPrefix = traceFamily + ":"
+
+	// Define the row types.
+	typDigestMap  = "d"
+	typeIdCounter = "i"
+	typOPS        = "o"
+	typTrace      = "t"
+
+	// This is the size of the tile in Big Table. That is, how many commits do we store in one tile.
+	// We can have up to 2^32 tiles in big table, so this would let us store 1 trillion
+	// commits worth of data. This tile size does not  need to be related to the tile size that
+	// Gold operates on (although when tuning, it should be greater than, or an even divisor
+	// of the Gold tile size). The first commit in the repo belongs to tile 2^32-1 and tile numbers
+	// decrease for newer commits.
+	DefaultTileSize = 256
+
+	// Default number of shards used, if not shards provided in BTConfig. A shard splits the traces
+	// up on a tile. If a tile exists on shard N in tile A, it will be on shard N for all tiles.
+	// Having traces on shards lets BT split up the work more evenly.
+	DefaultShards = 32
+
+	// To avoid many increment calls to the id counter cell, we request a number of
+	// ids at once. This number is arbitrarily picked and can be increased if need be.
+	batchIdRequest = 256
+
+	readTimeout  = 4 * time.Minute
+	writeTimeout = 10 * time.Minute
+
+	// BadTileKey is returned in error conditions.
+	BadTileKey = TileKey(-1)
+)
+
+// List of families (conceptually similar to tables) we are creating in BT.
+var btColumnFamilies = []string{
+	traceFamily,
+	opsFamily,
+	idCounterFamily,
+	digestMapFamily,
+}
+
+// We have one global digest id counter, so just hard-code it to tile 0.
+var idCounterRow = rowName(typeIdCounter, 0, "")
+
+// TileKey is the identifier for each tile held in BigTable.
+//
+// Note that tile keys are in the opposite order of tile offset, that is, the first commit
+// in a repo goes in the first tile, which has key 2^32-1. We do this so more recent
+// tiles come first in sort order.
+type TileKey int32
+
+// DigestID is an arbitrary number for referring to a types.Digest (string)
+// that is stored in a DigestMap.
+type DigestID int64
+
+// MissingDigestId is always assigned to 0.
+const MissingDigestId = DigestID(0)
+
+// EncodedTraceId is a shortened form of a tiling.TraceId, e.g. 0=1,1=3,3=0,
+// Those indices are references to the OrderedParamSet stored in EncTile.
+// See  params.paramsEncoder
+type EncodedTraceId string
+
+// EncTile contains an encoded tile.
+type EncTile struct {
+	// maps a trace id to the list of digest ids. The list corresponds to the commits, with index
+	// 0 being the most recent commit.
+	traces    map[EncodedTraceId][]DigestID
+	ops       *paramtools.OrderedParamSet
+	digestMap *DigestMap
+}
+
+// When ingesting we keep a cache of the OrderedParamSets we have seen per-tile.
+type OpsCacheEntry struct {
+	ops  *paramtools.OrderedParamSet
+	hash string // md5 has of the serialized ops - used for deterministic querying.
+}
+
+// opsCacheEntryFromOPS creates and fills in an OpsCacheEntry from the given
+// OrderedParamSet and sets the hash appropriately.
+func opsCacheEntryFromOPS(ops *paramtools.OrderedParamSet) (*OpsCacheEntry, error) {
+	buf, err := ops.Encode()
+	if err != nil {
+		return nil, skerr.Fmt("could not encode the given ops to bytes: %s", err)
+	}
+	hash := fmt.Sprintf("%x", md5.Sum(buf))
+	return &OpsCacheEntry{
+		ops:  ops,
+		hash: hash,
+	}, nil
+}
+
+// NewOpsCacheEntry returns an empty OpsCacheEntry.
+func NewOpsCacheEntry() (*OpsCacheEntry, error) {
+	return opsCacheEntryFromOPS(paramtools.NewOrderedParamSet())
+}
+
+// NewOpsCacheEntryFromRow loads the appropriate data from the given BT row
+// and returns a OpsCacheEntry with that data.
+func NewOpsCacheEntryFromRow(row bigtable.Row) (*OpsCacheEntry, error) {
+	family := row[opsFamily]
+	if len(family) != 2 {
+		// This should never happen
+		return nil, skerr.Fmt("incorrect number of of OPS columns in BT, %d != 2", len(family))
+	}
+	ops := &paramtools.OrderedParamSet{}
+	hash := ""
+	for _, col := range family {
+		if col.Column == opsFullColName {
+			var err error
+			ops, err = paramtools.NewOrderedParamSetFromBytes(col.Value)
+			if err != nil {
+				// should never happen
+				return nil, skerr.Fmt("corrupted paramset in BT: %s", err)
+			}
+		} else if col.Column == hashFullColName {
+			hash = string(col.Value)
+		}
+	}
+	if hash == "" {
+		return nil, skerr.Fmt("missing hash for OPS %#v", ops)
+	}
+	// You might be tempted to use opsCacheEntryFromOps and
+	// check that entry.hash == hash here, but that will fail
+	// because GoB encoding of maps is not deterministic.
+	entry := OpsCacheEntry{
+		ops:  ops,
+		hash: hash,
+	}
+	return &entry, nil
+}
+
+// A DigestMap keeps track of the mapping between encoded DigestID and their corresponding
+// types.Digest.
+type DigestMap struct {
+	intMap map[DigestID]types.Digest
+	strMap map[types.Digest]DigestID
+}
+
+// NewDigestMap creates and empty DigestMap with the given capacity.
+func NewDigestMap(cap int) *DigestMap {
+	ret := &DigestMap{
+		intMap: make(map[DigestID]types.Digest, cap),
+		strMap: make(map[types.Digest]DigestID, cap),
+	}
+	ret.intMap[MissingDigestId] = types.MISSING_DIGEST
+	ret.strMap[types.MISSING_DIGEST] = MissingDigestId
+	return ret
+}
+
+// Delta returns a []types.Digest of those passed in digests that are
+// not in this mapping currently.
+func (d *DigestMap) Delta(digests map[types.Digest]bool) []types.Digest {
+	ret := make([]types.Digest, 0, len(digests))
+	for digest := range digests {
+		if _, ok := d.strMap[digest]; !ok {
+			ret = append(ret, digest)
+		}
+	}
+	return ret
+}
+
+// Add expands the map with the given entries. It fails if any key or any value
+// is already in the mapping.
+func (d *DigestMap) Add(newEntries map[types.Digest]DigestID) error {
+	for digest, id := range newEntries {
+		if digest == types.MISSING_DIGEST || id <= 0 {
+			return skerr.Fmt("invalid input id or digest: (%q -> %d)", digest, id)
+		}
+
+		foundID, strExists := d.strMap[digest]
+		foundDigest, intExists := d.intMap[id]
+		if strExists && intExists {
+			if (foundID != id) || (foundDigest != digest) {
+				return skerr.Fmt("inconsistent data - got (%q -> %d) when (%q -> %d) was expected", digest, id, foundDigest, foundID)
+			}
+			// Already contained so this is a no-op.
+			return nil
+		}
+
+		if strExists || intExists {
+			return skerr.Fmt("internal inconsistency - input pair (%q -> %d) resolves to (%d -> %q)", digest, foundID, id, foundDigest)
+		}
+
+		// New mapping. Add it.
+		d.intMap[id] = digest
+		d.strMap[digest] = id
+	}
+	return nil
+}
+
+// ID returns the DigestID for a given types.Digest.
+func (d *DigestMap) ID(digest types.Digest) (DigestID, error) {
+	ret, ok := d.strMap[digest]
+	if !ok {
+		return 0, skerr.Fmt("unable to find id for %q", digest)
+	}
+	return ret, nil
+}
+
+// DecodeIDs is like Digest but in bulk.
+func (d *DigestMap) DecodeIDs(ids []DigestID) ([]types.Digest, error) {
+	ret := make([]types.Digest, len(ids))
+	var ok bool
+	for idx, id := range ids {
+		ret[idx], ok = d.intMap[id]
+		if !ok {
+			return nil, skerr.Fmt("unable to find id %d in intMap", id)
+		}
+	}
+	return ret, nil
+}
+
+// Digest returns the types.Digest for a given DigestID.
+func (d *DigestMap) Digest(id DigestID) (types.Digest, error) {
+	ret, ok := d.intMap[id]
+	if !ok {
+		return "", skerr.Fmt("unable to find digest for %d", id)
+	}
+	return ret, nil
+}
+
+// Len returns how many map entries are in this map.
+func (d *DigestMap) Len() int {
+	return len(d.strMap)
+}
