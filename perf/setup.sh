8b3cdb372aa5d0478655517a4cb8a2dab114fa6b
Joe Gregorio <jcgregorio@google.com>
2019-05-23 09:22:07 -0400

    [perf] Write indices for traces.
    Bug: skia:
    Change-Id: I5a978de5f0cbdf77aa53581e92a1d71d9c906cdc

--- perf/setup.sh
+++ perf/setup.sh
@@ -3,6 +3,6 @@
 # Creates the tables for Perf in BigTable.
 
 # Skia instance.
-cbt --instance production createtable perf-skia families=V:maxversions=1,S:maxversions=1,D:maxversions=1,H:maxversions=1
-cbt --instance production createtable perf-android families=V:maxversions=1,S:maxversions=1,D:maxversions=1,H:maxversions=1
-cbt --instance production createtable perf-ct families=V:maxversions=1,S:maxversions=1,D:maxversions=1,H:maxversions=1
+cbt --instance production createtable perf-skia families=V:maxversions=1,S:maxversions=1,D:maxversions=1,H:maxversions=1,I:maxversions=1
+cbt --instance production createtable perf-android families=V:maxversions=1,S:maxversions=1,D:maxversions=1,H:maxversions=1,I:maxversions=1
+cbt --instance production createtable perf-ct families=V:maxversions=1,S:maxversions=1,D:maxversions=1,H:maxversions=1,I:maxversions=1
