155879b5f10bcfae6a43a5085865d56853ab4f6a
Kevin Lubick <kjlubick@google.com>
2019-05-30 15:17:44 -0400

    [gold] Plan out revamped firestore expectationstore.
    More cleanup of datastore interface.
    
    Change-Id: Ib71af6f8ebacf900f904f53ebb6ad1cbc7cab9c8

--- cmd/dstool/main.go
+++ cmd/dstool/main.go
@@ -19,7 +19,7 @@ import (
 	"go.skia.org/infra/go/ds"
 	"go.skia.org/infra/go/sklog"
 	"go.skia.org/infra/go/util"
-	"go.skia.org/infra/golden/go/expstorage"
+	"go.skia.org/infra/golden/go/expstorage/ds_expstore"
 	"go.skia.org/infra/golden/go/tryjobstore"
 )
 
@@ -54,8 +54,8 @@ var (
 	// touchRegisteredEntities maps the entities to the types that should be
 	// used to load and save the entities for the touch operation.
 	touchRegisteredEntities = map[ds.Kind]interface{}{
-		ds.MASTER_EXP_CHANGE: &expstorage.ExpChange{},
-		ds.TRYJOB_EXP_CHANGE: &expstorage.ExpChange{},
+		ds.MASTER_EXP_CHANGE: &ds_expstore.ExpChange{},
+		ds.TRYJOB_EXP_CHANGE: &ds_expstore.ExpChange{},
 		ds.TRYJOB_RESULT:     &tryjobstore.TryjobResult{},
 	}
 )
