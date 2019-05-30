11d8fac5d1e1201ecb651ea3cd92305eeac41ca4
Kevin Lubick <kjlubick@google.com>
2019-05-30 09:41:00 -0400

    [gitsync] Make more robust to history changing
    Change-Id: I452edb9b4ca65f9d564b540451910221300a1055
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/216868
    Reviewed-by: Eric Boren <borenet@google.com>
    Commit-Queue: Kevin Lubick <kjlubick@google.com>

--- go/git/gitdir.go
+++ go/git/gitdir.go
@@ -188,11 +188,17 @@ func (g GitDir) IsAncestor(ctx context.Context, a, b string) (bool, error) {
 	out, err := g.Git(ctx, "merge-base", "--is-ancestor", a, b)
 	if err != nil {
 		// Either a is not an ancestor of b, or we got a real error. If
-		// the output is empty, assume it's the former case. Otherwise,
-		// return an error.
+		// the output is empty, assume it's the former case.
 		if out == "" {
 			return false, nil
 		}
+		// "Not a valid commit name" indicates that the given commit does not
+		//  exist and thus history was probably changed upstream.
+		// A non-existent commit cannot be an ancestor of one which does exist.
+		if strings.Contains(out, fmt.Sprintf("Not a valid commit name %s", a)) {
+			return false, nil
+		}
+		// Otherwise, return the presumably real error.
 		return false, fmt.Errorf("%s: %s", err, out)
 	}
 	return true, nil
