9e4dc15f0e004fda273826690d5dbbd8c8bca2a6
Kevin Lubick <kjlubick@google.com>
2019-06-12 13:20:03 -0400

    [gold] Move ingestion stuff to gold proper
    Change-Id: I9556291b6b810ef1fe0c235777a07b6079884d8a

---
+++ golden/cmd/bt_reingester/main.go
@@ -0,0 +1,27 @@
+package main
+
+// bt_reingester will scan through all the files in a GCS bucket and ingest
+// them into the bt_tracestore.
+import (
+	"flag"
+
+	"go.skia.org/infra/golden/go/tracestore/bt_tracestore"
+)
+
+func main() {
+	var (
+		btInstance = flag.String("bt_instance", "", "Bigtable instance to use in the project identified by 'project_id'")
+		projectID  = flag.String("project_id", "skia-public", "GCP project ID.")
+		btTableID  = flag.String("bt_table_id", "production", "BigTable table ID.")
+	)
+	flag.Parse()
+
+	btc := bt_tracestore.BTConfig{
+		ProjectID:  *projectID,
+		InstanceID: *btInstance,
+		TableID:    *btTableID,
+		// TODO(kjlubick): VCS
+	}
+
+	_ = bt_tracestore.InitBT(btc)
+}
