b5de346de3217aca52029766785311d2e3dce344
Kevin Lubick <kjlubick@google.com>
2019-06-13 14:25:33 -0400

    [gold] Add logic to ingest data into tracestore.
    Bug: skia:9096
    Change-Id: I09f43a7f10d2750b12c3de7a5941d3710c6c495c

--- golden/cmd/bt_reingester/main.go
+++ golden/cmd/bt_reingester/main.go
@@ -3,25 +3,109 @@ package main
 // bt_reingester will scan through all the files in a GCS bucket and ingest
 // them into the bt_tracestore.
 import (
+	"context"
 	"flag"
+	"sync"
+	"time"
 
+	"cloud.google.com/go/storage"
+	"go.skia.org/infra/go/auth"
+	"go.skia.org/infra/go/bt"
+	"go.skia.org/infra/go/gitstore/bt_gitstore"
+	"go.skia.org/infra/go/httputils"
+	"go.skia.org/infra/go/ingestion"
+	"go.skia.org/infra/go/sklog"
+	"go.skia.org/infra/go/vcsinfo/bt_vcs"
+	"go.skia.org/infra/golden/go/goldingestion"
 	"go.skia.org/infra/golden/go/tracestore/bt_tracestore"
 )
 
 func main() {
 	var (
-		btInstance = flag.String("bt_instance", "", "Bigtable instance to use in the project identified by 'project_id'")
+		btInstance = flag.String("bt_instance", "production", "BigTable instance to use in the project identified by 'project_id'")
 		projectID  = flag.String("project_id", "skia-public", "GCP project ID.")
-		btTableID  = flag.String("bt_table_id", "production", "BigTable table ID.")
+		btTableID  = flag.String("bt_table_id", "", "BigTable table ID for the traces.")
+
+		gitBTTableID = flag.String("git_table_id", "git-repos", "BigTable table ID that has the git data.")
+		gitRepoURL   = flag.String("git_repo_url", "", "The URL of the git repo to look up in BigTable.")
+
+		srcBucket  = flag.String("src_bucket", "", "Source bucket to ingest files from.")
+		srcRootDir = flag.String("src_root_dir", "", "Source root directory to ingest files in.")
 	)
 	flag.Parse()
+
+	bt.EnsureNotEmulator()
+
+	tokenSrc, err := auth.NewDefaultTokenSource(true, storage.ScopeReadOnly)
+	if err != nil {
+		sklog.Fatalf("Failed to auth: %s", err)
+	}
+	client := httputils.DefaultClientConfig().WithTokenSource(tokenSrc).With2xxOnly().WithDialTimeout(time.Second * 10).Client()
+
+	if *srcBucket == "" {
+		sklog.Fatalf("You must supply --src_bucket")
+	}
+
+	gss, err := ingestion.NewGoogleStorageSource("", *srcBucket, *srcRootDir, client, nil)
+	if err != nil {
+		sklog.Fatalf("Could not set up GCS ingester for gs://%s/%s: %s", *srcBucket, *srcRootDir, err)
+	}
+
+	gtc := &bt_gitstore.BTConfig{
+		ProjectID:  *projectID,
+		InstanceID: *btInstance,
+		TableID:    *gitBTTableID,
+	}
+
+	gitStore, err := bt_gitstore.New(context.Background(), gtc, *gitRepoURL)
+	if err != nil {
+		sklog.Fatalf("Error instantiating gitstore: %s", err)
+	}
+
+	// gittiles is nil because we don't need to fetch files
+	// eventbus is nil because we don't want it to send events when new commits come in.
+	vcs, err := bt_vcs.New(gitStore, "master", nil, nil, 0)
+	if err != nil {
+		sklog.Fatalf("Could not create VCS: %s", err)
+	}
 
 	btc := bt_tracestore.BTConfig{
 		ProjectID:  *projectID,
 		InstanceID: *btInstance,
 		TableID:    *btTableID,
-		// TODO(kjlubick): VCS
+		VCS:        vcs,
 	}
 
-	_ = bt_tracestore.InitBT(btc)
+	processor, err := goldingestion.NewTraceStoreProcessor(btc)
+	if err != nil {
+		sklog.Fatalf("Cannot create bt trace store: %s", err)
+	}
+
+	concurrentIngesters := make(chan bool, 250)
+	var wg sync.WaitGroup
+
+	sklog.Infof("Beginning ingestion at %s", beginning)
+	results := gss.Poll(beginning.Unix(), time.Now().Unix())
+	for r := range results {
+		concurrentIngesters <- true
+		wg.Add(1)
+		go func() {
+			defer func() {
+				<-concurrentIngesters
+				wg.Done()
+			}()
+			err := processor.Process(context.Background(), r)
+			if err != nil && err != ingestion.IgnoreResultsFileErr {
+				sklog.Errorf("could not process %s: %s", r.Name(), err)
+			}
+		}()
+	}
+
+	wg.Wait()
+	sklog.Infof("done")
 }
+
+// In the early days, there was several invalid entries, because they did not specify
+// gitHash. Starting re-ingesting Skia on October 1, 2014 seems to be around when
+// the data is correct.
+var beginning = time.Date(2014, time.October, 1, 0, 0, 0, 0, time.UTC)
