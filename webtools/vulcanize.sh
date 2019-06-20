bb0f7ee364cec8314d6b555a276aa94bfdf5d2d5
Joe Gregorio <jcgregorio@google.com>
2019-06-20 15:03:34 -0400

    perf - Switch to npm from bower for web assets.
    Change-Id: I983b6fab6fcbbf583a8faa6331aa3588f4917112

--- webtools/vulcanize.sh
+++ webtools/vulcanize.sh
@@ -8,6 +8,7 @@ set -e
 
 tmpfile=res/vul/elements.html.plzminify
 mkdir -p res/vul
-./node_modules/.bin/vulcanize --inline-css --inline-scripts --strip-comments --abspath=./ elements.html > $tmpfile
-./node_modules/.bin/html-minifier -o res/vul/elements.html --minify-js --remove-comments --collapse-whitespace --conservative-collapse $tmpfile
-rm $tmpfile
+./node_modules/.bin/vulcanize --inline-css --inline-scripts --strip-comments --abspath=./ elements.html > res/vul/elements.html
+#./node_modules/.bin/vulcanize --inline-css --inline-scripts --strip-comments --abspath=./ elements.html > $tmpfile
+#./node_modules/.bin/html-minifier -o res/vul/elements.html --minify-js --remove-comments --collapse-whitespace --conservative-collapse $tmpfile
+#rm $tmpfile
