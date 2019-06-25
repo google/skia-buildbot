e74275b3ddd931eaf834943ca8a298081cbfe58a
Ravi Mistry <rmistry@google.com>
2019-06-25 14:09:11 -0400

    [Autoroll] Check to see if we are already at CIPD head hash
    Bug: skia:9078
    Change-Id: I9a5c3edcf70a797e51bc7bcbf2945f96380257b1
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/223706
    Commit-Queue: Ravi Mistry <rmistry@google.com>
    Reviewed-by: Eric Boren <borenet@google.com>

--- autoroll/go/repo_manager/github_cipd_deps_repo_manager.go
+++ autoroll/go/repo_manager/github_cipd_deps_repo_manager.go
@@ -209,6 +209,9 @@ func getNotRolledRevs(ctx context.Context, cipdClient cipd.CIPDClient, lastRollR
 	if err != nil {
 		return nil, err
 	}
+	if lastRollRev == head.InstanceID {
+		return []*revision.Revision{}, nil
+	}
 	iter, err := cipdClient.ListInstances(ctx, cipdAssetName)
 	if err != nil {
 		return nil, err
@@ -229,7 +232,7 @@ func getNotRolledRevs(ctx context.Context, cipdClient cipd.CIPDClient, lastRollR
 				foundHead = true
 			}
 			if id == lastRollRev {
-				break
+				return notRolledRevs, nil
 			}
 			if foundHead {
 				notRolledRevs = append(notRolledRevs, &revision.Revision{
