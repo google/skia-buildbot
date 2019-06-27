4b02d5da3ed430ed4eb18fd53cb8ac9631fce8cc
Kevin Lubick <kjlubick@google.com>
2019-06-27 14:37:36 -0400

    [gold] Add logic to ingest data into tracestore.
    Bug: skia:9096
    Change-Id: I09f43a7f10d2750b12c3de7a5941d3710c6c495c

--- golden/go/goldingestion/common.go
+++ golden/go/goldingestion/common.go
@@ -1,17 +1,20 @@
 package goldingestion
 
 import (
+	"context"
 	"fmt"
 	"io"
 	"sort"
 	"strings"
 
 	"go.skia.org/infra/go/ingestion"
+	"go.skia.org/infra/go/skerr"
 	"go.skia.org/infra/go/sklog"
 	"go.skia.org/infra/go/tiling"
-	tracedb "go.skia.org/infra/go/trace/db"
 	"go.skia.org/infra/go/util"
+	"go.skia.org/infra/go/vcsinfo"
 	"go.skia.org/infra/golden/go/jsonio"
+	"go.skia.org/infra/golden/go/shared"
 	"go.skia.org/infra/golden/go/types"
 )
 
@@ -19,7 +22,7 @@ import (
 // It returns the id as a string of all the values, in the alphabetic order of the
 // keys, separated by a colon. The trace params returned are a single map of
 // key-> values. "Options" are omitted from the trace id, as per design.
-func idAndParams(dm *DMResults, r *jsonio.Result) (tiling.TraceId, map[string]string) {
+func idAndParams(dm *dmResults, r *jsonio.Result) (tiling.TraceId, map[string]string) {
 	combinedLen := len(dm.Key) + len(r.Key)
 	traceIdParts := make(map[string]string, combinedLen)
 	params := make(map[string]string, combinedLen+len(r.Options))
@@ -47,34 +50,13 @@ func idAndParams(dm *DMResults, r *jsonio.Result) (tiling.TraceId, map[string]st
 	return tiling.TraceId(strings.Join(values, ":")), params
 }
 
-// extractTraceDBEntries returns the traceDB entries to be inserted into the data store.
-func extractTraceDBEntries(dm *DMResults) (map[tiling.TraceId]*tracedb.Entry, error) {
-	ret := make(map[tiling.TraceId]*tracedb.Entry, len(dm.Results))
-	for _, result := range dm.Results {
-		traceId, params := idAndParams(dm, result)
-		if ignoreResult(dm, params) {
-			continue
-		}
-
-		ret[traceId] = &tracedb.Entry{
-			Params: params,
-			Value:  []byte(result.Digest),
-		}
-	}
-
-	// If all results were ignored then we return an error.
-	if len(ret) == 0 {
-		return nil, fmt.Errorf("No valid results in file %s.", dm.name)
-	}
-
-	return ret, nil
-}
-
 // ignoreResult returns true if the result with the given parameters should be
 // ignored.
-func ignoreResult(dm *DMResults, params map[string]string) bool {
-	// Ignore anything that is not a png.
-	if ext, ok := params["ext"]; !ok || (ext != "png") {
+func ignoreResult(dm *dmResults, params map[string]string) bool {
+	// Ignore anything that is not a png. In the early days (pre-2015), ext was omitted
+	// but implied to be "png". Thus if ext is not provided, it will be ingested.
+	// New entries (created by goldctl) will always have ext set.
+	if ext, ok := params["ext"]; ok && (ext != "png") {
 		return true
 	}
 
@@ -97,8 +79,8 @@ func ignoreResult(dm *DMResults, params map[string]string) bool {
 	return false
 }
 
-// DMResults enhances GoldResults with fields used for internal processing.
-type DMResults struct {
+// dmResults enhances GoldResults with fields used for internal processing.
+type dmResults struct {
 	*jsonio.GoldResults
 
 	// name is the name/path of the file where this came from.
@@ -106,32 +88,71 @@ type DMResults struct {
 }
 
 // Name returns the name/path from which these results were parsed.
-func (d *DMResults) Name() string {
+func (d *dmResults) Name() string {
 	return d.name
 }
 
-// ParseDMResultsFromReader parses the JSON stream out of the io.ReadCloser
+// parseDMResultsFromReader parses the JSON stream out of the io.ReadCloser
 // into a DMResults instance and closes the reader.
-func ParseDMResultsFromReader(r io.ReadCloser, name string) (*DMResults, error) {
+func parseDMResultsFromReader(r io.ReadCloser, name string) (*dmResults, error) {
 	defer util.Close(r)
 
 	goldResults, _, err := jsonio.ParseGoldResults(r)
 	if err != nil {
-		return nil, fmt.Errorf("Failed to decode JSON: %s", err)
+		return nil, skerr.Fmt("Failed to decode JSON: %s", err)
 	}
 
-	dmResults := &DMResults{GoldResults: goldResults}
+	dmResults := &dmResults{GoldResults: goldResults}
 	dmResults.name = name
 	return dmResults, nil
 }
 
 // processDMResults opens the given JSON input file and processes it, converting
-// it into a goldingestion.DMResults object and returning it.
-func processDMResults(resultsFile ingestion.ResultFileLocation) (*DMResults, error) {
-	r, err := resultsFile.Open()
+// it into a goldingestion.dmResults object and returning it.
+func processDMResults(rf ingestion.ResultFileLocation) (*dmResults, error) {
+	defer shared.NewMetricsTimer("read_dm_results").Stop()
+	r, err := rf.Open()
 	if err != nil {
-		return nil, err
+		return nil, skerr.Fmt("could not open file %s: %s", rf.Name(), err)
 	}
 
-	return ParseDMResultsFromReader(r, resultsFile.Name())
+	return parseDMResultsFromReader(r, rf.Name())
+}
+
+// getCanonicalCommitHash returns the commit hash in the primary repository. If the given
+// target hash is not in the primary repository it will try and find it in the secondary
+// repository which has the primary as a dependency.
+func getCanonicalCommitHash(ctx context.Context, vcs vcsinfo.VCS, targetHash string) (string, error) {
+	// If it is not in the primary repo.
+	if !isCommit(ctx, vcs, targetHash) {
+		// Extract the commit.
+		foundCommit, err := vcs.ResolveCommit(ctx, targetHash)
+		if err != nil && err != vcsinfo.NoSecondaryRepo {
+			return "", fmt.Errorf("Unable to resolve commit %s in primary or secondary repo. Got err: %s", targetHash, err)
+		}
+
+		if foundCommit == "" {
+			if err == vcsinfo.NoSecondaryRepo {
+				sklog.Warningf("Unable to find commit %s in primary or secondary repo.", targetHash)
+			} else {
+				sklog.Warningf("Unable to find commit %s in primary repo and no secondary configured.", targetHash)
+			}
+			return "", ingestion.IgnoreResultsFileErr
+		}
+
+		// Check if the found commit is actually in the primary repository. This could indicate misconfiguration
+		// of the secondary repo.
+		if !isCommit(ctx, vcs, foundCommit) {
+			return "", fmt.Errorf("Found invalid commit %s in secondary repo at commit %s. Not contained in primary repo.", foundCommit, targetHash)
+		}
+		sklog.Infof("Commit translation: %s -> %s", targetHash, foundCommit)
+		targetHash = foundCommit
+	}
+	return targetHash, nil
+}
+
+// isCommit returns true if the given commit is in vcs.
+func isCommit(ctx context.Context, vcs vcsinfo.VCS, commitHash string) bool {
+	ret, err := vcs.Details(ctx, commitHash, false)
+	return (err == nil) && (ret != nil)
 }
