9c7db5697116423d3c92def38592f2e58734e45c
Kevin Lubick <kjlubick@google.com>
2019-06-27 13:58:28 -0400

    [gold] Remove digestmap from tracestore
    This adds some unneeded complexity and caused an issue while trying
    to ingest at scale (the digest map got out of sync (?) and then everything
    completely froze up). I don't think we are so tight on space we have to
    resort to this.
    
    Bug: skia:9096
    Change-Id: Ife7371fda4b6c7de8dbb68a2544f19f5a4715cbf

--- golden/go/tracestore/bt_tracestore/util_test.go
+++ golden/go/tracestore/bt_tracestore/util_test.go
@@ -6,6 +6,7 @@ import (
 
 	assert "github.com/stretchr/testify/require"
 	"go.skia.org/infra/go/testutils/unittest"
+	"go.skia.org/infra/golden/go/types"
 )
 
 // The tests in this package are mainly to make sure changes that
@@ -40,18 +41,47 @@ func TestShardedRowName(t *testing.T) {
 	encodedTrace := ",0=1,1=3,3=0,"
 	assert.Equal(t, "03:ts:t:2147483646:,0=1,1=3,3=0,", shardedRowName(shard, typeTrace, tileZeroKey, encodedTrace))
 	assert.Equal(t, "03:ts:t:0000000057:,0=1,1=3,3=0,", shardedRowName(shard, typeTrace, veryNewTileKey, encodedTrace))
-
-	// Example RowName for a digest
-	// digests are stored in a row based on the first three characters and a
-	// column with the remaining characters.
-	digestPrefix := string(AlphaDigest[:3])
-	assert.Equal(t, "03:ts:d:2147483646:aaa", shardedRowName(shard, typeDigestMap, tileZeroKey, digestPrefix))
 }
 
 func TestExtractKeyFromRowName(t *testing.T) {
 	unittest.SmallTest(t)
 
-	assert.Equal(t, "ae3", extractSubkey("07:ts:d:2147483646:ae3"))
 	assert.Equal(t, "", extractSubkey(":ts:o:2147483646:"))
 	assert.Equal(t, ",0=1,1=3,3=0,", extractSubkey("03:ts:t:2147483646:,0=1,1=3,3=0,"))
 }
+
+func TestDigestBytesSunnyDay(t *testing.T) {
+	unittest.SmallTest(t)
+
+	assert.Equal(t, missingDigestBytes, toBytes(types.MISSING_DIGEST))
+	assert.Equal(t, types.MISSING_DIGEST, fromBytes(missingDigestBytes))
+	assert.Equal(t, types.MISSING_DIGEST, fromBytes(nil))
+	assert.Equal(t, types.MISSING_DIGEST, fromBytes([]byte{}))
+
+	assert.Equal(t, arbitraryDigestBytes, toBytes(arbitraryDigest))
+	assert.Equal(t, arbitraryDigest, fromBytes(arbitraryDigestBytes))
+}
+
+func TestDigestBytesBadData(t *testing.T) {
+	unittest.SmallTest(t)
+	assert.Equal(t, arbitraryDigestBytes, toBytes(arbitraryDigestCap))
+
+	assert.Equal(t, missingDigestBytes, toBytes(corruptDigest))
+
+	assert.Equal(t, missingDigestBytes, toBytes(truncatedDigest))
+	assert.Equal(t, types.MISSING_DIGEST, fromBytes(truncatedDigestBytes))
+}
+
+const (
+	arbitraryDigest    = types.Digest("8db9913366b07bd14df9a68459e2958b")
+	arbitraryDigestCap = types.Digest("8DB9913366B07BD14DF9A68459E2958B")
+	corruptDigest      = types.Digest("NOPE")
+	truncatedDigest    = types.Digest("8db9913366b07b")
+)
+
+var (
+	arbitraryDigestBytes = []byte{0x8d, 0xb9, 0x91, 0x33, 0x66, 0xb0, 0x7b, 0xd1,
+		0x4d, 0xf9, 0xa6, 0x84, 0x59, 0xe2, 0x95, 0x8b}
+
+	truncatedDigestBytes = []byte{0x8d, 0xb9, 0x91, 0x33, 0x66, 0xb0, 0x7b}
+)
