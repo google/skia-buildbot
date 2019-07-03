e8ae6a5f03612e55c5deffab6e261e584e69547d
Kevin Lubick <kjlubick@google.com>
2019-07-03 10:01:10 -0400

    [gold] Add logic to ingest data into tracestore.
    Bug: skia:9096
    Change-Id: I09f43a7f10d2750b12c3de7a5941d3710c6c495c

--- golden/go/tracestore/bt_tracestore/bt_tracestore.go
+++ golden/go/tracestore/bt_tracestore/bt_tracestore.go
@@ -4,6 +4,7 @@ package bt_tracestore
 
 import (
 	"context"
+	"errors"
 	"hash/crc32"
 	"sort"
 	"strconv"
@@ -30,6 +31,9 @@ import (
 // to get auth information from the environment and must be called with an account that has
 // admin rights.
 func InitBT(conf BTConfig) error {
+	if conf.ProjectID == "" || conf.InstanceID == "" || conf.TableID == "" {
+		return errors.New("invalid config: must specify all parts of BTConfig")
+	}
 	return bt.InitBigtable(conf.ProjectID, conf.InstanceID, conf.TableID, btColumnFamilies)
 }
 
