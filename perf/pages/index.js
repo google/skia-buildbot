b89d98b67475a1856b866ef46c9d83b0865053de
Joe Gregorio <jcgregorio@google.com>
2019-07-03 14:40:19 -0400

    Perf - Migrated commit-detail-panel-sk to lit-html.
    Also clean up the behavior a bit.
    
    Bug: skia:9219
    Change-Id: I3917a4006cad88ce77eb1830971d2f0236a61e52

--- perf/pages/index.js
+++ perf/pages/index.js
@@ -1,7 +1,10 @@
 import '../node_modules/@webcomponents/custom-elements/custom-elements.min.js'
-import 'infra-sk/modules/login-sk'
+
 import 'elements-sk/collapse-sk'
-import '../modules/triage2-sk'
-import '../modules/word-cloud-sk'
+import 'infra-sk/modules/login-sk'
+
+import '../modules/commit-detail-panel-sk'
 import '../modules/commit-detail-sk'
 import '../modules/plot-simple-sk'
+import '../modules/triage2-sk'
+import '../modules/word-cloud-sk'
