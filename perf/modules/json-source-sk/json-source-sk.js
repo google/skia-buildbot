ecbaeb13ed129bf0e3ab5cb06ba2a912e1a26e8f
Joe Gregorio <jcgregorio@google.com>
2019-07-08 07:23:53 -0400

    Perf - Port json-source away from Polymer.
    Bug: skia:9219
    Change-Id: I263d8df6b32ffcdb683b907b4e75653c5c8fa7bd
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/225700
    Reviewed-by: Joe Gregorio <jcgregorio@google.com>

---
+++ perf/modules/json-source-sk/json-source-sk.js
@@ -0,0 +1,94 @@
+/**
+ * @module modules/json-source-sk
+ * @description <h2><code>json-source-sk</code></h2>
+ *
+ * Displays buttons that, when pressed, retrieve and show the JSON file that
+ * was ingested for the point in the trace identified by commit id and trace
+ * id.
+ *
+ */
+import { html, render } from 'lit-html'
+import { $$ } from 'common-sk/modules/dom'
+import { errorMessage } from 'elements-sk/errorMessage'
+import { jsonOrThrow } from 'common-sk/modules/jsonOrThrow'
+import 'elements-sk/spinner-sk'
+
+const template = (ele) => html`
+  <button @click=${ele._loadSource}>View Source File</button>
+  <button @click=${ele._loadSourceSmall}>View Source File Without Results</button>
+  <spinner-sk id=spinner></spinner-sk>
+  <pre id=display>${ele._json}</pre>
+`;
+
+window.customElements.define('json-source-sk', class extends HTMLElement {
+  constructor() {
+    super();
+    this._json = '';
+  }
+
+  connectedCallback() {
+    this._render();
+    this._spinner = $$('#spinner', this);
+  }
+
+  /** @prop cid {string} A serialized cid.CommitID. */
+  get cid() { return this._cid }
+  set cid(val) {
+    this._cid = val;
+    this._json = '';
+    this._render();
+  }
+
+  /** @prop traceid {string} The ID of the trace. */
+  get traceid() { return this._traceid }
+  set traceid(val) {
+    this._traceid = val;
+    this._json = '';
+    this._render();
+  }
+
+  _loadSource() {
+    this._loadSourceImpl(false);
+  }
+
+  _loadSourceSmall() {
+    this._loadSourceImpl(true);
+  }
+
+  _loadSourceImpl(isSmall) {
+    if (!this.traceid) {
+      return
+    }
+    if (this.cid === undefined || this.cid.offset === undefined) {
+      return
+    }
+    const body = {
+      cid: this.cid,
+      traceid: this.traceid,
+    };
+    this._spinner.active = true;
+    let url = "/_/details/";
+    if (isSmall) {
+      url += "?results=false";
+    }
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    }).then(jsonOrThrow).then((json) => {
+      this._spinner.active = false;
+      this._json = JSON.stringify(json, null, '  ');
+      this._render();
+    }).catch((e) => {
+      this._spinner.active = false;
+      errorMessage(e);
+    });
+  }
+
+  _render() {
+    render(template(this), this, {eventContext: this});
+  }
+
+});
