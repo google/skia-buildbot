757acd705f2b917213f6ccbb90568bf84827cb37
Joe Gregorio <jcgregorio@google.com>
2019-07-09 09:22:12 -0400

    Perf - Migrate day-range-sk away from Polymer.
    Change-Id: Id5af6e088022d2173495c47d6bdf051eaaa6941d

---
+++ perf/modules/day-range-sk/day-range-sk.js
@@ -0,0 +1,83 @@
+/**
+ * @module modules/day-range-sk
+ * @description <h2><code>day-range-sk</code></h2>
+ *
+ * @evt day-range-change - Fired when the selection has stopped changing. Contains
+ *      the begin and end timestamps in the details:
+ *
+ *      {
+ *        begin: 1470084997,
+ *        end: 1474184677
+ *      }
+ *
+ * @attr {Number} begin - The beginning of the time range in seconds since the epoch.
+ * @attr {Number} end - The end of the time range in seconds since the epoch.
+ *
+ * @example
+ */
+import { html, render } from 'lit-html'
+import 'elix/src/DateComboBox.js'
+import { upgradeProperty } from 'elements-sk/upgradeProperty';
+
+const template = (ele) => html`
+  <label>Begin <elix-date-combo-box @date-changed=${ele._beginChanged} .date=${new Date(ele.begin * 1000)}></elix-date-combo-box></label>
+  <label>End <elix-date-combo-box @date-changed=${ele._endChanged} .date=${new Date(ele.end * 1000)}></elix-date-combo-box></label>
+`;
+
+window.customElements.define('day-range-sk', class extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    upgradeProperty(this, 'begin');
+    upgradeProperty(this, 'end');
+    const now = Date.now();
+    if (!this.begin) {
+      this.begin = now/1000 - 24*60*60;
+    }
+    if (!this.end) {
+      this.end = now/1000;
+    }
+    this._render();
+  }
+
+  _sendEvent() {
+    const detail = {
+      begin: this.begin,
+      end: this.end,
+    };
+    this.dispatchEvent(new CustomEvent('day-range-change', {detail: detail, bubbles: true}));
+  }
+
+  _beginChanged(e) {
+    this.begin = e.detail.date / 1000;
+    this._sendEvent();
+  }
+
+  _endChanged(e) {
+    this.end = e.detail.date / 1000;
+    this._sendEvent();
+  }
+
+  static get observedAttributes() {
+    return ['begin', 'end'];
+  }
+
+  /** @prop begin {string} Mirrors the 'begin' attribute. */
+  get begin() { return +this.getAttribute('begin'); }
+  set begin(val) { this.setAttribute('begin', ''+val); }
+
+  /** @prop end {string} Mirros the 'end' attribute. */
+  get end() { return +this.getAttribute('end'); }
+  set end(val) { this.setAttribute('end', val); }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    this._render();
+  }
+
+  _render() {
+    render(template(this), this, {eventContext: this});
+  }
+
+});
