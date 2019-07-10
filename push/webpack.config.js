e1cf1f6d30ec3dab344994c9073a4d39a02a4d44
Joe Gregorio <jcgregorio@google.com>
2019-07-10 12:31:15 -0400

    Fix for infra-sk location.
    It turns out that the default location to resolve modules is "node_modules",
    i.e.  a relative directory, so we have to over-ride that with an absolute
    path.
    
    Also, each project that uses infra-sk must add in the deps that
    infra-sk needs, i.e. common-sk, elements-sk, and lit-html.
    
    Change-Id: I5154b86ca8efe22759c5f607e3e90c7ca8f42750

--- push/webpack.config.js
+++ push/webpack.config.js
@@ -3,5 +3,7 @@ const { resolve } = require('path')
 
 module.exports = (env, argv) => {
   let config = commonBuilder(env, argv, __dirname);
+  config.resolve = config.resolve || {};
+  config.resolve.modules = [resolve(__dirname, 'node_modules')];
   return config
 }
--- task_driver/webpack.config.js
+++ task_driver/webpack.config.js
@@ -4,5 +4,7 @@ const { resolve } = require('path')
 module.exports = (env, argv) => {
   let config = commonBuilder(env, argv, __dirname);
   config.output.publicPath='/dist/';
+  config.resolve = config.resolve || {};
+  config.resolve.modules = [resolve(__dirname, 'node_modules')];
   return config
 }
