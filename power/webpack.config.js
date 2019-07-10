8a0f80d2da7f957239d1bf7a05bc3ef070d933c7
Joe Gregorio <jcgregorio@google.com>
2019-07-10 11:45:43 -0400

    Fix for infra-sk location.
    It turns out that the default location to resolve modules in "node_modules",
    i.e.  a relative directory, so we have to over-ride that with an absolute
    path.
    
    Change-Id: I5154b86ca8efe22759c5f607e3e90c7ca8f42750

--- power/webpack.config.js
+++ power/webpack.config.js
@@ -1,5 +1,9 @@
 const commonBuilder = require('pulito');
+const { resolve } = require('path')
 
 module.exports = (env, argv) => {
-  return commonBuilder(env, argv, __dirname);
+  let config = commonBuilder(env, argv, __dirname);
+  config.resolve = config.resolve || {};
+  config.resolve.modules = [resolve(__dirname, 'node_modules')];
+  return config;
 }
