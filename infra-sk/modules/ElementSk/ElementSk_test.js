477233c49a10da8ceb9b83189c2a3fe163678d7e
Joe Gregorio <jcgregorio@google.com>
2019-07-12 12:49:07 -0400

    Add ElementSk to infra-sk.
    A base element to work around some pathologies in custom elements.
    
    Change-Id: I6bc13ae0725fbdfff5f0a1ce1b0c6021d2516c71

---
+++ infra-sk/modules/ElementSk/ElementSk_test.js
@@ -0,0 +1,45 @@
+import { ElementSk } from './ElementSk.js'
+import { html } from 'lit-html'
+
+let container = document.createElement('div');
+document.body.appendChild(container);
+
+afterEach(function() {
+  container.innerHTML = "";
+});
+
+window.customElements.define('my-test-element-sk', class extends ElementSk {
+  constructor() {
+    super((ele) => html`<p>Hello World!</p>`);
+    assert.isNull(this.querySelector('p'));
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    this._render();
+  }
+
+  static get observedAttributes() {
+    return ['some-attribute'];
+  }
+
+  attributeChangedCallback() {
+    super._render();
+    assert.isFalse(this._connected);
+    assert.isNull(this.querySelector('p'));
+    this._attributeCalled = true;
+  }
+});
+
+describe('ElementSk', function() {
+  describe('render', function() {
+    it('only renders if connected', function() {
+        container.innerHTML = `<my-test-element-sk some-attribute><my-test-element-sk>`;
+        let ele = container.firstElementChild;
+        assert.isNotNull(ele.querySelector('p'));
+        assert.isTrue(ele._attributeCalled);
+    });
+  });
+});
+
+
