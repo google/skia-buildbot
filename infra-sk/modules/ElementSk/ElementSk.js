f2d1e2658bef83653443ce1361b283da26b283b2
Joe Gregorio <jcgregorio@google.com>
2019-07-12 12:23:36 -0400

    Add ElementSk to infra-sk.
    A base element to work around some pathologies in custom elements.
    
    Change-Id: I6bc13ae0725fbdfff5f0a1ce1b0c6021d2516c71

---
+++ infra-sk/modules/ElementSk/ElementSk.js
@@ -0,0 +1,58 @@
+/**
+ * @module infra-sk/modules/ElementSk
+ * @description <h2><code>ElementSk</code></h2>
+ *
+ */
+import { html, render } from 'lit-html'
+
+/**
+ * A base class that records the connected status of the element in
+ * this._connected and provides a _render() function that doesn't do anything
+ * this this._connected is false.
+ *
+ * @property {Boolean} _connected - True if the connectedCallback has been
+ * called.
+ *
+ * @example
+ *
+ * class MyElement extends ElementSk {
+ *   constructor() {
+ *     super();
+ *     this._template = (ele) => html`<p>Hello World!</p>`;
+ *   }
+ *
+ *   connectedCallback() {
+ *     super.connectedCallback();
+ *     this._render();
+ *   }
+ * }
+ *
+ * @todo Do we also want to capture all the attributeChangedCallback's that
+ *   occur before connectedCallback is called and replay them once
+ *   connectedCallback finishes?
+ */
+export class ElementSk extends HTMLElement {
+  constructor() {
+    super();
+    this._template = null; // this._template = (ele) => html``;
+    this._connected = false;
+  }
+
+  connectedCallback() {
+    this._connected = true;
+  }
+
+  disconnectedCallback() {
+    this._connected = false;
+  }
+
+  /**
+   * Renders the lit-html template found at this._template if not-null, but
+   * only if connectedCallback has been called.
+   */
+  _render() {
+    if (this._connected && this._template) {
+      render(this._template(this), this, {eventContext: this});
+    }
+  }
+};
