eb9372c599e433046bb0ca703462e70472f0cc6e
Kevin Lubick <kjlubick@google.com>
2019-08-06 09:41:05 -0400

    [gold] Simplify baseliner
    Rather than writing baselines to GCS and having some "per commit baseline",
    we just have one baseline that is pulled from the expectations store
    (in FireStore) and served up.
    
    Since we are planning to change baselines to be images we have seen in last NN days
    combined with rules from last NNN days, writing these out per commit is expensive
    in terms of RAM and time.
    
    PS1 refactors a few interfaces and removes unneeded parts from Baseline.
    
    Change-Id: I185465638550b85bd8ca599399502b0bd1294d06

--- golden/go/types/expectations_test.go
+++ golden/go/types/expectations_test.go
@@ -33,3 +33,28 @@ beta:
 	hash3 : negative
 `, te.String())
 }
+
+func TestAsBaseline(t *testing.T) {
+	input := Expectations{
+		"beta": map[Digest]Label{
+			"hash1": POSITIVE,
+			"hash3": NEGATIVE,
+			"hash2": UNTRIAGED,
+			"hash4": POSITIVE,
+		},
+		"alpha": map[Digest]Label{
+			"hashB": UNTRIAGED,
+			"hashA": NEGATIVE,
+			"hashC": UNTRIAGED,
+		},
+	}
+
+	expectedOutput := Expectations{
+		"beta": map[Digest]Label{
+			"hash1": POSITIVE,
+			"hash4": POSITIVE,
+		},
+	}
+
+	assert.Equal(t, expectedOutput, input.AsBaseline())
+}
