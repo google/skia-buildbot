2ff97c603bf6ac2c7e7c8187b04e4d5ab462fc3d
Joe Gregorio <jcgregorio@google.com>
2019-08-07 15:22:41 -0400

    Perf - Create a new config for androidx.
    Bug: skia:9315
    Change-Id: I68a4fc3a45bfdfc2b62726b55f7956756dc93d18

---
+++ perf/create-ingestion-android-x-pubsub-topics.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+# Creates the PubSub topic for AndroidX Perf files and then ties it to GCS
+# notifications.
+
+set -e -x
+
+source ../kube/config.sh
+
+TOPIC=perf-ingestion-android-x-production
+
+gcloud pubsub topics create ${TOPIC} || true
+gsutil notification create -f json -e OBJECT_FINALIZE -t projects/${PROJECT_ID}/topics/${TOPIC} -p android-master-ingest gs://skia-perf
