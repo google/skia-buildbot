d1c7dac3c7fee91d0a69a1e133d437511f878768
Eric Boren <borenet@google.com>
2019-08-13 09:17:03 -0400

    [gitstore] Make BT GitStore use repograph.Graph
    Change-Id: Ibf17265bff10455582ed3616e4bd5228b4e2610f

--- go/git/repograph/repo_test.go
+++ go/git/repograph/repo_test.go
@@ -3,74 +3,44 @@ package repograph
 import (
 	"context"
 	"io/ioutil"
+	"path"
 	"testing"
 
-	assert "github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/assert"
+	"go.skia.org/infra/go/deepequal"
 	git_testutils "go.skia.org/infra/go/git/testutils"
 	"go.skia.org/infra/go/testutils"
 	"go.skia.org/infra/go/testutils/unittest"
-	"go.skia.org/infra/go/vcsinfo"
 )
 
-type repoRefresher struct{}
+func TestSerialize(t *testing.T) {
+	unittest.MediumTest(t)
+	ctx := context.Background()
+	g := git_testutils.GitInit(t, ctx)
+	defer g.Cleanup()
 
-// No-op, since the repo held by Graph is updated by the Graph during Update.
-func (u *repoRefresher) refresh(...*vcsinfo.LongCommit) {}
+	tmp1, err := ioutil.TempDir("", "")
+	assert.NoError(t, err)
+	defer testutils.RemoveAll(t, tmp1)
 
-// setupRepo performs common setup for git.Repo based Graphs.
-func setupRepo(t *testing.T) (context.Context, *git_testutils.GitBuilder, *Graph, refresher, func()) {
-	ctx, g, cleanup := commonSetup(t)
-
-	tmp, err := ioutil.TempDir("", "")
+	repo, err := NewLocalGraph(ctx, g.Dir(), tmp1)
 	assert.NoError(t, err)
 
-	repo, err := NewLocalGraph(ctx, g.Dir(), tmp)
-	assert.NoError(t, err)
-
-	return ctx, g, repo, &repoRefresher{}, func() {
-		testutils.RemoveAll(t, tmp)
-		cleanup()
+	// Add some commits.
+	for i := 0; i < 10; i++ {
+		g.CommitGen(ctx, "dummy.txt")
 	}
-}
+	assert.NoError(t, repo.Update(ctx))
+	assert.Equal(t, 10, repo.Len())
 
-func TestGraphRepo(t *testing.T) {
-	unittest.MediumTest(t)
-	ctx, g, repo, ud, cleanup := setupRepo(t)
-	defer cleanup()
-	testGraph(t, ctx, g, repo, ud)
-}
-
-func TestRecurseRepo(t *testing.T) {
-	unittest.MediumTest(t)
-	ctx, g, repo, ud, cleanup := setupRepo(t)
-	defer cleanup()
-	testRecurse(t, ctx, g, repo, ud)
-}
-
-func TestRecurseAllBranchesRepo(t *testing.T) {
-	unittest.MediumTest(t)
-	ctx, g, repo, ud, cleanup := setupRepo(t)
-	defer cleanup()
-	testRecurseAllBranches(t, ctx, g, repo, ud)
-}
-
-func TestUpdateHistoryChangedRepo(t *testing.T) {
-	unittest.MediumTest(t)
-	ctx, g, repo, ud, cleanup := setupRepo(t)
-	defer cleanup()
-	testUpdateHistoryChanged(t, ctx, g, repo, ud)
-}
-
-func TestUpdateAndReturnCommitDiffsRepo(t *testing.T) {
-	unittest.MediumTest(t)
-	ctx, g, repo, ud, cleanup := setupRepo(t)
-	defer cleanup()
-	testUpdateAndReturnCommitDiffs(t, ctx, g, repo, ud)
-}
-
-func TestRevListRepo(t *testing.T) {
-	unittest.MediumTest(t)
-	ctx, g, repo, ud, cleanup := setupRepo(t)
-	defer cleanup()
-	testRevList(t, ctx, g, repo, ud)
+	tmp2, err := ioutil.TempDir("", "")
+	defer testutils.RemoveAll(t, tmp2)
+	cacheFile := path.Join(tmp2, CACHE_FILE)
+	assert.NoError(t, writeCacheFile(repo, cacheFile))
+	repo2 := &Graph{
+		repoImpl: repo.repoImpl,
+	}
+	assert.NoError(t, initFromFile(repo2, cacheFile))
+	assert.NoError(t, err)
+	deepequal.AssertDeepEqual(t, repo, repo2)
 }
