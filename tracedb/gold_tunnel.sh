56a583e29d32a79f436d5f3ab449212b6ee48390
Kevin Lubick <kjlubick@google.com>
2019-08-16 07:50:15 -0400

    [gold] remove tracedb and traceserver
    Change-Id: Idb4c30e0a60d0c151c4917340468594907c0b46b

--- tracedb/gold_tunnel.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Sets up an SSH port forwarding from localhost:9090 to skia-tracedb:10000,
-# which is where the traceserver for Gold traces should be listening.
-gcloud compute ssh default@skia-tracedb --zone=us-central1-c --ssh-flag="-L 9090:localhost:9090"
