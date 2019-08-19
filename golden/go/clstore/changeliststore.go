7836b4c8d24c8c0f62ac75e843381b1c366777eb
Kevin Lubick <kjlubick@google.com>
2019-08-19 13:13:44 -0400

    [gold] Add types for new tryjobstore
    Bug: skia:9340
    Change-Id: I4d553684d14c6419e416ca73ab3b706e2b974cce

---
+++ golden/go/clstore/changeliststore.go
@@ -0,0 +1,29 @@
+// Package clstore defines an interface for storing ChangeList-related data.
+package clstore
+
+import (
+	"context"
+	"errors"
+
+	"go.skia.org/infra/golden/go/code_review"
+)
+
+// Store (sometimes called ChangeListStore) is an interface around a database
+// for storing ChangeLists and PatchSets.
+type Store interface {
+	// GetChangeList returns the ChangeList corresponding to the given id.
+	// Returns NotFound if it doesn't exist.
+	GetChangeList(ctx context.Context, id string) (code_review.ChangeList, error)
+	// GetPatchSet returns the PatchSet matching the given ChangeList ID and PatchSet ID.
+	// Returns NotFound if it doesn't exist.
+	GetPatchSet(ctx context.Context, clId, psID string) (code_review.PatchSet, error)
+
+	// PutChangeList stores the given ChangeList, overwriting any values for
+	// that ChangeList if they already existed.
+	PutChangeList(ctx context.Context, cl code_review.ChangeList) error
+	// PutChangeList stores the given PatchSet (which belongs to the ChangeList of the given id),
+	// overwriting any values for that ChangeList if they already existed.
+	PutPatchSet(ctx context.Context, clID string, ps code_review.PatchSet) error
+}
+
+var NotFound = errors.New("not found")
