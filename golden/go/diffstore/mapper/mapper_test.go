570b7b2de0087a5218c7bb494c7d093f669b082a
Leandro Lovisolo <lovisolo@google.com>
2019-08-21 15:24:40 -0400

    [gold] Move mapper.IsValidDiffImgID to validation package.
    Bug: skia:9350
    Change-Id: I38c7eb9a0661c17f35983d22ebd65ca21afcd045

--- golden/go/diffstore/mapper/mapper_test.go
+++ golden/go/diffstore/mapper/mapper_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"go.skia.org/infra/go/testutils/unittest"
 	"go.skia.org/infra/golden/go/types"
+	"go.skia.org/infra/golden/go/validation"
 )
 
 const (
@@ -24,9 +25,9 @@ func TestDiffID(t *testing.T) {
 	assert.Equal(t, diOne, diTwo)
 	assert.Equal(t, exampleDiffID, diOne)
 
-	assert.True(t, IsValidDiffImgID(diOne))
-	assert.False(t, IsValidDiffImgID("nope"))
-	assert.False(t, IsValidDiffImgID(string(imgOne)))
+	assert.True(t, validation.IsValidDiffImgID(diOne))
+	assert.False(t, validation.IsValidDiffImgID("nope"))
+	assert.False(t, validation.IsValidDiffImgID(string(imgOne)))
 }
 
 func TestSplitDiffID(t *testing.T) {
