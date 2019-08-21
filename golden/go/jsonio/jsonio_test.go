4cfd5b1ed4d6938efc61fd127bb4a458198ac620
Kevin Lubick <kjlubick@google.com>
2019-08-21 11:28:34 -0400

    [gold] Add more tryjob processing tests
    PS1 refactors out an unneeded type and fixes up some error contexts.
    
    Bug: skia:9340
    Change-Id: I29ebaf19a1003e4d9c6df7e5f6469c1f812e0730

--- golden/go/jsonio/jsonio_test.go
+++ golden/go/jsonio/jsonio_test.go
@@ -72,7 +72,7 @@ func TestValidate(t *testing.T) {
 
 func TestParseGoldResults(t *testing.T) {
 	unittest.SmallTest(t)
-	r := testParse(t, testJSON)
+	r := testParse(t, legacySkiaTryjobJSON)
 
 	// Make sure some key fields come out correctly, i.e. are converted correctly from string to int.
 	assert.Equal(t, "c4711517219f333c1116f47706eb57b51b5f8fc7", r.GitHash)
@@ -80,6 +80,23 @@ func TestParseGoldResults(t *testing.T) {
 	assert.Equal(t, int64(12345), r.GerritChangeListID)
 	assert.Equal(t, int64(10), r.GerritPatchSet)
 	assert.Equal(t, int64(549340494940393), r.BuildBucketID)
+	assert.Len(t, r.Results, 3)
+
+	r = testParse(t, legacySkiaJSON)
+	assert.Equal(t, types.MasterBranch, r.GerritChangeListID)
+	assert.Equal(t, "Test-Android-Clang-Nexus7-CPU-Tegra3-arm-Release-All-Android", r.Builder)
+	assert.Equal(t, r.Results[0].Key[types.PRIMARY_KEY_FIELD], "skottie_multiframe")
+	assert.Contains(t, r.Results[0].Options, "color_type")
+
+	r = testParse(t, legacyGoldCtlTryjobJSON)
+	assert.Equal(t, int64(1762193), r.GerritChangeListID)
+	assert.Equal(t, int64(2), r.GerritPatchSet)
+	assert.Equal(t, int64(8904604368086838672), r.BuildBucketID)
+	assert.Contains(t, r.Key, "vendor_id")
+
+	r = testParse(t, legacyGoldCtlJSON)
+	assert.Equal(t, types.LegacyMasterBranch, r.GerritChangeListID)
+	assert.Contains(t, r.Key, "vendor_id")
 
 	r = testParse(t, legacyMasterBranchJSON)
 	assert.Equal(t, types.LegacyMasterBranch, r.GerritChangeListID)
@@ -95,7 +112,7 @@ func TestGenJson(t *testing.T) {
 	unittest.SmallTest(t)
 
 	// Test parsing the test JSON.
-	goldResults := testParse(t, testJSON)
+	goldResults := testParse(t, legacySkiaTryjobJSON)
 
 	// For good measure we validate.
 	_, err := goldResults.Validate(false)
@@ -128,7 +145,9 @@ func assertErrorFields(t *testing.T, errMsgs []string, expectedFields ...string)
 }
 
 const (
-	testJSON = `{
+	// This is what Skia uploads before Aug 2019 when run on a tryjob, it
+	// should continue to be valid if the schema changes (so we can re-ingest).
+	legacySkiaTryjobJSON = `{
 		"gitHash" : "c4711517219f333c1116f47706eb57b51b5f8fc7",
 		"key" : {
 			 "arch" : "arm64",
@@ -186,6 +205,76 @@ const (
 		]
 }`
 
+	legacySkiaJSON = `{
+   "gitHash": "9c23a9e790b2f29b2cf204e67dbc67a363d0ce74",
+   "builder": "Test-Android-Clang-Nexus7-CPU-Tegra3-arm-Release-All-Android",
+   "buildbucket_build_id": "0",
+   "task_id": "56CzL86rgSDVSkFVK28r",
+   "swarming_bot_id": "skia-rpi-012",
+   "swarming_task_id": "469891c521e43d11",
+   "key": {
+      "arch": "arm",
+      "compiler": "Clang",
+      "extra_config": "Android",
+      "model": "Nexus7",
+      "os": "Android",
+      "style": "default"
+   },
+   "max_rss_MB": 274,
+   "results": [
+      {
+         "key": {
+            "name": "skottie_multiframe",
+            "config": "8888",
+            "source_type": "gm"
+         },
+         "options": {
+            "ext": "png",
+            "gamut": "untagged",
+            "transfer_fn": "untagged",
+            "color_type": "RGBA_8888",
+            "alpha_type": "Premul",
+            "color_depth": "8888"
+         },
+         "md5": "0abe3a2f7f58d2943f3b8b87f91dbff0"
+      },
+      {
+         "key": {
+            "name": "HTC.dng",
+            "config": "8888",
+            "source_type": "colorImage",
+            "source_options": "decode_native"
+         },
+         "options": {
+            "ext": "png",
+            "gamut": "untagged",
+            "transfer_fn": "untagged",
+            "color_type": "RGBA_8888",
+            "alpha_type": "Premul",
+            "color_depth": "8888"
+         },
+         "md5": "9cb31c854f22413841354f98b22a9acc"
+      },
+      {
+         "key": {
+            "name": "HTC.dng",
+            "config": "8888",
+            "source_type": "colorImage",
+            "source_options": "decode_to_dst"
+         },
+         "options": {
+            "ext": "png",
+            "gamut": "untagged",
+            "transfer_fn": "untagged",
+            "color_type": "RGBA_8888",
+            "alpha_type": "Premul",
+            "color_depth": "8888"
+         },
+         "md5": "9cb31c854f22413841354f98b22a9acc"
+      }
+   ]
+}`
+
 	legacyMasterBranchJSON = `{
 		"gitHash" : "c4711517219f333c1116f47706eb57b51b5f8fc7",
 		"key" : {
@@ -209,4 +298,39 @@ const (
 		},
 		"issue": ""
 	}`
+
+	// This is what goldctl spits out before Aug 2019 when run on a tryjob, it
+	// should continue to be valid if the schema changes (so we can re-ingest).
+	legacyGoldCtlTryjobJSON = `{
+  "gitHash": "e1681c90cf6a4c3b6be2bc4b4cea59849c16a438",
+  "key": {
+    "device_id": "0x1cb3",
+    "device_string": "None",
+    "model_name": "",
+    "msaa": "True",
+    "vendor_id": "0x10de",
+    "vendor_string": "None"
+  },
+  "results": [
+    {
+      "key": {
+        "name": "Pixel_CanvasDisplayLinearRGBUnaccelerated2DGPUCompositing",
+        "source_type": "chrome-gpu"
+      },
+      "options": {
+        "ext": "png"
+      },
+      "md5": "690f72c0b56ae014c8ac66e7f25c0779"
+    }
+  ],
+  "issue": "1762193",
+  "buildbucket_build_id": "8904604368086838672",
+  "patchset": "2",
+  "builder": "",
+  "task_id": ""
+}`
+
+	// This is what goldctl spits out before Aug 2019 when run on the waterfall, it
+	// should continue to be valid if the schema changes (so we can re-ingest).
+	legacyGoldCtlJSON = `{"gitHash":"7d3833876fb941a69bc3f49736eb8912c44156a8","key":{"device_id":"None","device_string":"Adreno (TM) 418","model_name":"Nexus 5X","msaa":"True","vendor_id":"None","vendor_string":"Qualcomm"},"results":[{"key":{"name":"Pixel_CanvasDisplayLinearRGBAccelerated2D","source_type":"chrome-gpu"},"options":{"ext":"png"},"md5":"301c213d21e2e0c8b0a0ddf5771453d2"}],"issue":"0","buildbucket_build_id":"0","patchset":"0","builder":"","task_id":""}`
 )
