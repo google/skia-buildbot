76e601bd2483107dfc27feb6c72f70e1511b8b35
Leandro Lovisolo <lovisolo@google.com>
2019-08-21 15:49:41 -0400

    [gold] Replace mapper.IsValidImgID with validation.IsValidDigest.
    Bug: skia:9350
    Change-Id: Ia6801887c2d2ba254bb1d25928d3325e8cd9c22d

--- golden/go/diffstore/mapper/mapper_test.go
+++ golden/go/diffstore/mapper/mapper_test.go
@@ -23,10 +23,6 @@ func TestDiffID(t *testing.T) {
 	diTwo := DiffID(imgTwo, imgOne)
 	assert.Equal(t, diOne, diTwo)
 	assert.Equal(t, exampleDiffID, diOne)
-
-	assert.True(t, IsValidDiffImgID(diOne))
-	assert.False(t, IsValidDiffImgID("nope"))
-	assert.False(t, IsValidDiffImgID(string(imgOne)))
 }
 
 func TestSplitDiffID(t *testing.T) {
@@ -37,12 +33,4 @@ func TestSplitDiffID(t *testing.T) {
 	assert.Equal(t, exampleDiffID, actualDiffID)
 	assert.Equal(t, imgOne, actualLeft)
 	assert.Equal(t, imgTwo, actualRight)
-}
-
-func TestIsValidImgID(t *testing.T) {
-	unittest.SmallTest(t)
-
-	assert.True(t, IsValidImgID(string(imgOne)))
-	assert.True(t, IsValidImgID(string(imgTwo)))
-	assert.False(t, IsValidImgID("nope"))
 }
