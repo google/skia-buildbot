2e20e05ae7dd67732ff098815df753d5a411e6b4
Eric Boren <borenet@google.com>
2019-08-21 13:47:55 -0400

    [gitstore] Use gitiles instead of a local git repo
    Use a repograph.Graph to update the GitStore, with a RepoImpl that reads from
    Gitiles and writes to GitStore as the Graph requests commits.
    
    Bug: skia:9084
    Change-Id: Ia19271890ec4240779885c5f7b79032fad3d9435

--- gitsync/go/gitsync/config.go
+++ gitsync/go/gitsync/config.go
@@ -10,15 +10,16 @@ import (
 // gitSyncConfig contains the configuration options that can be defined in a config file.
 // The JSON names of the fields match the flags defined in main.go.
 type gitSyncConfig struct {
-	BTInstanceID    string             `json:"bt_instance"` // BigTable instance
-	BTTableID       string             `json:"bt_table"`    // BigTable table ID.
-	HttpPort        string             `json:"http_port"`   // HTTP port for the health endpoint.
-	Local           bool               `json:"local"`       // Indicating whether this is running local.
-	ProjectID       string             `json:"project"`     // GCP project ID.
-	PromPort        string             `json:"prom_port"`   // Port at which the Prometheus metrics are be exposed.
-	RepoURLs        []string           `json:"repo_url"`    // List of repository URLs that should be updated.
-	RefreshInterval human.JSONDuration `json:"refresh"`     // Interval at which to poll each git repository.
-	WorkDir         string             `json:"workdir"`     // Work directory that should contain the checkouts.
+	BTInstanceID      string             `json:"bt_instance"`         // BigTable instance
+	BTTableID         string             `json:"bt_table"`            // BigTable table ID.
+	BTWriteGoroutines int                `json:"bt_write_goroutines"` // Number of goroutines to use when writing to BigTable.
+	HttpPort          string             `json:"http_port"`           // HTTP port for the health endpoint.
+	Local             bool               `json:"local"`               // Indicating whether this is running local.
+	ProjectID         string             `json:"project"`             // GCP project ID.
+	PromPort          string             `json:"prom_port"`           // Port at which the Prometheus metrics are be exposed.
+	RepoURLs          []string           `json:"repo_url"`            // List of repository URLs that should be updated.
+	RefreshInterval   human.JSONDuration `json:"refresh"`             // Interval at which to poll each git repository.
+	WorkDir           string             `json:"workdir"`             // Work directory that should contain the checkouts.
 }
 
 // String returns all configuration settings as a string intended to be printed upon startup
@@ -26,16 +27,17 @@ type gitSyncConfig struct {
 func (g *gitSyncConfig) String() string {
 	ret := ""
 	prefix := "      "
-	ret += fmt.Sprintf("%s bt_instance  : %s\n", prefix, g.BTInstanceID)
-	ret += fmt.Sprintf("%s bt_table     : %s\n", prefix, g.BTTableID)
-	ret += fmt.Sprintf("%s http_port    : %s\n", prefix, g.HttpPort)
-	ret += fmt.Sprintf("%s local        : %s\n", prefix, strconv.FormatBool(g.Local))
-	ret += fmt.Sprintf("%s project      : %s\n", prefix, g.ProjectID)
-	ret += fmt.Sprintf("%s prom_port    : %s\n", prefix, g.PromPort)
+	ret += fmt.Sprintf("%s bt_instance        : %s\n", prefix, g.BTInstanceID)
+	ret += fmt.Sprintf("%s bt_table           : %s\n", prefix, g.BTTableID)
+	ret += fmt.Sprintf("%s bt_write_goroutines: %d\n", prefix, g.BTWriteGoroutines)
+	ret += fmt.Sprintf("%s http_port          : %s\n", prefix, g.HttpPort)
+	ret += fmt.Sprintf("%s local              : %s\n", prefix, strconv.FormatBool(g.Local))
+	ret += fmt.Sprintf("%s project            : %s\n", prefix, g.ProjectID)
+	ret += fmt.Sprintf("%s prom_port          : %s\n", prefix, g.PromPort)
 	for _, url := range g.RepoURLs {
-		ret += fmt.Sprintf("%s repo_url     : %s\n", prefix, url)
+		ret += fmt.Sprintf("%s repo_url           : %s\n", prefix, url)
 	}
-	ret += fmt.Sprintf("%s refresh      : %s\n", prefix, g.RefreshInterval.String())
-	ret += fmt.Sprintf("%s workdir      : %s\n", prefix, g.WorkDir)
+	ret += fmt.Sprintf("%s refresh            : %s\n", prefix, g.RefreshInterval.String())
+	ret += fmt.Sprintf("%s workdir            : %s\n", prefix, g.WorkDir)
 	return ret
 }
