fc4fccd93940c7cb27a03e3aebc8ab1f46c8b356
Leandro Lovisolo <lovisolo@google.com>
2019-08-22 18:11:08 -0400

    [gold] imgloader.go: Stop writing images to disk.
    Bug: skia:9356
    Change-Id: I46eac4827d6dbb71da4b3736ed62d46367572aa8

--- golden/go/diffstore/common/common.go
+++ golden/go/diffstore/common/common.go
@@ -1,17 +1,14 @@
 package common
 
 import (
-	"fmt"
 	"image"
 	"image/png"
 	"io"
-	"os"
 	"path"
 	"strings"
 
 	"github.com/boltdb/bolt"
 	"go.skia.org/infra/go/fileutil"
-	"go.skia.org/infra/go/util"
 	"go.skia.org/infra/golden/go/diff"
 	"go.skia.org/infra/golden/go/types"
 )
@@ -24,28 +21,6 @@ const (
 	// resulting diff image.
 	DiffImageSeparator = "-"
 )
-
-// SaveFile writes the given file to disk.
-func SaveFile(targetPath string, r io.Reader) error {
-	f, err := os.Create(targetPath)
-	if err != nil {
-		return err
-	}
-	defer util.Close(f)
-
-	_, err = io.Copy(f, r)
-	return err
-}
-
-// LoadImg loads an image from disk.
-func LoadImg(sourcePath string) (*image.NRGBA, error) {
-	f, err := os.Open(sourcePath)
-	if err != nil {
-		return nil, err
-	}
-	defer util.Close(f)
-	return DecodeImg(f)
-}
 
 // EncodeImg encodes the given image as a PNG and writes the result to the
 // given writer.
@@ -66,11 +41,6 @@ func DecodeImg(reader io.Reader) (*image.NRGBA, error) {
 	return diff.GetNRGBA(im), nil
 }
 
-// GetDigestImageFileName returns the image name based on the digest.
-func GetDigestImageFileName(digest types.Digest) string {
-	return fmt.Sprintf("%s.%s", digest, IMG_EXTENSION)
-}
-
 // OpenBoltDB opens a boltDB in the given given directory with the given name.
 func OpenBoltDB(baseDir, name string) (*bolt.DB, error) {
 	baseDir, err := fileutil.EnsureDirExists(baseDir)
@@ -79,19 +49,6 @@ func OpenBoltDB(baseDir, name string) (*bolt.DB, error) {
 	}
 
 	return bolt.Open(path.Join(baseDir, name), 0600, nil)
-}
-
-// SaveFilePath saves the given buffer in path.
-func SaveFilePath(path string, r io.Reader) error {
-	err := fileutil.EnsureDirPathExists(path)
-	if err != nil {
-		return fmt.Errorf("Unable to create path for %s: %s", path, err)
-	}
-
-	if err := SaveFile(path, r); err != nil {
-		return fmt.Errorf("Unable to save file %s. Got error: %s", path, err)
-	}
-	return nil
 }
 
 func AsStrings(xd types.DigestSlice) []string {
--- golden/go/diffstore/imgloader_test.go
+++ golden/go/diffstore/imgloader_test.go
@@ -1,18 +1,14 @@
 package diffstore
 
 import (
-	"os"
 	"path"
-	"path/filepath"
 	"testing"
 
 	assert "github.com/stretchr/testify/require"
-	"go.skia.org/infra/go/fileutil"
 	"go.skia.org/infra/go/sktest"
 	"go.skia.org/infra/go/testutils"
 	"go.skia.org/infra/go/testutils/unittest"
 	"go.skia.org/infra/go/tiling"
-	"go.skia.org/infra/golden/go/diffstore/common"
 	"go.skia.org/infra/golden/go/diffstore/mapper"
 	"go.skia.org/infra/golden/go/diffstore/mapper/disk_mapper"
 	d_utils "go.skia.org/infra/golden/go/diffstore/testutils"
@@ -28,7 +24,7 @@ func TestImageLoader(t *testing.T) {
 	unittest.LargeTest(t)
 
 	m := &disk_mapper.DiskMapper{}
-	workingDir, tile, imageLoader, cleanup := getImageLoaderAndTile(t, m)
+	tile, imageLoader, cleanup := getImageLoaderAndTile(t, m)
 	defer cleanup()
 
 	// Iterate over the tile and get all the digests
@@ -46,41 +42,35 @@ func TestImageLoader(t *testing.T) {
 	digests := digestSet.Keys()[:100]
 	imageLoader.Warm(1, digests, true)
 
-	// Make sure they are all on disk.
+	// Make sure they are all cached.
 	for _, digest := range digests {
-		fn := common.GetDigestImageFileName(digest)
-		assert.True(t, fileutil.FileExists(fileutil.TwoLevelRadixPath(workingDir, fn)))
+		assert.True(t, imageLoader.Contains(digest))
 	}
 
 	// Fetch images from the secondary bucket.
-	_, _, err := imageLoader.Get(1, types.DigestSlice{TEST_IMG_DIGEST})
+	_, err := imageLoader.Get(1, types.DigestSlice{TEST_IMG_DIGEST})
 	assert.NoError(t, err)
-	_, _, err = imageLoader.Get(1, types.DigestSlice{"some-image-that-does-not-exist-at-all-in-any-bucket"})
+	_, err = imageLoader.Get(1, types.DigestSlice{"some-image-that-does-not-exist-at-all-in-any-bucket"})
 	assert.Error(t, err)
 }
 
-func getImageLoaderAndTile(t sktest.TestingT, m mapper.Mapper) (string, *tiling.Tile, *ImageLoader, func()) {
+func getImageLoaderAndTile(t sktest.TestingT, m mapper.Mapper) (*tiling.Tile, *ImageLoader, func()) {
 	w, cleanup := testutils.TempDir(t)
 	baseDir := path.Join(w, d_utils.TEST_DATA_BASE_DIR+"-imgloader")
 	client, tile := d_utils.GetSetupAndTile(t, baseDir)
 
-	workingDir := filepath.Join(baseDir, "images")
-	assert.Nil(t, os.Mkdir(workingDir, 0777))
-
 	imgCacheCount, _ := getCacheCounts(10)
 	gsBuckets := []string{d_utils.TEST_GCS_BUCKET_NAME, d_utils.TEST_GCS_SECONDARY_BUCKET}
-	imgLoader, err := NewImgLoader(client, baseDir, workingDir, gsBuckets, d_utils.TEST_GCS_IMAGE_DIR, imgCacheCount, m)
+	imgLoader, err := NewImgLoader(client, baseDir, gsBuckets, d_utils.TEST_GCS_IMAGE_DIR, imgCacheCount, m)
 	assert.NoError(t, err)
-	return workingDir, tile, imgLoader, cleanup
+	return tile, imgLoader, cleanup
 }
 
-func TestImagePaths(t *testing.T) {
+func TestGetGSRelPath(t *testing.T) {
 	unittest.SmallTest(t)
 
 	digest := types.Digest("098f6bcd4621d373cade4e832627b4f6")
-	expectedLocalPath := filepath.Join("09", "8f", string(digest)+".png")
 	expectedGSPath := string(digest + ".png")
-	localPath, gsPath := ImagePaths(digest)
-	assert.Equal(t, expectedLocalPath, localPath)
+	gsPath := getGSRelPath(digest)
 	assert.Equal(t, expectedGSPath, gsPath)
 }
