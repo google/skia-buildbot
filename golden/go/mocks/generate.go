839bb0c7cad6ef1c2ab83aeb731d18d08c3f2ef0
Kevin Lubick <kjlubick@google.com>
2019-08-27 08:28:38 -0400

    [gold] move diffwarmer mock to avoid dep cycle
    Change-Id: I6c78cb522b41e9675d6bd2437fecfa4c88a20686

--- golden/go/mocks/generate.go
+++ golden/go/mocks/generate.go
@@ -4,7 +4,6 @@ package mocks
 //go:generate mockery -name ClosestDiffFinder -dir ../digesttools -output .
 //go:generate mockery -name ComplexTile -dir ../types -output .
 //go:generate mockery -name DiffStore -dir ../diff -output .
-//go:generate mockery -name DiffWarmer -dir ../warmer -output .
 //go:generate mockery -name DigestCounter -dir ../digest_counter -output .
 //go:generate mockery -name ExpectationsStore -dir ../expstorage -output .
 //go:generate mockery -name GCSClient -dir ../storage -output .
