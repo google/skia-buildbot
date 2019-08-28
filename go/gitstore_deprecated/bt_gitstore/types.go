cb5e3be6e1b49d9eb59e234b8789ff8686bbdc6e
Eric Boren <borenet@google.com>
2019-08-28 08:31:33 -0400

    [gitstore] Remove deprecated implementation
    Change-Id: I7af24083d41e23fdd1ae9793f82ea1f507c3655f

--- go/gitstore_deprecated/bt_gitstore/types.go
+++
@@ -1,9 +0,0 @@
-package bt_gitstore
-
-// BTConfig contains the BigTable configuration to define where the repo should be stored.
-type BTConfig struct {
-	ProjectID  string
-	InstanceID string
-	TableID    string
-	Shards     int
-}
