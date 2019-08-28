acd899f86c9bcd18b6d54e9bdffe168347cc988e
Kevin Lubick <kjlubick@google.com>
2019-08-28 14:49:38 -0400

    [gold] Add tests/mocking for web's usage of new tryjobstore
    Bug: skia:9340
    Change-Id: If4104acc500dd57b457b137b762bc5886d23d4c5

--- golden/go/tjstore/fs_tjstore/fs_tjstore.go
+++ golden/go/tjstore/fs_tjstore/fs_tjstore.go
@@ -33,6 +33,11 @@ func (s *StoreImpl) GetRunningTryJobs(ctx context.Context) ([]ci.TryJob, error) 
 	return []ci.TryJob{}, errors.New("not impl")
 }
 
+// GetTryJobs implements the tjstore.Store interface.
+func (s *StoreImpl) GetTryJobs(ctx context.Context, psID tjstore.CombinedPSID) ([]ci.TryJob, error) {
+	return []ci.TryJob{}, errors.New("not impl")
+}
+
 // GetResults implements the tjstore.Store interface.
 func (s *StoreImpl) GetResults(ctx context.Context, psID tjstore.CombinedPSID) ([]tjstore.TryJobResult, error) {
 	return []tjstore.TryJobResult{}, errors.New("not impl")
@@ -46,6 +51,11 @@ func (s *StoreImpl) PutTryJob(ctx context.Context, psID tjstore.CombinedPSID, tj
 // PutResults implements the tjstore.Store interface.
 func (s *StoreImpl) PutResults(ctx context.Context, psID tjstore.CombinedPSID, r []tjstore.TryJobResult) error {
 	return errors.New("not impl")
+}
+
+// System implements the tjstore.Store interface.
+func (s *StoreImpl) System() string {
+	return s.cisName
 }
 
 // Make sure StoreImpl fulfills the tjstore.Store interface.
--- golden/go/tjstore/mocks/Store.go
+++ golden/go/tjstore/mocks/Store.go
@@ -83,6 +83,29 @@ func (_m *Store) GetTryJob(ctx context.Context, id string) (continuous_integrati
 	return r0, r1
 }
 
+// GetTryJobs provides a mock function with given fields: ctx, psID
+func (_m *Store) GetTryJobs(ctx context.Context, psID tjstore.CombinedPSID) ([]continuous_integration.TryJob, error) {
+	ret := _m.Called(ctx, psID)
+
+	var r0 []continuous_integration.TryJob
+	if rf, ok := ret.Get(0).(func(context.Context, tjstore.CombinedPSID) []continuous_integration.TryJob); ok {
+		r0 = rf(ctx, psID)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).([]continuous_integration.TryJob)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(context.Context, tjstore.CombinedPSID) error); ok {
+		r1 = rf(ctx, psID)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // PutResults provides a mock function with given fields: ctx, psID, r
 func (_m *Store) PutResults(ctx context.Context, psID tjstore.CombinedPSID, r []tjstore.TryJobResult) error {
 	ret := _m.Called(ctx, psID, r)
@@ -106,6 +129,20 @@ func (_m *Store) PutTryJob(ctx context.Context, psID tjstore.CombinedPSID, tj co
 		r0 = rf(ctx, psID, tj)
 	} else {
 		r0 = ret.Error(0)
+	}
+
+	return r0
+}
+
+// System provides a mock function with given fields:
+func (_m *Store) System() string {
+	ret := _m.Called()
+
+	var r0 string
+	if rf, ok := ret.Get(0).(func() string); ok {
+		r0 = rf()
+	} else {
+		r0 = ret.Get(0).(string)
 	}
 
 	return r0
