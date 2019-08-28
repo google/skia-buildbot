31fb93eef3e6ef73984c1cdd9893b78c5a4b4aec
Leandro Lovisolo <lovisolo@google.com>
2019-08-28 18:55:59 -0400

    [gold] skia_diff_server/main.go: Remove old auth method.
    This CL is based on https://skia-review.googlesource.com/c/buildbot/+/234117.
    
    Change-Id: Ib052094cbe9b4dc0932fbd71b0839e00a2caa2dd
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/237996
    Commit-Queue: Kevin Lubick <kjlubick@google.com>
    Reviewed-by: Kevin Lubick <kjlubick@google.com>
    Auto-Submit: Leandro Lovisolo <lovisolo@google.com>

--- golden/cmd/skia_diff_server/main.go
+++ golden/cmd/skia_diff_server/main.go
@@ -25,15 +25,15 @@ import (
 
 // Command line flags.
 var (
-	cacheSize          = flag.Int("cache_size", 1, "Approximate cachesize used to cache images and diff metrics in GiB. This is just a way to limit caching. 0 means no caching at all. Use default for testing.")
-	gsBucketName       = flag.String("gs_bucket", "", "[required] Name of the Google Storage bucket that holds the uploaded images.")
-	gsBaseDir          = flag.String("gs_basedir", diffstore.DEFAULT_GCS_IMG_DIR_NAME, "String that represents the google storage directory/directories following the GS bucket")
-	imageDir           = flag.String("image_dir", "/tmp/imagedir", "What directory to store test and diff images in.")
-	imagePort          = flag.String("image_port", ":9001", "Address that serves image files via HTTP.")
-	noCloudLog         = flag.Bool("no_cloud_log", false, "Disables cloud logging. Primarily for running locally.")
-	grpcPort           = flag.String("grpc_port", ":9000", "gRPC service address (e.g., ':9000')")
-	promPort           = flag.String("prom_port", ":20000", "Metrics service address (e.g., ':10110')")
-	serviceAccountFile = flag.String("service_account_file", "", "Credentials file for service account.")
+	cacheSize    = flag.Int("cache_size", 1, "Approximate cachesize used to cache images and diff metrics in GiB. This is just a way to limit caching. 0 means no caching at all. Use default for testing.")
+	gsBucketName = flag.String("gs_bucket", "", "[required] Name of the Google Storage bucket that holds the uploaded images.")
+	gsBaseDir    = flag.String("gs_basedir", diffstore.DEFAULT_GCS_IMG_DIR_NAME, "String that represents the google storage directory/directories following the GS bucket")
+	imageDir     = flag.String("image_dir", "/tmp/imagedir", "What directory to store test and diff images in.")
+	imagePort    = flag.String("image_port", ":9001", "Address that serves image files via HTTP.")
+	noCloudLog   = flag.Bool("no_cloud_log", false, "Disables cloud logging. Primarily for running locally.")
+	grpcPort     = flag.String("grpc_port", ":9000", "gRPC service address (e.g., ':9000')")
+	promPort     = flag.String("prom_port", ":20000", "Metrics service address (e.g., ':10110')")
+	local        = flag.Bool("local", false, "Running locally if true. As opposed to in production.")
 )
 
 const (
@@ -65,7 +65,7 @@ func main() {
 	}
 
 	// Get the client to be used to access GCS.
-	ts, err := auth.NewJWTServiceAccountTokenSource("", *serviceAccountFile, gstorage.CloudPlatformScope, "https://www.googleapis.com/auth/userinfo.email")
+	ts, err := auth.NewDefaultTokenSource(*local, gstorage.CloudPlatformScope, "https://www.googleapis.com/auth/userinfo.email")
 	if err != nil {
 		sklog.Fatalf("Failed to authenticate service account: %s", err)
 	}
