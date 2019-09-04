562764052fda6bbf00bd645ed60b87bd95941996
Ravi Mistry <rmistry@google.com>
2019-09-04 11:24:04 -0400

    Experimenting with tracking master swarming task in CTFE
    Change-Id: I8bc1e27f35046a46ea8dec9ca8fa7d8dc0384b09

--- ct/go/ctfe/main.go
+++ ct/go/ctfe/main.go
@@ -54,6 +54,10 @@ var (
 	resourcesDir           = flag.String("resources_dir", "", "The directory to find templates, JS, and CSS files. If blank the current directory will be used.")
 	tasksSchedulerWaitTime = flag.Duration("tasks_scheduler_wait_time", 5*time.Minute, "How often the repeated tasks scheduler should run.")
 
+	// Email params
+	emailClientSecretFile = flag.String("email_client_secret_file", "/etc/ct-email-secrets/client_secret.json", "OAuth client secret JSON file for sending email.")
+	emailTokenCacheFile   = flag.String("email_token_cache_file", "/etc/ct-email-secrets/client_token.json", "OAuth token cache file for sending email.")
+
 	// Datastore params
 	namespace   = flag.String("namespace", "cluster-telemetry", "The Cloud Datastore namespace, such as 'cluster-telemetry'.")
 	projectName = flag.String("project_name", "skia-public", "The Google Cloud project name.")
@@ -163,6 +167,10 @@ func startCtfeMetrics(ctx context.Context) {
 			}
 		}
 	}()
+}
+
+func pollMasterScriptTasks(ctx context.Context) {
+
 }
 
 // repeatedTasksScheduler looks for all tasks that contain repeat_after_days
@@ -278,6 +286,13 @@ func main() {
 		serverURL = "http://" + *host + *port
 	}
 
+	if !*local {
+		// Initialize mailing library.
+		if err := ctutil.MailInit(*emailClientSecretFile, *emailTokenCacheFile); err != nil {
+			sklog.Fatalf("Could not initialize mailing library: %s", err)
+		}
+	}
+
 	if *local {
 		login.SimpleInitWithAllow(*port, *local, nil, nil, nil)
 	} else {
@@ -311,6 +326,9 @@ func main() {
 
 	// Start the repeated tasks scheduler.
 	go repeatedTasksScheduler(ctx)
+
+	// Start poller for keeping an eye on master script swarming tasks.
+	go pollerMasterScriptTasks(ctx)
 
 	runServer(serverURL)
 }
