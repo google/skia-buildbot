7b408fe8242cfd117a1e42061f61385cf2884d8a
Joe Gregorio <joe@bitworking.org>
2019-09-04 09:31:20 -0500

    Perf - Add perf-tool cmd to create PubSub topics.
    Also adds in creation of PubSub topics for event driven alerting.
    
    Bug: skia:9384
    Change-Id: I6b203b43fbcc024c404c113a1b73bc4a18cf7b84

--- perf/create-ingestion-ct-pubsub-topics.sh
+++ perf/create-ingestion-ct-pubsub-topics.sh
@@ -8,5 +8,5 @@ source ../kube/config.sh
 
 TOPIC=perf-ingestion-ct-production
 
-gcloud pubsub topics create ${TOPIC} || true
+perf-tool config create-pubsub-topics --big_table_config=ct-prod
 gsutil notification create -f json -e OBJECT_FINALIZE -t projects/${PROJECT_ID}/topics/${TOPIC} -p ingest gs://cluster-telemetry-perf
--- perf/create-ingestion-pubsub-topics.sh
+++ perf/create-ingestion-pubsub-topics.sh
@@ -8,7 +8,7 @@ source ../kube/config.sh
 
 TOPIC=perf-ingestion-skia-production
 
-gcloud pubsub topics create ${TOPIC} || true
+perf-tool config create-pubsub-topics --big_table_config=nano
 gsutil notification create -f json -e OBJECT_FINALIZE -t projects/${PROJECT_ID}/topics/${TOPIC} -p buildstats-json-v1  gs://skia-perf
 gsutil notification create -f json -e OBJECT_FINALIZE -t projects/${PROJECT_ID}/topics/${TOPIC} -p nano-json-v1  gs://skia-perf
 gsutil notification create -f json -e OBJECT_FINALIZE -t projects/${PROJECT_ID}/topics/${TOPIC} -p task-duration  gs://skia-perf
