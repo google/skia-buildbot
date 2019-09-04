543728b751fc296d2a3b3f1cf50aeb827a645b32
Ravi Mistry <rmistry@google.com>
2019-09-04 11:14:12 -0400

    Experimenting with tracking master swarming task in CTFE
    Change-Id: I8bc1e27f35046a46ea8dec9ca8fa7d8dc0384b09

--- ct/go/ctfe/chromium_builds/chromium_builds.go
+++ ct/go/ctfe/chromium_builds/chromium_builds.go
@@ -130,10 +130,11 @@ func (task DatastoreTask) Get(c context.Context, key *datastore.Key) (task_commo
 	return t, nil
 }
 
-func (task DatastoreTask) TriggerSwarmingTask(ctx context.Context) error {
+func (task DatastoreTask) TriggerSwarmingTaskAndMail(ctx context.Context) (string, error) {
 	runID := task_common.GetRunID(&task)
+	emails := []string{task.Username}
 	isolateArgs := map[string]string{
-		"EMAILS":          task.Username,
+		"EMAILS":          strings.Join(emails, ","),
 		"TASK_ID":         strconv.FormatInt(task.DatastoreKey.ID, 10),
 		"RUN_ID":          runID,
 		"TARGET_PLATFORM": ctutil.PLATFORM_LINUX,
@@ -143,10 +144,13 @@ func (task DatastoreTask) TriggerSwarmingTask(ctx context.Context) error {
 		"DS_PROJECT_NAME": task_common.DsProjectName,
 	}
 
-	if err := ctutil.TriggerMasterScriptSwarmingTask(ctx, runID, "build_chromium", ctutil.BUILD_CHROMIUM_MASTER_ISOLATE, task_common.ServiceAccountFile, ctutil.PLATFORM_LINUX, false, isolateArgs); err != nil {
-		return fmt.Errorf("Could not trigger master script for build_chromium with isolate args %v: %s", isolateArgs, err)
+	taskID, err := ctutil.TriggerMasterScriptSwarmingTask(ctx, runID, "build_chromium", ctutil.BUILD_CHROMIUM_MASTER_ISOLATE, task_common.ServiceAccountFile, ctutil.PLATFORM_LINUX, false, isolateArgs)
+	if err != nil {
+		return "", fmt.Errorf("Could not trigger master script for build_chromium with isolate args %v: %s", isolateArgs, err)
 	}
-	return nil
+	// Send start email.
+	skutil.LogErr(ctutil.SendTaskStartEmail(task.DatastoreKey.ID, emails, "Build chromium", runID, "", ""))
+	return taskID, nil
 }
 
 func addTaskView(w http.ResponseWriter, r *http.Request) {
