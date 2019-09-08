df88b1fea0b45ae3f80b344dcb069d9b0869d26c
Kevin Lubick <kjlubick@google.com>
2019-09-08 19:10:43 -0400

    [gold] POC using lit-html's login-sk
    Change-Id: Iea8f0a30686dd79b244380a8a601e41a29ff8b24

---
+++ golden/modules/changelists-page/changelists-page.js
@@ -0,0 +1,22 @@
+import { define } from 'elements-sk/define'
+import { html, render } from 'lit-html'
+
+
+const template = (ele) => html`
+<div>hello world</div>
+`;
+
+define('changelists-page', class extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.render();
+  }
+
+  render() {
+    render(template(this), this, {eventContext: this})
+  }
+
+});
