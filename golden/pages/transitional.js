d81a5fd045e865fe5ef6bfcabe892b8bb5905b2c
Kevin Lubick <kjlubick@google.com>
2019-09-08 18:59:56 -0400

    [gold] POC using lit-html's login-sk
    Change-Id: Iea8f0a30686dd79b244380a8a601e41a29ff8b24

---
+++ golden/pages/transitional.js
@@ -0,0 +1,5 @@
+// this holds all the lit-html modules that we make so they
+// can be incorporated into the legacy Polymer pages.
+
+import '../../infra-sk/modules/login-sk'
+import '../node_modules/@webcomponents/custom-elements/custom-elements.min.js'
