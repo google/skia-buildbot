1dcefc6e40ab711d90eff52666bbccf7cfb1909d
Ravi Mistry <rmistry@google.com>
2019-09-09 14:36:54 -0400

    [CT] Remove everything to do with UpdateVars
    Bug: skia:9405
    Change-Id: I7c6d75e12e54b1378f0fd5e9df55810a67f2d502

--- ct/go/ctfe/metrics_analysis/metrics_analysis.go
+++ ct/go/ctfe/metrics_analysis/metrics_analysis.go
@@ -102,10 +102,6 @@ func (task DatastoreTask) GetResultsLink() string {
 	return task.RawOutput
 }
 
-func (task DatastoreTask) GetUpdateTaskVars() task_common.UpdateTaskVars {
-	return &UpdateVars{}
-}
-
 func (task DatastoreTask) RunsOnGCEWorkers() bool {
 	return true
 }
@@ -158,7 +154,7 @@ func (task DatastoreTask) TriggerSwarmingTaskAndMail(ctx context.Context) error 
 		return fmt.Errorf("Could not trigger master script for metrics_analysis_on_workers with isolate args %v: %s", isolateArgs, err)
 	}
 	// Mark task as started in datastore.
-	if err := task_common.UpdateTaskSetStarted(ctx, &UpdateVars{}, task.DatastoreKey.ID, runID, sTaskID); err != nil {
+	if err := task_common.UpdateTaskSetStarted(ctx, runID, sTaskID, &task); err != nil {
 		return fmt.Errorf("Could not mark task as started in datastore: %s", err)
 	}
 	// Send start email.
@@ -207,6 +203,21 @@ func (task DatastoreTask) SendCompletionEmail(ctx context.Context, completedSucc
 	if err := ctutil.SendEmailWithMarkup(emails, emailSubject, emailBody, viewActionMarkup); err != nil {
 		return fmt.Errorf("Error while sending email: %s", err)
 	}
+	return nil
+}
+
+func (task DatastoreTask) SetCompleted(success bool) error {
+	if success {
+		runID := task_common.GetRunID(&task)
+		task.RawOutput = ctutil.GetMetricsAnalysisOutputLink(runID)
+	}
+	var err error
+	task.TsCompleted, err = ctutil.GetCurrentTsInt64()
+	if err != nil {
+		return err
+	}
+	task.Failure = !success
+	task.TaskDone = true
 	return nil
 }
 
@@ -308,34 +319,6 @@ func addTaskHandler(w http.ResponseWriter, r *http.Request) {
 
 func getTasksHandler(w http.ResponseWriter, r *http.Request) {
 	task_common.GetTasksHandler(&DatastoreTask{}, w, r)
-}
-
-type UpdateVars struct {
-	task_common.UpdateTaskCommonVars
-
-	RawOutput string
-}
-
-func (task *UpdateVars) GetTaskPrototype() task_common.Task {
-	return &DatastoreTask{}
-}
-
-func (vars *UpdateVars) UpdateExtraFields(t task_common.Task) error {
-	task := t.(*DatastoreTask)
-	if vars.RawOutput != "" {
-		task.RawOutput = vars.RawOutput
-	}
-	return nil
-}
-
-func (vars *UpdateVars) SetCompleted(success bool, t task_common.Task) {
-	if success {
-		runID := task_common.GetRunID(t)
-		vars.RawOutput = ctutil.GetMetricsAnalysisOutputLink(runID)
-	}
-	vars.TsCompleted = ctutil.GetCurrentTs()
-	vars.Failure = !success
-	vars.TaskDone = true
 }
 
 func deleteTaskHandler(w http.ResponseWriter, r *http.Request) {
