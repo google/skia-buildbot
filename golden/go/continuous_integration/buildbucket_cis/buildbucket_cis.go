ebc7930450ab515e0ee02d7e8dc7bb529bb24f24
Kevin Lubick <kjlubick@google.com>
2019-09-09 14:52:22 -0400

    [gold] Turn on new tryjobstore
    Need the following changes:
     - add GetPatchSetByOrder
     - add rate limiting to Gerrit and BuildBucket
     - Fix patchset order/id conflation
     - Fix templates for configmaps
    
    Bug: skia:9340
    Change-Id: I6f7b61d046d6b53b1d9de4af018d7a0e40ef30ac

--- golden/go/continuous_integration/buildbucket_cis/buildbucket_cis.go
+++ golden/go/continuous_integration/buildbucket_cis/buildbucket_cis.go
@@ -7,20 +7,34 @@ import (
 	"go.skia.org/infra/go/buildbucket"
 	"go.skia.org/infra/go/skerr"
 	ci "go.skia.org/infra/golden/go/continuous_integration"
+	"golang.org/x/time/rate"
+)
+
+const (
+	// These values are arbitrary guesses, roughly based on values observed
+	// by previous implementation in production.
+	maxQPS   = rate.Limit(10.0)
+	maxBurst = 40
 )
 
 type CISImpl struct {
 	bbClient buildbucket.BuildBucketInterface
+	rl       *rate.Limiter
 }
 
 func New(client buildbucket.BuildBucketInterface) *CISImpl {
 	return &CISImpl{
 		bbClient: client,
+		rl:       rate.NewLimiter(maxQPS, maxBurst),
 	}
 }
 
 // GetTryJob implements the continuous_integration.Client interface.
 func (c *CISImpl) GetTryJob(ctx context.Context, id string) (ci.TryJob, error) {
+	// Respect the rate limit.
+	if err := c.rl.Wait(ctx); err != nil {
+		return ci.TryJob{}, skerr.Wrap(err)
+	}
 	tj, err := c.bbClient.GetBuild(ctx, id)
 	if err != nil {
 		if strings.Contains(err.Error(), "NotFound") {
