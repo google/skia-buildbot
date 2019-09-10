9ad12df37cb82ce77cb924aea6f1d80e7983f0f1
Leandro Lovisolo <lovisolo@google.com>
2019-09-10 18:17:45 -0400

    [goldpushk] Program skeleton.
    Bug: skia:9076
    Change-Id: I75d6b2115668d3e39ee8b9e27882aa068ad037cc

---
+++ golden/cmd/goldpushk/goldpushk/types.go
@@ -0,0 +1,84 @@
+package goldpushk
+
+import (
+	"fmt"
+	"sort"
+)
+
+type GoldInstance string
+type GoldService string
+
+// Represents Gold instance/service pair.
+type GoldInstanceServicePair struct {
+	Instance GoldInstance
+	Service  GoldService
+}
+
+// Contains all the necessary information to deploy a service for a specific Gold instance.
+// TODO(lovisolo): Add any missing fields.
+type GoldServiceDeployment struct {
+	GoldInstanceServicePair
+	Internal      bool // If true, deploy to the "skia-corp" cluster, otherwise deploy to "skia-public".
+	ConfigMapName string
+}
+
+// GoldServiceDeployment constructor.
+func NewGoldServiceDeployment(instance GoldInstance, service GoldService) GoldServiceDeployment {
+	return GoldServiceDeployment{
+		GoldInstanceServicePair: GoldInstanceServicePair{
+			Instance: instance,
+			Service:  service,
+		},
+	}
+}
+
+// Returns its canonical name used e.g. to name its corresponding .yaml file.
+func (d GoldServiceDeployment) CanonicalName() string {
+	return fmt.Sprintf("gold-%s-%s", d.Instance, d.Service)
+}
+
+// Maps Gold instances to their required services and deployment information.
+// Used as the source of truth for all things instances/services.
+type GoldServicesMap map[GoldInstance]map[GoldService]GoldServiceDeployment
+
+// Adds a deployment to the services map, and makes sure the relevant fields in
+// the deployment match its keys in the map.
+func (m *GoldServicesMap) AddDeployment(instance GoldInstance, service GoldService, deployment GoldServiceDeployment) {
+	if (*m)[instance] == nil {
+		(*m)[instance] = make(map[GoldService]GoldServiceDeployment)
+	}
+
+	deployment.Instance = instance
+	deployment.Service = service
+	(*m)[instance][service] = deployment
+}
+
+// Iterates over all instance/service pairs.
+func (m GoldServicesMap) ForAll(f func(GoldInstance, GoldService, GoldServiceDeployment)) {
+	// Sort keys first to iterate over them later in a deterministic order.
+	instances := make([]GoldInstance, 0)
+	for instance := range m {
+		instances = append(instances, instance)
+	}
+	sort.Slice(instances, func(i, j int) bool {
+		return instances[i] < instances[j]
+	})
+
+	// Iterate over all instances.
+	for _, instance := range instances {
+
+		// Sort services for the current instance.
+		services := make([]GoldService, 0)
+		for service := range m[instance] {
+			services = append(services, service)
+		}
+		sort.Slice(services, func(i, j int) bool {
+			return services[i] < services[j]
+		})
+
+		// Iterate over all services for the current instance
+		for _, service := range services {
+			f(instance, service, m[instance][service])
+		}
+	}
+}
---
+++ golden/cmd/goldpushk/main_test.go
@@ -0,0 +1,415 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"go.skia.org/infra/go/testutils/unittest"
+	"go.skia.org/infra/golden/cmd/goldpushk/goldpushk"
+)
+
+func TestParseAndValidateFlags(t *testing.T) {
+	unittest.SmallTest(t)
+
+	// Deployments shared among test cases.
+	chromeBaselineServer := goldpushk.NewGoldServiceDeployment(goldpushk.Chrome, goldpushk.BaselineServer)
+	chromeDiffServer := goldpushk.NewGoldServiceDeployment(goldpushk.Chrome, goldpushk.DiffServer)
+	chromeSkiaCorrectness := goldpushk.NewGoldServiceDeployment(goldpushk.Chrome, goldpushk.SkiaCorrectness)
+	skiaDiffServer := goldpushk.NewGoldServiceDeployment(goldpushk.Skia, goldpushk.DiffServer)
+	skiaSkiaCorrectness := goldpushk.NewGoldServiceDeployment(goldpushk.Skia, goldpushk.SkiaCorrectness)
+	skiaPublicDiffServer := goldpushk.NewGoldServiceDeployment(goldpushk.SkiaPublic, goldpushk.DiffServer)
+
+	// Services map the test cases will run against.
+	servicesMap := goldpushk.GoldServicesMap{
+		goldpushk.Chrome: {
+			goldpushk.BaselineServer:  chromeBaselineServer,
+			goldpushk.DiffServer:      chromeDiffServer,
+			goldpushk.SkiaCorrectness: chromeSkiaCorrectness,
+		},
+		goldpushk.Skia: {
+			goldpushk.DiffServer:      skiaDiffServer,
+			goldpushk.SkiaCorrectness: skiaSkiaCorrectness,
+		},
+		goldpushk.SkiaPublic: {
+			goldpushk.DiffServer: skiaPublicDiffServer,
+		},
+	}
+
+	testCases := []struct {
+		message string // Test case name.
+
+		// Inputs.
+		flagInstances []string
+		flagServices  []string
+		flagCanaries  []string
+
+		// Expected outputs.
+		expectedDeployments         []goldpushk.GoldServiceDeployment
+		expectedCanariedDeployments []goldpushk.GoldServiceDeployment
+		hasError                    bool
+		errorMsg                    string
+	}{
+
+		////////////////////////////////////////////////////////////////////////////
+		// Errors                                                                 //
+		////////////////////////////////////////////////////////////////////////////
+
+		{
+			message:       "Error: --instances all,chrome",
+			flagInstances: []string{"all", "chrome"},
+			flagServices:  []string{"baselineserver"},
+			flagCanaries:  []string{},
+			hasError:      true,
+			errorMsg:      "flag --instances should contain either \"all\" or a list of Gold instances, but not both",
+		},
+
+		{
+			message:       "Error: --services all,baselineserver",
+			flagInstances: []string{"chrome"},
+			flagServices:  []string{"all", "baselineserver"},
+			flagCanaries:  []string{},
+			hasError:      true,
+			errorMsg:      "flag --services should contain either \"all\" or a list of Gold services, but not both",
+		},
+
+		{
+			message:       "Error: Unknown instance",
+			flagInstances: []string{"foo"},
+			flagServices:  []string{"baselineserver"},
+			flagCanaries:  []string{},
+			hasError:      true,
+			errorMsg:      "unknown Gold instance: \"foo\"",
+		},
+
+		{
+			message:       "Error: Unknown service",
+			flagInstances: []string{"chrome"},
+			flagServices:  []string{"foo"},
+			flagCanaries:  []string{},
+			hasError:      true,
+			errorMsg:      "unknown Gold service: \"foo\"",
+		},
+
+		{
+			message:       "Error: No instances/services matched.",
+			flagInstances: []string{"skia"},
+			flagServices:  []string{"baselineserver"},
+			hasError:      true,
+			errorMsg:      "no known Gold service deployments match the values supplied with --instances and --services",
+		},
+
+		{
+			message:       "Error: Invalid canary format",
+			flagInstances: []string{"chrome"},
+			flagServices:  []string{"baselineserver"},
+			flagCanaries:  []string{"xxxxxxxxx"},
+			hasError:      true,
+			errorMsg:      "invalid canary format: \"xxxxxxxxx\"",
+		},
+
+		{
+			message:       "Error: Invalid canary due to unknown instance",
+			flagInstances: []string{"chrome"},
+			flagServices:  []string{"baselineserver"},
+			flagCanaries:  []string{"foo:baselineserver"},
+			hasError:      true,
+			errorMsg:      "invalid canary - unknown Gold instance: \"foo:baselineserver\"",
+		},
+
+		{
+			message:       "Error: Invalid canary due to unknown service",
+			flagInstances: []string{"chrome"},
+			flagServices:  []string{"baselineserver"},
+			flagCanaries:  []string{"chrome:foo"},
+			hasError:      true,
+			errorMsg:      "invalid canary - unknown Gold service: \"chrome:foo\"",
+		},
+
+		{
+			message:       "Error: Canary doesn't match --instances / --services",
+			flagInstances: []string{"chrome"},
+			flagServices:  []string{"baselineserver"},
+			flagCanaries:  []string{"skia:diffserver"},
+			hasError:      true,
+			errorMsg:      "canary does not match any targeted services: \"skia:diffserver\"",
+		},
+
+		{
+			message:       "Error: All targeted services are canaried",
+			flagInstances: []string{"chrome"},
+			flagServices:  []string{"baselineserver"},
+			flagCanaries:  []string{"chrome:baselineserver"},
+			hasError:      true,
+			errorMsg:      "all targeted services are marked for canarying",
+		},
+
+		////////////////////////////////////////////////////////////////////////////
+		// No wildcards                                                           //
+		////////////////////////////////////////////////////////////////////////////
+
+		{
+			message:             "Single instance, single service, no canary",
+			flagInstances:       []string{"chrome"},
+			flagServices:        []string{"baselineserver"},
+			flagCanaries:        []string{},
+			expectedDeployments: []goldpushk.GoldServiceDeployment{chromeBaselineServer},
+		},
+
+		{
+			message:             "Single instance, multiple services, no canary",
+			flagInstances:       []string{"chrome"},
+			flagServices:        []string{"baselineserver", "diffserver"},
+			flagCanaries:        []string{},
+			expectedDeployments: []goldpushk.GoldServiceDeployment{chromeBaselineServer, chromeDiffServer},
+		},
+
+		{
+			message:                     "Single instance, multiple services, one canary",
+			flagInstances:               []string{"chrome"},
+			flagServices:                []string{"baselineserver", "diffserver", "skiacorrectness"},
+			flagCanaries:                []string{"chrome:skiacorrectness"},
+			expectedDeployments:         []goldpushk.GoldServiceDeployment{chromeBaselineServer, chromeDiffServer},
+			expectedCanariedDeployments: []goldpushk.GoldServiceDeployment{chromeSkiaCorrectness},
+		},
+
+		{
+			message:                     "Single instance, multiple services, multiple canaries",
+			flagInstances:               []string{"chrome"},
+			flagServices:                []string{"baselineserver", "diffserver", "skiacorrectness"},
+			flagCanaries:                []string{"chrome:diffserver", "chrome:skiacorrectness"},
+			expectedDeployments:         []goldpushk.GoldServiceDeployment{chromeBaselineServer},
+			expectedCanariedDeployments: []goldpushk.GoldServiceDeployment{chromeDiffServer, chromeSkiaCorrectness},
+		},
+
+		{
+			message:             "Multiple instances, single service, no canary",
+			flagInstances:       []string{"chrome", "skia", "skia-public"},
+			flagServices:        []string{"diffserver"},
+			flagCanaries:        []string{},
+			expectedDeployments: []goldpushk.GoldServiceDeployment{chromeDiffServer, skiaDiffServer, skiaPublicDiffServer},
+		},
+
+		{
+			message:                     "Multiple instances, single service, one canary",
+			flagInstances:               []string{"chrome", "skia", "skia-public"},
+			flagServices:                []string{"diffserver"},
+			flagCanaries:                []string{"skia-public:diffserver"},
+			expectedDeployments:         []goldpushk.GoldServiceDeployment{chromeDiffServer, skiaDiffServer},
+			expectedCanariedDeployments: []goldpushk.GoldServiceDeployment{skiaPublicDiffServer},
+		},
+
+		{
+			message:                     "Multiple instances, single service, multiple canaries",
+			flagInstances:               []string{"chrome", "skia", "skia-public"},
+			flagServices:                []string{"diffserver"},
+			flagCanaries:                []string{"skia:diffserver", "skia-public:diffserver"},
+			expectedDeployments:         []goldpushk.GoldServiceDeployment{chromeDiffServer},
+			expectedCanariedDeployments: []goldpushk.GoldServiceDeployment{skiaDiffServer, skiaPublicDiffServer},
+		},
+
+		{
+			message:             "Multiple instances, multiple services, no canary",
+			flagInstances:       []string{"chrome", "skia", "skia-public"},
+			flagServices:        []string{"diffserver", "skiacorrectness"},
+			flagCanaries:        []string{},
+			expectedDeployments: []goldpushk.GoldServiceDeployment{chromeDiffServer, chromeSkiaCorrectness, skiaDiffServer, skiaSkiaCorrectness, skiaPublicDiffServer},
+		},
+
+		{
+			message:                     "Multiple instances, multiple services, one canary",
+			flagInstances:               []string{"chrome", "skia", "skia-public"},
+			flagServices:                []string{"diffserver", "skiacorrectness"},
+			flagCanaries:                []string{"skia-public:diffserver"},
+			expectedDeployments:         []goldpushk.GoldServiceDeployment{chromeDiffServer, chromeSkiaCorrectness, skiaDiffServer, skiaSkiaCorrectness},
+			expectedCanariedDeployments: []goldpushk.GoldServiceDeployment{skiaPublicDiffServer},
+		},
+
+		{
+			message:                     "Multiple instances, multiple services, multiple canaries",
+			flagInstances:               []string{"chrome", "skia", "skia-public"},
+			flagServices:                []string{"diffserver", "skiacorrectness"},
+			flagCanaries:                []string{"skia:diffserver", "skia-public:diffserver"},
+			expectedDeployments:         []goldpushk.GoldServiceDeployment{chromeDiffServer, chromeSkiaCorrectness, skiaSkiaCorrectness},
+			expectedCanariedDeployments: []goldpushk.GoldServiceDeployment{skiaDiffServer, skiaPublicDiffServer},
+		},
+
+		////////////////////////////////////////////////////////////////////////////
+		// Wildcard: --service all                                                //
+		////////////////////////////////////////////////////////////////////////////
+
+		{
+			message:             "Single instance, all services, no canary",
+			flagInstances:       []string{"chrome"},
+			flagServices:        []string{"all"},
+			flagCanaries:        []string{},
+			expectedDeployments: []goldpushk.GoldServiceDeployment{chromeBaselineServer, chromeDiffServer, chromeSkiaCorrectness},
+		},
+
+		{
+			message:                     "Single instance, all services, one canary",
+			flagInstances:               []string{"chrome"},
+			flagServices:                []string{"all"},
+			flagCanaries:                []string{"chrome:skiacorrectness"},
+			expectedDeployments:         []goldpushk.GoldServiceDeployment{chromeBaselineServer, chromeDiffServer},
+			expectedCanariedDeployments: []goldpushk.GoldServiceDeployment{chromeSkiaCorrectness},
+		},
+
+		{
+			message:                     "Single instance, all services, multiple canaries",
+			flagInstances:               []string{"chrome"},
+			flagServices:                []string{"all"},
+			flagCanaries:                []string{"chrome:diffserver", "chrome:skiacorrectness"},
+			expectedDeployments:         []goldpushk.GoldServiceDeployment{chromeBaselineServer},
+			expectedCanariedDeployments: []goldpushk.GoldServiceDeployment{chromeDiffServer, chromeSkiaCorrectness},
+		},
+
+		{
+			message:             "Multiple instances, all services, no canary",
+			flagInstances:       []string{"chrome", "skia"},
+			flagServices:        []string{"all"},
+			flagCanaries:        []string{},
+			expectedDeployments: []goldpushk.GoldServiceDeployment{chromeBaselineServer, chromeDiffServer, chromeSkiaCorrectness, skiaDiffServer, skiaSkiaCorrectness},
+		},
+
+		{
+			message:                     "Multiple instances, all services, one canary",
+			flagInstances:               []string{"chrome", "skia"},
+			flagServices:                []string{"all"},
+			flagCanaries:                []string{"skia:skiacorrectness"},
+			expectedDeployments:         []goldpushk.GoldServiceDeployment{chromeBaselineServer, chromeDiffServer, chromeSkiaCorrectness, skiaDiffServer},
+			expectedCanariedDeployments: []goldpushk.GoldServiceDeployment{skiaSkiaCorrectness},
+		},
+
+		{
+			message:                     "Multiple instances, all services, multiple canaries",
+			flagInstances:               []string{"chrome", "skia"},
+			flagServices:                []string{"all"},
+			flagCanaries:                []string{"skia:diffserver", "skia:skiacorrectness"},
+			expectedDeployments:         []goldpushk.GoldServiceDeployment{chromeBaselineServer, chromeDiffServer, chromeSkiaCorrectness},
+			expectedCanariedDeployments: []goldpushk.GoldServiceDeployment{skiaDiffServer, skiaSkiaCorrectness},
+		},
+
+		////////////////////////////////////////////////////////////////////////////
+		// Wildcard: --instance all                                               //
+		////////////////////////////////////////////////////////////////////////////
+
+		{
+			message:             "All instances, single service, no canary",
+			flagInstances:       []string{"all"},
+			flagServices:        []string{"diffserver"},
+			flagCanaries:        []string{},
+			expectedDeployments: []goldpushk.GoldServiceDeployment{chromeDiffServer, skiaDiffServer, skiaPublicDiffServer},
+		},
+
+		{
+			message:                     "All instances, single service, one canary",
+			flagInstances:               []string{"all"},
+			flagServices:                []string{"diffserver"},
+			flagCanaries:                []string{"skia-public:diffserver"},
+			expectedDeployments:         []goldpushk.GoldServiceDeployment{chromeDiffServer, skiaDiffServer},
+			expectedCanariedDeployments: []goldpushk.GoldServiceDeployment{skiaPublicDiffServer},
+		},
+
+		{
+			message:                     "All instances, single service, multiple canaries",
+			flagInstances:               []string{"all"},
+			flagServices:                []string{"diffserver"},
+			flagCanaries:                []string{"skia:diffserver", "skia-public:diffserver"},
+			expectedDeployments:         []goldpushk.GoldServiceDeployment{chromeDiffServer},
+			expectedCanariedDeployments: []goldpushk.GoldServiceDeployment{skiaDiffServer, skiaPublicDiffServer},
+		},
+
+		{
+			message:             "All instances, multiple services, no canary",
+			flagInstances:       []string{"all"},
+			flagServices:        []string{"diffserver", "skiacorrectness"},
+			flagCanaries:        []string{},
+			expectedDeployments: []goldpushk.GoldServiceDeployment{chromeDiffServer, chromeSkiaCorrectness, skiaDiffServer, skiaSkiaCorrectness, skiaPublicDiffServer},
+		},
+
+		{
+			message:                     "All instances, multiple services, one canary",
+			flagInstances:               []string{"all"},
+			flagServices:                []string{"diffserver", "skiacorrectness"},
+			flagCanaries:                []string{"skia-public:diffserver"},
+			expectedDeployments:         []goldpushk.GoldServiceDeployment{chromeDiffServer, chromeSkiaCorrectness, skiaDiffServer, skiaSkiaCorrectness},
+			expectedCanariedDeployments: []goldpushk.GoldServiceDeployment{skiaPublicDiffServer},
+		},
+
+		{
+			message:                     "All instances, multiple services, multiple canaries",
+			flagInstances:               []string{"all"},
+			flagServices:                []string{"diffserver", "skiacorrectness"},
+			flagCanaries:                []string{"skia:skiacorrectness", "skia-public:diffserver"},
+			expectedDeployments:         []goldpushk.GoldServiceDeployment{chromeDiffServer, chromeSkiaCorrectness, skiaDiffServer},
+			expectedCanariedDeployments: []goldpushk.GoldServiceDeployment{skiaSkiaCorrectness, skiaPublicDiffServer},
+		},
+
+		////////////////////////////////////////////////////////////////////////////
+		// Wildcards: --instance all --services all                               //
+		////////////////////////////////////////////////////////////////////////////
+
+		{
+			message:             "All instances, all services, no canary",
+			flagInstances:       []string{"all"},
+			flagServices:        []string{"all"},
+			flagCanaries:        []string{},
+			expectedDeployments: []goldpushk.GoldServiceDeployment{chromeBaselineServer, chromeDiffServer, chromeSkiaCorrectness, skiaDiffServer, skiaSkiaCorrectness, skiaPublicDiffServer},
+		},
+
+		{
+			message:                     "All instances, all services, one canary",
+			flagInstances:               []string{"all"},
+			flagServices:                []string{"all"},
+			flagCanaries:                []string{"skia-public:diffserver"},
+			expectedDeployments:         []goldpushk.GoldServiceDeployment{chromeBaselineServer, chromeDiffServer, chromeSkiaCorrectness, skiaDiffServer, skiaSkiaCorrectness},
+			expectedCanariedDeployments: []goldpushk.GoldServiceDeployment{skiaPublicDiffServer},
+		},
+
+		{
+			message:                     "All instances, all services, multiple canaries",
+			flagInstances:               []string{"all"},
+			flagServices:                []string{"all"},
+			flagCanaries:                []string{"skia:skiacorrectness", "skia-public:diffserver"},
+			expectedDeployments:         []goldpushk.GoldServiceDeployment{chromeBaselineServer, chromeDiffServer, chromeSkiaCorrectness, skiaDiffServer},
+			expectedCanariedDeployments: []goldpushk.GoldServiceDeployment{skiaSkiaCorrectness, skiaPublicDiffServer},
+		},
+
+		////////////////////////////////////////////////////////////////////////////
+		// Miscellaneous                                                          //
+		////////////////////////////////////////////////////////////////////////////
+
+		{
+			message:                     "Repeated inputs are ignored",
+			flagInstances:               []string{"chrome", "chrome", "skia", "chrome", "skia", "skia-public", "skia-public"},
+			flagServices:                []string{"diffserver", "skiacorrectness", "diffserver", "skiacorrectness"},
+			flagCanaries:                []string{"skia:diffserver", "skia-public:diffserver", "skia:diffserver", "skia-public:diffserver"},
+			expectedDeployments:         []goldpushk.GoldServiceDeployment{chromeDiffServer, chromeSkiaCorrectness, skiaSkiaCorrectness},
+			expectedCanariedDeployments: []goldpushk.GoldServiceDeployment{skiaDiffServer, skiaPublicDiffServer},
+		},
+
+		{
+			message:                     "Outputs sorted by instance, then service",
+			flagInstances:               []string{"skia-public", "chrome", "skia"},
+			flagServices:                []string{"skiacorrectness", "diffserver"},
+			flagCanaries:                []string{"skia-public:diffserver", "skia:diffserver"},
+			expectedDeployments:         []goldpushk.GoldServiceDeployment{chromeDiffServer, chromeSkiaCorrectness, skiaSkiaCorrectness},
+			expectedCanariedDeployments: []goldpushk.GoldServiceDeployment{skiaDiffServer, skiaPublicDiffServer},
+		},
+	}
+
+	for _, tc := range testCases {
+		deployments, canariedDeployments, err := parseAndValidateFlags(servicesMap, tc.flagInstances, tc.flagServices, tc.flagCanaries)
+
+		if tc.hasError {
+			assert.Nil(t, deployments, tc.message)
+			assert.Nil(t, canariedDeployments, tc.message)
+			assert.Error(t, err, tc.message)
+			assert.Contains(t, err.Error(), tc.errorMsg, tc.message)
+		} else {
+			assert.NoError(t, err, tc.message)
+			assert.Equal(t, tc.expectedDeployments, deployments, tc.message)
+			assert.Equal(t, tc.expectedCanariedDeployments, canariedDeployments, tc.message)
+		}
+	}
+}
