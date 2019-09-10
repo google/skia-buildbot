d605885e32012a0a401ecb5d7128f4dcdd9d5fd8
Ravi Mistry <rmistry@google.com>
2019-09-10 07:46:44 -0400

    [CT] Remove everything to do with UpdateVars
    Much simpler to set fields directly on Task and update the Datastore. Deletes a lot of code.
    
    Bug: skia:9405
    Change-Id: I7c6d75e12e54b1378f0fd5e9df55810a67f2d502

--- ct/go/ctfe/lua_scripts/lua_scripts.go
+++ ct/go/ctfe/lua_scripts/lua_scripts.go
@@ -85,10 +85,6 @@ func (task DatastoreTask) GetPopulatedAddTaskVars() (task_common.AddTaskVars, er
 	return taskVars, nil
 }
 
-func (task DatastoreTask) GetUpdateTaskVars() task_common.UpdateTaskVars {
-	return &UpdateVars{}
-}
-
 func (task DatastoreTask) RunsOnGCEWorkers() bool {
 	return true
 }
@@ -150,7 +146,7 @@ func (task DatastoreTask) TriggerSwarmingTaskAndMail(ctx context.Context) error 
 		return fmt.Errorf("Could not trigger master script for run_lua_on_workers with isolate args %v: %s", isolateArgs, err)
 	}
 	// Mark task as started in datastore.
-	if err := task_common.UpdateTaskSetStarted(ctx, &UpdateVars{}, task.DatastoreKey.ID, runID, sTaskID); err != nil {
+	if err := task_common.UpdateTaskSetStarted(ctx, runID, sTaskID, &task); err != nil {
 		return fmt.Errorf("Could not mark task as started in datastore: %s", err)
 	}
 	// Send start email.
@@ -202,6 +198,19 @@ func (task DatastoreTask) SendCompletionEmail(ctx context.Context, completedSucc
 	return nil
 }
 
+func (task *DatastoreTask) SetCompleted(success bool) {
+	if success {
+		runID := task_common.GetRunID(task)
+		task.ScriptOutput = ctutil.GetLuaOutputRemoteLink(runID)
+		if task.LuaAggregatorScript != "" {
+			task.AggregatedOutput = ctutil.GetLuaAggregatorOutputRemoteLink(runID)
+		}
+	}
+	task.TsCompleted = ctutil.GetCurrentTsInt64()
+	task.Failure = !success
+	task.TaskDone = true
+}
+
 func addTaskView(w http.ResponseWriter, r *http.Request) {
 	ctfeutil.ExecuteSimpleTemplate(addTaskTemplate, w, r)
 }
@@ -249,40 +258,6 @@ func addTaskHandler(w http.ResponseWriter, r *http.Request) {
 
 func getTasksHandler(w http.ResponseWriter, r *http.Request) {
 	task_common.GetTasksHandler(&DatastoreTask{}, w, r)
-}
-
-type UpdateVars struct {
-	task_common.UpdateTaskCommonVars
-	ScriptOutput     string
-	AggregatedOutput string
-}
-
-func (task *UpdateVars) GetTaskPrototype() task_common.Task {
-	return &DatastoreTask{}
-}
-
-func (vars *UpdateVars) UpdateExtraFields(t task_common.Task) error {
-	task := t.(*DatastoreTask)
-	if vars.ScriptOutput != "" {
-		task.ScriptOutput = vars.ScriptOutput
-	}
-	if vars.AggregatedOutput != "" {
-		task.AggregatedOutput = vars.AggregatedOutput
-	}
-	return nil
-}
-
-func (vars *UpdateVars) SetCompleted(success bool, t task_common.Task) {
-	if success {
-		runID := task_common.GetRunID(t)
-		vars.ScriptOutput = ctutil.GetLuaOutputRemoteLink(runID)
-		if t.(*DatastoreTask).LuaAggregatorScript != "" {
-			vars.AggregatedOutput = ctutil.GetLuaAggregatorOutputRemoteLink(runID)
-		}
-	}
-	vars.TsCompleted = ctutil.GetCurrentTs()
-	vars.Failure = !success
-	vars.TaskDone = true
 }
 
 func deleteTaskHandler(w http.ResponseWriter, r *http.Request) {
