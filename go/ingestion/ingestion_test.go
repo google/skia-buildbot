5e962c996cbc5e3112f0805b11b95a452419111a
Kevin Lubick <kjlubick@google.com>
2019-09-13 15:31:40 -0400

    [gold] Fix tryjob ingestion polling
    I recently removed NCommits from ingestion polling and overlooked this.
    
    Change-Id: Icf5f0aae71c03c2df434f62483e9917c2877da86
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/241396
    Commit-Queue: Kevin Lubick <kjlubick@google.com>
    Reviewed-by: Eric Boren <borenet@google.com>

--- go/ingestion/ingestion_test.go
+++ go/ingestion/ingestion_test.go
@@ -27,10 +27,6 @@ import (
 const (
 	RFLOCATION_CONTENT = "result file content"
 
-	PROJECT_ID      = "test-project-ingestion"
-	TEST_TOPIC      = "test-topic-ingestion-testing"
-	TEST_SUBSCRIBER = "test-subscriber-ingestion-testing"
-
 	TEST_BUCKET_ID = "test-bucket"
 )
 
@@ -142,11 +138,10 @@ func TestGetStartTimeOfInterestDays(t *testing.T) {
 
 	i, err := NewIngester("test-ingester-1", conf, mvs, nil, nil, mis, meb)
 	assert.NoError(t, err)
-	defer testutils.AssertCloses(t, i)
 
 	ts, err := i.getStartTimeOfInterest(context.Background(), now)
 	assert.NoError(t, err)
-	assert.Equal(t, alphaTime.Unix(), ts)
+	assert.Equal(t, alphaTime, ts)
 }
 
 // TestGetStartTimeOfInterestCommits checks that we compute the time to start
@@ -188,11 +183,38 @@ func TestGetStartTimeOfInterestCommits(t *testing.T) {
 
 	i, err := NewIngester("test-ingester-2", conf, mvs, nil, nil, mis, meb)
 	assert.NoError(t, err)
-	defer testutils.AssertCloses(t, i)
 
 	ts, err := i.getStartTimeOfInterest(context.Background(), now)
 	assert.NoError(t, err)
-	assert.Equal(t, betaTime.Unix(), ts)
+	assert.Equal(t, betaTime, ts)
+}
+
+// TestGetStartTimeOfInterestTryJobs checks that we compute the time to start
+// polling for commits properly in the case of TryJobs, where NCommits is 0 and
+// the time is short enough that we haven't seen new commits in that time.
+func TestGetStartTimeOfInterestTryJobs(t *testing.T) {
+	unittest.SmallTest(t)
+	// We have to provide NewIngester non-nil eventbus and ingestionstore.
+	meb := &mockeventbus.EventBus{}
+	mis := &mocks.IngestionStore{}
+
+	defer meb.AssertExpectations(t)
+	defer mis.AssertExpectations(t)
+
+	// arbitrary date
+	now := time.Date(2019, 8, 5, 11, 20, 0, 0, time.UTC)
+	oneHourAgo := now.Add(-1 * time.Hour)
+
+	conf := &sharedconfig.IngesterConfig{
+		MinHours: 1,
+	}
+
+	i, err := NewIngester("test-ingester-1", conf, nil, nil, nil, mis, meb)
+	assert.NoError(t, err)
+
+	ts, err := i.getStartTimeOfInterest(context.Background(), now)
+	assert.NoError(t, err)
+	assert.Equal(t, oneHourAgo, ts)
 }
 
 // TestGetStartTimeOfInterestNotEnough makes sure we don't loop infinitely
@@ -230,11 +252,10 @@ func TestGetStartTimeOfInterestNotEnough(t *testing.T) {
 
 	i, err := NewIngester("test-ingester-3", conf, mvs, nil, nil, mis, meb)
 	assert.NoError(t, err)
-	defer testutils.AssertCloses(t, i)
 
 	ts, err := i.getStartTimeOfInterest(context.Background(), now)
 	assert.NoError(t, err)
-	assert.Equal(t, alphaTime.Unix(), ts)
+	assert.Equal(t, alphaTime, ts)
 }
 
 // TODO(kjlubick): replace these with mockery-based mocks
