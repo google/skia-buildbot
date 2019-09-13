b261d4e81fd2fcf42f256b55862ffa1386a486c8
Kevin Lubick <kjlubick@google.com>
2019-09-13 10:15:54 -0400

    [gold] Add new ChangeLists page
    Note to the reviewer - it's probably good to look at it in chunks
    broken up by PatchSet.
    
    PS1 adds gold-scaffold-sk, modeled after perf-scaffold-sk
    
    PS4 adds in the base of changelists-page-sk (renamed from
      changelists-page)
    PS 5 makes the demo page look nice and adds in the URL to
      the data returned by the frontend. Additionally, it adds
      a testing_offline attribute/property to login-sk, which was
      very handy tool when working on swarming UI.
    PS 6 and onwards adds tests for the new elements.
    
    Bug: skia:9340
    Change-Id: I810ed93f2843e85cc752c6a3479ae476b462a5d5

--- infra-sk/modules/login-sk/login-sk.js
+++ infra-sk/modules/login-sk/login-sk.js
@@ -7,6 +7,11 @@
  * current login status and provides login/logout links. Reports errors via
  * {@linkcode module:elements-sk/error-toast-sk}.
  * </p>
+ *
+ * @attr {boolean} testing_offline - If we should really poll for Login status
+ *  or use mock data.
+ *
+ * <p>
  */
 import { define } from 'elements-sk/define'
 import { errorMessage } from 'elements-sk/errorMessage';
@@ -15,16 +20,34 @@ import { Login } from '../login';
 define('login-sk', class extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `<span class=email>Loading...</span><a class=logInOut></a>`;
-    Login.then(status => {
-      this.querySelector('.email').textContent = status.Email;
-      let logInOut = this.querySelector('.logInOut');
-      if (!status.Email) {
-          logInOut.href = status.LoginURL;
-          logInOut.textContent = 'Login';
-      } else {
-          logInOut.href = 'https://skia.org/logout/?redirect=' + encodeURIComponent(document.location);
-          logInOut.textContent = 'Logout';
-      }
-    }).catch(errorMessage);
+    if (this.testingOffline) {
+      this.querySelector('.email').textContent = "test@example.com";
+      const logInOut = this.querySelector('.logInOut');
+      logInOut.href = 'https://skia.org/logout/?redirect=' + encodeURIComponent(document.location);
+      logInOut.textContent = 'Logout';
+    } else {
+      Login.then((status) => {
+        this.querySelector('.email').textContent = status.Email;
+        let logInOut = this.querySelector('.logInOut');
+        if (!status.Email) {
+            logInOut.href = status.LoginURL;
+            logInOut.textContent = 'Login';
+        } else {
+            logInOut.href = 'https://skia.org/logout/?redirect=' + encodeURIComponent(document.location);
+            logInOut.textContent = 'Logout';
+        }
+      }).catch(errorMessage);
+    }
+  }
+
+  /** @prop testingOffline {boolean} Reflects the testing_offline attribute for ease of use.
+   */
+  get testingOffline() { return this.hasAttribute('testing_offline'); }
+  set testingOffline(val) {
+    if (val) {
+      this.setAttribute('testing_offline', '');
+    } else {
+      this.removeAttribute('testing_offline');
+    }
   }
 });
