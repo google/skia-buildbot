83a30d49f2f02c22b0a57d482f47e6b527859627
Kevin Lubick <kjlubick@google.com>
2019-09-15 14:57:10 -0400

    [gold] Add pagination to changelists-page
    Bug: skia:9340
    Change-Id: Ia3558b7606331e0047160b2b41dbe71b8ffdd90d

--- golden/modules/demo_util.js
+++ golden/modules/demo_util.js
@@ -1,13 +1,17 @@
 // delay wraps the value toReturn in a Promise that will resolve
 // after "delay" milliseconds.
 export function delay(toReturn, delay=100) {
-  return new Promise((resolve) => {
-    setTimeout(resolve, delay);
-  }).then(() => {
-    return {
-      status: 200,
-      body: JSON.stringify(toReturn),
-      headers: {'content-type':'application/json'},
-    };
-  });
+  // we return a function that returns the promise so each call
+  // has a "fresh" promise and waits for the time.
+  return function() {
+    return new Promise((resolve) => {
+      setTimeout(resolve, delay);
+    }).then(() => {
+      return {
+        status: 200,
+        body: JSON.stringify(toReturn),
+        headers: {'content-type':'application/json'},
+      };
+    });
+  };
 }
