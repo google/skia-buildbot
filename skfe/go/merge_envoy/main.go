f9d30793ccaa50b466760165baa0e482d6887760
Joe Gregorio <jcgregorio@google.com>
2019-09-23 14:30:05 -0400

    Move to envoy
    The simple.json and computed.json are generated files included here only for review.
    
    Has already been pushed, and confirmed to work (jsdoc doesn't have an entry in skia-ingress.yaml):
    
    $ curl --include  --resolve 'jsdoc.skia.org:443:35.201.76.220'  https://jsdoc.skia.org
    HTTP/2 200
    accept-ranges: bytes
    cache-control: max-age=300
    content-length: 4187
    content-type: text/html; charset=utf-8
    last-modified: Fri, 20 Sep 2019 13:01:24 GMT
    date: Mon, 23 Sep 2019 16:15:49 GMT
    x-envoy-upstream-service-time: 1
    server: envoy
    via: 1.1 google
    alt-svc: clear
    
    <!DOCTYPE html>
    ...
    
    
    
    
    DO NOT SUBMIT until simple.json and computed.json are removed.
    
    Bug: skia:9407
    Change-Id: I3352e691cd90cd4410a2ef5634d2abb9de949a4c

---
+++ skfe/go/merge_envoy/main.go
@@ -0,0 +1,45 @@
+// merge_envoy takes two Envoy config files, adds all the Virtual Hosts and
+// Clusters from the second file to the first file and then serializes the
+// results of the merge as JSON to stdout.
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/Jeffail/gabs/v2"
+	"go.skia.org/infra/go/sklog"
+)
+
+func main() {
+	filenames := os.Args[1:]
+	if len(filenames) != 2 {
+		sklog.Fatalf("Usage: merge_envoy file1 file2")
+	}
+	file1, err := gabs.ParseJSONFile(filenames[0])
+	if err != nil {
+		sklog.Fatal(err)
+	}
+	file2, err := gabs.ParseJSONFile(filenames[1])
+	if err != nil {
+		sklog.Fatal(err)
+	}
+
+	// Merge Clusters.
+	clusterPath := []string{"static_resources", "clusters"}
+	for _, cluster := range file2.Search(clusterPath...).Children() {
+		if err := file1.ArrayAppend(cluster.Data(), clusterPath...); err != nil {
+			sklog.Fatal(err)
+		}
+	}
+
+	// Merge Virtual Hosts.
+	hostsPath := []string{"static_resources", "listeners", "0", "filter_chains", "filters", "0", "config", "route_config", "virtual_hosts"}
+	for _, cluster := range file2.Search(hostsPath...).Children() {
+		if err := file1.ArrayAppend(cluster.Data(), hostsPath...); err != nil {
+			sklog.Fatal(err)
+		}
+	}
+
+	fmt.Print(file1.StringIndent("", "  "))
+}
