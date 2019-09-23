93bfa81df8d47c87a10a369f33f3c6a3339a0996
Joe Gregorio <jcgregorio@google.com>
2019-09-23 14:45:53 -0400

    Move to envoy
    The simple.json and computed.json are generated files included here only for review.
    
    Has already been pushed, and confirmed to work (jsdoc doesn't have an entry in skia-ingress.yaml):
    
    $ curl --include  --resolve 'jsdoc.skia.org:443:35.201.76.220'  https://jsdoc.skia.org
    HTTP/2 200
    accept-ranges: bytes
    cache-control: max-age=300
    content-length: 4187
    content-type: text/html; charset=utf-8
    last-modified: Fri, 20 Sep 2019 13:01:24 GMT
    date: Mon, 23 Sep 2019 16:15:49 GMT
    x-envoy-upstream-service-time: 1
    server: envoy
    via: 1.1 google
    alt-svc: clear
    
    <!DOCTYPE html>
    ...
    
    
    Bug: skia:9407
    Change-Id: I3352e691cd90cd4410a2ef5634d2abb9de949a4c
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/242619
    Commit-Queue: Joe Gregorio <jcgregorio@google.com>
    Reviewed-by: Ravi Mistry <rmistry@google.com>
    Reviewed-by: Kevin Lubick <kjlubick@google.com>
    Reviewed-by: Ben Wagner aka dogben <benjaminwagner@google.com>
    Reviewed-by: Eric Boren <borenet@google.com>

---
+++ skfe/go/update_probers/main.go
@@ -0,0 +1,82 @@
+// update_probers is a command line application that updates the
+// "envoy-redirects" section of probersk.json5 with all the redirects in
+// envoy-starter.json.
+//
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"sort"
+
+	"github.com/Jeffail/gabs/v2"
+	"go.skia.org/infra/go/sklog"
+)
+
+const (
+	probersFilename = "probersk.json5"
+
+	envoyFilename = "envoy-starter.json"
+)
+
+var (
+	proberRedirectURLsPath = []string{"envoy-redirects", "urls"}
+)
+
+func main() {
+	// Load the existing probers file.
+	probers, err := gabs.ParseJSONFile(probersFilename)
+	if err != nil {
+		sklog.Fatal(err)
+	}
+
+	// Zero our the urls array.
+	err = probers.Delete(proberRedirectURLsPath...)
+	if err != nil {
+		sklog.Fatal(err)
+	}
+	_, err = probers.Array(proberRedirectURLsPath...)
+	if err != nil {
+		sklog.Fatal(err)
+	}
+
+	// Load the envoy config file.
+	redirects, err := gabs.ParseJSONFile(envoyFilename)
+	if err != nil {
+		sklog.Fatal(err)
+	}
+
+	// Find the domain names of all redirects in the envoy file.
+	hostsPath := []string{"static_resources", "listeners", "0", "filter_chains", "filters", "0", "config", "route_config", "virtual_hosts"}
+	domains := []string{}
+	for _, cluster := range redirects.Search(hostsPath...).Children() {
+		domain := cluster.Search("domains").Data().(string)
+		// Is there a routes redirect?
+		redirect := false
+		for _, route := range cluster.Search("routes").Children() {
+			if route.Exists("redirect") {
+				redirect = true
+				break
+			}
+		}
+
+		if redirect && domain != "*" {
+			domains = append(domains, fmt.Sprintf("https://%s", domain))
+		}
+
+	}
+
+	// Add the domains to the probers file.
+	sort.Strings(domains)
+	for _, domain := range domains {
+
+		if err := probers.ArrayAppend(domain, proberRedirectURLsPath...); err != nil {
+			sklog.Fatal(err)
+		}
+	}
+
+	// Rewrite the probers file.
+	if err := ioutil.WriteFile(probersFilename, []byte(probers.StringIndent("", "  ")), 0644); err != nil {
+		sklog.Fatal(err)
+	}
+}
