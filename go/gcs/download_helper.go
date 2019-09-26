208159264aa6b7462bbb07172448b045fdc8a495
Eric Boren <borenet@google.com>
2019-09-26 07:08:12 -0400

    Remove some instances of util.Close(io.Writer)
    Change-Id: I4938b1f4b596bf77a809a1b2862a0d88920e2a41

--- go/gcs/download_helper.go
+++ go/gcs/download_helper.go
@@ -11,8 +11,8 @@ import (
 	"runtime"
 
 	"cloud.google.com/go/storage"
+	"go.skia.org/infra/go/skerr"
 	"go.skia.org/infra/go/sklog"
-	"go.skia.org/infra/go/util"
 )
 
 // DownloadHelper provides convenience methods for downloading binaries by SHA1
@@ -35,57 +35,74 @@ func NewDownloadHelper(s *storage.Client, gsBucket, gsSubdir, workdir string) *D
 }
 
 // Download downloads the given binary from Google Storage.
-func (d *DownloadHelper) Download(name, hash string) error {
+func (d *DownloadHelper) Download(name, hash string) (err error) {
 	sklog.Infof("Downloading new binary for %s...", name)
 	filepath := path.Join(d.workdir, name)
 	object := hash
 	if d.subdir != "" {
 		object = d.subdir + "/" + object
 	}
-	resp, err := d.s.Bucket(d.bucket).Object(object).NewReader(context.Background())
+	var resp io.Reader
+	resp, err = d.s.Bucket(d.bucket).Object(object).NewReader(context.Background())
 	if err != nil {
-		return fmt.Errorf("Download helper can't get reader for %s: %s", name, err)
+		return skerr.Fmt("Download helper can't get reader for %s: %s", name, err)
 	}
-	f, err := os.Create(filepath)
+	var f *os.File
+	f, err = os.Create(filepath)
 	if err != nil {
-		return fmt.Errorf("Download helper cannot create filepath %s: %s", filepath, err)
+		return skerr.Fmt("Download helper cannot create filepath %s: %s", filepath, err)
 	}
-	defer util.Close(f)
-	if _, err := io.Copy(f, resp); err != nil {
-		return fmt.Errorf("Download helper can't download %s: %s", name, err)
+	defer func() {
+		err2 := f.Close()
+		if err == nil && err2 != nil {
+			err = skerr.Fmt("Failed to close the file with: %s", err2)
+		}
+	}()
+	_, err = io.Copy(f, resp)
+	if err != nil {
+		return skerr.Fmt("Download helper can't download %s: %s", name, err)
 	}
 	if runtime.GOOS != "windows" {
-		if err := f.Chmod(0755); err != nil {
-			return err
+		err = f.Chmod(0755)
+		if err != nil {
+			return skerr.Wrap(err)
 		}
 	}
 	return nil
 }
 
 // MaybeDownload downloads the given binary from Google Storage if necessary.
-func (d *DownloadHelper) MaybeDownload(name, hash string) error {
+func (d *DownloadHelper) MaybeDownload(name, hash string) (err error) {
 	filepath := path.Join(d.workdir, name)
-	f, err := os.Open(filepath)
+	var f *os.File
+	f, err = os.Open(filepath)
 	if err != nil {
 		if os.IsNotExist(err) {
 			return d.Download(name, hash)
 		} else {
-			return fmt.Errorf("Failed to open %s: %s", filepath, err)
+			return skerr.Fmt("Failed to open %s: %s", filepath, err)
 		}
 	}
-	defer util.Close(f)
-	info, err := f.Stat()
+	defer func() {
+		err2 := f.Close()
+		if err == nil && err2 != nil {
+			err = skerr.Fmt("Failed to close the file with: %s", err2)
+		}
+	}()
+	var info os.FileInfo
+	info, err = f.Stat()
 	if err != nil {
-		return fmt.Errorf("Failed to stat %s: %s", filepath, err)
+		return skerr.Fmt("Failed to stat %s: %s", filepath, err)
 	}
 	if info.Mode() != 0755 {
 		sklog.Infof("Binary %s is not executable.", filepath)
 		return d.Download(name, hash)
 	}
 
-	contents, err := ioutil.ReadAll(f)
+	var contents []byte
+	contents, err = ioutil.ReadAll(f)
 	if err != nil {
-		return fmt.Errorf("Failed to read %s: %s", filepath, err)
+		return skerr.Fmt("Failed to read %s: %s", filepath, err)
 	}
 	sha1sum := sha1.Sum(contents)
 	sha1str := fmt.Sprintf("%x", sha1sum)
