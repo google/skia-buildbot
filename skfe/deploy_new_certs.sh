d61010426726d1eeac1ceaf32237b7bb5fdb07da
Joe Gregorio <jcgregorio@google.com>
2019-10-02 14:44:48 -0400

    SKFE - Add script to automate deploying new certs.
    Sample run:
    
    $ ./deploy_new_certs.sh
    Cloning into 'skia-public-config'...
    remote: Sending approximately 10.95 MiB ...
    remote: Total 53687 (delta 42488), reused 53687 (delta 42488)
    Receiving objects: 100% (53687/53687), 10.95 MiB | 27.08 MiB/s, done.
    Resolving deltas: 100% (42488/42488), done.
    
    
    Confirm that the change to skia-ingress.yaml makes sense:
    
    diff --git a/skia-ingress.yaml b/skia-ingress.yaml
    index 435e91a6..e846c363 100644
    --- a/skia-ingress.yaml
    +++ b/skia-ingress.yaml
    @@ -4,7 +4,7 @@ metadata:
       name: skia-ingress
       annotations:
         kubernetes.io/ingress.global-static-ip-name: skia-org
    -    ingress.gcp.kubernetes.io/pre-shared-cert: skia-org-8
    +    ingress.gcp.kubernetes.io/pre-shared-cert: skia-org-2019-10-02
     spec:
       backend:
         serviceName: envoy-skia-org
    
    
    Then apply the modified yaml file:
    
      kubectl apply -f ./skia-public-config/skia-ingress.yaml
    
    And commit and push the updated config file.
    
      cd skia-public-config; git add --all; git commit -m 'Update skia.org certs on 2019-10-02'; git push
    
    Also remove unused certs:
    
       gcloud compute ssl-certificates delete --project=skia-public skia-org
       gcloud compute ssl-certificates delete --project=skia-public skia-org-4
       gcloud compute ssl-certificates delete --project=skia-public skia-org-5
       gcloud compute ssl-certificates delete --project=skia-public skia-org-6
       gcloud compute ssl-certificates delete --project=skia-public skia-org-8
       gcloud compute ssl-certificates delete --project=skia-public skiatest
       gcloud compute ssl-certificates delete --project=skia-public skiatest-cert
    
    Change-Id: I3706f625204de83f46be75a74a2c22ef9e5f159d

---
+++ skfe/deploy_new_certs.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# A script to automate some parts of new certificate deployment.
+#
+# See http://go/skia-ssl-cert for a longer description and the command you need
+# to run before running this script.
+#
+# This script adds a new cert to gcloud, and makes a local change to the
+# skia-ingress.yaml file. Applying the change to kubernetes, pushing the change
+# in git, and deleting old certs are left as manual steps to allow for manual
+# inspection of the change.
+
+DATE=$(date +%Y-%m-%d)
+
+gcloud compute ssl-certificates create skia-org-$DATE \
+    --private-key=$HOME/ssl-cert-requests/WILDCARD.skia.org-$DATE/WILDCARD.skia.org.key \
+    --certificate=$HOME/ssl-cert-requests/WILDCARD.skia.org-$DATE/WILDCARD.skia.org.chained.pem \
+    --project=skia-public
+
+git clone https://skia.googlesource.com/skia-public-config
+
+sed --in-place s#ingress.gcp.kubernetes.io/pre-shared-cert:.*#ingress.gcp.kubernetes.io/pre-shared-cert:\ skia-org-$DATE# ./skia-public-config/skia-ingress.yaml
+
+printf "\n\nConfirm that the change to skia-ingress.yaml makes sense:\n\n"
+
+cd skia-public-config; git diff
+
+printf "\n\nThen apply the modified yaml file after checking that you are working in skia-public:\n\n"
+printf "  kubectl apply -f ./skia-public-config/skia-ingress.yaml\n\n"
+printf "And commit and push the updated config file.\n\n"
+printf "  cd skia-public-config; git add --all; git commit -m 'Update skia.org certs on $DATE'; git push\n\n"
+
+printf "Also remove unused certs: \n\n"
+gcloud compute ssl-certificates list --project=skia-public --format=json | jq '.[].name' | grep --invert-match skia-org-$DATE | xargs -L1 echo "  " gcloud compute ssl-certificates delete --project=skia-public
