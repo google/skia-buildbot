6e01d099a825c98af23ec2fb92f41d0a3ecbc55d
Leandro Lovisolo <lovisolo@google.com>
2019-10-03 16:07:14 -0400

    [gold] MemDiffStore: Remove unused field MemDiffStore.baseDir (and ctor parameter to set it).
    Change-Id: I7d027e3e76f5dd825cb320632cf36ab64ca036d9
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/246008
    Commit-Queue: Leandro Lovisolo <lovisolo@google.com>
    Auto-Submit: Leandro Lovisolo <lovisolo@google.com>
    Reviewed-by: Kevin Lubick <kjlubick@google.com>

--- golden/go/diffstore/net_diffstore_test.go
+++ golden/go/diffstore/net_diffstore_test.go
@@ -36,7 +36,7 @@ func TestNetDiffStore(t *testing.T) {
 	mfs := &mocks.FailureStore{}
 	mStore, err := bolt_metricsstore.New(baseDir, m)
 	assert.NoError(t, err)
-	memDiffStore, err := NewMemDiffStore(gcsClient, baseDir, d_utils.TEST_GCS_IMAGE_DIR, 10, m, mStore, mfs)
+	memDiffStore, err := NewMemDiffStore(gcsClient, d_utils.TEST_GCS_IMAGE_DIR, 10, m, mStore, mfs)
 	assert.NoError(t, err)
 
 	// Start the server that wraps around the MemDiffStore.
