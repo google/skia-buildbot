ba6cbb0b6e59a0735f402c69404882998e84315e
Leandro Lovisolo <lovisolo@google.com>
2019-10-07 15:35:09 -0400

    [gold] bolt_metricsstore_test.go: Test PurgeDiffMetrics().
    Motivation: to have a more complete test harness in preparation for some changes in the MetricsStore interface and the upcoming Firestore-backed MetricsStore implementation.
    
    Change-Id: Iff552252fd7b6fc13bf638347521b8b39408994c
    
    Bug: skia:9507
    Change-Id: Iff552252fd7b6fc13bf638347521b8b39408994c
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/246520
    Commit-Queue: Leandro Lovisolo <lovisolo@google.com>
    Reviewed-by: Kevin Lubick <kjlubick@google.com>
    Auto-Submit: Leandro Lovisolo <lovisolo@google.com>

--- golden/go/diffstore/metricsstore/bolt_metricsstore/bolt_metricsstore_test.go
+++ golden/go/diffstore/metricsstore/bolt_metricsstore/bolt_metricsstore_test.go
@@ -8,6 +8,7 @@ import (
 	"go.skia.org/infra/go/testutils/unittest"
 	"go.skia.org/infra/go/util"
 	d_utils "go.skia.org/infra/golden/go/diffstore/testutils"
+	"go.skia.org/infra/golden/go/types"
 )
 
 func TestAddGet(t *testing.T) {
@@ -35,4 +36,52 @@ func TestAddGet(t *testing.T) {
 	dm, err = ms.LoadDiffMetrics(id)
 	assert.NoError(t, err)
 	assert.Equal(t, expected, dm)
+}
+
+func TestPurge(t *testing.T) {
+	unittest.MediumTest(t)
+
+	w, cleanup := testutils.TempDir(t)
+	defer cleanup()
+
+	ms, err := New(w, util.JSONCodec(d_utils.DummyDiffMetrics{}))
+	assert.NoError(t, err)
+
+	// Purge non-existent digest.
+	err = ms.PurgeDiffMetrics(types.DigestSlice{"abc"})
+	assert.NoError(t, err)
+
+	// Add metrics.
+	leftId := types.Digest("abc")
+	rightId := types.Digest("def")
+	diffId := string(leftId + "-" + rightId)
+	expected := &d_utils.DummyDiffMetrics{
+		NumDiffPixels:     3,
+		PercentDiffPixels: 0.3,
+	}
+	assert.NoError(t, ms.SaveDiffMetrics(diffId, expected))
+
+	// Purging by coercing the diffId as a types.Digest does nothing.
+	err = ms.PurgeDiffMetrics(types.DigestSlice{types.Digest(diffId)})
+	assert.NoError(t, err)
+	dm, err := ms.LoadDiffMetrics(diffId)
+	assert.NoError(t, err)
+	assert.Equal(t, expected, dm)
+
+	// Purging by leftId works.
+	err = ms.PurgeDiffMetrics(types.DigestSlice{leftId})
+	assert.NoError(t, err)
+	dm, err = ms.LoadDiffMetrics(diffId)
+	assert.NoError(t, err)
+	assert.Nil(t, dm)
+
+	// Re-add metric.
+	assert.NoError(t, ms.SaveDiffMetrics(diffId, expected))
+
+	// Purging by rightId works.
+	err = ms.PurgeDiffMetrics(types.DigestSlice{rightId})
+	assert.NoError(t, err)
+	dm, err = ms.LoadDiffMetrics(diffId)
+	assert.NoError(t, err)
+	assert.Nil(t, dm)
 }
