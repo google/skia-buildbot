cec15607b7254366b06997bed767d40b1ca08cdf
Joe Gregorio <jcgregorio@google.com>
2019-10-07 12:15:49 -0400

    Perf - Fix dfbuilder.PreflightQuery.
    The second tile may actually have less data than the most recent tile, so
    we query both the latest tile and the tile before that and use the max results
    from either tile.
    
    Bug: skia:9510
    Change-Id: I7c4d7f0fa85743bf27f585c355d2d4754d39fdea

--- go/paramtools/params_test.go
+++ go/paramtools/params_test.go
@@ -476,3 +476,43 @@ func TestOrderedParamSet(t *testing.T) {
 	_, err = p.DecodeParamsFromString(",-1=0,")
 	assert.Error(t, err, "Should fail since -1 isn't found.")
 }
+
+func TestParamSet_Size(t *testing.T) {
+	tests := []struct {
+		name string
+		p    ParamSet
+		want int
+	}{
+		{
+			name: "nil",
+			p:    nil,
+			want: 0,
+		},
+
+		{
+			name: "empty",
+			p:    ParamSet{},
+			want: 0,
+		},
+		{
+			name: "simple",
+			p:    ParamSet{"foo": []string{"bar", "baz"}},
+			want: 2,
+		},
+		{
+			name: "2 values",
+			p: ParamSet{
+				"foo": []string{"bar", "baz"},
+				"bar": []string{"baz"},
+			},
+			want: 3,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.p.Size(); got != tt.want {
+				t.Errorf("ParamSet.Size() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
