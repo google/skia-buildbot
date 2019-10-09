97697668ed8ce7b6ffb6d89ac19cb7922b61c97a
Ben Wagner <benjaminwagner@google.com>
2019-10-09 15:49:09 -0400

    Remove metrics and alerting for Swarming bot reboot_required
    Also remove unused Windows Skolo version metric.
    
    Change-Id: I7146707ea2e7797fcc2b7bc3d4363e36842c7539

--- datahopper/go/swarming_metrics/bots.go
+++ datahopper/go/swarming_metrics/bots.go
@@ -15,12 +15,10 @@ import (
 )
 
 const (
-	MEASUREMENT_SWARM_BOTS_LAST_SEEN           = "swarming_bots_last_seen"
-	MEASUREMENT_SWARM_BOTS_QUARANTINED         = "swarming_bots_quarantined"
-	MEASUREMENT_SWARM_BOTS_LAST_TASK           = "swarming_bots_last_task"
-	MEASUREMENT_SWARM_BOTS_DEVICE_TEMP         = "swarming_bots_device_temp"
-	MEASUREMENT_SWARM_BOTS_REBOOT_REQUIRED     = "swarming_bots_reboot_required"
-	MEASUREMENT_WINDOWS_SKOLO_OS_VERSION_COUNT = "windows_skolo_os_version_count"
+	MEASUREMENT_SWARM_BOTS_LAST_SEEN   = "swarming_bots_last_seen"
+	MEASUREMENT_SWARM_BOTS_QUARANTINED = "swarming_bots_quarantined"
+	MEASUREMENT_SWARM_BOTS_LAST_TASK   = "swarming_bots_last_task"
+	MEASUREMENT_SWARM_BOTS_DEVICE_TEMP = "swarming_bots_device_temp"
 )
 
 var batteryBlacklist = []*regexp.Regexp{
@@ -56,10 +54,6 @@ func reportBotMetrics(now time.Time, client swarming.ApiClient, metricsClient me
 		return nil, fmt.Errorf("Could not get list of bots for pool %s: %s", pool, err)
 	}
 	sklog.Infof("  Found %d bots in pool %s", len(bots), pool)
-
-	// Keep track of the unique "os" dimensions for Windows Skolo bots. (Currently we expect only one
-	// version of Windows running in Skolo.)
-	var windowsSkoloOSDimensions []util.StringSet
 
 	newMetrics := []metrics2.Int64Metric{}
 	for _, bot := range bots {
@@ -149,14 +143,6 @@ func reportBotMetrics(now time.Time, client swarming.ApiClient, metricsClient me
 				continue
 			}
 
-			m5 := metricsClient.GetInt64Metric(MEASUREMENT_SWARM_BOTS_REBOOT_REQUIRED, tags)
-			if st.RebootRequired {
-				m5.Update(1)
-			} else {
-				m5.Update(0)
-			}
-			newMetrics = append(newMetrics, m5)
-
 			for zone, temp := range st.BotTemperatureMap {
 				tags["temp_zone"] = zone
 				m4 := metricsClient.GetInt64Metric(MEASUREMENT_SWARM_BOTS_DEVICE_TEMP, tags)
@@ -206,41 +192,13 @@ func reportBotMetrics(now time.Time, client swarming.ApiClient, metricsClient me
 			}
 
 		}
-
-		// Check Windows OS version.
-		dimensions := swarming.BotDimensionsToStringMap(bot.Dimensions)
-		os := dimensions["os"]
-		zones := dimensions["zone"]
-		if util.In("us-skolo", zones) && util.In("Windows", os) {
-			osSet := util.NewStringSet(os)
-			found := false
-			for _, otherSet := range windowsSkoloOSDimensions {
-				if otherSet.Equals(osSet) {
-					found = true
-					break
-				}
-			}
-			if !found {
-				windowsSkoloOSDimensions = append(windowsSkoloOSDimensions, osSet)
-			}
-		}
 	}
-
-	windowsSkoloOSVersionCountTags := map[string]string{
-		"pool":     pool,
-		"swarming": server,
-	}
-	m := metricsClient.GetInt64Metric(MEASUREMENT_WINDOWS_SKOLO_OS_VERSION_COUNT, windowsSkoloOSVersionCountTags)
-	m.Update(int64(len(windowsSkoloOSDimensions)))
-	newMetrics = append(newMetrics, m)
-
 	return newMetrics, nil
 }
 
 type botState struct {
 	BotTemperatureMap map[string]float32       `json:"temp"`
 	DeviceMap         map[string]androidDevice `json:"devices"`
-	RebootRequired    bool                     `json:"reboot_required,omitempty"`
 }
 
 type androidDevice struct {
--- go/bt/testutil/testutil.go
+++ go/bt/testutil/testutil.go
@@ -7,12 +7,14 @@ import (
 	assert "github.com/stretchr/testify/require"
 	"go.skia.org/infra/go/bt"
 	"go.skia.org/infra/go/sktest"
+	"go.skia.org/infra/go/testutils/unittest"
 )
 
 // SetupBigTable creates the given BigTable table and column families. Returns
 // the project and instance names which can be passed to tests which use
 // BigTable, and a cleanup function which should be deferred.
 func SetupBigTable(t sktest.TestingT, tableID string, colFamilies ...string) (string, string, func()) {
+	unittest.RequiresBigTableEmulator(t)
 	project := "test-project"
 	instance := fmt.Sprintf("test-instance-%s", uuid.New())
 	assert.NoError(t, bt.InitBigtable(project, instance, tableID, colFamilies))
