271f039b78c5f7a0e1a88e6021b497ce5d13c2ec
Eric Boren <borenet@google.com>
2019-10-15 08:57:13 -0400

    [task scheduler] Split frontend and backend
    Change-Id: I6fd32208d7125a531a0d48e55ba716c3547b2d5d

--- task_scheduler/go/task-scheduler-fe/main.go
+++ task_scheduler/go/task-scheduler-fe/main.go
@@ -7,8 +7,6 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
-	"os"
-	"path"
 	"path/filepath"
 	"runtime"
 	"time"
@@ -16,27 +14,19 @@ import (
 	"cloud.google.com/go/bigtable"
 	"cloud.google.com/go/datastore"
 	"cloud.google.com/go/pubsub"
-	"cloud.google.com/go/storage"
 	"github.com/gorilla/mux"
 	swarming_api "go.chromium.org/luci/common/api/swarming/swarming/v1"
 	"go.skia.org/infra/go/allowed"
 	"go.skia.org/infra/go/auth"
 	"go.skia.org/infra/go/cleanup"
 	"go.skia.org/infra/go/common"
-	"go.skia.org/infra/go/depot_tools"
-	"go.skia.org/infra/go/gcs/gcsclient"
 	"go.skia.org/infra/go/gerrit"
-	"go.skia.org/infra/go/git"
 	"go.skia.org/infra/go/git/repograph"
-	"go.skia.org/infra/go/gitauth"
 	"go.skia.org/infra/go/gitstore/bt_gitstore"
 	gs_pubsub "go.skia.org/infra/go/gitstore/pubsub"
 	"go.skia.org/infra/go/httputils"
-	"go.skia.org/infra/go/human"
-	"go.skia.org/infra/go/isolate"
 	"go.skia.org/infra/go/login"
 	"go.skia.org/infra/go/metrics2"
-	"go.skia.org/infra/go/periodic"
 	"go.skia.org/infra/go/skiaversion"
 	"go.skia.org/infra/go/sklog"
 	"go.skia.org/infra/go/swarming"
@@ -44,29 +34,19 @@ import (
 	"go.skia.org/infra/task_scheduler/go/blacklist"
 	"go.skia.org/infra/task_scheduler/go/db"
 	"go.skia.org/infra/task_scheduler/go/db/firestore"
-	"go.skia.org/infra/task_scheduler/go/scheduling"
-	"go.skia.org/infra/task_scheduler/go/testutils"
-	"go.skia.org/infra/task_scheduler/go/tryjobs"
+	"go.skia.org/infra/task_scheduler/go/task_cfg_cache"
 	"go.skia.org/infra/task_scheduler/go/types"
-	"golang.org/x/oauth2"
-	"google.golang.org/api/option"
 )
 
 const (
 	// APP_NAME is the name of this app.
-	APP_NAME = "task_scheduler"
-
-	PUBSUB_SUBSCRIBER_TASK_SCHEDULER          = "task-scheduler"
-	PUBSUB_SUBSCRIBER_TASK_SCHEDULER_INTERNAL = "task-scheduler-internal"
+	APP_NAME = "task-scheduler-fe"
 
 	// PubSub subscriber ID used for GitStore.
-	GITSTORE_SUBSCRIBER_ID = "task-scheduler"
+	GITSTORE_SUBSCRIBER_ID = APP_NAME
 )
 
 var (
-	// Task Scheduler instance.
-	ts *scheduling.TaskScheduler
-
 	// Task Scheduler database.
 	tsDb db.DBCloser
 
@@ -78,6 +58,9 @@ var (
 
 	// Swarming API client.
 	swarm swarming.ApiClient
+
+	// Task cfg cache.
+	taskCfgCache *task_cfg_cache.TaskCfgCache
 
 	// HTML templates.
 	blacklistTemplate   *template.Template = nil
@@ -93,27 +76,13 @@ var (
 	btProject         = flag.String("bigtable_project", "", "GCE project to use for BigTable.")
 	host              = flag.String("host", "localhost", "HTTP service host")
 	port              = flag.String("port", ":8000", "HTTP service port for the web server (e.g., ':8000')")
-	dbPort            = flag.String("db_port", ":8008", "HTTP service port for the database RPC server (e.g., ':8008')")
-	disableTryjobs    = flag.Bool("disable_try_jobs", false, "If set, no try jobs will be picked up.")
 	firestoreInstance = flag.String("firestore_instance", "", "Firestore instance to use, eg. \"production\"")
 	gitstoreTable     = flag.String("gitstore_bt_table", "git-repos2", "BigTable table used for GitStore.")
-	isolateServer     = flag.String("isolate_server", isolate.ISOLATE_SERVER_URL, "Which Isolate server to use.")
 	local             = flag.Bool("local", false, "Whether we're running on a dev machine vs in production.")
 	repoUrls          = common.NewMultiStringFlag("repo", nil, "Repositories for which to schedule tasks.")
-	recipesCfgFile    = flag.String("recipes_cfg", "", "Path to the recipes.cfg file.")
 	resourcesDir      = flag.String("resources_dir", "", "The directory to find templates, JS, and CSS files. If blank, assumes you're running inside a checkout and will attempt to find the resources relative to this source file.")
-	scoreDecay24Hr    = flag.Float64("scoreDecay24Hr", 0.9, "Task candidate scores are penalized using linear time decay. This is the desired value after 24 hours. Setting it to 1.0 causes commits not to be prioritized according to commit time.")
-	swarmingPools     = common.NewMultiStringFlag("pool", nil, "Which Swarming pools to use.")
 	swarmingServer    = flag.String("swarming_server", swarming.SWARMING_SERVER, "Which Swarming server to use.")
-	timePeriod        = flag.String("timeWindow", "4d", "Time period to use.")
-	tryJobBucket      = flag.String("tryjob_bucket", tryjobs.BUCKET_PRIMARY, "Which Buildbucket bucket to use for try jobs.")
-	commitWindow      = flag.Int("commitWindow", 10, "Minimum number of recent commits to keep in the timeWindow.")
-	diagnosticsBucket = flag.String("diagnostics_bucket", "skia-task-scheduler-diagnostics", "Name of Google Cloud Storage bucket to use for diagnostics data.")
-	workdir           = flag.String("workdir", "workdir", "Working directory to use.")
 	promPort          = flag.String("prom_port", ":20000", "Metrics service address (e.g., ':10110')")
-
-	pubsubTopicName      = flag.String("pubsub_topic", swarming.PUBSUB_TOPIC_SWARMING_TASKS, "Pub/Sub topic to use for Swarming tasks.")
-	pubsubSubscriberName = flag.String("pubsub_subscriber", PUBSUB_SUBSCRIBER_TASK_SCHEDULER, "Pub/Sub subscriber name.")
 )
 
 func reloadTemplates() {
@@ -168,15 +137,10 @@ func mainHandler(w http.ResponseWriter, r *http.Request) {
 	if *local {
 		reloadTemplates()
 	}
-	enc, err := json.Marshal(ts.Status())
-	if err != nil {
-		httputils.ReportError(w, err, "Failed to encode JSON.", http.StatusInternalServerError)
-		return
-	}
 	if err := mainTemplate.Execute(w, struct {
 		Data string
 	}{
-		Data: string(enc),
+		Data: "",
 	}); err != nil {
 		httputils.ReportError(w, err, "Failed to execute template.", http.StatusInternalServerError)
 		return
@@ -190,7 +154,7 @@ func blacklistHandler(w http.ResponseWriter, r *http.Request) {
 	if *local {
 		reloadTemplates()
 	}
-	rules := ts.GetBlacklist().GetRules()
+	rules := bl.GetRules()
 	enc, err := json.Marshal(&struct {
 		Rules []*blacklist.Rule `json:"rules"`
 	}{
@@ -235,7 +199,7 @@ func jsonBlacklistHandler(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 		defer util.Close(r.Body)
-		if err := ts.GetBlacklist().RemoveRule(msg.Id); err != nil {
+		if err := bl.RemoveRule(msg.Id); err != nil {
 			httputils.ReportError(w, err, fmt.Sprintf("Failed to delete blacklist rule: %s", err), http.StatusInternalServerError)
 			return
 		}
@@ -255,7 +219,7 @@ func jsonBlacklistHandler(w http.ResponseWriter, r *http.Request) {
 			}
 			rule = *rangeRule
 		}
-		if err := ts.GetBlacklist().AddRule(&rule, repos); err != nil {
+		if err := bl.AddRule(&rule, repos); err != nil {
 			httputils.ReportError(w, err, fmt.Sprintf("Failed to add blacklist rule: %s", err), http.StatusInternalServerError)
 			return
 		}
@@ -263,12 +227,27 @@ func jsonBlacklistHandler(w http.ResponseWriter, r *http.Request) {
 	resp := &struct {
 		Rules []*blacklist.Rule `json:"rules"`
 	}{
-		Rules: ts.GetBlacklist().GetRules(),
+		Rules: bl.GetRules(),
 	}
 	if err := json.NewEncoder(w).Encode(resp); err != nil {
 		httputils.ReportError(w, err, fmt.Sprintf("Failed to encode response: %s", err), http.StatusInternalServerError)
 		return
 	}
+}
+
+// makeJob creates a Job for the given repo, revision, and name.
+func makeJob(ctx context.Context, repo, revision, jobName string) (*types.Job, error) {
+	j, err := taskCfgCache.MakeJob(ctx, types.RepoState{
+		Repo:     repo,
+		Revision: revision,
+	}, jobName)
+	if err != nil {
+		return nil, err
+	}
+	j.Requested = j.Created
+	j.IsForce = true
+	sklog.Infof("Created manually-triggered Job %q", j.Id)
+	return j, nil
 }
 
 func jsonTriggerHandler(w http.ResponseWriter, r *http.Request) {
@@ -285,19 +264,27 @@ func jsonTriggerHandler(w http.ResponseWriter, r *http.Request) {
 		httputils.ReportError(w, err, fmt.Sprintf("Failed to decode request body: %s", err), http.StatusInternalServerError)
 		return
 	}
-	ids := make([]string, 0, len(msg))
+	jobs := make([]*types.Job, 0, len(msg))
 	for _, j := range msg {
 		_, repoName, _, err := repos.FindCommit(j.Commit)
 		if err != nil {
 			httputils.ReportError(w, err, "Unable to find the given commit in any repo.", http.StatusInternalServerError)
 			return
 		}
-		id, err := ts.TriggerJob(context.Background(), repoName, j.Commit, j.Name)
+		job, err := makeJob(r.Context(), repoName, j.Commit, j.Name)
 		if err != nil {
 			httputils.ReportError(w, err, "Failed to trigger jobs.", http.StatusInternalServerError)
 			return
 		}
-		ids = append(ids, id)
+		jobs = append(jobs, job)
+	}
+	if err := tsDb.PutJobsInChunks(jobs); err != nil {
+		httputils.ReportError(w, err, "Failed to insert jobs.", http.StatusInternalServerError)
+		return
+	}
+	ids := make([]string, 0, len(jobs))
+	for _, job := range jobs {
+		ids = append(ids, job.Id)
 	}
 	if err := json.NewEncoder(w).Encode(ids); err != nil {
 		httputils.ReportError(w, err, "Failed to encode response.", http.StatusInternalServerError)
@@ -313,7 +300,8 @@ func jsonJobHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	job, dimsByTask, err := ts.GetJob(r.Context(), id)
+	// Retrieve the Job from the DB.
+	job, err := tsDb.GetJobById(id)
 	if err != nil {
 		if err == db.ErrNotFound {
 			http.Error(w, "Unknown Job", 404)
@@ -322,6 +310,25 @@ func jsonJobHandler(w http.ResponseWriter, r *http.Request) {
 		httputils.ReportError(w, err, "Error retrieving Job.", http.StatusInternalServerError)
 		return
 	}
+
+	// Retrieve the task specs, so that we can include the task dimensions
+	// in the results.
+	cfg, err := taskCfgCache.Get(r.Context(), job.RepoState)
+	if err != nil {
+		httputils.ReportError(w, err, "Failed to retrieve Tasks cfg", http.StatusInternalServerError)
+		return
+	}
+	dimsByTask := make(map[string][]string, len(job.Dependencies))
+	for taskName := range job.Dependencies {
+		taskSpec, ok := cfg.Tasks[taskName]
+		if !ok {
+			httputils.ReportError(w, fmt.Errorf("Job %s (%s) points to unknown task %q at repo state: %+v", job.Id, job.Name, taskName, job.RepoState), "Job points to unknown task", http.StatusInternalServerError)
+			return
+		}
+		dimsByTask[taskName] = taskSpec.Dimensions
+	}
+
+	// Encode the response.
 	if err := json.NewEncoder(w).Encode(struct {
 		*types.Job
 		TaskDimensions map[string][]string `json:"taskDimensions"`
@@ -342,9 +349,20 @@ func jsonCancelJobHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	job, err := ts.CancelJob(id)
+	job, err := tsDb.GetJobById(id)
 	if err != nil {
-		httputils.ReportError(w, err, "Failed to cancel job.", http.StatusInternalServerError)
+		httputils.ReportError(w, err, "Failed to retrieve job.", http.StatusInternalServerError)
+		return
+	}
+	if job.Done() {
+		err := fmt.Errorf("Job %s is already finished with status %s", id, job.Status)
+		httputils.ReportError(w, err, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	job.Finished = time.Now()
+	job.Status = types.JOB_STATUS_CANCELED
+	if err := tsDb.PutJob(job); err != nil {
+		httputils.ReportError(w, err, "Failed to insert Job", http.StatusInternalServerError)
 		return
 	}
 	if err := json.NewEncoder(w).Encode(job); err != nil {
@@ -503,7 +521,7 @@ func jsonGetTaskHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	task, err := ts.GetTask(id)
+	task, err := tsDb.GetTaskById(id)
 	if err != nil {
 		if err == db.ErrNotFound {
 			http.Error(w, "Unknown Task", 404)
@@ -558,7 +576,8 @@ func jsonTaskSearchHandler(w http.ResponseWriter, r *http.Request) {
 
 // jsonTaskCandidateSearchHandler allows for searching task candidates based on
 // their TaskKey.
-func jsonTaskCandidateSearchHandler(w http.ResponseWriter, r *http.Request) {
+// TODO(borenet): Re-enable this if/when candidates have their own DB.
+/*func jsonTaskCandidateSearchHandler(w http.ResponseWriter, r *http.Request) {
 	var params scheduling.TaskCandidateSearchTerms
 	if err := httputils.ParseFormValues(r, &params); err != nil {
 		httputils.ReportError(w, err, "Failed to parse request parameters.", http.StatusInternalServerError)
@@ -570,7 +589,7 @@ func jsonTaskCandidateSearchHandler(w http.ResponseWriter, r *http.Request) {
 		httputils.ReportError(w, err, fmt.Sprintf("Failed to encode response: %s", err), http.StatusInternalServerError)
 		return
 	}
-}
+}*/
 
 func googleVerificationHandler(w http.ResponseWriter, r *http.Request) {
 	if _, err := w.Write([]byte("google-site-verification: google2c59f97e1ced9fdc.html")); err != nil {
@@ -593,7 +612,8 @@ func runServer(serverURL string) {
 	r.HandleFunc("/json/job/{id}/cancel", login.RestrictEditorFn(jsonCancelJobHandler)).Methods(http.MethodPost)
 	r.HandleFunc("/json/jobs/search", jsonJobSearchHandler)
 	r.HandleFunc("/json/task/{id}", jsonGetTaskHandler)
-	r.HandleFunc("/json/taskCandidates/search", jsonTaskCandidateSearchHandler)
+	// TODO(borenet): Re-enable this if/when candidates have their own DB.
+	//r.HandleFunc("/json/taskCandidates/search", jsonTaskCandidateSearchHandler)
 	r.HandleFunc("/json/tasks/search", jsonTaskSearchHandler)
 	r.HandleFunc("/json/trigger", login.RestrictEditorFn(jsonTriggerHandler)).Methods(http.MethodPost, http.MethodOptions)
 	r.HandleFunc("/json/version", skiaversion.JsonHandler)
@@ -635,39 +655,12 @@ func main() {
 	ctx, cancelFn := context.WithCancel(context.Background())
 	cleanup.AtExit(cancelFn)
 
-	// Get the absolute workdir.
-	wdAbs, err := filepath.Abs(*workdir)
-	if err != nil {
-		sklog.Fatal(err)
-	}
-
 	// Set up token source and authenticated API clients.
-	isolateServerUrl := *isolateServer
-	if *local {
-		isolateServerUrl = isolate.ISOLATE_SERVER_URL_FAKE
-	}
-	var isolateClient *isolate.Client
-	var tokenSource oauth2.TokenSource
-	gitcookiesPath := "/tmp/.gitcookies"
-	tokenSource, err = auth.NewDefaultTokenSource(*local, auth.SCOPE_USERINFO_EMAIL, auth.SCOPE_GERRIT, auth.SCOPE_READ_WRITE, pubsub.ScopePubSub, datastore.ScopeDatastore, bigtable.Scope, swarming.AUTH_SCOPE)
+	// TODO(borenet): Should we create a new service account with fewer
+	// permissions?
+	tokenSource, err := auth.NewDefaultTokenSource(*local, auth.SCOPE_USERINFO_EMAIL, pubsub.ScopePubSub, datastore.ScopeDatastore, bigtable.Scope, swarming.AUTH_SCOPE)
 	if err != nil {
 		sklog.Fatalf("Failed to create token source: %s", err)
-	}
-	isolateClient, err = isolate.NewClientWithServiceAccount(wdAbs, isolateServerUrl, os.Getenv("GOOGLE_APPLICATION_CREDENTIALS"))
-	if err != nil {
-		sklog.Fatal(err)
-	}
-	if _, err := gitauth.New(tokenSource, gitcookiesPath, true, ""); err != nil {
-		sklog.Fatalf("Failed to create git cookie updater: %s", err)
-	}
-
-	// Authenticated HTTP client.
-	httpClient := httputils.DefaultClientConfig().WithTokenSource(tokenSource).With2xxOnly().Client()
-
-	// Gerrit API client.
-	gerrit, err := gerrit.NewGerrit(gerrit.GERRIT_SKIA_URL, gitcookiesPath, nil)
-	if err != nil {
-		sklog.Fatal(err)
 	}
 
 	// Initialize the database.
@@ -684,6 +677,7 @@ func main() {
 	if err != nil {
 		sklog.Fatal(err)
 	}
+	bl.AutoUpdate(ctx)
 
 	// Git repos.
 	if *repoUrls == nil {
@@ -701,76 +695,24 @@ func main() {
 	}
 	repos = autoUpdateRepos.Map
 
+	// Task Cfg Cache.
+	taskCfgCache, err = task_cfg_cache.NewTaskCfgCache(ctx, repos, *btProject, *btInstance, tokenSource)
+	if err != nil {
+		sklog.Fatal(err)
+	}
+
 	// Initialize Swarming client.
-	if *local {
-		wd := filepath.Join(wdAbs, "local_git_repos")
-		gitRepos := make(map[string]*git.Repo, len(repos))
-		for url := range repos {
-			r, err := git.NewRepo(ctx, url, wd)
-			if err != nil {
-				sklog.Fatal(err)
-			}
-			gitRepos[url] = r
-		}
-		swarmTestClient := testutils.NewTestClient()
-		swarmTestClient.MockBots(testutils.MockSwarmingBotsForAllTasksForTesting(ctx, gitRepos))
-		go testutils.PeriodicallyUpdateMockTasksForTesting(swarmTestClient)
-		swarm = swarmTestClient
-	} else {
-		cfg := httputils.DefaultClientConfig().WithTokenSource(tokenSource).WithDialTimeout(time.Minute).With2xxOnly()
-		cfg.RequestTimeout = time.Minute
-		swarm, err = swarming.NewApiClient(cfg.Client(), *swarmingServer)
-		if err != nil {
-			sklog.Fatal(err)
-		}
-	}
-
-	storageClient, err := storage.NewClient(ctx, option.WithTokenSource(tokenSource))
-	diagClient := gcsclient.New(storageClient, *diagnosticsBucket)
-	diagInstance := *firestoreInstance
-
-	// Find depot_tools.
-	// TODO(borenet): Package depot_tools in the Docker image.
-	if *recipesCfgFile == "" {
-		*recipesCfgFile = path.Join(wdAbs, "recipes.cfg")
-	}
-	depotTools, err := depot_tools.Sync(ctx, wdAbs, *recipesCfgFile)
+	cfg := httputils.DefaultClientConfig().WithTokenSource(tokenSource).WithDialTimeout(time.Minute).With2xxOnly()
+	cfg.RequestTimeout = time.Minute
+	swarm, err = swarming.NewApiClient(cfg.Client(), *swarmingServer)
 	if err != nil {
 		sklog.Fatal(err)
 	}
 
-	// Parse the time period.
-	period, err := human.ParseDuration(*timePeriod)
-	if err != nil {
-		sklog.Fatal(err)
-	}
-
-	// Create and start the task scheduler.
-	sklog.Infof("Creating task scheduler.")
-	ts, err = scheduling.NewTaskScheduler(ctx, tsDb, bl, period, *commitWindow, wdAbs, serverURL, repos, isolateClient, swarm, httpClient, *scoreDecay24Hr, tryjobs.API_URL_PROD, *tryJobBucket, common.PROJECT_REPO_MAPPING, *swarmingPools, *pubsubTopicName, depotTools, gerrit, *btProject, *btInstance, tokenSource, diagClient, diagInstance)
-	if err != nil {
-		sklog.Fatal(err)
-	}
-	cleanup.AtExit(func() {
-		util.LogErr(ts.Close())
-	})
-	if err := swarming.InitPubSub(*pubsubTopicName, *pubsubSubscriberName, ts.HandleSwarmingPubSub); err != nil {
-		sklog.Fatal(err)
-	}
-
-	sklog.Infof("Created task scheduler. Starting loop.")
-	ts.Start(ctx, !*disableTryjobs, func() {})
-	if err := autoUpdateRepos.Start(ctx, GITSTORE_SUBSCRIBER_ID, tokenSource, 5*time.Minute, ts.HandleRepoUpdate); err != nil {
-		sklog.Fatal(err)
-	}
-
-	// Set up periodic triggers.
-	if err := periodic.Listen(ctx, fmt.Sprintf("task-scheduler-%s", *firestoreInstance), tokenSource, func(ctx context.Context, name, id string) bool {
-		if err := ts.MaybeTriggerPeriodicJobs(ctx, name); err != nil {
-			sklog.Errorf("Failed to trigger periodic jobs; will retry later: %s", err)
-			return false // We will retry later.
-		}
-		return true
+	// Auto-update the git repos.
+	if err := autoUpdateRepos.Start(ctx, GITSTORE_SUBSCRIBER_ID, tokenSource, 5*time.Minute, func(_ context.Context, _ string, _ *repograph.Graph, ack, _ func()) error {
+		ack()
+		return nil
 	}); err != nil {
 		sklog.Fatal(err)
 	}
