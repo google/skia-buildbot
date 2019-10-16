b4178e1756fbb4d7dd237a1fd6b6c7f071c61b64
Joe Gregorio <jcgregorio@google.com>
2019-10-16 13:23:01 -0400

    hashtag - Start sketching the hashtag service.
    Change-Id: I1ee3ebd7ac620d6ccdd2640e3c6733c63ea3daed

---
+++ hashtag/modules/team-drive-sk/team-drive-sk.js
@@ -0,0 +1,97 @@
+/**
+ * @module modules/team-drive-sk
+ * @description <h2><code>team-drive-sk</code></h2>
+ *
+ * @evt
+ *
+ * @attr
+ *
+ * @example
+ */
+import { html, render } from 'lit-html'
+import { ElementSk } from '../../../infra-sk/modules/ElementSk'
+import { errorMessage } from 'elements-sk/errorMessage'
+
+const template = (ele) => html`
+  ${ele.isSignedIn() ? html`<button @click=${() => gapi.auth2.getAuthInstance().signIn()}>Authorize</button>` : html`<h2>Logged In</h2>`}
+
+  <h2>Stuff</h2>
+  <li>
+    ${ele._files.map((file) => html`<li>${file.name}</li>`)}
+  </li>
+`;
+
+// Client ID and API key from the Developer Console
+const CLIENT_ID = '145247227042-9dc658m0dj9mbah94kn5i3nnejirvrgl.apps.googleusercontent.com';
+const API_KEY = 'AIzaSyB8YZ_bFpEVOyICD-pUutJ3mxm6c1to8q0';
+
+// Array of API discovery doc URLs for APIs used by the quickstart
+const DISCOVERY_DOCS = ["https://www.googleapis.com/discovery/v1/apis/drive/v3/rest"];
+
+// Authorization scopes required by the API; multiple scopes can be included,
+// separated by spaces.
+const SCOPES = 'https://www.googleapis.com/auth/drive.readonly';
+
+window.customElements.define('team-drive-sk', class extends ElementSk {
+    constructor() {
+        super(template);
+        this._files = [];
+    }
+
+    connectedCallback() {
+        super.connectedCallback();
+        gapi.load('client:auth2', () => this._initClient());
+    }
+
+    _initClient() {
+        gapi.client.init({
+            apiKey: API_KEY,
+            clientId: CLIENT_ID,
+            discoveryDocs: DISCOVERY_DOCS,
+            scope: SCOPES
+        }).then(() => {
+            // Listen for sign-in state changes.
+            gapi.auth2.getAuthInstance().isSignedIn.listen(() => this.listFiles());
+
+            // Handle the initial sign-in state.
+            this.listFiles();
+        }).catch(errorMessage);
+    }
+
+    isSignedIn() {
+        return gapi.auth2.getAuthInstance().isSignedIn.get();
+    }
+
+    listFiles() {
+        if (this.isSignedIn()) {
+            this._files = [];
+            gapi.client.drive.files.list({
+                'pageSize': 10,
+                'corpora': 'drive',
+                'driveId': '0AOGploz136NUUk9PVA',
+                'includeItemsFromAllDrives': true,
+                'q': 'fullText contains \"forklift\"',
+                'supportsAllDrives': true,
+                // TODO(jcgregorio): iterate the pages of the response.
+                //'fields': "nextPageToken, files(id, name)"
+            }).then((response) => {
+                var files = response.result.files;
+                if (files && files.length > 0) {
+                    for (var i = 0; i < files.length; i++) {
+                        this._files.push(files[i]);
+                    }
+                } else {
+                    // None found.
+                }
+                this._render();
+            });
+        } else {
+            this._render();
+        }
+
+    }
+
+    disconnectedCallback() {
+    }
+
+});
