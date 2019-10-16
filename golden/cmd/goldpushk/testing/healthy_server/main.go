3a7232cd673c9e8d9cbfbfee740136aab0fa5840
Leandro Lovisolo <lovisolo@google.com>
2019-10-16 14:24:19 -0400

    [goldpushk] Add boot delay to the "healthy server" test service, and run multiple replicas.
    This makes "healthy server" a bit more realistic and will help debugging some monitoring issues in goldpushk.
    
    Bug: skia:9076
    Change-Id: Idd7524506763d102732c788a76d51dc287cd0364

--- golden/cmd/goldpushk/testing/healthy_server/main.go
+++ golden/cmd/goldpushk/testing/healthy_server/main.go
@@ -6,6 +6,8 @@ package main
 import (
 	"flag"
 	"fmt"
+	"math/rand"
+	"time"
 
 	"go.skia.org/infra/go/config"
 	"go.skia.org/infra/go/httputils"
@@ -17,6 +19,9 @@ func main() {
 	flagInstance := flag.String("instance", "", "Instance name.")
 	flagPort := flag.Int("port", 0, "Port to listen to.")
 	flagConfigFilename := flag.String("config_filename", "", "Configuration file in JSON5 format.")
+	usageActualDelayStr := "Actual delay will be a random duration between --min_delay and --max_delay."
+	flagMinDelay := flag.Duration("min_delay", 0, "Minimum delay before launching the health check server (default: 0). "+usageActualDelayStr)
+	flagMaxDelay := flag.Duration("max_delay", 0, "Minimum delay before launching the health check server (default: 0). "+usageActualDelayStr)
 	flag.Parse()
 	if *flagInstance == "" {
 		sklog.Fatal("You must provide flag --instance.")
@@ -30,6 +35,15 @@ func main() {
 	if *flagConfigFilename == "" {
 		sklog.Fatal("You must provide flag --config_filename.")
 	}
+	if *flagMinDelay < 0 {
+		sklog.Fatal("Flag --min_delay cannot be less than 0.")
+	}
+	if *flagMaxDelay < 0 {
+		sklog.Fatal("Flag --max_delay cannot be less than 0.")
+	}
+	if *flagMinDelay > *flagMaxDelay {
+		sklog.Fatalf("Flag --min_delay should be less than --max_delay (given values are %d and %d seconds, respectively).", *flagMinDelay/time.Second, *flagMaxDelay/time.Second)
+	}
 
 	// Read configuration file.
 	json5Config := struct {
@@ -41,6 +55,12 @@ func main() {
 
 	// Log the salutation read from the config file.
 	sklog.Infof("Salutation: %s", json5Config.Salutation)
+
+	// Sleep for a random amount of time between --min_delay and --max_delay.
+	rand.Seed(time.Now().UnixNano())
+	delay := time.Duration(int(*flagMinDelay) + rand.Intn(int(*flagMaxDelay)-int(*flagMinDelay)+1))
+	sklog.Infof("Sleeping for %d seconds before starting healthy_server.", delay/time.Second)
+	time.Sleep(delay)
 
 	// Log additional flags and start the health check server.
 	sklog.Infof("Running goldpushk healthy_server (instance name: \"%s\") on port %d.\n", *flagInstance, *flagPort)
