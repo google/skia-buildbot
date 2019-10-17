3985fcb02d47c91be0178556aeceadfe78a531d5
Kevin Lubick <kjlubick@google.com>
2019-10-17 13:41:38 -0400

    [gold] pre-warm image cache before computing diffs
    This should work around the simplification of the ReadThroughCache in
    https://skia-review.googlesource.com/c/buildbot/+/248917
    and deduplicate fetches of the main image.
    
    Additionally, this cleans up the mutex, which is no longer needed
    and adds a test documenting the partial success of Get
    (should we keep this partial success around? To be discussed)
    
    Bug: skia:9080
    Change-Id: Ibbb4e4f394e77b4c611748cc3fcdb5f89e395966

--- golden/go/diff/diff.go
+++ golden/go/diff/diff.go
@@ -106,7 +106,9 @@ func NewDigestFailure(digest types.Digest, reason DiffErr) *DigestFailure {
 // diffs images. How it retrieves the images is up to the implementation.
 type DiffStore interface {
 	// Get returns the DiffMetrics of the provided dMain digest vs all digests
-	// specified in dRest.
+	// specified in dRest. If one or more of the diffs is unable to be computed, the
+	// returned map will contain any data not resulting from an error, with the (hopefully
+	// transient) errors logged.
 	Get(ctx context.Context, mainDigest types.Digest, rightDigests types.DigestSlice) (map[types.Digest]*DiffMetrics, error)
 
 	// ImageHandler returns a http.Handler for the given path prefix. The caller
