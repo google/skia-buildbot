78dc0fe30c07c9170c7e8545efe079728278bcc7
Kevin Lubick <kjlubick@google.com>
2019-10-17 14:43:30 -0400

    [gold] Performance tuning for diffserver
    Ran diffserver on staging and noticed some places where we are making
    bad or outdated assumptions and places where we can have some easy
    wins for performance.
    
    Summary of changes:
    1. Add GetAll function to memrtc
    2. Add option to expose netpprof on diffserver
    3. Use errgroup in imgloader.
    4. Recompute assumptions of cache size to keep more images in ram.
    5. Add a few timers and metrics to make diffserver less opaque.
    6. Stop saving diffmetrics Asynchronously.
    7. Make restriction of goroutines more focused around where
       the expensive calls are (i.e. decoding the images and diffing it)
    8. Make LoadDiffMetrics take an array of strings to make use
       of batch queries where appropriate.
    
    Bug: skia:9080
    Change-Id: I8ed47f3514660383e97ae40f4d180670259decc6

--- go/rtcache/rtcache.go
+++ go/rtcache/rtcache.go
@@ -13,6 +13,14 @@ type ReadThroughCache interface {
 	// is called to retrieve it.
 	Get(ctx context.Context, id string) (interface{}, error)
 
+	// GetAll returns all item identified by 'id' or an error if any of them
+	// cannot be retrieved. If any items are not in the cache, a worker function
+	// is called per item to retrieve them.
+	GetAll(ctx context.Context, id []string) ([]interface{}, error)
+
+	// Len returns the number of items that are cached.
+	Len() int
+
 	// Keys returns the keys of the items that are cached.
 	Keys() []string
 
@@ -24,5 +32,5 @@ type ReadThroughCache interface {
 }
 
 // ReadThroughFunc defines the function that is called when an item is not in the
-// cache. 'ctx' and 'id' are the same that were passed to Get(...).
-type ReadThroughFunc func(ctx context.Context, id string) (interface{}, error)
+// cache. 'ctx' and 'id' are the same that were passed to GetAll(...).
+type ReadThroughFunc func(ctx context.Context, ids []string) ([]interface{}, error)
