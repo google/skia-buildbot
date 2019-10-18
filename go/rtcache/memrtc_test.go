1e99d2441ffad264c51a2a844062e70e34d59c21
Kevin Lubick <kjlubick@google.com>
2019-10-18 08:05:55 -0400

    [gold] Remove priority from rtcache. Add Context.
    Of note, this simplified cache does handle the case
    of two successive Gets to the same thing as "gracefully"
    as the previous implementation. i.e. it does not
    deduplicate them.
    
    Bug: skia:9080
    Change-Id: Idddca04b3572e7b6051cfb4001c32412ea7ffea5

--- go/rtcache/memrtc_test.go
+++ go/rtcache/memrtc_test.go
@@ -1,146 +1,180 @@
 package rtcache
 
 import (
-	"container/heap"
-	"fmt"
-	"math/rand"
-	"runtime"
+	"context"
+	"errors"
 	"sync"
 	"testing"
-	"time"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.skia.org/infra/go/testutils/unittest"
 )
 
-const (
-	N_TASKS      = 1000
-	PACKAGE_SIZE = 1024 * 512
-)
-
-func TestPriorityQueue(t *testing.T) {
+// TestMemReadThroughCacheSunnyDay checks that we cache the values after reading them
+// from the passed in worker function
+func TestMemReadThroughCacheSunnyDay(t *testing.T) {
 	unittest.SmallTest(t)
-	pq := &priorityQueue{}
-	vals := []*workItem{
-		{id: "0", priority: 0},
-		{id: "1", priority: 1},
-		{id: "2", priority: 2},
-		{id: "3", priority: 3},
-		{id: "4", priority: 4},
-		{id: "5", priority: 5},
-		{id: "6", priority: 6},
-		{id: "7", priority: 7},
-		{id: "8", priority: 8},
-		{id: "9", priority: 9},
+
+	const alpha = "alpha"
+	const beta = "beta"
+
+	called := map[string]int{}
+
+	workerFn := func(ctx context.Context, id string) (interface{}, error) {
+		assert.NotNil(t, ctx)
+		called[id]++
+		return id + id, nil
 	}
-	indices := rand.Perm(len(vals))
-	for _, idx := range indices {
-		heap.Push(pq, vals[idx])
-	}
-	require.Equal(t, len(vals), len(*pq))
-	result := ""
-	for len(*pq) > 0 {
-		item := heap.Pop(pq).(*workItem)
-		result += item.id
-	}
-	require.Equal(t, "0123456789", result)
+
+	rtc, err := New(workerFn, 10, 10)
+	require.NoError(t, err)
+
+	assert.Empty(t, rtc.Keys())
+
+	v, err := rtc.Get(context.Background(), alpha)
+	require.NoError(t, err)
+	assert.Equal(t, "alphaalpha", v)
+	v, err = rtc.Get(context.Background(), beta)
+	require.NoError(t, err)
+	assert.Equal(t, "betabeta", v)
+
+	// Make sure we've only called the rtf once each.
+	assert.Equal(t, map[string]int{
+		alpha: 1,
+		beta:  1,
+	}, called)
+
+	// Check a few more times to make sure it is cached:
+	v, err = rtc.Get(context.Background(), alpha)
+	require.NoError(t, err)
+	assert.Equal(t, "alphaalpha", v)
+	v, err = rtc.Get(context.Background(), alpha)
+	require.NoError(t, err)
+	assert.Equal(t, "alphaalpha", v)
+	v, err = rtc.Get(context.Background(), beta)
+	require.NoError(t, err)
+	assert.Equal(t, "betabeta", v)
+
+	// Make sure we cache the calls after the first
+	assert.Equal(t, map[string]int{
+		alpha: 1,
+		beta:  1,
+	}, called)
+
+	assert.Len(t, rtc.Keys(), 2)
+	assert.Contains(t, rtc.Keys(), alpha)
+	assert.Contains(t, rtc.Keys(), beta)
 }
 
-func TestReadThroughCache(t *testing.T) {
-	unittest.MediumTest(t)
+// TestMemReadThroughCacheRace checks that multiple concurrent calls to Get
+func TestMemReadThroughCacheRace(t *testing.T) {
+	unittest.SmallTest(t)
 
-	randBytes := make([]byte, PACKAGE_SIZE)
-	_, err := rand.Read(randBytes)
-	require.NoError(t, err)
+	const alpha = "alpha"
+	const beta = "beta"
 
-	worker := func(priority int64, id string) (interface{}, error) {
-		// Create a unique version of the random array.
-		return []byte(id + string(randBytes)), nil
+	workerFn := func(ctx context.Context, id string) (interface{}, error) {
+		assert.NotNil(t, ctx)
+		return id + id, nil
 	}
 
-	// create a worker queue for a given type
-	q, err := New(worker, 10000, runtime.NumCPU()-2)
+	rtc, err := New(workerFn, 10, 3)
 	require.NoError(t, err)
 
-	// make sure all results arrive.
-	var allDone sync.WaitGroup
-	retCh := make(chan interface{}, N_TASKS)
-	errCh := make(chan error, N_TASKS)
-
-	for i := 0; i < N_TASKS; i++ {
-		allDone.Add(1)
-		go func(idx, priority int) {
-			// time.Sleep(time.Second * 5)
-			id := "id-" + fmt.Sprintf("%07d", idx)
-			result, err := q.Get(int64(priority), id)
-			if err != nil {
-				errCh <- err
-			} else {
-				retCh <- result
-			}
-
-			allDone.Done()
-		}(i, i)
-	}
-	allDone.Wait()
-
-	close(errCh)
-	close(retCh)
-
-	if len(errCh) > 0 {
-		for err := range errCh {
-			fmt.Printf("Error: %s", err)
+	var wg sync.WaitGroup
+	for i := 0; i < 10; i++ {
+		s := alpha
+		if i%2 == 0 {
+			s = beta
 		}
-		require.Fail(t, "Received above error messages.")
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			_, err := rtc.Get(context.Background(), s)
+			require.NoError(t, err)
+		}()
 	}
 
-	require.Equal(t, 0, len(errCh))
-	found := make(map[string]bool, N_TASKS)
-	resultIds := make([]string, 0, len(retCh))
-	resultVals := make([][]byte, 0, len(retCh))
-	for ret := range retCh {
-		require.IsType(t, []byte(""), ret)
-		resultVal := ret.([]byte)
-		resultIds = append(resultIds, string(resultVal[:10]))
-		resultVals = append(resultVals, resultVal)
-
-		// Add the prefix size to PACKAGE_SIZE to account for prefix added above.
-		require.Equal(t, PACKAGE_SIZE+10, len(ret.([]byte)))
-		found[string(ret.([]byte))] = true
-	}
-
-	// Make sure all strings are unique.
-	require.Equal(t, N_TASKS, len(found))
-	for i, resultID := range resultIds {
-		val, err := q.Get(0, resultID)
-		require.NoError(t, err)
-		require.Equal(t, resultVals[i], val)
-	}
-
-	require.True(t, q.Contains("id-0000000"))
-	require.False(t, q.Contains("some-random-never-before-seen-key"))
-	q.(*MemReadThroughCache).shutdown()
+	wg.Wait()
 }
 
-func TestErrHandling(t *testing.T) {
+// TestMemReadThroughCacheRemove checks that if we call remove, we re-fetch the value
+// on the next call
+func TestMemReadThroughCacheRemove(t *testing.T) {
 	unittest.SmallTest(t)
-	errWorker := func(priority int64, id string) (interface{}, error) {
-		return nil, fmt.Errorf("id: %v", time.Now())
+
+	const alpha = "alpha"
+	const beta = "beta"
+
+	called := map[string]int{}
+
+	workerFn := func(ctx context.Context, id string) (interface{}, error) {
+		assert.NotNil(t, ctx)
+		called[id]++
+		return id + id, nil
 	}
 
-	testID := "id-1"
-	q, err := New(errWorker, 10000, runtime.NumCPU())
+	rtc, err := New(workerFn, 10, 10)
 	require.NoError(t, err)
-	_, err = q.Get(1, testID)
-	require.Error(t, err)
-	time.Sleep(time.Millisecond)
-	_, err = q.Get(1, testID)
-	_, errTwo := q.Get(1, testID)
-	require.Error(t, errTwo)
-	require.Equal(t, err, errTwo)
-	q.(*MemReadThroughCache).errCache.Flush()
-	time.Sleep(time.Millisecond)
-	_, errThree := q.Get(1, testID)
-	require.Error(t, errThree)
-	require.NotEqual(t, err, errThree)
+
+	v, err := rtc.Get(context.Background(), alpha)
+	require.NoError(t, err)
+	assert.Equal(t, "alphaalpha", v)
+
+	assert.True(t, rtc.Contains(alpha))
+	// If we remove keys that don't exist, it shouldn't crash.
+	rtc.Remove([]string{alpha, beta})
+	assert.False(t, rtc.Contains(alpha))
+
+	v, err = rtc.Get(context.Background(), alpha)
+	require.NoError(t, err)
+	assert.Equal(t, "alphaalpha", v)
+
+	// Make sure we have called it twice, because we removed the cached value
+	// after the first fetch.
+	assert.Equal(t, map[string]int{
+		alpha: 2,
+	}, called)
+}
+
+// TestMemReadThroughCacheGetErrors checks that if a worker function call returns error, we will
+// try it again later
+func TestMemReadThroughCacheGetErrors(t *testing.T) {
+	unittest.SmallTest(t)
+
+	const alpha = "alpha"
+
+	called := map[string]int{}
+
+	// Fails on the first call, succeeds on subsequent calls.
+	workerFn := func(ctx context.Context, id string) (interface{}, error) {
+		assert.NotNil(t, ctx)
+		called[id]++
+		if called[id] == 1 {
+			return nil, errors.New("oops")
+		}
+		return id + id, nil
+	}
+
+	rtc, err := New(workerFn, 10, 10)
+	require.NoError(t, err)
+
+	_, err = rtc.Get(context.Background(), alpha)
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "oops")
+
+	v, err := rtc.Get(context.Background(), alpha)
+	require.NoError(t, err)
+	assert.Equal(t, "alphaalpha", v)
+
+	// This should be a cached call
+	v, err = rtc.Get(context.Background(), alpha)
+	require.NoError(t, err)
+	assert.Equal(t, "alphaalpha", v)
+
+	// Make sure we have called it twice, because the first time was an error.
+	assert.Equal(t, map[string]int{
+		alpha: 2,
+	}, called)
 }
--- go/rtcache/rtcache.go
+++ go/rtcache/rtcache.go
@@ -1,47 +1,27 @@
 package rtcache
 
 import (
-	"math"
-
-	"go.skia.org/infra/go/util"
+	"context"
 )
 
-// ReadThroughCache defines a caching work queue with priorities. If the item
-// identified by 'id' is not in the cache then it will call a
-// worker function to calculate it. 'priority' is an positive integer.
-// Lower values have higher priorities, so 0 is the highest priority.
+// ReadThroughCache defines a caching work queue with priorities. If the item identified by 'id'
+// is not in the cache then it will call a provided ReadThroughFunc in order to calculate it.
 type ReadThroughCache interface {
 	// Get returns the item identified by 'id' or an error if the item
 	// cannot be retrieved. If the item is not in the cache a worker function
 	// is called to retrieve it.
-	Get(priority int64, id string) (interface{}, error)
+	Get(ctx context.Context, id string) (interface{}, error)
 
-	// Warm is identical to Get except it does not return the cached elements
-	// just makes sure they are in the cache. If an error occurs generating the
-	// item desired item via the worker function, an error is returned.
-	Warm(priority int64, id string) error
+	// Keys returns the keys of the items that are cached.
+	Keys() []string
 
 	// Contains returns true if the identified item is currently cached.
 	Contains(id string) bool
-
-	// Keys returns the keys of the cache.
-	Keys() []string
 
 	// Remove removes the element with the given ids from the cache.
 	Remove(ids []string)
 }
 
-// WorkerFn defines the function that is called when an item is not in the
-// cache. 'priority' and 'id' are the same that were passed to Get(...).
-type ReadThroughFunc func(priority int64, id string) (interface{}, error)
-
-// PriorityTimeCombined combines a priority with a timestamp where the
-// priority becomes the most significant digit and the time in Milliseconds
-// the less significant digits. i.e. it allows to process elements of the
-// same priority in order of their timestamps, but ahead of items with
-// less priority.
-func PriorityTimeCombined(priority int64) int64 {
-	ts := util.TimeStampMs()
-	exp := int(math.Ceil(math.Log10(float64(ts)))) + 1
-	return ts + int64(float64(priority)*math.Pow10(exp))
-}
+// ReadThroughFunc defines the function that is called when an item is not in the
+// cache. 'ctx' and 'id' are the same that were passed to Get(...). error might be wrapped.
+type ReadThroughFunc func(ctx context.Context, id string) (interface{}, error)
