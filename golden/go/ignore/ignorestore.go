6a169eefd9abbe9cb1af6a70bd5b67cea72eaa23
Kevin Lubick <kjlubick@google.com>
2019-10-18 07:41:09 -0400

    [gold] Delete unused aspects of ignorestore
    Change-Id: I96cab108131b0bf07d4ce28dd1de3f4dccb2af12

--- golden/go/ignore/ignorestore.go
+++ golden/go/ignore/ignorestore.go
@@ -25,16 +25,6 @@ type Store interface {
 	// Delete removes a Rule from the store. The return value is the number of
 	// records that were deleted (either 0 or 1).
 	Delete(ctx context.Context, id int64) (int, error)
-
-	// Revision returns a monotonically increasing int64 that goes up each time
-	// the ignores have been changed. It will not persist nor will it be the same
-	// between different instances of Store. I.e. it will probably start at
-	// zero each time a Store is instantiated.
-	Revision() int64
-
-	// BuildRuleMatcher returns a RuleMatcher based on the current content
-	// of the ignore store.
-	BuildRuleMatcher(context.Context) (RuleMatcher, error)
 }
 
 // Rule is the GUI struct for dealing with Ignore rules.
@@ -47,8 +37,8 @@ type Rule struct {
 	Note      string    `json:"note"`
 }
 
-// ToQuery makes a slice of url.Values from the given slice of Rules.
-func ToQuery(ignores []*Rule) ([]url.Values, error) {
+// toQuery makes a slice of url.Values from the given slice of Rules.
+func toQuery(ignores []*Rule) ([]url.Values, error) {
 	var ret []url.Values
 	for _, ignore := range ignores {
 		v, err := url.ParseQuery(ignore.Query)
@@ -60,6 +50,7 @@ func ToQuery(ignores []*Rule) ([]url.Values, error) {
 	return ret, nil
 }
 
+// NewRule creates a new ignore rule with the given data.
 func NewRule(createdByUser string, expires time.Time, queryStr string, note string) *Rule {
 	return &Rule{
 		Name:      createdByUser,
@@ -68,41 +59,4 @@ func NewRule(createdByUser string, expires time.Time, queryStr string, note stri
 		Query:     queryStr,
 		Note:      note,
 	}
-}
-
-// TODO(stephana): Factor out QueryRule into the shared library and consolidate
-// with the Matches function in the ./go/tiling package.
-
-// QueryRule wraps around a web query and allows matching of
-// parameter sets against it.
-type QueryRule map[string]map[string]bool
-
-func NewQueryRule(v url.Values) QueryRule {
-	ret := make(map[string]map[string]bool, len(v))
-	for k, paramValues := range v {
-		ret[k] = make(map[string]bool, len(paramValues))
-		for _, oneVal := range paramValues {
-			ret[k][oneVal] = true
-		}
-	}
-	return ret
-}
-
-// IsMatch returns true if the set of parameters in params matches this query.
-func (q QueryRule) IsMatch(params map[string]string) bool {
-	if len(q) > len(params) {
-		return false
-	}
-
-	for ruleKey, ruleValues := range q {
-		paramVal, ok := params[ruleKey]
-		if !(ok && ruleValues[paramVal]) {
-			return false
-		}
-	}
-	return true
-}
-
-func noopRuleMatcher(_ map[string]string) ([]*Rule, bool) {
-	return nil, false
 }
