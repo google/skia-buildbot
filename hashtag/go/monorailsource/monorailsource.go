f38b926cf74904777283a6cb73ec9d35262a59e6
Joe Gregorio <jcgregorio@google.com>
2019-10-21 09:33:36 -0400

    hashtag - Start sketching the hashtag service.
    Change-Id: I1ee3ebd7ac620d6ccdd2640e3c6733c63ea3daed

---
+++ hashtag/go/monorailsource/monorailsource.go
@@ -0,0 +1,56 @@
+package monorailsource
+
+import (
+	"time"
+
+	"go.skia.org/infra/go/auth"
+	"go.skia.org/infra/go/httputils"
+	"go.skia.org/infra/go/issues"
+	"go.skia.org/infra/go/skerr"
+	"go.skia.org/infra/go/sklog"
+	"go.skia.org/infra/hashtag/go/source"
+)
+
+type monorailSource struct {
+	m issues.IssueTracker
+}
+
+// New returns a new Source.
+func New() (source.Source, error) {
+	c, err := google.DefaultClient(context.Background(), "https://www.googleapis.com/auth/userinfo.email")
+	if err != nil {
+		return nil, skerr.Wrapf(err, "Unable to create tokenSource")
+	}
+	m := issues.NewMonorailIssueTracker(c, issues.PROJECT_SKIA)
+
+	return &monorailSource{
+		m: m,
+	}, nil
+}
+
+func (m *monorailSource) ByHashtag(hashtag string) <-chan source.Artifact {
+	ret := make(chan source.Artifact)
+	go func() {
+		defer close(ret)
+		matchingIssues, err := m.m.FromQuery(hashtag)
+		if err != nil {
+			sklog.Errorf("Failed to build Monorail search: %s", err)
+			return
+		}
+		for _, issue := range matchingIssues {
+			ret <- source.Artifact{
+				Title:        issue.Title,
+				URL:          "",
+				LastModified: time.Now(),
+				Kind:         source.Monorail,
+			}
+		}
+	}()
+
+	return ret
+}
+func (m *monorailSource) ByUser(string) <-chan source.Artifact {
+	ret := make(chan source.Artifact)
+	close(ret)
+	return ret
+}
