7354d8504c6146629fb22588f6b583164d46603b
Joe Gregorio <jcgregorio@google.com>
2019-10-23 13:08:15 -0400

    hashtag - Start sketching the hashtag service.
    Change-Id: I1ee3ebd7ac620d6ccdd2640e3c6733c63ea3daed

---
+++ go/monorail/v1/rebuild.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Script to rebuild the androidbuildinternal.go file.
+
+# Get an updated generator.
+go get -u google.golang.org/api/google-api-go-generator@v0.10.0
+
+# Retrieve the discovery document for the API.
+wget https://monorail-prod.appspot.com/_ah/api/discovery/v1/apis/monorail/v1/rest
+
+# Generate the Go file.
+google-api-go-generator \
+  -api_json_file=rest \
+  -api_pkg_base="go.skia.org/infra/go" \
+  -output="monorail.go"
+
+# Clean up the generated file.
+goimports -w .
+
+# Cleanup.
+rm rest
---
+++ hashtag/README.md
@@ -0,0 +1,6 @@
+# Project HashTag
+
+A system for gathering disparate data such as bugs, CLs, and Google Drive
+documents into a comprehensive view on project status.
+
+  * [Design Doc](http://go/skia-hashtag)
