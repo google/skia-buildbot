afeaff8e3ab33d2b5c7495bd2f29702d3df2cbec
Joe Gregorio <jcgregorio@google.com>
2019-10-23 14:56:02 -0400

    hashtag - Initial commit of hashtag code.
    Bug: skia:9559
    Change-Id: I1ee3ebd7ac620d6ccdd2640e3c6733c63ea3daed

--- go/monorail/v1/rebuild.sh
+++ go/monorail/v1/rebuild.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# Script to rebuild the androidbuildinternal.go file.
+# Script to rebuild the monorail.go file.
 
 # Get an updated generator. Pin to v0.10.0 because https://github.com/googleapis/google-api-go-client/issues/416.
 go get -u google.golang.org/api/google-api-go-generator@v0.10.0
 
 # Retrieve the discovery document for the API.
-wget https://www.googleapis.com/discovery/v1/apis/androidbuildinternal/v2beta1/rest
+wget https://monorail-prod.appspot.com/_ah/api/discovery/v1/apis/monorail/v1/rest
 
 # Generate the Go file.
 google-api-go-generator \
   -api_json_file=rest \
   -api_pkg_base="go.skia.org/infra/go" \
-  -output="androidbuildinternal.go"
+  -output="monorail.go"
 
 # Clean up the generated file.
 goimports -w .
