7e691d9d7aa2f4c62492b19f3b9ba84eca6819a7
Kevin Lubick <kjlubick@google.com>
2019-10-24 08:03:12 -0400

    [gold] Performance enhancements for /digests
    Bug: skia:9557,skia:9080
    Change-Id: Ifaf14917d8fa3fa9b5a22c85b3ab9b2bbef914a4

--- golden/go/types/types_test.go
+++ golden/go/types/types_test.go
@@ -3,6 +3,8 @@ package types
 import (
 	"testing"
 
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"go.skia.org/infra/go/testutils/unittest"
 	"go.skia.org/infra/go/tiling"
 )
@@ -11,82 +13,47 @@ func TestGoldenTrace(t *testing.T) {
 	unittest.SmallTest(t)
 	N := 5
 	// Test NewGoldenTrace.
-	g := NewGoldenTraceN(N)
-	if got, want := g.Len(), N; got != want {
-		t.Errorf("Wrong Digests Size: Got %v Want %v", got, want)
-	}
-	if got, want := len(g.Keys), 0; got != want {
-		t.Errorf("Wrong Keys initial size: Got %v Want %v", got, want)
-	}
-
+	g := NewGoldenTraceN(N, nil)
+	assert.Equal(t, N, g.Len(), "wrong digests size")
+	assert.Equal(t, 0, len(g.Keys), "wrong keys initial size")
 	g.Digests[0] = "a digest"
 
-	if got, want := g.IsMissing(1), true; got != want {
-		t.Errorf("All values should start as missing: Got %v Want %v", got, want)
-	}
-	if got, want := g.IsMissing(0), false; got != want {
-		t.Errorf("Set values shouldn't be missing: Got %v Want %v", got, want)
-	}
+	assert.True(t, g.IsMissing(1), "values start missing")
+	assert.False(t, g.IsMissing(0), "set values shouldn't be missing")
 
 	// Test Merge.
 	M := 7
-	gm := NewGoldenTraceN(M)
+	gm := NewGoldenTraceN(M, nil)
 	gm.Digests[1] = "another digest"
 	g2 := g.Merge(gm)
-	if got, want := g2.Len(), N+M; got != want {
-		t.Errorf("Merge length wrong: Got %v Want %v", got, want)
-	}
-	if got, want := g2.(*GoldenTrace).Digests[0], Digest("a digest"); got != want {
-		t.Errorf("Digest not copied correctly: Got %v Want %v", got, want)
-	}
-	if got, want := g2.(*GoldenTrace).Digests[6], Digest("another digest"); got != want {
-		t.Errorf("Digest not copied correctly: Got %v Want %v", got, want)
-	}
+	assert.Equal(t, N+M, g2.Len(), "merge length wrong")
+	assert.Equal(t, Digest("a digest"), g2.(*GoldenTrace).Digests[0])
+	assert.Equal(t, Digest("another digest"), g2.(*GoldenTrace).Digests[6])
 
 	// Test Grow.
-	g = NewGoldenTraceN(N)
+	g = NewGoldenTraceN(N, nil)
 	g.Digests[0] = "foo"
 	g.Grow(2*N, tiling.FILL_BEFORE)
-	if got, want := g.Digests[N], Digest("foo"); got != want {
-		t.Errorf("Grow didn't FILL_BEFORE correctly: Got %v Want %v", got, want)
-	}
+	assert.Equal(t, Digest("foo"), g.Digests[N], "Grow didn't FILL_BEFORE correctly")
 
-	g = NewGoldenTraceN(N)
+	g = NewGoldenTraceN(N, nil)
 	g.Digests[0] = "foo"
 	g.Grow(2*N, tiling.FILL_AFTER)
-	if got, want := g.Digests[0], Digest("foo"); got != want {
-		t.Errorf("Grow didn't FILL_AFTER correctly: Got %v Want %v", got, want)
-	}
+	assert.Equal(t, Digest("foo"), g.Digests[0], "Grow didn't FILL_AFTER correctly")
 
 	// Test Trim
-	g = NewGoldenTraceN(N)
+	g = NewGoldenTraceN(N, nil)
 	g.Digests[1] = "foo"
-	if err := g.Trim(1, 3); err != nil {
-		t.Fatalf("Trim Failed: %s", err)
-	}
-	if got, want := g.Digests[0], Digest("foo"); got != want {
-		t.Errorf("Trim didn't copy correctly: Got %v Want %v", got, want)
-	}
-	if got, want := g.Len(), 2; got != want {
-		t.Errorf("Trim wrong length: Got %v Want %v", got, want)
-	}
+	require.NoError(t, g.Trim(1, 3))
+	assert.Equal(t, Digest("foo"), g.Digests[0], "Trim didn't copy correctly")
+	assert.Equal(t, 2, g.Len(), "Trim wrong length")
 
-	if err := g.Trim(-1, 1); err == nil {
-		t.Error("Trim failed to error.")
-	}
-	if err := g.Trim(1, 3); err == nil {
-		t.Error("Trim failed to error.")
-	}
-	if err := g.Trim(2, 1); err == nil {
-		t.Error("Trim failed to error.")
-	}
+	assert.Error(t, g.Trim(-1, 1))
+	assert.Error(t, g.Trim(1, 3))
+	assert.Error(t, g.Trim(2, 1))
 
-	if err := g.Trim(1, 1); err != nil {
-		t.Fatalf("Trim Failed: %s", err)
-	}
-	if got, want := g.Len(), 0; got != want {
-		t.Errorf("Trim wrong length: Got %v Want %v", got, want)
-	}
+	require.NoError(t, g.Trim(1, 1))
+	assert.Equal(t, 0, g.Len(), "final size wrong")
 }
 
 func TestSetAt(t *testing.T) {
@@ -104,15 +71,11 @@ func TestSetAt(t *testing.T) {
 			want: MISSING_DIGEST,
 		},
 	}
-	tr := NewGoldenTraceN(len(testCases))
+	tr := NewGoldenTraceN(len(testCases), nil)
 	for i, tc := range testCases {
-		if err := tr.SetAt(i, []byte(tc.want)); err != nil {
-			t.Fatalf("SetAt(%d, %#v) failed: %s", i, []byte(tc.want), err)
-		}
+		require.NoError(t, tr.SetAt(i, []byte(tc.want)))
 	}
 	for i, tc := range testCases {
-		if got, want := tr.Digests[i], tc.want; got != want {
-			t.Errorf("SetAt(%d, %#v)failed: Got %s Want %s", i, []byte(tc.want), got, want)
-		}
+		assert.Equal(t, tc.want, tr.Digests[i], "Bad at test case %d", i)
 	}
 }
