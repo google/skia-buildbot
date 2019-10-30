7488f3b14171bdc9bf2f810d85e1551985585e9c
Ravi Mistry <rmistry@google.com>
2019-10-30 10:40:58 -0400

    [alert-manager] Add regex support to silences
    Screenshot: https://screenshot.googleplex.com/XWXO8FTbyHy
    
    Bug: skia:9587
    Change-Id: Iad6d97f76f87147bcc00fd9033fe68bcc7d1365d

--- am/modules/paramset/index.js
+++ am/modules/paramset/index.js
@@ -22,18 +22,32 @@ export function add(paramset, params, ignored = []) {
 /**
  * Determines if the params given match the ParamSet.
  *
- * @param {Object} paramset - Key-value pairs where keys are strings and values are arrays of strings.
+ * @param {Object} paramset - Key-value pairs where keys are strings and values are arrays of strings
+ *   or regexes (see skbug.com/9587).
  * @param {Object} params - Key-value pairs where both keys and values are strings.
  *
  * @return {bool} True if every key in ParamSet is present in 'params' and
- *   the value seen in params is in the ParamSet values.
+ *   the value seen in params is in the ParamSet values or it matches one of
+ *   the regexes.
  */
 export function match(paramset, params) {
-  for (let key in paramset) {
-    if (!paramset[key].includes(params[key])) {
-      return false
+  for (const key in paramset) {
+    if (paramset[key].includes(params[key])) {
+      continue;
+    }
+    const values = paramset[key] || [];
+    let valMatched = false;
+    for (const i in values) {
+      const re = new RegExp("^" + values[i] + "$")
+      if (re.test(params[key])) {
+        valMatched = true;
+        break;
+      }
+    }
+    if (!valMatched) {
+      return false;
     }
   }
-  return true
+  return true;
 }
 
