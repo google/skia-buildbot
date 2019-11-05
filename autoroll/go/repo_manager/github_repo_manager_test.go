a97c2eda4f8df89698d1b08ede4e77569a2bab37
Ravi Mistry <rmistry@google.com>
2019-11-05 11:23:01 -0500

    [Autoroller] Checkpoint
    Change-Id: I77c41ba4a87dd342a65b1ec50106c2088b453540

--- autoroll/go/repo_manager/github_repo_manager_test.go
+++ autoroll/go/repo_manager/github_repo_manager_test.go
@@ -26,10 +26,8 @@ import (
 
 func githubCR(t *testing.T, g *github.GitHub) codereview.CodeReview {
 	rv, err := (&codereview.GithubConfig{
-		RepoOwner:     "me",
-		RepoName:      "my-repo",
-		ChecksNum:     3,
-		ChecksWaitFor: []string{"a", "b", "c"},
+		RepoOwner: "me",
+		RepoName:  "my-repo",
 	}).Init(nil, g)
 	require.NoError(t, err)
 	return rv
@@ -128,7 +126,7 @@ func setupFakeGithub(t *testing.T, childCommits []string) (*github.GitHub, *mock
 	urlMock.MockOnce(githubApiUrl+"/repos/superman/krypton/issues/12345", mockhttpclient.MockGetDialogue(serializedIssue))
 	patchRespBody := []byte(testutils.MarshalJSON(t, &github_api.PullRequest{}))
 	patchReqType := "application/json"
-	patchReqBody := []byte(`{"labels":["autoroller: commit"]}
+	patchReqBody := []byte(`{"labels":["waiting for tree to go green"]}
 `)
 	patchMd := mockhttpclient.MockPatchDialogue(patchReqType, patchReqBody, patchRespBody)
 	urlMock.MockOnce(githubApiUrl+"/repos/superman/krypton/issues/12345", patchMd)
