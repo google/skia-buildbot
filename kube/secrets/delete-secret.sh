60b059b902381f7fee6919d1b94427ac457f31c4
Joe Gregorio <jcgregorio@google.com>
2019-11-08 17:05:51 -0500

    Start on skolo_rpi2 rack as a kubernetes cluster.
    Change-Id: I391eb61d33e88d0ec72271ad3775049bd45a74eb

---
+++ kube/secrets/delete-secret.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Delete a secret from berglas.
+
+if [ $# -ne 2 ]; then
+    echo "$0 <cluster-name> <secret-name>"
+    exit 1
+fi
+
+CLUSTER=$1
+SECRET_NAME=$2
+
+REL=$(dirname "$0")
+source ${REL}/config.sh
+
+berglas delete ${BUCKET_ID}/${CLUSTER}/${SECRET_NAME}
