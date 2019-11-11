93799fc14e532c1c6d6bbf58a13ade2e07e27d37
Joe Gregorio <jcgregorio@google.com>
2019-11-11 11:23:54 -0500

    Start on skolo_rpi2 rack as a kubernetes cluster.
    Change-Id: I391eb61d33e88d0ec72271ad3775049bd45a74eb

---
+++ kube/secrets/init-berglas.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Initializes the berglas secret storage.
+
+REL=$(dirname "$0")
+source ${REL}/config.sh
+
+gcloud services enable --project ${PROJECT_ID} \
+  cloudkms.googleapis.com \
+  storage-api.googleapis.com \
+  storage-component.googleapis.com
+
+berglas bootstrap --project $PROJECT_ID --bucket $BUCKET_ID
