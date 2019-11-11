03fad3dba156ea48642c481241676f7005131ca8
Kevin Lubick <kjlubick@google.com>
2019-11-11 11:57:28 -0500

    [gold] Add GitHub code_review client
    PS 1 adds ChangeList queries
    PS 2 adds PatchSets and the reverse lookup
    PS 3+ rounds out the testing and docs
    
    Bug: skia:9008
    Change-Id: I74ea28852f06eeeb6cad11a116a5867d0ce1efae

---
+++ golden/go/code_review/github_crs/github_crs.go
@@ -0,0 +1,186 @@
+// Package github_crs provides a client for Gold's interaction with
+// the GitHub code review system.
+package github_crs
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"regexp"
+	"strconv"
+	"time"
+
+	"go.skia.org/infra/go/skerr"
+	"go.skia.org/infra/go/sklog"
+	"go.skia.org/infra/go/util"
+	"go.skia.org/infra/go/vcsinfo"
+	"go.skia.org/infra/golden/go/code_review"
+	"golang.org/x/time/rate"
+)
+
+const (
+	// Authenticated clients can do up to 5000 queries per hour. These limits
+	// are conservative based on that.
+	maxQPS   = rate.Limit(1)
+	maxBurst = 100
+)
+
+type CRSImpl struct {
+	client *http.Client
+	rl     *rate.Limiter
+	repo   string
+}
+
+// New returns a new instance of CRSImpl, ready to target a single
+// GitHub repo. repo should be the user/repo, e.g. "google/skia",
+// "flutter/flutter", etc.
+func New(client *http.Client, repo string) *CRSImpl {
+	return &CRSImpl{
+		client: client,
+		rl:     rate.NewLimiter(maxQPS, maxBurst),
+		repo:   repo,
+	}
+}
+
+type user struct {
+	UserName string `json:"login"`
+}
+
+// See https://developer.github.com/v3/pulls/#get-a-single-pull-request
+type pullRequestResponse struct {
+	Title   string `json:"title"`
+	User    user   `json:"user"`
+	State   string `json:"state"`
+	Updated string `json:"updated_at"` // e.g.  "2011-01-26T19:01:12Z"
+	Merged  string `json:"merged_at"`
+}
+
+// GetChangeList implements the code_review.Client interface.
+func (c *CRSImpl) GetChangeList(ctx context.Context, id string) (code_review.ChangeList, error) {
+	if _, err := strconv.ParseInt(id, 10, 64); err != nil {
+		return code_review.ChangeList{}, skerr.Fmt("invalid ChangeList ID")
+	}
+	// Respect the rate limit.
+	if err := c.rl.Wait(ctx); err != nil {
+		return code_review.ChangeList{}, skerr.Wrap(err)
+	}
+	u := fmt.Sprintf("https://api.github.com/repos/%s/pulls/%s", c.repo, id)
+	// TODO(kjlubick): use https://golang.org/pkg/net/http/#NewRequestWithContext
+	resp, err := c.client.Get(u)
+	if err != nil {
+		sklog.Errorf("Error getting ChangeList from %s: %s", u, err)
+		// Assume an error here is the ChangeList is not found
+		return code_review.ChangeList{}, code_review.ErrNotFound
+	}
+	defer util.Close(resp.Body)
+
+	var prr pullRequestResponse
+	err = json.NewDecoder(resp.Body).Decode(&prr)
+	if err != nil {
+		return code_review.ChangeList{}, skerr.Wrapf(err, "received invalid JSON from GitHub: %s", u)
+	}
+
+	state := code_review.Open
+	if prr.State == "closed" {
+		if prr.Merged != "" {
+			state = code_review.Landed
+		} else {
+			state = code_review.Abandoned
+		}
+	}
+
+	updated, err := time.Parse(time.RFC3339, prr.Updated)
+	if err != nil {
+		return code_review.ChangeList{}, skerr.Wrapf(err, "invalid time %q", prr.Updated)
+	}
+
+	return code_review.ChangeList{
+		SystemID: id,
+		Owner:    prr.User.UserName,
+		Subject:  prr.Title,
+		Status:   state,
+		Updated:  updated,
+	}, nil
+}
+
+type commit struct {
+	Hash string `json:"sha"`
+}
+
+// https://developer.github.com/v3/pulls/#list-commits-on-a-pull-request
+type commitsOnPullRequestResponse []commit
+
+// GetPatchSets implements the code_review.Client interface.
+func (c *CRSImpl) GetPatchSets(ctx context.Context, clID string) ([]code_review.PatchSet, error) {
+	if _, err := strconv.ParseInt(clID, 10, 64); err != nil {
+		return nil, skerr.Fmt("invalid ChangeList ID")
+	}
+	// Respect the rate limit.
+	if err := c.rl.Wait(ctx); err != nil {
+		return nil, skerr.Wrap(err)
+	}
+	u := fmt.Sprintf("https://api.github.com/repos/%s/pulls/%s/commits", c.repo, clID)
+	// TODO(kjlubick): use https://golang.org/pkg/net/http/#NewRequestWithContext
+	resp, err := c.client.Get(u)
+	if err != nil {
+		sklog.Errorf("Error getting commits on PR %s with url %s: %s", clID, u, err)
+		// Assume an error here is the ChangeList is not found
+		return nil, code_review.ErrNotFound
+	}
+	defer util.Close(resp.Body)
+
+	var cprr commitsOnPullRequestResponse
+	err = json.NewDecoder(resp.Body).Decode(&cprr)
+	if err != nil {
+		return nil, skerr.Wrapf(err, "received invalid JSON from GitHub: %s", u)
+	}
+
+	// Assume GitHub returns these in ascending order
+	var xps []code_review.PatchSet
+	for i, c := range cprr {
+		xps = append(xps, code_review.PatchSet{
+			SystemID:     c.Hash,
+			ChangeListID: clID,
+			Order:        i + 1,
+			GitHash:      c.Hash,
+		})
+	}
+
+	return xps, nil
+}
+
+// GetChangeListForCommit implements the code_review.Client interface.
+func (c *CRSImpl) GetChangeListForCommit(ctx context.Context, commit *vcsinfo.LongCommit) (code_review.ChangeList, error) {
+	if commit == nil {
+		return code_review.ChangeList{}, skerr.Fmt("commit cannot be nil")
+	}
+	id, err := extractPRFromTitle(commit.Subject)
+	if err != nil {
+		return code_review.ChangeList{}, skerr.Wrap(err)
+	}
+
+	return c.GetChangeList(ctx, id)
+}
+
+// We assume a PR has the pull request number in the Subject/Title, at the end.
+// e.g. "Turn off docs upload temporarily (#44365) (#44413)" refers to PR 44413
+var prSuffix = regexp.MustCompile(`.+\(#(?P<id>\d+)\)\s*$`)
+
+// extractPRFromTitle returns the pull request id extracted from the title
+// of a landed PR, or an error if it cannot.
+func extractPRFromTitle(t string) (string, error) {
+	if match := prSuffix.FindStringSubmatch(t); match != nil {
+		// match[0] is the whole string, match[1] is the first group
+		return match[1], nil
+	}
+	return "", skerr.Fmt("Could not find PR in Subject %q", t)
+}
+
+// System implements the code_review.Client interface.
+func (c *CRSImpl) System() string {
+	return "github"
+}
+
+// Make sure CRSImpl fulfills the code_review.Client interface.
+var _ code_review.Client = (*CRSImpl)(nil)
---
+++ golden/go/code_review/github_crs/github_crs_test.go
@@ -0,0 +1,268 @@
+package github_crs
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.skia.org/infra/go/mockhttpclient"
+	"go.skia.org/infra/go/testutils/unittest"
+	"go.skia.org/infra/go/vcsinfo"
+	"go.skia.org/infra/golden/go/code_review"
+)
+
+func TestGetChangeListSunnyDay(t *testing.T) {
+	unittest.SmallTest(t)
+
+	m := mockhttpclient.NewURLMock()
+	resp := mockhttpclient.MockGetDialogue([]byte(landedPullRequestResponse))
+	m.Mock("https://api.github.com/repos/unit/test/pulls/44380", resp)
+	c := New(m.Client(), "unit/test")
+
+	id := "44380"
+	ts := time.Date(2019, time.November, 7, 23, 39, 17, 0, time.UTC)
+
+	cl, err := c.GetChangeList(context.Background(), id)
+	require.NoError(t, err)
+	assert.Equal(t, code_review.ChangeList{
+		SystemID: id,
+		Owner:    "engine-flutter-autoroll",
+		Status:   code_review.Landed,
+		Subject:  "Roll engine ddceed5f7af1..629930e8887c (1 commits) (#44380)",
+		Updated:  ts,
+	}, cl)
+}
+
+func TestGetChangeListOpen(t *testing.T) {
+	unittest.SmallTest(t)
+
+	m := mockhttpclient.NewURLMock()
+	resp := mockhttpclient.MockGetDialogue([]byte(openPullRequestResponse))
+	m.Mock("https://api.github.com/repos/unit/test/pulls/44380", resp)
+	c := New(m.Client(), "unit/test")
+
+	id := "44380"
+	ts := time.Date(2019, time.November, 7, 23, 39, 17, 0, time.UTC)
+
+	cl, err := c.GetChangeList(context.Background(), id)
+	require.NoError(t, err)
+	assert.Equal(t, code_review.ChangeList{
+		SystemID: id,
+		Owner:    "engine-flutter-autoroll",
+		Status:   code_review.Open,
+		Subject:  "Roll engine ddceed5f7af1..629930e8887c (1 commits)",
+		Updated:  ts,
+	}, cl)
+}
+
+func TestGetChangeListAbandoned(t *testing.T) {
+	unittest.SmallTest(t)
+
+	m := mockhttpclient.NewURLMock()
+	resp := mockhttpclient.MockGetDialogue([]byte(abandonedPullRequestResponse))
+	m.Mock("https://api.github.com/repos/unit/test/pulls/44345", resp)
+	c := New(m.Client(), "unit/test")
+
+	id := "44345"
+	ts := time.Date(2019, time.November, 7, 14, 14, 0, 0, time.UTC)
+
+	cl, err := c.GetChangeList(context.Background(), id)
+	require.NoError(t, err)
+	assert.Equal(t, code_review.ChangeList{
+		SystemID: id,
+		Owner:    "a-user",
+		Status:   code_review.Abandoned,
+		Subject:  "Make BorderRadius.circular() a const constructor",
+		Updated:  ts,
+	}, cl)
+}
+
+func TestGetChangeListDoesNotExist(t *testing.T) {
+	unittest.SmallTest(t)
+
+	m := mockhttpclient.NewURLMock()
+	// By not mocking anything, an error will be returned from GitHub,
+	// as we would expect for a 404
+	c := New(m.Client(), "unit/test")
+
+	_, err := c.GetChangeList(context.Background(), "44345")
+	require.Error(t, err)
+	assert.Equal(t, code_review.ErrNotFound, err)
+}
+
+func TestGetChangeListInvalidID(t *testing.T) {
+	unittest.SmallTest(t)
+
+	c := New(nil, "unit/test")
+
+	_, err := c.GetChangeList(context.Background(), "bad")
+	require.Error(t, err)
+	assert.Contains(t, err.Error(), "invalid")
+}
+
+func TestGetPatchSetsSunnyDay(t *testing.T) {
+	unittest.SmallTest(t)
+
+	m := mockhttpclient.NewURLMock()
+	resp := mockhttpclient.MockGetDialogue([]byte(fiveCommitsOnPullRequestResponse))
+	m.Mock("https://api.github.com/repos/unit/test/pulls/44419/commits", resp)
+	c := New(m.Client(), "unit/test")
+
+	id := "44419"
+
+	xps, err := c.GetPatchSets(context.Background(), id)
+	require.NoError(t, err)
+	assert.Equal(t, []code_review.PatchSet{
+		{
+			SystemID:     "a892f9f299e91924405cb8bd244efc1a6c28e4fa",
+			ChangeListID: id,
+			Order:        1,
+			GitHash:      "a892f9f299e91924405cb8bd244efc1a6c28e4fa",
+		},
+		{
+			SystemID:     "042f0382b7ec0efdb7570c3e6c891cf2a20379a7",
+			ChangeListID: id,
+			Order:        2,
+			GitHash:      "042f0382b7ec0efdb7570c3e6c891cf2a20379a7",
+		},
+		{
+			SystemID:     "a332b7085723c13fa96777a1830c7113e7ffba96",
+			ChangeListID: id,
+			Order:        3,
+			GitHash:      "a332b7085723c13fa96777a1830c7113e7ffba96",
+		},
+		{
+			SystemID:     "d3e3d639d8a1cca0929829b04d90f35011b50fbf",
+			ChangeListID: id,
+			Order:        4,
+			GitHash:      "d3e3d639d8a1cca0929829b04d90f35011b50fbf",
+		},
+		{
+			SystemID:     "74a239b99ee360397a22cede6d9d16aacd245af1",
+			ChangeListID: id,
+			Order:        5,
+			GitHash:      "74a239b99ee360397a22cede6d9d16aacd245af1",
+		},
+	}, xps)
+}
+
+func TestGetPatchSetsDoesNotExist(t *testing.T) {
+	unittest.SmallTest(t)
+
+	m := mockhttpclient.NewURLMock()
+	// By not mocking anything, an error will be returned from Git,
+	// as we would expect for a 404
+	c := New(m.Client(), "unit/test")
+
+	_, err := c.GetPatchSets(context.Background(), "44345")
+	require.Error(t, err)
+	require.Equal(t, code_review.ErrNotFound, err)
+}
+
+func TestGetChangeListForCommitSunnyDay(t *testing.T) {
+	unittest.SmallTest(t)
+
+	m := mockhttpclient.NewURLMock()
+	resp := mockhttpclient.MockGetDialogue([]byte(landedPullRequestResponse))
+	m.Mock("https://api.github.com/repos/unit/test/pulls/44380", resp)
+	c := New(m.Client(), "unit/test")
+
+	cl, err := c.GetChangeListForCommit(context.Background(), &vcsinfo.LongCommit{
+		// This is the only field the implementation cares about.
+		ShortCommit: &vcsinfo.ShortCommit{
+			Subject: "Roll engine ddceed5f7af1..629930e8887c (1 commits) (#44380)",
+		},
+	})
+	require.NoError(t, err)
+	assert.Equal(t, code_review.ChangeList{
+		SystemID: "44380",
+		Owner:    "engine-flutter-autoroll",
+		Status:   code_review.Landed,
+		Subject:  "Roll engine ddceed5f7af1..629930e8887c (1 commits) (#44380)",
+		Updated:  time.Date(2019, time.November, 7, 23, 39, 17, 0, time.UTC),
+	}, cl)
+}
+
+func TestGetPatchSetsInvalidID(t *testing.T) {
+	unittest.SmallTest(t)
+
+	c := New(nil, "unit/test")
+
+	_, err := c.GetPatchSets(context.Background(), "bad")
+	require.Error(t, err)
+	assert.Contains(t, err.Error(), "invalid")
+}
+
+func TestGetChangeListForCommitMalformed(t *testing.T) {
+	unittest.SmallTest(t)
+
+	c := New(nil, "unit/test")
+
+	_, err := c.GetChangeListForCommit(context.Background(), &vcsinfo.LongCommit{
+		// This is the only field the implementation cares about.
+		ShortCommit: &vcsinfo.ShortCommit{
+			Subject: "Roll engine ddceed5f7af1..629930e8887c (1 commits)",
+		},
+	})
+	require.Error(t, err)
+	assert.Contains(t, err.Error(), "not find PR")
+}
+
+// There's a lot more data here, but these JSON strings contain
+// only the fields which we care about.
+// This is based on https://github.com/flutter/flutter/pull/44380
+const landedPullRequestResponse = `
+{
+	"title": "Roll engine ddceed5f7af1..629930e8887c (1 commits) (#44380)",
+	"state": "closed",
+	"user": {
+		"login": "engine-flutter-autoroll"
+	},
+	"updated_at": "2019-11-07T23:39:17Z",
+	"merged_at": "2019-11-07T23:39:17Z"
+}`
+
+const openPullRequestResponse = `
+{
+	"title": "Roll engine ddceed5f7af1..629930e8887c (1 commits)",
+	"state": "open",
+	"user": {
+		"login": "engine-flutter-autoroll"
+	},
+	"updated_at": "2019-11-07T23:39:17Z",
+	"merged_at": null
+}`
+
+// This is based on https://github.com/flutter/flutter/pull/44345
+const abandonedPullRequestResponse = `
+{
+	"title": "Make BorderRadius.circular() a const constructor",
+	"state": "closed",
+	"user": {
+		"login": "a-user"
+	},
+	"updated_at": "2019-11-07T14:14:00Z",
+	"merged_at": null
+}`
+
+// This is based on https://github.com/flutter/flutter/pull/44419
+const fiveCommitsOnPullRequestResponse = `
+[
+  {
+    "sha": "a892f9f299e91924405cb8bd244efc1a6c28e4fa"
+  },
+  {
+    "sha": "042f0382b7ec0efdb7570c3e6c891cf2a20379a7"
+  },
+  {
+    "sha": "a332b7085723c13fa96777a1830c7113e7ffba96"
+  },
+  {
+    "sha": "d3e3d639d8a1cca0929829b04d90f35011b50fbf"
+  },
+  {
+    "sha": "74a239b99ee360397a22cede6d9d16aacd245af1"
+  }
+]`
