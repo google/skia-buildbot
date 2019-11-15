20a3b19dc167e205777e98260823626e185bd87a
Joe Gregorio <joe@bitworking.org>
2019-11-15 09:07:50 -0800

    Use berglas for secrets handling.
    See README.md for more details along with http://go/skia-infra-multi-k8s.
    
    Change-Id: Ie0e768b5213879ef692a848234e356c26cea3960

---
+++ kube/secrets/edit-secret.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Retrieve a secret from berglas, edit, then write back to berglas.
+
+if [ $# -ne 2 ]; then
+    echo "$0 <cluster-name> <secret-name>"
+    exit 1
+fi
+
+REL=$(dirname "$0")
+source ${REL}/config.sh
+
+CLUSTER=$1
+SECRET_NAME=$2
+
+source ../../bash/ramdisk.sh
+
+FILES=$(${REL}/get-secret.sh ${CLUSTER} ${SECRET_NAME} \
+  | kubectl apply -f - --dry-run -o json \
+  | jq -j '.data | keys | .[] | . + " "')
+echo ${FILES}
+for FILE in ${FILES}; do
+  ${REL}/get-secret.sh ${CLUSTER} ${SECRET_NAME} \
+  | kubectl apply -f - --dry-run -o json \
+  | jq -r ".data[\"${FILE}\"]" \
+  | base64 -d > /tmp/ramdisk/${FILE}
+done
+
+echo "Downloaded the ${SECRET_NAME} secrets to /tmp/ramdisk."
+echo ""
+read -r -p "Press enter when you are done editing..." key
+
+${REL}/add-secret-from-directory.sh . ${CLUSTER} ${SECRET_NAME}
---
+++ kube/secrets/get-secret.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Retrieve a secret from berglas.
+
+if [ $# -ne 2 ]; then
+    echo "$0 <cluster-name> <secret-name>"
+    exit 1
+fi
+
+CLUSTER=$1
+SECRET_NAME=$2
+
+REL=$(dirname "$0")
+source ${REL}/config.sh
+
+berglas access ${BUCKET_ID}/${CLUSTER}/${SECRET_NAME} \
+| base64 --decode
