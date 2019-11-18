911446f656578a502cee6be2e2183ce9a5699666
Joe Gregorio <jcgregorio@google.com>
2019-11-18 12:53:03 -0500

    [hashtag] Add controls for full query.
    Breaks out the HTML form values that represent a query to
    be a different thing than source.Query, which is the query
    that is constructed from those form values.
    
    Also update each Source so that it uses all the info
    supplied in source.Query, if applicable.
    
    Screenshots:
      https://screenshot.googleplex.com/VU5ccQe7NTY.png
      https://screenshot.googleplex.com/yHx2vaY4XXr.png
    
    Bug: skia:9559
    Change-Id: I1733896e0782ebfa18049149923dea3117032497

--- hashtag/go/drivesource/drivesource.go
+++ hashtag/go/drivesource/drivesource.go
@@ -3,6 +3,7 @@ package drivesource
 import (
 	"context"
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/spf13/viper"
@@ -27,6 +28,26 @@ func New() (source.Source, error) {
 	}, nil
 }
 
+// toString converts a source.Query into a search string for the Drive API.
+func (d *driveSource) toString(q source.Query) string {
+	ret := []string{}
+	// Documents stored in a Teams folder have their owners changed to the group
+	// that owns the team drive, so we always just do a fullText search.
+	ret = append(ret, fmt.Sprintf("fullText contains '%s'", q.Value))
+	/*
+		TODO(jcgregorio) This code currently makes the query invalid. I have no idea why.
+
+		if !q.Begin.IsZero() {
+			ret = append(ret, fmt.Sprintf("modifiedTime>'%s'", q.Begin.Format("2006-01-02")))
+		}
+		if !q.End.IsZero() {
+			ret = append(ret, fmt.Sprintf("modifiedTime<'%s'", q.End.Format("2006-01-02")))
+		}
+	*/
+	sklog.Infof("Drive: %q", strings.Join(ret, " AND "))
+	return strings.Join(ret, " AND ")
+}
+
 // See source.Source.
 func (d *driveSource) Search(ctx context.Context, q source.Query) <-chan source.Artifact {
 	ret := make(chan source.Artifact)
@@ -39,7 +60,7 @@ func (d *driveSource) Search(ctx context.Context, q source.Query) <-chan source.
 			DriveId(viper.GetString("sources.drive.id")).
 			IncludeItemsFromAllDrives(true).
 			SupportsAllDrives(true).
-			Q(fmt.Sprintf("fullText contains %q", q.Value)).
+			Q(d.toString(q)).
 			Do()
 		if err != nil {
 			sklog.Errorf("Failed to make Drive request: %s", err)
