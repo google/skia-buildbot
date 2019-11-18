930d1a818d65419666090864f28eea1970ce2698
Joe Gregorio <jcgregorio@google.com>
2019-11-18 12:49:16 -0500

    [hashtag] Add controls for full query.
    Breaks our the HTML form values that represent a query to
    be a different thing than source.Query, which is the query
    that is constructed from those form values.
    
    Also update each Source so that it uses all the info
    supplied in source.Query, if applicable.
    
    Screenshots:
      https://screenshot.googleplex.com/VU5ccQe7NTY.png
      https://screenshot.googleplex.com/yHx2vaY4XXr.png
    
    Bug: skia:9559
    Change-Id: I1733896e0782ebfa18049149923dea3117032497

--- hashtag/go/hashtag/main.go
+++ hashtag/go/hashtag/main.go
@@ -6,12 +6,14 @@ import (
 	"strings"
 	"sync"
 	"text/template"
+	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/spf13/viper"
 	"github.com/unrolled/secure"
 	"go.skia.org/infra/go/allowed"
 	"go.skia.org/infra/go/baseapp"
+	"go.skia.org/infra/go/httputils"
 	"go.skia.org/infra/go/login"
 	"go.skia.org/infra/go/sklog"
 	"go.skia.org/infra/hashtag/go/codesearchsource"
@@ -105,6 +107,15 @@ type result struct {
 	Artifacts   []source.Artifact
 }
 
+// Form is the values in the search form. Form{} will give you the default values.
+type Form struct {
+	Type  string
+	Value string
+	Range string
+	Begin string
+	End   string
+}
+
 // TemplateContext is the context for the index.html template.
 type TemplateContext struct {
 	// Nonce is the CSP Nonce. Look in webpack.config.js for where the nonce
@@ -114,14 +125,14 @@ type TemplateContext struct {
 	// IsSearch is true if we contain search results.
 	IsSearch bool
 
-	// Query is the search query made if IsSearch is true.
-	Query source.Query
-
 	// Hashtags is the list of "official" hashtags.
 	Hashtags []string
 
 	// Results of the search.
 	Results []result
+
+	// Form contains the values of the search form.
+	Form Form
 }
 
 func (srv *server) indexHandler(w http.ResponseWriter, r *http.Request) {
@@ -134,15 +145,58 @@ func (srv *server) indexHandler(w http.ResponseWriter, r *http.Request) {
 		// Look in webpack.config.js for where the nonce templates are injected.
 		Nonce:    secure.CSPNonce(r.Context()),
 		Hashtags: viper.GetStringSlice("hashtags"),
-		Query:    source.Query{},
 	}
 
-	hashtag := strings.TrimSpace(r.FormValue("hashtag"))
-	if hashtag != "" {
-		templateContext.Query.Type = source.HashtagQuery
-		templateContext.Query.Value = hashtag
+	value := strings.TrimSpace(r.FormValue("value"))
+	if value != "" {
+		templateContext.Form.Value = value
+		templateContext.Form.Begin = r.FormValue("begin")
+		templateContext.Form.End = r.FormValue("end")
+		templateContext.Form.Range = r.FormValue("range")
+		queryType := source.QueryType(r.FormValue("type"))
+		if queryType == "" {
+			queryType = source.HashtagQuery
+		}
+		templateContext.Form.Type = string(queryType)
+		// TODO(jcgregorio) If email Value isn't formatted as an email then
+		// append "@google.com".
 		templateContext.IsSearch = true
 		templateContext.Results = make([]result, len(srv.sources))
+
+		query := source.Query{
+			Type:  queryType,
+			Value: value,
+		}
+
+		now := time.Now()
+		switch r.FormValue("range") {
+		case "":
+			// Don't do anything, the default is to search across all time.
+		case "30":
+			query.Begin = now.Add(-1 * time.Hour * 24 * 30)
+		case "90":
+			query.Begin = now.Add(-1 * time.Hour * 24 * 90)
+		case "180":
+			query.Begin = now.Add(-1 * time.Hour * 24 * 180)
+		case "custom":
+			if beginValue := r.FormValue("begin"); beginValue != "" {
+				if begin, err := time.Parse("2006-01-02", beginValue); err != nil {
+					httputils.ReportError(w, err, "Invalid value for begin.", http.StatusNotFound)
+					return
+				} else {
+					query.Begin = begin
+				}
+			}
+			if endValue := r.FormValue("end"); endValue != "" {
+				if end, err := time.Parse("2006-01-02", r.FormValue("end")); err != nil {
+					httputils.ReportError(w, err, "Invalid value for end.", http.StatusNotFound)
+					return
+				} else {
+					query.End = end
+				}
+			}
+		}
+		sklog.Infof("Query: %#v", query)
 
 		// Do searches in parallel.
 		var wg sync.WaitGroup
@@ -151,7 +205,7 @@ func (srv *server) indexHandler(w http.ResponseWriter, r *http.Request) {
 			go func(i int, s sourceDescriptor) {
 				defer wg.Done()
 				results := []source.Artifact{}
-				for artifact := range s.source.Search(r.Context(), templateContext.Query) {
+				for artifact := range s.source.Search(r.Context(), query) {
 					results = append(results, artifact)
 				}
 				templateContext.Results[i] = result{
