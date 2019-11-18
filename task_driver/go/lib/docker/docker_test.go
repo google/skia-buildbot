821d548a63c89b8e083f33d7682a51a0945f638d
Ravi Mistry <rmistry@google.com>
2019-11-18 10:18:13 -0500

    Checkpoint
    Change-Id: Ieb1566a67b95d280f8998fe4663bfe51f5e47116

--- task_driver/go/lib/docker/docker_test.go
+++ task_driver/go/lib/docker/docker_test.go
@@ -3,18 +3,23 @@ package docker
 
 import (
 	"context"
+	"fmt"
 	"path/filepath"
 	"runtime"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.skia.org/infra/go/exec"
 	"go.skia.org/infra/go/testutils/unittest"
 	"go.skia.org/infra/task_driver/go/td"
 )
 
+// There is a problem here with sub steps vs the master step..
 func TestBuild(t *testing.T) {
 	unittest.MediumTest(t)
+
 	// Strip our PATH so we find our version of `docker` which is in the
 	// test_bin directory. Then add `/bin` to the PATH since we are running a
 	// Bash shell.
@@ -38,7 +43,8 @@ func TestBuild(t *testing.T) {
 			args: args{
 				tag: "success",
 			},
-			subSteps:             7,
+			subSteps: 7,
+			// subSteps:             8,
 			timeout:              time.Minute,
 			expected:             td.STEP_RESULT_SUCCESS,
 			expectedFirstSubStep: td.STEP_RESULT_SUCCESS,
@@ -96,11 +102,65 @@ func TestBuild(t *testing.T) {
 
 			// Ensure that we got the expected step results.
 			results := tr.EndRun(false, nil)
+			fmt.Println("========EXPECTED========")
+			fmt.Println(tt.subSteps)
+			fmt.Println(tt.expected)
+			fmt.Println(tt.expectedFirstSubStep)
+			fmt.Println("========RESULTS========")
+			fmt.Println(results.Result)
+			fmt.Println(results.Steps)
+			for _, s := range results.Steps {
+				fmt.Println(s.Name)
+				fmt.Println(s.Steps)
+				fmt.Println(s.Result)
+			}
 
 			assert.Equal(t, tt.subSteps, len(results.Steps[0].Steps))
+			//require.Equal(t, stepResult.Result, expect[idx])
+			//assert.Equal(t, tt.subSteps, len(results.Steps))
 			assert.Equal(t, tt.expected, results.Result)
 			assert.Equal(t, tt.expectedFirstSubStep, results.Steps[0].Result)
 		})
 	}
 
+}
+
+func TestLogin(t *testing.T) {
+	unittest.SmallTest(t)
+
+	_ = td.RunTestSteps(t, false, func(ctx context.Context) error {
+		mockRun := &exec.CommandCollector{}
+		mockRun.SetDelegateRun(func(ctx context.Context, cmd *exec.Command) error {
+			assert.Equal(t, dockerCmd, cmd.Name)
+			assert.Equal(t, []string{"login", "-u", "oauth2accesstoken", "-p", "token", "https://gcr.io"}, cmd.Args)
+			assert.Equal(t, "", cmd.Dir)
+			return nil
+		})
+		ctx = td.WithExecRunFn(ctx, mockRun.Run)
+
+		err := Login(ctx, "token", "https://gcr.io")
+		require.NoError(t, err)
+
+		return nil
+	})
+}
+
+func TestPush(t *testing.T) {
+	unittest.SmallTest(t)
+
+	_ = td.RunTestSteps(t, false, func(ctx context.Context) error {
+		mockRun := &exec.CommandCollector{}
+		mockRun.SetDelegateRun(func(ctx context.Context, cmd *exec.Command) error {
+			assert.Equal(t, dockerCmd, cmd.Name)
+			assert.Equal(t, []string{"push", "https://gcr.io/skia-public/skia-release:123"}, cmd.Args)
+			assert.Equal(t, "", cmd.Dir)
+			return nil
+		})
+		ctx = td.WithExecRunFn(ctx, mockRun.Run)
+
+		err := Push(ctx, "https://gcr.io/skia-public/skia-release:123")
+		require.NoError(t, err)
+
+		return nil
+	})
 }
