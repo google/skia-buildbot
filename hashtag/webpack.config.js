341e6ca817e0a0a8e921df49401fbb36a17fe0cc
Joe Gregorio <jcgregorio@google.com>
2019-11-18 15:05:33 -0500

    [hashtag] - Fix webpack production mode.
    Don't minify any HTML since it contains Go templates.
    
    Bug: skia:9559
    Change-Id: I522dbd128a680e0c3393a37353261f7f205d6843
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/255139
    Reviewed-by: Joe Gregorio <jcgregorio@google.com>

--- hashtag/webpack.config.js
+++ hashtag/webpack.config.js
@@ -1,11 +1,19 @@
 const commonBuilder = require('pulito');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const HtmlWebpackInjectAttributesPlugin = require('html-webpack-inject-attributes-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { resolve } = require('path')
 
 module.exports = (env, argv) => {
   let config = commonBuilder(env, argv, __dirname);
   config.output.publicPath='/static/';
+
+  // Don't minify the HTML since it contains Go template tags.
+  config.plugins.forEach((c) => {
+    if (c instanceof HtmlWebpackPlugin) {
+      c.options.minify = false;
+    }
+  });
 
   config.plugins.push(
     new CopyWebpackPlugin([
