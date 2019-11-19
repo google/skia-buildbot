69d1d73bd37438fcf08cbe76f1969735040a1c65
Eric Boren <borenet@google.com>
2019-11-19 09:19:17 -0500

    [git] Only complain about non-CIPD git when running in production
    Bug: skia:9538
    Change-Id: I3b7fb32a2da9de98c6f524d861b8baf8f91596ef
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/255257
    Reviewed-by: Kevin Lubick <kjlubick@google.com>
    Commit-Queue: Eric Boren <borenet@google.com>

--- go/git/git_common/git_common.go
+++ go/git/git_common/git_common.go
@@ -16,6 +16,7 @@ import (
 	"go.skia.org/infra/go/exec"
 	"go.skia.org/infra/go/skerr"
 	"go.skia.org/infra/go/sklog"
+	"go.skia.org/infra/go/util"
 )
 
 var (
@@ -41,7 +42,7 @@ func FindGit(ctx context.Context) (string, int, int, error) {
 			return "", 0, 0, skerr.Wrapf(err, "Failed to obtain git version")
 		}
 		sklog.Infof("Git is %s; version %d.%d", gitPath, maj, min)
-		if !IsFromCIPD(gitPath) {
+		if !IsFromCIPD(gitPath) && !util.IsLocal() {
 			sklog.Errorf("Git at %s does not appear to be obtained via CIPD; this will be a fatal error soon.", gitPath)
 		}
 		git = gitPath
