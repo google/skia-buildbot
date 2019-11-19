b0fa2fa4443a30cf792fd4723479b28632bfcd86
Leandro Lovisolo <lovisolo@google.com>
2019-11-19 14:07:32 -0500

    [gold] Extract out eventPromise() from corpus-selector-sk_test.js into test_util.js.
    Bug: skia:9525
    Change-Id: I278a64a33aab35806ff27b6ef2a0a60d1134c9cd
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/255141
    Commit-Queue: Leandro Lovisolo <lovisolo@google.com>
    Reviewed-by: Kevin Lubick <kjlubick@google.com>

--- golden/modules/corpus-selector-sk/corpus-selector-sk_test.js
+++ golden/modules/corpus-selector-sk/corpus-selector-sk_test.js
@@ -1,6 +1,7 @@
 import './index.js';
 import { $, $$ } from 'common-sk/modules/dom';
 import { deepCopy } from 'common-sk/modules/object';
+import { eventPromise } from '../test_util';
 import { fetchMock } from 'fetch-mock';
 import { trstatus } from './test_data';
 
@@ -249,53 +250,3 @@ describe('corpus-selector-sk', () => {
     expect(corporaLiText(corpusSelectorSk)).to.be.empty;
   })
 });
-
-// TODO(lovisolo): Move to test_util.js.
-// Returns a promise that will resolve when the given event is caught at the
-// document's body element, or reject if the event isn't caught within the given
-// amount of time.
-//
-// Set timeoutMillis = 0 to skip call to setTimeout(). This is necessary on
-// tests that simulate the passing of time with sinon.useFakeTimers(), which
-// could trigger the timeout before the promise has a chance to catch the event.
-//
-// Sample usage:
-//
-//   // Code under test.
-//   function doSomethingThatTriggersCustomEvent() {
-//     ...
-//     this.dispatchEvent(
-//         new CustomEvent('my-event', {detail: {foo: 'bar'}, bubbles: true});
-//   }
-//
-//   // Test.
-//   it('should trigger a custom event', async () => {
-//     const myEvent = eventPromise('my-event');
-//     doSomethingThatTriggersCustomEvent();
-//     const ev = await myEvent;
-//     expect(ev.detail.foo).to.equal('bar');
-//   });
-function eventPromise(event, timeoutMillis = 5000) {
-  // The executor function passed as a constructor argument to the Promise
-  // object is executed immediately. This guarantees that the event handler
-  // is added to document.body before returning.
-  return new Promise((resolve, reject) => {
-    let timeout;
-    const handler = (e) => {
-      document.body.removeEventListener(event, handler);
-      clearTimeout(timeout);
-      resolve(e);
-    };
-    // Skip setTimeout() call with timeoutMillis = 0. Useful when faking time in
-    // tests with sinon.useFakeTimers(). See
-    // https://sinonjs.org/releases/v7.5.0/fake-timers/.
-    if (timeoutMillis !== 0) {
-      timeout = setTimeout(() => {
-        document.body.removeEventListener(event, handler);
-        reject(new Error(`timed out after ${timeoutMillis} ms ` +
-            `while waiting to catch event "${event}"`));
-      }, timeoutMillis);
-    }
-    document.body.addEventListener(event, handler);
-  });
-}
