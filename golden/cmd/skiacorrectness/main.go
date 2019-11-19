c327ebbbb495d2203890bf31aec8123e080c4e76
Leandro Lovisolo <lovisolo@google.com>
2019-11-18 20:08:17 -0500

    [gold] Serve lit-html byblame-page-sk from /byblame2.
    Bug: skia:9525
    Change-Id: Ic00f2f3fb5d14169ffb9e86c092989229c204d1a

--- golden/cmd/skiacorrectness/main.go
+++ golden/cmd/skiacorrectness/main.go
@@ -557,6 +557,7 @@ func main() {
 	}
 
 	// These are the new lit-html pages.
+	loggedRouter.HandleFunc("/byblame2", templateHandler("byblame.html"))
 	loggedRouter.HandleFunc("/changelists", templateHandler("changelists.html"))
 	loggedRouter.HandleFunc("/triagelog2", templateHandler("triagelog.html"))
 
