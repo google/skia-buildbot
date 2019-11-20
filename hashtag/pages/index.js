84d27bdb96231ea4250589cbef77d30f4498df5e
Joe Gregorio <jcgregorio@google.com>
2019-11-20 13:07:12 -0500

    hashtag - Add ignores for search results.
    Bug: skia:9559
    Change-Id: I9afe5798acf618b6f55c0327371322cf161f5154

--- hashtag/pages/index.js
+++ hashtag/pages/index.js
@@ -1,1 +1,21 @@
 import './index.scss'
+import { $$, findParent} from 'common-sk/modules/dom'
+
+// Handle the toggle hidden forms.
+document.addEventListener('submit', async (e) => {
+    if (e.target.target != "__toggle_hidden") {
+        return;
+    }
+    e.preventDefault();
+    const resp = await fetch(e.target.action, {
+        method: 'POST',
+        body: new FormData(e.target),
+    });
+    await resp.text();
+    findParent(e.target, 'LI').classList.toggle('hidden');
+});
+
+$$('#edit-toggle').addEventListener('change', (e) => {
+    console.log(e);
+    $$('main').classList.toggle('edit-mode', e.target.checked);
+});
