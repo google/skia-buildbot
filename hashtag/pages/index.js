0a9ced67fb6ed1043d4839c634fd2a5fa382035d
Joe Gregorio <jcgregorio@google.com>
2019-11-20 15:45:17 -0500

    hashtag - Add ignores for search results.
    Adds an Edit button to the main page of search results that allows
    hiding search results for a specific search value.
    
    Screenshot:
      https://screenshot.googleplex.com/HCyUf5dAbEj.png
      https://screenshot.googleplex.com/YrUp8Yfhrpn.png
    
    Bug: skia:9559
    Change-Id: I9afe5798acf618b6f55c0327371322cf161f5154

--- hashtag/pages/index.js
+++ hashtag/pages/index.js
@@ -1,1 +1,27 @@
 import './index.scss'
+
+import 'elements-sk/error-toast-sk'
+import { $$, findParent } from 'common-sk/modules/dom'
+import { errorMessage } from 'elements-sk/errorMessage'
+import { jsonOrThrow } from 'common-sk/modules/jsonOrThrow'
+
+// Handle the form action for toggling the 'hidden' state of any Artifact.
+document.addEventListener('submit', (e) => {
+    if (e.target.target != "__toggle_hidden") {
+        return;
+    }
+    e.preventDefault();
+    fetch(e.target.action, {
+        method: 'POST',
+        body: new FormData(e.target),
+    }).then(jsonOrThrow).then((json) => {
+        findParent(e.target, 'LI').classList.toggle('hidden');
+    }).catch(errorMessage);
+});
+
+// When someone clicks the 'Edit' checkbox we display the edit buttons next to
+// each Artifact that allows the hidden state to be toggled. The displaying is
+// done via CSS, here we just toggle a CSS class on the 'main' element.
+$$('#edit-toggle').addEventListener('change', (e) => {
+    $$('main').classList.toggle('edit-mode', e.target.checked);
+});
