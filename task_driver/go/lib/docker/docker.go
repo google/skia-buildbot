a7028d9977507e5ecd081c354c2a5f62c016095e
Ravi Mistry <rmistry@google.com>
2019-11-20 15:00:57 -0500

    Checkpoint
    Change-Id: Ieb1566a67b95d280f8998fe4663bfe51f5e47116

--- task_driver/go/lib/docker/docker.go
+++ task_driver/go/lib/docker/docker.go
@@ -9,6 +9,7 @@ import (
 	"regexp"
 	"sync"
 
+	sk_exec "go.skia.org/infra/go/exec"
 	"go.skia.org/infra/task_driver/go/td"
 )
 
@@ -21,11 +22,31 @@ var (
 	dockerCmd = "docker"
 )
 
+// Login to docker to be able to run authenticated commands (Eg: docker.Push).
+func Login(ctx context.Context, accessToken, hostname string) error {
+	loginCmd := fmt.Sprintf("%s login -u oauth2accesstoken -p %s %s", dockerCmd, accessToken, hostname)
+	_, err := sk_exec.RunSimple(ctx, loginCmd)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Push a Docker file.
+func Push(ctx context.Context, tag string) error {
+	pushCmd := fmt.Sprintf("%s push %s", dockerCmd, tag)
+	_, err := sk_exec.RunSimple(ctx, pushCmd)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 // Build a Dockerfile.
 //
 // There must be a Dockerfile in the 'directory' and the resulting output is
 // tagged with 'tag'.
-func Build(ctx context.Context, directory string, tag string) error {
+func Build(ctx context.Context, directory string, tag string) (err error) {
 	ctx = td.StartStep(ctx, td.Props(fmt.Sprintf("docker build -t %s %s", tag, directory)))
 	defer td.EndStep(ctx)
 
@@ -52,7 +73,7 @@ func Build(ctx context.Context, directory string, tag string) error {
 	//   Step 5/7 : ENV CIPD_CACHE_DIR /workspace/__cache
 	//   Step 6/7 : USER skia
 
-	cmd := exec.CommandContext(ctx, dockerCmd, "build", "-t", tag, directory)
+	cmd := exec.CommandContext(ctx, dockerCmd, "build", "-t", tag, ".")
 	cmd.Dir = directory
 	cmd.Env = append(cmd.Env, td.GetEnv(ctx)...)
 
@@ -72,7 +93,6 @@ func Build(ctx context.Context, directory string, tag string) error {
 	// wg let's us wait for the Go routine to finish.
 	var wg sync.WaitGroup
 	wg.Add(1)
-
 	// logStreamError records any errors that occur writing to the logStream.
 	var logStreamError error = nil
 
