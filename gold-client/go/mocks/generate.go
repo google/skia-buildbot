3f0c26154a6dd76823156f80e9ead060f70756e5
Kevin Lubick <kjlubick@google.com>
2019-11-25 14:17:53 -0500

    [gold] Partially implement goldctl's interaction with /json/digests
    goldclient/golduploader.go -> goldclient/gcs.go (with
    addition of download implementation)
    
    This adds a bulk of the implementation and a simple test.
    
    Follow up CLs will:
    1) refactor the reused test code from diff_test
    2) integrate with the CLI part
    
    Bug: skia:9528
    
    Change-Id: I442daf91def931a31f0b74a4b00a6b151cc36a16

--- gold-client/go/mocks/generate.go
+++ gold-client/go/mocks/generate.go
@@ -1,8 +1,9 @@
 package mocks
 
-//go:generate mockery -name HTTPClient -dir ../goldclient -output .
+//go:generate mockery -name GCSDownloader -dir ../goldclient -output .
+//go:generate mockery -name GCSUploader -dir ../goldclient -output .
 //go:generate mockery -name GoldClient -dir ../goldclient -output .
-//go:generate mockery -name GoldUploader -dir ../goldclient -output .
+//go:generate mockery -name HTTPClient -dir ../goldclient -output .
 
 // We must make this one in the same package to avoid circular dependency
 //go:generate mockery -name AuthOpt -dir ../goldclient/ -inpkg
