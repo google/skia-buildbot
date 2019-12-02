d8bd3019ad60ee2d57e47db7e4093cc8a6cce307
Kevin Lubick <kjlubick@google.com>
2019-12-02 10:38:20 -0500

    [gold] Add tests for gsutil code
    Followup from last week.
    
    Change-Id: Icc4999b981ace4e58ac4794895b75b1cc1157034

--- gold-client/go/goldclient/gcs_test.go
+++ gold-client/go/goldclient/gcs_test.go
@@ -1,7 +1,52 @@
 package goldclient
 
-import "testing"
+import (
+	"context"
+	"io/ioutil"
+	"path/filepath"
+	"testing"
 
-func TestGSutilUpload(t *testing.T) {
-	t.Skip("WIP")
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	"go.skia.org/infra/go/exec"
+	"go.skia.org/infra/go/testutils"
+	"go.skia.org/infra/go/testutils/unittest"
+)
+
+func TestGSutilUploadBytes(t *testing.T) {
+	unittest.SmallTest(t)
+
+	mcc := exec.CommandCollector{}
+	ctx := exec.NewContext(context.Background(), mcc.Run)
+
+	gu := gsutilImpl{}
+	err := gu.UploadBytes(ctx, nil, "/path/to/file", "gs://bucket/foo/bar")
+	require.NoError(t, err)
+	require.Len(t, mcc.Commands(), 1)
+	assert.Equal(t, "gsutil cp /path/to/file gs://bucket/foo/bar", exec.DebugString(mcc.Commands()[0]))
+}
+
+func TestGSutilDownload(t *testing.T) {
+	unittest.MediumTest(t)
+
+	wd, cleanup := testutils.TempDir(t)
+	defer cleanup()
+
+	// Since we don't actually download something, write something to disk to pretend the gsutil
+	// command worked.
+	tf := filepath.Join(wd, "temp.png")
+	const fakeData = "an image"
+	require.NoError(t, ioutil.WriteFile(tf, []byte(fakeData), 0666))
+
+	mcc := exec.CommandCollector{}
+	ctx := exec.NewContext(context.Background(), mcc.Run)
+
+	gu := gsutilImpl{}
+	b, err := gu.Download(ctx, "gs://bucket/foo/bar.png", wd)
+	require.NoError(t, err)
+	require.NotNil(t, b)
+	require.Len(t, mcc.Commands(), 1)
+	assert.Equal(t, "gsutil cp gs://bucket/foo/bar.png "+tf, exec.DebugString(mcc.Commands()[0]))
+	assert.Equal(t, []byte(fakeData), b)
 }
