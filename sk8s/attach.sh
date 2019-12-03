27e306d7c38ef5707303d3f91511ae4ff4e1ef3f
Joe Gregorio <jcgregorio@google.com>
2019-12-03 12:26:33 -0500

    Start on skolo_rpi2 rack as a kubernetes cluster.
    Change-Id: If0b22ed7eb809294bbed2e0832f9a2c01a566425

---
+++ sk8s/attach.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Creates a shell where kubectl is hooked up to the RPI2 cluster.
+
+set -e -x
+
+CLUSTER=skolo-rpi2-01
+
+DIR=${HOME}/.config/skia-infra/skolo/${CLUSTER}
+
+# Create dir to store config.
+mkdir -p ${DIR}
+
+
+# Grab config from the kubernetes cluster and store in the temp dir.
+ssh chrome-bot@100.115.95.135 "sudo kubectl config view --raw" > ${DIR}/config
+
+# Make kubectl use that config.
+export KUBECONFIG=${DIR}/config
+
+# Set up port-forward to the k83 control endpoint and record the PID of the
+# background task.
+ssh -N -L 6443:localhost:6443 chrome-bot@100.115.95.135 &
+PID=$!
+
+echo ${PID} > ${DIR}/pid
+
+# Change the name so we can track which cluster we are talking to.
+kubectl config rename-context default ${CLUSTER}
+
+# Start bash.
+/bin/bash
+
+# Clean up on exit.
+kill ${PID}
+rm ${DIR}/config
