58d0a20211728b2d34a67fa5077eb66801f7780b
Leandro Lovisolo <lovisolo@google.com>
2019-12-06 14:49:31 -0500

    Add Infra-PerCommit-Puppeteer tryjob.
    Bug: skia:9608
    Change-Id: Ie7acb8e6a0ca9e008aa54ec28e5817f31a9f398a

---
+++ infra/bots/recipes/puppeteer_tests.py
@@ -0,0 +1,54 @@
+DEPS = [
+    'recipe_engine/context',
+    'recipe_engine/path',
+    'recipe_engine/properties',
+    'recipe_engine/step',
+]
+
+
+def RunSteps(api):
+  # Retrieve issue and patchset if we're in a trybot.
+  is_trybot = False
+  issue = None
+  patchset = None
+  if (api.properties.get('patch_issue', '') and
+      api.properties['patch_issue'] != '0' and
+      api.properties.get('patch_set', '') and
+      api.properties['patch_set'] != '0' and
+      api.properties.get('patch_ref', '')):
+    is_trybot = True
+    issue = api.properties['patch_issue']
+    patchset = api.properties['patch_set']
+
+  api.step('echo is_trybot', cmd=['echo', str(is_trybot)])
+  api.step('echo  issue', cmd=['echo', str(issue)])
+  api.step('echo  patchset', cmd=['echo', str(patchset)])
+
+  # Hack start_dir to remove the "k" directory which is added by Kitchen.
+  # Otherwise, we can't get to the CIPD packages, caches, and isolates which
+  # were put into the task workdir.
+  if api.path.c.base_paths['start_dir'][-1] == 'k':  # pragma: nocover
+    api.path.c.base_paths['start_dir'] = api.path.c.base_paths['start_dir'][:-1]
+
+  golden_dir = api.path['start_dir'].join('buildbot').join('golden')
+  with api.context(cwd=golden_dir, env={'DOCKER_CONFIG': '/home/chrome-bot/.docker'}):
+    api.step('run puppeteer tests', cmd=['make', 'puppeteer-test'])
+
+  upload_digests_cmd = ['python', 'upload-screenshots-to-gold.py']
+  puppeteer_tests_dir = golden_dir.join('puppeteer-tests')
+  with api.context(cwd=puppeteer_tests_dir):
+    if is_trybot:
+      cmd = upload_digests_cmd + ['--issue', issue, '--patch_set', patchset]
+      api.step('upload digests (with issue and patch_set)', cmd)
+    else:
+      api.step('upload digests (no issue nor patch_set)', upload_digests_cmd)
+
+def GenTests(api):
+  yield (
+      api.test('Infra-PerCommit-Puppeteer')
+  )
+
+  yield (
+      api.test('Infra-PerCommit-Puppeteer_tryjob') +
+      api.properties(patch_issue='123456', patch_set='3', patch_ref='a/b/c')
+  )
