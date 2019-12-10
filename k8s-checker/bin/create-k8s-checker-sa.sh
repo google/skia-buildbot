7358cdbac50d2667882b77ac45f981e0927f1908
Joe Gregorio <jcgregorio@google.com>
2019-12-10 12:41:01 -0500

    [sk8s] k8s-checker update to new YAML locations.
    Bug: skia:9602
    Change-Id: Id5f1fe3d486a169415f6c68bc646d762b283bfa5

---
+++ k8s-checker/bin/create-k8s-checker-sa.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+
+# Creates the service account used by K8s Checker, and export a key for
+# it into the kubernetes cluster as a secret.
+
+../kube/secrets/add-service-account.sh \
+  skia-public \
+  skia-public \
+  skia-k8s-checker \
+  "Service account for K8s Checker for access to Gerrit."
