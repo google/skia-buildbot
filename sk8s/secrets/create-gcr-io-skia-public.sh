6e59e459cb2bfb0bdd415481056a3093febb32af
Joe Gregorio <jcgregorio@google.com>
2019-12-11 11:12:39 -0500

    Start on skolo_rpi2 rack as a kubernetes cluster.
    Change-Id: If0b22ed7eb809294bbed2e0832f9a2c01a566425

---
+++ sk8s/secrets/create-gcr-io-skia-public.sh
@@ -0,0 +1,22 @@
+#/bin/bash
+
+set -x -e
+
+SA_EMAIL=$(../../kube/secrets/add-service-account.sh \
+  skia-public \
+  skolo-rack4 \
+  gcr-io-skia-public \
+  "cluster service account to access gcr.io/skia-public images"
+  roles/storage.objectViewer)
+
+##################################################################
+#
+# Add the ability for the new cluster to pull docker images from
+# gcr.io/skia-public container registry.
+#
+##################################################################
+
+# Add service account as reader of docker images bucket.
+# First remove the account so the add is fresh.
+gsutil iam ch -d "serviceAccount:${SA_EMAIL}:objectViewer" gs://artifacts.skia-public.appspot.com
+gsutil iam ch "serviceAccount:${SA_EMAIL}:objectViewer" gs://artifacts.skia-public.appspot.com
