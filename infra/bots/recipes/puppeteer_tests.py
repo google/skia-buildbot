2fd0048efd572f6b288258939572f667953f4756
Leandro Lovisolo <lovisolo@google.com>
2019-12-11 16:21:30 -0500

    Add Infra-PerCommit-Puppeteer tryjob.
    Note to reviewer: recommended review order is gen_tasks.go, then
    the recipe (puppeteer_tests.py), then upload-screenshots-to-gold.py.
    
    Bug: skia:9608
    Change-Id: Ie7acb8e6a0ca9e008aa54ec28e5817f31a9f398a

---
+++ infra/bots/recipes/puppeteer_tests.py
@@ -0,0 +1,73 @@
+DEPS = [
+    'recipe_engine/context',
+    'recipe_engine/path',
+    'recipe_engine/properties',
+    'recipe_engine/step',
+]
+
+
+def RunSteps(api):
+  revision = api.properties['revision']
+
+  # Are we in a trybot? If so, retrieve issue, patchset, etc.
+  is_trybot = False
+  issue = None
+  patch_set = None
+  buildbucket_build_id = None
+  # pylint: disable=too-many-boolean-expressions
+  if (api.properties.get('patch_issue', '') and
+      api.properties['patch_issue'] != '0' and
+      api.properties.get('patch_set', '') and
+      api.properties['patch_set'] != '0' and
+      api.properties.get('revision', '') and
+      api.properties['revision'] != '0' and
+      api.properties.get('buildbucket_build_id', '') and
+      api.properties['buildbucket_build_id'] != '0'):
+    is_trybot = True
+    issue = api.properties['patch_issue']
+    patch_set = api.properties['patch_set']
+    buildbucket_build_id = api.properties['buildbucket_build_id']
+
+  # Hack start_dir to remove the "k" directory which is added by Kitchen.
+  # Otherwise, we can't get to the CIPD packages, caches, and isolates which
+  # were put into the task workdir.
+  if api.path.c.base_paths['start_dir'][-1] == 'k':  # pragma: nocover
+    api.path.c.base_paths['start_dir'] = api.path.c.base_paths['start_dir'][:-1]
+
+  # Run Puppeteer tests in Docker container.
+  golden_dir = api.path['start_dir'].join('buildbot').join('golden')
+  with api.context(cwd=golden_dir,
+                   env={'DOCKER_CONFIG': '/home/chrome-bot/.docker'}):
+    api.step('run puppeteer tests', cmd=['make', 'puppeteer-test'])
+
+  # Upload any digests produced by Puppeteer tests to Gold.
+  with api.context(cwd=golden_dir.join('puppeteer-tests')):
+    upload_digests_cmd = [
+        'python',
+        'upload-screenshots-to-gold.py',
+        '--path_to_goldctl',
+        '../../../goldctl/goldctl',  # This is [START_DIR]/goldctl/goldctl.
+        '--revision', revision
+    ]
+    if is_trybot:
+      api.step('upload digests (tryjob)',
+               upload_digests_cmd + ['--issue', issue,
+                                     '--patch_set', patch_set,
+                                     '--task_id', buildbucket_build_id])
+    else:
+      api.step('upload digests (non-tryjob)', upload_digests_cmd)
+
+
+def GenTests(api):
+  yield (
+      api.test('Infra-PerCommit-Puppeteer') +
+      api.properties(revision='78e0b810cc3adc002a09c5190bb104afdcbbe3e1')
+  )
+
+  yield (
+      api.test('Infra-PerCommit-Puppeteer_tryjob') +
+      api.properties(patch_issue='123456',
+                     patch_set='3',
+                     revision='78e0b810cc3adc002a09c5190bb104afdcbbe3e1',
+                     buildbucket_build_id='8894409419339087024')
+  )
