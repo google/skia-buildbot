96941f788a356338f6bdb98fc9f943aa3ed01549
Leandro Lovisolo <lovisolo@google.com>
2019-12-12 17:32:25 -0500

    Add Infra-PerCommit-Puppeteer tryjob.
    Note to reviewer: recommended review order is gen_tasks.go, then
    the recipe (puppeteer_tests.py), then upload-screenshots-to-gold.py.
    
    Bug: skia:9608
    Change-Id: Ie7acb8e6a0ca9e008aa54ec28e5817f31a9f398a

--- golden/puppeteer-tests/test/puppeteer_test.js
+++ golden/puppeteer-tests/test/puppeteer_test.js
@@ -9,31 +9,57 @@ const fs = require('fs');
 const path = require('path');
 const puppeteer = require('puppeteer');
 
+
+
+// DO NOT SUBMIT before removing this.
+let firstNs = 0;
+function dbg(msg) {
+  let hrtime = process.hrtime();
+  let ns = hrtime[0] * 1000000000 + hrtime[1];
+  if (firstNs == 0) firstNs = ns;
+  let delta = (ns - firstNs) / 1000000;
+  console.log(`DEBUG (t=${delta}): ${msg}`);
+}
+
+
+
 describe('puppeteer', function() {
   let browser, page, server;
 
   before(async () => {
+    dbg('entering before()');
+    dbg('calling startTestServer()');
     server = await startTestServer();
+    dbg('calling launchBrowser()');
     browser = await launchBrowser();
+    dbg('leaving before()');
   });
 
   after(async () => {
+    dbg('entering after()');
+    dbg('calling browser.close()');
     await browser.close();
+    dbg('calling server.close()');
     await server.close();
+    dbg('leaving after()');
   });
 
   beforeEach(async () => { page = await browser.newPage(); });
   afterEach(async () => { await page.close(); });
 
   it('queries the DOM', async () => {
+    dbg('entering "queries the DOM"');
     await page.goto(`http://localhost:${server.address().port}`);
     expect(await page.$eval('h1', (el) => el.innerText)).to.equal('hello');
     expect(await page.$eval('p', (el) => el.innerText)).to.equal('world');
+    dbg('leaving "queries the DOM"');
   });
 
   it('takes screenshots', async () => {
+    dbg('entering "takes screenshots"');
     await page.goto(`http://localhost:${server.address().port}`);
     await page.screenshot({path: path.join(outputDir(), 'screenshot.png')});
+    dbg('leaving "takes screenshots"');
   });
 });
 
@@ -44,18 +70,32 @@ const startTestServer = () => {
     res.send('<html><body><h1>hello</h1><p>world</p></body></html>');
   });
   return new Promise((resolve) => {
-    const server = app.listen(0, () => resolve(server));
+    dbg('calling app.listen()');
+    const server = app.listen(0, () => {
+      dbg('resolving startTestServer() promise');
+      resolve(server);
+    });
   });
 };
 
 // TODO(lovisolo): Extract out the functions below into a file named e.g.
 //                 "testbed.js" under directory "puppeteer-tests".
 
-const inDocker = () => fs.existsSync('/.dockerenv');
+const inDocker = () => {
+  dbg('entering inDocker()');
+  const retval = fs.existsSync('/.dockerenv');
+  dbg('leaving inDocker()');
+  return retval;
+}
 
-const launchBrowser = () => puppeteer.launch(inDocker() ? {
-  args: ['--disable-dev-shm-usage', '--no-sandbox'],
-} : {});
+const launchBrowser = () => {
+  dbg('entering launchBrowser()');
+  const promise = puppeteer.launch(inDocker() ? {
+    args: ['--disable-dev-shm-usage', '--no-sandbox'],
+  } : {});
+  dbg('leaving launchBrowser()');
+  return promise;
+};
 
 const outputDir =
     () => inDocker()
