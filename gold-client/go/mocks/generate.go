ff3cf199c0092520b400071886b5ce691d23cd28
Kevin Lubick <kjlubick@google.com>
2019-12-12 12:22:49 -0500

    [gold] Remove production dep on mock
    When rolling into CIPD, I noticed this dep.
    
    Change-Id: I7c4638a0c3a466ca6ed0f152b0368339afc33641
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/259738
    Commit-Queue: Kevin Lubick <kjlubick@google.com>
    Reviewed-by: Leandro Lovisolo <lovisolo@google.com>

--- gold-client/go/mocks/generate.go
+++ gold-client/go/mocks/generate.go
@@ -4,6 +4,3 @@ package mocks
 //go:generate mockery -name GCSUploader -dir ../goldclient -output .
 //go:generate mockery -name GoldClient -dir ../goldclient -output .
 //go:generate mockery -name HTTPClient -dir ../goldclient -output .
-
-// We must make this one in the same package to avoid circular dependency
-//go:generate mockery -name AuthOpt -dir ../goldclient/ -inpkg
