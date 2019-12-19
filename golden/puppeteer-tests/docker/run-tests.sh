12ff2119ec9e47709323cc33a510a857830fe7f6
Leandro Lovisolo <lovisolo@google.com>
2019-12-19 01:41:18 -0500

    [gold] Puppeteer tests: Utility function startDemoPageServer().
    Bug: skia:9608
    Change-Id: I3d56a66918ad6ecf692dee598b60ac38ff4f67f8

--- golden/puppeteer-tests/docker/run-tests.sh
+++ golden/puppeteer-tests/docker/run-tests.sh
@@ -5,15 +5,30 @@
 
 # Populate /tests with all the files required to run the Puppeteer tests, which
 # are copied from the buildbot repository checkout.
-cp -r /src/golden/package.json      /tests
-cp -r /src/golden/webpack.config.js /tests
-cp -r /src/golden/modules           /tests
-cp -r /src/golden/puppeteer-tests   /tests
+mkdir /tests/common-sk
+cp -r /src/common-sk/package.json   /tests/common-sk
+cp -r /src/common-sk/*.js           /tests/common-sk
+cp -r /src/common-sk/modules        /tests/common-sk
+cp -r /src/common-sk/plugins        /tests/common-sk
+mkdir /tests/infra-sk
+cp -r /src/infra-sk/package.json    /tests/infra-sk
+cp -r /src/infra-sk/*.js            /tests/infra-sk
+cp -r /src/infra-sk/modules         /tests/infra-sk
+mkdir /tests/golden
+cp -r /src/golden/package.json      /tests/golden
+cp -r /src/golden/webpack.config.js /tests/golden
+cp -r /src/golden/modules           /tests/golden
+cp -r /src/golden/puppeteer-tests   /tests/golden
 
-# Populate /tests/node_modules.
-cd /tests
-npm install  # TODO(lovisolo): Use "npm ci" once package-lock.json is in repo.
+# Populate the various node_modules directories.
+# TODO(lovisolo): Use "npm ci" once package-lock.json files are in repo.
+cd /tests/common-sk
+npm install
+cd /tests/infra-sk
+npm install
+cd /tests/golden
+npm install
 
 # Run tests.
-cd /tests/puppeteer-tests
+cd /tests/golden/puppeteer-tests
 npx mocha
