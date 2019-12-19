81581c946c922a4d551dc0ff567144e9f3040f80
Leandro Lovisolo <lovisolo@google.com>
2019-12-19 15:23:04 -0500

    [gold] Puppeteer tests: Extract out common before/after(Each) hooks into setUpPuppeteerAndDemoPageServer().
    Based on the idea of shared behaviors, see https://github.com/mochajs/mocha/wiki/Shared-Behaviours.
    
    Note: the switch from arrow functions to regular functions (e.g. the it(...) calls) is due to the fact that these tests now make use of "this". See https://mochajs.org/#arrow-functions.
    
    Bug: skia:9608
    Change-Id: Ib9b37a06dce371c8f37855b1aec97f0db5f33545

--- golden/puppeteer-tests/test/corpus-selector-sk_puppeteer_test.js
+++ golden/puppeteer-tests/test/corpus-selector-sk_puppeteer_test.js
@@ -1,34 +1,16 @@
 const expect = require('chai').expect;
 const addEventListenersToPuppeteerPage = require('./util').addEventListenersToPuppeteerPage;
-const launchBrowser = require('./util').launchBrowser;
-const startDemoPageServer = require('./util').startDemoPageServer;
+const setUpPuppeteerAndDemoPageServer = require('./util').setUpPuppeteerAndDemoPageServer;
 const takeScreenshot = require('./util').takeScreenshot;
 
 describe('corpus-selector-sk', function() {
-  let browser, baseUrl, stopDemoPageServer;
+  setUpPuppeteerAndDemoPageServer();  // Sets up this.page and this.baseUrl.
 
-  before(async () => {
-    ({baseUrl, stopDemoPageServer} = await startDemoPageServer());
-    browser = await launchBrowser();
-  });
-
-  after(async () => {
-    await browser.close();
-    await stopDemoPageServer();
-  });
-
-  let page;
-
-  beforeEach(async () => {
-    page = await browser.newPage();
-
-    // Tell the demo page this is a Puppeteer test so it won't fake RPC latency.
-    await page.setCookie({url: baseUrl, name: 'puppeteer', value: 'true'});
-
+  beforeEach(async function() {
     // Listen for custom event triggered when component finishes loading.
     const eventPromise =
         await addEventListenersToPuppeteerPage(
-            page, ['corpus-selector-sk-loaded']);
+            this.page, ['corpus-selector-sk-loaded']);
 
     // Page has three corpus selectors so we wait until all of them have loaded.
     const loaded = Promise.all([
@@ -36,21 +18,17 @@ describe('corpus-selector-sk', function() {
         eventPromise('corpus-selector-sk-loaded'),
         eventPromise('corpus-selector-sk-loaded'),
     ]);
-    await page.goto(`${baseUrl}/dist/corpus-selector-sk.html`);
+    await this.page.goto(`${this.baseUrl}/dist/corpus-selector-sk.html`);
     await loaded;
   });
 
-  afterEach(async () => {
-    await page.close();
-  });
-
-  it('should render the demo page', async () => {
+  it('should render the demo page', async function() {
     // Basic sanity check.
-    expect(await page.$$('corpus-selector-sk')).to.have.length(3);
+    expect(await this.page.$$('corpus-selector-sk')).to.have.length(3);
   });
 
-  it('should take a screenshot', async () => {
-    await page.setViewport({ width: 1200, height: 1200 });
-    await takeScreenshot(page, 'Test-Corpus-Selector-Sk');
+  it('should take a screenshot', async function() {
+    await this.page.setViewport({ width: 1200, height: 1200 });
+    await takeScreenshot(this.page, 'Test-Corpus-Selector-Sk');
   });
 });
