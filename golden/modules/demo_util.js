7503d99df5e0e2f6fa5af14ddfe449af2babaab9
Leandro Lovisolo <lovisolo@google.com>
2019-12-20 04:34:24 -0500

    [gold] Extract function isPuppeteerTest() into demo_util.js.
    Bug: skia:9608
    Change-Id: Idc59dce2e07d61260204a24e2fbee38148681083

--- golden/modules/demo_util.js
+++ golden/modules/demo_util.js
@@ -1,8 +1,13 @@
-// delay wraps the value toReturn in a Promise that will resolve
-// after "delay" milliseconds.
-export function delay(toReturn, delay=100) {
-  // we return a function that returns the promise so each call
-  // has a "fresh" promise and waits for the time.
+/**
+ * Wraps the value toReturn in a Promise that will resolve after "delay"
+ * milliseconds. Used to fake RPC latency in demo pages.
+ * @param toReturn {Object} Object to be returned.
+ * @param delay {number} Delay in milliseconds.
+ * @return {Function}
+ */
+exports.delay = function(toReturn, delay=100) {
+  // We return a function that returns the promise so each call has a "fresh"
+  // promise and waits for the time.
   return function() {
     return new Promise((resolve) => {
       setTimeout(resolve, delay);
@@ -15,3 +20,9 @@ export function delay(toReturn, delay=100) {
     });
   };
 }
+
+/**
+ * Returns true if the page is running from within a Puppeteer-managed browser.
+ * @return {boolean}
+ */
+exports.isPuppeteerTest = () => document.cookie.indexOf('puppeteer') !== -1;
