853b1fd2cb5e79480fc354692476c868b05303d4
Ben Wagner <benjaminwagner@google.com>
2019-12-20 23:15:21 -0500

    Update Win GCE bots to Win2019
    Also add SSH server.
    
    Change-Id: If4686c5618720cc69220e5c4c39078d710dbeee0
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/261376
    Commit-Queue: Ravi Mistry <rmistry@google.com>
    Auto-Submit: Ben Wagner aka dogben <benjaminwagner@google.com>
    Reviewed-by: Ravi Mistry <rmistry@google.com>

--- go/gce/swarming/instance_types/instance_types.go
+++ go/gce/swarming/instance_types/instance_types.go
@@ -23,7 +23,7 @@ const (
 
 	DEBIAN_SOURCE_IMAGE_EXTERNAL = "skia-swarming-base-v2019-03-04-000"
 	DEBIAN_SOURCE_IMAGE_INTERNAL = "skia-swarming-base-v2019-04-18-000"
-	WIN_SOURCE_IMAGE             = "projects/windows-cloud/global/images/windows-server-2016-dc-v20190108"
+	WIN_SOURCE_IMAGE             = "projects/windows-cloud/global/images/windows-server-2019-dc-v20191210"
 
 	INSTANCE_TYPE_CT            = "ct"
 	INSTANCE_TYPE_LINUX_SMALL   = "linux-small"
