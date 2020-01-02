43f6ad18dbec4c7184c904a4c4808ab3f402012c
Leandro Lovisolo <lovisolo@google.com>
2020-01-02 10:31:18 -0500

    [gold] Add gold-scaffold-sk_puppeteer_test.js.
    Bug: skia:9608
    Change-Id: Ic1ff568f2430dc1027cac3252e9809cc4ce943d9

---
+++ golden/puppeteer-tests/test/gold-scaffold-sk_puppeteer_test.js
@@ -0,0 +1,21 @@
+const expect = require('chai').expect;
+const setUpPuppeteerAndDemoPageServer = require('./util').setUpPuppeteerAndDemoPageServer;
+const takeScreenshot = require('./util').takeScreenshot;
+
+describe('gold-scaffold-sk', function() {
+  setUpPuppeteerAndDemoPageServer();  // Sets up this.page and this.baseUrl.
+
+  beforeEach(async function() {
+    await this.page.goto(`${this.baseUrl}/dist/gold-scaffold-sk.html`);
+  });
+
+  it('should render the demo page', async function() {
+    // Smoke test.
+    expect(await this.page.$$('gold-scaffold-sk')).to.have.length(1);
+  });
+
+  it('should take a screenshot', async function() {
+    await this.page.setViewport({ width: 1200, height: 600 });
+    await takeScreenshot(this.page, 'Test-Gold-Scaffold-Sk');
+  });
+});
