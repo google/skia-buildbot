148fa8d77298a29b77e4a1626e6f2b6545a94d12
Leandro Lovisolo <lovisolo@google.com>
2020-01-02 10:34:21 -0500

    [gold] Add changelists-page-sk_puppeteer_test.js and changelist-controls-sk_puppeteer_test.js.
    Bug: skia:9608
    Change-Id: If260736e5ce3857964f3da95b86720780e442dfe
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/261921
    Commit-Queue: Leandro Lovisolo <lovisolo@google.com>
    Reviewed-by: Kevin Lubick <kjlubick@google.com>

---
+++ golden/puppeteer-tests/test/changelists-page-sk_puppeteer_test.js
@@ -0,0 +1,26 @@
+const expect = require('chai').expect;
+const addEventListenersToPuppeteerPage = require('./util').addEventListenersToPuppeteerPage;
+const setUpPuppeteerAndDemoPageServer = require('./util').setUpPuppeteerAndDemoPageServer;
+const takeScreenshot = require('./util').takeScreenshot;
+
+describe('changelists-page-sk', function() {
+  setUpPuppeteerAndDemoPageServer();  // Sets up this.page and this.baseUrl.
+
+  beforeEach(async function() {
+    const eventPromise =
+        await addEventListenersToPuppeteerPage(this.page, ['end-task']);
+    const loaded = eventPromise('end-task');  // Emitted when page is loaded.
+    await this.page.goto(`${this.baseUrl}/dist/changelists-page-sk.html`);
+    await loaded;
+  });
+
+  it('should render the demo page', async function() {
+    // Smoke test.
+    expect(await this.page.$$('changelists-page-sk')).to.have.length(1);
+  });
+
+  it('should take a screenshot', async function() {
+    await this.page.setViewport({ width: 1200, height: 700 });
+    await takeScreenshot(this.page, 'Test-Changelists-Page-Sk');
+  });
+});
