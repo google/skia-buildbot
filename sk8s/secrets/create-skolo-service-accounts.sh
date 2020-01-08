aaf4bb3c527e495b13af391a993c7b395206a83c
Joe Gregorio <jcgregorio@google.com>
2020-01-08 15:08:11 -0500

    [sk8s] Copy metadata server over to run on kubernetes.
    Bug: skia:9602
    Change-Id: Ic0d2434b3a868a885dd881b27c6bfdd3697e12f8

---
+++ sk8s/secrets/create-skolo-service-accounts.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+
+# Creates the service account key used by metadata, and export a key for
+# it into the kubernetes cluster as a secret.
+
+set -e -x
+
+../../kube/secrets/add-service-account.sh \
+  skia-public \
+  skolo-rack4 \
+  skolo-service-accounts \
+  "cluster service account for bots to access swarming."
