d1a8149a53e1c5bceb6531c40155e766586d71ab
Joe Gregorio <jcgregorio@google.com>
2020-01-08 09:37:12 -0500

    Start on skolo_rpi2 rack as a kubernetes cluster.
    Change-Id: If0b22ed7eb809294bbed2e0832f9a2c01a566425

---
+++ kube/secrets/copy-secret-between-clusters.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Copy secret from one cluster to another in berglas.
+
+if [ $# -ne 3 ]; then
+    echo "$0 <cluster-source-name> <cluster-dest-name> <secret-name>"
+    exit 1
+fi
+
+SRC=$1
+DST=$2
+SECRET=$3
+
+REL=$(dirname "$0")
+source ${REL}/config.sh
+
+${REL}/get-secret.sh ${SRC} ${SECRET} \
+  | ${REL}/add-secret-from-stdin.sh ${DST} ${SECRET}
