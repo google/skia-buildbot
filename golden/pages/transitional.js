0393d702d302f58dfb7e410c43c695bdf432ad13
Leandro Lovisolo <lovisolo@google.com>
2020-01-08 10:36:57 -0500

    [gold] digest-details-sk.html: Swap out legacy dots-sk Polymer component for its lit-html replacement.
    Bug: skia:9525
    Change-Id: I6e840af482347206f5cff75d5cd03de154d223cd

--- golden/pages/transitional.js
+++ golden/pages/transitional.js
@@ -4,3 +4,4 @@
 import '../../infra-sk/modules/login-sk'
 import '../node_modules/@webcomponents/custom-elements/custom-elements.min.js'
 import '../modules/changelist-controls-sk'
+import '../modules/dots-sk'
