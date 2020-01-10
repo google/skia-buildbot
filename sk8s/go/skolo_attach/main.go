f844de1eb7d79c208965d8b4265e23c9e811a093
Joe Gregorio <jcgregorio@google.com>
2020-01-10 11:23:30 -0500

    Start on skolo_rpi2 rack as a kubernetes cluster.
    Change-Id: If0b22ed7eb809294bbed2e0832f9a2c01a566425

---
+++ sk8s/go/skolo_attach/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os/user"
+	"path/filepath"
+
+	"golang.org/x/crypto/ssh"
+)
+
+func main() {
+	// Load the users private ssh key.
+	u, err := user.Current()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	privateBytes, err := ioutil.ReadFile(filepath.Join(u.HomeDir, ".ssh", "id_rsa"))
+	if err != nil {
+		log.Fatal("Failed to load private key: ", err)
+	}
+
+	private, err := ssh.ParsePrivateKey(privateBytes)
+	if err != nil {
+		log.Fatal("Failed to parse private key: ", err)
+	}
+
+	// Connect to the jumphost.
+	config := &ssh.ClientConfig{
+		User: "chrome-bot",
+		Auth: []ssh.AuthMethod{
+			ssh.PublicKeys(private),
+		},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	}
+	client, err := ssh.Dial("tcp", "100.115.95.135:22", config)
+	if err != nil {
+		log.Fatal("Failed to dial: ", err)
+	}
+
+	session, err := client.NewSession()
+	if err != nil {
+		log.Fatal("Failed to create session: ", err)
+	}
+	defer session.Close()
+
+	// Once a Session is created, you can execute a single command on
+	// the remote side using the Run method.
+	var b bytes.Buffer
+	session.Stdout = &b
+	if err := session.Run("sudo kubectl config view --raw"); err != nil {
+		log.Fatal("Failed to run: " + err.Error())
+	}
+	fmt.Println(b.String())
+
+}
