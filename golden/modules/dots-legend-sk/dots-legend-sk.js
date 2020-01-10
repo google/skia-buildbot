49951de187ab2f761b3bf2067571a3c63951d923
Leandro Lovisolo <lovisolo@google.com>
2020-01-10 11:16:33 -0500

    [gold] Add dots-legend-sk lit-html component.
    Bug: skia:9525
    Change-Id: I11b0d672a80980e21d16f342899ff5e3f79d2303
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/263556
    Commit-Queue: Leandro Lovisolo <lovisolo@google.com>
    Reviewed-by: Kevin Lubick <kjlubick@google.com>

---
+++ golden/modules/dots-legend-sk/dots-legend-sk.js
@@ -0,0 +1,125 @@
+/**
+ * @module modules/dots-legend-sk
+ * @description <h2><code>dots-legend-sk</code></h2>
+ *
+ * A legend for the dots-sk element.
+ */
+
+import { define } from 'elements-sk/define';
+import { ElementSk } from '../../../infra-sk/modules/ElementSk';
+import { html } from 'lit-html';
+import {
+  DOT_STROKE_COLORS,
+  DOT_FILL_COLORS,
+  MAX_UNIQUE_DIGESTS,
+} from '../dots-sk/constants';
+import 'elements-sk/icon/cancel-icon-sk';
+import 'elements-sk/icon/check-circle-icon-sk';
+import 'elements-sk/icon/help-icon-sk';
+
+const template = (el) => html`
+  ${el._digests
+      .slice(0, MAX_UNIQUE_DIGESTS)
+      .map((digest, index) => digestTemplate(digest, index, el))}
+
+  ${el._digests.length > MAX_UNIQUE_DIGESTS
+      ? oneOfManyOtherDigestsTemplate()
+      : ''}
+`;
+
+const digestTemplate = (digest, index, el) => html`
+  ${dotTemplate(index)}
+  <a target=_blank class=digest href="${el._digestDetailHref(index)}">
+    ${digest.digest}
+  </a>
+  ${statusIconTemplate(digest.status)}
+  ${index > 0
+      ? html`<a target=_blank class=diff href="${el._digestDiffHref(index)}">
+               diff
+             </a>`
+      : html`<span></span>`}
+`;
+
+const oneOfManyOtherDigestsTemplate = () => html`
+  ${dotTemplate(MAX_UNIQUE_DIGESTS)}
+  <span class=one-of-many-other-digests>One of many other digests.</span>
+`;
+
+const dotTemplate = (index) => {
+  const style =
+      `border-color: ${DOT_STROKE_COLORS[index]};` +
+      `background-color: ${DOT_FILL_COLORS[index]};`;
+  return html`<div class=dot style="${style}"></div>`;
+};
+
+const statusIconTemplate = (status) => {
+  switch (status) {
+    case 'negative':
+      return html`<cancel-icon-sk class=negative-icon></cancel-icon-sk>`;
+    case 'positive':
+      return html`
+        <check-circle-icon-sk class=positive-icon></check-circle-icon-sk>
+      `;
+    case 'untriaged':
+      return html`<help-icon-sk class=untriaged-icon></help-icon-sk>`;
+    default:
+      throw `Unknown status: "${status}"`;
+  }
+};
+
+define('dots-legend-sk', class extends ElementSk {
+  constructor() {
+    super(template);
+    this._digests = [];
+    this._issue = '';
+    this._test = '';
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    this._render();
+  }
+
+  /**
+   * @prop digests {Array} An array of {digest: 'a4f32...', status: 'positive'}
+   *   objects.
+   */
+  get digests() { return this._digests; }
+  set digests(digests) {
+    this._digests = digests;
+    this._render();
+  }
+
+  /**
+   * @prop issue {string} An issue number/ID.
+   */
+  get issue() { return this._issue; }
+  set issue(issue) {
+    this._issue = issue;
+    this._render();
+  }
+
+  /**
+   * @prop test {string} Test name.
+   */
+  get test() { return this._test; }
+  set test(test) {
+    this._test = test;
+    this._render();
+  }
+
+  _digestDetailHref(index) {
+    return `/detail`
+        + `?test=${encodeURIComponent(this._test)}`
+        + `&digest=${this._digests[index].digest}`
+        + (this._issue ? `&issue=${this._issue}` : '');
+  }
+
+  _digestDiffHref(index) {
+    return `/diff`
+        + `?test=${encodeURIComponent(this._test)}`
+        + `&left=${this._digests[0].digest}`
+        + `&right=${this._digests[index].digest}`
+        + (this._issue ? `&issue=${this._issue}` : '');
+  }
+});
---
+++ golden/modules/dots-legend-sk/dots-legend-sk_test.js
@@ -0,0 +1,245 @@
+import './index.js';
+import {
+  DOT_STROKE_COLORS,
+  DOT_FILL_COLORS,
+} from '../dots-sk/constants';
+import { $ } from 'common-sk/modules/dom';
+
+describe('dots-legend-sk', () => {
+  let dotsLegendSk;
+
+  beforeEach(() => {
+    dotsLegendSk = document.createElement('dots-legend-sk');
+    dotsLegendSk.test = 'My Test';
+    document.body.appendChild(dotsLegendSk);
+  });
+
+  afterEach(() => {
+    // Remove the stale instance under test.
+    if (dotsLegendSk) {
+      document.body.removeChild(dotsLegendSk);
+      dotsLegendSk = null;
+    }
+  });
+
+  describe('with less than MAX_UNIQUE_DIGESTS unique digests', () => {
+    beforeEach(() => {
+      dotsLegendSk.digests = [
+        {'digest': '00000000000000000000000000000000', 'status': 'untriaged'},
+        {'digest': '11111111111111111111111111111111', 'status': 'positive' },
+        {'digest': '22222222222222222222222222222222', 'status': 'negative'},
+        {'digest': '33333333333333333333333333333333', 'status': 'negative'},
+        {'digest': '44444444444444444444444444444444', 'status': 'positive'},
+      ];
+    });
+
+    it('renders dots correctly', () => {
+      expect(dotColors(dotsLegendSk)).to.deep.equal([
+        [DOT_STROKE_COLORS[0], DOT_FILL_COLORS[0]],
+        [DOT_STROKE_COLORS[1], DOT_FILL_COLORS[1]],
+        [DOT_STROKE_COLORS[2], DOT_FILL_COLORS[2]],
+        [DOT_STROKE_COLORS[3], DOT_FILL_COLORS[3]],
+        [DOT_STROKE_COLORS[4], DOT_FILL_COLORS[4]],
+      ]);
+    });
+
+    it('renders digests correctly', () => {
+      expect(digests(dotsLegendSk)).to.deep.equal([
+        '00000000000000000000000000000000',
+        '11111111111111111111111111111111',
+        '22222222222222222222222222222222',
+        '33333333333333333333333333333333',
+        '44444444444444444444444444444444',
+      ]);
+    });
+
+    it('renders digest links correctly', () => {
+      const digestLinkFor = (d) => `/detail?test=My%20Test&digest=${d}`;
+      expect(digestLinks(dotsLegendSk)).to.deep.equal([
+        digestLinkFor('00000000000000000000000000000000'),
+        digestLinkFor('11111111111111111111111111111111'),
+        digestLinkFor('22222222222222222222222222222222'),
+        digestLinkFor('33333333333333333333333333333333'),
+        digestLinkFor('44444444444444444444444444444444'),
+      ]);
+    });
+
+    it('renders status icons correctly', () => {
+      expect(statusIcons(dotsLegendSk)).to.deep.equal([
+        'untriaged',
+        'positive',
+        'negative',
+        'negative',
+        'positive',
+      ]);
+    });
+
+    it('renders diff links correctly', () => {
+      const diffLinkFor = (d) =>
+          '/diff?test=My%20Test&left=00000000000000000000000000000000' +
+          `&right=${d}`;
+      expect(diffLinks(dotsLegendSk)).to.deep.equal([
+        diffLinkFor('11111111111111111111111111111111'),
+        diffLinkFor('22222222222222222222222222222222'),
+        diffLinkFor('33333333333333333333333333333333'),
+        diffLinkFor('44444444444444444444444444444444'),
+      ]);
+    });
+
+    describe('with issue number', () => {
+      beforeEach(() => {
+        dotsLegendSk.issue = '123456'
+      });
+
+      it('renders digest links correctly', () => {
+        const digestLinkFor = (d) =>
+            `/detail?test=My%20Test&digest=${d}&issue=123456`;
+        expect(digestLinks(dotsLegendSk)).to.deep.equal([
+          digestLinkFor('00000000000000000000000000000000'),
+          digestLinkFor('11111111111111111111111111111111'),
+          digestLinkFor('22222222222222222222222222222222'),
+          digestLinkFor('33333333333333333333333333333333'),
+          digestLinkFor('44444444444444444444444444444444'),
+        ]);
+      });
+
+      it('renders diff links correctly', () => {
+        const diffLinkFor = (d) =>
+            '/diff?test=My%20Test&left=00000000000000000000000000000000' +
+            `&right=${d}&issue=123456`;
+        expect(diffLinks(dotsLegendSk)).to.deep.equal([
+          diffLinkFor('11111111111111111111111111111111'),
+          diffLinkFor('22222222222222222222222222222222'),
+          diffLinkFor('33333333333333333333333333333333'),
+          diffLinkFor('44444444444444444444444444444444'),
+        ]);
+      });
+    });
+  });
+
+  describe('with more than MAX_UNIQUE_DIGESTS unique digests', () => {
+    beforeEach(() => {
+      dotsLegendSk.digests = [
+        {'digest': '00000000000000000000000000000000', 'status': 'untriaged'},
+        {'digest': '11111111111111111111111111111111', 'status': 'positive' },
+        {'digest': '22222222222222222222222222222222', 'status': 'negative'},
+        {'digest': '33333333333333333333333333333333', 'status': 'negative'},
+        {'digest': '44444444444444444444444444444444', 'status': 'positive'},
+        {'digest': '55555555555555555555555555555555', 'status': 'positive'},
+        {'digest': '66666666666666666666666666666666', 'status': 'untriaged'},
+        {'digest': '77777777777777777777777777777777', 'status': 'untriaged'},
+        {'digest': '88888888888888888888888888888888', 'status': 'untriaged'},
+        // The API currently tops out at 8 unique digests. The ninth unique
+        // digest below is included to test that this component behaves
+        // gracefully in the event that the API behavior changes and the
+        // front-end and back-end code fall out of sync.
+        {'digest': '99999999999999999999999999999999', 'status': 'untriaged'},
+      ];
+    });
+
+    it('renders dots correctly', () => {
+      expect(dotColors(dotsLegendSk)).to.deep.equal([
+        [DOT_STROKE_COLORS[0], DOT_FILL_COLORS[0]],
+        [DOT_STROKE_COLORS[1], DOT_FILL_COLORS[1]],
+        [DOT_STROKE_COLORS[2], DOT_FILL_COLORS[2]],
+        [DOT_STROKE_COLORS[3], DOT_FILL_COLORS[3]],
+        [DOT_STROKE_COLORS[4], DOT_FILL_COLORS[4]],
+        [DOT_STROKE_COLORS[5], DOT_FILL_COLORS[5]],
+        [DOT_STROKE_COLORS[6], DOT_FILL_COLORS[6]],
+        [DOT_STROKE_COLORS[7], DOT_FILL_COLORS[7]],
+        [DOT_STROKE_COLORS[8], DOT_FILL_COLORS[8]],
+      ]);
+    });
+
+    it('renders digests correctly', () => {
+      expect(digests(dotsLegendSk)).to.deep.equal([
+        '00000000000000000000000000000000',
+        '11111111111111111111111111111111',
+        '22222222222222222222222222222222',
+        '33333333333333333333333333333333',
+        '44444444444444444444444444444444',
+        '55555555555555555555555555555555',
+        '66666666666666666666666666666666',
+        '77777777777777777777777777777777',
+        'One of many other digests.',
+      ]);
+    });
+
+    it('renders status icons correctly', () => {
+      expect(statusIcons(dotsLegendSk)).to.deep.equal([
+        'untriaged',
+        'positive',
+        'negative',
+        'negative',
+        'positive',
+        'positive',
+        'untriaged',
+        'untriaged',
+      ]);
+    });
+
+    it('renders diff links correctly', () => {
+      const diffLinkFor = (d) =>
+          '/diff?test=My%20Test&left=00000000000000000000000000000000' +
+          `&right=${d}`;
+      expect(diffLinks(dotsLegendSk)).to.deep.equal([
+        diffLinkFor('11111111111111111111111111111111'),
+        diffLinkFor('22222222222222222222222222222222'),
+        diffLinkFor('33333333333333333333333333333333'),
+        diffLinkFor('44444444444444444444444444444444'),
+        diffLinkFor('55555555555555555555555555555555'),
+        diffLinkFor('66666666666666666666666666666666'),
+        diffLinkFor('77777777777777777777777777777777'),
+      ]);
+    });
+
+  });
+});
+
+// Takes a color represented as an RGB string (e.g. "rgb(10, 187, 204)") and
+// returns the equivalent hex string (e.g. "#0ABBCC").
+const rgbToHex = (rgb) =>
+    '#' + rgb.match(/rgb\((\d+), (\d+), (\d+)\)/)
+        .slice(1)                      // ['10', '187', '204'].
+        .map(x => parseInt(x))         // [10, 187, 204]
+        .map(x => x.toString(16))      // ['a', 'bb', 'cc']
+        .map(x => x.padStart(2, '0'))  // ['0a', 'bb', 'cc']
+        .map(x => x.toUpperCase())     // ['0A', 'BB', 'CC']
+        .join('');                     // '0ABBCC'
+
+// Returns the dot colors as an array of arrays of the form
+// ["stroke color", "fill color"], where the colors are represented as hex
+// strings (e.g. "#AABBCC").
+const dotColors = (dotsLegendSk) =>
+    $('div.dot', dotsLegendSk)
+        .map((dot) => [
+            rgbToHex(dot.style.borderColor),
+            rgbToHex(dot.style.backgroundColor)
+        ]);
+
+const digests = (dotsLegendSk) =>
+    $('a.digest, span.one-of-many-other-digests', dotsLegendSk)
+        .map(a => a.innerText.trim());
+
+// Returns the status icons  as an array of strings. Possible values are
+// are "negative", "positive", "untriaged".
+const statusIcons = (dotsLegendSk) =>
+    $([
+      'cancel-icon-sk.negative-icon',
+      'check-circle-icon-sk.positive-icon',
+      'help-icon-sk.untriaged-icon'
+    ].join(', '), dotsLegendSk)
+        .map(icon => icon.className.replace('-icon', ''));
+
+// Takes an URL string (e.g. "http://example.com/search?q=hello") and returns
+// only the path and query string (e.g. "/search?q=hello").
+const urlToPathAndQueryString = (urlStr) => {
+  const url = new URL(urlStr);
+  return url.pathname + url.search;
+};
+
+const digestLinks = (dotsLegendSk) =>
+    $('a.digest', dotsLegendSk).map(a => urlToPathAndQueryString(a.href));
+
+const diffLinks = (dotsLegendSk) =>
+    $('a.diff', dotsLegendSk).map(a => urlToPathAndQueryString(a.href));
