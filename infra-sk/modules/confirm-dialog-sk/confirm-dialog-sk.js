b11210873280daeef684f95c8c2bd1b6ad190877
Kevin Lubick <kjlubick@google.com>
2020-01-10 15:48:28 -0500

    Fix confirm-dialog-sk render
    Noticed this while I was adding one to gold.
    
    Change-Id: I49657887e323948db5567b5e2a48ea7ce739fc2c

--- infra-sk/modules/confirm-dialog-sk/confirm-dialog-sk.js
+++ infra-sk/modules/confirm-dialog-sk/confirm-dialog-sk.js
@@ -13,7 +13,7 @@
  *
  * <script>
  *   (function(){
- *     $('#confirm-dialog').open("Proceed with taking over the world?").then(() => {
+ *     $$('#confirm-dialog').open("Proceed with taking over the world?").then(() => {
  *       // Do some thing on confirm.
  *     }).catch(() => {
  *       // Do some thing on cancel.
@@ -60,6 +60,7 @@ define('confirm-dialog-sk', class extends HTMLElement {
    */
   open(message) {
     this._message = message;
+    this._render();
     this._dialog.showModal();
     return new Promise((resolve, reject) => {
       this._resolve = resolve;
