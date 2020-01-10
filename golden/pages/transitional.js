6b9cf9542143125b73288615e5fea875bbb39269
Leandro Lovisolo <lovisolo@google.com>
2020-01-10 12:15:58 -0500

    [gold] digest-details-sk.html: Swap out legacy dot-legend-sk Polymer component for its lit-html replacement.
    Bug: skia:9525
    Change-Id: I830f0bf064d83fddd54c08385b70a6ab6606b866
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/263557
    Commit-Queue: Leandro Lovisolo <lovisolo@google.com>
    Reviewed-by: Kevin Lubick <kjlubick@google.com>

--- golden/pages/transitional.js
+++ golden/pages/transitional.js
@@ -1,7 +1,8 @@
 // this holds all the lit-html modules that we make so they
 // can be incorporated into the legacy Polymer pages.
 
-import '../../infra-sk/modules/login-sk'
-import '../node_modules/@webcomponents/custom-elements/custom-elements.min.js'
-import '../modules/changelist-controls-sk'
-import '../modules/dots-sk'
+import '../../infra-sk/modules/login-sk';
+import '../node_modules/@webcomponents/custom-elements/custom-elements.min.js';
+import '../modules/changelist-controls-sk';
+import '../modules/dots-sk';
+import '../modules/dots-legend-sk';
