a0ce38d91444e493cdeab8de19a2c612b4986752
Kevin Lubick <kjlubick@google.com>
2020-01-10 12:49:19 -0500

    [gold] Add new ignores page
    This adds the HTML and the CSS scaffolding - no logic to deal with
    clickable things; those will come in a followup.
    
    Change-Id: Ie2859d8c46a55ecb1fe01ce70a3becf77d39beaf
    Bug: skia:9525

---
+++ golden/modules/ignores-page-sk/ignores-page-sk-demo.js
@@ -0,0 +1,12 @@
+import './index.js'
+import '../gold-scaffold-sk'
+
+import { delay } from '../demo_util'
+import { ignoreRules_10, fakeNow } from './test_data'
+
+const fetchMock = require('fetch-mock');
+
+Date.now = () => fakeNow;
+
+fetchMock.get('/json/ignores?counts=1', delay(ignoreRules_10, 300));
+fetchMock.catch(404);
