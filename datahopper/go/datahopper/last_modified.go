f40fc57f6950f442bf23de6f6dbfc4e2a5cc2b1b
Eric Boren <borenet@google.com>
2020-01-13 06:42:46 -0500

    More work on UpdateGoDEPS
    1. Add some documentation for what to do when it fails.
    2. Add metrics and alerts which fire when go.mod is not being updated.
    
    Bug: skia:9509
    Change-Id: If1ce7a58b224e03724ff9b6efda92b5238c42a18
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/263709
    Reviewed-by: Ravi Mistry <rmistry@google.com>
    Commit-Queue: Eric Boren <borenet@google.com>

---
+++ datahopper/go/datahopper/last_modified.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+
+	"go.skia.org/infra/go/gitiles"
+	"go.skia.org/infra/go/metrics2"
+	"go.skia.org/infra/go/skerr"
+	"go.skia.org/infra/go/sklog"
+	"go.skia.org/infra/go/util"
+)
+
+const (
+	METRIC_LAST_MODIFIED = "liveness_last_file_modification_s"
+)
+
+func updateLastModifiedMetrics(ctx context.Context, client *http.Client, reposToFiles map[*gitiles.Repo][]string) error {
+	now := time.Now()
+	for repo, files := range reposToFiles {
+		for _, file := range files {
+			log, err := repo.Log(ctx, fmt.Sprintf("master/%s", file), gitiles.LogLimit(1))
+			if err != nil {
+				return skerr.Wrapf(err, "Failed loading %s", file)
+			}
+			if len(log) != 1 {
+				return skerr.Fmt("Failed to obtain the last commit which modified %s in %s; expected 1 commit but got %d", file, repo.URL, len(log))
+			}
+			metrics2.GetInt64Metric(METRIC_LAST_MODIFIED, map[string]string{
+				"repo": repo.URL,
+				"file": file,
+			}).Update(int64(now.Sub(log[0].Timestamp)))
+		}
+	}
+	return nil
+}
+
+func StartLastModifiedMetrics(ctx context.Context, client *http.Client, filesByRepo map[string][]string) {
+	reposToFiles := make(map[*gitiles.Repo][]string, len(filesByRepo))
+	for repo, filePaths := range filesByRepo {
+		r := gitiles.NewRepo(repo, client)
+		reposToFiles[r] = filePaths
+	}
+
+	lv := metrics2.NewLiveness("last_successful_last_modified_metrics")
+	go util.RepeatCtx(5*time.Minute, ctx, func(ctx context.Context) {
+		if err := updateLastModifiedMetrics(ctx, client, reposToFiles); err == nil {
+			lv.Reset()
+		} else {
+			sklog.Errorf("Failed to update file last-modified metrics: %s", err)
+		}
+	})
+}
