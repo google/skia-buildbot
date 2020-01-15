595958fef1fb3196174f1a1f5562130fa548717c
Kevin Lubick <kjlubick@google.com>
2020-01-15 07:57:32 -0500

    [gold] Add checkbox and delete rule interaction to ignore page.
    Change-Id: I1f239d44c56e57a766402e08ab0c679ba6e322f2
    Bug: skia:9525
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/263710
    Commit-Queue: Kevin Lubick <kjlubick@google.com>
    Reviewed-by: Leandro Lovisolo <lovisolo@google.com>

--- golden/modules/ignores-page-sk/ignores-page-sk-demo.js
+++ golden/modules/ignores-page-sk/ignores-page-sk-demo.js
@@ -9,4 +9,5 @@ const fetchMock = require('fetch-mock');
 Date.now = () => fakeNow;
 
 fetchMock.get('/json/ignores?counts=1', delay(ignoreRules_10, 300));
+fetchMock.post('glob:/json/ignores/del/*', delay({}, 600));
 fetchMock.catch(404);
