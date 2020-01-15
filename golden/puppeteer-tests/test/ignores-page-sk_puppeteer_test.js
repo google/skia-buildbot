e849eca2804cb7c0546c59de452ec37bbe5372c2
Kevin Lubick <kjlubick@google.com>
2020-01-15 10:56:09 -0500

    [gold] Add screenshots for ignore-page-sk's checkbox and delete popup
    Change-Id: I948c8f8cf500001d1a3e3ec561c3ae070e7242d8
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/264556
    Reviewed-by: Leandro Lovisolo <lovisolo@google.com>

--- golden/puppeteer-tests/test/ignores-page-sk_puppeteer_test.js
+++ golden/puppeteer-tests/test/ignores-page-sk_puppeteer_test.js
@@ -6,21 +6,39 @@ const takeScreenshot = require('./util').takeScreenshot;
 describe('ignores-page-sk', function() {
   setUpPuppeteerAndDemoPageServer();  // Sets up this.page and this.baseUrl.
 
-  beforeEach(async function() {
-    const eventPromise =
-      await addEventListenersToPuppeteerPage(this.page, ['end-task']);
-    const loaded = eventPromise('end-task');  // Emitted when page is loaded.
-    await this.page.goto(`${this.baseUrl}/dist/ignores-page-sk.html`);
-    await loaded;
-  });
-
   it('should render the demo page', async function() {
+    await navigateTo(this.page, this.baseUrl, '');
     // Smoke test.
     expect(await this.page.$$('ignores-page-sk')).to.have.length(1);
   });
 
-  it('should take a screenshot', async function() {
-    await this.page.setViewport({ width: 1300, height: 2100 });
-    await takeScreenshot(this.page, 'Test-Ignores-Page-Sk');
+  describe('screenshots', function() {
+    it('should show the default page', async function() {
+      await navigateTo(this.page, this.baseUrl, '');
+      await this.page.setViewport({ width: 1300, height: 2100 });
+      await takeScreenshot(this.page, 'Test-Ignores-Page-Sk');
+    });
+
+    it('should show a popup when delete is clicked', async function() {
+      await navigateTo(this.page, this.baseUrl, '');
+      // zoom in a little to see better.
+      await this.page.setViewport({ width: 1300, height: 1300 });
+      await this.page.click('ignores-page-sk tbody > tr:nth-child(1) > td.mutate-icons > delete-icon-sk');
+      await takeScreenshot(this.page, 'Test-Ignores-Page-Sk_DeletePopup');
+    });
+
+    it('should show the counts of all traces', async function() {
+      await navigateTo(this.page, this.baseUrl, '?count_all=true');
+      await this.page.setViewport({ width: 1300, height: 2100 });
+      await takeScreenshot(this.page, 'Test-Ignores-Page-Sk_AllTraces');
+    });
   });
 });
+
+async function navigateTo(page, base, queryParams) {
+  const eventPromise =
+    await addEventListenersToPuppeteerPage(page, ['end-task']);
+  const loaded = eventPromise('end-task');  // Emitted when page is loaded.
+  await page.goto(`${base}/dist/ignores-page-sk.html${queryParams}`);
+  await loaded;
+}
