068f6667c41ff9ec02b5b983aadcf40ea99ed39b
Joe Gregorio <jcgregorio@google.com>
2020-01-15 15:00:03 -0500

    [perf] Add support for absolute and percentage alerts.
    Bug: skia:8521
    Change-Id: Id2f70207b570551434ac7354cb052bc474712412

--- perf/go/regression/async.go
+++ perf/go/regression/async.go
@@ -69,6 +69,7 @@ type ClusterRequest struct {
 	TZ          string             `json:"tz"`
 	Algo        types.ClusterAlgo  `json:"algo"`
 	Interesting float32            `json:"interesting"`
+	Delta       float32            `json:"delta"` // Used by both absolute and percent Algos.
 	Sparse      bool               `json:"sparse"`
 	Type        ClusterRequestType `json:"type"`
 	N           int32              `json:"n"`
@@ -373,6 +374,11 @@ func (p *ClusterRequestProcess) Run(ctx context.Context) {
 			summary, err = clustering2.CalculateClusterSummaries(df, k, config.MIN_STDDEV, p.clusterProgress, p.request.Interesting)
 		case types.STEPFIT_ALGO:
 			summary, err = StepFit(df, k, config.MIN_STDDEV, p.clusterProgress, p.request.Interesting)
+		case types.ABSOLUTE_ALGO:
+			summary, err = Absolute(df, p.clusterProgress, p.request.Interesting, false)
+		case types.PERCENT_ALGO:
+			summary, err = Absolute(df, p.clusterProgress, p.request.Interesting, false)
+
 		default:
 			p.reportError(skerr.Fmt("Invalid type of clustering: %s", p.request.Algo), "Invalid type of clustering.")
 		}
