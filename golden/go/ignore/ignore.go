7a6a3066600bfccf3ab7af593010f9effff2a18d
Kevin Lubick <kjlubick@google.com>
2020-01-15 14:56:00 -0500

    [gold] Run metrics collection for ignores on a background process
    Change-Id: Iad54231a70fecdae25b81994669da6373edca097

--- golden/go/ignore/ignore.go
+++ golden/go/ignore/ignore.go
@@ -134,16 +134,15 @@ func StartMetrics(ctx context.Context, store Store, interval time.Duration) erro
 	if err != nil {
 		return skerr.Wrapf(err, "starting to monitor ignore rules")
 	}
-	util.RepeatCtx(interval, ctx, func(ctx context.Context) {
-		for range time.Tick(interval) {
+	go func() {
+		util.RepeatCtx(interval, ctx, func(ctx context.Context) {
 			err = oneStep(ctx, store, numExpired)
 			if err != nil {
 				sklog.Errorf("Failed one step of monitoring ignore rules: %s", err)
-				continue
+				return
 			}
 			liveness.Reset()
-		}
-	})
-
+		})
+	}()
 	return nil
 }
