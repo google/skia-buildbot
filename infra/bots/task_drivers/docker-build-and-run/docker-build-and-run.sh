306e555c5091498eddb685a23d2af2175cd3f2a1
Eric Boren <borenet@google.com>
2020-01-17 12:54:48 -0500

    Infra
    WIP playing with docker tasks
    
    Change-Id: I04a54148a9af6f523f97ba51f8b89f934567d3e1

---
+++ infra/bots/task_drivers/docker-build-and-run/docker-build-and-run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# We need a wrapper script because Swarming doesn't run the command in a shell,
+# which means we can't use environment variables directly in the command.
+set -e -x
+
+env
+docker run \
+  -v $(pwd):/SRC \
+  -v $(dirname ${LUCI_CONTEXT}):/AUTH \
+  -v ${SWARMING_OUT_DIR}:/OUT \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  --env LUCI_CONTEXT=/AUTH/$(basename ${LUCI_CONTEXT}) \
+  --env SWARMING_BOT_ID \
+  --env SWARMING_SERVER \
+  --env SWARMING_TASK_ID \
+  --network host \
+  gcr.io/skia-public/docker-build-and-run:prod \
+  $@
