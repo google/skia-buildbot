5bb789621a8a0fbadef060f2743f793526d924b5
Joe Gregorio <jcgregorio@google.com>
2020-01-17 15:57:45 -0500

    [perf] Rationalize step detection and clustering.
    Bug: skia:8521
    Change-Id: I98ad7b8e4115aafaedbd3ad1ded2615f087dce80

--- perf/go/types/types.go
+++ perf/go/types/types.go
@@ -27,11 +27,34 @@ type ClusterAlgo string
 // Update algo-select-sk if this enum is changed.
 const (
 	KMEANS_ALGO  ClusterAlgo = "kmeans"  // Cluster traces using k-means clustering on their shapes.
-	STEPFIT_ALGO ClusterAlgo = "stepfit" // Look at each trace individually and determing if it steps up or down.
+	STEPFIT_ALGO ClusterAlgo = "stepfit" // Look at each trace individually and determine if it steps up or down.
+)
+
+// StepDetection are the different ways we can look at an individual trace, or a
+// cluster centroid (which is also a single trace), and detect if a step has
+// occurred.
+type StepDetection string
+
+const (
+	// ORIGINAL_STEP is the original type of step detection. Note we leave as empty string so we pick up the right default from old alerts.
+	ORIGINAL_STEP StepDetection = ""
+	ABSOLUTE_STEP StepDetection = "absolute" // Look for an absolute magnitude change.
+	PERCENT_STEP  StepDetection = "percent"  // Look for a percentage change.
+	COHEN_STEP    StepDetection = "cohen"    // Use Cohen's d method to detect a change.
 )
 
 var (
-	AllClusterAlgos = []ClusterAlgo{KMEANS_ALGO, STEPFIT_ALGO}
+	AllClusterAlgos = []ClusterAlgo{
+		KMEANS_ALGO,
+		STEPFIT_ALGO,
+	}
+
+	AllStepDetections = []StepDetection{
+		ORIGINAL_STEP,
+		ABSOLUTE_STEP,
+		PERCENT_STEP,
+		COHEN_STEP,
+	}
 )
 
 func ToClusterAlgo(s string) (ClusterAlgo, error) {
@@ -42,4 +65,14 @@ func ToClusterAlgo(s string) (ClusterAlgo, error) {
 		}
 	}
 	return ret, fmt.Errorf("%q is not a valid ClusterAlgo, must be a value in %v", s, AllClusterAlgos)
+}
+
+func ToStepDetection(s string) (StepDetection, error) {
+	ret := StepDetection(s)
+	for _, c := range AllStepDetections {
+		if c == ret {
+			return ret, nil
+		}
+	}
+	return ret, fmt.Errorf("%q is not a valid StepDetection, must be a value is %v", s, AllStepDetections)
 }
