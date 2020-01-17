b5812345ad501427a9fb935deb272ec37a6ed464
Ben Wagner <benjaminwagner@google.com>
2020-01-17 15:21:18 -0500

    Fix libimobiledevice build.sh
    Change-Id: I16d470e2306263f7703e2e462445dfc67e52adbc

--- skolo/ios/build.sh
+++ skolo/ios/build.sh
@@ -8,7 +8,7 @@ set -x -e
 PREFIX=`pwd`/out
 mkdir -p ${PREFIX}
 
-rm -rf out libplist libusbmuxd usbmuxd libimobiledevice ifuse ideviceinstaller
+rm -rf out libplist libusbmuxd usbmuxd libimobiledevice ifuse ideviceinstaller idevice-app-runner
 
 git clone https://github.com/libimobiledevice/libplist.git
 git clone https://github.com/libimobiledevice/usbmuxd.git
@@ -16,29 +16,58 @@ git clone https://github.com/libimobiledevice/libusbmuxd.git
 git clone https://github.com/libimobiledevice/libimobiledevice.git
 git clone https://github.com/libimobiledevice/ifuse.git
 git clone https://github.com/libimobiledevice/ideviceinstaller.git
+git clone https://github.com/storoj/idevice-app-runner.git
 
 # Make sure the libraries below are found.
 export PKG_CONFIG_PATH="${PREFIX}/lib/pkgconfig"
+export CPPFLAGS="-I${PREFIX}/include"
+export LDFLAGS="-L${PREFIX}/lib"
 
 # Build and install in order of dependencies.
-cd libplist && ./autogen.sh --prefix=$PREFIX --without-cython && make && make install && cd ..
-cd libusbmuxd && ./autogen.sh --prefix=$PREFIX && make && make install && cd ..
-cd libimobiledevice && ./autogen.sh --prefix=$PREFIX --without-cython && make && make install && cd ..
-cd ifuse && ./autogen.sh --prefix=$PREFIX && make && make install && cd ..
+cd libplist
+./autogen.sh --prefix=$PREFIX --without-cython
+make
+make install
+cd ..
 
-# Patch a specific commit of ideviceinstaller so it can be compile on RPi Stretch
-# The reason might be 32-bit vs 64-bit architectures or the fact that RPi Stretch 
-# uses gcc 6 by default while Debian uses gcc 7. 
+cd libusbmuxd
+./autogen.sh --prefix=$PREFIX
+make
+make install
+cd ..
+
+cd libimobiledevice
+# Apply patch for iOS 13.
+git fetch origin pull/860/head
+git cherry-pick FETCH_HEAD
+./autogen.sh --prefix=$PREFIX --without-cython
+make
+make install
+cd ..
+
+cd ifuse
+./autogen.sh --prefix=$PREFIX
+make
+make install
+cd ..
+
 cd ideviceinstaller
-git checkout f7988de8279051f3d2d7973b8d7f2116aa5d9317
-git am ../patches/ideviceinstaller.patch
 ./autogen.sh --prefix=$PREFIX
 make
 make install 
 cd ..
 
 cd usbmuxd
+# Revert broken udev rules.
+git revert --no-edit ec5ff91cfabd30637f8af8f5c79baf4d7818ce57
 ./autogen.sh --prefix=$PREFIX \
-              --with-udevrulesdir=$PREFIX/udev-rules \
-              --with-systemdsystemunitdir=$PREFIX/systemd
-make && make install && cd ..
+  --with-udevrulesdir=$PREFIX/udev-rules \
+  --with-systemdsystemunitdir=$PREFIX/systemd
+make
+make install
+cd ..
+
+cd idevice-app-runner
+CPATH=$PREFIX/include LIBRARY_PATH=$PREFIX/lib make
+DESTDIR=$PREFIX make install
+cd ..
