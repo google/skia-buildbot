b313797abee22b53d9c056f0bb4e3a6d230d9157
Leandro Lovisolo <lovisolo@google.com>
2020-01-21 15:27:53 -0500

    [gold] changelists-page-sk-demo.js: Make demo page deterministic.
    Minor, mostly cosmetic changes.
    
    Bug: skia:9525
    Change-Id: Ic9e5ad01757fb76db6f92f9715c386803a23642e

--- golden/modules/changelists-page-sk/changelists-page-sk-demo.js
+++ golden/modules/changelists-page-sk/changelists-page-sk-demo.js
@@ -2,13 +2,13 @@ import './index.js'
 import '../gold-scaffold-sk'
 
 import { delay } from '../demo_util'
-import { changelistSummaries_5, empty } from './test_data'
+import { fakeNow, changelistSummaries_5, empty } from './test_data'
+import { fetchMock } from 'fetch-mock';
+import { deepCopy } from 'common-sk/modules/object';
 
-(function(){
+Date.now = () => fakeNow;
 
-const fetchMock = require('fetch-mock');
-
-const ten = JSON.parse(JSON.stringify(changelistSummaries_5));
+const ten = deepCopy(changelistSummaries_5);
 ten.data.push(...ten.data);
 ten.pagination = {
   offset: 0,
@@ -22,7 +22,7 @@ changelistSummaries_5.pagination = {
   total: 15,
 };
 
-const open = JSON.parse(JSON.stringify(changelistSummaries_5));
+const open = deepCopy(changelistSummaries_5);
 open.data = open.data.slice(0, 3);
 open.pagination = {
   offset: 0,
@@ -34,6 +34,3 @@ fetchMock.get('/json/changelists?offset=0&size=10', delay(ten, 300));
 fetchMock.get('/json/changelists?offset=0&size=10&active=true', delay(open, 300));
 fetchMock.get('/json/changelists?offset=10&size=10', delay(changelistSummaries_5, 300));
 fetchMock.get('glob:/json/changelists*', delay(empty, 300));
-
-fetchMock.catch(404);
-})();
