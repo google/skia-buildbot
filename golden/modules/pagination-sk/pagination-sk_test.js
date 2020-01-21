6194fc63a0ab6ab1271291e6c8bcf8d72ac33fed
Leandro Lovisolo <lovisolo@google.com>
2020-01-21 17:07:01 -0500

    [gold] Simplify some tests.
    Specifically:
     - Don't call window.customElements.whenDefined('my-component-sk').then(...), as custom elements are already defined before our tests run.
     - Don't use container elements; add the component under test directly as a child of document.body.
    
    This CL makes our testing more uniform and will facilitate the extraction of common patterns from our tests into shared functions.
    
    Bug: skia:9525
    Change-Id: I7e1ccb1f15c1d772ab22dc10a7edaa06a4ae1dbc

--- golden/modules/pagination-sk/pagination-sk_test.js
+++ golden/modules/pagination-sk/pagination-sk_test.js
@@ -3,103 +3,89 @@ import './index.js'
 import { $, $$ } from 'common-sk/modules/dom'
 
 describe('pagination-sk', () => {
+  let paginationSk;
 
-  // A reusable HTML element in which we create our element under test.
-  const container = document.createElement('div');
-  document.body.appendChild(container);
-
-  afterEach(function() {
-    container.innerHTML = '';
+  beforeEach(() => {
+    paginationSk = document.createElement('pagination-sk');
+    paginationSk.setAttribute('offset', '0');
+    paginationSk.setAttribute('total', '127');
+    paginationSk.setAttribute('page_size', '20');
+    document.body.appendChild(paginationSk);
   });
 
-  // calls the test callback with an element under test 'ele'.
-  // We can't put the describes inside the whenDefined callback because
-  // that doesn't work on Firefox (and possibly other places).
-  function createElement(test) {
-    return window.customElements.whenDefined('pagination-sk').then(() => {
-      container.innerHTML = `
-        <pagination-sk offset=0 total=127 page_size=20></pagination-sk>`;
-      expect(container.firstElementChild).to.not.be.null;
-      test(container.firstElementChild);
-    });
-  }
-
-  //===============TESTS START====================================
+  afterEach(() => {
+    // Remove the stale instance under test.
+    if (paginationSk) {
+      document.body.removeChild(paginationSk);
+      paginationSk = null;
+    }
+  });
 
   describe('html layout', () => {
     it('has three buttons', () => {
-      return createElement((ele) => {
-        const btns = $('button', ele);
-        expect(btns.length).to.equal(3);
-        // backward
-        expect(btns[0].hasAttribute('disabled')).to.be.true;
-        // forward
-        expect(btns[1].hasAttribute('disabled')).to.be.false;
-        // forward+5
-        expect(btns[2].hasAttribute('disabled')).to.be.false;
+      const btns = $('button', paginationSk);
+      expect(btns.length).to.equal(3);
+      // backward
+      expect(btns[0].hasAttribute('disabled')).to.be.true;
+      // forward
+      expect(btns[1].hasAttribute('disabled')).to.be.false;
+      // forward+5
+      expect(btns[2].hasAttribute('disabled')).to.be.false;
 
-        ele.offset = 20;
-        ele._render();
-        expect(btns[0].hasAttribute('disabled')).to.be.false;
-        expect(btns[1].hasAttribute('disabled')).to.be.false;
-        expect(btns[2].hasAttribute('disabled')).to.be.false;
+      paginationSk.offset = 20;
+      paginationSk._render();
+      expect(btns[0].hasAttribute('disabled')).to.be.false;
+      expect(btns[1].hasAttribute('disabled')).to.be.false;
+      expect(btns[2].hasAttribute('disabled')).to.be.false;
 
-        ele.offset = 40;
-        ele._render();
-        expect(btns[0].hasAttribute('disabled')).to.be.false;
-        expect(btns[1].hasAttribute('disabled')).to.be.false;
-        expect(btns[2].hasAttribute('disabled')).to.be.true;
+      paginationSk.offset = 40;
+      paginationSk._render();
+      expect(btns[0].hasAttribute('disabled')).to.be.false;
+      expect(btns[1].hasAttribute('disabled')).to.be.false;
+      expect(btns[2].hasAttribute('disabled')).to.be.true;
 
-        ele.offset = 120;
-        ele._render();
-        expect(btns[0].hasAttribute('disabled')).to.be.false;
-        expect(btns[1].hasAttribute('disabled')).to.be.true;
-        expect(btns[2].hasAttribute('disabled')).to.be.true;
-      });
+      paginationSk.offset = 120;
+      paginationSk._render();
+      expect(btns[0].hasAttribute('disabled')).to.be.false;
+      expect(btns[1].hasAttribute('disabled')).to.be.true;
+      expect(btns[2].hasAttribute('disabled')).to.be.true;
     });
 
     it('displays the page count', () => {
-      return createElement((ele) => {
-        const cnt = $$('.counter', ele);
-        expect(cnt).to.not.be.null;
-        expect(cnt.textContent).to.have.string('page 1');
-      });
+      const cnt = $$('.counter', paginationSk);
+      expect(cnt).to.not.be.null;
+      expect(cnt.textContent).to.have.string('page 1');
     });
 
     it('has several properties', () => {
-      return createElement((ele) => {
-        expect(ele.total).to.equal(127);
-        expect(ele.offset).to.equal(0);
-        expect(ele.page_size).to.equal(20);
-      });
+      expect(paginationSk.total).to.equal(127);
+      expect(paginationSk.offset).to.equal(0);
+      expect(paginationSk.page_size).to.equal(20);
     });
   });// end describe('html layout')
 
   describe('paging behavior', () => {
     it('creates page events', (done) => {
-      createElement((ele) => {
-        ele.offset = 20;
-        ele._render();
-        const btns = $('button', ele);
-        expect(btns.length).to.equal(3);
-        const bck =  btns[0];
-        const fwd =  btns[1];
-        const pls5 = btns[2];
+      paginationSk.offset = 20;
+      paginationSk._render();
+      const btns = $('button', paginationSk);
+      expect(btns.length).to.equal(3);
+      const bck =  btns[0];
+      const fwd =  btns[1];
+      const pls5 = btns[2];
 
-        let d = 0;
-        const deltas = [1 ,-1, 5];
-        ele.addEventListener('page-changed', (e) => {
-          expect(e.detail.delta).to.equal(deltas[d]);
-          d++;
-          if (d === 3) {
-            done();
-          }
-        });
-        fwd.click();
-        bck.click();
-        pls5.click();
+      let d = 0;
+      const deltas = [1 ,-1, 5];
+      paginationSk.addEventListener('page-changed', (e) => {
+        expect(e.detail.delta).to.equal(deltas[d]);
+        d++;
+        if (d === 3) {
+          done();
+        }
       });
+      fwd.click();
+      bck.click();
+      pls5.click();
     });
   }); // end describe('paging behavior')
-
 });
