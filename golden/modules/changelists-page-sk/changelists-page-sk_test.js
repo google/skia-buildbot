2f9cc21cca0963b4b8c80bdbcc78b14ace86f443
Leandro Lovisolo <lovisolo@google.com>
2020-01-22 14:48:31 -0500

    [gold] Simplify some tests.
    Specifically:
     - Don't call window.customElements.whenDefined('my-component-sk').then(...), as custom elements are already defined before our tests run.
     - Don't use container elements; add the component under test directly as a child of document.body.
    
    This CL makes our testing more uniform and will facilitate the extraction of common patterns from our tests into shared functions.
    
    Bug: skia:9525
    Change-Id: I7e1ccb1f15c1d772ab22dc10a7edaa06a4ae1dbc
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/265216
    Commit-Queue: Leandro Lovisolo <lovisolo@google.com>
    Reviewed-by: Kevin Lubick <kjlubick@google.com>

--- golden/modules/changelists-page-sk/changelists-page-sk_test.js
+++ golden/modules/changelists-page-sk/changelists-page-sk_test.js
@@ -11,130 +11,89 @@ import { eventPromise, expectQueryStringToEqual } from '../test_util';
 import { fetchMock }  from 'fetch-mock';
 
 describe('changelists-page-sk', () => {
-  // A reusable HTML element in which we create our element under test.
-  const container = document.createElement('div');
-  document.body.appendChild(container);
+  let changelistsPageSk;
 
-  beforeEach(function() {
+  const loadChangelistsPageSk = async () => {
+    const endTask = eventPromise('end-task');
+    changelistsPageSk = document.createElement('changelists-page-sk');
+    document.body.appendChild(changelistsPageSk);
+    await endTask;
+  };
+
+  beforeEach(async () => {
     // Clear out any query params we might have to not mess with our current state.
     setQueryString('');
-  });
 
-  beforeEach(function() {
     // These are the default offset/page_size params
     fetchMock.get('/json/changelists?offset=0&size=50&active=true', JSON.stringify(changelistSummaries_5));
-
-    fetchMock.catch(404);
   });
 
   afterEach(function() {
+    // Remove the stale instance under test.
+    if (changelistsPageSk) {
+      document.body.removeChild(changelistsPageSk);
+      changelistsPageSk = null;
+    }
+
     // Completely remove the mocking which allows each test
     // to be able to mess with the mocked routes w/o impacting other tests.
     fetchMock.reset();
   });
 
-  afterEach(function() {
-    container.innerHTML = '';
-  });
-
-  // calls the test callback with an element under test 'ele'.
-  // We can't put the describes inside the whenDefined callback because
-  // that doesn't work on Firefox (and possibly other places).
-  function createElement(test) {
-    return window.customElements.whenDefined('changelists-page-sk').then(() => {
-      container.innerHTML = `<changelists-page-sk></changelists-page-sk>`;
-      expect(container.firstElementChild).to.not.be.null;
-      test(container.firstElementChild);
-    });
-  }
-
-  function whenPageLoads(test) {
-    // The changelists-page-sk emits an 'end-task' event when each fetch finishes.
-    // For now, there is only one, but this logic may have to be tweaked if we
-    // do multiple.
-    let ran = false;
-    let ele = null;
-    const fn = (e) => {
-      e.stopPropagation(); // Prevent interference with eventPromise('end-task').
-      // reset for next time
-      container.removeEventListener('end-task', fn);
-      if (!ran) {
-        ran = true; // prevent multiple runs if the test makes the
-                    // app go busy (e.g. if it calls fetch).
-        test(ele);
-      }
-    };
-    // add the listener and then create the element to make sure we don't miss
-    // the busy-end event. The busy-end event should trigger when all fetches
-    // are done and the page is rendered.
-    container.addEventListener('end-task', fn);
-    createElement((e) => {
-      ele = e;
-    });
-  }
-
-  //===============TESTS START====================================
-
   describe('html layout', () => {
-    it('should make a table with 5 rows in the body', (done) => {
-      whenPageLoads((ele) => {
-        const tbl = $$('table', ele);
-        expect(tbl).to.not.be.null;
-        const rows = $('tbody tr', ele);
-        expect(rows.length).to.equal(5); // one row per item in changelistSummaries_5
-        done();
-      });
+    beforeEach(loadChangelistsPageSk);
+
+    it('should make a table with 5 rows in the body', () => {
+      const tbl = $$('table', changelistsPageSk);
+      expect(tbl).to.not.be.null;
+      const rows = $('tbody tr', changelistsPageSk);
+      expect(rows.length).to.equal(5); // one row per item in changelistSummaries_5
     });
 
-    it('has icons that indicate the status', (done) => {
-      whenPageLoads((ele) => {
-        const rows = $('tbody tr', ele);
-        // First row has an open CL.
-        let icon = $$('cached-icon-sk', rows[0]);
-        expect(icon).to.not.be.null;
-        // Fourth row has an abandoned CL.
-        icon = $$('block-icon-sk', rows[3]);
-        expect(icon).to.not.be.null;
-        // Fifth row has an closed CL.
-        icon = $$('done-icon-sk', rows[4]);
-        expect(icon).to.not.be.null;
-        done();
-      });
+    it('has icons that indicate the status', () => {
+      const rows = $('tbody tr', changelistsPageSk);
+      // First row has an open CL.
+      let icon = $$('cached-icon-sk', rows[0]);
+      expect(icon).to.not.be.null;
+      // Fourth row has an abandoned CL.
+      icon = $$('block-icon-sk', rows[3]);
+      expect(icon).to.not.be.null;
+      // Fifth row has an closed CL.
+      icon = $$('done-icon-sk', rows[4]);
+      expect(icon).to.not.be.null;
     });
   }); // end describe('html layout')
 
   describe('api calls', () => {
-    it('includes pagination params in request to changelists', (done) => {
-      whenPageLoads((ele) => {
-        fetchMock.resetHistory();
+    beforeEach(loadChangelistsPageSk);
 
-        fetchMock.get('/json/changelists?offset=100&size=10', JSON.stringify(empty));
-        // pretend these were loaded in via stateReflector
-        ele._offset = 100;
-        ele._page_size = 10;
-        ele._showAll = true;
+    it('includes pagination params in request to changelists', async () => {
+      fetchMock.resetHistory();
 
-        ele._fetch().then(done);
-      });
+      fetchMock.get('/json/changelists?offset=100&size=10', JSON.stringify(empty));
+      // pretend these were loaded in via stateReflector
+      changelistsPageSk._offset = 100;
+      changelistsPageSk._page_size = 10;
+      changelistsPageSk._showAll = true;
+
+      await changelistsPageSk._fetch();
     });
 
-    it('includes the active params unless show_all is set', (done) => {
-      whenPageLoads((ele) => {
-        fetchMock.resetHistory();
+    it('includes the active params unless show_all is set', async () => {
+      fetchMock.resetHistory();
 
-        fetchMock.get('/json/changelists?offset=100&size=10&active=true', JSON.stringify(empty));
-        // pretend these were loaded in via stateReflector
-        ele._offset = 100;
-        ele._page_size = 10;
-        ele._showAll = false;
+      fetchMock.get('/json/changelists?offset=100&size=10&active=true', JSON.stringify(empty));
+      // pretend these were loaded in via stateReflector
+      changelistsPageSk._offset = 100;
+      changelistsPageSk._page_size = 10;
+      changelistsPageSk._showAll = false;
 
-        ele._fetch().then(done);
-      });
+      changelistsPageSk._fetch();
     });
   }); // end describe('api calls')
 
   describe('navigation', () => {
-    it('responds to the browser back/forward buttons', (done) => {
+    it('responds to the browser back/forward buttons', async () => {
       // First page of results.
       fetchMock.get(
           '/json/changelists?offset=0&size=5&active=true',
@@ -157,69 +116,65 @@ describe('changelists-page-sk', () => {
       // required for the mock RPCs above to work.
       setQueryString('?page_size=5');
 
-      whenPageLoads(async (el) => {
-        expectQueryStringToEqual('?page_size=5');
-        expectFirstPage();
+      await loadChangelistsPageSk();  // Instantiate component.
+      expectQueryStringToEqual('?page_size=5');
+      expectFirstPage();
 
-        await goToNextPage(el);
-        expectQueryStringToEqual('?offset=5&page_size=5');
-        expectSecondPage();
+      await goToNextPage(changelistsPageSk);
+      expectQueryStringToEqual('?offset=5&page_size=5');
+      expectSecondPage();
 
-        await goToNextPage(el);
-        expectQueryStringToEqual('?offset=10&page_size=5');
-        expectThirdPage();
+      await goToNextPage(changelistsPageSk);
+      expectQueryStringToEqual('?offset=10&page_size=5');
+      expectThirdPage();
 
-        await goBack();
-        expectQueryStringToEqual('?offset=5&page_size=5');
-        expectSecondPage();
+      await goBack();
+      expectQueryStringToEqual('?offset=5&page_size=5');
+      expectSecondPage();
 
-        // State at component instantiation.
-        await goBack();
-        expectQueryStringToEqual('?page_size=5');
-        expectFirstPage();
+      // State at component instantiation.
+      await goBack();
+      expectQueryStringToEqual('?page_size=5');
+      expectFirstPage();
 
-        // State before the component was instantiated.
-        await goBack();
-        expectQueryStringToEqual('?hello=world');
+      // State before the component was instantiated.
+      await goBack();
+      expectQueryStringToEqual('?hello=world');
 
-        await goForward();
-        expectQueryStringToEqual('?page_size=5');
-        expectFirstPage();
+      await goForward();
+      expectQueryStringToEqual('?page_size=5');
+      expectFirstPage();
 
-        await goForward();
-        expectQueryStringToEqual('?offset=5&page_size=5');
-        expectSecondPage();
+      await goForward();
+      expectQueryStringToEqual('?offset=5&page_size=5');
+      expectSecondPage();
 
-        await goForward();
-        expectQueryStringToEqual('?offset=10&page_size=5');
-        expectThirdPage();
-
-        done();
-      });
+      await goForward();
+      expectQueryStringToEqual('?offset=10&page_size=5');
+      expectThirdPage();
     });
   }); // end describe('navigation')
 
   describe('dynamic content', () => {
-    it('responds to clicking the show all checkbox', (done) => {
-      whenPageLoads((ele) => {
-        fetchMock.get('/json/changelists?offset=0&size=50', JSON.stringify(empty));
-        // click on the input inside the checkbox, otherwise, we see double
-        // events, since checkbox-sk "re-throws" the click event.
-        const showAllBox = $$('.controls checkbox-sk input', ele);
-        expect(showAllBox).to.not.be.null;
-        expect(ele._showAll).to.equal(false);
-        expectQueryStringToEqual('');
-        showAllBox.click();
-        expect(ele._showAll).to.equal(true);
-        expectQueryStringToEqual('?page_size=50&show_all=true');
-        showAllBox.click();
-        expect(ele._showAll).to.equal(false);
-        expectQueryStringToEqual('?page_size=50');
-        showAllBox.click();
-        expect(ele._showAll).to.equal(true);
-        expectQueryStringToEqual('?page_size=50&show_all=true');
-        done();
-      });
+    beforeEach(loadChangelistsPageSk);
+
+    it('responds to clicking the show all checkbox', () => {
+      fetchMock.get('/json/changelists?offset=0&size=50', JSON.stringify(empty));
+      // click on the input inside the checkbox, otherwise, we see double
+      // events, since checkbox-sk "re-throws" the click event.
+      const showAllBox = $$('.controls checkbox-sk input', changelistsPageSk);
+      expect(showAllBox).to.not.be.null;
+      expect(changelistsPageSk._showAll).to.equal(false);
+      expectQueryStringToEqual('');
+      showAllBox.click();
+      expect(changelistsPageSk._showAll).to.equal(true);
+      expectQueryStringToEqual('?page_size=50&show_all=true');
+      showAllBox.click();
+      expect(changelistsPageSk._showAll).to.equal(false);
+      expectQueryStringToEqual('?page_size=50');
+      showAllBox.click();
+      expect(changelistsPageSk._showAll).to.equal(true);
+      expectQueryStringToEqual('?page_size=50&show_all=true');
     });
 
   }); // end describe('dynamic content')
@@ -229,9 +184,9 @@ describe('changelists-page-sk', () => {
         null, '', window.location.origin + window.location.pathname + q);
   }
 
-  function goToNextPage(el) {
+  function goToNextPage(changelistsPageSk) {
     const event = eventPromise('end-task');
-    $$('pagination-sk button.next', el).click();
+    $$('pagination-sk button.next', changelistsPageSk).click();
     return event;
   }
 
