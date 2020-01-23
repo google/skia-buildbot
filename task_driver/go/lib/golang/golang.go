e75da14807da595a1face6f32b32e2b861927e5d
Eric Boren <borenet@google.com>
2020-01-23 07:31:06 -0500

    WIP add task driver adapter for Go tests
    Change-Id: I27393f32a3ea46efff1b76e234930acf030f85db

--- task_driver/go/lib/golang/golang.go
+++ task_driver/go/lib/golang/golang.go
@@ -1,13 +1,22 @@
 package golang
 
 import (
+	"bufio"
+	"bytes"
 	"context"
+	"encoding/json"
+	"errors"
 	"fmt"
+	"io"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"strings"
+	"sync"
 
-	"go.skia.org/infra/go/exec"
+	skexec "go.skia.org/infra/go/exec"
+	"go.skia.org/infra/go/sklog"
+	"go.skia.org/infra/go/test2json"
 	"go.skia.org/infra/task_driver/go/lib/dirs"
 	"go.skia.org/infra/task_driver/go/lib/os_steps"
 	"go.skia.org/infra/task_driver/go/td"
@@ -51,7 +60,7 @@ func WithEnv(ctx context.Context, workdir string) context.Context {
 
 // Go runs the given Go command in the given working directory.
 func Go(ctx context.Context, cwd string, args ...string) (string, error) {
-	return exec.RunCommand(ctx, &exec.Command{
+	return skexec.RunCommand(ctx, &skexec.Command{
 		Name: "go",
 		Args: args,
 		Dir:  cwd,
@@ -96,6 +105,113 @@ func InstallCommonDeps(ctx context.Context, workdir string) error {
 		}); err != nil {
 			return err
 		}
+	}
+	return nil
+}
+
+// Test runs "go test", parses the output, and creates sub-steps for individual
+// tests.
+func Test(ctx context.Context, cwd string, args ...string) error {
+	ctx = td.StartStep(ctx, td.Props(fmt.Sprintf("go test --json %s", strings.Join(args, " "))))
+	defer td.EndStep(ctx)
+
+	// Set up the "go test" command.
+	cmd := exec.CommandContext(ctx, "go", append([]string{"test", "--json"}, args...)...)
+	cmd.Dir = cwd
+	cmd.Env = td.GetEnv(ctx)
+	stdout, err := cmd.StdoutPipe()
+	if err != nil {
+		return td.FailStep(ctx, err)
+	}
+	runErrors := td.NewLogStream(ctx, "golang", td.Error)
+	scanner := bufio.NewScanner(stdout)
+
+	// Spin up a goroutine which parses the JSON output of "go test" and
+	// creates sub-steps.
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	// runErr records any errors that occur within the goroutine.
+	var runErr error
+
+	go func() {
+		defer wg.Done()
+
+		// We create sub-steps for individual packages, which in turn
+		// have sub-steps for individual tests.
+		pkgContexts := map[string]context.Context{}
+		testContexts := map[string]map[string]context.Context{}
+		logs := map[context.Context]io.Writer{}
+		for scanner.Scan() {
+			line := scanner.Text()
+			fmt.Fprintln(os.Stderr, line)
+			var event test2json.Event
+			if err := json.NewDecoder(bytes.NewReader([]byte(line))).Decode(&event); err != nil {
+				runErr = err
+				if _, logErr := runErrors.Write([]byte(err.Error())); logErr != nil {
+					sklog.Errorf("Failed to write log: %s", logErr)
+				}
+				continue
+			}
+			pkgCtx, ok := pkgContexts[event.Package]
+			if !ok {
+				// This is the first time we've seen this package;
+				// create a sub-step for it.
+				pkgCtx = td.StartStep(ctx, td.Props(event.Package))
+				pkgContexts[event.Package] = pkgCtx
+				testContexts[event.Package] = map[string]context.Context{}
+			}
+			stepCtx := pkgCtx
+			if event.Test != "" {
+				testCtx, ok := testContexts[event.Package][event.Test]
+				if !ok {
+					// This is the first time we've seen this test;
+					// create a sub-step for it.
+					testCtx = td.StartStep(pkgCtx, td.Props(event.Test))
+					testContexts[event.Package][event.Test] = testCtx
+				}
+				stepCtx = testCtx
+			}
+			switch event.Action {
+			case test2json.ACTION_OUTPUT:
+				stream, ok := logs[stepCtx]
+				if !ok {
+					stream = td.NewLogStream(stepCtx, "go-test", td.Error)
+				}
+				_, err := stream.Write([]byte(event.Output))
+				if err != nil {
+					runErr = err
+					if _, logErr := runErrors.Write([]byte(err.Error())); logErr != nil {
+						sklog.Errorf("Failed to write log: %s", logErr)
+					}
+					continue
+				}
+			// These indicate that the step has finished.
+			case test2json.ACTION_FAIL:
+				td.FailStep(stepCtx, errors.New("step failed")) // TODO
+				fallthrough
+			case test2json.ACTION_SKIP:
+				fallthrough
+			case test2json.ACTION_PASS:
+				fallthrough
+			default:
+				td.EndStep(stepCtx)
+
+			}
+		}
+	}()
+
+	// Run the command.
+	if err := cmd.Run(); err != nil {
+		// Wait for log processing goroutine to finish.
+		wg.Wait()
+		return td.FailStep(ctx, err)
+	}
+
+	// Wait for log processing goroutine to finish.
+	wg.Wait()
+	if runErr != nil {
+		return td.FailStep(ctx, runErr)
 	}
 	return nil
 }
