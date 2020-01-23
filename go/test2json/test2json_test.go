27085e8a8df035ec6ddbd3721f7a3a9c3b066905
Eric Boren <borenet@google.com>
2020-01-23 10:27:29 -0500

    WIP add task driver adapter for Go tests
    Change-Id: I27393f32a3ea46efff1b76e234930acf030f85db

---
+++ go/test2json/test2json_test.go
@@ -0,0 +1,231 @@
+package test2json
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"regexp"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"go.skia.org/infra/go/deepequal/assertdeep"
+	"go.skia.org/infra/go/exec"
+	"go.skia.org/infra/go/sklog"
+	"go.skia.org/infra/go/testutils"
+	"go.skia.org/infra/go/testutils/unittest"
+	"go.skia.org/infra/go/util"
+)
+
+const (
+	// We use actual Go tests instead of just mocking output and parsing it
+	// so that if the output format changes our tests will catch it.
+	tmpl = `package %s
+
+import "testing"
+
+func %s(t *testing.T) {
+%s
+}
+`
+	ModulePath      = "fake.com/test2json_test"
+	PackageName     = "test2json_test"
+	PackageFullPath = ModulePath + "/go/" + PackageName
+	TestName        = "TestCase"
+)
+
+var (
+	CONTENT_FAIL TestContent = []byte(fmt.Sprintf(tmpl, PackageName, TestName, "t.FailNow()"))
+	CONTENT_PASS TestContent = []byte(fmt.Sprintf(tmpl, PackageName, TestName, "// No-op tests pass!"))
+	CONTENT_SKIP TestContent = []byte(fmt.Sprintf(tmpl, PackageName, TestName, "t.Skip(\"no thanks!\")"))
+
+	EVENTS_FAIL = []*Event{
+		{
+			Action:  ACTION_RUN,
+			Package: PackageFullPath,
+			Test:    TestName,
+		},
+		{
+			Action:  ACTION_OUTPUT,
+			Package: PackageFullPath,
+			Test:    TestName,
+			Output:  fmt.Sprintf("=== RUN   %s\n", TestName),
+		},
+		{
+			Action:  ACTION_OUTPUT,
+			Package: PackageFullPath,
+			Test:    TestName,
+			Output:  fmt.Sprintf("--- FAIL: %s (0.00s)\n", TestName),
+		},
+		{
+			Action:  ACTION_FAIL,
+			Package: PackageFullPath,
+			Test:    TestName,
+		},
+		{
+			Action:  ACTION_OUTPUT,
+			Package: PackageFullPath,
+			Output:  fmt.Sprintf("FAIL\n"),
+		},
+		{
+			Action:  ACTION_OUTPUT,
+			Package: PackageFullPath,
+			Output:  fmt.Sprintf("FAIL\t%s\t0.00s\n", PackageFullPath),
+		},
+		{
+			Action:  ACTION_FAIL,
+			Package: PackageFullPath,
+		},
+	}
+
+	EVENTS_PASS = []*Event{
+		{
+			Action:  ACTION_RUN,
+			Package: PackageFullPath,
+			Test:    TestName,
+		},
+		{
+			Action:  ACTION_OUTPUT,
+			Package: PackageFullPath,
+			Test:    TestName,
+			Output:  fmt.Sprintf("=== RUN   %s\n", TestName),
+		},
+		{
+			Action:  ACTION_OUTPUT,
+			Package: PackageFullPath,
+			Test:    TestName,
+			Output:  fmt.Sprintf("--- PASS: %s (0.00s)\n", TestName),
+		},
+		{
+			Action:  ACTION_PASS,
+			Package: PackageFullPath,
+			Test:    TestName,
+		},
+		{
+			Action:  ACTION_OUTPUT,
+			Package: PackageFullPath,
+			Output:  "PASS\n",
+		},
+		{
+			Action:  ACTION_OUTPUT,
+			Package: PackageFullPath,
+			Output:  fmt.Sprintf("ok  \t%s\t0.00s\n", PackageFullPath),
+		},
+		{
+			Action:  ACTION_PASS,
+			Package: PackageFullPath,
+		},
+	}
+
+	EVENTS_SKIP = []*Event{
+		{
+			Action:  ACTION_RUN,
+			Package: PackageFullPath,
+			Test:    TestName,
+		},
+		{
+			Action:  ACTION_OUTPUT,
+			Package: PackageFullPath,
+			Test:    TestName,
+			Output:  fmt.Sprintf("=== RUN   %s\n", TestName),
+		},
+		{
+			Action:  ACTION_OUTPUT,
+			Package: PackageFullPath,
+			Test:    TestName,
+			Output:  fmt.Sprintf("--- SKIP: %s (0.00s)\n", TestName),
+		},
+		{
+			Action:  ACTION_OUTPUT,
+			Package: PackageFullPath,
+			Test:    TestName,
+			Output:  "    test2json_test.go:6: no thanks!\n",
+		},
+		{
+			Action:  ACTION_SKIP,
+			Package: PackageFullPath,
+			Test:    TestName,
+		},
+		{
+			Action:  ACTION_OUTPUT,
+			Package: PackageFullPath,
+			Output:  "PASS\n",
+		},
+		{
+			Action:  ACTION_OUTPUT,
+			Package: PackageFullPath,
+			Output:  fmt.Sprintf("ok  \t%s\t0.00s\n", PackageFullPath),
+		},
+		{
+			Action:  ACTION_PASS,
+			Package: PackageFullPath,
+		},
+	}
+
+	tsRegex = regexp.MustCompile(`\d+\.\d+s`)
+)
+
+type TestContent []byte
+
+func runTest(t *testing.T, w io.Writer, content TestContent) {
+	// Setup.
+	tmp, cleanup := testutils.TempDir(t)
+	defer cleanup()
+	pkgPath := filepath.Join(tmp, "go", "test2json_test")
+	require.NoError(t, os.MkdirAll(pkgPath, os.ModePerm))
+	require.NoError(t, ioutil.WriteFile(filepath.Join(pkgPath, "test2json_test.go"), content, os.ModePerm))
+
+	// Make go modules happy.
+	ctx := context.Background()
+	_, err := exec.RunCwd(ctx, tmp, "go", "mod", "init", "fake.com/test2json_test")
+	require.NoError(t, err)
+
+	// Ignore the error, since some cases expect it.
+	_, _ = exec.RunCommand(ctx, &exec.Command{
+		Name:   "go",
+		Args:   []string{"test", "-json", "./..."},
+		Dir:    tmp,
+		Stdout: w,
+	})
+}
+
+func runAndCompare(t *testing.T, expectEvents []*Event, content TestContent) {
+	r, w := io.Pipe()
+	go func() {
+		defer testutils.AssertCloses(t, w)
+		runTest(t, w, content)
+	}()
+	i := 0
+	for actual := range EventStream(r) {
+		expect := expectEvents[i]
+
+		// Fake out some fields.
+		require.False(t, util.TimeIsZero(actual.Time))
+		actual.Time = expect.Time
+		actual.Output = tsRegex.ReplaceAllString(actual.Output, "0.00s")
+		actual.Elapsed = 0.0
+
+		// Compare to the expected event.
+		sklog.Errorf("Event %d", i)
+		assertdeep.Equal(t, expect, actual)
+
+		i++
+	}
+}
+
+func TestEventStreamFail(t *testing.T) {
+	unittest.MediumTest(t)
+	runAndCompare(t, EVENTS_FAIL, CONTENT_FAIL)
+}
+
+func TestEventStreamPass(t *testing.T) {
+	unittest.MediumTest(t)
+	runAndCompare(t, EVENTS_PASS, CONTENT_PASS)
+}
+
+func TestEventStreamSkip(t *testing.T) {
+	unittest.MediumTest(t)
+	runAndCompare(t, EVENTS_SKIP, CONTENT_SKIP)
+}
