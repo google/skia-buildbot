4129309300496154c97264f6422b100b494f4a58
Joe Gregorio <jcgregorio@google.com>
2020-01-28 10:46:56 -0500

    [perf] Add k-d tree implemenation.
    Bug: skia:9831
    Change-Id: I9184163b9661e63c4a2bea8056f119556b33c8b0

---
+++ perf/modules/plot-simple-sk/kd_test.js
@@ -0,0 +1,45 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import { kdTree } from './kd'
+
+describe('kdTree search', function () {
+
+    const points = [
+        { x: 5, y: 5 },
+        { x: 2, y: 2 },
+        { x: 6, y: 6 },
+    ];
+
+    const distance = function (a, b) {
+        return Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2);
+    }
+
+    const tree = new kdTree(points, distance, ["x", "y"]);
+
+    it('finds the closest point', function () {
+        // Nearby
+        let nearest = tree.nearest({ x: 2.1, y: 2.1 }, 1);
+        assert.deepEqual(nearest, { x: 2, y: 2 });
+
+        // Exact match.
+        nearest = tree.nearest({ x: 5, y: 5 }, 1);
+        assert.deepEqual(nearest, { x: 5, y: 5 });
+
+        // This tests checking down the alternate tree.
+        nearest = tree.nearest({ x: 4.5, y: 7 }, 1);
+        assert.deepEqual(nearest, { x: 6, y: 6 });
+
+    });
+});
