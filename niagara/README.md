d04476022d265b679010c3354c5e9799b888207e
Kevin Lubick <kjlubick@google.com>
2020-01-31 16:23:15 -0500

    [niagara] Proof of concept cycle
    Change-Id: Ib561e93d5161620e1e85ba30295eab20582d264b

---
+++ niagara/README.md
@@ -0,0 +1,2 @@
+See <https://docs.google.com/document/d/1emwoLtZx_mfGye8A5Bi6Ra00HrVCO-RcIIQLJZCqehE/edit>
+for design and initial ideas.
---
+++ niagara/go/niagara/db.go
@@ -0,0 +1,27 @@
+package niagara
+
+import (
+	"time"
+)
+
+type FirestoreMachineEntry struct {
+	State           MachineStatus       `firestore:"state"`
+	LastEvent       MachineEvent        `firestore:"last_event"`
+	Updated         time.Time           `firestore:"updated"`
+	LastStateChange time.Time           `firestore:"last_state_change"` // TODO(kjlubick)
+	Dimensions      map[string][]string `firestore:"dimensions"`
+	CurrentTask     string              `firestore:"current_task"`
+}
+
+type FirestoreTaskEntry struct {
+	MachineAssigned string     `firestore:"machine_assigned"`
+	Command         string     `firestore:"command"`
+	Status          TaskStatus `firestore:"status"`
+
+	Created   time.Time `firestore:"created"`
+	Started   time.Time `firestore:"started"`
+	Completed time.Time `firestore:"completed"`
+	Abandoned time.Time `firestore:"abandoned"`
+	Updated   time.Time `firestore:"updated"`
+	// TODO(kjlubick) Expires
+}
