678be86d9b435d93836d724b22b47dbb71983a39
Kevin Lubick <kjlubick@google.com>
2020-01-31 09:53:33 -0500

    [gold] Add create/edit ignore rule to lit-html ignores page.
    Change-Id: I4e5958dceb0c7a2fe7cd47ff35990604a147880a
    Bug: skia:9525

---
+++ golden/modules/edit-ignore-rule-sk/edit-ignore-rule-sk-demo.js
@@ -0,0 +1,14 @@
+import './index.js';
+import { $$ } from 'common-sk/modules/dom';
+
+import { manyParams} from './test_data';
+
+function newEditIgnoreRule(parentSelector) {
+  const er = document.createElement('edit-ignore-rule-sk');
+  er.params = manyParams;
+  er.query = 'configuration=Release';
+  $$(parentSelector).appendChild(er);
+}
+
+newEditIgnoreRule('#container');
+
