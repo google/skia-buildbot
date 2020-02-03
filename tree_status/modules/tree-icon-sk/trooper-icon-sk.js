25cb23f43e2051861c59afc1a17a8cc0c9e0888b
Ravi Mistry <rmistry@google.com>
2020-02-03 13:30:33 -0500

    Checkpoint for tree
    Change-Id: I824e3206f6fd29e990e52a9795cb340a964e1751

---
+++ tree_status/modules/tree-icon-sk/trooper-icon-sk.js
@@ -0,0 +1,15 @@
+import './tree-icon-sk.css';
+import { define } from 'elements-sk/define'
+
+const iconSkTemplate = document.createElement('template');
+iconSkTemplate.innerHTML = '<svg class="icon-sk-svg" xmlns="http://www.w3.org/2000/svg" width=24 height=24 viewBox="0 0 24 24"><g id="trooper" transform="translate(0,-1028.3622)"></svg>';
+
+define('trooper-icon-sk', class extends HTMLElement {
+	connectedCallback() {
+		let icon = iconSkTemplate.content.cloneNode(true);
+    while (this.firstChild) {
+      this.removeChild(this.firstChild);
+    }
+		this.appendChild(icon);
+	}
+});
