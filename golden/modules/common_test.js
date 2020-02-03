ce6c44008bbae34b5625a7635f73751d7df2509f
Kevin Lubick <kjlubick@google.com>
2020-02-03 11:06:18 -0500

    [gold] Add element for editing/creating ignore rules
    Change-Id: Id515d9696bf519e86e6ba859cc97ac2df5ea4d82
    Bug: skia:9525

---
+++ golden/modules/common_test.js
@@ -0,0 +1,12 @@
+import {
+  humanReadableQuery
+} from './common';
+
+describe('humanReadableQuery', () => {
+  it('turns url encoded queries into human readable version', () => {
+    expect(humanReadableQuery('alpha=beta&gamma=delta')).to.equal(
+      'alpha=beta\ngamma=delta');
+    expect(humanReadableQuery("mind%20the%20gap=tube&woody=There's%20a%20space%20in%20my%20boot")).to.equal(
+      "mind the gap=tube\nwoody=There's a space in my boot");
+  });
+});
