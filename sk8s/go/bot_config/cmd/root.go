ff99aaa490868fa09d3a050097241f8248ea1efe
Joe Gregorio <jcgregorio@google.com>
2020-02-05 15:33:33 -0500

    [sk8s] Land the rest of the code to run swarming in the rpi k8s cluster.
    Bug: skia:9602
    Change-Id: I8db4c6f1c6ee144f2b12c7c58c7e6dc1ddd6d053

---
+++ sk8s/go/bot_config/cmd/root.go
@@ -0,0 +1,33 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"go.skia.org/infra/go/sklog"
+)
+
+var cfgFile string
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "bot_config",
+	Short: "Determines the configuration of a bot.",
+	Long:  `Each sub-command implements a get_* call in bot_config.py`,
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		sklog.Fatal(err)
+	}
+}
+
+func init() {
+	cobra.OnInitialize()
+
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.bot_config.yaml)")
+}
