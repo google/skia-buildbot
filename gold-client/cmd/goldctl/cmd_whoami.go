08f959e537468707f2a57643e6931a2055102def
Leandro Lovisolo <lovisolo@google.com>
2020-02-06 17:07:00 -0500

    [goldctl] Add command "goldctl imgtest whoami".
    This command makes a request to Gold's /json/whoami endpoint and prints out its response to standard output.
    
    Useful for debugging authentication issues against Gold.
    
    Bug: skia:9527
    Change-Id: I48e841ddb0e8a2b3dc53fdfa3bff3c284847d4a8

---
+++ gold-client/cmd/goldctl/cmd_whoami.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"go.skia.org/infra/gold-client/go/goldclient"
+)
+
+type whoamiEnv struct {
+	workDir    string
+	instanceID string
+}
+
+func getWhoamiCmd() *cobra.Command {
+	env := &whoamiEnv{}
+	cmd := &cobra.Command{
+		Use:   "whoami",
+		Short: "Make a request to Gold's /json/whoami endpoint and print out its output.",
+		Long: `
+Will print out the email address of the user or service account used to authenticate the request.
+For debugging purposes only.`,
+		Run: env.runWhoamiCmd,
+	}
+
+	cmd.Flags().StringVar(&env.workDir, fstrWorkDir, "", "Work directory for intermediate results")
+	cmd.Flags().StringVar(&env.instanceID, "instance", "", "ID of the Gold instance.")
+	Must(cmd.MarkFlagRequired(fstrWorkDir))
+	Must(cmd.MarkFlagRequired("instance"))
+
+	return cmd
+}
+
+func (w *whoamiEnv) runWhoamiCmd(cmd *cobra.Command, args []string) {
+	auth, err := goldclient.LoadAuthOpt(w.workDir)
+	ifErrLogExit(cmd, err)
+
+	if auth == nil {
+		logErrf(cmd, "Auth is empty - did you call goldctl auth first?")
+		exitProcess(cmd, 1)
+	}
+
+	config := goldclient.GoldClientConfig{
+		InstanceID: w.instanceID,
+		WorkDir:    w.workDir,
+	}
+
+	// Overwrite any existing config in the work directory.
+	goldClient, err := goldclient.NewCloudClient(auth, config)
+	ifErrLogExit(cmd, err)
+
+	url := goldclient.GetGoldInstanceURL(w.instanceID)
+	logVerbose(cmd, "Making request to %s/json/whoami\n", url)
+	email, err := goldClient.Whoami()
+	ifErrLogExit(cmd, err)
+	fmt.Printf("%s/json/whoami returned \"%s\".\n", url, email)
+}
