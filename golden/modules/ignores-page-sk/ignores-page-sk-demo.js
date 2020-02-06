98dd8bd6572b5d364bac5cd019ac04ee84c00ff1
Kevin Lubick <kjlubick@google.com>
2020-02-06 11:31:30 -0500

    [gold] Add create/edit ignore rule to lit-html ignores page.
    Change-Id: I4e5958dceb0c7a2fe7cd47ff35990604a147880a
    Bug: skia:9525

--- golden/modules/ignores-page-sk/ignores-page-sk-demo.js
+++ golden/modules/ignores-page-sk/ignores-page-sk-demo.js
@@ -1,13 +1,16 @@
-import './index.js'
-import '../gold-scaffold-sk'
+import './index.js';
+import '../gold-scaffold-sk';
 
-import { delay } from '../demo_util'
-import { ignoreRules_10, fakeNow } from './test_data'
-
+import { delay } from '../demo_util';
+import { ignoreRules_10, fakeNow } from './test_data';
+import { manyParams } from '../edit-ignore-rule-sk/demo_data.js';
 const fetchMock = require('fetch-mock');
 
 Date.now = () => fakeNow;
 
+fetchMock.get('/json/paramset', delay(manyParams, 100));
 fetchMock.get('/json/ignores?counts=1', delay(ignoreRules_10, 300));
 fetchMock.post('glob:/json/ignores/del/*', delay({}, 600));
+fetchMock.post('glob:/json/ignores/add/', delay({}, 600));
+fetchMock.post('glob:/json/ignores/save/*', delay({}, 600));
 fetchMock.catch(404);
