4503863f57b8ad6701e9819ca02b80e900ff7d77
Leandro Lovisolo <lovisolo@google.com>
2020-02-07 16:34:46 -0500

    [goldctl] Add command "goldctl imgtest whoami".
    This command makes a request to Gold's /json/whoami endpoint and prints out its response to standard output.
    
    Useful for debugging authentication issues against Gold.
    
    Bug: skia:9527
    Change-Id: I48e841ddb0e8a2b3dc53fdfa3bff3c284847d4a8

--- gold-client/go/mocks/GoldClient.go
+++ gold-client/go/mocks/GoldClient.go
@@ -100,3 +100,24 @@ func (_m *GoldClient) Test(name types.TestName, imgFileName string, additionalKe
 
 	return r0, r1
 }
+
+// Whoami provides a mock function with given fields:
+func (_m *GoldClient) Whoami() (string, error) {
+	ret := _m.Called()
+
+	var r0 string
+	if rf, ok := ret.Get(0).(func() string); ok {
+		r0 = rf()
+	} else {
+		r0 = ret.Get(0).(string)
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func() error); ok {
+		r1 = rf()
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
