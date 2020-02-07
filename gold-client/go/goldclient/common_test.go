25a5b0ac4adf5e2d4f42be6f0f2a8cd64552dfad
Leandro Lovisolo <lovisolo@google.com>
2020-02-07 18:49:42 -0500

    [goldctl] Add function postWithRetries().
    This will enable us to call the /json/triage RPC, which accepts the POST method.
    
    Bug: skia:9527
    Change-Id: I5bf7c77ba6c972b5eb059c074bcf1f5ef4e65604

--- gold-client/go/goldclient/common_test.go
+++ gold-client/go/goldclient/common_test.go
@@ -3,6 +3,7 @@ package goldclient
 import (
 	"errors"
 	"net/http"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -50,6 +51,59 @@ func TestGetWithRetries_MultipleAttempts_Error(t *testing.T) {
 	mh.On("Get", url).Return(httpResponse([]byte("Should be ignored."), "404 Not found", http.StatusNotFound), nil).Times(5)
 
 	_, err := getWithRetries(mh, url)
+	assert.Error(t, err)
+	assert.Contains(t, err.Error(), "404")
+}
+
+func TestPostWithRetries_OneAttempt_Success(t *testing.T) {
+	unittest.SmallTest(t)
+
+	mh := &mocks.HTTPClient{}
+	defer mh.AssertExpectations(t)
+
+	url := "example.com"
+	contentType := "text/plain"
+	body := strings.NewReader("Payload")
+
+	mh.On("Post", url, contentType, body).Return(httpResponse([]byte("Hello, world!"), "200 OK", http.StatusOK), nil).Once()
+
+	b, err := postWithRetries(mh, url, contentType, body)
+	assert.NoError(t, err)
+	assert.Equal(t, []byte("Hello, world!"), b)
+}
+
+func TestPostWithRetries_MultipleAttempts_Success(t *testing.T) {
+	unittest.LargeTest(t) // Function under test sleeps for several milliseconds before retries.
+
+	mh := &mocks.HTTPClient{}
+	defer mh.AssertExpectations(t)
+
+	url := "example.com"
+	contentType := "text/plain"
+	body := strings.NewReader("Payload")
+
+	mh.On("Post", url, contentType, body).Return(nil, errors.New("http.Client error")).Once()
+	mh.On("Post", url, contentType, body).Return(httpResponse([]byte("Should be ignored."), "500 Internal Server Error", http.StatusInternalServerError), nil).Once()
+	mh.On("Post", url, contentType, body).Return(httpResponse([]byte("Hello, world!"), "200 OK", http.StatusOK), nil).Once()
+
+	b, err := postWithRetries(mh, url, contentType, body)
+	assert.NoError(t, err)
+	assert.Equal(t, []byte("Hello, world!"), b)
+}
+
+func TestPostWithRetries_MultipleAttempts_Error(t *testing.T) {
+	unittest.LargeTest(t) // Function under test sleeps for several milliseconds before retries.
+
+	mh := &mocks.HTTPClient{}
+	defer mh.AssertExpectations(t)
+
+	url := "example.com"
+	contentType := "text/plain"
+	body := strings.NewReader("Payload")
+
+	mh.On("Post", url, contentType, body).Return(httpResponse([]byte("Should be ignored."), "404 Not found", http.StatusNotFound), nil).Times(5)
+
+	_, err := postWithRetries(mh, url, contentType, body)
 	assert.Error(t, err)
 	assert.Contains(t, err.Error(), "404")
 }
