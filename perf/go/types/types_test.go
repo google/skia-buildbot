8dc851854ab4b84887a86b3ef81669e2310538ab
Joe Gregorio <jcgregorio@google.com>
2020-02-12 07:44:43 -0500

    [perf] Hide btts.TileKey WIP
    Change-Id: Ia12d6685eb8f587b7424fcc0e03b8c4005ea2e1d

---
+++ perf/go/types/types_test.go
@@ -0,0 +1,23 @@
+package types
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTileNumber_Prev(t *testing.T) {
+	assert.Equal(t, TileNumber(0), TileNumber(1).Prev())
+	assert.Equal(t, BadTileNumber, TileNumber(0).Prev())
+}
+
+func TestTileNumberFromCommitNumber(t *testing.T) {
+	assert.Equal(t, TileNumber(0), TileNumberFromCommitNumber(CommitNumber(0), 256))
+	assert.Equal(t, TileNumber(0), TileNumberFromCommitNumber(CommitNumber(255), 256))
+	assert.Equal(t, TileNumber(1), TileNumberFromCommitNumber(CommitNumber(256), 256))
+	assert.Equal(t, TileNumber(1), TileNumberFromCommitNumber(CommitNumber(257), 256))
+}
+
+func TestTileNumberFromCommitNumber_BadTileSize(t *testing.T) {
+	assert.Equal(t, BadTileNumber, TileNumberFromCommitNumber(CommitNumber(256), 0))
+}
