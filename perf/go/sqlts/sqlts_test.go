6b80ba75302f30a98dd9cc96154eb6be960b095a
Joe Gregorio <jcgregorio@google.com>
2020-02-13 10:43:09 -0500

    [perf] Experimenting with sqlite.
    Change-Id: I9e562e4499711b67c47c70de49b6ba0dcb8a76f2

---
+++ perf/go/sqlts/sqlts_test.go
@@ -0,0 +1,38 @@
+package sqlts
+
+import (
+	"io/ioutil"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"go.skia.org/infra/go/paramtools"
+	"go.skia.org/infra/go/testutils/unittest"
+)
+
+func TestNewSQLite(t *testing.T) {
+	unittest.SmallTest(t)
+	tmpfile, err := ioutil.TempFile("", "sqlts")
+	assert.NoError(t, err)
+	err = tmpfile.Close()
+	assert.NoError(t, err)
+
+	defer os.Remove(tmpfile.Name())
+
+	s, err := NewSQLite(tmpfile.Name(), 8)
+	assert.NoError(t, err)
+
+	err = s.WriteTraces(0, []paramtools.Params{
+		{"config": "8888", "arch": "x86"},
+		{"config": "565", "arch": "x86"},
+	},
+		[]float32{1.5, 2.3},
+		paramtools.ParamSet{
+			"config": []string{"8888", "565"},
+			"arch":   []string{"x86"},
+		},
+		"gs://perf-bucket/2020/02/08/11/testdata.json",
+		time.Now())
+	assert.NoError(t, err)
+}
