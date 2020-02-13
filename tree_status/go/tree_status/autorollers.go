2660aa70348e5d12605d27ff50128700bfe18396
Ravi Mistry <rmistry@google.com>
2020-02-13 12:14:34 -0500

    Checkpoint for tree
    Change-Id: I824e3206f6fd29e990e52a9795cb340a964e1751

---
+++ tree_status/go/tree_status/autorollers.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"time"
+
+	"golang.org/x/oauth2"
+	"google.golang.org/api/option"
+
+	autoroll_status "go.skia.org/infra/autoroll/go/status"
+	"go.skia.org/infra/go/common"
+	"go.skia.org/infra/go/ds"
+	"go.skia.org/infra/go/skerr"
+	"go.skia.org/infra/go/sklog"
+)
+
+var (
+	// AUTOROLLERS maps autoroll frontend host to maps of roller IDs to
+	// their human-friendly display names.
+	AUTOROLLERS = map[string]map[string]string{
+		"autoroll.skia.org": map[string]string{
+			"skia-flutter-autoroll":     "Flutter",
+			"skia-autoroll":             "Chrome",
+			"angle-skia-autoroll":       "ANGLE",
+			"skcms-skia-autoroll":       "Skcms",
+			"swiftshader-skia-autoroll": "SwiftShader",
+		},
+		"skia-autoroll.corp.goog": map[string]string{
+			"android-master-autoroll": "Android",
+			"google3-autoroll":        "Google3",
+		},
+	}
+
+	AutorollStatuses = []autoRollStatus{}
+	NamesToRollers   = map[string]string{}
+	// Channel that will determine which rollers need to be watched.
+	RollersToWatch chan string
+)
+
+type autoRollStatus struct {
+	Name      string `json:"name"`
+	NumFailed int    `json:"num_failed"`
+	Url       string `json:"url"`
+}
+
+func AutorollersInit(ctx context.Context, ts oauth2.TokenSource) error {
+	if err := ds.InitWithOpt(common.PROJECT_ID, ds.AUTOROLL_NS, option.WithTokenSource(ts)); err != nil {
+		return skerr.Wrapf(err, "Failed to initialize Cloud Datastore for autorollers")
+	}
+
+	// Below is initial call. In start watching you will be doing more.
+	AutorollStatuses = []autoRollStatus{}
+	updateAutorollStatus := func(ctx context.Context) error {
+		AutorollStatuses = []autoRollStatus{}
+		for host, subMap := range AUTOROLLERS {
+			for roller, friendlyName := range subMap {
+				s, err := autoroll_status.Get(ctx, roller)
+				if err != nil {
+					return err
+				}
+				status := autoRollStatus{
+					Name:      friendlyName,
+					NumFailed: s.AutoRollMiniStatus.NumFailedRolls,
+					Url:       fmt.Sprintf("https://%s/r/%s", host, roller),
+				}
+				AutorollStatuses = append(AutorollStatuses, status)
+				NamesToRollers[friendlyName] = roller
+			}
+		}
+		return nil
+	}
+	if err := updateAutorollStatus(ctx); err != nil {
+		return err
+	}
+
+	RollersToWatch = make(chan string, len(AutorollStatuses))
+
+	go func() {
+		for {
+			select {
+			case roller := <-RollersToWatch:
+				fmt.Printf("Need to watch %s\n", roller)
+				fmt.Println("Starting goroutine to poll every 10 seconds")
+				// Start another loop here.
+				for {
+					s, err := autoroll_status.Get(ctx, roller)
+					if err != nil {
+						sklog.Errorf("Could not get status of %s: %s", roller, err)
+						// Continue so that we can try again.
+						continue
+					}
+					if s.AutoRollMiniStatus.NumFailedRolls == 0 {
+						sklog.Infof("Roller %s has 0 NumFailedRolls", roller)
+						fmt.Println("Going to move on from this roller now")
+						break
+					}
+					sklog.Infof("Roller %s still has %d NumFailedRolls", roller, s.AutoRollMiniStatus.NumFailedRolls)
+					time.Sleep(10 * time.Second)
+				}
+			}
+			fmt.Println("OUT OF THE LOOP, WHAT DOES THIS MEAN NOW???")
+		}
+	}()
+
+	fmt.Println("GOT THISSSS")
+	fmt.Printf("%+v", AutorollStatuses)
+	return nil
+}
+
+func StartWatchingAutorollers(rollers string) {
+	fmt.Println("STARTING TO WATCH AUTOROLLERS")
+	fmt.Println(rollers)
+	fmt.Println(RollersToWatch)
+	rollerNames := strings.Split(rollers, ",")
+	for _, r := range rollerNames {
+		RollersToWatch <- NamesToRollers[r]
+	}
+}
+
+func StopWatchingAutorollers() {
+
+}
