61cfd0511b7875db757c604c8ac57c37cf61aaaa
Joe Gregorio <jcgregorio@google.com>
2020-02-13 15:54:05 -0500

    [perf] Experimenting with sqlite.
    Change-Id: I9e562e4499711b67c47c70de49b6ba0dcb8a76f2

---
+++ perf/go/sqlts/sqlts.go
@@ -0,0 +1,349 @@
+// Package sqlts implements a types.TraceStore on top of SQL.
+package sqlts
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"sort"
+	"strings"
+	"time"
+
+	lru "github.com/hashicorp/golang-lru"
+	_ "github.com/mattn/go-sqlite3" // Get sqlite.	// Get sqlite.
+	"go.skia.org/infra/go/paramtools"
+	"go.skia.org/infra/go/query"
+	"go.skia.org/infra/go/vec32"
+	"go.skia.org/infra/perf/go/types"
+)
+
+const cacheSize = 10 * 1000 * 1000
+
+// Dialect is type for the dialect of SQL that can be used.
+type Dialect int
+
+const (
+	// SQLiteDialect covers both SQLite and DQLite.
+	SQLiteDialect = iota
+)
+
+// statement is an SQL statement or fragment of an SQL statement.
+type statement int
+
+// All the different statements we need.
+const (
+	createTables = iota
+
+	insertIntoSourceFiles
+	getSourceFileID
+
+	insertIntoTraceIDs
+	getTraceID
+
+	insertIntoPostings
+
+	replaceTraceValues
+)
+
+type statements map[statement]string
+
+var statementsByDialect = map[Dialect]statements{
+	SQLiteDialect: statements{
+		createTables: `
+		CREATE TABLE IF NOT EXISTS TraceIDs  (
+			trace_id INTEGER PRIMARY KEY,
+			trace_name TEXT UNIQUE NOT NULL
+		);
+
+		CREATE TABLE IF NOT EXISTS Postings  (
+			tile_number INTEGER,
+			key_value text NOT NULL,
+			trace_id INTEGER,
+			PRIMARY KEY (tile_number, key_value, trace_id)
+		);
+
+		CREATE TABLE IF NOT EXISTS SourceFiles (
+			source_file_id INTEGER PRIMARY KEY,
+			source_file TEXT UNIQUE NOT NULL
+		);
+
+		CREATE TABLE IF NOT EXISTS TraceValues (
+			tile_number INTEGER,
+			trace_id INTEGER,
+			commit_number INTEGER,
+			val REAL,
+			source_file_id INTEGER,
+			PRIMARY KEY (tile_number, trace_id, commit_number)
+		);
+		`,
+
+		insertIntoSourceFiles: `INSERT OR IGNORE INTO SourceFiles (source_file) VALUES (?);`,
+		getSourceFileID:       `SELECT source_file_id FROM SourceFiles WHERE source_file=?`,
+
+		insertIntoTraceIDs: `INSERT OR IGNORE INTO TraceIDs (trace_name) VALUES (?)`,
+		getTraceID:         `SELECT trace_id FROM TraceIDs WHERE trace_name=?`,
+
+		insertIntoPostings: `INSERT OR IGNORE INTO Postings (tile_number, key_value, trace_id) VALUES (?, ?, ?)`,
+
+		replaceTraceValues: `INSERT OR REPLACE INTO TraceValues (tile_number, trace_id, commit_number, val, source_file_id) VALUES( ?, ?, ?, ?, ?)`,
+	},
+}
+
+// SQLTraceStore implements types.TraceStore backed onto an SQL database.
+type SQLTraceStore struct {
+	db                    *sql.DB
+	insertIntoSourceFiles *sql.Stmt
+	getSourceFileID       *sql.Stmt
+
+	insertIntoTraceIDs *sql.Stmt
+	getTraceID         *sql.Stmt
+
+	insertIntoPostings *sql.Stmt
+
+	replaceTraceValues *sql.Stmt
+
+	cache    *lru.Cache
+	st       statements
+	tileSize int32
+}
+
+// NewSQLite returns a new *SQLTraceStore that implements types.TraceStore on
+// top of SQLite.
+//
+// The filename is the name of the sqlite3 database, which will be created if
+// not present.
+func NewSQLite(db *sql.DB, dialect Dialect, tileSize int32) (*SQLTraceStore, error) {
+	_, err := db.Exec(statementsByDialect[dialect][createTables])
+	if err != nil {
+		return nil, err
+	}
+	st := statementsByDialect[dialect]
+	insertIntoSourceFilesStmt, err := db.Prepare(st[insertIntoSourceFiles])
+	if err != nil {
+		return nil, err
+	}
+	getSourceFileIDStmt, err := db.Prepare(st[getSourceFileID])
+	if err != nil {
+		return nil, err
+	}
+	insertIntoTraceIDsStmt, err := db.Prepare(st[insertIntoTraceIDs])
+	if err != nil {
+		return nil, err
+	}
+	getTraceIDStmt, err := db.Prepare(st[getTraceID])
+	if err != nil {
+		return nil, err
+	}
+	insertIntoPostingsStmt, err := db.Prepare(st[insertIntoPostings])
+	if err != nil {
+		return nil, err
+	}
+	replaceTraceValuesStmt, err := db.Prepare(st[replaceTraceValues])
+	if err != nil {
+		return nil, err
+	}
+	cache, err := lru.New(cacheSize)
+	if err != nil {
+		return nil, err
+	}
+
+	return &SQLTraceStore{
+		db: db,
+
+		insertIntoSourceFiles: insertIntoSourceFilesStmt,
+		getSourceFileID:       getSourceFileIDStmt,
+
+		insertIntoTraceIDs: insertIntoTraceIDsStmt,
+		getTraceID:         getTraceIDStmt,
+
+		insertIntoPostings: insertIntoPostingsStmt,
+
+		replaceTraceValues: replaceTraceValuesStmt,
+
+		st: statementsByDialect[dialect],
+
+		cache:    cache,
+		tileSize: tileSize,
+	}, nil
+}
+
+type int64Slice []int64
+
+func (p int64Slice) Len() int           { return len(p) }
+func (p int64Slice) Less(i, j int) bool { return p[i] < p[j] }
+func (p int64Slice) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+
+// ReadTraces implements the types.TraceStore interface.
+func (s *SQLTraceStore) ReadTraces(tileNumber types.TileNumber, keys []string) (map[string][]float32, error) {
+	// Eventually should be broken into batches so we don't exceed the sql
+	// engines limit on query sizes.
+
+	// Get the traceIDs for the given keys.
+	stmt := `SELECT trace_id, trace_name FROM TraceIDS WHERE `
+	whereClauses := make([]string, len(keys))
+	for i, key := range keys {
+		whereClauses[i] = fmt.Sprintf(`trace_name=%q`, key)
+	}
+	rows, err := s.db.Query(stmt + strings.Join(whereClauses, " OR "))
+	if err != nil {
+		return nil, err
+	}
+	mapIDToName := map[int64]string{}
+	traceIDs := make([]int64, len(keys))
+	i := 0
+	for rows.Next() {
+		var traceID int64
+		var traceName string
+		if err := rows.Scan(&traceID, &traceName); err != nil {
+			return nil, err
+		}
+		mapIDToName[traceID] = traceName
+		traceIDs[i] = traceID
+		i++
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	sort.Sort(int64Slice(traceIDs))
+	stmt = fmt.Sprintf(`SELECT trace_id, commit_number, val FROM TraceValues WHERE tile_number=%d AND (`, tileNumber)
+	for i, id := range traceIDs {
+		whereClauses[i] = fmt.Sprintf(`trace_id=%d`, id)
+	}
+
+	fullQuery := stmt + strings.Join(whereClauses, " OR ") + ")"
+	fmt.Printf(fullQuery)
+	rows, err = s.db.Query(fullQuery)
+	if err != nil {
+		return nil, err
+	}
+
+	ret := map[string][]float32{}
+	for _, key := range keys {
+		ret[key] = vec32.New(int(s.tileSize))
+	}
+
+	for rows.Next() {
+		var traceID int64
+		var commitNumber int64
+		var val float64
+		if err := rows.Scan(&traceID, &commitNumber, &val); err != nil {
+			return nil, err
+		}
+		ret[mapIDToName[traceID]][commitNumber%int64(s.tileSize)] = float32(val)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	// Load all the values for each traceID.
+	return ret, nil
+}
+
+// TileNumber implements the types.TraceStore interface.
+func (s *SQLTraceStore) TileNumber(commitNumber types.CommitNumber) types.TileNumber {
+	return types.TileNumberFromCommitNumber(commitNumber, s.tileSize)
+}
+
+// TileSize implements the types.TraceStore interface.
+func (s *SQLTraceStore) TileSize() int32 {
+	return s.tileSize
+}
+
+// WriteIndices implements the types.TraceStore interface.
+func (s *SQLTraceStore) WriteIndices(ctx context.Context) error {
+	return nil
+}
+
+func (s *SQLTraceStore) updateSourceFile(source string) (int64, error) {
+	if iret, ok := s.cache.Get(source); ok {
+		return iret.(int64), nil
+	}
+
+	ret := int64(-1)
+	res, err := s.insertIntoSourceFiles.Exec(source)
+	if err != nil {
+		return ret, err
+	}
+	sourceID, err := res.LastInsertId()
+	if err != nil {
+		return ret, err
+	}
+	s.cache.Add(source, sourceID)
+	return sourceID, nil
+}
+
+func (s *SQLTraceStore) updateIndex(p paramtools.Params, tileNumber types.TileNumber, traceID int64) error {
+	for k, v := range p {
+		keyValue := fmt.Sprintf("%s=%s", k, v)
+		_, err := s.insertIntoPostings.Exec(tileNumber, keyValue, traceID)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (s *SQLTraceStore) updateTraceID(traceNameAsParams paramtools.Params, tileNumber types.TileNumber) (int64, error) {
+	ret := int64(-1)
+
+	traceName, err := query.MakeKeyFast(traceNameAsParams)
+	if err != nil {
+		return ret, err
+	}
+
+	// Get an int64 trace id for the traceName.
+	if iret, ok := s.cache.Get(traceName); ok {
+		ret = iret.(int64)
+	} else {
+		res, err := s.insertIntoTraceIDs.Exec(traceName)
+		if err != nil {
+			return ret, err
+		}
+		ret, err = res.LastInsertId()
+		if err != nil {
+			return ret, err
+		}
+		s.cache.Add(traceName, ret)
+	}
+
+	// Update postings.
+	if err := s.updateIndex(traceNameAsParams, tileNumber, ret); err != nil {
+		return ret, err
+	}
+
+	return ret, nil
+}
+
+func (s *SQLTraceStore) updateTraceValues(tileNumber types.TileNumber, traceID int64, commitNumber types.CommitNumber, x float32, sourceID int64) error {
+	_, err := s.replaceTraceValues.Exec(tileNumber, traceID, commitNumber, x, sourceID)
+	return err
+}
+
+// WriteTraces implements the types.TraceStore interface.
+func (s *SQLTraceStore) WriteTraces(commitNumber types.CommitNumber, params []paramtools.Params, values []float32, _ paramtools.ParamSet, source string, _ time.Time) error {
+	tileNumber := types.TileNumberFromCommitNumber(commitNumber, s.tileSize)
+	// Get the row id for the source file.
+	sourceID, err := s.updateSourceFile(source)
+	if err != nil {
+		return err
+	}
+
+	// Get trace ids for each trace and add trace ids to the index/postings.
+	traceIDs := make([]int64, len(params))
+	for i, p := range params {
+		traceID, err := s.updateTraceID(p, tileNumber)
+		if err != nil {
+			return err
+		}
+		traceIDs[i] = traceID
+	}
+
+	// Now add each trace value.
+	for i, x := range values {
+		if err := s.updateTraceValues(tileNumber, traceIDs[i], commitNumber, x, sourceID); err != nil {
+			return err
+		}
+	}
+	return nil
+}
---
+++ perf/go/sqlts/sqlts_test.go
@@ -0,0 +1,208 @@
+package sqlts
+
+import (
+	"database/sql"
+	"io/ioutil"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"go.skia.org/infra/go/paramtools"
+	"go.skia.org/infra/go/query"
+	"go.skia.org/infra/go/testutils/unittest"
+	"go.skia.org/infra/go/vec32"
+)
+
+const e = vec32.MISSING_DATA_SENTINEL
+
+type cleanup func()
+
+func newTestDB(t *testing.T) (*SQLTraceStore, cleanup) {
+	unittest.SmallTest(t)
+	tmpfile, err := ioutil.TempFile("", "sqlts")
+	assert.NoError(t, err)
+	err = tmpfile.Close()
+	assert.NoError(t, err)
+
+	db, err := sql.Open("sqlite3", tmpfile.Name())
+	assert.NoError(t, err)
+
+	s, err := NewSQLite(db, SQLiteDialect, 8)
+	assert.NoError(t, err)
+
+	return s, func() {
+		os.Remove(tmpfile.Name())
+	}
+}
+
+func newBenchTestDB(b *testing.B) (*SQLTraceStore, cleanup) {
+	tmpfile, _ := ioutil.TempFile("", "sqlts")
+	_ = tmpfile.Close()
+	db, _ := sql.Open("sqlite3", tmpfile.Name())
+	s, _ := NewSQLite(db, SQLiteDialect, 8)
+
+	return s, func() {
+		os.Remove(tmpfile.Name())
+	}
+}
+
+func TestNewSQLite(t *testing.T) {
+	s, cleanup := newTestDB(t)
+	defer cleanup()
+
+	err := s.WriteTraces(0, []paramtools.Params{
+		{"config": "8888", "arch": "x86"},
+		{"config": "565", "arch": "x86"},
+	},
+		[]float32{1.5, 2.3},
+		paramtools.ParamSet{},
+		"gs://perf-bucket/2020/02/08/11/testdata.json",
+		time.Now())
+	assert.NoError(t, err)
+}
+
+func TestUpdateSourceFile(t *testing.T) {
+	s, cleanup := newTestDB(t)
+	defer cleanup()
+
+	// Do each update twice to ensure the IDs don't change.
+	id, err := s.updateSourceFile("foo.txt")
+	assert.NoError(t, err)
+	assert.Equal(t, int64(1), id)
+
+	id, err = s.updateSourceFile("foo.txt")
+	assert.NoError(t, err)
+	assert.Equal(t, int64(1), id)
+
+	id, err = s.updateSourceFile("bar.txt")
+	assert.NoError(t, err)
+	assert.Equal(t, int64(2), id)
+
+	id, err = s.updateSourceFile("bar.txt")
+	assert.NoError(t, err)
+	assert.Equal(t, int64(2), id)
+}
+
+func TestUpdateTraceID(t *testing.T) {
+	s, cleanup := newTestDB(t)
+	defer cleanup()
+
+	p := paramtools.NewParams(",config=8888,arch=x86,")
+
+	// Do each update twice to ensure the IDs don't change.
+	traceID, err := s.updateTraceID(p, 1)
+	assert.NoError(t, err)
+	assert.Equal(t, int64(1), traceID)
+
+	traceID, err = s.updateTraceID(p, 1)
+	assert.NoError(t, err)
+	assert.Equal(t, int64(1), traceID)
+
+	p2 := paramtools.NewParams(",config=8888,arch=arm,")
+
+	traceID, err = s.updateTraceID(p2, 1)
+	assert.NoError(t, err)
+	assert.Equal(t, int64(2), traceID)
+
+	traceID, err = s.updateTraceID(p2, 1)
+	assert.NoError(t, err)
+	assert.Equal(t, int64(2), traceID)
+
+	// Confirm the created postings are correct. Here we confirm the postings
+	// for config=8888 are the trace ids for both traces, since they both
+	// contain that key value pair.
+	row, err := s.db.Query(
+		`SELECT tile_number, key_value, trace_id FROM Postings
+		 WHERE key_value="config=8888"`,
+	)
+	assert.NoError(t, err)
+	count := 0
+	for row.Next() {
+		var tileNumber int64
+		var keyValue string
+		var traceID int64
+		row.Scan(&tileNumber, &keyValue, &traceID)
+		assert.Equal(t, int64(1), tileNumber)
+		assert.Equal(t, "config=8888", keyValue)
+		assert.Contains(t, []int64{1, 2}, traceID)
+		count++
+	}
+	assert.Equal(t, 2, count)
+
+	// Here we confirm the postings for arch=arm are the trace ids for just the
+	// second trace, since it only appears in that trace name.
+	row, err = s.db.Query(
+		`SELECT tile_number, key_value, trace_id FROM Postings WHERE key_value="arch=arm"`,
+	)
+	assert.NoError(t, err)
+	count = 0
+	for row.Next() {
+		var tileNumber int64
+		var keyValue string
+		var traceID int64
+		row.Scan(&tileNumber, &keyValue, &traceID)
+		assert.Equal(t, int64(1), tileNumber)
+		assert.Equal(t, "arch=arm", keyValue)
+		assert.Contains(t, []int64{2}, traceID)
+		count++
+	}
+	assert.Equal(t, 1, count)
+}
+
+func TestReadTraces(t *testing.T) {
+	s, _ := newTestDB(t)
+	//defer cleanup()
+
+	traceNames := []paramtools.Params{
+		{"config": "8888", "arch": "x86"},
+		{"config": "565", "arch": "x86"},
+	}
+	err := s.WriteTraces(1, traceNames,
+		[]float32{1.5, 2.3},
+		paramtools.ParamSet{},
+		"gs://perf-bucket/2020/02/08/11/testdata.json",
+		time.Now())
+	assert.NoError(t, err)
+	err = s.WriteTraces(2, traceNames,
+		[]float32{2.5, 3.3},
+		paramtools.ParamSet{},
+		"gs://perf-bucket/2020/02/08/12/testdata.json",
+		time.Now())
+	assert.NoError(t, err)
+
+	keys := make([]string, len(traceNames))
+	for i, p := range traceNames {
+		keys[i], err = query.MakeKeyFast(p)
+		assert.NoError(t, err)
+	}
+
+	ts, err := s.ReadTraces(0, keys)
+	assert.NoError(t, err)
+	assert.Equal(t, ts, map[string][]float32{
+		",arch=x86,config=565,":  {e, 2.3, 3.3, e, e, e, e, e},
+		",arch=x86,config=8888,": {e, 1.5, 2.5, e, e, e, e, e},
+	})
+}
+
+func BenchmarkUpdateSourceFile(b *testing.B) {
+	s, cleanup := newBenchTestDB(b)
+	defer cleanup()
+	b.ResetTimer()
+
+	for n := 0; n < b.N; n++ {
+		_, _ = s.updateSourceFile("foo.txt")
+	}
+}
+
+func BenchmarkUpdateTraceID(b *testing.B) {
+	s, cleanup := newBenchTestDB(b)
+	p := paramtools.NewParams(",config=8888,arch=x86,")
+	defer cleanup()
+
+	b.ResetTimer()
+
+	for n := 0; n < b.N; n++ {
+		_, _ = s.updateTraceID(p, 1)
+	}
+}
