93da9ef7e42fc8ae120ae26729d3f9205481ae9b
Ben Wagner <benjaminwagner@google.com>
2020-02-14 15:27:21 -0500

    Fix libimobiledevice build.sh
    Change-Id: I16d470e2306263f7703e2e462445dfc67e52adbc
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/259831
    Reviewed-by: Weston Tracey <westont@google.com>
    Commit-Queue: Ben Wagner aka dogben <benjaminwagner@google.com>

--- skolo/ios/build.sh
+++ skolo/ios/build.sh
@@ -19,19 +19,55 @@ git clone https://github.com/libimobiledevice/ideviceinstaller.git
 
 # Make sure the libraries below are found.
 export PKG_CONFIG_PATH="${PREFIX}/lib/pkgconfig"
+export CPPFLAGS="-I${PREFIX}/include"
+export LDFLAGS="-L${PREFIX}/lib"
 
 # Build and install in order of dependencies.
-cd libplist && ./autogen.sh --prefix=$PREFIX --without-cython && make && make install && cd ..
-cd libusbmuxd && ./autogen.sh --prefix=$PREFIX && make && make install && cd ..
-cd libimobiledevice && ./autogen.sh --prefix=$PREFIX --without-cython && make && make install && cd ..
-cd ifuse && ./autogen.sh --prefix=$PREFIX && make && make install && cd ..
+cd libplist
+./autogen.sh --prefix=$PREFIX --without-cython
+make
+make install
+cd ..
 
-# Patch a specific commit of ideviceinstaller so it can be compile on RPi Stretch
-# The reason might be 32-bit vs 64-bit architectures or the fact that RPi Stretch 
-# uses gcc 6 by default while Debian uses gcc 7. 
+cd libusbmuxd
+./autogen.sh --prefix=$PREFIX
+make
+make install
+cd ..
+
+cd libimobiledevice
+# Apply patch for idevicedebug debug output.
+# https://github.com/libimobiledevice/libimobiledevice/pull/716
+git fetch origin pull/716/head
+git merge --no-edit FETCH_HEAD
+# Apply patch for iOS 13.
+# https://github.com/libimobiledevice/libimobiledevice/pull/860
+git fetch origin pull/860/head
+git merge --no-edit FETCH_HEAD
+# Apply patch to fix handling of replies.
+# https://github.com/libimobiledevice/libimobiledevice/pull/914
+git fetch origin pull/914/head
+git merge --no-edit FETCH_HEAD
+# Apply patch to enable debugserver debug logging to syslog.
+# https://github.com/libimobiledevice/libimobiledevice/pull/646
+# Pull request no longer applies cleanly; pull from skia-dev mirror instead.
+#git fetch origin pull/646/head
+#git merge --no-edit FETCH_HEAD
+git remote add skiadev https://github.com/skia-dev/libimobiledevice.git
+git fetch skiadev
+git cherry-pick 6cb9578fc441d8f2a345a77b35422bba87416e11
+./autogen.sh --prefix=$PREFIX --without-cython --enable-debug-code
+make
+make install
+cd ..
+
+cd ifuse
+./autogen.sh --prefix=$PREFIX
+make
+make install
+cd ..
+
 cd ideviceinstaller
-git checkout f7988de8279051f3d2d7973b8d7f2116aa5d9317
-git am ../patches/ideviceinstaller.patch
 ./autogen.sh --prefix=$PREFIX
 make
 make install 
@@ -39,6 +75,8 @@ cd ..
 
 cd usbmuxd
 ./autogen.sh --prefix=$PREFIX \
-              --with-udevrulesdir=$PREFIX/udev-rules \
-              --with-systemdsystemunitdir=$PREFIX/systemd
-make && make install && cd ..
+  --with-udevrulesdir=$PREFIX/udev-rules \
+  --with-systemdsystemunitdir=$PREFIX/systemd
+make
+make install
+cd ..
