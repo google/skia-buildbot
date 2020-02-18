caad23343c4b4d05ec8d86f566b8eeb3a235084d
Joe Gregorio <jcgregorio@google.com>
2020-02-18 10:14:16 -0500

    [perf] Format numbers better on hover.
    Bug: skia:9918
    Change-Id: I1c1d53b7a2bab6f7fb5054d87169be7e8f5de90d

--- perf/modules/plot-simple-sk/plot-simple-sk.js
+++ perf/modules/plot-simple-sk/plot-simple-sk.js
@@ -433,6 +433,10 @@ define('plot-simple-sk', class extends ElementSk {
     // for details on tasks vs microtasks.
     this._zoomTask = 0;
 
+    // A formatter that prints numbers nicely, such as adding commas. Used when
+    // display the hover text.
+    this._numberFormatter = new Intl.NumberFormat();
+
     this._upgradeProperty('width');
     this._upgradeProperty('height');
     this._upgradeProperty('bands');
@@ -984,7 +988,7 @@ define('plot-simple-sk', class extends ElementSk {
           // Draw the label offset from the crosshair.
           ctx.font = LABEL_FONT;
           ctx.textBaseline = 'bottom';
-          const label = `${this._hoverPt.y}`;
+          const label = this._numberFormatter.format(this._hoverPt.y);
           let x = this._crosshair.x + MARGIN;
           let y = this._crosshair.y - MARGIN;
 
