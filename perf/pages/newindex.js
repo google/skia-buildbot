fc2da26e9d64b8b2f16da1dfb3805d6311fbb9e3
Joe Gregorio <jcgregorio@google.com>
2020-02-24 12:17:53 -0500

    [perf] Fix almost all eslint issues.
    Change-Id: Ia945938274723fc262bf8986969a5e2e537cdfc2

--- perf/pages/newindex.js
+++ perf/pages/newindex.js
@@ -1,8 +1,8 @@
-import '../node_modules/@webcomponents/custom-elements/custom-elements.min.js'
+import '../node_modules/@webcomponents/custom-elements/custom-elements.min';
 
-import '../modules/body'
-import '../modules/perf-scaffold-sk'
-import '../modules/explore-sk'
+import '../modules/body';
+import '../modules/perf-scaffold-sk';
+import '../modules/explore-sk';
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
