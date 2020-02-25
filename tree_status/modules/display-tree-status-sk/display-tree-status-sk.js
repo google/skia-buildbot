3ccdd8f88d8615084ddeb681876a3cb53be5a335
Ravi Mistry <rmistry@google.com>
2020-02-25 11:30:50 -0500

    Checkpoint for tree
    Change-Id: I824e3206f6fd29e990e52a9795cb340a964e1751

---
+++ tree_status/modules/display-tree-status-sk/display-tree-status-sk.js
@@ -0,0 +1,80 @@
+/**
+ * @module display-tree-status-sk
+ * @description <h2><code>display-tree-status-sk</code></h2>
+ *
+ *   Displays the recent tree statuses.
+ *
+ */
+
+import { ElementSk } from '../../../infra-sk/modules/ElementSk'
+import { define } from 'elements-sk/define'
+import { html, render } from 'lit-html'
+import { errorMessage } from 'elements-sk/errorMessage'
+import { jsonOrThrow } from 'common-sk/modules/jsonOrThrow'
+
+import 'elements-sk/error-toast-sk'
+
+// States of the tree.
+const OPEN = 'open';
+const CAUTION = 'caution';
+const CLOSED = 'closed';
+
+
+function recentStatuses(ele) {
+  return ele._statuses.map(status => html`
+<tr class=${getStatusClass(status.message)}>
+  <td>${status.username}</td>
+  <td>${getLocalDate(status.date)}</td>
+  <td>${status.message}</td>
+  <td>${status.rollers}</td>
+</tr>`);
+}
+
+function getLocalDate(timestamp) {
+  return new Date(timestamp).toLocaleString();
+}
+
+function getStatusClass(message) {
+  let treeState = '';
+  const lowerCaseMessage = message.toLowerCase();
+  if (lowerCaseMessage.includes(OPEN)) {
+    treeState = OPEN;
+  } else if (lowerCaseMessage.includes(CAUTION)) {
+    treeState = CAUTION;
+  } else if (lowerCaseMessage.includes(CLOSED)) {
+    treeState = CLOSED;
+  }
+  return treeState;
+}
+
+const template = (ele) => html`
+<table class="recent_statuses">
+  <tr>
+    <th>Who</th>
+    <th>When</th>
+    <th>Message</th>
+    <th>Wait for</th>
+  </tr>
+  ${recentStatuses(ele)}
+</table>
+`;
+
+define('display-tree-status-sk', class extends ElementSk {
+  constructor() {
+    super(template);
+    this._statuses = [];
+  }
+
+  /** @prop statuses {string} The list of recent tree statuses. */
+  get statuses() { return this._statuses }
+  set statuses(val) {
+    this._statuses = val;
+    this._render();
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    this._render();
+  }
+
+});
