32401a3ca4f9fb19da1adfbfd9503b7f434a456d
Ben Wagner <benjaminwagner@google.com>
2020-02-25 15:10:16 -0500

    Remove outdated Skolo Docker NFS server
    Change-Id: I294b5aaffc95a30d1805871ac22d47a6b2492749
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/273272
    Commit-Queue: Ben Wagner aka dogben <benjaminwagner@google.com>
    Commit-Queue: Kevin Lubick <kjlubick@google.com>
    Auto-Submit: Ben Wagner aka dogben <benjaminwagner@google.com>
    Reviewed-by: Kevin Lubick <kjlubick@google.com>

--- skolo/docker/print_auth_token.py
+++
@@ -1,8 +0,0 @@
-import json
-import urllib
-
-AUTH_URL = ('http://metadata/computeMetadata/v1/instance'
-            '/service-accounts/default/token')
-response = urllib.urlopen(AUTH_URL)
-auth = json.loads(response.read())
-print auth.access_token
