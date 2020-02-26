4282b0dc183306150ee3a91fe3240f33bef75c27
Ravi Mistry <rmistry@google.com>
2020-02-26 10:58:13 -0500

    [Tree Status] Add lit-html pages and modules
    Old vs new pages
    ----------------
    http://skia-tree-status.appspot.com/ :
    http://skia-tree-status.appspot.com/sheriff :
    http://skia-tree-status.appspot.com/trooper :
    http://skia-tree-status.appspot.com/gpu-sheriff :
    http://skia-tree-status.appspot.com/robocop :
    
    Bug: skia:9962
    Change-Id: I5ae2b8101215b1a01654306322ab075e26c19e0b

---
+++ tree_status/modules/display-tree-status-sk/display-tree-status-sk.js
@@ -0,0 +1,77 @@
+/**
+ * @module display-tree-status-sk
+ * @description <h2><code>display-tree-status-sk</code></h2>
+ *
+ * <p>
+ *   Displays recent tree statuses in a table with rows colored occording to
+ *   the tree status (green=open,yellow=caution,red=closed).
+ * </p>
+ *
+ */
+
+import { ElementSk } from '../../../infra-sk/modules/ElementSk'
+import { define } from 'elements-sk/define'
+import { html } from 'lit-html'
+
+// States of the tree.
+const OPEN = 'open';
+const CAUTION = 'caution';
+const CLOSED = 'closed';
+
+function recentStatuses(ele) {
+  return ele._statuses.map(status => html`
+<tr class=${getStatusClass(status.message)}>
+  <td>${status.username}</td>
+  <td>${getLocalDate(status.date)}</td>
+  <td>${status.message}</td>
+  <td>${status.rollers}</td>
+</tr>`);
+}
+
+function getLocalDate(timestamp) {
+  return new Date(timestamp).toLocaleString();
+}
+
+function getStatusClass(message) {
+  let treeState = '';
+  const lowerCaseMessage = message.toLowerCase();
+  if (lowerCaseMessage.includes(OPEN)) {
+    treeState = OPEN;
+  } else if (lowerCaseMessage.includes(CAUTION)) {
+    treeState = CAUTION;
+  } else if (lowerCaseMessage.includes(CLOSED)) {
+    treeState = CLOSED;
+  }
+  return treeState;
+}
+
+const template = (ele) => html`
+<table class="recent_statuses">
+  <tr>
+    <th>Who</th>
+    <th>When</th>
+    <th>Message</th>
+    <th>Wait for</th>
+  </tr>
+  ${recentStatuses(ele)}
+</table>
+`;
+
+define('display-tree-status-sk', class extends ElementSk {
+  constructor() {
+    super(template);
+    this._statuses = [];
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    this._render();
+  }
+
+  /** @prop statuses {string} The list of recent tree statuses. */
+  get statuses() { return this._statuses }
+  set statuses(val) {
+    this._statuses = val;
+    this._render();
+  }
+});
