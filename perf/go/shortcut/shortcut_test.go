c52dc1f15ea277ca2d08f38bf74758bfb7b03e8f
Joe Gregorio <jcgregorio@google.com>
2020-02-26 15:01:50 -0500

    [perf] Add SQL implementation of shortcut.Store.
    Change-Id: Ie824d8821be9b7ed4c6200b073cd1c6f4e22dbe6

---
+++ perf/go/shortcut/shortcut_test.go
@@ -0,0 +1,29 @@
+package shortcut
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"go.skia.org/infra/go/testutils/unittest"
+)
+
+func TestIDFromKeys(t *testing.T) {
+	unittest.SmallTest(t)
+
+	assert.Equal(t, "X", IDFromKeys(nil))
+
+	assert.Equal(t, "X8a18d2be561b75dc48c3afcd8145767c", IDFromKeys(&Shortcut{
+		Keys: []string{
+			",arch=x86,config=8888,",
+			",arch=arm,config=8888,",
+		},
+	}))
+
+	// Test that order doesn't matter.
+	assert.Equal(t, "X8a18d2be561b75dc48c3afcd8145767c", IDFromKeys(&Shortcut{
+		Keys: []string{
+			",arch=arm,config=8888,",
+			",arch=x86,config=8888,",
+		},
+	}))
+}
---
+++ perf/go/sql/sqltest/sqltest.go
@@ -0,0 +1,84 @@
+package sqltest
+
+import (
+	"database/sql"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	perfsql "go.skia.org/infra/perf/go/sql"
+	"go.skia.org/infra/perf/go/sql/migrations"
+)
+
+// Cleanup is a function to call after the test has ended to clean up any
+// database resources.
+type Cleanup func()
+
+// NewSQLite3DBForTests creates a new temporary sqlite3 database with all
+// migrations applied for testing. It also returns a function to call to clean
+// up the database after the tests have completed.
+func NewSQLite3DBForTests(t *testing.T) (*sql.DB, Cleanup) {
+	// Get a temp file to use as an sqlite3 database.
+	tmpfile, err := ioutil.TempFile("", "sqlts")
+	require.NoError(t, err)
+	require.NoError(t, tmpfile.Close())
+
+	cleanup := func() {
+		err := os.Remove(tmpfile.Name())
+		assert.NoError(t, err)
+	}
+
+	db, err := sql.Open("sqlite3", tmpfile.Name())
+	require.NoError(t, err)
+
+	migrationsDir := "../../../migrations/sqlite"
+	migrationsConnection := fmt.Sprintf("sqlite3://%s", tmpfile.Name())
+
+	err = migrations.Up(migrationsDir, migrationsConnection)
+	require.NoError(t, err)
+
+	return db, cleanup
+}
+
+// NewCockroachDBForTests creates a new temporary sqlite3 database with all
+// migrations applied for testing. It also returns a function to call to clean
+// up the database after the tests have completed.
+//
+// We pass in a database name so that different tests work in different
+// databases, even though they may be in the same CockroachDB instance, so that
+// if a test fails it doesn't leave the database in a bad state for a subsequent
+// test.
+func NewCockroachDBForTests(t *testing.T, databaseName string) (*sql.DB, Cleanup) {
+	migrationsDir := "../../../migrations/cockroachdb"
+	migrationsConnection := fmt.Sprintf("cockroach://root@%s/%s?sslmode=disable", perfsql.GetCockroachDBEmulatorHost(), databaseName)
+	// Note that the migrationsConnection is different from the sql.Open
+	// connection string since migrations know about CockroachDB, but we use the
+	// Postgres driver for the database/sql connection since there's no native
+	// CockroachDB golang driver, and the suggested SQL drive for CockroachDB is
+	// the Postgres driver since that's the underlying communication protocol it
+	// uses.
+	connectionString := fmt.Sprintf("postgresql://root@%s/%s?sslmode=disable", perfsql.GetCockroachDBEmulatorHost(), databaseName)
+	db, err := sql.Open("postgres", connectionString)
+	require.NoError(t, err)
+
+	// Create a database in the cockroachdb just for this test.
+	_, err = db.Exec(`
+ 		CREATE DATABASE IF NOT EXISTS shortcutstore;
+ 		SET DATABASE = shortcutstore;`)
+	require.NoError(t, err)
+
+	err = migrations.Up(migrationsDir, migrationsConnection)
+	require.NoError(t, err)
+
+	cleanup := func() {
+		err := migrations.Down(migrationsDir, migrationsConnection)
+		assert.NoError(t, err)
+		_, err = db.Exec("DROP DATABASE shortcutstore CASCADE;")
+		assert.NoError(t, err)
+	}
+
+	return db, cleanup
+}
