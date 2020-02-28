ee5b9a2ad3b2d6eb664d2dab2b9bd1cd3991e00f
Joe Gregorio <jcgregorio@google.com>
2020-02-28 14:29:41 -0500

    [perf] Add sqlalertstore.
    Change-Id: I8ce42515726a2300d47a70d5d582775789e44d3e

---
+++ perf/go/alerts/sqlalertstore/sqlalertstore_test.go
@@ -0,0 +1,35 @@
+package sqlalertstore
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"go.skia.org/infra/go/testutils/unittest"
+	"go.skia.org/infra/perf/go/alerts/alertstest"
+	perfsql "go.skia.org/infra/perf/go/sql"
+	"go.skia.org/infra/perf/go/sql/sqltest"
+)
+
+func TestSQLAlertStore_SQLite(t *testing.T) {
+	unittest.LargeTest(t)
+
+	db, cleanup := sqltest.NewSQLite3DBForTests(t)
+	defer cleanup()
+
+	store, err := New(db, perfsql.SQLiteDialect)
+	require.NoError(t, err)
+
+	alertstest.Store_SaveListDelete(t, store)
+}
+
+func TestSQLAlertStore_CockroachDB(t *testing.T) {
+	unittest.LargeTest(t)
+
+	db, _ := sqltest.NewCockroachDBForTests(t, "alertstore")
+	//defer cleanup()
+
+	store, err := New(db, perfsql.CockroachDBDialect)
+	require.NoError(t, err)
+
+	alertstest.Store_SaveListDelete(t, store)
+}
