5cc2f695bdcb7c244876c855a2e62d45bd7cd3f2
Joe Gregorio <jcgregorio@google.com>
2020-03-02 10:54:59 -0500

    [perf] Add GetAll to shortcut.Store
    Change-Id: I9996cbad9434b91ba9bb8d08196e92f8fc179520

--- perf/go/shortcut/shortcuttest/shortcuttest.go
+++ perf/go/shortcut/shortcuttest/shortcuttest.go
@@ -6,7 +6,9 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
+	"fmt"
 	"sort"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -14,7 +16,7 @@ import (
 	"go.skia.org/infra/perf/go/shortcut"
 )
 
-// InsertGet does the core testing of an instance of shortcut.Store.
+// Shortcut_InsertGet does the core testing of an instance of shortcut.Store.
 func InsertGet(t *testing.T, store shortcut.Store) {
 	ctx := context.Background()
 	// Write a shortcut.
@@ -37,4 +39,55 @@ func InsertGet(t *testing.T, store shortcut.Store) {
 	assert.NotEqual(t, sh, sh2)
 	sort.Strings(sh.Keys)
 	assert.Equal(t, sh, sh2)
+}
+
+// Shortcut_GetNonExistent tests that we fail when retrieving an unknown
+// shortcut.
+func GetNonExistent(t *testing.T, store shortcut.Store) {
+	ctx := context.Background()
+
+	_, err := store.Get(ctx, "X-unknown")
+	require.Error(t, err)
+}
+
+// readAll reads all the Shortcuts from the channel and returns them in a slice.
+func readAll(ch <-chan *shortcut.Shortcut) []*shortcut.Shortcut {
+	ret := []*shortcut.Shortcut{}
+	for s := range ch {
+		ret = append(ret, s)
+	}
+	return ret
+}
+
+// Shortcut_GetAll tests that GetAll produces a channel of all the shortcuts in
+// the database.
+func GetAll(t *testing.T, store shortcut.Store) {
+	ctx := context.Background()
+	const numShortcuts = 12
+	// Write some shortcuts.
+	for i := 0; i < numShortcuts; i++ {
+		sh := &shortcut.Shortcut{
+			Keys: []string{
+				fmt.Sprintf("https://foo/%d", i),
+			},
+		}
+		_, err := store.InsertShortcut(ctx, sh)
+		require.NoError(t, err)
+	}
+	ch, err := store.GetAll(ctx)
+	require.NoError(t, err)
+	all := readAll(ch)
+	assert.Len(t, all, numShortcuts)
+	assert.True(t, strings.HasPrefix(all[0].Keys[0], "https://foo/"))
+}
+
+// SubTestFunction is a func we will call to test one aspect of an
+// implementation of regression.Store.
+type SubTestFunction func(t *testing.T, store shortcut.Store)
+
+// SubTests are all the subtests we have for regression.Store.
+var SubTests = map[string]SubTestFunction{
+	"Shortcut_GetAll":         GetAll,
+	"Shortcut_InsertGet":      InsertGet,
+	"Shortcut_GetNonExistent": GetNonExistent,
 }
