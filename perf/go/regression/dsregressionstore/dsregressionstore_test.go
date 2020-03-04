7a3612250007b217166c715add63515306740fb8
Joe Gregorio <jcgregorio@google.com>
2020-03-04 10:23:22 -0500

    [perf] Another refactor to simplify regression.Store.
      * Move from using unix timestamps to types.CommitNumber in the regression.Store interface.
      * Move the regression.DetailLookup out of the interface since it's only needed for the Cloud Datastore impl.
      * Add Context as arg to regression.DetailLookup.
    
    Change-Id: I42e123cda883bdf0ef8ce41c58bd6a7444b1fdce

--- perf/go/regression/dsregressionstore/dsregressionstore_test.go
+++ perf/go/regression/dsregressionstore/dsregressionstore_test.go
@@ -1,13 +1,41 @@
 package dsregressionstore
 
 import (
+	"context"
 	"testing"
+	"time"
 
 	"go.skia.org/infra/go/ds"
 	"go.skia.org/infra/go/ds/testutil"
 	"go.skia.org/infra/go/testutils/unittest"
+	"go.skia.org/infra/perf/go/cid"
+	"go.skia.org/infra/perf/go/regression"
 	"go.skia.org/infra/perf/go/regression/regressiontest"
 )
+
+// getDetailLookupForTests returns a lookup function that will work with the
+// tests we run against the datastore, that is we are faking CommitIDs and
+// timestamps so they align with what the tests use.
+func getDetailLookupForTests() regression.DetailLookup {
+	now := time.Now()
+	lookupValues := []*cid.CommitDetail{}
+	for i := 0; i < 4; i++ {
+		lookupValues = append(lookupValues, &cid.CommitDetail{
+			CommitID: cid.CommitID{
+				Offset: i,
+			},
+			// Use time.Duration(i-1) to ensure that we catch commitNumber=1,
+			// which will be written with a timestamp of ~now, but not exactly,
+			// so we fudge the numbers here.
+			Timestamp: now.Add(time.Duration(i-1) * time.Minute).Unix(),
+		})
+	}
+
+	lookup := func(ctx context.Context, c *cid.CommitID) (*cid.CommitDetail, error) {
+		return lookupValues[c.Offset], nil
+	}
+	return lookup
+}
 
 // TestDS test storing regressions in Google Cloud Datastore.
 func TestDS(t *testing.T) {
@@ -20,7 +48,7 @@ func TestDS(t *testing.T) {
 		t.Run(name, func(t *testing.T) {
 			cleanup := testutil.InitDatastore(t, ds.REGRESSION)
 			defer cleanup()
-			store := NewRegressionStoreDS()
+			store := NewRegressionStoreDS(getDetailLookupForTests())
 			subTest(t, store)
 		})
 	}
