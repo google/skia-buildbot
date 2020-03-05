7937e30a2b3f58e5c318086210930039f99c036e
Weston Tracey <westont@google.com>
2020-03-05 08:58:13 -0500

    It works.
    Begin migrating CT to lit-html.
    
    First pass, currently only a PoC, with the new task-queue-ski module
    replacing the existing pending-tasks-sk.html.  Have it duplicated on the
    skia.org/queue page to ensure I get feature parity.
    
    Change-Id: I0094d99c19b288739ca981495cac8cadede883c0

---
+++ ct/pages/transitional.js
@@ -0,0 +1,5 @@
+// Placeholder for lit-html modules so they can be incorporated into legacy
+// Polymer pages.
+
+import '../node_modules/@webcomponents/custom-elements/custom-elements.min.js'
+import '../modules/task-queue-sk'
