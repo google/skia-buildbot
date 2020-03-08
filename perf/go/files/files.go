958d2ae61ec00a1553bb5dc70da2799a41053cf8
Joe Gregorio <joe@bitworking.org>
2020-03-07 22:11:41 -0500

    [perf] Breaking out files sources as their own interface.
    Change-Id: Id902812a311a8637868cca55ad37f966cb6106a9

---
+++ perf/go/files/files.go
@@ -0,0 +1,23 @@
+// Package files is a source of file names and contents that contain data
+// for Perf to ingest.
+package files
+
+import (
+	"context"
+	"io"
+	"time"
+)
+
+// File represents a single file.
+type File struct {
+	Name     string
+	Contents io.ReadCloser
+	Created  time.Time
+}
+
+// Source is a source of Files.
+type Source interface {
+	// Start begins the process of looking for new files and as they arrive they
+	// are sent on the returned channel.
+	Start(ctx context.Context) (<-chan File, error)
+}
---
+++ perf/go/files/gcssource/gcssource_test.go
@@ -0,0 +1,98 @@
+package gcssource
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"cloud.google.com/go/pubsub"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.skia.org/infra/go/testutils/unittest"
+	"go.skia.org/infra/perf/go/config"
+)
+
+const (
+	projectName = "test-project"
+	topicName   = "test-file-source"
+	testFile    = "gs://skia-infra/testdata/perf-files-gcssource-test.json"
+)
+
+func setupPubSub(t *testing.T, cfg *config.InstanceConfig) {
+	ctx := context.Background()
+
+	// This test presumes the pubsub emulator is running.
+	//
+	// Add code to test for existence and print helpful info if not present.
+	pubSubClient, err := pubsub.NewClient(ctx, projectName)
+	require.NoError(t, err)
+
+	// Create the topic if it doesn't exist.
+	topic := pubSubClient.Topic(topicName)
+	exists, err := topic.Exists(ctx)
+	require.NoError(t, err)
+
+	if !exists {
+		topic, err = pubSubClient.CreateTopic(ctx, topicName)
+		require.NoError(t, err)
+	}
+
+	// Create the subscription if it doesn't exists. For some reason we need to
+	// do this here when running against the emulator.
+	hostname, err := os.Hostname()
+	require.NoError(t, err)
+	subName := fmt.Sprintf("%s-%s", cfg.IngestionConfig.SourceConfig.Topic, hostname)
+	sub := pubSubClient.Subscription(subName)
+	exists, err = sub.Exists(ctx)
+	require.NoError(t, err)
+	if !exists {
+		_, err = pubSubClient.CreateSubscription(ctx, subName, pubsub.SubscriptionConfig{
+			Topic: pubSubClient.Topic(topicName),
+		})
+		require.NoError(t, err)
+	}
+
+	// Publish a message that looks like the arrival of a file.
+	b, err := json.Marshal(pubSubEvent{
+		Bucket: "skia-infra",
+		Name:   "testdata/perf-files-gcssource-test.json",
+	})
+	require.NoError(t, err)
+
+	msg := &pubsub.Message{
+		Data: b,
+	}
+	res := topic.Publish(ctx, msg)
+
+	// Wait for the message to be sent.
+	_, err = res.Get(ctx)
+	assert.NoError(t, err)
+}
+
+func TestStart(t *testing.T) {
+	unittest.ManualTest(t)
+	ctx := context.Background()
+
+	cfg := &config.InstanceConfig{
+		IngestionConfig: config.IngestionConfig{
+			SourceConfig: config.SourceConfig{
+				Project: projectName,
+				Topic:   topicName,
+			},
+		},
+	}
+
+	setupPubSub(t, cfg)
+	source := New(cfg, true)
+	ch, err := source.Start(ctx)
+	require.NoError(t, err)
+
+	file := <-ch
+	assert.Equal(t, testFile, file.Name)
+	b, err := ioutil.ReadAll(file.Contents)
+	assert.NoError(t, err)
+	assert.Equal(t, "{\n  \"status\": \"Success\"\n}\n", string(b))
+}
