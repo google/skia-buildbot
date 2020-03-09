388e6f26c825b4d41c4d2f075da70e18e669cb3a
Ravi Mistry <rmistry@google.com>
2020-03-09 12:22:53 -0400

    [Autorollers] Add ability to filter revisions by successful luci builds
    Replaces the filtering by storage paths.
    
    Bug: skia:10010
    Change-Id: I0e0f85c27e9b4f77bcfd2a1fe344074f70a3292c

--- autoroll/go/repo_manager/github_repo_manager.go
+++ autoroll/go/repo_manager/github_repo_manager.go
@@ -10,12 +10,12 @@ import (
 	"regexp"
 	"strings"
 
-	"cloud.google.com/go/storage"
+	buildbucketpb "go.chromium.org/luci/buildbucket/proto"
+
 	"go.skia.org/infra/autoroll/go/codereview"
 	"go.skia.org/infra/autoroll/go/config_vars"
 	"go.skia.org/infra/autoroll/go/revision"
-	"go.skia.org/infra/go/gcs"
-	"go.skia.org/infra/go/gcs/gcsclient"
+	"go.skia.org/infra/go/buildbucket"
 	"go.skia.org/infra/go/git"
 	"go.skia.org/infra/go/github"
 	"go.skia.org/infra/go/sklog"
@@ -60,25 +60,26 @@ type GithubRepoManagerConfig struct {
 	ChildRepoURL string `json:"childRepoURL"`
 	// The roller will update this file with the child repo's revision.
 	RevisionFile string `json:"revisionFile"`
-	// GCS bucket to use if filtering revisions by presence of files in GCS.
-	StorageBucket string `json:"storageBucket"`
-	// Templates of GCS files to look for; if provided, revisions to roll
-	// will be filtered by the presence of these files in GCS.
-	StoragePathTemplates []string `json:"storagePathTemplates"`
+
+	// Buildbucket project to query for luci builds.
+	BuildbucketProject string `json:"buildbucketProject"`
+	// Buildbucket bucket to query for luci builds.
+	BuildbucketBucket string `json:"buildbucketBucket"`
 }
 
 // githubRepoManager is a struct used by the autoroller for managing checkouts.
 type githubRepoManager struct {
 	*commonRepoManager
 	filterRevisionsByGCS []string
-	gcs                  gcs.GCSClient
 	githubClient         *github.GitHub
 	parentRepo           *git.Checkout
 	parentRepoURL        string
 	childRepoURL         string
 	revisionFile         string
-	gsBucket             string
-	gsPathTemplates      []string
+
+	bb        buildbucket.BuildBucketInterface
+	bbProject string
+	bbBucket  string
 }
 
 // NewGithubRepoManager returns a RepoManager instance which operates in the given
@@ -120,25 +121,17 @@ func NewGithubRepoManager(ctx context.Context, c *GithubRepoManagerConfig, reg *
 		}
 	}
 
-	var gcsClient gcs.GCSClient
-	if len(c.StoragePathTemplates) > 0 {
-		storageClient, err := storage.NewClient(ctx)
-		if err != nil {
-			return nil, err
-		}
-		gcsClient = gcsclient.New(storageClient, c.StorageBucket)
-	}
-
 	gr := &githubRepoManager{
 		commonRepoManager: crm,
-		gcs:               gcsClient,
 		githubClient:      githubClient,
 		parentRepo:        parentRepo,
 		parentRepoURL:     c.ParentRepo,
 		childRepoURL:      c.ChildRepoURL,
 		revisionFile:      c.RevisionFile,
-		gsBucket:          c.StorageBucket,
-		gsPathTemplates:   c.StoragePathTemplates,
+
+		bb:        buildbucket.NewClient(client),
+		bbProject: c.BuildbucketProject,
+		bbBucket:  c.BuildbucketBucket,
 	}
 
 	return gr, nil
@@ -220,29 +213,32 @@ func (rm *githubRepoManager) Update(ctx context.Context) (*revision.Revision, *r
 		rm.fixPullRequestLinks(rev)
 	}
 
-	// Optionally filter not-rolled revisions by the existence of matching
-	// files in GCS.
-	if len(rm.gsPathTemplates) > 0 {
+	// Optionally filter not-rolled revisions by successful buildbucket builds.
+	if rm.bbProject != "" && rm.bbBucket != "" {
 		if len(notRolledRevs) > 0 {
 			for _, notRolledRev := range notRolledRevs {
-				// Check to see if this commit exists in the gsPath locations.
-				for _, gsPathTemplate := range rm.gsPathTemplates {
-					gsPath := fmt.Sprintf(gsPathTemplate, notRolledRev.Id)
-					fileExists, err := rm.gcs.DoesFileExist(ctx, gsPath)
-					if err != nil {
-						return nil, nil, nil, err
-					}
-					if fileExists {
-						sklog.Infof("[gcsFileFilter] Found %s", gsPath)
+				pred := &buildbucketpb.BuildPredicate{
+					Builder: &buildbucketpb.BuilderID{Project: rm.bbProject, Bucket: rm.bbBucket},
+					Tags: []*buildbucketpb.StringPair{
+						&buildbucketpb.StringPair{Key: "buildset", Value: fmt.Sprintf("commit/git/%s", notRolledRev.Id)},
+					},
+				}
+				builds, err := rm.bb.Search(ctx, pred)
+				if err != nil {
+					return nil, nil, nil, err
+				}
+				for _, build := range builds {
+					if build.Status == buildbucketpb.Status_SUCCESS {
+						sklog.Infof("[bbFilter] Build status of \"%s\" for %s was %s", build.Builder.Builder, notRolledRev.Id, build.Status)
 						continue
 					} else {
-						sklog.Infof("[gcsFileFilter] Could not find %s", gsPath)
-						notRolledRev.InvalidReason = fmt.Sprintf("Missing required GCS file %q", gsPath)
+						sklog.Infof("[bbFilter] Build status of \"%s\" for %s was %s", build.Builder.Builder, notRolledRev.Id, build.Status)
+						notRolledRev.InvalidReason = fmt.Sprintf("Luci build of \"%s\" for %s was %s", build.Builder.Builder, notRolledRev.Id, build.Status)
 						break
 					}
 				}
 				if notRolledRev.InvalidReason == "" {
-					sklog.Infof("[gcsFileFilter] Found all %s paths for %s", rm.gsPathTemplates, notRolledRev.Id)
+					sklog.Infof("[bbFilter] All builds of %s were %s", notRolledRev.Id, buildbucketpb.Status_SUCCESS)
 				}
 			}
 		}
