1787740ced77cf9881b169f5f4a1d08d0a6f2c1d
Kevin Lubick <kjlubick@google.com>
2020-03-10 13:47:30 -0400

    [gold] Add lit-html element for showing triage history.
    This is as small as an element as I would probably make - any simpler
    and a function would probably suffice.
    
    Even though the backend returns all history, we just show the most
    recent one for simplicity.
    
    It makes it easier to integrate with Polymer though, so that's a win.
    
    Change-Id: I36f2f8e14e460a0b40cf833e73b0caaae4121854
    Bug: skia:9351

---
+++ golden/modules/triage-history-sk/triage-history-sk_test.js
@@ -0,0 +1,63 @@
+import './index';
+import { $$ } from 'common-sk/modules/dom';
+import {
+  setUpElementUnderTest,
+} from '../test_util';
+
+describe('triage-history-sk', () => {
+  const newInstance = setUpElementUnderTest('triage-history-sk');
+
+  const originalNow = Date.now;
+  let triageHistorySk;
+  beforeEach(() => {
+    Date.now = () => new Date('2020-03-05T05:06:07-04:00');
+    triageHistorySk = newInstance();
+  });
+
+  afterEach(() => {
+    Date.now = originalNow;
+  });
+
+  it('renders nothing on an empty history', () => {
+    triageHistorySk.history = [];
+    expect(triageHistorySk.innerText).to.be.empty;
+  });
+
+  it('renders only the most recent history entry', () => {
+    triageHistorySk.history = [{
+      user: 'helpfuluser@example.com',
+      ts: new Date('2020-03-04T05:06:07.000000000-04:00'),
+    }, {
+      user: 'thisshouldnotshowup@example.com',
+      ts: new Date('2020-01-01T05:06:07.000000000-04:00'),
+    }];
+    expect(triageHistorySk.innerText).to.equal('1d ago by helpfuluser@');
+    const msg = $$('.message', triageHistorySk);
+    expect(msg.getAttribute('title')).to.equal(
+      'Last triaged on Wed Mar 04 2020 04:06:07 GMT-0500 (Eastern Standard Time) by helpfuluser@example.com',
+    );
+  });
+
+  it('renders the full user name if it is not an email', () => {
+    triageHistorySk.history = [{
+      user: 'expectation_cleaner',
+      ts: new Date('2020-03-04T05:06:07.000000000-04:00'),
+    }];
+    expect(triageHistorySk.innerText).to.equal('1d ago by expectation_cleaner');
+  });
+
+  describe('"history" property setter/getter', () => {
+    it('converts date strings into real dates', () => {
+      triageHistorySk.history = [{
+        user: 'helpfuluser@example.com',
+        ts: '2020-03-04T05:06:07.000000000-04:00',
+      }];
+
+      expect(triageHistorySk.history.length).to.equal(1);
+      expect(triageHistorySk.history[0].user).to.equal('helpfuluser@example.com');
+      expect(triageHistorySk.history[0].ts.getTime()).to.equal(
+        Date.parse('2020-03-04T05:06:07.000000000-04:00'),
+      );
+    });
+  });
+});
