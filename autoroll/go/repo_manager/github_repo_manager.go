ab13c679aa6adada00cd605cee311f83584eccbd
Ravi Mistry <rmistry@google.com>
2020-03-12 12:49:26 -0400

    [Autoroller] Account for luci build retries in engine->framework roller
    Bug: skia:10038
    Change-Id: I564fbb80d7dc41cd17743f9a0fbbee972b4649a3

--- autoroll/go/repo_manager/github_repo_manager.go
+++ autoroll/go/repo_manager/github_repo_manager.go
@@ -129,13 +129,26 @@ func (f bbRevisionFilter) Skip(ctx context.Context, r *revision.Revision) (strin
 		sklog.Infof("[bbFilter] Builds for %s have not started yet", r.Id)
 		return fmt.Sprintf("Builds have not started yet"), nil
 	}
+
+	// statuses stores the statuses of builders. This is used to account for luci build retries.
+	// It is used to determine if there was any successful build for a builder. We should have ideally used
+	// the most recent status but there appears to be strange behavior with flutter luci builds where
+	// INFRA_FAILURE builds appear to be coming after SUCCESSFUL builds. Eg:
+	// https://cr-buildbucket.appspot.com/rpcexplorer/services/buildbucket.v2.Builds/SearchBuilds?request={"predicate":{"builder":{"project": "flutter","bucket": "prod"},"tags":[{"key": "buildset","value": "commit/git/18962926012965f815c273e58409cda3144998f5"}]}}
+	// This has been brought up with the flutter team.
+	statuses := map[string]buildbucketpb.Status{}
 	for _, build := range builds {
-		if build.Status == buildbucketpb.Status_SUCCESS {
-			sklog.Infof("[bbFilter] Build status of \"%s\" for %s was %s", build.Builder.Builder, r.Id, build.Status)
-			continue
+		prev, ok := statuses[build.Builder.Builder]
+		if !ok || prev != buildbucketpb.Status_SUCCESS {
+			statuses[build.Builder.Builder] = build.Status
+		}
+	}
+	for b, status := range statuses {
+		if status == buildbucketpb.Status_SUCCESS {
+			sklog.Infof("[bbFilter] Found successful build of \"%s\" for %s", b, r.Id)
 		} else {
-			sklog.Infof("[bbFilter] Build status of \"%s\" for %s was %s", build.Builder.Builder, r.Id, build.Status)
-			return fmt.Sprintf("Luci build of \"%s\" for %s was %s", build.Builder.Builder, r.Id, build.Status), nil
+			sklog.Infof("[bbFilter] Could not find successful build of \"%s\" for %s: %s", b, r.Id, status)
+			return fmt.Sprintf("Luci builds of \"%s\" for %s was %s", b, r.Id, status), nil
 		}
 	}
 	sklog.Infof("[bbFilter] All builds of %s were %s", r.Id, buildbucketpb.Status_SUCCESS)
