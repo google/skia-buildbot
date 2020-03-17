498f5277f8206d233e1ad74e64fd941c81533909
Joe Gregorio <joe@bitworking.org>
2020-03-17 12:24:05 -0400

    [perf] Use go.rice to embed the migration files.
    Initially tried 'statik' but it didn't work with cockroachdb migrations.
    
    Change-Id: I07271ecc417a38e374af2c3d4955979ae672ac09

--- perf/go/sql/migrations/migrations_test.go
+++ perf/go/sql/migrations/migrations_test.go
@@ -6,8 +6,11 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"go.skia.org/infra/go/testutils/unittest"
 	perfsql "go.skia.org/infra/perf/go/sql"
+	"go.skia.org/infra/perf/go/sql/migrations/cockroachdb"
+	"go.skia.org/infra/perf/go/sql/migrations/sqlite3"
 )
 
 func getEmulatorHost() string {
@@ -21,11 +24,12 @@ func getEmulatorHost() string {
 func TestUpDown_CockroachDB(t *testing.T) {
 	unittest.LargeTest(t)
 
-	const cockroachMigrations = "../../../migrations/cockroachdb"
+	cockroachMigrations, err := cockroachdb.New()
+	require.NoError(t, err)
 
 	cockroachDBTest := fmt.Sprintf("cockroach://root@%s?sslmode=disable", perfsql.GetCockroachDBEmulatorHost())
 
-	err := Up(cockroachMigrations, cockroachDBTest)
+	err = Up(cockroachMigrations, cockroachDBTest)
 	assert.NoError(t, err)
 	err = Down(cockroachMigrations, cockroachDBTest)
 	assert.NoError(t, err)
@@ -41,10 +45,11 @@ func TestUpDown_CockroachDB(t *testing.T) {
 func TestUpDown_Sqlite3(t *testing.T) {
 	unittest.LargeTest(t)
 
-	const sqlite3Migrations = "../../../migrations/sqlite"
+	sqlite3Migrations, err := sqlite3.New()
+	require.NoError(t, err)
 	const sqlite3DBTest = "sqlite3:///tmp/test-sqlite3-migrations.db"
 
-	err := Up(sqlite3Migrations, sqlite3DBTest)
+	err = Up(sqlite3Migrations, sqlite3DBTest)
 	assert.NoError(t, err)
 	err = Down(sqlite3Migrations, sqlite3DBTest)
 	assert.NoError(t, err)
