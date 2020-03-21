bfb6b4dad1fe11b41b67b85a1b141c8fa9b5e5df
Joe Gregorio <joe@bitworking.org>
2020-03-20 22:40:59 -0400

    [perf] Update builders so they can build SQL backed stores.
    Change-Id: Iba46a5eb91ad646713478722f905c913b7ff878d

--- perf/go/builders/builders_test.go
+++ perf/go/builders/builders_test.go
@@ -13,8 +13,11 @@ import (
 	"github.com/stretchr/testify/require"
 	"go.skia.org/infra/go/paramtools"
 	"go.skia.org/infra/go/testutils/unittest"
+	"go.skia.org/infra/perf/go/alerts/alertstest"
 	"go.skia.org/infra/perf/go/config"
 	"go.skia.org/infra/perf/go/file/dirsource"
+	"go.skia.org/infra/perf/go/regression/regressiontest"
+	"go.skia.org/infra/perf/go/shortcut/shortcuttest"
 	perfsql "go.skia.org/infra/perf/go/sql"
 	"go.skia.org/infra/perf/go/sql/sqltest"
 	"go.skia.org/infra/perf/go/types"
@@ -63,14 +66,16 @@ func TestNewSourceFromConfig_MissingSourceForDirSourceIsError(t *testing.T) {
 	assert.Error(t, err)
 }
 
-func TestNewTraceStoreFromConfig_SQLite3_Success(t *testing.T) {
+type cleanup func()
+
+func newSqlite3ConfigForTest(t *testing.T) (context.Context, *config.InstanceConfig, cleanup) {
 	unittest.LargeTest(t)
 	ctx := context.Background()
 	dir, err := ioutil.TempDir("", "perf-builders")
 	require.NoError(t, err)
-	defer func() {
+	cleanup := func() {
 		assert.NoError(t, os.RemoveAll(dir))
-	}()
+	}
 
 	instanceConfig := &config.InstanceConfig{
 		DataStoreConfig: config.DataStoreConfig{
@@ -78,14 +83,10 @@ func TestNewTraceStoreFromConfig_SQLite3_Success(t *testing.T) {
 			ConnectionString: filepath.Join(dir, "test.db"),
 		},
 	}
-
-	store, err := NewTraceStoreFromConfig(ctx, true, instanceConfig)
-	require.NoError(t, err)
-	err = store.WriteTraces(types.CommitNumber(0), []paramtools.Params{{"config": "8888"}}, []float32{1.2}, nil, "gs://foobar", time.Now())
-	assert.NoError(t, err)
+	return ctx, instanceConfig, cleanup
 }
 
-func TestNewTraceStoreFromConfig_CockroachDB_Success(t *testing.T) {
+func newCockroachDBConfigForTest(t *testing.T) (context.Context, *config.InstanceConfig, sqltest.Cleanup) {
 	unittest.LargeTest(t)
 	ctx := context.Background()
 
@@ -94,7 +95,6 @@ func TestNewTraceStoreFromConfig_CockroachDB_Success(t *testing.T) {
 	connectionString := fmt.Sprintf("postgresql://root@%s/%s?sslmode=disable", perfsql.GetCockroachDBEmulatorHost(), databaseName)
 
 	_, cleanup := sqltest.NewCockroachDBForTests(t, databaseName, sqltest.DoNotApplyMigrations)
-	defer cleanup()
 
 	instanceConfig := &config.InstanceConfig{
 		DataStoreConfig: config.DataStoreConfig{
@@ -102,6 +102,22 @@ func TestNewTraceStoreFromConfig_CockroachDB_Success(t *testing.T) {
 			ConnectionString: connectionString,
 		},
 	}
+	return ctx, instanceConfig, cleanup
+}
+
+func TestNewTraceStoreFromConfig_SQLite3_Success(t *testing.T) {
+	ctx, instanceConfig, cleanup := newSqlite3ConfigForTest(t)
+	defer cleanup()
+
+	store, err := NewTraceStoreFromConfig(ctx, true, instanceConfig)
+	require.NoError(t, err)
+	err = store.WriteTraces(types.CommitNumber(0), []paramtools.Params{{"config": "8888"}}, []float32{1.2}, nil, "gs://foobar", time.Now())
+	assert.NoError(t, err)
+}
+
+func TestNewTraceStoreFromConfig_CockroachDB_Success(t *testing.T) {
+	ctx, instanceConfig, cleanup := newCockroachDBConfigForTest(t)
+	defer cleanup()
 
 	store, err := NewTraceStoreFromConfig(ctx, true, instanceConfig)
 	require.NoError(t, err)
@@ -110,23 +126,109 @@ func TestNewTraceStoreFromConfig_CockroachDB_Success(t *testing.T) {
 }
 
 func TestNewTraceStoreFromConfig_InvalidDatastoreTypeIsError(t *testing.T) {
-	unittest.LargeTest(t)
-	ctx := context.Background()
-	dir, err := ioutil.TempDir("", "perf-builders")
-	require.NoError(t, err)
-	defer func() {
-		assert.NoError(t, os.RemoveAll(dir))
-	}()
+	ctx, instanceConfig, cleanup := newSqlite3ConfigForTest(t)
+	defer cleanup()
 
 	const invalidDataStoreType = config.DataStoreType("not-a-valid-datastore-type")
-	instanceConfig := &config.InstanceConfig{
-		DataStoreConfig: config.DataStoreConfig{
-			DataStoreType:    invalidDataStoreType,
-			ConnectionString: filepath.Join(dir, "test.db"),
-		},
-	}
+	instanceConfig.DataStoreConfig.DataStoreType = invalidDataStoreType
 
-	_, err = NewTraceStoreFromConfig(ctx, true, instanceConfig)
+	_, err := NewTraceStoreFromConfig(ctx, true, instanceConfig)
+	require.Error(t, err)
+	assert.Contains(t, err.Error(), invalidDataStoreType)
+}
+
+func TestNewAlertStoreFromConfig_Sqlite3_Success(t *testing.T) {
+	ctx, instanceConfig, cleanup := newSqlite3ConfigForTest(t)
+	defer cleanup()
+
+	store, err := NewAlertStoreFromConfig(ctx, false, instanceConfig)
+	require.NoError(t, err)
+
+	alertstest.Store_SaveListDelete(t, store)
+}
+
+func TestNewAlertStoreFromConfig_CockroachDB_Success(t *testing.T) {
+	ctx, instanceConfig, cleanup := newCockroachDBConfigForTest(t)
+	defer cleanup()
+
+	store, err := NewAlertStoreFromConfig(ctx, false, instanceConfig)
+	require.NoError(t, err)
+
+	alertstest.Store_SaveListDelete(t, store)
+}
+
+func TestNewAlertStoreFromConfig_InvalidDatastoreTypeIsError(t *testing.T) {
+	ctx, instanceConfig, cleanup := newSqlite3ConfigForTest(t)
+	defer cleanup()
+
+	const invalidDataStoreType = config.DataStoreType("not-a-valid-datastore-type")
+	instanceConfig.DataStoreConfig.DataStoreType = invalidDataStoreType
+
+	_, err := NewAlertStoreFromConfig(ctx, true, instanceConfig)
+	require.Error(t, err)
+	assert.Contains(t, err.Error(), invalidDataStoreType)
+}
+
+func TestNewRegressionStoreFromConfig_Sqlite3_Success(t *testing.T) {
+	_, instanceConfig, cleanup := newSqlite3ConfigForTest(t)
+	defer cleanup()
+
+	store, err := NewRegressionStoreFromConfig(false, nil, instanceConfig)
+	require.NoError(t, err)
+
+	regressiontest.SetLowAndTriage(t, store)
+}
+
+func TestNewRegressionStoreFromConfig_CochroachDB_Success(t *testing.T) {
+	_, instanceConfig, cleanup := newSqlite3ConfigForTest(t)
+	defer cleanup()
+
+	store, err := NewRegressionStoreFromConfig(false, nil, instanceConfig)
+	require.NoError(t, err)
+
+	regressiontest.SetLowAndTriage(t, store)
+}
+
+func TestNewRegressionStoreFromConfig_InvalidDatastoreTypeIsError(t *testing.T) {
+	_, instanceConfig, cleanup := newSqlite3ConfigForTest(t)
+	defer cleanup()
+
+	const invalidDataStoreType = config.DataStoreType("not-a-valid-datastore-type")
+	instanceConfig.DataStoreConfig.DataStoreType = invalidDataStoreType
+
+	_, err := NewRegressionStoreFromConfig(false, nil, instanceConfig)
+	require.Error(t, err)
+	assert.Contains(t, err.Error(), invalidDataStoreType)
+}
+
+func TestNewShortcutStoreFromConfig_Sqlite3_Success(t *testing.T) {
+	_, instanceConfig, cleanup := newSqlite3ConfigForTest(t)
+	defer cleanup()
+
+	store, err := NewShortcutStoreFromConfig(instanceConfig)
+	require.NoError(t, err)
+
+	shortcuttest.InsertGet(t, store)
+}
+
+func TestNewShortcutStoreFromConfig_CockroachDB_Success(t *testing.T) {
+	_, instanceConfig, cleanup := newCockroachDBConfigForTest(t)
+	defer cleanup()
+
+	store, err := NewShortcutStoreFromConfig(instanceConfig)
+	require.NoError(t, err)
+
+	shortcuttest.InsertGet(t, store)
+}
+
+func TestNewShortcutStoreFromConfig_Sqlite3_InvalidDatastoreTypeIsError(t *testing.T) {
+	_, instanceConfig, cleanup := newSqlite3ConfigForTest(t)
+	defer cleanup()
+
+	const invalidDataStoreType = config.DataStoreType("not-a-valid-datastore-type")
+	instanceConfig.DataStoreConfig.DataStoreType = invalidDataStoreType
+
+	_, err := NewShortcutStoreFromConfig(instanceConfig)
 	require.Error(t, err)
 	assert.Contains(t, err.Error(), invalidDataStoreType)
 }
