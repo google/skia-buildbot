5fcb871b802711b0976b7394bfbd24bdc0ba4426
Joe Gregorio <joe@bitworking.org>
2020-03-23 13:34:58 -0400

    [perf] Recreate ingestion as part of perfserver.
    Change-Id: I00b186e1da545dc12cacb0a4f222b6b7b780fbba

---
+++ perf/configs/README.md
@@ -0,0 +1,3 @@
+These files are configurations for known instances of Perf.
+
+The `demo.json` config is used for a local demo of Perf.
--- perf/go/perfserver/cmd/ingest.go
+++ perf/go/perfserver/cmd/ingest.go
@@ -1,9 +1,17 @@
 package cmd
 
 import (
-	"fmt"
+	"context"
+	"time"
 
 	"github.com/spf13/cobra"
+	"go.skia.org/infra/go/git/gitinfo"
+	"go.skia.org/infra/go/metrics2"
+	"go.skia.org/infra/go/paramtools"
+	"go.skia.org/infra/go/sklog"
+	"go.skia.org/infra/perf/go/builders"
+	"go.skia.org/infra/perf/go/ingest/parser"
+	"go.skia.org/infra/perf/go/types"
 )
 
 // ingestCmd represents the ingest command
@@ -14,8 +22,76 @@ var ingestCmd = &cobra.Command{
 the configured ingestion sources and populates the datastore
 with that data.
 `,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("ingest called")
+	RunE: func(cmd *cobra.Command, args []string) error {
+		ctx := context.Background()
+
+		// Metrics.
+		filesReceived := metrics2.GetCounter("perfserver_ingest_files_received")
+		failedToParse := metrics2.GetCounter("perfserver_ingest_failed_to_parse")
+		badGitHash := metrics2.GetCounter("perfserver_ingest_bad_githash")
+		failedToWrite := metrics2.GetCounter("perfserver_ingest_failed_to_write")
+		successfulWrite := metrics2.GetCounter("perfserver_ingest_successful_write")
+
+		// New file.Source.
+		source, err := builders.NewSourceFromConfig(ctx, instanceConfig, false)
+		ch, err := source.Start(ctx)
+		if err != nil {
+			return err
+		}
+
+		// New Parser.
+		parser := parser.New(instanceConfig)
+
+		// New TraceStore.
+		store, err := builders.NewTraceStoreFromConfig(ctx, false, instanceConfig)
+		if err != nil {
+			return err
+		}
+
+		// New gitinfo.GitInfo.
+		sklog.Infof("Cloning repo %q into %q", instanceConfig.GitRepoConfig.URL, instanceConfig.GitRepoConfig.Dir)
+		vcs, err := gitinfo.CloneOrUpdate(ctx, instanceConfig.GitRepoConfig.URL, instanceConfig.GitRepoConfig.Dir, false)
+		if err != nil {
+			return err
+		}
+
+		sklog.Info("Waiting on files to process.")
+		for f := range ch {
+			sklog.Infof("Ingest received: %v", f)
+			filesReceived.Inc(1)
+
+			// Parse the file.
+			params, values, gitHash, err := parser.Parse(f)
+			if err != nil {
+				sklog.Errorf("Failed to parse %v: %s", f, err)
+				failedToParse.Inc(1)
+				continue
+			}
+
+			// Convert gitHash to commitNumber.
+			index, err := vcs.IndexOf(ctx, gitHash)
+			if err != nil {
+				badGitHash.Inc(1)
+				sklog.Error("Failed to find gitHash %v: %s", f, err)
+				continue
+			}
+
+			// Build paramset from params.
+			ps := paramtools.NewParamSet()
+			for _, p := range params {
+				ps.AddParams(p)
+			}
+
+			// Write data to the trace store.
+			if err := store.WriteTraces(types.CommitNumber(index), params, values, ps, f.Name, time.Now()); err != nil {
+				failedToWrite.Inc(1)
+				sklog.Error("Failed to write %v: %s", f, err)
+			}
+			successfulWrite.Inc(1)
+		}
+		sklog.Infof("Exited while waiting on files. Should only happen on source_type=dir.")
+
+		return nil
 	},
 }
 
