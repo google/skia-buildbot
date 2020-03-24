0b8d5d17213f231adeb4690cb5dece7962d95ead
Joe Gregorio <joe@bitworking.org>
2020-03-24 09:59:01 -0400

    [perf] Recreate ingestion as a part of perfserver.
      * Adds some ingestion files for integration tests, along with the Go code
        that generated them.
      * Also updated Parser to return []float32 instead of []float64 since the
        rest of Perf works solely in float32.
      * perfserver also inits logging and metrics.
    
    Change-Id: I00b186e1da545dc12cacb0a4f222b6b7b780fbba

---
+++ perf/go/ingest/process/process_test.go
@@ -0,0 +1,61 @@
+// Package process does the whole process of ingesting files into a trace store.
+package process
+
+import (
+	"context"
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.skia.org/infra/go/metrics2"
+	"go.skia.org/infra/go/testutils/unittest"
+	"go.skia.org/infra/perf/go/config"
+)
+
+func TestStart_IngestDemoRepoWithSQLite3TraceStore_Success(t *testing.T) {
+	unittest.LargeTest(t)
+
+	// Get a temp file to use as an sqlite3 database.
+	tmpfile, err := ioutil.TempFile("", "ingest-process")
+	require.NoError(t, err)
+	require.NoError(t, tmpfile.Close())
+
+	// Get tmp dir to use for repo checkout.
+	tmpDir, err := ioutil.TempDir("", "ingest-process")
+	require.NoError(t, err)
+
+	defer func() {
+		err = os.Remove(tmpfile.Name())
+		assert.NoError(t, err)
+		err = os.RemoveAll(tmpDir)
+		assert.NoError(t, err)
+	}()
+
+	instanceConfig := config.InstanceConfig{
+		DataStoreConfig: config.DataStoreConfig{
+			DataStoreType:    config.SQLite3DataStoreType,
+			TileSize:         256,
+			ConnectionString: tmpfile.Name(),
+		},
+		IngestionConfig: config.IngestionConfig{
+			SourceConfig: config.SourceConfig{
+				SourceType: config.DirSourceType,
+				Sources:    []string{"../../../integration/data"},
+			},
+		},
+		GitRepoConfig: config.GitRepoConfig{
+			URL: "https://github.com/skia-dev/perf-demo-repo.git",
+			Dir: tmpDir,
+		},
+	}
+
+	err = Start(context.Background(), &instanceConfig)
+	require.NoError(t, err)
+	// The integration data set has 9 good files, 1 file with a bad commit, and
+	// 1 malformed JSON file.
+	assert.Equal(t, int64(11), metrics2.GetCounter("perfserver_ingest_files_received").Get())
+	assert.Equal(t, int64(1), metrics2.GetCounter("perfserver_ingest_bad_githash").Get())
+	assert.Equal(t, int64(1), metrics2.GetCounter("perfserver_ingest_failed_to_parse").Get())
+}
--- perf/go/perfserver/cmd/root.go
+++ perf/go/perfserver/cmd/root.go
@@ -5,10 +5,14 @@ import (
 	"os"
 
 	"github.com/spf13/cobra"
-	"go.skia.org/infra/go/sklog"
+	"go.skia.org/infra/go/metrics2"
+	"go.skia.org/infra/go/sklog/glog_and_cloud"
+	"go.skia.org/infra/perf/go/config"
 )
 
 var instanceConfigFile string
+var instanceConfig *config.InstanceConfig
+var promPort string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -22,16 +26,27 @@ to run the ingestion process:
 	perfserver ingest --config=instance_config.json ...
 
 `,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
+	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+		glog_and_cloud.SetLogger(glog_and_cloud.NewStdErrCloudLogger(glog_and_cloud.SLogStderr))
+
+		var err error
+		instanceConfig, err = config.InstanceConfigFromFile(instanceConfigFile)
+		if err != nil {
+			return err
+		}
+
+		metrics2.InitPrometheus(promPort)
+
+		return nil
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	if err := initSubCommands(); err != nil {
-		sklog.Fatal(err)
+		fmt.Println(err)
+		os.Exit(1)
 	}
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
@@ -48,6 +63,8 @@ func initSubCommands() error {
 	if err != nil {
 		return err
 	}
+
+	rootCmd.PersistentFlags().StringVar(&promPort, "prom_port", ":20000", "Metrics service address (e.g., ':20000')")
 
 	alertInit()
 	ingestInit()
