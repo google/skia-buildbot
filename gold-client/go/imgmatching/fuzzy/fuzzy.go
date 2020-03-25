506e84f0f5d10871ac1a0ffa701a2e11ab44a0bb
Leandro Lovisolo <lovisolo@google.com>
2020-03-25 13:35:09 -0400

    [goldctl] Implement imgmatching.FuzzyMatcher#Match().
    Bug: skia:9527
    Change-Id: If6af6812ff562852c47620ec1aabf356cd01c6f5
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/278319
    Commit-Queue: Leandro Lovisolo <lovisolo@google.com>
    Reviewed-by: Kevin Lubick <kjlubick@google.com>

--- gold-client/go/imgmatching/fuzzy/fuzzy.go
+++ gold-client/go/imgmatching/fuzzy/fuzzy.go
@@ -2,6 +2,7 @@ package fuzzy
 
 import (
 	"image"
+	"image/draw"
 )
 
 // FuzzyMatcher is a non-exact image matching algorithm.
@@ -23,6 +24,51 @@ type FuzzyMatcher struct {
 
 // Match implements the imagmatching.Matcher interface.
 func (m *FuzzyMatcher) Match(expected, actual image.Image) bool {
-	// TODO(lovisolo): Implement.
-	return false
+	// Images must be the same size.
+	if !expected.Bounds().Eq(actual.Bounds()) {
+		return false
+	}
+
+	// Convert both images to NRGBA.
+	bounds := expected.Bounds()
+	expectedNRGBA := image.NewNRGBA(bounds)
+	actualNRGBA := image.NewNRGBA(bounds)
+	draw.Draw(expectedNRGBA, bounds, expected, bounds.Min, draw.Src)
+	draw.Draw(actualNRGBA, bounds, actual, bounds.Min, draw.Src)
+
+	// We'll track the number of different pixels between the two images.
+	numDiffPixels := 0
+
+	// Iterate over all pixels.
+	for x := bounds.Min.X; x <= bounds.Max.X; x++ {
+		for y := bounds.Min.Y; y <= bounds.Max.Y; y++ {
+			p1 := expectedNRGBA.NRGBAAt(x, y)
+			p2 := actualNRGBA.NRGBAAt(x, y)
+
+			// Total number of different pixels must be below the given threshold.
+			if p1 != p2 {
+				numDiffPixels++
+				if numDiffPixels > m.MaxDifferentPixels {
+					return false
+				}
+			}
+
+			// Pixel-wise differences must be below the given threshold.
+			delta := absDiff(p1.R, p2.R) + absDiff(p1.G, p2.G) + absDiff(p1.B, p2.B) + absDiff(p1.A, p2.A)
+			if delta > m.PixelDeltaThreshold {
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
+// absDiff takes two uint8 values m and n, computes |m - n|, and converts the result into an int
+// suitable for addition without the risk of overflowing.
+func absDiff(m, n uint8) int {
+	if m > n {
+		return int(m - n)
+	}
+	return int(n - m)
 }
