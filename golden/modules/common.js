fdfd71a323ccc89b0d31403cbf2e97752a7ab771
Kevin Lubick <kjlubick@google.com>
2020-03-30 08:10:37 -0400

    [gold] Add some helpers and tests.
    These will be used in followup cls
    
    Bug: skia:9525
    Change-Id: Ie7101b40e0bc1611fd96bfa3738106538538e144

--- golden/modules/common.js
+++ golden/modules/common.js
@@ -10,3 +10,98 @@ export function humanReadableQuery(queryStr) {
   }
   return queryStr.split('&').map(decodeURIComponent).join('\n');
 }
+
+/**
+ * Takes a string and trims it to be no longer than maxLength. If the string needs to be trimmed,
+ * an ellipsis (...) will be added as a suffix, but the total string length (with ellipsis) will
+ * stay under maxLength.
+ * @param str {string}
+ * @param maxLength {number}
+ * @return {string}
+ */
+export function truncateWithEllipses(str, maxLength = 15) {
+  if (maxLength < 3) {
+    throw 'maxLength must be greater than the length of the ellipsis.';
+  }
+  if (str.length <= maxLength) {
+    return str;
+  }
+  return `${str.substr(0, maxLength - 3)}...`;
+}
+
+let imagePrefix = '/img/images';
+let diffPrefix = '/img/diffs';
+
+/**
+ * Changes the image endpoints to be based on /dist, which is where they are served when running
+ * the demo pages locally. It should *not* be called by tests, as the effects will persist and make
+ * for interdependent tests.
+ */
+export function setImageEndpointsForDemos() {
+  imagePrefix = '/dist';
+  diffPrefix = '/dist';
+}
+
+/**
+ * Returns a link to the PNG image associated with the given digest.
+ * @param digest {string}
+ * @return {string}
+ */
+export function digestImagePath(digest) {
+  if (!digest) {
+    return '';
+  }
+
+  return `${imagePrefix}/${digest}.png`;
+}
+
+/**
+ * Returns a link to the PNG image associated with the diff between the given digests.
+ * @param d1 {string}
+ * @param d2 {string}
+ * @return {string}
+ */
+export function digestDiffImagePath(d1, d2) {
+  if (!d1 || !d2) {
+    return '';
+  }
+  // We have a canonical diff order where we sort the two digests alphabetically then join them
+  // in order.
+  const order = d1 < d2 ? `${d1}-${d2}` : `${d2}-${d1}`;
+  return `${diffPrefix}/${order}.png`;
+}
+
+/**
+ * Returns a link to the details page for a given test-digest pair.
+ * @param test {string}
+ * @param digest {string}
+ * @param issue {string} Optional, omit or use empty string for master branch.
+ * @return {string}
+ */
+export function detailHref(test, digest, issue = '') {
+  const u = `/detail?test=${test}&digest=${digest}`;
+  if (issue) {
+    return `${u}&issue=${issue}`;
+  }
+  return u;
+}
+
+/**
+ * Returns a link to the diff page for a given pair of digests.
+ * @param grouping {string}
+ * @param left {string}
+ * @param right {string}
+ * @param issue {string} Optional, omit or use empty string for master branch.
+ * @return {string}
+ */
+export function diffPageHref(grouping, left, right, issue = '') {
+  if (!left || !right) {
+    return '';
+  }
+
+  const u = `/diff?test=${grouping}&left=${left}&right=${right}`;
+  if (issue) {
+    return `${u}&issue=${issue}`;
+  }
+  return u;
+}
