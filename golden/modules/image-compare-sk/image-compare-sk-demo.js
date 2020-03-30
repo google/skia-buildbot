333f3d9ebfd2b307cc18a7ea5365a4f363a94585
Kevin Lubick <kjlubick@google.com>
2020-03-30 08:54:47 -0400

    [gold] Add image-compare-sk element
    Known issue: images don't show up in puppeteer tests for
    reasons I do not know.
    
    Bug: skia:9525
    Change-Id: I110a82f83e677453b82697f61f1eede100c909d6

---
+++ golden/modules/image-compare-sk/image-compare-sk-demo.js
@@ -0,0 +1,32 @@
+import './index';
+import { $$ } from '../../../common-sk/modules/dom';
+import { setImageEndpointsForDemos } from '../common';
+
+const aDigest = '6246b773851984c726cb2e1cb13510c2';
+const bDigest = '99c58c7002073346ff55f446d47d6311';
+
+setImageEndpointsForDemos();
+let ele = document.createElement('image-compare-sk');
+ele.left = {
+  digest: aDigest,
+  title: 'a digest title',
+  detail: 'example.com#aDigest',
+};
+ele.right = {
+  digest: bDigest,
+  title: 'the other image',
+  detail: 'example.com#bDigest',
+};
+$$('#normal').appendChild(ele);
+
+ele = document.createElement('image-compare-sk');
+ele.left = {
+  digest: aDigest,
+  title: 'a digest title',
+  detail: 'example.com#aDigest',
+};
+$$('#no_right').appendChild(ele);
+
+document.addEventListener('zoom-clicked', (e) => {
+  $$('#event').textContent = `zoom-clicked: ${JSON.stringify(e.detail)}`;
+});
