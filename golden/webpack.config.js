454fdd5985163a88e63352a35ceef269e7a88a4a
Kevin Lubick <kjlubick@google.com>
2020-03-30 13:01:53 -0400

    [gold] Add the ability to serve demo_assests on dist/ in make serve
    Bug: skia:9525
    Change-Id: I07c63916ba6735a9504697ca65963eb41311a1f7
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/279858
    Reviewed-by: Leandro Lovisolo <lovisolo@google.com>

--- golden/webpack.config.js
+++ golden/webpack.config.js
@@ -1,12 +1,24 @@
 const commonBuilder = require('pulito');
-const { resolve } = require('path')
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { resolve } = require('path');
 
 module.exports = (env, argv) => {
   const config = commonBuilder(env, argv, __dirname);
-  config.output.publicPath='/dist/';
+  config.output.publicPath = '/dist/';
   config.resolve = config.resolve || {};
   // https://github.com/webpack/node-libs-browser/issues/26#issuecomment-267954095
   config.resolve.modules = [resolve(__dirname, 'node_modules'), 'node_modules'];
 
+  if (argv.mode !== 'production') {
+    console.log('serving demo assets');
+    config.plugins.push(
+      new CopyWebpackPlugin([
+        {
+          from: 'demo-page-assets',
+        },
+      ]),
+    );
+  }
+
   return config;
-}
+};
