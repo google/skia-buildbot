2d37eca7d60accf968e2b28caa5142240840336a
Eric Boren <borenet@google.com>
2020-04-01 15:29:59 -0400

    [autoroll] Add Parent, Child, and parent-child RepoManager
    Change-Id: If41f5fe8827ee6c625a6a63f0f6284af15ef5173

---
+++ autoroll/go/repo_manager/child/child.go
@@ -0,0 +1,24 @@
+package child
+
+/*
+   Package child contains implementations of the Child interface.
+*/
+
+import (
+	"context"
+
+	"go.skia.org/infra/autoroll/go/revision"
+)
+
+// Child represents a Child (git repo or otherwise) which can be rolled into a
+// Parent.
+type Child interface {
+	// Update updates the local view of the Child and returns the tip-of-
+	// tree Revision and the list of not-yet-rolled revisions, or any error
+	// which occurred, given the last-rolled revision.
+	Update(context.Context, *revision.Revision) (*revision.Revision, []*revision.Revision, error)
+
+	// GetRevision returns a Revision instance associated with the given
+	// revision ID.
+	GetRevision(context.Context, string) (*revision.Revision, error)
+}
