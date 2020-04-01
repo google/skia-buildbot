1691ecd35c45381fcca5c9f5453990569d3759a4
Kevin Lubick <kjlubick@google.com>
2020-04-01 08:49:07 -0400

    [gold] Follow up on a few puppeteer TODOs
    Change-Id: I88ee0ea9e583d734f3c92b9c10985b6807d65b11
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/280346
    Reviewed-by: Leandro Lovisolo <lovisolo@google.com>

--- golden/puppeteer-tests/docker/run-tests.sh
+++ golden/puppeteer-tests/docker/run-tests.sh
@@ -3,32 +3,32 @@
 # This script is designed to run inside the gold-puppeteer-tests Docker
 # container.
 
+set -x
 # Populate /tests with all the files required to run the Puppeteer tests, which
 # are copied from the buildbot repository checkout.
 mkdir /tests/common-sk
-cp -r /src/common-sk/package.json   /tests/common-sk
+cp -r /src/common-sk/package*       /tests/common-sk
 cp -r /src/common-sk/*.js           /tests/common-sk
 cp -r /src/common-sk/modules        /tests/common-sk
 cp -r /src/common-sk/plugins        /tests/common-sk
 mkdir /tests/infra-sk
-cp -r /src/infra-sk/package.json    /tests/infra-sk
+cp -r /src/infra-sk/package*        /tests/infra-sk
 cp -r /src/infra-sk/*.js            /tests/infra-sk
 cp -r /src/infra-sk/modules         /tests/infra-sk
 mkdir /tests/golden
-cp -r /src/golden/package.json      /tests/golden
+cp -r /src/golden/package*          /tests/golden
 cp -r /src/golden/webpack.config.js /tests/golden
 cp -r /src/golden/modules           /tests/golden
 cp -r /src/golden/puppeteer-tests   /tests/golden
 cp -r /src/golden/demo-page-assets  /tests/golden
 
 # Populate the various node_modules directories.
-# TODO(lovisolo): Use "npm ci" once package-lock.json files are in repo.
 cd /tests/common-sk
-npm install
+npm ci
 cd /tests/infra-sk
-npm install
+npm ci
 cd /tests/golden
-npm install
+npm ci
 
 # Run tests.
 cd /tests/golden/puppeteer-tests
