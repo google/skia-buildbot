4bceb15dd7d654be6ecbdf6a4c04d36cd2daa75b
Kevin Lubick <kjlubick@google.com>
2020-04-01 08:33:54 -0400

    Make all references to CopyWebpackPlugin use absolute paths
    While adding some static assets to Gold's demo pages, I ran into a
    difficult-to-debug problem where the static assets weren't available to
    the puppeteer tests (which re-use the top level webpack.config.js).
    
    The root cause ended up being that the path wasn't absolute, so the
    images weren't being copied correctly. To remedy it, I just made the
    path we pass to CopyWebpackPlugin be absolute, using resolve.
    
    Seeing as we plan to use the puppeteer screenshots more broadly in the
    future, I figured I would change all other uses of CopyWebpackPlugin to
    also use absolute paths to reduce the risk of this biting us in the future.
    
    Change-Id: If7fa9343d8ea27b52f96eece47b30374bdb2777a

--- skottie/webpack.config.js
+++ skottie/webpack.config.js
@@ -1,23 +1,24 @@
 const commonBuilder = require('pulito');
-const CopyWebpackPlugin = require('copy-webpack-plugin')
-const { resolve } = require('path')
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { resolve } = require('path');
 
 module.exports = (env, argv) => {
-  let config = commonBuilder(env, argv, __dirname);
-  config.output.publicPath='/static/';
+  const config = commonBuilder(env, argv, __dirname);
+  config.output.publicPath = '/static/';
   config.plugins.push(
     new CopyWebpackPlugin([{
-      from: './node_modules/jsoneditor/dist/jsoneditor.min.css',
-      to: 'jsoneditor.css'
-    },{
-      from: './node_modules/jsoneditor/dist/img/jsoneditor-icons.svg',
-      to: 'img/jsoneditor-icons.svg'
-    },{ from: 'build/canvaskit/canvaskit.wasm' },
-      { from: 'node_modules/@webcomponents/custom-elements/custom-elements.min.js' }
-    ])
+      from: resolve(__dirname, 'node_modules/jsoneditor/dist/jsoneditor.min.css'),
+      to: 'jsoneditor.css',
+    }, {
+      from: resolve(__dirname, 'node_modules/jsoneditor/dist/img/jsoneditor-icons.svg'),
+      to: 'img/jsoneditor-icons.svg',
+    },
+    { from: resolve(__dirname, 'build/canvaskit/canvaskit.wasm') },
+    { from: resolve(__dirname, 'node_modules/@webcomponents/custom-elements/custom-elements.min.js') },
+    ]),
   );
   config.node = {
-    fs: 'empty'
+    fs: 'empty',
   };
   return config;
-}
+};
