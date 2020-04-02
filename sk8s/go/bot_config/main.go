d302aa4c0a485676146c523da455efe5af371e43
Joe Gregorio <joe@bitworking.org>
2020-04-02 10:05:50 -0400

    [sk8s] Make bot_config.go into an http server that launches swarming.
    Bug: skia:9602
    Change-Id: Iad20b1e5d2c85f252da62c79215c8360b29244ef

--- sk8s/go/bot_config/main.go
+++ sk8s/go/bot_config/main.go
@@ -2,11 +2,40 @@
 package main
 
 import (
+	"context"
+	"flag"
+
 	"go.skia.org/infra/go/common"
-	"go.skia.org/infra/sk8s/go/bot_config/cmd"
+	"go.skia.org/infra/go/sklog"
+	"go.skia.org/infra/sk8s/go/bot_config/server"
+	"go.skia.org/infra/sk8s/go/bot_config/swarming"
+)
+
+// flags
+var (
+	port           = flag.String("port", ":8000", "HTTP service address (e.g., ':8000')")
+	promPort       = flag.String("prom_port", ":20000", "Metrics service address (e.g., ':10110')")
+	pythonExe      = flag.String("python_exe", "/usr/bin/python2.7", "Absolute path to Python.")
+	swarmingBotZip = flag.String("swarming_bot_zip", "/b/s/swarming_bot.zip", "Absolute path to where the swarming_bot.zip code should run from.")
 )
 
 func main() {
-	common.Init()
-	cmd.Execute()
+	common.InitWithMust(
+		"bot_config",
+		common.PrometheusOpt(promPort),
+		common.MetricsLoggingOpt(),
+	)
+
+	sklog.Infof("Starting the server.")
+	server, err := server.NewServer()
+	if err != nil {
+		sklog.Fatal(err)
+	}
+	go func() {
+		sklog.Fatal(server.Start(*port))
+	}()
+
+	sklog.Infof("Starting swarming_bot.")
+	bot := swarming.New(*pythonExe, *swarmingBotZip)
+	sklog.Fatal(bot.Launch(context.Background()))
 }
