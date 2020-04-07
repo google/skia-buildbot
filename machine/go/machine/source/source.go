c11e7a0ec72f3e905b5e3f6450b126a1863ef956
Joe Gregorio <jcgregorio@google.com>
2020-04-07 10:45:25 -0400

    [machine] Initial code structure, README, and interfaces.
    Bug: skia:9602
    Change-Id: Ieec25ed897563b9c8ceedd250e562e4b4ca3898c

---
+++ machine/go/machine/source/source.go
@@ -0,0 +1,16 @@
+package source
+
+import (
+	"context"
+
+	"go.skia.org/infra/machine/go/machine"
+)
+
+// Source provides a channel of machine.MachineUpdateEvents, implemented by using
+// PubSub events sent by each machine.
+//
+// Note that machines should only send updates if state or dimensions has actually changed.
+type Source interface {
+	// Start the process of receiving events.
+	Start(ctx context.Context) (chan<- machine.Event, error)
+}
