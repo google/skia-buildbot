881ac01e34e6399681881d29d26bb1dfa3ef99dc
Ravi Mistry <rmistry@google.com>
2020-04-07 09:43:55 -0400

    Checkpoint for leasing lit-html
    Change-Id: I9c3e50af1637a065dddd29cab6ff04e87c93e034

---
+++ leasing/modules/leasing-list-sk/leasing-list-sk.js
@@ -0,0 +1,76 @@
+/**
+ * @module module/leasing-list-sk
+ * @description <h2><code>leasing-list-sk</code></h2>
+ *
+ * <p>
+ *   Contains the title bar and error-toast for all the leasing server pages.
+ *   The rest of pages should be a child of this element.
+ * </p>
+ *
+ */
+
+import { define } from 'elements-sk/define';
+import { html } from 'lit-html';
+import { ElementSk } from '../../../infra-sk/modules/ElementSk';
+
+import '../leasing-task-sk'
+
+import 'elements-sk/error-toast-sk';
+import 'elements-sk/icon/folder-icon-sk';
+import 'elements-sk/icon/gesture-icon-sk';
+import 'elements-sk/icon/help-icon-sk';
+import 'elements-sk/icon/home-icon-sk';
+import 'elements-sk/icon/star-icon-sk';
+import 'elements-sk/nav-button-sk';
+import 'elements-sk/nav-links-sk';
+import { device, getAKAStr, doImpl } from '../leasing'
+
+import '../../../infra-sk/modules/login-sk';
+
+function displayTasks(ele) {
+  return ele._tasks.map((task) => html`
+    <leasing-task-sk .task=${task}></leasing-task-sk>
+  `);
+}
+
+const template = (ele) => html`${displayTasks(ele)}`;
+
+define('leasing-list-sk', class extends ElementSk {
+  constructor() {
+    super(template);
+    this._tasks = [];
+
+    this._fetchTasks();
+  }
+
+  _fetchTasks() {
+    let url = '/_/get_leases'
+    const details = {
+      'filter_by_user': this.filterByUser,
+    };
+    console.log("CALLING WITH THIS");
+    console.log(details);
+    doImpl(url, details, (json) => {
+      this._tasks = json;
+      this._render();
+    });
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    this._render();
+  }
+
+  /** @prop filter_by_user {String} User tasks should be filtered by. */
+  get filterByUser() {
+    return this.getAttribute('filter_by_user');
+  }
+
+  set filterByUser(val) {
+    this.setAttribute('filter_by_user', val);
+  }
+
+  disconnectedCallback() {
+    super.disconnectedCallback();
+  }
+});
