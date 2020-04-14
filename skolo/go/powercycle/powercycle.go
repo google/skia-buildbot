8f7d7696b12a95f430a8f25ac408f5bedae452f0
Kevin Lubick <kjlubick@google.com>
2020-04-14 15:32:31 -0400

    [power] Rename to Client and introduce DeviceID
    This makes the package align with Go style a bit better. No functional
    changes intended.
    
    Note to the reviewer: start in powercycle.go for the renames to
    interfaces and the new types, then look at the rest in any order.
    
    I did not do much to edgeswitch_client because I have larger plans
    for that (including combining it with edgeswitch_group) and did not
    want to complicate the diff.
    
    Bug: skia:9673
    Change-Id: I25250f21b841939b097a1f93523af48cc7167488

--- skolo/go/powercycle/powercycle.go
+++ skolo/go/powercycle/powercycle.go
@@ -1,132 +1,138 @@
 package powercycle
 
 import (
-	"fmt"
 	"io/ioutil"
-	"os"
 	"sort"
 	"time"
 
 	"github.com/flynn/json5"
-	"go.skia.org/infra/go/util"
+	"go.skia.org/infra/go/skerr"
 )
 
-// DeviceGroup describes a set of devices that can all be
-// controlled together. Any switch or power strip needs to
-// implement this interface.
-type DeviceGroup interface {
-	// DeviceIDs returns a list of strings that uniquely identify
-	// the devices that can be controlled through this group.
-	DeviceIDs() []string
+// DeviceID is a unique identifier for a given machine or attached device.
+type DeviceID string
 
-	// PowerCycle turns the device off for a reasonable amount of time
-	// (i.e. 10 seconds) and then turns it back on. If delayOverride
-	// is larger than zero it overrides the default delay between
-	// turning the port off and on again.
-	PowerCycle(devID string, delayOverride time.Duration) error
+// DeviceIn returns true if the given id is in the slice of DeviceID.
+func DeviceIn(id DeviceID, ids []DeviceID) bool {
+	for _, other := range ids {
+		if other == id {
+			return true
+		}
+	}
+	return false
 }
 
-// Config is the overall structure to aggregate configuration options
-// for different device types.
-type Config struct {
+func sortIDs(ids []DeviceID) {
+	sort.Slice(ids, func(i, j int) bool {
+		return ids[i] < ids[j]
+	})
+}
+
+// Client abstracts a set of devices that can all be controlled together.
+type Client interface {
+	// DeviceIDs returns a list of strings that uniquely identify the devices that can be controlled
+	// through this group.
+	DeviceIDs() []DeviceID
+
+	// PowerCycle turns the device off for a reasonable amount of time (i.e. 10 seconds) and then
+	// turns it back on. If delayOverride is larger than zero it overrides the default delay between
+	// turning the port off and on again.
+	PowerCycle(id DeviceID, delayOverride time.Duration) error
+}
+
+// config is the overall structure to aggregate configuration options for different device types.
+type config struct {
 	// MPower aggregates all mPower configurations.
-	MPower map[string]*MPowerConfig `json:"mpower"`
+	MPower map[string]*mPowerConfig `json:"mpower"`
 
 	// EdgeSwitch aggregates all EdgeSwitch configurations.
-	EdgeSwitch map[string]*EdgeSwitchConfig `json:"edgeswitch"`
+	EdgeSwitch map[string]*edgeSwitchConfig `json:"edgeswitch"`
 }
 
-// aggregatedDevGroup implements the DeviceGroup interface and allows
-// to combine multiple device groups into one.
-type aggregatedDevGroup struct {
-	idDevGroupMap map[string]DeviceGroup // Maps from a deviceID to a device group.
+// aggregatedClients allows us to combine multiple Client implementations into one.
+type aggregatedClients struct {
+	clientForID map[DeviceID]Client // Maps from a deviceID to a device group.
 }
 
 // add adds a new device group.
-func (a *aggregatedDevGroup) add(devGroup DeviceGroup) error {
-	for _, id := range devGroup.DeviceIDs() {
-		if _, ok := a.idDevGroupMap[id]; ok {
-			return fmt.Errorf("Device '%s' already exists.", id)
+func (a *aggregatedClients) add(client Client) error {
+	for _, id := range client.DeviceIDs() {
+		if _, ok := a.clientForID[id]; ok {
+			return skerr.Fmt("Device '%s' already exists.", id)
 		}
-		a.idDevGroupMap[id] = devGroup
+		a.clientForID[id] = client
 	}
 	return nil
 }
 
-// DeviceIDs, see the DeviceGroup interface.
-func (a *aggregatedDevGroup) DeviceIDs() []string {
-	ret := make([]string, 0, len(a.idDevGroupMap))
-	for id := range a.idDevGroupMap {
+// DeviceIDs implements the Client interface.
+func (a *aggregatedClients) DeviceIDs() []DeviceID {
+	ret := make([]DeviceID, 0, len(a.clientForID))
+	for id := range a.clientForID {
 		ret = append(ret, id)
 	}
-	sort.Strings(ret)
+	sortIDs(ret)
 	return ret
 }
 
-// PowerCycle, see the DeviceGroup interface.
-func (a *aggregatedDevGroup) PowerCycle(devID string, delayOverride time.Duration) error {
-	dev, ok := a.idDevGroupMap[devID]
+// PowerCycle implements the Client interface.
+func (a *aggregatedClients) PowerCycle(id DeviceID, delayOverride time.Duration) error {
+	client, ok := a.clientForID[id]
 	if !ok {
-		return fmt.Errorf("Unknown device id: %s", devID)
+		return skerr.Fmt("Unknown device id: %s", id)
 	}
-	return dev.PowerCycle(devID, delayOverride)
+	return client.PowerCycle(id, delayOverride)
 }
 
-// DeviceGroupFromJson5File parses a Json5 file and instantiates the
-// defined devices.
-func DeviceGroupFromJson5File(path string, connect bool) (DeviceGroup, error) {
+// ParseJSON5 parses a JSON5 file and instantiates the defined devices. If connect is true, an
+// attempt will be made to connect to the subclients an errors will be returned if they are not
+// accessible.
+func ParseJSON5(path string, connect bool) (Client, error) {
 	conf, err := readConfig(path)
 	if err != nil {
-		return nil, err
+		return nil, skerr.Wrap(err)
 	}
 
-	ret := &aggregatedDevGroup{
-		idDevGroupMap: map[string]DeviceGroup{},
+	ret := &aggregatedClients{
+		clientForID: map[DeviceID]Client{},
 	}
 
 	// Add the mpower devices.
 	for _, c := range conf.MPower {
-		mp, err := NewMPowerClient(c, connect)
+		mp, err := newMPowerClient(c, connect)
 		if err != nil {
-			return nil, err
+			return nil, skerr.Wrap(err)
 		}
 
 		if err := ret.add(mp); err != nil {
-			return nil, err
+			return nil, skerr.Wrap(err)
 		}
 	}
 
 	// Add the EdgeSwitch devices.
 	for _, c := range conf.EdgeSwitch {
-		es, err := NewEdgeSwitchDevGroup(c, connect)
+		es, err := newEdgeSwitchClient(c, connect)
 		if err != nil {
-			return nil, err
+			return nil, skerr.Wrap(err)
 		}
 
 		if err := ret.add(es); err != nil {
-			return nil, err
+			return nil, skerr.Wrap(err)
 		}
 	}
 
 	return ret, nil
 }
 
-func readConfig(path string) (*Config, error) {
-	f, err := os.Open(path)
+func readConfig(path string) (config, error) {
+	conf := config{}
+	jsonBytes, err := ioutil.ReadFile(path)
 	if err != nil {
-		return nil, err
-	}
-	defer util.Close(f)
-
-	jsonBytes, err := ioutil.ReadAll(f)
-	if err != nil {
-		return nil, err
+		return conf, skerr.Wrapf(err, "reading %s", path)
 	}
 
-	conf := &Config{}
-	if err := json5.Unmarshal(jsonBytes, conf); err != nil {
-		return nil, err
+	if err := json5.Unmarshal(jsonBytes, &conf); err != nil {
+		return conf, skerr.Wrapf(err, "reading JSON5 from %s", path)
 	}
-
 	return conf, nil
 }
