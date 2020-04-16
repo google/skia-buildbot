16837e0d3c7f4b041dbcc1d5d51b5e738748000a
Kevin Lubick <kjlubick@google.com>
2020-04-16 16:04:12 -0400

    [powercycle] introduce new impl using native ssh.
    This is not suceptible to golang's lack of support for old
    SSH algorithms by using a native ssh binary. On the downside,
    it's not necessarily portable to Windows. On the upside, we have
    no plans of using Windows for jumphosts.
    
    Suggested review order:
     - read go/skolo-powercycle-setup; particularly the sections on
       setting up. Leave feedback there if anything is unclear.
     - Look at command_runner.go
     - Look at mpower.go for usage of CommandRunner.
     - Look at edgeswitch.go
     - Any order for the rest.
    
    Follow up steps (in no particular order):
     - add tests for mpower.go and edgeswitch.go
     - add tests for CommandRunner (might need a different exec
       helper for mocking and standard in)
     - update censustaker to use arp and flesh out the
       powercycle-rack-01 config.
    
    This code was manually tested on rack 01 and rack 03 for the
    edgeswitch code and the mpower code respectively.
    
    Bug: skia:9673
    Change-Id: Ia1fc24df65823b32e930d503acea77e5c7b41a80

---
+++ skolo/go/powercycle/command_runner.go
@@ -0,0 +1,86 @@
+package powercycle
+
+import (
+	"context"
+	"io"
+	"os/exec"
+	"strings"
+
+	"go.skia.org/infra/go/skerr"
+)
+
+// The CommandRunner interface adds a layer of abstraction around sending commands to powercycle
+// Controllers. It is not meant to be a general purpose interface or a robust implementation beyond
+// exactly that use.
+type CommandRunner interface {
+	// ExecCmds executes a series of commands and returns the accumulated output of all commands.
+	// If one command fails, an error is returned, but no other guarantees are made.
+	ExecCmds(ctx context.Context, cmds ...string) (string, error)
+}
+
+// standardInRunner implements the CommandRunner interface by sending commands through standard in
+// to the given executable running with the given args.
+type standardInRunner struct {
+	executable string
+	args       []string
+}
+
+// PublicKeySSHCommandRunner returns a CommandRunner that will operate over a native ssh binary
+// with the following arguments. One of the provided arguments should be the user/ip address.
+// It presumes that the target is configured to authenticate via a shared public key (e.g. in
+// .ssh/authorized_keys), as it does not expect or support ssh prompting for a password.
+func PublicKeySSHCommandRunner(sshArgs ...string) *standardInRunner {
+	return &standardInRunner{
+		executable: "ssh",
+		args:       sshArgs,
+	}
+}
+
+// PasswordSSHCommandRunner returns a CommandRunner that will operate over a native ssh binary
+// with the following arguments. One of the provided arguments should be the user/ip address.
+// It passes the password into ssh via sshpass. See
+// http://manpages.ubuntu.com/manpages/trusty/man1/sshpass.1.html for more details on why sshpass
+// is needed to give the password to ssh.
+// Note: ssh is known to return errors even when the command executed normally. To work around
+// this, ignore the error returned by ExecCmds and look at the standard out.
+func PasswordSSHCommandRunner(password string, sshArgs ...string) *standardInRunner {
+	args := append([]string{"-p", password, "ssh"}, sshArgs...)
+	return &standardInRunner{
+		executable: "sshpass",
+		args:       args,
+	}
+}
+
+// ExecCmds implements the CommandRunner interface. It makes a connection to the target and then
+// feeds the commands into standard in joined by newlines. It returns any output it receives and
+// any errors.
+func (s *standardInRunner) ExecCmds(ctx context.Context, cmds ...string) (string, error) {
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+	cmd := exec.CommandContext(ctx, s.executable, s.args...)
+	stdin, err := cmd.StdinPipe()
+	if err != nil {
+		return "", skerr.Wrapf(err, "getting stdin pipe")
+	}
+
+	// Commands sent via standard in are executed after a newline is seen.
+	cmdStr := strings.Join(cmds, "\n") + "\n"
+	if _, err := io.WriteString(stdin, cmdStr); err != nil {
+		return "", skerr.Wrapf(err, "sending command %q to stdin", cmdStr)
+	}
+
+	// SSH will keep running until stdin is closed, so we need to close it before we start, otherwise
+	// CombinedOutput will block forever.
+	if err := stdin.Close(); err != nil {
+		return "", skerr.Wrapf(err, "closing stdin pipe")
+	}
+
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		// out could have valid input if err is non-nil, e.g. why it crashed.
+		return string(out), skerr.Wrapf(err, "running %q", cmds)
+	}
+	return string(out), nil
+}
+
+var _ CommandRunner = (*standardInRunner)(nil)
