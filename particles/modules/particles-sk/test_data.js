551ec155e25307afe7aa0c537936e5f73bd02978
Brian Osman <brianosman@google.com>
2020-04-21 10:03:43 -0400

    Fix clear color and test data for particles.skia.org
    Change-Id: I294a2cfaf0516a49493b1e8902a66f7648b9c37e

--- particles/modules/particles-sk/test_data.js
+++ particles/modules/particles-sk/test_data.js
@@ -4,8 +4,8 @@ export const spiral = {
     "uniform float slider_speed;",
     "uniform float slider_lifetime;",
     "void spawn(inout Particle p) {",
-    "  p.lifetime = mix(2, 5, slider_lifetime+rand);",
-    "  p.vel = p.dir * mix(40, 60, rand) * mix(0.5, 3, slider_speed);",
+    "  p.lifetime = mix(2, 5, slider_lifetime+rand(p.seed));",
+    "  p.vel = p.dir * mix(40, 60, rand(p.seed)) * mix(0.5, 3, slider_speed);",
     "}",
     "",
     "void update(inout Particle p) {",
@@ -14,7 +14,7 @@ export const spiral = {
     "  float3 a1 = float3(0.525, 0.886, 0.980);",
     "  float3 b0 = float3(0.376, 0.121, 0.705);",
     "  float3 b1 = float3(0.933, 0.227, 0.953);",
-    "  p.color.rgb = mix(mix(a0, a1, p.age), mix(b0, b1, p.age), rand);",
+    "  p.color.rgb = mix(mix(a0, a1, p.age), mix(b0, b1, p.age), rand(p.seed));",
     "}"
 ],
   "Drawable": {
