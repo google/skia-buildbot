01ec576e044e8d5aac39cf8de876824eafba8a97
Joe Gregorio <jcgregorio@google.com>
2020-04-22 16:05:26 -0400

    [machine] Add thermal monitoring to machine.
    Change-Id: I9e38508a873945caa3fec5cebc14502b5e7951fd

--- machine/go/machine/processor/impl_test.go
+++ machine/go/machine/processor/impl_test.go
@@ -368,6 +368,72 @@ func TestProcess_QuarantineIfDeviceBatteryTooLow(t *testing.T) {
 
 }
 
+func TestProcess_QuarantineIfDeviceTooHot(t *testing.T) {
+	unittest.SmallTest(t)
+	ctx := context.Background()
+
+	previous := machine.NewDescription()
+	event := machine.Event{
+		EventType: machine.EventTypeRawState,
+		Host: machine.Host{
+			Name: "skia-rpi2-0001",
+		},
+		Android: machine.Android{
+			DumpsysThermalService: `IsStatusOverride: false
+ThermalEventListeners:
+	callbacks: 3
+	killed: false
+	broadcasts count: -1
+ThermalStatusListeners:
+	callbacks: 1
+	killed: false
+	broadcasts count: -1
+Thermal Status: 0
+Cached temperatures:
+	Temperature{mValue=32.401, mType=3, mName=mb-therm-monitor, mStatus=0}
+	Temperature{mValue=46.100002, mType=0, mName=cpu0-silver-usr, mStatus=0}
+	Temperature{mValue=44.800003, mType=0, mName=cpu1-silver-usr, mStatus=0}
+	Temperature{mValue=45.100002, mType=0, mName=cpu2-silver-usr, mStatus=0}
+	Temperature{mValue=40.600002, mType=1, mName=gpu0-usr, mStatus=0}
+	Temperature{mValue=40.300003, mType=1, mName=gpu1-usr, mStatus=0}
+	Temperature{mValue=44.100002, mType=0, mName=cpu3-silver-usr, mStatus=0}
+	Temperature{mValue=45.4, mType=0, mName=cpu4-silver-usr, mStatus=0}
+	Temperature{mValue=45.4, mType=0, mName=cpu5-silver-usr, mStatus=0}
+	Temperature{mValue=30.000002, mType=2, mName=battery, mStatus=0}
+	Temperature{mValue=48.300003, mType=0, mName=cpu1-gold-usr, mStatus=0}
+	Temperature{mValue=46.7, mType=0, mName=cpu0-gold-usr, mStatus=0}
+	Temperature{mValue=27.522001, mType=4, mName=usbc-therm-monitor, mStatus=0}
+HAL Ready: true
+HAL connection:
+	ThermalHAL 2.0 connected: yes
+Current temperatures from HAL:
+	Temperature{mValue=28.000002, mType=2, mName=battery, mStatus=0}
+	Temperature{mValue=33.800003, mType=0, mName=cpu0-gold-usr, mStatus=0}
+	Temperature{mValue=33.800003, mType=0, mName=cpu0-silver-usr, mStatus=0}
+	Temperature{mValue=33.5, mType=0, mName=cpu1-gold-usr, mStatus=0}
+	Temperature{mValue=44.1, mType=0, mName=cpu1-silver-usr, mStatus=0}
+	Temperature{mValue=43.8, mType=0, mName=cpu2-silver-usr, mStatus=0}
+	Temperature{mValue=33.5, mType=0, mName=cpu3-silver-usr, mStatus=0}
+	Temperature{mValue=33.5, mType=0, mName=cpu4-silver-usr, mStatus=0}
+	Temperature{mValue=33.5, mType=0, mName=cpu5-silver-usr, mStatus=0}
+	Temperature{mValue=32.9, mType=1, mName=gpu0-usr, mStatus=0}
+	Temperature{mValue=32.9, mType=1, mName=gpu1-usr, mStatus=0}
+	Temperature{mValue=30.147001, mType=3, mName=mb-therm-monitor, mStatus=0}
+	Temperature{mValue=26.926, mType=4, mName=usbc-therm-monitor, mStatus=0}
+Current cooling devices from HAL:
+	CoolingDevice{mValue=0, mType=1, mName=battery}
+	CoolingDevice{mValue=0, mType=2, mName=thermal-cpufreq-0}
+	CoolingDevice{mValue=0, mType=2, mName=thermal-cpufreq-6}
+	CoolingDevice{mValue=0, mType=3, mName=thermal-devfreq-0}`,
+		},
+	}
+
+	p := newProcessorForTest(t)
+	next := p.Process(ctx, previous, event)
+	assert.Equal(t, "Temperature is too hot: 44.1 > 35 (C)", next.Dimensions[machine.DimQuarantined][0])
+	assert.Equal(t, float64(44.1), metrics2.GetFloat64Metric("machine_processor_device_temperature_c", map[string]string{"machine": "skia-rpi2-0001", "sensor": "cpu1-silver-usr"}).Get())
+
+}
 func TestProcess_DoNotQuarantineIfDeviceBatteryIsChargedEnough(t *testing.T) {
 	unittest.SmallTest(t)
 	ctx := context.Background()
@@ -460,4 +526,82 @@ func TestBatteryFromAndroidDumpSys_FailOnBadScale(t *testing.T) {
   scale: 0
   `)
 	assert.False(t, ok)
+}
+
+func TestTemperatureFromAndroid_FindTempInThermalServiceOutput(t *testing.T) {
+	unittest.SmallTest(t)
+	thermalServiceOutput := `IsStatusOverride: false
+ThermalEventListeners:
+	callbacks: 1
+	killed: false
+	broadcasts count: -1
+ThermalStatusListeners:
+	callbacks: 1
+	killed: false
+	broadcasts count: -1
+Thermal Status: 0
+Cached temperatures:
+ Temperature{mValue=-99.9, mType=6, mName=TYPE_POWER_AMPLIFIER, mStatus=0}
+	Temperature{mValue=25.3, mType=4, mName=TYPE_SKIN, mStatus=0}
+	Temperature{mValue=24.0, mType=1, mName=TYPE_CPU, mStatus=0}
+	Temperature{mValue=24.4, mType=3, mName=TYPE_BATTERY, mStatus=0}
+	Temperature{mValue=24.2, mType=5, mName=TYPE_USB_PORT, mStatus=0}
+HAL Ready: true
+HAL connection:
+	Sdhms connected: yes
+Current temperatures from HAL:
+	Temperature{mValue=24.0, mType=1, mName=TYPE_CPU, mStatus=0}
+	Temperature{mValue=24.4, mType=3, mName=TYPE_BATTERY, mStatus=0}
+	Temperature{mValue=25.3, mType=4, mName=TYPE_SKIN, mStatus=0}
+	Temperature{mValue=24.2, mType=5, mName=TYPE_USB_PORT, mStatus=0}
+	Temperature{mValue=-99.9, mType=6, mName=TYPE_POWER_AMPLIFIER, mStatus=0}
+Current cooling devices from HAL:
+	CoolingDevice{mValue=0, mType=2, mName=TYPE_CPU}
+	CoolingDevice{mValue=0, mType=3, mName=TYPE_GPU}
+	CoolingDevice{mValue=0, mType=1, mName=TYPE_BATTERY}
+	CoolingDevice{mValue=1, mType=4, mName=TYPE_MODEM}`
+	a := machine.Android{
+		DumpsysThermalService: thermalServiceOutput,
+	}
+	temp, ok := temperatureFromAndroid(a)
+	assert.True(t, ok)
+	assert.Equal(t, map[string]float64{
+		"TYPE_CPU":      24.0,
+		"TYPE_BATTERY":  24.4,
+		"TYPE_SKIN":     25.3,
+		"TYPE_USB_PORT": 24.2,
+	}, temp)
+}
+
+func TestTemperatureFromAndroid_ReturnFalseIfNoOutputFromThermalOrBatteryService(t *testing.T) {
+	unittest.SmallTest(t)
+	a := machine.Android{}
+	_, ok := temperatureFromAndroid(a)
+	assert.False(t, ok)
+}
+
+func TestTemperatureFromAndroid_FindTempInBatteryServiceOutput(t *testing.T) {
+	unittest.SmallTest(t)
+	batteryOutput := `Current Battery Service state:
+	AC powered: true
+	USB powered: false
+	Wireless powered: false
+	Max charging current: 1500000
+	Max charging voltage: 5000000
+	Charge counter: 2448973
+	status: 2
+	health: 2
+	present: true
+	level: 94
+	scale: 100
+	voltage: 4248
+	temperature: 281
+	technology: Li-ion
+  `
+	a := machine.Android{
+		DumpsysBattery: batteryOutput,
+	}
+	temp, ok := temperatureFromAndroid(a)
+	assert.True(t, ok)
+	assert.Equal(t, map[string]float64{batteryTemperatureKey: 28.1}, temp)
 }
