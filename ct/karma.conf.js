41986f9511584380ea61a9587d972d8e24d1ebe9
Leandro Lovisolo <lovisolo@google.com>
2020-04-24 15:45:38 -0400

    [ct] Add Puppeteer test for task-queue-sk.
    Bug: skia:10167
    Change-Id: I8d59950048229d6f453ba20cf6ad3ca5edb37df0

--- ct/karma.conf.js
+++ ct/karma.conf.js
@@ -1,1 +1,11 @@
-module.exports = require('../common-sk/karma.common.js')(__dirname);
+// TODO(lovisolo): Move the exclude list below to common-sk.
+module.exports = function(config) {
+  commonJsFn = module.exports = require('common-sk/karma.common.js')(__dirname);
+  commonJsFn(config);
+  config.set({
+    exclude: [
+      'modules/*_puppeteer_test.js',
+      'modules/**/*_puppeteer_test.js',
+    ]
+  });
+};
