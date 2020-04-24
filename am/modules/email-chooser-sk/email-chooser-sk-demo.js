b147405459bc3cfe3a7e4203ffd0413caaac8d54
Leandro Lovisolo <lovisolo@google.com>
2020-04-24 15:18:48 -0400

    [am] email-chooser-sk: Add demo page.
    Bug: skia:10167
    Change-Id: Icf01f3b8c1134607b0a04c923fca754232a06305
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/285300
    Commit-Queue: Leandro Lovisolo <lovisolo@google.com>
    Reviewed-by: Kevin Lubick <kjlubick@google.com>

---
+++ am/modules/email-chooser-sk/email-chooser-sk-demo.js
@@ -0,0 +1,7 @@
+import './index';
+
+window.addEventListener('load', () => {
+  document.getElementById('chooser').open(
+      ["alice@example.com", "bob@example.com", "claire@example.com"],
+      "bob@example.com");
+});
