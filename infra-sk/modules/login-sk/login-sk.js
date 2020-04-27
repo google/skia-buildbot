11b8c151e88c0608228c056f1407b6f3e4ec0649
Ravi Mistry <rmistry@google.com>
2020-04-27 13:22:29 -0400

    Make login-sk take in a login_host.
    I found that login-sk does not work in corp.goog domains because when it
    hits skia.org/loginstatus it gets empty strings for Emails.
    For example, you can see the network calls in https://fuchsia-gold.corp.goog/
    to skia.org/loginstatus. Login appears to be broken in fuchsia-gold.corp.goog because of this.
    
    But this flow does work when you call loginstatus under the same host. For
    example you can see the network calls in https://skia-status.corp.goog/
    to https://skia-status.corp.goog/loginstatus
    
    It was easy to directly set the email in the old polymer login-sk
    element. This change makes it possible to set the login_host to use in
    the lit-html login-sk element.
    
    Also, use in skia-android-compile.corp.goog
    
    Bug: skia:10157
    Change-Id: Ia724f990eff5b3ca609821d3b911f66dfefb4e24

--- infra-sk/modules/login-sk/login-sk.js
+++ infra-sk/modules/login-sk/login-sk.js
@@ -10,30 +10,33 @@
  *
  * @attr {boolean} testing_offline - If we should really poll for Login status
  *  or use mock data.
+ * @attr {string} login_host - If it needs to be something other than skia.org.
+ *  If login_host is not specified then skia.org will be used.
  *
  * <p>
  */
 import { define } from 'elements-sk/define'
 import { errorMessage } from 'elements-sk/errorMessage';
-import { Login } from '../login';
+import { LoginTo } from '../login';
 
 define('login-sk', class extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `<span class=email>Loading...</span><a class=logInOut></a>`;
+    const host = this.loginHost ? this.loginHost : 'skia.org';
     if (this.testingOffline) {
       this.querySelector('.email').textContent = "test@example.com";
       const logInOut = this.querySelector('.logInOut');
-      logInOut.href = 'https://skia.org/logout/?redirect=' + encodeURIComponent(document.location);
+      logInOut.href = `https://${host}/logout/?redirect=` + encodeURIComponent(document.location);
       logInOut.textContent = 'Logout';
     } else {
-      Login.then((status) => {
+      LoginTo(`https://${host}/loginstatus/`).then((status) => {
         this.querySelector('.email').textContent = status.Email;
         let logInOut = this.querySelector('.logInOut');
         if (!status.Email) {
             logInOut.href = status.LoginURL;
             logInOut.textContent = 'Login';
         } else {
-            logInOut.href = 'https://skia.org/logout/?redirect=' + encodeURIComponent(document.location);
+            logInOut.href = `https://${host}/logout/?redirect=` + encodeURIComponent(document.location);
             logInOut.textContent = 'Logout';
         }
       }).catch(errorMessage);
@@ -48,6 +51,15 @@ define('login-sk', class extends HTMLElement {
       this.setAttribute('testing_offline', '');
     } else {
       this.removeAttribute('testing_offline');
+    }
+  }
+
+  /** @prop loginHost {string} Which host should be used for login URLs.
+   */
+  get loginHost() { return this.getAttribute('login_host'); }
+  set loginHost(val) {
+    if (val) {
+      this.setAttribute('login_host', val);
     }
   }
 });
