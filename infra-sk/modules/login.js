ee504c19b426296f2aba084e425d6f0ec985bac4
Ravi Mistry <rmistry@google.com>
2020-04-27 13:23:52 -0400

    Make login-sk take in a login_host.
    I found that login-sk does not work in corp.goog domains because when it
    hits skia.org/loginstatus it gets empty strings for Emails.
    For example, you can see the network calls in https://fuchsia-gold.corp.goog/
    to skia.org/loginstatus. Login appears to be broken in fuchsia-gold.corp.goog because of this.
    
    But this flow does work when you call loginstatus under the same host. For
    example you can see the network calls in https://skia-status.corp.goog/
    to https://skia-status.corp.goog/loginstatus
    
    It was easy to directly set the email in the old polymer login-sk
    element. This change makes it possible to set the login_host to use in
    the lit-html login-sk element.
    
    Also, use in skia-android-compile.corp.goog
    
    Bug: skia:10157
    Change-Id: Ia724f990eff5b3ca609821d3b911f66dfefb4e24
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/285219
    Commit-Queue: Ravi Mistry <rmistry@google.com>
    Reviewed-by: Joe Gregorio <jcgregorio@google.com>

--- infra-sk/modules/login.js
+++ infra-sk/modules/login.js
@@ -22,6 +22,31 @@ export var Login = fetch('https://skia.org/loginstatus/', {
   throw new Error('Problem reading /loginstatus/:' + res.statusText);
 });
 
+/**
+ * A function that returns a Promise that will be resolved with the users
+ * current login status by accessing the specified loginStatusURL.
+ *
+ * The resolution object looks like:
+ * <pre>
+ *   {
+ *     "Email": "fred@example.com",
+ *     "LoginURL": "https://..."
+ *   }
+ * </pre>
+ *
+ * The Email will be the empty string if the user is not logged in.
+ */
+export var LoginTo = function(loginStatusURL) {
+  return fetch(loginStatusURL, {
+    credentials: 'include',
+  }).then(res => {
+    if (res.ok) {
+      return res.json()
+    }
+    throw new Error('Problem reading /loginstatus/:' + res.statusText);
+  });
+};
+
 // Add to the global sk namespace while we migrate away from Polymer.
 if (window.sk !== undefined) {
   window.sk.Login = Login;
