ac1ce59cd7159f165fae227b02c147085a3c05f7
Ravi Mistry <rmistry@google.com>
2020-04-29 10:20:16 -0400

    [CT] Convert all (worker+master) CT swarming tasks to use task-based auth
    Highlights:
    * Need to use absolute paths now in GetPathToPyFiles and GetPathToIsolates. Probably because of using luci-auth as main command in isolates.
    
    Bug: skia:10168
    Change-Id: Ia6999da2aab7ac277014112e6f7f8591f12581fa

--- ct/go/worker_scripts/isolate_telemetry/main.go
+++ ct/go/worker_scripts/isolate_telemetry/main.go
@@ -31,7 +31,10 @@ var (
 
 func buildRepo() error {
 	ctx := context.Background()
-	worker_common.Init(ctx, true /* useDepotTools */)
+	httpClient, err := worker_common.Init(ctx, true /* useDepotTools */)
+	if err != nil {
+		return skerr.Wrap(err)
+	}
 	defer util.TimeTrack(time.Now(), "Isolating Telemetry")
 	defer sklog.Flush()
 
@@ -50,7 +53,7 @@ func buildRepo() error {
 	}
 
 	// Instantiate GcsUtil object.
-	gs, err := util.NewGcsUtil(nil)
+	gs, err := util.NewGcsUtil(httpClient)
 	if err != nil {
 		return err
 	}
@@ -67,7 +70,10 @@ func buildRepo() error {
 			}
 		}
 	}
-	pathToPyFiles := util.GetPathToPyFiles(*worker_common.Local)
+	pathToPyFiles, err := util.GetPathToPyFiles(*worker_common.Local)
+	if err != nil {
+		return fmt.Errorf("Could not get path to py files: %s", err)
+	}
 	if err = util.CreateTelemetryIsolates(ctx, *runID, *chromiumHash, pathToPyFiles, gitExec, applyPatches); err != nil {
 		return fmt.Errorf("Could not create telemetry isolates: %s", err)
 	}
