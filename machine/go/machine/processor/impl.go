769e4cebcde6308621f15755c037dd98b343b285
Joe Gregorio <jcgregorio@google.com>
2020-04-29 22:13:41 +0000

    Revert "[machine] Carry over the Galaxy S7 device_os capture."
    This reverts commit 69fb49369d0ae35f0e4dd529d0fa7a8bfe4b3b4c.
    
    Reason for revert: Breaks the bots, the jobs.json logic is too rigid.
    
    Original change's description:
    > [machine] Carry over the Galaxy S7 device_os capture.
    > 
    > Change-Id: I42935d89ca070e77c20e247d47d26536abf2fc9c
    > Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/286217
    > Reviewed-by: Joe Gregorio <jcgregorio@google.com>
    > Commit-Queue: Joe Gregorio <jcgregorio@google.com>
    
    TBR=jcgregorio@google.com,kjlubick@google.com
    
    Change-Id: Id142a3dfd5f52b938f871f6cdaa5e92ff22897b2
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true

--- machine/go/machine/processor/impl.go
+++ machine/go/machine/processor/impl.go
@@ -220,18 +220,10 @@ func dimensionsFromAndroidProperties(prop map[string]string) map[string][]string
 		}
 	}
 
-	// Devices such as the Galaxy S7 update build w/o updating android,
-	// so we merge them together to catch subtle updates.
-	incremental := prop["ro.build.version.incremental"]
-	if incremental != "" {
-		ret["device_os"] = append(ret["device_os"], fmt.Sprintf("%s_%s", ret["device_os"][0], incremental))
-	}
-
 	// Add the first character of each device_os to the dimension.
 	osList := append([]string{}, ret["device_os"]...)
 	for _, os := range ret["device_os"] {
-		firstChar := os[:1]
-		if firstChar != "" && strings.ToUpper(firstChar) == firstChar && !util.In(firstChar, osList) {
+		if os[:1] != "" && strings.ToUpper(os[:1]) == os[:1] {
 			osList = append(osList, os[:1])
 		}
 	}
