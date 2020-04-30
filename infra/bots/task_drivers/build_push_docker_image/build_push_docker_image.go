3523a2bbc6972c23b81949d1ef694446b4fc0376
Ravi Mistry <rmistry@google.com>
2020-04-30 12:39:27 -0400

    Testing
    Change-Id: If1c83a7354981631fbc3de06d1d5a541d434b766

--- infra/bots/task_drivers/build_push_docker_image/build_push_docker_image.go
+++ infra/bots/task_drivers/build_push_docker_image/build_push_docker_image.go
@@ -107,6 +107,12 @@ func main() {
 		}
 	}()
 
+	buildArgs := map[string]string{}
+	if rs.Issue != "" && rs.Patchset != "" {
+		buildArgs["PATCHSET_REF"] = fmt.Sprintf("refs/changes/%s/%s/%s", rs.Issue[len(rs.Issue)-2:], rs.Issue, rs.Patchset)
+	} else {
+		buildArgs["HASH"] = rs.Revision
+	}
 	// Retry docker commands if there are errors. Sometimes the access token expires between the
 	// login and the push.
 	NUM_ATTEMPTS := 2
@@ -129,7 +135,7 @@ func main() {
 		}
 
 		// Build docker image.
-		if buildErr := docker.BuildHelper(ctx, filepath.Join(co.Dir(), *dockerfileDir), imageWithTag, configDir, nil); buildErr != nil {
+		if buildErr := docker.BuildHelper(ctx, filepath.Join(co.Dir(), *dockerfileDir), imageWithTag, configDir, buildArgs); buildErr != nil {
 			dockerErr = buildErr
 			continue
 		}
