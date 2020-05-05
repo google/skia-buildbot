79205b5715a1014577128affe26d56156d569006
Joe Gregorio <jcgregorio@google.com>
2020-05-05 14:10:50 -0400

    [sk8s] Vastly simplify metadata server.
    Change-Id: Iab88524f19f0f3a97bff22506e2197d7719b5450

--- sk8s/go/metadata/main.go
+++ sk8s/go/metadata/main.go
@@ -1,99 +1,135 @@
 package main
 
-// This program emulates the meta data server in GCE and is intended to be used in the Skolo.
-// It is derived from infra/skolo/go/metadata_server and designed to run in Kubernetes (but that
-// is not strictly necessary).
-// It also incorporates the functionality of skolo/go/get_oauth2_token, uniting the fetching
-// and serving the oauth tokens in a single process.
+// This program emulates the meta data server in GCE and is intended to be used
+// in the Skolo just for serving tokens to swarming clients. Note that the
+// serving path is hard-coded to
+// /computeMetadata/v1/instance/service-accounts/default/token.
+//
+// Note that the token comes from using Google default credentials, so the
+// GOOGLE_APPLICATION_CREDENTIALS environment variable must be set.
 
 import (
-	"context"
+	"encoding/json"
 	"flag"
-	"fmt"
 	"net/http"
+	"sync"
+	"time"
 
 	"github.com/gorilla/mux"
+	"go.skia.org/infra/go/auth"
 	"go.skia.org/infra/go/common"
 	"go.skia.org/infra/go/httputils"
+	"go.skia.org/infra/go/metrics2"
+	"go.skia.org/infra/go/skerr"
 	"go.skia.org/infra/go/sklog"
-	"go.skia.org/infra/skolo/go/skmetadata"
+	"golang.org/x/oauth2"
 )
 
-const (
-	// APP_NAME is the name of this app.
-	APP_NAME = "metadata"
+// refreshInterval is how often we should refresh the token.
+var refreshInterval = 10 * time.Minute
+
+// Flags.
+var (
+	local    = flag.Bool("local", false, "True if running locally, not in prod.")
+	port     = flag.String("port", ":8000", "HTTP service port for the web server (e.g., ':8000')")
+	promPort = flag.String("prom_port", ":20000", "Metrics service address (e.g., ':10110')")
 )
 
-type instanceMetadataMap map[string]map[string]string
+type server struct {
+	tokenSource       oauth2.TokenSource
+	successfulRefresh metrics2.Counter
+	failedRefresh     metrics2.Counter
 
-func (m instanceMetadataMap) Get(instance, key string) (string, error) {
-	rv, ok := m[instance][key]
-	if !ok {
-		return "", fmt.Errorf("Unknown instance or key.")
-	}
-	return rv, nil
+	// mutex protects latestToken.
+	mutex       sync.Mutex
+	latestToken *oauth2.Token
 }
 
-type projectMetadataMap map[string]string
-
-func (m projectMetadataMap) Get(key string) (string, error) {
-	rv, ok := m[key]
-	if !ok {
-		return "", fmt.Errorf("Unknown key.")
+// newServer creates a new *server with a running Go routine that refreshes the token.
+func newServer() (*server, error) {
+	tokenSource, err := auth.NewDefaultTokenSource(*local, auth.SCOPE_USERINFO_EMAIL)
+	if err != nil {
+		return nil, skerr.Wrapf(err, "Failed to create token source.")
 	}
-	return rv, nil
+
+	ret := &server{
+		tokenSource:       tokenSource,
+		successfulRefresh: metrics2.GetCounter("metadata_successful_refresh"),
+		failedRefresh:     metrics2.GetCounter("metadata_failed_refresh"),
+	}
+
+	if err := ret.step(); err != nil {
+		return nil, skerr.Wrapf(err, "Failed to get token.")
+	}
+
+	go ret.refresh()
+
+	return ret, nil
+}
+
+func (s *server) handleTokenRequest(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	s.mutex.Lock()
+	t := s.latestToken
+	s.mutex.Unlock()
+	// Copied from
+	// https://github.com/golang/oauth2/blob/f6093e37b6cb4092101a298aba5d794eb570757f/google/google.go#L185
+	res := struct {
+		AccessToken  string `json:"access_token"`
+		ExpiresInSec int    `json:"expires_in"`
+		TokenType    string `json:"token_type"`
+	}{
+		AccessToken:  t.AccessToken,
+		ExpiresInSec: int(t.Expiry.Sub(time.Now()).Seconds()),
+		TokenType:    t.TokenType,
+	}
+	sklog.Infof("Token requested by %s, serving %s", r.RemoteAddr, res.AccessToken[len(res.AccessToken)-8:])
+	if err := json.NewEncoder(w).Encode(res); err != nil {
+		httputils.ReportError(w, err, "Failed to write response.", http.StatusInternalServerError)
+		return
+	}
+}
+
+// step does a single refresh of the oauth token.
+func (s *server) step() error {
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+	latestToken, err := s.tokenSource.Token()
+	if err != nil {
+		s.failedRefresh.Inc(1)
+		return skerr.Wrapf(err, "Failed to retrieve token.")
+	}
+
+	sklog.Info("Successfully refreshed.")
+	s.successfulRefresh.Inc(1)
+	s.latestToken = latestToken
+	return nil
+}
+
+// refresh handles periodically refreshing the token.
+//
+// This function does not return and should be run as a Go routine.
+func (s *server) refresh() {
+	for range time.Tick(refreshInterval) {
+		if err := s.step(); err != nil {
+			sklog.Errorf("Failed to refresh token: %s")
+		}
+	}
 }
 
 func main() {
-	// Flags.
-	var (
-		port     = flag.String("port", ":8000", "HTTP service port for the web server (e.g., ':8000')")
-		promPort = flag.String("prom_port", ":20000", "Metrics service address (e.g., ':10110')")
-		confFile = flag.String("conf", "", "Configuration file that defines which tokens should be served.")
-	)
-
 	common.InitWithMust(
-		APP_NAME,
+		"metadata",
 		common.PrometheusOpt(promPort),
 	)
 
-	// Load these from a k8s secret.
-	var pm projectMetadataMap = map[string]string{}
-	var im instanceMetadataMap = map[string]map[string]string{}
-
-	// Read the config file.
-	configs, err := readConfigFile(*confFile)
+	s, err := newServer()
 	if err != nil {
-		sklog.Fatalf("Error reading config file %q: %s", *confFile, err)
-	}
-
-	// svcAccountTokens maps [token_file][ServiceAccountToken] and ensures there is one instance
-	// of ServiceAccountToken per token file. That instance is responsible for refreshing it.
-	svcAccountTokens := make(map[string]*skmetadata.ServiceAccountToken, len(configs))
-
-	// clientTokenMapping maps [ip | host_name | self] ServiceAccountToken.
-	clientTokenMapping := make(map[string]*skmetadata.ServiceAccountToken, len(configs))
-	for _, config := range configs {
-		token, ok := svcAccountTokens[config.KeyFile]
-		if !ok {
-			token, err = skmetadata.NewServiceAccountToken(config.KeyFile, true)
-			if err != nil {
-				sklog.Fatalf("Error retrieving service account token: %s", err)
-			}
-
-			// Start the update loop as a background process.
-			go token.UpdateLoop(context.Background())
-			svcAccountTokens[config.KeyFile] = token
-		}
-
-		// Create one entry per client which is an IP address, a hostname or 'self'.
-		for _, clientAddr := range config.Clients {
-			clientTokenMapping[clientAddr] = token
-		}
+		sklog.Fatal(err)
 	}
 
 	r := mux.NewRouter()
-	skmetadata.SetupServer(r, pm, im, clientTokenMapping)
+	r.HandleFunc("/computeMetadata/v1/instance/service-accounts/default/token", s.handleTokenRequest)
 	http.Handle("/", httputils.Healthz(httputils.LoggingGzipRequestResponse(r)))
 	sklog.Infof("Ready to serve on http://localhost%s", *port)
 	sklog.Fatal(http.ListenAndServe(*port, nil))
