2b8b18a141ba3b3f7dcb642ef59113d27c4fba62
Kevin Lubick <kjlubick@google.com>
2020-05-06 07:27:13 -0400

    [gold] Interface change for indexer to support CL indices
    Bug: skia:9710
    Change-Id: If3b3a34a56f060fbe04a62e8659559b74e348ebb

---
+++ golden/go/indexer/types_test.go
@@ -0,0 +1,39 @@
+package indexer
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+	"go.skia.org/infra/go/testutils/unittest"
+	"go.skia.org/infra/golden/go/tjstore"
+)
+
+func TestChangeListIndex_Copy(t *testing.T) {
+	unittest.SmallTest(t)
+
+	alphaPSID := tjstore.CombinedPSID{CRS: "github", CL: "alpha", PS: "whatever"}
+	betaPSID := tjstore.CombinedPSID{CRS: "github", CL: "beta", PS: "whatever"}
+
+	clIdx := &ChangeListIndex{
+		UntriagedResultsProduced: map[tjstore.CombinedPSID][]tjstore.TryJobResult{
+			alphaPSID: {{Digest: "1111"}, {Digest: "2222"}},
+		},
+		ComputedTS: time.Date(2020, time.April, 1, 2, 3, 4, 0, time.UTC),
+	}
+
+	copiedIdx := clIdx.Copy()
+	assert.Equal(t, clIdx, copiedIdx)
+
+	copiedIdx.ComputedTS = time.Date(2020, time.May, 10, 10, 10, 10, 0, time.UTC)
+	assert.NotEqual(t, clIdx, copiedIdx)
+
+	// Mutate the map of the copy.
+	copiedIdx.UntriagedResultsProduced[alphaPSID] = []tjstore.TryJobResult{{Digest: "3333"}}
+	copiedIdx.UntriagedResultsProduced[betaPSID] = []tjstore.TryJobResult{{Digest: "3333"}}
+
+	// Make sure the original map didn't get changed.
+	assert.Len(t, clIdx.UntriagedResultsProduced, 1) // still should just have one psID entry
+	assert.Equal(t, []tjstore.TryJobResult{{Digest: "1111"}, {Digest: "2222"}},
+		clIdx.UntriagedResultsProduced[alphaPSID])
+}
