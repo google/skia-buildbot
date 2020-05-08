736fb67ba0027832c8f2885699537b07a2464612
Weston Tracey <westont@google.com>
2020-05-08 12:11:22 -0400

    Main page for skia-demos.
    -- load-demo-assets writes a metadata.js file about the available demos.
    -- this is read by the main page to produce a list of available demos with metadata (right now just author).
    -- https://screenshot.googleplex.com/oaSdhSYfmsG
    Change-Id: Ib6bab914caf8dd5efc2785ac4bde16d114d45563

--- demos/go/demoserver/main.go
+++ demos/go/demoserver/main.go
@@ -3,32 +3,94 @@ package main
 // The webserver for demos.skia.org. It serves a main page and a set of js+html+css demos.
 
 import (
+	"context"
 	"flag"
 	"net/http"
 	"path/filepath"
+	"sync"
+	"time"
 
 	"github.com/gorilla/mux"
 	"go.skia.org/infra/go/common"
+	"go.skia.org/infra/go/git"
 	"go.skia.org/infra/go/httputils"
 	"go.skia.org/infra/go/sklog"
+	"go.skia.org/infra/go/util"
 )
 
 var (
-	port         = flag.String("port", ":8000", "HTTP service address (e.g., ':8000')")
-	demosDir     = flag.String("demos_dir", "./demos/public", "The directory to find named subdirectories for each demo. If blank ./demos/public")
-	resourcesDir = flag.String("resources_dir", "./dist", "The directory to find templates, JS, and CSS files. If blank ./dist will be used.")
+	port = flag.String("port", ":8000", "HTTP service address (e.g., ':8000')")
+	//demosDir      = flag.String("demos_dir", "./demos/public", "The directory to find named subdirectories for each demo. If blank ./demos/public")
+	resourcesDir  = flag.String("resources_dir", "./dist", "The directory to find templates, JS, and CSS files. If blank ./dist will be used.")
+	demosRepo     = flag.String("repo_url", "https://skia.googlesource.com/infra-internal", "The repo from where to fetch the demos. Defaults to https://skia.googlesource.com/infra-internal")
+	demosRepoPath = flag.String("demos_dir", "scripts", "The top level directory in the repo that holds the demos.")
 )
 
+type syncedDemos struct {
+	sync.RWMutex
+	repo *git.Checkout
+}
+
+func newSyncedDemos(ctx context.Context, repoURL, dir string) *syncedDemos {
+	sklog.Info("Creating new syncedDemos")
+	s := new(syncedDemos)
+	var err error
+	s.repo, err = git.NewCheckout(ctx, repoURL, dir)
+	sklog.Info("got repo")
+	if err != nil {
+		sklog.Fatal(err)
+	}
+	go util.RepeatCtx(ctx, 1*time.Minute, s.Sync)
+	return s
+}
+func (s *syncedDemos) Sync(ctx context.Context) {
+	sklog.Info("Syncing")
+	s.Lock()
+	defer s.Unlock()
+	hash, err := s.repo.GetBranchHead(ctx, "master")
+	if err != nil {
+		sklog.Fatal(err)
+	}
+	sklog.Info("pre update: branch head" + hash)
+	hash, err = s.repo.FullHash(ctx, "HEAD")
+	if err != nil {
+		sklog.Fatal(err)
+	}
+	sklog.Info("pre update: Full hash" + hash)
+	s.repo.Update(ctx)
+	hash, err = s.repo.GetBranchHead(ctx, "master")
+	if err != nil {
+		sklog.Fatal(err)
+	}
+	sklog.Info("post update: branch head" + hash)
+	hash, err = s.repo.FullHash(ctx, "HEAD")
+	if err != nil {
+		sklog.Fatal(err)
+	}
+	sklog.Info("post update: Full hash" + hash)
+
+}
+
+func demoHandler(demos *syncedDemos, dir string) func(http.ResponseWriter, *http.Request) {
+	h := http.StripPrefix("/demo", http.FileServer(http.Dir("tmp/infra-internal/demos/internal")))
+	return func(w http.ResponseWriter, r *http.Request) {
+		demos.RLock()
+		defer demos.RUnlock()
+		h.ServeHTTP(w, r)
+	}
+}
 func main() {
 	common.InitWithMust(
 		"demos",
 	)
+	ctx := context.Background()
 
+	syncedDemos := newSyncedDemos(ctx, *demosRepo, "tmp")
+	sklog.Info("Got the demo thing going.")
 	r := mux.NewRouter()
-	r.PathPrefix("/demo/").Handler(http.StripPrefix("/demo", http.FileServer(http.Dir(*demosDir))))
-	// PathPrefix above needs a slash to make FileServer relative paths work.
-	// For cleanliness, make sure users get to the directory listing even without the slash.
-	r.Handle("/demo", http.RedirectHandler("/demo/", 301))
+	r.PathPrefix("/demo/").HandlerFunc(demoHandler(syncedDemos, "tmp"))
+	//r.PathPrefix("/demo/").Handler(http.StripPrefix("/demo", http.FileServer(http.Dir(*demosDir))))
+
 	r.PathPrefix("/dist/").Handler(http.StripPrefix("/dist/", http.FileServer(http.Dir(*resourcesDir))))
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, filepath.Join(*resourcesDir, "main.html"))
