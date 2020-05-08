4312075c6fe2a636fade090470500410b3f00a13
Kevin Lubick <kjlubick@google.com>
2020-05-08 11:27:49 -0400

    [gold] Add settings util
    This consolidates the way to get global constants into (potentially
    nested) elements. In the Polymer version, Gold would
    inject variables from the server side into the html by
    templating some JS and creating a global sk.app_config variable.
    [1], [2].
    
    Up to this point, the lit-html pages have only need data that
    was fine to be passed in to the top level. [3] However, now
    that we need BaseRepo to link to the source code, trickling down
    all variables we need seems overly complicated and an unnecessary
    breakdown in abstraction.
    
    This is a similar approach to the app_config approach, except we
    add a helper function to access the methods instead of having
    a lot of magic "window level variables" all over the place.
    
    [1] https://github.com/google/skia-buildbot/blob/8387de40b46b3df28f0b11145244c4083ee10d30/golden/frontend/index.html#L13
    [2] https://github.com/google/skia-buildbot/blob/8387de40b46b3df28f0b11145244c4083ee10d30/golden/cmd/skiacorrectness/main.go#L545
    [3] https://github.com/google/skia-buildbot/blob/8387de40b46b3df28f0b11145244c4083ee10d30/golden/pages/diff.html#L8
    
    
    Bug: skia:9525
    Change-Id: I5c1956efeed4cca01adfea6a2f39880d245494d6

--- golden/modules/byblameentry-sk/byblameentry-sk-demo.js
+++ golden/modules/byblameentry-sk/byblameentry-sk-demo.js
@@ -1,11 +1,14 @@
 import './index';
 import { entry, fakeNow, gitLog } from './test_data';
+import { testOnlySetSettings } from '../settings';
 
 Date.now = () => fakeNow;
+testOnlySetSettings({
+  baseRepoURL: 'https://skia.googlesource.com/skia.git',
+});
 
 const byBlameEntrySk = document.createElement('byblameentry-sk');
 byBlameEntrySk.byBlameEntry = entry;
 byBlameEntrySk.gitLog = gitLog;
-byBlameEntrySk.baseRepoUrl = 'https://skia.googlesource.com/skia.git';
 byBlameEntrySk.corpus = 'gm';
 document.body.appendChild(byBlameEntrySk);
