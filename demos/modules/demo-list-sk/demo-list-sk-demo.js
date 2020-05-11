a12edb33b19d6e38bc04605fc9b4158aaa729fa3
Weston Tracey <westont@google.com>
2020-05-11 11:05:45 -0400

    Main page for skia-demos.
    -- load-demo-assets writes a metadata.js file about the available demos.
    -- this is read by the main page to produce a list of available demos with metadata (right now just author).
    Change-Id: Ib6bab914caf8dd5efc2785ac4bde16d114d45563

---
+++ demos/modules/demo-list-sk/demo-list-sk-demo.js
@@ -0,0 +1,10 @@
+import './index';
+import { $$ } from 'common-sk/modules/dom';
+import { fetchMock } from 'fetch-mock';
+import {
+  twoDemoEntries,
+} from './test_data';
+
+fetchMock.getOnce('/demo/metadata.json', twoDemoEntries);
+const dl = document.createElement('demo-list-sk');
+$$('#main').appendChild(dl);
---
+++ demos/modules/header-sk/header-sk-demo.js
@@ -0,0 +1,5 @@
+import './index';
+import { $$ } from 'common-sk/modules/dom';
+
+const dl = document.createElement('header-sk');
+$$('#main').appendChild(dl);
