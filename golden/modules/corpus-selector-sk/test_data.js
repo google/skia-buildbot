a7b2df66a45bee46d3d714c57bc378b8efa1881a
Kevin Lubick <kjlubick@google.com>
2020-05-13 09:49:44 -0400

    [gold] Move rpc call upstack from corpus-selector-sk
    This necessitates changing how tests work in byblame page.
    
    Bug: skia:9525, skia:9901
    Change-Id: Ia376aee5301efbeede5e20c0d7054cfe8cdadcd3

--- golden/modules/corpus-selector-sk/test_data.js
+++ golden/modules/corpus-selector-sk/test_data.js
@@ -1,58 +1,44 @@
-export const trstatus = {
+// This is taken from a production /json/trstatus request (specifically the corpStatus section).
+export const exampleCorpora = [{
+  name: 'canvaskit',
   ok: false,
-  firstCommit: {
-    commit_time: 1572357082,
-    hash: 'ee08d523f60a04499c9023a349ef8174ab301f8f',
-    author: 'Alice (alice@example.com)',
-  },
-  lastCommit: {
-    commit_time: 1573598625,
-    hash: '9501212cd0580acfed85a90c3a16b81847fde482',
-    author: 'Bob (bob@example.com)',
-  },
-  totalCommits: 256,
-  filledCommits: 256,
-  corpStatus: [{
-    name: 'canvaskit',
-    ok: false,
-    minCommitHash: '',
-    untriagedCount: 2,
-    negativeCount: 2,
-  }, {
-    name: 'colorImage',
-    ok: true,
-    minCommitHash: '',
-    untriagedCount: 0,
-    negativeCount: 1,
-  }, {
-    name: 'gm',
-    ok: false,
-    minCommitHash: '',
-    untriagedCount: 61,
-    negativeCount: 1494,
-  }, {
-    name: 'image',
-    ok: false,
-    minCommitHash: '',
-    untriagedCount: 22,
-    negativeCount: 35,
-  }, {
-    name: 'pathkit',
-    ok: true,
-    minCommitHash: '',
-    untriagedCount: 0,
-    negativeCount: 0,
-  }, {
-    name: 'skp',
-    ok: true,
-    minCommitHash: '',
-    untriagedCount: 0,
-    negativeCount: 1,
-  }, {
-    name: 'svg',
-    ok: false,
-    minCommitHash: '',
-    untriagedCount: 19,
-    negativeCount: 21,
-  }],
-};
+  minCommitHash: '',
+  untriagedCount: 2,
+  negativeCount: 2,
+}, {
+  name: 'colorImage',
+  ok: true,
+  minCommitHash: '',
+  untriagedCount: 0,
+  negativeCount: 1,
+}, {
+  name: 'gm',
+  ok: false,
+  minCommitHash: '',
+  untriagedCount: 61,
+  negativeCount: 1494,
+}, {
+  name: 'image',
+  ok: false,
+  minCommitHash: '',
+  untriagedCount: 22,
+  negativeCount: 35,
+}, {
+  name: 'pathkit',
+  ok: true,
+  minCommitHash: '',
+  untriagedCount: 0,
+  negativeCount: 0,
+}, {
+  name: 'skp',
+  ok: true,
+  minCommitHash: '',
+  untriagedCount: 0,
+  negativeCount: 1,
+}, {
+  name: 'svg',
+  ok: false,
+  minCommitHash: '',
+  untriagedCount: 19,
+  negativeCount: 21,
+}];
