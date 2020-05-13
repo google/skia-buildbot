d243bb395dfb859bc718f9350fdb10f4488a242a
Joe Gregorio <jcgregorio@google.com>
2020-05-13 10:10:25 -0400

    [jsdoc] Fixes for jsdoc.
     - Fixes reference to common-sk which is now a separate repo.
     - Removes common-sk demos, since there were none.
     - Updates all libs.
    
    Change-Id: Idabf0638afc1f6df20b51034e7071fb57a8ccbb4
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/289437
    Reviewed-by: Ravi Mistry <rmistry@google.com>
    Commit-Queue: Joe Gregorio <jcgregorio@google.com>

--- jsdoc/go/jsdoc/main.go
+++ jsdoc/go/jsdoc/main.go
@@ -4,6 +4,7 @@ package main
 import (
 	"flag"
 	"net/http"
+	"path/filepath"
 
 	"github.com/gorilla/mux"
 	"go.skia.org/infra/go/common"
@@ -13,9 +14,10 @@ import (
 
 // flags
 var (
-	local    = flag.Bool("local", false, "Running locally if true. As opposed to in production.")
-	port     = flag.String("port", ":8000", "HTTP service address (e.g., ':8000')")
-	promPort = flag.String("prom_port", ":20000", "Metrics service address (e.g., ':10110')")
+	local        = flag.Bool("local", false, "Running locally if true. As opposed to in production.")
+	port         = flag.String("port", ":8000", "HTTP service address (e.g., ':8000')")
+	promPort     = flag.String("prom_port", ":20000", "Metrics service address (e.g., ':10110')")
+	resourcesDir = flag.String("resources_dir", "/usr/local/share/jsdoc/", "Root directory of resources to serve.")
 )
 
 func main() {
@@ -23,10 +25,10 @@ func main() {
 		"jsdocserver",
 		common.PrometheusOpt(promPort),
 	)
-	docsDir := "/usr/local/share/jsdoc/docs"
-	elementsDemoDir := "/usr/local/share/jsdoc/elements-sk"
-	commonDemoDir := "/usr/local/share/jsdoc/common-sk"
-	infraDemoDir := "/usr/local/share/jsdoc/infra-sk"
+	docsDir := filepath.Join(*resourcesDir, "docs")
+	elementsDemoDir := filepath.Join(*resourcesDir, "elements-sk")
+	commonDemoDir := filepath.Join(*resourcesDir, "common-sk")
+	infraDemoDir := filepath.Join(*resourcesDir, "infra-sk")
 	r := mux.NewRouter()
 	r.PathPrefix("/common-sk/").Handler(http.StripPrefix("/common-sk/", http.HandlerFunc(httputils.MakeResourceHandler(commonDemoDir))))
 	r.PathPrefix("/elements-sk/").Handler(http.StripPrefix("/elements-sk/", http.HandlerFunc(httputils.MakeResourceHandler(elementsDemoDir))))
