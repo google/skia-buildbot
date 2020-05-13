be23a17cecb5aa07a5c6472d921f8567e74bdac0
Joe Gregorio <jcgregorio@google.com>
2020-05-13 10:10:25 -0400

    [jsdoc] Fixes for jsdoc.
     - Fixes reference to common-sk which is now a separate repo.
     - Removes common-sk demos, since there were none.
     - Updates all libs.
    
    Change-Id: Idabf0638afc1f6df20b51034e7071fb57a8ccbb4

--- jsdoc/jsdoc.config.js
+++ jsdoc/jsdoc.config.js
@@ -1,3 +1,4 @@
-let config = require('../common-sk/jsdoc.common.js')
-config.source.include = ['node_modules/elements-sk', 'node_modules/elements-sk/styles', '../infra-sk/modules', '../common-sk/modules', './jsdoc/README.md'];
+const config = require('common-sk/jsdoc.common.js');
+
+config.source.include = ['node_modules/elements-sk', 'node_modules/elements-sk/styles', '../infra-sk/modules', 'node_modules/common-sk/modules', './jsdoc/README.md'];
 module.exports = config;
