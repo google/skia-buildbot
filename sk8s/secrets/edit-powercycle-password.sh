b1c7cef9733e8119c4d40a6368f7d771e0aa000f
Joe Gregorio <jcgregorio@google.com>
2020-05-14 10:12:38 -0400

    [sk8s] powercycle password.
    Change-Id: Ib359c2a5c7879fd68c4bd7e370372738f77a9bc3
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/289782
    Reviewed-by: Kevin Lubick <kjlubick@google.com>

---
+++ sk8s/secrets/edit-powercycle-password.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+
+# Downloads the 'password' secret files for editing.
+#
+# Once you are done editing the file, the file will be uploaded.
+
+../../kube/secrets/edit-secret.sh skolo-rack4 powercycle-password
