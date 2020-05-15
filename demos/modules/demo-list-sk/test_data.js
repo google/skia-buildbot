b68d20446b2859c0690061c070df704d1bea4001
Weston Tracey <westont@google.com>
2020-05-15 07:21:03 -0400

    Main page for skia-demos.
    -- load-demo-assets writes a metadata.js file about the available demos.
    -- this is read by the main page to produce a list of available demos with metadata (right now just author).
    
    Change-Id: Ib6bab914caf8dd5efc2785ac4bde16d114d45563

---
+++ demos/modules/demo-list-sk/test_data.js
@@ -0,0 +1,35 @@
+export const singleDemoEntry = {
+  myDemo: {
+    hash: '0123456789',
+    author: 'Jane Doe (jd@example.com)',
+    subject: 'Add a sweet demo.',
+    parent: [
+      '9876543210',
+    ],
+    body: 'Change-Id: 7\nFoo bar baz',
+    timestamp: '2020-04-28T16:13:24Z',
+  },
+};
+
+export const twoDemoEntries = {
+  aDemo: {
+    hash: '10123456789',
+    author: 'John Doe (jd1@example.com)',
+    subject: 'Add a sweet demo.',
+    parent: [
+      '19876543210',
+    ],
+    body: 'Change-Id: 2\nFoo bar baz',
+    timestamp: '2019-04-28T16:13:24Z',
+  },
+  myDemo: {
+    hash: '0123456789',
+    author: 'Jane Doe (jd@example.com)',
+    subject: 'Add a sweet demo.',
+    parent: [
+      '9876543210',
+    ],
+    body: 'Change-Id: 7\nFoo bar baz',
+    timestamp: '2020-04-28T16:13:24Z',
+  },
+};
