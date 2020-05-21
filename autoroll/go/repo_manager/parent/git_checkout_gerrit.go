4a4e86d7b2854995a3323e2cb11f8fb03b337c9f
Eric Boren <borenet@google.com>
2020-05-21 08:14:25 -0400

    [autoroll] Add CommandRepoManager
    It uses a local checkout and runs commands specified in the config file
    to retrieve the tip revision and to get and set the pinned revision.
    
    Bug: skia:10109
    Change-Id: I94f35f05f01e316f557d418364d7a82c41704c3e

--- autoroll/go/repo_manager/parent/git_checkout_gerrit.go
+++ autoroll/go/repo_manager/parent/git_checkout_gerrit.go
@@ -6,6 +6,7 @@ import (
 
 	"go.skia.org/infra/autoroll/go/codereview"
 	"go.skia.org/infra/autoroll/go/repo_manager/common/gerrit_common"
+	"go.skia.org/infra/autoroll/go/repo_manager/common/git_common"
 	"go.skia.org/infra/go/gerrit"
 	"go.skia.org/infra/go/git"
 	"go.skia.org/infra/go/skerr"
@@ -31,7 +32,7 @@ func (c GitCheckoutGerritConfig) Validate() error {
 
 // GitCheckoutUploadGerritRollFunc returns a GitCheckoutUploadRollFunc which
 // uploads a CL to Gerrit.
-func GitCheckoutUploadGerritRollFunc(g gerrit.GerritInterface) GitCheckoutUploadRollFunc {
+func GitCheckoutUploadGerritRollFunc(g gerrit.GerritInterface) git_common.UploadRollFunc {
 	return func(ctx context.Context, co *git.Checkout, upstreamBranch, hash string, emails []string, dryRun bool, commitMsg string) (int64, error) {
 		// Find the change ID in the commit message.
 		out, err := co.Git(ctx, "log", "-n1", hash)
@@ -62,17 +63,4 @@ func GitCheckoutUploadGerritRollFunc(g gerrit.GerritInterface) GitCheckoutUpload
 
 		return ci.Issue, nil
 	}
-}
-
-// SetupGerrit performs additional setup for a GitCheckoutParent which uses
-// Gerrit. This is required for all users of GitCheckoutUploadGerritRollFunc.
-// TODO(borenet): This is needed for RepoManagers which use NewDEPSLocal, since
-// they need to pass in a GitCheckoutUploadRollFunc but can't do other
-// initialization. Find a way to make this unnecessary.
-func SetupGerrit(ctx context.Context, p *GitCheckoutParent, g gerrit.GerritInterface) error {
-	// Install the Gerrit Change-Id hook.
-	if err := gerrit_common.DownloadCommitMsgHook(ctx, g, p.Checkout.Checkout); err != nil {
-		return skerr.Wrap(err)
-	}
-	return nil
 }
