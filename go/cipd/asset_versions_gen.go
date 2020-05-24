dbb4c0a8b5501adb2096effadf5c2fd8dc7c8672
skia-recreate-skps <skia-recreate-skps@skia-swarming-bots.iam.gserviceaccount.com>
2020-05-24 05:03:53 +0000

    Update CIPD Packages
    Change-Id: I875b94dcf129aa54b80cb430d072ae10ef896167
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/291684
    Reviewed-by: <skia-recreate-skps@skia-swarming-bots.iam.gserviceaccount.com>
    Commit-Queue: <skia-recreate-skps@skia-swarming-bots.iam.gserviceaccount.com>

--- go/cipd/asset_versions_gen.go
+++ go/cipd/asset_versions_gen.go
@@ -11,17 +11,17 @@ var PACKAGES = map[string]*Package{
 	"infra/3pp/tools/git/linux-amd64": {
 		Path:    "cipd_bin_packages",
 		Name:    "infra/3pp/tools/git/linux-amd64",
-		Version: "version:2.27.0-rc0.chromium.6",
+		Version: "version:2.27.0-rc1.chromium.6",
 	},
 	"infra/3pp/tools/git/linux-arm64": {
 		Path:    "cipd_bin_packages",
 		Name:    "infra/3pp/tools/git/linux-arm64",
-		Version: "version:2.27.0-rc0.chromium.6",
+		Version: "version:2.27.0-rc1.chromium.6",
 	},
 	"infra/3pp/tools/git/mac-amd64": {
 		Path:    "cipd_bin_packages",
 		Name:    "infra/3pp/tools/git/mac-amd64",
-		Version: "version:2.27.0-rc0.chromium.6",
+		Version: "version:2.27.0-rc1.chromium.6",
 	},
 	"infra/3pp/tools/git/windows-386": {
 		Path:    "cipd_bin_packages",
@@ -41,47 +41,47 @@ var PACKAGES = map[string]*Package{
 	"infra/tools/cipd/${os}-${arch}": {
 		Path:    ".",
 		Name:    "infra/tools/cipd/${os}-${arch}",
-		Version: "git_revision:0f9f9c2747f04f0cecefab160f7fa3f353603990",
+		Version: "git_revision:ae0579c73e57a0e6730a28509eb1b5ea71b35df5",
 	},
 	"infra/tools/git/${platform}": {
 		Path:    "cipd_bin_packages",
 		Name:    "infra/tools/git/${platform}",
-		Version: "git_revision:0f9f9c2747f04f0cecefab160f7fa3f353603990",
+		Version: "git_revision:ae0579c73e57a0e6730a28509eb1b5ea71b35df5",
 	},
 	"infra/tools/luci-auth/${platform}": {
 		Path:    "cipd_bin_packages",
 		Name:    "infra/tools/luci-auth/${platform}",
-		Version: "git_revision:0f9f9c2747f04f0cecefab160f7fa3f353603990",
+		Version: "git_revision:ae0579c73e57a0e6730a28509eb1b5ea71b35df5",
 	},
 	"infra/tools/luci/git-credential-luci/${platform}": {
 		Path:    "cipd_bin_packages",
 		Name:    "infra/tools/luci/git-credential-luci/${platform}",
-		Version: "git_revision:0f9f9c2747f04f0cecefab160f7fa3f353603990",
+		Version: "git_revision:ae0579c73e57a0e6730a28509eb1b5ea71b35df5",
 	},
 	"infra/tools/luci/isolate/${platform}": {
 		Path:    "cipd_bin_packages",
 		Name:    "infra/tools/luci/isolate/${platform}",
-		Version: "git_revision:0f9f9c2747f04f0cecefab160f7fa3f353603990",
+		Version: "git_revision:ae0579c73e57a0e6730a28509eb1b5ea71b35df5",
 	},
 	"infra/tools/luci/isolated/${platform}": {
 		Path:    "cipd_bin_packages",
 		Name:    "infra/tools/luci/isolated/${platform}",
-		Version: "git_revision:0f9f9c2747f04f0cecefab160f7fa3f353603990",
+		Version: "git_revision:ae0579c73e57a0e6730a28509eb1b5ea71b35df5",
 	},
 	"infra/tools/luci/kitchen/${platform}": {
 		Path:    ".",
 		Name:    "infra/tools/luci/kitchen/${platform}",
-		Version: "git_revision:0f9f9c2747f04f0cecefab160f7fa3f353603990",
+		Version: "git_revision:ae0579c73e57a0e6730a28509eb1b5ea71b35df5",
 	},
 	"infra/tools/luci/swarming/${platform}": {
 		Path:    "cipd_bin_packages",
 		Name:    "infra/tools/luci/swarming/${platform}",
-		Version: "git_revision:0f9f9c2747f04f0cecefab160f7fa3f353603990",
+		Version: "git_revision:ae0579c73e57a0e6730a28509eb1b5ea71b35df5",
 	},
 	"infra/tools/luci/vpython/${platform}": {
 		Path:    "cipd_bin_packages",
 		Name:    "infra/tools/luci/vpython/${platform}",
-		Version: "git_revision:0f9f9c2747f04f0cecefab160f7fa3f353603990",
+		Version: "git_revision:ae0579c73e57a0e6730a28509eb1b5ea71b35df5",
 	},
 	"skia/bots/cockroachdb": {
 		Path:    "cockroachdb",
@@ -116,6 +116,6 @@ var PACKAGES = map[string]*Package{
 	"skia/tools/goldctl/${platform}": {
 		Path:    "cipd_bin_packages",
 		Name:    "skia/tools/goldctl/${platform}",
-		Version: "git_revision:0f9f9c2747f04f0cecefab160f7fa3f353603990",
+		Version: "git_revision:ae0579c73e57a0e6730a28509eb1b5ea71b35df5",
 	},
 }
