ad82d834e4be37434521c8fe830b638852f0a8d2
Leandro Lovisolo <lovisolo@google.com>
2020-05-27 23:25:27 -0400

    [gold] [infra-sk]: Move karma.common.ts to //infra-sk.
    Bug: skia:10292
    Change-Id: I40145f326e8dfa1bbf99c39a433b485c1d4ffbc1

--- golden/karma.conf.ts
+++ golden/karma.conf.ts
@@ -1,3 +1,9 @@
-import * as common from './karma.common';
+import webpack from 'webpack';
+import karma from 'karma';
+import webpackConfigFactory from './webpack.config';
+import { setCommonConfigOptions } from '../infra-sk/karma.common';
 
-export = common;
+export = function(karmaConfig: karma.Config) {
+  const webpackConfig = webpackConfigFactory('', { mode: 'development' }) as webpack.Configuration;
+  setCommonConfigOptions(karmaConfig, webpackConfig);
+}
