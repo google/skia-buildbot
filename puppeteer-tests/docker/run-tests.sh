5bb98063ae317f29f0dd36c8471259f2413c8f92
Leandro Lovisolo <lovisolo@google.com>
2020-05-28 15:57:45 -0400

    [am] Fix Puppeteer tests.
    Tests were accidentally broken after migrating to the TypeScript-enabled Webpack configuration.
    
    Bug: skia:10292
    Change-Id: Ifc812f9d9dec34a5600b8515ab29468cc09fe5d9

--- puppeteer-tests/docker/run-tests.sh
+++ puppeteer-tests/docker/run-tests.sh
@@ -82,6 +82,7 @@ cp -r /src/perf/modules             /tests/perf
 mkdir /tests/am
 cp -r /src/am/package*              /tests/am
 cp -r /src/am/webpack.config.ts     /tests/am
+cp -r /src/am/tsconfig.json         /tests/am
 cp -r /src/am/modules               /tests/am
 
 mkdir /tests/ct
@@ -131,7 +132,7 @@ cd /tests/perf
 npx mocha ./**/*_puppeteer_test.js
 
 cd /tests/am
-npx mocha ./**/*_puppeteer_test.js
+npx mocha -r ts-node/register ./**/*_puppeteer_test.ts
 
 cd /tests/ct
 npx mocha ./**/*_puppeteer_test.js
