f080263e688eeaef8c92418563b0f8fcbf5d4290
Kevin Lubick <kjlubick@google.com>
2020-05-28 08:36:20 -0400

    [gold] Let frontend.Commit represent CLs
    Big idea here is to have a new setting with the URL for
    CLs, so we can link to them from the frontend.
    
    Bug: skia:9901
    Change-Id: I6abdabde23109f131b085d32c592c60b987d12f3

--- golden/modules/blamelist-panel-sk/blamelist-panel-sk-demo.ts
+++ golden/modules/blamelist-panel-sk/blamelist-panel-sk-demo.ts
@@ -1,18 +1,23 @@
 import './index';
 import { $$ } from 'common-sk/modules/dom';
 import { BlamelistPanelSk } from './blamelist-panel-sk';
-import { blamelist19, fakeNow } from './demo_data';
+import { blamelist19, clBlamelist, fakeNow } from './demo_data';
 import { testOnlySetSettings } from '../settings';
 
 Date.now = () => fakeNow;
 
 testOnlySetSettings({
   baseRepoURL: 'https://github.com/example/example',
+  crsTemplate: 'https://skia-review.googlesource.com/%s',
 });
 
 let ele = new BlamelistPanelSk();
 ele.commits = blamelist19.slice(0, 1);
 $$('#single_commit')!.appendChild(ele);
+
+ele = new BlamelistPanelSk();
+ele.commits = clBlamelist;
+$$('#single_cl_commit')!.appendChild(ele);
 
 ele = new BlamelistPanelSk();
 ele.commits = blamelist19.slice(0, 3);
--- golden/modules/settings.js
+++ golden/modules/settings.js
@@ -28,6 +28,13 @@ export function baseRepoURL() {
 }
 
 /**
+ * @return {string}
+ */
+export function codeReviewURLTemplate() {
+  return window.GoldSettings && window.GoldSettings.crsTemplate;
+}
+
+/**
  * @param newSettings {Object}
  */
 export function testOnlySetSettings(newSettings) {
