60fb46124279f541e233d967b394e53cb5d09d64
Leandro Lovisolo <lovisolo@google.com>
2020-05-28 04:02:10 -0400

    [infra-sk] Add (empty) type declarations for html-webpack-inject-attributes-plugin.
    This is necessary to prevent TypeScript errors due to implicit "any" types in follow-up CLs.
    
    Bug: skia:10292
    Change-Id: I2ce8c93b01906b3268002367598fc771ba03e5b5
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/292480
    Reviewed-by: Joe Gregorio <jcgregorio@google.com>
    Commit-Queue: Leandro Lovisolo <lovisolo@google.com>

---
+++ infra-sk/html-webpack-inject-attributes-plugin.d.ts
@@ -0,0 +1,13 @@
+// Module html-webpack-inject-attributes-plugin does not provide TypeScript type definitions. This
+// file defines an empty module for the sole purpose of making the TypeScript compiler happy when
+// notImplicitAny is set to true.
+//
+// To use it, include the following directive at the top of e.g. your webpack.config.ts file:
+//
+//     /// <reference path="../infra-sk/html-webpack-inject-attributes-plugin.d.ts" />
+//
+// See also:
+//  - https://medium.com/@steveruiz/using-a-javascript-library-without-type-declarations-in-a-typescript-project-3643490015f3
+//  - https://github.com/Urigo/angular-meteor/issues/1909
+
+declare module 'html-webpack-inject-attributes-plugin';
