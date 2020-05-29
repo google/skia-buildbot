7d91d742418970cab1159a3a17c448f872f8de3f
Leandro Lovisolo <lovisolo@google.com>
2020-05-28 22:57:45 -0400

    [jsfiddle] Add TypeScript support via Webpack.
    Bug: skia:10292
    Change-Id: I0f29adf1c5274609cae4717df4c5c9d95ce7646e
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/292760
    Reviewed-by: Kevin Lubick <kjlubick@google.com>
    Commit-Queue: Leandro Lovisolo <lovisolo@google.com>

--- jsfiddle/webpack.config.ts
+++ jsfiddle/webpack.config.ts
@@ -1,20 +1,27 @@
-const commonBuilder = require('pulito');
-const { resolve } = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+import { resolve } from 'path';
+import webpack from 'webpack';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
+import commonBuilder from '../infra-sk/pulito/webpack.common';
 
-module.exports = (env, argv) => {
-  const config = commonBuilder(env, argv, __dirname);
+const configFactory: webpack.ConfigurationFactory = (_, args) => {
+  const config = commonBuilder(__dirname, args.mode);
+
   // Make all CSS/JS files appear at the /res location.
-  config.output.publicPath = '/res/';
-  config.plugins.push(
+  config.output!.publicPath = '/res/';
+
+  config.plugins!.push(
     new CopyWebpackPlugin([
       { from: resolve(__dirname, 'build/canvaskit/canvaskit.wasm') },
       { from: resolve(__dirname, 'build/pathkit/pathkit.wasm') },
       { from: resolve(__dirname, 'node_modules/@webcomponents/custom-elements/custom-elements.min.js') },
     ]),
   );
+
   config.node = {
     fs: 'empty',
   };
+
   return config;
 };
+
+export = configFactory;
