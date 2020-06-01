640c819584c5def13fc857ba674b70a2d2cb39c6
Leandro Lovisolo <lovisolo@google.com>
2020-06-01 04:40:53 -0400

    [demos] Add TypeScript support via Webpack.
    Bug: skia:10292
    Change-Id: I91c979de7f7c2fe7eeabb82ed06d67d88a5b3f78
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/292756
    Reviewed-by: Weston Tracey <westont@google.com>
    Commit-Queue: Leandro Lovisolo <lovisolo@google.com>

--- demos/webpack.config.ts
+++ demos/webpack.config.ts
@@ -1,23 +1,29 @@
-const commonBuilder = require('pulito');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-const { resolve } = require('path');
+import { resolve } from 'path';
+import webpack from 'webpack';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
+import commonBuilder from '../infra-sk/pulito/webpack.common';
 
-module.exports = (env, argv) => {
-  const config = commonBuilder(env, argv, __dirname);
-  config.output.publicPath = '/dist/';
+const configFactory: webpack.ConfigurationFactory = (_, args) => {
+  const config = commonBuilder(__dirname, args.mode);
+  config.output!.publicPath = '/dist/';
   config.resolve = config.resolve || {};
+
   // https://github.com/webpack/node-libs-browser/issues/26#issuecomment-267954095
   config.resolve.modules = [resolve(__dirname, 'node_modules'), 'node_modules'];
-  config.plugins.push(
+
+  config.plugins!.push(
     new CopyWebpackPlugin([
       { from: resolve(__dirname, 'build/canvaskit/canvaskit.wasm') },
       { from: resolve(__dirname, 'build/canvaskit/canvaskit.js') },
       { from: resolve(__dirname, 'node_modules/@webcomponents/custom-elements/custom-elements.min.js') },
     ]),
   );
+
   config.node = {
     fs: 'empty',
   };
 
   return config;
 };
+
+export = configFactory;
