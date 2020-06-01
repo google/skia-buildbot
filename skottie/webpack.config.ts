17a311082772c483928423f791d27adbc4d5350f
Leandro Lovisolo <lovisolo@google.com>
2020-06-01 04:43:54 -0400

    [skottie] Add TypeScript support via Webpack.
    Bug: skia:10292
    Change-Id: I4b2335eed3b85696c7d73f0b78cf0d92e425bb53
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/293040
    Reviewed-by: Kevin Lubick <kjlubick@google.com>
    Commit-Queue: Leandro Lovisolo <lovisolo@google.com>

---
+++ skottie/webpack.config.ts
@@ -0,0 +1,32 @@
+import { resolve } from 'path';
+import webpack from 'webpack';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
+import commonBuilder from '../infra-sk/pulito/webpack.common';
+
+const configFactory: webpack.ConfigurationFactory = (_, args) => {
+  const config = commonBuilder(__dirname, args.mode);
+
+  config.output!.publicPath = '/static/';
+
+  config.plugins!.push(
+    new CopyWebpackPlugin([
+      {
+        from: resolve(__dirname, 'node_modules/jsoneditor/dist/jsoneditor.min.css'),
+        to: 'jsoneditor.css',
+      }, {
+        from: resolve(__dirname, 'node_modules/jsoneditor/dist/img/jsoneditor-icons.svg'),
+        to: 'img/jsoneditor-icons.svg',
+      },
+      { from: resolve(__dirname, 'build/canvaskit/canvaskit.wasm') },
+      { from: resolve(__dirname, 'node_modules/@webcomponents/custom-elements/custom-elements.min.js') },
+    ]),
+  );
+
+  config.node = {
+    fs: 'empty',
+  };
+
+  return config;
+};
+
+export = configFactory;
