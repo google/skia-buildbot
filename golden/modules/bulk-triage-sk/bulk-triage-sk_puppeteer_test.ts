63674fb837340a9724c595212738f1c47f539646
Kevin Lubick <kjlubick@google.com>
2020-06-05 16:53:02 -0400

    [gold] Port bulk-triage-sk to lit-html
    Change-Id: Ib4fcbb9c021b937170e5c0211b2379a8f5fe565e

---
+++ golden/modules/bulk-triage-sk/bulk-triage-sk_puppeteer_test.ts
@@ -0,0 +1,61 @@
+import * as path from 'path';
+import { expect } from 'chai';
+import { setUpPuppeteerAndDemoPageServer, takeScreenshot } from '../../../puppeteer-tests/util';
+
+describe.only('bulk-triage-sk', () => {
+  // Contains page and baseUrl.
+  const testBed = setUpPuppeteerAndDemoPageServer(path.join(__dirname, '..', '..', 'webpack.config.ts'));
+
+  beforeEach(async () => {
+    await testBed.page.goto(`${testBed.baseUrl}/dist/bulk-triage-sk.html`);
+  });
+
+  it('should render the demo page', async () => {
+    expect(await testBed.page.$$('bulk-triage-sk')).to.have.length(2); // Smoke test.
+  });
+
+  describe('screenshots', async () => {
+    it('should be closest by default', async () => {
+      const bulkTriageSk = await testBed.page.$('#default');
+      await takeScreenshot(bulkTriageSk!, 'gold', 'bulk-triage-sk_closest');
+    });
+
+    it('should be negative', async () => {
+      await testBed.page.click('#default button.untriaged');
+      await testBed.page.click('body'); // Remove focus from button.
+      const bulkTriageSk = await testBed.page.$('#default');
+      await takeScreenshot(bulkTriageSk!, 'gold', 'bulk-triage-sk_untriaged');
+    });
+
+    it('should be negative', async () => {
+      await testBed.page.click('#default button.negative');
+      await testBed.page.click('body'); // Remove focus from button.
+      const bulkTriageSk = await testBed.page.$('#default');
+      await takeScreenshot(bulkTriageSk!, 'gold', 'bulk-triage-sk_negative');
+    });
+
+    it('should be positive', async () => {
+      await testBed.page.click('#default button.positive');
+      await testBed.page.click('body'); // Remove focus from button.
+      const bulkTriageSk = await testBed.page.$('#default');
+      await takeScreenshot(bulkTriageSk!, 'gold', 'bulk-triage-sk_positive');
+    });
+
+    it('should be positive, with button focused', async () => {
+      await testBed.page.click('#default button.positive');
+      const bulkTriageSk = await testBed.page.$('#default');
+      await takeScreenshot(bulkTriageSk!, 'gold', 'bulk-triage-sk_positive-button-focused');
+    });
+
+    it('changes views when checkbox clicked', async () => {
+      await testBed.page.click('#default checkbox-sk.toggle_all');
+      const bulkTriageSk = await testBed.page.$('#default');
+      await takeScreenshot(bulkTriageSk!, 'gold', 'bulk-triage-sk_triage-all');
+    });
+
+    it('shows some extra information for changelists', async () => {
+      const bulkTriageSk = await testBed.page.$('#changelist');
+      await takeScreenshot(bulkTriageSk!, 'gold', 'bulk-triage-sk_changelist');
+    });
+  });
+});
