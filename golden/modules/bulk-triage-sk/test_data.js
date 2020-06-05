4d1afe0af896d5db2396d391d4b10ff70e79a7cb
Kevin Lubick <kjlubick@google.com>
2020-06-05 09:51:27 -0400

    [gold] Port bulk-triage-sk to lit-html
    Change-Id: Ib4fcbb9c021b937170e5c0211b2379a8f5fe565e

---
+++ golden/modules/bulk-triage-sk/test_data.js
@@ -0,0 +1,27 @@
+export const examplePageData = {
+  alpha_test: {
+    aaaaaaaaaaaaaaaaaaaaaaaaaaa: 'positive',
+    bbbbbbbbbbbbbbbbbbbbbbbbbbb: 'negative',
+  },
+
+  beta_test: {
+    ccccccccccccccccccccccccccc: 'positive',
+  },
+};
+
+export const exampleAllData = {
+  alpha_test: {
+    aaaaaaaaaaaaaaaaaaaaaaaaaaa: 'positive',
+    bbbbbbbbbbbbbbbbbbbbbbbbbbb: 'negative',
+    ddddddddddddddddddddddddddd: 'positive',
+  },
+
+  beta_test: {
+    ccccccccccccccccccccccccccc: 'positive',
+    ddddddddddddddddddddddddddd: 'negative',
+  },
+
+  gamma_test: {
+    eeeeeeeeeeeeeeeeeeeeeeeeeee: '', // pretend this has no closest reference image.
+  },
+};
