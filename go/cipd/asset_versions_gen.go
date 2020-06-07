9c9e45a2e6ea89700feda678ac130a342de62a8d
skia-recreate-skps <skia-recreate-skps@skia-swarming-bots.iam.gserviceaccount.com>
2020-06-07 05:04:23 +0000

    Update CIPD Packages

--- go/cipd/asset_versions_gen.go
+++ go/cipd/asset_versions_gen.go
@@ -11,27 +11,27 @@ var PACKAGES = map[string]*Package{
 	"infra/3pp/tools/git/linux-amd64": {
 		Path:    "cipd_bin_packages",
 		Name:    "infra/3pp/tools/git/linux-amd64",
-		Version: "version:2.27.0-rc2.chromium.6",
+		Version: "version:2.27.0.chromium.6",
 	},
 	"infra/3pp/tools/git/linux-arm64": {
 		Path:    "cipd_bin_packages",
 		Name:    "infra/3pp/tools/git/linux-arm64",
-		Version: "version:2.27.0-rc2.chromium.6",
+		Version: "version:2.27.0.chromium.6",
 	},
 	"infra/3pp/tools/git/mac-amd64": {
 		Path:    "cipd_bin_packages",
 		Name:    "infra/3pp/tools/git/mac-amd64",
-		Version: "version:2.27.0-rc2.chromium.6",
+		Version: "version:2.27.0.chromium.6",
 	},
 	"infra/3pp/tools/git/windows-386": {
 		Path:    "cipd_bin_packages",
 		Name:    "infra/3pp/tools/git/windows-386",
-		Version: "version:2.26.2.chromium.6",
+		Version: "version:2.27.0.chromium.6",
 	},
 	"infra/3pp/tools/git/windows-amd64": {
 		Path:    "cipd_bin_packages",
 		Name:    "infra/3pp/tools/git/windows-amd64",
-		Version: "version:2.26.2.chromium.6",
+		Version: "version:2.27.0.chromium.6",
 	},
 	"infra/gsutil": {
 		Path:    "cipd_bin_packages",
@@ -41,47 +41,47 @@ var PACKAGES = map[string]*Package{
 	"infra/tools/cipd/${os}-${arch}": {
 		Path:    ".",
 		Name:    "infra/tools/cipd/${os}-${arch}",
-		Version: "git_revision:36809f9486cc4246142f92897b7e15f6564bd94f",
+		Version: "git_revision:00ed4f248e3011ef391ff0ca76197e7ffa84eb3c",
 	},
 	"infra/tools/git/${platform}": {
 		Path:    "cipd_bin_packages",
 		Name:    "infra/tools/git/${platform}",
-		Version: "git_revision:36809f9486cc4246142f92897b7e15f6564bd94f",
+		Version: "git_revision:00ed4f248e3011ef391ff0ca76197e7ffa84eb3c",
 	},
 	"infra/tools/luci-auth/${platform}": {
 		Path:    "cipd_bin_packages",
 		Name:    "infra/tools/luci-auth/${platform}",
-		Version: "git_revision:36809f9486cc4246142f92897b7e15f6564bd94f",
+		Version: "git_revision:00ed4f248e3011ef391ff0ca76197e7ffa84eb3c",
 	},
 	"infra/tools/luci/git-credential-luci/${platform}": {
 		Path:    "cipd_bin_packages",
 		Name:    "infra/tools/luci/git-credential-luci/${platform}",
-		Version: "git_revision:36809f9486cc4246142f92897b7e15f6564bd94f",
+		Version: "git_revision:00ed4f248e3011ef391ff0ca76197e7ffa84eb3c",
 	},
 	"infra/tools/luci/isolate/${platform}": {
 		Path:    "cipd_bin_packages",
 		Name:    "infra/tools/luci/isolate/${platform}",
-		Version: "git_revision:36809f9486cc4246142f92897b7e15f6564bd94f",
+		Version: "git_revision:00ed4f248e3011ef391ff0ca76197e7ffa84eb3c",
 	},
 	"infra/tools/luci/isolated/${platform}": {
 		Path:    "cipd_bin_packages",
 		Name:    "infra/tools/luci/isolated/${platform}",
-		Version: "git_revision:36809f9486cc4246142f92897b7e15f6564bd94f",
+		Version: "git_revision:00ed4f248e3011ef391ff0ca76197e7ffa84eb3c",
 	},
 	"infra/tools/luci/kitchen/${platform}": {
 		Path:    ".",
 		Name:    "infra/tools/luci/kitchen/${platform}",
-		Version: "git_revision:36809f9486cc4246142f92897b7e15f6564bd94f",
+		Version: "git_revision:00ed4f248e3011ef391ff0ca76197e7ffa84eb3c",
 	},
 	"infra/tools/luci/swarming/${platform}": {
 		Path:    "cipd_bin_packages",
 		Name:    "infra/tools/luci/swarming/${platform}",
-		Version: "git_revision:36809f9486cc4246142f92897b7e15f6564bd94f",
+		Version: "git_revision:00ed4f248e3011ef391ff0ca76197e7ffa84eb3c",
 	},
 	"infra/tools/luci/vpython/${platform}": {
 		Path:    "cipd_bin_packages",
 		Name:    "infra/tools/luci/vpython/${platform}",
-		Version: "git_revision:36809f9486cc4246142f92897b7e15f6564bd94f",
+		Version: "git_revision:00ed4f248e3011ef391ff0ca76197e7ffa84eb3c",
 	},
 	"skia/bots/cockroachdb": {
 		Path:    "cockroachdb",
@@ -116,6 +116,6 @@ var PACKAGES = map[string]*Package{
 	"skia/tools/goldctl/${platform}": {
 		Path:    "cipd_bin_packages",
 		Name:    "skia/tools/goldctl/${platform}",
-		Version: "git_revision:36809f9486cc4246142f92897b7e15f6564bd94f",
+		Version: "git_revision:00ed4f248e3011ef391ff0ca76197e7ffa84eb3c",
 	},
 }
