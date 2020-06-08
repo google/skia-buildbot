4aac43dc4c2856d1ea8340677efb606d5d730590
Weston Tracey <westont@google.com>
2020-06-08 08:48:16 -0400

    [ct] Add label and re-style suggest-input-sk.
    Change-Id: Ieabb9e273d1937247ae137d3d8371e66f8c4fa15

--- ct/modules/suggest-input-sk/suggest-input-sk-demo.js
+++ ct/modules/suggest-input-sk/suggest-input-sk-demo.js
@@ -6,6 +6,7 @@ import { languageList } from './test_data';
 function newTaskQueue(parentSelector) {
   const si = document.createElement('suggest-input-sk');
   si.options = languageList;
+  si.label = 'Select a language';
   $$(parentSelector).appendChild(si);
 }
 
