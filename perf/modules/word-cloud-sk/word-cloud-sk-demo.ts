4c597e9b646bf76eb0df3eea3dfa7719808ce661
Joe Gregorio <jcgregorio@google.com>
2020-06-10 13:22:06 -0400

    Add theme support to word-cloud-sk.
    Also add gold test.
    
    Screenshot:
      https://skia-infra-gold.skia.org/img/images/fa5d07486b028e65808a4778717508f8.png
    
    Change-Id: I0fd5aefc9670e0f6933678ff9909321bf3174647

--- perf/modules/word-cloud-sk/word-cloud-sk-demo.ts
+++ perf/modules/word-cloud-sk/word-cloud-sk-demo.ts
@@ -1,1 +1,13 @@
 import './index';
+import { WordCloudSk } from './word-cloud-sk';
+
+let items = [
+  { value: 'arch=x86', percent: 100 },
+  { value: 'config=565', percent: 60 },
+  { value: 'config=8888', percent: 50 },
+  { value: 'arch=arm', percent: 20 },
+];
+
+document.querySelectorAll<WordCloudSk>('word-cloud-sk').forEach((wc) => {
+  wc.items = items;
+});
---
+++ perf/modules/word-cloud-sk/word-cloud-sk_puppeteer_test.ts
@@ -0,0 +1,28 @@
+import * as path from 'path';
+import { expect } from 'chai';
+import {
+  setUpPuppeteerAndDemoPageServer,
+  takeScreenshot,
+} from '../../../puppeteer-tests/util';
+
+describe('word-cloud-sk', () => {
+  const testBed = setUpPuppeteerAndDemoPageServer(
+    path.join(__dirname, '..', '..', 'webpack.config.ts')
+  );
+
+  beforeEach(async () => {
+    await testBed.page.goto(`${testBed.baseUrl}/dist/word-cloud-sk.html`);
+    await testBed.page.setViewport({ width: 400, height: 550 });
+  });
+
+  it('should render the demo page', async () => {
+    // Smoke test.
+    expect(await testBed.page.$$('word-cloud-sk')).to.have.length(3);
+  });
+
+  describe('screenshots', () => {
+    it('shows the default view', async () => {
+      await takeScreenshot(testBed.page, 'perf', 'word-cloud-sk');
+    });
+  });
+});
