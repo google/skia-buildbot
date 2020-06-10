4b13c6a48a75af7aa231e8d1ef141ad3a2f30e27
Joe Gregorio <jcgregorio@google.com>
2020-06-10 12:21:08 -0400

    [perf] Port tricon-sk to TypeScript.
    Screenshot:
      https://skia-infra-gold.skia.org/img/images/0d188b24d4ef8ada43ee2a2396e7fe66.png
    
    Change-Id: Id6b15d95c32f4757153954c1bc22d1d983de3263
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/295455
    Reviewed-by: Leandro Lovisolo <lovisolo@google.com>
    Commit-Queue: Joe Gregorio <jcgregorio@google.com>

---
+++ perf/modules/tricon2-sk/tricon2-sk_puppeteer_test.ts
@@ -0,0 +1,28 @@
+import * as path from 'path';
+import { expect } from 'chai';
+import {
+  setUpPuppeteerAndDemoPageServer,
+  takeScreenshot,
+} from '../../../puppeteer-tests/util';
+
+describe('tricon2-sk', () => {
+  const testBed = setUpPuppeteerAndDemoPageServer(
+    path.join(__dirname, '..', '..', 'webpack.config.ts')
+  );
+
+  beforeEach(async () => {
+    await testBed.page.goto(`${testBed.baseUrl}/dist/tricon2-sk.html`);
+    await testBed.page.setViewport({ width: 400, height: 400 });
+  });
+
+  it('should render the demo page', async () => {
+    // Smoke test.
+    expect(await testBed.page.$$('tricon2-sk')).to.have.length(9);
+  });
+
+  describe('screenshots', () => {
+    it('shows the default view', async () => {
+      await takeScreenshot(testBed.page, 'perf', 'tricon2-sk');
+    });
+  });
+});
