40a84d70a8fd283970873e240aeccc82f2b100c0
Eric Boren <borenet@google.com>
2020-06-15 15:08:34 -0400

    WIP experimenting with docker and caching go dependencies
    Change-Id: Iea8b343f6aa63951c8f4447bc4bd54e156f38f30

---
+++ go_deps/build_release.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+
+set -x -e
+
+# Create and upload a container image for go_build
+APPNAME=go_build
+
+REPO_ROOT="$(git rev-parse --show-toplevel)"
+
+# At the time of writing, this is "golang:1.14"
+IMAGE="$(cat ${REPO_ROOT}/go_deps/image.sha256)"
+
+# CIPD package information.
+CIPD_URL="https://chrome-infra-packages.appspot.com"
+CIPD_PKG="skia/bots/go_cache"
+HASH="$(git rev-parse HEAD)"
+CIPD_TAG="revision:${HASH}"
+# TODO(borenet): This was copied from docker_build.sh.
+REPO_STATE=clean
+# Detect if we have unchecked in local changes, or if we're not on the master
+# branch (possibly at an older revision).
+git fetch
+# diff-index requires update-index --refresh; see:
+# https://stackoverflow.com/questions/36367190/git-diff-files-output-changes-after-git-status/36439778#36439778
+if git update-index --refresh ; then
+  if ! git diff-index --quiet HEAD -- ; then
+    REPO_STATE=dirty
+  elif ! git merge-base --is-ancestor HEAD origin/master ; then
+    REPO_STATE=dirty
+  fi
+else
+  REPO_STATE=dirty
+fi
+if [ "${REPO_STATE}" = "dirty" ]; then
+  CIPD_TAG="${CIPD_TAG}_${USER}_$(date --utc "+%Y-%m-%dT%H_%M_%SZ")_dirty"
+fi
+
+TMP="$(mktemp -d)"
+GOPATH="${TMP}/go"
+GOCACHE="${GOPATH}/cache"
+mkdir -p "${GOCACHE}"
+
+function go_build() {
+  docker run --rm \
+      --user $(id -u ${USER}):$(id -g ${USER}) \
+      --mount type=bind,destination=/repo,source=${REPO_ROOT},readonly \
+      --mount type=bind,destination=/out,source=${GOPATH} \
+      --workdir /repo \
+      --env GOFLAGS=-mod=readonly \
+      --env GOPATH=/out \
+      --env GOCACHE=/out/cache \
+      --env GOOS=$2 \
+      --env GOARCH=$3 \
+      ${IMAGE} \
+      go $1 -v --trimpath $4
+}
+
+# Populate the GOPATH and GOCACHE.
+PLATFORMS=(linux-amd64 linux-arm darwin-amd64 windows-amd64)
+for PLATFORM in ${PLATFORMS[@]}; do
+  GOOS="$(echo "$PLATFORM" | cut -d'-' -f1)"
+  GOARCH="$(echo "$PLATFORM" | cut -d'-' -f2)"
+  go_build build ${GOOS} ${GOARCH} ./...
+done
+
+# Install the "go-build" task driver. This is required because "go-build" itself
+# builds task drivers, resulting in a chicken-and-egg problem.
+go_build install linux amd64 ./infra/bots/task_drivers/go_build
+
+echo "Wrote ${TMP}"
+
+# Upload the CIPD package.
+cipd create \
+    --service-url ${CIPD_URL} \
+    --name ${CIPD_PKG} \
+    --in ${TMP} \
+    --tag ${CIPD_TAG} \
+    --verification-timeout 30m0s \
+    --install-mode copy \
+    --preserve-writable
+
+echo ${CIPD_TAG} > ${REPO_ROOT}/go_deps/pkg.version
+
+#rm -rf ${TMP}
+
+# Copy files into the right locations in ${ROOT}.
+copy_release_files()
+{
+INSTALL="install -D --verbose --backup=none"
+INSTALL_DIR="install -d --verbose --backup=none"
+
+# Add the dockerfile and binary.
+${INSTALL} --mode=444 -T ../go.mod     ${ROOT}/go.mod
+${INSTALL} --mode=444 -T ../go.sum     ${ROOT}/go.sum
+${INSTALL} --mode=644 -T ./Dockerfile  ${ROOT}/Dockerfile
+for script in $(ls install*.sh); do
+  ${INSTALL} --mode=755 -T ./${script} ${ROOT}/${script}
+done
+}
+
+#source ../bash/docker_build.sh
+#docker inspect --format='{{index .RepoDigests 0}}' go_deps > image.sha256
---
+++ infra/bots/task_drivers/go_build/go_build.go
@@ -0,0 +1,246 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"path"
+	"path/filepath"
+	"regexp"
+	"strings"
+
+	"go.skia.org/infra/go/auth"
+	"go.skia.org/infra/go/common"
+	"go.skia.org/infra/go/test2json"
+	"go.skia.org/infra/go/util"
+	"go.skia.org/infra/task_driver/go/lib/auth_steps"
+	"go.skia.org/infra/task_driver/go/lib/docker"
+	"go.skia.org/infra/task_driver/go/lib/os_steps"
+	"go.skia.org/infra/task_driver/go/td"
+)
+
+/*
+	Build all Golang binaries and tests for the given platform(s).
+*/
+
+var (
+	// Required properties for this task.
+	projectID       = flag.String("project_id", "", "ID of the Google Cloud project.")
+	taskID          = flag.String("task_id", "", "ID of this task.")
+	taskName        = flag.String("task_name", "", "Name of the task.")
+	cachePath       = flag.String("cache-path", "", "Path to the pre-built cache.")
+	repoPath        = flag.String("repo-path", ".", "Path to the repo containing the main module.")
+	image           = flag.String("image", "", "Docker image to use to compile.")
+	outputPath      = flag.String("output-path", "", "Write binaries to this path.")
+	platforms       = common.NewMultiStringFlag("platform", nil, "Platform(s) to test, eg. \"linux-amd64\"")
+	outputWhitelist = common.NewMultiStringFlag("output-whitelist", nil, "Include these binaries in output-path.")
+
+	// Optional flags.
+	local  = flag.Bool("local", false, "True if running locally (as opposed to on the bots)")
+	output = flag.String("o", "", "If provided, dump a JSON blob of step data to the given file. Prints to stdout if '-' is given.")
+)
+
+var (
+	// copyTestPathRegex matches the output of //go_deps/copy_test.sh. It needs
+	// To be kept in sync.
+	copyTestPathRegex = regexp.MustCompile(`Wrote test executable (.+)`)
+)
+
+func main() {
+	// Setup.
+	ctx := td.StartRun(projectID, taskID, taskName, output, local)
+	defer td.EndRun(ctx)
+
+	if *image == "" {
+		td.Fatalf(ctx, "--image is required")
+	}
+	if *platforms == nil {
+		td.Fatalf(ctx, "--platform is required")
+	}
+	if *outputPath == "" {
+		td.Fatalf(ctx, "--output-path is required")
+	}
+	if *cachePath == "" {
+		td.Fatalf(ctx, "--cache-path is required")
+	}
+	repoAbs, err := filepath.Abs(*repoPath)
+	if err != nil {
+		td.Fatal(ctx, err)
+	}
+	hostCachePath, err := filepath.Abs(*cachePath)
+	if err != nil {
+		td.Fatal(ctx, err)
+	}
+	hostOutPath, err := filepath.Abs(*outputPath)
+	if err != nil {
+		td.Fatal(ctx, err)
+	}
+
+	ts, err := auth_steps.Init(ctx, *local, auth.SCOPE_USERINFO_EMAIL, auth.SCOPE_FULL_CONTROL)
+	if err != nil {
+		td.Fatal(ctx, err)
+	}
+	d, err := docker.New(ctx, ts)
+	if err != nil {
+		td.Fatal(ctx, err)
+	}
+	defer func() {
+		if err := d.Cleanup(ctx); err != nil {
+			td.Fatal(ctx, err)
+		}
+	}()
+
+	// Pull the Docker image and create the container.
+	if err := d.Pull(ctx, *image); err != nil {
+		td.Fatal(ctx, err)
+	}
+
+	// Set up the Docker container.
+	dockerRepoPath := "/repo"
+	dockerOutPath := "/out"
+	dockerCachePath := "/gopath"
+	props := &docker.ContainerProps{
+		Image: *image,
+		Mounts: []*docker.Mount{
+			{
+				Type:        docker.MountTypeBind,
+				Source:      repoAbs,
+				Destination: dockerRepoPath,
+				Readonly:    true,
+			},
+			{
+				Type:        docker.MountTypeBind,
+				Source:      hostOutPath,
+				Destination: dockerOutPath,
+				Readonly:    false,
+			},
+			{
+				Type:        docker.MountTypeBind,
+				Source:      hostCachePath,
+				Destination: dockerCachePath,
+				Readonly:    false,
+			},
+		},
+		Command: []string{"/bin/sh"},
+	}
+	platformErrGroup := util.NewNamedErrGroup()
+	for _, platform := range *platforms {
+		// Set up the environment for this platform.
+		platform := platform
+		split := strings.Split(platform, "-")
+		if len(split) != 2 {
+			td.Fatalf(ctx, "Bad format for platform; expected \"os-arch\" but got: %s", platform)
+		}
+		goos := split[0]
+		goarch := split[1]
+		env := map[string]string{
+			"GOOS":    goos,
+			"GOARCH":  goarch,
+			"GOPATH":  dockerCachePath,
+			"GOCACHE": path.Join(dockerCachePath, "cache"),
+		}
+		hostPlatformOutPath := filepath.Join(hostOutPath, platform)
+		dockerPlatformOutPath := path.Join(dockerOutPath, platform)
+		platformErrGroup.Go(platform, func() error {
+			return td.Do(ctx, td.Props(fmt.Sprintf("Build %s", platform)), func(ctx context.Context) error {
+				hostBinTmpPath := filepath.Join(hostPlatformOutPath, "bin-tmp")
+				if err := os_steps.MkdirAll(ctx, hostBinTmpPath); err != nil {
+					return err
+				}
+				dockerBinTmpPath := path.Join(dockerPlatformOutPath, "bin-tmp")
+				hostTestTmpPath := filepath.Join(hostPlatformOutPath, "test-tmp")
+				if err := os_steps.MkdirAll(ctx, hostTestTmpPath); err != nil {
+					return err
+				}
+				dockerTestTmpPath := path.Join(dockerPlatformOutPath, "test-tmp")
+				hostTestPath := filepath.Join(hostPlatformOutPath, "test")
+				if err := os_steps.MkdirAll(ctx, hostTestPath); err != nil {
+					return err
+				}
+				return d.RunInContainer(ctx, props, func(ctx context.Context, c *docker.Container) error {
+					// Build all non-test binaries for this platform.
+					if _, err := c.Run(ctx, dockerRepoPath, env, "ls", "-alh", dockerCachePath); err != nil {
+						return err
+					}
+					if _, err := c.Run(ctx, dockerRepoPath, env, "go", "build", "-v", "--trimpath", "-o", dockerBinTmpPath, "./..."); err != nil {
+						return err
+					}
+					// Copy the whitelisted regular binaries.
+					hostBinOutPath := filepath.Join(hostPlatformOutPath, "bin")
+					if *outputWhitelist == nil {
+						if err := os_steps.Rename(ctx, hostBinTmpPath, hostBinOutPath); err != nil {
+							return err
+						}
+					} else {
+						if err := os_steps.MkdirAll(ctx, hostBinOutPath); err != nil {
+							return err
+						}
+						for _, wl := range *outputWhitelist {
+							src := filepath.Join(hostBinTmpPath, wl)
+							dst := filepath.Join(hostBinOutPath, wl)
+							if err := os_steps.Rename(ctx, src, dst); err != nil {
+								return err
+							}
+						}
+					}
+					os_steps.RemoveAll(ctx, hostBinTmpPath)
+
+					// Build the tests. Unfortunately, there is no command which
+					// builds all of the tests at once and saves the binaries
+					// for future use[1], so we use the -exec flag to "go test",
+					// using a script which copies the executable to the output
+					// directory. Note that the test executables passed to this
+					// script only contain the base name of the package, eg.
+					// "types" => "types.test". To prevent conflicts, the script
+					// appends a random number to the file name, and we use the
+					// JSON output from "go test" to match the resulting files
+					// name back to the full package import paths and then
+					// rename them accordingly.
+					//
+					// As an alternative, I tried using "go list" to find the
+					// packages with test files and then running "go test -c"
+					// for each of those. Doing so sequentially took about 10
+					// minutes; running in parallel resulted in timeouts after a
+					// much longer time, presumably due to contention of some
+					// kind.
+					//
+					// [1] https://github.com/golang/go/issues/15513
+					dockerCopyTestPath := path.Join(dockerRepoPath, "go_deps", "copy_test.sh")
+					execCmd := fmt.Sprintf("%s %s", dockerCopyTestPath, dockerTestTmpPath)
+					// Note: We're skipping vet here because we want to compile
+					// all of the tests and run them regardless of whether vet
+					// passes. We'll need to explicitly run it somewhere else.
+					output, err := c.Run(ctx, dockerRepoPath, env, "go", "test", "-vet=off", "-json", "--trimpath", "-exec", execCmd, "./...")
+					if err != nil {
+						return err
+					}
+					return td.Do(ctx, td.Props("Copy test binaries"), func(ctx context.Context) error {
+						lines := strings.Split(strings.TrimSpace(output), "\n")
+						for _, line := range lines {
+							ev, err := test2json.ParseEvent(line)
+							if err != nil {
+								return err
+							}
+							m := copyTestPathRegex.FindStringSubmatch(ev.Output)
+							if len(m) == 2 {
+								// Get the host-side path to the test binary.
+								testSrc := strings.ReplaceAll(m[1], dockerTestTmpPath, hostTestTmpPath)
+								testDst := filepath.Join(hostTestPath, ev.Package+".test")
+								if err := os_steps.MkdirAll(ctx, filepath.Dir(testDst)); err != nil {
+									return err
+								}
+								if err := os_steps.Rename(ctx, testSrc, testDst); err != nil {
+									return err
+								}
+							}
+						}
+						return os_steps.RemoveAll(ctx, hostTestTmpPath)
+					})
+				})
+			})
+		})
+	}
+	if err := platformErrGroup.Wait(); err != nil {
+		td.Fatal(ctx, err)
+	}
+}
