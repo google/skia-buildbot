46b68d189d299ba74e9aa690e75485a70fbfad0f
Joe Gregorio <jcgregorio@google.com>
2020-06-16 13:54:40 -0400

    [perf] Port json-source-sk to TypeScript.
    Also add demo page and gold test.
    
    Change-Id: I96076e9287ed15216d23ba8192587322e4fbed4c
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/296727
    Reviewed-by: Joe Gregorio <jcgregorio@google.com>
    Commit-Queue: Joe Gregorio <jcgregorio@google.com>

--- perf/modules/json-source-sk/json-source-sk.ts
+++ perf/modules/json-source-sk/json-source-sk.ts
@@ -13,21 +13,33 @@ import { $$ } from 'common-sk/modules/dom';
 import { errorMessage } from 'elements-sk/errorMessage';
 import { jsonOrThrow } from 'common-sk/modules/jsonOrThrow';
 import { ElementSk } from '../../../infra-sk/modules/ElementSk';
+import { SpinnerSk } from 'elements-sk/spinner-sk/spinner-sk';
+import { CommitID, CommitDetailsRequest } from '../json';
+
 import 'elements-sk/spinner-sk';
+import 'elements-sk/styles/buttons';
 
-const template = (ele) => html`
-  <div id=controls>
-    <button @click=${ele._loadSource}>View Source File</button>
-    <button @click=${ele._loadSourceSmall}>View Source File Without Results</button>
-    <spinner-sk id=spinner></spinner-sk>
-  </div>
-  <pre>${ele._json}</pre>
-`;
+export class JSONSourceSk extends ElementSk {
+  private _json: string;
+  private _cid: CommitID | null = null;
+  private _traceid: string;
+  private _spinner: SpinnerSk | null = null;
 
-define('json-source-sk', class extends ElementSk {
+  private static template = (ele: JSONSourceSk) => html`
+    <div id="controls">
+      <button @click=${ele._loadSource}>View Source File</button>
+      <button @click=${ele._loadSourceSmall}
+        >View Source File Without Results</button
+      >
+      <spinner-sk id="spinner"></spinner-sk>
+    </div>
+    <pre>${ele._json}</pre>
+  `;
+
   constructor() {
-    super(template);
+    super(JSONSourceSk.template);
     this._json = '';
+    this._traceid = '';
   }
 
   connectedCallback() {
@@ -36,8 +48,10 @@ define('json-source-sk', class extends ElementSk {
     this._spinner = $$('#spinner', this);
   }
 
-  /** @prop cid {string} A serialized cid.CommitID. */
-  get cid() { return this._cid; }
+  /** @prop cid - The Commit ID. */
+  get cid() {
+    return this._cid;
+  }
 
   set cid(val) {
     this._cid = val;
@@ -45,8 +59,10 @@ define('json-source-sk', class extends ElementSk {
     this._render();
   }
 
-  /** @prop traceid {string} The ID of the trace. */
-  get traceid() { return this._traceid; }
+  /** @prop traceid - The ID of the trace. */
+  get traceid() {
+    return this._traceid;
+  }
 
   set traceid(val) {
     this._traceid = val;
@@ -62,8 +78,8 @@ define('json-source-sk', class extends ElementSk {
     this._loadSourceImpl(true);
   }
 
-  _loadSourceImpl(isSmall) {
-    if (this._spinner.active === true) {
+  _loadSourceImpl(isSmall: boolean) {
+    if (this._spinner!.active === true) {
       return;
     }
     if (!this.traceid) {
@@ -72,11 +88,11 @@ define('json-source-sk', class extends ElementSk {
     if (!this.cid || this.cid.offset === undefined) {
       return;
     }
-    const body = {
+    const body: CommitDetailsRequest = {
       cid: this.cid,
       traceid: this.traceid,
     };
-    this._spinner.active = true;
+    this._spinner!.active = true;
     let url = '/_/details/';
     if (isSmall) {
       url += '?results=false';
@@ -87,13 +103,18 @@ define('json-source-sk', class extends ElementSk {
         'Content-Type': 'application/json',
       },
       body: JSON.stringify(body),
-    }).then(jsonOrThrow).then((json) => {
-      this._spinner.active = false;
-      this._json = JSON.stringify(json, null, '  ');
-      this._render();
-    }).catch((e) => {
-      this._spinner.active = false;
-      errorMessage(e);
-    });
+    })
+      .then(jsonOrThrow)
+      .then((json) => {
+        this._spinner!.active = false;
+        this._json = JSON.stringify(json, null, '  ');
+        this._render();
+      })
+      .catch((e) => {
+        this._spinner!.active = false;
+        errorMessage(e);
+      });
   }
-});
+}
+
+define('json-source-sk', JSONSourceSk);
---
+++ perf/modules/json-source-sk/json-source-sk_puppeteer_test.ts
@@ -0,0 +1,28 @@
+import * as path from 'path';
+import { expect } from 'chai';
+import {
+  setUpPuppeteerAndDemoPageServer,
+  takeScreenshot,
+} from '../../../puppeteer-tests/util';
+
+describe('json-source-sk', () => {
+  const testBed = setUpPuppeteerAndDemoPageServer(
+    path.join(__dirname, '..', '..', 'webpack.config.ts')
+  );
+
+  beforeEach(async () => {
+    await testBed.page.goto(`${testBed.baseUrl}/dist/json-source-sk.html`);
+    await testBed.page.setViewport({ width: 600, height: 600 });
+  });
+
+  it('should render the demo page', async () => {
+    // Smoke test.
+    expect(await testBed.page.$$('json-source-sk')).to.have.length(3);
+  });
+
+  describe('screenshots', () => {
+    it('shows the default view', async () => {
+      await takeScreenshot(testBed.page, 'perf', 'json-source-sk');
+    });
+  });
+});
