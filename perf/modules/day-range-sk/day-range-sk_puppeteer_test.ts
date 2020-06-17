ad6e6b64d3b1f624799410a608c65dc6c59056a3
Joe Gregorio <jcgregorio@google.com>
2020-06-17 17:14:37 -0400

    [perf] Convert day-range-sk to TypeScript.
     - Use the native input control since it it slightly more stylable.
     - Add theme support.
     - Update puppeteer tests.
    
    Change-Id: I0a67e1258ed0d27c9ceb9e641ca615107e9f6e13

--- perf/modules/day-range-sk/day-range-sk_puppeteer_test.ts
+++ perf/modules/day-range-sk/day-range-sk_puppeteer_test.ts
@@ -1,9 +1,14 @@
 import * as path from 'path';
 import { expect } from 'chai';
-import { setUpPuppeteerAndDemoPageServer, takeScreenshot } from '../../../puppeteer-tests/util';
+import {
+  setUpPuppeteerAndDemoPageServer,
+  takeScreenshot,
+} from '../../../puppeteer-tests/util';
 
 describe('day-range-sk', () => {
-  const testBed = setUpPuppeteerAndDemoPageServer(path.join(__dirname, '..', '..', 'webpack.config.ts'));
+  const testBed = setUpPuppeteerAndDemoPageServer(
+    path.join(__dirname, '..', '..', 'webpack.config.ts')
+  );
 
   beforeEach(async () => {
     await testBed.page.goto(`${testBed.baseUrl}/dist/day-range-sk.html`);
@@ -12,22 +17,12 @@ describe('day-range-sk', () => {
 
   it('should render the demo page', async () => {
     // Smoke test.
-    expect(await testBed.page.$$('day-range-sk')).to.have.length(1);
+    expect(await testBed.page.$$('day-range-sk')).to.have.length(3);
   });
 
   describe('screenshots', () => {
     it('shows the default view', async () => {
       await takeScreenshot(testBed.page, 'perf', 'day-range-sk');
-    });
-
-    it('shows the begin date selector', async () => {
-      await testBed.page.click('.begin elix-date-combo-box');
-      await takeScreenshot(testBed.page, 'perf', 'day-range-sk_begin-selector');
-    });
-
-    it('shows the end date selector', async () => {
-      await testBed.page.click('.end elix-date-combo-box');
-      await takeScreenshot(testBed.page, 'perf', 'day-range-sk_end-selector');
     });
   });
 });
