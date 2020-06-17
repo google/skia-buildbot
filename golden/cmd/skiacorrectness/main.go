ab44ced19dc0d9ebd55bf66adf993a729b72a55c
Kevin Lubick <kjlubick@google.com>
2020-06-17 15:22:34 -0400

    [gold] use new by-list page
    Old Polymer page took 1.5-1.6 seconds to load on Skia Infra instance.
    New lit-html page loads in 200ms.
    
    Old polymer code will be deleted in followup CL.
    
    Bug: skia:9525
    Change-Id: Ie8bdd3cd39e78609e8d66a59f97e653b0f577b3e

--- golden/cmd/skiacorrectness/main.go
+++ golden/cmd/skiacorrectness/main.go
@@ -577,6 +577,7 @@ func main() {
 	loggedRouter.HandleFunc("/detail", templateHandler("details.html"))
 	loggedRouter.HandleFunc("/details", templateHandler("details.html"))
 	loggedRouter.HandleFunc("/cl/{system}/{id}", handlers.ChangeListSearchRedirect)
+	loggedRouter.HandleFunc("/list", templateHandler("by_test_list.html"))
 
 	// This route handles the legacy polymer "single page" app model
 	loggedRouter.PathPrefix("/").Handler(templateHandler("index.html"))
