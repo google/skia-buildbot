9012ab4c65c7a91b484fc53e6c9afa2837e88016
Eric Boren <borenet@google.com>
2020-06-18 15:31:52 -0400

    New Docker-based Golang CI flow
    Change-Id: I6396fe05d967c90b557884d3f808c83950f79752

--- perf/go/config/config_test.go
+++ perf/go/config/config_test.go
@@ -1,22 +1,26 @@
 package config
 
 import (
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"go.skia.org/infra/go/testutils"
 	"go.skia.org/infra/go/testutils/unittest"
 )
 
 func TestInstanceConfigFromFile_Success(t *testing.T) {
 	unittest.SmallTest(t)
-	instanceConfig, err := InstanceConfigFromFile("./testdata/good_instance_config.json")
+	f := filepath.Join(testutils.TestDataDir(t), "good_instance_config.json")
+	instanceConfig, err := InstanceConfigFromFile(f)
 	require.NoError(t, err)
 	assert.Equal(t, int32(256), instanceConfig.DataStoreConfig.TileSize)
 }
 
 func TestInstanceConfigFromFile_FailureOnMalformedJSON(t *testing.T) {
 	unittest.SmallTest(t)
-	_, err := InstanceConfigFromFile("./testdata/malformed_instance_config.json")
+	f := filepath.Join(testutils.TestDataDir(t), "malformed_instance_config.json")
+	_, err := InstanceConfigFromFile(f)
 	require.Error(t, err)
 }
