94f2cbe3721ead8f39deea1d493aad078e72287e
Joe Gregorio <jcgregorio@google.com>
2020-06-18 12:40:11 -0400

    [perf] Port query-chooser-sk to TypeScript.
    Change-Id: I598859b77ccb81dc75e6d070665cf227d8f15759
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/297337
    Reviewed-by: Joe Gregorio <jcgregorio@google.com>
    Commit-Queue: Joe Gregorio <jcgregorio@google.com>

--- perf/modules/query-chooser-sk/query-chooser-sk-demo.js
+++
@@ -1,5 +0,0 @@
-import './index';
-import { fetchMock } from '@bundled-es-modules/fetch-mock';
-
-// Wait 1s before returning the content so we can see the spinner in action.
-fetchMock.post('/', async () => new Promise((res) => setTimeout(() => res({ count: Math.floor(Math.random() * 2000) }), 1000)));
