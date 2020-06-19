edac2c1edcf5342a2d8bae307b543f676e4f27cd
Joe Gregorio <jcgregorio@google.com>
2020-06-19 16:11:31 -0400

    sort-sk to typescript.
    Change-Id: Id7b21c52072e41c3b4613d5495caa3c71c0202e0

---
+++ infra-sk/modules/sort-sk/sort-sk_puppeteer_test.ts
@@ -0,0 +1,28 @@
+import * as path from 'path';
+import { expect } from 'chai';
+import {
+  setUpPuppeteerAndDemoPageServer,
+  takeScreenshot,
+} from '../../../puppeteer-tests/util';
+
+describe('sort-sk', () => {
+  const testBed = setUpPuppeteerAndDemoPageServer(
+    path.join(__dirname, '..', '..', 'webpack.config.ts')
+  );
+
+  beforeEach(async () => {
+    await testBed.page.goto(`${testBed.baseUrl}/sort-sk.html`);
+    await testBed.page.setViewport({ width: 400, height: 750 });
+  });
+
+  it('should render the demo page', async () => {
+    // Smoke test.
+    expect(await testBed.page.$$('sort-sk')).to.have.length(4);
+  });
+
+  describe('screenshots', () => {
+    it('shows the default view', async () => {
+      await takeScreenshot(testBed.page, 'perf', 'sort-sk');
+    });
+  });
+});
