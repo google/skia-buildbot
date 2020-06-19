4d31df4f21bbd4828fd38c39358e62e8bbaefc17
Joe Gregorio <jcgregorio@google.com>
2020-06-19 13:07:17 -0400

    [perf] Add new bucket for Flutter instance.
    Change-Id: I5458d23a8cf74c212ece0e187650347e05dea3e8

--- perf/create-ingestion-flutter-pubsub-topics.sh
+++ perf/create-ingestion-flutter-pubsub-topics.sh
@@ -7,5 +7,6 @@ set -e -x
 PROJECT_ID=skia-public
 TOPIC=perf-ingestion-flutter
 
-perf-tool config create-pubsub-topics --big_table_config=flutter
+#perf-tool config create-pubsub-topics --config_filename=./configs/flutter.json
 gsutil notification create -f json -e OBJECT_FINALIZE -t projects/${PROJECT_ID}/topics/${TOPIC} -p flutter-engine gs://flutter-skia-perf
+gsutil notification create -f json -e OBJECT_FINALIZE -t projects/${PROJECT_ID}/topics/${TOPIC} -p flutter-flutter gs://flutter-skia-perf
