8af5ca4bb6c1fa6e4c56491a298dcfad5d57c3be
Leandro Lovisolo <lovisolo@google.com>
2020-06-19 14:46:00 -0400

    [gold] Port filter-dialog-sk to lit-html.
    Bug: skia:9525
    Change-Id: I368e28148d18e89b03434438da49d37ee685a798

---
+++ golden/modules/filter-dialog-sk/filter-dialog-sk.ts
@@ -0,0 +1,222 @@
+/**
+ * @module modules/filter-dialog-sk
+ * @description <h2><code>filter-dialog-sk</code></h2>
+ *
+ * A dialog that provides input elements to filter search results by metric values.
+ *
+ * Events:
+ *   edit: Emitted when the user clicks the "Filter" button (and closes the dialog in the process).
+ *         The "detail" field of the event contains the filter values entered by the user.
+ */
+
+import { define } from 'elements-sk/define';
+import { html } from 'lit-html';
+import { live } from 'lit-html/directives/live';
+import { ElementSk } from '../../../infra-sk/modules/ElementSk';
+import dialogPolyfill from 'dialog-polyfill';
+import { $$ } from 'common-sk/modules/dom';
+import { deepCopy } from 'common-sk/modules/object';
+import { ParamSet, fromParamSet, toParamSet } from 'common-sk/modules/query';
+import { QueryDialogSk } from '../query-dialog-sk/query-dialog-sk';
+
+import 'elements-sk/styles/buttons';
+import 'elements-sk/checkbox-sk';
+import '../query-dialog-sk';
+import '../../../infra-sk/modules/paramset-sk';
+
+export interface FilterDialogSkValue {
+  diffConfig: ParamSet;
+  minRGBADelta: number;
+  maxRGBADelta: number;
+  maxDiff: number;
+  metric: 'pixel' | 'percent' | 'combined';
+  sortOrder: 'ascending' | 'descending';
+  mustHaveReferenceImage: boolean;
+};
+
+// This template produces an <input type=range> (a "slider") and an <input type=number> that
+// reflect each other's values. When one changes, the other is updated with the new value, and
+// viceversa.
+const numericParamTemplate = (id: string,
+                              label: string,
+                              setterFn: (value: number) => void,
+                              value: number = 0,
+                              min: number,
+                              max: number,
+                              step: number) => {
+  const onInput = (e: InputEvent) => {
+    const target = e.target as HTMLInputElement;
+
+    // Set the corresponding field in the FilterDialogSk instance.
+    setterFn(parseFloat(target.value));
+
+    // Sync up the range and number inputs.
+    const otherSelector = `input[type="${target.type === 'range' ? 'number' : 'range'}"]`;
+    const other = target.parentElement!.querySelector<HTMLInputElement>(otherSelector)!;
+    other.value = target.value;
+  };
+  return html`
+    <label for="${id}">${label}</label>
+    <div class=numeric-param
+         id=${id}-numeric-param>
+      <input type=range
+             id=${id}
+             .value=${live(value.toString())}
+             min=${min}
+             max=${max}
+             step=${step}
+             @input=${onInput}/>
+      <input type=number
+             .value=${live(value.toString())}
+             min=${min}
+             max=${max}
+             step=${step}
+             @input=${onInput}/>
+    </div>`;
+}
+
+export class FilterDialogSk extends ElementSk {
+
+  private static _template = (el: FilterDialogSk) => html`
+    <query-dialog-sk .submitButtonLabel=${'Select'}
+                     @edit=${el._queryDialogEdit}>
+    </query-dialog-sk>
+
+    <dialog class=filter-dialog>
+      <div class=content>
+        <span class=label>Right hand query:</span>
+        <div class=right-hand-query>
+          <div class=query>
+            ${!el._value || Object.keys(el._value!.diffConfig).length === 0
+              ? html`<div class=empty-placeholder>Empty.</div>`
+              : html`<paramset-sk .paramsets=${live([el._value?.diffConfig])}></paramset-sk>`}
+          </div>
+          <button class=edit-query @click=${el._openQueryDialog}>Edit query</button>
+        </div>
+
+        ${numericParamTemplate(
+          'min-rgba-delta',
+          'Min RGBA delta:',
+          /* setterFn= */ (val) => el._value!.minRGBADelta = val,
+          /* value= */ el._value?.minRGBADelta,
+          /* min= */ 0,
+          /* max= */ 255,
+          /* step= */ 1)}
+
+        ${numericParamTemplate(
+          'max-rgba-delta',
+          'Max RGBA delta:',
+          /* setterFn= */ (val) => el._value!.maxRGBADelta = val,
+          /* value= */ el._value?.maxRGBADelta,
+          /* min= */ 0,
+          /* max= */ 255,
+          /* step= */ 1)}
+
+        ${numericParamTemplate(
+          'max-diff',
+          'Max diff:',
+          /* setterFn= */ (val) => el._value!.maxDiff = val,
+          /* value= */ el._value?.maxDiff,
+          /* min= */ -1,
+          /* max= */ 1,
+          /* step= */ 0.05)}
+
+        <label for=metric>Metric:</label>
+        <select id=metric
+                .value=${live(el._value?.metric)}
+                @change=${el._metricChanged}>
+          <option value=combined>Combined</option>
+          <option value=percent>Percent</option>
+          <option value=pixel>Pixel</option>
+        </select>
+
+        <label for=sort-order>Sort order:</label>
+        <select id=sort-by
+                .value=${live(el._value?.sortOrder)}
+                @change=${el._sortOrderChanged}>
+          <option value=ascending>Ascending</option>
+          <option value=descending>Descending</option>
+        </select>
+
+        <checkbox-sk id=must-have-reference-image
+                     label="Must have a reference image."
+                     ?checked=${live(el._value?.mustHaveReferenceImage)}
+                     @change=${el._mustHaveReferenceImageChanged}>
+        </checkbox-sk>
+      </div>
+
+      <div class=buttons>
+        <button class="filter action" @click=${el._filterBtnClicked}>Filter</button>
+        <button class=cancel @click=${el._cancelBtnClicked}>Cancel</button>
+      </div>
+    </dialog>`;
+
+  private _dialog: HTMLDialogElement | null = null;
+  private _queryDialogSk: QueryDialogSk | null = null;
+
+  private _paramSet: ParamSet | null = null;
+  private _value: FilterDialogSkValue | null = null;
+
+  constructor() {
+    super(FilterDialogSk._template);
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    this._render();
+    this._dialog = $$('dialog.filter-dialog', this);
+    this._queryDialogSk = $$('query-dialog-sk', this);
+    dialogPolyfill.registerDialog(this._dialog!);
+  }
+
+  open(paramSet: ParamSet, value: FilterDialogSkValue) {
+    this._paramSet = paramSet;
+
+    // We make a copy of the caller's value object because the value object gets updated on user
+    // input, and if the user clicks the "Cancel" button after making some changes, we want the
+    // caller's value object to remain intact.
+    this._value = deepCopy(value);
+
+    this._render();
+    this._dialog?.showModal();
+  }
+
+  private _openQueryDialog() {
+    this._queryDialogSk!.open(this._paramSet!, fromParamSet(this._value!.diffConfig));
+  }
+
+  private _queryDialogEdit(e: CustomEvent<string>) {
+    e.stopPropagation(); // Necessary because filter-dialog-sk also emits an "edit" event.
+    this._value!.diffConfig = toParamSet(e.detail);
+    this._render();
+  }
+
+  private _metricChanged(e: InputEvent) {
+    const value = (e.target as HTMLSelectElement).value as 'pixel' | 'percent' | 'combined';
+    this._value!.metric = value;
+  }
+
+  private _sortOrderChanged(e: InputEvent) {
+    const value = (e.target as HTMLSelectElement).value as 'ascending' | 'descending';
+    this._value!.sortOrder = value;
+  }
+
+  private _mustHaveReferenceImageChanged(e: InputEvent) {
+    const value = (e.target as HTMLInputElement).checked;
+    this._value!.mustHaveReferenceImage = value;
+  }
+
+  private _filterBtnClicked() {
+    this._dialog!.close();
+    this.dispatchEvent(new CustomEvent<FilterDialogSkValue>('edit', {
+      bubbles: true,
+      detail: this._value!
+    }));
+  }
+
+  private _cancelBtnClicked() {
+    this._dialog!.close();
+  }
+}
+
+define('filter-dialog-sk', FilterDialogSk);
---
+++ golden/modules/filter-dialog-sk/filter-dialog-sk_test.ts
@@ -0,0 +1,183 @@
+import './index';
+
+import { setUpElementUnderTest, eventPromise, noEventPromise } from '../../../infra-sk/modules/test_util';
+import { FilterDialogSk, FilterDialogSkValue } from './filter-dialog-sk';
+import { ParamSet } from 'common-sk/modules/query';
+import { $, $$ } from 'common-sk/modules/dom';
+import { CheckOrRadio } from 'elements-sk/checkbox-sk/checkbox-sk';
+
+const expect = chai.expect;
+
+describe('filter-dialog-sk', () => {
+  const newInstance = setUpElementUnderTest<FilterDialogSk>('filter-dialog-sk');
+
+  let filterDialogSk: FilterDialogSk;
+  beforeEach(() => {
+    filterDialogSk = newInstance();
+  });
+
+  const paramSet: ParamSet = {
+    'car make': ['chevrolet', 'dodge', 'ford', 'lincoln motor company'],
+    'color': ['blue', 'green', 'red'],
+    'used': ['yes', 'no'],
+    'year': ['2020', '2019', '2018', '2017', '2016', '2015']
+  };
+
+  const value: FilterDialogSkValue = {
+    diffConfig: {
+      'car make': ['chevrolet', 'dodge', 'ford'],
+      'color': ['blue'],
+      'year': ['2020', '2019']
+    },
+    minRGBADelta: 0,
+    maxRGBADelta: 255,
+    maxDiff: -1,
+    metric: 'combined',
+    sortOrder: 'descending',
+    mustHaveReferenceImage: false
+  };
+
+  const differentValue: FilterDialogSkValue = {
+    diffConfig: {
+      'color': ['green'],
+      'used': ['yes', 'no'],
+    },
+    minRGBADelta: 50,
+    maxRGBADelta: 100,
+    maxDiff: 0,
+    metric: 'percent',
+    sortOrder: 'ascending',
+    mustHaveReferenceImage: true
+  };
+
+  it('should open the dialog with the given values', () => {
+    expect(isDialogOpen()).to.be.false;
+    filterDialogSk.open(paramSet, value);
+    expect(isDialogOpen()).to.be.true;
+    expect(getDisplayedValue()).to.deep.equal(value);
+  });
+
+  it('should be possible to reopen the dialog with a different value', () => {
+    filterDialogSk.open(paramSet, value);
+    clickCancelBtn();
+
+    filterDialogSk.open(paramSet, differentValue);
+    expect(getDisplayedValue()).to.deep.equal(differentValue);
+  });
+
+  describe('filter button', () => {
+    it('closes the dialog', () => {
+      filterDialogSk.open(paramSet, value);
+      clickFilterBtn();
+      expect(isDialogOpen()).to.be.false;
+    })
+
+    it('returns unmodified value via the "edit" event when clicked with no changes', async () => {
+      filterDialogSk.open(paramSet, value);
+
+      const editEvent = eventPromise<CustomEvent<FilterDialogSkValue>>('edit');
+      clickFilterBtn();
+      const newValue = (await editEvent).detail;
+
+      expect(newValue).to.deep.equal(value);
+    });
+
+    it('returns new value via the "edit" event when clicked with some changes', async () => {
+      filterDialogSk.open(paramSet, value);
+      setDifferentValue();
+
+      const editEvent = eventPromise<CustomEvent<FilterDialogSkValue>>('edit');
+      clickFilterBtn();
+      const newValue = (await editEvent).detail;
+
+      expect(newValue).to.deep.equal(differentValue);
+    });
+  });
+
+  describe('cancel button', () => {
+    it('closes the dialog without emitting the "edit" event', async () => {
+      filterDialogSk.open(paramSet, value);
+
+      const noEvent = noEventPromise('edit');
+      clickCancelBtn();
+      await noEvent;
+
+      expect(isDialogOpen()).to.be.false;
+    });
+
+    it('discards any changes when reopened with same value', async () => {
+      filterDialogSk.open(paramSet, value);
+      setDifferentValue();
+      clickCancelBtn();
+
+      filterDialogSk.open(paramSet, value);
+      expect(getDisplayedValue()).to.deep.equal(value);
+    });
+  });
+
+  // Clicks the "Filter" button. This closes the dialog.
+  const clickFilterBtn =
+    () => $$<HTMLButtonElement>('.filter-dialog button.filter', filterDialogSk)!.click();
+
+  // Clicks the "Cancel" button. This closes the dialog.
+  const clickCancelBtn =
+    () => $$<HTMLButtonElement>('.filter-dialog button.cancel', filterDialogSk)!.click();
+
+  const isDialogOpen = () => $$<HTMLDialogElement>('dialog.filter-dialog', filterDialogSk)!.open;
+
+  // Extracts the FilterDialogSkValue currently displayed by looking at the component's UI.
+  const getDisplayedValue = (): FilterDialogSkValue => ({
+    diffConfig: getRightHandQuery(),
+    minRGBADelta: parseFloat($$<HTMLInputElement>('#min-rgba-delta', filterDialogSk)!.value),
+    maxRGBADelta: parseFloat($$<HTMLInputElement>('#max-rgba-delta', filterDialogSk)!.value),
+    maxDiff: parseFloat($$<HTMLInputElement>('#max-diff', filterDialogSk)!.value),
+    metric: $$<HTMLSelectElement>('#metric', filterDialogSk)!.value as
+            'pixel' | 'percent' | 'combined',
+    sortOrder: $$<HTMLSelectElement>('#sort-by', filterDialogSk)!.value as
+               'ascending' | 'descending',
+    mustHaveReferenceImage: $$<CheckOrRadio>('#must-have-reference-image')!.checked
+  });
+
+  // Returns the ParamSet displayed by the paramset-sk component.
+  const getRightHandQuery = (): ParamSet => {
+    const paramSet: ParamSet = {};
+    $('.filter-dialog paramset-sk tr', filterDialogSk).forEach((tr, i) => {
+      if (i === 0) return; // Skip the first row, which usually displays titles (empty in our case).
+      const key = $$('th', tr)!.textContent!;
+      const values = $('div', tr).map(div => div.textContent!);
+      paramSet[key] = values;
+    })
+    return paramSet;
+  };
+
+  // Sets dialog's value to match differentValue via simulated UI interactions.
+  const setDifferentValue = () => {
+    // Set right hand query.
+    $$<HTMLButtonElement>('.edit-query', filterDialogSk)!.click();
+    const queryDialogSk = $$('query-dialog-sk', filterDialogSk)!;
+    $$<HTMLButtonElement>('.clear_selections', queryDialogSk)!.click();
+    $$<HTMLDivElement>('select-sk div:nth-child(2)', queryDialogSk)!.click(); // Color.
+    $$<HTMLDivElement>('multi-select-sk div:nth-child(2)', queryDialogSk)!.click(); // Green.
+    $$<HTMLDivElement>('select-sk div:nth-child(3)', queryDialogSk)!.click(); // Used.
+    $$<HTMLDivElement>('multi-select-sk div:nth-child(1)', queryDialogSk)!.click(); // Yes.
+    $$<HTMLDivElement>('multi-select-sk div:nth-child(2)', queryDialogSk)!.click(); // No.
+    $$<HTMLButtonElement>('.show-matches', queryDialogSk)!.click();
+
+    setValueAndDispatchEvent('#min-rgba-delta', '50', 'input');
+    setValueAndDispatchEvent('#max-rgba-delta', '100', 'input');
+    setValueAndDispatchEvent('#max-diff', '0', 'input');
+    setValueAndDispatchEvent('#metric', 'percent', 'change');
+    setValueAndDispatchEvent('#sort-by', 'ascending', 'change');
+
+    const mustHaveReferenceImageCheckBox =
+      $$<CheckOrRadio>('#must-have-reference-image', filterDialogSk)!;
+    mustHaveReferenceImageCheckBox.checked = true;
+    mustHaveReferenceImageCheckBox.dispatchEvent(new Event('change', {bubbles: true}));
+  };
+
+  const setValueAndDispatchEvent = (elementId: string, value: string, event: string) => {
+    const element = $$<HTMLInputElement | HTMLSelectElement>(elementId, filterDialogSk)!;
+    element.value = value;
+    element.dispatchEvent(new Event(event, {bubbles: true}));
+  }
+});
