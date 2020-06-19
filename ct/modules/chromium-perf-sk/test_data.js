2fd70e1f9ba19ac79f7bcc7d55c4290a3590f081
Weston Tracey <westont@google.com>
2020-06-19 00:19:35 -0400

    Initial addition of chromium-perf-sk directory.
    Change-Id: Ib09ecb2b12a19ec7eb6494bfb6135afa22d6bafc

---
+++ ct/modules/chromium-perf-sk/test_data.js
@@ -0,0 +1,17 @@
+export const benchmarks = [
+  'ad_tagging.cluster_telemetry',
+  'generic_trace_ct',
+  'leak_detection.cluster_telemetry',
+  'loading.cluster_telemetry',
+  'memory.cluster_telemetry',
+  'rasterize_and_record_micro_ct',
+  'rendering.cluster_telemetry',
+  'repaint_ct',
+  'usecounter_ct',
+  'v8.loading.cluster_telemetry',
+  'v8.loading_runtime_stats.cluster_telemetry'];
+
+export const platforms = [
+  'Android (Pixel2 devices)',
+  'Linux (Ubuntu18.04 machines)',
+  'Windows (2016 DataCenter Server cloud instances)'];
