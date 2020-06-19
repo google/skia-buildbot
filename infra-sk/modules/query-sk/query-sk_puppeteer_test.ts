3060cfae7069e850b7875f46a21d8f5a48a5f396
Joe Gregorio <jcgregorio@google.com>
2020-06-19 09:06:08 -0400

    [infra-sk] Add gold tests for query-sk.
    Also add theme'd versions to the demo page.
    
    Change-Id: I2b2ede4b07f8b3cd11e9800c43f9cafec5aef106
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/296802
    Reviewed-by: Joe Gregorio <jcgregorio@google.com>
    Commit-Queue: Joe Gregorio <jcgregorio@google.com>

---
+++ infra-sk/modules/query-sk/query-sk_puppeteer_test.ts
@@ -0,0 +1,28 @@
+import * as path from 'path';
+import { expect } from 'chai';
+import {
+  setUpPuppeteerAndDemoPageServer,
+  takeScreenshot,
+} from '../../../puppeteer-tests/util';
+
+describe('query-sk', () => {
+  const testBed = setUpPuppeteerAndDemoPageServer(
+    path.join(__dirname, '..', '..', 'webpack.config.ts')
+  );
+
+  beforeEach(async () => {
+    await testBed.page.goto(`${testBed.baseUrl}/query-sk.html`);
+    await testBed.page.setViewport({ width: 600, height: 1100 });
+  });
+
+  it('should render the demo page', async () => {
+    // Smoke test.
+    expect(await testBed.page.$$('query-sk')).to.have.length(3);
+  });
+
+  describe('screenshots', () => {
+    it('shows the default view', async () => {
+      await takeScreenshot(testBed.page, 'perf', 'query-sk');
+    });
+  });
+});
