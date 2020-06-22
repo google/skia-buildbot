5f9f35c72da7573ffac6ff04d5d65df1a1fd942b
Eric Boren <borenet@google.com>
2020-06-22 12:20:05 -0400

    New Docker-based Golang CI flow
    Change-Id: I6396fe05d967c90b557884d3f808c83950f79752

--- fiddlek/go/source/source_test.go
+++ fiddlek/go/source/source_test.go
@@ -1,15 +1,17 @@
 package source
 
 import (
+	"path/filepath"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"go.skia.org/infra/go/testutils"
 	"go.skia.org/infra/go/testutils/unittest"
 )
 
 func TestNew(t *testing.T) {
 	unittest.SmallTest(t)
-	s, err := New("../../source")
+	s, err := New(filepath.Join(testutils.GetRepoRoot(t), "fiddlek", "source"))
 	assert.NoError(t, err)
 	assert.True(t, len(s.thumbnails) > 5)
 }
