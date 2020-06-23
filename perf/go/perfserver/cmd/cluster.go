8dc650a12e828ef86dbebe5aa772152ecd1ca9ce
Joe Gregorio <jcgregorio@google.com>
2020-06-23 14:54:30 -0400

    [perf] Rename the alert sub-command to cluster and actually implement it.
    Change-Id: I891fbfcd2dff4db2c43dc892819cb5688ec28395
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/298498
    Reviewed-by: Joe Gregorio <jcgregorio@google.com>
    Reviewed-by: Ravi Mistry <rmistry@google.com>
    Commit-Queue: Joe Gregorio <jcgregorio@google.com>

---
+++ perf/go/perfserver/cmd/cluster.go
@@ -0,0 +1,33 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"go.skia.org/infra/perf/go/config"
+	"go.skia.org/infra/perf/go/frontend"
+)
+
+var clusterFlags config.FrontendFlags
+
+// clusterCmd represents the alert command
+var clusterCmd = &cobra.Command{
+	Use:   "cluster",
+	Short: "Run the regression detection process.",
+	Long: `Continuously runs over all the configured alerts
+and looks for regressions as new data arrives.
+`,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		// Always to clustering.
+		clusterFlags.DoClustering = true
+		f, err := frontend.New(&clusterFlags, cmd.LocalFlags())
+		if err != nil {
+			return err
+		}
+		f.Serve()
+		return nil
+	},
+}
+
+func clusterInit() {
+	rootCmd.AddCommand(clusterCmd)
+	frontendFlags.Register(clusterCmd.Flags())
+}
