26c69790a4f6114bc22578017a235444e0dbe9b2
Eric Boren <borenet@google.com>
2020-06-23 07:28:49 -0400

    [autoroll] No error for failure to retrieve sheriff
    Change-Id: Ie0d2b91acb98642eecbca648cecf44e027f9e3a7
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/298336
    Reviewed-by: Ravi Mistry <rmistry@google.com>
    Commit-Queue: Eric Boren <borenet@google.com>

--- autoroll/go/roller/sheriff.go
+++ autoroll/go/roller/sheriff.go
@@ -9,6 +9,7 @@ import (
 	"go.skia.org/infra/go/metrics2"
 	"go.skia.org/infra/go/rotations"
 	"go.skia.org/infra/go/sklog"
+	"go.skia.org/infra/go/util"
 )
 
 // GetSheriff retrieves the current sheriff list.
@@ -17,21 +18,19 @@ func GetSheriff(metricsName string, sheriffSources, backupSheriffs []string) ([]
 		"roller": metricsName,
 	}
 	m := metrics2.GetInt64Metric("autoroll_get_sheriff_success", tags)
-	allEmails := []string{}
+	success := int64(1)
+	allEmails := util.StringSet{}
 	for _, s := range sheriffSources {
 		emails, err := getSheriffHelper(s)
 		if err != nil {
-			sklog.Errorf("Failed to retrieve sheriff(s): %s", err)
-			m.Update(0)
-			if len(backupSheriffs) == 0 {
-				return nil, fmt.Errorf("Failed to retrieve sheriffs and no backup sheriffs supplied!  %s", err)
-			}
-			return backupSheriffs, nil
+			sklog.Errorf("Failed to retrieve sheriff(s) from %s: %s", s, err)
+			success = 0
+			emails = backupSheriffs
 		}
-		allEmails = append(allEmails, emails...)
+		allEmails.AddLists(emails)
 	}
-	m.Update(1)
-	return allEmails, nil
+	m.Update(success)
+	return allEmails.Keys(), nil
 }
 
 // Helper for loading the sheriff list.
