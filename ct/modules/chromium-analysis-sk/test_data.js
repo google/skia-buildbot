0274c75620dc2342e326a87f6df92324776f7d48
Weston Tracey <westont@google.com>
2020-06-24 16:32:20 -0400

    Visually complete except for run_on_gce
    Change-Id: Ia51e0143dd171ebbd0538bc73e5573f27f7aefb0

---
+++ ct/modules/chromium-analysis-sk/test_data.js
@@ -0,0 +1,20 @@
+export const benchmarks_platforms = {
+  benchmarks: {
+    'ad_tagging.cluster_telemetry': 'https://docs.google.com/document/d/1zlWQoLjGuYOWDR_vkVRYoVbU89JetNDOlcDuOaNAzDc/',
+    generic_trace_ct: 'https://docs.google.com/document/d/1vGd7dnrxayMYHPO72wWkwTvjMnIRrel4yxzCr1bMiis/',
+    'leak_detection.cluster_telemetry': 'https://docs.google.com/document/d/1wUWa7dWUdvr6dLdYHFfMQdnvgzt7lrrvzYfpAK-_6e0/',
+    'loading.cluster_telemetry': 'https://cs.chromium.org/chromium/src/tools/analysis/contrib/cluster_telemetry/v8_loading_ct.py',
+    'memory.cluster_telemetry': 'https://cs.chromium.org/chromium/src/tools/analysis/contrib/cluster_telemetry/memory_ct.py',
+    rasterize_and_record_micro_ct: 'https://cs.chromium.org/chromium/src/tools/analysis/contrib/cluster_telemetry/rasterize_and_record_micro_ct.py',
+    'rendering.cluster_telemetry': 'https://cs.chromium.org/chromium/src/tools/analysis/contrib/cluster_telemetry/rendering_ct.py',
+    repaint_ct: 'https://cs.chromium.org/chromium/src/tools/analysis/contrib/cluster_telemetry/repaint.py',
+    usecounter_ct: 'https://docs.google.com/document/d/1FSzJm2L2ow6pZTM_CuyHNJecXuX7Mx3XmBzL4SFHyLA/',
+    'v8.loading.cluster_telemetry': 'https://cs.chromium.org/chromium/src/tools/analysis/contrib/cluster_telemetry/v8_loading_ct.py',
+    'v8.loading_runtime_stats.cluster_telemetry': 'https://cs.chromium.org/chromium/src/tools/analysis/contrib/cluster_telemetry/v8_loading_runtime_stats_ct.py',
+  },
+  platforms: {
+    Android: 'Android (Pixel2 devices)',
+    Linux: 'Linux (Ubuntu18.04 machines)',
+    Windows: 'Windows (2016 DataCenter Server cloud instances)',
+  },
+};
