5eb14e6b15f9d208a28f85e40f1add48b86410b1
Weston Tracey <westont@google.com>
2020-06-25 12:39:02 -0400

    Visually complete except for run_on_gce
    Change-Id: Ia51e0143dd171ebbd0538bc73e5573f27f7aefb0

---
+++ ct/modules/chromium-analysis-sk/chromium-analysis-sk-demo.js
@@ -0,0 +1,25 @@
+import './index';
+import '../../../infra-sk/modules/theme-chooser-sk';
+import { $$ } from 'common-sk/modules/dom';
+import { fetchMock } from 'fetch-mock';
+import { benchmarks_platforms } from './test_data';
+import { pageSets } from '../pageset-selector-sk/test_data';
+import { priorities } from '../task-priority-sk/test_data';
+import { chromiumPatchResult } from '../patch-sk/test_data';
+import 'elements-sk/error-toast-sk';
+
+fetchMock.config.overwriteRoutes = false;
+fetchMock.post('begin:/_/page_sets/', pageSets);
+fetchMock.post('begin:/_/benchmarks_platforms/', benchmarks_platforms);
+fetchMock.get('begin:/_/task_priorities/', priorities);
+fetchMock.post('begin:/_/cl_data', chromiumPatchResult, { delay: 1000 });
+// For determining running tasks for the user we just say 2.
+fetchMock.postOnce('begin:/_/get', {
+  data: [], ids: [], pagination: { offset: 0, size: 1, total: 2 }, permissions: [],
+});
+fetchMock.post('begin:/_/get', {
+  data: [], ids: [], pagination: { offset: 0, size: 1, total: 0 }, permissions: [],
+});
+
+const chromiumPerf = document.createElement('chromium-analysis-sk');
+$$('#container').appendChild(chromiumPerf);
