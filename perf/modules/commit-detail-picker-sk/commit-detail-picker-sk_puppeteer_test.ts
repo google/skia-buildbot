9d2e04d4baafa08a590fbf9cf047e0a0fed7d8b9
Joe Gregorio <jcgregorio@google.com>
2020-06-26 16:20:26 -0400

    [perf] Port commit-detail-picker-sk to TypeScript.
    Also add Gold test.
    
    Change-Id: I87e212ec7232d67369e0c65786b97eb5d057ca05

---
+++ perf/modules/commit-detail-picker-sk/commit-detail-picker-sk_puppeteer_test.ts
@@ -0,0 +1,30 @@
+import * as path from 'path';
+import { expect } from 'chai';
+import {
+  setUpPuppeteerAndDemoPageServer,
+  takeScreenshot,
+} from '../../../puppeteer-tests/util';
+
+describe('commit-detail-picker-sk', () => {
+  const testBed = setUpPuppeteerAndDemoPageServer(
+    path.join(__dirname, '..', '..', 'webpack.config.ts')
+  );
+
+  beforeEach(async () => {
+    await testBed.page.goto(
+      `${testBed.baseUrl}/dist/commit-detail-picker-sk.html`
+    );
+    await testBed.page.setViewport({ width: 500, height: 500 });
+  });
+
+  it('should render the demo page', async () => {
+    // Smoke test.
+    expect(await testBed.page.$$('commit-detail-picker-sk')).to.have.length(2);
+  });
+
+  describe('screenshots', () => {
+    it('shows the default view', async () => {
+      await takeScreenshot(testBed.page, 'perf', 'commit-detail-picker-sk');
+    });
+  });
+});
