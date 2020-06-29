feb775c7a6335a717cc705bc7f4849c8fa4d66dd
Joe Gregorio <jcgregorio@google.com>
2020-06-29 09:51:38 -0400

    [perf] calendar-sk
    Change-Id: I421e48106ed6649fb490b22c0ca5b3cdc2c11d04

---
+++ perf/modules/calendar-sk/calendar-sk-demo.ts
@@ -0,0 +1,18 @@
+import './index.ts';
+import { CalendarSk, CalendarSkChangeEventDetail } from './calendar-sk';
+
+const evt = document.getElementById('evt')!;
+const locales = [undefined, undefined, 'de-DE', 'zh-Hans-CN'];
+document.querySelectorAll<CalendarSk>('calendar-sk').forEach((ele, i) => {
+  ele.displayDate = new Date(2020, 4, 21);
+  ele.locale = locales[i];
+  ele.addEventListener('change', (e) => {
+    evt.innerText = (e as CustomEvent<
+      CalendarSkChangeEventDetail
+    >).detail.date.toString();
+  });
+});
+
+document.addEventListener('keydown', (e) =>
+  document.querySelector<CalendarSk>('calendar-sk')!.keyboardHandler(e)
+);
