5b4399509651557033f47216c82f6bfb45f8d1e9
Joe Gregorio <jcgregorio@google.com>
2020-06-29 12:58:15 -0400

    [perf] Standup perf flutter-flutter instance.
    Bug: skia: 10440
    Change-Id: If5bed1331ca3fe1c88113c9a6de612c5c0a148e0
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/299680
    Reviewed-by: Joe Gregorio <jcgregorio@google.com>

--- perf/create-ingestion-flutter-flutter-pubsub-topics.sh
+++ perf/create-ingestion-flutter-flutter-pubsub-topics.sh
@@ -5,7 +5,7 @@
 set -e -x
 
 PROJECT_ID=skia-public
-TOPIC=perf-ingestion-flutter
+TOPIC=perf-ingestion-flutter-flutter
 
 #perf-tool config create-pubsub-topics --config_filename=./configs/flutter.json
-gsutil notification create -f json -e OBJECT_FINALIZE -t projects/${PROJECT_ID}/topics/${TOPIC} -p flutter-engine gs://flutter-skia-perf
+gsutil notification create -f json -e OBJECT_FINALIZE -t projects/${PROJECT_ID}/topics/${TOPIC} -p flutter-flutter gs://flutter-skia-perf
