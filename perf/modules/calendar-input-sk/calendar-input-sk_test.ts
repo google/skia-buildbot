802442b8e9f64c330f56806bf567aacc770068da
Joe Gregorio <jcgregorio@google.com>
2020-07-01 16:05:20 -0400

    [perf] calendar-input-sk.
    Change-Id: Icc31d02d4d24fcedd09f9cb411280f0728854d3f

---
+++ perf/modules/calendar-input-sk/calendar-input-sk_test.ts
@@ -0,0 +1,32 @@
+import './index';
+import {assert} from 'chai';
+import {CalendarInputSk} from './calendar-input-sk';
+import {setUpElementUnderTest} from '../../../infra-sk/modules/test_util';
+
+const container = document.createElement('div');
+document.body.appendChild(container);
+
+afterEach(() => {
+  container.innerHTML = '';
+});
+
+describe('calendar-input-sk', () => {
+  const newInstance = setUpElementUnderTest<CalendarInputSk>(
+    'calendar-input-sk'
+  );
+  let calendarInputSk: CalendarInputSk;
+  beforeEach(() => {
+    calendarInputSk = newInstance();
+    calendarInputSk.displayDate = new Date(2020, 4, 21);
+  });
+
+  describe('input control', () => {
+    it('displays the date correctly', () =>
+      window.customElements.whenDefined('calendar-input-sk').then(async () => {
+        assert.equal(
+          calendarInputSk.querySelector<HTMLInputElement>('input')!.value,
+          '2020-5-21'
+        );
+      }));
+  });
+});
