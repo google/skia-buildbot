924e7221b337648179b21b7f2e4b6e1dc350bba3
Leandro Lovisolo <lovisolo@google.com>
2020-07-03 05:52:14 -0400

    [infra-sk] Add PageObjectElement class.
    PageObjectElement is a very thin wrapper around either a DOM node or a Puppeteer element handle. It allows writing composable page objects[1] that can be used on in-browser tests, as well as on Puppeteer tests.
    
    Main benefits:
    
    - Portability: A page object FooPO for a web component Foo can be used to interact with Foo's UI from both foo_test.ts (i.e. in-browser tests) and foo_puppeteer_test.ts (i.e. Puppeteer tests).
    
    - Composability / code reuse: If component Bar has a component Foo, BarPO can be composed from FooPO. No need to repeat any query selectors for Foo's elements.
    
    - Less brittle tests: If Foo's HTML changes, only FooPO's query selectors need to be updated.
    
    - Higher level tests: Write fooPO.getFirstName(), fooPO.setLastName(lastName), fooPO.clickSubmitBtn(), etc.
    
    In addition, an abstract PageObject convenience base class is included to facilitate writing page objects (i.e. the base class for the FooPO and BarPO objects in the above examples).
    
    See the downstream CL https://skia-review.googlesource.com/c/buildbot/+/300217 for an example of a component with a page object used both from its in-browser and Puppeteer tests.
    
    See PageLoader[2] for a similar idea in the Dart language.
    
    Recommended review order:
    - page_object_element.ts
    - page_object_element_test_cases.ts
    - page_object_element_test.ts
    - page_object_element_puppeteer_tests.ts
    - page_object.ts
    
    [1] https://martinfowler.com/bliki/PageObject.html
    [2] https://github.com/google/pageloader
    
    Change-Id: I2db4fdf120a3d94bdc435e208f193b41dea9e0a3

---
+++ infra-sk/modules/page_object/page_object_element_puppeteer_test.ts
@@ -0,0 +1,75 @@
+import http from 'http';
+import net from 'net';
+import express from 'express';
+import { Browser, Page, ElementHandle, Serializable } from 'puppeteer';
+import { launchBrowser } from '../../../puppeteer-tests/util';
+import { PageObjectElement } from './page_object_element';
+import { TestBed, describePageObjectElement } from './page_object_element_test_cases';
+
+describe('PageObjectElement on Puppeter', () => {
+  let browser: Browser;
+  let page: Page;
+
+  let httpServer: http.Server;
+  let testPageUrl: string;
+  let testPageHtml: string;
+
+  before(async () => {
+    // Configure an Express app to serve the test page.
+    const app = express();
+    app.get('/', (_, res) => res.send(testPageHtml));
+
+    // Launch HTTP server on random port.
+    await new Promise((resolve) => {
+      httpServer = app.listen(0, resolve);
+    });
+    const port = (httpServer!.address() as net.AddressInfo).port;
+
+    // The test page will be served on this URL.
+    testPageUrl = `http://localhost:${port}/`;
+
+    // Launch browser.
+    browser = await launchBrowser();
+  });
+
+  after(async () => {
+    // Shut down browser and HTTP server.
+    await browser.close();
+    await new Promise((resolve) => httpServer.close(resolve));
+  });
+
+  beforeEach(async () => { page = await browser.newPage(); });
+  afterEach(async () => { await page.close(); });
+
+  // A handle for the top-level element in the HTML provided via the test bed.
+  let container: ElementHandle<HTMLElement>;
+
+  const testBed: TestBed = {
+    setUpPageObjectElement: async (html: string) => {
+      // Set up the test page.
+      testPageHtml = `
+        <html>
+          <head><title>Test page</title></head>
+          <body>${html}</body>
+        </html>
+      `;
+      await page.goto(testPageUrl);
+
+      // Make sure there is only one top-level element.
+      const elements = await page.$$('body > *');
+      if (elements.length !== 1) {
+        throw new Error('the given HTML contains more than one top-level element');
+      }
+
+      // Retrieve the top-level element and wrap it inside a PageObjectElement.
+      container = elements[0];
+      return new PageObjectElement(container);
+    },
+
+    evaluate: async <T extends Serializable | void = void>(fn: (el: HTMLElement) => T) => {
+      return await container.evaluate(fn) as T;
+    }
+  };
+
+  describePageObjectElement(testBed);
+});
