8ac9e5109f572dac6125e2b420af9253e6a120eb
Leandro Lovisolo <lovisolo@google.com>
2020-07-06 17:57:32 -0400

    [infra-sk] Add PageObjectElement class.
    PageObjectElement is a very thin wrapper around either a DOM node or a Puppeteer element handle. It allows writing composable page objects[1] that can be used on in-browser tests, as well as on Puppeteer tests.
    
    Main benefits:
    
    - Portability: A page object FooPO for a web component Foo can be used to interact with Foo's UI from both foo_test.ts (i.e. in-browser tests) and foo_puppeteer_test.ts (i.e. Puppeteer tests).
    
    - Composability / code reuse: If component Bar has a component Foo, BarPO can be composed from FooPO. No need to repeat any query selectors for Foo's elements.
    
    - Less brittle tests: If Foo's HTML changes, only FooPO's query selectors need to be updated.
    
    - Higher level tests: Write fooPO.getFirstName(), fooPO.setLastName(lastName), fooPO.clickSubmitBtn(), etc.
    
    In addition, an abstract PageObject convenience base class is included to facilitate writing page objects (i.e. the base class for the FooPO and BarPO objects in the above examples).
    
    See the downstream CL https://skia-review.googlesource.com/c/buildbot/+/300217 for an example of a component with a page object used both from its in-browser and Puppeteer tests.
    
    See PageLoader[2] for a similar idea in the Dart language.
    
    Recommended review order:
    - page_object_element.ts
    - page_object_element_test_cases.ts
    - page_object_element_test.ts
    - page_object_element_puppeteer_tests.ts
    - page_object.ts
    
    [1] https://martinfowler.com/bliki/PageObject.html
    [2] https://github.com/google/pageloader
    
    Change-Id: I2db4fdf120a3d94bdc435e208f193b41dea9e0a3

---
+++ infra-sk/modules/page_object/page_object.ts
@@ -0,0 +1,95 @@
+import { ElementHandle, Serializable } from 'puppeteer';
+import { PageObjectElement } from './page_object_element';
+
+/**
+ * A base class for writing page objects[1] that work both on in-browser and Puppeteer tests.
+ *
+ * A page object written as a subclass of PageObject will have two layers of wrapping:
+ *
+ *   1. The PageObject wraps a PageObjectElement.
+ *   2. The PageObjectElement wraps either a DOM node (HTMLElement) or a Puppeteer handle
+ *      (ElementHandle).
+ *
+ * The PageObjectElement wraps the root node of the component under test, and provides an
+ * abstraction layer on top of the underlying DOM node or Puppeteer handle. A page object interacts
+ * with the component under test exclusively via the PageObjectElement's API. This guarantees
+ * compatibility with both in-browser and Puppeteer tests.
+ *
+ * PageObject and PageObjectElement are inspired by PageLoader[2], a Dart framework for creating
+ * page objects compatible with both in-browser and WebDriver tests.
+ *
+ * For best practices, please see PageLoader Best Practices[3].
+ *
+ * [1] https://martinfowler.com/bliki/PageObject.html
+ * [2] https://github.com/google/pageloader
+ * [3] https://github.com/google/pageloader/blob/master/best_practices.md
+ */
+export abstract class PageObject {
+  protected element: PageObjectElement;
+
+  constructor(element: HTMLElement | ElementHandle | PageObjectElement) {
+    if (element instanceof PageObjectElement) {
+      this.element = element;
+    } else {
+      this.element = new PageObjectElement(element);
+    }
+  }
+
+  /**
+   * Returns the result of calling PageObjectElement#querySelector() on the underlying
+   * PageObjectElement.
+   */
+  protected $$(selector: string) {
+    return this.element.$$(selector);
+  }
+
+  /**
+   * Returns the result of calling PageObjectElement#$$apply() on the underlying
+   * PageObjectElement.
+   */
+  protected $$apply<T>(selector: string, fn: (element: PageObjectElement) => Promise<T>) {
+    return this.element.$$apply(selector, fn);
+  }
+
+  /**
+   * Returns the result of calling PageObjectElement#$$evalDom() on the underlying
+   * PageObjectElement.
+   */
+  protected $$evalDom<T extends Serializable | void>(
+      selector: string, fn: (element: HTMLElement) => T, ...args: Serializable[]) {
+    return this.element.$$evalDom(selector, fn, ...args);
+  }
+
+  /**
+   * Returns the result of calling PageObjectElement#querySelectorAll() on the underlying
+   * PageObjectElement.
+   */
+  protected $(selector: string) {
+    return this.element.$(selector);
+  }
+
+  /**
+   * Returns the result of calling PageObjectElement#$map() on the underlying
+   * PageObjectElement.
+   */
+  protected $map<T>(
+      selector: string, fn: (element: PageObjectElement, index: number) => Promise<T>) {
+    return this.element.$map(selector, fn);
+  }
+
+  /**
+   * Returns the result of calling PageObjectElement#$each() on the underlying
+   * PageObjectElement.
+   */  protected $each<T>(
+      selector: string, fn: (element: PageObjectElement, index: number) => Promise<void>) {
+    return this.element.$each(selector, fn);
+  }
+
+  /**
+   * Returns the result of calling PageObjectElement#$find() on the underlying
+   * PageObjectElement.
+   */  protected $find<T>(
+      selector: string, fn: (element: PageObjectElement, index: number) => Promise<boolean>) {
+    return this.element.$find(selector, fn);
+  }
+};
