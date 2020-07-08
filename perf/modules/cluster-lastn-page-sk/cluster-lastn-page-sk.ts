2e58205b835ca5870e17837ddfd79bd667e3faa9
Joe Gregorio <jcgregorio@google.com>
2020-07-07 21:41:42 -0400

    [perf] Port cluster-lastn-page-sk to TypeScript.
    Change-Id: I36625f3078432504c44dd81f15559f4fb4b93f15

---
+++ perf/modules/cluster-lastn-page-sk/cluster-lastn-page-sk.ts
@@ -0,0 +1,409 @@
+/**
+ * @module module/cluster-lastn-page-sk
+ * @description <h2><code>cluster-lastn-page-sk</code></h2>
+ *
+ *  Allows trying out an alert by clustering over a range of commits.
+ */
+import dialogPolyfill from 'dialog-polyfill';
+import { define } from 'elements-sk/define';
+import { errorMessage } from 'elements-sk/errorMessage';
+import { fromObject } from 'common-sk/modules/query';
+import { html } from 'lit-html';
+import { jsonOrThrow } from 'common-sk/modules/jsonOrThrow';
+import { stateReflector } from 'common-sk/modules/stateReflector';
+import { ElementSk } from '../../../infra-sk/modules/ElementSk';
+
+import 'elements-sk/spinner-sk';
+import 'elements-sk/styles/buttons';
+
+import '../cluster-summary2-sk';
+import '../commit-detail-sk';
+import '../triage-status-sk';
+import '../alert-config-sk';
+import '../domain-picker-sk';
+import {
+  UIDomain,
+  ParamSet,
+  Alert,
+  FrameResponse,
+  RegressionRow,
+  Regression,
+  DryRunStatus,
+  Direction,
+  ClusterSummary,
+} from '../json';
+import { ParamSetSk } from '../../../infra-sk/modules/paramset-sk/paramset-sk';
+import { AlertConfigSk } from '../alert-config-sk/alert-config-sk';
+import { HintableObject } from 'common-sk/modules/hintable';
+import { TriageStatusSkStartTriageEventDetails } from '../triage-status-sk/triage-status-sk';
+import { ClusterSummary2SkOpenKeysEventDetail } from '../cluster-summary2-sk/cluster-summary2-sk';
+import { DomainPickerSk } from '../domain-picker-sk/domain-picker-sk';
+
+export class ClusterLastNPageSk extends ElementSk {
+  private static template = (ele: ClusterLastNPageSk) => html`
+    <dialog id="alert-config-dialog">
+      <alert-config-sk
+        .config=${ele._state}
+        .paramset=${ele._paramset}
+        .key_order=${window.sk.perf.key_order}
+      ></alert-config-sk>
+      <div class="buttons">
+        <button @click=${ele._alertClose}>Cancel</button>
+        <button @click=${ele._alertAccept}>Accept</button>
+      </div>
+    </dialog>
+    <div class="controls">
+      <label>
+        <h2>Alert Configuration</h2>
+        <button @click=${ele._alertEdit}>
+          ${ClusterLastNPageSk._configTitle(ele)}
+        </button>
+      </label>
+      <label>
+        <h2>Time Range</h2>
+        <domain-picker-sk
+          id="range"
+          .state=${ele._domain}
+          force_request_type="dense"
+        ></domain-picker-sk>
+      </label>
+      <div class="running">
+        <button
+          class="action"
+          ?disabled=${!ele._state!.query || !!ele._requestId}
+          @click=${ele._run}
+        >
+          Run
+        </button>
+        <spinner-sk ?active=${!!ele._requestId}></spinner-sk>
+        <pre class="messages">${ele._runningStatus}</pre>
+      </div>
+    </div>
+    <hr />
+
+    <dialog id="triage-cluster-dialog" @open-keys=${ele._openKeys}>
+      <cluster-summary2-sk
+        .full_summary=${ele._dialog_state!.full_summary}
+        .triage=${ele._dialog_state!.triage}
+        notriage
+      ></cluster-summary2-sk>
+      <div class="buttons">
+        <button @click=${ele._triageClose}>Close</button>
+      </div>
+    </dialog>
+
+    ${ClusterLastNPageSk._table(ele)}
+  `;
+
+  private static _stepUpAt(dir: Direction) {
+    return dir === 'UP' || dir === 'BOTH';
+  }
+
+  private static _stepDownAt(dir: Direction) {
+    return dir === 'DOWN' || dir === 'BOTH';
+  }
+
+  private static _notBoth(dir: Direction) {
+    return dir !== 'BOTH';
+  }
+
+  private static _tableHeader = (ele: ClusterLastNPageSk) => {
+    const ret = [
+      html`
+        <th></th>
+      `,
+    ];
+    if (ClusterLastNPageSk._stepDownAt(ele._state!.direction)) {
+      ret.push(
+        html`
+          <th>Low</th>
+        `
+      );
+    }
+    if (ClusterLastNPageSk._stepUpAt(ele._state!.direction)) {
+      ret.push(
+        html`
+          <th>High</th>
+        `
+      );
+    }
+    if (ClusterLastNPageSk._notBoth(ele._state!.direction)) {
+      ret.push(
+        html`
+          <th></th>
+        `
+      );
+    }
+    return ret;
+  };
+
+  private static _full_summary(frame: FrameResponse, summary: ClusterSummary) {
+    return {
+      frame,
+      summary,
+    };
+  }
+
+  private static _low = (ele: ClusterLastNPageSk, reg: RegressionRow) => {
+    if (!ClusterLastNPageSk._stepDownAt(ele._state!.direction)) {
+      return html``;
+    }
+    if (reg.regression!.low) {
+      return html`
+        <td class="cluster">
+          <triage-status-sk
+            .alert=${ele._state}
+            cluster_type="low"
+            .full_summary=${ClusterLastNPageSk._full_summary(
+              reg.regression!.frame!,
+              reg.regression!.low
+            )}
+            .triage=${reg.regression!.low_status}
+          ></triage-status-sk>
+        </td>
+      `;
+    }
+    return html`
+      <td class="cluster"></td>
+    `;
+  };
+
+  private static _high = (ele: ClusterLastNPageSk, reg: RegressionRow) => {
+    if (!ClusterLastNPageSk._stepUpAt(ele._state!.direction)) {
+      return html``;
+    }
+    if (reg.regression!.high) {
+      return html`
+        <td class="cluster">
+          <triage-status-sk
+            .alert=${ele._state}
+            cluster_type="high"
+            .full_summary=${ClusterLastNPageSk._full_summary(
+              reg.regression!.frame!,
+              reg.regression!.high
+            )}
+            .triage=${reg.regression!.high_status}
+          ></triage-status-sk>
+        </td>
+      `;
+    }
+    return html`
+      <td class="cluster"></td>
+    `;
+  };
+
+  private static _filler = (ele: ClusterLastNPageSk) => {
+    if (ClusterLastNPageSk._notBoth(ele._state!.direction)) {
+      return html`
+        <td></td>
+      `;
+    }
+    return html``;
+  };
+
+  private static _tableRows = (ele: ClusterLastNPageSk) =>
+    ele._regressions.map(
+      (reg) => html`
+        <tr>
+          <td class="fixed">
+            <commit-detail-sk .cid=${reg.cid}></commit-detail-sk>
+          </td>
+
+          ${ClusterLastNPageSk._low(ele, reg)}
+          ${ClusterLastNPageSk._high(ele, reg)}
+          ${ClusterLastNPageSk._filler(ele)}
+        </tr>
+      `
+    );
+
+  private static _configTitle = (ele: ClusterLastNPageSk) =>
+    html`
+      Algo: ${ele._state!.algo} - Radius: ${ele._state!.radius} - Sparse:
+      ${ele._state!.sparse} - Threshold: ${ele._state!.interesting}
+    `;
+
+  private static _table = (ele: ClusterLastNPageSk) => {
+    if (ele._requestId && !ele._regressions.length) {
+      return html`
+        No regressions found yet.
+      `;
+    }
+    return html`
+      <table @start-triage=${ele._triage_start}>
+        <tr>
+          <th>Commit</th>
+          <th colspan="2">Regressions</th>
+        </tr>
+        <tr>
+          ${ClusterLastNPageSk._tableHeader(ele)}
+        </tr>
+        ${ClusterLastNPageSk._tableRows(ele)}
+      </table>
+    `;
+  };
+
+  // The range of commits over which we are clustering.
+  private _domain: UIDomain = {
+    begin: 0,
+    end: Math.floor(Date.now() / 1000),
+    num_commits: 200,
+    request_type: 1,
+  };
+
+  // The paramsets for the alert config.
+  private _paramset: ParamSet = {};
+
+  // The id of the currently running request.
+  private _requestId: string = '';
+
+  // The text status of the currently running request.
+  private _runningStatus = '';
+
+  // The fetch in connectedCallback will fill this is with the defaults.
+  private _state: Alert | null = null;
+
+  private _alertDialog: HTMLDialogElement | null = null;
+  private _triageDialog: HTMLDialogElement | null = null;
+  private _alertConfig: AlertConfigSk | null = null;
+
+  // The state of the cluster-summary2-sk dialog.
+  private _dialog_state: Partial<
+    TriageStatusSkStartTriageEventDetails
+  > | null = {
+    full_summary: null,
+    triage: undefined,
+  };
+
+  // Call this anytime something in private state is changed. Will be replaced
+  // with the real function once stateReflector has been setup.
+  // tslint:disable-next-line: no-empty
+  private _stateHasChanged = () => {};
+
+  private _regressions: RegressionRow[] = [];
+
+  constructor() {
+    super(ClusterLastNPageSk.template);
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    const init = fetch('/_/initpage/')
+      .then(jsonOrThrow)
+      .then((json: FrameResponse) => {
+        this._paramset = json.dataframe!.paramset;
+      });
+    const alertNew = fetch('/_/alert/new')
+      .then(jsonOrThrow)
+      .then((json: Alert) => {
+        this._state = json;
+      });
+    Promise.all([init, alertNew]).then(() => {
+      this._render();
+      this._alertDialog = this.querySelector('#alert-config-dialog');
+      this._triageDialog = this.querySelector('#triage-cluster-dialog');
+      dialogPolyfill.registerDialog(this._alertDialog!);
+      dialogPolyfill.registerDialog(this._triageDialog!);
+      this._alertConfig = this.querySelector('alert-config-sk');
+      this._stateHasChanged = stateReflector(
+        () => (this._state as unknown) as HintableObject,
+        (state) => {
+          this._state = (state as unknown) as Alert;
+          this._render();
+        }
+      );
+    });
+    //.catch(errorMessage);
+  }
+
+  _alertEdit() {
+    this._alertDialog!.showModal();
+  }
+
+  _alertClose() {
+    this._alertDialog!.close();
+  }
+
+  _alertAccept() {
+    this._alertDialog!.close();
+    this._state = this._alertConfig!.config;
+    this._stateHasChanged();
+    this._render();
+  }
+
+  _triage_start(e: CustomEvent<TriageStatusSkStartTriageEventDetails>) {
+    this._dialog_state = e.detail;
+    this._render();
+    this._triageDialog!.show();
+  }
+
+  _triageClose() {
+    this._triageDialog!.close();
+  }
+
+  _openKeys(e: CustomEvent<ClusterSummary2SkOpenKeysEventDetail>) {
+    const query = {
+      keys: e.detail.shortcut,
+      begin: e.detail.begin,
+      end: e.detail.end,
+      xbaroffset: e.detail.xbar.offset,
+    };
+    window.open(`/e/?${fromObject(query)}`, '_blank');
+  }
+
+  _catch(msg: string) {
+    this._requestId = '';
+    this._runningStatus = '';
+    this._render();
+    if (msg) {
+      errorMessage(msg, 10000);
+    }
+  }
+
+  _run() {
+    if (this._requestId) {
+      errorMessage('There is a pending query already running.');
+      return;
+    }
+    this._domain = this.querySelector<DomainPickerSk>('#range')!.state;
+    const body = {
+      domain: this._domain,
+      config: this._state,
+    };
+    fetch('/_/dryrun/start', {
+      method: 'POST',
+      body: JSON.stringify(body),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(jsonOrThrow)
+      .then((json) => {
+        this._requestId = json.id;
+        this._render();
+        this._checkDryRunStatus((regressions: RegressionRow[]) => {
+          this._regressions = regressions;
+          this._render();
+        });
+      })
+      .catch((msg) => this._catch(msg));
+  }
+
+  _checkDryRunStatus(cb: (regressions: RegressionRow[]) => void) {
+    fetch(`/_/dryrun/status/${this._requestId}`)
+      .then(jsonOrThrow)
+      .then((json: DryRunStatus) => {
+        this._runningStatus = json.message;
+        if (!json.finished) {
+          window.setTimeout(() => this._checkDryRunStatus(cb), 300);
+        } else {
+          this._requestId = '';
+        }
+        // json.regressions will get filled in incrementally, so display them
+        // as they arrive.
+        cb(json.regressions!);
+      })
+      .catch((msg) => this._catch(msg));
+  }
+}
+
+define('cluster-lastn-page-sk', ClusterLastNPageSk);
