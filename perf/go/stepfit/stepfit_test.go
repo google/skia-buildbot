8140ddde3f36cae727c7f0969948b3b696133b82
Joe Gregorio <jcgregorio@google.com>
2020-07-09 16:56:55 -0400

    [perf] Don't display LSE where it doesn't apply.
    Also fix bug so LSE gets reported when it does apply.
    
    Bug: skia:10392
    Change-Id: I221a1dd6f29fd3db7225c16c9c56e32ad8dc363f

--- perf/go/stepfit/stepfit_test.go
+++ perf/go/stepfit/stepfit_test.go
@@ -41,75 +41,76 @@ func TestStepFit_SimpleStepDown(t *testing.T) {
 func TestStepFit_NoStep(t *testing.T) {
 	unittest.SmallTest(t)
 	assert.Equal(t,
-		&StepFit{TurningPoint: 2, StepSize: -1, Status: UNINTERESTING, Regression: -2.7105057e-19},
+		&StepFit{TurningPoint: 2, StepSize: -1, Status: UNINTERESTING, Regression: -2.7105057e-19,
+			LeastSquares: 3.6893486e+18},
 		GetStepFitAtMid([]float32{1, 1, 1, 1, 1}, minStdDev, 50, types.OriginalStep))
 }
 
 func TestStepFit_Absolute_NoStep(t *testing.T) {
 	unittest.SmallTest(t)
 	assert.Equal(t,
-		&StepFit{TurningPoint: 2, StepSize: 0, Status: UNINTERESTING, Regression: 0},
+		&StepFit{TurningPoint: 2, StepSize: 0, Status: UNINTERESTING, Regression: 0, LeastSquares: InvalidLeastSquaresError},
 		GetStepFitAtMid([]float32{1, 2, 1, 2, x}, minStdDev, 1.0, types.AbsoluteStep))
 }
 
 func TestStepFit_Absolute_StepExactMatch(t *testing.T) {
 	unittest.SmallTest(t)
 	assert.Equal(t,
-		&StepFit{TurningPoint: 2, StepSize: -1, Status: HIGH, Regression: -1},
+		&StepFit{TurningPoint: 2, StepSize: -1, Status: HIGH, Regression: -1, LeastSquares: InvalidLeastSquaresError},
 		GetStepFitAtMid([]float32{1, 1, 2, 2, x}, minStdDev, 1.0, types.AbsoluteStep))
 }
 
 func TestStepFit_Absolute_StepTooSmall(t *testing.T) {
 	unittest.SmallTest(t)
 	assert.Equal(t,
-		&StepFit{TurningPoint: 2, StepSize: -0.5, Status: UNINTERESTING, Regression: -0.5},
+		&StepFit{TurningPoint: 2, StepSize: -0.5, Status: UNINTERESTING, Regression: -0.5, LeastSquares: InvalidLeastSquaresError},
 		GetStepFitAtMid([]float32{1, 1, 1.5, 1.5, x}, minStdDev, 1.0, types.AbsoluteStep))
 }
 
 func TestStepFit_Percent_NoStep(t *testing.T) {
 	unittest.SmallTest(t)
 	assert.Equal(t,
-		&StepFit{TurningPoint: 2, StepSize: 0, Status: UNINTERESTING, Regression: 0},
+		&StepFit{TurningPoint: 2, StepSize: 0, Status: UNINTERESTING, Regression: 0, LeastSquares: InvalidLeastSquaresError},
 		GetStepFitAtMid([]float32{1, 2, 1, 2, x}, minStdDev, 1.0, types.PercentStep))
 }
 
 func TestStepFit_Percent_StepExactMatch(t *testing.T) {
 	unittest.SmallTest(t)
 	assert.Equal(t,
-		&StepFit{TurningPoint: 1, StepSize: -1, Status: HIGH, Regression: -1},
+		&StepFit{TurningPoint: 1, StepSize: -1, Status: HIGH, Regression: -1, LeastSquares: InvalidLeastSquaresError},
 		GetStepFitAtMid([]float32{1, 2, x}, minStdDev, 1.0, types.PercentStep))
 }
 
 func TestStepFit_Percent_StepTooSmall(t *testing.T) {
 	unittest.SmallTest(t)
 	assert.Equal(t,
-		&StepFit{TurningPoint: 1, StepSize: -0.5, Status: UNINTERESTING, Regression: -0.5},
+		&StepFit{TurningPoint: 1, StepSize: -0.5, Status: UNINTERESTING, Regression: -0.5, LeastSquares: InvalidLeastSquaresError},
 		GetStepFitAtMid([]float32{1, 1.5, x}, minStdDev, 1.0, types.PercentStep))
 }
 
 func TestStepFit_Cohen_NoStep(t *testing.T) {
 	unittest.SmallTest(t)
 	assert.Equal(t,
-		&StepFit{TurningPoint: 3, StepSize: -0.1999998, Status: UNINTERESTING, Regression: -0.1999998},
+		&StepFit{TurningPoint: 3, StepSize: -0.1999998, Status: UNINTERESTING, Regression: -0.1999998, LeastSquares: InvalidLeastSquaresError},
 		GetStepFitAtMid([]float32{1, 1.1, 0.9, 1.02, 1.12, 0.92, x}, minStdDev, 1.0, types.CohenStep))
 }
 
 func TestStepFit_Cohen_Step(t *testing.T) {
 	unittest.SmallTest(t)
 	assert.Equal(t,
-		&StepFit{TurningPoint: 3, StepSize: -0.1999998, Status: HIGH, Regression: -0.1999998},
+		&StepFit{TurningPoint: 3, StepSize: -0.1999998, Status: HIGH, Regression: -0.1999998, LeastSquares: InvalidLeastSquaresError},
 		GetStepFitAtMid([]float32{1, 1.1, 0.9, 1.02, 1.12, 0.92, x}, minStdDev, 0.1, types.CohenStep))
 }
 
 func TestStepFit_Cohen_StepWithZeroStandardDeviation(t *testing.T) {
 	unittest.SmallTest(t)
 	assert.Equal(t,
-		&StepFit{TurningPoint: 2, StepSize: -10, Status: HIGH, Regression: -10},
+		&StepFit{TurningPoint: 2, StepSize: -10, Status: HIGH, Regression: -10, LeastSquares: InvalidLeastSquaresError},
 		GetStepFitAtMid([]float32{1, 1, 2, 2, x}, minStdDev, 0.2, types.CohenStep))
 }
 func TestStepFit_Cohen_StepWithLargeStandardDeviation(t *testing.T) {
 	unittest.SmallTest(t)
 	assert.Equal(t,
-		&StepFit{LeastSquares: 0, TurningPoint: 2, StepSize: -2.828427, Regression: -2.828427, Status: "High"},
+		&StepFit{LeastSquares: InvalidLeastSquaresError, TurningPoint: 2, StepSize: -2.828427, Regression: -2.828427, Status: "High"},
 		GetStepFitAtMid([]float32{1, 2, 3, 4, x}, minStdDev, 0.2, types.CohenStep))
 }
