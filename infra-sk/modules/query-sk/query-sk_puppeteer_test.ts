e8a9c66994533fcc911442834865577aa4cd25d3
Joe Gregorio <jcgregorio@google.com>
2020-07-10 15:12:02 -0400

    [infra-sk] Move the fast filter on query-sk.
    I got a bug report that fast filter should work on both params and values, when
    it already does that. So it appears that it isn't clear that is what filtering
    does.
    
    This CL moves the filter to below both params and values and adds placeholder
    text that indicates what the control does.
    
    Bug: skia:10327
    Change-Id: I4c14e2a7e00b4db9ceced1df1db82b6bb26b4800

--- infra-sk/modules/query-sk/query-sk_puppeteer_test.ts
+++ infra-sk/modules/query-sk/query-sk_puppeteer_test.ts
@@ -12,12 +12,12 @@ describe('query-sk', () => {
 
   beforeEach(async () => {
     await testBed.page.goto(`${testBed.baseUrl}/query-sk.html`);
-    await testBed.page.setViewport({ width: 600, height: 1100 });
+    await testBed.page.setViewport({ width: 600, height: 1400 });
   });
 
   it('should render the demo page', async () => {
     // Smoke test.
-    expect(await testBed.page.$$('query-sk')).to.have.length(3);
+    expect(await testBed.page.$$('query-sk')).to.have.length(4);
   });
 
   describe('screenshots', () => {
