831747f57f560f1de3fa20125ca70ee7316e8354
Kevin Lubick <kjlubick@google.com>
2020-07-10 08:29:15 -0400

    [gold] Add click handlers to cluster-digests-sk
    Bug: skia:9525
    Change-Id: Ie5b694fc276629e2000cd8a4aca7867aff83b568

--- golden/modules/cluster-digests-sk/cluster-digests-sk_puppeteer_test.ts
+++ golden/modules/cluster-digests-sk/cluster-digests-sk_puppeteer_test.ts
@@ -1,6 +1,9 @@
 import { expect } from 'chai';
-import { addEventListenersToPuppeteerPage, takeScreenshot, TestBed } from '../../../puppeteer-tests/util';
+import { addEventListenersToPuppeteerPage, EventName,
+    takeScreenshot, TestBed } from '../../../puppeteer-tests/util';
 import { loadGoldWebpack } from '../common_puppeteer_test/common_puppeteer_test';
+import { ElementHandle } from 'puppeteer';
+import { positiveDigest, negativeDigest, untriagedDigest } from './test_data';
 
 describe('cluster-digests-sk', () => {
     let testBed: TestBed;
@@ -8,12 +11,16 @@ describe('cluster-digests-sk', () => {
         testBed = await loadGoldWebpack();
     });
 
+    let promiseFactory: <T>(eventName: EventName) => Promise<T>;
+    let clusterDigestsSk: ElementHandle;
+
     beforeEach(async () => {
-        const eventPromise = await addEventListenersToPuppeteerPage(testBed.page,
-            ['layout-complete']);
-        const loaded = eventPromise('layout-complete'); // Emitted when layout stabilizes.
+        promiseFactory = await addEventListenersToPuppeteerPage(testBed.page,
+            ['layout-complete', 'selection-changed']);
+        const loaded = promiseFactory('layout-complete'); // Emitted when layout stabilizes.
         await testBed.page.goto(`${testBed.baseUrl}/dist/cluster-digests-sk.html`);
         await loaded;
+        clusterDigestsSk = (await testBed.page.$('#cluster svg'))!;
     });
 
     it('should render the demo page', async () => {
@@ -22,7 +29,65 @@ describe('cluster-digests-sk', () => {
     });
 
     it('should take a screenshot', async () => {
-        const clusterDigestsSk = await testBed.page.$('#cluster svg');
-        await takeScreenshot(clusterDigestsSk!, 'gold', 'cluster-digests-sk');
+        await takeScreenshot(clusterDigestsSk, 'gold', 'cluster-digests-sk');
     });
+
+    it('supports single digest selection via clicking', async () => {
+        await clickNodeAndExpectSelectionChangedEvent(positiveDigest, [positiveDigest]);
+
+        await takeScreenshot(clusterDigestsSk, 'gold', 'cluster-digests-sk_one-positive-selected');
+
+        await clickNodeAndExpectSelectionChangedEvent(untriagedDigest, [untriagedDigest]);
+
+        await takeScreenshot(clusterDigestsSk, 'gold',
+            'cluster-digests-sk_one-untriaged-selected');
+    });
+
+    it('supports multiple digest selection via shift clicking', async () => {
+        await clickNodeAndExpectSelectionChangedEvent(negativeDigest, [negativeDigest]);
+
+        await shiftClickNodeAndExpectSelectionChangedEvent(positiveDigest,
+            [negativeDigest, positiveDigest]);
+
+        await takeScreenshot(clusterDigestsSk, 'gold', 'cluster-digests-sk_two-digests-selected');
+
+        await shiftClickNodeAndExpectSelectionChangedEvent(untriagedDigest,
+            [negativeDigest, positiveDigest, untriagedDigest]);
+
+        await takeScreenshot(clusterDigestsSk, 'gold',
+            'cluster-digests-sk_three-digests-selected');
+    });
+
+    it('clears selection by clicking anywhere on the svg that is not on a node', async () => {
+        await clickNodeAndExpectSelectionChangedEvent(negativeDigest, [negativeDigest]);
+
+        const clickEvent = promiseFactory<Array<string>>('selection-changed');
+        await clusterDigestsSk.click();
+        const evt = await clickEvent;
+        expect(evt).to.deep.equal([]);
+    });
+
+    async function clickNodeWithDigest(digest: string) {
+        await testBed.page.click(`circle.node[data-digest="${digest}"]`);
+    }
+
+    async function shiftClickNodeWithDigest(digest: string) {
+        await testBed.page.keyboard.down('Shift');
+        await clickNodeWithDigest(digest);
+        await testBed.page.keyboard.up('Shift');
+    }
+
+    async function clickNodeAndExpectSelectionChangedEvent(digest: string, expectedSelection: string[]) {
+        const clickEvent = promiseFactory<Array<string>>('selection-changed');
+        await clickNodeWithDigest(digest);
+        const evt = await clickEvent;
+        expect(evt).to.deep.equal(expectedSelection);
+    }
+
+    async function shiftClickNodeAndExpectSelectionChangedEvent(digest: string, expectedSelection: string[]) {
+        const clickEvent = promiseFactory<Array<string>>('selection-changed');
+        await shiftClickNodeWithDigest(digest);
+        const evt = await clickEvent;
+        expect(evt).to.deep.equal(expectedSelection);
+    }
 });
