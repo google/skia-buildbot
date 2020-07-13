d1daff06b038e9f0f37b2b0c6e62491a8e7a1c21
Leandro Lovisolo <lovisolo@google.com>
2020-07-13 13:57:06 -0400

    [gold] Add code generator for TypeScript RPC types.
    Notes:
    - Tryjob Infra-PerCommit-Lage will turn red upon any diffs on rpc_types.ts, forcing the backend/frontend types to stay in sync.
    - This CL includes an initial set of types required by the upcoming lit-html/TypeScript search page.
    
    Change-Id: I9c3de42c640ebc65995dda54fd430abb3a9d9ce1
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/302176
    Reviewed-by: Joe Gregorio <jcgregorio@google.com>
    Commit-Queue: Leandro Lovisolo <lovisolo@google.com>

---
+++ golden/go/web/frontend/generate_typescript_rpc_types/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"flag"
+	"io"
+
+	"github.com/skia-dev/go2ts"
+	"go.skia.org/infra/go/skerr"
+	"go.skia.org/infra/go/sklog"
+	"go.skia.org/infra/go/util"
+	search_frontend "go.skia.org/infra/golden/go/search/frontend"
+	"go.skia.org/infra/golden/go/status"
+	"go.skia.org/infra/golden/go/tiling"
+	"go.skia.org/infra/golden/go/web/frontend"
+)
+
+func main() {
+	var outputPath = flag.String("o", "", "Path to the output TypeScript file.")
+	flag.Parse()
+
+	generator := go2ts.New()
+	if err := addTypes(generator); err != nil {
+		sklog.Fatal(err)
+	}
+
+	err := util.WithWriteFile(*outputPath, func(w io.Writer) error {
+		return generator.Render(w)
+	})
+	if err != nil {
+		sklog.Fatal(err)
+	}
+}
+
+func addTypes(generator *go2ts.Go2TS) error {
+	// Response for the /json/changelist/{system}/{id} RPC endpoint.
+	if err := generator.AddWithName(frontend.ChangeListSummary{}, "ChangeListSummaryResponse"); err != nil {
+		return skerr.Wrap(err)
+	}
+
+	// Response for the /json/paramset RPC endpoint.
+	if err := generator.AddWithName(tiling.Tile{}.ParamSet, "ParamSetResponse"); err != nil {
+		return skerr.Wrap(err)
+	}
+
+	// Response for the /json/search RPC endpoint.
+	if err := generator.AddWithName(search_frontend.SearchResponse{}, "SearchResponse"); err != nil {
+		return skerr.Wrap(err)
+	}
+
+	// Response for the /json/trstatus RPC endpoint.
+	if err := generator.AddWithName(status.GUIStatus{}, "StatusResponse"); err != nil {
+		return skerr.Wrap(err)
+	}
+
+	return nil
+}
