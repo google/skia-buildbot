2248cf800477123cbb60d5629991d17ef35598bf
Ravi Mistry <rmistry@google.com>
2020-07-16 12:56:50 -0400

    Add comment to ManualRollRequest and update bot schema
    This is addressing comments from
    https://skia-review.googlesource.com/c/skia/+/300700
    and
    https://skia-review.googlesource.com/c/skia/+/300780
    
    Bug: skia:10477
    Change-Id: I1bb3f207b28f01e0b9e8bb960b085d88ae988258
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/303339
    Reviewed-by: Eric Boren <borenet@google.com>
    Commit-Queue: Ravi Mistry <rmistry@google.com>

--- go/taskname/task_name_schema_gen.go
+++ go/taskname/task_name_schema_gen.go
@@ -5,6 +5,7 @@ package taskname
 var SCHEMA_FROM_GIT = map[string]*Schema{
 	"Build":       {Keys: []string{"os", "compiler", "target_arch", "configuration"}, OptionalKeys: []string{"extra_config"}, RecurseRoles: []string(nil)},
 	"BuildStats":  {Keys: []string{"os", "compiler", "target_arch", "configuration"}, OptionalKeys: []string{"extra_config"}, RecurseRoles: []string(nil)},
+	"Canary":      {Keys: []string{"project"}, OptionalKeys: []string(nil), RecurseRoles: []string(nil)},
 	"FM":          {Keys: []string{"os", "compiler", "model", "cpu_or_gpu", "cpu_or_gpu_value", "arch", "configuration", "test_filter"}, OptionalKeys: []string{"extra_config"}, RecurseRoles: []string(nil)},
 	"Housekeeper": {Keys: []string{"frequency"}, OptionalKeys: []string{"extra_config"}, RecurseRoles: []string(nil)},
 	"Infra":       {Keys: []string{"frequency"}, OptionalKeys: []string{"extra_config"}, RecurseRoles: []string(nil)},
