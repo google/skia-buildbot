2b99b05104ea4b8b6b952b11fd11acbc7852b5a0
Leandro Lovisolo <lovisolo@google.com>
2020-07-16 14:47:26 -0400

    [gold] Add type expectations.LabelStr and use it everywhere.
    This change allows us to generate a LabelStr type union in TypeScript will all three possible values.
    
    It also makes the structs that generate our JSON RPC responses more self-documenting by using LabelStr rather than raw strings.
    
    Recommended review order:
    - labels.go
    - labels_test.go
    - types.go
    - search.go
    - web.go
    - generate_typescript_rpc_types/main.go
    - rpc_types.ts
    
    Bug: skia:9525
    Change-Id: I2842397df1af57eabc912e49118f5c4be017156a

--- golden/go/expectations/labels.go
+++ golden/go/expectations/labels.go
@@ -4,42 +4,53 @@ package expectations
 type Label int
 
 const (
-	// Untriaged represents a previously unseen digest
+	// Untriaged represents a previously unseen digest.
 	Untriaged Label = iota // == 0
-	// Positive represents a known good digest
+	// Positive represents a known good digest.
 	Positive
-	// Negative represents a known bad digest
+	// Negative represents a known bad digest.
 	Negative
 )
 
-// String representation for Labels. The order must match order above.
-var labelStringRepresentation = []string{
-	"untriaged",
-	"positive",
-	"negative",
+// LabelStr is the string version of Label. Used e.g. to represent digest classifications in JSON.
+type LabelStr string
+
+const (
+	// UntriagedStr represents a previously unseen digest. String version of Untriaged.
+	UntriagedStr = LabelStr("untriaged")
+
+	// PositiveStr represents a known good digest. String version of Positive.
+	PositiveStr = LabelStr("positive")
+
+	// NegativeStr represents a known bad digest. String version of Negative.
+	NegativeStr = LabelStr("negative")
+)
+
+// AllLabelStr is a list of all possible LabelStr values. The index of each element in this list
+// must match its Label value (Untriaged = 0, etc.).
+var AllLabelStr = []LabelStr{UntriagedStr, PositiveStr, NegativeStr}
+
+func (l Label) String() LabelStr {
+	return AllLabelStr[l]
 }
 
-func (l Label) String() string {
-	return labelStringRepresentation[l]
+var labels = map[LabelStr]Label{
+	UntriagedStr: Untriaged,
+	PositiveStr:  Positive,
+	NegativeStr:  Negative,
 }
 
-var labels = map[string]Label{
-	"untriaged": Untriaged,
-	"positive":  Positive,
-	"negative":  Negative,
-}
-
-// LabelFromString returns the Label corresponding to the serialized string or Untriaged
-// if there is no match.
-func LabelFromString(s string) Label {
+// LabelFromString returns the Label corresponding to the given LabelStr, or Untriaged if there is
+// no match.
+func LabelFromString(s LabelStr) Label {
 	if l, ok := labels[s]; ok {
 		return l
 	}
 	return Untriaged
 }
 
-// ValidLabel returns true if the given label is a valid label string.
-func ValidLabel(s string) bool {
+// ValidLabelStr returns true if the given LabelStr is valid.
+func ValidLabelStr(s LabelStr) bool {
 	_, ok := labels[s]
 	return ok
 }
