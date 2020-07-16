f9dac24164cf1f25eb9cbb6e479a698f1a209dde
Kevin Lubick <kjlubick@google.com>
2020-07-16 13:25:11 -0400

    [gold] Tools (and small fixes) from poking around time travel.
    Change-Id: Ibfff2c69b409fa09c8be034bc5a516ed2fe10985

--- golden/go/tracestore/bt_tracestore/util_test.go
+++ golden/go/tracestore/bt_tracestore/util_test.go
@@ -17,9 +17,9 @@ func TestTileKeyFromIndex(t *testing.T) {
 	unittest.SmallTest(t)
 
 	// spot-check some arbitrary values
-	require.Equal(t, tileKey(2147483647), tileKeyFromIndex(0))
-	require.Equal(t, tileKey(2147483451), tileKeyFromIndex(196))
-	require.Equal(t, tileKey(908536335), tileKeyFromIndex(1238947312))
+	require.Equal(t, TileKey(2147483647), tileKeyFromIndex(0))
+	require.Equal(t, TileKey(2147483451), tileKeyFromIndex(196))
+	require.Equal(t, TileKey(908536335), tileKeyFromIndex(1238947312))
 }
 
 func TestOpsRowName(t *testing.T) {
@@ -35,8 +35,8 @@ func TestShardedRowName(t *testing.T) {
 	unittest.SmallTest(t)
 
 	shard := int32(3) // arbitrarily picked
-	tileZeroKey := tileKey(math.MaxInt32 - 1)
-	veryNewTileKey := tileKey(57)
+	tileZeroKey := TileKey(math.MaxInt32 - 1)
+	veryNewTileKey := TileKey(57)
 
 	// Example RowName for a trace
 	encodedTrace := ",0=1,1=3,3=0,"
