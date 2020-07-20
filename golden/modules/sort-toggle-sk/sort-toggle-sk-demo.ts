5575a87c8936c842d736404c6b66a4f4638fb62b
Kevin Lubick <kjlubick@google.com>
2020-07-20 09:01:04 -0400

    [gold] Add fork of infra-sk/sort-sk called sort-toggle-sk
    sort-sk was not working for 2 reasons:
    1) it was slow because it was always moving the nodes, which
    lit-html can do faster when it keeps the nodes in place and just
    changes their content (which it does when you do map).
    2) It didn't work right when the data array changed (see
    screencast in attached bug).
    
    This behaves a little different in that you pass sort-toggle-sk
    the array directly (instead of it sniffing it from the DOM)
    and then it sorts that array (which the client uses to
    render the nodes). Given the client control on whether to use
    map or repeat() allows for the performance optimal solution
    to be chosen (which speeds up sorting on the followup CL
    by a factor of 2).
    
    Change-Id: Ib04b2cacc22f538725c39650a67854a26c2293ea
    Bug: skia:10504

---
+++ golden/modules/sort-toggle-sk/sort-toggle-sk-demo.ts
@@ -0,0 +1,73 @@
+import './index';
+import { html, render } from 'lit-html';
+import { $$ } from 'common-sk/modules/dom';
+import { repeat } from 'lit-html/directives/repeat';
+import { SortToggleSk } from './sort-toggle-sk';
+
+interface DemoSortable {
+  name: string;
+  cost: number;
+  weight: number;
+}
+
+const data: DemoSortable[] = [
+  {
+    name: 'bravo',
+    cost: 10,
+    weight: 36,
+  },
+  {
+    name: 'alfa',
+    cost: 8,
+    weight: 13,
+  },
+  {
+    name: 'charlie',
+    cost: 4,
+    weight: 200,
+  },
+  {
+    name: 'delta',
+    cost: 2,
+    weight: 4,
+  }
+];
+
+
+const rowTemplate = (row: DemoSortable) => html`
+<tr>
+  <td>${row.name}</td>
+  <td>${row.cost}</td>
+  <td>${row.weight}</td>
+</tr>
+`;
+
+// lit-html (or maybe html in general) doesn't like sort-toggle-sk to go inside the table.
+const usingMap = html`
+<sort-toggle-sk .data=${data} @sort-changed=${renderTemplates}>
+  <table>
+     <thead>
+         <tr>
+          <th data-key=name data-sort-toggle-sk=up>Item</th>
+          <th data-key=cost>Cost</th>
+          <th data-key=weight>Weight</th>
+        </tr>
+    </thead>
+    <tbody>
+      <!-- map is generally faster than repeat when the rowTemplate is small, but
+           for this demo, map wasn't working quite right with data being a global.-->
+      ${repeat(data, (row) => row.name, rowTemplate)}
+    </tbody>
+  </table>
+</sort-toggle-sk>`;
+
+
+function renderTemplates() {
+  render(usingMap, $$('#container')!);
+}
+
+renderTemplates();
+// Clients should call sort using the appropriate key and direction after the data is loaded.
+const sortToggleSK = $$('sort-toggle-sk')! as SortToggleSk<DemoSortable>;
+sortToggleSK.sort('name', 'up');
+
---
+++ golden/modules/sort-toggle-sk/sort-toggle-sk_puppeteer_test.ts
@@ -0,0 +1,69 @@
+import { expect } from 'chai';
+import { addEventListenersToPuppeteerPage, EventName,
+    takeScreenshot, TestBed } from '../../../puppeteer-tests/util';
+import { loadGoldWebpack } from '../common_puppeteer_test/common_puppeteer_test';
+import { ElementHandle } from 'puppeteer';
+
+describe('sort-toggle-sk', () => {
+    let testBed: TestBed;
+    before(async () => {
+        testBed = await loadGoldWebpack();
+    });
+
+    let promiseFactory: <T>(eventName: EventName) => Promise<T>;
+    let sortToggleSk: ElementHandle;
+
+    beforeEach(async () => {
+        promiseFactory = await addEventListenersToPuppeteerPage(testBed.page,
+            ['sort-changed']);
+        const loaded = promiseFactory('sort-changed'); // Emitted when sorted.
+        await testBed.page.goto(`${testBed.baseUrl}/dist/sort-toggle-sk.html`);
+        await loaded;
+        sortToggleSk = (await testBed.page.$('#container sort-toggle-sk'))!;
+    });
+
+    it('should render the demo page', async () => {
+        // Smoke test.
+        expect(await testBed.page.$$('sort-toggle-sk')).to.have.length(1);
+    });
+
+    it('should respect the default sort order', async () => {
+        await expectSortOrderToMatch(['alfa', 'bravo', 'charlie', 'delta']);
+        await takeScreenshot(sortToggleSk, 'gold', 'sort-toggle-sk_sort-alpha-ascending');
+    });
+
+    it('can sort alphabetically in descending order', async () => {
+        await clickSortHeader('name');
+
+        await expectSortOrderToMatch(['delta', 'charlie', 'bravo', 'alfa']);
+        await takeScreenshot(sortToggleSk, 'gold', 'sort-toggle-sk_sort-alpha-descending');
+    });
+
+    it('it can sort by numeric values in descending order', async () => {
+        await clickSortHeader('weight');
+
+        await expectSortOrderToMatch(['charlie', 'bravo', 'alfa', 'delta']);
+        await takeScreenshot(sortToggleSk, 'gold', 'sort-toggle-sk_sort-numeric-descending');
+    });
+
+    it('it can sort by numeric values in ascending order', async () => {
+        await clickSortHeader('weight'); // first in descending order
+        await clickSortHeader('weight'); // then should toggle to be in ascending order
+
+        await expectSortOrderToMatch(['delta', 'alfa', 'bravo', 'charlie']);
+        await takeScreenshot(sortToggleSk, 'gold', 'sort-toggle-sk_sort-numeric-ascending');
+    });
+
+    async function expectSortOrderToMatch(names: string[]) {
+        const nameOrder = await sortToggleSk.$$eval('tbody tr td:first-child',
+            (tds: Element[]) => tds.map(td => td.textContent));
+        expect(names).to.deep.equal(nameOrder);
+    }
+
+    async function clickSortHeader(key: string) {
+        const sortEvent = promiseFactory('sort-changed');
+        const header = await sortToggleSk.$(`th[data-key="${key}"]`);
+        await header!.click();
+        await sortEvent;
+    }
+});
