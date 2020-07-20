f93c38fb340550e15564cfda719720cc5ad58754
Joe Gregorio <jcgregorio@google.com>
2020-07-20 11:55:10 -0400

    [perf] Re-create shortcuts from restored regressions.
    Change-Id: I56e3f191a994128d775ead80b6070c3940b5f077

--- perf/go/file/gcssource/gcssource.go
+++ perf/go/file/gcssource/gcssource.go
@@ -73,7 +73,6 @@ func New(ctx context.Context, instanceConfig *config.InstanceConfig, local bool)
 	if err != nil {
 		return nil, skerr.Wrap(err)
 	}
-
 	client := httputils.DefaultClientConfig().WithTokenSource(ts).WithoutRetries().Client()
 	gcsClient, err := storage.NewClient(ctx, option.WithHTTPClient(client))
 	if err != nil {
