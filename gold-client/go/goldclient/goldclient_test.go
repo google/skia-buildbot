3a9eea422fc57e67d97f03a93c5b95d599f8d27f
Leandro Lovisolo <lovisolo@google.com>
2020-07-21 19:57:34 -0400

    [gold] Remove type expectations.BaselineInt and all references.
    Bug: skia:10522
    Change-Id: Id0c79c9fe60a8bc7724f577af1bd1d41e9ffdd9f

--- gold-client/go/goldclient/goldclient_test.go
+++ gold-client/go/goldclient/goldclient_test.go
@@ -2402,7 +2402,7 @@ var diff12 = text.MustToNRGBA(one_by_five.DiffImageOneAndTwo)
 // An example baseline that has a single test at a single commit with a good
 // image and a bad image.
 //
-// TODO(skbug.com/10522): Remove int expectations and rename master_str to master.
+// TODO(skbug.com/10522): Rename master_str to master.
 const mockBaselineJSON = `
 {
   "md5": "7e4081337b3258555906970002a04a59",
@@ -2410,12 +2410,6 @@ const mockBaselineJSON = `
     "ThisIsTheOnlyTest": {
       "beef00d3a1527db19619ec12a4e0df68": "positive",
       "badbadbad1325855590527db196112e0": "negative"
-    }
-  },
-  "master": {
-    "ThisIsTheOnlyTest": {
-      "beef00d3a1527db19619ec12a4e0df68": 1,
-      "badbadbad1325855590527db196112e0": 2
     }
   },
   "Issue": -1
