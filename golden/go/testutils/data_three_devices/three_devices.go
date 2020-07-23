4838f253903d9f3f6b92d1629f0e756f5b4b0d86
Leandro Lovisolo <lovisolo@google.com>
2020-07-23 15:27:21 -0400

    [gold] Remove type expectations.BaselineInt and all references.
    Step 9 of the plan outlined in skbug.com/10522.
    
    Bug: skia:10522
    Change-Id: Id0c79c9fe60a8bc7724f577af1bd1d41e9ffdd9f

--- golden/go/testutils/data_three_devices/three_devices.go
+++ golden/go/testutils/data_three_devices/three_devices.go
@@ -67,12 +67,11 @@ func MakeTestBaseline() *baseline.Baseline {
 	e := MakeTestExpectations()
 	b := baseline.Baseline{
 		Expectations:     e.AsBaseline(),
-		ExpectationsInt:  e.AsBaselineInt(),
 		ChangeListID:     "",
 		CodeReviewSystem: "",
 	}
 	var err error
-	b.MD5, err = util.MD5Sum(b.ExpectationsInt)
+	b.MD5, err = util.MD5Sum(b.Expectations)
 	if err != nil {
 		panic(fmt.Sprintf("Error computing MD5 of the baseline: %s", err))
 	}
