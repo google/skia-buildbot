4450baa2efa66eb1c2846513cbe391f82dbeafa5
Joe Gregorio <jcgregorio@google.com>
2020-07-25 12:23:44 -0400

    [perf]]Add namespaces to memcached cache.Cache implementation.
    Change-Id: I29c49836f47503b86a9a710e9cb1f1c5d5e12b66
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/305897
    Reviewed-by: Joe Gregorio <jcgregorio@google.com>
    Commit-Queue: Joe Gregorio <jcgregorio@google.com>

--- perf/go/cache/memcached/memcached_test.go
+++ perf/go/cache/memcached/memcached_test.go
@@ -8,17 +8,17 @@ import (
 	"go.skia.org/infra/go/testutils/unittest"
 )
 
-const localServerAddress = "127.0.0.1:11211"
+var localServerAddress = []string{"127.0.0.1:11211"}
 
 func TestCache_New_Failure(t *testing.T) {
 	unittest.ManualTest(t)
-	_, err := New("")
+	_, err := New([]string{""}, "")
 	require.Error(t, err)
 }
 
 func TestCache_Get_Success(t *testing.T) {
 	unittest.ManualTest(t)
-	c, err := New(localServerAddress)
+	c, err := New(localServerAddress, "test-namespace")
 	require.NoError(t, err)
 
 	c.Add("foo", "bar")
@@ -32,7 +32,7 @@ func TestCache_Get_Success(t *testing.T) {
 
 func TestCache_Get_FalseOnMiss(t *testing.T) {
 	unittest.ManualTest(t)
-	c, err := New(localServerAddress)
+	c, err := New(localServerAddress, "test-namespace")
 	require.NoError(t, err)
 
 	_, ok := c.Get("quux")
@@ -41,21 +41,36 @@ func TestCache_Get_FalseOnMiss(t *testing.T) {
 
 func TestCache_Exists_Success(t *testing.T) {
 	unittest.ManualTest(t)
-	c, err := New(localServerAddress)
+	c, err := New(localServerAddress, "test-namespace")
 	require.NoError(t, err)
 
-	c.Add("foo", "bar")
+	c.Add("foo", "baz")
 	ok := c.Exists("foo")
 	assert.True(t, ok)
 }
 
-func TestCache_Exists_FalseOnMiss(t *testing.T) {
+func TestCache_ExistsOnlyInOneNamespace_Success(t *testing.T) {
 	unittest.ManualTest(t)
-	c, err := New(localServerAddress)
+	c, err := New(localServerAddress, "test-namespace")
 	require.NoError(t, err)
 
-	_ = c.client.Delete("foo")
-
+	c.Add("foo", "quux")
 	ok := c.Exists("foo")
+	assert.True(t, ok)
+
+	c, err = New(localServerAddress, "test-namespace-2")
+	require.NoError(t, err)
+
+	ok = c.Exists("foo")
+	assert.False(t, ok)
+
+}
+
+func TestCache_Exists_FalseOnMiss(t *testing.T) {
+	unittest.ManualTest(t)
+	c, err := New(localServerAddress, "test-namespace")
+	require.NoError(t, err)
+
+	ok := c.Exists("qux")
 	assert.False(t, ok)
 }
