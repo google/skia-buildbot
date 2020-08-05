83b6f91f33a0fe24431a638f9b9e37a37bdc4033
Kevin Lubick <kjlubick@google.com>
2020-08-05 10:57:22 -0400

    [gold] WIP demo data to experiment with relational DB
    Change-Id: Id8b0ea8ae9557b961bfa6fad7703279f61bb994e

---
+++ golden/go/sql/schema.go
@@ -0,0 +1,108 @@
+package sql
+
+// CockroachDBSchema is the schema for all tables used to store Gold data in CockroachDB.
+const CockroachDBSchema = `
+CREATE TABLE IF NOT EXISTS TraceValues (
+  trace_hash BYTES, -- MD5 hash of the key/values
+  shard BYTES, -- The first N bytes of trace_hash; N is currently 1
+  commit_number INT4,
+  grouping_hash BYTES, -- MD5 hash of the key/values belonging to the grouping. If the grouping 
+                       -- changes, this would require altering the table (should be done very rarely).
+  digest BYTES NOT NULL, -- MD5 hash of the pixel data
+  options_hash BYTES, -- MD5 hash of the options string
+  source_file_hash BYTES NOT NULL, -- MD5 hash of the source file name
+  INDEX (commit_number, grouping_hash, digest), -- Allows for easier joins with Expectations
+  INDEX (trace_hash, commit_number), 
+-- Could add an index on just trace_hash
+  PRIMARY KEY (shard, commit_number, trace_hash) -- gives some locality for both commits and traces
+);
+-- Pre-split this table so to avoid the one range it starts on from being hot during initial
+-- ingestion.
+ALTER TABLE TraceValues SPLIT AT VALUES ('\x03', 0), ('\x07', 0), ('\x0b', 0);
+
+CREATE TABLE IF NOT EXISTS TryJobValues (
+  trace_hash BYTES, -- MD5 hash of the trace string
+  crs_cl_id STRING, -- CodeReviewSystem and CL ID e.g. "gerrit_12345"
+  ps_id STRING, -- PatchSet id
+  digest BYTES NOT NULL, -- MD5 hash of the pixel data
+  options_hash BYTES, -- MD5 hash of the options string
+  cis_tryjob_id STRING NOT NULL, -- ContinuousIntegrationSystem and ID e.g. "buildbucket_12345"
+  source_file_hash BYTES NOT NULL, -- MD5 hash of the source file name
+  PRIMARY KEY (trace_hash, crs_cl_id, ps_id)
+);
+
+CREATE TABLE IF NOT EXISTS Commits (
+  commit_number INT4 PRIMARY KEY, -- The commit_number; a monotonically increasing number as we follow master branch through time.
+  git_hash STRING NOT NULL,
+  commit_time TIMESTAMP WITH TIME ZONE NOT NULL,
+  author STRING,
+  subject STRING
+);
+
+CREATE TABLE IF NOT EXISTS KeyValueMaps ( -- contains trace keys, option keys, etc
+  keys_hash BYTES PRIMARY KEY, -- MD5 hash of the stringified JSON.
+  keys JSONB NOT NULL, -- The trace's keys, e.g. {"color mode":"RGB", "device":"walleye"}
+  INVERTED INDEX (keys)
+);
+
+CREATE TABLE IF NOT EXISTS SourceFiles (
+  source_file_hash BYTES PRIMARY KEY, -- The MD5 hash of the source file name
+  source_file STRING NOT NULL,  -- The full name of the source file, e.g. gs://bucket/2020/01/02/03/15/foo.json
+  last_ingested TIMESTAMP WITH TIME ZONE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS Expectations (
+  grouping_hash BYTES, -- MD5 hash of the grouping JSON
+  digest BYTES NOT NULL, -- MD5 hash of the pixel data
+  label SMALLINT, -- 0 for untriaged, 1 for positive, 2 for negative
+  start_index INT4, -- Reserved for future use with expectation ranges
+  end_index INT4, -- Reserved for future use with expectation ranges
+  INDEX (label),
+  PRIMARY KEY (digest, grouping_hash) -- start_index should be on primary key too eventually.
+);
+
+CREATE TABLE IF NOT EXISTS CLExpectations (
+  crs_cl_id STRING, -- e.g. CodeReviewSystem and CL ID "gerrit_12345"
+  grouping_hash BYTES, -- MD5 hash of the grouping JSON
+  digest BYTES NOT NULL, -- MD5 hash of the pixel data
+  label SMALLINT, -- 0 for untriaged, 1 for positive, 2 for negative
+  start_index INT4, -- Reserved for future use with expectation ranges
+  end_index INT4, -- Reserved for future use with expectation ranges
+  PRIMARY KEY (digest, crs_cl_id, grouping_hash) -- start_index should be on primary key too eventually.
+);
+
+CREATE TABLE IF NOT EXISTS ExpectationsDeltas (
+  expectations_delta_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  expectations_record_id UUID,
+  grouping_hash BYTES, -- MD5 hash of the grouping JSON
+  digest BYTES NOT NULL, -- MD5 hash of the pixel data
+  label_before SMALLINT, -- 0 for untriaged, 1 for positive, 2 for negative
+  label_after SMALLINT, -- 0 for untriaged, 1 for positive, 2 for negative
+  start_index INT4, -- Reserved for future use with expectation ranges
+  end_index_before INT4, -- Reserved for future use with expectation ranges
+  end_index_after INT4 -- Reserved for future use with expectation ranges
+);
+
+CREATE TABLE IF NOT EXISTS ExpectationsRecords (
+  expectations_record_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  crs_cl_id STRING, -- e.g. CodeReviewSystem and CL ID "gerrit_12345". Can be empty string.
+  user_name STRING,
+  time TIMESTAMP WITH TIME ZONE,
+  num_changes INT4
+);
+
+CREATE TABLE IF NOT EXISTS DiffMetrics (
+  left_digest BYTES NOT NULL, -- MD5 hash of the pixel data.
+  right_digest BYTES NOT NULL, -- MD5 hash of the pixel data
+  num_diff_pixels INT4,
+  pixel_diff_percent FLOAT4,
+-- This is what the RGBAMinFilter and RGBAMaxFilter apply to. There does not appear to be a way to
+-- do this via SQL statements (in a clean way).
+  max_channel_diff INT2,
+  max_rgba_diff INT2[], -- max delta in the red, green, blue, alpha channels.
+  dimensions_differ BOOL,
+  PRIMARY KEY (left_digest, right_digest)
+);
+`
+
+// TODO(kjlubick) tables for PS/CL/TJ etc
---
+++ golden/go/sql/statements.go
@@ -0,0 +1,99 @@
+package sql
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"encoding/json"
+	"strconv"
+	"strings"
+
+	"go.skia.org/infra/go/skerr"
+	"go.skia.org/infra/golden/go/expectations"
+	"go.skia.org/infra/golden/go/types"
+)
+
+func ValuesPlaceholders(valuesPerRow, numRows int) (string, error) {
+	if valuesPerRow <= 0 || numRows <= 0 {
+		return "", skerr.Fmt("Cannot make ValuesPlaceholder with 0 rows or 0 values per row")
+	}
+	values := strings.Builder{}
+	// There are at most 5 bytes per value that need to be written
+	values.Grow(5 * valuesPerRow * numRows)
+	// All WriteString calls below return nil errors, as specified in the documentation of
+	// strings.Builder, so it is safe to ignore them.
+	for argIdx := 1; argIdx <= valuesPerRow*numRows; argIdx += valuesPerRow {
+		if argIdx != 1 {
+			_, _ = values.WriteString(",")
+		}
+		_, _ = values.WriteString("(")
+		for i := 0; i < valuesPerRow; i++ {
+			if i != 0 {
+				_, _ = values.WriteString(",")
+			}
+			_, _ = values.WriteString("$")
+			_, _ = values.WriteString(strconv.Itoa(argIdx + i))
+		}
+		_, _ = values.WriteString(")")
+	}
+	return values.String(), nil
+}
+
+// SerializeMap returns the given map in JSON and the md5 of that json string. nil maps will be
+// treated as empty maps.
+func SerializeMap(m map[string]string) (string, []byte, error) {
+	var str []byte
+	var err error
+	if len(m) == 0 {
+		str = []byte("{}")
+	} else {
+		str, err = json.Marshal(m)
+		if err != nil {
+			return "", nil, err
+		}
+	}
+
+	h := md5.Sum(str)
+	return string(str), h[:], err
+}
+
+// DigestToBytes returns the given digest as bytes. It returns an error if a missing digest is
+// passed in
+func DigestToBytes(d types.Digest) ([]byte, error) {
+	if len(d) == 0 {
+		return nil, skerr.Fmt("Empty/missing digest passed in")
+	}
+	return hex.DecodeString(string(d))
+}
+
+const numShards = 8
+
+// ComputeTraceValueShard computes a shard in the range [0,numShards) based off the given trace
+// hash bytes. This shard range was chosen to be a small number (relative to the thousands of ranges
+// that the TraceValues database occupies) that is larger than the number of nodes in the
+// cockroachdb cluster. See the overall design doc for more explanation about data locality and
+// sharding.
+func ComputeTraceValueShard(traceHash []byte) []byte {
+	return []byte{traceHash[0] % numShards}
+}
+
+type ExpectationsLabel int
+
+const (
+	LabelUntriaged ExpectationsLabel = 0
+	LabelPositive  ExpectationsLabel = 1
+	LabelNegative  ExpectationsLabel = 2
+)
+
+// ConvertLabelFromString converts the string form of a label to an int which is stored in the SQL
+// database.
+func ConvertLabelFromString(e expectations.Label) ExpectationsLabel {
+	switch e {
+	case expectations.Untriaged:
+		return LabelUntriaged
+	case expectations.Positive:
+		return LabelPositive
+	case expectations.Negative:
+		return LabelNegative
+	}
+	panic("unknown label " + e)
+}
