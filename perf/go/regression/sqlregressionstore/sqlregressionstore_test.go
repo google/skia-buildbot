475490ff2cdc8087f8bc47972b9f0d46fabe8c48
Joe Gregorio <jcgregorio@google.com>
2020-08-05 14:20:27 -0400

    [perf] Don't apply migations on startup.
    Way too many "Error: can't acquire lock. At migrations.go:22" on startup.
    
    Change-Id: I1c22b07f3f315d9bd9276edd02c243e8000c18cb

--- perf/go/regression/sqlregressionstore/sqlregressionstore_test.go
+++ perf/go/regression/sqlregressionstore/sqlregressionstore_test.go
@@ -18,7 +18,7 @@ func TestSQLRegressionStore_CockroachDB(t *testing.T) {
 	// Common regressiontest tests.
 	for name, subTest := range regressiontest.SubTests {
 		t.Run(name, func(t *testing.T) {
-			db, cleanup := sqltest.NewCockroachDBForTests(t, "regstore", sqltest.ApplyMigrations)
+			db, cleanup := sqltest.NewCockroachDBForTests(t, "regstore")
 			// If this test times out then comment out the cleanup(), as it may hide the
 			// actual errors.
 			defer cleanup()
@@ -32,7 +32,7 @@ func TestSQLRegressionStore_CockroachDB(t *testing.T) {
 	// SQLRegressionStore specific tests.
 	for name, subTest := range subTests {
 		t.Run(name, func(t *testing.T) {
-			db, cleanup := sqltest.NewCockroachDBForTests(t, "regstore", sqltest.ApplyMigrations)
+			db, cleanup := sqltest.NewCockroachDBForTests(t, "regstore")
 			// If this test times out then comment out the cleanup(), as it may hide the
 			// actual errors.
 			defer cleanup()
