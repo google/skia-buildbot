ffbded92ba36381fd367e05a243739460a9a8ac7
Weston Tracey <westont@google.com>
2020-08-13 11:18:11 -0400

    [status] Add basic Twirp skeleton.
    Placeholder protobuf definition for now, followup CLs typify the needed pieces.
    
    Change-Id: I45c5664952cf9133d982df8b46ba125653735e76

---
+++ status/go/rpc/server_impl.go
@@ -0,0 +1,13 @@
+package rpc
+
+// Generate Go structs and Typescript classes from protobuf definitions.
+//go:generate protoc --twirp_out=. --go_out=. statusFe.proto
+//go:generate mv ./go.skia.org/infra/status/go/rpc/statusFe.twirp.go ./statusFe.twirp.go
+//go:generate mv ./go.skia.org/infra/status/go/rpc/statusFe.pb.go ./statusFe.pb.go
+//go:generate rm -rf ./go.skia.org
+//go:generate goimports -w statusFe.pb.go
+//go:generate goimports -w statusFe.twirp.go
+//go:generate protoc --twirp_typescript_out=../../modules/rpc statusFe.proto
+//go:generate ../../node_modules/typescript-formatter/bin/tsfmt -r ../../modules/rpc/statusFe.ts ../../modules/rpc/twirp.ts
+
+// TODO(westont): Implement a server.
