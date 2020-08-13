7f9d5df1ba80c8be02b305f44f7e4a7b2cf81434
Joe Gregorio <jcgregorio@google.com>
2020-08-13 13:28:52 -0400

    [perf] Add monitoring for cockroachdb instances.
    Follows the guidance here:
      https://www.cockroachlabs.com/docs/v20.1/monitor-cockroachdb-with-prometheus.html for details on CockroachDB monitoring.
    
    Also includes updates to promk-absent to ignore computed
    and multi-comparison expressions when building Absent() rules.
    
    
    Change-Id: I91a90bd2b704b2c55ba79a1e7aa1ac6594073ff5
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/310036
    Reviewed-by: Kevin Lubick <kjlubick@google.com>

--- promk/go/promk-absent/main_test.go
+++ promk/go/promk-absent/main_test.go
@@ -3,47 +3,62 @@ package main
 import (
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"go.skia.org/infra/go/testutils/unittest"
 )
 
-func TestEquationFromExpr(t *testing.T) {
+func TestEquationFromExpr_Equality(t *testing.T) {
 	unittest.SmallTest(t)
 
-	testCases := []struct {
-		value    string
-		expected string
-		message  string
-	}{
-		{
-			value:    "up == 0",
-			expected: "up",
-			message:  "==",
-		},
-		{
-			value:    "liveness_ci_pubsub_receive_s > 60 * 60 * 24 * 2",
-			expected: "liveness_ci_pubsub_receive_s",
-			message:  ">",
-		},
-		{
-			value:    "cq_watcher_in_flight_waiting_in_cq{app=\"cq-watcher\"} >= 10",
-			expected: "cq_watcher_in_flight_waiting_in_cq{app=\"cq-watcher\"}",
-			message:  "{app=...}",
-		},
-		{
-			value:    "healthy{app=\"ct-master\"} != 1",
-			expected: "healthy{app=\"ct-master\"}",
-			message:  "!",
-		},
-		{
-			value:    "",
-			expected: "",
-			message:  "empty string",
-		},
-	}
+	got, ignore := equationFromExpr("up == 0")
+	assert.False(t, ignore)
+	assert.Equal(t, "up", got)
+}
 
-	for _, tc := range testCases {
-		if got, want := equationFromExpr(tc.value), tc.expected; got != want {
-			t.Errorf("Failed case Got %v Want %v: %s", got, want, tc.message)
-		}
-	}
+func TestEquationFromExpr_LessThan(t *testing.T) {
+	unittest.SmallTest(t)
+
+	got, ignore := equationFromExpr("liveness_ci_pubsub_receive_s > 60 * 60 * 24 * 2")
+	assert.False(t, ignore)
+	assert.Equal(t, "liveness_ci_pubsub_receive_s", got)
+}
+
+func TestEquationFromExpr_LessThanOrEqual(t *testing.T) {
+	unittest.SmallTest(t)
+
+	got, ignore := equationFromExpr("cq_watcher_in_flight_waiting_in_cq{app=\"cq-watcher\"} >= 10")
+	assert.False(t, ignore)
+	assert.Equal(t, "cq_watcher_in_flight_waiting_in_cq{app=\"cq-watcher\"}", got)
+}
+
+func TestEquationFromExpr_NotEqual(t *testing.T) {
+	unittest.SmallTest(t)
+
+	got, ignore := equationFromExpr("healthy{app=\"ct-perf\"} != 1")
+	assert.False(t, ignore)
+	assert.Equal(t, "healthy{app=\"ct-perf\"}", got)
+}
+
+func TestEquationFromExpr_Empty(t *testing.T) {
+	unittest.SmallTest(t)
+
+	got, ignore := equationFromExpr("")
+	assert.False(t, ignore)
+	assert.Equal(t, "", got)
+}
+
+func TestEquationFromExpr_IgnoreComputedEquations(t *testing.T) {
+	unittest.SmallTest(t)
+
+	got, ignore := equationFromExpr("computed:value")
+	assert.True(t, ignore)
+	assert.Equal(t, "", got)
+}
+
+func TestEquationFromExpr_IgnoreMultipleComparisons(t *testing.T) {
+	unittest.SmallTest(t)
+
+	got, ignore := equationFromExpr("a < b and b > c")
+	assert.True(t, ignore)
+	assert.Equal(t, "", got)
 }
