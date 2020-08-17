671f9c9fdf01f956af6ed222224c7b47fbd0ca82
Joe Gregorio <jcgregorio@google.com>
2020-08-17 14:58:09 -0400

    [sk8s] Reboot Android devices after each test.
    Change-Id: I0618224d0f7b26aa9ef8497ed1bfa29d78a1cfc4

--- sk8s/go/bot_config/adb/adb_test.go
+++ sk8s/go/bot_config/adb/adb_test.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
@@ -35,8 +36,64 @@ voltage: 3997
 temperature: 248
 technology: Li-ion`
 
+	adbShellGetUptimeSuccess = `135.7 523.8`
+
 	nonZeroExitCode = 123
 )
+
+func TestReboot_HappyPath(t *testing.T) {
+	unittest.SmallTest(t)
+
+	ctx := executil.FakeTestsContext("Test_FakeExe_AdbReboot_Success")
+
+	a := New()
+	err := a.Reboot(ctx)
+	require.NoError(t, err)
+}
+
+func TestReboot_ErrFromAdbNonZeroExitCode(t *testing.T) {
+	unittest.SmallTest(t)
+
+	ctx := executil.FakeTestsContext("Test_FakeExe_Reboot_NonZeroExitCode")
+
+	a := New()
+	err := a.Reboot(ctx)
+	require.Error(t, err)
+}
+
+func TestUptime_HappyPath(t *testing.T) {
+	unittest.SmallTest(t)
+
+	ctx := executil.FakeTestsContext("Test_FakeExe_AdbShellGetUptime_Success")
+
+	a := New()
+	got, err := a.Uptime(ctx)
+	require.NoError(t, err)
+	// Note that we truncate the 135.7 seconds to 135.
+	assert.Equal(t, 135*time.Second, got)
+}
+
+func TestUptime_ErrOnMalformedUptimeContents(t *testing.T) {
+	unittest.SmallTest(t)
+
+	ctx := executil.FakeTestsContext("Test_FakeExe_AdbShellGetUptime_MalformedContents")
+
+	a := New()
+	got, err := a.Uptime(ctx)
+	require.Error(t, err)
+	assert.Equal(t, time.Duration(0), got)
+}
+
+func TestUptime_ErrFromAdbNonZeroExitCode(t *testing.T) {
+	unittest.SmallTest(t)
+
+	ctx := executil.FakeTestsContext("Test_FakeExe_Uptime_NonZeroExitCode")
+
+	a := New()
+	got, err := a.Uptime(ctx)
+	require.Error(t, err)
+	assert.Equal(t, time.Duration(0), got)
+}
 
 func TestRawProperties_HappyPath(t *testing.T) {
 	unittest.SmallTest(t)
@@ -148,6 +205,74 @@ func TestRawDumpSys_ErrOnNonZeroExitCode(t *testing.T) {
 }
 
 func Test_FakeExe_RawDumpSys_NonZeroExitCode(t *testing.T) {
+	unittest.FakeExeTest(t)
+	if os.Getenv(executil.OverrideEnvironmentVariable) == "" {
+		return
+	}
+
+	fmt.Fprintf(os.Stderr, "error: no devices/emulators found")
+
+	os.Exit(nonZeroExitCode)
+}
+
+func Test_FakeExe_AdbShellGetUptime_Success(t *testing.T) {
+	unittest.FakeExeTest(t)
+	if os.Getenv(executil.OverrideEnvironmentVariable) == "" {
+		return
+	}
+
+	// Check the input arguments to make sure they were as expected.
+	args := executil.OriginalArgs()
+	require.Equal(t, []string{"adb", "shell", "cat", "/proc/uptime"}, args)
+
+	fmt.Print(adbShellGetUptimeSuccess)
+
+	// Force exit so we don't get PASS in the output.
+	os.Exit(0)
+}
+
+func Test_FakeExe_Uptime_NonZeroExitCode(t *testing.T) {
+	unittest.FakeExeTest(t)
+	if os.Getenv(executil.OverrideEnvironmentVariable) == "" {
+		return
+	}
+
+	fmt.Fprintf(os.Stderr, "error: no devices/emulators found")
+
+	os.Exit(nonZeroExitCode)
+}
+
+func Test_FakeExe_AdbShellGetUptime_MalformedContents(t *testing.T) {
+	unittest.FakeExeTest(t)
+	if os.Getenv(executil.OverrideEnvironmentVariable) == "" {
+		return
+	}
+
+	// Check the input arguments to make sure they were as expected.
+	args := executil.OriginalArgs()
+	require.Equal(t, []string{"adb", "shell", "cat", "/proc/uptime"}, args)
+
+	fmt.Print("this is not valid contents for /proc/uptime")
+
+	// Force exit so we don't get PASS in the output.
+	os.Exit(0)
+}
+
+func Test_FakeExe_AdbReboot_Success(t *testing.T) {
+	unittest.FakeExeTest(t)
+	if os.Getenv(executil.OverrideEnvironmentVariable) == "" {
+		return
+	}
+
+	// Check the input arguments to make sure they were as expected.
+	args := executil.OriginalArgs()
+	require.Equal(t, []string{"adb", "reboot"}, args)
+
+	// Force exit so we don't get PASS in the output.
+	os.Exit(0)
+}
+
+func Test_FakeExe_Reboot_NonZeroExitCode(t *testing.T) {
 	unittest.FakeExeTest(t)
 	if os.Getenv(executil.OverrideEnvironmentVariable) == "" {
 		return
