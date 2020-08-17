da66f40b39b527fe0e195b4dff7b0a61623fcbb7
Joe Gregorio <jcgregorio@google.com>
2020-08-17 12:00:47 -0400

    [kube] Add download-secret.sh command.
    Change-Id: I986d675171acf2e7763364d3b2c162551eee2b10
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/311037
    Reviewed-by: Ravi Mistry <rmistry@google.com>

---
+++ kube/secrets/download-secret.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Download a secret from berglas.
+
+if [ $# -ne 2 ]; then
+    echo "$0 <cluster-name> <secret-name>"
+    exit 1
+fi
+
+REL=$(dirname "$0")
+source ${REL}/config.sh
+
+CLUSTER=$1
+SECRET_NAME=$2
+
+source ../../bash/ramdisk.sh
+
+FILES=$(${REL}/get-secret.sh ${CLUSTER} ${SECRET_NAME} \
+  | kubectl apply -f - --dry-run -o json \
+  | jq -j '.data | keys | .[] | . + " "')
+echo ${FILES}
+for FILE in ${FILES}; do
+  ${REL}/get-secret.sh ${CLUSTER} ${SECRET_NAME} \
+  | kubectl apply -f - --dry-run -o json \
+  | jq -r ".data[\"${FILE}\"]" \
+  | base64 -d > /tmp/ramdisk/${FILE}
+done
+
+echo "Downloaded the ${SECRET_NAME} secrets to /tmp/ramdisk."
+echo ""
+read -r -p "Press enter when you are done. /tmp/ramdisk will be deleted." key
