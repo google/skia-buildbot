acd491393afb866c39359bfc5f7b8b3498972e5b
Joe Gregorio <jcgregorio@google.com>
2020-08-17 11:48:31 -0400

    [perf] Migrate android to cdb.
    Change-Id: I6c6cfa26f224940969c3752ef778814fc232f32c
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/311038
    Reviewed-by: Joe Gregorio <jcgregorio@google.com>

---
+++ perf/migrate.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# An example migration script.
+
+perf-tool database backup alerts      --local --config_filename=./configs/android-prod.json \
+  --connection_string=postgresql://root@localhost:26257/android?sslmode=disable --out=/tmp/alerts.dat
+perf-tool database backup regressions  --config_filename=./configs/android-prod.json \
+  --connection_string=postgresql://root@localhost:26257/android?sslmode=disable --out=/tmp/regressions.dat \
+  --backup_to_date=2020-01-01
+
+perf-tool database restore alerts      --local --config_filename=./configs/cdb-android-prod.json \
+  --connection_string=postgresql://root@localhost:26257/android?sslmode=disable --in=/tmp/alerts.dat
+perf-tool database restore regressions --local --config_filename=./configs/cdb-android-prod.json \
+  --connection_string=postgresql://root@localhost:26257/android?sslmode=disable --in=/tmp/regressions.dat
