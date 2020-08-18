b6ea16339042f81ec1544a197fa5e26e0059bc20
Joe Gregorio <jcgregorio@google.com>
2020-08-18 17:20:39 -0400

    [perf] Add database migration to perf-tool.
    Change-Id: I5f9881f386d61e07f57dff5954370371c378534b

--- perf/go/perf-tool/main.go
+++ perf/go/perf-tool/main.go
@@ -10,6 +10,7 @@ import (
 	"io"
 	"net/url"
 	"os"
+	"strings"
 	"time"
 
 	"cloud.google.com/go/pubsub"
@@ -30,6 +31,8 @@ import (
 	"go.skia.org/infra/perf/go/config"
 	"go.skia.org/infra/perf/go/regression"
 	"go.skia.org/infra/perf/go/shortcut"
+	"go.skia.org/infra/perf/go/sql/migrations"
+	"go.skia.org/infra/perf/go/sql/migrations/cockroachdb"
 	"go.skia.org/infra/perf/go/tracestore"
 	"go.skia.org/infra/perf/go/types"
 	"google.golang.org/api/option"
@@ -112,6 +115,13 @@ func main() {
 		Use: "database [sub]",
 	}
 	databaseCmd.PersistentFlags().String(connectionStringFlag, "", "Override the connection_string in the config file.")
+
+	databaseBackupMigrateSubCmd := &cobra.Command{
+		Use:   "migrate",
+		Short: "Migrate the database to the latest schema.",
+		RunE:  databaseDatabaseMigrateSubAction,
+	}
+	databaseCmd.AddCommand(databaseBackupMigrateSubCmd)
 
 	databaseBackupSubCmd := &cobra.Command{
 		Use: "backup [sub]",
@@ -273,6 +283,24 @@ func updateInstanceConfigWithOverride(c *cobra.Command) {
 	if connectionStringOverride != "" {
 		instanceConfig.DataStoreConfig.ConnectionString = connectionStringOverride
 	}
+}
+
+func databaseDatabaseMigrateSubAction(c *cobra.Command, args []string) error {
+	updateInstanceConfigWithOverride(c)
+
+	cockroachdbMigrations, err := cockroachdb.New()
+	if err != nil {
+		return skerr.Wrapf(err, "failed to load migrations")
+	}
+
+	connectionString := strings.Replace(instanceConfig.DataStoreConfig.ConnectionString, "postgresql://", "cockroachdb://", 1)
+
+	err = migrations.Up(cockroachdbMigrations, connectionString)
+	if err != nil {
+		return skerr.Wrapf(err, "failed to apply migrations to %q", connectionString)
+	}
+	fmt.Printf("Successfully applied SQL Schema migrations to %q\n", connectionString)
+	return nil
 }
 
 func databaseDatabaseBackupAlertsSubAction(c *cobra.Command, args []string) error {
