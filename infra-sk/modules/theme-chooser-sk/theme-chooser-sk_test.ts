42a81b9b53a1dcad54f6c25fe04ef3ca5adcbda6
Leandro Lovisolo <lovisolo@google.com>
2020-08-18 05:28:10 -0400

    [infra-sk] Port theme-chooser-sk to TypeScript.
    Rationale for doing this now: this is the only infra-sk component used from //machine, which we'll use to experiment with Bazel.
    
    Will add BUIILD rules for this component in a follow-up CL.
    
    Bug: skia:10614
    Change-Id: I411a0cf03be7d0fab1399c205dd8489e92b47e29

---
+++ infra-sk/modules/theme-chooser-sk/theme-chooser-sk_test.ts
@@ -0,0 +1,30 @@
+import './index';
+import { setUpElementUnderTest, eventPromise } from '../../../infra-sk/modules/test_util';
+import { ThemeChooserSk, ThemeChooserSkEventDetail, DARKMODE_CLASS} from './theme-chooser-sk';
+import { expect } from 'chai';
+
+describe('theme-chooser-sk', () => {
+  const newInstance = setUpElementUnderTest<ThemeChooserSk>('theme-chooser-sk');
+
+  let themeChooserSk: ThemeChooserSk;
+
+  beforeEach(() => {
+    themeChooserSk = newInstance();
+  })
+
+  it('generates event and toggles from darkmode when clicked', async () => {
+    themeChooserSk.darkmode = true;
+    const event = eventPromise<CustomEvent<ThemeChooserSkEventDetail>>('theme-chooser-toggle');
+    themeChooserSk.click();
+    expect((await event).detail.darkmode).to.be.false;
+    expect(document.body.classList.contains(DARKMODE_CLASS)).to.be.false;
+  });
+
+  it('generates event and toggles to darkmode when clicked', async () => {
+    themeChooserSk.darkmode = false;
+    const event = eventPromise<CustomEvent<ThemeChooserSkEventDetail>>('theme-chooser-toggle');
+    themeChooserSk.click();
+    expect((await event).detail.darkmode).to.be.true;
+    expect(document.body.classList.contains(DARKMODE_CLASS)).to.be.true;
+  });
+});
