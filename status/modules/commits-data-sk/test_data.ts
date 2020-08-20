9fed9bf3a20f09f9eea96805532b3c90e2c5ebe5
Weston Tracey <westont@google.com>
2020-08-20 12:32:10 -0400

    Add commits-table-sk element.
    Change-Id: I26f175fb1b4d21f1df468764aec8e7a3d39511a8

--- status/modules/commits-data-sk/test_data.ts
+++ status/modules/commits-data-sk/test_data.ts
@@ -46,6 +46,14 @@ export const commentTaskSpec: Comment =
   taskspecname: 'Build-Some-Stuff',
   commithash: ''
 };
+const task0 = { commits: ['abc123'], id: '1', name: 'Build-Android-Stuff-Metal', revision: 'whatsarevisioninthiscontext', status: 'SUCCESS', swarmingtaskid: 'idforswarming' };
+const task1 = Object.assign(JSON.parse(JSON.stringify(task0)), {id: '2', name: 'Test-Android-Pixel12-Stuff'});
+const task2 = Object.assign(JSON.parse(JSON.stringify(task0)), {id: '3', name: 'Housekeeper-PerCommit-Small'});
+const task3 = Object.assign(JSON.parse(JSON.stringify(task0)), {id: '4', name: 'Housekeeper-PerCommit-Medium'});
+const task4 = Object.assign(JSON.parse(JSON.stringify(task0)), {id: '5', name: 'Housekeeper-PerCommit-Large'});
+const task5 = Object.assign(JSON.parse(JSON.stringify(task0)), {id: '6', name: 'Build-iOS-Mac15.5-Metal'});
+const task6 = Object.assign(JSON.parse(JSON.stringify(task0)), {id: '7', name: 'Build-iOS-Mac15-Metal'});
+const task7 = Object.assign(JSON.parse(JSON.stringify(task0)), {id: '8', name: 'Test-iOS-Mac15.5-Metal'});
 export const incrementalResponse0: IncrementalCommitsResponse = {
   metadata: { pod: 'podd' },
   update: {
@@ -58,6 +66,6 @@ export const incrementalResponse0: IncrementalCommitsResponse = {
     commits: [
       { hash: 'abc123', author: 'bob@example.com', parents: ['parentofabc123'], subject: 'current HEAD', body: 'the most recent commit', timestamp: '34613488' },
       { hash: 'parentofabc123', author: 'alison@example.com', parents: ['grandparentofabc123'], subject: '2nd from HEAD', body: 'the commit that comes before the most recent commit', timestamp: '34613288' }],
-    tasks: [{commits: ['abc123'], id: '99999', name: 'Build-Some-Stuff', revision: 'whatsarevisioninthiscontext', status: 'SUCCESS', swarmingtaskid: 'idforswarming'}],
+    tasks: [task0, task1, task2, task3, task4, task5, task6, task7],
   }
 };
