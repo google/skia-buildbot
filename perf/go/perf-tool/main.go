0b19d125ebbdf8b19d6708405e5f582c55a8eb85
Joe Gregorio <jcgregorio@google.com>
2020-08-20 11:22:31 -0400

    [perf] Add tiles list action to perf-tool.
    Change-Id: If4c87a56c6b1b7f7cdd576840c8e35d734441aba

--- perf/go/perf-tool/main.go
+++ perf/go/perf-tool/main.go
@@ -47,6 +47,8 @@ var (
 
 // flags
 var (
+	tileListNumFlag int32
+
 	indicesTileFlag types.TileNumber
 	tracesTileFlag  types.TileNumber
 
@@ -121,7 +123,6 @@ func main() {
 		Short: "Migrate the database to the latest version of the schema.",
 		RunE:  databaseMigrateSubAction,
 	}
-	databaseCmd.AddCommand(databaseMigrateSubCmd)
 
 	databaseBackupSubCmd := &cobra.Command{
 		Use: "backup [sub]",
@@ -162,8 +163,6 @@ using the same input file for both restores.
 	databaseBackupRegressionsSubCmd.Flags().String(backupToDateFlag, "", "How far back in time to back up Regressions. Defaults to four weeks.")
 	databaseBackupSubCmd.AddCommand(databaseBackupRegressionsSubCmd)
 
-	databaseCmd.AddCommand(databaseBackupSubCmd)
-
 	databaseRestoreSubCmd := &cobra.Command{
 		Use: "restore [sub]",
 	}
@@ -190,7 +189,10 @@ using the same input file for both restores.
 	}
 	databaseRestoreSubCmd.AddCommand(databaseRestoreRegressionsSubCmd)
 
-	databaseCmd.AddCommand(databaseRestoreSubCmd)
+	databaseCmd.AddCommand(
+		databaseMigrateSubCmd,
+		databaseBackupSubCmd,
+		databaseRestoreSubCmd)
 
 	indicesCmd := &cobra.Command{
 		Use: "indices [sub]",
@@ -230,9 +232,16 @@ using the same input file for both restores.
 		Short: "Prints the offset of the last (most recent) tile.",
 		RunE:  tilesLastAction,
 	}
+	tilesList := &cobra.Command{
+		Use:   "list",
+		Short: "Prints the last N tiles and the number of traces they contain.",
+		RunE:  tilesListAction,
+	}
+	tilesList.Flags().Int32Var(&tileListNumFlag, "num", 10, "The number of tiles to display.")
 
 	tilesCmd.AddCommand(
 		tilesLast,
+		tilesList,
 	)
 
 	tracesCmd := &cobra.Command{
@@ -769,6 +778,26 @@ func tilesLastAction(c *cobra.Command, args []string) error {
 		return err
 	}
 	fmt.Println(tileNumber)
+	return nil
+}
+
+func tilesListAction(c *cobra.Command, args []string) error {
+	ctx := context.Background()
+	store := mustGetStore()
+
+	latestTileNumber, err := store.GetLatestTile()
+	if err != nil {
+		return err
+	}
+	fmt.Println("tile\tnum traces")
+	for tileNumber := latestTileNumber; tileNumber > latestTileNumber-types.TileNumber(tileListNumFlag); tileNumber-- {
+		count, err := store.TraceCount(ctx, tileNumber)
+		if err != nil {
+			return skerr.Wrapf(err, "failed to count traces for tile %d", tileNumber)
+		}
+		fmt.Printf("%d\t%d\n", tileNumber, count)
+	}
+
 	return nil
 }
 
