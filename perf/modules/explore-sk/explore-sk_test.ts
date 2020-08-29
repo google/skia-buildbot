fdfc98370cbf26e1d94b456897eb11b0a0766979
Joe Gregorio <jcgregorio@google.com>
2020-08-29 09:21:40 -0400

    [perf] Remove shift keys and just use zooming.
    Change-Id: Ie8e7942a36a887e7dd65f25167c192c523f81093

---
+++ perf/modules/explore-sk/explore-sk_test.ts
@@ -0,0 +1,63 @@
+import { calculateRangeChange } from './explore-sk';
+import { assert } from 'chai';
+
+describe('calculateRangeChange', () => {
+  const offsets: [number, number] = [100, 120];
+
+  it('finds a left range increase', () => {
+    const zoom: [number, number] = [-1, 10];
+    const clampedZoom: [number, number] = [0, 10];
+
+    const ret = calculateRangeChange(zoom, clampedZoom, offsets);
+    assert.isTrue(ret.rangeChange);
+
+    // We shift the beginning of the range by RANGE_CHANGE_ON_ZOOM_PERCENT of
+    // the total range.
+    assert.deepEqual(ret.newOffsets, [90, 120]);
+  });
+
+  it('finds a right range increase', () => {
+    const zoom: [number, number] = [0, 12];
+    const clampedZoom: [number, number] = [0, 10];
+
+    const ret = calculateRangeChange(zoom, clampedZoom, offsets);
+    assert.isTrue(ret.rangeChange);
+
+    // We shift the end of the range by RANGE_CHANGE_ON_ZOOM_PERCENT of the
+    // total range.
+    assert.deepEqual(ret.newOffsets, [100, 130]);
+  });
+
+  it('find an increase in the range in both directions', () => {
+    const zoom: [number, number] = [-1, 11];
+    const clampedZoom: [number, number] = [0, 10];
+
+    const ret = calculateRangeChange(zoom, clampedZoom, offsets);
+    assert.isTrue(ret.rangeChange);
+
+    // We shift both the begin and end of the range by
+    // RANGE_CHANGE_ON_ZOOM_PERCENT of the total range.
+    assert.deepEqual(ret.newOffsets, [90, 130]);
+  });
+
+  it('find an increase in the range in both directions and clamps the offset', () => {
+    const zoom: [number, number] = [-1, 11];
+    const clampedZoom: [number, number] = [0, 10];
+    const offsets: [number, number] = [0, 100];
+
+    const ret = calculateRangeChange(zoom, clampedZoom, offsets);
+    assert.isTrue(ret.rangeChange);
+
+    // We shift both the begin and end of the range by
+    // RANGE_CHANGE_ON_ZOOM_PERCENT of the total range.
+    assert.deepEqual(ret.newOffsets, [0, 150]);
+  });
+
+  it('does not find a range change', () => {
+    const zoom: [number, number] = [0, 10];
+    const clampedZoom: [number, number] = [0, 10];
+
+    const ret = calculateRangeChange(zoom, clampedZoom, offsets);
+    assert.isFalse(ret.rangeChange);
+  });
+});
