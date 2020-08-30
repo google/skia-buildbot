4979499df7bf9fa30e724c28789686be931eeb28
Joe Gregorio <jcgregorio@google.com>
2020-08-30 08:44:13 -0400

    [perf] Convert perf notify from cid.CommitDetails to perfgit.Commit.
    Change-Id: I56421c8392536fe488549779eed8ef08a88bdab6
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/314016
    Reviewed-by: Joe Gregorio <jcgregorio@google.com>
    Commit-Queue: Joe Gregorio <jcgregorio@google.com>

--- perf/go/notify/notify.go
+++ perf/go/notify/notify.go
@@ -9,8 +9,8 @@ import (
 
 	"go.skia.org/infra/go/sklog"
 	"go.skia.org/infra/perf/go/alerts"
-	"go.skia.org/infra/perf/go/cid"
 	"go.skia.org/infra/perf/go/clustering2"
+	perfgit "go.skia.org/infra/perf/go/git"
 )
 
 const (
@@ -20,7 +20,7 @@ const (
 	A Perf Regression has been found at:
 </p>
 <p style="padding: 1em;">
-	<a href="{{.URL}}/g/t/{{.Commit.Hash}}">{{.URL}}/g/t/{{.Commit.Hash}}</a>
+	<a href="{{.URL}}/g/t/{{.Commit.GitHash}}">{{.URL}}/g/t/{{.Commit.GitHash}}</a>
 </p>
 <p>
   For:
@@ -73,12 +73,12 @@ func New(email Email, url string) *Notifier {
 // context is used in expanding the emailTemplate.
 type context struct {
 	URL     string
-	Commit  *cid.CommitDetail
+	Commit  perfgit.Commit
 	Alert   *alerts.Alert
 	Cluster *clustering2.ClusterSummary
 }
 
-func (n *Notifier) formatEmail(c *cid.CommitDetail, alert *alerts.Alert, cl *clustering2.ClusterSummary) (string, error) {
+func (n *Notifier) formatEmail(c perfgit.Commit, alert *alerts.Alert, cl *clustering2.ClusterSummary) (string, error) {
 	templateContext := &context{
 		URL:     n.url,
 		Commit:  c,
@@ -104,7 +104,7 @@ func splitEmails(s string) []string {
 }
 
 // Send a notification for the given cluster found at the given commit. Where to send it is defined in the alerts.Config.
-func (n *Notifier) Send(c *cid.CommitDetail, alert *alerts.Alert, cl *clustering2.ClusterSummary) error {
+func (n *Notifier) Send(c perfgit.Commit, alert *alerts.Alert, cl *clustering2.ClusterSummary) error {
 	if alert.Alert == "" {
 		return fmt.Errorf("No notification sent. No email address set for alert #%d", alert.ID)
 	}
@@ -112,7 +112,7 @@ func (n *Notifier) Send(c *cid.CommitDetail, alert *alerts.Alert, cl *clustering
 	if err != nil {
 		return err
 	}
-	subject := fmt.Sprintf("%s - Regression found for %q", alert.DisplayName, c.Message)
+	subject := fmt.Sprintf("%s - Regression found for %q", alert.DisplayName, c.Subject)
 	if _, err := n.email.Send(fromAddress, splitEmails(alert.Alert), subject, body, ""); err != nil {
 		return fmt.Errorf("Failed to send email: %s", err)
 	}
@@ -122,10 +122,10 @@ func (n *Notifier) Send(c *cid.CommitDetail, alert *alerts.Alert, cl *clustering
 
 // ExampleSend sends an example for dummy data for the given alerts.Config.
 func (n *Notifier) ExampleSend(alert *alerts.Alert) error {
-	c := &cid.CommitDetail{
-		Message: "Re-enable opList dependency tracking",
+	c := perfgit.Commit{
+		Subject: "Re-enable opList dependency tracking",
 		URL:     "https://skia.googlesource.com/skia/+show/d261e1075a93677442fdf7fe72aba7e583863664",
-		Hash:    "d261e1075a93677442fdf7fe72aba7e583863664",
+		GitHash: "d261e1075a93677442fdf7fe72aba7e583863664",
 	}
 	cl := &clustering2.ClusterSummary{
 		Num: 10,
