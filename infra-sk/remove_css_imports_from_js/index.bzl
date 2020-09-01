81e1b70bd39c58834d433e4c88ae9b50b4c7d1a2
Leandro Lovisolo <lovisolo@google.com>
2020-09-01 16:28:45 -0400

    [infra-sk] Update and simplify Bazel BUILD rules after elements-sk migration from CSS to Sass.
    This CL is the Bazel counterpart of https://skia-review.googlesource.com/c/buildbot/+/314520.
    
    Macro css_to_scss was deleted as it's no longer necessary.
    
    Bug: skia:10614
    Change-Id: I6f2b0a888a79bdee9ff02084a2e6b8eeef7fc98b
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/314557
    Reviewed-by: Joe Gregorio <jcgregorio@google.com>
    Commit-Queue: Leandro Lovisolo <lovisolo@google.com>

--- infra-sk/remove_css_imports_from_js/index.bzl
+++ infra-sk/remove_css_imports_from_js/index.bzl
@@ -7,5 +7,5 @@ def remove_css_imports_from_js(name, src, out):
         name = name,
         srcs = [src],
         outs = [out],
-        cmd = "sed 's/^[ \t]*require(.*\.css.);\?//' $< > $@",
+        cmd = "sed 's/^[ \t]*require(.*\.[s]\?css.);\?//' $< > $@",
     )
--- infra-sk/remove_css_imports_from_js/testdata/input.js
+++ infra-sk/remove_css_imports_from_js/testdata/input.js
@@ -1,15 +1,16 @@
 (function() {
   require("valid_import");
-  require("foo.css");  // foo
-  require("./bar.css");  // bar
-  require("../path/to/baz.css");  // path to baz
-  require("concatenated/" + "path.css");  // concatenated path
-  require('single_quotes.css');  // single quotes
-  require('no_semicolon.css')  // no semicolon
-		require('indented_with_tabs.css')  // indented with tabs
-require('not_indented.css')  // not indented
-  console.log('hello'); require('sandwiched.css'); console.log('bye');
-  // require('commented_out.css')
-  console.log(`require('inside_a_string.css');`);
+  require("foo.scss");  // foo
+  require("foo.css");  // foo (plain CSS)
+  require("./bar.scss");  // bar
+  require("../path/to/baz.scss");  // path to baz
+  require("concatenated/" + "path.scss");  // concatenated path
+  require('single_quotes.scss');  // single quotes
+  require('no_semicolon.scss')  // no semicolon
+		require('indented_with_tabs.scss')  // indented with tabs
+require('not_indented.scss')  // not indented
+  console.log('hello'); require('sandwiched.scss'); console.log('bye');
+  // require('commented_out.scss')
+  console.log(`require('inside_a_string.scss');`);
   require("another_valid_import");
 })();
