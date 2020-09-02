134127b529a45d5be4d5ab547adaa06fc7ac400f
Joe Gregorio <jcgregorio@google.com>
2020-09-02 11:52:57 -0400

    [new_element] Update to emit TypeScript.
    - Also adds in test and puppeteer files.
    - Moved cmd line application from //infra/go to //infra/cmd.
    - Added rice-box so the templates are packaged in the executable.
    - Added an example control to make reviewing easier.
    
    Change-Id: Iaa710b4f419ac18ef29512307c3e81d4f902e3ad

---
+++ new_element/templates/file-demo.ts
@@ -0,0 +1,11 @@
+import './index';
+
+document
+  .querySelector('{{.ElementName}}')!
+  .addEventListener('some-event-name', (e) => {
+    document.querySelector('#events')!.textContent = JSON.stringify(
+      e,
+      null,
+      '  '
+    );
+  });
