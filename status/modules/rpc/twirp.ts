248ab97c9a3fd3e3cde20564e00b444d70727449
Weston Tracey <westont@google.com>
2020-09-03 12:23:57 -0400

    [status] status.skia.org FE service definition.
    Change-Id: I437626a701dbbd83dfe3df48ba261da76b657264

--- status/modules/rpc/twirp.ts
+++ status/modules/rpc/twirp.ts
@@ -21,14 +21,25 @@ export const throwTwirpError = (resp: Response) => {
     return resp.json().then((err: TwirpErrorJSON) => { throw new TwirpError(err); })
 };
 
-export const createTwirpRequest = (url: string, body: object): Request => {
-    return new Request(url, {
+export const createTwirpRequest = (url: string, body: object, options: any): Request => {
+    const defaultOptions = {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
         },
-        body: JSON.stringify(body)
-    });
+        body: JSON.stringify(body),
+    };
+
+    const realOpts = {
+        ...defaultOptions,
+        ...options,
+        headers: {
+            ...defaultOptions.headers,
+            ...options.headers,
+        },
+    };
+
+    return new Request(url, realOpts);
 };
 
 export type Fetch = (input: RequestInfo, init?: RequestInit) => Promise<Response>;
