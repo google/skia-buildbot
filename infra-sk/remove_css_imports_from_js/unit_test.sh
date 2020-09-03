198c9f058495f86adaaeaef24096fe408b78d85f
Leandro Lovisolo <lovisolo@google.com>
2020-09-03 14:53:22 -0400

    [infra-sk] Simplify remove_css_imports_from_js unit test.
    This follows the same style as the test for the html_insert_nonce_attribute macro introduced here: https://skia-review.googlesource.com/c/buildbot/+/315137.
    
    See https://github.com/bazelbuild/rules_nodejs/blob/master/docs/Built-ins.md#generated_file_test for more.
    
    Bug: skia:10614
    Change-Id: I4f5b6430664c8a17c5a020d46c07f5eb5345ab43
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/315139
    Commit-Queue: Leandro Lovisolo <lovisolo@google.com>
    Reviewed-by: Joe Gregorio <jcgregorio@google.com>

--- infra-sk/remove_css_imports_from_js/unit_test.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-TESTDATA=infra-sk/remove_css_imports_from_js/testdata
-
-# diff returns a non-zero exit code if the files are not identical.
-diff $TESTDATA/expected_output.js $TESTDATA/actual_output.js
