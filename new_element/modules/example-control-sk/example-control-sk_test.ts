fd00c4e60b1a4e5976a59af89614d7169e69374f
Joe Gregorio <jcgregorio@google.com>
2020-09-02 21:22:43 -0400

    [new_element] Update to emit TypeScript.
    - Also adds in test and puppeteer files.
    - Moved cmd line application from //infra/go to //infra/cmd.
    - Added rice-box so the templates are packaged in the executable.
    - Added an example control to make reviewing easier.
    
    Change-Id: Iaa710b4f419ac18ef29512307c3e81d4f902e3ad

---
+++ new_element/modules/example-control-sk/example-control-sk_test.ts
@@ -0,0 +1,22 @@
+import { ExampleControlSk } from './example-control-sk';
+
+import { setUpElementUnderTest } from '../../../infra-sk/modules/test_util';
+import { expect } from 'chai';
+
+describe('example-control-sk', () => {
+  const newInstance = setUpElementUnderTest<ExampleControlSk>('example-control-sk');
+
+  let element: ExampleControlSk;
+  beforeEach(() => {
+    element = newInstance((el: ExampleControlSk) => {
+      // Place here any code that must run after the element is instantiated but
+      // before it is attached to the DOM (e.g. property setter calls,
+      // document-level event listeners, etc.).
+    });
+  });
+
+  describe('some action', () => {
+    it('some result', () => {});
+      expect(element).to.not.be.null;
+  });
+});
