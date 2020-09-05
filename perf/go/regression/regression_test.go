c16ec1b9dc43ec138afa47102adc2ec875f38418
Joe Gregorio <jcgregorio@google.com>
2020-09-05 16:43:25 -0400

    [perf] Remove some unused funcs in regression.
    Change-Id: If2b231f47a9b9d4c04b2725226a203cc8ff2668a

--- perf/go/regression/regression_test.go
+++ perf/go/regression/regression_test.go
@@ -16,51 +16,11 @@ var testTime = time.Date(2020, 05, 01, 12, 00, 00, 00, time.UTC)
 func TestRegressions(t *testing.T) {
 	unittest.SmallTest(t)
 	r := New()
-	assert.True(t, r.Triaged(), "With no clusters, it should have Triaged() == true.")
-
-	df := &dataframe.FrameResponse{}
-	cl := clustering2.NewClusterSummary()
-	cl.Timestamp = testTime
-	r.SetLow("source_type=skp", df, cl)
-	assert.False(t, r.Triaged(), "Should not be Triaged.")
-
-	// Triage the low cluster.
-	err := r.TriageLow("source_type=skp", TriageStatus{
-		Status:  Positive,
-		Message: "SKP Update",
-	})
-	assert.NoError(t, err)
-	assert.True(t, r.Triaged())
-
-	// Trying to triage a high cluster that doesn't exists.
-	err = r.TriageHigh("source_type=skp", TriageStatus{
-		Status: Negative,
-	})
-	assert.Equal(t, err, ErrNoClusterFound)
-	assert.True(t, r.Triaged())
-
-	// Set a high cluster.
-	r.SetHigh("source_type=skp", df, cl)
-	assert.False(t, r.Triaged())
-
-	// And triage the high cluster.
-	err = r.TriageHigh("source_type=skp", TriageStatus{
-		Status:  Negative,
-		Message: "See bug #foo.",
-	})
-	assert.NoError(t, err)
-	assert.True(t, r.Triaged())
-
-	// Trying to triage an unknown query.
-	err = r.TriageHigh("uknownquery", TriageStatus{
-		Status: Negative,
-	})
-	assert.Equal(t, err, ErrNoClusterFound)
 
 	// Try serializing to JSON.
 	b, err := r.JSON()
 	assert.NoError(t, err)
-	assert.Equal(t, "{\"by_query\":{\"source_type=skp\":{\"low\":{\"centroid\":null,\"shortcut\":\"\",\"param_summaries2\":[],\"step_fit\":{\"least_squares\":0,\"turning_point\":0,\"step_size\":0,\"regression\":0,\"status\":\"\"},\"step_point\":{\"offset\":0,\"timestamp\":0},\"num\":0,\"ts\":\"2020-05-01T12:00:00Z\"},\"high\":{\"centroid\":null,\"shortcut\":\"\",\"param_summaries2\":[],\"step_fit\":{\"least_squares\":0,\"turning_point\":0,\"step_size\":0,\"regression\":0,\"status\":\"\"},\"step_point\":{\"offset\":0,\"timestamp\":0},\"num\":0,\"ts\":\"2020-05-01T12:00:00Z\"},\"frame\":{\"dataframe\":null,\"skps\":null,\"msg\":\"\"},\"low_status\":{\"status\":\"positive\",\"message\":\"SKP Update\"},\"high_status\":{\"status\":\"negative\",\"message\":\"See bug #foo.\"}}}}", string(b))
+	assert.Equal(t, "{\"by_query\":{}}", string(b))
 }
 
 func TestMerge(t *testing.T) {
