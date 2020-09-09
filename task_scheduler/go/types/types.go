9bcead03d6c3702087f52ce339a3067745f393ec
Eric Boren <borenet@google.com>
2020-09-09 14:54:08 -0400

    [task scheduler] Add rpc proto
    This just adds the proto and the generated files. They are unused.
    
    Change-Id: If4d931710ba747e0010d206ce75cd78420cd9d45

--- task_scheduler/go/types/types.go
+++ task_scheduler/go/types/types.go
@@ -1,5 +1,9 @@
 package types
 
+// Generate the go code from the protocol buffer definitions.
+//go:generate protoc --go_opt=paths=source_relative --go_out=. ./rpc.proto
+//go:generate goimports -w rpc.pb.go
+
 import "errors"
 
 var (
