9b0b37a2f26d64c6d93d03a3dc1e838a468547e8
Joe Gregorio <jcgregorio@google.com>
2020-09-09 11:56:18 -0400

    [perf] Move core functionality of Dry Run to regression.Detector.
    Change-Id: Ic97b0dfae0b14e20d40e2c6aeca79d86cf9e987a

--- perf/go/dryrun/dryrun.go
+++ perf/go/dryrun/dryrun.go
@@ -15,15 +15,14 @@ import (
 	"go.skia.org/infra/go/httputils"
 	"go.skia.org/infra/go/metrics2"
 	"go.skia.org/infra/go/sklog"
-	"go.skia.org/infra/perf/go/dataframe"
 	perfgit "go.skia.org/infra/perf/go/git"
 	"go.skia.org/infra/perf/go/regression"
-	"go.skia.org/infra/perf/go/shortcut"
 	"go.skia.org/infra/perf/go/types"
 )
 
 const (
-	cleanupDuration = 5 * time.Minute
+	maxCacheAge     = 5 * time.Minute
+	cleanupDuration = time.Second
 )
 
 // StartDryRunResponse is the JSON response sent from StartHandler.
@@ -31,8 +30,8 @@ type StartDryRunResponse struct {
 	ID string `json:"id"`
 }
 
-// Running is the data stored for each running dryrun.
-type Running struct {
+// running is the data stored for each running dryrun.
+type running struct {
 	mutex        sync.Mutex
 	whenFinished time.Time
 	Finished     bool                                          `json:"finished"`    // True if the dry run is complete.
@@ -42,22 +41,19 @@ type Running struct {
 
 // Requests handles HTTP request for doing dryruns.
 type Requests struct {
-	dfBuilder      dataframe.DataFrameBuilder
-	perfGit        *perfgit.Git
-	paramsProvider regression.ParamsetProvider // TODO build the paramset from dfBuilder.
-	shortcutStore  shortcut.Store
-	mutex          sync.Mutex
-	inFlight       map[string]*Running
+	detector regression.Detector
+	perfGit  *perfgit.Git
+
+	mutex    sync.Mutex
+	inFlight map[string]*running
 }
 
 // New create a new dryrun Request processor.
-func New(dfBuilder dataframe.DataFrameBuilder, shortcutStore shortcut.Store, paramsProvider regression.ParamsetProvider, perfGit *perfgit.Git) *Requests {
+func New(perfGit *perfgit.Git, detector regression.Detector) *Requests {
 	ret := &Requests{
-		dfBuilder:      dfBuilder,
-		paramsProvider: paramsProvider,
-		shortcutStore:  shortcutStore,
-		perfGit:        perfGit,
-		inFlight:       map[string]*Running{},
+		detector: detector,
+		perfGit:  perfGit,
+		inFlight: map[string]*running{},
 	}
 	// Start a go routine to clean up old dry runs.
 	go ret.cleaner()
@@ -66,15 +62,33 @@ func New(dfBuilder dataframe.DataFrameBuilder, shortcutStore shortcut.Store, par
 
 // cleanerStep does a single step of cleaner().
 func (d *Requests) cleanerStep() {
-	cutoff := time.Now().Add(-cleanupDuration)
+	cutoff := time.Now().Add(-maxCacheAge)
 	d.mutex.Lock()
 	defer d.mutex.Unlock()
-	for k, v := range d.inFlight {
-		if !v.Finished {
+	for id, running := range d.inFlight {
+		// First check on each unfinished request and see if it has completed.
+		if !running.Finished {
+			state, msg, err := d.detector.Status(id)
+			if err != nil {
+				sklog.Error("Failed to get status if DryRun: %s", id)
+				continue
+			}
+			if state != regression.ProcessRunning {
+				running.mutex.Lock()
+				running.Finished = true
+				running.whenFinished = time.Now()
+				if state == regression.ProcessError {
+					running.Message = msg
+				} else {
+					running.Message = "Finished"
+				}
+				running.mutex.Unlock()
+			}
 			continue
 		}
-		if v.whenFinished.Before(cutoff) {
-			delete(d.inFlight, k)
+
+		if running.whenFinished.Before(cutoff) {
+			delete(d.inFlight, id)
 		}
 	}
 	metrics2.GetInt64Metric("dryrun_inflight", nil).Update(int64(len(d.inFlight)))
@@ -109,57 +123,45 @@ func (d *Requests) StartHandler(w http.ResponseWriter, r *http.Request) {
 	id := req.Id()
 	if p, ok := d.inFlight[id]; ok {
 		p.mutex.Lock()
-		defer p.mutex.Unlock()
 		if p.Finished {
 			delete(d.inFlight, id)
 		}
+		p.mutex.Unlock()
 	}
 	if _, ok := d.inFlight[id]; !ok {
-		running := &Running{
+		instance := &running{
 			Finished:    false,
 			Message:     "Starting dry run.",
 			Regressions: map[types.CommitNumber]*regression.Regression{},
 		}
-		d.inFlight[id] = running
-		go func() {
-			ctx := context.Background()
-			// Create a callback that will be passed each found Regression.
-			cb := func(queryRequest *regression.RegressionDetectionRequest, clusterResponse []*regression.RegressionDetectionResponse, message string) {
-				running.mutex.Lock()
-				defer running.mutex.Unlock()
-				// Loop over clusterResponse, convert each one to a regression, and merge with running.Regressions.
-				for _, cr := range clusterResponse {
-					c, reg, err := regression.RegressionFromClusterResponse(ctx, cr, req.Alert, d.perfGit)
-					if err != nil {
-						running.Message = "Failed to convert to Regression, some data may be missing."
-						sklog.Errorf("Failed to convert to Regression: %s", err)
-						return
-					}
-					running.Message = fmt.Sprintf("Step: %d/%d\nQuery: %q\nLooking for regressions in query results.\n  Commit: %d\n  Details: %q", queryRequest.Step+1, queryRequest.TotalQueries, queryRequest.Query, c.CommitNumber, message)
-					// We might not have found any regressions.
-					if reg.Low == nil && reg.High == nil {
-						continue
-					}
-					if origReg, ok := running.Regressions[c.CommitNumber]; !ok {
-						running.Regressions[c.CommitNumber] = reg
-					} else {
-						running.Regressions[c.CommitNumber] = origReg.Merge(reg)
-					}
+		d.inFlight[id] = instance
+		ctx := context.Background()
+		// Create a callback that will be passed each found Regression.
+		detectorResponseProcessor := func(queryRequest *regression.RegressionDetectionRequest, clusterResponse []*regression.RegressionDetectionResponse, message string) {
+			instance.mutex.Lock()
+			defer instance.mutex.Unlock()
+			// Loop over clusterResponse, convert each one to a regression, and merge with running.Regressions.
+			for _, cr := range clusterResponse {
+				c, reg, err := regression.RegressionFromClusterResponse(ctx, cr, req.Alert, d.perfGit)
+				if err != nil {
+					instance.Message = "Failed to convert to Regression, some data may be missing."
+					sklog.Errorf("Failed to convert to Regression: %s", err)
+					return
+				}
+				instance.Message = fmt.Sprintf("Step: %d/%d\nQuery: %q\nLooking for regressions in query results.\n  Commit: %d\n  Details: %q", queryRequest.Step+1, queryRequest.TotalQueries, queryRequest.Query, c.CommitNumber, message)
+				// We might not have found any regressions.
+				if reg.Low == nil && reg.High == nil {
+					continue
+				}
+				if origReg, ok := instance.Regressions[c.CommitNumber]; !ok {
+					instance.Regressions[c.CommitNumber] = reg
+				} else {
+					instance.Regressions[c.CommitNumber] = origReg.Merge(reg)
 				}
 			}
-			progressCallback := func(message string) {
-				running.mutex.Lock()
-				defer running.mutex.Unlock()
-				running.Message = message
-			}
+		}
 
-			regression.RegressionsForAlert(ctx, req.Alert, req.Domain, d.paramsProvider(), d.shortcutStore, cb, d.perfGit, d.dfBuilder, progressCallback)
-			running.mutex.Lock()
-			defer running.mutex.Unlock()
-			running.Finished = true
-			running.whenFinished = time.Now()
-			running.Message = running.Message + "\nDry run complete."
-		}()
+		d.detector.Add(ctx, detectorResponseProcessor, &req)
 	}
 	resp := StartDryRunResponse{
 		ID: id,
@@ -203,27 +205,25 @@ func (d *Requests) StatusHandler(w http.ResponseWriter, r *http.Request) {
 		Regressions: []*RegressionAtCommit{},
 	}
 
-	// Convert the Running.Regressions into a properly formed Status response.
-	if running.Finished {
-		running.mutex.Lock()
-		defer running.mutex.Unlock()
-		commitNumbers := []types.CommitNumber{}
-		for id := range running.Regressions {
-			commitNumbers = append(commitNumbers, id)
-		}
-		sort.Sort(types.CommitNumberSlice(commitNumbers))
+	// Convert the Running.Regressions into a properly formed DryRunStatus response.
+	running.mutex.Lock()
+	defer running.mutex.Unlock()
+	commitNumbers := []types.CommitNumber{}
+	for id := range running.Regressions {
+		commitNumbers = append(commitNumbers, id)
+	}
+	sort.Sort(types.CommitNumberSlice(commitNumbers))
 
-		for _, commitNumber := range commitNumbers {
-			details, err := d.perfGit.CommitFromCommitNumber(ctx, commitNumber)
-			if err != nil {
-				sklog.Errorf("Failed to look up commit %d: %s", commitNumber, err)
-				continue
-			}
-			status.Regressions = append(status.Regressions, &RegressionAtCommit{
-				CID:        details,
-				Regression: running.Regressions[commitNumber],
-			})
+	for _, commitNumber := range commitNumbers {
+		details, err := d.perfGit.CommitFromCommitNumber(ctx, commitNumber)
+		if err != nil {
+			sklog.Errorf("Failed to look up commit %d: %s", commitNumber, err)
+			continue
 		}
+		status.Regressions = append(status.Regressions, &RegressionAtCommit{
+			CID:        details,
+			Regression: running.Regressions[commitNumber],
+		})
 	}
 	if err := json.NewEncoder(w).Encode(status); err != nil {
 		sklog.Errorf("Failed to encode paramset: %s", err)
