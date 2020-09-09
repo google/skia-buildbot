44126d619da148c3acf6fb8f0c3b981655288d3d
Kevin Lubick <kjlubick@google.com>
2020-09-09 10:36:49 -0400

    [gold] Add versioned RPCs that are identical to existing RPCs
    Bug: skia:10591
    Change-Id: I0fe37f7fab2bf410de9de09a05ee5790220fad0d

--- golden/cmd/baseline_server/main.go
+++ golden/cmd/baseline_server/main.go
@@ -137,10 +137,25 @@ func main() {
 		})
 	}
 
+	v1 := func(rpcRoute string, handlerFunc http.HandlerFunc) *mux.Route {
+		baseRoute := strings.TrimPrefix(rpcRoute, "/json")
+		counter := metrics2.GetCounter(web.RPCCallCounterMetric, map[string]string{
+			"route":   baseRoute,
+			"version": "v1",
+		})
+		fullRoute := "/json/v1" + baseRoute
+		return appRouter.HandleFunc(fullRoute, func(w http.ResponseWriter, r *http.Request) {
+			counter.Inc(1)
+			handlerFunc(w, r)
+		})
+	}
+
 	// Serve the known hashes from GCS.
 	v0(shared.KnownHashesRoute, handlers.TextKnownHashesProxy).Methods("GET")
+	v1(shared.KnownHashesRoute, handlers.TextKnownHashesProxy).Methods("GET")
 	// Serve the expectations for the master branch and for CLs in progress.
 	v0(shared.ExpectationsRoute, handlers.BaselineHandler).Methods("GET")
+	v1(shared.ExpectationsRoute, handlers.BaselineHandler).Methods("GET")
 	// TODO(lovisolo): Remove the below route once goldctl is fully migrated.
 	v0(shared.ExpectationsLegacyRoute, handlers.BaselineHandler).Methods("GET")
 
