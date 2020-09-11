979928fb9a3f985c7827935d25467b04a8797333
Eric Boren <borenet@google.com>
2020-09-11 08:14:46 -0400

    [autoroll] Add back datastore init
    I don't know why this was removed in https://skia-review.googlesource.com/c/buildbot/+/302504
    
    Change-Id: If718069e1e76f26f35b6559a3f8121993808e2a0
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/316437
    Reviewed-by: Eric Boren <borenet@google.com>
    Reviewed-by: Ravi Mistry <rmistry@google.com>
    Commit-Queue: Eric Boren <borenet@google.com>

--- autoroll/go/autoroll-be/main.go
+++ autoroll/go/autoroll-be/main.go
@@ -26,6 +26,7 @@ import (
 	"go.skia.org/infra/go/auth"
 	"go.skia.org/infra/go/chatbot"
 	"go.skia.org/infra/go/common"
+	"go.skia.org/infra/go/ds"
 	"go.skia.org/infra/go/email"
 	"go.skia.org/infra/go/fileutil"
 	"go.skia.org/infra/go/firestore"
@@ -40,6 +41,7 @@ import (
 	"go.skia.org/infra/go/sklog"
 	"go.skia.org/infra/go/util"
 	"golang.org/x/oauth2"
+	"google.golang.org/api/option"
 )
 
 const (
@@ -116,6 +118,13 @@ func main() {
 		sklog.Fatal(err)
 	}
 	client := httputils.DefaultClientConfig().WithTokenSource(ts).With2xxOnly().Client()
+	namespace := ds.AUTOROLL_NS
+	if cfg.IsInternal {
+		namespace = ds.AUTOROLL_INTERNAL_NS
+	}
+	if err := ds.InitWithOpt(common.PROJECT_ID, namespace, option.WithTokenSource(ts)); err != nil {
+		sklog.Fatal(err)
+	}
 
 	chatbot.Init(fmt.Sprintf("%s -> %s AutoRoller", cfg.ChildDisplayName, cfg.ParentDisplayName))
 
