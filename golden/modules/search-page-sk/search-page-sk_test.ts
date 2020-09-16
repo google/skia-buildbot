2bcd146214dca147ea1ed967577e0d7d8fb2e43d
Leandro Lovisolo <lovisolo@google.com>
2020-09-15 21:43:27 -0400

    [gold] search-page-sk (lit-html): Support URL parameters "crs" and "issue".:
    Bug: skia:9525
    Change-Id: I4010b9af7208cb73ab5616ea5b3307c76676f068

--- golden/modules/search-page-sk/search-page-sk_test.ts
+++ golden/modules/search-page-sk/search-page-sk_test.ts
@@ -3,7 +3,8 @@ import { setUpElementUnderTest, eventSequencePromise, eventPromise, setQueryStri
 import { searchResponse, statusResponse, paramSetResponse } from './demo_data';
 import fetchMock from 'fetch-mock';
 import { deepCopy } from 'common-sk/modules/object';
-import { SearchPageSk } from './search-page-sk';
+import { fromObject } from 'common-sk/modules/query';
+import { SearchPageSk, SearchRequest } from './search-page-sk';
 import { SearchPageSkPO } from './search-page-sk_po';
 import { SearchResponse } from '../rpc_types';
 import { testOnlySetSettings } from '../settings';
@@ -16,28 +17,35 @@ describe('search-page-sk', () => {
   let searchPageSk: SearchPageSk;
   let searchPageSkPO: SearchPageSkPO;
 
-  const defaultSearchRpcQueryString =
-    'fref=false&' +
-    'frgbamax=255&' +
-    'frgbamin=0&' +
-    'head=true&' +
-    'include=false&' +
-    'neg=false&' +
-    'pos=false&' +
-    'query=source_type%3Dinfra&' +
-    'rquery=source_type%3Dinfra&' +
-    'sort=desc&' +
-    'unt=true';
+  const defaultSearchRequest: SearchRequest = {
+    fref: false,
+    frgbamax: 255,
+    frgbamin: 0,
+    head: true,
+    include: false,
+    neg: false,
+    pos: false,
+    query: 'source_type=infra',
+    rquery: 'source_type=infra',
+    sort: 'desc',
+    unt: true,
+  }
 
   const emptySearchResponse = deepCopy(searchResponse);
   emptySearchResponse.size = 0;
   emptySearchResponse.digests = [];
 
+  // Instantiates the search page after setting up the mock search RPC with the given search query
+  // and response.
+  //
+  // The search page hits the search RPC immediately after instantiation. This function ensures that
+  // the RPC is correctly mocked before the search page is instantiated.
   const instantiate =
       async (
-        searchRpcQueryString: string = defaultSearchRpcQueryString,
+        initialSearchRequest: SearchRequest = defaultSearchRequest,
         initialSearchResponse: SearchResponse = searchResponse) => {
-    fetchMock.get('/json/v1/search?' + searchRpcQueryString, () => initialSearchResponse);
+    fetchMock.get(
+      '/json/v1/search?' + fromObject(initialSearchRequest as any), () => initialSearchResponse);
     const events = eventSequencePromise(['end-task', 'end-task', 'end-task']);
     searchPageSk = newInstance();
     searchPageSkPO = new SearchPageSkPO(searchPageSk);
@@ -64,7 +72,7 @@ describe('search-page-sk', () => {
   });
 
   it('shows empty search results', async () => {
-    await instantiate(defaultSearchRpcQueryString, emptySearchResponse);
+    await instantiate(defaultSearchRequest, emptySearchResponse);
 
     expect(await searchPageSkPO.getSummary()).to.equal('No results matched your search criteria.');
     expect(await searchPageSkPO.getDigests()).to.be.empty;
@@ -78,7 +86,7 @@ describe('search-page-sk', () => {
       'Left: fbd3de3fff6b852ae0bb6751b9763d27',
       'Left: 2fa58aa430e9c815755624ca6cca4a72',
       'Left: ed4a8cf9ea9fbb57bf1f302537e07572'
-    ])
+    ]);
   });
 
   // TODO(lovisolo): Test this more thoroughly (exercise all search parameters, etc.).
@@ -86,9 +94,9 @@ describe('search-page-sk', () => {
     await instantiate();
 
     // We will pretend that the user unchecked "Include untriaged digests".
-    fetchMock.get(
-      '/json/v1/search?' + defaultSearchRpcQueryString.replace('unt=true', 'unt=false'),
-      () => emptySearchResponse);
+    const searchRequest = deepCopy(defaultSearchRequest);
+    searchRequest.unt = false;
+    fetchMock.get('/json/v1/search?' + fromObject(searchRequest as any), () => emptySearchResponse);
 
     const event = eventPromise('end-task');
     const searchControlsSkPO = await searchPageSkPO.getSearchControlsSkPO();
@@ -98,14 +106,62 @@ describe('search-page-sk', () => {
     expect(await searchPageSkPO.getSummary()).to.equal('No results matched your search criteria.');
   });
 
-  it('reads the "blame" URL parameter', async () => {
-    setQueryString('?blame=aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa:bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb');
+  describe('changelist support', () => {
+    const searchRequestWithCL = deepCopy(defaultSearchRequest);
+    searchRequestWithCL.crs = 'Gerrit';
+    searchRequestWithCL.issue = '123456';
 
-    await instantiate(
-      'blame=aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa%3Abbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb&' +
-      defaultSearchRpcQueryString);
+    beforeEach(() => {
+      setQueryString('?crs=Gerrit&issue=123456');
+    });
 
-    // Nothing to assert here. If the RPC wasn't called with the query string above, the call to
-    // fetchMock.done() in the afterEach hook will fail.
+    it('shows search results with changelist information', async () => {
+      await instantiate(searchRequestWithCL);
+
+      expect(await searchPageSkPO.getDigests()).to.deep.equal([
+        'Left: fbd3de3fff6b852ae0bb6751b9763d27',
+        'Left: 2fa58aa430e9c815755624ca6cca4a72',
+        'Left: ed4a8cf9ea9fbb57bf1f302537e07572'
+      ]);
+
+      const diffDetailsHrefs = await searchPageSkPO.getDiffDetailsHrefs();
+      expect(diffDetailsHrefs[0]).to.contain('changelist_id=123456&crs=Gerrit');
+      expect(diffDetailsHrefs[1]).to.contain('changelist_id=123456&crs=Gerrit');
+      expect(diffDetailsHrefs[2]).to.contain('changelist_id=123456&crs=Gerrit');
+    });
+
+    // TODO(lovisolo): Assert that the changelist controls are visible.
+  });
+
+  describe('optional URL query parameters', () => {
+    // The test cases below assert that the search page's optional URL parameters (blame, crs,
+    // issue) are reflected in the /json/v1/search RPC.
+    //
+    // No explicit asserts are necessary because if the search RPC is not called with the expected
+    // SearchRequest then the fetchMock.done() call in the afterEach hook above will fail.
+
+    it('reads the "blame" URL parameter', async () => {
+      setQueryString('?blame=aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa:bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb');
+
+      const searchRequest = deepCopy(defaultSearchRequest);
+      searchRequest.blame = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa:bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb';
+      await instantiate(searchRequest);
+    });
+
+    it('reads the "crs" URL parameter', async () => {
+      setQueryString('?crs=gerrit');
+
+      const searchRequest = deepCopy(defaultSearchRequest);
+      searchRequest.crs = 'gerrit';
+      await instantiate(searchRequest);
+    });
+
+    it('reads the "issue" URL parameter', async () => {
+      setQueryString('?issue=123456');
+
+      const searchRequest = deepCopy(defaultSearchRequest);
+      searchRequest.issue = '123456';
+      await instantiate(searchRequest);
+    });
   });
 });
