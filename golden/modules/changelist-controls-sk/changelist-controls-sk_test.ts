507ea4f63a19af2ceae3f1dd032b1c18976cf020
Leandro Lovisolo <lovisolo@google.com>
2020-09-16 18:23:57 -0400

    [gold] changelist-controls-sk: Add page object and use it in tests.
    This makes the changelist-controls-sk tests more readable, and will make it easier to write tests for the search-page-sk.
    
    Bug: skia:9525
    Change-Id: If0ffc66de9a3cdb356dbf623fe1287493d5518e7

--- golden/modules/changelist-controls-sk/changelist-controls-sk_test.ts
+++ golden/modules/changelist-controls-sk/changelist-controls-sk_test.ts
@@ -1,110 +1,102 @@
 import './index';
 
 import { ChangelistControlsSk, ChangelistControlsSkChangeEventDetail } from './changelist-controls-sk';
-import { $, $$ } from 'common-sk/modules/dom';
+import { ChangelistControlsSkPO  } from './changelist-controls-sk_po';
 import { twoPatchSets } from './test_data';
-import { setUpElementUnderTest } from '../../../infra-sk/modules/test_util';
+import { setUpElementUnderTest, eventPromise } from '../../../infra-sk/modules/test_util';
 import { expect } from 'chai';
 
 describe('changelist-controls-sk', () => {
   const newInstance = setUpElementUnderTest<ChangelistControlsSk>('changelist-controls-sk');
 
   let changelistControlsSk: ChangelistControlsSk;
-  beforeEach(() => changelistControlsSk = newInstance());
+  let changelistControlsSkPO: ChangelistControlsSkPO;
+
+  beforeEach(() => {
+    changelistControlsSk = newInstance()
+    changelistControlsSkPO = new ChangelistControlsSkPO(changelistControlsSk);
+  });
 
   describe('html layout', () => {
-    it('is empty with no data', () => {
-      expect(changelistControlsSk.children.length).to.equal(0);
+    it('is empty with no data', async () => {
+      expect(await changelistControlsSkPO.isVisible()).to.be.false;
     });
 
-    it('shows the latest patchset by default', () => {
+    it('shows the latest patchset by default', async () => {
       expect(changelistControlsSk.ps_order).to.equal(0);
       expect(changelistControlsSk.include_master).to.equal(false);
 
       changelistControlsSk.setSummary(twoPatchSets);
-      const psSelector = $$<HTMLSelectElement>('.inputs select', changelistControlsSk);
-      expect(psSelector).to.not.be.null;
-      expect(psSelector!.value).to.equal('PS 4');
-
-      const includePrimaryRadios = $('.inputs radio-sk', changelistControlsSk);
-      expect(includePrimaryRadios.length).to.equal(2);
-      expect(includePrimaryRadios[0].hasAttribute('checked')).to.equal(true);
-
       expect(changelistControlsSk.ps_order).to.equal(4);
 
-      const tryJobs = $('.tryjob-container .tryjob', changelistControlsSk);
-      expect(tryJobs).to.not.be.null;
-      expect(tryJobs.length).to.equal(4);
-      // spot check a tryjob
-      expect(tryJobs[0].textContent!.trim()).to.equal('android-marshmallow-arm64-rel');
+      expect(await changelistControlsSkPO.getPatchSet()).to.equal('PS 4');
+      expect(await changelistControlsSkPO.isExcludeResultsFromPrimaryRadioChecked()).to.be.true;
+      expect(await changelistControlsSkPO.isShowAllResultsRadioChecked()).to.be.false;
+      expect(await changelistControlsSkPO.getTryJobs()).to.deep.equal([
+        'android-marshmallow-arm64-rel',
+        'linux-rel',
+        'mac-rel',
+        'win10_chromium_x64_rel_ng',
+      ]);
     });
 
-    it('shows other patchsets when ps_order is changed', () => {
+    it('shows other patchsets when ps_order is changed', async () => {
       changelistControlsSk.setSummary(twoPatchSets);
       changelistControlsSk.ps_order = 1;
-      const psSelector = $$<HTMLSelectElement>('.inputs select', changelistControlsSk);
-      expect(psSelector).to.not.be.null;
-      expect(psSelector!.value).to.equal('PS 1');
 
-      const tryJobs = $('.tryjob-container .tryjob', changelistControlsSk);
-      expect(tryJobs).to.not.be.null;
-      expect(tryJobs.length).to.equal(1);
-      // spot check a tryjob
-      expect(tryJobs[0].textContent!.trim()).to.equal('android-nougat-arm64-rel');
+      expect(await changelistControlsSkPO.getPatchSet()).to.equal('PS 1');
+      expect(await changelistControlsSkPO.getTryJobs()).to.deep.equal(['android-nougat-arm64-rel']);
     });
 
-    it('flips the radio buttons on include_master', () => {
+    it('flips the radio buttons on include_master', async () => {
       changelistControlsSk.setSummary(twoPatchSets);
-      const includePrimaryRadios = $('.inputs radio-sk', changelistControlsSk);
-      expect(includePrimaryRadios.length).to.equal(2);
-      expect(includePrimaryRadios[0].hasAttribute('checked')).to.equal(true);
-      expect(includePrimaryRadios[1].hasAttribute('checked')).to.equal(false);
+      expect(await changelistControlsSkPO.isExcludeResultsFromPrimaryRadioChecked()).to.be.true;
+      expect(await changelistControlsSkPO.isShowAllResultsRadioChecked()).to.be.false;
 
       changelistControlsSk.include_master = true;
-      expect(includePrimaryRadios[0].hasAttribute('checked')).to.equal(false);
-      expect(includePrimaryRadios[1].hasAttribute('checked')).to.equal(true);
+      expect(await changelistControlsSkPO.isExcludeResultsFromPrimaryRadioChecked()).to.be.false;
+      expect(await changelistControlsSkPO.isShowAllResultsRadioChecked()).to.be.true;
     });
   }); // end describe('html layout')
 
   describe('events', () => {
-    it('generates a cl-control-change event on master toggle', (done) => {
+    it('generates a cl-control-change event on "include results from primary" toggle', async () => {
       changelistControlsSk.include_master = false;
+      changelistControlsSk.ps_order = 4;
       changelistControlsSk.setSummary(twoPatchSets);
 
-      changelistControlsSk.addEventListener('cl-control-change', (e) => {
-        const detail = (e as CustomEvent<ChangelistControlsSkChangeEventDetail>).detail;
-        expect(detail.include_master).to.equal(true);
-        expect(detail.ps_order).to.equal(4);
-        done();
-      });
+      expect(await changelistControlsSkPO.isExcludeResultsFromPrimaryRadioChecked()).to.be.true;
+      expect(await changelistControlsSkPO.isShowAllResultsRadioChecked()).to.be.false;
 
-      const includePrimaryRadios = $<HTMLElement>('.inputs radio-sk', changelistControlsSk);
-      expect(includePrimaryRadios.length).to.equal(2);
-      includePrimaryRadios[1].click();
+      const event =
+        eventPromise<CustomEvent<ChangelistControlsSkChangeEventDetail>>('cl-control-change');
+      await changelistControlsSkPO.clickShowAllResultsRadio();
+      const eventDetail = (await event).detail;
+
+      expect(eventDetail.include_master).to.be.true;
+      expect(eventDetail.ps_order).to.equal(4);
+
       expect(changelistControlsSk.include_master).to.equal(true);
-      expect(includePrimaryRadios[0].hasAttribute('checked')).to.equal(false);
-      expect(includePrimaryRadios[1].hasAttribute('checked')).to.equal(true);
+      expect(await changelistControlsSkPO.isExcludeResultsFromPrimaryRadioChecked()).to.be.false;
+      expect(await changelistControlsSkPO.isShowAllResultsRadioChecked()).to.be.true;
     });
 
-    it('generates a cl-control-change event on patchset change', (done) => {
-      changelistControlsSk.ps_order = 0;
+    it('generates a cl-control-change event on patchset change', async () => {
+      changelistControlsSk.ps_order = 0; // Use the latest patchset, i.e. 'PS 4'.
       changelistControlsSk.setSummary(twoPatchSets);
 
-      changelistControlsSk.addEventListener('cl-control-change', (e) => {
-        const detail = (e as CustomEvent<ChangelistControlsSkChangeEventDetail>).detail;
-        expect(detail.include_master).to.equal(false);
-        expect(detail.ps_order).to.equal(1);
-        done();
-      });
+      expect(await changelistControlsSkPO.getPatchSet()).to.equal('PS 4');
 
-      const psSelector = $$<HTMLSelectElement>('.inputs select', changelistControlsSk);
-      expect(psSelector).to.not.be.null;
-      expect(psSelector!.value).to.equal('PS 4');
+      const event =
+        eventPromise<CustomEvent<ChangelistControlsSkChangeEventDetail>>('cl-control-change');
+      await changelistControlsSkPO.setPatchSet('PS 1');
+      const eventDetail = (await event).detail;
 
-      psSelector!.selectedIndex = 0;
-      // we have to manually send this because just changing selectedIdx isn't enough.
-      // https://stackoverflow.com/a/23612498
-      psSelector!.dispatchEvent(new Event('input'));
+      expect(eventDetail.include_master).to.equal(false);
+      expect(eventDetail.ps_order).to.equal(1);
+
+      expect(changelistControlsSk.ps_order).to.equal(1);
+      expect(await changelistControlsSkPO.getPatchSet()).to.equal('PS 1');
     });
   }); // end describe('events')
 });
