bd52de7833a07778e34dd970154898daafd7250d
Ravi Mistry <rmistry@google.com>
2020-09-17 15:07:39 -0400

    [am] Detect and display alerts from recently expired silences
    A common usecase for the trooper on alert manager is to go through
    alerts looking for recently expired silences. This change detects which
    alerts have recently expired silences and displays an icon next to them.
    
    A flag controls what it means to be "recently" expired.
    
    Also changes the text for flaky alerts into an icon.
    
    Screencast: https://screencast.googleplex.com/cast/NDcyNTA5MTIxODc1MTQ4OHxlYTU2YjAxOC1lYg
    
    Bug: skia:10733
    Change-Id: Ib6e6af6041bcd0c1f1015ba827c2a3c30d7dd7af

--- am/go/alert-manager/main.go
+++ am/go/alert-manager/main.go
@@ -43,6 +43,8 @@ var (
 	internalPort       = flag.String("internal_port", ":9000", "HTTP internal service address (e.g., ':9000') for unauthenticated in-cluster requests.")
 	project            = flag.String("project", "skia-public", "The Google Cloud project name.")
 
+	silenceRecentlyExpiredDuration = flag.Duration("recently_expired_duration", 2*time.Hour, "Incidents with silences that recently expired within this duration are shown with an icon.")
+
 	emailClientSecretFile = flag.String("email_client_secret_file", "", "OAuth client secret JSON file for sending email.")
 	emailTokenCacheFile   = flag.String("email_token_cache_file", "", "OAuth token cache file for sending email.")
 )
@@ -516,7 +518,7 @@ func (srv *server) silencesHandler(w http.ResponseWriter, r *http.Request) {
 	if silences == nil {
 		silences = []silence.Silence{}
 	}
-	recents, err := srv.silenceStore.GetRecentlyArchived()
+	recents, err := srv.silenceStore.GetRecentlyArchived(0)
 	if err != nil {
 		httputils.ReportError(w, err, "Failed to load recents.", http.StatusInternalServerError)
 		return
@@ -534,7 +536,25 @@ func (srv *server) incidentHandler(w http.ResponseWriter, r *http.Request) {
 		httputils.ReportError(w, err, "Failed to load incidents.", http.StatusInternalServerError)
 		return
 	}
-	if err := json.NewEncoder(w).Encode(ins); err != nil {
+
+	idsToRecentlyExpiredSilences := map[string]bool{}
+	archivedSilences, err := srv.silenceStore.GetRecentlyArchived(*silenceRecentlyExpiredDuration)
+	if err != nil {
+		httputils.ReportError(w, err, "Failed to load archived silences.", http.StatusInternalServerError)
+	}
+	if len(archivedSilences) > 0 {
+		for _, i := range ins {
+			idsToRecentlyExpiredSilences[i.ID] = i.IsSilenced(archivedSilences, false)
+		}
+	}
+	resp := struct {
+		Incidents                    []incident.Incident `json:"incidents"`
+		IdsToRecentlyExpiredSilences map[string]bool     `json:"ids_to_recently_expired_silences"`
+	}{
+		Incidents:                    ins,
+		IdsToRecentlyExpiredSilences: idsToRecentlyExpiredSilences,
+	}
+	if err := json.NewEncoder(w).Encode(resp); err != nil {
 		sklog.Errorf("Failed to send response: %s", err)
 	}
 }
@@ -549,12 +569,25 @@ func (srv *server) recentIncidentsHandler(w http.ResponseWriter, r *http.Request
 		return
 	}
 
+	// Get recently archived silences to see if the incident recently expired.
+	archivedSilences, err := srv.silenceStore.GetRecentlyArchived(*silenceRecentlyExpiredDuration)
+	if err != nil {
+		httputils.ReportError(w, err, "Failed to load archived silences.", http.StatusInternalServerError)
+		return
+	}
+	recentlyExpired := false
+	if len(ins) > 0 {
+		recentlyExpired = ins[0].IsSilenced(archivedSilences, false)
+	}
+
 	resp := struct {
-		Incidents []incident.Incident `json:"incidents"`
-		Flaky     bool                `json:"flaky"`
+		Incidents              []incident.Incident `json:"incidents"`
+		Flaky                  bool                `json:"flaky"`
+		RecentlyExpiredSilence bool                `json:"recently_expired_silence"`
 	}{
-		Incidents: ins,
-		Flaky:     incident.AreIncidentsFlaky(ins, reminderNumThreshold, reminderDurationThreshold, reminderDurationPercentage),
+		Incidents:              ins,
+		Flaky:                  incident.AreIncidentsFlaky(ins, reminderNumThreshold, reminderDurationThreshold, reminderDurationPercentage),
+		RecentlyExpiredSilence: recentlyExpired,
 	}
 	if err := json.NewEncoder(w).Encode(resp); err != nil {
 		sklog.Errorf("Failed to send response: %s", err)
