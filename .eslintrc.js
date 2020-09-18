d28784438ce832f22d4482a425fa6cea84418d17
Joe Gregorio <jcgregorio@google.com>
2020-09-18 10:38:39 -0400

    [fiddle] Port fiddle-sk to lit-html/TS.
    Change-Id: Ic825c8441f059654fc1956059483ae9aff2ae82e

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,26 +21,72 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
-    'camelcase': ['off'],
-    'class-methods-use-this': ['off'],
-    'func-names': ['off'],
-    'import/prefer-default-export': ['off'],
-    'max-classes-per-file': ['off'],
-    'max-len': ['off'],
-    'no-alert': ['off'],
-    'no-bitwise': ['warn'],
-    'no-continue': ['off'],
-    'no-lone-blocks':['off'],
-    'no-param-reassign': ['off'],
-    'no-plusplus': ['off'],
-    'no-restricted-syntax': ['warn'],
-    'no-return-assign': ['off'],
-    'no-shadow': ['warn'],
-    'no-underscore-dangle': ['off'],
-    'no-use-before-define': ['error', { 'functions': false, 'variables': false }],
-    'object-shorthand': ['off'],
-    'prefer-destructuring': ['off'],
-    'prefer-object-spread': ['off'],
-    'space-before-function-paren': ['error', { 'anonymous': 'never', 'named': 'never', 'asyncArrow': 'always' }],
+    'camelcase': [ 'off' ],
+    'class-methods-use-this': [ 'off' ],
+    'func-names': [ 'off' ],
+    'import/prefer-default-export': [ 'off' ],
+    'max-classes-per-file': [ 'off' ],
+    'max-len': [ 'off' ],
+    'no-alert': [ 'off' ],
+    'no-bitwise': [ 'warn' ],
+    'no-continue': [ 'off' ],
+    'no-lone-blocks': [ 'off' ],
+    'no-param-reassign': [ 'off' ],
+    'no-plusplus': [ 'off' ],
+    'no-restricted-syntax': [ 'warn' ],
+    'no-return-assign': [ 'off' ],
+    'no-shadow': [ 'warn' ],
+    'no-underscore-dangle': [ 'off' ],
+    'no-use-before-define': [ 'error', { 'functions': false, 'variables': false } ],
+    'object-shorthand': [ 'off' ],
+    'prefer-destructuring': [ 'off' ],
+    'prefer-object-spread': [ 'off' ],
+    'space-before-function-paren': [ 'error', { 'anonymous': 'never', 'named': 'never', 'asyncArrow': 'always' } ],
   },
+  overrides: [ {
+    files: [ "*.ts", "*.tsx" ],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+      tsconfigRootDir: __dirname,
+      project: [ './tsconfig.json' ],
+    },
+
+    // Start with the recommended rules, but turn some of them off in the
+    // 'rules' section below.
+    extends: [
+      "plugin:@typescript-eslint/recommended",
+    ],
+    settings: {
+      "import/resolver": {
+        "node": {
+          "extensions": [ ".js", ".jsx", ".ts", ".tsx" ]
+        }
+      }
+    },
+
+    plugins: [ "@typescript-eslint" ],
+
+    /**
+     * Typescript Rules
+     * https://github.com/bradzacher/eslint-plugin-typescript
+     * Enable your own typescript rules.
+     */
+    rules: {
+      // Prevent TypeScript-specific constructs from being erroneously flagged as unused
+      '@typescript-eslint/no-unused-vars': 'error',
+
+      '@typescript-eslint/no-non-null-assertion': 'off',
+
+      // Require a specific member delimiter style for interfaces and type literals
+      // Default Semicolon style
+      '@typescript-eslint/member-delimiter-style': 'error',
+
+      // Require a consistent member declaration order
+      '@typescript-eslint/member-ordering': 'error',
+
+      // Require consistent spacing around type annotations
+      '@typescript-eslint/type-annotation-spacing': 'error',
+      'import/extensions': [ 'off' ]
+    },
+  } ]
 };
