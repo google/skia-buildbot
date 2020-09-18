78a80264aa9f216c8c8e35168075823b9db0b5b2
Joe Gregorio <jcgregorio@google.com>
2020-09-18 12:00:04 -0400

    Add TypeScript support to eslint.
    There was a separate TS linter, but the approved path is to use
    eslint with a TS parser:
    
        https://medium.com/palantir/tslint-in-2019-1a144c2317a9
    
        https://github.com/typescript-eslint/typescript-eslint
    
    Change-Id: I3686139aa7755415438ebebe0e42e1ab40a32268

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,9 +9,7 @@ module.exports = {
     browser: true,
     es6: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ['airbnb-base'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
@@ -21,7 +19,7 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
-    'camelcase': ['off'],
+    camelcase: ['off'],
     'class-methods-use-this': ['off'],
     'func-names': ['off'],
     'import/prefer-default-export': ['off'],
@@ -30,17 +28,55 @@ module.exports = {
     'no-alert': ['off'],
     'no-bitwise': ['warn'],
     'no-continue': ['off'],
-    'no-lone-blocks':['off'],
+    'no-lone-blocks': ['off'],
     'no-param-reassign': ['off'],
     'no-plusplus': ['off'],
     'no-restricted-syntax': ['warn'],
     'no-return-assign': ['off'],
     'no-shadow': ['warn'],
     'no-underscore-dangle': ['off'],
-    'no-use-before-define': ['error', { 'functions': false, 'variables': false }],
+    'no-use-before-define': ['error', { functions: false, variables: false }],
     'object-shorthand': ['off'],
     'prefer-destructuring': ['off'],
     'prefer-object-spread': ['off'],
-    'space-before-function-paren': ['error', { 'anonymous': 'never', 'named': 'never', 'asyncArrow': 'always' }],
+    'space-before-function-paren': [
+      'error',
+      { anonymous: 'never', named: 'never', asyncArrow: 'always' },
+    ],
   },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        tsconfigRootDir: __dirname,
+        project: ['./tsconfig.json'],
+      },
+
+      // Start with the recommended rules, but turn some of them off in the
+      // 'rules' section below.
+      extends: ['plugin:@typescript-eslint/recommended'],
+      settings: {
+        'import/resolver': {
+          node: {
+            extensions: ['.js', '.jsx', '.ts', '.tsx'],
+          },
+        },
+      },
+      plugins: ['@typescript-eslint'],
+      rules: {
+        // Allow ! non-null assertions.
+        '@typescript-eslint/no-non-null-assertion': 'off',
+
+        // Require a consistent member declaration order
+        '@typescript-eslint/member-ordering': 'warning',
+
+        // Don't require the .ts extension for imports.
+        'import/extensions': ['off'],
+
+        // a: string = 'foo' might be redundant, but it's not harmful.
+        '@typescript-eslint/no-inferrable-types': 'off',
+      },
+    },
+  ],
 };
