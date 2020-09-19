e2f8121f9baeefcfcaab4d63ef3df7b629bb1c25
Leandro Lovisolo <lovisolo@google.com>
2020-09-18 20:29:36 -0400

    [gold] bulk-triage-sk: Add page object and use it in tests.
    Bug: skia:10246
    Change-Id: Iece121ddd5ecb047a4b26fcabb532b8b351f8c94

--- golden/modules/bulk-triage-sk/bulk-triage-sk_test.ts
+++ golden/modules/bulk-triage-sk/bulk-triage-sk_test.ts
@@ -2,40 +2,49 @@ import './index';
 import fetchMock from 'fetch-mock';
 import { eventPromise, setUpElementUnderTest } from '../../../infra-sk/modules/test_util';
 import { BulkTriageSk } from './bulk-triage-sk';
+import { BulkTriageSkPO } from './bulk-triage-sk_po';
 import { examplePageData, exampleAllData, expectedPageData, expectedAllData } from './test_data';
-import { $$ } from 'common-sk/modules/dom';
 import { expect } from 'chai';
-import { CheckOrRadio } from 'elements-sk/checkbox-sk/checkbox-sk';
 
 describe('bulk-triage-sk', () => {
   const newInstance = setUpElementUnderTest<BulkTriageSk>('bulk-triage-sk');
 
   let bulkTriageSk: BulkTriageSk;
+  let bulkTriageSkPO: BulkTriageSkPO;
 
   beforeEach(() => {
     bulkTriageSk = newInstance();
     bulkTriageSk.currentPageDigests = examplePageData;
     bulkTriageSk.allDigests = exampleAllData;
+    bulkTriageSkPO = new BulkTriageSkPO(bulkTriageSk);
   });
 
-  it('defaults to bulk-triaging to closest', () => {
-    expectValueAndToggledButtonToBe(bulkTriageSk, 'closest');
+  it('defaults to bulk-triaging to closest', async () => {
+    expect(await bulkTriageSkPO.isClosestBtnSelected()).to.be.true;
+    expect(bulkTriageSk.value).to.equal('closest');
   });
 
-  it('has value respond to button clicks', () => {
-    $$<HTMLButtonElement>('button.untriaged', bulkTriageSk)!.click();
-    expectValueAndToggledButtonToBe(bulkTriageSk, 'untriaged');
-    $$<HTMLButtonElement>('button.positive', bulkTriageSk)!.click();
-    expectValueAndToggledButtonToBe(bulkTriageSk, 'positive');
-    $$<HTMLButtonElement>('button.negative', bulkTriageSk)!.click();
-    expectValueAndToggledButtonToBe(bulkTriageSk, 'negative');
-    $$<HTMLButtonElement>('button.closest', bulkTriageSk)!.click();
-    expectValueAndToggledButtonToBe(bulkTriageSk, 'closest');
+  it('has value respond to button clicks', async () => {
+    await bulkTriageSkPO.clickUntriagedBtn();
+    expect(await bulkTriageSkPO.isUntriagedBtnSelected()).to.be.true;
+    expect(bulkTriageSk.value).to.equal('untriaged');
+
+    await bulkTriageSkPO.clickPositiveBtn();
+    expect(await bulkTriageSkPO.isPositiveBtnSelected()).to.be.true;
+    expect(bulkTriageSk.value).to.equal('positive');
+
+    await bulkTriageSkPO.clickNegativeBtn();
+    expect(await bulkTriageSkPO.isNegativeBtnSelected()).to.be.true;
+    expect(bulkTriageSk.value).to.equal('negative');
+
+    await bulkTriageSkPO.clickClosestBtn();
+    expect(await bulkTriageSkPO.isClosestBtnSelected()).to.be.true;
+    expect(bulkTriageSk.value).to.equal('closest');
   });
 
   it('emits a bulk_triage_cancelled event when the cancel button is clicked', async () => {
     const cancelEvent = eventPromise('bulk_triage_cancelled', 100);
-    $$<HTMLButtonElement>('button.cancel', bulkTriageSk)!.click();
+    await bulkTriageSkPO.clickCancelBtn();
     await cancelEvent;
   });
 
@@ -52,27 +61,22 @@ describe('bulk-triage-sk', () => {
         return 200;
       });
 
-      $$<HTMLButtonElement>('button.triage', bulkTriageSk)!.click();
+      await bulkTriageSkPO.clickTriageBtn();
       await finishedPromise;
     });
 
     it('POSTs for all results', async () => {
       bulkTriageSk.changeListID = 'someCL';
       bulkTriageSk.crs = 'gerrit';
-      $$<CheckOrRadio>('checkbox-sk.toggle_all', bulkTriageSk)!.click();
+      await bulkTriageSkPO.clickToggleAllCheckbox();
       const finishedPromise = eventPromise('bulk_triage_finished');
       fetchMock.post('/json/v1/triage', (url, req) => {
         expect(req.body).to.equal(expectedAllData);
         return 200;
       });
 
-      $$<HTMLButtonElement>('button.triage', bulkTriageSk)!.click();
+      bulkTriageSkPO.clickTriageBtn();
       await finishedPromise;
     });
   });
 });
-
-const expectValueAndToggledButtonToBe = (bulkTriageSk: BulkTriageSk, value: string) => {
-  expect(bulkTriageSk.value).to.equal(value);
-  expect($$<HTMLButtonElement>(`button.${value}`, bulkTriageSk)!.className).to.contain('selected');
-};
