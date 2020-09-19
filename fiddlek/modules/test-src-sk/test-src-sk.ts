2e4919816e899bd777aff887a48dc51e41be8a9d
Joe Gregorio <jcgregorio@google.com>
2020-09-19 10:12:11 -0400

    [fiddle] Port fiddle-sk to lit-html/TS.
    Change-Id: Ic825c8441f059654fc1956059483ae9aff2ae82e

--- fiddlek/modules/test-src-sk/test-src-sk.ts
+++ fiddlek/modules/test-src-sk/test-src-sk.ts
@@ -8,6 +8,7 @@
  */
 import { define } from 'elements-sk/define';
 import { html } from 'lit-html';
+import { jsonOrThrow } from 'common-sk/modules/jsonOrThrow';
 import { ElementSk } from '../../../infra-sk/modules/ElementSk';
 
 export class TestSrcSk extends ElementSk {
@@ -17,38 +18,34 @@ export class TestSrcSk extends ElementSk {
   // The URL to retrieve the text from.
   private _src: string = '';
 
-  private static template = (ele: TestSrcSk) =>
-    html`<pre class="output">${ele._text}</pre>`;
-
   constructor() {
     super(TestSrcSk.template);
   }
 
-  connectedCallback() {
+  private static template = (ele: TestSrcSk) => html`<pre class="output">${ele._text}</pre>`;
+
+  connectedCallback() : void {
     super.connectedCallback();
     this._upgradeProperty('src');
   }
 
   /** @prop src - URL to retrieve the text from. */
-  get src() {
+  get src():string {
     return this._src;
   }
-  set src(val) {
+
+  set src(val:string) {
     this._src = val;
-    // Property funcs can't be async, thus the IIAAFE (immediately invoked async
-    // arrow function expression).
-    (async () => {
-      if (val === '') {
-        return;
-      }
-      const resp = await fetch(val);
+    fetch(val).then((resp) => {
       if (!resp.ok) {
-        throw `Failed to retrieve text from ${val}`;
+        throw new Error(`Failed to retrieve text from ${val}`);
       }
-      this._text = await resp.text();
+      return resp.text();
+    }).then((text) => {
+      this._text = text;
       this._render();
       this.dispatchEvent(new CustomEvent('change', { bubbles: true }));
-    })();
+    });
   }
 }
 
