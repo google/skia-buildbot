7eb1f0df37a84b162324b50174d0057a05f23b77
Joe Gregorio <jcgregorio@google.com>
2020-09-19 11:04:43 -0400

    [fiddle] Fix lint errors in textarea-numbers-sk.
    Change-Id: I4172c36e9bd68e37ed6bcea43d990322beeba24e

--- fiddlek/modules/textarea-numbers-sk/textarea-numbers-sk.ts
+++ fiddlek/modules/textarea-numbers-sk/textarea-numbers-sk.ts
@@ -7,9 +7,9 @@
  */
 import 'codemirror/mode/clike/clike'; // Syntax highlighting for c-like languages.
 import { define } from 'elements-sk/define';
+import CodeMirror from 'codemirror';
 import { ElementSk } from '../../../infra-sk/modules/ElementSk';
 import { isDarkMode } from '../../../infra-sk/modules/theme-chooser-sk/theme-chooser-sk';
-import CodeMirror from 'codemirror';
 
 export class TextareaNumbersSk extends ElementSk {
   /** The CodeMirror control. */
@@ -23,14 +23,9 @@ export class TextareaNumbersSk extends ElementSk {
    * For this to work the associated CSS themes must be loaded. See
    * textarea-numbers-sk.scss.
    */
-  private static themeFromCurrentMode = () =>
-    isDarkMode() ? 'base16-dark' : 'base16-light';
+  private static themeFromCurrentMode = () => (isDarkMode() ? 'base16-dark' : 'base16-light');
 
-  constructor() {
-    super();
-  }
-
-  connectedCallback() {
+  connectedCallback():void {
     super.connectedCallback();
 
     // Creates and attaches the CodeMirror control as this elements only child.
@@ -39,52 +34,59 @@ export class TextareaNumbersSk extends ElementSk {
       lineNumbers: true,
       mode: 'text/x-c++src',
       theme: TextareaNumbersSk.themeFromCurrentMode(),
+      viewportMargin: Infinity,
     });
 
     this._upgradeProperty('value');
 
     // Listen for theme changes.
-    document.addEventListener('theme-chooser-toggle', (e) => {
-      this.codeMirror?.setOption(
+    document.addEventListener('theme-chooser-toggle', () => {
+      this.codeMirror!.setOption(
         'theme',
-        TextareaNumbersSk.themeFromCurrentMode()
+        TextareaNumbersSk.themeFromCurrentMode(),
       );
     });
   }
 
   /** Removes all error line annotations. */
-  clearErrors() {
+  clearErrors() : void{
     this.errorLines.forEach((textMarker) => {
       textMarker.clear();
     });
   }
 
   /** Indicates there is an error on line n. */
-  setErrorLine(n: number) {
+  setErrorLine(n: number) :void {
     // Set the class of that line to 'cm-error'.
     this.errorLines.push(
-      this.codeMirror?.markText(
-        { line: n, ch: 0 },
-        { line: n, ch: 200 }, // Some large number for the character offset.
+      this.codeMirror!.markText(
+        { line: n - 1, ch: 0 },
+        { line: n - 1, ch: 200 }, // Some large number for the character offset.
         {
           className: 'cm-error', // See the base16-dark.css file in CodeMirror for the class name.
-        }
-      )!
+        },
+      )!,
     );
   }
 
   /** Move the cursor to the given row and column. */
-  setCursor(row: number, col: number) {
-    this.codeMirror?.setCursor(row, col);
+  setCursor(row: number, col: number) :void {
+    this.codeMirror!.focus();
+    this.codeMirror!.setCursor({ line: row - 1, ch: col - 1 });
   }
 
-  /** @prop value {string} The text content of the edit box. */
-  get value() {
+  /** @prop The text content of the edit box. */
+  get value() : string {
+    if (!this.codeMirror) {
+      return '';
+    }
     return this.codeMirror!.getValue();
   }
 
   set value(val: string) {
-    this.codeMirror!.setValue(val);
+    if (this.codeMirror) {
+      this.codeMirror!.setValue(val);
+    }
     this.clearErrors();
   }
 }
