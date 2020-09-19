fd4e52aa18f6b95160a1667708b743ec85e0ab31
Leandro Lovisolo <lovisolo@google.com>
2020-09-18 20:33:41 -0400

    [gold] bulk-triage-sk: Add page object and use it in tests.
    Bug: skia:10246
    Change-Id: Iece121ddd5ecb047a4b26fcabb532b8b351f8c94

--- golden/modules/bulk-triage-sk/bulk-triage-sk_puppeteer_test.ts
+++ golden/modules/bulk-triage-sk/bulk-triage-sk_puppeteer_test.ts
@@ -1,15 +1,24 @@
 import { expect } from 'chai';
 import { takeScreenshot, TestBed } from '../../../puppeteer-tests/util';
 import { loadGoldWebpack } from '../common_puppeteer_test/common_puppeteer_test';
+import { BulkTriageSkPO } from './bulk-triage-sk_po';
+import { ElementHandle } from 'puppeteer';
 
 describe('bulk-triage-sk', () => {
   let testBed: TestBed;
+
+  let bulkTriageSk: ElementHandle;
+  let bulkTriageSkPO: BulkTriageSkPO;
+
   before(async () => {
     testBed = await loadGoldWebpack();
   });
 
   beforeEach(async () => {
     await testBed.page.goto(`${testBed.baseUrl}/dist/bulk-triage-sk.html`);
+
+    bulkTriageSk = (await testBed.page.$('#default'))!;
+    bulkTriageSkPO = new BulkTriageSkPO(bulkTriageSk);
   });
 
   it('should render the demo page', async () => {
@@ -18,46 +27,40 @@ describe('bulk-triage-sk', () => {
 
   describe('screenshots', async () => {
     it('should be closest by default', async () => {
-      const bulkTriageSk = await testBed.page.$('#default');
-      await takeScreenshot(bulkTriageSk!, 'gold', 'bulk-triage-sk_closest');
+      await takeScreenshot(bulkTriageSk, 'gold', 'bulk-triage-sk_closest');
     });
 
     it('should be negative', async () => {
-      await testBed.page.click('#default button.untriaged');
+      await bulkTriageSkPO.clickUntriagedBtn();
       await testBed.page.click('body'); // Remove focus from button.
-      const bulkTriageSk = await testBed.page.$('#default');
-      await takeScreenshot(bulkTriageSk!, 'gold', 'bulk-triage-sk_untriaged');
+      await takeScreenshot(bulkTriageSk, 'gold', 'bulk-triage-sk_untriaged');
     });
 
     it('should be negative', async () => {
-      await testBed.page.click('#default button.negative');
+      await bulkTriageSkPO.clickNegativeBtn();
       await testBed.page.click('body'); // Remove focus from button.
-      const bulkTriageSk = await testBed.page.$('#default');
-      await takeScreenshot(bulkTriageSk!, 'gold', 'bulk-triage-sk_negative');
+      await takeScreenshot(bulkTriageSk, 'gold', 'bulk-triage-sk_negative');
     });
 
     it('should be positive', async () => {
-      await testBed.page.click('#default button.positive');
+      await bulkTriageSkPO.clickPositiveBtn();
       await testBed.page.click('body'); // Remove focus from button.
-      const bulkTriageSk = await testBed.page.$('#default');
-      await takeScreenshot(bulkTriageSk!, 'gold', 'bulk-triage-sk_positive');
+      await takeScreenshot(bulkTriageSk, 'gold', 'bulk-triage-sk_positive');
     });
 
     it('should be positive, with button focused', async () => {
-      await testBed.page.click('#default button.positive');
-      const bulkTriageSk = await testBed.page.$('#default');
-      await takeScreenshot(bulkTriageSk!, 'gold', 'bulk-triage-sk_positive-button-focused');
+      await bulkTriageSkPO.clickPositiveBtn();
+      await takeScreenshot(bulkTriageSk, 'gold', 'bulk-triage-sk_positive-button-focused');
     });
 
     it('changes views when checkbox clicked', async () => {
-      await testBed.page.click('#default checkbox-sk.toggle_all');
-      const bulkTriageSk = await testBed.page.$('#default');
-      await takeScreenshot(bulkTriageSk!, 'gold', 'bulk-triage-sk_triage-all');
+      await bulkTriageSkPO.clickToggleAllCheckbox();
+      await takeScreenshot(bulkTriageSk, 'gold', 'bulk-triage-sk_triage-all');
     });
 
     it('shows some extra information for changelists', async () => {
-      const bulkTriageSk = await testBed.page.$('#changelist');
-      await takeScreenshot(bulkTriageSk!, 'gold', 'bulk-triage-sk_changelist');
+      const bulkTriageSkWithCL = await testBed.page.$('#changelist');
+      await takeScreenshot(bulkTriageSkWithCL!, 'gold', 'bulk-triage-sk_changelist');
     });
   });
 });
