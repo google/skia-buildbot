ba7390e4d361f395eeba8a6de8cd827d7b50a7b9
Leandro Lovisolo <lovisolo@google.com>
2020-09-24 22:26:15 -0400

    [gold] search-page-sk: Add keyboard shortcuts.
    Bug: skia:10246
    Change-Id: I394dec3620051e3c82d5a9a37e8b5595da1ed6c1

--- golden/modules/search-page-sk/search-page-sk_po.ts
+++ golden/modules/search-page-sk/search-page-sk_po.ts
@@ -2,6 +2,7 @@ import { PageObject } from '../../../infra-sk/modules/page_object/page_object';
 import { SearchControlsSkPO } from '../search-controls-sk/search-controls-sk_po';
 import { ChangelistControlsSkPO } from '../changelist-controls-sk/changelist-controls-sk_po';
 import { BulkTriageSkPO } from '../bulk-triage-sk/bulk-triage-sk_po';
+import { Label } from '../rpc_types';
 
 /** A page object for the SearchPageSkPO component. */
 export class SearchPageSkPO extends PageObject {
@@ -45,6 +46,16 @@ export class SearchPageSkPO extends PageObject {
 
   // TODO(lovisolo): Replace with DigestDetailsSkPO when DigestDetailsSk is ported to TypeScript
   //                 and tested with a page object.
+  getSelectedDigest() {
+    // By returning an array instead of a single digest (or null) we can assert in tests that there
+    // is at most one selected digest at any given time.
+    return this.selectAllPOEThenMap(
+      'digest-details-sk.selected .digest_label:nth-child(1)', (el) => el.innerText);
+  }
+
+
+  // TODO(lovisolo): Replace with DigestDetailsSkPO when DigestDetailsSk is ported to TypeScript
+  //                 and tested with a page object.
   getDigests() {
     return this.selectAllPOEThenMap('.digest_label:nth-child(1)', (el) => el.innerText);
   }
@@ -53,5 +64,37 @@ export class SearchPageSkPO extends PageObject {
   //                 and tested with a page object.
   getDiffDetailsHrefs() {
     return this.selectAllPOEThenMap('.diffpage_link', (el) => el.href);
+  }
+
+  // TODO(lovisolo): Replace with DigestDetailsSkPO when DigestDetailsSk is ported to TypeScript
+  //                 and tested with a page object.
+  async getDigestLabels() {
+    return this.selectAllPOEThenMap<Label>(
+        'digest-details-sk triage-sk button.selected', async (el) => {
+      // The className attribute can be e.g. "positive selected", so we look for label substrings.
+      const className = await el.className;
+      if (className.includes('positive')) return 'positive';
+      if (className.includes('negative')) return 'negative';
+      return 'untriaged';
+    });
+  }
+
+  // TODO(lovisolo): Replace with DigestDetailsSkPO when DigestDetailsSk is ported to TypeScript
+  //                 and tested with a page object.
+  async getDigestWithOpenZoomDialog() {
+    // By returning an array instead of a single digest (or null) we can assert in tests that there
+    // is at most one digest at any given time with its zoom dialog open.
+
+    const digests = await this.getDigests();
+    if (digests.length === 0) return [];
+
+    const openZoomDialogs =
+      await this.selectAllPOEThenMap(
+        'digest-details-sk dialog.zoom_dialog', (el) => el.hasAttribute('open'));
+    return digests.filter((_, idx) => openZoomDialogs[idx]);
+  }
+
+  typeKey(key: string) {
+    return this.element.typeKey(key);
   }
 };
