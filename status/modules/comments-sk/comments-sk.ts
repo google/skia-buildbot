8d052404bab3fda492cfc4e6d2a3237a406778a8
Weston Tracey <westont@google.com>
2020-09-25 11:36:02 -0400

    [status] Add comments-sk, and support for Twirp addition and removal of comments.
    Change-Id: I72db21fda70cef7b0306b226caa44485e6b79ac1

---
+++ status/modules/comments-sk/comments-sk.ts
@@ -0,0 +1,253 @@
+/**
+ * @module modules/comments-sk
+ * @description <h2><code>comments-sk</code></h2>
+ *
+ * @evt
+ *
+ * @property addCommentUrl: string - Endpoint for posting comments.
+ * @property allowAdd: boolean - Support adding comments, if permissions allow.
+ * @property allowDelete: boolean - Support deleting comments, if permissions allow.
+ * @property allowEmpty: boolean - Support empty comments.
+ * @property showFlaky': boolean - Display flaky field of comments.
+ * @property showIgnoreFailure: boolean - Display ignoreFailure field of comments.
+ *
+ */
+import { define } from 'elements-sk/define';
+import { html, TemplateResult } from 'lit-html';
+import { ElementSk } from '../../../infra-sk/modules/ElementSk';
+import { Comment, GetStatusService, StatusService } from '../rpc';
+import { escapeAndLinkify } from '../../../infra-sk/modules/linkify';
+import { Login } from '../../../infra-sk/modules/login';
+
+import '../../../ct/modules/input-sk';
+import '../../../infra-sk/modules/human-date-sk';
+import 'elements-sk/icon/check-box-icon-sk';
+import 'elements-sk/icon/check-box-outline-blank-icon-sk';
+import 'elements-sk/icon/delete-icon-sk';
+import 'elements-sk/checkbox-sk';
+import 'elements-sk/styles/buttons';
+import { errorMessage } from 'elements-sk/errorMessage';
+import { $$ } from 'common-sk/modules/dom';
+
+// Helper class for parent to set comments to display, as well as metadata
+// about them and any comments added by the user.
+export class CommentData {
+  repo: string = '';
+  taskId: string = '';
+  taskSpec: string = '';
+  commit: string = '';
+  comments: Array<Comment> = [];
+}
+
+export class CommentsSk extends ElementSk {
+  private static template = (el: CommentsSk) =>
+    html`
+      <table class="comments">
+        ${el.headings()}
+        ${el.comments().map(
+          (c) => html`
+            <tr class="comment">
+              <td><human-date-sk .date=${c.timestamp} .diff=${true}></human-date-sk> ago</td>
+              <td>${c.user}</td>
+              <td class="commentMessage">${escapeAndLinkify(c.message)}</td>
+              ${el.optionalCommentFields(c)}
+            </tr>
+          `
+        )}
+        ${el.addCommentTemplate()}
+      </table>
+    `;
+
+  private headings(): TemplateResult {
+    return this.comments().length === 0
+      ? html``
+      : html`
+          <tr>
+            <th>Time</th>
+            <th>User</th>
+            <th>Message</th>
+            ${this.showFlaky ? html`<th>Flaky</th> ` : html``}
+            ${this.showIgnoreFailure ? html`<th>Ignore Failure</th> ` : html``}
+            ${this.allowDelete && this.editRights ? html`<th>Delete</th>` : html``}
+          </tr>
+        `;
+  }
+  private optionalCommentFields(comment: Comment): Array<TemplateResult> {
+    const ret: Array<TemplateResult> = [];
+    if (this.showFlaky) {
+      ret.push(
+        comment.flaky
+          ? html`<td><check-box-icon-sk></check-box-icon-sk></td> `
+          : html`<td><check-box-outline-blank-icon-sk></check-box-outline-blank-icon-sk></td>`
+      );
+    }
+    if (this.showIgnoreFailure) {
+      ret.push(
+        comment.ignoreFailure
+          ? html`<td><check-box-icon-sk></check-box-icon-sk></td> `
+          : html`<td><check-box-outline-blank-icon-sk></check-box-outline-blank-icon-sk></td>`
+      );
+    }
+    if (this.allowDelete && this.editRights) {
+      ret.push(
+        html`<td @click=${() => this.deleteComment(comment)}>
+          <delete-icon-sk></delete-icon-sk>
+        </td>`
+      );
+    }
+    return ret;
+  }
+
+  private addCommentTemplate(): TemplateResult {
+    return this.allowAdd && this.editRights
+      ? html`
+          <tr>
+            <td colspan="3">
+              <input-sk value="" class="commentField" label="Comment"></input-sk>
+            </td>
+            ${this.showFlaky
+              ? html`<td><checkbox-sk class="commentFlaky" label="Flaky"></checkbox-sk></td>`
+              : html``}
+            ${this.showIgnoreFailure
+              ? html`<td>
+                  <checkbox-sk class="commentIgnoreFailure" label="IgnoreFailure"></checkbox-sk>
+                </td>`
+              : html``}
+            <td>
+              <button @click=${() => this.addComment()}>Submit</button>
+            </td>
+          </tr>
+        `
+      : html``;
+  }
+
+  private _commentData: CommentData = new CommentData();
+  private _addCommentUrl: string = '';
+  private _allowAdd: boolean = false;
+  private _allowEmpty: boolean = false;
+  private _allowDelete: boolean = false;
+  private _showFlaky: boolean = false;
+  private _showIgnoreFailure: boolean = false;
+  private _editRights: boolean = false;
+  private client: StatusService = GetStatusService();
+
+  constructor() {
+    super(CommentsSk.template);
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    this._upgradeProperty('allowAdd');
+    this._upgradeProperty('allowEmpty');
+    this._upgradeProperty('allowDelete');
+    this._upgradeProperty('editRights');
+    this._upgradeProperty('showIngoreFailure');
+    this._upgradeProperty('showFlaky');
+    this._upgradeProperty('comments');
+    this._render();
+  }
+
+  comments() {
+    return this.commentData.comments;
+  }
+
+  get editRights(): boolean {
+    return this._editRights;
+  }
+
+  set editRights(v: boolean) {
+    this._editRights = v;
+    this._render();
+  }
+
+  get allowAdd(): boolean {
+    return this._allowAdd;
+  }
+
+  set allowAdd(v: boolean) {
+    this._allowAdd = v;
+    this._render();
+  }
+
+  get allowEmpty(): boolean {
+    return this._allowEmpty;
+  }
+
+  set allowEmpty(v: boolean) {
+    this._allowEmpty = v;
+    this._render();
+  }
+
+  get allowDelete(): boolean {
+    return this._allowDelete;
+  }
+
+  set allowDelete(v: boolean) {
+    this._allowDelete = v;
+    this._render();
+  }
+
+  get showIgnoreFailure(): boolean {
+    return this._showIgnoreFailure;
+  }
+
+  set showIgnoreFailure(v: boolean) {
+    this._showIgnoreFailure = v;
+    this._render();
+  }
+
+  get showFlaky(): boolean {
+    return this._showFlaky;
+  }
+
+  set showFlaky(v: boolean) {
+    this._showFlaky = v;
+    this._render();
+  }
+
+  set commentData(c: CommentData) {
+    this._commentData = c;
+    this._render();
+  }
+
+  get commentData(): CommentData {
+    return this._commentData;
+  }
+
+  private deleteComment(comment: Comment) {
+    this.client
+      .deleteComment({
+        timestamp: comment.timestamp,
+        repo: comment.repo,
+        taskId: comment.taskId,
+        taskSpec: comment.taskSpecName,
+        commit: comment.commit,
+      })
+      .then(() => {
+        // Visually get rid of the comment that was removed, and fire an event for the parent
+        // element to refresh so it doesn't reappear.
+        this.commentData.comments = this.comments().filter((c) => c != comment);
+        this.dispatchEvent(new Event('data-update', { bubbles: true }));
+        this._render();
+      })
+      .catch(errorMessage);
+  }
+  private addComment() {
+    this.client
+      .addComment({
+        repo: this.commentData.repo,
+        taskId: this.commentData.taskId,
+        taskSpec: this.commentData.taskSpec,
+        commit: this.commentData.commit,
+        message: ($$('.commentField') as any).value,
+        flaky: ($$('.commentFlaky', this) as any)?.checked,
+        ignoreFailure: ($$('.commentIgnoreFailure', this) as any)?.checked,
+      })
+      .then(() => {
+        this.dispatchEvent(new Event('data-update', { bubbles: true }));
+      })
+      .catch(errorMessage);
+  }
+}
+
+define('comments-sk', CommentsSk);
