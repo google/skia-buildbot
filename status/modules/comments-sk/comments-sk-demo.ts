121a1cb21516a4e0c05ad4063c0f351c7442da58
Weston Tracey <westont@google.com>
2020-09-25 09:48:55 -0400

    [status] Add comments-sk, and support for Twirp addition and removal of comments.
    Change-Id: I72db21fda70cef7b0306b226caa44485e6b79ac1

---
+++ status/modules/comments-sk/comments-sk-demo.ts
@@ -0,0 +1,22 @@
+import { $$ } from 'common-sk/modules/dom';
+import { CommentsSk } from './comments-sk';
+import './index';
+import { taskspecComments } from './test_data';
+
+document.querySelector('comments-sk')!.addEventListener('some-event-name', (e) => {
+  document.querySelector('#events')!.textContent = JSON.stringify(e, null, '  ');
+});
+
+Date.now = () => 1600883976659;
+(<CommentsSk>$$('comments-sk')).commentData = {
+  repo: 'skia',
+  taskId: '',
+  taskSpec: 'Build-The-Thing',
+  commit: '',
+  comments: taskspecComments,
+};
+(<CommentsSk>$$('comments-sk')).showFlaky = true;
+(<CommentsSk>$$('comments-sk')).showIgnoreFailure = true;
+(<CommentsSk>$$('comments-sk')).allowDelete = true;
+(<CommentsSk>$$('comments-sk')).allowEmpty = true;
+(<CommentsSk>$$('comments-sk')).allowAdd = true;
