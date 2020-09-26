ead09aa0c8c8a988cf3382c1a8033ce61e4fec24
Joe Gregorio <jcgregorio@google.com>
2020-09-26 10:41:56 -0400

    [fiddle] Switch over main UI to webpack based UI away from Polymer.
    Change-Id: I06bf1d7ff987215cf825549da4ae063a01b5d31a

--- fiddlek/pages/embed.ts
+++ fiddlek/pages/embed.ts
@@ -1,1 +1,7 @@
 import '../modules/fiddle-embed';
+import { FiddleSkFiddleSuccessEventDetail } from '../modules/fiddle-sk/fiddle-sk';
+
+document.querySelector('fiddle-sk')!.addEventListener('fiddle-success', (e) => {
+  const name = (e as CustomEvent<FiddleSkFiddleSuccessEventDetail>).detail;
+  window.history.pushState(null, '', `/c/${name}`);
+});
