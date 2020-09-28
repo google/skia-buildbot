7912fafa04503c30dc6719eadf33bf79eae0341e
Leandro Lovisolo <lovisolo@google.com>
2020-09-28 15:09:36 -0400

    Fix "go generate ./..." diffs.
    This CL contains the diffs obtained after running the above command from the repository's root directory.
    
    Change-Id: I0e9f39d1f54273f8940da09b47da920d45793ca0

--- go/taskname/task_name_schema_gen.go
+++ go/taskname/task_name_schema_gen.go
@@ -7,6 +7,7 @@ var SCHEMA_FROM_GIT = map[string]*Schema{
 	"BuildStats":  {Keys: []string{"os", "compiler", "target_arch", "configuration"}, OptionalKeys: []string{"extra_config"}, RecurseRoles: []string(nil)},
 	"Canary":      {Keys: []string{"project"}, OptionalKeys: []string(nil), RecurseRoles: []string(nil)},
 	"FM":          {Keys: []string{"os", "compiler", "model", "cpu_or_gpu", "cpu_or_gpu_value", "arch", "configuration", "test_filter"}, OptionalKeys: []string{"extra_config"}, RecurseRoles: []string(nil)},
+	"Fuzz":        {Keys: []string{"os", "compiler"}, OptionalKeys: []string{"extra_config"}, RecurseRoles: []string(nil)},
 	"Housekeeper": {Keys: []string{"frequency"}, OptionalKeys: []string{"extra_config"}, RecurseRoles: []string(nil)},
 	"Infra":       {Keys: []string{"frequency"}, OptionalKeys: []string{"extra_config"}, RecurseRoles: []string(nil)},
 	"Perf":        {Keys: []string{"os", "compiler", "model", "cpu_or_gpu", "cpu_or_gpu_value", "arch", "configuration", "test_filter"}, OptionalKeys: []string{"extra_config"}, RecurseRoles: []string(nil)},
