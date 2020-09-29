54d310ac37be7b0b3d9280e516506e93ef22de44
Ravi Mistry <rmistry@google.com>
2020-09-29 14:30:15 -0400

    Checkpoint
    Change-Id: I70e67ee9b534c1a119c7d7b43effd5f82b29d4d4

---
+++ bugs/go/bug_framework/bug_framework.go
@@ -0,0 +1,179 @@
+package bug_framework
+
+// CALL THIS bug_framework instead.
+
+import (
+	"io/ioutil"
+	"time"
+
+	"cloud.google.com/go/storage"
+	"go.skia.org/infra/go/skerr"
+	"go.skia.org/infra/go/util"
+)
+
+const ()
+
+type Issue struct {
+	Id       string `json:"id"`
+	State    string `json:"state"`
+	Priority string `json:"priority"`
+	Owner    string `json:"owner"`
+	Link     string `json:"link"`
+
+	CreatedTime  time.Time `json:"created"`
+	ModifiedTime time.Time `json:"modified"`
+	ClosedTime   time.Time `json:"closed,omitempty"`
+
+	Title   string `json:"title"`
+	Summary string `json:"summary"`
+}
+
+type BugFramework interface {
+
+	// GetBugFrameworkName returns the name of the bug framework. Eg: Monorail, IssueTracker, Github.
+	GetBugFrameworkName() string
+
+	// Search returns issues that match the provided parameters.
+	Search(username string, statuses []string) ([]Issue, error)
+
+	// SetState sets a state to the specified issue.
+	SetState(i Issue, state string) error
+}
+
+////////////////////////////////////////////////////////////// IssueTracker //////////////////////////////////////////////////////////////
+
+const (
+	IssueTrackerBucket = "skia-issuetracker-details"
+	ResultsFileName    = "results.json"
+)
+
+type IssueTracker struct {
+	storageClient *storage.Client
+
+	// ComponentIds []int64  `json:"component_ids"`
+	// UserNames    []string `json:"usernames"`
+}
+
+func InitIssueTracker(storageClient *storage.Client) (BugFramework, error) {
+	return &IssueTracker{
+		storageClient: storageClient,
+	}, nil
+}
+
+func (it *IssueTracker) GetBugFrameworkName() string {
+	return "IssueTracker"
+}
+
+func (it *IssueTracker) Search(username string, statuses []string) ([]Issue, error) {
+
+	obj := it.storageClient.Bucket(IssueTrackerBucket).Object(ResultsFileName
+	reader, err := obj.NewReader(ctx)
+	if err != nil {
+		return skerr.Fmt("accessing %s/%s failed: %s", g.bucket, g.name, err)
+	}
+	defer util.Close(reader)
+
+	// Read the entire file into memory and return a buffer.
+	if g.content, err = ioutil.ReadAll(reader); err != nil {
+		g.content = nil
+		return skerr.Fmt("error reading content of %s/%s: %s", g.bucket, g.name, err)
+	}
+
+	/*
+		query := ""
+		if username != "" {
+			query += fmt.Sprintf("assignee:%s", username)
+		}
+		// Do something with statuses as well!
+		payLoad := struct {
+			PageNum int    `json:"p"`
+			Count   int    `json:"count"`
+			Sort    string `json:"s"`
+			Query   string `json:"q"`
+		}{
+			PageNum: 1,
+			Count:   25,
+			Sort:    "modified_time:desc",
+			Query:   query,
+		}
+
+		b := new(bytes.Buffer)
+		e := json.NewEncoder(b)
+		if err := e.Encode(payLoad); err != nil {
+			return nil, fmt.Errorf("Problem encoding json for request: %s", err)
+		}
+
+		httpClient := httputils.DefaultClientConfig().With2xxOnly().Client()
+		// httpClient := httputils.DefaultClientConfig().WithTokenSource(ts).With2xxOnly().Client()
+
+		url := "https://issuetracker.google.com/action/issues/list"
+		resp, err := httpClient.Post(url, "application/json", b)
+
+		if err != nil || resp == nil || resp.StatusCode != 200 {
+			return nil, fmt.Errorf("Failed to retrieve issue tracker response: %s", err)
+		}
+		defer util.Close(resp.Body)
+
+		msg, err := ioutil.ReadAll(resp.Body)
+		sklog.Infof("%s\n\nErr: %v", string(msg), err)
+
+		return nil, nil
+	*/
+}
+
+func (it *IssueTracker) AddComment(i Issue, comment string) error {
+	return nil
+}
+
+func (it *IssueTracker) SetState(i Issue, status string) error {
+	return nil
+}
+
+// func get(client *http.Client, u string) ([]Issue, error) {
+// 	resp, err := client.Get(u)
+// 	if err != nil || resp == nil || resp.StatusCode != 200 {
+// 		return nil, fmt.Errorf("Failed to retrieve issue tracker response: %s Status Code: %d", err, resp.StatusCode)
+// 	}
+// 	defer util.Close(resp.Body)
+
+// 	issueResponse := &IssueResponse{
+// 		Items: []Issue{},
+// 	}
+// 	if err := json.NewDecoder(resp.Body).Decode(&issueResponse); err != nil {
+// 		return nil, err
+// 	}
+
+// 	return issueResponse.Items, nil
+// }
+
+// func post(client *http.Client, dst string, request interface{}) error {
+// 	b := new(bytes.Buffer)
+// 	e := json.NewEncoder(b)
+// 	if err := e.Encode(request); err != nil {
+// 		return fmt.Errorf("Problem encoding json for request: %s", err)
+// 	}
+
+// 	resp, err := client.Post(dst, "application/json", b)
+
+// 	if err != nil || resp == nil || resp.StatusCode != 200 {
+// 		return fmt.Errorf("Failed to retrieve issue tracker response: %s", err)
+// 	}
+// 	defer util.Close(resp.Body)
+// 	msg, err := ioutil.ReadAll(resp.Body)
+// 	sklog.Infof("%s\n\nErr: %v", string(msg), err)
+// 	return nil
+// }
+
+////////////////////////////////////////////////////////////// MONORAIL //////////////////////////////////////////////////////////////
+
+type Monorail struct {
+	Projects  []string `json:"projects"`
+	UserNames []string `json:"usernames"`
+}
+
+////////////////////////////////////////////////////////////// GITHUB //////////////////////////////////////////////////////////////
+
+type Github struct {
+	Projects  []string `json:"projects"`
+	UserNames []string `json:"usernames"`
+}
