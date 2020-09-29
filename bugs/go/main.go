e2478c3e7fa8067ecf48c7fbf5fcc22a0b4e8d71
Ravi Mistry <rmistry@google.com>
2020-09-29 14:57:26 -0400

    Checkpoint
    Change-Id: I70e67ee9b534c1a119c7d7b43effd5f82b29d4d4

---
+++ bugs/go/main.go
@@ -0,0 +1,199 @@
+/*
+	Leasing Server for Swarming Bots.
+*/
+
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"html/template"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+
+	"cloud.google.com/go/storage"
+	"github.com/gorilla/mux"
+	"github.com/unrolled/secure"
+	"google.golang.org/api/option"
+
+	"go.skia.org/infra/bugs/go/bug_framework"
+	"go.skia.org/infra/bugs/go/mail"
+	"go.skia.org/infra/go/allowed"
+	"go.skia.org/infra/go/auth"
+	"go.skia.org/infra/go/baseapp"
+	"go.skia.org/infra/go/cleanup"
+	"go.skia.org/infra/go/httputils"
+	"go.skia.org/infra/go/login"
+	"go.skia.org/infra/go/sklog"
+	"go.skia.org/infra/go/util"
+)
+
+const ()
+
+var (
+	// Flags
+	host                  = flag.String("host", "bugs-central.skia.org", "HTTP service host")
+	workdir               = flag.String("workdir", ".", "Directory to use for scratch work.")
+	pollInterval          = flag.Duration("poll_interval", 1*time.Minute, "How often the server will poll the different issue frameworks.")
+	emailClientSecretFile = flag.String("email_client_secret_file", "/etc/bugs-central-email-secrets/client_secret.json", "OAuth client secret JSON file for sending email.")
+	emailTokenCacheFile   = flag.String("email_token_cache_file", "/etc/bugs-central-email-secrets/client_token.json", "OAuth token cache file for sending email.")
+	serviceAccountFile    = flag.String("service_account_file", "/var/secrets/google/key.json", "Service account JSON file.")
+
+	// OAUTH params
+	authAllowList = flag.String("auth_allowlist", "google.com", "White space separated list of domains and email addresses that are allowed to login.")
+)
+
+type ClientConfig struct {
+	ClientID     string `json:"client_id"`
+	ClientSecret string `json:"client_secret"`
+}
+
+type ClientSecretJSON struct {
+	Installed ClientConfig `json:"installed"`
+}
+
+// See baseapp.Constructor.
+func New() (baseapp.App, error) {
+	// Create workdir if it does not exist.
+	if err := os.MkdirAll(*workdir, 0755); err != nil {
+		sklog.Fatalf("Could not create %s: %s", *workdir, err)
+	}
+
+	// Initialize mailing library.
+	var cfg ClientSecretJSON
+	err := util.WithReadFile(*emailClientSecretFile, func(f io.Reader) error {
+		return json.NewDecoder(f).Decode(&cfg)
+	})
+	if err != nil {
+		sklog.Fatalf("Failed to read client secrets from %q: %s", *emailClientSecretFile, err)
+	}
+	// Create a copy of the token cache file since mounted secrets are read-only
+	// and the access token will need to be updated for the oauth2 flow.
+	if !*baseapp.Local {
+		fout, err := ioutil.TempFile("", "")
+		if err != nil {
+			sklog.Fatalf("Unable to create temp file %q: %s", fout.Name(), err)
+		}
+		err = util.WithReadFile(*emailTokenCacheFile, func(fin io.Reader) error {
+			_, err := io.Copy(fout, fin)
+			if err != nil {
+				err = fout.Close()
+			}
+			return err
+		})
+		if err != nil {
+			sklog.Fatalf("Failed to write token cache file from %q to %q: %s", *emailTokenCacheFile, fout.Name(), err)
+		}
+		*emailTokenCacheFile = fout.Name()
+	}
+	if err := mail.MailInit(cfg.Installed.ClientID, cfg.Installed.ClientSecret, *emailTokenCacheFile); err != nil {
+		sklog.Fatalf("Failed to init mail library: %s", err)
+	}
+
+	var allow allowed.Allow
+	if !*baseapp.Local {
+		allow = allowed.NewAllowedFromList([]string{*authAllowList})
+	} else {
+		allow = allowed.NewAllowedFromList([]string{"fred@example.org", "barney@example.org", "wilma@example.org"})
+	}
+	login.SimpleInitWithAllow(*baseapp.Port, *baseapp.Local, nil, nil, allow)
+
+	// HERE HER
+	// Initialize bug frameworks.
+	ctx := context.Background()
+	ts, err := auth.NewDefaultTokenSource(*baseapp.Local, auth.SCOPE_USERINFO_EMAIL, auth.SCOPE_FULL_CONTROL)
+	httpClient := httputils.DefaultClientConfig().WithTokenSource(ts).With2xxOnly().Client()
+	storageClient, err := storage.NewClient(ctx, option.WithHTTPClient(httpClient))
+	if err != nil {
+		sklog.Fatalf("Failed to init storage client: %s", err)
+	}
+	issueTracker, err := bug_framework.InitIssueTracker(storageClient)
+	if err != nil {
+		sklog.Fatalf("Failed to init issuetracker: %s", err)
+	}
+	fmt.Println(issueTracker)
+
+	cleanup.Repeat(*pollInterval, func(_ context.Context) {
+		// This is where the polling happens!
+		// Ignore he passed-in context; this allows us to continue runing even if the
+		// context is canceled due to transient errors.
+		ctx := context.Background()
+		issues, err := issueTracker.Search(ctx)
+		if err != nil {
+			sklog.Errorf("Error when polling issuetracker: %s", err)
+			return
+		}
+		fmt.Printf("\n\nGOT THESE ISSUES: %+v\n\n", issues)
+	}, nil)
+	srv := &Server{}
+	srv.loadTemplates()
+
+	return srv, nil
+}
+
+// Server is the state of the server.
+type Server struct {
+	templates *template.Template
+}
+
+func (srv *Server) loadTemplates() {
+	// srv.templates = template.Must(template.New("").Delims("{%", "%}").ParseFiles(
+	// 	filepath.Join(*baseapp.ResourcesDir, "index.html"),
+	// ))
+}
+
+// user returns the currently logged in user, or a placeholder if running locally.
+func (srv *Server) user(r *http.Request) string {
+	user := "barney@example.org"
+	if !*baseapp.Local {
+		user = login.LoggedInAs(r)
+	}
+	return user
+}
+
+// See baseapp.App.
+func (srv *Server) AddHandlers(r *mux.Router) {
+	// For login/logout.
+	r.HandleFunc(login.DEFAULT_OAUTH2_CALLBACK, login.OAuth2CallbackHandler)
+	r.HandleFunc("/logout/", login.LogoutHandler)
+	r.HandleFunc("/loginstatus/", login.StatusHandler)
+
+	// All endpoints that require authentication should be added to this router.
+	appRouter := mux.NewRouter()
+	appRouter.HandleFunc("/", srv.indexHandler)
+
+	// Use the appRouter as a handler and wrap it into middleware that enforces authentication.
+	appHandler := http.Handler(appRouter)
+	if !*baseapp.Local {
+		appHandler = login.ForceAuth(appRouter, login.DEFAULT_REDIRECT_URL)
+	}
+
+	r.PathPrefix("/").Handler(appHandler)
+}
+
+func (srv *Server) indexHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+
+	if err := srv.templates.ExecuteTemplate(w, "index.html", map[string]string{
+		// Look in webpack.config.js for where the nonce templates are injected.
+		"Nonce": secure.CSPNonce(r.Context()),
+	}); err != nil {
+		httputils.ReportError(w, err, "Failed to expand template.", http.StatusInternalServerError)
+		return
+	}
+	return
+}
+
+// See baseapp.App.
+func (srv *Server) AddMiddleware() []mux.MiddlewareFunc {
+	return []mux.MiddlewareFunc{}
+}
+
+func main() {
+	baseapp.Serve(New, []string{*host})
+}
