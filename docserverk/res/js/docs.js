34be000d21d2791466a3da12998af43c6298b6f2
Joe Gregorio <jcgregorio@google.com>
2020-09-30 08:41:04 -0400

    [docserver] Remove all vestiges of Polymer.
    Also simplify by using prettify directly from a CDN.
    
    Change-Id: If1a36cc2d289ca7a7033f00e31c458db9bd7d1bf

--- docserverk/res/js/docs.js
+++ docserverk/res/js/docs.js
@@ -1,40 +1,39 @@
-sk.DomReady.then(function() {
-  prettyPrint();
-
+// eslint-disable-next-line wrap-iife
+(() => {
   // Open the side drawer with the navigation menu.
-  $$$('button').addEventListener('click', function(e) {
-    $$$('#drawer').classList.toggle('opened');
+  document.querySelector('#menu').addEventListener('click', (e) => {
+    document.querySelector('#drawer').classList.toggle('opened');
     e.stopPropagation();
   });
 
   // Close the side drawer.
-  $$$('body').addEventListener('click', function() {
-    $$$('#drawer').classList.remove('opened');
+  document.querySelector('body').addEventListener('click', () => {
+    document.querySelector('#drawer').classList.remove('opened');
   });
 
   // highlightNav highlights where we are in the navigation.
-  var highlightNav = function() {
-    $$('#drawer li a').forEach(function(e) {
-      if (e.dataset.path == window.location.pathname) {
+  const highlightNav = () => {
+    document.querySelectorAll('#drawer li a').forEach((e) => {
+      if (e.dataset.path === window.location.pathname) {
         e.classList.add('selected');
-        $$$('title').innerText = e.innerText;
+        document.querySelector('title').innerText = e.innerText;
       } else {
         e.classList.remove('selected');
       }
     });
-  }
+  };
 
   // Shortcut the links and handle them via XHR, that way we only
   // pay the loading time once, yet still retain full URLs.
-  $$('#drawer li a').forEach(function(e) {
-    e.addEventListener('click', function(e) {
+  document.querySelectorAll('#drawer li a').forEach((link) => {
+    link.addEventListener('click', (e) => {
       // Preserve query parameters as we navigate.
-      var q = window.location.search;
-      var url = e.target.dataset.path;
-      if (q != "") {
+      const q = window.location.search;
+      let url = e.target.dataset.path;
+      if (q !== '') {
         url += q;
       }
-      sk.get('/_'+url).then(function(content) {
+      fetch(`/_${url}`).then((content) => {
         if (content.indexOf('<script') !== -1) {
           // We can't render <script> using innerHTML, so we just go
           // directly to the page.
@@ -43,9 +42,8 @@ sk.DomReady.then(function() {
         } else {
           window.history.pushState(null, null, url);
           highlightNav();
-          $$$('main').innerHTML = content;
-          window.scrollTo(0,0);
-          prettyPrint();
+          document.querySelector('main').innerHTML = content;
+          window.scrollTo(0, 0);
         }
       });
       e.preventDefault();
@@ -53,15 +51,17 @@ sk.DomReady.then(function() {
   });
 
   highlightNav();
-});
+})();
 
-(function() {
-  var cx = '009791159600898516779:8-nlv0iznho';
-  var gcse = document.createElement('script');
+// eslint-disable-next-line wrap-iife
+(() => {
+  const cx = '009791159600898516779:8-nlv0iznho';
+  const gcse = document.createElement('script');
   gcse.type = 'text/javascript';
   gcse.async = true;
-  gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
-    '//cse.google.com/cse.js?cx=' + cx;
-  var s = document.getElementsByTagName('script')[0];
+  gcse.src = `${
+    document.location.protocol === 'https:' ? 'https:' : 'http:'
+  }//cse.google.com/cse.js?cx=${cx}`;
+  const s = document.getElementsByTagName('script')[0];
   s.parentNode.insertBefore(gcse, s);
 })();
