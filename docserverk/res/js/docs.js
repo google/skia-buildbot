c850be6615a2ea64fb2d2c2ec6c8f1cb9eb990fa
Joe Gregorio <jcgregorio@google.com>
2020-09-30 09:07:01 -0400

    [docserver] Remove all vestiges of Polymer.
    Also:
     - Simplify by using prettify directly from a CDN.
     - Don't bother prefixing the CSS, we are only doing simple stuff.
     - Don't include any res/common JS.
     - Don't bother minifying the tiny amount of JS remaining.
    
    BYPASS_INCLUSIVE_LANGUAGE_REASON=Can't control external URLs.
    Change-Id: If1a36cc2d289ca7a7033f00e31c458db9bd7d1bf

--- docserverk/res/js/docs.js
+++ docserverk/res/js/docs.js
@@ -1,67 +1,75 @@
-sk.DomReady.then(function() {
-  prettyPrint();
-
+(() => {
   // Open the side drawer with the navigation menu.
-  $$$('button').addEventListener('click', function(e) {
-    $$$('#drawer').classList.toggle('opened');
+  document.querySelector('#menu').addEventListener('click', (e) => {
+    document.querySelector('#drawer').classList.toggle('opened');
     e.stopPropagation();
   });
 
   // Close the side drawer.
-  $$$('body').addEventListener('click', function() {
-    $$$('#drawer').classList.remove('opened');
+  document.querySelector('body').addEventListener('click', () => {
+    document.querySelector('#drawer').classList.remove('opened');
   });
 
   // highlightNav highlights where we are in the navigation.
-  var highlightNav = function() {
-    $$('#drawer li a').forEach(function(e) {
-      if (e.dataset.path == window.location.pathname) {
+  const highlightNav = () => {
+    // PR is the Preffify global namespace.
+    if (PR && PR.prettyPrint) {
+      PR.prettyPrint();
+    }
+    document.querySelectorAll('#drawer li a').forEach((e) => {
+      if (e.dataset.path === window.location.pathname) {
         e.classList.add('selected');
-        $$$('title').innerText = e.innerText;
+        document.querySelector('title').innerText = e.innerText;
       } else {
         e.classList.remove('selected');
       }
     });
-  }
+  };
 
   // Shortcut the links and handle them via XHR, that way we only
   // pay the loading time once, yet still retain full URLs.
-  $$('#drawer li a').forEach(function(e) {
-    e.addEventListener('click', function(e) {
+  document.querySelectorAll('#drawer li a').forEach((link) => {
+    link.addEventListener('click', (e) => {
       // Preserve query parameters as we navigate.
-      var q = window.location.search;
-      var url = e.target.dataset.path;
-      if (q != "") {
+      const q = window.location.search;
+      let url = e.target.dataset.path;
+      if (q !== '') {
         url += q;
       }
-      sk.get('/_'+url).then(function(content) {
-        if (content.indexOf('<script') !== -1) {
-          // We can't render <script> using innerHTML, so we just go
-          // directly to the page.
-          // https://developer.mozilla.org/en-US/docs/Web/API/Element/innerHTML#Security_considerations
-          window.location.href = url;
-        } else {
-          window.history.pushState(null, null, url);
-          highlightNav();
-          $$$('main').innerHTML = content;
-          window.scrollTo(0,0);
-          prettyPrint();
-        }
-      });
+      fetch(`/_${url}`)
+        .then((resp) => {
+          if (resp.ok) {
+            return resp.text();
+          }
+          throw new Error('Failed to load.');
+        })
+        .then((content) => {
+          if (content.indexOf('<script') !== -1) {
+            // We can't render <script> using innerHTML, so we just go
+            // directly to the page.
+            // https://developer.mozilla.org/en-US/docs/Web/API/Element/innerHTML#Security_considerations
+            window.location.href = url;
+          } else {
+            window.history.pushState(null, null, url);
+            highlightNav();
+            document.querySelector('main').innerHTML = content;
+            window.scrollTo(0, 0);
+          }
+        });
       e.preventDefault();
     });
   });
 
   highlightNav();
-});
 
-(function() {
-  var cx = '009791159600898516779:8-nlv0iznho';
-  var gcse = document.createElement('script');
+  // Turn on search.
+  const cx = '009791159600898516779:8-nlv0iznho';
+  const gcse = document.createElement('script');
   gcse.type = 'text/javascript';
   gcse.async = true;
-  gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
-    '//cse.google.com/cse.js?cx=' + cx;
-  var s = document.getElementsByTagName('script')[0];
+  gcse.src = `${
+    document.location.protocol === 'https:' ? 'https:' : 'http:'
+  }//cse.google.com/cse.js?cx=${cx}`;
+  const s = document.getElementsByTagName('script')[0];
   s.parentNode.insertBefore(gcse, s);
 })();
