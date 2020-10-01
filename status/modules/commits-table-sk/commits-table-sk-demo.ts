1413458f2a5c83b2328c9b7c109b69927a86329f
Weston Tracey <westont@google.com>
2020-10-01 12:19:17 -0400

    [status] Add dialog-sk element.
    This adds a dialog element for use on the tasks table. It is a single form that
    displays details of either a task, taskspec, or commit.
    
    Change-Id: If20a021e975988abce3e96c41046afb9dc607036

--- status/modules/commits-table-sk/commits-table-sk-demo.ts
+++ status/modules/commits-table-sk/commits-table-sk-demo.ts
@@ -3,8 +3,15 @@ import '../../../infra-sk/modules/theme-chooser-sk';
 import '../commits-data-sk';
 import { $$ } from 'common-sk/modules/dom';
 import { mockIncrementalResponse, SetupMocks } from '../rpc-mock';
+import { SetTestSettings } from '../settings';
 
 SetupMocks().expectGetIncrementalCommits(mockIncrementalResponse);
+SetTestSettings({
+  swarmingUrl: 'example.com/swarming',
+  taskSchedulerUrl: 'example.com/ts',
+  defaultRepo: 'skia',
+  repos: new Map([['skia', 'https://skia.googlesource.com/skia/+show/']]),
+});
 
 const data = document.createElement('commits-data-sk');
 ($$('#container') as HTMLElement).appendChild(data);
