f5a068246e295a381e40298a7698af6d03599545
Eric Boren <borenet@google.com>
2020-10-01 07:33:31 -0400

    [autoroll] Add Tint -> Dawn roller
    Bug: skia:10789
    Change-Id: I68f71ba14a3e5ce1128ec2a5b7c50e56403b067a

---
+++ autoroll/bin/create-autoroll-be-dawn-sa.sh
@@ -0,0 +1,26 @@
+#/bin/bash
+
+# Creates the service account used by AutoRoll Backend, and export a key for it
+# into the kubernetes cluster as a secret.
+
+set -e -x
+source ../kube/config.sh
+source ../bash/ramdisk.sh
+
+# New service account we will create.
+SA_NAME="dawn-autoroll"
+SA_EMAIL="${SA_NAME}@${PROJECT_SUBDOMAIN}.iam.gserviceaccount.com"
+
+cd /tmp/ramdisk
+
+gcloud --project=${PROJECT_ID} iam service-accounts create "${SA_NAME}" \
+    --display-name="Service account for AutoRolls into Dawn"
+gcloud projects add-iam-policy-binding google.com:skia-buildbots \
+    --member serviceAccount:${SA_EMAIL} --role roles/datastore.user
+gsutil iam ch serviceAccount:${SA_EMAIL}:objectAdmin gs://skia-autoroll
+gcloud beta iam service-accounts keys create ${SA_NAME}.json \
+    --iam-account="${SA_EMAIL}"
+
+kubectl create secret generic "${SA_NAME}" --from-file=key.json=${SA_NAME}.json
+
+cd -
