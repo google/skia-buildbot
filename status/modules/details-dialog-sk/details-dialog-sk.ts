fe19e47ec2fbbf024f5b4ab75be8fc32e6e63f95
Weston Tracey <westont@google.com>
2020-10-01 12:20:57 -0400

    [status] Add dialog-sk element.
    This adds a dialog element for use on the tasks table.
    It is a single element of the form that is essentially:
    
    {{TitleSection}} {{action button}}
    ---
    {{DetailSection}}
    ---
    {{comments-sk}}
    
    These sections are filled in based on task, taskspec, or commit data.
    
    In additional to the element there is some minor cleanup:
    --make comments-sk more robust for use in parent elements
    --refactor task-driver-sk test data for use in details-dialog-sk tests
    --add settings injection missing from commits-table-sk-demo
    
    Change-Id: If20a021e975988abce3e96c41046afb9dc607036

---
+++ status/modules/details-dialog-sk/details-dialog-sk.ts
@@ -0,0 +1,299 @@
+/**
+ * @module modules/details-dialog-sk
+ * @description <h2><code>details-dialog-sk</code></h2>
+ *
+ * @property repo {string} - The repo associated with the tasks/taskspecs/commits that will be
+ * displayed.
+ */
+import { define } from 'elements-sk/define';
+import { errorMessage } from 'elements-sk/errorMessage';
+import { html, TemplateResult } from 'lit-html';
+import { until } from 'lit-html/directives/until.js';
+import { ElementSk } from '../../../infra-sk/modules/ElementSk';
+import { Login } from '../../../infra-sk/modules/login';
+import { escapeAndLinkify } from '../../../infra-sk/modules/linkify';
+import { jsonOrThrow } from 'common-sk/modules/jsonOrThrow';
+import { Commit } from '../commits-data-sk/commits-data-sk';
+import { Task, Comment } from '../rpc';
+import { CommentData } from '../comments-sk/comments-sk';
+import { revisionUrlTemplate, swarmingUrl, taskSchedulerUrl } from '../settings';
+
+import '../comments-sk';
+import 'elements-sk/styles/buttons';
+import 'elements-sk/icon/launch-icon-sk';
+import '../../../infra-sk/modules/task-driver-sk';
+
+// Type defining the text and action of the upper-right button of the dialog.
+// For reverts of commits and re-running of tasks.
+interface Action {
+  buttonText: string;
+  handler: () => void;
+}
+
+export class DetailsDialogSk extends ElementSk {
+  // This template is essentially a title section with optional action button, an optional details
+  // section, and a comments-sk section. Task, TaskSpec, and Commits set the appropriate sections
+  // before rendering.
+  private static template = (el: DetailsDialogSk) =>
+    html`
+      <div class="dialog">
+        <div class="horizontal">
+          <div class="flex">${el.titleSection}</div>
+          ${el.actionButton
+            ? html`<button class="action" @click=${el.actionButton.handler}>
+                ${el.actionButton.buttonText}
+              </button>`
+            : html``}
+        </div>
+        <br />
+        <hr />
+        ${el.detailsSection
+          ? [
+              el.detailsSection,
+              html`
+                <br />
+                <hr />
+              `,
+            ]
+          : html``}
+        <div>
+          <comments-sk
+            .commentData=${el.commentData}
+            .allowAdd=${true}
+            .allowDelete=${true}
+            .showIgnoreFailure=${el.showCommentsIgnoreFailure}
+            .showFlaky=${el.showCommentsFlaky}
+            .editRights=${el.canEditComments}
+          ></comments-sk>
+        </div>
+      </div>
+    `;
+
+  private titleSection: TemplateResult = html``;
+  private detailsSection: TemplateResult | null = null;
+  private actionButton: Action | null = null;
+  private showCommentsIgnoreFailure: boolean = false;
+  private showCommentsFlaky: boolean = false;
+  private canEditComments = false;
+  private _repo: string = '';
+  private commentData?: CommentData;
+
+  constructor() {
+    super(DetailsDialogSk.template);
+    this._upgradeProperty('repo');
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    Login.then((res: any) => {
+      this.canEditComments = res.Email !== '';
+      this._render();
+    });
+    this._render();
+  }
+
+  private open() {
+    this._render();
+    (<HTMLElement>this).style.display = 'block';
+  }
+
+  close() {
+    (<HTMLElement>this).style.display = 'none';
+  }
+
+  // reset clears the computed templates to ensure we don't have any leftovers if switching from
+  // e.g.a task view to a taskspec view.
+  private reset() {
+    this.actionButton = null;
+    this.titleSection = html``;
+    this.detailsSection = null;
+    this.showCommentsFlaky = false;
+    this.showCommentsIgnoreFailure = false;
+  }
+
+  displayTask(task: Task, comments: Array<Comment>, commitsByHash: Map<string, Commit>) {
+    this.reset();
+    this.commentData = {
+      comments: comments,
+      taskId: task.id,
+      commit: '',
+      taskSpec: '',
+      repo: this.repo,
+    };
+    const td = fetch(`/json/td/${task.id}`)
+      .then(jsonOrThrow)
+      .then((td) => {
+        return html`<task-driver-sk id="tdStatus" .data=${td} embedded></task-driver-sk>`;
+      });
+    // We don't catch failures, since we don't want the promise to resolve (and be used below)
+    // unless the task-driver-sk has data.
+    this.titleSection = html`${until(
+      td,
+      html`
+        <h3>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="${swarmingUrl()}/task?id=${task.swarmingTaskId}"
+          >
+            <span>${task.name}</span><launch-icon-sk></launch-icon-sk>
+          </a>
+        </h3>
+        <div>
+          <table>
+            <tr>
+              <td>Status:</td>
+              <td class=${`task-${(task.status || 'PENDING').toLowerCase()}`}>${task.status}</td>
+            </tr>
+            <tr>
+              <td>Context:</td>
+              <td>
+                <a href=${this.taskUrl(task)} target="_blank" rel="noopener noreferrer">
+                  View on Task Scheduler
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <td>Other Tasks Like This:</td>
+              <td>
+                <a target="_blank" rel="noopener noreferrer" href=${this.swarmingUrl(task.name)}>
+                  View on Swarming
+                </a>
+              </td>
+            </tr>
+          </table>
+        </div>
+      `
+    )}`;
+
+    this.detailsSection = html`
+      <h3>Blamelist</h3>
+      <table class="blamelist">
+        ${task.commits?.map((hash: string) => {
+          const commit = commitsByHash.get(hash);
+          return html`
+            <tr>
+              <td>
+                <a href="${revisionUrlTemplate(this.repo)}${hash}}">${commit?.shortHash || ''}</a>
+              </td>
+              <td>${commit?.shortAuthor || ''}</td>
+              <td>${commit?.shortSubject || ''}</td>
+            </tr>
+          `;
+        })}
+      </table>
+    `;
+
+    this.actionButton = { buttonText: 'Re-run Job', handler: () => this.rerunJob(task) };
+    this.open();
+  }
+
+  displayTaskSpec(taskspec: string, comments: Comment[]) {
+    this.reset();
+    this.showCommentsFlaky = true;
+    this.showCommentsIgnoreFailure = true;
+    this.commentData = {
+      comments: comments,
+      taskId: '',
+      commit: '',
+      taskSpec: taskspec,
+      repo: this.repo,
+    };
+    this.titleSection = html` <h3>
+      <a href="${this.swarmingUrl(taskspec)}" target="_blank" rel="noopener noreferrer">
+        ${taskspec}
+      </a>
+    </h3>`;
+    this.open();
+  }
+
+  displayCommit(commit: Commit, comments: Array<Comment>) {
+    this.reset();
+    this.showCommentsIgnoreFailure = true;
+    this.commentData = {
+      comments: comments,
+      taskId: '',
+      commit: commit.hash,
+      taskSpec: '',
+      repo: this.repo,
+    };
+    this.titleSection = html`
+      <p>
+        <a
+          href="${revisionUrlTemplate(this.repo)}${commit.hash}"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          ${commit.hash}
+        </a>
+        <br />
+        ${commit.author}
+        <br />
+        <span title="${commit.timestamp!}">${this.humanDate(commit.timestamp!)}</span>
+      </p>
+    `;
+
+    this.detailsSection = html`
+      <h3>${escapeAndLinkify(commit.subject)}</h3>
+      <p>${escapeAndLinkify(commit.body)}</p>
+    `;
+    if (commit.issue) {
+      this.actionButton = { buttonText: 'Revert', handler: () => this.revertCommit(commit) };
+    }
+    this.open();
+  }
+
+  private rerunJob(task: Task) {
+    if (!task || !task.name || !task.revision) {
+      errorMessage("Invalid task, can't be re-run");
+    }
+    // TODO(borenet): This is not correct in some cases. Now that we have a
+    // link from Task to Job in the scheduler, we should be able to come up
+    // with a better way to "re-open" a failed Job, essentially resetting
+    // the attempt counts so that we can retry the failed task(s).
+    let job = task.name;
+    const uploadPrefix = 'Upload-';
+    if (job.indexOf(uploadPrefix) == 0) {
+      job = job.substring(uploadPrefix.length);
+    }
+    fetch(`${taskSchedulerUrl()}/trigger?submit=true&job=${job}@${task.revision}`).catch(
+      errorMessage
+    );
+  }
+
+  private revertCommit(commit: Commit) {
+    const url =
+      commit.patchStorage === 'gerrit'
+        ? `https://skia-review.googlesource.com/c/${commit.issue}/?revert`
+        : `https://codereview.chromium.org/${commit.issue}/revert`;
+    const win = window.open(url, '_blank') as Window;
+    win.focus();
+  }
+
+  private taskUrl(task: Task) {
+    const url = taskSchedulerUrl();
+    if (!task || !task.id || !url) {
+      return '';
+    }
+    return `${url}/task/${task.id}`;
+  }
+
+  private swarmingUrl(taskSpec: string) {
+    return `${swarmingUrl()}/tasklist?f=sk_name%3A${taskSpec}`;
+  }
+
+  private humanDate(timestamp: string) {
+    const date = new Date(timestamp);
+    const str = date.toString();
+    return `${date.toLocaleString()} ${str.substring(str.indexOf('('))}`;
+  }
+
+  get repo(): string {
+    return this._repo;
+  }
+  set repo(value) {
+    this._repo = value;
+  }
+}
+
+define('details-dialog-sk', DetailsDialogSk);
