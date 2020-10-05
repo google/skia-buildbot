f3e0d68ddbf96247cf980b4c8b517ce72c0ab162
Joe Gregorio <jcgregorio@google.com>
2020-10-05 14:14:33 -0400

    [perf] perf-try
    Change-Id: Ic2e1b813a1245dcd076f59d58b3b293445caf0bb

---
+++ perf/go/perf-try/main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"math"
+	"os"
+	"sort"
+
+	"go.skia.org/infra/go/paramtools"
+	"go.skia.org/infra/go/query"
+	"go.skia.org/infra/go/util"
+	"go.skia.org/infra/go/vec32"
+	"go.skia.org/infra/perf/go/types"
+)
+
+type sample struct {
+	traceID string
+	params  paramtools.Params
+	values  []float32
+}
+
+type total struct {
+	keyValue string
+	total    float32
+}
+
+type totalSlice []total
+
+func (p totalSlice) Len() int           { return len(p) }
+func (p totalSlice) Less(i, j int) bool { return p[i].total > p[j].total }
+func (p totalSlice) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+
+func main() {
+	// Open and read TraceSet
+	var ts types.TraceSet
+
+	err := util.WithReadFile(os.Args[1], func(r io.Reader) error {
+		return json.NewDecoder(r).Decode(&ts)
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ps := paramtools.ParamSet{}
+	samples := make([]sample, 0, len(ts))
+	for traceID, values := range ts {
+		params, err := query.ParseKey(traceID)
+		if err != nil {
+			log.Fatal(err)
+		}
+		ps.AddParams(params)
+		samples = append(samples, sample{
+			traceID: traceID,
+			params:  params,
+			values:  values,
+		})
+	}
+
+	// Do analysis by key=value
+
+	totals := map[string]float32{}
+	for _, s := range samples {
+		tryValue := s.values[0]
+		currentValue := s.values[1]
+		if tryValue == vec32.MissingDataSentinel || currentValue == vec32.MissingDataSentinel {
+			continue
+		}
+		percentChange := (tryValue - currentValue) / currentValue
+		if math.IsNaN(float64(percentChange)) {
+			continue
+		}
+		for key, value := range s.params {
+			keyValue := key + "=" + value
+			totals[keyValue] += percentChange
+		}
+	}
+
+	sortableTotals := []total{}
+
+	for keyValue, value := range totals {
+		sortableTotals = append(sortableTotals, total{
+			keyValue: keyValue,
+			total:    value,
+		})
+	}
+
+	sort.Sort(totalSlice(sortableTotals))
+
+	for i, top := range sortableTotals[:5] {
+		fmt.Printf("%4d %40s %g\n", i+1, top.keyValue, top.total)
+	}
+	fmt.Println("...")
+	for i, top := range sortableTotals[len(sortableTotals)-5:] {
+		fmt.Printf("%4d %40s %g\n", len(sortableTotals)-5+i+1, top.keyValue, top.total)
+	}
+
+}
---
+++ perf/try.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# SKP update with big change occurs at 51050.
+
+rm $HOME/traces.json
+
+#perf-tool traces export --config_filename=./configs/cdb-nano.json --connection_string=postgresql://root@localhost:26257/skia?sslmode=disable --begin=51049 --end=51050 --filename=$HOME/traces.json --query=source_type=skp&sub_result=min_ms
+
+# No alerts triggered on the previous two commits.
+perf-tool traces export --config_filename=./configs/cdb-nano.json --connection_string=postgresql://root@localhost:26257/skia?sslmode=disable --begin=51043 --end=51044 --filename=$HOME/traces.json --query='source_type=skp&sub_result=min_ms'
+
+perf-try $HOME/traces.json
