99992bcf37542afad4f15940ce29f810bb3b54af
Joe Gregorio <jcgregorio@google.com>
2020-10-06 10:51:29 -0400

    [perf] perf-try
    Change-Id: Ic2e1b813a1245dcd076f59d58b3b293445caf0bb

---
+++ perf/try.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#rm $HOME/regression-traces.json
+#rm $HOME/no-regression-traces.json
+
+# SKP update with big change occurs at 51050.
+#perf-tool traces export --config_filename=./configs/cdb-nano.json --connection_string=postgresql://root@localhost:26257/#skia?sslmode=disable --begin=51049 --end=51050 --filename=$HOME/regression-traces.json --query='sub_result=min_ms'
+
+# Subtle change in skp at 50867
+#perf-tool traces export --config_filename=./configs/cdb-nano.json --connection_string=postgresql://root@localhost:26257/skia?sslmode=disable --begin=50866 --end=50867 --filename=$HOME/regression-traces.json --query='sub_result=min_ms&source_type=skp'
+
+# Medium regression at 50654
+perf-tool traces export --config_filename=./configs/cdb-nano.json --connection_string=postgresql://root@localhost:26257/skia?sslmode=disable --begin=50653 --end=50654 --filename=$HOME/regression-traces.json --query='sub_result=min_ms&source_type=skp'
+
+# No alerts triggered on the previous two commits.
+#perf-tool traces export --config_filename=./configs/cdb-nano.json --connection_string=postgresql://root@localhost:26257/skia?sslmode=disable --begin=51043 --end=51044 --filename=$HOME/no-regression-traces.json --query='sub_result=min_ms&source_type=skp'
+
+echo "Regression in SKPs"
+perf-try $HOME/regression-traces.json
+
+#echo "No Regressions"
+#perf-try $HOME/no-regression-traces.json
