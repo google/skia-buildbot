9693b3126f215e872b5ba11d08bb59ac551dcc6e
Ravi Mistry <rmistry@google.com>
2020-10-06 09:37:52 -0400

    am protoypt
    Change-Id: I496126d665fb91bd33c1a6e3c79938b863d9df56

--- am/modules/alert-manager-sk/alert-manager-sk.js
+++ am/modules/alert-manager-sk/alert-manager-sk.js
@@ -19,6 +19,7 @@ import 'elements-sk/tabs-sk';
 import 'elements-sk/toast-sk';
 
 import '../incident-sk';
+import '../bot-chooser-sk';
 import '../email-chooser-sk';
 import '../silence-sk';
 
@@ -138,6 +139,7 @@ function assignedTo(incident, ele) {
 }
 
 function botCentricView(ele, incidents) {
+    /*
     // Reset bots_to_incidents and populate it from scratch.
     ele._bots_to_incidents = {};
     for (let i = 0; i < incidents.length; i++) {
@@ -156,6 +158,7 @@ function botCentricView(ele, incidents) {
         }
       }
     }
+    */
 
     const botsHTML = [];
     for (const botName in ele._bots_to_incidents) {
@@ -290,6 +293,7 @@ const template = (ele) => html`
 </section>
 <footer>
   <spinner-sk id=busy></spinner-sk>
+  <bot-chooser-sk id=bot-chooser></bot-chooser-sk>
   <email-chooser-sk id=chooser></email-chooser-sk>
   <error-toast-sk></error-toast-sk>
 <footer>
@@ -355,6 +359,7 @@ define('alert-manager-sk', class extends HTMLElement {
     this.addEventListener('add-note', (e) => this._addNote(e));
     this.addEventListener('del-note', (e) => this._delNote(e));
     this.addEventListener('take', (e) => this._take(e));
+    this.addEventListener('bot-chooser', (e) => this._botChooser(e));
     this.addEventListener('assign', (e) => this._assign(e));
     this.addEventListener('assign-to-owner', (e) => this._assignToOwner(e));
 
@@ -379,6 +384,26 @@ define('alert-manager-sk', class extends HTMLElement {
       credentials: 'include',
     }).then(jsonOrThrow).then((json) => {
       this._incidents = json.incidents;
+
+      // Reset bots_to_incidents and populate it from scratch.
+      this._bots_to_incidents = {};
+      for (let i = 0; i < this._incidents.length; i++) {
+        const incident = this._incidents[i];
+        if (incident.params && incident.params['bot']) {
+          // Only consider active bot incidents that are not assigned or silenced.
+          if (!incident.active || incident.params.__silence_state === 'silenced'
+                  || incident.params.assigned_to) {
+            continue;
+          }
+          const botName = incident.params['bot'];
+          if(this._bots_to_incidents[botName]) {
+            this._bots_to_incidents[botName].push(incident);
+          } else {
+            this._bots_to_incidents[botName] = [incident];
+          }
+        }
+      }
+
       // If alert_id is specified and it is in supported rhs_states then display
       // an incident.
       if ((this._rhs_state == START || this._rhs_state == INCIDENT) &&
@@ -658,6 +683,13 @@ define('alert-manager-sk', class extends HTMLElement {
 
   _delSilenceNote(e) {
     this._doImpl('/_/del_silence_note', e.detail, (json) => this._silenceAction(json, false));
+  }
+
+  _botChooser(e) {
+    $$('#bot-chooser', this).open(this._bots_to_incidents).then((bot) => {
+      this._current_silence.param_set['bot'] = [`${this._current_silence.param_set['bot'].join('|')}|${bot}`];
+      this._modifySilenceParam(this._current_silence);
+    });
   }
 
   _assign(e) {
