3158635e0ff6b553057111486116b1394cc233f9
Ravi Mistry <rmistry@google.com>
2020-10-09 13:21:29 -0400

    Checkpoint
    Change-Id: I70e67ee9b534c1a119c7d7b43effd5f82b29d4d4

---
+++ bugs-central/go/bugs/poller.go
@@ -0,0 +1,175 @@
+package bugs
+
+// Initializes and polls the different support issue frameworks
+
+import (
+	"context"
+	"fmt"
+	"os/user"
+	"path/filepath"
+	"time"
+
+	"cloud.google.com/go/storage"
+	"golang.org/x/oauth2"
+	"google.golang.org/api/option"
+
+	"go.skia.org/infra/bugs-central/go/db"
+	"go.skia.org/infra/go/baseapp"
+	"go.skia.org/infra/go/cleanup"
+	"go.skia.org/infra/go/github"
+	"go.skia.org/infra/go/httputils"
+	"go.skia.org/infra/go/skerr"
+	"go.skia.org/infra/go/sklog"
+)
+
+type IssuesPoller struct {
+	issueTrackerClient BugFramework
+	githubClient       BugFramework
+	monorailClient     BugFramework
+
+	dbClient *db.FirestoreDB
+}
+
+func InitPoller(ctx context.Context, ts oauth2.TokenSource, serviceAccountFile string) (*IssuesPoller, error) {
+	httpClient := httputils.DefaultClientConfig().WithTokenSource(ts).With2xxOnly().Client()
+	storageClient, err := storage.NewClient(ctx, option.WithHTTPClient(httpClient))
+	if err != nil {
+		return nil, skerr.Wrapf(err, "failed to init storage client")
+	}
+
+	// Init issuetracker.
+	issueTrackerClient, err := InitIssueTracker(storageClient)
+	if err != nil {
+		return nil, skerr.Wrapf(err, "failed to init issuetracker")
+	}
+
+	// Init github.
+	pathToGithubToken := filepath.Join(github.GITHUB_TOKEN_SERVER_PATH, github.GITHUB_TOKEN_FILENAME)
+	if *baseapp.Local {
+		usr, err := user.Current()
+		if err != nil {
+			return nil, err
+		}
+		pathToGithubToken = filepath.Join(usr.HomeDir, github.GITHUB_TOKEN_FILENAME)
+	}
+	githubClient, err := InitGithub(ctx, "flutter", "flutter", pathToGithubToken)
+	if err != nil {
+		return nil, skerr.Wrapf(err, "failed to init github")
+	}
+
+	// Init monorail.
+	monorailClient, err := InitMonorail(ctx, serviceAccountFile)
+	if err != nil {
+		return nil, skerr.Wrapf(err, "failed to init monorail")
+	}
+
+	// Init db for writing out bug counts for the different clients and sources.
+	dbClient, err := db.Init(ctx, ts)
+	if err != nil {
+		return nil, skerr.Wrapf(err, "fould not init DB")
+	}
+
+	return &IssuesPoller{
+		issueTrackerClient: issueTrackerClient,
+		githubClient:       githubClient,
+		monorailClient:     monorailClient,
+
+		dbClient: dbClient,
+	}, nil
+}
+
+// StartPoll polls the different issue frameworks and populates DB and an in-memory object with that data.
+// It hardcodes information about Skia's various clients. It may be possible to extract some/all of these into
+// flags or YAML config files in the future.
+func (p *IssuesPoller) StartPoll(pollInterval time.Duration) {
+
+	openCounts, untriagedCounts, err := p.dbClient.GetCountsFromDB(context.Background(), "", "", "")
+	if err != nil {
+		sklog.Fatal(err)
+	}
+	fmt.Println("GOT THIS:")
+	fmt.Println(openCounts)
+	fmt.Println(untriagedCounts)
+	return
+
+	// Let this keep collecting open issues. Then the different endpoints can return various things from those issues.
+	cleanup.Repeat(pollInterval, func(_ context.Context) {
+		// Ignore the passed-in context; this allows us to continue running even if the
+		// context is canceled due to transient errors.
+		ctx := context.Background()
+
+		//////////////////// Android - IssueTracker ////////////////////
+		androidQueryConfig := IssueTrackerQueryConfig{
+			Query:  "componentid:1346 status:open",
+			Client: AndroidClient,
+		}
+		if err := p.issueTrackerClient.SearchClientAndPersist(ctx, androidQueryConfig, p.dbClient); err != nil {
+			sklog.Errorf("Error when searching and saving issues: %s", err)
+		}
+
+		//////////////////// Flutter_on_web - Github ////////////////////
+		flutterOnWebQueryConfig := GithubQueryConfig{
+			Labels:           []string{"e: web_canvaskit"},
+			Open:             true,
+			PriorityRequired: true,
+			Client:           FlutterOnWebClient,
+		}
+		if err := p.githubClient.SearchClientAndPersist(ctx, flutterOnWebQueryConfig, p.dbClient); err != nil {
+			sklog.Errorf("Error when searching and saving issues: %s", err)
+		}
+
+		//////////////////// Flutter_native - Github ////////////////////
+		flutterNativeQueryConfig := GithubQueryConfig{
+			Labels:           []string{"dependency: skia"},
+			ExcludeLabels:    []string{"e: web_canvaskit"}, // These issues are already covered by flutter-on-web
+			Open:             true,
+			PriorityRequired: false,
+			Client:           FlutterNativeClient,
+		}
+		if err := p.githubClient.SearchClientAndPersist(ctx, flutterNativeQueryConfig, p.dbClient); err != nil {
+			sklog.Errorf("Error when searching and saving issues: %s", err)
+		}
+
+		//////////////////// Chromium:Internals>Skia - Monorail ////////////////////
+		crQueryConfig1 := MonorailQueryConfig{
+			Instance: "chromium",
+			Query:    "is:open component=Internals>Skia",
+			Client:   ChromiumClient,
+		}
+		if err := p.monorailClient.SearchClientAndPersist(ctx, crQueryConfig1, p.dbClient); err != nil {
+			sklog.Errorf("Error when searching and saving issues: %s", err)
+		}
+
+		//////////////////// Chromium:Internals>Skia>Compositing - Monorail ////////////////////
+		crQueryConfig2 := MonorailQueryConfig{
+			Instance: "chromium",
+			Query:    "is:open component=Internals>Skia>Compositing",
+			Client:   ChromiumClient,
+		}
+		if err := p.monorailClient.SearchClientAndPersist(ctx, crQueryConfig2, p.dbClient); err != nil {
+			sklog.Errorf("Error when searching and saving issues: %s", err)
+		}
+
+		//////////////////// Chromium:Internals>Skia>PDF - Monorail ////////////////////
+		crQueryConfig3 := MonorailQueryConfig{
+			Instance: "chromium",
+			Query:    "is:open component=Internals>Skia>PDF",
+			Client:   ChromiumClient,
+		}
+		if err := p.monorailClient.SearchClientAndPersist(ctx, crQueryConfig3, p.dbClient); err != nil {
+			sklog.Errorf("Error when searching and saving issues: %s", err)
+		}
+
+		//////////////////// Skia - Monorail ////////////////////
+		skQueryConfig := MonorailQueryConfig{
+			Instance: "skia",
+			Query:    "is:open",
+			Client:   SkiaClient,
+		}
+		if err := p.monorailClient.SearchClientAndPersist(ctx, skQueryConfig, p.dbClient); err != nil {
+			sklog.Errorf("Error when searching and saving issues: %s", err)
+		}
+
+		PrettyPrintOpenIssues()
+	}, nil)
+}
---
+++ bugs-central/go/db/db.go
@@ -0,0 +1,267 @@
+package db
+
+import (
+	"context"
+	"errors"
+	"flag"
+	"sync"
+	"time"
+
+	firestore_api "cloud.google.com/go/firestore"
+	"golang.org/x/oauth2"
+	"google.golang.org/api/iterator"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	"go.skia.org/infra/bugs-central/go/types"
+	"go.skia.org/infra/go/firestore"
+	"go.skia.org/infra/go/skerr"
+	"go.skia.org/infra/go/sklog"
+	"go.skia.org/infra/go/util"
+)
+
+var (
+	fsNamespace = flag.String("fs_namespace", "", "Typically the instance id. e.g. 'bugs-central'")
+	fsProjectID = flag.String("fs_project_id", "skia-firestore", "The project with the firestore instance. Datastore and Firestore can't be in the same project.")
+
+	// mtx to control access to firestore
+	mtx sync.RWMutex
+)
+
+const (
+	// For accessing Firestore.
+	DEFAULT_ATTEMPTS      = 3
+	PUT_SINGLE_TIMEOUT    = 10 * time.Second
+	DELETE_SINGLE_TIMEOUT = 10 * time.Second
+)
+
+// firestoreDB uses Cloud Firestore for store.
+type FirestoreDB struct {
+	client *firestore.Client
+}
+
+// The type that will stored in firestoreDB.
+type QueryData struct {
+	QueryLink       string    `json:"query_link"`
+	OpenCount       int       `json:"open_count"`
+	UnassignedCount int       `json:"unassigned_count"`
+	Created         time.Time `json:"created"`
+}
+
+func Init(ctx context.Context, ts oauth2.TokenSource) (*FirestoreDB, error) {
+	// Instantiate firestore.
+	fsClient, err := firestore.NewClient(ctx, *fsProjectID, "bugs-central", *fsNamespace, ts)
+	if err != nil {
+		return nil, skerr.Wrapf(err, "could not init firestore")
+	}
+	return &FirestoreDB{
+		client: fsClient,
+	}, nil
+}
+
+func (f *FirestoreDB) queryDocId(client types.RecognizedClient, source types.IssueSource, queryShortDesc string, ts time.Time) string {
+	// return fmt.Sprintf("%s#%s#%s#%s", client, source, queryShortDesc, firestore.FixTimestamp(ts).Format(util.SAFE_TIMESTAMP_FORMAT))
+	return firestore.FixTimestamp(ts).Format(util.SAFE_TIMESTAMP_FORMAT)
+}
+
+func (f *FirestoreDB) queryCol(client types.RecognizedClient, source types.IssueSource, queryShortDesc string) *firestore_api.CollectionRef {
+	clientCol := f.client.Collection(string(client))
+	sourceDoc := clientCol.Doc(string(source))
+	queryCol := sourceDoc.Collection(queryShortDesc)
+	return queryCol
+}
+
+func (f *FirestoreDB) getAllClientsCountsFromDB(ctx context.Context) (int, int, error) {
+	openCount := 0
+	unassignedCount := 0
+	clients := f.client.Collections(ctx)
+	for {
+		c, err := clients.Next()
+		if err == iterator.Done {
+			break
+		} else if err != nil {
+			return -1, -1, err
+		}
+		o, u, err := f.getAllSourcesCountsFromDB(ctx, c)
+		if err != nil {
+			return -1, -1, skerr.Wrapf(err, "could not get all sources counts from db")
+		}
+		openCount += o
+		unassignedCount += u
+	}
+	return openCount, unassignedCount, nil
+}
+
+func (f *FirestoreDB) getAllSourcesCountsFromDB(ctx context.Context, clientCol *firestore_api.CollectionRef) (int, int, error) {
+	openCount := 0
+	unassignedCount := 0
+	sources := clientCol.DocumentRefs(ctx)
+	for {
+		s, err := sources.Next()
+		if err == iterator.Done {
+			break
+		} else if err != nil {
+			return -1, -1, err
+		}
+		o, u, err := f.getAllQueriesCountsFromDB(ctx, s)
+		if err != nil {
+			return -1, -1, skerr.Wrapf(err, "could not get all queries counts from db")
+		}
+		openCount += o
+		unassignedCount += u
+	}
+	return openCount, unassignedCount, nil
+}
+
+func (f *FirestoreDB) getAllQueriesCountsFromDB(ctx context.Context, sourceDoc *firestore_api.DocumentRef) (int, int, error) {
+	openCount := 0
+	unassignedCount := 0
+
+	sources := sourceDoc.Collections(ctx)
+	for {
+		qry, err := sources.Next()
+		if err == iterator.Done {
+			break
+		} else if err != nil {
+			return -1, -1, err
+		}
+
+		var qd *QueryData
+		q := qry.OrderBy("Created", firestore_api.Desc).Limit(1)
+		if err := f.client.IterDocs(context.TODO(), "GetFromDB", "", q, DEFAULT_ATTEMPTS, PUT_SINGLE_TIMEOUT, func(doc *firestore_api.DocumentSnapshot) error {
+			if err := doc.DataTo(&qd); err != nil {
+				return err
+			}
+			return nil
+		}); err != nil {
+			return -1, -1, err
+		}
+		openCount += qd.OpenCount
+		unassignedCount += qd.UnassignedCount
+	}
+
+	return openCount, unassignedCount, nil
+}
+
+// Add documentation and call the above methods!
+// HERE HERE
+func (f *FirestoreDB) GetCountsFromDB(ctx context.Context, client types.RecognizedClient, source types.IssueSource, query string) (int, int, error) {
+	mtx.RLock()
+	defer mtx.RUnlock()
+
+	openCount := 0
+	unassignedCount := 0
+	if client == "" {
+		// Client has not been specified. Return the total count of all clients.
+		itClients := f.client.Collections(ctx)
+		for {
+			c, err := itClients.Next()
+			if err == iterator.Done {
+				break
+			} else if err != nil {
+				return -1, -1, err
+			}
+
+			itSources := c.DocumentRefs(ctx)
+			for {
+				s, err := itSources.Next()
+				if err == iterator.Done {
+					break
+				} else if err != nil {
+					return -1, -1, err
+				}
+
+				itQueries := s.Collections(ctx)
+				for {
+					qry, err := itQueries.Next()
+					if err == iterator.Done {
+						break
+					} else if err != nil {
+						return -1, -1, err
+					}
+
+					var qd *QueryData
+					q := qry.OrderBy("Created", firestore_api.Desc).Limit(1)
+					if err := f.client.IterDocs(context.TODO(), "GetFromDB", f.queryDocId(client, source, query, time.Now()), q, DEFAULT_ATTEMPTS, PUT_SINGLE_TIMEOUT, func(doc *firestore_api.DocumentSnapshot) error {
+						if err := doc.DataTo(&qd); err != nil {
+							return err
+						}
+						return nil
+					}); err != nil {
+						return -1, -1, err
+					}
+					openCount += qd.OpenCount
+					unassignedCount += qd.UnassignedCount
+				}
+			}
+		}
+	}
+
+	return openCount, unassignedCount, nil
+
+}
+
+// Change this to return counts!
+func (f *FirestoreDB) GetLeafNodeFromDB(ctx context.Context, client types.RecognizedClient, source types.IssueSource, query string) (*QueryData, error) {
+	if client == "" || source == "" || query == "" {
+		return nil, errors.New("Need client and source and query specified to get from leaf node in DB")
+	}
+
+	mtx.RLock()
+	defer mtx.RUnlock()
+
+	// Query firestore for this client+source+subComponent combination.
+	queryCol := f.queryCol(client, source, query)
+
+	// var existing *QueryData
+	var qd *QueryData
+	q := queryCol.OrderBy("Created", firestore_api.Desc).Limit(1)
+	if err := f.client.IterDocs(context.TODO(), "GetFromDB", f.queryDocId(client, source, query, time.Now()), q, DEFAULT_ATTEMPTS, PUT_SINGLE_TIMEOUT, func(doc *firestore_api.DocumentSnapshot) error {
+		if err := doc.DataTo(&qd); err != nil {
+			return err
+		}
+		return nil
+	}); err != nil {
+		return nil, err
+	}
+
+	return qd, nil
+}
+
+// putInDB if value is different
+func (f *FirestoreDB) PutInDB(ctx context.Context, client types.RecognizedClient, source types.IssueSource, query, queryLink string, openCount, unassignedCount int) error {
+	if client == "" || source == "" || query == "" {
+		return errors.New("Need client and source and query specified to put in DB")
+	}
+	// GetLeafNodeFromDB to see if the value of the query changed.
+	existingData, err := f.GetLeafNodeFromDB(ctx, client, source, query)
+	if err != nil {
+		return skerr.Wrapf(err, "could not get from DB")
+	}
+	if existingData != nil && existingData.OpenCount == openCount && existingData.UnassignedCount == unassignedCount {
+		sklog.Info("Not putting in DB because counts match")
+		return nil
+	}
+	sklog.Info("Putting in DB because counts did not match")
+
+	mtx.Lock()
+	defer mtx.Unlock()
+	now := time.Now()
+	qd := &QueryData{
+		QueryLink:       queryLink,
+		OpenCount:       openCount,
+		UnassignedCount: unassignedCount,
+		Created:         now,
+	}
+	queryDocId := f.queryDocId(client, source, query, now)
+	queryCol := f.queryCol(client, source, query)
+	_, createErr := f.client.Create(ctx, queryCol.Doc(queryDocId), qd, DEFAULT_ATTEMPTS, PUT_SINGLE_TIMEOUT)
+	if st, ok := status.FromError(createErr); ok && st.Code() == codes.AlreadyExists {
+		return skerr.Wrapf(createErr, "%s already exists in firestore", queryDocId)
+	}
+	if createErr != nil {
+		return createErr
+	}
+
+	return nil
+}
