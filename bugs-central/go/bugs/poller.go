86f28f714725d9a1612d23ebaa29dd6c4358312c
Ravi Mistry <rmistry@google.com>
2020-10-13 10:43:10 -0400

    Checkpoint
    Change-Id: I70e67ee9b534c1a119c7d7b43effd5f82b29d4d4

---
+++ bugs-central/go/bugs/poller.go
@@ -0,0 +1,245 @@
+package bugs
+
+// Initializes and polls the different support issue frameworks
+
+import (
+	"context"
+	"fmt"
+	"os/user"
+	"path/filepath"
+	"time"
+
+	"cloud.google.com/go/storage"
+	"golang.org/x/oauth2"
+	"google.golang.org/api/option"
+
+	"go.skia.org/infra/bugs-central/go/db"
+	"go.skia.org/infra/bugs-central/go/types"
+	"go.skia.org/infra/go/baseapp"
+	"go.skia.org/infra/go/cleanup"
+	"go.skia.org/infra/go/github"
+	"go.skia.org/infra/go/httputils"
+	"go.skia.org/infra/go/skerr"
+	"go.skia.org/infra/go/sklog"
+)
+
+type IssuesPoller struct {
+	issueTrackerClient BugFramework
+	githubClient       BugFramework
+	monorailClient     BugFramework
+
+	dbClient *db.FirestoreDB
+}
+
+func InitPoller(ctx context.Context, ts oauth2.TokenSource, serviceAccountFile string) (*IssuesPoller, error) {
+	httpClient := httputils.DefaultClientConfig().WithTokenSource(ts).With2xxOnly().Client()
+	storageClient, err := storage.NewClient(ctx, option.WithHTTPClient(httpClient))
+	if err != nil {
+		return nil, skerr.Wrapf(err, "failed to init storage client")
+	}
+
+	// Init issuetracker.
+	issueTrackerClient, err := InitIssueTracker(storageClient)
+	if err != nil {
+		return nil, skerr.Wrapf(err, "failed to init issuetracker")
+	}
+
+	// Init github.
+	pathToGithubToken := filepath.Join(github.GITHUB_TOKEN_SERVER_PATH, github.GITHUB_TOKEN_FILENAME)
+	if *baseapp.Local {
+		usr, err := user.Current()
+		if err != nil {
+			return nil, err
+		}
+		pathToGithubToken = filepath.Join(usr.HomeDir, github.GITHUB_TOKEN_FILENAME)
+	}
+	githubClient, err := InitGithub(ctx, "flutter", "flutter", pathToGithubToken)
+	if err != nil {
+		return nil, skerr.Wrapf(err, "failed to init github")
+	}
+
+	// Init monorail.
+	monorailClient, err := InitMonorail(ctx, serviceAccountFile)
+	if err != nil {
+		return nil, skerr.Wrapf(err, "failed to init monorail")
+	}
+
+	// Init db for writing out bug counts for the different clients and sources.
+	dbClient, err := db.Init(ctx, ts)
+	if err != nil {
+		return nil, skerr.Wrapf(err, "fould not init DB")
+	}
+
+	return &IssuesPoller{
+		issueTrackerClient: issueTrackerClient,
+		githubClient:       githubClient,
+		monorailClient:     monorailClient,
+
+		dbClient: dbClient,
+	}, nil
+}
+
+func (p *IssuesPoller) GetClients(ctx context.Context) (map[types.RecognizedClient]map[types.IssueSource]map[string]bool, error) {
+	return p.dbClient.GetClientsFromDB(ctx)
+}
+
+func (p *IssuesPoller) GetCounts(ctx context.Context, client types.RecognizedClient, source types.IssueSource, query string) (int, int, error) {
+	return p.dbClient.GetCountsFromDB(ctx, client, source, query)
+}
+
+// StartPoll polls the different issue frameworks and populates DB and an in-memory object with that data.
+// It hardcodes information about Skia's various clients. It may be possible to extract some/all of these into
+// flags or YAML config files in the future.
+func (p *IssuesPoller) StartPoll(pollInterval time.Duration) {
+
+	fmt.Println("XXXXXXXXXXXX")
+	m, err := p.dbClient.GetClientsFromDB(context.Background())
+	fmt.Printf("\n%+v\n", m)
+
+	openCounts, untriagedCounts, err := p.dbClient.GetCountsFromDB(context.Background(), "", "", "")
+	if err != nil {
+		sklog.Fatal(err)
+	}
+	fmt.Println("GOT THIS:")
+	fmt.Println(openCounts)
+	fmt.Println(untriagedCounts)
+	openCounts, untriagedCounts, err = p.dbClient.GetCountsFromDB(context.Background(), AndroidClient, "", "")
+	if err != nil {
+		sklog.Fatal(err)
+	}
+	fmt.Println("GOT THIS:")
+	fmt.Println(openCounts)
+	fmt.Println(untriagedCounts)
+	openCounts, untriagedCounts, err = p.dbClient.GetCountsFromDB(context.Background(), ChromiumClient, "", "")
+	if err != nil {
+		sklog.Fatal(err)
+	}
+	fmt.Println("GOT THIS:")
+	fmt.Println(openCounts)
+	fmt.Println(untriagedCounts)
+	openCounts, untriagedCounts, err = p.dbClient.GetCountsFromDB(context.Background(), FlutterNativeClient, "", "")
+	if err != nil {
+		sklog.Fatal(err)
+	}
+	fmt.Println("GOT THIS:")
+	fmt.Println(openCounts)
+	fmt.Println(untriagedCounts)
+	openCounts, untriagedCounts, err = p.dbClient.GetCountsFromDB(context.Background(), FlutterOnWebClient, "", "")
+	if err != nil {
+		sklog.Fatal(err)
+	}
+	fmt.Println("GOT THIS:")
+	fmt.Println(openCounts)
+	fmt.Println(untriagedCounts)
+	openCounts, untriagedCounts, err = p.dbClient.GetCountsFromDB(context.Background(), SkiaClient, "", "")
+	if err != nil {
+		sklog.Fatal(err)
+	}
+	fmt.Println("GOT THIS:")
+	fmt.Println(openCounts)
+	fmt.Println(untriagedCounts)
+	openCounts, untriagedCounts, err = p.dbClient.GetCountsFromDB(context.Background(), AndroidClient, IssueTrackerSource, "")
+	if err != nil {
+		sklog.Fatal(err)
+	}
+	fmt.Println("GOT THIS:")
+	fmt.Println(openCounts)
+	fmt.Println(untriagedCounts)
+	openCounts, untriagedCounts, err = p.dbClient.GetCountsFromDB(context.Background(), AndroidClient, IssueTrackerSource, "componentid:1346 status:open")
+	if err != nil {
+		sklog.Fatal(err)
+	}
+	fmt.Println("GOT THIS:")
+	fmt.Println(openCounts)
+	fmt.Println(untriagedCounts)
+	openCounts, untriagedCounts, err = p.dbClient.GetCountsFromDB(context.Background(), AndroidClient, MonorailSource, "")
+	if err != nil {
+		sklog.Fatal(err)
+	}
+	fmt.Println("GOT THIS:")
+	fmt.Println(openCounts)
+	fmt.Println(untriagedCounts)
+	// FOR TESTING TESTING TESTING
+	// return
+
+	// Let this keep collecting open issues. Then the different endpoints can return various things from those issues.
+	cleanup.Repeat(pollInterval, func(_ context.Context) {
+		// Ignore the passed-in context; this allows us to continue running even if the
+		// context is canceled due to transient errors.
+		ctx := context.Background()
+
+		//////////////////// Android - IssueTracker ////////////////////
+		androidQueryConfig := IssueTrackerQueryConfig{
+			Query:  "componentid:1346 status:open",
+			Client: AndroidClient,
+		}
+		if err := p.issueTrackerClient.SearchClientAndPersist(ctx, androidQueryConfig, p.dbClient); err != nil {
+			sklog.Errorf("Error when searching and saving issues: %s", err)
+		}
+
+		//////////////////// Flutter_on_web - Github ////////////////////
+		flutterOnWebQueryConfig := GithubQueryConfig{
+			Labels:           []string{"e: web_canvaskit"},
+			Open:             true,
+			PriorityRequired: true,
+			Client:           FlutterOnWebClient,
+		}
+		if err := p.githubClient.SearchClientAndPersist(ctx, flutterOnWebQueryConfig, p.dbClient); err != nil {
+			sklog.Errorf("Error when searching and saving issues: %s", err)
+		}
+
+		//////////////////// Flutter_native - Github ////////////////////
+		flutterNativeQueryConfig := GithubQueryConfig{
+			Labels:           []string{"dependency: skia"},
+			ExcludeLabels:    []string{"e: web_canvaskit"}, // These issues are already covered by flutter-on-web
+			Open:             true,
+			PriorityRequired: false,
+			Client:           FlutterNativeClient,
+		}
+		if err := p.githubClient.SearchClientAndPersist(ctx, flutterNativeQueryConfig, p.dbClient); err != nil {
+			sklog.Errorf("Error when searching and saving issues: %s", err)
+		}
+
+		//////////////////// Chromium:Internals>Skia - Monorail ////////////////////
+		crQueryConfig1 := MonorailQueryConfig{
+			Instance: "chromium",
+			Query:    "is:open component=Internals>Skia",
+			Client:   ChromiumClient,
+		}
+		if err := p.monorailClient.SearchClientAndPersist(ctx, crQueryConfig1, p.dbClient); err != nil {
+			sklog.Errorf("Error when searching and saving issues: %s", err)
+		}
+
+		//////////////////// Chromium:Internals>Skia>Compositing - Monorail ////////////////////
+		crQueryConfig2 := MonorailQueryConfig{
+			Instance: "chromium",
+			Query:    "is:open component=Internals>Skia>Compositing",
+			Client:   ChromiumClient,
+		}
+		if err := p.monorailClient.SearchClientAndPersist(ctx, crQueryConfig2, p.dbClient); err != nil {
+			sklog.Errorf("Error when searching and saving issues: %s", err)
+		}
+
+		//////////////////// Chromium:Internals>Skia>PDF - Monorail ////////////////////
+		crQueryConfig3 := MonorailQueryConfig{
+			Instance: "chromium",
+			Query:    "is:open component=Internals>Skia>PDF",
+			Client:   ChromiumClient,
+		}
+		if err := p.monorailClient.SearchClientAndPersist(ctx, crQueryConfig3, p.dbClient); err != nil {
+			sklog.Errorf("Error when searching and saving issues: %s", err)
+		}
+
+		//////////////////// Skia - Monorail ////////////////////
+		skQueryConfig := MonorailQueryConfig{
+			Instance: "skia",
+			Query:    "is:open",
+			Client:   SkiaClient,
+		}
+		if err := p.monorailClient.SearchClientAndPersist(ctx, skQueryConfig, p.dbClient); err != nil {
+			sklog.Errorf("Error when searching and saving issues: %s", err)
+		}
+
+		PrettyPrintOpenIssues()
+	}, nil)
+}
---
+++ bugs-central/modules/bugs-central-sk/bugs-central-sk.js
@@ -0,0 +1,232 @@
+/**
+ * @module bugs-central-sk
+ * @description <h2><code>bugs-central-sk</code></h2>
+ *
+ * <p>
+ *   Displays the enter-bugs-central-sk and display-bugs-central-sk elements.
+ *   Handles calls to the backend from events originating from those elements.
+ * </p>
+ *
+ */
+
+import { define } from 'elements-sk/define';
+import { html } from 'lit-html';
+import { errorMessage } from 'elements-sk/errorMessage';
+import { jsonOrThrow } from 'common-sk/modules/jsonOrThrow';
+import { stateReflector } from 'common-sk/modules/stateReflector';
+
+// import '../display-bugs-central-sk';
+// import '../enter-bugs-central-sk';
+
+import { $$ } from 'common-sk/modules/dom';
+import { ElementSk } from '../../../infra-sk/modules/ElementSk';
+
+const CLIENT_KEY_DELIMITER = ' > ';
+
+// Make this take state instead.
+function getClientKey(c, s, q) {
+  if (!c) {
+    return '';
+  } else if (!s) {
+    return `${c}`;
+  } else if (!q) {
+    return `${c}${CLIENT_KEY_DELIMITER}${s}`;
+  } else {
+    return `${c}${CLIENT_KEY_DELIMITER}${s}${CLIENT_KEY_DELIMITER}${q}`;
+  }
+}
+
+function breakupClientKey(clientKey) {
+  const ret = {
+    client: '',
+    source: '',
+    query: '',
+  }
+  const tokens = clientKey.split(CLIENT_KEY_DELIMITER);
+  if (tokens.length === 0) {
+    // Leave all values blank.
+  } else if (tokens.length === 1) {
+    ret.client = tokens[0];
+  } else if (tokens.length === 2) {
+    ret.client = tokens[0];
+    ret.source = tokens[1];
+  } else if (tokens.length === 3) {
+    ret.client = tokens[0];
+    ret.source = tokens[1];
+    ret.query = tokens[2];
+  }
+  return ret;
+}
+
+
+// <enter-bugs-central-sk .autorollers=${ele._autorollers}></enter-bugs-central-sk>
+// <display-bugs-central-sk .statuses=${ele._statuses}></display-bugs-central-sk>
+
+const template = (ele) => html`
+<h2>${getTitle(ele)}</h2>
+<br/>
+Open Count: ${ele._open_count}
+<br/>
+Unassigned Count: ${ele._unassigned_count}
+<br/><br/>
+${displayClientsTable(ele)}
+`;
+
+function getTitle(ele) {
+  if (!ele._state.client) {
+    return 'Displaying all clients'
+  }
+  return getClientKey(ele._state.client, ele._state.source, ele._state.query);
+}
+
+function displayClientsRows(ele) {
+  const rowsHTML = [];
+  const clientKeys = Object.keys(ele._clients_to_counts);
+  clientKeys.sort();
+  for (let i=0; i<clientKeys.length; i++) {
+    const clientKey = clientKeys[i];
+    const tokens = breakupClientKey(clientKey);
+    const c = tokens.client;
+    const s = tokens.source;
+    const q = tokens.query;
+    rowsHTML.push(html`
+      <tr>
+        <td class=client-link @click=${() => ele._click(clientKey)}>${clientKey}</td>
+        <td>${ele._clients_to_counts[clientKey].open_count}</td>
+        <td>${ele._clients_to_counts[clientKey].unassigned_count}</td>
+      </tr>
+    `);
+  }
+  return rowsHTML;
+}
+
+function displayClientsTable(ele) {
+  const rows = displayClientsRows(ele);
+  return html`
+    <table class=client-counts>
+      <tr>
+        <th>Client</th>
+        <th>Open</th>
+        <th>Unassigned</th>
+      </tr>
+       ${rows}
+    </table>
+  `;
+}
+
+define('bugs-central-sk', class extends ElementSk {
+  constructor() {
+    super(template);
+
+    this._open_count = 0;
+    this._unassigned_count = 0;
+
+    this._clients_to_counts = {};
+    this._clients_map = {};
+
+    this._state = {
+      client: '',
+      source: '',
+      query: '',
+    }
+  }
+
+  // CLEAN THIS UP!
+  _click(clientKey) {
+    const tokens = breakupClientKey(clientKey);
+    this._state.client = tokens.client ? tokens.client : '';
+    this._state.source = tokens.source ? tokens.source : '';
+    this._state.query = tokens.query ? tokens.query : '';
+    this._stateHasChanged();
+    // statehashchanged does not call render during set.
+    this._populateClients();
+   //  this._render();
+  }
+
+  async _populateClients() {
+    this._doImpl('/_/get_clients_sources_queries', {}, async (json) => {
+        this._clients_map = json.clients;
+        this._clients_to_counts = {};
+        const c = this._state.client;
+        let s = this._state.source;
+        const q = this._state.query;
+
+        // Client is specified and there is only one source then directly display
+        // it's queries.
+        if (c && !s && !q) {
+          const sources = Object.keys(this._clients_map[c]);
+          if (sources.length === 1) {
+            this._state.source = sources[0];
+            s = this._state.source;
+          }
+        }
+
+        if (!c) {
+          await Promise.all(Object.keys(this._clients_map).map(async (c) => this._clients_to_counts[getClientKey(c)] = await this._getCounts(c)));
+        } else if (!s) {
+          await Promise.all(Object.keys(this._clients_map[c]).map(async (s) => this._clients_to_counts[getClientKey(c, s)] = await this._getCounts(c, s)));
+        } else if (!q) {
+          await Promise.all(Object.keys(this._clients_map[c][s]).map(async (q) => this._clients_to_counts[getClientKey(c, s, q)] = await this._getCounts(c, s, q)));
+        } else {
+          this._clients_to_counts[getClientKey(c, s, q)] = await this._getCounts(c, s, q);
+        }
+
+        this._render();  // look at all _renders and see if they make sense or not..
+    });
+  }
+
+  async connectedCallback() {
+    super.connectedCallback();
+
+    this._stateHasChanged = stateReflector(
+      /* getState */ () => this._state,
+      /* setState */ (state) => {
+        this._state = state;
+        this._populateClients();
+        this._render();
+      },
+    );
+
+    this._populateClients();
+
+    let { open_count, unassigned_count } = await this._getCounts();
+    this._render();
+  }
+
+  // Common work done for all fetch requests.
+  async _doImpl(url, detail, action) {
+    await fetch(url, {
+      body: JSON.stringify(detail),
+      headers: {
+        'content-type': 'application/json',
+      },
+      credentials: 'include',
+      method: 'POST',
+    }).then(jsonOrThrow).then((json) => {
+      action(json);
+      //this._render();
+    }).catch((msg) => {
+      console.log(msg);
+      msg.resp.text().then(errorMessage);
+    });
+  }
+
+  // Await and async.
+  async _getCounts(client, source, query) {
+    // Parse params...
+    const detail = {
+      'client': client,
+      'source': source,
+      'query': query,
+    };
+    let open_count = 0;
+    let unassigned_count = 0;
+    await this._doImpl('/_/get_issue_counts', detail, (json) => {
+        this._open_count = json.open_count;
+        open_count = json.open_count;
+        this._unassigned_count = json.unassigned_count;
+        unassigned_count = json.unassigned_count;
+    });
+    return {open_count, unassigned_count};
+  }
+});
