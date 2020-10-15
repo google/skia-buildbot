1d81fabcc0d6fe201029f5a4300e640c3573e42b
Joe Gregorio <jcgregorio@google.com>
2020-10-15 11:46:57 -0400

    [perf] Add hash and human date to commit-detail-sk.
    Change-Id: I939975047d2d8122bec5530478d5c16417b03d72

--- perf/modules/commit-detail-sk/commit-detail-sk-demo.ts
+++ perf/modules/commit-detail-sk/commit-detail-sk-demo.ts
@@ -1,13 +1,13 @@
 import './index';
 import { CommitDetailSk } from './commit-detail-sk';
 
+Date.now = () => Date.parse('2020-03-22T00:00:00.000Z');
+
 document.querySelectorAll<CommitDetailSk>('commit-detail-sk').forEach((ele) => {
   ele.cid = {
     hash: 'e699a3a2373bc4c2a4bfa93c7af8602cb15f2d1d',
-    url:
-      'https://skia.googlesource.com/skia/+show/e699a3a2373bc4c2a4bfa93c7af8602cb15f2d1d',
-    message:
-      'e699a3a - 19h 59m - Roll third_party/externals/swiftshader 522d5121905',
+    url: 'https://skia.googlesource.com/skia/+show/e699a3a2373bc4c2a4bfa93c7af8602cb15f2d1d',
+    message: 'Roll third_party/externals/swiftshader 522d5121905',
     author: '',
     ts: 0,
     offset: 0,
