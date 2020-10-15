00f7d13bfdba4a7f4b41344739f0809d4752fc06
Ravi Mistry <rmistry@google.com>
2020-10-15 11:22:03 -0400

    Checkpoint
    Change-Id: I70e67ee9b534c1a119c7d7b43effd5f82b29d4d4

---
+++ bugs-central/go/types/types.go
@@ -0,0 +1,74 @@
+package types
+
+const (
+	// All bug frameworks will be standardized to these priorities.
+	PriorityP0 StandardizedPriority = "P0"
+	PriorityP1 StandardizedPriority = "P1"
+	PriorityP2 StandardizedPriority = "P2"
+	PriorityP3 StandardizedPriority = "P3"
+	PriorityP4 StandardizedPriority = "P4"
+	PriorityP5 StandardizedPriority = "P5"
+	PriorityP6 StandardizedPriority = "P6"
+)
+
+type IssueSource string
+type RecognizedClient string
+type StandardizedPriority string
+
+type IssueCountsData struct {
+	OpenCount       int    `json:"open_count"`
+	UnassignedCount int    `json:"unassigned_count"`
+	P0Count         int    `json:"p0_count"`
+	P1Count         int    `json:"p1_count"`
+	P2Count         int    `json:"p2_count"`
+	P3Count         int    `json:"p3_count"`
+	P4Count         int    `json:"p4_count"`
+	P5Count         int    `json:"p5_count"`
+	P6Count         int    `json:"p6_count"`
+	QueryLink       string `json:"query_link"`
+}
+
+func (qcd *IssueCountsData) MergeInto(from ...*IssueCountsData) {
+	for _, f := range from {
+		qcd.OpenCount += f.OpenCount
+		qcd.UnassignedCount += f.UnassignedCount
+		qcd.P0Count += f.P0Count
+		qcd.P1Count += f.P1Count
+		qcd.P2Count += f.P2Count
+		qcd.P3Count += f.P3Count
+		qcd.P4Count += f.P4Count
+		qcd.P5Count += f.P5Count
+		qcd.P6Count += f.P6Count
+	}
+}
+
+func (qcd *IssueCountsData) IsEqual(compareWith *IssueCountsData) bool {
+	return qcd.OpenCount == compareWith.OpenCount &&
+		qcd.UnassignedCount == compareWith.UnassignedCount &&
+		qcd.P0Count == compareWith.P0Count &&
+		qcd.P1Count == compareWith.P1Count &&
+		qcd.P2Count == compareWith.P2Count &&
+		qcd.P3Count == compareWith.P3Count &&
+		qcd.P4Count == compareWith.P4Count &&
+		qcd.P5Count == compareWith.P5Count &&
+		qcd.P6Count == compareWith.P6Count
+}
+
+func (qcd *IssueCountsData) IncPriority(priority StandardizedPriority) {
+	switch priority {
+	case PriorityP0:
+		qcd.P0Count++
+	case PriorityP1:
+		qcd.P1Count++
+	case PriorityP2:
+		qcd.P2Count++
+	case PriorityP3:
+		qcd.P3Count++
+	case PriorityP4:
+		qcd.P4Count++
+	case PriorityP5:
+		qcd.P5Count++
+	case PriorityP6:
+		qcd.P6Count++
+	}
+}
