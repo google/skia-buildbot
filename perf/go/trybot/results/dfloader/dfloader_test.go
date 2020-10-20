a43cc8ce2910566b8313d4d0820cfd9e81561cfa
Joe Gregorio <jcgregorio@google.com>
2020-10-20 16:46:34 -0400

    [perf] Add trybot.Ingester and a Gerrit impl.
    Change-Id: Iaafa2134f2827af153799b18943716800fc4185b

---
+++ perf/go/trybot/results/dfloader/dfloader_test.go
@@ -0,0 +1,53 @@
+// Package dfloader implements results.Loader using a DataFrameBuilder.
+package dfloader
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+	"go.skia.org/infra/go/testutils/unittest"
+	"go.skia.org/infra/perf/go/dataframe/mocks"
+	perfgit "go.skia.org/infra/perf/go/git"
+	"go.skia.org/infra/perf/go/git/gittest"
+	"go.skia.org/infra/perf/go/trybot/results"
+	storeMocks "go.skia.org/infra/perf/go/trybot/store/mocks"
+)
+
+var errFromMock = fmt.Errorf("MockError")
+
+const testTileSize = 4
+
+// CleanupFunc is the type of clean up function that NewForTest returns.
+type CleanupFunc func()
+
+func setupForTest(t *testing.T) (context.Context, *perfgit.Git, CleanupFunc, []string) {
+	ctx, db, _, hashes, instanceConfig, _, gitCleanup := gittest.NewForTest(t)
+	instanceConfig.DataStoreConfig.TileSize = testTileSize
+	g, err := perfgit.New(ctx, true, db, instanceConfig)
+	require.NoError(t, err)
+	return ctx, g, CleanupFunc(gitCleanup), hashes
+}
+
+func TestLoader_LoadWithDataFrameBuilderThatErrors_LoadReturnsError(t *testing.T) {
+	unittest.MediumTest(t)
+	ctx, g, cleanup, _ := setupForTest(t)
+	defer cleanup()
+
+	dfb := &mocks.DataFrameBuilder{}
+	dfb.On("NewNFromQuery", mock.Anything, mock.Anything, mock.Anything, mock.Anything, mock.Anything).Return(nil, errFromMock)
+	storeMock := &storeMocks.TryBotStore{}
+
+	loader := New(dfb, storeMock, g)
+
+	request := results.TryBotRequest{
+		Kind:         results.Commit,
+		CommitNumber: 2,
+	}
+	_, err := loader.Load(ctx, request, nil)
+	require.Error(t, err)
+	assert.Contains(t, err.Error(), errFromMock.Error())
+}
