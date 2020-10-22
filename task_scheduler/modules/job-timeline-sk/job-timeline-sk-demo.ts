64324ebb3ac592f4c30b7a334864ae28b59d5cbb
Eric Boren <borenet@google.com>
2020-10-22 10:56:50 -0400

    [task scheduler] Mock Date.now for testing
    This should make screenshots from puppeteer tests consistent.
    
    Change-Id: I1092442ee727001ef368ca051ac63b7153e0016d

--- task_scheduler/modules/job-timeline-sk/job-timeline-sk-demo.ts
+++ task_scheduler/modules/job-timeline-sk/job-timeline-sk-demo.ts
@@ -1,6 +1,9 @@
 import './index';
 import { JobTimelineSk } from './job-timeline-sk';
-import { job1, task0, task1, task2, task3, task4 } from '../rpc-mock';
+import { job1, task0, task1, task2, task3, task4, fakeNow } from '../rpc-mock';
+
+// Override the current date to keep puppeteer tests consistent.
+Date.now = () => fakeNow;
 
 const ele = <JobTimelineSk>document.querySelector('job-timeline-sk')!;
 ele.draw(job1, [task0, task1, task2, task3, task4], []);
