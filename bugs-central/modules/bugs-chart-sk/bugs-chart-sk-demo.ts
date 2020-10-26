db4b9ae4214091bc9ca19433ec4cd7f94104f537
Ravi Mistry <rmistry@google.com>
2020-10-26 13:12:37 -0400

    [bugs-central] Optimize fetching of charts data
    Previously we relied on google-charts to fetch it's own data. This
    resulted in 3 separate calls for 3 charts. This was time consuming and
    wasteful.
    Optimize this into a single call and directly provide google-charts with the
    data it needs to display.
    
    Controlling the chart data fetches also gives us the ability to turn on the
    spinner while chart data is still loading.
    
    Bug: skia:10783
    Change-Id: Ic1da2e344e803ad3cf4a2943f328f2def3df4144

--- bugs-central/modules/bugs-chart-sk/bugs-chart-sk-demo.ts
+++ bugs-central/modules/bugs-chart-sk/bugs-chart-sk-demo.ts
@@ -1,5 +1,4 @@
 import './index';
-import fetchMock from 'fetch-mock';
 
 const chartData = [
   ['date', 'col1', 'col2'],
@@ -7,28 +6,25 @@ const chartData = [
   ['2020-10-02', 32, 24],
 ];
 
-fetchMock.get('/_/get_chart_data?client=Android&source=&query=&type=open', chartData);
-fetchMock.get('/_/get_chart_data?client=Android&source=Github&query=&type=slo', chartData);
-
 customElements.whenDefined('bugs-chart-sk').then(() => {
-  // Insert the elements later, which should given enough time for fetchMock to be in place.
   const page1 = document.createElement('bugs-chart-sk');
   page1.setAttribute('chart_type', 'open');
   page1.setAttribute('chart_title', 'Bug Count');
-  page1.setAttribute('client', 'Android');
-  page1.setAttribute('source', '');
-  page1.setAttribute('query', '');
+  page1.setAttribute('data', JSON.stringify(chartData));
 
   const page2 = document.createElement('bugs-chart-sk');
   page2.setAttribute('chart_type', 'slo');
   page2.setAttribute('chart_title', 'SLO Violations');
-  page2.setAttribute('client', 'Android');
-  page2.setAttribute('source', 'Github');
-  page2.setAttribute('query', '');
+  page2.setAttribute('data', JSON.stringify(chartData));
 
+  const page3 = document.createElement('bugs-chart-sk');
+  page3.setAttribute('chart_type', 'untriaged');
+  page3.setAttribute('chart_title', 'Untriaged Bugs');
+  page3.setAttribute('data', JSON.stringify(chartData));
 
   document
     .querySelector('h1')!
     .insertAdjacentElement('afterend', page1)!
-    .insertAdjacentElement('afterend', page2);
+    .insertAdjacentElement('afterend', page2)!
+    .insertAdjacentElement('afterend', page3);
 });
