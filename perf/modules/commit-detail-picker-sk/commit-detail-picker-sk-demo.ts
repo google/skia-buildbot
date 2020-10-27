402a8e2752e3e201459b14effeedbf9e0a326c6c
Joe Gregorio <jcgregorio@google.com>
2020-10-27 16:11:50 -0400

    [perf] Move day-range selection into commit-detail-picker-sk.
    Bug: skia:10844
    Change-Id: I03ce7b12462cc9f58e861bd003f7bb5199ba1b1e
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/329365
    Reviewed-by: Joe Gregorio <jcgregorio@google.com>
    Commit-Queue: Joe Gregorio <jcgregorio@google.com>

--- perf/modules/commit-detail-picker-sk/commit-detail-picker-sk-demo.ts
+++ perf/modules/commit-detail-picker-sk/commit-detail-picker-sk-demo.ts
@@ -1,48 +1,52 @@
-import './index';
-import { CommitDetailPickerSk } from './commit-detail-picker-sk';
 
-const commitinfo = [
+import 'elements-sk/error-toast-sk';
+import fetchMock from 'fetch-mock';
+import { CommitDetailPickerSk } from './commit-detail-picker-sk';
+import { Commit } from '../json';
+
+Date.now = () => Date.parse('2020-03-22T00:00:00.000Z');
+
+fetchMock.post('/_/cidRange/', (): Commit[] => [
   {
-    ts: 1439649751,
-    author: 'foo (foo@example.org)',
-    url: 'skia.googlesource.com/bar',
-    message: 'Commit from foo.',
-    hash: 'abcdef123',
-    offset: 1,
+    offset: 43389,
+    author: 'Avinash Parchuri (aparchur@google.com)',
+    message: 'Reland "[skottie] Add onTextProperty support into ',
+    url:
+      'https://skia.googlesource.com/skia/+show/3a543aafd4e68af182ef88572086c094cd63f0b2',
+    hash: '3a543aafd4e68af182ef88572086c094cd63f0b2',
+    ts: 1565099441,
   },
   {
-    ts: 1439648914,
-    author: 'bar (bar@example.org)',
-    url: 'skia.googlesource.com/foo',
-    message: 'Commit from bar',
-    hash: 'abcdef456',
-    offset: 2,
+    offset: 43390,
+    author: 'Robert Phillips (robertphillips@google.com)',
+    message: 'Use GrComputeTightCombinedBufferSize in GrMtlGpu::',
+    url:
+      'https://skia.googlesource.com/skia/+show/bdb0919dcc6a700b41492c53ecf06b40983d13d7',
+    hash: 'bdb0919dcc6a700b41492c53ecf06b40983d13d7',
+    ts: 1565107798,
   },
   {
-    ts: 1439649951,
-    author: 'foo (foo@example.org)',
-    url: 'https://codereview.chromium.org/1490543002',
-    message: 'Whitespace change',
-    hash: 'abcdef789',
-    offset: 3,
+    offset: 43391,
+    author: 'Hal Canary (halcanary@google.com)',
+    message: 'experimental/editor: interface no longer uses stri',
+    url:
+      'https://skia.googlesource.com/skia/+show/e45bf6a603b7990f418eaf19ef0e2a2e59a9f449',
+    hash: 'e45bf6a603b7990f418eaf19ef0e2a2e59a9f449',
+    ts: 1565220328,
   },
-  {
-    ts: 1439699951,
-    author: 'foo (foo@example.org)',
-    url: 'https://codereview.chromium.org/1490543002',
-    message: 'Another whitespace change',
-    hash: 'abcdef101112',
-    offset: 4,
-  },
-];
+]);
+
+// eslint-disable-next-line import/first
+import './index';
 
 const evt = document.querySelector('#evt')!;
 
 document
   .querySelectorAll<CommitDetailPickerSk>('commit-detail-picker-sk')
   .forEach((panel) => {
-    panel.details = commitinfo;
     panel.addEventListener('commit-selected', (e: Event) => {
       evt.textContent = JSON.stringify((e as CustomEvent).detail, null, '  ');
     });
   });
+
+document.querySelector<CommitDetailPickerSk>('#darkmode-picker button')!.click();
