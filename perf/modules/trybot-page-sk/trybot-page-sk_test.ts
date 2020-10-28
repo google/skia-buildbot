87f4c2dcb7b8135b87cd2755170e080aa9a19dd1
Joe Gregorio <jcgregorio@google.com>
2020-10-28 12:36:57 -0400

    [perf] Skeleton for trybot page.
    Bug: skia:10844
    Change-Id: I8a6466c7acc3061f0ef359169a58f7b1da0dc63c

---
+++ perf/modules/trybot-page-sk/trybot-page-sk_test.ts
@@ -0,0 +1,23 @@
+import './index';
+import { TrybotPageSk } from './trybot-page-sk';
+
+import { setUpElementUnderTest } from '../../../infra-sk/modules/test_util';
+import { expect } from 'chai';
+
+describe('trybot-page-sk', () => {
+  const newInstance = setUpElementUnderTest<TrybotPageSk>('trybot-page-sk');
+
+  let element: TrybotPageSk;
+  beforeEach(() => {
+    element = newInstance((el: TrybotPageSk) => {
+      // Place here any code that must run after the element is instantiated but
+      // before it is attached to the DOM (e.g. property setter calls,
+      // document-level event listeners, etc.).
+    });
+  });
+
+  describe('some action', () => {
+    it('some result', () => {});
+      expect(element).to.not.be.null;
+  });
+});
