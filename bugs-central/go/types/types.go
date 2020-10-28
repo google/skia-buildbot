2fb303eb68d278960df8c492c6d10b0e88909774
Ravi Mistry <rmistry@google.com>
2020-10-28 17:50:34 -0400

    [bugs-central] Display issues outside SLO in a popup
    Issues will be grouped by their priorities and sorted by how far
    they are from SLO. Also refactor SLO calculations.
    
    Screenshot: https://screenshot.googleplex.com/5SWx7i5m7CUjjDs
    
    Bug: skia:10783
    Change-Id: Ide12acf49acde3eb76ed20c8ebfd06468b9c4d01

--- bugs-central/go/types/types.go
+++ bugs-central/go/types/types.go
@@ -1,6 +1,11 @@
 package types
 
-import "time"
+import (
+	"fmt"
+	"time"
+
+	"github.com/hako/durafmt"
+)
 
 const (
 	// All bug frameworks will be standardized to these priorities.
@@ -21,6 +26,35 @@ const (
 	Biennialy = 2 * Yearly
 )
 
+type sloViolationData struct {
+	// If an open issue's last modified time is beyond this duration then it is an SLO violation.
+	modifiedDuration time.Duration
+	// If an open issue's creation time is beyond this duration then it is an SLO violation.
+	createdDuration time.Duration
+}
+
+var (
+	// Uses data from https://docs.google.com/document/d/1OgpX1KDDq3YkHzRJjqRHSPJ9CJ8hH0RTvMAApKVxwm8/edit
+	priorityToSLOViolationData = map[StandardizedPriority]sloViolationData{
+		PriorityP0: {
+			modifiedDuration: Daily,
+			createdDuration:  Weekly,
+		},
+		PriorityP1: {
+			modifiedDuration: Weekly,
+			createdDuration:  Monthly,
+		},
+		PriorityP2: {
+			modifiedDuration: Biannualy,
+			createdDuration:  Yearly,
+		},
+		PriorityP3: {
+			modifiedDuration: Yearly,
+			createdDuration:  Biennialy,
+		},
+	}
+)
+
 // IssueSource types will be all the recognized issue frameworks (eg: Github, IssueTracker, Monorail).
 type IssueSource string
 
@@ -37,6 +71,10 @@ type Issue struct {
 	Priority StandardizedPriority `json:"priority"`
 	Owner    string               `json:"owner"`
 	Link     string               `json:"link"`
+
+	SLOViolation         bool          `json:"slo_violation"`
+	SLOViolationReason   string        `json:"slo_violation_reason"`
+	SLOViolationDuration time.Duration `json:"slo_violation_duration"`
 
 	CreatedTime  time.Time `json:"created"`
 	ModifiedTime time.Time `json:"modified"`
@@ -73,23 +111,27 @@ type IssueCountsData struct {
 	UntriagedQueryLink string `json:"untriaged_query_link"`
 }
 
-// CalculateSLOViolations uses data from https://docs.google.com/document/d/1OgpX1KDDq3YkHzRJjqRHSPJ9CJ8hH0RTvMAApKVxwm8/edit
-func (icd *IssueCountsData) CalculateSLOViolations(now, created, modified time.Time, priority StandardizedPriority) {
+// IncSLOViolations will increment the priority's corresponding slo count.
+func (icd *IssueCountsData) IncSLOViolation(violation bool, priority StandardizedPriority) {
+	if !violation {
+		// Nothing to do here.
+		return
+	}
 	switch priority {
 	case PriorityP0:
-		if now.After(modified.Add(Daily)) || now.After(created.Add(Weekly)) {
+		if violation {
 			icd.P0SLOViolationCount++
 		}
 	case PriorityP1:
-		if now.After(modified.Add(Weekly)) || now.After(created.Add(Monthly)) {
+		if violation {
 			icd.P1SLOViolationCount++
 		}
 	case PriorityP2:
-		if now.After(modified.Add(Biannualy)) || now.After(created.Add(Yearly)) {
+		if violation {
 			icd.P2SLOViolationCount++
 		}
 	case PriorityP3:
-		if now.After(modified.Add(Yearly)) || now.After(created.Add(Biennialy)) {
+		if violation {
 			icd.P3SLOViolationCount++
 		}
 	}
@@ -136,4 +178,20 @@ func (icd *IssueCountsData) IncPriority(priority StandardizedPriority) {
 	case PriorityP6:
 		icd.P6Count++
 	}
+}
+
+// IsPrioritySLOViolation returns whether the priority is outside the SLO.
+// If issue has violated SLO then returns description and a duration that shows by how much
+// it was surpassed.
+func IsPrioritySLOViolation(now, created, modified time.Time, priority StandardizedPriority) (bool, string, time.Duration) {
+	if sloViolationData, ok := priorityToSLOViolationData[priority]; ok {
+		if now.After(modified.Add(sloViolationData.modifiedDuration)) {
+			duration := now.Sub(modified.Add(sloViolationData.modifiedDuration))
+			return true, fmt.Sprintf("exceeded modified time SLO by %s", durafmt.Parse(duration).LimitFirstN(2)), duration
+		} else if now.After(created.Add(sloViolationData.createdDuration)) {
+			duration := now.Sub(created.Add(sloViolationData.createdDuration))
+			return true, fmt.Sprintf("exceeded creation time SLO by %s", durafmt.Parse(duration).LimitFirstN(2)), duration
+		}
+	}
+	return false, "", 0
 }
---
+++ bugs-central/modules/bugs-slo-popup-sk/bugs-slo-popup-sk.ts
@@ -0,0 +1,92 @@
+/**
+ * @module bugs-slo-popup-sk
+ * @description <h2><code>bugs-slo-popup-sk</code></h2>
+ *
+ * A dialog that displays all bugs that are outside SLO.
+ */
+
+import { define } from 'elements-sk/define';
+import { html, TemplateResult } from 'lit-html';
+import dialogPolyfill from 'dialog-polyfill';
+
+import { $$ } from 'common-sk/modules/dom';
+
+import { ElementSk } from '../../../infra-sk/modules/ElementSk';
+import 'elements-sk/styles/buttons';
+
+// TODO(rmistry): Generate this using go2ts.
+export interface Issue {
+  id: string,
+  priority: string,
+  link: string,
+
+  slo_violation_reason: string,
+}
+
+export class BugsSLOPopupSk extends ElementSk {
+  private priToSLOIssues: Record<string, Issue[]> = {};
+
+  private dialog: HTMLDialogElement | null = null;
+
+
+  constructor() {
+    super(BugsSLOPopupSk.template);
+  }
+
+  private static template = (el: BugsSLOPopupSk) => html`
+    <dialog class=slo-dialog>
+      <button class=done @click=${el.closeClicked}>Close</button>
+      ${el.displayIssues()}
+    </dialog>
+    `;
+
+
+  connectedCallback(): void {
+    super.connectedCallback();
+    this._render();
+    this.dialog = $$('dialog', this);
+  }
+
+  // Opens up the dialog and displays the specified map of issues.
+  open(mapOfIssues: Record<string, Issue[]>): void {
+    this.priToSLOIssues = mapOfIssues;
+    this._render();
+    this.dialog!.showModal();
+    dialogPolyfill.registerDialog(this.dialog!);
+  }
+
+  private displayIssues(): TemplateResult[] {
+    const priorities = Object.keys(this.priToSLOIssues);
+    priorities.sort();
+    const prioritiesHTML: TemplateResult[] = [];
+    priorities.forEach((priority) => {
+      const issues = this.priToSLOIssues[priority];
+      const issuesHTML: TemplateResult[] = [];
+      issues.forEach((issue) => {
+        issuesHTML.push(html`
+          <tr>
+            <td>
+              <a href=${issue.link} target=_blank>${issue.id}</a>
+            </td>
+            <td>
+              ${issue.slo_violation_reason}
+            </td>
+          </tr>
+        `);
+      });
+      prioritiesHTML.push(html`
+        <span class=priority-name>Priority: ${priority}</span>
+        <table class=slo-table>
+          ${issuesHTML}
+        </table>
+      `);
+    });
+    return prioritiesHTML;
+  }
+
+  private closeClicked() {
+    this.dialog!.close();
+  }
+}
+
+define('bugs-slo-popup-sk', BugsSLOPopupSk);
