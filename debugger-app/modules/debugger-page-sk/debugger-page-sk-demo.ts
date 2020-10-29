29c883d8503221b9e605380b68b7d3778ec67170
Nathaniel Nifong <nifong@google.com>
2020-10-29 11:03:21 -0400

    Add histogram module to debugger
    Change-Id: Ic3abbd09920d06293d876a86ea5eb096e58b6a40

--- debugger-app/modules/debugger-page-sk/debugger-page-sk-demo.ts
+++ debugger-app/modules/debugger-page-sk/debugger-page-sk-demo.ts
@@ -1,1 +1,17 @@
 import './index';
+
+// Since this is the only module which can load a file, puppeteer screenshot tests of all modules
+// go through it.
+
+
+// TODO(nifong): load a test file in the application.
+
+// document
+//   .querySelector('histogram-sk')!
+//   .addEventListener('some-event-name', (e) => {
+//     document.querySelector('#events')!.textContent = JSON.stringify(
+//       e,
+//       null,
+//       '  '
+//     );
+//   });
