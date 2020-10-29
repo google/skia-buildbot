2aa9b3a5c9e37ff4f44f03e37518ce7d2e8310db
Ravi Mistry <rmistry@google.com>
2020-10-29 13:07:54 -0400

    [bugs-central] Aliases watcher
    Change-Id: Ia0e5cca3589f376bfa314dafcb60fdd4b1453a04

---
+++ bugs-central/go/watcher/watcher.go
@@ -0,0 +1,116 @@
+package watcher
+
+// Watches different clients+issue frameworks for:
+// * Open issues assigned to a rotation alias. The issue is then re-assigned to
+//   the person on rotation.
+// * Maybe in the future will be used to ping issues beyond SLO.
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+
+	"golang.org/x/oauth2"
+
+	"go.skia.org/infra/bugs-central/go/bugs"
+	"go.skia.org/infra/bugs-central/go/bugs/monorail"
+	"go.skia.org/infra/go/baseapp"
+	"go.skia.org/infra/go/cleanup"
+	"go.skia.org/infra/go/httputils"
+	"go.skia.org/infra/go/rotations"
+	"go.skia.org/infra/go/skerr"
+	"go.skia.org/infra/go/sklog"
+)
+
+var (
+	aliasCommentTemplate = `Issue update by bugs-central.skia.org\n\nAuto-assigning the issue from %s to the current %s from %s.`
+)
+
+type aliasToWatch struct {
+	rotationAlias string
+	rotationURL   string
+	comment       string
+	bugFramework  bugs.BugFramework
+}
+
+// Watcher will be used to watch for specific issues in the different issue frameworks.
+type Watcher struct {
+	httpClient               *http.Client
+	pathToServiceAccountFile string
+}
+
+// New returns an instance of Watcher.
+func New(ctx context.Context, ts oauth2.TokenSource, pathToServiceAccountFile string) (*Watcher, error) {
+	httpClient := httputils.DefaultClientConfig().WithTokenSource(ts).With2xxOnly().Client()
+
+	return &Watcher{
+		httpClient:               httpClient,
+		pathToServiceAccountFile: pathToServiceAccountFile,
+	}, nil
+}
+
+// Start watches different clients+issue frameworks for:
+// * Open issues assigned to a rotation alias. The issue is then re-assigned to
+//   the person on rotation.
+// * Maybe in the future will be used to ping issues beyond SLO.
+func (p *Watcher) Start(ctx context.Context, pollInterval time.Duration) error {
+
+	// Collect all aliases to watch and then poll them.
+	aliasesToWatch := []aliasToWatch{}
+
+	//////////////////// Skia - Monorail - Wrangler reassigner ////////////////////
+	wranglerAlias := "skia-gpu-wrangler@google.com"
+	wranglerRotation := "https://tree-status.skia.org/current-wrangler"
+	wranglerQueryConfig := &monorail.MonorailQueryConfig{
+		Instance: "skia",
+		Query:    "is:open owner:skia-gpu-wrangler@google.com",
+		Client:   "Skia",
+	}
+	skMonorailWrangler, err := monorail.New(ctx, p.pathToServiceAccountFile, nil, wranglerQueryConfig)
+	if err != nil {
+		return skerr.Wrapf(err, "failed to init monorail for skia")
+	}
+	aliasesToWatch = append(aliasesToWatch, aliasToWatch{
+		rotationAlias: wranglerAlias,
+		rotationURL:   wranglerRotation,
+		comment:       fmt.Sprintf(aliasCommentTemplate, wranglerAlias, "wrangler", wranglerRotation),
+		bugFramework:  skMonorailWrangler,
+	})
+
+	cleanup.Repeat(pollInterval, func(ctx context.Context) {
+		if !*baseapp.Local {
+			// Ignore the passed-in context; this allows us to continue running even if the
+			// context is canceled due to transient errors.
+			ctx = context.Background()
+		}
+
+		for _, a := range aliasesToWatch {
+			issues, _, err := a.bugFramework.Search(ctx)
+			if err != nil {
+				sklog.Errorf("Error when searching issues: %s", err)
+				return
+			}
+			emails, err := rotations.FromURL(p.httpClient, a.rotationURL)
+			if err != nil {
+				sklog.Errorf("Error getting the current rotation: %s", err)
+				return
+			}
+			// Skia rotations always have exactly one person on each rotation.
+			if len(emails) != 1 {
+				sklog.Errorf("Got %d emails from %s. Expected 1.", len(emails), a.rotationURL)
+				return
+			}
+			rotationEmail := emails[0]
+
+			for _, i := range issues {
+				fmt.Println(i.Link)
+				if err := skMonorailWrangler.SetOwnerAndAddComment(rotationEmail, a.comment, i.Id); err != nil {
+					sklog.Fatal(err)
+				}
+			}
+		}
+	}, nil)
+
+	return nil
+}
