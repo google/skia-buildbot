2651377d3e6edf05857a32ed85779e44a2cfa13c
Eric Boren <borenet@google.com>
2020-10-30 12:12:54 -0400

    [task scheduler] Fix some theming problems
    Include the theme chooser and support dark mode on all demo pages.
    
    Change-Id: I6e5bba43b6a5ae899c3cb116f9b26e8eb4a0d37b

--- task_scheduler/modules/gantt-chart-sk/gantt-chart-sk-demo.ts
+++ task_scheduler/modules/gantt-chart-sk/gantt-chart-sk-demo.ts
@@ -1,5 +1,6 @@
 import './index';
 import { draw, Data } from './gantt-chart-sk';
+import '../../../infra-sk/modules/theme-chooser-sk';
 
 // This container holds the active chart, if any.
 let container: HTMLDivElement | null = null;
@@ -8,45 +9,45 @@ let container: HTMLDivElement | null = null;
 const clearAndDraw = (data: Data) => {
   // Delete and recreate the container div so that we render from scratch.
   container?.remove();
-  container = document.createElement("div");
-  container.id = "container";
-  container.style.height = "500px";
-  container.style.width = "1000px";
+  container = document.createElement('div');
+  container.id = 'container';
+  container.style.height = '500px';
+  container.style.width = '1000px';
   document.body.appendChild(container);
 
   // Draw the new chart.
   draw(container, data);
-}
+};
 
 // getData returns some placeholder data for a chart.
 const getData = (): Data => {
   return {
     lanes: [
       {
-        label: "lane1",
+        label: 'lane1',
         blocks: [
           {
-            start: new Date("1970-01-19T12:54:10.000Z"),
-            end:   new Date("1970-01-19T12:54:20.000Z"),
-            color: "blue",
-            label: "block1",
+            start: new Date('1970-01-19T12:54:10.000Z'),
+            end: new Date('1970-01-19T12:54:20.000Z'),
+            color: 'blue',
+            label: 'block1',
           },
         ],
       },
       {
-        label: "lane2",
+        label: 'lane2',
         blocks: [
           {
-            start: new Date("1970-01-19T12:54:11.000Z"),
-            end:   new Date("1970-01-19T12:54:12.000Z"),
-            color: "red",
-            label: "block1",
+            start: new Date('1970-01-19T12:54:11.000Z'),
+            end: new Date('1970-01-19T12:54:12.000Z'),
+            color: 'red',
+            label: 'block1',
           },
           {
-            start: new Date("1970-01-19T12:54:13.000Z"),
-            end:   new Date("1970-01-19T12:54:14.000Z"),
-            color: "green",
-            label: "block2",
+            start: new Date('1970-01-19T12:54:13.000Z'),
+            end: new Date('1970-01-19T12:54:14.000Z'),
+            color: 'green',
+            label: 'block2',
           },
         ],
       },
@@ -55,35 +56,41 @@ const getData = (): Data => {
     end: undefined,
     epochs: undefined,
   };
-}
+};
 
 // Set up event handlers for the buttons to render charts.
 
-document.getElementById("simple")!.addEventListener("click", (ev: MouseEvent) => {
-  clearAndDraw(getData());
-});
+document
+  .getElementById('simple')!
+  .addEventListener('click', (ev: MouseEvent) => {
+    clearAndDraw(getData());
+  });
 
-document.getElementById("simple-start-end")!.addEventListener("click", (ev: MouseEvent) => {
-  const data = getData();
-  data.start = new Date("1970-01-19T12:54:05.000Z");
-  data.end = new Date("1970-01-19T12:54:25.000Z");
-  clearAndDraw(data);
-});
+document
+  .getElementById('simple-start-end')!
+  .addEventListener('click', (ev: MouseEvent) => {
+    const data = getData();
+    data.start = new Date('1970-01-19T12:54:05.000Z');
+    data.end = new Date('1970-01-19T12:54:25.000Z');
+    clearAndDraw(data);
+  });
 
-document.getElementById("simple-epochs")!.addEventListener("click", (ev: MouseEvent) => {
-  const data = getData();
-  data.epochs = [
-    new Date("1970-01-19T12:54:10.000Z"),
-    new Date("1970-01-19T12:54:11.000Z"),
-    new Date("1970-01-19T12:54:12.000Z"),
-    new Date("1970-01-19T12:54:13.000Z"),
-    new Date("1970-01-19T12:54:14.000Z"),
-    new Date("1970-01-19T12:54:15.000Z"),
-    new Date("1970-01-19T12:54:16.000Z"),
-    new Date("1970-01-19T12:54:17.000Z"),
-    new Date("1970-01-19T12:54:18.000Z"),
-    new Date("1970-01-19T12:54:19.000Z"),
-    new Date("1970-01-19T12:54:20.000Z"),
-  ];
-  clearAndDraw(data);
-});
+document
+  .getElementById('simple-epochs')!
+  .addEventListener('click', (ev: MouseEvent) => {
+    const data = getData();
+    data.epochs = [
+      new Date('1970-01-19T12:54:10.000Z'),
+      new Date('1970-01-19T12:54:11.000Z'),
+      new Date('1970-01-19T12:54:12.000Z'),
+      new Date('1970-01-19T12:54:13.000Z'),
+      new Date('1970-01-19T12:54:14.000Z'),
+      new Date('1970-01-19T12:54:15.000Z'),
+      new Date('1970-01-19T12:54:16.000Z'),
+      new Date('1970-01-19T12:54:17.000Z'),
+      new Date('1970-01-19T12:54:18.000Z'),
+      new Date('1970-01-19T12:54:19.000Z'),
+      new Date('1970-01-19T12:54:20.000Z'),
+    ];
+    clearAndDraw(data);
+  });
