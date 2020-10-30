c6411007b5d812e17a5e332e75d9231b697fee3f
Eric Boren <borenet@google.com>
2020-10-30 08:22:39 -0400

    [task scheduler] Switch to new Lit-HTML / TypeScript UI
    Change-Id: I6f516ccc6b8ef89b76a549801c6584a0189689c9
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/330104
    Commit-Queue: Eric Boren <borenet@google.com>
    Reviewed-by: Weston Tracey <westont@google.com>

--- task_scheduler/go/task-scheduler-fe/main.go
+++ task_scheduler/go/task-scheduler-fe/main.go
@@ -7,15 +7,14 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"os"
 	"path/filepath"
-	"runtime"
 	"time"
 
 	"cloud.google.com/go/bigtable"
 	"cloud.google.com/go/datastore"
 	"cloud.google.com/go/pubsub"
 	"github.com/gorilla/mux"
-	swarming_api "go.chromium.org/luci/common/api/swarming/swarming/v1"
 	"go.skia.org/infra/go/allowed"
 	"go.skia.org/infra/go/auth"
 	"go.skia.org/infra/go/cleanup"
@@ -86,46 +85,33 @@ var (
 )
 
 func reloadTemplates() {
-	// Change the current working directory to two directories up from this source file so that we
-	// can read templates and serve static (res/) files.
 	if *resourcesDir == "" {
-		_, filename, _, _ := runtime.Caller(0)
-		*resourcesDir = filepath.Join(filepath.Dir(filename), "../..")
+		wd, err := os.Getwd()
+		if err != nil {
+			sklog.Fatal(err)
+		}
+		*resourcesDir = filepath.Join(filepath.Dir(wd), "dist")
 	}
-	skipTasksTemplate = template.Must(template.ParseFiles(
-		filepath.Join(*resourcesDir, "templates/skip_tasks.html"),
-		filepath.Join(*resourcesDir, "templates/header.html"),
-		filepath.Join(*resourcesDir, "templates/footer.html"),
-	))
 	jobTemplate = template.Must(template.ParseFiles(
-		filepath.Join(*resourcesDir, "templates/job.html"),
-		filepath.Join(*resourcesDir, "templates/header.html"),
-		filepath.Join(*resourcesDir, "templates/footer.html"),
+		filepath.Join(*resourcesDir, "job.html"),
 	))
 	jobSearchTemplate = template.Must(template.ParseFiles(
-		filepath.Join(*resourcesDir, "templates/job_search.html"),
-		filepath.Join(*resourcesDir, "templates/header.html"),
-		filepath.Join(*resourcesDir, "templates/footer.html"),
+		filepath.Join(*resourcesDir, "job_search.html"),
 	))
 	jobTimelineTemplate = template.Must(template.ParseFiles(
-		filepath.Join(*resourcesDir, "templates/job_timeline.html"),
-		filepath.Join(*resourcesDir, "templates/header.html"),
-		filepath.Join(*resourcesDir, "templates/footer.html"),
+		filepath.Join(*resourcesDir, "job_timeline.html"),
 	))
 	mainTemplate = template.Must(template.ParseFiles(
-		filepath.Join(*resourcesDir, "templates/main.html"),
-		filepath.Join(*resourcesDir, "templates/header.html"),
-		filepath.Join(*resourcesDir, "templates/footer.html"),
+		filepath.Join(*resourcesDir, "index.html"),
+	))
+	skipTasksTemplate = template.Must(template.ParseFiles(
+		filepath.Join(*resourcesDir, "skip_tasks.html"),
 	))
 	taskTemplate = template.Must(template.ParseFiles(
-		filepath.Join(*resourcesDir, "templates/task.html"),
-		filepath.Join(*resourcesDir, "templates/header.html"),
-		filepath.Join(*resourcesDir, "templates/footer.html"),
+		filepath.Join(*resourcesDir, "task.html"),
 	))
 	triggerTemplate = template.Must(template.ParseFiles(
-		filepath.Join(*resourcesDir, "templates/trigger.html"),
-		filepath.Join(*resourcesDir, "templates/header.html"),
-		filepath.Join(*resourcesDir, "templates/footer.html"),
+		filepath.Join(*resourcesDir, "job_trigger.html"),
 	))
 }
 
@@ -137,11 +123,7 @@ func mainHandler(w http.ResponseWriter, r *http.Request) {
 	if *local {
 		reloadTemplates()
 	}
-	if err := mainTemplate.Execute(w, struct {
-		Data string
-	}{
-		Data: "",
-	}); err != nil {
+	if err := mainTemplate.Execute(w, nil); err != nil {
 		httputils.ReportError(w, err, "Failed to execute template.", http.StatusInternalServerError)
 		return
 	}
@@ -188,53 +170,6 @@ func triggerHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func jsonSkipTasksHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	if r.Method == http.MethodDelete {
-		var msg struct {
-			Id string `json:"id"`
-		}
-		if err := json.NewDecoder(r.Body).Decode(&msg); err != nil {
-			httputils.ReportError(w, err, fmt.Sprintf("Failed to decode request body: %s", err), http.StatusInternalServerError)
-			return
-		}
-		defer util.Close(r.Body)
-		if err := skipTasks.RemoveRule(msg.Id); err != nil {
-			httputils.ReportError(w, err, fmt.Sprintf("Failed to delete skip rule: %s", err), http.StatusInternalServerError)
-			return
-		}
-	} else if r.Method == http.MethodPost {
-		var rule skip_tasks.Rule
-		if err := json.NewDecoder(r.Body).Decode(&rule); err != nil {
-			httputils.ReportError(w, err, fmt.Sprintf("Failed to decode request body: %s", err), http.StatusInternalServerError)
-			return
-		}
-		defer util.Close(r.Body)
-		rule.AddedBy = login.LoggedInAs(r)
-		if len(rule.Commits) == 2 {
-			rangeRule, err := skip_tasks.NewCommitRangeRule(context.Background(), rule.Name, rule.AddedBy, rule.Description, rule.TaskSpecPatterns, rule.Commits[0], rule.Commits[1], repos)
-			if err != nil {
-				httputils.ReportError(w, err, fmt.Sprintf("Failed to create commit range rule: %s", err), http.StatusInternalServerError)
-				return
-			}
-			rule = *rangeRule
-		}
-		if err := skipTasks.AddRule(&rule, repos); err != nil {
-			httputils.ReportError(w, err, fmt.Sprintf("Failed to add skip rule: %s", err), http.StatusInternalServerError)
-			return
-		}
-	}
-	resp := &struct {
-		Rules []*skip_tasks.Rule `json:"rules"`
-	}{
-		Rules: skipTasks.GetRules(),
-	}
-	if err := json.NewEncoder(w).Encode(resp); err != nil {
-		httputils.ReportError(w, err, fmt.Sprintf("Failed to encode response: %s", err), http.StatusInternalServerError)
-		return
-	}
-}
-
 // makeJob creates a Job for the given repo, revision, and name.
 func makeJob(ctx context.Context, repo, revision, jobName string) (*types.Job, error) {
 	j, err := taskCfgCache.MakeJob(ctx, types.RepoState{
@@ -248,131 +183,6 @@ func makeJob(ctx context.Context, repo, revision, jobName string) (*types.Job, e
 	j.IsForce = true
 	sklog.Infof("Created manually-triggered Job %q", j.Id)
 	return j, nil
-}
-
-func jsonTriggerHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	if r.Method == "OPTIONS" {
-		return
-	}
-	var msg []struct {
-		Name   string `json:"name"`
-		Commit string `json:"commit"`
-	}
-	defer util.Close(r.Body)
-	if err := json.NewDecoder(r.Body).Decode(&msg); err != nil {
-		httputils.ReportError(w, err, fmt.Sprintf("Failed to decode request body: %s", err), http.StatusInternalServerError)
-		return
-	}
-	jobs := make([]*types.Job, 0, len(msg))
-	for _, j := range msg {
-		_, repoName, _, err := repos.FindCommit(j.Commit)
-		if err != nil {
-			httputils.ReportError(w, err, "Unable to find the given commit in any repo.", http.StatusInternalServerError)
-			return
-		}
-		job, err := makeJob(r.Context(), repoName, j.Commit, j.Name)
-		if err != nil {
-			httputils.ReportError(w, err, "Failed to trigger jobs.", http.StatusInternalServerError)
-			return
-		}
-		jobs = append(jobs, job)
-	}
-	if err := tsDb.PutJobsInChunks(jobs); err != nil {
-		httputils.ReportError(w, err, "Failed to insert jobs.", http.StatusInternalServerError)
-		return
-	}
-	ids := make([]string, 0, len(jobs))
-	for _, job := range jobs {
-		ids = append(ids, job.Id)
-	}
-	if err := json.NewEncoder(w).Encode(ids); err != nil {
-		httputils.ReportError(w, err, "Failed to encode response.", http.StatusInternalServerError)
-		return
-	}
-}
-
-func jsonJobHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	id, ok := mux.Vars(r)["id"]
-	if !ok {
-		httputils.ReportError(w, nil, "Job ID is required.", http.StatusInternalServerError)
-		return
-	}
-
-	// Retrieve the Job from the DB.
-	job, err := tsDb.GetJobById(id)
-	if err != nil {
-		if err == db.ErrNotFound {
-			http.Error(w, "Unknown Job", 404)
-			return
-		}
-		httputils.ReportError(w, err, "Error retrieving Job.", http.StatusInternalServerError)
-		return
-	}
-	if job == nil {
-		http.Error(w, "Unknown Job", 404)
-		return
-	}
-
-	// Retrieve the task specs, so that we can include the task dimensions
-	// in the results.
-	cfg, err := taskCfgCache.Get(r.Context(), job.RepoState)
-	if err != nil {
-		httputils.ReportError(w, err, "Failed to retrieve Tasks cfg", http.StatusInternalServerError)
-		return
-	}
-	dimsByTask := make(map[string][]string, len(job.Dependencies))
-	for taskName := range job.Dependencies {
-		taskSpec, ok := cfg.Tasks[taskName]
-		if !ok {
-			httputils.ReportError(w, fmt.Errorf("Job %s (%s) points to unknown task %q at repo state: %+v", job.Id, job.Name, taskName, job.RepoState), "Job points to unknown task", http.StatusInternalServerError)
-			return
-		}
-		dimsByTask[taskName] = taskSpec.Dimensions
-	}
-
-	// Encode the response.
-	if err := json.NewEncoder(w).Encode(struct {
-		*types.Job
-		TaskDimensions map[string][]string `json:"taskDimensions"`
-	}{
-		Job:            job,
-		TaskDimensions: dimsByTask,
-	}); err != nil {
-		httputils.ReportError(w, err, "Failed to encode response.", http.StatusInternalServerError)
-		return
-	}
-}
-
-func jsonCancelJobHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	id, ok := mux.Vars(r)["id"]
-	if !ok {
-		httputils.ReportError(w, nil, "Job ID is required.", http.StatusInternalServerError)
-		return
-	}
-
-	job, err := tsDb.GetJobById(id)
-	if err != nil {
-		httputils.ReportError(w, err, "Failed to retrieve job.", http.StatusInternalServerError)
-		return
-	}
-	if job.Done() {
-		err := fmt.Errorf("Job %s is already finished with status %s", id, job.Status)
-		httputils.ReportError(w, err, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	job.Finished = time.Now()
-	job.Status = types.JOB_STATUS_CANCELED
-	if err := tsDb.PutJob(job); err != nil {
-		httputils.ReportError(w, err, "Failed to insert Job", http.StatusInternalServerError)
-		return
-	}
-	if err := json.NewEncoder(w).Encode(job); err != nil {
-		httputils.ReportError(w, err, "Failed to encode response.", http.StatusInternalServerError)
-		return
-	}
 }
 
 func jobHandler(w http.ResponseWriter, r *http.Request) {
@@ -438,52 +248,10 @@ func jobTimelineHandler(w http.ResponseWriter, r *http.Request) {
 		httputils.ReportError(w, nil, "Job ID is required.", http.StatusInternalServerError)
 		return
 	}
-
-	job, err := tsDb.GetJobById(jobId)
-	if err != nil {
-		httputils.ReportError(w, err, "Failed to retrieve Job.", http.StatusInternalServerError)
-		return
-	}
-	type unifiedTask struct {
-		*types.Task
-		Swarming *swarming_api.SwarmingRpcsTaskResult `json:"swarming"`
-	}
-	var tasks = make([]*unifiedTask, 0, len(job.Tasks))
-	for _, summaries := range job.Tasks {
-		for _, t := range summaries {
-			task, err := tsDb.GetTaskById(t.Id)
-			if err != nil {
-				httputils.ReportError(w, err, "Failed to retrieve Task.", http.StatusInternalServerError)
-				return
-			}
-			swarmingTask, err := swarm.GetTask(task.SwarmingTaskId, true)
-			if err != nil {
-				httputils.ReportError(w, err, "Failed to retrieve Swarming task.", http.StatusInternalServerError)
-				return
-			}
-			tasks = append(tasks, &unifiedTask{
-				Task:     task,
-				Swarming: swarmingTask,
-			})
-		}
-	}
-	enc, err := json.Marshal(&struct {
-		Job    *types.Job     `json:"job"`
-		Tasks  []*unifiedTask `json:"tasks"`
-		Epochs []time.Time    `json:"epochs"`
-	}{
-		Job:    job,
-		Tasks:  tasks,
-		Epochs: []time.Time{}, // TODO(borenet): Record tick timestamps.
-	})
-	if err != nil {
-		httputils.ReportError(w, err, "Failed to encode JSON.", http.StatusInternalServerError)
-		return
-	}
 	if err := jobTimelineTemplate.Execute(w, struct {
-		Data string
+		JobId string
 	}{
-		Data: string(enc),
+		JobId: jobId,
 	}); err != nil {
 		httputils.ReportError(w, err, "Failed to execute template.", http.StatusInternalServerError)
 		return
@@ -517,84 +285,6 @@ func taskHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func jsonGetTaskHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	id, ok := mux.Vars(r)["id"]
-	if !ok {
-		httputils.ReportError(w, nil, "Task ID is required.", http.StatusInternalServerError)
-		return
-	}
-
-	task, err := tsDb.GetTaskById(id)
-	if err != nil {
-		if err == db.ErrNotFound {
-			http.Error(w, "Unknown Task", 404)
-			return
-		}
-		httputils.ReportError(w, err, "Error retrieving Job.", http.StatusInternalServerError)
-		return
-	}
-	if err := json.NewEncoder(w).Encode(task); err != nil {
-		httputils.ReportError(w, err, "Failed to encode response.", http.StatusInternalServerError)
-		return
-	}
-}
-
-// jsonJobSearchHandler allows for searching Jobs based on various parameters.
-func jsonJobSearchHandler(w http.ResponseWriter, r *http.Request) {
-	var params db.JobSearchParams
-	if err := httputils.ParseFormValues(r, &params); err != nil {
-		httputils.ReportError(w, err, "Failed to parse request parameters.", http.StatusInternalServerError)
-		return
-	}
-	jobs, err := db.SearchJobs(tsDb, &params)
-	if err != nil {
-		httputils.ReportError(w, err, "Failed to search for jobs.", http.StatusInternalServerError)
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	if err := json.NewEncoder(w).Encode(jobs); err != nil {
-		httputils.ReportError(w, err, fmt.Sprintf("Failed to encode response: %s", err), http.StatusInternalServerError)
-		return
-	}
-}
-
-// jsonTaskSearchHandler allows searching for Tasks based on various parameters.
-func jsonTaskSearchHandler(w http.ResponseWriter, r *http.Request) {
-	var params db.TaskSearchParams
-	if err := httputils.ParseFormValues(r, &params); err != nil {
-		httputils.ReportError(w, err, "Failed to parse request parameters.", http.StatusInternalServerError)
-		return
-	}
-	tasks, err := db.SearchTasks(tsDb, &params)
-	if err != nil {
-		httputils.ReportError(w, err, "Failed to search for tasks.", http.StatusInternalServerError)
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	if err := json.NewEncoder(w).Encode(tasks); err != nil {
-		httputils.ReportError(w, err, fmt.Sprintf("Failed to encode response: %s", err), http.StatusInternalServerError)
-		return
-	}
-}
-
-// jsonTaskCandidateSearchHandler allows for searching task candidates based on
-// their TaskKey.
-// TODO(borenet): Re-enable this if/when candidates have their own DB.
-/*func jsonTaskCandidateSearchHandler(w http.ResponseWriter, r *http.Request) {
-	var params scheduling.TaskCandidateSearchTerms
-	if err := httputils.ParseFormValues(r, &params); err != nil {
-		httputils.ReportError(w, err, "Failed to parse request parameters.", http.StatusInternalServerError)
-		return
-	}
-	candidates := ts.SearchQueue(&params)
-	w.Header().Set("Content-Type", "application/json")
-	if err := json.NewEncoder(w).Encode(candidates); err != nil {
-		httputils.ReportError(w, err, fmt.Sprintf("Failed to encode response: %s", err), http.StatusInternalServerError)
-		return
-	}
-}*/
-
 func googleVerificationHandler(w http.ResponseWriter, r *http.Request) {
 	if _, err := w.Write([]byte("google-site-verification: google2c59f97e1ced9fdc.html")); err != nil {
 		httputils.ReportError(w, err, fmt.Sprintf("Failed to write response: %s", err), http.StatusInternalServerError)
@@ -605,21 +295,14 @@ func googleVerificationHandler(w http.ResponseWriter, r *http.Request) {
 func runServer(serverURL string, srv http.Handler) {
 	r := mux.NewRouter()
 	r.HandleFunc("/", httputils.OriginTrial(mainHandler, *local))
+	r.PathPrefix("/dist/").Handler(http.StripPrefix("/dist/", http.HandlerFunc(httputils.MakeResourceHandler(*resourcesDir))))
+	r.PathPrefix(rpc.TaskSchedulerServicePathPrefix).Handler(srv)
 	r.HandleFunc("/skip_tasks", httputils.OriginTrial(skipTasksHandler, *local))
 	r.HandleFunc("/job/{id}", httputils.OriginTrial(jobHandler, *local))
 	r.HandleFunc("/job/{id}/timeline", httputils.OriginTrial(jobTimelineHandler, *local))
 	r.HandleFunc("/jobs/search", httputils.OriginTrial(jobSearchHandler, *local))
 	r.HandleFunc("/task/{id}", httputils.OriginTrial(taskHandler, *local))
 	r.HandleFunc("/trigger", httputils.OriginTrial(triggerHandler, *local))
-	r.HandleFunc("/json/skip_tasks", login.RestrictEditorFn(jsonSkipTasksHandler)).Methods(http.MethodPost, http.MethodDelete)
-	r.HandleFunc("/json/job/{id}", jsonJobHandler)
-	r.HandleFunc("/json/job/{id}/cancel", login.RestrictEditorFn(jsonCancelJobHandler)).Methods(http.MethodPost)
-	r.HandleFunc("/json/jobs/search", jsonJobSearchHandler)
-	r.HandleFunc("/json/task/{id}", jsonGetTaskHandler)
-	// TODO(borenet): Re-enable this if/when candidates have their own DB.
-	//r.HandleFunc("/json/taskCandidates/search", jsonTaskCandidateSearchHandler)
-	r.HandleFunc("/json/tasks/search", jsonTaskSearchHandler)
-	r.HandleFunc("/json/trigger", login.RestrictEditorFn(jsonTriggerHandler)).Methods(http.MethodPost, http.MethodOptions)
 	r.HandleFunc("/google2c59f97e1ced9fdc.html", googleVerificationHandler)
 	r.PathPrefix(rpc.TaskSchedulerServicePathPrefix).Handler(srv)
 	r.PathPrefix("/res/").HandlerFunc(httputils.MakeResourceHandler(*resourcesDir))
