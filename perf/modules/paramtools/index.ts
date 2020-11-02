3575c70d60d581f419efe7a210e8ca9377d8fe75
Joe Gregorio <jcgregorio@google.com>
2020-11-02 11:14:39 -0500

    [perf] Add some paramtools implementations in TS.
    Bug: skia:10844
    Change-Id: Ie4093e4e0d2096ce48a371d35087171a72f12905

---
+++ perf/modules/paramtools/index.ts
@@ -0,0 +1,43 @@
+// Contains mirror functions to /infra/go/paramtools. See that module for more
+// details.
+//
+// All the validation is done on the server, so these functions do less checking
+// on the validity of structured keys and Params.
+import { Params, ParamSet } from '../json';
+
+/** Create a structured key from a Params. */
+export function makeKey(params: Params): string {
+  if (Object.keys(params).length === 0) {
+    throw new Error('Params must have at least one entry');
+  }
+  const keys = Object.keys(params).sort();
+  return `,${keys.map((key) => `${key}=${params[key]}`).join(',')},`;
+}
+
+/** Parse a structured key into a Params. */
+export function fromKey(structuredKey: string): Params {
+  const ret: Params = {};
+  structuredKey.split(',').forEach((keyValue) => {
+    if (!keyValue) {
+      return;
+    }
+    const [key, value] = keyValue.split('=');
+    ret[key] = value;
+  });
+  return ret;
+}
+
+/** Add the given Params to the ParamSet. */
+export function addParamsToParamSet(ps: ParamSet, p: Params): void {
+  Object.entries(p).forEach((keyValue) => {
+    const [key, value] = keyValue;
+    let values = ps[key];
+    if (!values) {
+      values = [];
+    }
+    if (values.indexOf(value) === -1) {
+      values.push(value);
+    }
+    ps[key] = values;
+  });
+}
---
+++ perf/modules/paramtools/index_test.ts
@@ -0,0 +1,56 @@
+import { assert } from 'chai';
+import { ParamSet } from '../json';
+import { addParamsToParamSet, fromKey, makeKey } from './index';
+
+describe('paramtooms', () => {
+  describe('makeKey', () => {
+    it('constructs a key correctly', () => {
+      assert.equal(',a=1,b=2,c=3,', makeKey({ b: '2', a: '1', c: '3' }));
+    });
+
+    it('throws on empty Params', () => {
+      assert.throw(() => makeKey({}));
+    });
+  });
+
+  describe('fromKey', () => {
+    it('parses a key correctly', () => {
+      assert.deepEqual({ b: '2', a: '1', c: '3' }, fromKey(',a=1,b=2,c=3,'));
+    });
+
+    it('handles empty string as key', () => {
+      assert.deepEqual({}, fromKey(''));
+    });
+  });
+
+
+  describe('addParamsToParamSet', () => {
+    it('works on empty values', () => {
+      const ps: ParamSet = {};
+      addParamsToParamSet(ps, {});
+      assert.deepEqual({}, ps);
+    });
+
+    it('handles duplicate keys and values', () => {
+      const ps: ParamSet = {};
+      addParamsToParamSet(ps, { a: '1' });
+      addParamsToParamSet(ps, { a: '1' });
+      assert.deepEqual({ a: ['1'] }, ps);
+    });
+
+    it('handles distinct keys and values', () => {
+      const ps: ParamSet = {};
+      addParamsToParamSet(ps, { a: '1' });
+      addParamsToParamSet(ps, { b: '2' });
+      assert.deepEqual({ a: ['1'], b: ['2'] }, ps);
+    });
+
+    it('handles distinct keys and multiple values', () => {
+      const ps: ParamSet = {};
+      addParamsToParamSet(ps, { a: '1' });
+      addParamsToParamSet(ps, { b: '2' });
+      addParamsToParamSet(ps, { b: '3' });
+      assert.deepEqual({ a: ['1'], b: ['2', '3'] }, ps);
+    });
+  });
+});
