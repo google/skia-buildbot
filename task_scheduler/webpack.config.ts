a2c47e4add3fcfca717862c6335a3c05bcb89b9b
Eric Boren <borenet@google.com>
2020-11-03 09:48:05 -0500

    [task scheduler] Delete old UI code, support favicon
    Change-Id: I8cb6344896d20efc4a13d9006ec78e173e293ae0
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/331489
    Reviewed-by: Weston Tracey <westont@google.com>
    Commit-Queue: Eric Boren <borenet@google.com>

--- task_scheduler/webpack.config.ts
+++ task_scheduler/webpack.config.ts
@@ -1,10 +1,19 @@
 import { resolve } from 'path';
 import webpack from 'webpack';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
 import commonBuilder from '../infra-sk/pulito/webpack.common';
 
 const configFactory: webpack.ConfigurationFactory = (_, args) => {
   const config = commonBuilder(__dirname, args.mode);
   config.output!.publicPath = '/dist/';
+  config.plugins!.push(
+    new CopyWebpackPlugin([
+      {
+        from: resolve(__dirname, 'res/img/favicon.ico'),
+        to: 'favicon.ico',
+      },
+    ])
+  );
   config.resolve = config.resolve || {};
   // https://github.com/webpack/node-libs-browser/issues/26#issuecomment-267954095
   config.resolve.modules = [resolve(__dirname, 'node_modules'), 'node_modules'];
