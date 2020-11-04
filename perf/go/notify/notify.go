2414f3bb449fa7917914e0ebf7a54440d7a8db2a
Joe Gregorio <jcgregorio@google.com>
2020-11-04 16:16:17 -0500

    [perf] Fix notify email.
    Bug: skia:10743
    Change-Id: I04f2c4e2619c304f9f9ed8b0a7de966c4a28b17d

--- perf/go/notify/notify.go
+++ perf/go/notify/notify.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"html/template"
 	"regexp"
+	"time"
 
 	"go.skia.org/infra/go/sklog"
 	"go.skia.org/infra/perf/go/alerts"
@@ -13,6 +14,8 @@ import (
 	perfgit "go.skia.org/infra/perf/go/git"
 	"go.skia.org/infra/perf/go/stepfit"
 )
+
+var timeNow = time.Now
 
 const (
 	fromAddress = "alertserver@skia.org"
@@ -31,7 +34,11 @@ const (
 </p>
 <p>
 	With {{.Cluster.Num}} matching traces.
-</p>`
+</p>
+<p>
+   And direction {{.Cluster.StepFit.Status}}.
+</p>
+`
 )
 
 var (
@@ -113,7 +120,7 @@ func (n *Notifier) Send(c perfgit.Commit, alert *alerts.Alert, cl *clustering2.C
 	if err != nil {
 		return err
 	}
-	subject := fmt.Sprintf("%s - Regression found for %q", alert.DisplayName, c.Subject)
+	subject := fmt.Sprintf("%s - Regression found for %s", alert.DisplayName, c.Display(timeNow()))
 	if _, err := n.email.Send(fromAddress, splitEmails(alert.Alert), subject, body, ""); err != nil {
 		return fmt.Errorf("Failed to send email: %s", err)
 	}
@@ -124,9 +131,10 @@ func (n *Notifier) Send(c perfgit.Commit, alert *alerts.Alert, cl *clustering2.C
 // ExampleSend sends an example for dummy data for the given alerts.Config.
 func (n *Notifier) ExampleSend(alert *alerts.Alert) error {
 	c := perfgit.Commit{
-		Subject: "Re-enable opList dependency tracking",
-		URL:     "https://skia.googlesource.com/skia/+show/d261e1075a93677442fdf7fe72aba7e583863664",
-		GitHash: "d261e1075a93677442fdf7fe72aba7e583863664",
+		Subject:   "Re-enable opList dependency tracking",
+		URL:       "https://skia.googlesource.com/skia/+show/d261e1075a93677442fdf7fe72aba7e583863664",
+		GitHash:   "d261e1075a93677442fdf7fe72aba7e583863664",
+		Timestamp: 1498176000,
 	}
 	cl := &clustering2.ClusterSummary{
 		Num: 10,
--- perf/go/notify/notify_test.go
+++ perf/go/notify/notify_test.go
@@ -2,6 +2,7 @@ package notify
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 	"go.skia.org/infra/go/testutils/unittest"
@@ -34,10 +35,13 @@ func TestExampleSend(t *testing.T) {
 		Alert:       "someone@example.org, someother@example.com ",
 		DisplayName: "MyAlert",
 	}
+	timeNow = func() time.Time {
+		return time.Date(2020, 04, 01, 0, 0, 0, 0, time.UTC)
+	}
 	err := n.ExampleSend(alert)
 	assert.NoError(t, err)
 	assert.Equal(t, []string{"someone@example.org", "someother@example.com"}, e.to)
 	assert.Equal(t, fromAddress, e.from)
-	assert.Equal(t, "MyAlert - Regression found for \"Re-enable opList dependency tracking\"", e.subject)
-	assert.Equal(t, "<b>Alert</b><br><br>\n<p>\n\tA Perf Regression (High) has been found at:\n</p>\n<p style=\"padding: 1em;\">\n\t<a href=\"https://perf.skia.org/g/t/d261e1075a93677442fdf7fe72aba7e583863664\">https://perf.skia.org/g/t/d261e1075a93677442fdf7fe72aba7e583863664</a>\n</p>\n<p>\n  For:\n</p>\n<p style=\"padding: 1em;\">\n  <a href=\"https://skia.googlesource.com/skia/&#43;show/d261e1075a93677442fdf7fe72aba7e583863664\">https://skia.googlesource.com/skia/&#43;show/d261e1075a93677442fdf7fe72aba7e583863664</a>\n</p>\n<p>\n\tWith 10 matching traces.\n</p>", e.body)
+	assert.Equal(t, "MyAlert - Regression found for d261e10 -  2y 40w - Re-enable opList dependency tracking", e.subject)
+	assert.Equal(t, "<b>Alert</b><br><br>\n<p>\n\tA Perf Regression (High) has been found at:\n</p>\n<p style=\"padding: 1em;\">\n\t<a href=\"https://perf.skia.org/g/t/d261e1075a93677442fdf7fe72aba7e583863664\">https://perf.skia.org/g/t/d261e1075a93677442fdf7fe72aba7e583863664</a>\n</p>\n<p>\n  For:\n</p>\n<p style=\"padding: 1em;\">\n  <a href=\"https://skia.googlesource.com/skia/&#43;show/d261e1075a93677442fdf7fe72aba7e583863664\">https://skia.googlesource.com/skia/&#43;show/d261e1075a93677442fdf7fe72aba7e583863664</a>\n</p>\n<p>\n\tWith 10 matching traces.\n</p>\n<p>\n   And direction High.\n</p>\n", e.body)
 }
