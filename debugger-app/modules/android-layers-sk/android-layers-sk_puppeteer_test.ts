33337b8dffea2f73f31bc74e6af900a64070fc80
Nathaniel Nifong <nifong@google.com>
2020-11-09 16:48:04 -0500

    Add module for android layers
    TODO: fix lint errors, check over for console logs and trash
    
    Change-Id: Ib53e8d6f43b39b7103189e7a82d82b6c50a26b80

---
+++ debugger-app/modules/android-layers-sk/android-layers-sk_puppeteer_test.ts
@@ -0,0 +1,27 @@
+import * as path from 'path';
+import { expect } from 'chai';
+import {
+  setUpPuppeteerAndDemoPageServer,
+  takeScreenshot,
+} from '../../../puppeteer-tests/util';
+
+describe('android-layers-sk', () => {
+  const testBed = setUpPuppeteerAndDemoPageServer(
+    path.join(__dirname, '..', '..', 'webpack.config.ts')
+  );
+
+  beforeEach(async () => {
+    await testBed.page.goto(`${testBed.baseUrl}/dist/android-layers-sk.html`);
+    await testBed.page.setViewport({ width: 400, height: 550 });
+  });
+
+  it('should render the demo page (smoke test)', async () => {
+    expect(await testBed.page.$$('android-layers-sk')).to.have.length(1);
+  });
+
+  describe('screenshots', () => {
+    it('shows the default view', async () => {
+      await takeScreenshot(testBed.page, 'debugger-app', 'android-layers-sk');
+    });
+  });
+});
