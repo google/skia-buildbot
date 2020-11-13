507c15b74cdab58072ef50ef1e8cdd04f765048c
Joe Gregorio <jcgregorio@google.com>
2020-11-13 11:35:55 -0500

    [perf] TypeScript client that work with go/progress.
    Change-Id: I31492f66476534c1827e8ce3461d93659650fbe1

---
+++ perf/modules/progress/progress.ts
@@ -0,0 +1,41 @@
+// A module to start and monitor the progress of long running server tasks.
+
+import { jsonOrThrow } from 'common-sk/modules/jsonOrThrow';
+import { SpinnerSk } from 'elements-sk/spinner-sk/spinner-sk';
+import { progress } from '../json';
+
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+export const startRequest = (startingURL: string, body: any, period: number, spinner: SpinnerSk): Promise<progress.SerializedProgress> => new Promise<progress.SerializedProgress>((resolve, reject) => {
+  spinner.active = true;
+
+  // Regardless if this is the first fetch, or any of the subsequent polling
+  // fetches, we do the same exact processing on the Promise, so consolidate all
+  // the functionality into a single function.
+  const processFetch = (fetchPromise: Promise<Response>) => {
+    fetchPromise
+      .then(jsonOrThrow)
+      .then((json: progress.SerializedProgress) => {
+        if (json.status === 'Running') {
+          window.setTimeout(() => {
+            processFetch(fetch(json.url, {
+              method: 'GET',
+            }));
+          }, period);
+        } else {
+          resolve(json);
+        }
+      }).catch((msg) => {
+        spinner.active = false;
+        reject(msg);
+      });
+  };
+
+  // Make the initial request that starts the polling process.
+  processFetch(fetch(startingURL, {
+    method: 'POST',
+    body: JSON.stringify(body),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }));
+});
