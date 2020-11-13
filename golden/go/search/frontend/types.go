5b52361ff9fbee9c2f5cfbf9d7fb970f260056b2
Kevin Lubick <kjlubick@google.com>
2020-11-13 13:46:46 -0500

    [gold] Clean up diff metrics.
    Change-Id: I0881d1be40e8f274f93cf3d071617751c067dab7

--- golden/go/search/frontend/types.go
+++ golden/go/search/frontend/types.go
@@ -7,7 +7,6 @@ import (
 
 	"go.skia.org/infra/go/paramtools"
 	"go.skia.org/infra/golden/go/comment/trace"
-	"go.skia.org/infra/golden/go/diff"
 	"go.skia.org/infra/golden/go/expectations"
 	"go.skia.org/infra/golden/go/search/common"
 	"go.skia.org/infra/golden/go/tiling"
@@ -72,7 +71,22 @@ type SearchResult struct {
 // The primary digest is generally shown on the left in the frontend UI, and the data here
 // represents a digest on the right that the primary digest is being compared to.
 type SRDiffDigest struct {
-	*diff.DiffMetrics
+	// NumDiffPixels is the absolute number of pixels that are different.
+	NumDiffPixels int `json:"numDiffPixels"`
+
+	// CombinedMetric is a value in [0, 1] that represents how large the diff is between two
+	// images. It is based off the MaxRGBADiffs and PixelDiffPercent
+	CombinedMetric float32 `json:"combinedMetric"`
+
+	// PixelDiffPercent is the percentage of pixels that are different.
+	PixelDiffPercent float32 `json:"pixelDiffPercent"`
+
+	// MaxRGBADiffs contains the maximum difference of each channel.
+	MaxRGBADiffs [4]int `json:"maxRGBADiffs"`
+
+	// DimDiffer is true if the dimensions between the two images are different.
+	DimDiffer bool `json:"dimDiffer"`
+
 	// Digest identifies which image we are comparing the primary digest to. Put another way, what
 	// is the image on the right side of the comparison.
 	Digest types.Digest `json:"digest"`
