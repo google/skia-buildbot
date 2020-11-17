669d6887c88969de8cd79f9cbea4c111c02e2f65
Leandro Lovisolo <lovisolo@google.com>
2020-11-17 03:43:06 -0500

    [infra-sk] theme-chooser-sk.ts: Add Puppeteer test.
    I'll be using this to experiment with Puppeteer tests on Bazel. I chose this component because I've already added Bazel build rules for it.
    
    Bug: skia:10614
    Change-Id: I9ec240d6329c35a5bbed373fda8857368f097d7f
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/335536
    Reviewed-by: Joe Gregorio <jcgregorio@google.com>
    Commit-Queue: Leandro Lovisolo <lovisolo@google.com>

---
+++ infra-sk/modules/theme-chooser-sk/theme-chooser-sk_puppeteer_test.ts
@@ -0,0 +1,33 @@
+import * as path from 'path';
+import { expect } from 'chai';
+import {
+  setUpPuppeteerAndDemoPageServer,
+  takeScreenshot,
+} from '../../../puppeteer-tests/util';
+
+describe('theme-chooser-sk', () => {
+  const testBed = setUpPuppeteerAndDemoPageServer(
+    path.join(__dirname, '..', '..', 'webpack.config.ts')
+  );
+
+  beforeEach(async () => {
+    await testBed.page.goto(`${testBed.baseUrl}/theme-chooser-sk.html`);
+    await testBed.page.setViewport({ width: 400, height: 400 });
+  });
+
+  it('should render the demo page', async () => {
+    // Smoke test.
+    expect(await testBed.page.$$('theme-chooser-sk')).to.have.length(1);
+  });
+
+  describe('screenshots', () => {
+    it('shows the default theme', async () => {
+      await takeScreenshot(testBed.page, 'infra-sk', 'theme-chooser-sk_default');
+    });
+
+    it('shows the dark theme', async () => {
+      await testBed.page.click('theme-chooser-sk');
+      await takeScreenshot(testBed.page, 'infra-sk', 'theme-chooser-sk_dark');
+    });
+  });
+});
