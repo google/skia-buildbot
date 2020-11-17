e6084d365b118cba1fe3e9c6a50ee886635d13a4
Kevin Lubick <kjlubick@google.com>
2020-11-17 10:32:36 -0500

    [gold] Add bespoke string map marshaler
    The goal is to be faster than the default impl because a lot of JSON
    maps get created and hashed when ingesting for SQL.
    
    This is about 2x faster at the moment, could be better with memory
    pooling, some smarter sorting, or some stack memory allocations.
    
    Bug: skia:10582
    Change-Id: I5e4f81557d2584921b03ed6b5b4859a5176cb96c
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/334919
    Commit-Queue: Kevin Lubick <kjlubick@google.com>
    Reviewed-by: Joe Gregorio <jcgregorio@google.com>

--- go/jsonutils/jsonutils_test.go
+++ go/jsonutils/jsonutils_test.go
@@ -5,7 +5,10 @@ import (
 	"testing"
 	"time"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+
+	"go.skia.org/infra/go/testutils"
 	"go.skia.org/infra/go/testutils/unittest"
 )
 
@@ -100,5 +103,89 @@ func TestTime(t *testing.T) {
 		err = json.Unmarshal(b, &got)
 		require.NoError(t, err)
 		require.Equal(t, tc.in.UTC(), time.Time(got).UTC())
+	}
+}
+func TestMarshalStringMap_NonEmptyMap_MatchesBuiltInImpl(t *testing.T) {
+	unittest.MediumTest(t)
+	input := map[string]string{}
+	testutils.MustReadJsonFile("mediumparams.json", &input)
+	require.Len(t, input, 50)
+	actual := MarshalStringMap(input)
+	expected, err := json.Marshal(input)
+	require.NoError(t, err)
+	assert.Equal(t, expected, actual, "%s != %s", string(expected), string(actual))
+
+	input2 := map[string]string{}
+	testutils.MustReadJsonFile("smallparams.json", &input2)
+	require.Len(t, input2, 10)
+	actual = MarshalStringMap(input2)
+	expected, err = json.Marshal(input2)
+	require.NoError(t, err)
+	assert.Equal(t, expected, actual, "%s != %s", string(expected), string(actual))
+}
+
+func TestMarshalStringMap_EmptyMap_MatchesBuiltInImpl(t *testing.T) {
+	unittest.SmallTest(t)
+	input := map[string]string{}
+	actual := MarshalStringMap(input)
+	expected, err := json.Marshal(input)
+	require.NoError(t, err)
+	assert.Equal(t, expected, actual, "%s != %s", string(expected), string(actual))
+}
+
+func TestMarshalStringMap_NilMap_MatchesBuiltInImpl(t *testing.T) {
+	unittest.SmallTest(t)
+	var input map[string]string
+	actual := MarshalStringMap(input)
+	expected, err := json.Marshal(input)
+	require.NoError(t, err)
+	assert.Equal(t, expected, actual, "%s != %s", string(expected), string(actual))
+}
+
+func BenchmarkMarshalStringMap_10Items(b *testing.B) {
+	input := map[string]string{}
+	testutils.MustReadJsonFile("smallparams.json", &input)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		b := MarshalStringMap(input)
+		if b[0] == 'N' {
+			panic("this is to keep the call from being optimized out")
+		}
+	}
+}
+
+func BenchmarkMarshalStringMap_50Items(b *testing.B) {
+	input := map[string]string{}
+	testutils.MustReadJsonFile("mediumparams.json", &input)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		b := MarshalStringMap(input)
+		if b[0] == 'N' {
+			panic("this is to keep the call from being optimized out")
+		}
+	}
+}
+
+func BenchmarkBuiltInJSONMarshal_10Items(b *testing.B) {
+	input := map[string]string{}
+	testutils.MustReadJsonFile("smallparams.json", &input)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		b, _ := json.Marshal(input)
+		if b[0] == 'N' {
+			panic("this is to keep the call from being optimized out")
+		}
+	}
+}
+
+func BenchmarkBuiltInJSONMarshal_50Items(b *testing.B) {
+	input := map[string]string{}
+	testutils.MustReadJsonFile("mediumparams.json", &input)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		b, _ := json.Marshal(input)
+		if b[0] == 'N' {
+			panic("this is to keep the call from being optimized out")
+		}
 	}
 }
