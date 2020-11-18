ef13ab665a55210e2f09f1e17ce8d247e56da814
Weston Tracey <westont@google.com>
2020-11-18 09:42:40 -0500

    [status] Make close button on dialog with task driver easier to hit.
    Change-Id: Ic3eb7cd028c0e95f266fc7ade53a2a28fbbe9ff7

--- go/jsonutils/jsonutils.go
+++ go/jsonutils/jsonutils.go
@@ -3,6 +3,7 @@ package jsonutils
 import (
 	"bytes"
 	"encoding/json"
+	"sort"
 	"strconv"
 	"time"
 )
@@ -38,4 +39,54 @@ func (t *Time) UnmarshalJSON(data []byte) error {
 	}
 	*t = Time(time.Unix(0, int64(timeN)*int64(time.Microsecond)).UTC())
 	return nil
+}
+
+// MarshalStringMap turns the given string map into a []byte slice that is the JSON encoded version
+// of that map. It will produce the same output as json.Marshal. Unlike json.Marshal, it does not
+// return an error because the errors json.Marshal could return (e.g. for cyclic data) do not apply.
+func MarshalStringMap(m map[string]string) (data []byte) {
+	if len(m) == 0 {
+		// This behavior matches json.Marshal
+		if m == nil {
+			return []byte("null")
+		}
+		return []byte("{}")
+	}
+	keyValues := make([]string, 0, len(m))
+	byteCount := 0
+	var buf bytes.Buffer
+	for k, v := range m {
+		buf.WriteRune('"')
+		buf.WriteString(k)
+		buf.WriteString(`":"`)
+		buf.WriteString(v)
+		buf.WriteRune('"')
+		keyValues = append(keyValues, buf.String())
+		byteCount += buf.Len()
+		buf.Reset()
+	}
+	// sort for determinism and to match the default impl
+	// We go with insertion sort unless there are a lot of values.
+	if len(keyValues) <= 30 {
+		for i := 0; i < len(keyValues); i++ {
+			for j := i; j > 0 && keyValues[j] < keyValues[j-1]; j-- {
+				keyValues[j], keyValues[j-1] = keyValues[j-1], keyValues[j]
+			}
+		}
+	} else {
+		sort.Strings(keyValues)
+	}
+
+	var result bytes.Buffer
+	// Need to account for an open and closed curly brace, and n-1 commas
+	result.Grow(2 + byteCount + len(keyValues) - 1)
+	result.WriteRune('{')
+	for i := range keyValues {
+		if i != 0 {
+			result.WriteRune(',')
+		}
+		result.WriteString(keyValues[i])
+	}
+	result.WriteRune('}')
+	return result.Bytes()
 }
