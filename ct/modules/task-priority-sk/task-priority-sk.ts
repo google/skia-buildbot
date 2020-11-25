5ab3cbe14b988a7c962641e878367985ac84fe80
Ravi Mistry <rmistry@google.com>
2020-11-25 10:24:45 -0500

    [CT] admin-task-runs-sk -> TS
    Bug: skia:10920
    Change-Id: I6ee31fac353c3cb279bcdf70727ac0a39fdc981e

---
+++ ct/modules/task-priority-sk/task-priority-sk.ts
@@ -0,0 +1,71 @@
+/**
+ * @module modules/task-priority-sk
+ * @description A custom element for allowing the user to select their
+ * task's priority.
+ */
+
+import { $$ } from 'common-sk/modules/dom';
+import { define } from 'elements-sk/define';
+import { html } from 'lit-html';
+import { jsonOrThrow } from 'common-sk/modules/jsonOrThrow';
+import { errorMessage } from 'elements-sk/errorMessage';
+
+import {
+  SelectSk,
+} from 'elements-sk/select-sk/select-sk';
+
+import { ElementSk } from '../../../infra-sk/modules/ElementSk';
+import 'elements-sk/select-sk';
+
+import {
+  TaskPrioritiesResponse,
+} from '../json';
+
+export class TaskPrioritySk extends ElementSk {
+private _priorities: string[][] = [];
+
+private _selector: SelectSk | null = null;
+
+constructor() {
+  super(TaskPrioritySk.template);
+  this._priorities = [];
+}
+
+  private static template = (el: TaskPrioritySk) => html`
+<div class=tr-container>
+  <select-sk>
+    ${el._priorities.map((p) => html`
+    <div>${p[1]}</div>`)}
+  </select-sk>
+</div>
+`;
+
+  connectedCallback(): void {
+    super.connectedCallback();
+    fetch('/_/task_priorities/', { method: 'GET' })
+      .then(jsonOrThrow)
+      .then((json: TaskPrioritiesResponse) => {
+        // { 'p1' : 'Desc1', ...}  -> [['p1', 'Desc1'], ...]
+        this._priorities = Object.entries(json.task_priorities);
+        this._render();
+        this._selector!.selection = 1;
+      })
+      .catch(errorMessage);
+    this._render();
+    this._selector = $$('select-sk', this);
+  }
+
+  /**
+   * @prop {string} priority - Priority string representing user selected
+   * priority of their task.
+   */
+  get priority(): string {
+    return this._priorities[this._selector!.selection as number][0];
+  }
+
+  set priority(val: string) {
+    this._selector!.selection = this._priorities.findIndex((p) => p[0] === val);
+  }
+}
+
+define('task-priority-sk', TaskPrioritySk);
