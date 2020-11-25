b2a71fe4eef4e4c4027cfb1bcab94a62dc14968a
Leandro Lovisolo <lovisolo@google.com>
2020-11-24 21:19:50 -0500

    [bazel] Simplify support for Karma tests.
    This CL gets rid of the karma_mocha_test rule, which is a fork of the karma_web_test[1] rule in rules_nodejs that uses Mocha instead of Jasmine, and replaces it with a much simpler karma_test macro.
    
    Rationale:
    
    The karma_web_test rule is based on a google3 rule, and is hardcoded to use Google's concatjs bundler, which requires all dependencies to be provided as UMD modules. (The karma_web_test rule is actually scheduled to be renamed as concatjs_web_test[2] because of this; see this discussion on the Bazel Slack[3].)
    
    To make the karma_mocha_test rule work, I had to add npm_umd_bundle rules to generate UMD bundles for all NPM dependencies used by our tests, and each individual karma_mocha_test target had to explicitly list all its direct and indirect NPM dependencies as UMD modules. This was tedious and error prone.
    
    Solution:
    
    This CL replaces said rule with a new, much simpler karma_test rule that only takes as inputs the test's *.ts files and a list of any *direct* ts_library dependencies. This rule uses the rollup_bundle rule (as do our rules for demo and production HTML pages), which bundles the test code and its transitive dependency closure (including any NPM modules) as a single JS file. The resulting JS bundle is then passed to the Karma test runner as its only input file.
    
    The resulting rule uses the same pipeline (ts_library -> rollup_bundle -> output JS) as our sk_page rule, which makes things easier to reason about, and leads to much shorter BUILD rules (usually just a *.ts file and a small list of ts_library deps).
    
    See //infra-sk/modules/theme-chooser-sk/BUILD.bazel for a before/after example.
    
    [1] https://bazelbuild.github.io/rules_nodejs/Karma.html#karma_web_test
    [2] https://github.com/bazelbuild/rules_nodejs/issues/2254
    [3] https://bazelbuild.slack.com/archives/CSACXKUBE/p1604350233020900
    
    Bug: skia:10614
    Change-Id: I84de0eef5ff9730769edb27f19e96e8b45ae9eed

---
+++ infra-sk/karma_test/karma.conf.js
@@ -0,0 +1,73 @@
+// The path to the JS bundle is passed by the karma_test rule as the last command-line argument to
+// the Karma binary.
+const jsTestFile = process.argv[process.argv.length - 1];
+
+// Detect whether we're running as a test (e.g. "bazel test //path/to/my:karma_test").
+//
+// https://github.com/bazelbuild/rules_nodejs/blob/681c6683dac742f1e375a401c0399ec7783ac8fd/packages/karma/karma_web_test.bzl#L257
+const isBazelTest = !process.env.BUILD_WORKSPACE_DIRECTORY; // Set when running via "bazel run".
+
+module.exports = function (config) {
+  config.set({
+    plugins: [
+      'karma-chrome-launcher',
+      'karma-sinon',
+      'karma-mocha',
+      'karma-chai',
+      'karma-chai-dom',
+      'karma-spec-reporter',
+    ],
+
+    // Frameworks are loaded in reverse order, so chai-dom loads after chai.
+    frameworks: ['mocha', 'chai-dom', 'chai', 'sinon'],
+
+    files: [{
+      pattern: jsTestFile,
+      // Force the test files to be served from disk on each request. Without this, interactive mode
+      // with ibazel does not work (e.g. "ibazel run //path/to/my:karma_test").
+      nocache: true,
+    }],
+
+    // Only use a headless browser when running as a test (i.e. "bazel test").
+    //
+    // Do not launch any browsers when running interactively (i.e. with "bazel run"). The developer
+    // can open the printed out URL for the Karma server in their browser of choice.
+    browsers: isBazelTest ? ['ChromeHeadlessCustom'] : [],
+
+    // Headless Chrome that works on the RBE toolchain container, and any other Docker container.
+    //
+    // https://github.com/puppeteer/puppeteer/blob/master/docs/troubleshooting.md#running-puppeteer-in-docker
+    customLaunchers: {
+      ChromeHeadlessCustom: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox', '--disable-dev-shm-usage', '--disable-gpu'],
+      },
+    },
+
+    // Report the outcome of each individual test case to stdout.
+    //
+    // This is consistent with the output format of Puppeteer tests (and any other Node.js tests
+    // using Mocha).
+    colors: true,
+    reporters: ['spec'],
+
+    // Run tests only once when invoked via "bazel test", otherwise Karma will run forever and Bazel
+    // will eventually time out and report test failure.
+    singleRun: isBazelTest,
+
+    // Autowatch is disabled because it is currently broken in two major ways:
+    //
+    //   1. It doesn't force a browser refresh when files change.
+    //   2. Sometimes it randomly reports changed files as deleted, and stops executing the contents
+    //      of those files for the remainder of the session.
+    //
+    // ibazel can be used to achieve a similar workflow:
+    //
+    //   - `ibazel test //path/to/my:karma_test`: Reruns the tests on a headless Chrome every time
+    //     the tests change, and reports the results on the console.
+    //   - `ibazel run //path/to/my:karma_test`: Interactive mode. Prints out a URL that can be
+    //     opened in the browser. Tests are rebuilt automatically when the code changes. Reload the
+    //     page manually to see the changes.
+    autoWatch: false,
+  });
+};
