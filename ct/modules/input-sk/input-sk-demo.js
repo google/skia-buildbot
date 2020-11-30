09232feb31e0f6935931bf59d408b15601046b43
Ravi Mistry <rmistry@google.com>
2020-11-30 13:32:05 -0500

    [CT] admin-task-runs-sk -> TS
    Bug: skia:10920
    Change-Id: I6ee31fac353c3cb279bcdf70727ac0a39fdc981e

--- ct/modules/input-sk/input-sk-demo.js
+++
@@ -1,7 +0,0 @@
-import './index';
-import '../../../infra-sk/modules/theme-chooser-sk';
-import { $$ } from 'common-sk/modules/dom';
-
-const si = document.createElement('input-sk');
-si.label = 'Type something here';
-$$('#container').appendChild(si);
