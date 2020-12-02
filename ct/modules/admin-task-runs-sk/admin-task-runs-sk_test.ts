f57490cdeb8bd53688790194c41942b1f9a21cad
Ravi Mistry <rmistry@google.com>
2020-12-02 15:22:31 -0500

    [CT] admin-task-runs-sk -> TS
    Bug: skia:10920
    Change-Id: I006601bb03e6090189a5614193fcc08cb30c51d5

--- ct/modules/admin-task-runs-sk/admin-task-runs-sk_test.ts
+++ ct/modules/admin-task-runs-sk/admin-task-runs-sk_test.ts
@@ -1,7 +1,10 @@
 import './index';
 
+import sinon from 'sinon';
+import { expect } from 'chai';
 import { $, $$ } from 'common-sk/modules/dom';
-import { fetchMock } from 'fetch-mock';
+import fetchMock from 'fetch-mock';
+import { AdminTaskRunsSk } from './admin-task-runs-sk';
 
 import {
   tasksResult0, tasksResult1,
@@ -20,34 +23,35 @@ describe('admin-task-runs-sk', () => {
 
   fetchMock.config.overwriteRoutes = false;
 
-  let adminRuns;
+  let adminRuns: HTMLElement;
   beforeEach(async () => {
     await expectReload(() => adminRuns = newInstance(
-      (el) => {
-        el.taskType = taskType;
-        el.getUrl = getUrl;
-        el.deleteUrl = deleteUrl;
-        el.redoUrl = redoUrl;
-      }));
+      (el: HTMLElement) => {
+        const a = el as AdminTaskRunsSk;
+        a.taskType = taskType;
+        a.getUrl = getUrl;
+        a.deleteUrl = deleteUrl;
+        a.redoUrl = redoUrl;
+      },
+    ), null);
   });
 
-  afterEach(() => {
+  afterEach(async () => {
     //  Check all mock fetches called at least once and reset.
     expect(fetchMock.done()).to.be.true;
     fetchMock.reset();
+    sinon.restore();
   });
 
   // Expect 'trigger' to cause a reload, and execute it.
   // Optionally pass desired result from server.
-  const expectReload = async (trigger, result) => {
+  const expectReload = async (trigger: any, result: any) => {
     result = result || tasksResult0;
     const event = eventPromise('end-task');
     fetchMock.postOnce(`begin:${getUrl}`, result);
     trigger();
     await event;
   };
-
-  const confirmDialog = () => $$('dialog', adminRuns).querySelectorAll('button')[1].click();
 
   it('shows table entries', async () => {
     expect($('table.runssummary>tbody>tr', adminRuns)).to.have.length(11);
@@ -59,13 +63,13 @@ describe('admin-task-runs-sk', () => {
 
   it('filters by user', async () => {
     expect(fetchMock.lastUrl()).to.not.contain('filter_by_logged_in_user=true');
-    await expectReload(() => $$('#userFilter', adminRuns).click());
+    await expectReload(() => ($$('#userFilter', adminRuns)! as HTMLElement).click(), null);
     expect(fetchMock.lastUrl()).to.contain('filter_by_logged_in_user=true');
   });
 
   it('filters by tests', async () => {
     expect(fetchMock.lastUrl()).to.contain('exclude_dummy_page_sets=true');
-    await expectReload(() => $$('#testFilter', adminRuns).click());
+    await expectReload(() => ($$('#testFilter', adminRuns)! as HTMLElement).click(), null);
     expect(fetchMock.lastUrl()).to.not.contain('exclude_dummy_page_sets=true');
   });
 
@@ -75,22 +79,27 @@ describe('admin-task-runs-sk', () => {
     result.pagination.offset = 10;
     // 'Next page' button.
     await expectReload(
-      () => $('pagination-sk button.action', adminRuns)[2].click(), result);
+      () => ($('pagination-sk button.action', adminRuns)[2] as HTMLElement).click(), result,
+    );
     expect(fetchMock.lastUrl()).to.contain('offset=10');
     expect($('table.runssummary>tbody>tr', adminRuns)).to.have.length(5);
   });
 
   it('deletes tasks', async () => {
-    $$('delete-icon-sk', adminRuns).click();
+    sinon.stub(window, 'confirm').returns(true);
+    sinon.stub(window, 'alert');
     fetchMock.post(`begin:${deleteUrl}`, 200);
-    await expectReload(confirmDialog);
-    expect(fetchMock.lastOptions('begin:/_/delete').body).to.contain('"id":66');
+    fetchMock.postOnce(`begin:${getUrl}`, tasksResult0);
+    ($$('delete-icon-sk', adminRuns) as HTMLElement).click();
+    expect(fetchMock.lastOptions('begin:/_/delete')!.body).to.contain('"id":66');
   });
 
   it('reschedules tasks', async () => {
-    $$('redo-icon-sk', adminRuns).click();
+    sinon.stub(window, 'confirm').returns(true);
+    sinon.stub(window, 'alert');
     fetchMock.post(`begin:${redoUrl}`, 200);
-    await expectReload(confirmDialog);
-    expect(fetchMock.lastOptions('begin:/_/redo').body).to.contain('"id":66');
+    fetchMock.postOnce(`begin:${getUrl}`, tasksResult0);
+    ($$('redo-icon-sk', adminRuns) as HTMLElement).click();
+    expect(fetchMock.lastOptions('begin:/_/redo')!.body).to.contain('"id":66');
   });
 });
