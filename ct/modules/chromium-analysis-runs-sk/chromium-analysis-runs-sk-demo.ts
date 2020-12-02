d21afd22813d0904622d95ca5ae5e5b805b1ddad
Ravi Mistry <rmistry@google.com>
2020-12-02 15:17:20 -0500

    [CT] chromium-analysis-runs-sk -> TS
    Bug: skia:10920
    Change-Id: I7ae02193b4dbb4343ffff2e5aa599855e1ecb887

--- ct/modules/chromium-analysis-runs-sk/chromium-analysis-runs-sk-demo.ts
+++ ct/modules/chromium-analysis-runs-sk/chromium-analysis-runs-sk-demo.ts
@@ -1,7 +1,7 @@
 import './index';
 import '../../../infra-sk/modules/theme-chooser-sk';
 import { $$ } from 'common-sk/modules/dom';
-import { fetchMock } from 'fetch-mock';
+import fetchMock from 'fetch-mock';
 import {
   tasksResult0, tasksResult1, tasksResult2,
 } from './test_data';
@@ -12,4 +12,4 @@ fetchMock.post('begin:/_/get_chromium_analysis_tasks',
 fetchMock.post('begin:/_/delete_chromium_analysis_task', 200);
 fetchMock.post('begin:/_/redo_chromium_analysis_task', 200);
 const cpr = document.createElement('chromium-analysis-runs-sk');
-$$('#container').appendChild(cpr);
+($$('#container') as HTMLElement).appendChild(cpr);
