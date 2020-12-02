6980cca5aa1da7ac4846cf1c9f956422aa0feac5
Ravi Mistry <rmistry@google.com>
2020-12-02 14:23:37 -0500

    [CT] task-priority-sk -> TS
    Bug: skia:10920
    Change-Id: I27eefe1020d717ce93f9577e6e9e13480695a9b7

--- ct/modules/task-priority-sk/task-priority-sk_test.ts
+++ ct/modules/task-priority-sk/task-priority-sk_test.ts
@@ -1,12 +1,15 @@
 import './index';
 
+import { expect } from 'chai';
 import { $, $$ } from 'common-sk/modules/dom';
-import { fetchMock } from 'fetch-mock';
+import fetchMock from 'fetch-mock';
+import { SelectSk } from 'elements-sk/select-sk/select-sk';
 import { priorities } from './test_data';
 import { setUpElementUnderTest } from '../../../infra-sk/modules/test_util';
+import { TaskPrioritySk } from './task-priority-sk';
 
 describe('task-priority-sk', () => {
-  const factory = setUpElementUnderTest('task-priority-sk');
+  const factory = setUpElementUnderTest<TaskPrioritySk>('task-priority-sk');
   // Returns a new element with the pagesets fetch complete.
   const newInstance = async () => {
     const ele = factory();
@@ -33,7 +36,7 @@ describe('task-priority-sk', () => {
 
   it('reflects changes to selection', async () => {
     selector = await newInstance();
-    $$('select-sk', selector).selection = 2;
+    ($$('select-sk', selector) as SelectSk).selection = 2;
     expect(selector).to.have.property('priority', '110');
     selector.priority = '90';
     expect(selector).to.have.property('priority', '90');
