a61bc409224d3d6a2631981fcb13b066cd0c5690
Ravi Mistry <rmistry@google.com>
2020-12-02 14:49:06 -0500

    [CT] pageset-selector-sk -> TS
    Bug: skia:10920
    Change-Id: Id993060d915b170189350b853016227486745f2c

--- ct/modules/pageset-selector-sk/test_data.ts
+++ ct/modules/pageset-selector-sk/test_data.ts
@@ -1,3 +1,5 @@
+import { PageSet } from '../json';
+
 export const pageSets = [
   { key: '100k', description: 'Top 100K (with desktop user-agent)' },
   { key: 'Mobile100k', description: 'Top 100K (with mobile user-agent)' },
@@ -6,4 +8,4 @@ export const pageSets = [
   { key: 'Dummy1k', description: 'Top 1K (with desktop user-agent, for testing, hidden from Runs History by default)' },
   { key: 'DummyMobile1k', description: 'Top 1K (with mobile user-agent, for testing, hidden from Runs History by default)' },
   { key: 'All', description: 'Top 1M (with desktop user-agent)' },
-  { key: 'VoltMobile10k', description: 'Volt 10K (with mobile user-agent)' }];
+  { key: 'VoltMobile10k', description: 'Volt 10K (with mobile user-agent)' }] as PageSet[];
