09d0ab015c1eefc59f2640cf47f9efb8418499af
Weston Tracey <westont@google.com>
2020-12-03 13:08:20 -0500

    [status] Use earlier timestamp for 'from'.
    Change-Id: I460586684c9868b39415bcb2d15e297a516d1495

---
+++ perf/go/perf-tool/application/mocks/generate.go
@@ -0,0 +1,3 @@
+package mocks
+
+//go:generate mockery -name Application -dir ../ -output .
---
+++ perf/go/perf-tool/main_test.go
@@ -0,0 +1,38 @@
+// Command-line application for interacting with Perf.
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+	"go.skia.org/infra/go/testutils/unittest"
+	"go.skia.org/infra/machine/go/machineserver/config"
+	"go.skia.org/infra/perf/go/perf-tool/application/mocks"
+)
+
+func createInstanceConfigFile(t *testing.T) string {
+	instanceConfig := &config.InstanceConfig{}
+	f, err := ioutil.TempFile("", "perf-tool")
+	require.NoError(t, err)
+	t.Cleanup(func() { require.NoError(t, os.Remove(f.Name())) })
+	err = json.NewEncoder(f).Encode(instanceConfig)
+	require.NoError(t, err)
+	require.NoError(t, f.Close())
+	return f.Name()
+}
+
+func TestActualMain_ConfigCreatePubSubTopics_Success(t *testing.T) {
+	unittest.SmallTest(t)
+	app := &mocks.Application{}
+	app.On("ConfigCreatePubSubTopics", mock.AnythingOfType("*config.InstanceConfig")).Return(nil)
+
+	filename := createInstanceConfigFile(t)
+
+	os.Args = []string{"perf-tool", "config", "create-pubsub-topics", "--config_filename=" + filename}
+	actualMain(app)
+	app.AssertExpectations(t)
+}
