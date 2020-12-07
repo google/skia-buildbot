72fba1758f55ae3716f1251c463e2dee5b136061
Joe Gregorio <jcgregorio@google.com>
2020-12-07 12:31:25 -0500

    [perf] Remove cobra comment.
    Change-Id: I1de52b2690e3a73fc02b9cd4aa58874452ded392
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/341438
    Reviewed-by: Joe Gregorio <jcgregorio@google.com>

--- perf/go/perfserver/main.go
+++ perf/go/perfserver/main.go
@@ -1,9 +1,6 @@
 // perfserver is the single executable that contains the sub-commands that make
 // up a running Perf system, including the web ui, the ingestion process, and
 // the regression detection process.
-//
-// This cli is built using Cobra (https://github.com/spf13/cobra/) and the cobra
-// cli should be used to add new sub-commands.
 package main
 
 import (
