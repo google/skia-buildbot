f16c116602859f243568f2582b58d7a5f98db174
Ravi Mistry <rmistry@google.com>
2020-12-08 16:25:03 -0500

    [CT] suggest-input-sk -> TS
    Bug: skia:10920
    Change-Id: I654d9cf689797131f013b2a9ea962eb6f527e322
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/340225
    Reviewed-by: Weston Tracey <westont@google.com>

--- ct/modules/suggest-input-sk/suggest-input-sk-demo.ts
+++ ct/modules/suggest-input-sk/suggest-input-sk-demo.ts
@@ -2,12 +2,13 @@ import './index';
 import '../../../infra-sk/modules/theme-chooser-sk';
 import { $$ } from 'common-sk/modules/dom';
 import { languageList } from './test_data';
+import { SuggestInputSk } from './suggest-input-sk';
 
-function newTaskQueue(parentSelector) {
-  const si = document.createElement('suggest-input-sk');
+function newTaskQueue(parentSelector: string) {
+  const si = document.createElement('suggest-input-sk') as SuggestInputSk;
   si.options = languageList;
   si.label = 'Select a language';
-  $$(parentSelector).appendChild(si);
+  ($$(parentSelector) as HTMLElement).appendChild(si);
 }
 
 newTaskQueue('#container');
