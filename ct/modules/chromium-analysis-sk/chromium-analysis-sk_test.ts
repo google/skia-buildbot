c26447e30145a6b386296c0f19725768a9b0d90c
Ravi Mistry <rmistry@google.com>
2020-12-09 10:15:50 -0500

    [CT] chromium-analysis-sk -> TS
    Bug: skia:10920
    Change-Id: I1ff8f4586a7e9e449dcdb8f4fa984e108758e8ee
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/340301
    Reviewed-by: Weston Tracey <westont@google.com>

--- ct/modules/chromium-analysis-sk/chromium-analysis-sk_test.ts
+++ ct/modules/chromium-analysis-sk/chromium-analysis-sk_test.ts
@@ -1,11 +1,16 @@
 import './index';
 
-import { $, $$ } from 'common-sk/modules/dom';
-import { fetchMock } from 'fetch-mock';
+import sinon from 'sinon';
+import { $$ } from 'common-sk/modules/dom';
+import fetchMock from 'fetch-mock';
+import { expect } from 'chai';
+import { SelectSk } from 'elements-sk/select-sk/select-sk';
 import { benchmarks_platforms } from './test_data';
 import { pageSets } from '../pageset-selector-sk/test_data';
 import { priorities } from '../task-priority-sk/test_data';
 import { chromiumPatchResult } from '../patch-sk/test_data';
+import { InputSk } from '../input-sk/input-sk';
+import { ChromiumAnalysisSk } from './chromium-analysis-sk';
 import {
   eventPromise,
   setUpElementUnderTest,
@@ -13,34 +18,30 @@ import {
 
 describe('chromium-analysis-sk', () => {
   fetchMock.config.overwriteRoutes = false;
-  const factory = setUpElementUnderTest('chromium-analysis-sk');
+  const factory = setUpElementUnderTest<ChromiumAnalysisSk>('chromium-analysis-sk');
   // Returns a new element with the pagesets, task priorirites, and
   // active tasks fetches complete, and benchmarks and platforms set.
-  const newInstance = async (activeTasks, init) => {
+  const newInstance = async (activeTasks?: number) => {
     fetchMock.postOnce('begin:/_/page_sets/', pageSets);
     fetchMock.postOnce('begin:/_/benchmarks_platforms/', benchmarks_platforms);
     fetchMock.getOnce('begin:/_/task_priorities/', priorities);
     mockActiveTasks(activeTasks);
-    const wrappedInit = (ele) => {
-      if (init) {
-        init(ele);
-      }
-    };
-    const ele = factory(wrappedInit);
+    const ele = factory();
     await fetchMock.flush(true);
     return ele;
   };
 
   // Make our test object global to make helper functions convenient.
-  let chromiumAnalysis;
+  let chromiumAnalysis: ChromiumAnalysisSk;
 
   afterEach(() => {
     //  Check all mock fetches called at least once and reset.
     expect(fetchMock.done()).to.be.true;
     fetchMock.reset();
+    sinon.restore();
   });
 
-  const mockActiveTasks = (n) => {
+  const mockActiveTasks = (n: number|undefined) => {
     n = n || 0;
     // For running tasks for the user we put a nonzero total in one of the
     // responses, and 0 in the remaining 6.
@@ -58,17 +59,17 @@ describe('chromium-analysis-sk', () => {
     }, { repeat: 6 });
   };
 
-  const setDescription = (d) => {
-    $$('#description', chromiumAnalysis).value = d;
+  const setDescription = (d: string) => {
+    ($$('#description', chromiumAnalysis) as InputSk).value = d;
   };
 
-  const setBenchmark = (b) => {
-    $$('#benchmark_name', chromiumAnalysis).value = b;
+  const setBenchmark = (b: string) => {
+    ($$('#benchmark_name', chromiumAnalysis) as InputSk).value = b;
   };
 
-  const setPatch = async (patchtype, value, response) => {
+  const setPatch = async (patchtype: string, value: string, response: any) => {
     fetchMock.postOnce('begin:/_/cl_data', response);
-    const input = $$(`#${patchtype}_patch input-sk`);
+    const input = $$(`#${patchtype}_patch input-sk`) as InputSk;
     input.value = value;
     input.dispatchEvent(new Event('input', {
       bubbles: true,
@@ -77,7 +78,7 @@ describe('chromium-analysis-sk', () => {
   };
 
   const clickSubmit = () => {
-    $$('#submit', chromiumAnalysis).click();
+    ($$('#submit', chromiumAnalysis) as InputSk).click();
   };
 
   const expectTaskTriggered = () => {
@@ -86,16 +87,16 @@ describe('chromium-analysis-sk', () => {
 
   it('loads, has defaults set', async () => {
     chromiumAnalysis = await newInstance();
-    expect(chromiumAnalysis._platforms[$$('#platform_selector', chromiumAnalysis)
-      .selection][0]).to.equal('Linux');
+    expect(chromiumAnalysis._platforms[+($$('#platform_selector', chromiumAnalysis) as SelectSk)!
+      .selection!][0]).to.equal('Linux');
     expect($$('#pageset_selector', chromiumAnalysis)).to.have.property('selected', '10k');
     expect($$('#pageset_selector', chromiumAnalysis)).to.have.property('customPages', '');
-    expect($$('#repeat_after_days', this)).to.have.property('frequency', '0');
-    expect($$('#task_priority', this)).to.have.property('priority', '100');
-    expect($$('#benchmark_args', this)).to.have.property('value',
+    expect($$('#repeat_after_days', chromiumAnalysis)).to.have.property('frequency', '0');
+    expect($$('#task_priority', chromiumAnalysis)).to.have.property('priority', '100');
+    expect($$('#benchmark_args', chromiumAnalysis)).to.have.property('value',
       '--output-format=csv --skip-typ-expectations-tags-validation'
       + ' --legacy-json-trace-format');
-    expect($$('#value_column_name', this)).to.have.property('value', 'avg');
+    expect($$('#value_column_name', chromiumAnalysis)).to.have.property('value', 'avg');
   });
 
   it('requires description', async () => {
@@ -103,7 +104,7 @@ describe('chromium-analysis-sk', () => {
     const event = eventPromise('error-sk');
     clickSubmit();
     const err = await event;
-    expect(err.detail.message).to.equal('Please specify a description');
+    expect((err as CustomEvent).detail.message).to.equal('Please specify a description');
   });
 
   it('requires benchmark', async () => {
@@ -112,7 +113,7 @@ describe('chromium-analysis-sk', () => {
     const event = eventPromise('error-sk');
     clickSubmit();
     const err = await event;
-    expect(err.detail.message).to.equal('Please specify a benchmark');
+    expect((err as CustomEvent).detail.message).to.equal('Please specify a benchmark');
   });
 
   it('rejects bad patch', async () => {
@@ -123,13 +124,13 @@ describe('chromium-analysis-sk', () => {
     let event = eventPromise('error-sk');
     clickSubmit();
     let err = await event;
-    expect(err.detail.message).to.contain('Unable to fetch skia patch from CL 1234');
+    expect((err as CustomEvent).detail.message).to.contain('Unable to fetch skia patch from CL 1234');
 
     await setPatch('skia', '1234', {}); // CL doesn't load.
     event = eventPromise('error-sk');
     clickSubmit();
     err = await event;
-    expect(err.detail.message).to.contain('Unable to load skia CL 1234');
+    expect((err as CustomEvent).detail.message).to.contain('Unable to load skia CL 1234');
   });
 
   it('triggers a new task', async () => {
@@ -137,38 +138,38 @@ describe('chromium-analysis-sk', () => {
     setDescription('Testing things');
     setBenchmark('a benchmark');
     await setPatch('chromium', '1234', chromiumPatchResult);
-    // Karma can't handle page reloads, so disable it.
-    chromiumAnalysis._gotoRunsHistory = () => { };
+    chromiumAnalysis._gotoRunsHistory = () => {
+      // Karma can't handle page reloads, so disable it.
+    };
     expectTaskTriggered();
+    sinon.stub(window, 'confirm').returns(true);
     clickSubmit();
-    $('#confirm_dialog button')[1].click(); // brittle way to press 'ok'
     await fetchMock.flush(true);
-    const taskJson = JSON.parse(fetchMock.lastOptions().body);
+    const taskJson = JSON.parse(fetchMock.lastOptions()!.body as any);
     // Here we test the 'interesting' arguments. We try a single patch,
     // and we don't bother filling in the simple string arguments.
     const expectation = {
+      apk_gs_path: '',
       benchmark: 'a benchmark',
-      page_sets: '10k',
-      match_stdout_txt: '',
       benchmark_args: '--output-format=csv --skip-typ-expectations-tags-validation --legacy-json-trace-format',
       browser_args: '',
-      value_column_name: 'avg',
-      desc: 'Testing https://chromium-review.googlesource.com/c/2222715/3 (Roll Skia from cc7ec24ca824 to 1dbc3b533962 (3 revisions))',
-      chromium_patch: '\n\ndiff --git a/DEPS b/DEPS\nindex 849ae22..ee07579 100644\n--- a/DEPS\n+++ b/DEPS\n@@ -178,7 +178,7 @@\n   # Three lines of non-changing comments so that\n   # the commit queue can handle CLs rolling Skia\n   # and whatever else without interference from each other.\n-  \'skia_revision\': \'cc7ec24ca824ca13d5a8a8e562fcec695ae54390\',\n+  \'skia_revision\': \'1dbc3b533962b0ae803a2a5ee89f61146228d73b\',\n   # Three lines of non-changing comments so that\n   # the commit queue can handle CLs rolling V8\n   # and whatever else without interference from each other.\n',
-      chromium_hash: '',
-      apk_gs_path: '',
-      telemetry_isolate_hash: '',
-      repeat_after_days: '0',
-      task_priority: '100',
-      run_in_parallel: true,
-      platform: 'Linux',
-      run_on_gce: true,
-      custom_webpages: '',
-      skia_patch: '',
-      v8_patch: '',
       catapult_patch: '',
+      chromium_hash: '',
+      chromium_patch: '\n\ndiff --git a/DEPS b/DEPS\nindex 849ae22..ee07579 100644\n--- a/DEPS\n+++ b/DEPS\n@@ -178,7 +178,7 @@\n   # Three lines of non-changing comments so that\n   # the commit queue can handle CLs rolling Skia\n   # and whatever else without interference from each other.\n-  \'skia_revision\': \'cc7ec24ca824ca13d5a8a8e562fcec695ae54390\',\n+  \'skia_revision\': \'1dbc3b533962b0ae803a2a5ee89f61146228d73b\',\n   # Three lines of non-changing comments so that\n   # the commit queue can handle CLs rolling V8\n   # and whatever else without interference from each other.\n',
+      custom_webpages: '',
+      desc: 'Testing https://chromium-review.googlesource.com/c/2222715/3 (Roll Skia from cc7ec24ca824 to 1dbc3b533962 (3 revisions))',
+      match_stdout_txt: '',
+      page_sets: '10k',
+      platform: 'Linux',
+      repeat_after_days: '0',
+      run_on_gce: true,
+      run_in_parallel: true,
+      skia_patch: '',
+      task_priority: '100',
+      telemetry_isolate_hash: '',
+      value_column_name: 'avg',
+      v8_patch: '',
     };
-
     expect(taskJson).to.deep.equal(expectation);
   });
 
@@ -180,6 +181,6 @@ describe('chromium-analysis-sk', () => {
     const event = eventPromise('error-sk');
     clickSubmit();
     const err = await event;
-    expect(err.detail.message).to.contain('You have 4 currently running tasks');
+    expect((err as CustomEvent).detail.message).to.contain('You have 4 currently running tasks');
   });
 });
