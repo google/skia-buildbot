a0d4eebab1e6bd87b7b893847075de73162518f1
Ravi Mistry <rmistry@google.com>
2020-12-09 09:55:52 -0500

    [CT] metrics-analysis-sk -> TS
    Bug: skia:10920
    Change-Id: Ia9cbdcf402e5e230bb864bbfa334790969324a09

--- ct/modules/metrics-analysis-sk/metrics-analysis-sk_test.ts
+++ ct/modules/metrics-analysis-sk/metrics-analysis-sk_test.ts
@@ -1,9 +1,13 @@
 import './index';
 
-import { $, $$ } from 'common-sk/modules/dom';
-import { fetchMock } from 'fetch-mock';
+import sinon from 'sinon';
+import { $$ } from 'common-sk/modules/dom';
+import fetchMock from 'fetch-mock';
+import { expect } from 'chai';
 import { priorities } from '../task-priority-sk/test_data';
 import { chromiumPatchResult } from '../patch-sk/test_data';
+import { InputSk } from '../input-sk/input-sk';
+import { MetricsAnalysisSk } from './metrics-analysis-sk';
 import {
   eventPromise,
   setUpElementUnderTest,
@@ -11,32 +15,28 @@ import {
 
 describe('metrics-analysis-sk', () => {
   fetchMock.config.overwriteRoutes = false;
-  const factory = setUpElementUnderTest('metrics-analysis-sk');
+  const factory = setUpElementUnderTest<MetricsAnalysisSk>('metrics-analysis-sk');
   // Returns a new element with the pagesets, task priorirites, and
   // active tasks fetches complete, and benchmarks and platforms set.
-  const newInstance = async (activeTasks, init) => {
+  const newInstance = async (activeTasks?: number) => {
     fetchMock.getOnce('begin:/_/task_priorities/', priorities);
     mockActiveTasks(activeTasks);
-    const wrappedInit = (ele) => {
-      if (init) {
-        init(ele);
-      }
-    };
-    const ele = factory(wrappedInit);
+    const ele = factory();
     await fetchMock.flush(true);
     return ele;
   };
 
   // Make our test object global to make helper functions convenient.
-  let metricsAnalysis;
+  let metricsAnalysis: MetricsAnalysisSk;
 
   afterEach(() => {
     //  Check all mock fetches called at least once and reset.
     expect(fetchMock.done()).to.be.true;
     fetchMock.reset();
+    sinon.restore();
   });
 
-  const mockActiveTasks = (n) => {
+  const mockActiveTasks = (n: number|undefined) => {
     n = n || 0;
     // For running tasks for the user we put a nonzero total in one of the
     // responses, and 0 in the remaining 6.
@@ -54,21 +54,21 @@ describe('metrics-analysis-sk', () => {
     }, { repeat: 6 });
   };
 
-  const setDescription = (d) => {
-    $$('#description', metricsAnalysis).value = d;
+  const setDescription = (d: string) => {
+    ($$('#description', metricsAnalysis) as InputSk).value = d;
   };
 
-  const setMetricName = (m) => {
-    $$('#metric_name', metricsAnalysis).value = m;
+  const setMetricName = (m: string) => {
+    ($$('#metric_name', metricsAnalysis) as InputSk).value = m;
   };
 
-  const setAnalysisTaskId = (a) => {
-    $$('#analysis_task_id', metricsAnalysis).value = a;
+  const setAnalysisTaskId = (a: string) => {
+    ($$('#analysis_task_id', metricsAnalysis) as InputSk).value = a;
   };
 
-  const setPatch = async (patchtype, value, response) => {
+  const setPatch = async (patchtype: string, value: string, response: any) => {
     fetchMock.postOnce('begin:/_/cl_data', response);
-    const input = $$(`#${patchtype}_patch input-sk`);
+    const input = $$(`#${patchtype}_patch input-sk`) as InputSk;
     input.value = value;
     input.dispatchEvent(new Event('input', {
       bubbles: true,
@@ -77,7 +77,7 @@ describe('metrics-analysis-sk', () => {
   };
 
   const clickSubmit = () => {
-    $$('#submit', metricsAnalysis).click();
+    ($$('#submit', metricsAnalysis) as InputSk).click();
   };
 
   const expectTaskTriggered = () => {
@@ -86,10 +86,10 @@ describe('metrics-analysis-sk', () => {
 
   it('loads, has defaults set', async () => {
     metricsAnalysis = await newInstance();
-    expect($$('#repeat_after_days', this)).to.have.property('frequency', '0');
-    expect($$('#task_priority', this)).to.have.property('priority', '100');
-    expect($$('#benchmark_args', this)).to.have.property('value', '--output-format=csv');
-    expect($$('#value_column_name', this)).to.have.property('value', 'avg');
+    expect($$('#repeat_after_days', metricsAnalysis)).to.have.property('frequency', '0');
+    expect($$('#task_priority', metricsAnalysis)).to.have.property('priority', '100');
+    expect($$('#benchmark_args', metricsAnalysis)).to.have.property('value', '--output-format=csv');
+    expect($$('#value_column_name', metricsAnalysis)).to.have.property('value', 'avg');
   });
 
   it('requires metric name', async () => {
@@ -97,7 +97,7 @@ describe('metrics-analysis-sk', () => {
     const event = eventPromise('error-sk');
     clickSubmit();
     const err = await event;
-    expect(err.detail.message).to.equal('Please specify a metric name');
+    expect((err as CustomEvent).detail.message).to.equal('Please specify a metric name');
   });
 
   it('requires traces', async () => {
@@ -106,7 +106,7 @@ describe('metrics-analysis-sk', () => {
     const event = eventPromise('error-sk');
     clickSubmit();
     const err = await event;
-    expect(err.detail.message).to.equal('Please specify an analysis task id or custom traces');
+    expect((err as CustomEvent).detail.message).to.equal('Please specify an analysis task id or custom traces');
   });
 
   it('requires description', async () => {
@@ -116,7 +116,7 @@ describe('metrics-analysis-sk', () => {
     const event = eventPromise('error-sk');
     clickSubmit();
     const err = await event;
-    expect(err.detail.message).to.equal('Please specify a description');
+    expect((err as CustomEvent).detail.message).to.equal('Please specify a description');
   });
 
   it('rejects bad patch', async () => {
@@ -127,13 +127,13 @@ describe('metrics-analysis-sk', () => {
     let event = eventPromise('error-sk');
     clickSubmit();
     let err = await event;
-    expect(err.detail.message).to.contain('Unable to fetch chromium patch from CL 1234');
+    expect((err as CustomEvent).detail.message).to.contain('Unable to fetch chromium patch from CL 1234');
 
     await setPatch('chromium', '1234', {}); // CL doesn't load.
     event = eventPromise('error-sk');
     clickSubmit();
     err = await event;
-    expect(err.detail.message).to.contain('Unable to load chromium CL 1234');
+    expect((err as CustomEvent).detail.message).to.contain('Unable to load chromium CL 1234');
   });
 
   it('triggers a new task', async () => {
@@ -141,13 +141,14 @@ describe('metrics-analysis-sk', () => {
     setMetricName('foo');
     setAnalysisTaskId('bar');
     await setPatch('chromium', '1234', chromiumPatchResult);
-    // Karma can't handlje page reloads, so disable it.
-    metricsAnalysis._gotoRunsHistory = () => { };
+    metricsAnalysis._gotoRunsHistory = () => {
+      // Karma can't handlje page reloads, so disable it.
+    };
     expectTaskTriggered();
+    sinon.stub(window, 'confirm').returns(true);
     clickSubmit();
-    $('#confirm_dialog button')[1].click(); // brittle way to press 'ok'
     await fetchMock.flush(true);
-    const taskJson = JSON.parse(fetchMock.lastOptions().body);
+    const taskJson = JSON.parse(fetchMock.lastOptions()!.body as any);
     // Here we test the 'interesting' arguments. We try a single patch,
     // and we don't bother filling in the simple string arguments.
     const expectation = {
@@ -173,6 +174,6 @@ describe('metrics-analysis-sk', () => {
     const event = eventPromise('error-sk');
     clickSubmit();
     const err = await event;
-    expect(err.detail.message).to.contain('You have 4 currently running tasks');
+    expect((err as CustomEvent).detail.message).to.contain('You have 4 currently running tasks');
   });
 });
