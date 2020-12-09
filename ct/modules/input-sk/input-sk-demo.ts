1765c5acc6e8a9394481388fcb60ceb2561ce5bc
Ravi Mistry <rmistry@google.com>
2020-12-09 09:44:22 -0500

    [CT] input-sk -> TS
    Also increased the width of the input to 100%: It was incorrectly getting cut off before: https://screenshot.googleplex.com/B3FuAhZrFwWqCgE
    
    Bug: skia:10920
    Change-Id: I4750b9d79ce9a75e6ab471213a8b1e65937685f3
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/340231
    Reviewed-by: Weston Tracey <westont@google.com>

--- ct/modules/input-sk/input-sk-demo.ts
+++ ct/modules/input-sk/input-sk-demo.ts
@@ -1,7 +1,8 @@
 import './index';
 import '../../../infra-sk/modules/theme-chooser-sk';
 import { $$ } from 'common-sk/modules/dom';
+import { InputSk } from './input-sk';
 
-const si = document.createElement('input-sk');
+const si = document.createElement('input-sk') as InputSk;
 si.label = 'Type something here';
-$$('#container').appendChild(si);
+($$('#container') as HTMLElement).appendChild(si);
