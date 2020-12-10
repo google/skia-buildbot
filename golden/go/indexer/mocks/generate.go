a90d7428b8c1cd0358c214906f6508990d11de7b
Kevin Lubick <kjlubick@google.com>
2020-12-10 14:34:36 -0500

    [infra] use mockery 2.4.0 for generating mocks
    Mockery 1.0.0 is no longer supported and they have a new suggested way
    for installation/use.
    
    The mocks themselves haven't changed.
    
    Depends on https://skia-review.googlesource.com/c/skia/+/341040 to
    add the CIPD asset
    
    Change-Id: I1945cea9cdb03a45b0d738a1bb9380c86533be74

--- golden/go/indexer/mocks/generate.go
+++ golden/go/indexer/mocks/generate.go
@@ -1,4 +1,4 @@
 package mocks
 
-//go:generate mockery -name IndexSource -dir ../ -output .
-//go:generate mockery -name IndexSearcher -dir ../ -output .
+//go:generate mockery --name IndexSource --dir ../ --output .
+//go:generate mockery --name IndexSearcher --dir ../ --output .
