866b3fa995cbbbee0d91c4bbde21b34f49aa1137
Eric Boren <borenet@google.com>
2020-12-11 10:25:40 -0500

    [leasing] Remove isolate support
    Bug: skia:10883
    Change-Id: I108a2a2a5d7bd40b2749aafa37c4661b29f89fa1

--- leasing/go/leasing/main.go
+++ leasing/go/leasing/main.go
@@ -59,7 +59,7 @@ var (
 	// Flags
 	host                       = flag.String("host", "leasing.skia.org", "HTTP service host")
 	workdir                    = flag.String("workdir", ".", "Directory to use for scratch work.")
-	isolatesDir                = flag.String("isolates_dir", "", "The directory to find leasing server's isolates files.")
+	artifactsDir               = flag.String("artifacts_dir", "", "The directory to find leasing server's artifacts.")
 	pollInterval               = flag.Duration("poll_interval", 1*time.Minute, "How often the leasing server will check if tasks have expired.")
 	emailClientSecretFile      = flag.String("email_client_secret_file", "/etc/leasing-email-secrets/client_secret.json", "OAuth client secret JSON file for sending email.")
 	emailTokenCacheFile        = flag.String("email_token_cache_file", "/etc/leasing-email-secrets/client_token.json", "OAuth token cache file for sending email.")
@@ -132,9 +132,9 @@ func New() (baseapp.App, error) {
 	}
 	login.SimpleInitWithAllow(*baseapp.Port, *baseapp.Local, nil, nil, allow)
 
-	// Initialize isolate and swarming.
+	// Initialize swarming.
 	if err := SwarmingInit(*serviceAccountFile); err != nil {
-		sklog.Fatalf("Failed to init isolate and swarming: %s", err)
+		sklog.Fatalf("Failed to init swarming: %s", err)
 	}
 
 	// Initialize cloud datastore.
@@ -496,7 +496,7 @@ func (srv *Server) addTaskHandler(w http.ResponseWriter, r *http.Request) {
 	// Set to pending.
 	task.SwarmingTaskState = swarming.TASK_STATE_PENDING
 
-	// Isolate artifacts.
+	// Upload artifacts.
 	var swarmingProps *swarming_api.SwarmingRpcsTaskProperties
 	if task.TaskIdForIsolates != "" {
 		t, err := GetSwarmingTaskMetadata(task.SwarmingPool, task.TaskIdForIsolates)
@@ -515,13 +515,7 @@ func (srv *Server) addTaskHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	var casDigest string
-	if swarmingProps.InputsRef != nil {
-		casDigest, err = IsolateLeasingArtifacts(ctx, task.SwarmingPool, swarmingProps.InputsRef)
-		if err != nil {
-			httputils.ReportError(w, err, fmt.Sprintf("Error when getting isolate hash: %v", err), http.StatusInternalServerError)
-			return
-		}
-	} else if swarmingProps.CasInputRoot != nil {
+	if swarmingProps.CasInputRoot != nil {
 		casDigest, err = AddLeasingArtifactsToCAS(ctx, task.SwarmingPool, swarmingProps.CasInputRoot)
 	}
 	// Trigger the swarming task.
