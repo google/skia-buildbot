3ad6ceafd2a26838ca6e12020424a08b2a79c898
Joe Gregorio <jcgregorio@google.com>
2020-12-14 09:35:00 -0500

    [perf] Add "trybot reference" the the perf-tool command.
    Change-Id: Idcd7d83cf4cc7b7946e94d4f5ed2f3488a8327d0

---
+++ perf/migrations/cockroachdb/trybot-experiments.sql
@@ -0,0 +1,27 @@
+
+
+
+select source_file_id from tracevalues where trace_id='\x0d883d3839a26e1e7b1852fc10795f89' order by commit_number desc limit 5;
+
+
+select trace_id from tracevalues where source_file_id in (select source_file_id from tracevalues where trace_id='\x0d883d3839a26e1e7b1852fc10795f89' order by commit_number desc limit 5)
+
+select source_file_id from tracevalues where trace_id='\x670f0532af09ebce8c238bf67d99558d' order by commit_number desc limit 5;
+
+select trace_id from tracevalues where trace_id in ('\x670f0532af09ebce8c238bf67d99558d', '\x0d883d3839a26e1e7b1852fc10795f89')
+
+
+create index by_source_file_id  on TraceValues (source_file_id, trace_id);
+
+
+select source_file_id from tracevalues where trace_id='\x670f0532af09ebce8c238bf67d99558d' order by commit_number desc limit 5;
+
+select distinct trace_id from tracevalues where source_file_id in ( 615447013218516993, 615303501470105601) limit 5;
+
+-- Start with a single trace, find the source_file_id's of the last 5 commits,
+-- and then use those source_file_id's to list all the trace_id's that appear in
+-- those files.
+select  trace_id from tracevalues@by_source_file_id where source_file_id in (
+     select source_file_id from tracevalues@primary where trace_id='\x670f0532af09ebce8c238bf67d99558d' order by commit_number desc limit 5
+      ) limit 5;
+
