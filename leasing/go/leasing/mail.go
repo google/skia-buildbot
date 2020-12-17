98f798549b386d99b891eeab03dc7a9d8cde6975
Ravi Mistry <rmistry@google.com>
2020-12-17 10:39:08 -0500

    [leasing] Use new Infra Gardener endpoint
    Bug: skia:11034
    Change-Id: Id7ab616cb2648636e96dc6d4467ba07f0ef863b8

--- leasing/go/leasing/mail.go
+++ leasing/go/leasing/mail.go
@@ -10,6 +10,7 @@ import (
 
 	"go.skia.org/infra/go/email"
 	"go.skia.org/infra/go/httputils"
+	"go.skia.org/infra/go/rotations"
 	"go.skia.org/infra/go/sklog"
 )
 
@@ -40,14 +41,14 @@ func MailInit(emailClientId, emailClientSecret, tokenFile string) error {
 func getRecipients(taskOwner string) []string {
 	// Figure out the list of recipients.
 	recipients := []string{taskOwner}
-	trooper, err := GetTrooperEmail(httpClient)
+	gardeners, err := rotations.FromURL(httpClient, rotations.InfraGardenerURL)
 	if err != nil {
-		sklog.Errorf("Could not get trooper email: %s", err)
+		sklog.Errorf("Could not get gardener email: %s", err)
 		return recipients
 	}
 	// Make sure rmistry@ is included on all emails for now.
 	recipients = append(recipients, "rmistry@google.com")
-	return append(recipients, trooper)
+	return append(recipients, gardeners[0])
 }
 
 // SendStartEmail sends an email notifying user that the leasing task has started.
