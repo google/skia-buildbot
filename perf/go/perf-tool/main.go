165aa380c9fc40907ac8668376805c08325b8370
Joe Gregorio <jcgregorio@google.com>
2020-12-21 09:48:54 -0500

    [perf] Add "trybot reference" the the perf-tool command.
    NAME:
       perf-tool trybot reference - Generates a reference file to be used by nanostat for the given trybot file.
    
    USAGE:
       perf-tool trybot reference [command options] [arguments...]
    
    DESCRIPTION:
       This command constructs a synthetic nanobench file filled with 'samples' for
       each of the trace ids found in the nanobench file given in --filename.
       The samples in the sythentic file are the samples found in the last --num
       commits of ingested perf data for all the traces found in --filename.
    
       This allows building a nanobench file with a large number of samples
       that can be compared with a nanobench file from a trybot using the nanostat
       application.
    
       This is an experimental function that may go away in the future.
    
    OPTIONS:
       --local                    If true then use gcloud credentials. (default: true)
       --config_filename FILE     Load configuration from FILE [$PERF_CONFIG_FILENAME]
       --connection_string value  Override the connection string in the config file. [$PERF_CONNECTION_STRING]
       --filename value           The full URL of a nanobench trybot results files, e.g.: 'gs://skia-perf/...foo.json'
       --num value                The number of ingestion files to load. (default: 5)
       --out value                The output filename.
       --help, -h                 show help (default: false)
    
    Change-Id: Idcd7d83cf4cc7b7946e94d4f5ed2f3488a8327d0

--- perf/go/perf-tool/main.go
+++ perf/go/perf-tool/main.go
@@ -20,23 +20,31 @@ import (
 // flag names
 const (
 	backupToDateFlagName     = "backup_to_date"
+	beginCommitFlagName      = "begin"
 	configFilenameFlagName   = "config_filename"
 	connectionStringFlagName = "connection_string"
+	dryrunFlagName           = "dryrun"
+	endCommitFlagName        = "end"
 	inputFilenameFlagName    = "in"
 	localFlagName            = "local"
+	loggingFlagName          = "logging"
 	numTilesListFlagName     = "num"
 	outputFilenameFlagName   = "out"
 	queryFlagName            = "query"
-	tileNumberFlagName       = "tile"
-	beginCommitFlagName      = "begin"
-	endCommitFlagName        = "end"
 	startTimeFlagName        = "start"
 	stopTimeFlagName         = "stop"
-	dryrunFlagName           = "dryrun"
-	loggingFlagName          = "logging"
+	tileNumberFlagName       = "tile"
+	trybotFilenameFlagName   = "filename"
+	trybotNumCommitsFlagName = "num"
 )
 
 // flags
+var trybotFilenameFlag = &cli.StringFlag{
+	Name:  trybotFilenameFlagName,
+	Value: "",
+	Usage: "The full URL of a nanobench trybot results files, e.g.: 'gs://skia-perf/...foo.json'",
+}
+
 var connectionStringFlag = &cli.StringFlag{
 	Name:    connectionStringFlagName,
 	Value:   "",
@@ -102,6 +110,12 @@ var numTilesListFlag = &cli.IntFlag{
 	Value:   10,
 	Usage:   "The number of tiles to display.",
 	EnvVars: []string{"PERF_CONFIG_FILENAME"},
+}
+
+var trybotNumCommitsFlag = &cli.IntFlag{
+	Name:  trybotNumCommitsFlagName,
+	Value: 5,
+	Usage: "The number of ingestion files to load.",
 }
 
 var beginCommitFlag = &cli.Int64Flag{
@@ -477,6 +491,47 @@ using the same input file for both restores.
 					},
 				},
 			},
+			{
+				Name: "trybot",
+				Subcommands: []*cli.Command{
+					{
+						Name:  "reference",
+						Usage: "Generates a reference file to be used by nanostat for the given trybot file.",
+						Description: `
+This command constructs a synthetic nanobench file filled with 'samples' for
+each of the trace ids found in the nanobench file given in --filename.
+The samples in the sythentic file are the samples found in the last --num
+commits of ingested perf data for all the traces found in --filename.
+
+This allows building a nanobench file with a large number of samples
+that can be compared with a nanobench file from a trybot using the nanostat
+application.
+
+This is an experimental function that may go away in the future.
+`,
+						Flags: []cli.Flag{
+							localFlag,
+							configFilenameFlag,
+							connectionStringFlag,
+							trybotFilenameFlag,
+							trybotNumCommitsFlag,
+							requiredOutputFilenameFlag,
+						},
+						Action: func(c *cli.Context) error {
+							instanceConfig, err := instanceConfigFromFlags(c)
+							if err != nil {
+								return skerr.Wrap(err)
+							}
+							store, err := getStore(c)
+							if err != nil {
+								return skerr.Wrap(err)
+							}
+							return app.TrybotReference(c.Bool(localFlagName), store, instanceConfig, c.String(trybotFilenameFlagName), c.String(outputFilenameFlagName), c.Int(trybotNumCommitsFlagName))
+						},
+					},
+				},
+			},
+
 			{
 				Name:  "markdown",
 				Usage: "Generates markdown help for perf-tool.",
