e07d35ebcd82f43f2d6c0cefde4daef2d7d2496e
Kevin Lubick <kjlubick@google.com>
2020-12-30 10:08:50 -0500

    [gold] Don't have data builder serialize JSON maps.
    This ends up with the the data being double-encoded when inserted
    into SQL.
    
    Bug: skia:10582
    Change-Id: I1f8e2312b47b09c7566a4822a93bb066cb0f37b4

--- golden/go/sql/util.go
+++ golden/go/sql/util.go
@@ -3,7 +3,6 @@ package sql
 import (
 	"crypto/md5"
 	"encoding/hex"
-	"encoding/json"
 
 	"go.skia.org/infra/go/jsonutils"
 	"go.skia.org/infra/go/skerr"
@@ -24,7 +23,7 @@ func DigestToBytes(d types.Digest) (schema.DigestBytes, error) {
 
 // SerializeMap returns the given map in JSON and the MD5 of that json string. nil maps will be
 // treated as empty maps. Keys will be sorted lexicographically.
-func SerializeMap(m map[string]string) (schema.SerializedParams, []byte) {
+func SerializeMap(m map[string]string) (string, []byte) {
 	var jsonBytes []byte
 	if len(m) == 0 {
 		jsonBytes = []byte("{}")
@@ -33,17 +32,7 @@ func SerializeMap(m map[string]string) (schema.SerializedParams, []byte) {
 	}
 
 	h := md5.Sum(jsonBytes)
-	return schema.SerializedParams(jsonBytes), h[:]
-}
-
-// DeserializeMap returns the given JSON string as a map of string to string.
-func DeserializeMap(s schema.SerializedParams) (map[string]string, error) {
-	m := map[string]string{}
-	err := json.Unmarshal([]byte(s), &m)
-	if err != nil {
-		return nil, skerr.Wrap(err)
-	}
-	return m, nil
+	return string(jsonBytes), h[:]
 }
 
 // TraceValuesShards is the number of shards we use in the TraceValues Table.
