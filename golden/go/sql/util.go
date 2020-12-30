fb0b2df6e06ab2e9b5ecee28043f7f99462cbf3d
Kevin Lubick <kjlubick@google.com>
2020-12-30 10:59:10 -0500

    [gold] Add ToSQL model for writing tables directly to SQL
    This adds some important code for bulk inserts -
    sql.ValuesPlaceholders
    This allows us to insert multiple items at once, but we do not
    have to convert them to strings ourselves, but instead can
    the SQL driver to do that for us. This has benefits including
    performance and resistance to SQL injections.
    
    Bug: skia:10582
    Change-Id: Ide55695a4a20cd18faf2fa0e5dfafa19f6c11692

--- golden/go/sql/util.go
+++ golden/go/sql/util.go
@@ -3,6 +3,8 @@ package sql
 import (
 	"crypto/md5"
 	"encoding/hex"
+	"strconv"
+	"strings"
 
 	"go.skia.org/infra/go/jsonutils"
 	"go.skia.org/infra/go/skerr"
@@ -57,4 +59,33 @@ func AsMD5Hash(b []byte) schema.MD5Hash {
 	var m schema.MD5Hash
 	copy(m[:], b)
 	return m
+}
+
+// ValuesPlaceholders returns a set of SQL placeholder numbers grouped for use in an INSERT
+// statement. For example, ValuesPlaceholders(2,3) returns ($1, $2), ($3, $4), ($5, $6)
+// It panics if either param is <= 0.
+func ValuesPlaceholders(valuesPerRow, numRows int) string {
+	if valuesPerRow <= 0 || numRows <= 0 {
+		panic("Cannot make ValuesPlaceholder with 0 rows or 0 values per row")
+	}
+	values := strings.Builder{}
+	// There are at most 5 bytes per value that need to be written
+	values.Grow(5 * valuesPerRow * numRows)
+	// All WriteString calls below return nil errors, as specified in the documentation of
+	// strings.Builder, so it is safe to ignore them.
+	for argIdx := 1; argIdx <= valuesPerRow*numRows; argIdx += valuesPerRow {
+		if argIdx != 1 {
+			_, _ = values.WriteString(",")
+		}
+		_, _ = values.WriteString("(")
+		for i := 0; i < valuesPerRow; i++ {
+			if i != 0 {
+				_, _ = values.WriteString(",")
+			}
+			_, _ = values.WriteString("$")
+			_, _ = values.WriteString(strconv.Itoa(argIdx + i))
+		}
+		_, _ = values.WriteString(")")
+	}
+	return values.String()
 }
