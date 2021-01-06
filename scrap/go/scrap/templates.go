ef95f052c76fff747fd8a545a60092134a399a43
Joe Gregorio <jcgregorio@google.com>
2021-01-06 14:34:59 -0500

    [scrap] Fix CPP template for SVG.
    Bug: skia:11108
    Change-Id: I0646f48901abaf7a0adc64be6d64ee315b6de5f9
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/350797
    Reviewed-by: Kevin Lubick <kjlubick@google.com>
    Commit-Queue: Joe Gregorio <jcgregorio@google.com>

--- scrap/go/scrap/templates.go
+++ scrap/go/scrap/templates.go
@@ -2,6 +2,7 @@ package scrap
 
 import (
 	"fmt"
+	"strings"
 	"text/template"
 
 	"go.skia.org/infra/go/skerr"
@@ -13,12 +14,52 @@ func templateName(t Type, lang Lang) string {
 	return fmt.Sprintf("%s-%s", t, lang)
 }
 
+// bodyAsStringSlice is a template helper function that breaks up a ScrapBody
+// into a form suitable as a multi-line C++ const char * string.
+//
+// That is, it takes "<svg> \n</svg>"
+//
+// And turns it into:
+//
+//  []string{
+//    `"<svg> \n"`,
+//    `"</svg>";`,
+//  }
+func bodyAsStringSlice(body string) []string {
+	// Escape all the double quotes.
+	body = strings.ReplaceAll(body, `"`, `\"`)
+
+	// Break into individual lines.
+	lines := strings.Split(body, "\n")
+	numLines := len(lines)
+	ret := make([]string, 0, numLines)
+	for i, line := range lines {
+		// Quote each line.
+		quotedLine := `"` + line
+		if i == numLines-1 {
+			// Since this is the very last line, add end quote and terminating semicolon.
+			quotedLine += `";`
+		} else {
+			// Add newline and end quote.
+			quotedLine += `\n"`
+		}
+
+		ret = append(ret, quotedLine)
+	}
+	return ret
+}
+
+// funcMap are the template helper functions available in each template.
+var funcMap = template.FuncMap{
+	"bodyAsStringSlice": bodyAsStringSlice,
+}
+
 func loadTemplates() (templateMap, error) {
 	ret := templateMap{}
 	for _, lang := range allLangs {
 		ret[lang] = map[Type]*template.Template{}
 		for _, t := range allTypes {
-			tmpl, err := template.New("").Parse(templates[templateName(t, lang)])
+			tmpl, err := template.New("").Funcs(funcMap).Parse(templates[templateName(t, lang)])
 			if err != nil {
 				return nil, skerr.Wrapf(err, "Failed to parse template %v %v", lang, t)
 			}
@@ -39,13 +80,22 @@ var templates = map[string]string{
 	"particles-js":  "",
 }
 
-// TODO(jcgregorio) Doesn't actually work yet, svg isn't turned on in fiddle.
-const svgCpp = `
-void draw(SkCanvas* canvas) {
-    sk_sp<SkData> data(SkData::MakeWithCString("{{ .Body }}"));
+const svgCpp = `void draw(SkCanvas* canvas) {
+    const char * svg ={{ range bodyAsStringSlice .Body }}
+        {{.}}{{end}}
+
+    sk_sp<SkData> data(SkData::MakeWithoutCopy(svg, strlen(svg)));
+    if (!data) {
+        SkDebugf("Failed to load SVG.");
+        return;
+    }
 
     SkMemoryStream stream(std::move(data));
     sk_sp<SkSVGDOM> svgDom = SkSVGDOM::MakeFromStream(stream);
+    if (!svgDom) {
+        SkDebugf("Failed to parse SVG.");
+        return;
+    }
 
     // Use the intrinsic SVG size if available, otherwise fall back to a default value.
     static const SkSize kDefaultContainerSize = SkSize::Make(128, 128);
@@ -53,5 +103,5 @@ void draw(SkCanvas* canvas) {
         svgDom->setContainerSize(kDefaultContainerSize);
     }
 
-  	svgDom->render(canvas);
+    svgDom->render(canvas);
 }`
