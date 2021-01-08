9251eabe483bb9ead8de2904968c401379fc76dc
Kevin Lubick <kjlubick@google.com>
2021-01-08 17:21:19 -0500

    [infra] Replace uses of setUpPuppeteer* with cached version.
    This should make the Puppeteer suite much faster.
    
    Change-Id: Iee793949bbc1f6f53473e68573c3091e91dbef48

--- infra-sk/modules/theme-chooser-sk/theme-chooser-sk_puppeteer_test.ts
+++ infra-sk/modules/theme-chooser-sk/theme-chooser-sk_puppeteer_test.ts
@@ -1,15 +1,17 @@
 import * as path from 'path';
 import { expect } from 'chai';
 import {
-  inBazel,
-  setUpPuppeteerAndDemoPageServer,
-  takeScreenshot,
+  inBazel, loadCachedTestBed,
+  takeScreenshot, TestBed,
 } from '../../../puppeteer-tests/util';
 
 describe('theme-chooser-sk', () => {
-  const testBed = setUpPuppeteerAndDemoPageServer(
-    path.join(__dirname, '..', '..', 'webpack.config.ts')
-  );
+  let testBed: TestBed;
+  before(async () => {
+    testBed = await loadCachedTestBed(
+        path.join(__dirname, '..', '..', 'webpack.config.ts')
+    );
+  });
 
   beforeEach(async () => {
     await testBed.page.goto(
