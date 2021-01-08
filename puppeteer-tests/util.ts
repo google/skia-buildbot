319a1dec272c9ab287479c2537a5fa247a1fdd93
Kevin Lubick <kjlubick@google.com>
2021-01-08 14:25:20 -0500

    [infra] Use cached TestBed for Puppeteer tests for debugger.
    This reduces the time to test all debugger modules from 47 seconds to 13.
    
    This code was re-used from Gold. Future CLs will roll this out more
    broadly in an effort to speed up that test suite.
    
    Change-Id: Ie01ba9cbd06dd2d95d1a9cdea508d9b3e2649218
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/352024
    Commit-Queue: Kevin Lubick <kjlubick@google.com>
    Reviewed-by: Leandro Lovisolo <lovisolo@google.com>

--- puppeteer-tests/util.ts
+++ puppeteer-tests/util.ts
@@ -259,7 +259,6 @@ export const outputDir = () => {
   return path.join(__dirname, 'output');
 };
 
-
 /**
  * Takes a screenshot and saves it to the tests output directory to be uploaded
  * to Gold.
@@ -271,3 +270,49 @@ export const outputDir = () => {
 export const takeScreenshot =
     (handle: puppeteer.Page | puppeteer.ElementHandle, appName: string, testName: string) =>
   handle.screenshot({path: path.join(exports.outputDir(), `${appName}_${testName}.png`)});
+
+
+let browser: puppeteer.Browser;
+let testBed: Partial<TestBed>;
+
+/**
+ * Once per mocha invocation, loadCachedTestBed will compile all the demo pages and launch a
+ * puppeteer browser window to run the tests. On all subsequent calls, it will return essentially
+ * a cached handle to that invocation.
+ */
+export async function loadCachedTestBed(pathToWebpackConfigTs: string) {
+  if (testBed) {
+    return testBed as TestBed;
+  }
+  const newTestBed: Partial<TestBed> = {};
+
+  let baseUrl;
+  ({ baseUrl } = await startWebpackDemoPageServer(pathToWebpackConfigTs));
+  newTestBed.baseUrl = baseUrl;
+  browser = await launchBrowser();
+  testBed = newTestBed;
+  setBeforeAfterHooks();
+  return testBed as TestBed;
+}
+
+// This sets up some handy helpers to load a new page and shut it down w/o having to expose
+// the puppeteer.Browser object to the callers.
+function setBeforeAfterHooks() {
+  beforeEach(async () => {
+    testBed.page = await browser.newPage(); // Make page available to tests.
+
+    // Tell demo pages this is a Puppeteer test. Demo pages should not fake RPC
+    // latency, render animations or exhibit any other non-deterministic
+    // behavior that could result in differences in the screenshots uploaded to
+    // Gold.
+    await testBed.page.setCookie({
+      url: testBed.baseUrl,
+      name: 'puppeteer',
+      value: 'true'
+    });
+  });
+
+  afterEach(async () => {
+    await testBed.page!.close();
+  });
+}
