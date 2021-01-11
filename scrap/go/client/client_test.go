367046012760784a2b57a3d3815d74fe6dfd2129
Joe Gregorio <jcgregorio@google.com>
2021-01-11 14:13:32 -0500

    [scrap] Create client library for scrap REST API.
    Bug: skia:11108
    Change-Id: I374a1bfd857d985339a6fe250ffa0eda44b60c92

---
+++ scrap/go/client/client_test.go
@@ -0,0 +1,134 @@
+// Package client is a client for the Scrap Exchange REST API.
+package client
+
+import (
+	"bytes"
+	"context"
+	"fmt"
+	"io"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gorilla/mux"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+	"go.skia.org/infra/go/testutils"
+	"go.skia.org/infra/go/testutils/unittest"
+	"go.skia.org/infra/scrap/go/api"
+	"go.skia.org/infra/scrap/go/scrap"
+	"go.skia.org/infra/scrap/go/scrap/mocks"
+)
+
+var (
+	errMyMockError = fmt.Errorf("My mock error")
+)
+
+const (
+	hash = scrap.SHA256("f7b0bac33b5f5b3ac86bec9f33c2d1c3ef025a9e4282ca7a8b9bc01e40d40556")
+)
+
+func setupForTest(t *testing.T) (*mocks.ScrapExchange, *Client) {
+	scrapExchangeMock := &mocks.ScrapExchange{}
+	a := api.New(scrapExchangeMock)
+	router := mux.NewRouter()
+	a.AddHandlers(router, api.AddProtectedEndpoints)
+
+	// Create a test server that mocks out the scrap exchange.
+	testServer := httptest.NewServer(router)
+	t.Cleanup(testServer.Close)
+
+	// Configure main to point at the test server.
+	client, err := New(testServer.URL)
+	require.NoError(t, err)
+	return scrapExchangeMock, client
+}
+
+func TestExpand_HappyPath_Success(t *testing.T) {
+	unittest.SmallTest(t)
+	scrapExchangeMock, client := setupForTest(t)
+
+	scrapExchangeMock.On("Expand", testutils.AnyContext, scrap.SVG, "@smiley", scrap.CPP, mock.Anything).Run(func(args mock.Arguments) {
+		_, err := args[4].(io.Writer).Write([]byte("<svg></svg>"))
+		require.NoError(t, err)
+	}).Return(nil)
+
+	var b bytes.Buffer
+	err := client.Expand(context.Background(), scrap.SVG, "@smiley", scrap.CPP, &b)
+	require.NoError(t, err)
+	require.Equal(t, `<svg></svg>`, b.String())
+	scrapExchangeMock.AssertExpectations(t)
+}
+
+func TestExpand_ExpandError_ReturnsError(t *testing.T) {
+	unittest.SmallTest(t)
+	scrapExchangeMock, client := setupForTest(t)
+
+	scrapExchangeMock.On("Expand", testutils.AnyContext, scrap.SVG, "@smiley", scrap.CPP, mock.Anything).Return(errMyMockError)
+
+	var b bytes.Buffer
+	err := client.Expand(context.Background(), scrap.SVG, "@smiley", scrap.CPP, &b)
+	require.Error(t, err)
+}
+
+func TestLoadScrap_HappyPath_Success(t *testing.T) {
+	unittest.SmallTest(t)
+	scrapExchangeMock, client := setupForTest(t)
+
+	scrapBody := scrap.ScrapBody{
+		Type: scrap.SVG,
+		Body: "<svg></svg>",
+	}
+	scrapExchangeMock.On("LoadScrap", testutils.AnyContext, scrap.SVG, "@smiley").Return(scrapBody, nil)
+
+	returnedBody, err := client.LoadScrap(context.Background(), scrap.SVG, "@smiley")
+	require.NoError(t, err)
+	require.Equal(t, scrapBody, returnedBody)
+	scrapExchangeMock.AssertExpectations(t)
+}
+
+func TestLoadScrap_LoadScrapError_ReturnsError(t *testing.T) {
+	unittest.SmallTest(t)
+	scrapExchangeMock, client := setupForTest(t)
+
+	scrapBody := scrap.ScrapBody{
+		Type: scrap.SVG,
+		Body: "<svg></svg>",
+	}
+	scrapExchangeMock.On("LoadScrap", testutils.AnyContext, scrap.SVG, "@smiley").Return(scrapBody, errMyMockError)
+
+	_, err := client.LoadScrap(context.Background(), scrap.SVG, "@smiley")
+	require.Error(t, err)
+	scrapExchangeMock.AssertExpectations(t)
+}
+
+func TestCreateScrap_HappyPath_Success(t *testing.T) {
+	unittest.SmallTest(t)
+	scrapExchangeMock, client := setupForTest(t)
+
+	scrapBody := scrap.ScrapBody{
+		Type: scrap.SVG,
+		Body: "<svg></svg>",
+	}
+	scrapID := scrap.ScrapID{
+		Hash: hash,
+	}
+	scrapExchangeMock.On("CreateScrap", testutils.AnyContext, scrapBody).Return(scrapID, nil)
+
+	returnedScrapID, err := client.CreateScrap(context.Background(), scrapBody)
+	require.NoError(t, err)
+	require.Equal(t, scrapID, returnedScrapID)
+	scrapExchangeMock.AssertExpectations(t)
+}
+
+func TestCreateScrap_CreateReturnsError_ReturnsError(t *testing.T) {
+	unittest.SmallTest(t)
+	scrapExchangeMock, client := setupForTest(t)
+
+	scrapBody := scrap.ScrapBody{}
+	scrapID := scrap.ScrapID{}
+	scrapExchangeMock.On("CreateScrap", testutils.AnyContext, scrapBody).Return(scrapID, errMyMockError)
+
+	_, err := client.CreateScrap(context.Background(), scrapBody)
+	require.Error(t, err)
+	scrapExchangeMock.AssertExpectations(t)
+}
