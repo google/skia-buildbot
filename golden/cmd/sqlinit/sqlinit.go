e94c10a64e37871394501fd017633b7f34279d83
Kevin Lubick <kjlubick@google.com>
2021-01-11 10:53:35 -0500

    [gold] Add automatic scheduled SQL backups
    Bug: skia:10582
    Change-Id: Icd8c788a9ba40b91835b8efafc9ae8aa40fd420b

--- golden/cmd/sqlinit/sqlinit.go
+++ golden/cmd/sqlinit/sqlinit.go
@@ -1,11 +1,16 @@
 // The sqlinit executable initializes a database on the production SQL cluster with the appropriate
-// tables and schema. It is idempotent and will not modify any table schemas (e.g. add missing
-// indexes or change columns).
+// tables and schema. It will not modify any table schemas (e.g. add missing
+// indexes or change columns). This executable will schedule new automatic backups, so if there
+// are existing ones, one may have to drop the old schedules.
+// https://www.cockroachlabs.com/docs/v20.2/show-schedules
+// https://www.cockroachlabs.com/docs/v20.2/drop-schedules
 package main
 
 import (
 	"flag"
+	"fmt"
 	"os/exec"
+	"reflect"
 	"strings"
 
 	"go.skia.org/infra/go/sklog"
@@ -47,5 +52,78 @@ func main() {
 	if err != nil {
 		sklog.Fatalf("Error while creating tables: %s %s", err, out)
 	}
+
+	sklog.Infof("Initializing backup schedules")
+	out, err = exec.Command("kubectl", "run",
+		"gold-cockroachdb-init-"+normalizedDB,
+		"--restart=Never", "--image=cockroachdb/cockroach:v20.2.3",
+		"--rm", "-it", // -it forces this command to wait until it completes.
+		"--", "sql",
+		"--insecure", "--host=gold-cockroachdb:26234",
+		"--execute="+getSchedules(schema.Tables{}, normalizedDB),
+	).CombinedOutput()
+	if err != nil {
+		sklog.Fatalf("Error while creating tables: %s %s", err, out)
+	}
 	sklog.Info("Done")
+}
+
+type backupCadence struct {
+	cadence string
+	tables  []string
+}
+
+func getSchedules(inputType interface{}, dbName string) string {
+	var schedules []*backupCadence
+
+	t := reflect.TypeOf(inputType)
+	for i := 0; i < t.NumField(); i++ {
+		table := t.Field(i) // Fields of the outer type are expected to be tables.
+		if table.Type.Kind() != reflect.Slice {
+			panic(`Expected table should be a slice: ` + table.Name)
+		}
+		cadence, ok := table.Tag.Lookup("sql_backup")
+		if !ok {
+			panic(`Expected table should have backup cadence. Did you mean "none"? ` + table.Name)
+		}
+		if cadence == "none" {
+			continue
+		}
+		found := false
+		for _, s := range schedules {
+			if s.cadence == cadence {
+				found = true
+				s.tables = append(s.tables, table.Name)
+				break
+			}
+		}
+		if found {
+			continue
+		}
+		schedules = append(schedules, &backupCadence{
+			cadence: cadence,
+			tables:  []string{table.Name},
+		})
+	}
+	body := strings.Builder{}
+	for _, s := range schedules {
+		schedule := fmt.Sprintf("CREATE SCHEDULE %s_%s\n", dbName, s.cadence)
+		tables := "FOR BACKUP TABLE"
+		for i, t := range s.tables {
+			if i == 0 {
+				tables += " "
+			} else {
+				tables += ", "
+			}
+			tables += dbName + "." + t
+		}
+		schedule += tables + "\n"
+		schedule += fmt.Sprintf(`INTO 'gs://skia-gold-sql-backups/%s/%s'
+    RECURRING '@%s'
+    FULL BACKUP ALWAYS;
+`, dbName, s.cadence, s.cadence)
+
+		body.WriteString(schedule)
+	}
+	return body.String()
 }
