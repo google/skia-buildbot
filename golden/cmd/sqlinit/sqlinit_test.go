2affcb80b021d5a34063035469c42dc1455ec544
Kevin Lubick <kjlubick@google.com>
2021-01-12 09:41:50 -0500

    [gold] Add jitter to backup schedule.
    Bug: skia:10582
    Change-Id: I2cfb049066663c011d8213b63343a53db50e409a

--- golden/cmd/sqlinit/sqlinit_test.go
+++ golden/cmd/sqlinit/sqlinit_test.go
@@ -20,23 +20,25 @@ type testTable struct {
 func TestGetSchedules_AllCadencesSet_Success(t *testing.T) {
 	unittest.SmallTest(t)
 
-	schedules := getSchedules(testTable{}, "test-backups", "testdbname")
-	assert.Equal(t, schedules, `CREATE SCHEDULE testdbname_daily
+	r := &fakeRNG{n: 2}
+
+	schedules := getSchedules(testTable{}, "test-backups", "testdbname", r)
+	assert.Equal(t, `CREATE SCHEDULE testdbname_daily
 FOR BACKUP TABLE testdbname.TableOne, testdbname.TableThree, testdbname.TableFive
 INTO 'gs://test-backups/testdbname/daily'
-  RECURRING '@daily'
-  FULL BACKUP ALWAYS;
+  RECURRING '3 8 * * *'
+  FULL BACKUP ALWAYS WITH SCHEDULE OPTIONS ignore_existing_backups;
 CREATE SCHEDULE testdbname_weekly
 FOR BACKUP TABLE testdbname.TableTwo
 INTO 'gs://test-backups/testdbname/weekly'
-  RECURRING '@weekly'
-  FULL BACKUP ALWAYS;
+  RECURRING '5 5 * * 0'
+  FULL BACKUP ALWAYS WITH SCHEDULE OPTIONS ignore_existing_backups;
 CREATE SCHEDULE testdbname_monthly
 FOR BACKUP TABLE testdbname.TableFour
 INTO 'gs://test-backups/testdbname/monthly'
-  RECURRING '@monthly'
-  FULL BACKUP ALWAYS;
-`)
+  RECURRING '7 4 10 * *'
+  FULL BACKUP ALWAYS WITH SCHEDULE OPTIONS ignore_existing_backups;
+`, schedules)
 }
 
 type tableMissingFrequency struct {
@@ -48,6 +50,15 @@ func TestGetSchedules_MissingCadence_Panics(t *testing.T) {
 	unittest.SmallTest(t)
 
 	assert.Panics(t, func() {
-		getSchedules(tableMissingFrequency{}, "test-backups", "testdbname")
+		getSchedules(tableMissingFrequency{}, "test-backups", "testdbname", &fakeRNG{})
 	})
+}
+
+type fakeRNG struct {
+	n int
+}
+
+func (f *fakeRNG) Intn(n int) int {
+	f.n++
+	return f.n % n
 }
