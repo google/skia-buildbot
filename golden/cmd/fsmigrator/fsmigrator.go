034af41f1032ebad910fdb2b331c823dbc6ce1b0
Kevin Lubick <kjlubick@google.com>
2021-01-15 08:55:24 -0500

    [gold] Add data migrator tool for Changelists and Patchsets.
    Bug: skia:10582
    Change-Id: I004e3fb626103fd3a104527a1585174ec71798fa
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/353897
    Reviewed-by: Leandro Lovisolo <lovisolo@google.com>

--- golden/cmd/fsmigrator/fsmigrator.go
+++ golden/cmd/fsmigrator/fsmigrator.go
@@ -6,7 +6,7 @@ package main
 import (
 	"context"
 	"flag"
-	"net/url"
+	"strings"
 	"time"
 
 	"github.com/jackc/pgx/v4/pgxpool"
@@ -14,16 +14,21 @@ import (
 	ifirestore "go.skia.org/infra/go/firestore"
 	"go.skia.org/infra/go/skerr"
 	"go.skia.org/infra/go/sklog"
-	"go.skia.org/infra/golden/go/ignore"
-	"go.skia.org/infra/golden/go/ignore/fs_ignorestore"
+	"go.skia.org/infra/go/util"
+	"go.skia.org/infra/golden/go/clstore"
+	"go.skia.org/infra/golden/go/clstore/fs_clstore"
+	"go.skia.org/infra/golden/go/code_review"
 	"go.skia.org/infra/golden/go/sql"
+	"go.skia.org/infra/golden/go/sql/schema"
 )
 
 func main() {
 	var (
-		fsProjectID    = flag.String("fs_project_id", "skia-firestore", "The project with the firestore instance. Datastore and Firestore can't be in the same project.")
-		oldFSNamespace = flag.String("old_fs_namespace", "", "Typically the instance id. e.g. 'chrome-gpu', 'skia', etc")
-		newSQLDatabase = flag.String("new_sql_db", "", "Something like the instance id (no dashes)")
+		fsProjectID          = flag.String("fs_project_id", "skia-firestore", "The project with the firestore instance. Datastore and Firestore can't be in the same project.")
+		oldFSNamespace       = flag.String("old_fs_namespace", "", "Typically the instance id. e.g. 'chrome-gpu', 'skia', etc")
+		newSQLDatabase       = flag.String("new_sql_db", "", "Something like the instance id (no dashes)")
+		crsList              = flag.String("crs_list", "", "comma-separated list of valid Code Review Systems")
+		remakePatchSetSchema = flag.Bool("remake_ps_schema", false, "If true, will drop existing Patchsets table (and dependencies) and remake it.")
 	)
 	flag.Parse()
 
@@ -52,44 +57,136 @@ func main() {
 		sklog.Fatalf("error connecting to the database: %s", err)
 	}
 
-	old := fs_ignorestore.New(ctx, fsClient)
-	var rules []ignore.Rule
-	// Wait for initial fetch to complete
-	for len(rules) == 0 {
-		time.Sleep(1000)
-		rules, err = old.List(ctx)
-		if err != nil {
-			sklog.Fatalf("Loading old rules: %s", err)
+	if *remakePatchSetSchema {
+		sklog.Infof("Waiting 3s before dropping tables. Stop now if that is accidental.")
+		time.Sleep(3 * time.Second)
+		// Tryjobs table has Patchsets foreign key, so it must be dropped first.
+		if _, err := db.Exec(ctx, `DROP TABLE Tryjobs`); err != nil {
+			sklog.Fatalf("could not drop existing table Tryjobs: %s", err)
+		}
+		if _, err := db.Exec(ctx, `DROP TABLE Patchsets`); err != nil {
+			sklog.Fatalf("could not drop existing table Patchsets: %s", err)
+		}
+		if _, err := db.Exec(ctx, schema.Schema); err != nil {
+			sklog.Fatalf("could not recreate table(s): %s", err)
 		}
 	}
 
-	err = storeToSQL(ctx, db, rules)
-	if err != nil {
-		sklog.Fatalf("Error storing to SQL: %s", err)
+	crsNames := strings.Split(*crsList, ",")
+	for _, crs := range crsNames {
+		old := fs_clstore.New(fsClient, crs)
+
+		start := 0
+		const limit = 500
+		const numChunks = 8
+		for {
+			xcl, _, err := old.GetChangelists(ctx, clstore.SearchOptions{
+				StartIdx: start,
+				Limit:    limit,
+			})
+			if err != nil {
+				sklog.Fatalf("Fetching Changelists for system %s %d: %s", crs, start, err)
+			}
+			if len(xcl) == 0 {
+				break
+			}
+			if err := storeCLsToSQL(ctx, db, crs, xcl); err != nil {
+				sklog.Fatalf("Storing CLs for system %s %d: %s", crs, start, err)
+			}
+			// For each CL, fetch the patchsets associated with it in parallel.
+			patchsetsPerCL := make([][]code_review.Patchset, len(xcl))
+			chunkSize := (len(xcl) / numChunks) + 1 // add one to avoid integer truncation.
+			err = util.ChunkIterParallel(ctx, len(xcl), chunkSize, func(ctx context.Context, startIdx int, endIdx int) error {
+				for i, cl := range xcl[startIdx:endIdx] {
+					if err := ctx.Err(); err != nil {
+						sklog.Errorf("Context error: %s", err)
+						return nil
+					}
+					xps, err := old.GetPatchsets(ctx, cl.SystemID)
+					if err != nil {
+						return skerr.Wrapf(err, "fetching patchsets for %s-%s", crs, cl.SystemID)
+					}
+					if len(xps) == 0 {
+						sklog.Infof("CL %s has no Patchsets", cl.SystemID)
+					}
+					patchsetsPerCL[startIdx+i] = xps
+				}
+				return nil
+			})
+			if err != nil {
+				sklog.Fatalf("Fetching patchsets: %s", err)
+			}
+			if err := storePSsToSQL(ctx, db, crs, patchsetsPerCL); err != nil {
+				sklog.Fatalf("Storing PSs for system %s %d: %s", crs, start, err)
+			}
+
+			sklog.Infof("Stored data for %d CLs and Patchsets (%d)", len(xcl), start)
+			if len(xcl) != limit {
+				break
+			}
+			start += len(xcl)
+		}
+		sklog.Infof("Done with %s", crs)
 	}
-	sklog.Infof("Done")
 }
 
-func storeToSQL(ctx context.Context, db *pgxpool.Pool, rules []ignore.Rule) error {
-	if len(rules) == 0 {
-		return skerr.Fmt("Rules cannot be empty")
-	}
-	const statement = `INSERT INTO IgnoreRules (creator_email, updated_email, expires, note, query) VALUES `
-	const valuesPerRow = 5
-	placeholders := sql.ValuesPlaceholders(valuesPerRow, len(rules))
-	arguments := make([]interface{}, 0, valuesPerRow*len(rules))
-	for _, rule := range rules {
-		query, err := url.ParseQuery(rule.Query)
-		if err != nil {
-			// Hopefully never happens
-			return skerr.Wrapf(err, "invalid ignore query %q", rule.Query)
-		}
-		arguments = append(arguments, rule.CreatedBy, rule.UpdatedBy, rule.Expires, rule.Note, query)
-	}
+func qualify(system, id string) string {
+	return system + "_" + id
+}
 
+func convertFromStatusEnum(status code_review.CLStatus) schema.ChangelistStatus {
+	switch status {
+	case code_review.Abandoned:
+		return schema.StatusAbandoned
+	case code_review.Open:
+		return schema.StatusOpen
+	case code_review.Landed:
+		return schema.StatusLanded
+	}
+	sklog.Warningf("Unknown status: %d", status)
+	return schema.StatusAbandoned
+}
+
+func storeCLsToSQL(ctx context.Context, db *pgxpool.Pool, crs string, xcl []code_review.Changelist) error {
+	const statement = `
+UPSERT INTO Changelists (changelist_id, system, status, owner_email, subject, last_ingested_data) VALUES `
+	const valuesPerRow = 6
+	placeholders := sql.ValuesPlaceholders(valuesPerRow, len(xcl))
+	arguments := make([]interface{}, 0, valuesPerRow*len(xcl))
+	for _, cl := range xcl {
+		arguments = append(arguments, qualify(crs, cl.SystemID), crs,
+			convertFromStatusEnum(cl.Status), cl.Owner, cl.Subject, cl.Updated)
+	}
 	_, err := db.Exec(ctx, statement+placeholders, arguments...)
 	if err != nil {
-		return skerr.Wrapf(err, "storing %d rules", len(rules))
+		return skerr.Wrapf(err, "storing %d CLs", len(xcl))
 	}
+	return nil
+}
+
+func storePSsToSQL(ctx context.Context, db *pgxpool.Pool, crs string, patchsets [][]code_review.Patchset) error {
+	const statement = `
+UPSERT INTO Patchsets (patchset_id, system, changelist_id, ps_order, git_hash,
+  commented_on_cl, last_checked_if_comment_necessary) VALUES `
+	const valuesPerRow = 7
+
+	arguments := make([]interface{}, 0, valuesPerRow*len(patchsets))
+	totalPS := 0
+	for i := range patchsets {
+		for _, ps := range patchsets[i] {
+			totalPS++
+			arguments = append(arguments, qualify(crs, ps.SystemID), crs, qualify(crs, ps.ChangelistID),
+				ps.Order, ps.GitHash, ps.CommentedOnCL, ps.LastCheckedIfCommentNecessary)
+		}
+	}
+	if totalPS == 0 {
+		return nil
+	}
+	placeholders := sql.ValuesPlaceholders(valuesPerRow, totalPS)
+	_, err := db.Exec(ctx, statement+placeholders, arguments...)
+	if err != nil {
+		return skerr.Wrapf(err, "storing %d PSs", totalPS)
+	}
+	sklog.Infof("Stored %d PSs", totalPS)
 	return nil
 }
