48de4ac9f984fd5ea4434caab827111caf577c54
Kevin Lubick <kjlubick@google.com>
2021-01-19 10:11:35 -0500

    [gold] Add index to patchsets
    Time to find existing patchset when ingesting data
    no index: 150ms
    with index: 5ms
    
    I added this index by hand by running the following command
    for each database in prod:
    
    CREATE INDEX cl_order_idx ON angle.Patchsets (changelist_id, ps_order);
    
    Bug: skia:10582
    Change-Id: Ie88bbbdd492db19998f97b5c508f0a28c36f8f12

--- golden/go/sql/schema/sql.go
+++ golden/go/sql/schema/sql.go
@@ -76,7 +76,8 @@ CREATE TABLE IF NOT EXISTS Patchsets (
   ps_order INT2 NOT NULL,
   git_hash STRING NOT NULL,
   commented_on_cl BOOL NOT NULL,
-  last_checked_if_comment_necessary TIMESTAMP WITH TIME ZONE NOT NULL
+  last_checked_if_comment_necessary TIMESTAMP WITH TIME ZONE NOT NULL,
+  INDEX cl_order_idx (changelist_id, ps_order)
 );
 CREATE TABLE IF NOT EXISTS PrimaryBranchParams (
   start_commit_id INT4,
