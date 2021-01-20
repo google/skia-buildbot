5d340aa98f662f3e268302ddf4c69ab77f59fcf3
Joe Gregorio <jcgregorio@google.com>
2021-01-20 16:28:08 -0500

    Convert Particles to TypeScript.
    Change-Id: Iec1f7a877f004ca2605615a2ff7161d710d2f8d4

---
+++ particles/go/types/types.go
@@ -0,0 +1,6 @@
+package types
+
+// TODO(jcgregorio) Move to module so it can be exported to TS.
+type JSONBody struct {
+	Body string
+}
---
+++ particles/modules/particles-config-sk/particles-config-sk.ts
@@ -0,0 +1,95 @@
+/**
+ * @module particles-config-sk
+ * @description <h2><code>particles-config-sk</code></h2>
+ *
+ * <p>
+ *   A dialog for configuring how to render a Particles JSON file.
+ * </p>
+ *
+ */
+import 'elements-sk/styles/buttons';
+import { define } from 'elements-sk/define';
+import { errorMessage } from 'elements-sk/errorMessage';
+import { html } from 'lit-html';
+import { $$ } from 'common-sk/modules/dom';
+import { ElementSk } from '../../../infra-sk/modules/ElementSk';
+
+export interface ParticlesConfig {
+  body: any;
+}
+
+export class ParticlesConfigSk extends ElementSk {
+  private body: any | null = null;
+
+  private fileInput: HTMLInputElement | null = null;
+
+  private dialog: HTMLDialogElement | null = null;
+
+  private startValue: ParticlesConfig | null = null;
+
+  private resolve: ((p: ParticlesConfig)=> void) | null = null;
+
+  constructor() {
+    super(ParticlesConfigSk.template);
+  }
+
+  private static template = (ele: ParticlesConfigSk) => html`
+  <dialog>
+    <label class=file>Particles file to upload
+      <input type=file id=file @change=${ele.onFileChange}/>
+    </label>
+    <div id=dialog-buttons>
+      <button @click=${ele.cancel}>Cancel</button>
+      <button class=action ?disabled=${!ele.body} @click=${ele.ok}>OK</button>
+    </div>
+</dialog>
+  `;
+
+  show(config: ParticlesConfig): Promise<ParticlesConfig> {
+    this.startValue = Object.assign({}, config);
+    return new Promise<ParticlesConfig>((resolve) => {
+      this.body = config.body;
+      this.resolve = resolve;
+      this._render();
+      this.dialog!.showModal();
+    });
+  }
+
+  connectedCallback(): void {
+    super.connectedCallback();
+    this._render();
+    this.fileInput = $$<HTMLInputElement>('#file', this);
+    this.dialog = $$<HTMLDialogElement>('dialog', this);
+  }
+
+  private onFileChange() {
+    const reader = new FileReader();
+    reader.addEventListener('load', () => {
+      const body = reader.result as string;
+      let parsed: any = {};
+      try {
+        parsed = JSON.parse(body);
+      } catch (error) {
+        errorMessage(`Not a valid JSON file: ${error}`);
+        return;
+      }
+      this.body = parsed;
+      this._render();
+    });
+    reader.readAsText(this.fileInput!.files![0]);
+  }
+
+  private cancel() {
+    this.dialog!.close();
+    this.resolve!(this.startValue!);
+  }
+
+  private ok() {
+    this.dialog!.close();
+    this.resolve!({
+      body: this.body,
+    });
+  }
+}
+
+define('particles-config-sk', ParticlesConfigSk);
