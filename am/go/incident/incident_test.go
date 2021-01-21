ab9b6d7c79366c69fc7d8699171e752dd8fd0623
Leandro Lovisolo <lovisolo@google.com>
2021-01-20 23:02:02 -0500

    [bazel] Separate go_test targets for manual tests.
    Bug: skia:11110
    Change-Id: I4bc3dc35cc2fa910ce4678774fce8c08bdab045d

--- am/go/incident/incident_test.go
+++ am/go/incident/incident_test.go
@@ -5,11 +5,8 @@ import (
 	"time"
 
 	"github.com/stretchr/testify/assert"
-	"go.skia.org/infra/am/go/note"
 	"go.skia.org/infra/am/go/silence"
 	"go.skia.org/infra/go/alerts"
-	"go.skia.org/infra/go/ds"
-	"go.skia.org/infra/go/ds/testutil"
 	"go.skia.org/infra/go/paramtools"
 	"go.skia.org/infra/go/testutils/unittest"
 )
@@ -167,94 +164,6 @@ func TestIsSilenced(t *testing.T) {
 		},
 	}
 	assert.False(t, i.IsSilenced(silences, true))
-}
-
-func TestAlertArrival(t *testing.T) {
-	unittest.ManualTest(t) // Disabled due to flaky cloud emulator on the CI
-
-	cleanup := testutil.InitDatastore(t, ds.INCIDENT_AM, ds.INCIDENT_ACTIVE_PARENT_AM)
-	defer cleanup()
-
-	st := NewStore(ds.DS, []string{"ignore"})
-
-	m := map[string]string{
-		"ignore":     "pod_123",
-		alerts.TYPE:  alerts.TYPE_ALERTS,
-		alerts.STATE: alerts.STATE_ACTIVE,
-		ALERT_NAME:   "BotUnemployed",
-		"bot":        "skia-rpi-064",
-		CATEGORY:     "infra",
-	}
-	a, err := st.AlertArrival(m)
-	assert.NoError(t, err)
-	assert.Equal(t, true, a.Active)
-	assert.NotEqual(t, "", a.Key)
-
-	// A second alert arrival with the same config doesn't create
-	// a new Incident.
-	a, err = st.AlertArrival(m)
-	assert.NoError(t, err)
-	assert.Equal(t, true, a.Active)
-	assert.NotEqual(t, "", a.Key)
-
-	all, err := st.GetAll()
-	assert.NoError(t, err)
-	assert.Equal(t, 1, len(all))
-
-	// But an alert arrival with a different config does create
-	// a new incident.
-	m[ALERT_NAME] = "BotMissing"
-	delete(m, ID)
-	b, err := st.AlertArrival(m)
-	assert.NoError(t, err)
-	assert.Equal(t, true, b.Active)
-	assert.NotEqual(t, "", b.Key)
-	assert.NotEqual(t, a.ID, b.ID)
-
-	// Add a note.
-	bb, err := st.AddNote(b.Key, note.Note{
-		Text:   "Stuff happened.",
-		Author: "fred@example.com",
-		TS:     time.Now().Unix(),
-	})
-	assert.NoError(t, err)
-	assert.Equal(t, "Stuff happened.", bb.Notes[0].Text)
-	assert.Equal(t, b.Key, bb.Key)
-
-	// Fail to add note, bad key.
-	_, err = st.AddNote("badkey", note.Note{})
-	assert.Error(t, err)
-
-	// Delete note, bad index.
-	_, err = st.DeleteNote(b.Key, 1)
-	assert.Error(t, err)
-
-	// Delete note.
-	b, err = st.DeleteNote(b.Key, 0)
-	assert.NoError(t, err)
-	assert.Equal(t, 0, len(b.Notes))
-
-	// Assign
-	b, err = st.Assign(b.Key, "barney@example.org")
-	assert.NoError(t, err)
-	assert.Equal(t, "barney@example.org", b.Params[ASSIGNED_TO])
-
-	m[alerts.STATE] = alerts.STATE_RESOLVED
-	b, err = st.AlertArrival(m)
-	assert.NoError(t, err)
-	assert.False(t, b.Active)
-
-	// Archive
-	b, err = st.Archive(b.Key)
-	assert.NoError(t, err)
-
-	recent, err := st.GetRecentlyResolvedForID(b.ID, "")
-	assert.NoError(t, err)
-	assert.Len(t, recent, 1)
-
-	recent, err = st.GetRecentlyResolvedForID(b.ID, b.Key)
-	assert.NoError(t, err)
-	assert.Len(t, recent, 0)
 }
 
 func TestIdForAlert(t *testing.T) {
--- go/ds/ds_test.go
+++ go/ds/ds_test.go
@@ -2,23 +2,11 @@ package ds
 
 import (
 	"context"
-	"math/rand"
-	"sort"
 	"testing"
-	"time"
 
-	"cloud.google.com/go/datastore"
 	"github.com/stretchr/testify/require"
 	"go.skia.org/infra/go/testutils/unittest"
 )
-
-const TEST_KIND = Kind("DS_TEST_KIND")
-
-type testEntity struct {
-	Key      *datastore.Key `datastore:"__key__"`
-	Random   int64
-	Sortable int64
-}
 
 func TestDeleteAll(t *testing.T) {
 	unittest.LargeTest(t)
@@ -32,67 +20,12 @@ func TestDeleteAll(t *testing.T) {
 	// Ignore the cleanup-call returned by addRandEntities since we are
 	// calling DeleteAll in this function anyway.
 	_, _ = addRandEntities(t, client, nEntries, maxID)
-	_, err := DeleteAll(client, TEST_KIND, true)
+	_, err := DeleteAll(client, testKind, true)
 	require.NoError(t, err)
 
-	count, err := client.Count(context.TODO(), NewQuery(TEST_KIND))
+	count, err := client.Count(context.TODO(), NewQuery(testKind))
 	require.NoError(t, err)
 	require.Equal(t, 0, count)
-}
-
-func TestMigrateData(t *testing.T) {
-	// disabled: https://bugs.chromium.org/p/skia/issues/detail?id=9061
-	unittest.ManualTest(t)
-	ctx := context.TODO()
-
-	// Create the source project.
-	require.NoError(t, InitForTesting("src-project", "test-namespace"))
-	srcClient := DS
-
-	// Populate the source.
-	nEntries := 1200
-	maxID := int64(nEntries * 10)
-	entitiesSrc, cleanupSrc := addRandEntities(t, srcClient, nEntries, maxID)
-	defer cleanupSrc()
-	srcCount, err := srcClient.Count(ctx, NewQuery(TEST_KIND))
-	require.NoError(t, err)
-	require.Equal(t, nEntries, srcCount)
-
-	// Create the destination project.
-	require.NoError(t, InitForTesting("dest-project", "test-namespace"))
-	destClient := DS
-	defer func() {
-		_, err := DeleteAll(destClient, TEST_KIND, true)
-		require.NoError(t, err)
-	}()
-	destCountEmpty, err := destClient.Count(ctx, NewQuery(TEST_KIND))
-	require.NoError(t, err)
-	require.Equal(t, 0, destCountEmpty)
-
-	// Migrate from source to destination reusing the old keys.
-	require.NoError(t, MigrateData(ctx, srcClient, destClient, TEST_KIND, false /* createNewKey */))
-	wait(t, destClient, TEST_KIND, nEntries)
-	destCountPopulated, err := destClient.Count(ctx, NewQuery(TEST_KIND))
-	require.NoError(t, err)
-	require.Equal(t, nEntries, destCountPopulated)
-	// Spot check to make sure source and destination data match.
-	for _, entitySrc := range []*testEntity{entitiesSrc[0], entitiesSrc[10], entitiesSrc[nEntries-1]} {
-		entityDest := &testEntity{}
-		require.NoError(t, destClient.Get(ctx, entitySrc.Key, entityDest))
-		require.Equal(t, entitySrc.Key, entityDest.Key)
-		require.Equal(t, entitySrc.Random, entityDest.Random)
-		require.Equal(t, entitySrc.Sortable, entityDest.Sortable)
-	}
-	// Cleanup.
-	_, err = DeleteAll(destClient, TEST_KIND, true)
-	require.NoError(t, err)
-
-	// Migrate from source to destination by creating new keys.
-	require.NoError(t, MigrateData(ctx, srcClient, destClient, TEST_KIND, true /* createNewKey */))
-	wait(t, destClient, TEST_KIND, nEntries)
-	destCountPopulated, err = destClient.Count(ctx, NewQuery(TEST_KIND))
-	require.NoError(t, err)
-	require.Equal(t, nEntries, destCountPopulated)
 }
 
 func TestIterKeys(t *testing.T) {
@@ -106,7 +39,7 @@ func TestIterKeys(t *testing.T) {
 	defer cleanup()
 
 	// Iterate over the type and collect the instances
-	iterCh, err := IterKeys(client, TEST_KIND, 10)
+	iterCh, err := IterKeys(client, testKind, 10)
 	require.NoError(t, err)
 	ctx := context.TODO()
 	var found []*testEntity
@@ -119,43 +52,4 @@ func TestIterKeys(t *testing.T) {
 		found = append(found, target...)
 	}
 	require.Equal(t, exp, found)
-}
-
-func addRandEntities(t *testing.T, client *datastore.Client, nEntries int, maxID int64) ([]*testEntity, func()) {
-	_, err := DeleteAll(client, TEST_KIND, true)
-	require.NoError(t, err)
-
-	cleanup := func() {
-		_, err := DeleteAll(client, TEST_KIND, true)
-		require.NoError(t, err)
-	}
-
-	// Create a test type and fill it with random values
-	exp := make([]*testEntity, 0, nEntries)
-	ctx := context.TODO()
-	for i := 0; i < nEntries; i++ {
-		newEntry := &testEntity{
-			Random:   rand.Int63(),
-			Sortable: (rand.Int63() % maxID) + 1,
-		}
-		newEntry.Key, err = client.Put(ctx, NewKey(TEST_KIND), newEntry)
-		require.NoError(t, err)
-		exp = append(exp, newEntry)
-	}
-
-	sort.Slice(exp, func(i, j int) bool { return exp[i].Key.ID < exp[j].Key.ID })
-	wait(t, client, TEST_KIND, nEntries)
-	require.Equal(t, nEntries, len(exp))
-	return exp, cleanup
-}
-
-func wait(t *testing.T, client *datastore.Client, kind Kind, expectedCount int) {
-	for {
-		count, err := client.Count(context.TODO(), NewQuery(kind))
-		require.NoError(t, err)
-		if count == expectedCount {
-			return
-		}
-		time.Sleep(500 * time.Millisecond)
-	}
 }
