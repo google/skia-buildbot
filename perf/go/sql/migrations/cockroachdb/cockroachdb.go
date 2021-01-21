131a5ab764111bc3d0fb90bac7c3d3a6db282ae0
Leandro Lovisolo <lovisolo@google.com>
2021-01-21 15:09:55 -0500

    [bazel] Perf: Fix tests using go.rice.
    Bug: skia:11110
    Change-Id: I53c14eaa6abb0ef754b71ef842a4c0ec67b4d7aa
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/355500
    Commit-Queue: Leandro Lovisolo <lovisolo@google.com>
    Reviewed-by: Joe Gregorio <jcgregorio@google.com>

--- perf/go/sql/migrations/cockroachdb/cockroachdb.go
+++ perf/go/sql/migrations/cockroachdb/cockroachdb.go
@@ -3,13 +3,25 @@ package cockroachdb
 
 import (
 	"net/http"
+	"os"
 
 	rice "github.com/GeertJohan/go.rice"
+	"go.skia.org/infra/go/repo_root"
 	"go.skia.org/infra/go/skerr"
 )
 
 // New returns an http.FileSystem with all the migrations for a cockroachdb database.
 func New() (http.FileSystem, error) {
+	// For some reason, go.rice's path resolution algorithm does not work under "bazel test ..."
+	// unless we change into the workspace's root directory.
+	workspaceRoot, err := repo_root.Get()
+	if err != nil {
+		return nil, skerr.Wrap(err)
+	}
+	if err := os.Chdir(workspaceRoot); err != nil {
+		return nil, skerr.Wrap(err)
+	}
+
 	conf := rice.Config{
 		LocateOrder: []rice.LocateMethod{rice.LocateFS, rice.LocateEmbedded},
 	}
