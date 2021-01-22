ecfea0d0b3d121350a85f1151c2f9a83b7b0d089
Kevin Lubick <kjlubick@google.com>
2021-01-22 14:12:21 -0500

    [goldctl] Thread context through all calls instead of cmd
    This should make it easier to stub out things and write tests.
    
    Suggested reviewing order:
     - check out main.go first for the introduction of the values
       into the context.
     - cmd_auth_test.go for an example of what tests would be like
       using this approach.
     - Rest of the files for the replacements of things.
    
    Bug: skia:11202
    Change-Id: I261be357a19b91cd40ddf99c0387206b535216bc

--- gold-client/cmd/goldctl/main.go
+++ gold-client/cmd/goldctl/main.go
@@ -3,15 +3,21 @@ package main
 // goldctl is a CLI for working with the Gold service.
 
 import (
+	"context"
 	"fmt"
+	"io"
 	"os"
 
 	"github.com/spf13/cobra"
 )
 
-// Flag names used by various commands.
 const (
+	// All commands that use a work-dir have it defined as this string.
 	fstrWorkDir = "work-dir"
+
+	errWriterKey = contextKey("errWriter")
+	logWriterKey = contextKey("logWriter")
+	exitorKey    = contextKey("exitor")
 )
 
 // Flags used throughout all commands.
@@ -19,6 +25,8 @@ var (
 	flagVerbose bool
 	flagDryRun  bool
 )
+
+type contextKey string
 
 func main() {
 	// Set up the root command.
@@ -40,15 +48,18 @@ It can be used directly or in a scripted environment. `,
 	rootCmd.AddCommand(getMatchCmd())
 	rootCmd.AddCommand(getWhoamiCmd())
 
+	ctx := executionContext(context.Background(), os.Stdout, os.Stderr, os.Exit)
+
 	// Execute the root command.
-	if cmd, err := rootCmd.ExecuteC(); err != nil {
-		exitProcess(cmd, 1)
+	if err := rootCmd.ExecuteContext(ctx); err != nil {
+		exitProcess(ctx, 1)
 	}
 }
 
-func notImplemented(cmd *cobra.Command) {
-	logErr(cmd, fmt.Errorf("Command not implemented yet."))
-	exitProcess(cmd, 1)
+func executionContext(ctx context.Context, log, err io.Writer, exit exitWithCode) context.Context {
+	ctx = context.WithValue(ctx, logWriterKey, log)
+	ctx = context.WithValue(ctx, errWriterKey, err)
+	return context.WithValue(ctx, exitorKey, exit)
 }
 
 // getFileOrStdin returns an file to read from based on the whether file flag was set.
@@ -65,64 +76,59 @@ func getFileOrStdin(inputFile string) (*os.File, func() error, error) {
 }
 
 // logErrf logs a formatted error based on the output settings of the command.
-func logErrf(cmd *cobra.Command, format string, args ...interface{}) {
-	_, _ = fmt.Fprintf(cmd.OutOrStderr(), format, args...)
-}
-
-// logErr logs an error based on the output settings of the command.
-func logErr(cmd *cobra.Command, args ...interface{}) {
-	_, _ = fmt.Fprint(cmd.OutOrStderr(), args...)
-}
-
-// logErrAndExit logs a formatted error and exits with a non-zero exit code.
-func logErrAndExit(cmd *cobra.Command, err error) {
-	logErr(cmd, err)
-	exitProcess(cmd, 1)
+func logErrf(ctx context.Context, format string, args ...interface{}) {
+	w := ctx.Value(errWriterKey).(io.Writer)
+	_, _ = fmt.Fprintf(w, format, args...)
 }
 
 // logErrfAndExit logs an error and exits with a non-zero exit code.
-func logErrfAndExit(cmd *cobra.Command, format string, err error) {
-	logErrf(cmd, format, err)
-	exitProcess(cmd, 1)
+func logErrfAndExit(ctx context.Context, format string, err error) {
+	logErrf(ctx, format, err)
+	exitProcess(ctx, 1)
 }
 
 // ifErrLogExit logs an error if the proviced error is not nil and exits
 // with a non-zero exit code.
-func ifErrLogExit(cmd *cobra.Command, err error) {
+func ifErrLogExit(ctx context.Context, err error) {
 	if err != nil {
-		logErrf(cmd, "Error running command: ''%s''\n", err)
-		exitProcess(cmd, 1)
+		logErrf(ctx, "Error running command: ''%s''\n", err)
+		exitProcess(ctx, 1)
 	}
 }
 
 // logInfo logs the given arguments based on the output settings of the command.
-func logInfo(cmd *cobra.Command, args ...interface{}) {
-	_, _ = fmt.Fprint(cmd.OutOrStdout(), args...)
+func logInfo(ctx context.Context, args ...interface{}) {
+	w := ctx.Value(logWriterKey).(io.Writer)
+	_, _ = fmt.Fprint(w, args...)
 }
 
 // logInfo logs the given arguments based on the output settings of the command.
-func logInfof(cmd *cobra.Command, format string, args ...interface{}) {
-	_, _ = fmt.Fprintf(cmd.OutOrStdout(), format, args...)
+func logInfof(ctx context.Context, format string, args ...interface{}) {
+	w := ctx.Value(logWriterKey).(io.Writer)
+	_, _ = fmt.Fprintf(w, format, args...)
 }
 
 // logVerbose logs the given arguments if the verbose flag is true.
-func logVerbose(cmd *cobra.Command, args ...interface{}) {
+func logVerbose(ctx context.Context, args ...interface{}) {
 	if flagVerbose {
-		logInfo(cmd, args...)
+		logInfo(ctx, args...)
 	}
 }
 
-// exitProcess terminates the process with the given exit code.
-func exitProcess(cmd *cobra.Command, exitCode int) {
-	// If this is a dryrun don't return a non-zero exit code.
-	os.Exit(exitCode)
+type exitWithCode func(code int)
+
+// exitProcess terminates the process with the given exit code. Note, for dry runs, we will
+// typically return a zero exit code.
+func exitProcess(ctx context.Context, exitCode int) {
+	exit := ctx.Value(exitorKey).(exitWithCode)
+	exit(exitCode)
 }
 
-// Must is a helper for dealing with errors that shouldn't happen, or if they do,
+// must is a helper for dealing with errors that shouldn't happen, or if they do,
 // it's an error with the code, not how the user is holding it.
-func Must(err error) {
+func must(err error) {
 	if err != nil {
 		fmt.Printf("Fatal startup error: %s\n", err)
-		os.Exit(2)
+		panic(err)
 	}
 }
