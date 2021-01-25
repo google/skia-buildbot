b187402e93103a68ead9032eb58c038412103352
Joe Gregorio <jcgregorio@google.com>
2021-01-25 13:32:49 -0500

    [particles] Convert Particles to TypeScript and change the   backend to use ScrapExchange.
    Also:
     - Adds puppeteer tests.
     - Changes /static/ to /dist/ to conform to our convention. Thus
       the follow-on CL: https://skia-review.googlesource.com/c/skia/+/356762
     - The particles_particles-player-sk Golden will look better
       when a new npm version of CanvasKit lands with Particles support.
     - Also removed lockDown mode, we will just run a copy in skia-corp
       if we want a locked-down version.
    
    Bugs: skia:11108
    Change-Id: Iec1f7a877f004ca2605615a2ff7161d710d2f8d4

---
+++ particles/modules/particles-player-sk/particles-player-sk_puppeteer_test.ts
@@ -0,0 +1,39 @@
+import * as path from 'path';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { expect } from 'chai';
+import {
+  loadCachedTestBed, takeScreenshot, TestBed,
+} from '../../../puppeteer-tests/util';
+
+describe('particles-player-sk', () => {
+  let testBed: TestBed;
+  before(async () => {
+    testBed = await loadCachedTestBed(
+      path.join(__dirname, '..', '..', 'webpack.config.ts'),
+    );
+  });
+
+  beforeEach(async () => {
+    await testBed.page.goto(`${testBed.baseUrl}/dist/particles-player-sk.html`);
+    await testBed.page.setViewport({ width: 400, height: 800 });
+  });
+
+  it('should render the demo page', async () => {
+    // Smoke test.
+    expect(await testBed.page.$$('particles-player-sk')).to.have.length(1);
+  });
+
+  describe('screenshots', () => {
+    it('shows controls for uniforms', async () => {
+      // Wait for WASM to load.
+      await testBed.page.waitForSelector('#results #loaded');
+      await takeScreenshot(testBed.page, 'particles', 'particles-player-sk-with-uniforms');
+    });
+    it('does not show controls if there are no uniforms', async () => {
+      // Wait for WASM to load.
+      await testBed.page.click('#nouniforms');
+      await testBed.page.waitForSelector('#results #loaded');
+      await takeScreenshot(testBed.page, 'particles', 'particles-player-sk-without-uniforms');
+    });
+  });
+});
