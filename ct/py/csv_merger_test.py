bd9ec5e8d89030f6906e9a43c85fa48731b3f8b3
Ravi Mistry <rmistry@google.com>
2021-01-25 11:22:13 -0500

    [CT] Migrate CT's python scripts to python3
    Change-Id: I83948cd18c851d9bfe6dd7ba3a569d9d676b123e

--- ct/py/csv_merger_test.py
+++ ct/py/csv_merger_test.py
@@ -5,12 +5,32 @@
 
 """Tests for module csv_merger."""
 
+import csv
 import csv_merger
 import os
 import unittest
 
 
 ACTUAL_OUTPUT_FILENAME = 'actual_output'
+
+
+def assertCSVs(expected_csv, actual_csv):
+  with open(expected_csv, 'r') as f:
+    expected_output_list = list(csv.DictReader(f))
+  with open(actual_csv, 'r') as f:
+    actual_output_list = list(csv.DictReader(f))
+  assert len(expected_output_list) == len(actual_output_list)
+  alreadyMatchedIndices = []
+  for i in range(len(expected_output_list)):
+    for j in range(len(actual_output_list)):
+      if j in alreadyMatchedIndices:
+        continue
+      if(expected_output_list[i] == actual_output_list[j]):
+        alreadyMatchedIndices.append(j)
+        break
+    else:
+      raise AssertionError("%s and %s are not equal" % (
+                               expected_csv, actual_csv))
 
 
 class TestCsvMerger(unittest.TestCase):
@@ -32,12 +52,8 @@ class TestCsvMerger(unittest.TestCase):
     merger.Merge()
 
     # Compare actual with expected.
-    expected_output = os.path.join(self._test_csv_dir, 'expected_output')
-    with open(expected_output, 'rb') as f:
-      expected_output_lines = f.readlines()
-    with open(self._actual_output, 'rb') as f:
-      actual_output_lines = f.readlines()
-    self.assertTrue(set(expected_output_lines) == set(actual_output_lines))
+    expected_csv = os.path.join(self._test_csv_dir, 'expected_output')
+    assertCSVs(expected_csv, self._actual_output)
 
   def test_E2EMergerWithStrings(self):
     merger = csv_merger.CsvMerger(csv_dir=self._test_csv_dir,
@@ -46,13 +62,9 @@ class TestCsvMerger(unittest.TestCase):
     merger.Merge()
 
     # Compare actual with expected.
-    expected_output = os.path.join(self._test_csv_dir,
+    expected_csv = os.path.join(self._test_csv_dir,
                                    'expected_output_with_strings')
-    with open(expected_output, 'rb') as f:
-      expected_output_lines = f.readlines()
-    with open(self._actual_output, 'rb') as f:
-      actual_output_lines = f.readlines()
-    self.assertTrue(set(expected_output_lines) == set(actual_output_lines))
+    assertCSVs(expected_csv, self._actual_output)
 
 
 if __name__ == '__main__':
