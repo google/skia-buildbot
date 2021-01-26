f7928664c5aa48eded0c700761099f4a16d6f7af
hernan <hernantorrisi@gmail.com>
2021-01-26 22:36:04 +0000

    Text editor event changed
    I'm changing the event dispatched by the text editor, so it can be used in a future PR to modify multiple animations at once.
    In order to do that, on this PR, the text editor dispatches the text data and the `skottie-sk` component takes care of updating the animation with the new values.
    
    This is an imported pull request from
    https://github.com/google/skia-buildbot/pull/48
    
    GitOrigin-RevId: 868a000ba85d2073978516bef02bb7931e1b7a8d
    Change-Id: Iec65d1e95218c5b30f043eaacd95a8ea2f1d4ef1

--- skottie/modules/skottie-sk/skottie-sk.js
+++ skottie/modules/skottie-sk/skottie-sk.js
@@ -324,8 +324,30 @@ define('skottie-sk', class extends HTMLElement {
   }
 
   _applyTextEdits(event) {
-    const animation = event.detail;
-    this._state.lottie = animation;
+    const texts = event.detail.texts;
+    const currentAnimation = JSON.parse(JSON.stringify(this._state.lottie));
+    texts.forEach((textData) => {
+      textData.items.forEach((item) => {
+        let layers;
+        // Searches for a composition that contains this layer
+        if (!item.parentId) {
+          layers = currentAnimation.layers;
+        } else {
+          const asset = currentAnimation.assets.find((assetItem) => assetItem.id === item.parentId);
+          layers = asset ? asset.layers : [];
+        }
+
+        // Replaces current animation layer with new layer value
+        layers.forEach((layer, index) => {
+          if (layer.ind === item.layer.ind
+            && layer.nm === item.layer.nm
+          ) {
+            layers[index] = item.layer;
+          }
+        });
+      });
+    });
+    this._state.lottie = currentAnimation;
     this._upload();
   }
 
