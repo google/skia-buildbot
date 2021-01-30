b2de76b0d859d9251fcc74e4b5cd90033c374b97
Leandro Lovisolo <lovisolo@google.com>
2021-01-29 19:44:04 -0500

    [bazel] Make emulator tests work on RBE.
    Bug: skia:11110
    Change-Id: I55b9702bdc4027021b2eba95622d07598bea6aca
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/360800
    Commit-Queue: Leandro Lovisolo <lovisolo@google.com>
    Reviewed-by: Joe Gregorio <jcgregorio@google.com>
    Reviewed-by: Eric Boren <borenet@google.com>
    Reviewed-by: Kevin Lubick <kjlubick@google.com>

--- go/emulators/emulators.go
+++ go/emulators/emulators.go
@@ -1,7 +1,5 @@
 // Package emulators contains functions to start and stop emulators, and utilities to work with the
 // various *_EMULATOR_HOST environment variables.
-//
-// TODO(lovisolo): Make the start/stop operations work on Bazel and RBE.
 package emulators
 
 // This package uses "os/exec" as opposed to "go.skia.org/infra/go/exec" in order to avoid the
@@ -13,11 +11,14 @@ import (
 	"bytes"
 	"fmt"
 	"io/ioutil"
+	"net"
 	"os"
 	"os/exec"
 	"regexp"
 	"strings"
+	"syscall"
 
+	"go.skia.org/infra/bazel/go/bazel"
 	"go.skia.org/infra/go/skerr"
 )
 
@@ -52,31 +53,21 @@ type emulatorInfo struct {
 }
 
 // cachedEmulatorInfos is populated by getEmulatorInfo() on the first call. Do not use directly.
-var cachedEmulatorInfos = map[Emulator]emulatorInfo{}
+var cachedEmulatorInfos = map[Emulator]*emulatorInfo{}
 
 // getEmulatorInfo builds and returns an emulatorInfo struct for the given emulator. The struct's
 // contents will depend on whether we are running on Bazel and RBE or not. All structs are computed
 // once and cached. Subsequent calls will return the same struct given the same input.
 func getEmulatorInfo(emulator Emulator) emulatorInfo {
 	if len(cachedEmulatorInfos) == 0 {
-		// Read the CockroachDB storage directory from an environment variable, or create a temp dir.
-		cockroachDbStoreDir := os.Getenv("COCKROACHDB_EMULATOR_STORE_DIR")
-		if cockroachDbStoreDir == "" {
-			var err error
-			cockroachDbStoreDir, err = ioutil.TempDir("", "crdb-emulator-*")
-			if err != nil {
-				panic("Error while creating temporary directory: " + skerr.Wrap(err).Error())
-			}
-		}
-
-		cachedEmulatorInfos = map[Emulator]emulatorInfo{
+		cachedEmulatorInfos = map[Emulator]*emulatorInfo{
 			BigTable: {
 				cmd:    "gcloud beta emulators bigtable start --host-port=localhost:%d --project=test-project",
 				envVar: "BIGTABLE_EMULATOR_HOST",
 				port:   8892,
 			},
 			CockroachDB: {
-				cmd:    fmt.Sprintf("cockroach start-single-node --insecure --listen-addr=localhost:%%d --store=%s", cockroachDbStoreDir),
+				cmd:    computeCockroachDBCmd(),
 				envVar: "COCKROACHDB_EMULATOR_HOST",
 				port:   8895,
 			},
@@ -96,13 +87,57 @@ func getEmulatorInfo(emulator Emulator) emulatorInfo {
 				port:   8893,
 			},
 		}
+
+		// Under Bazel and RBE, we choose an unused port to minimize the chances of parallel tests from
+		// interfering with each other.
+		if bazel.InRBE() {
+			for _, emulatorInfo := range cachedEmulatorInfos {
+				emulatorInfo.port = findUnusedTCPPort()
+			}
+		}
 	}
 
 	emulatorInfo, ok := cachedEmulatorInfos[emulator]
 	if !ok {
 		panic("Unknown emulator: " + emulator)
 	}
-	return emulatorInfo
+	return *emulatorInfo
+}
+
+func computeCockroachDBCmd() string {
+	// Read the CockroachDB storage directory from an environment variable, or create a temp dir.
+	cockroachDbStoreDir := os.Getenv("COCKROACHDB_EMULATOR_STORE_DIR")
+	if cockroachDbStoreDir == "" {
+		var err error
+		cockroachDbStoreDir, err = ioutil.TempDir("", "crdb-emulator-*")
+		if err != nil {
+			panic("Error while creating temporary directory: " + skerr.Wrap(err).Error())
+		}
+	}
+
+	cmd := fmt.Sprintf("cockroach start-single-node --insecure --listen-addr=localhost:%%d --store=%s", cockroachDbStoreDir)
+
+	// Under RBE, we want the web UI to be served on a random TCP port. This minimizes the chance of
+	// parallel tests from interfering with each other.
+	if bazel.InRBE() {
+		cmd += " --http-addr=localhost:0"
+	}
+
+	return cmd
+}
+
+// findUnusedTCPPort finds an unused TCP port by opening a TCP port on an unused port chosen by the
+// operating system, recovering the port number and immediately closing the socket.
+func findUnusedTCPPort() int {
+	listener, err := net.Listen("tcp", ":0")
+	if err != nil {
+		panic(skerr.Wrap(err))
+	}
+	port := listener.Addr().(*net.TCPAddr).Port
+	if err = listener.Close(); err != nil {
+		panic(skerr.Wrap(err))
+	}
+	return port
 }
 
 // GetEmulatorHostEnvVar returns the contents of the *_EMULATOR_HOST environment variable corresponding to
@@ -111,29 +146,82 @@ func GetEmulatorHostEnvVar(emulator Emulator) string {
 	return os.Getenv(getEmulatorInfo(emulator).envVar)
 }
 
-// GetEmulatorHostEnvVarName returns the name of the *_EMULATOR_HOST environment variable corresponding
-// to the given emulator.
+// SetEmulatorHostEnvVar sets the *_EMULATOR_HOST environment variable for the given emulator.
+func SetEmulatorHostEnvVar(emulator Emulator) error {
+	emulatorInfo := getEmulatorInfo(emulator)
+	return skerr.Wrap(os.Setenv(emulatorInfo.envVar, fmt.Sprintf("localhost:%d", emulatorInfo.port)))
+}
+
+// UnsetAllEmulatorHostEnvVars unsets the *_EMULATOR_HOST environment variables for all known
+// emulators.
+func UnsetAllEmulatorHostEnvVars() error {
+	for _, emulator := range AllEmulators {
+		if err := os.Setenv(getEmulatorInfo(emulator).envVar, ""); err != nil {
+			return skerr.Wrap(err)
+		}
+	}
+	return nil
+}
+
+// GetEmulatorHostEnvVarName returns the name of the *_EMULATOR_HOST environment variable
+// corresponding to the given emulator.
 func GetEmulatorHostEnvVarName(emulator Emulator) string {
 	return getEmulatorInfo(emulator).envVar
 }
 
-// StartEmulator starts an emulator.
-func StartEmulator(emulator Emulator) error {
+// runningEmulators keeps track of which emulators have been started
+var runningEmulators = map[Emulator]bool{}
+
+// IsRunning returns true is the given emulator was started, or false if it hasn't been started or
+// if it's been stopped.
+func IsRunning(emulator Emulator) bool {
+	return runningEmulators[emulator]
+}
+
+// StartEmulatorIfNotRunning starts an emulator if it's not already running. Returns true if it
+// started the emulator, or false if the emulator was already running.
+func StartEmulatorIfNotRunning(emulator Emulator) (bool, error) {
+	if IsRunning(emulator) {
+		return false, nil
+	}
+
 	emulatorInfo := getEmulatorInfo(emulator)
 	programAndArgs := strings.Split(fmt.Sprintf(emulatorInfo.cmd, emulatorInfo.port), " ")
 	cmd := exec.Command(programAndArgs[0], programAndArgs[1:]...)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	if err := cmd.Start(); err != nil {
-		return skerr.Wrap(err)
+
+	if bazel.InRBE() {
+		cmd.SysProcAttr = &syscall.SysProcAttr{
+			// Under Bazel and RBE, emulators are launched by each individual go_test Bazel target. The
+			// below setting kills the emulator processes (and any other child processes) as soon as the
+			// parent process (i.e. the test runner) dies.
+			//
+			// If we don't do this, the emulators will continue running indefinitely, and Bazel will
+			// eventually time out while waiting for these child processes to die.
+			//
+			// This setting is Linux-only, but that's OK because our RBE instance consists of Linux
+			// machines exclusively. Alternative approaches include adding a TestMain function to our
+			// emulator tests that launches the emulators before running the test cases and kills them
+			// afterwards, or leveraging the test_on_env Bazel macro to run an environment binary
+			// alongside the tests which controls the emulators' lifecycle. Any of these approaches would
+			// work on non-Linux OSes as well.
+			Pdeathsig: syscall.SIGKILL,
+		}
 	}
-	return nil
+
+	if err := cmd.Start(); err != nil {
+		return false, skerr.Wrap(err)
+	}
+
+	runningEmulators[emulator] = true
+	return true, nil
 }
 
 // StartAllEmulators starts all known emulators.
 func StartAllEmulators() error {
 	for _, emulator := range AllEmulators {
-		if err := StartEmulator(emulator); err != nil {
+		if _, err := StartEmulatorIfNotRunning(emulator); err != nil {
 			return skerr.Wrap(err)
 		}
 	}
@@ -171,16 +259,29 @@ func StopAllEmulators() error {
 		procs[line] = fields[1]
 	}
 
+	signal := "SIGTERM"
+	if bazel.InRBE() {
+		// Under Bazel and RBE, we don't need graceful termination because the RBE containers are
+		// ephemeral. Killing the emulators with SIGKILL is faster and simpler.
+		signal = "SIGKILL"
+	}
+
 	// Kill each matching process.
 	for _, re := range emulatorProcsToKill {
 		for desc, id := range procs {
 			if re.MatchString(desc) {
-				if err := exec.Command("kill", id).Run(); err != nil {
+				if err := exec.Command("kill", "-s", signal, id).Run(); err != nil {
 					return skerr.Wrap(err)
 				}
 				delete(procs, desc)
 			}
 		}
 	}
+
+	// After this function returns, IsRunning(emulator) should return false for all known emulators.
+	for _, emulator := range AllEmulators {
+		runningEmulators[emulator] = false
+	}
+
 	return nil
 }
