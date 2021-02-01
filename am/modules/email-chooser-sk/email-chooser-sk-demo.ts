09181457d24f06b1e2cd7290f353e2d2cf175fe9
Ravi Mistry <rmistry@google.com>
2021-02-01 11:30:25 -0500

    [am] email-chooser-sk -> TS
    Compare with PS#1 for easier review.
    
    Bug: skia:11231
    Change-Id: Ib25b5f069083f08436f66fc3fc93ec18453564dd
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/361542
    Commit-Queue: Ravi Mistry <rmistry@google.com>
    Reviewed-by: Joe Gregorio <jcgregorio@google.com>

--- am/modules/email-chooser-sk/email-chooser-sk-demo.ts
+++ am/modules/email-chooser-sk/email-chooser-sk-demo.ts
@@ -1,7 +1,8 @@
 import './index';
+import { EmailChooserSk } from './email-chooser-sk';
 
 window.addEventListener('load', () => {
-  document.getElementById('email-chooser').open(
+  (document.getElementById('email-chooser') as EmailChooserSk).open(
     ['alice@example.com', 'bob@example.com', 'claire@example.com'],
     'bob@example.com',
   );
