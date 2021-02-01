31ca4a3d5ed79b4fd51dea5150a5fd27b7a4bcb6
Joe Gregorio <jcgregorio@google.com>
2021-02-01 16:31:30 -0500

    shaders wip
    Change-Id: Ie5fb29b46ada4337193a14f7ea533c852247a176

---
+++ shaders/go/shaders/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"flag"
+	"html/template"
+	"mime"
+	"net/http"
+	"path/filepath"
+
+	"github.com/gorilla/mux"
+	"github.com/unrolled/secure"
+	"go.skia.org/infra/go/baseapp"
+	"go.skia.org/infra/go/sklog"
+	"go.skia.org/infra/scrap/go/client"
+	"go.skia.org/infra/scrap/go/scrap"
+)
+
+// flags
+var (
+	scrapExchange = flag.String("scrapexchange", "http://scrapexchange:9000", "Scrap exchange service HTTP address.")
+)
+
+const ()
+
+// server is the state of the server.
+type server struct {
+	scrapClient scrap.ScrapExchange
+	templates   *template.Template
+}
+
+// See baseapp.Constructor.
+func new() (baseapp.App, error) {
+	// Need to set the mime-type for wasm files so streaming compile works.
+	if err := mime.AddExtensionType(".wasm", "application/wasm"); err != nil {
+		return nil, err
+	}
+	scrapClient, err := client.New(*scrapExchange)
+	if err != nil {
+		sklog.Fatalf("Failed to create scrap exchange client: %s", err)
+	}
+
+	srv := &server{
+		scrapClient: scrapClient,
+	}
+	srv.loadTemplates()
+	return srv, nil
+}
+
+func (srv *server) loadTemplates() {
+	srv.templates = template.Must(template.New("").Delims("{%", "%}").ParseFiles(
+		filepath.Join(*baseapp.ResourcesDir, "main.html"),
+	))
+}
+
+func (srv *server) mainHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	if *baseapp.Local {
+		srv.loadTemplates()
+	}
+	if err := srv.templates.ExecuteTemplate(w, "main.html", map[string]string{
+		// Look in webpack.config.js for where the nonce templates are injected.
+		"nonce": secure.CSPNonce(r.Context()),
+	}); err != nil {
+		sklog.Errorf("Failed to expand template: %s", err)
+	}
+}
+
+// See baseapp.App.
+func (srv *server) AddHandlers(r *mux.Router) {
+	r.HandleFunc("/", srv.mainHandler)
+}
+
+// See baseapp.App.
+func (srv *server) AddMiddleware() []mux.MiddlewareFunc {
+	return []mux.MiddlewareFunc{}
+}
+
+func main() {
+	baseapp.Serve(new, []string{"shaders.skia.org"})
+}
