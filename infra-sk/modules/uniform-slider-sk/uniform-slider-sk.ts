e87145d0f8ef5b3fec732f3a0eaef657fbd0b809
Joe Gregorio <jcgregorio@google.com>
2021-02-04 10:04:00 -0500

    [shaders] Add a uniform-slider-sk control for a single float uniform.
    Added to infra-sk since it will be shared between particles and shaders.
    
    Bug: skia:11272
    Change-Id: I3166f6ad667260704ebcbf32ad89841ba006689d

---
+++ infra-sk/modules/uniform-slider-sk/uniform-slider-sk.ts
@@ -0,0 +1,62 @@
+/**
+ * @module modules/uniform-slider-sk
+ * @description <h2><code>uniform-slider-sk</code></h2>
+ *
+ * Constructs a single slider for a single float uniform.
+ */
+import { $$ } from 'common-sk/modules/dom';
+import { define } from 'elements-sk/define';
+import { html } from 'lit-html';
+import { ElementSk } from '../../../infra-sk/modules/ElementSk';
+import { Uniform, UniformControl } from '../uniform/uniform';
+
+const defaultUniform: Uniform = {
+  name: 'u_time',
+  rows: 1,
+  columns: 1,
+  slot: 0,
+};
+
+export class UniformSliderSk extends ElementSk implements UniformControl {
+  private _uniform: Uniform = defaultUniform;
+
+  private input: HTMLInputElement | null = null;
+
+  private static template = (ele: UniformSliderSk) =>
+    html` <label>
+      <input min="0" max="1" step="0.001" type="range" />
+      ${ele.uniform.name}
+    </label>`;
+
+  constructor() {
+    super(UniformSliderSk.template);
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    this._render();
+    this.input = $$<HTMLInputElement>('input', this);
+  }
+
+  /** The description of the uniform. */
+  get uniform(): Uniform {
+    return this._uniform!;
+  }
+
+  set uniform(val: Uniform) {
+    if (val.columns !== 1 || val.rows !== 1) {
+      throw new Error(
+        'uniform-slider-sk can only work on a uniform of size 1.'
+      );
+    }
+    this._uniform = val;
+    this._render();
+  }
+
+  /** Copies the values of the control into the uniforms array. */
+  applyUniformValues(uniforms: Float32Array) {
+    uniforms[this.uniform.slot] = this.input!.valueAsNumber;
+  }
+}
+
+define('uniform-slider-sk', UniformSliderSk);
