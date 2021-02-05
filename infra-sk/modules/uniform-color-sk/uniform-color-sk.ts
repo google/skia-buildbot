0f11cb42da9da9a21f64f1f046bc4cf4de3027e4
Joe Gregorio <jcgregorio@google.com>
2021-02-05 10:08:27 -0500

    [shaders] Add uniform-color-sk control.
    Added to infra-sk since it will be used on particles and shaders.
    
    Bug:skia:11272
    Change-Id: I0f15a3a1ee6dcb441d22b5a28d522d2cc7c6c795

---
+++ infra-sk/modules/uniform-color-sk/uniform-color-sk.ts
@@ -0,0 +1,69 @@
+/**
+ * @module modules/uniform-color-sk
+ * @description <h2><code>uniform-color-sk</code></h2>
+ *
+ * A control for editing a float3 uniform which should be represented as a
+ * color.
+ *
+ * The color uniform values are floats in [0, 1] and are in RGB order.
+ */
+import { $$ } from 'common-sk/modules/dom';
+import { define } from 'elements-sk/define';
+import { html } from 'lit-html';
+import { ElementSk } from '../../../infra-sk/modules/ElementSk';
+import { Uniform, UniformControl } from '../uniform/uniform';
+
+const defaultUniform: Uniform = {
+  name: 'u_color',
+  rows: 1,
+  columns: 3,
+  slot: 0,
+};
+
+export class UniformColorSk extends ElementSk implements UniformControl {
+  private _uniform: Uniform = defaultUniform;
+
+  private input: HTMLInputElement | null = null;
+
+  private static template = (ele: UniformColorSk) => html` <label>
+    <input value="#808080" type="color" />
+    ${ele.uniform.name}
+  </label>`;
+
+  constructor() {
+    super(UniformColorSk.template);
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    this._render();
+    this.input = $$<HTMLInputElement>('input', this);
+  }
+
+  /** The description of the uniform. */
+  get uniform(): Uniform {
+    return this._uniform!;
+  }
+
+  set uniform(val: Uniform) {
+    if (val.columns !== 3 || val.rows !== 1) {
+      throw new Error('uniform-color-sk can only work on a uniform of size 3.');
+    }
+    this._uniform = val;
+    this._render();
+  }
+
+  /** Copies the values of the control into the uniforms array. */
+  applyUniformValues(uniforms: Float32Array): void {
+    // Set all three floats from the color.
+    const hex = this.input!.value;
+    const r = parseInt(hex.slice(1, 3), 16) / 255;
+    const g = parseInt(hex.slice(3, 5), 16) / 255;
+    const b = parseInt(hex.slice(5, 7), 16) / 255;
+    uniforms[this.uniform.slot] = r;
+    uniforms[this.uniform.slot + 1] = g;
+    uniforms[this.uniform.slot + 2] = b;
+  }
+}
+
+define('uniform-color-sk', UniformColorSk);
---
+++ infra-sk/modules/uniform/uniform.ts
@@ -0,0 +1,24 @@
+/** Describes a single shader uniform, which may be a single float value, or a 2
+ * dimensional value such as float4x4.
+ */
+export interface Uniform {
+  name: string;
+  rows: number;
+  columns: number;
+
+  /**
+   * The location in the uniforms value array that this uniform starts.
+   *
+   * Note that a uniform occupies rows*columns spots in the uniform value array.
+   * Note also that the values are in column major order.
+   */
+  slot: number;
+}
+
+/** The common interface that all controls for editing uniforms must implement. */
+export interface UniformControl {
+  uniform: Uniform;
+
+  /** Copies the values of the control into the uniforms array. */
+  applyUniformValues(uniforms: Float32Array): void;
+}
