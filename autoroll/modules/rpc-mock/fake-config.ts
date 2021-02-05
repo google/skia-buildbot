f2ee2584f368156c7ec7471ec61bd96b43f403fd
Eric Boren <borenet@google.com>
2021-02-05 14:47:18 -0500

    [autoroll] Add UI for editing configs
    Just a simple text box for now, but the end-to-end flow to CL creation works.
    
    Change-Id: Id7050832cf8657ef08d4fbd25339977676ef8bdd

---
+++ autoroll/modules/rpc-mock/fake-config.ts
@@ -0,0 +1,87 @@
+import {
+    Config,
+    CommitMsgConfig_BuiltIn,
+    GerritConfig_Config,
+    NotifierConfig_LogLevel,
+    NotifierConfig_MsgType,
+} from '../config';
+
+export function GetFakeConfig(): Config {
+    return {
+        rollerName: "skia-skiabot-test-autoroll",
+        childDisplayName: "Skia",
+        parentDisplayName: "Skiabot Test",
+        parentWaterfall: "https://status-staging.skia.org/repo/skiabot-test",
+        ownerPrimary: "borenet",
+        ownerSecondary: "rmistry",
+        contacts: ["borenet@google.com"],
+        serviceAccount: "skia-autoroll@skia-public.iam.gserviceaccount.com",
+        isInternal: false,
+        reviewer: ["borenet@google.com"],
+        rollCooldown: "",
+        timeWindow: "",
+        supportsManualRolls: true,
+        commitMsg: {
+            bugProject: "",
+            childLogUrlTmpl: "https://skia.googlesource.com/skia.git/+log/{{.RollingFrom}}..{{.RollingTo}}",
+            cqDoNotCancelTrybots: false,
+            cqExtraTrybots: undefined,
+            includeLog: true,
+            includeRevisionCount: true,
+            includeTbrLine: true,
+            includeTests: true,
+            builtIn: CommitMsgConfig_BuiltIn.DEFAULT,
+            custom: "",
+        },
+        gerrit: {
+            url: "https://skia-review.googlesource.com",
+            project: "skiabot-test",
+            config: GerritConfig_Config.CHROMIUM,
+        },
+        kubernetes: {
+            cpu: "1",
+            memory: "2Gi",
+            readinessFailureThreshold: 10,
+            readinessInitialDelaySeconds: 30,
+            readinessPeriodSeconds: 30,
+            disk: "",
+        },
+        parentChildRepoManager: {
+            gitilesParent: {
+                gitiles: {
+                    branch: "master",
+                    repoUrl: "https://skia.googlesource.com/skiabot-test.git",
+                },
+                dep: {
+                    primary: {
+                        id: "https://skia.googlesource.com/skia.git",
+                        path: "DEPS",
+                    },
+                },
+                gerrit: {
+                    url: "https://skia-review.googlesource.com",
+                    project: "skiabot-test",
+                    config: GerritConfig_Config.CHROMIUM,
+                },
+            },
+            gitilesChild: {
+                gitiles: {
+                    branch: "master",
+                    repoUrl: "https://skia.googlesource.com/skia.git",
+                },
+                path: "",
+            },
+        },
+        notifiers: [{
+            msgType: [NotifierConfig_MsgType.LAST_N_FAILED],
+            monorail: {
+                project: "skia",
+                owner: "borenet",
+                cc: ["rmistry@google.com"],
+                components: ["AutoRoll"],
+            },
+            subject: "",
+            logLevel: NotifierConfig_LogLevel.SILENT,
+        }],
+    };
+}
