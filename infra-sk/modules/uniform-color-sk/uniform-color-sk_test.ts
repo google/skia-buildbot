83b64a27359abfd50c9c7762e1d1f3f6ebaba1bb
Joe Gregorio <jcgregorio@google.com>
2021-02-05 22:40:37 -0500

    [shaders] Add support for alpha channels to uniform-color-sk.
    Also clean up some styles on the remaining uniform elements.
    
    Bug: skia:11272
    Change-Id: I838f279e0261c6ef3ae8b9cb8add7ca42c4414ad

--- infra-sk/modules/uniform-color-sk/uniform-color-sk_test.ts
+++ infra-sk/modules/uniform-color-sk/uniform-color-sk_test.ts
@@ -2,7 +2,6 @@ import './index';
 import { assert } from 'chai';
 import { $$ } from 'common-sk/modules/dom';
 import { UniformColorSk } from './uniform-color-sk';
-
 import { setUpElementUnderTest } from '../test_util';
 
 describe('uniform-color-sk', () => {
@@ -20,17 +19,35 @@ describe('uniform-color-sk', () => {
 
       element.uniform = {
         name: 'color',
-        columns: 1,
-        rows: 3,
+        columns: 3,
+        rows: 1,
         slot: 1,
       };
 
-      // Set the color to white, which gives us all ones as output uniforms.
       $$<HTMLInputElement>('input', element)!.value = '#8090a0';
       element.applyUniformValues(uniforms);
       assert.deepEqual(
         uniforms,
         new Float32Array([0, 128 / 255, 144 / 255, 160 / 255, 0]),
+      );
+    });
+
+    it('puts values in correct spot in uniforms array with an alpha', () => {
+      // Make uniforms longer than needed to show we don't disturb other values.
+      const uniforms = new Float32Array(6);
+
+      element.uniform = {
+        name: 'color',
+        columns: 4, // float4 implies an alpha channel.
+        rows: 1,
+        slot: 1,
+      };
+
+      $$<HTMLInputElement>('input', element)!.value = '#8090a0';
+      element.applyUniformValues(uniforms);
+      assert.deepEqual(
+        uniforms,
+        new Float32Array([0, 128 / 255, 144 / 255, 160 / 255, 0.5, 0]),
       );
     });
 
