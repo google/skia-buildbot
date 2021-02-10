5c399470892fbe16f2766b0ad7a4832da5ebbdaa
Ravi Mistry <rmistry@google.com>
2021-02-10 14:38:57 -0500

    CT windows tracing
    Change-Id: If441019b219438d995560667b09600415a4f34be

--- ct/py/csv_pivot_table_merger.py
+++ ct/py/csv_pivot_table_merger.py
@@ -73,6 +73,7 @@ class CsvMerger(object):
     return avg/len(l)
 
   def _GetTraceURLVal(self, values):
+    # HERE HERE (get rid of carriage returns I think)
     if not values:
       return ''
     # Deduplicate and maintain the order of items in the list.
@@ -88,7 +89,9 @@ class CsvMerger(object):
       fieldname = self._GetFieldNameFromRow(row)
       fieldname_to_values[OUTPUT_PAGE_NAME_KEY] = page_name
 
+      print 'TRACEURL GOT THIS'
       traceURL = row.get(TELEMETRY_TRACE_URLS_KEY)
+      print traceURL
       if traceURL:
         if TELEMETRY_TRACE_URLS_KEY in fieldname_to_values:
           fieldname_to_values[TELEMETRY_TRACE_URLS_KEY].append(traceURL)
