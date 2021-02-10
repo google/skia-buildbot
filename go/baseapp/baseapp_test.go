08b1a59a006df430451ba7c0ed472bfa3a19494e
Joe Gregorio <jcgregorio@google.com>
2021-02-10 16:12:06 -0500

    [shaders] Adding a single shader source image.
    Bug: skia:11272
    Change-Id: I4181c4d4af52875c72e57a964a496c2e2cc0f3c7

--- go/baseapp/baseapp_test.go
+++ go/baseapp/baseapp_test.go
@@ -19,5 +19,5 @@ func TestSecurityMiddleware_LocalNoOptions(t *testing.T) {
 
 func TestSecurityMiddleware_NotLocalAllowWASM(t *testing.T) {
 	unittest.SmallTest(t)
-	require.Equal(t, "base-uri 'none';  img-src 'self' ; object-src 'none' ; style-src 'self'  https://fonts.googleapis.com/ https://www.gstatic.com/ 'unsafe-inline' ; script-src 'strict-dynamic' $NONCE 'unsafe-eval' 'unsafe-inline' https: http: ; report-uri /cspreport ;", cspString([]string{"https://example.org"}, false, []Option{AllowWASM{}}))
+	require.Equal(t, "base-uri 'none';  img-src 'self' ; object-src 'none' ; style-src 'self'  https://fonts.googleapis.com/ https://www.gstatic.com/ 'unsafe-inline' ; script-src 'strict-dynamic' $NONCE 'unsafe-eval' 'unsafe-inline' https: http: ; report-uri /cspreport ;", cspString([]string{"https://example.org"}, false, []Option{AllowWASMOption{}}))
 }
