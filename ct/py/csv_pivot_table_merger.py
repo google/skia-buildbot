99b401e138d30ce9f8243d2fd9e558a5bc8781e5
Ravi Mistry <rmistry@google.com>
2021-02-11 09:42:27 -0500

    CT windows tracing
    Change-Id: If441019b219438d995560667b09600415a4f34be

--- ct/py/csv_pivot_table_merger.py
+++ ct/py/csv_pivot_table_merger.py
@@ -88,8 +88,10 @@ class CsvMerger(object):
       fieldname = self._GetFieldNameFromRow(row)
       fieldname_to_values[OUTPUT_PAGE_NAME_KEY] = page_name
 
-      traceURL = row.get(TELEMETRY_TRACE_URLS_KEY)
+      traceURL = (row.get(TELEMETRY_TRACE_URLS_KEY) or
+                  row.get(TELEMETRY_TRACE_URLS_KEY + r'\r'))
       if traceURL:
+        traceURL = traceURL.rstrip(r'\r')
         if TELEMETRY_TRACE_URLS_KEY in fieldname_to_values:
           fieldname_to_values[TELEMETRY_TRACE_URLS_KEY].append(traceURL)
         else:
