b4883d30d3806e219bc8944673d9b41976159974
Joe Gregorio <jcgregorio@google.com>
2021-02-11 11:43:37 -0500

    [perf] Restore GroupBy functionality.
    It got removed in a recent refactor.
    Also add tests.
    
    Bug: skia:11309
    Change-Id: I30ffbf4ea2d0d97e5174e6676775a0dcbfef353d

--- perf/go/regression/detector_test.go
+++ perf/go/regression/detector_test.go
@@ -7,8 +7,10 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	"go.skia.org/infra/go/paramtools"
 	"go.skia.org/infra/go/testutils/unittest"
 	"go.skia.org/infra/go/vec32"
+	"go.skia.org/infra/perf/go/alerts"
 	"go.skia.org/infra/perf/go/dataframe/mocks"
 	"go.skia.org/infra/perf/go/progress"
 	"go.skia.org/infra/perf/go/types"
@@ -75,14 +77,67 @@ func TestProcessRegressions_BadQueryValue_Fails(t *testing.T) {
 	req := &RegressionDetectionRequest{
 		Query:    "http://[::1]a", // A known query that will fail to parse.
 		Progress: progress.New(),
+		Alert:    alerts.NewConfig(),
 	}
 
 	dfb := &mocks.DataFrameBuilder{}
-	err := ProcessRegressions(context.Background(), req, nil, nil, nil, dfb)
+	err := ProcessRegressions(context.Background(), req, nil, nil, nil, dfb, paramtools.NewReadOnlyParamSet())
 	require.Error(t, err)
 	assert.Equal(t, progress.Running, req.Progress.Status())
 	var b bytes.Buffer
 	err = req.Progress.JSON(&b)
 	require.NoError(t, err)
 	assert.Equal(t, "{\"status\":\"Running\",\"messages\":[{\"key\":\"Stage\",\"value\":\"Loading data to analyze\"}],\"url\":\"\"}\n", b.String())
+}
+
+func TestAllRequestsFromBaseRequest_WithValidGroupBy_Success(t *testing.T) {
+	unittest.SmallTest(t)
+
+	baseRequest := NewRegressionDetectionRequest()
+	alert := alerts.NewConfig()
+	alert.GroupBy = "config"
+	alert.Query = "arch=x86"
+	baseRequest.Alert = alert
+	ps := paramtools.ReadOnlyParamSet{
+		"config": []string{"8888", "565"},
+		"arch":   []string{"x86", "arm"},
+	}
+	allRequests := allRequestsFromBaseRequest(baseRequest, ps)
+	assert.Len(t, allRequests, 2)
+	assert.Contains(t, []string{"arch=x86&config=8888", "arch=x86&config=565"}, allRequests[0].Query)
+}
+
+func TestAllRequestsFromBaseRequest_WithInvalidGroupBy_NoRequestsReturned(t *testing.T) {
+	unittest.SmallTest(t)
+
+	baseRequest := NewRegressionDetectionRequest()
+	alert := alerts.NewConfig()
+	alert.GroupBy = "SomeUnknownKey"
+	alert.Query = "arch=x86"
+	baseRequest.Alert = alert
+	ps := paramtools.ReadOnlyParamSet{
+		"config": []string{"8888", "565"},
+		"arch":   []string{"x86", "arm"},
+	}
+	allRequests := allRequestsFromBaseRequest(baseRequest, ps)
+	assert.Empty(t, allRequests)
+}
+
+func TestAllRequestsFromBaseRequest_WithoutGroupBy_BaseRequestReturnedUnchanged(t *testing.T) {
+	unittest.SmallTest(t)
+
+	baseRequest := NewRegressionDetectionRequest()
+	alert := alerts.NewConfig()
+	alert.GroupBy = ""
+	alert.Query = "arch=x86"
+	baseRequest.Alert = alert
+	ps := paramtools.ReadOnlyParamSet{
+		"config": []string{"8888", "565"},
+		"arch":   []string{"x86", "arm"},
+	}
+	allRequests := allRequestsFromBaseRequest(baseRequest, ps)
+	// With no GroupBy a slice with just the baseRequest is returned.
+	assert.Len(t, allRequests, 1)
+	// Intentionally comparing pointers.
+	assert.Equal(t, baseRequest, allRequests[0])
 }
