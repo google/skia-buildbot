9f3c5e3501646731f9d0c97e8ee7b6ae8d813e91
Jorge Betancourt <jmbetancourt@google.com>
2021-02-11 14:50:14 -0500

    [skottie] add audio layer support to skottie web player
    Change-Id: I18d059cd24c1d6447adbdafbf65a22f59bca1a35

---
+++ skottie/modules/audio.js
@@ -0,0 +1,48 @@
+import {Howl, Howler} from 'howler';
+
+// SoundMaps have string : player pairs
+export function SoundMap() {
+  this.map = new Map();
+  this.setPlayer = function(name, player) {
+    if (typeof name == "string" && player.hasOwnProperty('seek')) {
+      this.map.set(name, player);
+    }
+  };
+  this.getPlayer = function(name) {
+    return this.map.get(name);
+  };
+}
+
+/**
+ * AudioPlayers wrap a howl and control playback through seek calls
+ *
+ * @param source - URL or base64 data URI pointing to audio data
+ * @param format - only needed if extention is not provided by source (inline URI)
+ *
+ */
+export function AudioPlayer(source) {
+  this.playing = false;
+  this.howl = new Howl({
+    src: [source],
+    preload: true
+  });
+  this.seek = function(t) {
+    if (!this.playing && t >=0) {
+      this.howl.play();
+      this.playing = true;
+    }
+
+    if (this.playing) {
+      if (t < 0) {
+        this.howl.pause();
+      } else {
+        let kTolerance = 0.075;
+        let playerPos = this.howl.seek();
+
+        if (Math.abs(playerPos - t) > kTolerance) {
+          this.howl.seek(t);
+        }
+      }
+    }
+  };
+}
