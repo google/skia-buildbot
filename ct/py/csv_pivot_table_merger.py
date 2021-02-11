661b8b48a501d156f8a168679a9de87ab5b5e5f2
Ravi Mistry <rmistry@google.com>
2021-02-11 09:35:08 -0500

    CT windows tracing
    Change-Id: If441019b219438d995560667b09600415a4f34be

--- ct/py/csv_pivot_table_merger.py
+++ ct/py/csv_pivot_table_merger.py
@@ -55,6 +55,7 @@ class CsvMerger(object):
         for row in dict_reader:
           field_name = self._GetFieldNameFromRow(row)
           field_names.add(field_name)
+          # field_names.add(field_name.rstrip('\r') if field_name else field_name)
     # We use 'page_name' in the output CSV to ID the webpage.
     field_names.add(OUTPUT_PAGE_NAME_KEY);
     field_names.add(TELEMETRY_TRACE_URLS_KEY);
@@ -73,6 +74,7 @@ class CsvMerger(object):
     return avg/len(l)
 
   def _GetTraceURLVal(self, values):
+    # HERE HERE (get rid of carriage returns I think)
     if not values:
       return ''
     # Deduplicate and maintain the order of items in the list.
@@ -88,8 +90,9 @@ class CsvMerger(object):
       fieldname = self._GetFieldNameFromRow(row)
       fieldname_to_values[OUTPUT_PAGE_NAME_KEY] = page_name
 
-      traceURL = row.get(TELEMETRY_TRACE_URLS_KEY)
+      traceURL = row.get(TELEMETRY_TRACE_URLS_KEY + '\\r')
       if traceURL:
+        traceURL = traceURL.rstrip('\\r')
         if TELEMETRY_TRACE_URLS_KEY in fieldname_to_values:
           fieldname_to_values[TELEMETRY_TRACE_URLS_KEY].append(traceURL)
         else:
