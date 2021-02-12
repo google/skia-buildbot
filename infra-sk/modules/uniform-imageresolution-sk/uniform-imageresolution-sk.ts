1112e4649325c9a0ef5b2e8385b3004e1ac9d94b
Joe Gregorio <jcgregorio@google.com>
2021-02-11 22:17:16 -0500

    [shaders] Add iImageResolution.
    This uniform reports the dimensions of the input shader images in pixels.
    
    Bug: skia:11272
    Change-Id: Ia32fcea034739bb9977285185dc843a7b5e629d3
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/369581
    Reviewed-by: Joe Gregorio <jcgregorio@google.com>
    Commit-Queue: Joe Gregorio <jcgregorio@google.com>

---
+++ infra-sk/modules/uniform-imageresolution-sk/uniform-imageresolution-sk.ts
@@ -0,0 +1,47 @@
+/**
+ * @module modules/uniform-imageresolution-sk
+ * @description <h2><code>uniform-imageresolution-sk</code></h2>
+ *
+ * Control to handle the input image resolution.
+ *
+ * Note this control doesn't display anything.
+ */
+import { define } from 'elements-sk/define';
+import { Uniform, UniformControl } from '../uniform/uniform';
+
+// All source images are 512 x 512 px.
+export const imageSize = 512;
+
+const defaultUniform: Uniform = {
+  name: 'iImageResolution',
+  rows: 3,
+  columns: 1,
+  slot: 0,
+};
+
+export class UniformImageresolutionSk extends HTMLElement implements UniformControl {
+  private _uniform: Uniform = defaultUniform;
+
+  applyUniformValues(uniforms: number[]): void {
+    uniforms[this._uniform.slot] = imageSize;
+    uniforms[this._uniform.slot + 1] = imageSize;
+  }
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  restoreUniformValues(uniforms: number[]): void {
+    // This is a noop, we don't restore predefined uniform values.
+  }
+
+  get uniform(): Uniform {
+    return this._uniform;
+  }
+
+  set uniform(val: Uniform) {
+    if (val.columns !== 3 || val.rows !== 1) {
+      throw new Error('The imageresolution uniform must be a float3.');
+    }
+    this._uniform = val;
+  }
+}
+
+define('uniform-imageresolution-sk', UniformImageresolutionSk);
