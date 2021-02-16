f154068521b0c6801c4d499725b965baf5fb615b
Joe Gregorio <jcgregorio@google.com>
2021-02-16 00:03:33 -0500

    [auth-proxy] Add support for the X-WEBAUTH-USER header.
    See https://grafana.com/blog/2015/12/07/grafana-authproxy-have-it-your-way/
    for more details.
    
    Change-Id: I0c07f33d9bcbd0a3afaa09a4202503e8bbceaafd

---
+++ kube/go/auth-proxy/main_test.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestProxyServeHTTP_AllowPostNotAuthenticated_WebAuthHeaderValueIsEmptyString(t *testing.T) {
+	*allowPost = true
+
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Note that if the header webAuthHeaderName hadn't been set then the value would be nil.
+		require.Equal(t, []string{""}, r.Header.Values(webAuthHeaderName))
+		require.Equal(t, []string(nil), r.Header.Values("X-SOME-UNSET-HEADER"))
+	}))
+	defer ts.Close()
+	u, err := url.Parse(ts.URL)
+	require.NoError(t, err)
+	proxy := newProxy(u)
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("POST", ts.URL, nil)
+	proxy.ServeHTTP(w, r)
+}
