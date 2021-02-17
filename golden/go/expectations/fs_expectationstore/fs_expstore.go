2e728d487e1f701be76810363944d4e78f9ef366
Kevin Lubick <kjlubick@google.com>
2021-02-17 16:01:46 -0500

    [gold] Fix traces and make PubSub publication non-blocking.
    I think this should speed up CL indexing again and make the
    trace data sensible.
    
    Bug: skia:10582
    Change-Id: I418350032fc84c420496326daedce8234254e655
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/371516
    Reviewed-by: Kevin Lubick <kjlubick@google.com>

--- golden/go/expectations/fs_expectationstore/fs_expstore.go
+++ golden/go/expectations/fs_expectationstore/fs_expstore.go
@@ -11,6 +11,7 @@ import (
 	"time"
 
 	"cloud.google.com/go/firestore"
+	"go.opencensus.io/trace"
 	"golang.org/x/sync/errgroup"
 
 	ifirestore "go.skia.org/infra/go/firestore"
@@ -459,6 +460,8 @@ func (s *Store) updateEntryCacheIfNeeded(ctx context.Context) error {
 // Firestore if not.
 func (s *Store) Get(ctx context.Context) (expectations.ReadOnly, error) {
 	if s.hasSnapshotsRunning {
+		_, span := trace.StartSpan(ctx, "fsexpstore_getFromSnapshots")
+		defer span.End()
 		// If the snapshots are running, we first check to see if we have a fresh Expectations.
 		s.returnCacheMutex.Lock()
 		defer s.returnCacheMutex.Unlock()
@@ -503,7 +506,8 @@ func (s *Store) GetCopy(ctx context.Context) (*expectations.Expectations, error)
 // the assumption that loadExpectations will only be called for setups that do not have the
 // snapshot queries, and the entryCache is used to create the expectationChanges (for undoing).
 func (s *Store) loadExpectations(ctx context.Context) (*expectations.Expectations, error) {
-	defer metrics2.FuncTimer().Stop()
+	ctx, span := trace.StartSpan(ctx, "fsexpstore_loadExpectations")
+	defer span.End()
 	es := make([][]expectationEntry, s.numShards)
 	queries := fs_utils.ShardOnDigest(s.expectationsCollection(), digestField, s.numShards)
 
