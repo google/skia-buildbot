15ee4d92e18ab52cf8a35de01b7fb1318c0f07cc
Leandro Lovisolo <lovisolo@google.com>
2021-02-17 05:01:32 -0500

    Add Infra-PerCommit-BazelBuild tryjob.
    Bug: skia:11111
    Change-Id: I3f57587f117dcacd9f05808f5b59d8f7ac12102e

---
+++ infra/bots/task_drivers/bazel_build/bazel_build.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"flag"
+	"path/filepath"
+
+	"go.skia.org/infra/go/exec"
+	"go.skia.org/infra/task_driver/go/lib/os_steps"
+	"go.skia.org/infra/task_driver/go/td"
+)
+
+var (
+	// Required properties for this task.
+	projectID = flag.String("project_id", "", "ID of the Google Cloud project.")
+	taskID    = flag.String("task_id", "", "ID of this task.")
+	taskName  = flag.String("task_name", "", "Name of the task.")
+	workdir   = flag.String("workdir", ".", "Working directory")
+
+	// Optional flags.
+	local  = flag.Bool("local", false, "True if running locally (as opposed to on the bots)")
+	output = flag.String("o", "", "If provided, dump a JSON blob of step data to the given file. Prints to stdout if '-' is given.")
+)
+
+func main() {
+	// Setup.
+	ctx := td.StartRun(projectID, taskID, taskName, output, local)
+	defer td.EndRun(ctx)
+
+	wd, err := os_steps.Abs(ctx, *workdir)
+	if err != nil {
+		td.Fatal(ctx, err)
+	}
+	repoDir := filepath.Join(wd, "buildbot")
+
+	if _, err := exec.RunCwd(ctx, repoDir, "bazel", "version"); err != nil {
+		// 	td.Fatal(ctx, err)
+	}
+
+	if _, err := exec.RunCwd(ctx, repoDir, "gcloud", "auth", "list"); err != nil {
+		// 	td.Fatal(ctx, err)
+	}
+
+	if _, err := exec.RunCwd(ctx, repoDir, "bazel", "build", "//..."); err != nil {
+		// 	td.Fatal(ctx, err)
+	}
+
+	// if _, err := exec.RunCwd(ctx, repoDir, "bazel", "build", "--config=remote", "//..."); err != nil {
+	// 	// 	td.Fatal(ctx, err)
+	// }
+
+	// if _, err := exec.RunCwd(ctx, repoDir, filepath.Join(wd, "bazel", "bazel", "bin", "bazel"), "version"); err != nil {
+	// 	// 	td.Fatal(ctx, err)
+	// }
+
+	// if _, err := os_steps.Which(ctx, "bazel"); err != nil {
+	// 	td.Fatal(ctx, err)
+	// }
+
+	// if _, err := exec.RunCwd(ctx, repoDir, "bazel", "build", "--config=remote", "//..."); err != nil {
+	// 	td.Fatal(ctx, err)
+	// }
+}
