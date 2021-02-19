3a690569094c38bbd2678478b4ce96820e413e7c
Kevin Lubick <kjlubick@google.com>
2021-02-19 11:29:01 -0500

    Can we remove/replace/simplify assertdeep.Equal
    Change-Id: Ia6fa70bb1b61fdd58c373153aadecb9523419ee0

--- go/deepequal/assertdeep/assertdeep.go
+++ go/deepequal/assertdeep/assertdeep.go
@@ -6,86 +6,16 @@ import (
 	"fmt"
 	"reflect"
 
-	"github.com/davecgh/go-spew/spew"
-	"github.com/pmezard/go-difflib/difflib"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	"go.skia.org/infra/go/deepequal"
+
 	"go.skia.org/infra/go/sklog"
 	"go.skia.org/infra/go/sktest"
 )
 
-// Twiddle this flag for the complete spew.SDump of an object
-// This is useful when pumped into a diff tool, but likely extreme
-// overkill for most uses. The diff that is created by default should
-// be enough for the average test.
-var superVerbose = false
-
 // Equal fails the test if the two objects do not pass reflect.DeepEqual.
 func Equal(t sktest.TestingT, expected, actual interface{}) {
-	if !deepequal.DeepEqual(expected, actual) {
-		// The formatting is inspired by stretchr/testify's require.Equal() output.
-		extra := ""
-		if doDetailedDiff(expected, actual) {
-			e := spewConfig.Sdump(expected)
-			a := spewConfig.Sdump(actual)
-
-			diff, _ := difflib.GetUnifiedDiffString(difflib.UnifiedDiff{
-				A:        difflib.SplitLines(e),
-				B:        difflib.SplitLines(a),
-				FromFile: "Expected",
-				FromDate: "",
-				ToFile:   "Actual",
-				ToDate:   "",
-				Context:  2,
-			})
-
-			extra = "\n\nDiff:\n" + diff
-		}
-
-		if superVerbose {
-			require.FailNow(t, fmt.Sprintf("Objects do not match: \na:\n%s\n\nb:\n%s\n%s", spew.Sdump(expected), spew.Sdump(actual), extra))
-		} else {
-			require.FailNow(t, fmt.Sprintf("Objects do not match: \na:\n%#v\n\nb:\n%#v\n%s", expected, actual, extra))
-		}
-	}
-}
-
-// doDetailedDiff returns true if doing a detailed diff would help. This means if
-// the two objects are the same type and are one of the complicated types:
-// e.g. Map, Slice, Struct, etc.
-func doDetailedDiff(e, a interface{}) bool {
-	if e == nil || a == nil {
-		return false
-	}
-
-	et := reflect.TypeOf(e)
-	ek := et.Kind()
-	if ek == reflect.Ptr {
-		et = et.Elem()
-		ek = et.Kind()
-	}
-	at := reflect.TypeOf(a)
-	ak := at.Kind()
-	if ak == reflect.Ptr {
-		at = at.Elem()
-		ak = at.Kind()
-	}
-
-	if et != at {
-		return false
-	}
-
-	if ek != reflect.Struct && ek != reflect.Map && ek != reflect.Slice && ek != reflect.Array {
-		return false
-	}
-	return true
-}
-
-var spewConfig = spew.ConfigState{
-	Indent:                  "  ",
-	DisablePointerAddresses: true,
-	DisableCapacities:       true,
-	SortKeys:                true,
+	assert.Equal(t, expected, actual)
 }
 
 // Copy is Equal but also checks that none of the direct fields
