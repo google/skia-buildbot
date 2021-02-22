93085455eb1493f7b81f7cfb8795f7c64a6077e6
Leandro Lovisolo <lovisolo@google.com>
2021-02-22 03:06:38 -0500

    [bazel] Add Infra-PerCommit-Test-Bazel{RBE,Local} tryjobs.
    Bug: skia:11111
    Change-Id: I3bf611ac3f3eb2bb935d74ba874582bfede188e7

--- scripts/run_emulators/run_emulators.go
+++ scripts/run_emulators/run_emulators.go
@@ -38,6 +38,9 @@ func main() {
 		time.Sleep(5 * time.Second)
 		fmt.Println("Emulators started. Set environment variables as follows:")
 		for _, e := range emulators.AllEmulators {
+			// We need to set the *_EMULATOR_HOST environment variable before we can read its value via
+			// emulators.GetEmulatorHostEnvVar().p
+			emulators.SetEmulatorHostEnvVar(e)
 			fmt.Println(fmt.Sprintf("export %s=%s", emulators.GetEmulatorHostEnvVarName(e), emulators.GetEmulatorHostEnvVar(e)))
 		}
 	} else {
