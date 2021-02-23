7d73da1de5bb2691e5cdec90424bd06c0d657a05
Kevin Lubick <kjlubick@google.com>
2021-02-23 11:40:12 -0500

    [gold] Delete DiffStore
    PS1 removes NetDiffStore.
    
    Change-Id: Ied6377510a8ab7254444ad6d901b20c2ade79d7f

--- golden/go/validation/validation_test.go
+++ golden/go/validation/validation_test.go
@@ -17,15 +17,3 @@ func TestIsValidDigest(t *testing.T) {
 	assert.False(t, IsValidDigest("766923700b970e4e7ECf9508b8455e0x"))
 	assert.False(t, IsValidDigest("766923700b970e4e7ecf08b8455e0f"))
 }
-
-func TestIsValidDiffImgID(t *testing.T) {
-	unittest.SmallTest(t)
-
-	assert.False(t, IsValidDiffImgID(""))
-	assert.False(t, IsValidDiffImgID("nope"))
-	assert.False(t, IsValidDiffImgID("098f6b-1660f0783f4076284bc18c5f4bdc9608"))
-	assert.False(t, IsValidDiffImgID("098f6bcd4621d373cade4e832627b4f6-1660f0"))
-	assert.False(t, IsValidDiffImgID("HELLOWORLD21d373cade4e832627b4f6-1660f0783f4076284bc18c5f4bdc9608"))
-	assert.False(t, IsValidDiffImgID("098f6bcd4621d373cade4e832627b4f6-HELLOWORLD4076284bc18c5f4bdc9608"))
-	assert.True(t, IsValidDiffImgID("098f6bcd4621d373cade4e832627b4f6-1660f0783f4076284bc18c5f4bdc9608"))
-}
