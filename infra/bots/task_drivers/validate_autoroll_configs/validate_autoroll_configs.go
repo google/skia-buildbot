454d6f1e545f8be8a1c6d6b33df89ee8177b931b
Eric Boren <borenet@google.com>
2021-02-24 11:29:29 -0500

    [autoroll] Switch more rollers to Bot-Commit label
    Change-Id: I2a251154728c5cfe1541f2fd5ec9a2f745a50fad

--- infra/bots/task_drivers/validate_autoroll_configs/validate_autoroll_configs.go
+++ infra/bots/task_drivers/validate_autoroll_configs/validate_autoroll_configs.go
@@ -67,7 +67,7 @@ func validateConfig(ctx context.Context, f string) (string, error) {
 		}
 		if util.In(cfg.ServiceAccount, chromiumServiceAccounts) {
 			gerrit := cfg.GetGerrit()
-			if gerrit != nil && gerrit.Config == config.GerritConfig_CHROMIUM {
+			if gerrit != nil && gerrit.Config == config.GerritConfig_CHROMIUM || gerrit.Config == config.GerritConfig_CHROMIUM_NO_CQ {
 				return skerr.Fmt("Chromium rollers must use Gerrit config CHROMIUM_BOT_COMMIT")
 			}
 		}
