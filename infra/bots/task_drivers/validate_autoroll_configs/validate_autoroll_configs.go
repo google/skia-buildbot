060f464595ec1b5e853c00c66fdb9ff1d25da4ff
Eric Boren <borenet@google.com>
2021-02-25 15:04:35 +0000

    Revert "[autoroll] Switch ANGLE rollers to Bot-Commit label"
    This reverts commit 1550b3c5fc4a3bbfe4dee6b87d2ca31d5796851a.
    
    Reason for revert: service account doesn't have permission for Bot-Commit
    
    Original change's description:
    > [autoroll] Switch ANGLE rollers to Bot-Commit label
    >
    > Bug: skia:11338
    > Change-Id: I9b8fa1ad0234e222e09a0c8a6feae8bb96fa2070
    > Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/375068
    > Reviewed-by: Ravi Mistry <rmistry@google.com>
    > Commit-Queue: Eric Boren <borenet@google.com>
    
    TBR=borenet@google.com,rmistry@google.com
    
    Change-Id: Iac3a071eee3c340b334dfe88742f2c6896612bb0
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: skia:11338

--- infra/bots/task_drivers/validate_autoroll_configs/validate_autoroll_configs.go
+++ infra/bots/task_drivers/validate_autoroll_configs/validate_autoroll_configs.go
@@ -36,9 +36,10 @@ var (
 
 var (
 	// "constants"
-	chromiumGerritHosts = []string{
-		"https://chromium-review.googlesource.com",
-		"https://chrome-internal-review.googlesource.com",
+	chromiumServiceAccounts = []string{
+		"chromium-autoroll@skia-public.iam.gserviceaccount.com",
+		"chromium-release-autoroll@skia-public.iam.gserviceaccount.com",
+		"chromium-internal-autoroll@skia-corp.google.com.iam.gserviceaccount.com",
 	}
 )
 
@@ -64,10 +65,11 @@ func validateConfig(ctx context.Context, f string) (string, error) {
 		if err := cfg.Validate(); err != nil {
 			return skerr.Wrap(err)
 		}
-
-		gerrit := cfg.GetGerrit()
-		if gerrit != nil && util.In(gerrit.Url, chromiumGerritHosts) && (gerrit.Config != config.GerritConfig_CHROMIUM_BOT_COMMIT && gerrit.Config != config.GerritConfig_CHROMIUM_BOT_COMMIT_NO_CQ) {
-			return skerr.Fmt("Chromium rollers must use Gerrit config CHROMIUM_BOT_COMMIT")
+		if util.In(cfg.ServiceAccount, chromiumServiceAccounts) {
+			gerrit := cfg.GetGerrit()
+			if gerrit != nil && gerrit.Config == config.GerritConfig_CHROMIUM || gerrit.Config == config.GerritConfig_CHROMIUM_NO_CQ {
+				return skerr.Fmt("Chromium rollers must use Gerrit config CHROMIUM_BOT_COMMIT")
+			}
 		}
 
 		rollerName = cfg.RollerName
