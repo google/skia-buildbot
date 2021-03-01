5afbf035b58d20786e21f96363feebab08b11f80
Ravi Mistry <rmistry@google.com>
2021-03-01 11:23:11 -0500

    [status] Send tree-status desktop notifications
    * Requests permission to notify when element loads.
    * Notifications only appears if permission is granted and if the window
      is not in focus.
    * The notification contains the tree status message, who set the message,
      and how long ago it was set.
    * Clicking on the notification moves focus to the status window and
      closes the notification.
    
    Bug: skia:
    Change-Id: I2efeb6d1d315bfded322345475a96e10ea17b034

--- status/modules/tree-status-sk/test_data.ts
+++ status/modules/tree-status-sk/test_data.ts
@@ -7,6 +7,20 @@ export const treeStatusResp: TreeStatusResp = {
   general_state: 'open',
 };
 
+export const treeStatusResp2: TreeStatusResp = {
+  message: 'Closed: Broken again!',
+  username: 'superman@google.com',
+  date: '2020-10-08 22:58:02.575754',
+  general_state: 'closed',
+};
+
+export const treeStatusResp3: TreeStatusResp = {
+  message: 'Open: I fixed it (again)!',
+  username: 'batman@google.com',
+  date: '2020-10-08 23:51:02.575754',
+  general_state: 'open',
+};
+
 export const generalRoleResp: RoleResp = { emails: ['alice@google.com'] };
 export const gpuRoleResp: RoleResp = { emails: ['bob@google.com'] };
 export const androidRoleResp: RoleResp = { emails: ['christy@google.com'] };
