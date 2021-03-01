ac711fb0f11d0a31ee70a2418f2e7193707a0f62
Joe Gregorio <jcgregorio@google.com>
2021-03-01 13:33:51 -0500

    [shaders] Wrap comments on @inputs shader.
    Bug: skia:11272
    Change-Id: I0e58f2c7547c57ac8af4c1e7953e734fe0beae07
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/377956
    Reviewed-by: Joe Gregorio <jcgregorio@google.com>

--- shaders/create-named-scraps.sh
+++ shaders/create-named-scraps.sh
@@ -11,7 +11,7 @@
 # that is complete it should be the canconical way to create named scraps.
 
 # Create a name for each scrap.
-curl --silent -X PUT -d "{\"Hash\": \"e281d732554e8d64ac628d300d180efb41af6081772b15cc21623fd47af1ecb2\", \"Description\": \"Shader Inputs\"}" -H 'Content-Type: application/json' http://localhost:9000/_/names/sksl/@inputs
+curl --silent -X PUT -d "{\"Hash\": \"8faf0a93848175d382bc8feaef584339b75a911aa7a9d8a47834d4b05c801a08\", \"Description\": \"Shader Inputs\"}" -H 'Content-Type: application/json' http://localhost:9000/_/names/sksl/@inputs
 curl --silent -X PUT -d "{\"Hash\": \"3124d06f75fa4a5138784e80592909d094a50dfd7a53aed82f660c1c021fa628\", \"Description\": \"Shader Inputs\"}" -H 'Content-Type: application/json' http://localhost:9000/_/names/sksl/@iResolution
 curl --silent -X PUT -d "{\"Hash\": \"12f6c3ecf3f26b1b734d0f254b5bb97cb7d395a9e4829fbab9cda3fef9e3ad9e\", \"Description\": \"Shader Inputs\"}" -H 'Content-Type: application/json' http://localhost:9000/_/names/sksl/@iTime
 curl --silent -X PUT -d "{\"Hash\": \"229312432fbd5c54b471d766a0da29cd7d22f53b7a9a90f3b014a87114d02dd1\", \"Description\": \"Shader Inputs\"}" -H 'Content-Type: application/json' http://localhost:9000/_/names/sksl/@iMouse
