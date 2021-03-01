a801bb1c109fa4b90f6b050e0228ceacbc724719
Ravi Mistry <rmistry@google.com>
2021-03-01 09:50:46 -0500

    [status] Desktop tree-status notifications
    Change-Id: I2efeb6d1d315bfded322345475a96e10ea17b034

--- status/modules/tree-status-sk/tree-status-sk-demo.ts
+++ status/modules/tree-status-sk/tree-status-sk-demo.ts
@@ -6,15 +6,27 @@ import {
   generalRoleResp,
   gpuRoleResp,
   infraRoleResp,
-  treeStatusResp,
+  treeStatusResp, treeStatusResp2, treeStatusResp3,
 } from './test_data';
 
-fetchMock.getOnce('https://tree-status.skia.org/current', treeStatusResp);
-fetchMock.getOnce('https://chrome-ops-rotation-proxy.appspot.com/current/grotation:skia-gardener', generalRoleResp);
-fetchMock.getOnce('https://chrome-ops-rotation-proxy.appspot.com/current/grotation:skia-gpu-gardener', gpuRoleResp);
-fetchMock.getOnce('https://chrome-ops-rotation-proxy.appspot.com/current/grotation:skia-android-gardener', androidRoleResp);
-fetchMock.getOnce('https://chrome-ops-rotation-proxy.appspot.com/current/grotation:skia-infra-gardener', infraRoleResp);
+fetchMock.get('https://tree-status.skia.org/current', () => getTreeStatusResp());
+fetchMock.get('https://chrome-ops-rotation-proxy.appspot.com/current/grotation:skia-gardener', generalRoleResp);
+fetchMock.get('https://chrome-ops-rotation-proxy.appspot.com/current/grotation:skia-gpu-gardener', gpuRoleResp);
+fetchMock.get('https://chrome-ops-rotation-proxy.appspot.com/current/grotation:skia-android-gardener', androidRoleResp);
+fetchMock.get('https://chrome-ops-rotation-proxy.appspot.com/current/grotation:skia-infra-gardener', infraRoleResp);
 Date.now = () => 1600883976659;
+
+let treeStatusCalledNum = 0;
+function getTreeStatusResp(): fetchMock.MockResponse {
+  treeStatusCalledNum++;
+  if (treeStatusCalledNum === 1) {
+    return treeStatusResp;
+  }
+  if (treeStatusCalledNum === 2) {
+    return treeStatusResp2;
+  }
+  return treeStatusResp3;
+}
 
 const el = document.createElement('tree-status-sk');
 ($$('#container') as HTMLElement).appendChild(el);
