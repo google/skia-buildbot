f641e19d66845cf2d03283b2a648cd26680b78b7
Kevin Lubick <kjlubick@google.com>
2021-03-02 16:11:03 -0500

    [gold] WIP to simplify ingestion logic before SQL migration.
    The big change here is to remove gevent and eventbus, a generic
    system that we only needed a specific case for.
    
    Unfortunately, most of this has to happen as one big change.
    
    Smaller, related changes and cleanups:
     - The important part of the PubSub event to ingest the data
       is the "optionsId" attribute - we pay no attention to the body
       (nor to the bucket).
     - Rename bt_reingester to file_reingester and remove Big Table
       related code.
     - BackupPoll configuration moved to the top level ingester config
       rather than being per source.
     - Registering Ingesters removed; All ingesters assumed to have
       at least one source (primary branch), maybe two sources
       (primary and tryjobs).
     - Ingesting will not create PubSub topics/subscriptions if they
       do not exist (see pubsubtool for this).
     - Similar busy pulses metric to diffcalculator added for better
       understanding of utilization.
     - IgnoreResultsFileErr removed (I was not exactly sure how to
       use it correctly). A slightly more clear ErrRetryable takes
       its place.
     - ResultFileLocation replaced with just a string.
     - Ingesters now have a Source, they don't talk via a channel.
     - TryJob ingestion doesn't need anything Firestore related.
     - pubsubtool can now query a Bucket's notifications, which is
       how the PubSub events we listen for in order to ingest are
       produced. (I intend to add the functionality to pubsubtool
       to set these up in a future CL).
     - ingestion.IngestionStore => ingestion.Store
     - When ingesting data to the BT trace store, we use time.Now()
       instead of the updated time on the file.
    
    Recommended Review order:
     - gold_ingestion.go and gold_ingestion_test.go (start with
       ingestFile())
     - ingestion/types.go to see the changes in APIs
     - ingestion/sources.go to see the simpler Source implementation.
     - ingestion_processors/*.go to see that things haven't
       functionally changed too much.
     - Back to gold_ingestion.go to look at startBackupPolling()
       a bit more closely (this now has a test!)
     - Browse everything else (should be mostly deleted stuff).
    
    Bug: skia:10582
    Change-Id: I19745801e044a196400eeabb9c0f581b5414d2d8

--- golden/go/ingestion/mocks/generate.go
+++ golden/go/ingestion/mocks/generate.go
@@ -1,4 +1,6 @@
 package mocks
 
-//go:generate mockery --name IngestionStore --dir ../ --output .
-//go:generate mockery --name ResultFileLocation --dir ../ --output .
+//go:generate mockery --name FileSearcher --dir ../ --output .
+//go:generate mockery --name Processor --dir ../ --output .
+//go:generate mockery --name Source --dir ../ --output .
+//go:generate mockery --name Store --dir ../ --output .
