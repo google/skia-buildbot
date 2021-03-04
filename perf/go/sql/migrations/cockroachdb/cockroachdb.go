9b7fd949d998e7eab9ff14c9e75e079d85281924
Joe Gregorio <jcgregorio@google.com>
2021-03-04 12:03:12 -0500

    Remove last vestiges of ricebox.
    Bug: skia:11388
    Change-Id: I9f8e3997cffaa3afc043decb6f3396a3b0fa4503

--- perf/go/sql/migrations/cockroachdb/cockroachdb.go
+++ perf/go/sql/migrations/cockroachdb/cockroachdb.go
@@ -3,32 +3,13 @@ package cockroachdb
 
 import (
 	"net/http"
-	"os"
-
-	rice "github.com/GeertJohan/go.rice"
-	"go.skia.org/infra/go/repo_root"
-	"go.skia.org/infra/go/skerr"
+	"path/filepath"
+	"runtime"
 )
 
 // New returns an http.FileSystem with all the migrations for a cockroachdb database.
 func New() (http.FileSystem, error) {
-	// For some reason, go.rice's path resolution algorithm does not work under "bazel test ..."
-	// unless we change into the workspace's root directory.
-	workspaceRoot, err := repo_root.Get()
-	if err != nil {
-		return nil, skerr.Wrap(err)
-	}
-	if err := os.Chdir(workspaceRoot); err != nil {
-		return nil, skerr.Wrap(err)
-	}
-
-	conf := rice.Config{
-		LocateOrder: []rice.LocateMethod{rice.LocateFS, rice.LocateEmbedded},
-	}
 	// Directory is infra/perf/migrations.
-	box, err := conf.FindBox("../../../../migrations/cockroachdb")
-	if err != nil {
-		return nil, skerr.Wrap(err)
-	}
-	return box.HTTPBox(), nil
+	_, filename, _, _ := runtime.Caller(1)
+	return http.Dir(filepath.Join(filepath.Dir(filename), "../../../migrations/cockroachdb")), nil
 }
