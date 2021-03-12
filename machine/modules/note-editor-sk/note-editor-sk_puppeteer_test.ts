4d4a9455c8381209e53e1a83704011ef94172290
Joe Gregorio <jcgregorio@google.com>
2021-03-12 13:47:01 -0500

    [machine] Add user editable notes per machine.
    Change-Id: I0b508738f59c8d5b73a701ed53db5ef643b29559

---
+++ machine/modules/note-editor-sk/note-editor-sk_puppeteer_test.ts
@@ -0,0 +1,27 @@
+import * as path from 'path';
+import { expect } from 'chai';
+import {
+  inBazel, loadCachedTestBed, takeScreenshot, TestBed,
+} from '../../../puppeteer-tests/util';
+
+describe('note-editor-sk', () => {
+  let testBed: TestBed;
+  before(async () => {
+    testBed = await loadCachedTestBed(path.join(__dirname, '..', '..', 'webpack.config.ts'));
+  });
+
+  beforeEach(async () => {
+    await testBed.page.goto(inBazel() ? testBed.baseUrl : `${testBed.baseUrl}/dist/note-editor-sk.html`);
+    await testBed.page.setViewport({ width: 640, height: 480 });
+  });
+
+  it('should render the demo page (smoke test)', async () => {
+    expect(await testBed.page.$$('note-editor-sk')).to.have.length(1);
+  });
+
+  describe('screenshots', () => {
+    it('shows the default view', async () => {
+      await takeScreenshot(testBed.page, 'machine', 'note-editor-sk');
+    });
+  });
+});
---
+++ machine/modules/note-editor-sk/note-editor-sk_test.ts
@@ -0,0 +1,31 @@
+import './index';
+import { assert } from 'chai';
+import { $$ } from 'common-sk/modules/dom';
+import { NoteEditorSk } from './note-editor-sk';
+
+import { setUpElementUnderTest } from '../../../infra-sk/modules/test_util';
+import { Annotation } from '../json';
+
+const start: Annotation = {
+  Message: 'This is a test.',
+  User: '',
+  Timestamp: new Date(Date.now()).toString(),
+};
+
+
+describe('note-editor-sk', () => {
+  const newInstance = setUpElementUnderTest<NoteEditorSk>('note-editor-sk');
+
+  let element: NoteEditorSk;
+  beforeEach(() => {
+    element = newInstance();
+  });
+
+  describe('note-editor-sk', () => {
+    it('returns undefined on cancel', async () => {
+      const promise = element.edit(start);
+      $$<HTMLButtonElement>('#cancel', element)!.click();
+      assert.isUndefined(await promise);
+    });
+  });
+});
