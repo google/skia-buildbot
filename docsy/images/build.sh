c095b35c7e75494c5b85f3e3934882a1cd36de92
Joe Gregorio <jcgregorio@google.com>
2021-03-18 23:13:21 -0400

    [docsy] Create a docker image that can run docsy.
    Change-Id: I1e0518a87db303df20fe52bbedcbfc73cc76d31d

---
+++ docsy/images/build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /docsy-example
+/root/go/bin/hugo --config=/docsy-example/content/config.toml --contentDir=/docsy-example/content
---
+++ docsy/images/serve.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd /docsy-example
+/root/go/bin/hugo  serve --config=/docsy-example/content/config.toml --contentDir=/docsy-example/content --bind 0.0.0.0
