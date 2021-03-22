232d385faca1135e45cf2b3999bab30648702714
Leandro Lovisolo <lovisolo@google.com>
2021-03-22 19:26:47 -0400

    [bazel] Gazelle front-end extension: Skeleton code and test harness.
    This Gazelle extension will generate Bazel build targets for the rules proposed in http://go/skia-infra-bazel-frontend.
    
    This CL adds some skeleton code and the test harness. No functional changes are introduced.
    
    Suggested review order:
    - README.md
    - extension.go and test.
    - language.go and test.
    - resolver.go and test.
    - Everything else.
    
    Bug: skia:11766
    Change-Id: I40782d02a0fba3bfcb08547f6d8692c3d485b212

---
+++ bazel/gazelle/frontend/extension.go
@@ -0,0 +1,13 @@
+package frontend
+
+import (
+	gazelle "github.com/bazelbuild/bazel-gazelle/language"
+	"go.skia.org/infra/bazel/gazelle/frontend/language"
+)
+
+// NewLanguage returns an instance of the Gazelle extension for Skia Infrastructure front-end code.
+//
+// This function is called from the Gazelle binary.
+func NewLanguage() gazelle.Language {
+	return &language.Language{}
+}
