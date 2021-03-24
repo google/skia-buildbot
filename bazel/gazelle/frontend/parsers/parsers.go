5ff426523f6a3993709b3fe814793d2baa43784b
Leandro Lovisolo <lovisolo@google.com>
2021-03-24 14:15:48 -0400

    [bazel] Gazelle front-end extension: Parsers for TypeScript and Sass imports.
    This CL adds regexp-based parsers for import statements in TypeScript and Sass sources.
    
    These parsers will be used by the extension to determine the "deps", "ts_deps", and "sass_deps" arguments of the generated build targets.
    
    Bug: skia:11766
    Change-Id: If39c67cd04be0cd7b80646bbd8ab50fd868762aa

---
+++ bazel/gazelle/frontend/parsers/parsers.go
@@ -0,0 +1,229 @@
+// Package parsers defines parsers for the paths of "import" statements found in TypeScript and Sass
+// files.
+//
+// The ad-hoc parsers in this package utilize regular expressions to extract out import paths,
+// filter out comments, etc. While these parsers do not capture every aspect of the TypeScript and
+// Sass grammars, they are sufficient for the purpose of parsing the import statements in the
+// TypeScript and Sass files found in our codebase.
+//
+// The following alternatives were ruled out because of their high implementation and maintenance
+// cost:
+//
+//  - Using third-party parsers written in Go (none exist at this time).
+//  - Generate real parsers using e.g. Goyacc (https://pkg.go.dev/golang.org/x/tools/cmd/goyacc).
+//  - Use the TypeScript compiler API to inspect the AST of a TypeScript file (requires calling
+//    Node.js code from Gazelle).
+package parsers
+
+import (
+	"fmt"
+	"regexp"
+	"sort"
+	"strings"
+)
+
+////////////////////////////////
+// TypeScript imports parser. //
+////////////////////////////////
+
+// tsImportRegexps contains all the regular expressions necessary to extract imports from a
+// TypeScript source file.
+var tsImportRegexps = []*regexp.Regexp{
+	// Matches the following styles of imports:
+	//     import * from 'foo';
+	//     export * from 'foo';
+	//     import * as bar from 'foo';
+	//     import { bar, baz } from 'foo';
+	//     import { bar, baz as qux } from 'foo';
+	regexp.MustCompile(`^\s*(import|export)\s*(\*|[[:alnum:]]|_|\$|,|\{|\}|\s)*\s*from\s*'(?P<path>.*)'`), // Single quotes.
+	regexp.MustCompile(`^\s*(import|export)\s*(\*|[[:alnum:]]|_|\$|,|\{|\}|\s)*\s*from\s*"(?P<path>.*)"`), // Double quotes.
+
+	// Matches multiline imports, e.g.:
+	//     import {
+	//       bar,
+	//       baz as qux,
+	//     } from 'foo';
+	regexp.MustCompile(`^\s*}?\s*from\s*'(?P<path>.*)'`), // Single quotes.
+	regexp.MustCompile(`^\s*}?\s*from\s*"(?P<path>.*)"`), // Double quotes.
+
+	// Matches imports for side-effects only, e.g.:
+	//     import 'foo';
+	regexp.MustCompile(`^\s*import\s*'(?P<path>.*)'`), // Single quotes.
+	regexp.MustCompile(`^\s*import\s*"(?P<path>.*)"`), // Double quotes.
+}
+
+// ParseTSImports takes the contents of a TypeScript source file and extracts the verbatim paths of
+// any imported modules.
+func ParseTSImports(source string) []string {
+	// Remove comments from the source file.
+	lines := splitLinesAndRemoveComments(source)
+
+	// Extract all imports.
+	importsSet := map[string]bool{}
+	for _, line := range lines {
+		for _, re := range tsImportRegexps {
+			match := re.FindStringSubmatch(line)
+			if len(match) != 0 {
+				importPath := match[len(match)-1] // The path is the last capture group on all regexps.
+				importsSet[importPath] = true
+			}
+		}
+	}
+
+	// Sort imports lexicographically.
+	var imports []string
+	for path := range importsSet {
+		imports = append(imports, path)
+	}
+	sort.Strings(imports)
+
+	return imports
+}
+
+//////////////////////////
+// Sass imports parser. //
+//////////////////////////
+
+// sassImportRegexps match the following kinds of Sass imports:
+//
+//     @import 'foo';
+//     @use 'foo';
+//     @use 'foo' as bar;
+//     @use 'foo' with (
+//       $bar: 1px
+//     );
+//     @forward 'foo';
+//     @forward 'foo' as foo-*;
+//     @forward 'foo' hide $bar, $baz;
+//     @forward 'foo' with (
+//       $bar: $1px
+//     );
+//
+// See https://sass-lang.com/documentation/at-rules.
+var sassImportRegexps = []*regexp.Regexp{
+	regexp.MustCompile(`^\s*@(import|use|forward)\s*'(?P<path>[\w~_/\.\-]+)'`), // Single quotes.
+	regexp.MustCompile(`^\s*@(import|use|forward)\s*"(?P<path>[\w~_/\.\-]+)"`), // Double quotes.
+}
+
+// ParseSassImports takes the contents of a Sass source file and extracts the verbatim paths of any
+// imported modules.
+func ParseSassImports(source string) []string {
+	// Remove comments from the source file.
+	lines := splitLinesAndRemoveComments(source)
+
+	// Extract all imports.
+	importsSet := map[string]bool{}
+	for _, line := range lines {
+		for _, re := range sassImportRegexps {
+			match := re.FindStringSubmatch(line)
+			if len(match) != 0 {
+				importPath := match[len(match)-1] // The path is the last capture group on all regexps.
+				// Filter out plain CSS imports. See
+				// https://sass-lang.com/documentation/at-rules/import#plain-css-imports.
+				if strings.HasSuffix(importPath, ".css") {
+					continue
+				}
+				importsSet[importPath] = true
+			}
+		}
+	}
+
+	// Sort imports lexicographically.
+	var imports []string
+	for path := range importsSet {
+		imports = append(imports, path)
+	}
+	sort.Strings(imports)
+
+	return imports
+}
+
+///////////////////////////////////////////
+// Functions for filtering out comments. //
+///////////////////////////////////////////
+
+// splitLinesAndRemoveComments deletes "// line comments" and "/* block comments */" from the given
+// source, and splits the results into lines.
+//
+// This works for both TypeScript and Sass because both languages use the same syntax for comments.
+func splitLinesAndRemoveComments(source string) []string {
+	lines := strings.Split(source, "\n")
+	lines = stripBlockComments(lines)
+	lines = stripCommentedOutLines(lines)
+	return lines
+}
+
+var (
+	// singleLineBlockCommentRegexp matches a single-line /* block comment */, and captures any
+	// uncommented code before and after the block comment.
+	//
+	// Known limitation: This regexp ignores string literals.
+	singleLineBlockCommentRegexp = regexp.MustCompile(`(?P<uncommented_before>.*)/\*.*\*/(?P<uncommented_after>.*)`)
+
+	// blockCommentStartRegexp matches the "/*" at the beginning of a /* block comment */, and
+	// captures any uncommented code that precedes it.
+	//
+	// Known limitation: This regexp ignores the beginning of a block comment if it is preceded by a
+	// single or double quote, as the block comment itself might be part of a string literal.
+	blockCommentStartRegexp = regexp.MustCompile(fmt.Sprintf(`(?P<uncommented>[^'"%s]*)/\*`, "`"))
+
+	// blockCommentEndRegexp matches the "*/" at the end of a /* block comment */, and captures any
+	// uncommented code that succeeds it.
+	blockCommentEndRegexp = regexp.MustCompile(`\*/(?P<uncommented>.*)`)
+)
+
+// stripBlockComments strips /* block comments */ from the given lines of code.
+func stripBlockComments(lines []string) []string {
+	var outputLines []string
+	blockComment := false // Keeps track of whether we're currently inside a /* block comment */.
+
+	for _, line := range lines {
+		if !blockComment {
+			// We are not currently inside a /* block comment */. Does this line have a single-line block
+			// comment?
+			match := singleLineBlockCommentRegexp.FindStringSubmatch(line)
+			if len(match) > 0 {
+				// Remove the single-line block-comment and proceed as if it was never there.
+				line = match[1] + match[2]
+			}
+
+			// Does a multi-line block-comment start on the current line?
+			match = blockCommentStartRegexp.FindStringSubmatch(line)
+			if len(match) > 0 {
+				// Block comment found. Keep the portion of the line that precedes the "/*" characters.
+				blockComment = true
+				outputLines = append(outputLines, match[1])
+			} else {
+				// No block comment found. We can keep the current line as-is.
+				outputLines = append(outputLines, line)
+			}
+		} else {
+			// We are currently inside a /* block comment */. Does it end on the current line?
+			match := blockCommentEndRegexp.FindStringSubmatch(line)
+			if len(match) > 0 {
+				// Found the end of the block comment. Keep the portion of the line that succeeds the "*/"
+				// characters.
+				blockComment = false
+				outputLines = append(outputLines, match[1])
+			} else {
+				// We are still inside a block comment. The entire line can be discarded, so we do nothing.
+			}
+		}
+	}
+
+	return outputLines
+}
+
+// commentedOutLineRegexp matches lines that are commented out via a single-line comment.
+var commentedOutLineRegexp = regexp.MustCompile(`^\s*//`)
+
+// stripCommentedOutLines strips out any lines that begin with a "//" single-line comment.
+func stripCommentedOutLines(lines []string) []string {
+	var outputLines []string
+	for _, line := range lines {
+		if !commentedOutLineRegexp.MatchString(line) {
+			outputLines = append(outputLines, line)
+		}
+	}
+	return outputLines
+}
---
+++ bazel/gazelle/frontend/parsers/parsers_test.go
@@ -0,0 +1,188 @@
+package parsers
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"go.skia.org/infra/go/testutils/unittest"
+)
+
+func TestParseTSImports_Success(t *testing.T) {
+	unittest.SmallTest(t)
+
+	const source = `/* Sample TypeScript file with imports. */
+import 'path/to/a';                                // This comment should be ignored.
+import "path/to/b";                                // This comment should be ignored.
+import * from 'path/to/c';                         // This comment should be ignored.
+import * from "path/to/d";                         // This comment should be ignored.
+export * from 'path/to/e';                         // This comment should be ignored.
+export * from "path/to/f";                         // This comment should be ignored.
+import * as foo from 'path/to/g';                  // This comment should be ignored.
+import * as foo from "path/to/h";                  // This comment should be ignored.
+import { foo_bar, $ } from 'path/to/i';            // This comment should be ignored.
+import { foo_bar, $ } from "path/to/j";            // This comment should be ignored.
+import { foo as bar } from 'path/to/k';            // This comment should be ignored.
+import { foo as bar } from "path/to/l";            // This comment should be ignored.
+import foo, { bar, baz as qux } from 'path/to/m';  // This comment should be ignored.
+import foo, { bar, baz as qux } from "path/to/n";  // This comment should be ignored.
+import {                                           // This comment should be ignored.
+  foo,                                             // This comment should be ignored.
+  bar,                                             // This comment should be ignored.
+} from 'path/to/o';                                // This comment should be ignored.
+import {                                           // This comment should be ignored.
+  foo,                                             // This comment should be ignored.
+  bar,                                             // This comment should be ignored.
+} from "path/to/p";                                // This comment should be ignored.
+
+// Duplicate imports should be ignored.
+import 'path/to/a';
+
+// Line comments should be ignored.
+//
+// import 'line-comment/a';
+// import {
+//   foo,
+//   bar,
+// } from 'line-comment/b';
+
+// Block comments should be ignored.
+/*
+import 'block-comment/a';
+import * from 'block-comment/b';
+*/
+
+// A more complex block comment.
+thisWillBeIgnored(); /*
+import 'block-comment/c';
+import * from 'block-comment/d'; */ import 'path/to/q';  // This import should NOT be ignored.
+
+// A block comment that starts and ends on the same line.
+import /* 'block-comment/e' */ 'path/to/r':
+
+// Tests for various edge cases. Some of these are invalid TS because import is a reserved keyword.
+importfrom('ignored/a');
+import = 'from "ignored/b"';
+import['from "ignored/c"']();
+import.from = 'ignored/d';
+import.from('ignored/e');
+from('ignored/f');
+from = 'ignored/g';
+"import 'ignored/h'";
+`
+
+	expected := []string{
+		"path/to/a",
+		"path/to/b",
+		"path/to/c",
+		"path/to/d",
+		"path/to/e",
+		"path/to/f",
+		"path/to/g",
+		"path/to/h",
+		"path/to/i",
+		"path/to/j",
+		"path/to/k",
+		"path/to/l",
+		"path/to/m",
+		"path/to/n",
+		"path/to/o",
+		"path/to/p",
+		"path/to/q",
+		"path/to/r",
+	}
+
+	require.Equal(t, expected, ParseTSImports(source))
+}
+
+func TestParseSassImports_Success(t *testing.T) {
+	unittest.SmallTest(t)
+
+	const source = `/* Sample Sass file with @import, @use and @forward statements. */
+@import 'path/to/a';  // This comment should be ignored.
+@import "path/to/b";  // This comment should be ignored.
+
+@use 'path/to/c';         // This comment should be ignored.
+@use "path/to/d";         // This comment should be ignored.
+@use 'path/to/e' as foo;  // This comment should be ignored.
+@use "path/to/f" as foo;  // This comment should be ignored.
+@use 'path/to/g' with (   // This comment should be ignored.
+  $foo: 1px,              // This comment should be ignored.
+  $bar: #222              // This comment should be ignored.
+);                        // This comment should be ignored.
+@use "path/to/h" with (   // This comment should be ignored.
+  $foo: 1px,              // This comment should be ignored.
+  $bar: #222              // This comment should be ignored.
+);                        // This comment should be ignored.
+
+@forward 'path/to/i';                  // This comment should be ignored.
+@forward "path/to/j";                  // This comment should be ignored.
+@forward 'path/to/k' as foo-*;         // This comment should be ignored.
+@forward "path/to/l" as foo-*;         // This comment should be ignored.
+@forward 'path/to/m' hide $foo, $bar;  // This comment should be ignored.
+@forward "path/to/n" hide $foo, $bar;  // This comment should be ignored.
+@forward 'path/to/o' with (            // This comment should be ignored.
+  $foo: 1px,                           // This comment should be ignored.
+  $bar: #222                           // This comment should be ignored.
+)                                      // This comment should be ignored.
+@forward "path/to/p" with (            // This comment should be ignored.
+  $foo: 1px,                           // This comment should be ignored.
+  $bar: #222                           // This comment should be ignored.
+)                                      // This comment should be ignored.
+
+// Plain CSS imports should be ignored.
+@import "theme.css";
+@import "http://fonts.googleapis.com/css?family=Droid+Sans";
+@import url(theme);
+@import "landscape.css" screen and (orientation: landscape);
+
+// Duplicate imports should be ignored.
+@import 'path/to/a';
+
+// Line comments should be ignored.
+//
+// @import 'line-comment/a';
+// @use 'line-comment/b' with (
+//   $foo: 1px,
+//   $bar: #222
+// );
+
+// Block comments should be ignored.
+/*
+@import 'block-comment/a';
+@use 'block-comment/b';
+@forward 'block-comment/c';
+*/
+
+// A more complex block comment.
+.this-will-be-ignored {} /*
+@import 'block-comment/d';
+@use 'block-comment/e';
+@forward 'block-comment/f'; */ @import 'path/to/q';  // This import should NOT be ignored.
+
+// A block comment that starts and ends on the same line.
+@import /* 'block-comment/g' */ 'path/to/r':
+`
+
+	expected := []string{
+		"path/to/a",
+		"path/to/b",
+		"path/to/c",
+		"path/to/d",
+		"path/to/e",
+		"path/to/f",
+		"path/to/g",
+		"path/to/h",
+		"path/to/i",
+		"path/to/j",
+		"path/to/k",
+		"path/to/l",
+		"path/to/m",
+		"path/to/n",
+		"path/to/o",
+		"path/to/p",
+		"path/to/q",
+		"path/to/r",
+	}
+
+	require.Equal(t, expected, ParseSassImports(source))
+}
