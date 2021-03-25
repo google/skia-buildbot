efbd56ac32159a18f6fb2b4c21768c419a3bc64e
Joe Gregorio <jcgregorio@google.com>
2021-03-25 16:06:09 -0400

    [docsy] Create a docker image that can run docsy.
    Change-Id: I1e0518a87db303df20fe52bbedcbfc73cc76d31d

---
+++ docsy/go/docset/docset_test.go
@@ -0,0 +1,126 @@
+// docset keeps track of checkouts of a repository of Markdown documents.
+package docset
+
+import (
+	"context"
+	"io/ioutil"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+	"go.skia.org/infra/docsy/go/codereview"
+	crmocks "go.skia.org/infra/docsy/go/codereview/mocks"
+	"go.skia.org/infra/docsy/go/docsy/mocks"
+	gittestutils "go.skia.org/infra/go/git/testutils"
+	"go.skia.org/infra/go/testutils"
+	"go.skia.org/infra/go/testutils/unittest"
+)
+
+const (
+	docPath  = "site/"
+	docsyDir = "/docsy"
+
+	issue       codereview.Issue = "123"
+	patchsetRef                  = "123/17"
+)
+
+var mockTime = time.Unix(0, 12).UTC()
+
+func setupForTest(t *testing.T) (context.Context, string, string, string, *mocks.Docsy, *crmocks.CodeReview, *docSet) {
+	timeNow = func() time.Time {
+		return mockTime
+	}
+	ctx := context.Background()
+	gb := gittestutils.GitInit(t, ctx)
+	gb.Add(ctx, "site/_index.md", "This is an index file.")
+	gb.Commit(ctx)
+
+	workDir, err := ioutil.TempDir("", "docset")
+	require.NoError(t, err)
+
+	d := &mocks.Docsy{}
+	src := filepath.Join(workDir, contentSubDirectory, "-1", docPath)
+	dst := filepath.Join(workDir, destinationSubDirectory, "-1", docPath)
+	d.On("Render", testutils.AnyContext, src, dst).Return(nil)
+	cr := &crmocks.CodeReview{}
+	cr.On("MainIssue").Return(codereview.Issue("-1"))
+	docset, err := New(context.Background(), workDir, docPath, docsyDir, gb.Dir(), cr, d)
+	require.NoError(t, err)
+	return ctx, workDir, src, dst, d, cr, docset
+}
+
+func TestNew_Success(t *testing.T) {
+	unittest.SmallTest(t)
+	_, _, src, _, _, cr, docset := setupForTest(t)
+
+	require.NotNil(t, docset.cache[cr.MainIssue()])
+	require.Equal(t, mockTime, docset.cache[cr.MainIssue()].lastPatchsetCheck)
+	require.FileExists(t, filepath.Join(src, "_index.md"))
+
+}
+
+func TestFileSystem_IssueIsClosed_ReturnsError(t *testing.T) {
+	unittest.SmallTest(t)
+	ctx, _, _, _, _, cr, docset := setupForTest(t)
+
+	cr.On("GetPatchsetInfo", testutils.AnyContext, issue).Return(patchsetRef, true, nil)
+	_, err := docset.FileSystem(ctx, issue)
+	require.Error(t, err)
+}
+
+func TestFileSystem_NoFilesChanged_Success(t *testing.T) {
+	unittest.SmallTest(t)
+	ctx, workDir, _, _, docsy, cr, docset := setupForTest(t)
+
+	cr.On("GetPatchsetInfo", testutils.AnyContext, issue).Return(patchsetRef, false, nil)
+	cr.On("ListModifiedFiles", testutils.AnyContext, issue, patchsetRef).Return(nil, nil)
+	src := filepath.Join(workDir, contentSubDirectory, string(issue), docPath)
+	dst := filepath.Join(workDir, destinationSubDirectory, string(issue), docPath)
+
+	docsy.On("Render", testutils.AnyContext, src, dst).Return(nil)
+	_, err := docset.FileSystem(ctx, issue)
+	require.NoError(t, err)
+}
+
+func TestFileSystem_FileAdded_Success(t *testing.T) {
+	unittest.SmallTest(t)
+	ctx, workDir, _, _, docsy, cr, docset := setupForTest(t)
+
+	cr.On("GetPatchsetInfo", testutils.AnyContext, issue).Return(patchsetRef, false, nil)
+	cr.On("ListModifiedFiles", testutils.AnyContext, issue, patchsetRef).Return([]codereview.ListModifiedFilesResult{
+		{
+			Filename: "site/users.md",
+			Deleted:  false,
+		},
+	}, nil)
+	cr.On("GetFile", testutils.AnyContext, "site/users.md", patchsetRef).Return([]byte("This is file content."), nil)
+	src := filepath.Join(workDir, contentSubDirectory, string(issue), docPath)
+	dst := filepath.Join(workDir, destinationSubDirectory, string(issue), docPath)
+
+	docsy.On("Render", testutils.AnyContext, src, dst).Return(nil)
+	_, err := docset.FileSystem(ctx, issue)
+	require.NoError(t, err)
+	require.FileExists(t, filepath.Join(src, "users.md"))
+	require.FileExists(t, filepath.Join(src, "_index.md"))
+}
+
+func TestFileSystem_FileDeleted_Success(t *testing.T) {
+	unittest.SmallTest(t)
+	ctx, workDir, _, _, docsy, cr, docset := setupForTest(t)
+
+	cr.On("GetPatchsetInfo", testutils.AnyContext, issue).Return(patchsetRef, false, nil)
+	cr.On("ListModifiedFiles", testutils.AnyContext, issue, patchsetRef).Return([]codereview.ListModifiedFilesResult{
+		{
+			Filename: "site/_index.md",
+			Deleted:  true,
+		},
+	}, nil)
+	src := filepath.Join(workDir, contentSubDirectory, string(issue), docPath)
+	dst := filepath.Join(workDir, destinationSubDirectory, string(issue), docPath)
+
+	docsy.On("Render", testutils.AnyContext, src, dst).Return(nil)
+	_, err := docset.FileSystem(ctx, issue)
+	require.NoError(t, err)
+	require.NoFileExists(t, filepath.Join(src, "site", "_index.md"))
+}
