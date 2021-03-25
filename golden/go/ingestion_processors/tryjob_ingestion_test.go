81019f92d6bd973d50bc48802fe4589996fb36df
Kevin Lubick <kjlubick@google.com>
2021-03-25 15:36:36 -0400

    [gold] Add tryjob ingestion test for existing data
    Close inspection of previous tests showed that they could really
    be condensed into one for the SQL code and/or removed (we don't
    really care about Abandoned CLs like we once did).
    
    Bug: skia:10582
    Change-Id: I5274921c463520b71ee25aa97eb749d60fdfdea2

--- golden/go/ingestion_processors/tryjob_ingestion_test.go
+++ golden/go/ingestion_processors/tryjob_ingestion_test.go
@@ -10,6 +10,7 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"go.skia.org/infra/go/httputils"
+	"go.skia.org/infra/go/paramtools"
 	"go.skia.org/infra/go/testutils"
 	"go.skia.org/infra/go/testutils/unittest"
 	"go.skia.org/infra/golden/go/clstore"
@@ -76,7 +77,6 @@ func TestTryjobSQL_Process_FirstFileForCL_Success(t *testing.T) {
 	ctx := context.Background()
 	db := sqltest.NewCockroachDBForTestsWithProductionSchema(ctx, t)
 
-	// This file has data from 3 traces across 2 corpora. The data is for the patchset with order 3.
 	const clID = dks.ChangelistIDThatAttemptsToFixIOS
 	const psID = dks.PatchSetIDFixesIPadButNotIPhone
 	const tjID = dks.Tryjob01IPhoneRGB
@@ -111,6 +111,7 @@ func TestTryjobSQL_Process_FirstFileForCL_Success(t *testing.T) {
 		DisplayName: "Test-iPhone-RGB",
 	}, nil)
 
+	// This file has data from 3 traces across 2 corpora. The data is for the patchset with order 3.
 	src := fakeGCSSourceFromFile(t, "from_goldctl_legacy_fields.json")
 	gtp := initCaches(goldTryjobProcessor{
 		cisClients: map[string]ci.Client{
@@ -287,38 +288,277 @@ func TestTryjobSQL_Process_FirstFileForCL_Success(t *testing.T) {
 	assert.Empty(t, sqltest.GetAllRows(ctx, t, db, "TiledTraceDigests", &schema.TiledTraceDigestRow{}))
 }
 
-// TestTryJobProcessFreshStartGitHub tests the scenario in which we see data uploaded to GitHub for
-// a brand new CL, PS, and TryJob. The PS is derived by id, not by order. The uploaded digest
-// was not previously seen or triaged on master and is not covered by an ignore rule, so the
-// created Patchset object should be marked as having UntriagedDigests.
-func TestTryJobProcessFreshStartGitHub(t *testing.T) {
-	t.Skip("rebuild")
+func TestTryjobSQL_Process_SomeDataExists_Success(t *testing.T) {
+	unittest.LargeTest(t)
+
+	ctx := context.Background()
+	db := sqltest.NewCockroachDBForTestsWithProductionSchema(ctx, t)
+	const qualifiedCL = "gerrit_CL_fix_ios"
+	const qualifiedPS = "gerrit_PS_fixes_ipad_but_not_iphone"
+	const qualifiedTJ = "buildbucket_tryjob_01_iphonergb"
+	const squareTraceKeys = `{"color mode":"RGB","device":"iPhone12,1","name":"square","os":"iOS","source_type":"corners"}`
+	const triangleTraceKeys = `{"color mode":"RGB","device":"iPhone12,1","name":"triangle","os":"iOS","source_type":"corners"}`
+	const circleTraceKeys = `{"color mode":"RGB","device":"iPhone12,1","name":"circle","os":"iOS","source_type":"round"}`
+
+	// Load all the tables we write to with one existing row.
+	existingData := schema.Tables{
+		Changelists: []schema.ChangelistRow{{
+			ChangelistID:     qualifiedCL,
+			System:           dks.GerritCRS,
+			Status:           schema.StatusOpen,
+			OwnerEmail:       dks.UserOne,
+			Subject:          "Fix iOS [sentinel]",
+			LastIngestedData: time.Time{}, // should be updated.
+		}},
+		Patchsets: []schema.PatchsetRow{{
+			PatchsetID:                    qualifiedPS,
+			System:                        dks.GerritCRS,
+			ChangelistID:                  qualifiedCL,
+			Order:                         3,
+			GitHash:                       "ffff111111111111111111111111111111111111",
+			CommentedOnCL:                 true, // sentinel values
+			LastCheckedIfCommentNecessary: time.Date(2021, time.March, 26, 13, 6, 3, 0, time.UTC),
+		}},
+		Tryjobs: []schema.TryjobRow{{
+			TryjobID:         qualifiedTJ,
+			System:           dks.BuildBucketCIS,
+			ChangelistID:     qualifiedCL,
+			PatchsetID:       qualifiedPS,
+			DisplayName:      "Test-iPhone-RGB-sentinel",
+			LastIngestedData: time.Time{}, // should be updated.
+		}},
+		Groupings: []schema.GroupingRow{{
+			GroupingID: h(squareGrouping),
+			Keys: paramtools.Params{
+				types.CorpusField:     dks.CornersCorpus,
+				types.PrimaryKeyField: dks.SquareTest,
+			},
+		}},
+		Options: []schema.OptionsRow{{
+			OptionsID: h(pngOptions),
+			Keys: map[string]string{
+				"ext": "png",
+			},
+		}},
+		Traces: []schema.TraceRow{{
+			TraceID:    h(circleTraceKeys),
+			Corpus:     dks.RoundCorpus,
+			GroupingID: h(circleGrouping),
+			Keys: map[string]string{
+				types.CorpusField:     dks.RoundCorpus,
+				types.PrimaryKeyField: dks.CircleTest,
+				dks.ColorModeKey:      dks.RGBColorMode,
+				dks.OSKey:             dks.IOS,
+				dks.DeviceKey:         dks.IPhoneDevice,
+			},
+			MatchesAnyIgnoreRule: schema.NBFalse, // should not be overwritten
+		}},
+		SecondaryBranchParams: []schema.SecondaryBranchParamRow{{
+			Key:         dks.ColorModeKey,
+			Value:       dks.RGBColorMode,
+			BranchName:  qualifiedCL,
+			VersionName: qualifiedPS,
+		}},
+		SecondaryBranchValues: []schema.SecondaryBranchValueRow{{
+			BranchName: qualifiedCL, VersionName: qualifiedPS,
+			TraceID:      h(squareTraceKeys),
+			Digest:       d(dks.DigestA01Pos),
+			GroupingID:   h(squareGrouping),
+			OptionsID:    h(pngOptions),
+			SourceFileID: h(dks.Tryjob01FileIPhoneRGB),
+			TryjobID:     "Should be overwritten",
+		}},
+		SourceFiles: []schema.SourceFileRow{{
+			SourceFileID: h(dks.Tryjob01FileIPhoneRGB),
+			SourceFile:   dks.Tryjob01FileIPhoneRGB,
+			LastIngested: time.Date(2020, time.March, 1, 1, 1, 1, 0, time.UTC),
+		}},
+	}
+	require.NoError(t, sqltest.BulkInsertDataTables(ctx, db, existingData))
+
+	mcrs := &mock_crs.Client{}
+	mcis := &mock_cis.Client{}
+
+	// This file has data from 3 traces across 2 corpora. The data is for the patchset with order 3.
+	src := fakeGCSSourceFromFile(t, "from_goldctl_recent_fields.json")
+	gtp := initCaches(goldTryjobProcessor{
+		cisClients: map[string]ci.Client{
+			buildbucketCIS: mcis,
+		},
+		reviewSystems: []clstore.ReviewSystem{
+			{
+				ID:     gerritCRS,
+				Client: mcrs,
+				// Store and URLTemplate unused here
+			},
+		},
+		db:     db,
+		source: src,
+	})
+
+	ctx = overwriteNow(ctx, fakeIngestionTime)
+	err := gtp.Process(ctx, dks.Tryjob01FileIPhoneRGB)
+	require.NoError(t, err)
+
+	actualSourceFiles := sqltest.GetAllRows(ctx, t, db, "SourceFiles", &schema.SourceFileRow{}).([]schema.SourceFileRow)
+	assert.Equal(t, []schema.SourceFileRow{{
+		SourceFileID: h(dks.Tryjob01FileIPhoneRGB),
+		SourceFile:   dks.Tryjob01FileIPhoneRGB,
+		LastIngested: fakeIngestionTime,
+	}}, actualSourceFiles)
+
+	actualChangelists := sqltest.GetAllRows(ctx, t, db, "Changelists", &schema.ChangelistRow{}).([]schema.ChangelistRow)
+	assert.Equal(t, []schema.ChangelistRow{{
+		ChangelistID:     qualifiedCL,
+		System:           dks.GerritCRS,
+		Status:           schema.StatusOpen,
+		OwnerEmail:       dks.UserOne,
+		Subject:          "Fix iOS [sentinel]",
+		LastIngestedData: fakeIngestionTime,
+	}}, actualChangelists)
+
+	actualPatchsets := sqltest.GetAllRows(ctx, t, db, "Patchsets", &schema.PatchsetRow{}).([]schema.PatchsetRow)
+	assert.Equal(t, []schema.PatchsetRow{{
+		PatchsetID:                    qualifiedPS,
+		System:                        dks.GerritCRS,
+		ChangelistID:                  qualifiedCL,
+		Order:                         3,
+		GitHash:                       "ffff111111111111111111111111111111111111",
+		CommentedOnCL:                 true,
+		LastCheckedIfCommentNecessary: time.Date(2021, time.March, 26, 13, 6, 3, 0, time.UTC),
+	}}, actualPatchsets)
+
+	actualTryjobs := sqltest.GetAllRows(ctx, t, db, "Tryjobs", &schema.TryjobRow{}).([]schema.TryjobRow)
+	assert.Equal(t, []schema.TryjobRow{{
+		TryjobID:         qualifiedTJ,
+		System:           dks.BuildBucketCIS,
+		ChangelistID:     qualifiedCL,
+		PatchsetID:       qualifiedPS,
+		DisplayName:      "Test-iPhone-RGB-sentinel",
+		LastIngestedData: fakeIngestionTime,
+	}}, actualTryjobs)
+
+	actualGroupings := sqltest.GetAllRows(ctx, t, db, "Groupings", &schema.GroupingRow{}).([]schema.GroupingRow)
+	assert.ElementsMatch(t, []schema.GroupingRow{{
+		GroupingID: h(circleGrouping),
+		Keys: map[string]string{
+			types.CorpusField:     dks.RoundCorpus,
+			types.PrimaryKeyField: dks.CircleTest,
+		},
+	}, {
+		GroupingID: h(squareGrouping),
+		Keys: map[string]string{
+			types.CorpusField:     dks.CornersCorpus,
+			types.PrimaryKeyField: dks.SquareTest,
+		},
+	}, {
+		GroupingID: h(triangleGrouping),
+		Keys: map[string]string{
+			types.CorpusField:     dks.CornersCorpus,
+			types.PrimaryKeyField: dks.TriangleTest,
+		},
+	}}, actualGroupings)
+
+	actualOptions := sqltest.GetAllRows(ctx, t, db, "Options", &schema.OptionsRow{}).([]schema.OptionsRow)
+	assert.ElementsMatch(t, []schema.OptionsRow{{
+		OptionsID: h(pngOptions),
+		Keys: map[string]string{
+			"ext": "png",
+		},
+	}}, actualOptions)
+
+	actualTraces := sqltest.GetAllRows(ctx, t, db, "Traces", &schema.TraceRow{}).([]schema.TraceRow)
+	assert.Equal(t, []schema.TraceRow{{
+		TraceID:    h(circleTraceKeys),
+		Corpus:     dks.RoundCorpus,
+		GroupingID: h(circleGrouping),
+		Keys: map[string]string{
+			types.CorpusField:     dks.RoundCorpus,
+			types.PrimaryKeyField: dks.CircleTest,
+			dks.ColorModeKey:      dks.RGBColorMode,
+			dks.OSKey:             dks.IOS,
+			dks.DeviceKey:         dks.IPhoneDevice,
+		},
+		MatchesAnyIgnoreRule: schema.NBFalse, // existing status not overwritten
+	}, {
+		TraceID:    h(squareTraceKeys),
+		Corpus:     dks.CornersCorpus,
+		GroupingID: h(squareGrouping),
+		Keys: map[string]string{
+			types.CorpusField:     dks.CornersCorpus,
+			types.PrimaryKeyField: dks.SquareTest,
+			dks.ColorModeKey:      dks.RGBColorMode,
+			dks.OSKey:             dks.IOS,
+			dks.DeviceKey:         dks.IPhoneDevice,
+		},
+		MatchesAnyIgnoreRule: schema.NBNull,
+	}, {
+		TraceID:    h(triangleTraceKeys),
+		Corpus:     dks.CornersCorpus,
+		GroupingID: h(triangleGrouping),
+		Keys: map[string]string{
+			types.CorpusField:     dks.CornersCorpus,
+			types.PrimaryKeyField: dks.TriangleTest,
+			dks.ColorModeKey:      dks.RGBColorMode,
+			dks.OSKey:             dks.IOS,
+			dks.DeviceKey:         dks.IPhoneDevice,
+		},
+		MatchesAnyIgnoreRule: schema.NBNull,
+	}}, actualTraces)
+
+	actualParams := sqltest.GetAllRows(ctx, t, db, "SecondaryBranchParams", &schema.SecondaryBranchParamRow{}).([]schema.SecondaryBranchParamRow)
+	assert.Equal(t, []schema.SecondaryBranchParamRow{
+		{Key: dks.ColorModeKey, Value: dks.RGBColorMode, BranchName: qualifiedCL, VersionName: qualifiedPS},
+		{Key: dks.DeviceKey, Value: dks.IPhoneDevice, BranchName: qualifiedCL, VersionName: qualifiedPS},
+		{Key: "ext", Value: "png", BranchName: qualifiedCL, VersionName: qualifiedPS},
+		{Key: types.PrimaryKeyField, Value: dks.CircleTest, BranchName: qualifiedCL, VersionName: qualifiedPS},
+		{Key: types.PrimaryKeyField, Value: dks.SquareTest, BranchName: qualifiedCL, VersionName: qualifiedPS},
+		{Key: types.PrimaryKeyField, Value: dks.TriangleTest, BranchName: qualifiedCL, VersionName: qualifiedPS},
+		{Key: dks.OSKey, Value: dks.IOS, BranchName: qualifiedCL, VersionName: qualifiedPS},
+		{Key: types.CorpusField, Value: dks.CornersCorpus, BranchName: qualifiedCL, VersionName: qualifiedPS},
+		{Key: types.CorpusField, Value: dks.RoundCorpus, BranchName: qualifiedCL, VersionName: qualifiedPS},
+	}, actualParams)
+
+	actualValues := sqltest.GetAllRows(ctx, t, db, "SecondaryBranchValues", &schema.SecondaryBranchValueRow{}).([]schema.SecondaryBranchValueRow)
+	assert.ElementsMatch(t, []schema.SecondaryBranchValueRow{{
+		BranchName: qualifiedCL, VersionName: qualifiedPS,
+		TraceID:      h(squareTraceKeys),
+		Digest:       d(dks.DigestA01Pos),
+		GroupingID:   h(squareGrouping),
+		OptionsID:    h(pngOptions),
+		SourceFileID: h(dks.Tryjob01FileIPhoneRGB),
+		TryjobID:     qualifiedTJ,
+	}, {
+		BranchName: qualifiedCL, VersionName: qualifiedPS,
+		TraceID:      h(triangleTraceKeys),
+		Digest:       d(dks.DigestB01Pos),
+		GroupingID:   h(triangleGrouping),
+		OptionsID:    h(pngOptions),
+		SourceFileID: h(dks.Tryjob01FileIPhoneRGB),
+		TryjobID:     qualifiedTJ,
+	}, {
+		BranchName: qualifiedCL, VersionName: qualifiedPS,
+		TraceID:      h(circleTraceKeys),
+		Digest:       d(dks.DigestC07Unt_CL),
+		GroupingID:   h(circleGrouping),
+		OptionsID:    h(pngOptions),
+		SourceFileID: h(dks.Tryjob01FileIPhoneRGB),
+		TryjobID:     qualifiedTJ,
+	}}, actualValues)
+
+	// We only write to SecondaryBranchExpectations when something is explicitly triaged.
+	assert.Empty(t, sqltest.GetAllRows(ctx, t, db, "SecondaryBranchExpectations", &schema.SecondaryBranchExpectationRow{}))
+
+	// Unlike the primary branch ingestion, these should be empty
+	assert.Empty(t, sqltest.GetAllRows(ctx, t, db, "CommitsWithData", &schema.CommitWithDataRow{}))
+	assert.Empty(t, sqltest.GetAllRows(ctx, t, db, "TraceValues", &schema.TraceValueRow{}))
+	assert.Empty(t, sqltest.GetAllRows(ctx, t, db, "ValuesAtHead", &schema.ValueAtHeadRow{}))
+	assert.Empty(t, sqltest.GetAllRows(ctx, t, db, "Expectations", &schema.ExpectationRow{}))
+	assert.Empty(t, sqltest.GetAllRows(ctx, t, db, "PrimaryBranchParams", &schema.PrimaryBranchParamRow{}))
+	assert.Empty(t, sqltest.GetAllRows(ctx, t, db, "TiledTraceDigests", &schema.TiledTraceDigestRow{}))
 }
 
-// TestProcess_MultipleCIS_CorrectlyLooksUpTryJobs processes three results from different CIS
-// (and has the CIS return an error to short-circuit the process code) and verifies that the
-// two CIS we knew about were correctly contacted and the result with an unknown CIS was ignored.
-func TestProcess_MultipleCIS_CorrectlyLooksUpTryJobs(t *testing.T) {
-	t.Skip("rebuild")
-}
-
-// TestTryJobProcessCLExistsSunnyDay tests that the ingestion works when the CL already exists.
-func TestTryJobProcessCLExistsSunnyDay(t *testing.T) {
-	t.Skip("rebuild")
-}
-
-// TestTryJobProcessCLExistsPreviouslyAbandoned tests that the ingestion works when the
-// CL already exists, but was marked abandoned at some point (and presumably was re-opened).
-func TestTryJobProcessCLExistsPreviouslyAbandoned(t *testing.T) {
-	t.Skip("rebuild")
-}
-
-// TestTryJobProcessPSExistsSunnyDay tests that the ingestion works when the
-// CL and the PS already exists.
-func TestTryJobProcessPSExistsSunnyDay(t *testing.T) {
-	t.Skip("rebuild")
-
-}
+// TODO(kjlubick) tests for error cases with clients
 
 func TestTryjobSQL_Process_CLPSNeedResolution_Success(t *testing.T) {
 	t.Skip("waiting until after refactoring")
