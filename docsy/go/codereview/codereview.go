6c449200c4f8775a2b911a60a4f1329ef8ce4712
Joe Gregorio <jcgregorio@google.com>
2021-03-25 12:33:29 -0400

    [docsy] Create a docker image that can run docsy.
    Change-Id: I1e0518a87db303df20fe52bbedcbfc73cc76d31d

---
+++ docsy/go/codereview/codereview.go
@@ -0,0 +1,34 @@
+package codereview
+
+import "context"
+
+// Issue is the identifier for a CodeReview issue.
+type Issue string
+
+// ListModifiedFilesResult is the results from calling CodeReviewListModifiedFiles.
+type ListModifiedFilesResult struct {
+	// Filename relative to the root of the git repo.
+	Filename string
+
+	// Deleted is true if the file was deleted at the given patchset.
+	Deleted bool
+}
+
+// CodeReview represents an abstraction of the information we want from a code
+// review system such as Gerrit.
+type CodeReview interface {
+	// MainIssue returns an identifier used to signal the use of the main branch
+	// and not an actual Issue.
+	MainIssue() Issue
+
+	// ListModifiedFiles returns a list of the modified files for the given
+	// issue at the given Ref.
+	ListModifiedFiles(ctx context.Context, issue Issue, ref string) ([]ListModifiedFilesResult, error)
+
+	// GetFiles returns the contents of the given file at the given Ref as a byte slice.
+	GetFile(ctx context.Context, filename string, ref string) ([]byte, error)
+
+	// GetPatchsetInfo returns the most recent patchset Ref of the given issue and
+	// also if the issue has been closed.
+	GetPatchsetInfo(ctx context.Context, issue Issue) (string, bool, error)
+}
