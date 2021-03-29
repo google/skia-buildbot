cf04963d1f86d1f6be8975f6e11c423a3fbba9ad
Kevin Lubick <kjlubick@google.com>
2021-03-29 15:38:23 -0400

    [gold] Add way to look up CL and PS ID from Buildbucket.
    Bug: skia:11367
    Change-Id: Ic325d30c351560b6fa97f8babe9a17512b225b46

---
+++ golden/go/ingestion_processors/buildbucketlookup_manual_test.go
@@ -0,0 +1,26 @@
+package ingestion_processors
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"go.skia.org/infra/go/buildbucket"
+	"go.skia.org/infra/go/httputils"
+	"go.skia.org/infra/go/testutils/unittest"
+)
+
+func TestBuildbucketLookup_ValidTJID_Success(t *testing.T) {
+	unittest.ManualTest(t)
+
+	client := httputils.DefaultClientConfig().Client()
+	bbClient := buildbucket.NewClient(client)
+	bc := newBuildbucketLookupClient(bbClient)
+
+	crsName, clID, psOrder, err := bc.Lookup(context.Background(), "8851407306039688080")
+	require.NoError(t, err)
+	require.Equal(t, "gerrit", crsName)
+	require.Equal(t, "389927", clID)
+	require.Equal(t, 1, psOrder)
+}
