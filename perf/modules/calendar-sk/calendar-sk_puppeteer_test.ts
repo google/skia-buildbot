757028ff93bc0656af7fd1f47652b3be8bae870b
Leandro Lovisolo <lovisolo@google.com>
2021-03-30 14:56:14 -0400

    DO NOT SUBMIT - Gazelle: Generate BUILD files for some front-end targets.
    Change-Id: I7e0249aa7a1c919345a36dc55d4b6ddebfec6818

--- perf/modules/calendar-sk/calendar-sk_puppeteer_test.ts
+++ perf/modules/calendar-sk/calendar-sk_puppeteer_test.ts
@@ -1,6 +1,7 @@
 import * as path from 'path';
 import { expect } from 'chai';
 import {
+  inBazel,
   loadCachedTestBed, takeScreenshot, TestBed,
 } from '../../../puppeteer-tests/util';
 
@@ -13,7 +14,8 @@ describe('calendar-sk', () => {
   });
 
   beforeEach(async () => {
-    await testBed.page.goto(`${testBed.baseUrl}/dist/calendar-sk.html`);
+    await testBed.page.goto(
+        inBazel() ? testBed.baseUrl : `${testBed.baseUrl}/dist/calendar-sk.html`);
     await testBed.page.setViewport({ width: 600, height: 1200 });
   });
 
