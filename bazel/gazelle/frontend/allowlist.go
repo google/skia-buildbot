6321e558cba258b51d96ef3d6a9b932935055dc7
Leandro Lovisolo <lovisolo@google.com>
2021-03-30 15:26:57 -0400

    DO NOT SUBMIT - Gazelle: Generate BUILD files for some front-end targets.
    Change-Id: I7e0249aa7a1c919345a36dc55d4b6ddebfec6818

--- bazel/gazelle/frontend/allowlist.go
+++ bazel/gazelle/frontend/allowlist.go
@@ -7,5 +7,19 @@ package frontend
 //
 // TODO(lovisolo): Delete once we are targetting the entire repository.
 var targetDirectories = map[string]bool{
-	// TODO(lovisolo): Populate.
+	"infra-sk/modules":                       false,
+	"infra-sk/modules/ElementSk":             false,
+	"infra-sk/modules/login-sk":              false,
+	"infra-sk/modules/page_object":           false,
+	"infra-sk/modules/paramset-sk":           false,
+	"infra-sk/modules/query-values-sk":       false,
+	"infra-sk/modules/query-sk":              false,
+	"infra-sk/modules/sort-sk":               false,
+	"infra-sk/modules/theme-chooser-sk":      false,
+	"machine/modules":                        true,
+	"machine/pages":                          true,
+	"new_element/modules/example-control-sk": false,
+	"perf/modules":                           true,
+	"perf/pages":                             false,
+	"puppeteer-tests":                        false,
 }
