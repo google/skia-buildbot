3676ec1b6b0d3c4af95cce6b140bd3ba2ec92845
Leandro Lovisolo <lovisolo@google.com>
2021-03-30 15:26:37 -0400

    [bazel] Gazelle front-end extension: Add allowlist.go file with target dirs.
    This makes the "target directories" mechanism more easily discoverable, and subsequent changes to it easier to review.
    
    Bug: skia:11766
    Change-Id: Iaa7be247b618e14f08e3c27e05db98c0125265c8

---
+++ bazel/gazelle/frontend/allowlist.go
@@ -0,0 +1,11 @@
+package frontend
+
+// targetDirectories is the set of directories for which this Gazelle extension will generate or
+// update BUILD files.
+//
+// The value of this map indicates whether to recurse into the directory.
+//
+// TODO(lovisolo): Delete once we are targetting the entire repository.
+var targetDirectories = map[string]bool{
+	// TODO(lovisolo): Populate.
+}
