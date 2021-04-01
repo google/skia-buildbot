5b3dbb2e674a436df7e0e3cb125ddea5d2e646f9
Leandro Lovisolo <lovisolo@google.com>
2021-03-31 20:18:30 -0400

    DO NOT SUBMIT - Gazelle: Generate BUILD files for some front-end targets.
    Change-Id: I7e0249aa7a1c919345a36dc55d4b6ddebfec6818

--- perf/modules/commit-detail-panel-sk/commit-detail-panel-sk_puppeteer_test.ts
+++ perf/modules/commit-detail-panel-sk/commit-detail-panel-sk_puppeteer_test.ts
@@ -1,6 +1,7 @@
 import * as path from 'path';
 import { expect } from 'chai';
 import {
+  inBazel,
   loadCachedTestBed, takeScreenshot, TestBed,
 } from '../../../puppeteer-tests/util';
 
@@ -14,7 +15,7 @@ describe('commit-detail-panel-sk', () => {
 
   beforeEach(async () => {
     await testBed.page.goto(
-      `${testBed.baseUrl}/dist/commit-detail-panel-sk.html`,
+        inBazel() ? testBed.baseUrl : `${testBed.baseUrl}/dist/commit-detail-panel-sk.html`,
     );
     await testBed.page.setViewport({ width: 400, height: 550 });
   });
