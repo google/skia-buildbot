c9cc70c909920b5deaf4185f27c40cd8af5d428e
Kevin Lubick <kjlubick@google.com>
2021-04-06 10:56:25 -0400

    [gold] Add stub implementation of listening to repos
    Pub/Sub publish to topic set up with
    ```
    gcloud source repos update skia-infra --add-topic gold-skia-infra-repo-updates --message-format=json
    ```
    
    Latency note based on real-world data https://skia.googlesource.com/buildbot/+/6e3be80cf57f687bbadbd050c3a8aa3e958fe84c
    landed at 15:06:05
    event timestamp 15:07:10
    event processed 15:07:13
    
    JSON in message:
    {
      "name": "projects/skia-public/repos/skia-infra",
      "url": "https://source.developers.google.com/p/skia-public/r/skia-infra",
      "eventTime": "2021-04-06T15:07:10.751775Z",
      "refUpdateEvent": {
        "email": "repo-sync@skia-public.iam.gserviceaccount.com",
        "refUpdates": {
          "refs/heads/master": {
            "refName": "refs/heads/master",
            "updateType": "UPDATE_FAST_FORWARD",
            "oldId": "eaf7deacbb41a59ffd1285195c8df0a2ddeb27fd",
            "newId": "6e3be80cf57f687bbadbd050c3a8aa3e958fe84c"
          }
        }
      }
    }
    
    Bug: skia:10582
    Change-Id: I7c311bb3056ce6821ff192cee65b841539a0ed95

--- golden/cmd/periodictasks/periodictasks.go
+++ golden/cmd/periodictasks/periodictasks.go
@@ -5,6 +5,7 @@ import (
 	"flag"
 	"net/http"
 
+	"cloud.google.com/go/pubsub"
 	"github.com/jackc/pgx/v4/pgxpool"
 	"go.opencensus.io/trace"
 
@@ -33,6 +34,12 @@ type periodicTasksConfig struct {
 
 	// NullIgnorePeriod is how often we should try to apply the ignore rules to null traces.
 	NullIgnorePeriod config.Duration `json:"null_ignore_period"`
+	// RepoUpdateSubscription is the subscription to use to listen to the RepoUpdateTopic.
+	RepoUpdateSubscription string `json:"repo_update_subscription" optional:"true"`
+	// RepoUpdateTopic is the Pub/Sub topic to which repo updates will be pushed. We listen to this
+	// so we can determine when a CL lands. This topic should be setup via
+	// https://cloud.google.com/source-repositories/docs/configuring-notifications
+	RepoUpdateTopic string `json:"repo_update_topic" optional:"true"`
 }
 
 func main() {
@@ -76,6 +83,8 @@ func main() {
 
 	startUpdateTracesWithNullStatus(ctx, db, ptc)
 
+	startListenForLandedChangelists(ctx, db, ptc)
+
 	sklog.Infof("periodic tasks have been started")
 	http.HandleFunc("/healthz", httputils.ReadyHandleFunc)
 	sklog.Fatal(http.ListenAndServe(ptc.ReadyPort, nil))
@@ -114,4 +123,51 @@ func mustInitSQLDatabase(ctx context.Context, ptc periodicTasksConfig) *pgxpool.
 	}
 	sklog.Infof("Connected to SQL database %s", ptc.SQLDatabaseName)
 	return db
+}
+
+func startListenForLandedChangelists(ctx context.Context, _ *pgxpool.Pool, ptc periodicTasksConfig) {
+	if ptc.RepoUpdateTopic == "" || ptc.RepoUpdateSubscription == "" {
+		sklog.Info("Skipping listening for repo updates")
+		return
+	}
+	liveness := metrics2.NewLiveness("periodic_tasks", map[string]string{
+		"task": "commitLanded",
+	})
+
+	psc, err := pubsub.NewClient(ctx, ptc.PubsubProjectID)
+	if err != nil {
+		sklog.Fatalf("Error initializing pubsub client for project %s: %s", ptc.PubsubProjectID, err)
+	}
+
+	// Check that the topic exists. Fail if it does not.
+	t := psc.Topic(ptc.RepoUpdateTopic)
+	if exists, err := t.Exists(ctx); err != nil {
+		sklog.Fatalf("Error checking for existing topic %s: %s", ptc.RepoUpdateTopic, err)
+	} else if !exists {
+		sklog.Fatalf("Repo update topic %s does not exist in project %s", ptc.RepoUpdateTopic, ptc.PubsubProjectID)
+	}
+
+	// Check that the subscription exists. Fail if it does not.
+	sub := psc.Subscription(ptc.RepoUpdateSubscription)
+	if exists, err := sub.Exists(ctx); err != nil {
+		sklog.Fatalf("Error checking for existing subscription %s", ptc.RepoUpdateSubscription)
+	} else if !exists {
+		sklog.Fatalf("subscription %s does not exist in project %s", ptc.RepoUpdateSubscription, ptc.PubsubProjectID)
+	}
+
+	// This process will handle one message at a time. This allows us to more finely control the
+	// scaling up as necessary.
+	sub.ReceiveSettings.NumGoroutines = 1
+	go func() {
+		err := sub.Receive(ctx, func(ctx context.Context, message *pubsub.Message) {
+			ctx, span := trace.StartSpan(ctx, "periodic_RepoUpdate")
+			defer span.End()
+			liveness.Reset()
+			sklog.Infof("Repo Update: %v", message.Attributes)
+			sklog.Infof("JSON? %s", string(message.Data))
+			message.Ack()
+		})
+		sklog.Errorf("Pub/Sub receiver died: %s", err)
+	}()
+	sklog.Infof("Listening for repo updates on %s-%s", ptc.RepoUpdateTopic, ptc.RepoUpdateSubscription)
 }
