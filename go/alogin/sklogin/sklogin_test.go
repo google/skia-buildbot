71ff9028699e4837bf293080ebcbd3cf5a08584b
Joe Gregorio <jcgregorio@google.com>
2021-04-06 13:50:17 -0400

    [perf] Start decoupling auth from perf.
    Bug: skia:11827
    Change-Id: I885448af3822b261afe2305cf0c7963fa452a308

---
+++ go/alogin/sklogin/sklogin_test.go
@@ -0,0 +1,34 @@
+// Package sklogin implmements //go/alogin.Login using the //go/login package.
+package sklogin
+
+import (
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gorilla/mux"
+	"github.com/stretchr/testify/require"
+	"go.skia.org/infra/go/alogin"
+	"go.skia.org/infra/go/testutils/unittest"
+)
+
+func TestRegisterHandlers_HandlersAreRegistered(t *testing.T) {
+	unittest.SmallTest(t)
+
+	router := mux.NewRouter()
+	(&sklogin{}).RegisterHandlers(router)
+	var out mux.RouteMatch
+	r := httptest.NewRequest("GET", "/logout/", nil)
+	require.True(t, router.Match(r, &out))
+}
+
+func TestStatus_CookiesAreNotPresent_EMailIsNotReturnedInStatus(t *testing.T) {
+	unittest.SmallTest(t)
+
+	r := httptest.NewRequest("GET", "/", nil)
+	expected := alogin.Status{
+		EMail:     alogin.EMail(""),
+		LoginURL:  loginPath,
+		LogoutURL: logoutPath,
+	}
+	require.Equal(t, expected, (&sklogin{}).Status(r))
+}
