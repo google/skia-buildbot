b4988ee37dae0fffc616a1550e927f0c2a55ee7a
Joe Gregorio <jcgregorio@google.com>
2021-04-08 15:29:06 -0400

    [perf] Push GCS file access into an interface.
    Bug: skia: 11827
    Change-Id: I1c2fd512300e499f0dfef5fb1303462a66e07301

---
+++ perf/go/filestore/gcs/gcs_test.go
@@ -0,0 +1,32 @@
+// Package gcs implements http.FileSystem using Google Cloud Storage.
+package gcs
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"go.skia.org/infra/go/testutils/unittest"
+)
+
+func Test_filesystem_parseNameIntoBucketAndPath_Success(t *testing.T) {
+	unittest.SmallTest(t)
+
+	bucket, path, err := (&filesystem{}).parseNameIntoBucketAndPath("gs://bucket/this/is/the/path.txt")
+	require.NoError(t, err)
+	require.Equal(t, "bucket", bucket)
+	require.Equal(t, "/this/is/the/path.txt", path)
+}
+
+func Test_filesystem_parseNameIntoBucketAndPathWithEmptyURL_ReturnsError(t *testing.T) {
+	unittest.SmallTest(t)
+
+	_, _, err := (&filesystem{}).parseNameIntoBucketAndPath("")
+	require.Error(t, err)
+}
+
+func Test_filesystem_parseNameIntoBucketAndPathWithInvalidURL_ReturnsError(t *testing.T) {
+	unittest.SmallTest(t)
+
+	_, _, err := (&filesystem{}).parseNameIntoBucketAndPath("ht tp://foo.com")
+	require.Error(t, err)
+}
