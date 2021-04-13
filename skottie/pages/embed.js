021428a4f18e3404a90683c9bcbb361693d2f867
Leandro Lovisolo <lovisolo@google.com>
2021-04-13 18:48:09 -0400

    Migrate repository to a single package.json file.
    File //package.json now contains the union of all other package.json files, with most NPM packages updated to their latest versions.
    
    Files //html-template-minifier/package.json and //jsdoc/package.json remain unchanged. All other package.json files are deleted.
    
    Minor changes to some JS/TS files were necessary after updating the NPM packages.
    
    I added a //make/npm.mk file that defines an npm-ci make target that can be added as a prerequisite to any targets that depend on the node_modules directory.
    
    Added some apps to CI (see changes in //Makefile).
    
    Nearly all changes to BUILD.bazel file are automatic (Gazelle).
    
    Steps I took to generate //package.json:
    
        # Compute the list of target package.json files.
        $ allpackagejson=`find | grep "package\.json$" | grep -v html-template-minifier | grep -v jsdoc | grep -v node_modules | grep -v bower_components | grep -v .git | sort`
    
        # Collect all NPM packages packages from all package.json files.
        $ allpkgs=`jq -r '(.dependencies // {}) + (.devDependencies // {}) | keys | .[]' $allpackagejson | sort | uniq`
    
        # Uninstall all NPM packages from //package.json.
        $ npm uninstall `jq -r '(.dependencies // {}) + (.devDependencies // {}) | keys | .[]' package.json`
    
        # Delete all other package.json and package-lock.json files.
        $ git rm `find | grep "package\.json$" | grep -v ^./package.json$ | grep -v html-template-minifier | grep -v jsdoc | grep -v node_modules | grep -v bower_components | grep -v .git`
        $ git rm `find | grep package-lock.json | grep -v ^./package-lock.json$ | grep -v html-template-minifier | grep -v jsdoc | grep -v node_modules | grep -v bower_components | grep -v .git`
    
        # Install all collected packages at their most recent version.
        $ npm install --save-dev $allpkgs
    
        # Reinstall some packages at specific versions.
        $ npm install --save-dev webpack@^4.0.0 @types/webpack@^4.0.0
        $ npm install --save-dev webpack-dev-middleware@^3.0.0 @types/webpack-dev-middleware@^3.0.0
        $ npm install --save-dev webpack-dev-server@^3.0.0 @types/webpack-dev-server@^3.0.0
        $ npm install --save-dev webpack-cli@^3.0.0
        $ npm install --save-dev mini-css-extract-plugin@^0.9.0 @types/mini-css-extract-plugin@^0.9.0
        $ npm install --save-dev html-webpack-plugin@^4.0.0
        $ npm install --save-dev copy-webpack-plugin@^5.0.0 @types/copy-webpack-plugin@^5.0.0
        $ npm install --save-dev node-sass@^4.0.0 sass-loader@^7.0.0
        $ npm install --save-dev postcss-loader@^2.0.0
        $ npm install --save-dev autoprefixer@^9.0.0
        $ npm install --save-dev html-loader@^0.5.5
        $ npm install --save-dev cssnano@^3.0.0
        $ npm install --save-dev karma@^4.0.0 @types/karma@^4.0.0
        $ npm install --save-dev karma-webpack@^4.0.0
        $ npm install --save-dev sinon@^7.0.0 @types/sinon@^7.0.0
        $ npm install --save-dev d3-selection@^1.0.0
        $ npm install --save-dev @bazel/rollup@~2.0.0 @bazel/terser@~2.0.0 @bazel/typescript@~2.0.0
        $ npm install --save-dev @rollup/plugin-commonjs@~15.0.0 @rollup/plugin-node-resolve@~9.0.0
        $ npm install --save-dev terser@^5.0.0
    
        # Uninstall unneeded packages.
        $ npm uninstall @bundled-es-modules/fetch-mock @types/fetch-mock
        $ npm uninstall terser-webpack-plugin
    
    Bug: skia:10614
    Change-Id: I342f0c950af313752025087e70fd2b2d6fa6a709

--- skottie/pages/embed.js
+++ skottie/pages/embed.js
@@ -1,3 +1,3 @@
-import '../node_modules/@webcomponents/custom-elements/custom-elements.min.js'
+import '../../node_modules/@webcomponents/custom-elements/custom-elements.min.js'
 import './embed.scss'
 import '../modules/skottie-embed-sk'
