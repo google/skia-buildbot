f73a853ec1c9714a19a1e8183d403ad898eb3478
Joe Gregorio <jcgregorio@google.com>
2021-04-15 14:51:17 -0400

    [perf] Fix proxyauth for Perf.
    Bug: skia:11827
    Change-Id: I6554e17da558a6b3fbc279278862700ffd79163f

--- go/alogin/login.go
+++ go/alogin/login.go
@@ -14,6 +14,10 @@ import (
 // Email is an email address.
 type EMail string
 
+func (e EMail) String() string {
+	return string(e)
+}
+
 // NotLoggedIn is the EMail value used to indicate a user is not logged in.
 const NotLoggedIn EMail = ""
 
