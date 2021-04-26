73aebb82e096be98a2ec03766b1505ba57fff3a7
Leandro Lovisolo <lovisolo@google.com>
2021-04-26 15:29:12 -0400

    [gold] Port edit-ignore-rule-sk to TypeScript.
    Bug: skia:10246
    Change-Id: Id58ce9c01c2bc75bda9757d1e042ff13c8112fff

---
+++ golden/modules/edit-ignore-rule-sk/edit-ignore-rule-sk-demo.ts
@@ -0,0 +1,40 @@
+import './index';
+import { $$ } from 'common-sk/modules/dom';
+import { manyParams } from '../shared_demo_data';
+import { EditIgnoreRuleSk } from './edit-ignore-rule-sk';
+import { EditIgnoreRuleSkPO } from './edit-ignore-rule-sk_po';
+
+Date.now = () => Date.parse('2020-02-01T00:00:00Z');
+
+function newEditIgnoreRule(
+    parentSelector: string,
+    query = '',
+    expires = '',
+    note = ''): {el: EditIgnoreRuleSk, po: EditIgnoreRuleSkPO} {
+  const el = new EditIgnoreRuleSk();
+  el.paramset = manyParams;
+  el.query = query;
+  el.expires = expires;
+  el.note = note;
+  $$(parentSelector)!.appendChild(el);
+  return {el: el, po: new EditIgnoreRuleSkPO(el)};
+}
+
+async function populate() {
+   newEditIgnoreRule('#empty');
+
+  const {po: filledPO} = newEditIgnoreRule('#filled',
+    'alpha_type=Opaque&compiler=GCC&cpu_or_gpu_value=Adreno540&cpu_or_gpu_value=GTX660&'
+    + 'name=01_original.jpg_0.333&source_options=codec_animated_kNonNative_unpremul',
+    '2020-02-03T00:00:00Z', 'this is my note');
+  await (await filledPO.getQuerySkPO()).clickKey('alpha_type');
+
+  const {el: missing} = newEditIgnoreRule('#missing');
+  missing.verifyFields();
+
+  const {po: partialCustomValuesPO} = newEditIgnoreRule('#partial_custom_values');
+  await partialCustomValuesPO.setCustomKey('oops');
+  await partialCustomValuesPO.clickAddCustomParamBtn();
+}
+
+populate();
