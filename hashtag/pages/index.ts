581670b5be3b7400ca2246636a1733f74875fa33
Joe Gregorio <jcgregorio@google.com>
2021-04-29 15:50:07 -0400

    [hashtag] Convert to typescript.
    Bug: skia:11931
    Change-Id: Ia50934bf34fb17e19ace42edce5a1764977a0380
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/402637
    Auto-Submit: Joe Gregorio <jcgregorio@google.com>
    Reviewed-by: Ravi Mistry <rmistry@google.com>
    Commit-Queue: Ravi Mistry <rmistry@google.com>

--- hashtag/pages/index.ts
+++ hashtag/pages/index.ts
@@ -1,4 +1,3 @@
-
 import 'elements-sk/error-toast-sk';
 import { $$, findParent } from 'common-sk/modules/dom';
 import { errorMessage } from 'elements-sk/errorMessage';
@@ -11,21 +10,23 @@ import './index.scss';
 
 // Handle the form action for toggling the 'hidden' state of any Artifact.
 document.addEventListener('submit', (e) => {
-  if (e.target.target !== '__toggle_hidden') {
+  const form = e.target as HTMLFormElement;
+  if (form.getAttribute('target') !== '__toggle_hidden') {
     return;
   }
   e.preventDefault();
-  fetch(e.target.action, {
+
+  fetch(form.action, {
     method: 'POST',
-    body: new FormData(e.target),
+    body: new FormData(form),
   }).then(jsonOrThrow).then(() => {
-    findParent(e.target, 'LI').classList.toggle('hidden');
+    findParent(form, 'LI')!.classList.toggle('hidden');
   }).catch(errorMessage);
 });
 
 // When someone clicks the 'Edit' checkbox we display the edit buttons next to
 // each Artifact that allows the hidden state to be toggled. The displaying is
 // done via CSS, here we just toggle a CSS class on the 'main' element.
-$$('#edit-toggle').addEventListener('change', (e) => {
-  $$('main').classList.toggle('edit-mode', e.target.checked);
+$$<HTMLInputElement>('#edit-toggle')!.addEventListener('change', (e) => {
+  $$<HTMLDivElement>('main')!.classList.toggle('edit-mode', (e.target as HTMLInputElement).checked);
 });
