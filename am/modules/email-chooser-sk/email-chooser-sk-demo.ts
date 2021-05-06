a49a78aaf858f47d639472171ef5fe78406b022a
Ravi Mistry <rmistry@google.com>
2021-05-06 10:01:08 -0400

    Multi-select owners
    Change-Id: I2b8c04e4769973aaf662561abb8c24fbdb718a96

--- am/modules/email-chooser-sk/email-chooser-sk-demo.ts
+++ am/modules/email-chooser-sk/email-chooser-sk-demo.ts
@@ -2,7 +2,11 @@ import './index';
 import { EmailChooserSk } from './email-chooser-sk';
 
 window.addEventListener('load', () => {
-  (document.getElementById('email-chooser') as EmailChooserSk).open(
+  (document.getElementById('email-chooser-no-owner') as EmailChooserSk).open(
+    ['alice@example.com', 'bob@example.com', 'claire@example.com'],
+    '',
+  );
+  (document.getElementById('email-chooser-with-owner') as EmailChooserSk).open(
     ['alice@example.com', 'bob@example.com', 'claire@example.com'],
     'bob@example.com',
   );
