19609daa69b9b72bbed7874e01cc587773e5d973
Leandro Lovisolo <lovisolo@google.com>
2021-05-10 23:44:39 -0400

    [page object] Add @BySelector and @BySelectorAll decorators for shorter and cleaner POs.
    This CL aims to make page objects easier to read and easier to write.
    
    Currently, most page objects utilize PageObject methods such as selectOnePOE, selectAllPOE, selectAllPOEThenFind, selectAllPOEThenMap, etc., which are rather opaque and result in verbose code.
    
    The new decorators, plus the functions in async.ts, will replace most calls to said methods. For an example, please see query-values-sk_po.ts.
    
    In follow-up CLs, I'll simplify many existing CLs to use the new decorators.
    
    This CL is inspired by the use of decorators in the PageLoader library: https://github.com/google/pageloader/blob/master/quickstart_guide.md.
    
    Recommended review order:
    - query-values-sk_po.ts
    - page_object.ts
    - async.ts
    - everything else
    
    Change-Id: I668cbfdea40dd183e7c22efe380a63d393fca952

--- infra-sk/modules/page_object/page_object.ts
+++ infra-sk/modules/page_object/page_object.ts
@@ -25,6 +25,124 @@ import { PageObjectElement } from './page_object_element';
  * [3] https://github.com/google/pageloader/blob/master/best_practices.md
  */
 export abstract class PageObject {
+  /**
+   * A property decorator that returns the first child element that matches the selector.
+   *
+   * @example
+   *     class MyElementPO extends PageObject {
+   *       @BySelector('button.submit')
+   *       submitBtn!: Promise<PageObjectElement>;
+   *
+   *       async clickSubmitBtn() {
+   *         await (await this.submitBtn).click();
+   *       }
+   *
+   *       ...
+   *     }
+   */
+  public static BySelector(selector: string): any {
+    const decorator: PropertyDecorator = (target: Object, propertyKey: string | symbol) => {
+      const propertyDescriptor: PropertyDescriptor = {
+        get(): any {
+          const po = this as PageObject;
+          return po.selectOnePOE(selector);
+        },
+      };
+      Object.defineProperty(target, propertyKey, propertyDescriptor);
+    }
+    return decorator;
+  }
+
+  /**
+   * A property decorator that returns all the child elements that match the selector.
+   *
+   * @example
+   *     class MyElementPO extends PageObject {
+   *       @BySelectorAll('table.foo tr')
+   *       rows!: Promise<PageObjectElement[]>;
+   *
+   *       async getNumRows() {
+   *         return (await this.rows).length;
+   *       }
+   *
+   *       ...
+   *     }
+   */
+  public static BySelectorAll(selector: string): any {
+    const decorator: PropertyDecorator = (target: Object, propertyKey: string | symbol) => {
+      const propertyDescriptor: PropertyDescriptor = {
+        get(): any {
+          const po = this as PageObject;
+          return po.selectAllPOE(selector);
+        },
+      };
+      Object.defineProperty(target, propertyKey, propertyDescriptor);
+    }
+    return decorator;
+  }
+
+  /**
+   * A property decorator that instantiates a nested PageObject for the first child element that
+   * matches the selector.
+   *
+   * @example
+   *     import { AnotherElementPO } from 'path/to/another_po';
+   *
+   *     class MyElementPO extends PageObject {
+   *       @POBySelector('another-element', AnotherElementPO)
+   *       anotherElementPO!: Promise<AnotherElementPO>;
+   *
+   *       async doSomething() {
+   *         await (await this.anotherElementPO).clickSomeBtn();
+   *       }
+   *
+   *       ...
+   *     }
+   */
+  public static POBySelector<T extends PageObject>(
+      selector: string, ctor: { new(...args: any[]): T }): any {
+    const decorator: PropertyDecorator = (target: Object, propertyKey: string | symbol) => {
+      const propertyDescriptor: PropertyDescriptor = {
+        async get(): Promise<any> {
+          const po = this as PageObject;
+          return new ctor(await po.selectOnePOE(selector));
+        },
+      };
+      Object.defineProperty(target, propertyKey, propertyDescriptor);
+    }
+    return decorator;
+  }
+
+  /**
+   * A property decorator that instantiates nested PageObjects for all the child elements that
+   * match the selector.
+   *
+   * @example
+   *     class MyElementPO extends PageObject {
+   *       @POBySelectorAll('another-element')
+   *       anotherElementPOs!: Promise<AnotherElementPO[]>;
+   *
+   *       async clickNthElement(n: number) {
+   *         await (await this.anotherElementPOs)[n].clickSomeBtn();
+   *       }
+   *
+   *       ...
+   *     }
+   */
+  public static POBySelectorAll<T extends PageObject>(
+      selector: string, ctor: { new(...args: any[]): T }): any {
+    const decorator: PropertyDecorator = (target: Object, propertyKey: string | symbol) => {
+      const propertyDescriptor: PropertyDescriptor = {
+        get(): any {
+          const po = this as PageObject;
+          return po.selectAllPOE(selector).then((poes) => poes.map((poe) => new ctor(poe)));
+        },
+      };
+      Object.defineProperty(target, propertyKey, propertyDescriptor);
+    }
+    return decorator;
+  }
+
   protected element: PageObjectElement;
 
   constructor(element: HTMLElement | ElementHandle | PageObjectElement) {
@@ -33,6 +151,11 @@ export abstract class PageObject {
     } else {
       this.element = new PageObjectElement(element);
     }
+  }
+
+  /** Returns true if the underlying PageObjectElement is empty. */
+  get empty() {
+    return this.element.empty;
   }
 
   /**
@@ -95,4 +218,11 @@ export abstract class PageObject {
       selector: string, fn: (element: PageObjectElement, index: number) => Promise<boolean>) {
     return this.element.selectAllPOEThenFind(selector, fn);
   }
-};
+}
+
+// Re-export decorators as module-level constants for brevity on the client code's side.
+
+export const BySelector = PageObject.BySelector;
+export const BySelectorAll = PageObject.BySelectorAll;
+export const POBySelector = PageObject.POBySelector;
+export const POBySelectorAll = PageObject.POBySelectorAll;
