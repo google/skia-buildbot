f669be058e214a477c1b749d0c374c85351788d9
Leandro Lovisolo <lovisolo@google.com>
2021-05-11 16:18:30 -0400

    [page object] Use the @BySelector family of decorators in all POs.
    Change-Id: I5c1c7d74fe2f106a07cadf0ff2043d190250ddbf

--- golden/modules/query-dialog-sk/query-dialog-sk_po.ts
+++ golden/modules/query-dialog-sk/query-dialog-sk_po.ts
@@ -1,69 +1,61 @@
-import { PageObject } from '../../../infra-sk/modules/page_object/page_object';
+import { BySelector, PageObject, POBySelector } from '../../../infra-sk/modules/page_object/page_object';
 import { ParamSetSkPO } from '../../../infra-sk/modules/paramset-sk/paramset-sk_po';
 import { QuerySkPO } from '../../../infra-sk/modules/query-sk/query-sk_po';
 import { ParamSet } from 'common-sk/modules/query';
+import { PageObjectElement } from '../../../infra-sk/modules/page_object/page_object_element';
 
 /** A page object for the QueryDialogSk component. */
 export class QueryDialogSkPO extends PageObject {
-  getQuerySkPO() {
-    return this.selectOnePOEThenApplyFn('query-sk', async (el) => new QuerySkPO(el));
-  }
+  @POBySelector('query-sk', QuerySkPO)
+  querySkPO!: Promise<QuerySkPO>;
 
-  getParamSetSkPO() {
-    return this.selectOnePOEThenApplyFn('paramset-sk', async (el) => new ParamSetSkPO(el));
-  }
+  @POBySelector('paramset-sk', ParamSetSkPO)
+  paramSetSkPO!: Promise<ParamSetSkPO>;
+
+  @BySelector('dialog')
+  private dialog!: Promise<PageObjectElement>;
+
+  @BySelector('.empty-selection')
+  private emptySelectionMessage!: Promise<PageObjectElement>;
+
+  @BySelector('button.show-matches')
+  private showMatchesBtn!: Promise<PageObjectElement>;
+
+  @BySelector('button.cancel')
+  private cancelBtn!: Promise<PageObjectElement>;
 
   async isDialogOpen() {
-    return this.selectOneDOMNodeThenApplyFn(
-      'dialog', (dialog) => (dialog as HTMLDialogElement).open);
+    return (await this.dialog).applyFnToDOMNode((d) => (d as HTMLDialogElement).open);
   }
 
-  async isEmptySelectionMessageVisible() {
-    return (await this.selectOnePOE('.empty-selection')) !== null;
-  }
+  async isEmptySelectionMessageVisible() { return !(await this.emptySelectionMessage).isEmpty(); }
 
-  async isParamSetSkVisible() {
-    return (await this.selectOnePOE('paramset-sk')) !== null;
-  }
+  async isParamSetSkVisible() { return !(await this.paramSetSkPO).isEmpty(); }
 
-  async clickKey(key: string) {
-    return (await this.getQuerySkPO()).clickKey(key);
-  }
+  async clickKey(key: string) { await (await this.querySkPO).clickKey(key); }
 
-  async clickValue(value: string) {
-    return (await this.getQuerySkPO()).clickValue(value);
-  }
+  async clickValue(value: string) { await (await this.querySkPO).clickValue(value); }
 
-  async clickShowMatchesBtn() {
-    return this.selectOnePOEThenApplyFn('button.show-matches', (btn) => btn.click());
-  }
+  async clickShowMatchesBtn() { await (await this.showMatchesBtn).click(); }
 
-  async clickCancelBtn() {
-    return this.selectOnePOEThenApplyFn('button.cancel', (btn) => btn.click());
-  }
+  async clickCancelBtn() { await (await this.cancelBtn).click(); }
 
   async getParamSetSkContents() {
-    const paramSetSkPO = await this.getParamSetSkPO();
-    const paramSets = await paramSetSkPO.getParamSets();
+    const paramSets = await (await this.paramSetSkPO).getParamSets();
     return paramSets[0]; // There's only one ParamSet.
   }
 
   /** Returns the key/value pairs available for the user to choose from. */
-  async getParamSet() {
-    return (await this.getQuerySkPO()).getParamSet();
-  }
+  async getParamSet() { return (await this.querySkPO).getParamSet(); }
 
   /** Gets the selected query. */
-  async getSelection() {
-    return (await this.getQuerySkPO()).getCurrentQuery();
-  }
+  async getSelection() { return (await this.querySkPO).getCurrentQuery(); }
 
   /** Sets the selected query via simulated UI interactions. */
   async setSelection(selection: ParamSet) {
-    const querySkPO = await this.getQuerySkPO();
-    await querySkPO.setCurrentQuery(selection);
+    await (await this.querySkPO).setCurrentQuery(selection);
 
     // Remove focus from the last selected value in the query-sk component. This reduces flakiness.
-    await this.selectOnePOEThenApplyFn('dialog', (el) => el.click());
+    await (await this.dialog).click();
   }
-};
+}
