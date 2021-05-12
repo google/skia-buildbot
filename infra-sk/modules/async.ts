670d7a677410af96cc4a2ab8711f03a706b4fc88
Leandro Lovisolo <lovisolo@google.com>
2021-05-12 00:07:55 -0400

    [infra-sk] Add async.ts library.
    This library defines general-purpose, async versions of Array methods filter/find/forEach/map, where the input can be a promise of an array, and the callback returns a promise.
    
    These functions will be used from the Page Object library (see attached bug), but are completely orthogonal to said library.
    
    Bug: skia:11993
    Change-Id: I4e57eea9b6a77b5c4c3a5504a3b3e7b732c06163
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/405877
    Reviewed-by: Kevin Lubick <kjlubick@google.com>
    Commit-Queue: Leandro Lovisolo <lovisolo@google.com>

---
+++ infra-sk/modules/async.ts
@@ -0,0 +1,52 @@
+// This module contains async versions of various built-in JavaScript functions and methods.
+
+/** Async version of Array.prototype.find(), where the callback function returns a promise. */
+export async function asyncFind<T>(
+    items: T[] | Promise<T[]>,
+    predicate: (item: T, index: number) => Promise<boolean>): Promise<T | null> {
+  if (!items) {
+    return null;
+  }
+  const actualItems = items instanceof Promise ? await items : items;
+  for (let i = 0; i < actualItems.length; i++) {
+    if (await (predicate(actualItems[i], i))) {
+      return actualItems[i];
+    }
+  }
+  return null;
+}
+
+/** Async version of Array.prototype.filter(), where the callback function returns a promise. */
+export async function asyncFilter<T>(
+    items: T[] | Promise<T[]>,
+    predicate: (item: T, index: number) => Promise<boolean>): Promise<T[]> {
+  if (!items) {
+    return [];
+  }
+  const actualItems = items instanceof Promise ? await items : items;
+  const filteredItems: T[] = [];
+  for (let i = 0; i < actualItems.length; i++) {
+    if (await (predicate(actualItems[i], i))) {
+      filteredItems.push(actualItems[i]);
+    }
+  }
+  return filteredItems;
+}
+
+/** Async version of Array.prototype.map(), where the callback function returns a promise. */
+export async function asyncMap<F, T>(
+    items: F[] | Promise<F[]>,
+    fn: (from: F, index: number) => Promise<T>): Promise<T[]> {
+  if (!items) {
+    return [];
+  }
+  const actualItems = items instanceof Promise ? await items : items;
+  return Promise.all(actualItems.map(fn));
+}
+
+/** Async version of Array.prototype.forEach(), where the callback function returns a promise. */
+export async function asyncForEach<T>(
+    items: T[] | Promise<T[]>,
+    fn: (item: T, index: number) => Promise<void>): Promise<void> {
+  await asyncMap(items, fn);
+}
