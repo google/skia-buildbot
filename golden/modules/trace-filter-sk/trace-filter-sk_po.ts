e931a5305ddb4c09514762673017cc8ecc471d69
Leandro Lovisolo <lovisolo@google.com>
2021-05-12 16:47:03 -0400

    [page object] Only use select{One,All}POE methods in all POs.
    This CL drops all usages of the select{One,All}POEThen* methods. Instead, methods select{One,All}POE are used, in combination with the async{Filter,Find,ForEach,Map} functions in async.ts.
    
    Also:
    
    - All selectors in POs are extracted into property getters at the beginning of each PO with self-documenting property names. This avoids duplicating selectors and makes POs more stylistically consistent.
    
    - Methods poBySelector and poBySelectorAll added to PageObject for convenience (names are consistent with follow-up changes, where we rename selectOnePOE and selectAllPOE to bySelector and bySelectorAll, respectively).
    
    - Stop returning null from query selector functions when the node is not found, and replace that with a PageObjectElement.isEmpty() method. This will simplify a follow-up CL where we stop returning promises to prevent nested awaits (e.g. "await (await po.foo()).bar()").
    
    - Add isInnerTextEqualTo and hasClassName methods to PageObjectElement, which simplifies many page objects.
    
    Recommended review order:
    
     - page_object_element.ts
     - page_object_element_test_cases.ts
     - page_object.ts
     - Everything else.
    
    Bug: skia:11993
    Change-Id: I5c1c7d74fe2f106a07cadf0ff2043d190250ddbf

--- golden/modules/trace-filter-sk/trace-filter-sk_po.ts
+++ golden/modules/trace-filter-sk/trace-filter-sk_po.ts
@@ -1,60 +1,60 @@
 import { ParamSet } from 'common-sk/modules/query';
-import { PageObject } from '../../../infra-sk/modules/page_object/page_object';
+import { PageObject} from '../../../infra-sk/modules/page_object/page_object';
 import { ParamSetSkPO } from '../../../infra-sk/modules/paramset-sk/paramset-sk_po';
 import { QueryDialogSkPO } from '../query-dialog-sk/query-dialog-sk_po';
+import { PageObjectElement } from '../../../infra-sk/modules/page_object/page_object_element';
 
 /** A page object for the TraceFilterSk component. */
 export class TraceFilterSkPO extends PageObject {
-  getParamSetSkPO() {
-    return this.selectOnePOEThenApplyFn(
-      '.selection paramset-sk', async (el) => new ParamSetSkPO(el));
+  get paramSetSkPO(): Promise<ParamSetSkPO> {
+    return this.poBySelector('.selection paramset-sk', ParamSetSkPO);
   }
 
-  getQueryDialogSkPO() {
-    return this.selectOnePOEThenApplyFn('query-dialog-sk', async (el) => new QueryDialogSkPO(el));
+  get queryDialogSkPO(): Promise<QueryDialogSkPO> {
+    return this.poBySelector('query-dialog-sk', QueryDialogSkPO);
   }
 
-  async isQueryDialogSkOpen() {
-    return (await this.getQueryDialogSkPO()).isDialogOpen();
+  private get emptyFilterMessage(): Promise<PageObjectElement> {
+    return this.selectOnePOE('.selection .empty-placeholder');
   }
 
-  async isEmptyFilterMessageVisible() {
-    return (await this.selectOnePOE('.selection .empty-placeholder')) !== null;
+  private get editBtn(): Promise<PageObjectElement> {
+    return this.selectOnePOE('.edit-query');
   }
 
-  async isParamSetSkVisible() {
-    return (await this.selectOnePOE('.selection paramset-sk')) !== null;
-  }
+  async isQueryDialogSkOpen() { return (await this.queryDialogSkPO).isDialogOpen(); }
 
-  async clickEditBtn() {
-    return this.selectOnePOEThenApplyFn('.edit-query', (btn) => btn.click());
-  }
+  async isEmptyFilterMessageVisible() { return !(await this.emptyFilterMessage).isEmpty(); }
+
+  async isParamSetSkVisible() { return !(await this.paramSetSkPO).isEmpty(); }
+
+  async clickEditBtn() { await (await this.editBtn).click(); }
 
   async getParamSetSkContents() {
-    const paramSetSkPO = await this.getParamSetSkPO();
+    const paramSetSkPO = await this.paramSetSkPO;
     const paramSets = await paramSetSkPO.getParamSets();
     return paramSets[0]; // There's only one ParamSet.
   }
 
   async clickQueryDialogSkShowMatchesBtn() {
-    return (await this.getQueryDialogSkPO()).clickShowMatchesBtn();
+    return (await this.queryDialogSkPO).clickShowMatchesBtn();
   }
 
   async clickQueryDialogSkCancelBtn() {
-    return (await this.getQueryDialogSkPO()).clickCancelBtn();
+    return (await this.queryDialogSkPO).clickCancelBtn();
   }
 
   async getQueryDialogSkParamSet() {
-    return (await this.getQueryDialogSkPO()).getParamSet();
+    return (await this.queryDialogSkPO).getParamSet();
   }
 
   async getQueryDialogSkSelection() {
-    return (await this.getQueryDialogSkPO()).getSelection();
+    return (await this.queryDialogSkPO).getSelection();
   }
 
   /** Sets the selected query in the query-dialog-sk via simulated UI interactions. */
   async setQueryDialogSkSelection(selection: ParamSet) {
-    return (await this.getQueryDialogSkPO()).setSelection(selection);
+    return (await this.queryDialogSkPO).setSelection(selection);
   }
 
   /** Analogous to the "selection" property getter. */
@@ -64,4 +64,4 @@ export class TraceFilterSkPO extends PageObject {
     }
     return this.getParamSetSkContents();
   }
-};
+}
