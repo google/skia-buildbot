bcbee857ac8112ca45d69d1928dd9e9e69c876a4
Leandro Lovisolo <lovisolo@google.com>
2021-05-12 00:08:50 -0400

    [page object] Use the @BySelector family of decorators in all POs.
    Also:
    
    - As per the attached bug, stop using select{One,All}POEThen* methods, in favor of the async{Filter,Find,ForEach,Map} functions in async.ts.
    
    - Stop returning null from query selector functions when the node is not found, and replace that with a PageObjectElement.isEmpty() method. This will simplify a follow-up CL where we stop returning promises to prevent nested awaits (e.g. "await (await po.foo()).bar()").
    
    - Add isInnerTextEqualTo and hasClassName methods to PageObjectElement, which simplifies many page objects.
    
    Recommended review order:
    
     - page_object_element.ts
     - page_object_element_test_cases.ts
     - page_object.ts
     - Everything else.
    
    Bug: skia:11993
    Change-Id: I5c1c7d74fe2f106a07cadf0ff2043d190250ddbf

--- golden/modules/bulk-triage-sk/bulk-triage-sk_po.ts
+++ golden/modules/bulk-triage-sk/bulk-triage-sk_po.ts
@@ -1,74 +1,64 @@
-import { PageObject } from '../../../infra-sk/modules/page_object/page_object';
+import {BySelector, PageObject} from '../../../infra-sk/modules/page_object/page_object';
 import { CheckOrRadio } from 'elements-sk/checkbox-sk/checkbox-sk';
+import {PageObjectElement} from '../../../infra-sk/modules/page_object/page_object_element';
 
 /** A page object for the BulkTriageSkPO component. */
 export class BulkTriageSkPO extends PageObject {
-  async isAffectedChangelistIdVisible() {
-    return (await this.selectOnePOE('p.cl')) !== null;
+  @BySelector('p.cl')
+  private cl!: Promise<PageObjectElement>;
+
+  @BySelector('button.positive')
+  private positiveBtn!: Promise<PageObjectElement>;
+
+  @BySelector('button.negative')
+  private negativeBtn!: Promise<PageObjectElement>;
+
+  @BySelector('button.untriaged')
+  private untriagedBtn!: Promise<PageObjectElement>;
+
+  @BySelector('button.closest')
+  private closestBtn!: Promise<PageObjectElement>;
+
+  @BySelector('checkbox-sk.triage_all')
+  private triageAllCheckBox!: Promise<PageObjectElement>;
+
+  @BySelector('button.triage')
+  private triageBtn!: Promise<PageObjectElement>;
+
+  @BySelector('button.cancel')
+  private cancelBtn!: Promise<PageObjectElement>;
+
+  async isAffectedChangelistIdVisible() { return !(await this.cl).isEmpty(); }
+
+  async getAffectedChangelistId() { return (await this.cl).innerText; }
+
+  async isUntriagedBtnSelected() { return (await this.untriagedBtn).hasClassName('selected'); }
+
+  async clickUntriagedBtn() { await (await this.untriagedBtn).click(); }
+
+  async isPositiveBtnSelected() { return (await this.positiveBtn).hasClassName('selected'); }
+
+  async clickPositiveBtn() { return (await this.positiveBtn).click(); }
+
+  async isNegativeBtnSelected() { return (await this.negativeBtn).hasClassName('selected'); }
+
+  async clickNegativeBtn() { await (await this.negativeBtn).click(); }
+
+  async isClosestBtnSelected() { return (await this.closestBtn).hasClassName('selected'); }
+
+  async clickClosestBtn() { await (await this.closestBtn).click(); }
+
+  async getTriageAllCheckboxLabel() {  return (await this.triageAllCheckBox).innerText; }
+
+  async isTriageAllCheckboxChecked() {
+    return (await this.triageAllCheckBox).applyFnToDOMNode((c) => (c as CheckOrRadio).checked);
   }
 
-  async getAffectedChangelistId() {
-    return this.selectOnePOEThenApplyFn('p.cl', (el) => el.innerText);
-  }
+  async clickTriageAllCheckbox() { await (await this.triageAllCheckBox).click(); }
 
-  isUntriagedBtnSelected() {
-    return this.selectOnePOEThenApplyFn(
-      'button.untriaged', async (el) => (await el.className).includes('selected'));
-  }
+  async getTriageBtnLabel() { return (await this.triageBtn).innerText; }
 
-  async clickUntriagedBtn() {
-    await this.selectOnePOEThenApplyFn('button.untriaged', (el) => el.click());
-  }
+  async clickTriageBtn() { await (await this.triageBtn).click(); }
 
-  isPositiveBtnSelected() {
-    return this.selectOnePOEThenApplyFn(
-      'button.positive', async (el) => (await el.className).includes('selected'));
-  }
-
-  async clickPositiveBtn() {
-    await this.selectOnePOEThenApplyFn('button.positive', (el) => el.click());
-  }
-
-  isNegativeBtnSelected() {
-    return this.selectOnePOEThenApplyFn(
-      'button.negative', async (el) => (await el.className).includes('selected'));
-  }
-
-  async clickNegativeBtn() {
-    await this.selectOnePOEThenApplyFn('button.negative', (el) => el.click());
-  }
-
-  isClosestBtnSelected() {
-    return this.selectOnePOEThenApplyFn(
-      'button.closest', async (el) => (await el.className).includes('selected'));
-  }
-
-  async clickClosestBtn() {
-    await this.selectOnePOEThenApplyFn('button.closest', (el) => el.click());
-  }
-
-  async getTriageAllCheckboxLabel() {
-    return this.selectOnePOEThenApplyFn('checkbox-sk.triage_all', (el) => el.innerText);
-  }
-
-  isTriageAllCheckboxChecked() {
-    return this.selectOneDOMNodeThenApplyFn(
-      'checkbox-sk.triage_all', (c) => (c as CheckOrRadio).checked);
-  }
-
-  async clickTriageAllCheckbox() {
-    await this.selectOnePOEThenApplyFn('checkbox-sk.triage_all', (el) => el.click());
-  }
-
-  async getTriageBtnLabel() {
-    return this.selectOnePOEThenApplyFn('button.triage', (el) => el.innerText);
-  }
-
-  async clickTriageBtn() {
-    await this.selectOnePOEThenApplyFn('button.triage', (el) => el.click());
-  }
-
-  async clickCancelBtn() {
-    await this.selectOnePOEThenApplyFn('button.cancel', (el) => el.click());
-  }
-};
+  async clickCancelBtn() { await (await this.cancelBtn).click(); }
+}
