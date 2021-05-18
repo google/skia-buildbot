acc6a3df0483294a4db53d8190cde642c1badd25
Kevin Lubick <kjlubick@google.com>
2021-05-18 09:41:53 -0400

    [gold] Fix issues with CL searching
    The main issue here was that CRS was not getting parsed
    from the API call. We had some fallback logic on v1 of search
    to use the default CRS, but that was masking the problem.
    
    Additionally, the query struct had JSON tags which are
    never actually used, but confused me into thinking they
    handled parsing. Due to this, they have been removed.
    
    This also addresses an issue with v2 of search in which
    the systems never got plugged into the API.
    
    Bug: skia:10582
    Change-Id: I27086772ac661a25d030dd2dfc9888c190ed2c3c

--- golden/go/search/query/types.go
+++ golden/go/search/query/types.go
@@ -9,49 +9,49 @@ import (
 // Search represents the params to the Search function.
 type Search struct {
 	// Diff metric to use.
-	Metric string   `json:"metric"`
-	Sort   string   `json:"sort"`
-	Match  []string `json:"match"`
+	Metric string
+	Sort   string
+	Match  []string
 
 	// Blaming
-	BlameGroupID string `json:"blame"`
+	BlameGroupID string
 
 	// Image classification
-	IncludePositiveDigests           bool `json:"pos"`
-	IncludeNegativeDigests           bool `json:"neg"`
-	IncludeUntriagedDigests          bool `json:"unt"`
-	OnlyIncludeDigestsProducedAtHead bool `json:"head"`
-	IncludeIgnoredTraces             bool `json:"include"`
+	IncludePositiveDigests           bool
+	IncludeNegativeDigests           bool
+	IncludeUntriagedDigests          bool
+	OnlyIncludeDigestsProducedAtHead bool
+	IncludeIgnoredTraces             bool
 
 	// URL encoded query string
-	QueryStr    string              `json:"query"`
-	TraceValues paramtools.ParamSet `json:"-"`
+	QueryStr    string
+	TraceValues paramtools.ParamSet
 	// Not given to us by the frontend yet.
-	OptionsValues paramtools.ParamSet `json:"-"`
+	OptionsValues paramtools.ParamSet
 
 	// URL encoded query string to select the right hand side of comparisons.
-	RightQueryStr    string              `json:"rquery"`
-	RightTraceValues paramtools.ParamSet `json:"-"`
+	RightQueryStr    string
+	RightTraceValues paramtools.ParamSet
 
 	// TryJob support.
-	ChangelistID       string `json:"issue"`
-	CodeReviewSystemID string `json:"crs_id"`
+	ChangelistID       string
+	CodeReviewSystemID string
 	// TODO(kjlubick) Change this so only one patchset is allowed. It will simplify the backend code.
-	PatchsetsStr string  `json:"patchsets"` // Comma-separated list of patchsets.
-	Patchsets    []int64 `json:"-"`
+	PatchsetsStr string // Comma-separated list of patchsets.
+	Patchsets    []int64
 	// By default, we typically only want to see digests that were created exclusively on this CL,
 	// but sometimes the user wants to also see digests that are the same as on master, so this option
 	// allows for that.
-	IncludeDigestsProducedOnMaster bool `json:"master"`
+	IncludeDigestsProducedOnMaster bool
 
 	// Filtering.
-	RGBAMinFilter              int  `json:"frgbamin"` // Min RGBA delta
-	RGBAMaxFilter              int  `json:"frgbamax"` // Max RGBA delta
-	MustIncludeReferenceFilter bool `json:"fref"`     // Only digests with reference.
+	RGBAMinFilter              int  // Min RGBA delta
+	RGBAMaxFilter              int  // Max RGBA delta
+	MustIncludeReferenceFilter bool // Only digests with reference.
 
 	// Pagination.
-	Offset int `json:"offset"`
-	Limit  int `json:"limit"`
+	Offset int
+	Limit  int
 }
 
 // IgnoreState returns the types.IgnoreState that this
