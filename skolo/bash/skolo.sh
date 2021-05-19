54c33019a4a552bcd6b01cfab8d60c5605c3e633
Joe Gregorio <jcgregorio@google.com>
2021-05-19 10:46:23 -0400

    [skolo] Update skolo bash aliases to presume ssh.cfg.
    That is, presume //skolo/ansible/ssh.cfg has been appended to ~/.ssh/config so
    that short names can be used for jumphosts.
    
    Change-Id: I1a6b8a4fde735f89e3321a1aa7c6f56fe1979d84

--- skolo/bash/skolo.sh
+++ skolo/bash/skolo.sh
@@ -14,14 +14,17 @@ alias skolo_win2='ssh chrome-bot@100.115.95.133'
 alias skolo_win3='ssh chrome-bot@100.115.95.134'
 
 # Sets up port-forwarding to the Router.
-alias skolo_internal_router='google-chrome https://localhost:8888; ssh -L 8888:192.168.1.1:443 chrome-bot@100.115.95.131'
-alias skolo_rack1_router='google-chrome https://localhost:8888; ssh -L 8888:192.168.1.1:443 chrome-bot@100.115.95.143'
-alias skolo_rack2_router='google-chrome https://localhost:8888; ssh -L 8888:192.168.1.1:443 chrome-bot@100.115.95.133'
-alias skolo_rack3_router='google-chrome https://localhost:8888; ssh -L 8888:192.168.1.1:443 chrome-bot@100.115.95.134'
-alias skolo_rpi_router='google-chrome https://localhost:8888; ssh -L 8888:192.168.1.1:443 chrome-bot@100.115.95.143'
-alias skolo_rpi2_router='google-chrome https://localhost:8888; ssh -L 8888:192.168.1.1:443 chrome-bot@100.115.95.135'
-alias skolo_win2_router='google-chrome https://localhost:8888; ssh -L 8888:192.168.1.1:443 chrome-bot@100.115.95.133'
-alias skolo_win3_router='google-chrome https://localhost:8888; ssh -L 8888:192.168.1.1:443 chrome-bot@100.115.95.134'
+# Router ports start at 9000.
+alias skolo_rack1_router='google-chrome https://localhost:9000; ssh -L 9000:192.168.1.1:443 rack1'
+alias skolo_rack2_router='google-chrome https://localhost:9001; ssh -L 9001:192.168.1.1:443 rack2'
+alias skolo_rack3_router='google-chrome https://localhost:9002; ssh -L 9002:192.168.1.1:443 rack3'
+alias skolo_rack4_router='google-chrome https://localhost:9003; ssh -L 9003:192.168.1.1:443 rack4'
+alias skolo_rack5_router='google-chrome https://localhost:9004; ssh -L 9004:192.168.1.1:443 rack5'
 
-# Connects to both the router and the switch.
-alias skolo_rpi2_network='google-chrome https://localhost:8888; google-chrome https://localhost:8889; ssh -L 8888:192.168.1.1:443 -L 8889:rack4-shelf1-poe-switch:443 chrome-bot@100.115.95.135'
+
+# Shelf ports start at 7000, and the second digit is the rack number, the last number is the shelf.
+alias skolo_rack1_shelf2_switch='google-chrome https://localhost:7101; ssh -L 7101:rack1-shelf1-poe-switch:443 rack1'
+alias skolo_rack1_shelf2_switch='google-chrome https://localhost:7102; ssh -L 7102:rack1-shelf2-poe-switch:443 rack1'
+
+alias skolo_rack4_shelf1_switch='google-chrome https://localhost:7401; ssh -L 7401:rack4-shelf1-poe-switch:443 rack4'
+alias skolo_rack4_shelf2_switch='google-chrome https://localhost:7402; ssh -L 7402:rack4-shelf2-poe-switch:443 rack4'
