7a45658933f4a434c6a872a623e34088cc4c23da
Leandro Lovisolo <lovisolo@google.com>
2021-05-19 22:10:39 -0400

    [gold] Port image-compare-sk to TypeScript.
    Bug: skia:10246
    Change-Id: I47deae2efe3c5d198d467acd2d75f2754ff2682d

--- golden/modules/image-compare-sk/image-compare-sk-demo.ts
+++ golden/modules/image-compare-sk/image-compare-sk-demo.ts
@@ -1,12 +1,13 @@
 import './index';
 import { $$ } from 'common-sk/modules/dom';
 import { setImageEndpointsForDemos } from '../common';
+import {ImageCompareSk} from './image-compare-sk';
 
 const aDigest = '6246b773851984c726cb2e1cb13510c2';
 const bDigest = '99c58c7002073346ff55f446d47d6311';
 
 setImageEndpointsForDemos();
-let ele = document.createElement('image-compare-sk');
+let ele = new ImageCompareSk();
 ele.left = {
   digest: aDigest,
   title: 'a digest title',
@@ -17,16 +18,12 @@ ele.right = {
   title: 'the other image',
   detail: 'example.com#bDigest',
 };
-$$('#normal').appendChild(ele);
+$$('#normal')!.appendChild(ele);
 
-ele = document.createElement('image-compare-sk');
+ele = new ImageCompareSk();
 ele.left = {
   digest: aDigest,
   title: 'a digest title',
   detail: 'example.com#aDigest',
 };
-$$('#no_right').appendChild(ele);
-
-document.addEventListener('zoom-clicked', (e) => {
-  $$('#event').textContent = `zoom-clicked: ${JSON.stringify(e.detail)}`;
-});
+$$('#no_right')!.appendChild(ele);
---
+++ golden/modules/image-compare-sk/image-compare-sk.ts
@@ -0,0 +1,156 @@
+/**
+ * @module module/image-compare-sk
+ * @description <h2><code>image-compare-sk</code></h2>
+ *
+ * Shows a side by side comparison of two images. If there's nothing to compare against, it will
+ * only display one.
+ *
+ * @event zoom-dialog-opened when the user opens the multi-zoom-sk dialog.
+ *
+ * @event zoom-dialog-closed when the user closes the multi-zoom-sk dialog.
+ *
+ */
+import { define } from 'elements-sk/define';
+import { html } from 'lit-html';
+import dialogPolyfill from 'dialog-polyfill';
+import { ElementSk } from '../../../infra-sk/modules/ElementSk';
+import { MultiZoomSk } from '../multi-zoom-sk/multi-zoom-sk';
+
+import 'elements-sk/icon/open-in-new-icon-sk';
+import 'elements-sk/styles/buttons';
+import { digestDiffImagePath, digestImagePath } from '../common';
+
+import '../multi-zoom-sk';
+
+export interface ImageComparisonData {
+  digest: string;
+  title: string;
+  detail: string;
+}
+
+export class ImageCompareSk extends ElementSk {
+  private static template = (ele: ImageCompareSk) => html`
+    <div class=comparison_bar>
+      <figure>
+        <img class=thumbnail alt="left image" src=${digestImagePath(ele.left.digest)}>
+        <figcaption>
+          <span class=legend_dot></span>
+          <a target=_blank rel=noopener href=${ele.left.detail}>${ele.left.title}</a>
+        </figcaption>
+      </figure>
+      <a target=_blank rel=noopener href=${digestImagePath(ele.left.digest)}>
+        <open-in-new-icon-sk></open-in-new-icon-sk>
+      </a>
+      ${ImageCompareSk.comparison(ele)}
+    </div>
+
+    <button class=zoom_btn ?hidden=${!ele.right} @click=${ele.handleZoomClicked}>Zoom</button>
+    <dialog class=zoom_dialog @close=${ele.closeEvent}))}>
+      <button class=close_btn @click=${ele.closeDialog}>Close</button>
+    </dialog>
+  `;
+
+  private static comparison = (ele: ImageCompareSk) => {
+    if (!ele.right ) {
+      if (ele.isComputingDiffs) {
+        return html`<div class=computing title="Check back later">
+            Computing closest positive and negative image. Check back later.</div>`;
+      }
+      return html`<div class=no_compare>No other images to compare against.</div>`;
+    }
+    const diffSrc = digestDiffImagePath(ele.left.digest, ele.right.digest);
+    return html`
+      <img class="thumbnail diff" alt="diff between left and right image" src=${diffSrc}>
+      <a target=_blank rel=noopener href=${diffSrc}>
+        <open-in-new-icon-sk></open-in-new-icon-sk>
+      </a>
+
+      <figure>
+        <img class=thumbnail alt="right image" src=${digestImagePath(ele.right.digest)}>
+        <figcaption>
+          <a target=_blank rel=noopener href=${ele.right.detail}>${ele.right.title}</a>
+        </figcaption>
+      </figure>
+      <a target=_blank rel=noopener href=${digestImagePath(ele.right.digest)}>
+        <open-in-new-icon-sk></open-in-new-icon-sk>
+      </a>
+    `;
+  };
+
+  private _left: ImageComparisonData = {
+    digest: '',
+    title: '',
+    // We can't derive the detail url w/o also passing down changelistID, crs etc, so we have
+    // the caller compute those URLs and pass them into this element.
+    detail: '',
+  }
+  private _right: ImageComparisonData | null = null;
+  private computingDiffs = false;
+
+  constructor() {
+    super(ImageCompareSk.template);
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    this._render();
+    dialogPolyfill.registerDialog(this.querySelector('dialog.zoom_dialog')!);
+  }
+
+  get isComputingDiffs(): boolean {return this.computingDiffs; }
+
+  set isComputingDiffs(b: boolean) {
+    this.computingDiffs = b;
+    this._render();
+  }
+
+  get left(): ImageComparisonData { return this._left; }
+
+  set left(img: ImageComparisonData) {
+    this._left = img;
+    this._render();
+  }
+
+  get right(): ImageComparisonData | null { return this._right; }
+
+  set right(img: ImageComparisonData | null) {
+    this._right = img;
+    this._render();
+  }
+
+  private closeDialog() {
+    // This will fire a close event.
+    this.querySelector<HTMLDialogElement>('dialog.zoom_dialog')?.close();
+  }
+
+  private closeEvent() {
+    // We clean up both when the user clicks the close button as well if they hit escape by waiting
+    // for the close event (instead of handling this in closeDialog().
+    const dialog = this.querySelector<HTMLDialogElement>('dialog.zoom_dialog');
+    const zoom = this.querySelector<MultiZoomSk>('dialog multi-zoom-sk');
+    if (dialog && zoom) {
+      // Removing the element from the dom removes the keybinding handlers and lets the browser
+      // free up the image resources.
+      dialog.removeChild(zoom);
+    }
+    this.dispatchEvent(new CustomEvent('zoom-dialog-closed', { bubbles: true }));
+  }
+
+  private handleZoomClicked() {
+    const ele = new MultiZoomSk();
+    ele.details = {
+      leftImageSrc: digestImagePath(this.left.digest),
+      rightImageSrc: digestImagePath(this.right!.digest),
+      diffImageSrc: digestDiffImagePath(this.left.digest, this.right!.digest),
+      leftLabel: this.left.title,
+      rightLabel: this.right!.title,
+    };
+    const dialog = this.querySelector<HTMLDialogElement>('dialog.zoom_dialog')!;
+    // put the dialog before the button
+    dialog.insertBefore(ele, dialog.childNodes[0]);
+    dialog.showModal();
+    this.dispatchEvent(new CustomEvent('zoom-dialog-opened', { bubbles: true }));
+  }
+}
+
+define('image-compare-sk', ImageCompareSk);
