a2f8dddc94161f11981d74fb0f88b1bbcad0fa80
Leandro Lovisolo <lovisolo@google.com>
2021-05-20 14:24:02 -0400

    [cq] Mark a test case in //go/cq/update_test.go as manual.
    The affected test currently fails on RBE with "please run lucicfg auth-login". As discussed, let's mark this test as manual for now.
    
    Change-Id: I5c84585afff9bc32e21bf5386f84a45f138dd6c1
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/410792
    Reviewed-by: Eric Boren <borenet@google.com>
    Commit-Queue: Leandro Lovisolo <lovisolo@google.com>

--- go/cq/update_test.go
+++ go/cq/update_test.go
@@ -1,17 +1,12 @@
 package cq
 
 import (
-	"context"
-	"io/ioutil"
-	"os"
-	"path/filepath"
 	"regexp"
 	"testing"
 
 	"github.com/bazelbuild/buildtools/build"
 	"github.com/stretchr/testify/require"
 	"go.skia.org/infra/go/git"
-	"go.skia.org/infra/go/testutils"
 	"go.skia.org/infra/go/testutils/unittest"
 )
 
@@ -62,39 +57,4 @@ func TestDeleteBranch(t *testing.T) {
 	})
 	require.NoError(t, err)
 	require.Equal(t, deleteExpect, string(actual))
-}
-
-func TestWithUpdateCQConfig(t *testing.T) {
-	unittest.LargeTest(t)
-
-	ctx := context.Background()
-
-	tmp, err := ioutil.TempDir("", "")
-	require.NoError(t, err)
-	defer testutils.RemoveAll(t, tmp)
-
-	mainStarFile := filepath.Join(tmp, filename)
-	testutils.WriteFile(t, mainStarFile, fakeConfig)
-	// We use a directory other than the default "generated", to verify that we
-	// respect what the caller passed in.
-	generatedDir := filepath.Join(tmp, "my-generated-configs")
-	require.NoError(t, os.MkdirAll(generatedDir, os.ModePerm))
-
-	require.NoError(t, WithUpdateCQConfig(ctx, mainStarFile, generatedDir, func(f *build.File) error {
-		return DeleteBranch(f, "master")
-	}))
-
-	generatedFiles, err := os.ReadDir(generatedDir)
-	require.NoError(t, err)
-	generatedFileNames := make([]string, 0, len(generatedFiles))
-	for _, f := range generatedFiles {
-		generatedFileNames = append(generatedFileNames, f.Name())
-	}
-	require.Equal(t, []string{
-		"commit-queue.cfg",
-		"cr-buildbucket.cfg",
-		"luci-logdog.cfg",
-		"project.cfg",
-	}, generatedFileNames)
-
 }
