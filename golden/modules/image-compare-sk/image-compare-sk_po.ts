2f5ddda84d43c9ae681dbe56d5f4773a2b4b3de2
Leandro Lovisolo <lovisolo@google.com>
2021-05-20 13:55:52 -0400

    [gold] Port image-compare-sk to TypeScript.
    Bug: skia:10246
    Change-Id: I47deae2efe3c5d198d467acd2d75f2754ff2682d
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/410679
    Commit-Queue: Leandro Lovisolo <lovisolo@google.com>
    Reviewed-by: Kevin Lubick <kjlubick@google.com>

---
+++ golden/modules/image-compare-sk/image-compare-sk_po.ts
@@ -0,0 +1,58 @@
+import {PageObject} from '../../../infra-sk/modules/page_object/page_object';
+import {PageObjectElement, PageObjectElementList} from '../../../infra-sk/modules/page_object/page_object_element';
+import {MultiZoomSkPO} from '../multi-zoom-sk/multi-zoom-sk_po';
+
+/** A page object for the ImageCompareSk component. */
+export class ImageCompareSkPO extends PageObject {
+  get multiZoomSkPO(): MultiZoomSkPO {
+    return this.poBySelector('multi-zoom-sk', MultiZoomSkPO);
+  }
+
+  private get zoomBtn(): PageObjectElement {
+    return this.bySelector('button.zoom_btn');
+  }
+
+  private get closeZoomDialogBtn(): PageObjectElement {
+    return this.bySelector('dialog.zoom_dialog button.close_btn');
+  }
+
+  private get images(): PageObjectElementList {
+    return this.bySelectorAll('img');
+  }
+
+  private get imageAnchors(): PageObjectElementList {
+    return this.bySelectorAll('figcaption a');
+  }
+
+  private get zoomDialog(): PageObjectElement {
+    return this.bySelector('dialog.zoom_dialog');
+  }
+
+  getImageSrcs(): Promise<(string | null)[]> {
+    return this.images.map((img) => img.getAttribute('src'));
+  }
+
+  getImageCaptionTexts(): Promise<string[]> {
+    return this.imageAnchors.map((a) => a.innerText);
+  }
+
+  getImageCaptionHrefs(): Promise<(string | null)[]> {
+    return this.imageAnchors.map((a) => a.getAttribute('href'));
+  }
+
+  async isZoomBtnVisible(): Promise<boolean> {
+    return !(await this.zoomBtn.hasAttribute('hidden'));
+  }
+
+  async clickZoomBtn() {
+    await this.zoomBtn.click();
+  }
+
+  isZoomDialogVisible(): Promise<boolean> {
+    return this.zoomDialog.hasAttribute('open');
+  }
+
+  async clickCloseZoomDialogBtn() {
+    await this.closeZoomDialogBtn.click();
+  }
+}
