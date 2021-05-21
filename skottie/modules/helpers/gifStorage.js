cf90ff78191b0c30f765a7235c7827716cd383f8
airnan <hernantorrisi@gmail.com>
2021-05-20 20:48:24 -0400

    added localStorage helper and storing gif settings on localStorage
    Change-Id: Iac85f065165022ff446cf97695b798d5581ff17a

---
+++ skottie/modules/helpers/gifStorage.js
@@ -0,0 +1,10 @@
+import localStorage from './localStorage';
+
+const storageKey = 'skottie-gif-storage';
+
+const gifStorage = {
+  set: (key, value) => localStorage.setValueInObject(storageKey, key, value),
+  get: (key, defaultValue) => localStorage.getValueFromObject(storageKey, key, defaultValue),
+};
+
+export default gifStorage;
