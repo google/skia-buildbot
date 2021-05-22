76e9df5c26afea9add45d19bbc7d07ee03e1cb85
Leandro Lovisolo <lovisolo@google.com>
2021-05-21 20:43:39 -0400

    [gold] Port details-page-sk to TypeScript.
    Bug: skia:10246
    Change-Id: I528b9f28bfb824c0586e614f69ab4ac590f1e846

---
+++ golden/modules/details-page-sk/details-page-sk.ts
@@ -0,0 +1,119 @@
+/**
+ * @module module/details-page-sk
+ * @description <h2><code>details-page-sk</code></h2>
+ *
+ * Page to view details about a digest. This includes other digests similar to it and trace history.
+ */
+import { define } from 'elements-sk/define';
+import { html } from 'lit-html';
+import { jsonOrThrow } from 'common-sk/modules/jsonOrThrow';
+import { stateReflector } from 'common-sk/modules/stateReflector';
+import { ElementSk } from '../../../infra-sk/modules/ElementSk';
+
+import '../digest-details-sk';
+import { sendBeginTask, sendEndTask, sendFetchError } from '../common';
+import { Commit, DigestDetails, SearchResult } from '../rpc_types';
+
+export class DetailsPageSk extends ElementSk {
+  private static template = (ele: DetailsPageSk) => {
+    if (!ele.didInitialLoad) {
+      return html`<h1>Loading...</h1>`;
+    }
+    if (!ele.details?.digest) {
+      return html`
+        <div>
+          Could not load details for digest ${ele.digest} and test "${ele.grouping}".
+          <br>
+          It might not exist or be too new so as not to be indexed yet.
+        </div>
+      `;
+    }
+    return html`
+      <digest-details-sk .commits=${ele.commits}
+                         .changeListID=${ele.changeListID}
+                         .crs=${ele.crs}
+                         .details=${ele.details}>
+      </digest-details-sk>
+    `;
+  };
+
+  private grouping = '';
+  private digest = '';
+  private crs = '';
+  private changeListID = '';
+  private commits: Commit[] = [];
+  private details: SearchResult | null = null;
+  private didInitialLoad = false;
+
+  private stateChanged?: () => void;
+
+  // Allows us to abort fetches if we fetch again.
+  private fetchController?: AbortController;
+
+  constructor() {
+    super(DetailsPageSk.template);
+
+    this.stateChanged = stateReflector(
+      /* getState */() => ({
+        // provide empty values
+        test: this.grouping, // TODO(kjlubick) rename test -> grouping
+        digest: this.digest,
+        changelist_id: this.changeListID,
+        crs: this.crs,
+      }), /* setState */(newState) => {
+        if (!this._connected) {
+          return;
+        }
+        // default values if not specified.
+        this.grouping = newState.test as string || '';
+        this.digest = newState.digest as string || '';
+        this.changeListID = newState.changelist_id as string || '';
+        this.crs = newState.crs as string || '';
+        this.fetch();
+        this._render();
+      },
+    );
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    this._render();
+  }
+
+  private fetch() {
+    if (this.fetchController) {
+      // Kill any outstanding requests
+      this.fetchController.abort();
+    }
+
+    // Make a fresh abort controller for each set of fetches.
+    // They cannot be re-used once aborted.
+    this.fetchController = new AbortController();
+    const extra = {
+      signal: this.fetchController.signal,
+    };
+    sendBeginTask(this);
+
+    const url = `/json/v1/details?test=${encodeURIComponent(this.grouping)}`
+      + `&digest=${encodeURIComponent(this.digest)}&changelist_id=${this.changeListID}`
+      + `&crs=${this.crs}`;
+    fetch(url, extra)
+      .then(jsonOrThrow)
+      .then((digestDetails: DigestDetails) => {
+        this.commits = digestDetails.commits || [];
+        this.details = digestDetails.digest;
+        this.didInitialLoad = true;
+        this._render();
+        sendEndTask(this);
+      })
+      .catch((e) => {
+        this.commits = [];
+        this.details = null;
+        this.didInitialLoad = true;
+        this._render();
+        sendFetchError(this, e, 'digest-details');
+      });
+  }
+}
+
+define('details-page-sk', DetailsPageSk);
--- golden/modules/details-page-sk/details-page-sk_puppeteer_test.ts
+++ golden/modules/details-page-sk/details-page-sk_puppeteer_test.ts
@@ -7,16 +7,21 @@ import {
 } from '../../../puppeteer-tests/util';
 import { ElementHandle, Page } from 'puppeteer';
 import path from "path";
+import {DetailsPageSkPO} from './details-page-sk_po';
 
 describe('details-page-sk', () => {
   let testBed: TestBed;
+
   before(async () => {
     testBed = await loadCachedTestBed(
         path.join(__dirname, '..', '..', 'webpack.config.ts')
     );
   });
 
-  const baseParams = '?digest=6246b773851984c726cb2e1cb13510c2&test=My%20test%20has%20spaces';
+  // The demo page pushes a "default" query string to the URL when no query string is provided. All
+  // parameters in the below query string are different from said defaults. This allows this test
+  // to verify that the page correctly parses out the query parameters.
+  const baseParams = '?digest=99c58c7002073346ff55f446d47d6311&test=My%20test%20has%20spaces';
 
   it('should render the demo page', async () => {
     await navigateTo(testBed.page, testBed.baseUrl, baseParams);
@@ -57,36 +62,50 @@ describe('details-page-sk', () => {
 
   describe('url params', () => {
     it('correctly extracts the grouping and digest', async () => {
-      await navigateTo(testBed.page, testBed.baseUrl, baseParams);
-      const detailsPageSk = await testBed.page.$('details-page-sk');
+      const detailsPageSkPO = await navigateTo(testBed.page, testBed.baseUrl, baseParams);
 
-      expect(await getPropertyAsJSON(detailsPageSk!, '_grouping')).to.equal('My test has spaces');
-      expect(await getPropertyAsJSON(detailsPageSk!, '_digest')).to.equal('6246b773851984c726cb2e1cb13510c2');
-      expect(await getPropertyAsJSON(detailsPageSk!, '_changeListID')).to.equal('');
-      expect(await getPropertyAsJSON(detailsPageSk!, '_crs')).to.equal('');
+      expect(await detailsPageSkPO.digestDetailsSkPO.getTestName())
+          .to.equal('Test: My test has spaces');
+      expect(await detailsPageSkPO.digestDetailsSkPO.getLeftDigest())
+          .to.equal('Left: 99c58c7002073346ff55f446d47d6311');
+      expect(await detailsPageSkPO.digestDetailsSkPO.getRightDigest())
+          .to.equal('Right: 6246b773851984c726cb2e1cb13510c2');
+
+      // This link should not have a changelist ID or CRS.
+      expect(await detailsPageSkPO.digestDetailsSkPO.getDiffPageLink()).to.equal(
+          '/diff?test=My test has spaces'
+          + '&left=99c58c7002073346ff55f446d47d6311&right=6246b773851984c726cb2e1cb13510c2');
     });
 
-    it('correctly extracts the changelistID if provided', async () => {
-      await navigateTo(testBed.page, testBed.baseUrl, `${baseParams}&changelist_id=65432&crs=gerrit-internal`);
-      const detailsPageSk = await testBed.page.$('details-page-sk');
+    it('correctly extracts the changelist ID and CRS if provided', async () => {
+      const detailsPageSkPO =
+          await navigateTo(
+              testBed.page,
+              testBed.baseUrl,
+              `${baseParams}&changelist_id=65432&crs=gerrit-internal`);
 
-      expect(await getPropertyAsJSON(detailsPageSk!, '_grouping')).to.equal('My test has spaces');
-      expect(await getPropertyAsJSON(detailsPageSk!, '_digest')).to.equal('6246b773851984c726cb2e1cb13510c2');
-      expect(await getPropertyAsJSON(detailsPageSk!, '_changeListID')).to.equal('65432');
-      expect(await getPropertyAsJSON(detailsPageSk!, '_crs')).to.equal('gerrit-internal');
+      expect(await detailsPageSkPO.digestDetailsSkPO.getTestName())
+          .to.equal('Test: My test has spaces');
+      expect(await detailsPageSkPO.digestDetailsSkPO.getLeftDigest())
+          .to.equal('Left: 99c58c7002073346ff55f446d47d6311');
+      expect(await detailsPageSkPO.digestDetailsSkPO.getRightDigest())
+          .to.equal('Right: 6246b773851984c726cb2e1cb13510c2');
 
-      const digestDetails = await testBed.page.$('details-page-sk digest-details-sk');
-      expect(await getPropertyAsJSON(digestDetails!, 'changeListID')).to.equal('65432');
-      expect(await getPropertyAsJSON(digestDetails!, 'crs')).to.equal('gerrit-internal');
+      // The changelist ID and CRS should be reflected in this link.
+      expect(await detailsPageSkPO.digestDetailsSkPO.getDiffPageLink()).to.equal(
+          '/diff?test=My test has spaces'
+          + '&left=99c58c7002073346ff55f446d47d6311&right=6246b773851984c726cb2e1cb13510c2'
+          + '&changelist_id=65432&crs=gerrit-internal');
     });
   });
 });
 
-async function navigateTo(page: Page, base: string, queryParams = '') {
+async function navigateTo(page: Page, base: string, queryParams = ''): Promise<DetailsPageSkPO> {
   const eventPromise = await addEventListenersToPuppeteerPage(page, ['busy-end']);
   const loaded = eventPromise('busy-end'); // Emitted from gold-scaffold when page is loaded.
   await page.goto(`${base}/dist/details-page-sk.html${queryParams}`);
   await loaded;
+  return new DetailsPageSkPO(page.$('details-page-sk'));
 }
 
 async function getPropertyAsJSON(ele: ElementHandle, propName: string) {
