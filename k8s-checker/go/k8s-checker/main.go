6a144aeee0e6f8664af859d7b330e8a1a1e6e862
Ravi Mistry <rmistry@google.com>
2021-05-24 13:14:01 -0400

    Point k8s-checker to k8s-config main ref
    Bug: skia:12036
    Change-Id: Ib1e22f6d5227263b7bfb29a603d03a0a997defd3

--- k8s-checker/go/k8s-checker/main.go
+++ k8s-checker/go/k8s-checker/main.go
@@ -19,6 +19,7 @@ import (
 
 	"go.skia.org/infra/go/auth"
 	"go.skia.org/infra/go/common"
+	"go.skia.org/infra/go/git"
 	"go.skia.org/infra/go/gitiles"
 	"go.skia.org/infra/go/httputils"
 	"go.skia.org/infra/go/kube/clusterconfig"
@@ -235,7 +236,7 @@ func performChecks(ctx context.Context, clientset *kubernetes.Clientset, g *giti
 	}
 
 	// Read files from the k8sYamlRepo using gitiles.
-	fileInfos, err := g.ListDir(ctx, *cluster)
+	fileInfos, err := g.ListDirAtRef(ctx, *cluster, git.MainBranch)
 	if err != nil {
 		return nil, fmt.Errorf("Error when listing files from %s: %s", k8sYamlRepo, err)
 	}
@@ -251,7 +252,7 @@ func performChecks(ctx context.Context, clientset *kubernetes.Clientset, g *giti
 			// Only interested in YAML configs.
 			continue
 		}
-		yamlContents, err := g.ReadFile(ctx, filepath.Join(*cluster, f))
+		yamlContents, err := g.ReadFileAtRef(ctx, filepath.Join(*cluster, f), git.MainBranch)
 		if err != nil {
 			return nil, fmt.Errorf("Could not read file %s from %s %s: %s", f, k8sYamlRepo, *cluster, err)
 		}
