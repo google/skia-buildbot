3c9a7a4a75016ec7ed42b435c97129a00dfec0c5
Joe Gregorio <jcgregorio@google.com>
2021-05-24 14:50:27 -0400

    [jsfiddle] Port to TypeScript.
    Bug: skia:12030
    Change-Id: I3c530dfa2721c7950ccb83ac17abcfe9fe70c83e

---
+++ jsfiddle/modules/wasm-fiddle/wasm-fiddle_test.ts
@@ -0,0 +1,37 @@
+import './index';
+import { assert } from 'chai';
+import {
+  colorPickerRegex,
+  extractControlNames, sliderRegex,
+} from './wasm-fiddle';
+
+describe('wasm-fiddle', () => {
+  describe('extractControlNames', () => {
+    it('finds all sliders across multiple lines', () => {
+      const code = `
+#slider1:Foo
+#slider2:Bar
+      `;
+      assert.deepEqual([undefined, 'Foo', 'Bar'], extractControlNames(sliderRegex, code));
+    });
+
+    it('finds all sliders on the same line', () => {
+      const code = '  #slider1:Foo #slider2:Bar    ';
+      assert.deepEqual([undefined, 'Foo', 'Bar'], extractControlNames(sliderRegex, code));
+    });
+
+    it('does not crash on empty string', () => {
+      const code = '';
+      assert.deepEqual([], extractControlNames(sliderRegex, code));
+    });
+
+    it('finds all sliders in comments.', () => {
+      const code = `  // Comment
+  // #slider0:strokeWidth #color0:dashColor
+  // #slider1:Bar #color1:Foo
+    `;
+      assert.deepEqual(['strokeWidth', 'Bar'], extractControlNames(sliderRegex, code), 'sliders');
+      assert.deepEqual(['dashColor', 'Foo'], extractControlNames(colorPickerRegex, code), 'color pickers');
+    });
+  });
+});
