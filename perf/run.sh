05faeff047c6fa4e7acecc37b04855000fdc03cb
Joe Gregorio <jcgregorio@google.com>
2021-05-25 15:36:17 -0400

    [samplevariance] Perf analysis of sample variance.
    Change-Id: I42b6fee254a4218728c99cfac5ef7331eb55398e

---
+++ perf/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -x
+
+echo "traceid,mean,median,min,max,ratio"  > ./variance.csv
+
+declare -a files
+readarray -t files <<< $(gsutil ls "gs://skia-perf/nano-json-v1/2021/05/24/**")
+
+
+for i in "${files[@]}"
+do
+	echo $i
+    gsutil cat $i | gunzip | samplevariance >> ./variance.csv
+done
+
+mlr --icsv --ocsv sort -nr ratio variance.csv > sorted.csv
+head --lines=100 sorted.csv > short-sorted.csv
