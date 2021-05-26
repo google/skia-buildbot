4c6d49fa156f692e53d5c5cac379fd12ec92b528
Joe Gregorio <jcgregorio@google.com>
2021-05-26 09:56:31 -0400

    [samplevariance] Perf analysis of sample variance.
    Change-Id: I42b6fee254a4218728c99cfac5ef7331eb55398e

---
+++ perf/samplevariance/main.go
@@ -0,0 +1,43 @@
+// Take as input from stdin a single Perf JSON file and emits a single CSV line
+// per trace with the trace name, the mean, the min, the max, and the ratio of
+// median/min to stdout.
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+	"sort"
+
+	"github.com/aclements/go-moremath/stats"
+	"go.skia.org/infra/perf/go/ingest/format"
+	"go.skia.org/infra/perf/go/ingest/parser"
+)
+
+func main() {
+	benchData, err := format.ParseLegacyFormat(os.Stdin)
+	if err != nil {
+		log.Fatal(err)
+	}
+	w := csv.NewWriter(os.Stdout)
+	for traceid, samples := range parser.GetSamplesFromLegacyFormat(benchData) {
+		// Filter for the types of traces we are interested in.
+		if samples.Params["source_type"] != "skp" || samples.Params["sub_result"] != "min_ms" {
+			continue
+		}
+		sort.Float64s(samples.Values)
+		values := stats.Sample{Xs: samples.Values}
+		medianFloat := values.Quantile(0.5)
+		median := fmt.Sprintf("%f", medianFloat)
+		mean := fmt.Sprintf("%f", stats.Mean(samples.Values))
+		min := fmt.Sprintf("%f", samples.Values[0])
+		max := fmt.Sprintf("%f", samples.Values[len(samples.Values)-1])
+		ratio := fmt.Sprintf("%f", medianFloat/samples.Values[0])
+		w.Write([]string{
+			// If these columns get changed also update run.sh.
+			traceid, mean, median, min, max, ratio,
+		})
+	}
+	w.Flush()
+}
