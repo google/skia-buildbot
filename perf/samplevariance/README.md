64ad1e24131011cb13f23ceeb13b4653aa1437d7
Joe Gregorio <jcgregorio@google.com>
2021-05-26 09:52:09 -0400

    [samplevariance] Perf analysis of sample variance.
    A Go program and a bash script for analyzing sample variance of nanobench
    results stored in Google Cloud Storage (GCS). The nanobench program usually
    stores 10 samples from each run of a benchmark, and this tool allows looking at
    those samples across a wide number of runs stored on GCS.
    
    Change-Id: I42b6fee254a4218728c99cfac5ef7331eb55398e

---
+++ perf/samplevariance/README.md
@@ -0,0 +1,38 @@
+# SampleVariance
+
+A Go program and a bash script for analyzing sample variance of nanobench
+results stored in Google Cloud Storage (GCS). The nanobench program usually
+stores 10 samples from each run of a benchmark, and this tool allows looking at
+those samples across a wide number of runs stored on GCS.
+
+## Prerequisites
+
+- Read-only access to the GCS bucket.
+- The following installed:
+
+  - Go
+  - gsutil
+  - miller
+
+You can usually install miller via:
+
+    sudo apt install miller
+
+## Running
+
+Modify `run.sh` to list the files of interest, you can use wildcards in the
+gsutil command when specifying files, for example, to process all files of May
+24, 2021 you would use:
+
+    gsutil ls "gs://skia-perf/nano-json-v1/2021/05/24/**"
+
+Then:
+
+    $ ./run.sh
+
+The output will CSV files placed in `/tmp/variance-XXXX/`:
+
+- `variance.csv` - The full set of data.
+- `sorted.csv` - Which is the full set of data sorted in numerical descending
+  order of $ratio.
+- `short-sorted.csv` - The first 100 entries of `sorted.csv`.
---
+++ perf/samplevariance/run.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e -x
+
+# Compile the Go program.
+go install .
+
+# Store all the output in a temporary directory.
+DIR=`mktemp -d /tmp/variance-XXXX`
+
+echo "Output directory: ${DIR}"
+
+# We are going to run a single process per file, create a directory to store all
+# those individual files.
+INDIVIDUAL_FILES_DIR="${DIR}/parallel"
+mkdir ${INDIVIDUAL_FILES_DIR}
+
+# Loop over each file and extract data as lines of CSV.
+gsutil ls "gs://skia-perf/nano-json-v1/2021/05/25/**" | xargs -P 64 -I{} ./single_step.sh {} ${INDIVIDUAL_FILES_DIR}
+
+# Add column headers.
+echo "traceid,mean,median,min,max,ratio"  > ${DIR}/variance.csv
+
+# Concatenate all the individual files into a single CSV file.
+cat ${INDIVIDUAL_FILES_DIR}/* >> ${DIR}/variance.csv
+
+# Sort the data based on the ratio column in numerical descending order.
+mlr --icsv --ocsv sort -nr ratio ${DIR}/variance.csv > ${DIR}/sorted.csv
+
+# Trim out the top 100 lines from the sorted file, cause Google Sheets can't handle large files.
+head --lines=100 ${DIR}/sorted.csv > ${DIR}/short-sorted.csv
+
+# Print the full name of the most useful file we've generated.
+echo ${DIR}/short-sorted.csv
