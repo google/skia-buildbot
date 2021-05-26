5830ef9758b4e568860e97f4cfe25a526dc27540
Joe Gregorio <jcgregorio@google.com>
2021-05-26 08:29:56 -0400

    [jsfiddle] Add puppeteer captures of jsfiddle pages.
    Bug: skia:12030
    Change-Id: Ib04df470b5a1a743b7fde6a1a2f153c4da40fa66

---
+++ jsfiddle/modules/canvaskit-fiddle/canvaskit-fiddle_puppeteer_test.ts
@@ -0,0 +1,31 @@
+import * as path from 'path';
+import { expect } from 'chai';
+import {
+  loadCachedTestBed,
+  takeScreenshot,
+  TestBed,
+} from '../../../puppeteer-tests/util';
+
+describe('canvaskit-fiddle', () => {
+  let testBed: TestBed;
+  before(async () => {
+    testBed = await loadCachedTestBed(
+      path.join(__dirname, '..', '..', 'webpack.config.ts'),
+    );
+  });
+
+  beforeEach(async () => {
+    await testBed.page.goto(`${testBed.baseUrl}/dist/canvaskit-fiddle.html`);
+    await testBed.page.setViewport({ width: 1600, height: 800 });
+  });
+
+  it('should render the demo page (smoke test)', async () => {
+    expect(await testBed.page.$$('canvaskit-fiddle')).to.have.length(1);
+  });
+
+  describe('screenshots', () => {
+    it('shows the default view', async () => {
+      await takeScreenshot(testBed.page, 'jsfiddle', 'canvaskit-fiddle');
+    });
+  });
+});
