dfcbaabf72f2a465fd7681728ba001a12cf92539
Leandro Lovisolo <lovisolo@google.com>
2021-06-01 16:48:15 -0400

    [gold] Port cluster-digests-sk to TypeScript.
    Bug: skia:10246
    Change-Id: I41e8f14711264e941d152ba162e9e9ba5103a4f1

--- golden/modules/cluster-digests-sk/cluster-digests-sk-demo.js
+++
@@ -1,7 +0,0 @@
-import './index';
-import { $$ } from 'common-sk/modules/dom';
-import { clusterDiffJSON } from '../cluster-page-sk/test_data';
-
-const ele = document.createElement('cluster-digests-sk');
-ele.setData(clusterDiffJSON.nodes, clusterDiffJSON.links);
-$$('#cluster').appendChild(ele);
--- golden/modules/cluster-digests-sk/cluster-digests-sk.ts
+++ golden/modules/cluster-digests-sk/cluster-digests-sk.ts
@@ -19,30 +19,43 @@
  */
 import { define } from 'elements-sk/define';
 import { html } from 'lit-html';
-import { $$ } from 'common-sk/modules/dom';
 import * as d3Force from 'd3-force';
 import * as d3Select from 'd3-selection';
 import { ElementSk } from '../../../infra-sk/modules/ElementSk';
+import {ClusterDiffLink, ClusterDiffNode, Digest} from '../rpc_types';
+import {SimulationLinkDatum, SimulationNodeDatum} from 'd3-force';
 
-const template = (ele) => html`
-<svg width=${ele._width} height=${ele._height}></svg>
-`;
+/**
+ * A node returned by the Gold backend, with an optional label added by client-side code.
+ *
+ * If present, the label will be rendered in the cluster view, next to the node circle.
+ */
+export interface ClusterDiffNodeWithLabel extends ClusterDiffNode {
+  label?: string;
+}
 
-define('cluster-digests-sk', class extends ElementSk {
+type SimNode = ClusterDiffNodeWithLabel & SimulationNodeDatum;
+type SimLink = ClusterDiffLink & SimulationLinkDatum<SimNode>;
+
+export class ClusterDigestsSk extends ElementSk {
+  private static template = (ele: ClusterDigestsSk) => html`
+    <svg width=${ele.width} height=${ele.height}></svg>
+  `;
+
+  private nodes: SimNode[] = [];
+  private links: SimLink[] = [];
+
+  private linkTightness = 1 / 8;
+  private nodeRepulsion = 256;
+
+  private width = 400;
+  private height = 400;
+
+  // An array of digests (strings) that correspond to the currently selected digests (if any).
+  private selectedDigests: Digest[] = [];
+
   constructor() {
-    super(template);
-
-    this._nodes = [];
-    this._links = [];
-
-    this._linkTightness = 1 / 8;
-    this._nodeRepulsion = 256;
-
-    this._width = 400;
-    this._height = 400;
-
-    // An array of digests (strings) that correspond to the currently selected digests (if any).
-    this._selectedDigests = [];
+    super(ClusterDigestsSk.template);
   }
 
   connectedCallback() {
@@ -50,22 +63,22 @@ define('cluster-digests-sk', class extends ElementSk {
     this._render();
   }
 
-  changeLinkTightness(isScaleUp) {
+  changeLinkTightness(isScaleUp: boolean) {
     if (isScaleUp) {
-      this._linkTightness *= 1.5;
+      this.linkTightness *= 1.5;
     } else {
-      this._linkTightness /= 1.5;
+      this.linkTightness /= 1.5;
     }
-    this._layout();
+    this.layout();
   }
 
-  changeNodeRepulsion(isScaleUp) {
+  changeNodeRepulsion(isScaleUp: boolean) {
     if (isScaleUp) {
-      this._nodeRepulsion *= 1.5;
+      this.nodeRepulsion *= 1.5;
     } else {
-      this._nodeRepulsion /= 1.5;
+      this.nodeRepulsion /= 1.5;
     }
-    this._layout();
+    this.layout();
   }
 
   /**
@@ -73,14 +86,14 @@ define('cluster-digests-sk', class extends ElementSk {
    * elements (e.g. circles, lines) to already be created; this function will simply update the
    * X and Y values accordingly.
    */
-  _layout() {
-    const clusterSk = $$('svg', this);
+  private layout() {
+    const clusterSk = this.querySelector('svg')!;
 
     // This force acts as a repulsion force between digest nodes. This acts a lot like charged
     // particles repelling one another. The main purpose here is to keep nodes from overlapping.
     // See https://github.com/d3/d3-force#forceManyBody
     const chargeForce = d3Force.forceManyBody()
-      .strength(-this._nodeRepulsion)
+      .strength(-this.nodeRepulsion)
       // Given our nodes have a radius of 12, if two nodes are 60 pixels apart, they are definitely
       // not overlapping, so we can stop counting their "charge". This should help performance by
       // reducing computation needs.
@@ -89,23 +102,23 @@ define('cluster-digests-sk', class extends ElementSk {
     // This force acts as a spring force between digest nodes. More similar digests pull more
     // tightly and should be closer together.
     // See https://github.com/d3/d3-force#links
-    const linkForce = d3Force.forceLink(this._links)
-      .distance((d) => d.value / this._linkTightness);
+    const linkForce = d3Force.forceLink(this.links)
+      .distance((d) => d.value / this.linkTightness);
 
     // This force keeps the diagram centered in the SVG.
     // See https://github.com/d3/d3-force#centering
-    const centerForce = d3Force.forceCenter(this._width / 2, this._height / 2);
+    const centerForce = d3Force.forceCenter(this.width / 2, this.height / 2);
 
     // These forces help keep the nodes in the visible area.
-    const xForce = d3Force.forceX(this._width / 2);
+    const xForce = d3Force.forceX(this.width / 2);
     xForce.strength(0.1);
-    const yForce = d3Force.forceY(this._height / 2);
+    const yForce = d3Force.forceY(this.height / 2);
     yForce.strength(0.2); // slightly stronger force down since we have more width to draw into
 
     // This starts a simulation that will render over the next few seconds as the nodes are
     // simulated into place.
     // See https://github.com/d3/d3-force#forceSimulation
-    d3Force.forceSimulation(this._nodes)
+    d3Force.forceSimulation(this.nodes)
       .force('charge', chargeForce) // The names are arbitrary (and inspired by D3 documentation).
       .force('link', linkForce)
       .force('center', centerForce)
@@ -116,56 +129,54 @@ define('cluster-digests-sk', class extends ElementSk {
         // On each tick, the simulation will update the x,y values of the nodes. We can then
         // select and update those nodes.
         d3Select.select(clusterSk)
-          .selectAll('.node')
-          .attr('cx', (d) => d.x)
-          .attr('cy', (d) => d.y);
+          .selectAll<SVGCircleElement, SimNode>('.node')
+          .attr('cx', (d) => d.x!)
+          .attr('cy', (d) => d.y!);
 
         d3Select.select(clusterSk)
-          .selectAll('.label')
-          .attr('x', (d) => d.x + 14) // offset the labels from the center of the nodes.
-          .attr('y', (d) => d.y + 20);
+          .selectAll<SVGTextElement, SimNode>('.label')
+          .attr('x', (d) => d.x! + 14) // offset the labels from the center of the nodes.
+          .attr('y', (d) => d.y! + 20);
+
+        // Type guard to narrow down the type of the source and target fields in a SimLink. See
+        // https://github.com/DefinitelyTyped/DefinitelyTyped/blob/9bd58256d08405c4e0ee3b065efc984f9a28ca17/types/d3-force/d3-force-tests.ts#L647.
+        function isSimNode(maybeNode: SimNode | string | number): maybeNode is SimNode {
+          return typeof maybeNode !== 'string' && typeof maybeNode !== 'number';
+        }
 
         // source and target are supplied and updated by forceLink:
         // https://github.com/d3/d3-force#link_links
         d3Select.select(clusterSk)
-          .selectAll('.link')
-          .attr('x1', (d) => d.source.x)
-          .attr('y1', (d) => d.source.y)
-          .attr('x2', (d) => d.target.x)
-          .attr('y2', (d) => d.target.y);
+          .selectAll<SVGLineElement, SimLink>('.link')
+          .attr('x1', (d) => isSimNode(d.source) ? d.source.x! : 0)
+          .attr('y1', (d) => isSimNode(d.source) ? d.source.y! : 0)
+          .attr('x2', (d) => isSimNode(d.target) ? d.target.x! : 0)
+          .attr('y2', (d) => isSimNode(d.target) ? d.target.y! : 0);
       })
       .on('end', () => {
         this.dispatchEvent(new CustomEvent('layout-complete', { bubbles: true }));
       });
   }
 
-  _getNodeCSSClass(d) {
+  private getNodeCSSClass(d: SimNode) {
     let base = `node ${d.status}`;
-    if (this._selectedDigests.indexOf(d.name) >= 0) {
+    if (this.selectedDigests.includes(d.name)) {
       base += ' selected';
     }
     return base;
   }
 
-  /**
-   * Sets the new data to render in a cluster view.
-   *
-   * @param nodes Array<Object>: contains Strings for keys digest (called name for historical
-   *   reasons), status, and label.
-   * @param links Array<Object>: contains Numbers for keys source, target, and value. source and
-   *   target refer to the index of the nodes array. value represents how far apart those two
-   *   nodes should be.
-   */
-  setData(nodes, links) {
-    this._nodes = nodes;
-    this._links = links;
+  /** Sets the new data to render in a cluster view. */
+  setData(nodes: ClusterDiffNodeWithLabel[], links: ClusterDiffLink[]) {
+    this.nodes = nodes;
+    this.links = links;
 
     this._render();
     // For reasons unknown, after render, we don't always see the SVG element rendered in our
     // DOM, so we schedule the drawing for the next animation frame (when we *do* see the SVG
     // in the DOM).
     window.requestAnimationFrame(() => {
-      const clusterSk = $$('svg', this);
+      const clusterSk = this.querySelector('svg')!;
 
       // Delete existing SVG elements
       d3Select.select(clusterSk)
@@ -173,7 +184,7 @@ define('cluster-digests-sk', class extends ElementSk {
         .remove();
 
       // Reset selection.
-      this._selectedDigests = [];
+      this.selectedDigests = [];
 
       // We don't have any lines or dots spawn in or dynamically get removed from the drawing, so
       // we don't need to supply an id function to the data calls below.
@@ -181,7 +192,7 @@ define('cluster-digests-sk', class extends ElementSk {
       // Draw the lines first so they are behind the circles.
       d3Select.select(clusterSk)
         .selectAll('line.link')
-        .data(this._links)
+        .data(this.links)
         .enter()
         .append('line')
         .attr('class', 'link')
@@ -191,20 +202,21 @@ define('cluster-digests-sk', class extends ElementSk {
       // Draw the labels behind the circles because the circles are clickable.
       d3Select.select(clusterSk)
         .selectAll('text.label')
-        .data(this._nodes)
+        .data(this.nodes)
         .enter()
         .append('text')
         .attr('class', 'label');
       d3Select.select(clusterSk) // update all nodes with the correct label.
-        .selectAll('text.label')
+        .selectAll<SVGTextElement, SimNode>('text.label')
         .text((d) => d.label || '');
 
+      // Draw a circle for each node.
       d3Select.select(clusterSk)
         .selectAll('circle.node')
-        .data(this._nodes)
+        .data(this.nodes)
         .enter()
         .append('circle')
-        .attr('class', (d) => this._getNodeCSSClass(d))
+        .attr('class', (d) => this.getNodeCSSClass(d))
         .attr('r', 12)
         .attr('stroke', 'black')
         .attr('data-digest', (d) => d.name)
@@ -215,17 +227,17 @@ define('cluster-digests-sk', class extends ElementSk {
           evt.stopPropagation();
 
           const digest = d.name;
-          if (this._selectedDigests.indexOf(digest) >= 0) {
+          if (this.selectedDigests.includes(digest)) {
             return; // It's already selected, do nothing.
           }
           if (evt.shiftKey || evt.ctrlKey || evt.metaKey) {
             // Support multiselection if shift, control or meta is held.
-            this._selectedDigests.push(digest);
+            this.selectedDigests.push(digest);
           } else {
             // Clear the existing selection and replace it with this digest.
-            this._selectedDigests = [digest];
+            this.selectedDigests = [digest];
           }
-          this._updateSelection();
+          this.updateSelection();
         });
 
       d3Select.select(clusterSk).on('click tap', () => {
@@ -234,32 +246,34 @@ define('cluster-digests-sk', class extends ElementSk {
         evt.preventDefault();
         evt.stopPropagation();
 
-        this._selectedDigests = [];
-        this._updateSelection();
+        this.selectedDigests = [];
+        this.updateSelection();
       });
 
-      this._layout();
+      this.layout();
     });
   }
 
-  setWidth(w) {
-    if (w === this._width) {
+  setWidth(w: number) {
+    if (w === this.width) {
       // Don't need to re-render if the width is unchanged.
       return;
     }
-    this._width = w;
-    this._layout();
+    this.width = w;
+    this.layout();
   }
 
-  _updateSelection() {
-    d3Select.select($$('svg', this))
+  private updateSelection() {
+    d3Select.select(this.querySelector('svg'))
       .selectAll('circle.node')
-      .data(this._nodes)
-      .attr('class', (d) => this._getNodeCSSClass(d));
+      .data(this.nodes)
+      .attr('class', (d) => this.getNodeCSSClass(d));
 
-    this.dispatchEvent(new CustomEvent('selection-changed', {
+    this.dispatchEvent(new CustomEvent<Digest[]>('selection-changed', {
       bubbles: true,
-      detail: this._selectedDigests,
+      detail: this.selectedDigests,
     }));
   }
-});
+}
+
+define('cluster-digests-sk', ClusterDigestsSk);
