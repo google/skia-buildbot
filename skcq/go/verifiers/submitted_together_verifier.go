3b414266d5dea2631e8c65f491239ba4d836f7bb
Ravi Mistry <rmistry@google.com>
2021-06-02 12:57:47 -0400

    Prototype
    Change-Id: I0a21797b51df3b89b578e47fea0e3cc6156bc02a

---
+++ skcq/go/verifiers/submitted_together_verifier.go
@@ -0,0 +1,61 @@
+package verifiers
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"strings"
+
+	"go.skia.org/infra/go/gerrit"
+	"go.skia.org/infra/go/skerr"
+	"go.skia.org/infra/skcq/go/codereview"
+	"go.skia.org/infra/skcq/go/config"
+)
+
+func NewSubmittedTogetherVerifier(ctx context.Context, togetherChanges []*gerrit.ChangeInfo, httpClient *http.Client, cfg *config.SkCQCfg, cr codereview.CodeReview, ci *gerrit.ChangeInfo) (Verifier, error) {
+	togetherChangesToVerifiers := map[*gerrit.ChangeInfo][]Verifier{}
+	for _, tc := range togetherChanges {
+		tcVerifiers, err := GetVerifiers(ctx, httpClient, cfg, cr, tc, true /* isSubmittedTogetherChange */)
+		if err != nil {
+			return nil, skerr.Fmt("Could not get verifiers for the together change %d: %s", tc.Issue, err)
+		}
+		togetherChangesToVerifiers[tc] = tcVerifiers
+	}
+	return &SubmittedTogetherVerifier{
+		togetherChangesToVerifiers: togetherChangesToVerifiers,
+	}, nil
+}
+
+type SubmittedTogetherVerifier struct {
+	togetherChangesToVerifiers map[*gerrit.ChangeInfo][]Verifier
+}
+
+func (stv *SubmittedTogetherVerifier) Name() string {
+	return "[SubmittedTogetherVerifier]"
+}
+
+func (stv *SubmittedTogetherVerifier) Verify(ctx context.Context, ci *gerrit.ChangeInfo) (state VerifierState, reason string, err error) {
+	successMsgsFromVerfiers := []string{}
+	// Loop through all verifiers of all together changes and run them.
+	for ts, tsVerifiers := range stv.togetherChangesToVerifiers {
+		sm, wm, rm := RunVerifiers(ctx, ts, tsVerifiers)
+		if len(rm) > 0 {
+			return FailureState, fmt.Sprintf("%s Submitted together change https://skia-review.googlesource.com/c/%d has failed verifiers: %s", stv.Name(), ts.Issue, strings.Join(rm, "\n")), nil
+		} else if len(wm) > 0 {
+			return WaitingState, fmt.Sprintf("%s Submitted together change %d is waiting for verifiers: %s", stv.Name(), ts.Issue, strings.Join(wm, "\n")), nil
+		} else {
+			successMsgsFromVerfiers = append(successMsgsFromVerfiers, sm...)
+		}
+	}
+	return SuccessState, fmt.Sprintf("%s Successfully ran all verifiers of submitted together change: %s", stv.Name(), strings.Join(successMsgsFromVerfiers, "\n")), nil
+}
+
+func (stv *SubmittedTogetherVerifier) Cleanup(ctx context.Context, ci *gerrit.ChangeInfo) {
+	// Loop through all verifiers of all together changes and run cleanup on them.
+	for ts, tsVerifiers := range stv.togetherChangesToVerifiers {
+		for _, tsVerifier := range tsVerifiers {
+			tsVerifier.Cleanup(ctx, ts)
+		}
+	}
+	return
+}
---
+++ skcq/go/verifiers/tryjobs_verifier.go
@@ -0,0 +1,57 @@
+package verifiers
+
+// Notes:
+// Will have to add ability to trigger builds in go/buildbucket
+// Also look at task_scheduler/go/tryjobs
+// and buildbucket_util.py in cq
+// and verification/tryjob.py in cq
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"go.skia.org/infra/go/buildbucket"
+	"go.skia.org/infra/go/gerrit"
+	"go.skia.org/infra/task_scheduler/go/specs"
+)
+
+func NewTryJobsVerifier(httpClient *http.Client) (Verifier, error) {
+
+	// Get cqbots from tasks.json (or from the change...)
+
+	return &TryJobsVerifier{
+		bb2: buildbucket.NewClient(httpClient),
+	}, nil
+}
+
+type TryJobsVerifier struct {
+	// Maybe will not need this??
+	// bb: *buildbucket_Api.Service
+
+	bb2 buildbucket.BuildBucketInterface
+}
+
+func (av *TryJobsVerifier) Name() string {
+	return "[TryJobsVerifier]"
+}
+
+func (av *TryJobsVerifier) Verify(ctx context.Context, ci *gerrit.ChangeInfo) (state VerifierState, reason string, err error) {
+
+	// Get list of all running try jobs in buildbucket - _fetch_buildset_try_jobs_v1 ?? in go/buildbucket/common there is GetTrybotsForCLPredicate
+	// If they do not match the CQ list then manually trigger them.
+
+	// patchsetIDs := ci.GetPatchsetIDs()
+	// latestPatchsetID := patchsetIDs[len(patchsetIDs)-1]
+	// av.bb2.GetTrybotsForCL(ctx, ci.Issue, ci.GetLa)
+
+	// Trigger them (using experimental stuff) and the keywords in the Footers!
+
+	// Do not check for success/failure of experimental builds..
+
+	return FailureState, fmt.Sprintf("%s This CL somethingsomethingsomething", av.Name()), nil
+}
+
+func (cv *TryJobsVerifier) Cleanup(ctx context.Context, ci *gerrit.ChangeInfo) {
+	return
+}
