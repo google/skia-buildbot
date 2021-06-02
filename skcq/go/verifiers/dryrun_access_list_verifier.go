9004ea51977c86dfd139b5375427895111299bec
Ravi Mistry <rmistry@google.com>
2021-06-02 10:45:36 -0400

    Prototype
    Change-Id: I0a21797b51df3b89b578e47fea0e3cc6156bc02a

---
+++ skcq/go/verifiers/dryrun_access_list_verifier.go
@@ -0,0 +1,44 @@
+package verifiers
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"strings"
+
+	"go.skia.org/infra/go/allowed"
+	"go.skia.org/infra/go/gerrit"
+	"go.skia.org/infra/go/skerr"
+)
+
+func NewDryRunAccessListVerifier(httpClient *http.Client, criaGroup string) (Verifier, error) {
+	dryRunAllowed, err := allowed.NewAllowedFromChromeInfraAuth(httpClient, criaGroup)
+	if err != nil {
+		return nil, skerr.Fmt("Could not create an allowed from %s: %s", criaGroup, err)
+	}
+	return &DryRunAccessListVerifier{
+		criaGroupName: criaGroup,
+		dryRunAllowed: dryRunAllowed,
+	}, nil
+}
+
+type DryRunAccessListVerifier struct {
+	criaGroupName string
+	dryRunAllowed *allowed.AllowedFromChromeInfraAuth
+}
+
+func (dv *DryRunAccessListVerifier) Name() string {
+	return "[DryRunAccessListVerifier]"
+}
+
+func (dv *DryRunAccessListVerifier) Verify(ctx context.Context, ci *gerrit.ChangeInfo) (state VerifierState, reason string, err error) {
+	allowedDryRunVoters := GetAllowedVoters(ci, dv.dryRunAllowed, gerrit.LabelCommitQueue, gerrit.LabelCommitQueueDryRun)
+	if len(allowedDryRunVoters) > 0 {
+		return SuccessState, fmt.Sprintf("%s CQ+1 voted by allowed dry-run voters %s", dv.Name(), strings.Join(allowedDryRunVoters, ",")), nil
+	}
+	return FailureState, fmt.Sprintf("%s CQ+1 requires a vote by an allowed dry-run voter", dv.Name()), nil
+}
+
+func (dv *DryRunAccessListVerifier) Cleanup(ctx context.Context, ci *gerrit.ChangeInfo) {
+	return
+}
