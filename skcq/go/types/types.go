273d745baa1c0254d8973e54e7df209cbbcf3ec4
Ravi Mistry <rmistry@google.com>
2021-06-10 09:17:10 -0400

    Prototype
    Change-Id: I0a21797b51df3b89b578e47fea0e3cc6156bc02a

---
+++ skcq/go/types/types.go
@@ -0,0 +1,46 @@
+package types
+
+// StatusData is used in the response of the get_client_counts endpoint.
+type StatusData struct {
+	UntriagedCount int    `json:"untriaged_count"`
+	Link           string `json:"link"`
+}
+
+type CQRecord struct {
+	ID int64 `json:"id"`
+	// The time the CQ first looked at this change.
+	// Uses unix epoch time.
+	StartTime int64 `json:"start_time"`
+	// Whether this change is from an internal repo.
+	Internal bool `json:"internal"`
+}
+
+type ChangeAttempts struct {
+	Attempts []*ChangeAttempt `json:"attempts"`
+}
+
+type ChangeAttempt struct {
+	ID                 string            `json:"id"`
+	DryRun             bool              `json:"dry_run"`
+	RepoBranch         string            `json:"repo_branch"`
+	PatchStart         int64             `json:"created"`
+	PatchStop          int64             `json:"stop"`
+	PatchCommitted     int64             `json:"committed"`
+	SubmittableChanges []string          `json:"submittable_changes"`
+	VerifiersStatuses  []*VerifierStatus `json:"verifiers_status"`
+	CQAbandoned        bool              `json:"cq_abandoned"`
+}
+
+type VerifierState string
+
+const VerifierSuccessState VerifierState = "SUCCESSFUL"
+const VerifierWaitingState VerifierState = "WAITTING"
+const VerifierFailureState VerifierState = "FAILURE"
+
+type VerifierStatus struct {
+	Name   string        `json:"name"`
+	Start  int64         `json:"start"`
+	Stop   int64         `json:"stop"`
+	Reason string        `json:"reason"`
+	State  VerifierState `json:"state"`
+}
