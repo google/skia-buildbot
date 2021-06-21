e6f4c57f7ae590eb0e556aa295088293ea3ad89e
Joe Gregorio <jcgregorio@google.com>
2021-06-21 15:04:25 -0400

    [switchboard] Handle graceful shutdown in switch-pod-monitor.
    The switch-pod has a default grace period of one hour:
    
      https://skia.googlesource.com/k8s-config/+/refs/heads/main/skia-switchboard/switch-pod.yaml#33
    
    But we can exit earlier if all the MeetingPoints for that
    pod have been removed.
    
    Bug: skia:12063
    Change-Id: I9bba3d0ae49dea1d0422ca7e9431123695703fa5

--- switchboard/go/switch-pod-monitor/main.go
+++ switchboard/go/switch-pod-monitor/main.go
@@ -91,6 +91,9 @@ func connectToSwitchboardAndWait(ctx context.Context, hostname string, switchboa
 
 	consecutiveFailures := 0
 
+	// Are we in the graceful shutdown phase of the pod lifecycle?
+	gracefulShutdown := false
+
 	for {
 		select {
 		case <-ctx.Done():
@@ -100,20 +103,41 @@ func connectToSwitchboardAndWait(ctx context.Context, hostname string, switchboa
 			if err := switchboardInstance.RemovePod(ctx, hostname); err != nil {
 				sklog.Error(err)
 			}
+			gracefulShutdown = true
 			if sig == syscall.SIGINT {
 				sklog.Info("Exiting on SIGINT.")
 				os.Exit(0)
 			}
 		// Periodically call KeepAlivePod so we know the pod is still running.
 		case <-time.Tick(keepAliveDuration):
-			if err := switchboardInstance.KeepAlivePod(ctx, hostname); err != nil {
-				consecutiveFailures++
-				sklog.Errorf("Failed to keep pod alive: %s", err)
-				if consecutiveFailures >= maxConsecutiveKeepAliveErrors {
-					if err := switchboardInstance.RemovePod(ctx, hostname); err != nil {
-						sklog.Error(err)
+			if gracefulShutdown {
+				// Check to see if there are any meeting points left in this
+				// pod. If there are none then exit.
+				meetingPoints, err := switchboardInstance.ListMeetingPoints(ctx)
+				if err != nil {
+					sklog.Errorf("switchboard failed to list meeting points: %s", err)
+					continue
+				}
+				count := 0
+				for _, mp := range meetingPoints {
+					if mp.PodName == hostname {
+						count++
 					}
-					return skerr.Wrapf(err, "Exiting: Switchpod.KeepAlivePod failed %d consecutive times", consecutiveFailures)
+				}
+				if count == 0 {
+					return nil
+				}
+			} else {
+				err := switchboardInstance.KeepAlivePod(ctx, hostname)
+				if err != nil {
+					consecutiveFailures++
+					sklog.Errorf("Failed to keep pod alive: %s", err)
+					if consecutiveFailures >= maxConsecutiveKeepAliveErrors {
+						if err := switchboardInstance.RemovePod(ctx, hostname); err != nil {
+							sklog.Error(err)
+						}
+						return skerr.Wrapf(err, "Exiting: Switchpod.KeepAlivePod failed %d consecutive times", consecutiveFailures)
+					}
 				}
 			}
 			consecutiveFailures = 0
