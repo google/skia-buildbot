79fdd57f0532567ce3b85981e74bd2e6bcb3374a
Joe Gregorio <jcgregorio@google.com>
2021-06-21 11:22:54 -0400

    Mark TestWatch_StartWatchBeforeMachineExists as flaky.
    Change-Id: I6f8aecb71738ce1e0cf8312c0301419fe1ea8952

---
+++ machine/go/machine/store/impl_manual_test.go
@@ -0,0 +1,39 @@
+package store
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.skia.org/infra/go/testutils/unittest"
+	"go.skia.org/infra/machine/go/machine"
+)
+
+func TestWatch_StartWatchBeforeMachineExists(t *testing.T) {
+	unittest.ManualTest(t)
+	ctx, cfg := setupForFlakyTest(t)
+	store, err := New(ctx, true, cfg)
+	require.NoError(t, err)
+
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	// First add the watch.
+	ch := store.Watch(ctx, "skia-rpi2-rack2-shelf1-001")
+
+	// Then create the document.
+	err = store.Update(ctx, "skia-rpi2-rack2-shelf1-001", func(previous machine.Description) machine.Description {
+		ret := previous.Copy()
+		ret.Mode = machine.ModeMaintenance
+		return ret
+	})
+	require.NoError(t, err)
+
+	// Wait for first description.
+	m := <-ch
+	assert.Equal(t, machine.ModeMaintenance, m.Mode)
+	assert.Equal(t, int64(1), store.watchReceiveSnapshotCounter.Get())
+	assert.Equal(t, int64(0), store.watchDataToErrorCounter.Get())
+	assert.NoError(t, store.firestoreClient.Close())
+}
