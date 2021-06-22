9fde6a4814893c444e9e2b0df85b983905bb0745
Kevin Lubick <kjlubick@google.com>
2021-06-22 14:19:14 -0400

    [gold] Remove 'at head' option from list page
    I posit that this is not commonly used and would be expensive/annoying
    to implement in the SQL transition.
    
    We will only show information at head (the current default).
    
    Change-Id: I1438b6ada36c1eb767524bd226fadc186c7be09c

--- golden/modules/list-page-sk/list-page-sk.ts
+++ golden/modules/list-page-sk/list-page-sk.ts
@@ -49,8 +49,6 @@ export class ListPageSk extends ElementSk {
           <tune-icon-sk></tune-icon-sk>
         </button>
         <pre>${searchQuery(ele.currentCorpus, ele.currentQuery)}</pre>
-        <checkbox-sk label="Digests at Head Only" class=head_only
-                 ?checked=${!ele.showAllDigests} @click=${ele.toggleAllDigests}></checkbox-sk>
         <checkbox-sk label="Disregard Ignore Rules" class=ignore_rules
                  ?checked=${ele.disregardIgnoreRules} @click=${ele.toggleIgnoreRules}></checkbox-sk>
       </div>
@@ -96,8 +94,8 @@ export class ListPageSk extends ElementSk {
           includePositiveDigests: opts.positive,
           includeNegativeDigests: opts.negative,
           includeUntriagedDigests: opts.untriaged,
-          includeDigestsNotAtHead: ele.showAllDigests,
           includeIgnoredDigests: ele.disregardIgnoreRules,
+          includeDigestsNotAtHead: false,
         });
 
     const searchPageHref = (opts: MakeSearchCriteriaOpts) => {
@@ -111,7 +109,8 @@ export class ListPageSk extends ElementSk {
         ...makeSearchCriteria({
           positive: true,
           negative: true,
-          untriaged: true
+          untriaged: true,
+
         }),
         left_filter: '',
         grouping: row.name,
@@ -166,7 +165,6 @@ export class ListPageSk extends ElementSk {
   private currentQuery = '';
   private currentCorpus = '';
 
-  private showAllDigests = false;
   private disregardIgnoreRules = false;
 
   private byTestCounts: TestSummary[] = [];
@@ -182,7 +180,6 @@ export class ListPageSk extends ElementSk {
     this.stateChanged = stateReflector(
         /* getState */() => ({
           // provide empty values
-          all_digests: this.showAllDigests,
           disregard_ignores: this.disregardIgnoreRules,
           corpus: this.currentCorpus,
           query: this.currentQuery,
@@ -191,7 +188,6 @@ export class ListPageSk extends ElementSk {
             return;
           }
           // default values if not specified.
-          this.showAllDigests = newState.all_digests as boolean || false;
           this.disregardIgnoreRules = newState.disregard_ignores as boolean || false;
           this.currentCorpus = newState.corpus as string || defaultCorpus();
           this.currentQuery = newState.query as string || '';
@@ -239,9 +235,6 @@ export class ListPageSk extends ElementSk {
     sendBeginTask(this);
 
     let url = `/json/v1/list?corpus=${encodeURIComponent(this.currentCorpus)}`;
-    if (!this.showAllDigests) {
-      url += '&at_head_only=true';
-    }
     if (this.disregardIgnoreRules) {
       url += '&include_ignored_traces=true';
     }
@@ -280,14 +273,6 @@ export class ListPageSk extends ElementSk {
 
   private showQueryDialog() {
     $$<QueryDialogSk>('query-dialog-sk')!.open(this.paramset, this.currentQuery);
-  }
-
-  private toggleAllDigests(e: Event) {
-    e.preventDefault();
-    this.showAllDigests = !this.showAllDigests;
-    this.stateChanged();
-    this._render();
-    this.fetch();
   }
 
   private toggleIgnoreRules(e: Event) {
