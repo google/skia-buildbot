af61bb4b06cd71f69ec683f6d40c1719188035c4
Ravi Mistry <rmistry@google.com>
2021-06-23 11:38:39 -0400

    Add SubmittedTogether func to go/gerrit
    Bug: skia:12053
    Change-Id: Ib906e71d2c11501ae4be459a3f59ea22890d7071

--- go/gerrit/mocks/GerritInterface.go
+++ go/gerrit/mocks/GerritInterface.go
@@ -607,6 +607,36 @@ func (_m *GerritInterface) Submit(_a0 context.Context, _a1 *gerrit.ChangeInfo) e
 	return r0
 }
 
+// SubmittedTogether provides a mock function with given fields: _a0, _a1
+func (_m *GerritInterface) SubmittedTogether(_a0 context.Context, _a1 *gerrit.ChangeInfo) ([]*gerrit.ChangeInfo, int, error) {
+	ret := _m.Called(_a0, _a1)
+
+	var r0 []*gerrit.ChangeInfo
+	if rf, ok := ret.Get(0).(func(context.Context, *gerrit.ChangeInfo) []*gerrit.ChangeInfo); ok {
+		r0 = rf(_a0, _a1)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).([]*gerrit.ChangeInfo)
+		}
+	}
+
+	var r1 int
+	if rf, ok := ret.Get(1).(func(context.Context, *gerrit.ChangeInfo) int); ok {
+		r1 = rf(_a0, _a1)
+	} else {
+		r1 = ret.Get(1).(int)
+	}
+
+	var r2 error
+	if rf, ok := ret.Get(2).(func(context.Context, *gerrit.ChangeInfo) error); ok {
+		r2 = rf(_a0, _a1)
+	} else {
+		r2 = ret.Error(2)
+	}
+
+	return r0, r1, r2
+}
+
 // Url provides a mock function with given fields: _a0
 func (_m *GerritInterface) Url(_a0 int64) string {
 	ret := _m.Called(_a0)
