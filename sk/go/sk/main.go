4e2f57ce091b69ae037e24c7fef4ad6de48accfe
Eric Boren <borenet@google.com>
2021-06-23 07:37:47 -0400

    [sk] Actually fix logging
    Change-Id: I5ba5f07c190f5895b20ee68d1722cbcfb63cfaa7
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/420482
    Commit-Queue: Eric Boren <borenet@google.com>
    Reviewed-by: Ravi Mistry <rmistry@google.com>

--- sk/go/sk/main.go
+++ sk/go/sk/main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"os"
 
 	"github.com/urfave/cli/v2"
 
+	"go.skia.org/infra/go/exec"
 	"go.skia.org/infra/sk/go/asset"
 	release_branch "go.skia.org/infra/sk/go/release-branch"
 	"go.skia.org/infra/sk/go/try"
@@ -13,6 +16,16 @@ import (
 func main() {
 	// Make sklog happy so it doesn't log errors.
 	flag.Parse()
+	exec.WriteInfoLog = exec.WriteLog{
+		LogFunc: func(format string, args ...interface{}) {
+			_, _ = fmt.Fprintf(os.Stdout, format, args...)
+		},
+	}
+	exec.WriteWarningLog = exec.WriteLog{
+		LogFunc: func(format string, args ...interface{}) {
+			_, _ = fmt.Fprintf(os.Stderr, format, args...)
+		},
+	}
 
 	app := &cli.App{
 		Name:        "sk",
