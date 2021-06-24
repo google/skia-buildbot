abb8bdddb33695180f74a886f470d952fd4f36cb
Ravi Mistry <rmistry@google.com>
2021-06-24 16:46:54 -0400

    Get whether a change is submittable in gerrit.GetChange
    Bug: skia:12053
    Change-Id: Ica1ab556583f5baf5e99f3cb099b105d49f4b949

--- autoroll/go/repo_manager/semver_gcs_repo_manager_test.go
+++ autoroll/go/repo_manager/semver_gcs_repo_manager_test.go
@@ -325,7 +325,7 @@ func TestAFDORepoManager(t *testing.T) {
 	respBody, err = json.Marshal(ci)
 	require.NoError(t, err)
 	respBody = append([]byte(")]}'\n"), respBody...)
-	urlmock.MockOnce("https://fake-skia-review.googlesource.com/a/changes/123/detail?o=ALL_REVISIONS", mockhttpclient.MockGetDialogue(respBody))
+	urlmock.MockOnce("https://fake-skia-review.googlesource.com/a/changes/123/detail?o=ALL_REVISIONS&o=SUBMITTABLE", mockhttpclient.MockGetDialogue(respBody))
 
 	// Mock the request to set the CQ.
 	reqBody = []byte(`{"labels":{"Code-Review":1,"Commit-Queue":2},"message":"","reviewers":[{"reviewer":"reviewer@chromium.org"}]}`)
