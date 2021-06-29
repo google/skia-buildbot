01666b68ea2e53f64907e66f884f2dea05b9db55
Leandro Lovisolo <lovisolo@google.com>
2021-06-28 23:25:52 -0400

    [bazel] Fix Infra-PerCommit-Test-Bazel-Local.
    Bug: skia:11111
    Change-Id: Ie6ada5597eaec7e21256cac9261e4758f3c9ee46

--- infra/bots/task_drivers/bazel_test_all/bazel_test_all.go
+++ infra/bots/task_drivers/bazel_test_all/bazel_test_all.go
@@ -206,10 +206,16 @@ func testLocally(ctx context.Context, bzl *bazel.Bazel) (rvErr error) {
 			return err
 		}
 		ctx = td.WithEnv(ctx, []string{"PATH=%(PATH)s:" + depotToolsDir})
+
+		// If the emulators are already running for any reason, kill them first. This prevents "Address
+		// already in use" errors on GCE bots.
+		if err = emulators.StopAllEmulators(); err != nil {
+			return err
+		}
+		time.Sleep(5 * time.Second) // Give emulators time to shut down gracefully.
 	}
 
-	// Start the emulators. When running this task driver locally (e.g. with --local), this will kill
-	// any existing emulator instances prior to launching all emulators.
+	// Start the emulators.
 	if err := emulators.StartAllEmulators(); err != nil {
 		return err
 	}
@@ -221,7 +227,7 @@ func testLocally(ctx context.Context, bzl *bazel.Bazel) (rvErr error) {
 	time.Sleep(5 * time.Second) // Give emulators time to boot.
 
 	// Set *_EMULATOR_HOST environment variables.
-	emulatorHostEnvVars := []string{}
+	var emulatorHostEnvVars []string
 	for _, emulator := range emulators.AllEmulators {
 		// We need to set the *_EMULATOR_HOST variable for the current emulator before we can retrieve
 		// its value via emulators.GetEmulatorHostEnvVar().
@@ -234,7 +240,16 @@ func testLocally(ctx context.Context, bzl *bazel.Bazel) (rvErr error) {
 	}
 	ctx = td.WithEnv(ctx, emulatorHostEnvVars)
 
+	seconds := 1 * time.Second
+	err := td.Do(ctx, td.Props(fmt.Sprintf("SLEEPING FOR %d SECONDS", seconds)), func(ctx context.Context) error {
+		time.Sleep(seconds)
+		return nil
+	})
+	if err != nil {
+		return err
+	}
+
 	// Run all tests in the repository. The tryjob will fail upon any failing tests.
-	_, err := bzl.Do(ctx, "test", "//...", "--test_output=errors")
+	_, err = bzl.Do(ctx, "test", "//...", "--test_output=errors", "--jobs=4")
 	return err
 }
