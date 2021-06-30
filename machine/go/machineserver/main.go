6e30ac214cecb10fc82606b8ed5a0b842a977afd
Joe Gregorio <jcgregorio@google.com>
2021-06-30 16:23:32 -0400

    Use switchboard/cleanup in machineserver.
    Bug: skia:12063
    Change-Id: Ic5f1065d12f8dd6eaa55f438119bdef320e3bcf7

--- machine/go/machineserver/main.go
+++ machine/go/machineserver/main.go
@@ -29,6 +29,8 @@ import (
 	"go.skia.org/infra/machine/go/machine/source/pubsubsource"
 	machineStore "go.skia.org/infra/machine/go/machine/store"
 	"go.skia.org/infra/machine/go/machineserver/config"
+	"go.skia.org/infra/machine/go/switchboard"
+	"go.skia.org/infra/machine/go/switchboard/cleanup"
 )
 
 // flags
@@ -79,6 +81,11 @@ func new() (baseapp.App, error) {
 		return nil, skerr.Wrapf(err, "Failed to start pubsubsource.")
 	}
 	storeUpdateFail := metrics2.GetCounter("machineserver_store_update_fail")
+	switchboardImpl, err := switchboard.New(ctx, *baseapp.Local, instanceConfig)
+	if err != nil {
+		sklog.Fatal(err)
+	}
+	cleaner := cleanup.New(switchboardImpl)
 
 	// Start our main loop.
 	go func() {
@@ -92,6 +99,10 @@ func new() (baseapp.App, error) {
 			}
 		}
 	}()
+
+	// Start the process to clean up stale MeetingPoints.
+	go cleaner.Start(ctx)
+
 	s := &server{
 		store: store,
 	}
