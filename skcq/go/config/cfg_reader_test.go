bf9cb2879db0063a1b1ec9717765a7e888438fa8
Ravi Mistry <rmistry@google.com>
2021-06-30 11:08:40 -0400

    [skcq] Add config reader and skcq config
    Bug: skia:12053
    Change-Id: I744c10f42e5f0c1830e2c60afc60e8528690cc77
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/423236
    Commit-Queue: Ravi Mistry <rmistry@google.com>
    Reviewed-by: Eric Boren <borenet@google.com>

---
+++ skcq/go/config/cfg_reader_test.go
@@ -0,0 +1,171 @@
+package config
+
+import (
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"testing"
+
+	"go.skia.org/infra/task_scheduler/go/specs"
+
+	"github.com/stretchr/testify/require"
+
+	"go.skia.org/infra/go/allowed"
+	"go.skia.org/infra/go/gerrit"
+	gitiles_mocks "go.skia.org/infra/go/gitiles/mocks"
+	"go.skia.org/infra/go/mockhttpclient"
+	"go.skia.org/infra/go/testutils"
+	"go.skia.org/infra/go/testutils/unittest"
+	cr_mocks "go.skia.org/infra/skcq/go/codereview/mocks"
+)
+
+func setupGetSkCQCfg(t *testing.T, match bool, cfgContents []byte, readFileError error, changedFiles []string, cfgPath string, updatedInChange bool) *GitilesConfigReader {
+	matchedUser := "batman@gotham.com"
+	unmatchedUser := "superman@krypton.com"
+
+	changeOwner := unmatchedUser
+	if match {
+		changeOwner = matchedUser
+	}
+
+	ci := &gerrit.ChangeInfo{
+		Issue:   int64(123),
+		Owner:   &gerrit.Person{Email: changeOwner},
+		Project: "test-repo",
+		Branch:  "test-branch",
+	}
+	ref := "refs/changes/22/401222/140"
+	allowListName := "test-group-name"
+
+	// Setup codereview mock.
+	cr := &cr_mocks.CodeReview{}
+	if updatedInChange {
+		cr.On("GetChangeRef", ci).Return(ref).Once()
+	}
+
+	// Mock gitilesRepo.
+	gitilesRepo := &gitiles_mocks.GitilesRepo{}
+	if updatedInChange {
+		gitilesRepo.On("ReadFileAtRef", testutils.AnyContext, cfgPath, ref).Return(cfgContents, readFileError).Once()
+	} else {
+		gitilesRepo.On("ReadFileAtRef", testutils.AnyContext, cfgPath, ci.Branch).Return(cfgContents, readFileError).Once()
+	}
+
+	// Mock httpClient for allowlist.
+	mockClient := mockhttpclient.NewURLMock()
+	mockClient.Mock(fmt.Sprintf(allowed.GROUP_URL_TEMPLATE, allowListName), mockhttpclient.MockGetDialogue([]byte(fmt.Sprintf(`{"group": {"members": ["user:%s"]}}`, matchedUser))))
+	cria, err := allowed.NewAllowedFromChromeInfraAuth(mockClient.Client(), allowListName)
+	require.Nil(t, err)
+
+	return &GitilesConfigReader{
+		gitilesRepo:           gitilesRepo,
+		cr:                    cr,
+		ci:                    ci,
+		changedFiles:          changedFiles,
+		canModifyCfgsOnTheFly: cria,
+	}
+}
+
+func TestGetSkCQCfg_UpdatedInChange(t *testing.T) {
+	unittest.SmallTest(t)
+
+	treeStatusURL := "http://tree-status-url"
+	skCfg := &SkCQCfg{
+		TreeStatusURL:    treeStatusURL,
+		CommitterList:    "test-list",
+		DryRunAccessList: "test-list",
+	}
+	skCfgContents, err := json.Marshal(skCfg)
+	require.Nil(t, err)
+
+	configReader := setupGetSkCQCfg(t, true, skCfgContents, nil, []string{"dir1/*", SkCQCfgPath}, SkCQCfgPath, true)
+	cfg, err := configReader.GetSkCQCfg(context.Background())
+	require.Nil(t, err)
+	require.Equal(t, treeStatusURL, cfg.TreeStatusURL)
+}
+
+func TestGetSkCQCfg_UpdatedInChange_NotAllowed(t *testing.T) {
+	unittest.SmallTest(t)
+
+	configReader := setupGetSkCQCfg(t, false, []byte{}, nil, []string{"dir1/*", SkCQCfgPath}, SkCQCfgPath, true)
+	cfg, err := configReader.GetSkCQCfg(context.Background())
+	require.Nil(t, cfg)
+	require.NotNil(t, err)
+	_, ok := err.(*CannotModifyCfgsOnTheFlyError)
+	require.True(t, ok)
+}
+
+func TestGetSkCQCfg_NotUpdatedInChange(t *testing.T) {
+	unittest.SmallTest(t)
+
+	treeStatusURL := "http://tree-status-url"
+	skCfg := &SkCQCfg{
+		TreeStatusURL:    treeStatusURL,
+		CommitterList:    "test-list",
+		DryRunAccessList: "test-list",
+	}
+	skCfgContents, err := json.Marshal(skCfg)
+	require.Nil(t, err)
+
+	configReader := setupGetSkCQCfg(t, true, skCfgContents, nil, []string{"dir1/*", "dir2/dir3/DEPS"}, SkCQCfgPath, false)
+	cfg, err := configReader.GetSkCQCfg(context.Background())
+	require.Nil(t, err)
+	require.Equal(t, treeStatusURL, cfg.TreeStatusURL)
+}
+
+func TestGetSkCQCfg_NotFound(t *testing.T) {
+	unittest.SmallTest(t)
+
+	configReader := setupGetSkCQCfg(t, true, []byte{}, errors.New("NOT_FOUND"), []string{"dir1/*", "dir2/dir3/DEPS"}, SkCQCfgPath, false)
+	cfg, err := configReader.GetSkCQCfg(context.Background())
+	require.Nil(t, cfg)
+	require.NotNil(t, err)
+	_, ok := err.(*ConfigNotFoundError)
+	require.True(t, ok)
+}
+
+func TestGetSkCQCfg_ValidationFailed(t *testing.T) {
+	unittest.SmallTest(t)
+
+	treeStatusURL := "http://tree-status-url"
+	// Missing CommitterList and DryRunAccessList.
+	skCfg := &SkCQCfg{
+		TreeStatusURL: treeStatusURL,
+	}
+	skCfgContents, err := json.Marshal(skCfg)
+	require.Nil(t, err)
+
+	configReader := setupGetSkCQCfg(t, true, skCfgContents, nil, []string{"dir1/*", "dir2/dir3/DEPS"}, SkCQCfgPath, false)
+	cfg, err := configReader.GetSkCQCfg(context.Background())
+	require.Nil(t, cfg)
+	require.NotNil(t, err)
+}
+
+func TestGetTasksCfg_UpdatedInChange(t *testing.T) {
+	unittest.SmallTest(t)
+
+	tasksJSONPath := "test/path/tasks.json"
+	tasksJSON := &specs.TasksCfg{}
+	tasksJSONContents, err := json.Marshal(tasksJSON)
+	require.Nil(t, err)
+
+	configReader := setupGetSkCQCfg(t, true, tasksJSONContents, nil, []string{"dir1/*", tasksJSONPath}, tasksJSONPath, true)
+	cfg, err := configReader.GetTasksCfg(context.Background(), tasksJSONPath)
+	require.Nil(t, err)
+	require.NotNil(t, cfg)
+}
+
+func TestGetTasksCfg_NotUpdatedInChange(t *testing.T) {
+	unittest.SmallTest(t)
+
+	tasksJSONPath := "test/path/tasks.json"
+	tasksJSON := &specs.TasksCfg{}
+	tasksJSONContents, err := json.Marshal(tasksJSON)
+	require.Nil(t, err)
+
+	configReader := setupGetSkCQCfg(t, true, tasksJSONContents, nil, []string{"dir1/*"}, tasksJSONPath, false)
+	cfg, err := configReader.GetTasksCfg(context.Background(), tasksJSONPath)
+	require.Nil(t, err)
+	require.NotNil(t, cfg)
+}
