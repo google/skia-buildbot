45d5ad4f847128445cd16a09f238b087044ed51c
Ravi Mistry <rmistry@google.com>
2021-07-01 13:10:58 -0400

    [skcq] Add CommitFooterVerifier
    Bug: skia:12053
    Change-Id: I32cc6c34989fb88dfed22c26ec154c859f586f6f
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/423940
    Commit-Queue: Ravi Mistry <rmistry@google.com>
    Reviewed-by: Eric Boren <borenet@google.com>

---
+++ skcq/go/verifiers/commit_footer_verifier_test.go
@@ -0,0 +1,51 @@
+package verifiers
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"go.skia.org/infra/go/gerrit"
+	"go.skia.org/infra/go/testutils/unittest"
+	"go.skia.org/infra/skcq/go/footers"
+	"go.skia.org/infra/skcq/go/types"
+)
+
+func TestVerify_NoFooter(t *testing.T) {
+	unittest.SmallTest(t)
+
+	ci := &gerrit.ChangeInfo{Issue: int64(123)}
+	commitFooterVerifier := &CommitFooterVerifier{}
+	state, _, err := commitFooterVerifier.Verify(context.Background(), ci, int64(333))
+	require.Nil(t, err)
+	require.Equal(t, types.VerifierSuccessState, state)
+}
+
+func TestVerify_CommitFalseFooter(t *testing.T) {
+	unittest.SmallTest(t)
+
+	ci := &gerrit.ChangeInfo{Issue: int64(123)}
+	commitFooterVerifier := &CommitFooterVerifier{
+		footersMap: map[string]string{
+			string(footers.CommitFooter): "false",
+		},
+	}
+	state, _, err := commitFooterVerifier.Verify(context.Background(), ci, int64(333))
+	require.Nil(t, err)
+	require.Equal(t, types.VerifierFailureState, state)
+}
+
+func TestVerify_CommitTrueFooter(t *testing.T) {
+	unittest.SmallTest(t)
+
+	ci := &gerrit.ChangeInfo{Issue: int64(123)}
+	commitFooterVerifier := &CommitFooterVerifier{
+		footersMap: map[string]string{
+			string(footers.CommitFooter): "true",
+		},
+	}
+	state, _, err := commitFooterVerifier.Verify(context.Background(), ci, int64(333))
+	require.Nil(t, err)
+	require.Equal(t, types.VerifierSuccessState, state)
+}
