9ac124292038be977935309cc6a0ac2c1d916dba
Joe Gregorio <jcgregorio@google.com>
2021-07-02 11:35:32 -0400

    [perf] Create JSON Schema for InstanceConfig.
    We can then use this to validate config files
    fed to Perf.
    
    Change-Id: Ie8ea66a7692f0ab426245e6dc65adca20d55fa39

---
+++ perf/go/config/validate/validate_test.go
@@ -0,0 +1,32 @@
+package validate
+
+import (
+	"context"
+	_ "embed"
+	"io"
+	"io/ioutil"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	"go.skia.org/infra/go/testutils/unittest"
+	"go.skia.org/infra/go/util"
+)
+
+func TestInstanceConfigBytes_AllExistingConfigs_ShouldBeValid(t *testing.T) {
+	unittest.MediumTest(t)
+	ctx := context.Background()
+
+	allExistingConfigs, err := filepath.Glob("../../../configs/*.json")
+	require.NoError(t, err)
+	for _, filename := range allExistingConfigs {
+		err := util.WithReadFile(filename, func(r io.Reader) error {
+			b, err := ioutil.ReadAll(r)
+			require.NoError(t, err)
+			_, err = InstanceConfigBytes(ctx, b)
+			return err
+		})
+		require.NoError(t, err, filename)
+
+	}
+}
