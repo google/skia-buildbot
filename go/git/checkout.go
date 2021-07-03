3d3667ef97ecfe06b3ebde2c588ad344a4985544
Leandro Lovisolo <lovisolo@google.com>
2021-07-03 00:20:32 -0400

    [bazel] Fix Infra-PerCommit-Test-Bazel-Local.
    Bug: skia:11111
    Change-Id: Ie6ada5597eaec7e21256cac9261e4758f3c9ee46

--- go/git/checkout.go
+++ go/git/checkout.go
@@ -5,8 +5,11 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"os/user"
 	"path"
+	"runtime/debug"
 	"strings"
+	"time"
 
 	"go.skia.org/infra/go/sklog"
 )
@@ -117,7 +120,7 @@ type TempCheckout Checkout
 // directory and then clones the repoUrl into a subdirectory, based on default
 // "git clone" behavior.
 func NewTempCheckout(ctx context.Context, repoUrl string) (*TempCheckout, error) {
-	tmpDir, err := ioutil.TempDir("", "")
+	tmpDir, err := ioutil.TempDir("", "checkout_NewTempCheckout-*")
 	if err != nil {
 		return nil, err
 	}
@@ -130,6 +133,18 @@ func NewTempCheckout(ctx context.Context, repoUrl string) (*TempCheckout, error)
 
 // Delete removes the TempCheckout's working directory.
 func (c *TempCheckout) Delete() {
+	fmt.Println("********** TempCheckout.Cleanup STACK TRACE:")
+	debug.PrintStack()
+
+	fmt.Printf("********** CHECKOUT: ABOUT TO DELETE %s, WHICH IS THE PARENT IR OF %s\n", path.Dir(c.Dir()), c.Dir())
+	if u, err := user.Current(); err != nil {
+		panic(fmt.Sprintf("Error while retrieving current user: %v", err))
+	} else {
+		fmt.Printf("********** CURRENT USERNAME: %s\n", u)
+	}
+	fmt.Println("********** SLEEPING FOR 1 HOUR")
+	time.Sleep(1 * time.Hour)
+
 	if err := os.RemoveAll(path.Dir(c.Dir())); err != nil {
 		sklog.Errorf("Failed to remove git.TempCheckout: %s", err)
 	}
--- infra/bots/task_drivers/bazel_test_all/bazel_test_all.go
+++ infra/bots/task_drivers/bazel_test_all/bazel_test_all.go
@@ -65,11 +65,30 @@ func main() {
 	}
 
 	// Set up Bazel.
-	bzl, bzlCleanup, err := bazel.New(ctx, gitDir.Dir(), *local, *rbeKey)
-	if err != nil {
-		td.Fatal(ctx, err)
+	var bzl *bazel.Bazel
+	if *rbe {
+		var (
+			cleanup func()
+			err     error
+		)
+		bzl, cleanup, err = bazel.New(ctx, gitDir.Dir(), *local, *rbeKey)
+		if err != nil {
+			td.Fatal(ctx, err)
+		}
+		defer cleanup()
+	} else {
+		var (
+			cleanup func()
+			err     error
+		)
+		// Infra-PerCommit-Test-Bazel-Local uses a ramdisk as the Bazel cache in order to prevent "disk
+		// stall detected" errors on GCE VMs due to slow I/O.
+		bzl, cleanup, err = bazel.NewWithRamdisk(ctx, gitDir.Dir(), *rbeKey)
+		if err != nil {
+			td.Fatal(ctx, err)
+		}
+		defer cleanup()
 	}
-	defer bzlCleanup()
 
 	// Print out the Bazel version for debugging purposes.
 	if _, err := bzl.Do(ctx, "version"); err != nil {
@@ -206,10 +225,16 @@ func testLocally(ctx context.Context, bzl *bazel.Bazel) (rvErr error) {
 			return err
 		}
 		ctx = td.WithEnv(ctx, []string{"PATH=%(PATH)s:" + depotToolsDir})
+
+		// If the emulators are already running for any reason, kill them first. This prevents "Address
+		// already in use" errors on GCE bots.
+		if err = emulators.StopAllEmulators(); err != nil {
+			return err
+		}
+		time.Sleep(5 * time.Second) // Give emulators time to shut down gracefully.
 	}
 
-	// Start the emulators. When running this task driver locally (e.g. with --local), this will kill
-	// any existing emulator instances prior to launching all emulators.
+	// Start the emulators.
 	if err := emulators.StartAllEmulators(); err != nil {
 		return err
 	}
@@ -221,7 +246,7 @@ func testLocally(ctx context.Context, bzl *bazel.Bazel) (rvErr error) {
 	time.Sleep(5 * time.Second) // Give emulators time to boot.
 
 	// Set *_EMULATOR_HOST environment variables.
-	emulatorHostEnvVars := []string{}
+	var emulatorHostEnvVars []string
 	for _, emulator := range emulators.AllEmulators {
 		// We need to set the *_EMULATOR_HOST variable for the current emulator before we can retrieve
 		// its value via emulators.GetEmulatorHostEnvVar().
@@ -234,7 +259,17 @@ func testLocally(ctx context.Context, bzl *bazel.Bazel) (rvErr error) {
 	}
 	ctx = td.WithEnv(ctx, emulatorHostEnvVars)
 
+	//seconds := 1 * time.Second
+	//err := td.Do(ctx, td.Props(fmt.Sprintf("SLEEPING FOR %d SECONDS", seconds/time.Second)), func(ctx context.Context) error {
+	//	time.Sleep(seconds)
+	//	return nil
+	//})
+	//if err != nil {
+	//	return err
+	//}
+
 	// Run all tests in the repository. The tryjob will fail upon any failing tests.
-	_, err := bzl.Do(ctx, "test", "//...", "--test_output=errors")
+	//_, err := bzl.Do(ctx, "test", "//...", "--test_output=errors")
+	_, err := bzl.Do(ctx, "test", "//task_scheduler/go/syncer:syncer_test", "--test_output=streamed", "--test_timeout=3600", "--test_arg=--test.v", "--test_arg=--alsologtostderr")
 	return err
 }
