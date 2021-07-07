dc77be5f9270107d35446403d981a6a5658875e5
Joe Gregorio <jcgregorio@google.com>
2021-07-07 10:26:08 -0400

    Simplify using JSON Schema by creating helper functions.
    Use those helper functions in Perf.
    
    Change-Id: I47578e3a5e349001f8a5b37e101f5d0d1ae56209

--- perf/go/config/generate/main.go
+++ perf/go/config/generate/main.go
@@ -4,25 +4,10 @@
 package main
 
 import (
-	"encoding/json"
-	"io"
-
-	"github.com/alecthomas/jsonschema"
-	"go.skia.org/infra/go/sklog"
-	"go.skia.org/infra/go/util"
+	"go.skia.org/infra/go/jsonschema"
 	"go.skia.org/infra/perf/go/config"
 )
 
 func main() {
-	b, err := json.MarshalIndent(jsonschema.Reflect(&config.InstanceConfig{}), "", "  ")
-	if err != nil {
-		sklog.Fatal(err)
-	}
-	err = util.WithWriteFile("../instanceConfigSchema.json", func(w io.Writer) error {
-		_, err := w.Write(b)
-		return err
-	})
-	if err != nil {
-		sklog.Fatal(err)
-	}
+	jsonschema.GenerateSchema("../instanceConfigSchema.json", &config.InstanceConfig{})
 }
