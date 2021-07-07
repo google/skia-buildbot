948051af1b0c00a9b6c4c4c7fd4cd1af539b8c42
Joe Gregorio <jcgregorio@google.com>
2021-07-07 09:18:55 -0400

    Simplify using JSON Schema by creating helper functions.
    Use those helper functions in Perf.
    
    Change-Id: I47578e3a5e349001f8a5b37e101f5d0d1ae56209

--- go/jsonutils/jsonutils.go
+++ go/jsonutils/jsonutils.go
@@ -2,11 +2,25 @@ package jsonutils
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
 	"sort"
 	"strconv"
 	"time"
+
+	"github.com/alecthomas/jsonschema"
+	"github.com/xeipuuv/gojsonschema"
+	"go.skia.org/infra/go/skerr"
+	"go.skia.org/infra/go/sklog"
+	"go.skia.org/infra/go/util"
 )
+
+// ErrSchemaViolation is returned from Validate if the document doesn't conform
+// to the schema.
+var ErrSchemaViolation = errors.New("schema violation")
 
 // Number is an int64 which may be unmarshaled from a JSON string.
 type Number int64
@@ -90,4 +104,41 @@ func MarshalStringMap(m map[string]string) (data []byte) {
 	}
 	result.WriteRune('}')
 	return result.Bytes()
+}
+
+// Validate returns null if the document represents a JSON body that conforms to
+// the schema. If err is not nil then the slice of strings will contain a list
+// of schema violations.
+func Validate(ctx context.Context, document, schema []byte) ([]string, error) {
+	schemaLoader := gojsonschema.NewBytesLoader(schema)
+	documentLoader := gojsonschema.NewBytesLoader(document)
+	result, err := gojsonschema.Validate(schemaLoader, documentLoader)
+	if err != nil {
+		return nil, skerr.Wrapf(err, "failed while validating")
+	}
+	if len(result.Errors()) > 0 {
+		formattedResults := make([]string, len(result.Errors()))
+		for i, e := range result.Errors() {
+			formattedResults[i] = fmt.Sprintf("%d: %s", i, e.String())
+		}
+		return formattedResults, ErrSchemaViolation
+	}
+	return nil, nil
+}
+
+// GenerateSchema writes the JSON Schema for 'v' into 'filename' and will exit
+// via sklog.Fatal if any errors occur. This function is designed for use
+// in an app you would run via go generate.
+func GenerateSchema(filename string, v interface{}) {
+	b, err := json.MarshalIndent(jsonschema.Reflect(v), "", "  ")
+	if err != nil {
+		sklog.Fatal(err)
+	}
+	err = util.WithWriteFile(filename, func(w io.Writer) error {
+		_, err := w.Write(b)
+		return err
+	})
+	if err != nil {
+		sklog.Fatal(err)
+	}
 }
