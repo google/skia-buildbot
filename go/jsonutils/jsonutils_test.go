325d6e2b1be10ec21c1d1d74764a83e2d9ac0723
Joe Gregorio <jcgregorio@google.com>
2021-07-07 09:10:34 -0400

    Simplify using JSON Schema by creating helper functions.
    Use those helper functions in Perf.
    
    Change-Id: I47578e3a5e349001f8a5b37e101f5d0d1ae56209

--- go/jsonutils/jsonutils_test.go
+++ go/jsonutils/jsonutils_test.go
@@ -1,13 +1,15 @@
 package jsonutils
 
 import (
+	"context"
 	"encoding/json"
+	"io/ioutil"
+	"path/filepath"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-
 	"go.skia.org/infra/go/testutils"
 	"go.skia.org/infra/go/testutils/unittest"
 )
@@ -189,4 +191,58 @@ func BenchmarkBuiltInJSONMarshal_50Items(b *testing.B) {
 			panic("this is to keep the call from being optimized out")
 		}
 	}
+}
+
+func TestGenerateSchema_NotAnInstanceThatCanBeSerialized_Panics(t *testing.T) {
+	require.Panics(t, func() {
+		GenerateSchema("/tmp/not-used.json", struct{ ComplexValuesAreNotValidJSON complex128 }{})
+	})
+}
+
+type testStruct struct {
+	A int64
+}
+
+const testStructSchema = `{
+  "$schema": "http://json-schema.org/draft-04/schema#",
+  "$ref": "#/definitions/testStruct",
+  "definitions": {
+    "testStruct": {
+      "required": [
+        "A"
+      ],
+      "properties": {
+        "A": {
+          "type": "integer"
+        }
+      },
+      "additionalProperties": false,
+      "type": "object"
+    }
+  }
+}`
+
+func TestGenerateSchema_ValidStruct_WritesSchemaFile(t *testing.T) {
+	unittest.MediumTest(t)
+	filename := filepath.Join(t.TempDir(), "schema.json")
+	GenerateSchema(filename, testStruct{})
+	b, err := ioutil.ReadFile(filename)
+	require.NoError(t, err)
+	require.Equal(t, testStructSchema, string(b))
+}
+
+func TestValidate_JSONConformsToSchema_Success(t *testing.T) {
+	unittest.SmallTest(t)
+	schemaViolations, err := Validate(context.Background(), []byte(`{"A": 12}`), []byte(testStructSchema))
+	require.NoError(t, err)
+	require.Empty(t, schemaViolations)
+}
+
+func TestValidate_JSONDoesNotConformsToSchema_Success(t *testing.T) {
+	unittest.SmallTest(t)
+	schemaViolations, err := Validate(context.Background(), []byte(`{"B": 12}`), []byte(testStructSchema))
+	require.Error(t, err)
+	require.Len(t, schemaViolations, 2)
+	require.Contains(t, schemaViolations[0], "A is required")
+	require.Contains(t, schemaViolations[1], "Additional property B is not allowed")
 }
