56cfdbe5baa3645d5b3fed8617502b09f530ddc5
Kevin Lubick <kjlubick@google.com>
2021-07-08 08:47:30 -0400

    [gold] Implement new display of the v2 triage log
    Change-Id: Ie50414ea6b9f6e6e32944f0ca5c4533a9e47a3ae

--- golden/modules/triagelog-page-sk/triagelog-page-sk_puppeteer_test.ts
+++ golden/modules/triagelog-page-sk/triagelog-page-sk_puppeteer_test.ts
@@ -1,35 +1,49 @@
-import { expect } from 'chai';
+import path from 'path';
 import {
   addEventListenersToPuppeteerPage, inBazel,
   loadCachedTestBed,
   takeScreenshot,
-  TestBed
+  TestBed,
 } from '../../../puppeteer-tests/util';
-import path from "path";
 
 describe('triagelog-page-sk', () => {
   let testBed: TestBed;
   before(async () => {
     testBed = await loadCachedTestBed(
-        path.join(__dirname, '..', '..', 'webpack.config.ts')
+      path.join(__dirname, '..', '..', 'webpack.config.ts'),
     );
   });
 
-  beforeEach(async () => {
-    const eventPromise = await addEventListenersToPuppeteerPage(testBed.page, ['end-task']);
-    const loaded = eventPromise('end-task'); // Emitted when page is loaded.
-    await testBed.page.goto(
-        inBazel() ? testBed.baseUrl : `${testBed.baseUrl}/dist/triagelog-page-sk.html`);
-    await loaded;
+  describe('v1 APIs', () => {
+    beforeEach(async () => {
+      const eventPromise = await addEventListenersToPuppeteerPage(testBed.page, ['end-task']);
+      const loaded = eventPromise('end-task'); // Emitted when page is loaded.
+      await testBed.page.goto(
+        inBazel() ? testBed.baseUrl : `${testBed.baseUrl}/dist/triagelog-page-sk.html`,
+      );
+      await loaded;
+    });
+
+    it('should take a screenshot', async () => {
+      await testBed.page.setViewport({ width: 1200, height: 2600 });
+      await takeScreenshot(testBed.page, 'gold', 'triagelog-page-sk');
+    });
   });
 
-  it('should render the demo page', async () => {
-    // Basic smoke test.
-    expect(await testBed.page.$$('triagelog-page-sk')).to.have.length(1);
-  });
+  describe('v2 APIs', () => {
+    beforeEach(async () => {
+      const eventPromise = await addEventListenersToPuppeteerPage(testBed.page, ['end-task']);
+      const loaded = eventPromise('end-task'); // Emitted when page is loaded.
+      await testBed.page.goto(
+        inBazel() ? `${testBed.baseUrl}?use_new_api=true`
+          : `${testBed.baseUrl}/dist/triagelog-page-sk.html?use_new_api=true`,
+      );
+      await loaded;
+    });
 
-  it('should take a screenshot', async () => {
-    await testBed.page.setViewport({ width: 1200, height: 2600 });
-    await takeScreenshot(testBed.page, 'gold', 'triagelog-page-sk');
+    it('should take a screenshot', async () => {
+      await testBed.page.setViewport({ width: 1200, height: 1800 });
+      await takeScreenshot(testBed.page, 'gold', 'triagelog-page-sk-v2');
+    });
   });
 });
