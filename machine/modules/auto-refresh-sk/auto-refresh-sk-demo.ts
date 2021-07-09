09f6eb7720ae372dd3176f4aa91bc2705ea4a028
Joe Gregorio <jcgregorio@google.com>
2021-07-09 12:03:20 -0400

    [machine] Add auto-refresh-sk element.
    Bug: skia:12063
    Change-Id: Ib9660e955aabe4261c95c05c6b4c850907455cdc

---
+++ machine/modules/auto-refresh-sk/auto-refresh-sk-demo.ts
@@ -0,0 +1,12 @@
+import { $$ } from 'common-sk/modules/dom';
+import { AutoRefreshSk } from './auto-refresh-sk';
+import './index';
+
+const element = $$<AutoRefreshSk>('auto-refresh-sk', document)!;
+// Force into a known start at start.
+element.refreshing = false;
+
+// Display refresh-page events and when they occur.
+element.addEventListener('refresh-page', (e) => {
+    document.querySelector('#events')!.textContent = `${JSON.stringify(e, null, '  ')}\n${Date.now()}`;
+});
