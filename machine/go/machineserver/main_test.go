0b1ac519f8689683aa9dd457c9aca50f2777426c
Erik Rose <erikrose@google.com>
2021-07-08 21:20:44 -0400

    Add JSON endpoint to list meeting points. Factor up repetition among JSON endpoints.
    Change-Id: Ie5561be1c57c5262d97be581303b37400eb4d169

--- machine/go/machineserver/main_test.go
+++ machine/go/machineserver/main_test.go
@@ -39,6 +39,17 @@ func setupForTest(t *testing.T) (context.Context, config.InstanceConfig) {
 	return ctx, cfg
 }
 
+// responseOf tries an HTTP request of the given method and path against a
+// server and returns the response.
+func responseOf(s server, method string, path string) httptest.ResponseRecorder {
+	router := mux.NewRouter()
+	s.AddHandlers(router)
+	r := httptest.NewRequest(method, path, nil)
+	w := httptest.NewRecorder()
+	router.ServeHTTP(w, r)
+	return *w
+}
+
 func TestMachineToggleModeHandler_Success(t *testing.T) {
 	unittest.LargeTest(t)
 
@@ -452,27 +463,44 @@ func TestPodsHandler_Success(t *testing.T) {
 	unittest.SmallTest(t)
 
 	// Set up the expected Pod.
-	var podTime time.Time
-	require.NoError(t, podTime.UnmarshalText([]byte("2001-02-03T04:05:06.78901Z")))
-	pod := switchboard.Pod{Name: "switch-pod-3", LastUpdated: podTime}
+	var lastUpdated time.Time
+	require.NoError(t, lastUpdated.UnmarshalText([]byte("2001-02-03T04:05:06.78901Z")))
+	pod := switchboard.Pod{Name: "switch-pod-3", LastUpdated: lastUpdated}
 
 	// ListPods is already well-tested in switchboard/impl, so we can mock out the whole switchboard.
 	sw := switchboardMocks.Switchboard{}
 	sw.On("ListPods", testutils.AnyContext).Return([]switchboard.Pod{pod}, nil)
-	s := &server{
+	s := server{
 		switchboard: &sw,
 	}
 
-	// Serve the request.
-	router := mux.NewRouter()
-	s.AddHandlers(router)
-	r := httptest.NewRequest("GET", "/_/pods", nil)
-	w := httptest.NewRecorder()
-	router.ServeHTTP(w, r)
-
+	w := responseOf(s, "GET", "/_/pods")
 	assert.Equal(t, 200, w.Code)
 	assert.Equal(
 		t,
 		[]byte(`[{"Name":"switch-pod-3","LastUpdated":"2001-02-03T04:05:06.78901Z"}]`+"\n"),
+		w.Body.Bytes())
+}
+
+func TestMeetingPointsHandler_Success(t *testing.T) {
+	unittest.SmallTest(t)
+
+	// Set up the expected MeetingPoint.
+	var lastUpdated time.Time
+	require.NoError(t, lastUpdated.UnmarshalText([]byte("2001-02-03T04:05:06.78901Z")))
+	meetingPoint := switchboard.MeetingPoint{PodName: "somePod", Port: 33, Username: "someUser", MachineID: "someMachine", LastUpdated: lastUpdated}
+
+	// ListMeetingPoints is already well-tested in switchboard/impl, so we can mock out the whole switchboard.
+	sw := switchboardMocks.Switchboard{}
+	sw.On("ListMeetingPoints", testutils.AnyContext).Return([]switchboard.MeetingPoint{meetingPoint}, nil)
+	s := server{
+		switchboard: &sw,
+	}
+
+	w := responseOf(s, "GET", "/_/meeting_points")
+	assert.Equal(t, 200, w.Code)
+	assert.Equal(
+		t,
+		[]byte(`[{"PodName":"somePod","Port":33,"Username":"someUser","MachineID":"someMachine","LastUpdated":"2001-02-03T04:05:06.78901Z"}]`+"\n"),
 		w.Body.Bytes())
 }
