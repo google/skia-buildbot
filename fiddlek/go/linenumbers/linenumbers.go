635ed365ad29dc2b63b45bbb15384c97a130f7de
Joe Gregorio <jcgregorio@google.com>
2021-07-09 16:26:02 -0400

    [fiddle] Only add a single #line 1 at the beginning of user code.
    Bug: skia:12183
    Change-Id: I09fbc0510a8282cc1f460a0b8dbf1e39d5766182

--- fiddlek/go/linenumbers/linenumbers.go
+++ fiddlek/go/linenumbers/linenumbers.go
@@ -1,17 +1,6 @@
 package linenumbers
 
-import (
-	"fmt"
-	"strings"
-)
-
 // LineNumbers adds #line numbering to the user's code.
 func LineNumbers(c string) string {
-	lines := strings.Split(c, "\n")
-	ret := []string{}
-	for i, line := range lines {
-		ret = append(ret, fmt.Sprintf("#line %d", i+1))
-		ret = append(ret, line)
-	}
-	return strings.Join(ret, "\n")
+	return "#line 1\n" + c
 }
