cc30bde269fa094ecdda4550397d8c8625caf9a1
Ravi Mistry <rmistry@google.com>
2021-07-12 15:02:56 -0400

    Prototype
    Change-Id: I0a21797b51df3b89b578e47fea0e3cc6156bc02a

---
+++ skcq/webpack.config.ts
@@ -0,0 +1,38 @@
+/// <reference path="../infra-sk/html-webpack-inject-attributes-plugin.d.ts" />
+
+import { resolve } from 'path';
+import webpack from 'webpack';
+import CopyWebpackPlugin from 'copy-webpack-plugin';
+import HtmlWebpackInjectAttributesPlugin from 'html-webpack-inject-attributes-plugin';
+import commonBuilder from '../infra-sk/pulito/webpack.common';
+
+const configFactory: webpack.ConfigurationFactory = (_, args) => {
+  // Don't minify the HTML since it contains Go template tags.
+  const config = commonBuilder(__dirname, args.mode, /* neverMinifyHtml= */ true);
+
+  config.output!.publicPath = '/dist/';
+
+  config.plugins!.push(
+    new CopyWebpackPlugin([
+      {
+        from: resolve(__dirname, 'images/favicon.ico'),
+        to: 'favicon.ico',
+      },
+    ]),
+  );
+
+  config.plugins!.push(
+    new HtmlWebpackInjectAttributesPlugin({
+      nonce: '{% .Nonce %}',
+    }),
+  );
+
+  config.resolve = config.resolve || {};
+
+  // https://github.com/webpack/node-libs-browser/issues/26#issuecomment-267954095
+  config.resolve.modules = [resolve(__dirname, '..', 'node_modules')];
+
+  return config;
+};
+
+export = configFactory;
