a11ba7bdb2c95a84efef7ad94477cb896f0f52b2
Ravi Mistry <rmistry@google.com>
2021-07-12 09:59:38 -0400

    Prototype
    Change-Id: I0a21797b51df3b89b578e47fea0e3cc6156bc02a

---
+++ skcq/modules/skcq-sk/skcq-sk.ts
@@ -0,0 +1,45 @@
+/**
+ * @module module/skcq
+ * @description <h2><code>skcq</code></h2>
+ *
+ */
+
+import { define } from 'elements-sk/define';
+import { html } from 'lit-html';
+import { ElementSk } from '../../../infra-sk/modules/ElementSk';
+
+import 'elements-sk/error-toast-sk';
+import 'elements-sk/icon/folder-icon-sk';
+import 'elements-sk/icon/help-icon-sk';
+import 'elements-sk/icon/home-icon-sk';
+import 'elements-sk/spinner-sk';
+
+import '../../../infra-sk/modules/app-sk';
+import '../../../infra-sk/modules/login-sk';
+import '../../../infra-sk/modules/theme-chooser-sk';
+
+import '../processing-table-sk';
+
+export class SkCQ extends ElementSk {
+  private _main: HTMLElement | null = null;
+
+  constructor() {
+    super(SkCQ.template);
+  }
+
+  private static template = () => html`
+  <processing-table-sk id=cq-table is_dry_run=false></processing-table-sk>
+  <processing-table-sk id=dry-runs-table is_dry_run=true></processing-table-sk>
+`;
+
+  connectedCallback(): void {
+    super.connectedCallback();
+    this._render();
+  }
+
+  disconnectedCallback(): void {
+    super.disconnectedCallback();
+  }
+}
+
+define('skcq-sk', SkCQ);
