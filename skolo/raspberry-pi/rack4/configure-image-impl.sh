20f6e06a02627fd19ce0830f6b2535533633873b
Joe Gregorio <jcgregorio@google.com>
2021-07-12 13:58:01 -0400

    Minimal config for rpis on rack4 starting from an arm64 debian image.
    Change-Id: Ie4076e98042f8be3e8e70d55085ce14895ec77af

---
+++ skolo/raspberry-pi/rack4/configure-image-impl.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -x
+
+# Record the directory of this file.
+REL=$(dirname "$0")
+
+# Check argument count is valid.
+if [ $# -ne 2 ]; then
+    exit 1
+fi
+
+HOSTNAME=$1;
+MOUNT=$2
+
+echo ${HOSTNAME} > ${MOUNT}/etc/hostname
+install -D --mode=600 ${REL}/../../authorized_keys ${MOUNT}/root/.ssh/authorized_keys
+
