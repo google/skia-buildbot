ab8c775b47edf746524e72c0ee43d84fb055849c
Ravi Mistry <rmistry@google.com>
2021-07-12 14:29:10 -0400

    [skcq] Add skcq-sk
    Bug: skia:12053
    Change-Id: Id52b1c4cf7bd5e5f9dbaf49e6d1bc88fa0c8ca5b

---
+++ skcq/modules/skcq-sk/skcq-sk_puppeteer_test.ts
@@ -0,0 +1,31 @@
+import * as path from 'path';
+import { expect } from 'chai';
+import {
+  loadCachedTestBed,
+  takeScreenshot,
+  TestBed,
+} from '../../../puppeteer-tests/util';
+
+describe('skcq-sk', () => {
+  let testBed: TestBed;
+  before(async () => {
+    testBed = await loadCachedTestBed(
+      path.join(__dirname, '..', '..', 'webpack.config.ts'),
+    );
+  });
+  beforeEach(async () => {
+    await testBed.page.goto(`${testBed.baseUrl}/dist/skcq-sk.html`);
+    await testBed.page.setViewport({ width: 400, height: 500 });
+  });
+
+  it('should render the demo page', async () => {
+    // Smoke test.
+    expect(await testBed.page.$$('skcq-sk')).to.have.length(1);
+  });
+
+  describe('screenshots', () => {
+    it('shows the default view', async () => {
+      await takeScreenshot(testBed.page, 'skcq', 'skcq-sk');
+    });
+  });
+});
