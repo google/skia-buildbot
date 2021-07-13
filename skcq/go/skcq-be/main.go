e177ea6ee0455945d0059a6f7c7b062721d423c1
Ravi Mistry <rmistry@google.com>
2021-07-13 16:01:47 -0400

    Prototype
    Change-Id: I0a21797b51df3b89b578e47fea0e3cc6156bc02a

---
+++ skcq/go/skcq-be/main.go
@@ -0,0 +1,111 @@
+/*
+	Skia Commit Queue server
+*/
+
+package main
+
+import (
+	"context"
+	"flag"
+	"time"
+
+	"cloud.google.com/go/datastore"
+
+	"go.skia.org/infra/go/allowed"
+	"go.skia.org/infra/go/auth"
+	"go.skia.org/infra/go/baseapp"
+	"go.skia.org/infra/go/common"
+	"go.skia.org/infra/go/gerrit"
+	"go.skia.org/infra/go/httputils"
+	"go.skia.org/infra/go/sklog"
+	"go.skia.org/infra/skcq/go/caches"
+	"go.skia.org/infra/skcq/go/codereview"
+	"go.skia.org/infra/skcq/go/db"
+	"go.skia.org/infra/skcq/go/poller"
+	"google.golang.org/api/compute/v1"
+)
+
+var (
+	// Flags
+	host        = flag.String("host", "skcq.skia.org", "HTTP service host")
+	fsNamespace = flag.String("fs_namespace", "", "The namespace this instance should operate in. e.g. staging or prod")
+	fsProjectID = flag.String("fs_project_id", "skia-firestore", "The project with the firestore instance. Datastore and Firestore can't be in the same project.")
+
+	chromeInfraAuthJWT = flag.String("chrome_infra_auth_jwt", "/var/secrets/skia-public-auth/key.json", "The JWT key for the service account that has access to chrome infra auth.")
+
+	serviceAccountFile = flag.String("service_account_file", "/var/secrets/google/key.json", "Service account JSON file.")
+
+	// canModifyCfgsOnTheFly = flag.String("can_modify_cfgs_on_the_fly", "project-skia-tryjob-access", "Which go/cria group is allowed to modify skcq.json and tasks.json on the fly.")
+	// This should be more restrcitive...
+	canModifyCfgsOnTheFly = flag.String("can_modify_cfgs_on_the_fly", "project-skia-committers", "Which go/cria group is allowed to modify skcq.json and tasks.json on the fly.")
+
+	// TODO(rmistry): When all Skia repositories use SkCQ remove this flag. DO NOT NEED THIS IF WE ONLY GO BY THE CONFIG!
+	// supportedReposAllowlist = common.NewMultiStringFlag("repos_allowlist", nil, "All Skia repos supported by SkCQ")
+	// Keep this really really fast.
+	pollInterval = flag.Duration("poll_interval", 10*time.Second, "How often the server will poll Gerrit for CR+1 and CQ+1/CQ+2 changes.")
+
+	publicFEInstanceURL = flag.String("public_fe_url", "localhost", "The public FE instance URL.")
+	corpFEInstanceURL   = flag.String("corp_fe_url", "localhost", "The corp FE instance URL.")
+
+	reposAllowList = common.NewMultiStringFlag("allowed_repo", nil, "Which repos should be processed by SkCQ. If not specified then all repos will be processed.")
+	reposBlockList = common.NewMultiStringFlag("blocked_repo", nil, "Which repos should not be processed by SkCQ. If not specified then no repos will be skipped.")
+
+	// Useful for debugging k8s pod.
+	hang = flag.Bool("hang", false, "Hangs the backend.")
+)
+
+func main() {
+	common.InitWithMust("skcq-be", common.PrometheusOpt(baseapp.PromPort), common.MetricsLoggingOpt())
+	defer sklog.Flush()
+	ctx := context.Background()
+
+	ts, err := auth.NewDefaultTokenSource(*baseapp.Local, datastore.ScopeDatastore, auth.SCOPE_USERINFO_EMAIL, auth.SCOPE_GERRIT, auth.SCOPE_READ_WRITE, datastore.ScopeDatastore, compute.CloudPlatformScope, datastore.ScopeDatastore)
+	if err != nil {
+		sklog.Fatal("Could not create token source: %s", err)
+	}
+
+	// Instantiate DB client.
+	dbClient, err := db.New(ctx, ts, *fsNamespace, *fsProjectID)
+	if err != nil {
+		sklog.Fatalf("Could not init DB: %s", err)
+	}
+
+	// Instantiate authenticated http client.
+	httpClient := httputils.DefaultClientConfig().WithTokenSource(ts).With2xxOnly().Client()
+
+	// Instantiate codereview.
+	g, err := codereview.NewGerrit(httpClient, gerrit.ConfigChromium, gerrit.GerritSkiaURL)
+	if err != nil {
+		sklog.Fatalf("Could not init gerrit client: %s", err)
+	}
+
+	if *hang {
+		select {}
+	}
+
+	// Instantiate the current changes cache so that if the server was restarted
+	// we can pick up where we left off.
+	currentChangesCache, err := caches.GetCurrentChangesCache(ctx, dbClient)
+	if err != nil {
+		sklog.Fatalf("Could not get current changes cache: %s", err)
+	}
+	sklog.Infof("CurrentChangesCache: %+v", currentChangesCache.Get())
+
+	// Instantiate client for go/cria.
+	criaTs, err := auth.NewJWTServiceAccountTokenSource("", *chromeInfraAuthJWT, auth.SCOPE_USERINFO_EMAIL)
+	if err != nil {
+		sklog.Fatal(err)
+	}
+	criaClient := httputils.DefaultClientConfig().WithTokenSource(criaTs).With2xxOnly().Client()
+	cfgModifyAllowed, err := allowed.NewAllowedFromChromeInfraAuth(criaClient, *canModifyCfgsOnTheFly)
+	if err != nil {
+		sklog.Fatal(err)
+	}
+
+	// Instantiate poller and turn it on.
+	if err := poller.Start(ctx, *pollInterval, g, currentChangesCache, httpClient, criaClient, dbClient, cfgModifyAllowed, *publicFEInstanceURL, *corpFEInstanceURL, *reposAllowList, *reposBlockList); err != nil {
+		sklog.Fatalf("Could not init SkCQ poller: %s", err)
+	}
+
+	select {}
+}
