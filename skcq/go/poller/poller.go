c6e2577d41c937a426c7d9c359732d0c017c4795
Ravi Mistry <rmistry@google.com>
2021-07-14 10:33:09 -0400

    Prototype
    Change-Id: I0a21797b51df3b89b578e47fea0e3cc6156bc02a

--- skcq/go/poller/poller.go
+++ skcq/go/poller/poller.go
@@ -36,10 +36,10 @@ const (
 // and runs them.
 func Start(ctx context.Context, pollInterval time.Duration, cr codereview.CodeReview, currentChangesCache caches.CurrentChangesCache, httpClient, criaClient *http.Client, dbClient db.DB, canModifyCfgsOnTheFly *allowed.AllowedFromChromeInfraAuth, publicFEInstanceURL, corpFEInstanceURL string, reposAllowList, reposBlockList []string) error {
 	liveness := metrics2.NewLiveness(LivenessMetric)
+	tm := throttler.NewThrottler()
+	vm := verifiers.NewSkCQVerifiersManager(tm, httpClient, criaClient, cr)
 	cleanup.Repeat(pollInterval, func(ctx context.Context) {
 		sklog.Info("----------------Poll--------------")
-		tm := throttler.NewThrottler()
-		vm := verifiers.NewSkCQVerifiersManager(tm, httpClient, criaClient, cr)
 		cls, err := cr.Search(ctx)
 		if err != nil {
 			sklog.Errorf("Error when searching for issues: %s", err)
