52deb8958a5256543bbad06375a50588f14cc4be
Kevin Lubick <kjlubick@google.com>
2021-09-29 10:03:12 -0400

    [gold] Add support to filter tryjobs when re-ingesting.
    Used to reingest some internal CLs.
    
    Change-Id: Iaa9936c9c305b5c650a7e26b77ec9c36c42550c3
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/453938
    Reviewed-by: Leandro Lovisolo <lovisolo@google.com>

--- golden/cmd/file_reingester/file_reingester.go
+++ golden/cmd/file_reingester/file_reingester.go
@@ -5,12 +5,17 @@ package main
 import (
 	"context"
 	"flag"
+	"os"
 	"strings"
 	"time"
+
+	"go.skia.org/infra/go/sklog/sklogimpl"
+	"go.skia.org/infra/go/sklog/stdlogging"
 
 	"cloud.google.com/go/pubsub"
 	"cloud.google.com/go/storage"
 
+	"go.skia.org/infra/go/common"
 	"go.skia.org/infra/go/fileutil"
 	"go.skia.org/infra/go/gcs"
 	"go.skia.org/infra/go/sklog"
@@ -24,6 +29,8 @@ func main() {
 		srcBucket  = flag.String("src_bucket", "", "Source bucket to ingest files from.")
 		srcRootDir = flag.String("src_root_dir", "dm-json-v1", "Source root directory to ingest files in.")
 
+		changelistIDs = common.NewMultiStringFlag("changelists", nil, "If provided, will only ingest data from the provided changelists")
+
 		// In the early days, there was several invalid entries, because they did not specify
 		// gitHash. Starting re-ingesting Skia on October 1, 2014 seems to be around when
 		// the data is correct.
@@ -34,6 +41,7 @@ func main() {
 		sleepBetweenDays = flag.Duration("sleep_between_days", 0, "If non-zero, the amount of time to wait after reingesting one day's worth of data.")
 	)
 	flag.Parse()
+	sklogimpl.SetLogger(stdlogging.New(os.Stderr))
 
 	ctx := context.Background()
 	gcsClient, err := storage.NewClient(ctx)
@@ -58,12 +66,20 @@ func main() {
 
 	beginning := time.Date(*startYear, time.Month(*startMonth), *startDay, 0, 0, 0, 0, time.UTC)
 
-	dirs := fileutil.GetHourlyDirs(*srcRootDir, beginning, time.Now())
+	root := *srcRootDir
+	if *changelistIDs != nil && len(*changelistIDs) > 0 {
+		sklog.Infof("Only processing cls: %+v", *changelistIDs)
+		root = "trybot/dm-json-v1"
+	}
+
+	dirs := fileutil.GetHourlyDirs(root, beginning, time.Now())
 	for _, dir := range dirs {
 		sklog.Infof("Directory: %q", dir)
 		var last *pubsub.PublishResult
 		err := gcs.AllFilesInDir(gcsClient, *srcBucket, dir, func(item *storage.ObjectAttrs) {
-			last = publishSyntheticStorageEvent(ctx, topic, item.Bucket, item.Name)
+			if matchesChangelist(changelistIDs, item.Name) {
+				last = publishSyntheticStorageEvent(ctx, topic, item.Bucket, item.Name)
+			}
 		})
 		if err != nil {
 			sklog.Warningf("Error while processing dir %s: %s", dir, err)
@@ -87,6 +103,22 @@ func main() {
 	sklog.Infof("waiting for messages to publish")
 	topic.Stop()
 	sklog.Infof("done")
+}
+
+// matchesChangelist returns true if the given file name matches a changelist id. That is,
+// there exists "/[clid]" somewhere in the name. For example:
+//   trybot/dm-json-v1/2021/09/23/02/4140248__1/8835339621082367857/dm-1632364432749558598.json
+// has a match for CL 4140248. It's implemented simply, meant for some adhoc re-ingestion.
+func matchesChangelist(changelistIDs *[]string, name string) bool {
+	if changelistIDs == nil {
+		return true
+	}
+	for _, id := range *changelistIDs {
+		if strings.Contains(name, "/"+id) {
+			return true
+		}
+	}
+	return false
 }
 
 func publishSyntheticStorageEvent(ctx context.Context, topic *pubsub.Topic, bucket, fileName string) *pubsub.PublishResult {
