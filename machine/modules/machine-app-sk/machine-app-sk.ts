a9ab264daccbb1bf3e013e5f50ce0cf7e8c28ebb
Joe Gregorio <jcgregorio@google.com>
2021-10-15 13:32:28 -0400

    [machine] Add stateReflector to machine main page.
    Change-Id: I85f4eeae9643bec6f017d05d22f08e1c274c4141
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/459638
    Commit-Queue: Joe Gregorio <jcgregorio@google.com>
    Reviewed-by: Erik Rose <erikrose@google.com>

--- machine/modules/machine-app-sk/machine-app-sk.ts
+++ machine/modules/machine-app-sk/machine-app-sk.ts
@@ -5,15 +5,17 @@
  * UI wrapper for machineserver readouts which sticks them in tabs and has a
  * shared toolbar
  */
-import { html } from 'lit-html';
+import { html, TemplateResult } from 'lit-html';
 
 import { $$ } from 'common-sk/modules/dom';
 import { define } from 'elements-sk/define';
 import 'elements-sk/error-toast-sk';
 import 'elements-sk/tabs-sk';
 import 'elements-sk/tabs-panel-sk';
-import { TabSelectedSkEventDetail } from 'elements-sk/tabs-sk/tabs-sk';
+import { stateReflector } from 'common-sk/modules/stateReflector';
+import { TabSelectedSkEventDetail, TabsSk } from 'elements-sk/tabs-sk/tabs-sk';
 import { TabsPanelSk } from 'elements-sk/tabs-panel-sk/tabs-panel-sk';
+import { HintableObject } from 'common-sk/modules/hintable';
 import { ElementSk } from '../../../infra-sk/modules/ElementSk';
 import { MachinesTableSk } from '../machines-table-sk';
 import { PodsTableSk } from '../pods-table-sk';
@@ -29,12 +31,29 @@ import '../meeting-points-table-sk';
  */
 type AnyLiveTable = MachinesTableSk | MeetingPointsTableSk | PodsTableSk;
 
+/** The State of our page that gets reflected into the URL. */
+class State {
+  /** Which tab is shown. */
+  tab: number = 0;
+
+  /** The value of the search input element. */
+  search: string = '';
+}
+
 export class MachineAppSk extends ElementSk {
   private _inputElement: HTMLInputElement | null = null;
 
+  /** The state to reflect to the URL. */
+  private state: State = new State();
+
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
+  private stateHasChanged = () => {};
+
   private panel: TabsPanelSk | null = null;
 
-  protected _template = (ele: MachineAppSk) => html`
+  private tabs: TabsSk | null = null;
+
+  protected _template = (ele: MachineAppSk): TemplateResult => html`
     <header>
       <auto-refresh-sk @refresh-page=${ele.update}></auto-refresh-sk>
       <span id=header-rhs>
@@ -57,21 +76,42 @@ export class MachineAppSk extends ElementSk {
     <error-toast-sk></error-toast-sk>
   `;
 
-  async connectedCallback(): Promise<void> {
+  connectedCallback(): void {
     super.connectedCallback();
     this._render();
     this._inputElement = $$<HTMLInputElement>('#filter-input', this)!;
     this.panel = $$<TabsPanelSk>('tabs-panel-sk', this);
+    this.tabs = $$<TabsSk>('tabs-sk', this);
     this.update(WaitCursor.SHOW);
+    this.stateHasChanged = stateReflector(
+      () => this.state as unknown as HintableObject,
+      (hintableState) => {
+        const state = hintableState as unknown as State;
+        this.state = state;
+        this.setSelectedTableIndex(this.state.tab);
+        this.update();
+
+        this._inputElement!.value = this.state.search;
+        this.propagateFilterChange();
+        this._render();
+      },
+    );
+  }
+
+  /** Handle input changed event for the filter element. */
+  private filterChanged(): void {
+    this.propagateFilterChange();
+    this.state.search = this._inputElement!.value;
+    this.stateHasChanged();
   }
 
   /** Tell the active table to filter itself. */
-  filterChanged() {
+  private propagateFilterChange(): void {
     this.selectedTable().filterChanged(this._inputElement!.value);
   }
 
   /** Tell the active table to fetch new data and redraw itself. */
-  update(waitCursorPolicy = WaitCursor.DO_NOT_SHOW) {
+  private update(waitCursorPolicy = WaitCursor.DO_NOT_SHOW): void {
     this.selectedTable().update(waitCursorPolicy);
   }
 
@@ -84,12 +124,25 @@ export class MachineAppSk extends ElementSk {
     // I feel like updating is more useful than maintaining stale data, but
     // experience will tell. Show wait cursor so the user knows we're updating.
     this.update(WaitCursor.SHOW);
+
+    this.state.tab = this.getSelectedTableIndex();
+    this.stateHasChanged();
   }
 
   /** Return the table from the tab that is in the foreground. */
   private selectedTable(): AnyLiveTable {
     const n = this.panel!.selected;
     return this.panel!.children[n] as AnyLiveTable;
+  }
+
+  /** Returns the index of the tab panel being displayed. */
+  private getSelectedTableIndex(): number {
+    return this.panel!.selected;
+  }
+
+  /** Set the tab panel being displayed based on it's index. */
+  private setSelectedTableIndex(index: number) {
+    this.tabs!.select(index);
   }
 }
 
