e54cff55054cdcce7c95debe868b1d42e83d1eb8
Leandro Lovisolo <lovisolo@google.com>
2021-11-04 21:13:17 -0400

    [test_machine_monitor] Start Swarming under Windows.
    Bug: skia:12618
    Change-Id: Id3cf63d1c346f7d92d17706218a88b19e2b7f87b
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/467981
    Reviewed-by: Joe Gregorio <jcgregorio@google.com>
    Commit-Queue: Leandro Lovisolo <lovisolo@google.com>

--- skolo/ansible/switchboard/roles/install_test_machine_monitor/templates/test_machine_monitor.ps1
+++ skolo/ansible/switchboard/roles/install_test_machine_monitor/templates/test_machine_monitor.ps1
@@ -27,8 +27,20 @@ else {
     Write-Host "Running existing [$oldfile], no newer version found."
 }
 
+# Set environment.
+{% if install_test_machine_monitor__start_swarming is true %}
+$Env:SWARMING_BOT_ID = '{{ ansible_facts['hostname'] }}'
+$Env:SWARMING_EXTERNAL_BOT_SETUP = 1
+{% endif %}
+
 # Launch test_machine_monitor.
 .\test_machine_monitor.exe `
   --config=prod.json `
   --prom_port=:{{ all.prometheus.monitoring.ports.test_machine_monitor}} `
+  --metadata_url=http://metadata:{{ all.metadata_server_port }}/computeMetadata/v1/instance/service-accounts/default/token `
+  {% if install_test_machine_monitor__start_swarming is true %}
+  --python_exe={{ win_python3_path }}\python.exe `
+  --start_swarming `
+  --swarming_bot_zip=C:\b\s\swarming_bot.zip `
+  {% endif %}
   --username=chrome-bot
