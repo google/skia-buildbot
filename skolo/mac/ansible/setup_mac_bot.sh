57cffa066f17567c2cc47e6d14dec3ae29dbf2c4
Erik Rose <erikrose@google.com>
2021-12-08 11:38:03 -0500

    [skolo] Remove obsolete Mac ansible scripts.
    These have been replaced with switchboard/mac.yml.
    
    Change-Id: I661543615f82be2bb2bc7810017318f02e7d4fe2
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/488357
    Reviewed-by: Joe Gregorio <jcgregorio@google.com>

--- skolo/mac/ansible/setup_mac_bot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-if [ "$#" != "1" ]; then
-  >&2 echo "Usage: $0 <ip addr>"
-  exit 1
-fi
-
-ansible-playbook -i "$1," setup-skolo-bot.yml --ask-become-pass --extra-vars="swarming_server=https://chromium-swarm.appspot.com home=/Users/chrome-bot"
