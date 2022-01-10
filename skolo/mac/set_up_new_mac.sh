69477ccd2d2999010fcc75165c9e511dbd7ec58b
Erik Rose <erikrose@google.com>
2022-01-10 11:35:58 -0500

    [skolo] Stop Macs sleeping earlier, and reboot new ones after first ansible-ing them.
    Disabling sleep even earlier reduces the chances that they'll sleep
    before taking the rest of their medicine.
    Change-Id: I4f4656eacc3bb475b974f06a7faf0098f81ba215
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/493040
    Reviewed-by: Joe Gregorio <jcgregorio@google.com>
    Commit-Queue: Erik Rose <erikrose@google.com>

--- skolo/mac/set_up_new_mac.sh
+++ skolo/mac/set_up_new_mac.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Set up a Mac before it's had its ssh keys installed. This is just like
-# updating an existing Mac except that we use passwords.
+# updating an existing Mac except that we use passwords and we reboot afterward.
+set -e
 
 if [ "$#" != "1" ]; then
   >&2 echo "Usage: $0 <hostname>"
@@ -8,3 +9,5 @@ if [ "$#" != "1" ]; then
 fi
 
 ansible-playbook -i ../ansible/hosts.yml -l "$1," ../ansible/switchboard/mac.yml --ask-pass --ask-become-pass
+# Reboot so screensaver disabling takes effect; it's the cheapest way to log out and in again:
+ansible -i ../ansible/hosts.yml -m reboot --become "$1,"
