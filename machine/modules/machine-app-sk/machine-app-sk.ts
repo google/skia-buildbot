c54a4bdc67ff6194def13a9c479c4cabb61ecf04
Joe Gregorio <jcgregorio@google.com>
2022-01-20 16:39:34 -0500

    [machineserver] Update to infra-sk/theme.
    Also drop the Pods and MeetingPoints tables as it looks like
    we may not need Switchboard.
    
    Leaving the rest of the code in tact in case we change our minds.
    
    Change-Id: I7e0e5a110871f78d6314c607691978c2c1d4369f
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/497281
    Reviewed-by: Ravi Mistry <rmistry@google.com>
    Commit-Queue: Joe Gregorio <jcgregorio@google.com>

--- machine/modules/machine-app-sk/machine-app-sk.ts
+++ machine/modules/machine-app-sk/machine-app-sk.ts
@@ -13,29 +13,18 @@ import 'elements-sk/error-toast-sk';
 import 'elements-sk/tabs-sk';
 import 'elements-sk/tabs-panel-sk';
 import { stateReflector } from 'common-sk/modules/stateReflector';
-import { TabSelectedSkEventDetail, TabsSk } from 'elements-sk/tabs-sk/tabs-sk';
-import { TabsPanelSk } from 'elements-sk/tabs-panel-sk/tabs-panel-sk';
 import { HintableObject } from 'common-sk/modules/hintable';
 import { ElementSk } from '../../../infra-sk/modules/ElementSk';
 import { MachinesTableSk } from '../machines-table-sk';
-import { PodsTableSk } from '../pods-table-sk';
-import { MeetingPointsTableSk } from '../meeting-points-table-sk';
 import { WaitCursor } from '../live-table-sk';
 import '../machines-table-sk';
 import '../pods-table-sk';
 import '../meeting-points-table-sk';
-
-/**
- * Any of the readouts that are on our tabs. This could turn into an interface
- * if we diversify beyond LiveTableSk subclasses.
- */
-type AnyLiveTable = MachinesTableSk | MeetingPointsTableSk | PodsTableSk;
+import '../../../infra-sk/modules/theme-chooser-sk';
+import '../../../infra-sk/modules/app-sk';
 
 /** The State of our page that gets reflected into the URL. */
 class State {
-  /** Which tab is shown. */
-  tab: number = 0;
-
   /** The value of the search input element. */
   search: string = '';
 }
@@ -49,46 +38,38 @@ export class MachineAppSk extends ElementSk {
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   private stateHasChanged = () => {};
 
-  private panel: TabsPanelSk | null = null;
-
-  private tabs: TabsSk | null = null;
+  private machinesTable: MachinesTableSk | null = null;
 
   protected _template = (ele: MachineAppSk): TemplateResult => html`
-    <header>
-      <auto-refresh-sk @refresh-page=${ele.update}></auto-refresh-sk>
-      <span id=header-rhs>
-        <input id="filter-input" @input=${ele.filterChanged} type="text" placeholder="Filter">
-        <theme-chooser-sk title="Toggle between light and dark mode."></theme-chooser-sk>
-      </span>
-    </header>
-    <main>
-      <tabs-sk @tab-selected-sk=${ele.tabSwitched}>
-        <button class="selected">Machines</button>
-        <button>Pods</button>
-        <button>Meeting Points</button>
-      </tabs-sk>
-      <tabs-panel-sk>
+    <app-sk>
+      <header>
+        <span>
+          <auto-refresh-sk @refresh-page=${ele.update}></auto-refresh-sk>
+          <h1><a href="/">Machines</a></h1>
+        </span>
+        <span id=header-rhs>
+          <input id="filter-input" @input=${ele.filterChanged} type="text" placeholder="Filter">
+          <theme-chooser-sk title="Toggle between light and dark mode."></theme-chooser-sk>
+        </span>
+      </header>
+      <main>
         <machines-table-sk></machines-table-sk>
-        <pods-table-sk></pods-table-sk>
-        <meeting-points-table-sk></meeting-points-table-sk>
-      </tabs-panel-sk>
-    </main>
-    <error-toast-sk></error-toast-sk>
+      </main>
+      <error-toast-sk></error-toast-sk>
+    </app-sk>
   `;
 
   connectedCallback(): void {
     super.connectedCallback();
     this._render();
     this._inputElement = $$<HTMLInputElement>('#filter-input', this)!;
-    this.panel = $$<TabsPanelSk>('tabs-panel-sk', this);
-    this.tabs = $$<TabsSk>('tabs-sk', this);
+    this.machinesTable = $$<MachinesTableSk>('machines-table-sk', this);
     this.update(WaitCursor.SHOW);
     this.stateHasChanged = stateReflector(
       () => this.state as unknown as HintableObject,
       (hintableState) => {
         const state = hintableState as unknown as State;
         this.state = state;
-        this.setSelectedTableIndex(this.state.tab);
         this.update();
 
         this._inputElement!.value = this.state.search;
@@ -107,42 +88,12 @@ export class MachineAppSk extends ElementSk {
 
   /** Tell the active table to filter itself. */
   private propagateFilterChange(): void {
-    this.selectedTable().filterChanged(this._inputElement!.value);
+    this.machinesTable?.filterChanged(this._inputElement!.value);
   }
 
   /** Tell the active table to fetch new data and redraw itself. */
   private update(waitCursorPolicy = WaitCursor.DO_NOT_SHOW): void {
-    this.selectedTable().update(waitCursorPolicy);
-  }
-
-  /** Keep the UI stable as we change tabs. */
-  private tabSwitched(_: CustomEvent<TabSelectedSkEventDetail>) {
-    // Bring the filtration on the tab up to date, in case it changed while the
-    // tab was hidden:
-    this.filterChanged();
-
-    // I feel like updating is more useful than maintaining stale data, but
-    // experience will tell. Show wait cursor so the user knows we're updating.
-    this.update(WaitCursor.SHOW);
-
-    this.state.tab = this.getSelectedTableIndex();
-    this.stateHasChanged();
-  }
-
-  /** Return the table from the tab that is in the foreground. */
-  private selectedTable(): AnyLiveTable {
-    const n = this.panel!.selected;
-    return this.panel!.children[n] as AnyLiveTable;
-  }
-
-  /** Returns the index of the tab panel being displayed. */
-  private getSelectedTableIndex(): number {
-    return this.panel!.selected;
-  }
-
-  /** Set the tab panel being displayed based on it's index. */
-  private setSelectedTableIndex(index: number) {
-    this.tabs!.select(index);
+    this.machinesTable?.update(waitCursorPolicy);
   }
 }
 
