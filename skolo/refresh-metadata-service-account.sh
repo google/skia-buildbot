e7a7923a2160e2d324b235e48c190ebc48c635d4
Joe Gregorio <jcgregorio@google.com>
2022-01-20 08:47:42 -0500

    Fix service accounts used for metadata_server_ansible.
    Change-Id: I28a331160dd686dcb1f8ec3728538a336b793ee6
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/496889
    Reviewed-by: Ravi Mistry <rmistry@google.com>

--- skolo/refresh-metadata-service-account.sh
+++ skolo/refresh-metadata-service-account.sh
@@ -2,8 +2,10 @@
 
 set -e
 
-../kube/secrets/generate-new-key-for-service-account.sh skia-public etc skolo-service-accounts
+../kube/secrets/generate-new-key-for-service-account.sh skia-swarming-bots etc chromium-swarm-bots
+../kube/secrets/generate-new-key-for-service-account.sh skia-buildbots etc chrome-swarming-bots
 
 printf 'You should now run:\n'
 printf '    cd ansible\n'
+printf '    ansible-playbook ./switchboard/build_and_release_metadata_server_ansible.yml\n'
 printf '    ansible-playbook ./switchboard/jumphosts.yml\n'
