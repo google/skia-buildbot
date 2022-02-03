144f4744bfbba19661069b3906928c93c15232e1
John Stiles <johnstiles@google.com>
2022-02-03 10:58:35 -0500

    Add attribution to default shader on shaders.skia.org.
    Same shader as before, but with a comment on line 1:
    // Source: @notargs https://twitter.com/notargs/status/1250468645030858753
    
    Change-Id: I9dd2dfd9b91866f39ce1297c036ecf8270ff7873
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/503837
    Auto-Submit: John Stiles <johnstiles@google.com>
    Reviewed-by: Brian Osman <brianosman@google.com>
    Commit-Queue: Brian Osman <brianosman@google.com>

--- shaders/create-named-scraps.sh
+++ shaders/create-named-scraps.sh
@@ -15,7 +15,7 @@ curl --silent -X PUT -d "{\"Hash\": \"8faf0a93848175d382bc8feaef584339b75a911aa7
 curl --silent -X PUT -d "{\"Hash\": \"3124d06f75fa4a5138784e80592909d094a50dfd7a53aed82f660c1c021fa628\", \"Description\": \"Shader Inputs\"}" -H 'Content-Type: application/json' http://localhost:9000/_/names/sksl/@iResolution
 curl --silent -X PUT -d "{\"Hash\": \"12f6c3ecf3f26b1b734d0f254b5bb97cb7d395a9e4829fbab9cda3fef9e3ad9e\", \"Description\": \"Shader Inputs\"}" -H 'Content-Type: application/json' http://localhost:9000/_/names/sksl/@iTime
 curl --silent -X PUT -d "{\"Hash\": \"229312432fbd5c54b471d766a0da29cd7d22f53b7a9a90f3b014a87114d02dd1\", \"Description\": \"Shader Inputs\"}" -H 'Content-Type: application/json' http://localhost:9000/_/names/sksl/@iMouse
-curl --silent -X PUT -d "{\"Hash\": \"76819961bb22c1982492ed57a3972f855a01026b0686d636bd23773f4855d218\", \"Description\": \"Shader Inputs\"}" -H 'Content-Type: application/json' http://localhost:9000/_/names/sksl/@default
+curl --silent -X PUT -d "{\"Hash\": \"f1be7449cdd7b15ab39efa681f5191bbaa55b1c58f582cb16c55186dd95a24e0\", \"Description\": \"Shader Inputs\"}" -H 'Content-Type: application/json' http://localhost:9000/_/names/sksl/@default
 curl --silent -X PUT -d "{\"Hash\": \"498ba63d9e91c960fd7d242ba4d9e68ce16fbf1396aac2820f005a8464daac94\", \"Description\": \"Shader Inputs\"}" -H 'Content-Type: application/json' http://localhost:9000/_/names/sksl/@iImage
 curl --silent -X PUT -d "{\"Hash\": \"498ba63d9e91c960fd7d242ba4d9e68ce16fbf1396aac2820f005a8464daac94\", \"Description\": \"Shader Inputs\"}" -H 'Content-Type: application/json' http://localhost:9000/_/names/sksl/@defaultChildShader
 
