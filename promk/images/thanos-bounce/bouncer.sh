9fd5953b70abc9083a66434503213f3ae12dded4
Eric Boren <borenet@google.com>
2022-03-10 12:30:21 -0500

    [thanos-bounce] Fixes for new cluster
    Bug: skia:12544
    Change-Id: I0f3bf6e5752e15396306e4e41c2ed5a6d5938f7a
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/519418
    Commit-Queue: Eric Boren <borenet@google.com>
    Auto-Submit: Eric Boren <borenet@google.com>
    Reviewed-by: Joe Gregorio <jcgregorio@google.com>
    Commit-Queue: Joe Gregorio <jcgregorio@google.com>

--- promk/images/thanos-bounce/bouncer.sh
+++ promk/images/thanos-bounce/bouncer.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
-echo "Starting bouncer.sh"
 
-# KUBECONFIG should be set in yaml file.
-# PORT_ON_THANOS_QUERY should be set in the yaml file.
+# The following variables should be set in the yaml file:
+# - KUBECONFIG
+# - PORT_ON_THANOS_QUERY
+# - K8S_SERVER
+# - CLOUDSDK_COMPUTE_ZONE
+# - CLOUDSDK_CONTAINER_CLUSTER
+# - CLOUDSDK_COMPUTE_REGION
+
+set -xe
+
+echo "Starting bouncer.sh"
 
 # Echo something useful into the logs.
 kubectl config get-clusters
 
 # Set up the reverse port-forward.
-kubectl get pod -l app=thanos-query -o jsonpath="{.items[0].metadata.name}"
+SERVER_FLAG=""
+if [ -n "$K8S_SERVER" ]; then
+  SERVER_FLAG="--server=${K8S_SERVER}"
+fi
+kubectl get pod ${SERVER_FLAG} -l app=thanos-query -o jsonpath="{.items[0].metadata.name}"
 nc -vv 127.0.0.1 9001 -c "kubectl exec -i $(kubectl get pod -l app=thanos-query -o jsonpath=\"{.items[0].metadata.name}\") -- nc -vv -l -p ${PORT_ON_THANOS_QUERY}"
