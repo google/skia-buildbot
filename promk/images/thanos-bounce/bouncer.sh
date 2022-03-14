91a7e153e0164cad4be72a74e01270499c067d73
Eric Boren <borenet@google.com>
2022-03-14 09:55:16 -0400

    [thanos-bounce] Use Bazel to build image, based on Cloud Builders kubectl
    Change-Id: I0aa68c9fe785805c3654aba4bb7159701c5b756e
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/520156
    Reviewed-by: Joe Gregorio <jcgregorio@google.com>
    Commit-Queue: Eric Boren <borenet@google.com>

--- promk/images/thanos-bounce/bouncer.sh
+++ promk/images/thanos-bounce/bouncer.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
+
+# The following variables should be set in the yaml file:
+# - KUBECONFIG
+# - PORT_ON_THANOS_QUERY
+# - CLOUDSDK_COMPUTE_REGION
+# - CLOUDSDK_COMPUTE_ZONE
+# - CLOUDSDK_CONTAINER_CLUSTER
+# - CLOUDSDK_CORE_PROJECT
+
 echo "Starting bouncer.sh"
 
-# KUBECONFIG should be set in yaml file.
-# PORT_ON_THANOS_QUERY should be set in the yaml file.
+KUBECTL=/builder/kubectl.bash
 
 # Echo something useful into the logs.
-kubectl config get-clusters
+${KUBECTL} config get-clusters
+${KUBECTL} config view --raw
 
 # Set up the reverse port-forward.
-kubectl get pod -l app=thanos-query -o jsonpath="{.items[0].metadata.name}"
-nc -vv 127.0.0.1 9001 -c "kubectl exec -i $(kubectl get pod -l app=thanos-query -o jsonpath=\"{.items[0].metadata.name}\") -- nc -vv -l -p ${PORT_ON_THANOS_QUERY}"
+${KUBECTL} get pod -l app=thanos-query -o jsonpath="{.items[0].metadata.name}"
+nc -vv 127.0.0.1 9001 -c "${KUBECTL} exec -i $(${KUBECTL} get pod -l app=thanos-query -o jsonpath=\"{.items[0].metadata.name}\") -- nc -vv -l -p ${PORT_ON_THANOS_QUERY}"
