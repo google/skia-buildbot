bc7ef5080826d69b20863dec7ab27544dabc3ea7
Eric Boren <borenet@google.com>
2022-03-15 11:57:51 -0400

    [thanos-bounce] Update image requirements, remove kubeconfig.yaml
    Bug: skia:12544
    Change-Id: I685bc33c5c017a71af035fe20164ddd85700622e
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/520999
    Reviewed-by: Joe Gregorio <jcgregorio@google.com>
    Commit-Queue: Eric Boren <borenet@google.com>

--- promk/images/thanos-bounce/bouncer.sh
+++ promk/images/thanos-bounce/bouncer.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # The following variables should be set in the yaml file:
-# - KUBECONFIG
 # - PORT_ON_THANOS_QUERY
 # - CLOUDSDK_COMPUTE_REGION
 # - CLOUDSDK_COMPUTE_ZONE
@@ -10,12 +9,12 @@
 
 echo "Starting bouncer.sh"
 
-KUBECTL=/builder/kubectl.bash
+KUBECTL_WRAPPER=/builder/kubectl.bash
 
-# Echo something useful into the logs.
-${KUBECTL} config get-clusters
-${KUBECTL} config view --raw
+# Echo something useful into the logs. Use the wrapper to set up authentication.
+${KUBECTL_WRAPPER} config get-clusters
+${KUBECTL_WRAPPER} config view --raw
 
 # Set up the reverse port-forward.
-${KUBECTL} get pod -l app=thanos-query -o jsonpath="{.items[0].metadata.name}"
-nc -vv 127.0.0.1 9001 -c "${KUBECTL} exec -i $(${KUBECTL} get pod -l app=thanos-query -o jsonpath=\"{.items[0].metadata.name}\") -- nc -vv -l -p ${PORT_ON_THANOS_QUERY}"
+kubectl get pod --namespace=default -l app=thanos-query -o jsonpath="{.items[0].metadata.name}"
+nc -vv 127.0.0.1 9001 -c "kubectl exec -i --namespace=default $(kubectl get pod --namespace=default -l app=thanos-query -o jsonpath=\"{.items[0].metadata.name}\") -- nc -vv -l -p ${PORT_ON_THANOS_QUERY}"
