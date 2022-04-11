3fe98fb2b729ab9147f8d0115d7546d1e9ef2760
Joe Gregorio <jcgregorio@google.com>
2022-04-11 11:40:28 -0400

    [comp-ui] Add a mock script that emits fake benchmark results.
    Bug: skia:13089
    Change-Id: I09df7ac30a1e3818b4f38ea8e004f0cff6c3328c
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/529117
    Reviewed-by: Kevin Lubick <kjlubick@google.com>

---
+++ comp-ui/README.md
@@ -0,0 +1,6 @@
+# Competitive Performance UI
+
+Run popular browser benchmarks on a range of browsers and store the results in a
+Perf instance.
+
+[Design Doc](http://go/comp-ui)
---
+++ comp-ui/benchmark-mock.py
@@ -0,0 +1,85 @@
+# Mock of browserbench Python driver script for testing purposes.
+#
+# See
+# https://chromium.googlesource.com/chromium/src/+/refs/heads/main/tools/browserbench-webdriver/motionmark.py
+#
+# This mock takes the same arguments and emits valid Perf ingestion files with
+# random values.
+#
+# Example:
+#
+#    python3 benchmark-mock.py  --output=$HOME/standin.json --browser=mock
+from optparse import OptionParser
+
+import json
+import random
+
+
+def ParseArgs():
+  parser = OptionParser()
+  parser.add_option('-b',
+                    '--browser',
+                    dest='browser',
+                    help='The browser to use to run MotionMark in.')
+  parser.add_option('-s',
+                    '--suite',
+                    dest='suite',
+                    help='Run only the specified suite of tests.')
+  parser.add_option('-e',
+                    '--executable-path',
+                    dest='executable',
+                    help='Path to the executable to the driver binary.')
+  parser.add_option('-a',
+                    '--arguments',
+                    dest='arguments',
+                    help='Extra arguments to pass to the browser.')
+  parser.add_option('-g',
+                    '--githash',
+                    dest='githash',
+                    help='A git-hash associated with this run.')
+  parser.add_option('-o',
+                    '--output',
+                    dest='output',
+                    help='Path to the output json file.')
+  (optargs, _) = parser.parse_args()
+  optargs.suite = optargs.suite or 'Mock'
+  optargs.githash = optargs.githash or 'deadbeef'
+  return optargs
+
+def ProduceOutput(data, output_file):
+  print(json.dumps(data, sort_keys=True, indent=2, separators=(',', ': ')))
+  if output_file:
+    with open(output_file, 'w') as out:
+      out.write(json.dumps(data))
+
+def _extractScore():
+  return [{
+      'value': 'score',
+      'measurement': random.random(),
+  }]
+
+
+def main():
+  random.seed()
+  optargs = ParseArgs()
+
+  results = {
+      'version': 1,
+      'git_hash': optargs.githash,
+      "results": [
+        {
+          'key': {
+              'test': 'mock',
+              'browser': optargs.browser,
+          },
+          'measurements': {
+              'score': _extractScore(),
+          },
+        }
+      ]
+  }
+
+  ProduceOutput(results, optargs.output)
+
+if __name__ == '__main__':
+  main()
