cad53a167f48eb4c4b3365b820ba1346ddd4f486
Joe Gregorio <jcgregorio@google.com>
2022-05-16 09:00:32 -0400

    [skolo] Fix relative dirs in skolo/raspberry-pi/configure-image-impl.sh
    Change-Id: Ie3a4af07deaa1bbf21ad3b8afecc1759d5c03869
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/540816
    Commit-Queue: Joe Gregorio <jcgregorio@google.com>
    Reviewed-by: Ravi Mistry <rmistry@google.com>

--- skolo/raspberry-pi/configure-image-impl.sh
+++ skolo/raspberry-pi/configure-image-impl.sh
@@ -17,8 +17,8 @@ echo ${HOSTNAME} > ${MOUNT}/etc/hostname
 if [ "$(uname -s)" = Darwin ]; then
     # The Mac version of install acts a bit differently.
     mkdir -p ${MOUNT}/root/.ssh
-    install -m 600 ${REL}/../../authorized_keys ${MOUNT}/root/.ssh/authorized_keys
+    install -m 600 ${REL}/../authorized_keys ${MOUNT}/root/.ssh/authorized_keys
 else
-    install -D --mode=600 ${REL}/../../authorized_keys ${MOUNT}/root/.ssh/authorized_keys
+    install -D --mode=600 ${REL}/../authorized_keys ${MOUNT}/root/.ssh/authorized_keys
 fi
 sync --file-system ${MOUNT}/root/.ssh/authorized_keys
