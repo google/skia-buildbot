2dc2af79bcb55dbcbc8dd7ea893b6b3b241842b2
Kevin Lubick <kjlubick@google.com>
2022-08-16 09:20:52 -0400

    Create a search bar that shows all matching node search results.
    Follow-up to: https://skia-review.googlesource.com/c/buildbot/+/566370
    
    Change-Id: I2672a69937616afd2b00d4502df4f3f386c89556
    Bug: skia:13616
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/568173
    Commit-Queue: Kevin Lubick <kjlubick@google.com>
    Reviewed-by: Jorge Betancourt <jmbetancourt@google.com>

--- codesize/modules/binary-page-sk/binary-page-sk.ts
+++ codesize/modules/binary-page-sk/binary-page-sk.ts
@@ -1,7 +1,7 @@
 /** Shows code size statistics about a single binary. */
 
 import { define } from 'elements-sk/define';
-import { html } from 'lit-html';
+import { html, TemplateResult } from 'lit-html';
 import { load } from '@google-web-components/google-chart/loader';
 import { jsonOrThrow } from 'common-sk/modules/jsonOrThrow';
 import { ElementSk } from '../../../infra-sk/modules/ElementSk';
@@ -10,6 +10,7 @@ import { CodesizeScaffoldSk } from '../codesize-scaffold-sk/codesize-scaffold-sk
 import { BloatyOutputMetadata, BinaryRPCRequest, BinaryRPCResponse } from '../rpc_types';
 import '../../../infra-sk/modules/human-date-sk';
 import '@google-web-components/google-chart/';
+import Fuse from 'fuse.js';
 
 export class BinaryPageSk extends ElementSk {
   private static template = (el: BinaryPageSk) => {
@@ -49,10 +50,71 @@ export class BinaryPageSk extends ElementSk {
       <ul>
         <li><strong>Click</strong> on a node to navigate down the tree.</li>
         <li><strong>Right click</strong> anywhere on the treemap go back up one level.</li>
+        <li><strong> Use the seach bar</strong> to navigate to a node within the tree</li>
+
       </ul>
+
+      <div class="search-bar">
+        <input type="search" placeholder="Search for node..." aria-label="Search for node..."
+        autocomplete="on" @input=${el.search} @change=${el.retrieve}>
+        <ol id="searchSuggestions" class="search-match-list">${el.listHTML(el.listOfSearchResults)}
+        </ol>
+      </div>
 
       <div id="treemap"></div>
     `;
+  }
+
+  private tree: google.visualization.TreeMap | null = null;
+  private fuse: Fuse<string[]> | null = null;
+  private selectionIndex: number | null = null;
+  private listOfSearchResults: Fuse.FuseResult<string[]>[] = [];
+
+  /**
+   * Uses Fuse.js to match a users input to a node within the tree. Finds all matches
+   * @param e an event triggered by a user typing in the searchbar
+   */
+  private search(e: Event):void {
+    if (!this.fuse) {
+      return;
+    }
+    const target = e.target as HTMLInputElement;
+    this.listOfSearchResults = this.fuse.search({fileName: target.value});
+
+    if(this.listOfSearchResults.length === 0) {
+      this.selectionIndex = -1;
+    } else {
+      this.selectionIndex = this.listOfSearchResults[0].refIndex - 1;
+    }
+    this._render();
+  }
+
+  /**
+   * @param results fuse results.
+   * @returns a list of all of the matching node names from the search results.
+   */
+  private listHTML = (results: Fuse.FuseResult<string[]>[]):TemplateResult => {
+    return html `
+      ${results.map((match:Fuse.FuseResult<string[]>) =>
+         html `
+          <li class= "search-match-list-item">
+            ${match.item[0]}
+          </li>
+        `)}
+      `
+  }
+
+  /**
+   * Retrieves the node the user searched for in the searchbar.
+   * Does nothing if the selection index is -1 meaning that the user hasn't entered
+   * a new search entry
+   * @param e when a user presses enter
+   */
+  private retrieve(e: Event): void {
+      if(!this.tree || this.selectionIndex == -1) {
+        return;
+      }
+      this.tree.setSelection([{column: null, row: this.selectionIndex}]);
   }
 
   private metadata: BloatyOutputMetadata | null = null;
@@ -97,26 +159,41 @@ export class BinaryPageSk extends ElementSk {
       ]),
     ];
     const data = google.visualization.arrayToDataTable(rows);
-    const tree = new google.visualization.TreeMap(this.querySelector('#treemap')!);
+    this.tree = new google.visualization.TreeMap(this.querySelector('#treemap')!);
 
     // For some reason the type definition for TreeMapOptions does not include the generateTooltip
     // option (https://developers.google.com/chart/interactive/docs/gallery/treemap#tooltips), so
     // a type assertion is necessary to keep the TypeScript compiler happy.
     //TODO (anjulij): add categorical coloring
-    const options = {
+    const treeOptions = {
       generateTooltip: showTooltip,
       minColor: '#E8DAFF',
       midColor: '#E8DAFF',
       maxColor: '#E8DAFF',
     } as google.visualization.TreeMapOptions;
 
+    const searchOptions = {
+      isCaseSensitive: false,
+      //Ignore single character matches
+      minMatchCharLength: 1,
+      findAllMatches: true,
+      //At what point does the algorithm gives up (0.0 is a perfect match)
+      threshold: 0.6,
+      limit: 10,
+      keys: [
+        { name: 'fileName', getFn: (rowT:string[]):string => rowT[0]}
+      ]
+    };
+
+    this.fuse = new Fuse(rows as unknown as string[][], searchOptions);
+
     // Draw the tree and wait until the tree finishes drawing.
     await new Promise((resolve) => {
-      google.visualization.events.addOneTimeListener(tree, 'ready', resolve);
-      tree.draw(data, options);
+      google.visualization.events.addOneTimeListener(this.tree, 'ready', resolve);
+      this.tree!.draw(data, treeOptions);
       document.addEventListener('theme-chooser-toggle', () => {
         // if a user toggles the theme to/from darkmode then redraw
-        tree.draw(data, options);
+        this.tree!.draw(data, treeOptions);
       });
     });
 
