f527dfaa202768879b5678e7abfc21f6b5658fa6
Leandro Lovisolo <lovisolo@google.com>
2022-09-02 17:22:48 +0000

    Housekeeper-OnDemand-Presubmit: Simplify task driver.
    For consistency with the other Bazel tasks, this CL makes the task driver use RBE to compile the presubmit binary.
    
    Change-Id: I32055d6a88b8dc63f8090c199c141879598aec18
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/576519
    Reviewed-by: Kevin Lubick <kjlubick@google.com>
    Commit-Queue: Leandro Lovisolo <lovisolo@google.com>

--- infra/bots/task_drivers/presubmit/presubmit.go
+++ infra/bots/task_drivers/presubmit/presubmit.go
@@ -1,12 +1,9 @@
 package main
 
 import (
-	"context"
 	"flag"
-	"fmt"
 	"path/filepath"
 
-	sk_exec "go.skia.org/infra/go/exec"
 	"go.skia.org/infra/task_driver/go/lib/bazel"
 	"go.skia.org/infra/task_driver/go/lib/checkout"
 	"go.skia.org/infra/task_driver/go/lib/os_steps"
@@ -15,13 +12,11 @@ import (
 
 var (
 	// Required properties for this task.
-	projectID     = flag.String("project_id", "", "ID of the Google Cloud project.")
-	taskID        = flag.String("task_id", "", "ID of this task.")
-	taskName      = flag.String("task_name", "", "Name of the task.")
-	workDirFlag   = flag.String("workdir", ".", "Working directory.")
-	rbe           = flag.Bool("rbe", false, "Whether to run Bazel on RBE or locally.")
-	rbeKey        = flag.String("rbe_key", "", "Path to the service account key to use for RBE.")
-	ramdiskSizeGb = flag.Int("ramdisk_gb", 40, "Size of ramdisk to use, in GB.")
+	projectID   = flag.String("project_id", "", "ID of the Google Cloud project.")
+	taskID      = flag.String("task_id", "", "ID of this task.")
+	taskName    = flag.String("task_name", "", "Name of the task.")
+	workDirFlag = flag.String("workdir", ".", "Working directory.")
+	rbeKey      = flag.String("rbe_key", "", "Path to the service account key to use for RBE.")
 
 	checkoutFlags = checkout.SetupFlags(nil)
 
@@ -49,41 +44,18 @@ func main() {
 		td.Fatal(ctx, err)
 	}
 	repoPath := filepath.Join(workDir, "repo")
-	if _, err = checkout.EnsureGitCheckout(ctx, repoPath, repoState); err != nil {
+	gitDir, err := checkout.EnsureGitCheckout(ctx, repoPath, repoState)
+	if err != nil {
 		td.Fatal(ctx, err)
 	}
 
 	// Set up Bazel.
-	opts := bazel.BazelOptions{
-		// We want the cache to be on a bigger disk than default. The root disk, where the home
-		// directory (and default Bazel cache) lives, is only 15 GB on our GCE VMs.
-		CachePath: "/mnt/pd0/bazel_cache",
-	}
-	if err := bazel.EnsureBazelRCFile(ctx, opts); err != nil {
+	bzl, err := bazel.New(ctx, gitDir.Dir(), *local, *rbeKey)
+	if err != nil {
 		td.Fatal(ctx, err)
 	}
 
-	if err := bazelCmd(ctx, repoPath, "run", "//cmd/presubmit", "--", "--commit"); err != nil {
+	if _, err = bzl.DoOnRBE(ctx, "run", "//cmd/presubmit", "--", "--commit"); err != nil {
 		td.Fatal(ctx, err)
 	}
-}
-
-// bazelCmd invokes the given Bazel command in the provided directory.
-func bazelCmd(ctx context.Context, checkoutDir string, args ...string) error {
-	step := fmt.Sprintf("bazel %s", args)
-	return td.Do(ctx, td.Props(step), func(ctx context.Context) error {
-		runCmd := &sk_exec.Command{
-			Name:       "bazelisk",
-			Args:       args,
-			InheritEnv: true, // Makes sure bazelisk is on PATH
-			Dir:        checkoutDir,
-			LogStdout:  true,
-			LogStderr:  true,
-		}
-		_, err := sk_exec.RunCommand(ctx, runCmd)
-		if err != nil {
-			return err
-		}
-		return nil
-	})
 }
