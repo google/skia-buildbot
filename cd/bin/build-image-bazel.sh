02b6ac96fe80c3f93f43775cb71b69457b2a75c2
Eric Boren <borenet@google.com>
2022-09-20 16:09:08 -0400

    [BCID] Build using RBE in Louhi
    I'm not sure why this didn't make it over from the other repo.
    
    Change-Id: I5088d08cf6933326cf84f5f6374b993a3cbdc096
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/582819
    Reviewed-by: Ravi Mistry <rmistry@google.com>
    Commit-Queue: Eric Boren <borenet@google.com>
    Auto-Submit: Eric Boren <borenet@google.com>
    Commit-Queue: Ravi Mistry <rmistry@google.com>

--- cd/bin/build-image-bazel.sh
+++ cd/bin/build-image-bazel.sh
@@ -9,7 +9,7 @@ _BAZEL_TARGET=$4
 _IMAGE_PATH=$5
 
 pushd ${_WORKSPACE_DIR}/${_CHECKOUT_DIR}
-bazelisk run //${_BAZEL_PACKAGE}:${_BAZEL_TARGET}
+bazelisk run --config=remote --google_default_credentials //${_BAZEL_PACKAGE}:${_BAZEL_TARGET}
 image_tag="${_IMAGE_PATH}:$(USER="louhi" ${_WORKSPACE_DIR}/${_CHECKOUT_DIR}/bash/release_tag.sh)"
 echo "$image_tag" > ${_WORKSPACE_DIR}/${_BAZEL_TARGET}.tag
 docker tag bazel/${_BAZEL_PACKAGE}:${_BAZEL_TARGET} louhi_ws/$image_tag
