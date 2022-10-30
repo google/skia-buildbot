2b0e1cddd81cbf334f53fcf8248a8d78073328eb
skia-recreate-skps@skia-swarming-bots.iam.gserviceaccount.com <skia-recreate-skps@skia-swarming-bots.iam.gserviceaccount.com>
2022-10-30 05:13:03 +0000

    Update CIPD Packages
    Change-Id: Ie4894e68b355d4db28ab051002e7b3d44311f397
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/597069
    Bot-Commit: skia-recreate-skps@skia-swarming-bots.iam.gserviceaccount.com <skia-recreate-skps@skia-swarming-bots.iam.gserviceaccount.com>
    Commit-Queue: skia-recreate-skps@skia-swarming-bots.iam.gserviceaccount.com <skia-recreate-skps@skia-swarming-bots.iam.gserviceaccount.com>

--- go/cipd/asset_versions_gen.go
+++ go/cipd/asset_versions_gen.go
@@ -96,32 +96,32 @@ var PACKAGES = map[string]*Package{
 	"infra/tools/cipd/${os}-${arch}": {
 		Path:    ".",
 		Name:    "infra/tools/cipd/${os}-${arch}",
-		Version: "git_revision:7f910722b00846c8ac1f813ea0df431c6caf301f",
+		Version: "git_revision:4f585e471d9b3ba86b2ac0ec0cdefaa8fdb67024",
 	},
 	"infra/tools/git/${platform}": {
 		Path:    "cipd_bin_packages",
 		Name:    "infra/tools/git/${platform}",
-		Version: "git_revision:7f910722b00846c8ac1f813ea0df431c6caf301f",
+		Version: "git_revision:4f585e471d9b3ba86b2ac0ec0cdefaa8fdb67024",
 	},
 	"infra/tools/luci-auth/${platform}": {
 		Path:    "cipd_bin_packages",
 		Name:    "infra/tools/luci-auth/${platform}",
-		Version: "git_revision:7f910722b00846c8ac1f813ea0df431c6caf301f",
+		Version: "git_revision:4f585e471d9b3ba86b2ac0ec0cdefaa8fdb67024",
 	},
 	"infra/tools/luci/git-credential-luci/${platform}": {
 		Path:    "cipd_bin_packages",
 		Name:    "infra/tools/luci/git-credential-luci/${platform}",
-		Version: "git_revision:7f910722b00846c8ac1f813ea0df431c6caf301f",
+		Version: "git_revision:4f585e471d9b3ba86b2ac0ec0cdefaa8fdb67024",
 	},
 	"infra/tools/luci/gsutil/${platform}": {
 		Path:    "cipd_bin_packages",
 		Name:    "infra/tools/luci/gsutil/${platform}",
-		Version: "git_revision:7f910722b00846c8ac1f813ea0df431c6caf301f",
+		Version: "git_revision:4f585e471d9b3ba86b2ac0ec0cdefaa8fdb67024",
 	},
 	"infra/tools/luci/isolate/${platform}": {
 		Path:    "cipd_bin_packages",
 		Name:    "infra/tools/luci/isolate/${platform}",
-		Version: "git_revision:7f910722b00846c8ac1f813ea0df431c6caf301f",
+		Version: "git_revision:4f585e471d9b3ba86b2ac0ec0cdefaa8fdb67024",
 	},
 	"infra/tools/luci/isolated/${platform}": {
 		Path:    "cipd_bin_packages",
@@ -131,27 +131,27 @@ var PACKAGES = map[string]*Package{
 	"infra/tools/luci/kitchen/${platform}": {
 		Path:    ".",
 		Name:    "infra/tools/luci/kitchen/${platform}",
-		Version: "git_revision:7f910722b00846c8ac1f813ea0df431c6caf301f",
+		Version: "git_revision:4f585e471d9b3ba86b2ac0ec0cdefaa8fdb67024",
 	},
 	"infra/tools/luci/lucicfg/${platform}": {
 		Path:    "cipd_bin_packages",
 		Name:    "infra/tools/luci/lucicfg/${platform}",
-		Version: "git_revision:7f910722b00846c8ac1f813ea0df431c6caf301f",
+		Version: "git_revision:4f585e471d9b3ba86b2ac0ec0cdefaa8fdb67024",
 	},
 	"infra/tools/luci/swarming/${platform}": {
 		Path:    "cipd_bin_packages",
 		Name:    "infra/tools/luci/swarming/${platform}",
-		Version: "git_revision:7f910722b00846c8ac1f813ea0df431c6caf301f",
+		Version: "git_revision:4f585e471d9b3ba86b2ac0ec0cdefaa8fdb67024",
 	},
 	"infra/tools/luci/vpython-native/${platform}": {
 		Path:    "cipd_bin_packages",
 		Name:    "infra/tools/luci/vpython-native/${platform}",
-		Version: "git_revision:7f910722b00846c8ac1f813ea0df431c6caf301f",
+		Version: "git_revision:4f585e471d9b3ba86b2ac0ec0cdefaa8fdb67024",
 	},
 	"infra/tools/luci/vpython/${platform}": {
 		Path:    "cipd_bin_packages",
 		Name:    "infra/tools/luci/vpython/${platform}",
-		Version: "git_revision:7f910722b00846c8ac1f813ea0df431c6caf301f",
+		Version: "git_revision:4f585e471d9b3ba86b2ac0ec0cdefaa8fdb67024",
 	},
 	"skia/bots/bazel": {
 		Path:    "bazel",
@@ -196,6 +196,6 @@ var PACKAGES = map[string]*Package{
 	"skia/tools/goldctl/${platform}": {
 		Path:    "cipd_bin_packages",
 		Name:    "skia/tools/goldctl/${platform}",
-		Version: "git_revision:7f910722b00846c8ac1f813ea0df431c6caf301f",
+		Version: "git_revision:4f585e471d9b3ba86b2ac0ec0cdefaa8fdb67024",
 	},
 }
