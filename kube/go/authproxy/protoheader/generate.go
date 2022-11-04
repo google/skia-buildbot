269e1a83a42027d604a07047d4e09b46ec7a5fe7
Joe Gregorio <jcgregorio@google.com>
2022-11-04 10:25:00 -0400

    Add --config=mayberemote to two go:generate targets.
    Change-Id: Iaabd1fc7377bc825e8ab7e6bf099789cd1d0a463
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/600321
    Commit-Queue: Joe Gregorio <jcgregorio@google.com>
    Auto-Submit: Joe Gregorio <jcgregorio@google.com>
    Commit-Queue: Kevin Lubick <kjlubick@google.com>
    Reviewed-by: Kevin Lubick <kjlubick@google.com>

--- kube/go/authproxy/protoheader/generate.go
+++ kube/go/authproxy/protoheader/generate.go
@@ -1,4 +1,4 @@
 package protoheader
 
-//go:generate bazelisk run //:protoc -- --go_opt=paths=source_relative --go_out=. ./header.proto
-//go:generate bazelisk run //:goimports "--run_under=cd $PWD &&" -- -w header.pb.go
+//go:generate bazelisk run --config=mayberemote //:protoc -- --go_opt=paths=source_relative --go_out=. ./header.proto
+//go:generate bazelisk run --config=mayberemote //:goimports "--run_under=cd $PWD &&" -- -w header.pb.go
