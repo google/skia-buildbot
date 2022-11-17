83450cb8d105e86e3fdc4739815b4986c2ac6039
skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
2022-11-17 04:01:16 +0000

    Roll Skia Infra CIPD packages from b3e750a1fe2c to 4d31683e4bbf (7 revisions)
    https://skia.googlesource.com/buildbot.git/+log/b3e750a1fe2c..4d31683e4bbf
    
    2022-11-16 lovisolo@google.com [gold] worker2.go: Make getCommonAndRecentDigests() faster.
    2022-11-16 jcgregorio@google.com [machines] Change shortcut key for search.
    2022-11-16 jcgregorio@google.com [machines] Add quick copy for machine id.
    2022-11-16 jcgregorio@google.com [machines] Remove login.SimpleInit.
    2022-11-16 jcgregorio@google.com [perf] Enforce key-value pairs restrictions on V1 ingestion format.
    2022-11-16 borenet@google.com [infra] Make presubmit task driver use git auth
    2022-11-16 skia-autoroll@skia-public.iam.gserviceaccount.com Roll Skia Infra CIPD packages from 7da398475e3c to b3e750a1fe2c (6 revisions)
    
    If this roll has caused a breakage, revert this CL and stop the roller
    using the controls here:
    https://autoroll.skia.org/r/skia-infra-cipd-infra
    Please CC kjlubick@google.com on the revert to ensure that a human
    is aware of the problem.
    
    To file a bug in Skia Infra CIPD packages: https://bugs.chromium.org/p/skia/issues/entry
    To file a bug in Skia Infra: https://bugs.chromium.org/p/skia/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://bugs.chromium.org/p/skia/issues/entry?template=Autoroller+Bug
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Bug: None
    Tbr: kjlubick@google.com
    Change-Id: Id0dda812740d71a45d84bb9d04e7874938b2a4b5
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/607098
    Commit-Queue: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
    Bot-Commit: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>

--- go/cipd/asset_versions_gen.go
+++ go/cipd/asset_versions_gen.go
@@ -196,12 +196,12 @@ var PACKAGES = map[string]*Package{
 	"skia/tools/bazel_build_all/${platform}": {
 		Path:    "task_drivers",
 		Name:    "skia/tools/bazel_build_all/${platform}",
-		Version: "git_revision:b3e750a1fe2ce432537fe44a13a9772ebaa46c15",
+		Version: "git_revision:4d31683e4bbf7a17b425faf9969164bcd8113fcc",
 	},
 	"skia/tools/bazel_test_all/${platform}": {
 		Path:    "task_drivers",
 		Name:    "skia/tools/bazel_test_all/${platform}",
-		Version: "git_revision:b3e750a1fe2ce432537fe44a13a9772ebaa46c15",
+		Version: "git_revision:4d31683e4bbf7a17b425faf9969164bcd8113fcc",
 	},
 	"skia/tools/goldctl/${platform}": {
 		Path:    "cipd_bin_packages",
