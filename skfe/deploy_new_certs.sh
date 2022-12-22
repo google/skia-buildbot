19312edd895e111e612c5a7c30dc93a035d937b3
borenet <borenet@google.com>
2022-12-22 14:22:21 -0500

    Update deploy_new_certs.sh for skia-infra-public
    Bug: skia:12544
    Change-Id: I3639685c631f877992d49cf2944ea41156985b50
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/621867
    Commit-Queue: Eric Boren <borenet@google.com>
    Reviewed-by: Joe Gregorio <jcgregorio@google.com>

--- skfe/deploy_new_certs.sh
+++ skfe/deploy_new_certs.sh
@@ -5,13 +5,21 @@
 # See http://go/skia-ssl-cert for a longer description and the command you need
 # to run before running this script.
 #
+# This script must be run with breakglass. See http://go/skia-infra-iac-handbook
+# for instructions.
+#
 # This script adds a new cert to gcloud, and makes a local change to the
 # skia-ingress.yaml file. Applying the change to kubernetes, pushing the change
 # in git, and deleting old certs are left as manual steps to allow for manual
 # inspection of the change.
 
-DATE=$(date +%Y-%m-%d)
+set -e
 
+DATE=$(date +%Y-%m-%d)
+gcloud compute ssl-certificates create skia-org-$DATE \
+    --private-key=$HOME/ssl-cert-requests/WILDCARD.skia.org-$DATE/WILDCARD.skia.org.key \
+    --certificate=$HOME/ssl-cert-requests/WILDCARD.skia.org-$DATE/WILDCARD.skia.org.chained.pem \
+    --project=skia-infra-public
 gcloud compute ssl-certificates create skia-org-$DATE \
     --private-key=$HOME/ssl-cert-requests/WILDCARD.skia.org-$DATE/WILDCARD.skia.org.key \
     --certificate=$HOME/ssl-cert-requests/WILDCARD.skia.org-$DATE/WILDCARD.skia.org.chained.pem \
@@ -20,6 +28,7 @@ gcloud compute ssl-certificates create skia-org-$DATE \
 git clone https://skia.googlesource.com/k8s-config
 
 sed --in-place s#ingress.gcp.kubernetes.io/pre-shared-cert:.*#ingress.gcp.kubernetes.io/pre-shared-cert:\ skia-org-$DATE# ./k8s-config/skia-public/skia-ingress.yaml
+sed --in-place s#ingress.gcp.kubernetes.io/pre-shared-cert:.*#ingress.gcp.kubernetes.io/pre-shared-cert:\ skia-org-$DATE# ./k8s-config/skia-infra-public/skia-ingress.yaml
 
 printf "\n\nConfirm that the change to skia-ingress.yaml makes sense:\n\n"
 
@@ -33,3 +42,4 @@ printf "  git cl upload --skip-title --reviewers=\"rubber-stamper@appspot.gservi
 
 printf "Also remove unused certs: \n\n"
 gcloud compute ssl-certificates list --project=skia-public --format=json | jq '.[].name' | grep --invert-match skia-org-$DATE | xargs -L1 echo "  " gcloud compute ssl-certificates delete --project=skia-public
+gcloud compute ssl-certificates list --project=skia-infra-public --format=json | jq '.[].name' | grep --invert-match skia-org-$DATE | xargs -L1 echo "  " gcloud compute ssl-certificates delete --project=skia-infra-public
