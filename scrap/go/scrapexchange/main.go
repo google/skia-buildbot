d957004059e89fe1642b1c2565a7a96ec9e1a9bb
Joe Gregorio <jcgregorio@google.com>
2023-01-26 15:34:11 -0500

    [scrapexchange] Move scrapexchange to auth-proxy.
    See also: http://go/proxy-auth-skia
    
    Now running with --local really works to allow local development:
    
    $ curl http://localhost:8000/_/scraps/sksl/de2a4d7d893a7251eb33129ddf9d76ea517901cec960db116a1bbd7832757c1f
    {"Type":"sksl","Body":"// ...\n","SKSLMetaData":{"Uniforms":[],"ImageURL":"/dist/mandrill.png","Children":[]}}
    [skia-public] jcgregorio@jcgregorio840 ~/projects/buildbot (scrap-auth-proxy)
    $ curl http://localhost:9000/_/scraps/sksl/de2a4d7d893a7251eb33129ddf9d76ea517901cec960db116a1bbd7832757c1f
    {"Type":"sksl","Body":"// ...\n","SKSLMetaData":{"Uniforms":[],"ImageURL":"/dist/mandrill.png","Children":[]}}
    
    
    This CL needs to be coupled with a CL that runs auth-proxy in front
    of scrapexchange in the cluster.
    
    Bug: b/266820497
    Change-Id: I45ba41a6d5e041859ebfe11245b6cb47d8c0322e
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/634538
    Commit-Queue: Joe Gregorio <jcgregorio@google.com>
    Reviewed-by: Chris Mumford <cmumford@google.com>

--- scrap/go/scrapexchange/main.go
+++ scrap/go/scrapexchange/main.go
@@ -9,16 +9,18 @@ import (
 	"html/template"
 	"net/http"
 	"path/filepath"
+	"runtime"
 
 	"cloud.google.com/go/storage"
 	"github.com/gorilla/mux"
-	"github.com/unrolled/secure"
-	"go.skia.org/infra/go/allowed"
+	"go.skia.org/infra/go/alogin"
+	"go.skia.org/infra/go/alogin/proxylogin"
 	"go.skia.org/infra/go/auth"
 	"go.skia.org/infra/go/baseapp"
+	"go.skia.org/infra/go/common"
 	"go.skia.org/infra/go/gcs/gcsclient"
 	"go.skia.org/infra/go/httputils"
-	"go.skia.org/infra/go/login"
+	"go.skia.org/infra/go/roles"
 	"go.skia.org/infra/go/skerr"
 	"go.skia.org/infra/go/sklog"
 	"go.skia.org/infra/scrap/go/api"
@@ -27,49 +29,66 @@ import (
 	"google.golang.org/api/option"
 )
 
-// flags
-var (
-	authGroup    = flag.String("auth_group", "google/skia-staff@google.com", "The chrome infra auth group to use for restricting access.")
-	internalPort = flag.String("internal_port", ":9000", "HTTP internal service address (e.g., ':9000') for unauthenticated in-cluster requests.")
-	bucket       = flag.String("bucket", "", "The Google Cloud Storage bucket that scraps are stored in.")
-)
+type flags struct {
+	internalPort string
+	bucket       string
+	local        bool
+	port         string
+	promPort     string
+	resourcesDir string
+}
+
+func (f *flags) Register(fs *flag.FlagSet) {
+	fs.StringVar(&f.internalPort, "internal_port", ":9000", "HTTP internal service address (e.g., ':9000') for unauthenticated in-cluster requests.")
+	fs.StringVar(&f.bucket, "bucket", "", "The Google Cloud Storage bucket that scraps are stored in.")
+	fs.BoolVar(&f.local, "local", false, "Running locally if true. As opposed to in production.")
+	fs.StringVar(&f.port, "port", ":8000", "HTTP service address (e.g., ':8000')")
+	fs.StringVar(&f.promPort, "prom_port", ":20000", "Metrics service address (e.g., ':10110')")
+	fs.StringVar(&f.resourcesDir, "resources_dir", "", "The directory to find templates, JS, and CSS files. If blank the current directory will be used.")
+}
 
 // server is the state of the server.
 type server struct {
+	flags        *flags
 	apiEndpoints *api.Api
 	templates    *template.Template
+	login        alogin.Login
 }
 
-// New implements baseapp.Constructor.
-func New() (baseapp.App, error) {
-	if *bucket == "" {
+func new() (*server, error) {
+	// Register and parse flags.
+	flags := &flags{}
+	flagSet := flag.NewFlagSet("scrapexchange", flag.ExitOnError)
+	flags.Register(flagSet)
+
+	common.InitWithMust(
+		"scrapexchange",
+		common.PrometheusOpt(&flags.promPort),
+		common.MetricsLoggingOpt(),
+		common.FlagSetOpt(flagSet),
+	)
+
+	if flags.bucket == "" {
 		return nil, skerr.Fmt("--bucket is a required flag.")
 	}
+
+	// Fix up flag values.
+	if flags.resourcesDir == "" {
+		_, filename, _, _ := runtime.Caller(1)
+		flags.resourcesDir = filepath.Join(filepath.Dir(filename), "../../dist")
+	}
+
 	ctx := context.Background()
 	ts, err := google.DefaultTokenSource(ctx, storage.ScopeFullControl, auth.ScopeUserinfoEmail)
 	if err != nil {
 		return nil, skerr.Wrap(err)
 	}
-
-	var admin allowed.Allow
-	if !*baseapp.Local {
-		client := httputils.DefaultClientConfig().WithTokenSource(ts).With2xxOnly().Client()
-		admin, err = allowed.NewAllowedFromChromeInfraAuth(client, *authGroup)
-		if err != nil {
-			return nil, skerr.Wrap(err)
-		}
-	} else {
-		admin = allowed.NewAllowedFromList([]string{"fred@example.org", "barney@example.org", "wilma@example.org"})
-	}
-
-	login.SimpleInitWithAllow(*baseapp.Port, *baseapp.Local, admin, nil, nil)
-
 	client := httputils.DefaultClientConfig().WithTokenSource(ts).WithoutRetries().Client()
 	storageClient, err := storage.NewClient(ctx, option.WithHTTPClient(client))
 	if err != nil {
 		return nil, skerr.Wrap(err)
 	}
-	gcsClient := gcsclient.New(storageClient, *bucket)
+	gcsClient := gcsclient.New(storageClient, flags.bucket)
 	scrapExchange, err := scrap.New(gcsClient)
 	if err != nil {
 		return nil, skerr.Wrapf(err, "Failed to create ScrapExchange.")
@@ -78,10 +97,11 @@ func New() (baseapp.App, error) {
 	apiEndpoints := api.New(scrapExchange)
 
 	srv := &server{
+		flags:        flags,
 		apiEndpoints: apiEndpoints,
+		login:        proxylogin.NewWithDefaults(),
 	}
 	srv.loadTemplates()
-
 	srv.startInternalServer()
 
 	return srv, nil
@@ -89,46 +109,29 @@ func New() (baseapp.App, error) {
 
 func (srv *server) loadTemplates() {
 	srv.templates = template.Must(template.New("").Delims("{%", "%}").ParseFiles(
-		filepath.Join(*baseapp.ResourcesDir, "index.html"),
+		filepath.Join(srv.flags.resourcesDir, "index.html"),
 	))
 }
 
 func (srv *server) mainHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if *baseapp.Local {
+	if srv.flags.local {
 		srv.loadTemplates()
 	}
-	if err := srv.templates.ExecuteTemplate(w, "index.html", map[string]string{
-		// Look in //scrap/pages/BUILD.bazel for where the nonce templates are injected.
-		"Nonce": secure.CSPNonce(r.Context()),
-	}); err != nil {
+	if err := srv.templates.ExecuteTemplate(w, "index.html", map[string]string{}); err != nil {
 		sklog.Errorf("Failed to expand template: %s", err)
 	}
 }
 
-// user returns the currently logged in user, or a placeholder if running locally.
-func (srv *server) user(r *http.Request) string {
-	user := "barney@example.org"
-	if !*baseapp.Local {
-		user = login.LoggedInAs(r)
-	}
-	return user
+// gzip applies gzip to HTTP responses.
+func gzip(h http.Handler) http.Handler {
+	return httputils.GzipRequestResponse(h)
 }
 
-// AddHandlers implements baseapp.App.
 func (srv *server) AddHandlers(r *mux.Router) {
-	var h http.HandlerFunc = srv.mainHandler
-	if !*baseapp.Local {
-		h = login.RestrictAdminFn(h)
-	}
-	r.HandleFunc("/", h)
-	r.HandleFunc("/loginstatus/", login.StatusHandler).Methods("GET")
+	r.HandleFunc("/", srv.mainHandler).Methods("GET")
+	r.PathPrefix("/dist/").Handler(http.StripPrefix("/dist/", gzip(http.HandlerFunc(httputils.MakeResourceHandler(srv.flags.resourcesDir))))).Methods("GET")
 	srv.apiEndpoints.AddHandlers(r, api.DoNotAddProtectedEndpoints)
-}
-
-// AddMiddleware implements baseapp.App.
-func (srv *server) AddMiddleware() []mux.MiddlewareFunc {
-	return []mux.MiddlewareFunc{}
 }
 
 func (srv *server) startInternalServer() {
@@ -136,10 +139,33 @@ func (srv *server) startInternalServer() {
 	internal := mux.NewRouter()
 	srv.apiEndpoints.AddHandlers(internal, api.AddProtectedEndpoints)
 	go func() {
-		sklog.Fatal(http.ListenAndServe(*internalPort, internal))
+		sklog.Fatal(http.ListenAndServe(srv.flags.internalPort, internal))
 	}()
 }
 
 func main() {
-	baseapp.Serve(New, []string{"scrap.skia.org"})
+	s, err := new()
+	if err != nil {
+		sklog.Fatal(err)
+	}
+
+	// Add HTTP handlers.
+	r := mux.NewRouter()
+	s.AddHandlers(r)
+
+	// Do not wrap http.Handler with security or authentication middleware if we
+	// are running locally.
+	var h http.Handler = r
+	if !s.flags.local {
+		h = baseapp.SecurityMiddleware([]string{"scrap.skia.org"}, s.flags.local, nil)(h)
+		h = proxylogin.ForceRoleMiddleware(s.login, roles.Viewer)(h)
+	}
+
+	sklog.Infof("Ready to serve at: %q", s.flags.port)
+	server := &http.Server{
+		Addr:           s.flags.port,
+		Handler:        h,
+		MaxHeaderBytes: 1 << 20,
+	}
+	sklog.Fatal(server.ListenAndServe())
 }
