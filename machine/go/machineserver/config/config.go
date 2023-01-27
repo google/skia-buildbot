a07a31fd2c5232f4fd1bbf13382c566ad0e3d552
Joe Gregorio <jcgregorio@google.com>
2023-01-27 15:25:02 -0500

    [machines] Remove unused sections from the config.
    Now that we've migrated from Firestore to CockroachDB
    and replaced PubSub with SSE we can simplify the config struct.
    
    Bug: b/266964548
    Change-Id: Ieb48244ea4fad495105ffa40bc5473b00ef8e88a
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/635080
    Commit-Queue: Kevin Lubick <kjlubick@google.com>
    Auto-Submit: Joe Gregorio <jcgregorio@google.com>
    Reviewed-by: Kevin Lubick <kjlubick@google.com>

--- machine/go/machineserver/config/config.go
+++ machine/go/machineserver/config/config.go
@@ -1,30 +1,8 @@
 // Package config contains the configuration for a running machineserver instance.
 package config
 
-// Source is configuration for the source of machine.Events.
-type Source struct {
-	// Project is the Google Cloud project that contains the pubsub topic.
-	Project string `json:"project"`
-
-	// The pubsub topic to listen to for machine events.
-	Topic string `json:"topic"`
-}
-
-// DescriptionChangeSource is configuration for the source of pubsub events that
-// arrive when the machine.Description of this test machine has changed.
-type DescriptionChangeSource struct {
-	// Project is the Google Cloud project that contains the pubsub topic.
-	Project string `json:"project"`
-
-	// The pubsub topic to listen to for events.
-	Topic string `json:"topic"`
-}
-
 // InstanceConfig is the config for an instance of machineserver.
 type InstanceConfig struct {
-	Source                  Source                  `json:"source"`
-	DescriptionChangeSource DescriptionChangeSource `json:"desc_source"`
-
 	// ConnectionString, if supplied, points to the CockroachDB database to use
 	// for storage. If not supplied storage falls back to Firestore.
 	ConnectionString string `json:"connection_string"`
