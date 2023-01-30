8f8bafdcb67fda02a09c7ca385b29d7844bd263f
borenet <borenet@google.com>
2023-01-30 11:46:57 -0500

    Add note about expected failures to update-zone-records.sh
    Change-Id: I6cc926e4eb287088a5efa85e7dff3a2dd907ce56
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/635518
    Commit-Queue: Eric Boren <borenet@google.com>
    Auto-Submit: Eric Boren <borenet@google.com>
    Reviewed-by: Joe Gregorio <jcgregorio@google.com>
    Commit-Queue: Joe Gregorio <jcgregorio@google.com>

--- skfe/update-zone-records.sh
+++ skfe/update-zone-records.sh
@@ -4,4 +4,7 @@
 
 # Note that --zone refers to the DNS zone name, not a [GCS
 # zone](https://cloud.google.com/compute/docs/regions-zones).
+#
+# The command will throw errors for records which already exist. These can be
+# safely ignored.
 gcloud dns record-sets import --zone skia-org --zone-file-format skia.org.zone
