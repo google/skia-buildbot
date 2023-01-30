2ebf2401b791c7eb57049e43d7205bc3e9aa8f56
Chris Mumford <cmumford@google.com>
2023-01-30 08:53:28 -0800

    scrapexchange: Implement SkSL to C++ (fiddle) conversion
    Implement the SkSL to C++ scrap conversion. This outputs
    C++ code which can be put directly into fiddle.skia.org
    for a working implementation.
    
    Bug: skia:14081
    Change-Id: Ibede997b380e7440fa10ae095ee907cf2f43fdde
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/635519
    Commit-Queue: Chris Mumford <cmumford@google.com>
    Reviewed-by: Joe Gregorio <jcgregorio@google.com>

--- scrap/go/scrap/templates.go
+++ scrap/go/scrap/templates.go
@@ -14,18 +14,18 @@ func templateName(t Type, lang Lang) string {
 	return fmt.Sprintf("%s-%s", t, lang)
 }
 
-// bodyAsStringSlice is a template helper function that breaks up a ScrapBody
+// bodyAsQuotedStringSlice is a template helper function that breaks up a ScrapBody
 // into a form suitable as a multi-line C++ const char * string.
 //
 // That is, it takes "<svg> \n</svg>"
 //
 // And turns it into:
 //
-//  []string{
-//    `"<svg> \n"`,
-//    `"</svg>";`,
-//  }
-func bodyAsStringSlice(body string) []string {
+//	[]string{
+//	  `"<svg> \n"`,
+//	  `"</svg>";`,
+//	}
+func bodyAsQuotedStringSlice(body string) []string {
 	// Escape all the double quotes.
 	body = strings.ReplaceAll(body, `"`, `\"`)
 
@@ -49,9 +49,25 @@ func bodyAsStringSlice(body string) []string {
 	return ret
 }
 
+// bodyStringSlice is a template helper function that splits up a ScrapBody
+// into a slice of strings representing each line of the ScrapBody.
+//
+// That is, it takes "foo \nbar"
+//
+// And turns it into:
+//
+//	[]string{
+//	  "foo ",
+//	  "bar",
+//	}
+func bodyStringSlice(body string) []string {
+	return strings.Split(body, "\n")
+}
+
 // funcMap are the template helper functions available in each template.
 var funcMap = template.FuncMap{
-	"bodyAsStringSlice": bodyAsStringSlice,
+	"bodyAsQuotedStringSlice": bodyAsQuotedStringSlice,
+	"bodyStringSlice":         bodyStringSlice,
 }
 
 func loadTemplates() (templateMap, error) {
@@ -70,18 +86,18 @@ func loadTemplates() (templateMap, error) {
 	return ret, nil
 }
 
-// TODO(jcgregorio) Fill in the rest of the templates.
+// TODO(cmumford) Fill in the rest of the templates.
 var templates = map[string]string{
 	"svg-cpp":       svgCpp,
 	"svg-js":        "",
-	"sksl-cpp":      "",
+	"sksl-cpp":      skslCpp,
 	"sksl-js":       "",
 	"particles-cpp": "",
 	"particles-js":  "",
 }
 
 const svgCpp = `void draw(SkCanvas* canvas) {
-    const char * svg ={{ range bodyAsStringSlice .Body }}
+    const char * svg ={{ range bodyAsQuotedStringSlice .Body }}
         {{.}}{{end}}
 
     sk_sp<SkData> data(SkData::MakeWithoutCopy(svg, strlen(svg)));
@@ -104,4 +120,38 @@ const svgCpp = `void draw(SkCanvas* canvas) {
     }
 
     svgDom->render(canvas);
+}`
+
+const skslCpp = `void draw(SkCanvas *canvas) {
+    canvas->clear(SK_ColorBLACK);
+
+    constexpr char sksl[] = R"(
+        // Inputs supplied by shaders.skia.org:
+        uniform float3 iResolution;      // Viewport resolution (pixels)
+        uniform float  iTime;            // Shader playback time (s)
+        uniform float4 iMouse;           // Mouse drag pos=.xy Click pos=.zw (pixels)
+        uniform float3 iImageResolution; // iImage1 resolution (pixels)
+        uniform shader iImage1;          // An input image.
+{{ range bodyStringSlice .Body }}
+        {{.}}{{end}}
+    )";
+
+    // Parse the SkSL, and create an SkRuntimeEffect object:
+    auto [effect, err] = SkRuntimeEffect::MakeForShader(SkString(sksl));
+    SkRuntimeShaderBuilder builder(effect);
+    builder.uniform("iResolution") =
+        SkV3{(float)canvas->imageInfo().width(),
+             (float)canvas->imageInfo().height(), 1.0f};
+    builder.uniform("iTime") = 1.0f;
+    builder.uniform("iMouse") = SkV4{0.0f, 0.0f, 0.0f, 0.0f};
+    builder.uniform("iImageResolution") =
+        SkV3{(float)image->width(), (float)image->height(), 1.0f};
+    builder.child("iImage1") =
+        image->makeShader(SkSamplingOptions(SkFilterMode::kLinear));
+    sk_sp<SkShader> myShader = builder.makeShader();
+
+    // Fill the surface with |myShader|:
+    SkPaint p;
+    p.setShader(myShader);
+    canvas->drawPaint(p);
 }`
--- scrap/go/scrap/templates_test.go
+++ scrap/go/scrap/templates_test.go
@@ -45,3 +45,75 @@ func TestTemplateExpand_SVGToCPP_Success(t *testing.T) {
 }`
 	require.Equal(t, expected, b.String())
 }
+
+func TestTemplateExpand_SkSLToCPP_ResponseMatchesExpected(t *testing.T) {
+	tmplMap, err := loadTemplates()
+	require.NoError(t, err)
+	var b bytes.Buffer
+	body := ScrapBody{
+		Type: SKSL,
+		Body: "half4 main(in vec2 fragCoord ) {\n    return vec4( result, 1.0 );\n}",
+	}
+	err = tmplMap[CPP][SKSL].Execute(&b, body)
+	require.NoError(t, err)
+	expected := `void draw(SkCanvas *canvas) {
+    canvas->clear(SK_ColorBLACK);
+
+    constexpr char sksl[] = R"(
+        // Inputs supplied by shaders.skia.org:
+        uniform float3 iResolution;      // Viewport resolution (pixels)
+        uniform float  iTime;            // Shader playback time (s)
+        uniform float4 iMouse;           // Mouse drag pos=.xy Click pos=.zw (pixels)
+        uniform float3 iImageResolution; // iImage1 resolution (pixels)
+        uniform shader iImage1;          // An input image.
+
+        half4 main(in vec2 fragCoord ) {
+            return vec4( result, 1.0 );
+        }
+    )";
+
+    // Parse the SkSL, and create an SkRuntimeEffect object:
+    auto [effect, err] = SkRuntimeEffect::MakeForShader(SkString(sksl));
+    SkRuntimeShaderBuilder builder(effect);
+    builder.uniform("iResolution") =
+        SkV3{(float)canvas->imageInfo().width(),
+             (float)canvas->imageInfo().height(), 1.0f};
+    builder.uniform("iTime") = 1.0f;
+    builder.uniform("iMouse") = SkV4{0.0f, 0.0f, 0.0f, 0.0f};
+    builder.uniform("iImageResolution") =
+        SkV3{(float)image->width(), (float)image->height(), 1.0f};
+    builder.child("iImage1") =
+        image->makeShader(SkSamplingOptions(SkFilterMode::kLinear));
+    sk_sp<SkShader> myShader = builder.makeShader();
+
+    // Fill the surface with |myShader|:
+    SkPaint p;
+    p.setShader(myShader);
+    canvas->drawPaint(p);
+}`
+
+	require.Equal(t, expected, b.String())
+}
+
+func TestTemplateHelper_bodyAsQuotedStringSlice_ReturnsExpectedSlice(t *testing.T) {
+	test := func(name string, expected []string, input string) {
+		t.Run(name, func(t *testing.T) {
+			require.Equal(t, expected, bodyAsQuotedStringSlice(input))
+		})
+	}
+	test("OneLine", []string{`" <svg> ";`}, " <svg> ")
+	test("TwoLines", []string{`"<svg> \n"`, `"</svg>";`}, "<svg> \n</svg>")
+	test("EmptyBody", []string{`"";`}, "")
+}
+
+func TestTemplateHelper_bodyStringSlice_ReturnsExpectedSlice(t *testing.T) {
+	test := func(name string, expected []string, input string) {
+		t.Run(name, func(t *testing.T) {
+			actual := bodyStringSlice(input)
+			require.Equal(t, expected, actual)
+		})
+	}
+	test("OneLine", []string{"foo"}, "foo")
+	test("TwoLines", []string{"foo ", " bar"}, "foo \n bar")
+	test("EmptyBody", []string{""}, "")
+}
