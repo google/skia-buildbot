f8c5e2dc14c4894418915cff8d9971f1cdf12350
Chris Mumford <cmumford@google.com>
2023-02-14 06:26:49 -0800

    [scrap] Support scrap uniform values in C++ conversion
    Support user defined shader inputs when converting SkSL
    scraps to C++. This implementation only supports the following
    types: float, float2, float3, float4, float2x2. Unsupported
    types will be reported as a C++ comment if encountered - like:
    
        // Unsupported uniform type "double"
    
    Bug: skia:14081
    Change-Id: I125b30757ec9a2bcfee1340388ab971671f861dc
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/641096
    Reviewed-by: Kevin Lubick <kjlubick@google.com>
    Commit-Queue: Chris Mumford <cmumford@google.com>

--- scrap/go/scrap/templates.go
+++ scrap/go/scrap/templates.go
@@ -1,14 +1,99 @@
 package scrap
 
 import (
+	"bytes"
 	"fmt"
+	"regexp"
+	"strconv"
 	"strings"
 	"text/template"
 
 	"go.skia.org/infra/go/skerr"
 )
 
+const whitespaceChars = "\n\r\t "
+
+var (
+	uniformFloatScalarRegex = regexp.MustCompile(`^float([234]?)$`)
+	uniformFloatMatrixRegex = regexp.MustCompile(`^float([234])x([234])$`)
+	uniformDefinitionRegex  = regexp.MustCompile(`^\s*uniform\s+(?P<type>float[234x]*)\s+(?P<name>i[^\s;]+)\s*;$`)
+)
+
 type templateMap map[Lang]map[Type]*template.Template
+
+// floatSliceToString will convert a slice of float32's to a string
+// representation. e.g. []float{1,2,3} will become "1, 2, 3"
+func floatSliceToString(floats []float32) string {
+	return strings.Trim(strings.Join(strings.Fields(fmt.Sprint(floats)), ", "), "[]")
+}
+
+// uniformValue represents a single instance of a uniform definition
+// in a shader script. For example a definition like:
+//
+// uniform float3 iColor;
+//
+// will have a Name of "iColor", and a Type of "float3".
+type uniformValue struct {
+	Name string
+	Type string
+}
+
+// numFloats will return the number of floats needed to represent a
+// uniformValue.
+func (u uniformValue) numFloats() (int, error) {
+	if u.Type == "float" {
+		return 1, nil
+	}
+	m := uniformFloatScalarRegex.FindStringSubmatch(u.Type)
+	if m != nil {
+		n, err := strconv.Atoi(m[1])
+		if err != nil {
+			return 0, skerr.Wrap(err)
+		}
+		return n, nil
+	}
+	if m = uniformFloatMatrixRegex.FindStringSubmatch(u.Type); m != nil {
+		x, err := strconv.Atoi(m[1])
+		if err != nil {
+			return 0, skerr.Wrap(err)
+		}
+		y, err := strconv.Atoi(m[2])
+		if err != nil {
+			return 0, skerr.Wrap(err)
+		}
+		return x * y, nil
+	}
+	return 0, skerr.Fmt("Can't determine size of %q", u.Type)
+}
+
+func (u uniformValue) getCppDefinitionString(vals []float32) (string, error) {
+	switch u.Type {
+	case "float":
+		return fmt.Sprintf(`builder.uniform("%s") = %s;`, u.Name, floatSliceToString(vals[0:1])), nil
+	case "float2":
+		return fmt.Sprintf(`builder.uniform("%s") = SkV2{%s};`, u.Name, floatSliceToString(vals[0:2])), nil
+	case "float3":
+		return fmt.Sprintf(`builder.uniform("%s") = SkV3{%s};`, u.Name, floatSliceToString(vals[0:3])), nil
+	case "float4":
+		return fmt.Sprintf(`builder.uniform("%s") = SkV4{%s};`, u.Name, floatSliceToString(vals[0:4])), nil
+	case "float2x2":
+		return fmt.Sprintf(`builder.uniform("%s") = SkV4{%s};`, u.Name, floatSliceToString(vals[0:4])), nil
+	}
+	return "", skerr.Fmt("Unsupported uniform type %q", u.Type)
+}
+
+func extractBodyUniforms(body ScrapBody) ([]uniformValue, error) {
+	var uniforms []uniformValue
+	for _, line := range strings.Split(body.Body, "\n") {
+		if m := uniformDefinitionRegex.FindStringSubmatch(line); m != nil {
+			uniforms = append(uniforms, uniformValue{
+				Name: m[2],
+				Type: m[1],
+			})
+		}
+	}
+	return uniforms, nil
+}
 
 func templateName(t Type, lang Lang) string {
 	return fmt.Sprintf("%s-%s", t, lang)
@@ -49,19 +134,24 @@ func bodyAsQuotedStringSlice(body string) []string {
 	return ret
 }
 
-// bodyStringSlice is a template helper function that splits up a ScrapBody
-// into a slice of strings representing each line of the ScrapBody.
-//
-// That is, it takes "foo \nbar"
-//
-// And turns it into:
-//
-//	[]string{
-//	  "foo ",
-//	  "bar",
-//	}
-func bodyStringSlice(body string) []string {
-	return strings.Split(body, "\n")
+// indentMultilineString will indent each line of a multiline string,
+// if that line is not empty, by the number of spaces specified by |indent|.
+// Each line will also be trimmed of all trailing whitespace characters.
+func indentMultilineString(body string, indent int) string {
+	var buffer bytes.Buffer
+	first := true
+	for _, line := range strings.Split(body, "\n") {
+		if !first {
+			buffer.WriteString("\n")
+		}
+		first = false
+		line = strings.TrimRight(line, whitespaceChars)
+		if len(line) > 0 {
+			buffer.WriteString(strings.Repeat(" ", indent))
+			buffer.WriteString(line)
+		}
+	}
+	return buffer.String()
 }
 
 func getSkSLImageURL(body ScrapBody) string {
@@ -102,12 +192,50 @@ func getSkSLCustomUniforms(body ScrapBody) string {
 	return fmt.Sprintf("\n      // User supplied uniform values:\n      %s", vals)
 }
 
+// cppCustomUniformValues will return a string that is valid C++ to
+// define and initialize the custom uniforms defined in a SkSL scrap.
+func cppCustomUniformValues(body ScrapBody) string {
+	if body.Type != SKSL || body.SKSLMetaData == nil || len(body.SKSLMetaData.Uniforms) == 0 {
+		return ""
+	}
+	uniforms, err := extractBodyUniforms(body)
+	if err != nil {
+		return ""
+	}
+	requiredFloatCount := 0
+	for _, u := range uniforms {
+		num, err := u.numFloats()
+		if err != nil {
+			return "    // Cannot determine input size."
+		}
+		requiredFloatCount += num
+	}
+	if requiredFloatCount != len(body.SKSLMetaData.Uniforms) {
+		return fmt.Sprintf("    // User inputs size mismatch: %d != %d.",
+			requiredFloatCount, len(body.SKSLMetaData.Uniforms))
+	}
+	i := 0
+	var buffer bytes.Buffer
+	buffer.WriteString("    // Inputs supplied by user:\n")
+	for _, u := range uniforms {
+		s, err := u.getCppDefinitionString(body.SKSLMetaData.Uniforms[i:])
+		if err != nil {
+			return fmt.Sprintf("    // Cannot get C++ definition for %s (%s).", u.Name, u.Type)
+		}
+		buffer.WriteString(fmt.Sprintf("    %s\n", s))
+		num, _ := u.numFloats()
+		i += num
+	}
+	return buffer.String()
+}
+
 // funcMap are the template helper functions available in each template.
 var funcMap = template.FuncMap{
 	"bodyAsQuotedStringSlice": bodyAsQuotedStringSlice,
-	"bodyStringSlice":         bodyStringSlice,
 	"getSkSLImageURL":         getSkSLImageURL,
 	"getSkSLCustomUniforms":   getSkSLCustomUniforms,
+	"cppCustomUniformValues":  cppCustomUniformValues,
+	"indentMultilineString":   indentMultilineString,
 }
 
 func loadTemplates() (templateMap, error) {
@@ -172,8 +300,8 @@ const skslCpp = `void draw(SkCanvas *canvas) {
         uniform float4 iMouse;           // Mouse drag pos=.xy Click pos=.zw (pixels)
         uniform float3 iImageResolution; // iImage1 resolution (pixels)
         uniform shader iImage1;          // An input image.
-{{ range bodyStringSlice .Body }}
-        {{.}}{{end}}
+
+{{ indentMultilineString .Body 8 }}
     )";
 
     // Parse the SkSL, and create an SkRuntimeEffect object:
@@ -188,6 +316,7 @@ const skslCpp = `void draw(SkCanvas *canvas) {
         SkV3{(float)image->width(), (float)image->height(), 1.0f};
     builder.child("iImage1") =
         image->makeShader(SkSamplingOptions(SkFilterMode::kLinear));
+{{ cppCustomUniformValues . }}
     sk_sp<SkShader> myShader = builder.makeShader();
 
     // Fill the surface with |myShader|:
@@ -225,8 +354,8 @@ Promise.all([loadImage]).then((values) => {
     uniform float4 iMouse;           // Mouse drag pos=.xy Click pos=.zw (pixels)
     uniform float3 iImageResolution; // iImage1 resolution (pixels)
     uniform shader iImage1;          // An input image.
-{{ range bodyStringSlice .Body }}
-    {{.}}{{end}}
+
+{{ indentMultilineString .Body 4 }}
     ` + "`" + `;
 
   const effect = CanvasKit.RuntimeEffect.Make(prog);
--- scrap/go/scrap/templates_test.go
+++ scrap/go/scrap/templates_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"testing"
 
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -84,6 +85,63 @@ func TestTemplateExpand_SkSLToCPP_ResponseMatchesExpected(t *testing.T) {
         SkV3{(float)image->width(), (float)image->height(), 1.0f};
     builder.child("iImage1") =
         image->makeShader(SkSamplingOptions(SkFilterMode::kLinear));
+
+    sk_sp<SkShader> myShader = builder.makeShader();
+
+    // Fill the surface with |myShader|:
+    SkPaint p;
+    p.setShader(myShader);
+    canvas->drawPaint(p);
+}`
+
+	require.Equal(t, expected, b.String())
+}
+
+func TestTemplateExpand_SkSLToCPPWithMetadata_ResponseMatchesExpected(t *testing.T) {
+	tmplMap, err := loadTemplates()
+	require.NoError(t, err)
+	var b bytes.Buffer
+	body := ScrapBody{
+		Type:         SKSL,
+		Body:         "// Inputs supplied by user:\nuniform float iSomeSlider;\n\nhalf4 main(in vec2 fragCoord ) {\n    return vec4( result, iSomeSlider );\n}",
+		SKSLMetaData: &SKSLMetaData{Uniforms: []float32{0.5}},
+	}
+	err = tmplMap[CPP][SKSL].Execute(&b, body)
+	require.NoError(t, err)
+	expected := `void draw(SkCanvas *canvas) {
+    canvas->clear(SK_ColorBLACK);
+
+    constexpr char sksl[] = R"(
+        // Inputs supplied by shaders.skia.org:
+        uniform float3 iResolution;      // Viewport resolution (pixels)
+        uniform float  iTime;            // Shader playback time (s)
+        uniform float4 iMouse;           // Mouse drag pos=.xy Click pos=.zw (pixels)
+        uniform float3 iImageResolution; // iImage1 resolution (pixels)
+        uniform shader iImage1;          // An input image.
+
+        // Inputs supplied by user:
+        uniform float iSomeSlider;
+
+        half4 main(in vec2 fragCoord ) {
+            return vec4( result, iSomeSlider );
+        }
+    )";
+
+    // Parse the SkSL, and create an SkRuntimeEffect object:
+    auto [effect, err] = SkRuntimeEffect::MakeForShader(SkString(sksl));
+    SkRuntimeShaderBuilder builder(effect);
+    builder.uniform("iResolution") =
+        SkV3{(float)canvas->imageInfo().width(),
+             (float)canvas->imageInfo().height(), 1.0f};
+    builder.uniform("iTime") = 1.0f;
+    builder.uniform("iMouse") = SkV4{0.0f, 0.0f, 0.0f, 0.0f};
+    builder.uniform("iImageResolution") =
+        SkV3{(float)image->width(), (float)image->height(), 1.0f};
+    builder.child("iImage1") =
+        image->makeShader(SkSamplingOptions(SkFilterMode::kLinear));
+    // Inputs supplied by user:
+    builder.uniform("iSomeSlider") = 0.5;
+
     sk_sp<SkShader> myShader = builder.makeShader();
 
     // Fill the surface with |myShader|:
@@ -278,16 +336,16 @@ func TestTemplateHelper_bodyAsQuotedStringSlice_ReturnsExpectedSlice(t *testing.
 	test("EmptyBody", []string{`"";`}, "")
 }
 
-func TestTemplateHelper_bodyStringSlice_ReturnsExpectedSlice(t *testing.T) {
-	test := func(name string, expected []string, input string) {
+func TestTemplateHelper_indentMultilineString_ReturnsExpectedIndent(t *testing.T) {
+	test := func(name, expected, input string, indent int) {
 		t.Run(name, func(t *testing.T) {
-			actual := bodyStringSlice(input)
-			require.Equal(t, expected, actual)
+			require.Equal(t, expected, indentMultilineString(input, indent))
 		})
 	}
-	test("OneLine", []string{"foo"}, "foo")
-	test("TwoLines", []string{"foo ", " bar"}, "foo \n bar")
-	test("EmptyBody", []string{""}, "")
+	test("OneLine", "    foo", "foo", 4)
+	test("TwoLines", "    foo\n    bar", "foo\nbar", 4)
+	test("OneLineTrailingWhitespace", "    foo", "foo\t", 4)
+	test("MultilineOneEmptyLine", "    foo\n\n    bar", "foo\n\nbar", 4)
 }
 
 func TestGetSkSLImageURL_WithValidPathsAndURLs_ReturnsObjectsShaderURL(t *testing.T) {
@@ -371,4 +429,192 @@ func TestGetSkSLCustomUniforms_WithoutUniformValues_ReturnsEmptyString(t *testin
 		ScrapBody{
 			Type:         SKSL,
 			SKSLMetaData: &SKSLMetaData{Uniforms: []float32{}}})
+}
+
+func TestUniformValueNumFloats_WithValidTypes_ReturnsCorrectCount(t *testing.T) {
+	test := func(name string, expected int, u uniformValue) {
+		t.Run(name, func(t *testing.T) {
+			count, err := u.numFloats()
+			assert.NoError(t, err)
+			assert.Equal(t, expected, count)
+		})
+	}
+	test("float", 1, uniformValue{Name: "iScalar", Type: "float"})
+	test("float2", 2, uniformValue{Name: "iCoord", Type: "float2"})
+	test("float3", 3, uniformValue{Name: "iColor", Type: "float3"})
+	test("float4", 4, uniformValue{Name: "iColorWithAlpha", Type: "float4"})
+	test("float2x2", 4, uniformValue{Name: "iMatrix", Type: "float2x2"})
+}
+
+func TestUniformValueNumFloats_WithInvalidTypes_ReturnsError(t *testing.T) {
+	test := func(name string, u uniformValue) {
+		t.Run(name, func(t *testing.T) {
+			_, err := u.numFloats()
+			assert.Error(t, err)
+		})
+	}
+	test("TextBeforeFloat", uniformValue{Name: "iValue", Type: "Xfloat"})
+	test("TextAfterFloat", uniformValue{Name: "iValue", Type: "float;"})
+	test("FloatX", uniformValue{Name: "iValue", Type: "floatx"})
+	test("FloatZeroDimension", uniformValue{Name: "iValue", Type: "float0"})
+	test("MatrixZeroXDimension", uniformValue{Name: "iValue", Type: "float0x2"})
+	test("MatrixZeroYDimension", uniformValue{Name: "iValue", Type: "float2x0"})
+}
+
+func TestUniformValueGetCppDefinitionString_WithSupportedTypes_ReturnsValidCPP(t *testing.T) {
+	test := func(name, expected string, u uniformValue, vals []float32) {
+		t.Run(name, func(t *testing.T) {
+			values, err := u.getCppDefinitionString(vals)
+			assert.NoError(t, err)
+			assert.Equal(t, expected, values)
+		})
+	}
+	test("float", `builder.uniform("iSomeSlider") = 1;`,
+		uniformValue{Name: "iSomeSlider", Type: "float"},
+		[]float32{1.0},
+	)
+	test("float2", `builder.uniform("iCoordinate") = SkV2{0.5, 0.6};`,
+		uniformValue{Name: "iCoordinate", Type: "float2"},
+		[]float32{0.5, 0.6},
+	)
+	test("float3", `builder.uniform("iColor") = SkV3{0.5, 0.6, 0.7};`,
+		uniformValue{Name: "iColor", Type: "float3"},
+		[]float32{0.5, 0.6, 0.7},
+	)
+	test("float4", `builder.uniform("iColorWithAlpha") = SkV4{0.5, 0.6, 0.7, 1};`,
+		uniformValue{Name: "iColorWithAlpha", Type: "float4"},
+		[]float32{0.5, 0.6, 0.7, 1.0},
+	)
+	test("float2x2", `builder.uniform("iMatrix") = SkV4{1, 2, 3, 4};`,
+		uniformValue{Name: "iMatrix", Type: "float2x2"},
+		[]float32{1, 2, 3, 4},
+	)
+}
+
+func TestUniformValueGetCppDefinitionString_WithUnsupportedTypes_ReturnsError(t *testing.T) {
+	test := func(name, expected string, u uniformValue, vals []float32) {
+		t.Run(name, func(t *testing.T) {
+			_, err := u.getCppDefinitionString(vals)
+			assert.Error(t, err)
+		})
+	}
+	test("float", `builder.uniform("iSomeSlider") = 1;`,
+		uniformValue{Name: "iSomeSlider", Type: "double"},
+		[]float32{1.0},
+	)
+}
+
+func TestExtractBodyUniforms_WithValidUniforms_ReturnsExtractedValues(t *testing.T) {
+	test := func(name string, expected []uniformValue, body ScrapBody) {
+		t.Run(name, func(t *testing.T) {
+			values, err := extractBodyUniforms(body)
+			assert.NoError(t, err)
+			assert.Equal(t, expected, values)
+		})
+	}
+	test("SingleFloat", []uniformValue{{Name: "iSomeSlider", Type: "float"}},
+		ScrapBody{
+			Type: SKSL,
+			Body: `
+uniform float  iSomeSlider;
+
+half4 main(float2 fragCoord) {
+  return half4(iColorWithAlpha);
+}`,
+		})
+	test("MultipleUniforms", []uniformValue{
+		{Name: "iSomeSlider", Type: "float"},
+		{Name: "iColor", Type: "float3"},
+		{Name: "iColorWithAlpha", Type: "float4"},
+		{Name: "iSomeCoordinate", Type: "float2"},
+		{Name: "iMatrix", Type: "float2x2"},
+	},
+		ScrapBody{
+			Type: SKSL,
+			Body: `
+uniform float  iSomeSlider;
+
+// A float3 with 'color' in the name
+// (case insensitive) will have a color picker control.
+uniform float3 iColor;
+
+// A float4 with 'color' in the name will also have a
+// slider for the alpha channel.
+uniform float4 iColorWithAlpha;
+
+// uniforms of any other size and shape will have
+// a table of inputs as a control.
+uniform float2 iSomeCoordinate;
+uniform float2x2 iMatrix;
+
+half4 main(float2 fragCoord) {
+  return half4(iColorWithAlpha);
+}`,
+		})
+}
+
+func TestExtractBodyUniforms_WithoutUniformDefinitions_ReturnsNilSlice(t *testing.T) {
+	test := func(name string, body ScrapBody) {
+		t.Run(name, func(t *testing.T) {
+			values, err := extractBodyUniforms(body)
+			assert.NoError(t, err)
+			assert.Nil(t, values)
+		})
+	}
+	test("NoUniforms",
+		ScrapBody{
+			Type: SKSL,
+			Body: `
+half4 main(float2 fragCoord) {
+  return half4(iColorWithAlpha);
+}`,
+		})
+	test("CommentedOutUniform",
+		ScrapBody{
+			Type: SKSL,
+			Body: `
+// uniform float  iSomeSlider;
+half4 main(float2 fragCoord) {
+  return half4(iColorWithAlpha);
+}`,
+		})
+	test("NoUniformName",
+		ScrapBody{
+			Type: SKSL,
+			Body: `
+uniform float ;
+half4 main(float2 fragCoord) {
+  return half4(iColorWithAlpha);
+}`,
+		})
+	test("ExtraSemicolon",
+		ScrapBody{
+			Type: SKSL,
+			Body: `
+uniform float iSomeSlider;;
+
+half4 main(float2 fragCoord) {
+  return half4(iColorWithAlpha);
+}`,
+		})
+}
+
+func TestCppCustomUniformValues_WithUniformValue_ReturnsCPPCode(t *testing.T) {
+	test := func(name, expected string, body ScrapBody) {
+		t.Run(name, func(t *testing.T) {
+			assert.Equal(t, expected, cppCustomUniformValues(body))
+		})
+	}
+	test("OneFloat", `    // Inputs supplied by user:
+    builder.uniform("iSomeSlider") = 0.5;
+`,
+		ScrapBody{
+			Type: SKSL,
+			Body: `
+uniform float  iSomeSlider;
+half4 main(float2 fragCoord) {
+  return half4(iColorWithAlpha);
+}`,
+			SKSLMetaData: &SKSLMetaData{Uniforms: []float32{0.5}},
+		})
 }
