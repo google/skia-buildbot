a3e8392bca7d7e88f9ff6db7fb0f1c20900171db
Leandro Lovisolo <lovisolo@google.com>
2023-03-10 21:25:11 +0000

    [elements-sk] Rewrite imports in //new_element.
    Bug: skia:14161
    Change-Id: Idf9e2f2b71c5d0302ce8f1fcd04b0adc4fbddb7c
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/654404
    Reviewed-by: Joe Gregorio <jcgregorio@google.com>
    Commit-Queue: Leandro Lovisolo <lovisolo@google.com>

--- new_element/modules/example-control-sk/example-control-sk.ts
+++ new_element/modules/example-control-sk/example-control-sk.ts
@@ -8,7 +8,7 @@
  *
  * @example
  */
-import { define } from 'elements-sk/define';
+import { define } from '../../../elements-sk/modules/define';
 import { html } from 'lit-html';
 import { ElementSk } from '../../../infra-sk/modules/ElementSk';
 
