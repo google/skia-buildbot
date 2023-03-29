f8154021ecdc4e99c93dc1bf962e11e293ce90b0
seanmccullough <seanmccullough@google.com>
2023-03-29 11:21:25 -0700

    [cabe] add grpc health check handlers to cabeserver
    Bug: b:274985160
    Change-Id: I82c017858f35b00d8ec697bceb2a84ab7559b879
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/663536
    Reviewed-by: Joe Gregorio <jcgregorio@google.com>
    Commit-Queue: Sean McCullough <seanmccullough@google.com>

--- cabe/go/cabeserver/main.go
+++ cabe/go/cabeserver/main.go
@@ -7,6 +7,8 @@ import (
 	"net"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/health"
+	"google.golang.org/grpc/health/grpc_health_v1"
 	"google.golang.org/grpc/reflection"
 
 	"go.skia.org/infra/go/common"
@@ -35,16 +37,22 @@ func main() {
 		common.MetricsLoggingOpt(),
 	)
 
+	s := grpc.NewServer()
+
+	sklog.Infof("registering grpc health server")
+	healthServer := health.NewServer()
+	grpc_health_v1.RegisterHealthServer(s, healthServer)
+
+	sklog.Infof("registering grpc reflection server")
+	reflection.Register(s)
+
+	sklog.Infof("registering cabe grpc server")
 	cabeServer := NewServer()
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
 		sklog.Fatalf("failed to listen: %v", err)
 	}
-	s := grpc.NewServer()
 	cpb.RegisterAnalysisServer(s, cabeServer)
-
-	// Register reflection service on gRPC server.
-	reflection.Register(s)
 
 	sklog.Infof("server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
