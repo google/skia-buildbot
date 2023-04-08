a7f1542f9484ebde319d11ddb9db33c4e6c29d92
Joe Gregorio <jcgregorio@google.com>
2023-04-07 21:25:42 -0400

    Add TLS endpoint to envoy config.
    This adds a second endpoint to the envoy config for `skia-infra-public` only.
    
    It does that by duplicating the `listener` entry, changing the port it listens
    on, adds in the TLS config, and then appends it back as a new `listener`.
    
    The resulting file was validated using envoy. The only error is because
    I tested this on my desktop and not in the container we build, so the
    cert files are missing.
    
    ```
    $ docker run -ti -v $(pwd):/tmp/configs envoyproxy/envoy:v1.16.1  --mode validate --config-path /tmp/configs/skia-infra-public.json
    [2023-04-07 20:51:23.222][8][info][main] [source/server/server.cc:583] runtime: layers:
      - name: base
        static_layer:
          {}
      - name: admin
        admin_layer:
          {}
    [2023-04-07 20:51:23.222][8][info][config] [source/server/configuration_impl.cc:95] loading tracing configuration
    [2023-04-07 20:51:23.223][8][info][config] [source/server/configuration_impl.cc:70] loading 0 static secret(s)
    [2023-04-07 20:51:23.223][8][info][config] [source/server/configuration_impl.cc:76] loading 30 cluster(s)
    [2023-04-07 20:51:23.229][8][info][config] [source/server/configuration_impl.cc:80] loading 2 listener(s)
    [2023-04-07 20:51:23.236][8][critical][main] [source/server/config_validation/server.cc:60] error initializing configuration '/tmp/configs/skia-infra-public.json': Invalid path: /etc/envoy/front-proxy-crt.pem
    ```
    
    See: https://www.envoyproxy.io/docs/envoy/latest/start/quick-start/securing#upstream-and-downstream-tls-contexts
    Change-Id: I3d650984b4694e62ff3c5ab34150820d349ff157
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/667459
    Reviewed-by: Ravi Mistry <rmistry@google.com>
    Commit-Queue: Joe Gregorio <jcgregorio@google.com>

--- skfe/go/merge_envoy/main.go
+++ skfe/go/merge_envoy/main.go
@@ -6,10 +6,30 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/Jeffail/gabs/v2"
 	"go.skia.org/infra/go/sklog"
 )
+
+const tls = `{
+	"name": "envoy.transport_sockets.tls",
+	"typed_config": {
+		"@type": "type.googleapis.com/envoy.extensions.transport_sockets.tls.v3.DownstreamTlsContext",
+		"common_tls_context": {
+			"tls_certificates": [
+				{
+					"certificate_chain": {
+						"filename": "/etc/envoy/front-proxy-crt.pem"
+					},
+					"private_key": {
+						"filename": "/etc/envoy/front-proxy-key.pem"
+					}
+				}
+			]
+		}
+	}
+ }`
 
 func main() {
 	filenames := os.Args[1:]
@@ -37,6 +57,44 @@ func main() {
 	hostsPath := []string{"static_resources", "listeners", "0", "filter_chains", "filters", "0", "typed_config", "route_config", "virtual_hosts"}
 	for _, cluster := range file2.Search(hostsPath...).Children() {
 		if err := file1.ArrayAppend(cluster.Data(), hostsPath...); err != nil {
+			sklog.Fatal(err)
+		}
+	}
+
+	// Duplicate the one listener so we can add it again, but at a different
+	// port and with TLS.
+
+	// Only do this for `skia-infra-public` for now, until we know this won't
+	// break serving.
+	if strings.HasSuffix(filenames[1], "skia-infra-public.json") {
+
+		// Dulicate the first listener to create secondListener.
+		firstListener := file1.Search("static_resources", "listeners", "0")
+		copyAsString := firstListener.String()
+		secondListener, err := gabs.ParseJSON([]byte(copyAsString))
+		if err != nil {
+			sklog.Fatal(err)
+		}
+
+		// Change port for secondListener.
+		_, err = secondListener.Search("address", "socket_address").Set(8443, "port_value")
+		if err != nil {
+			sklog.Fatal(err)
+		}
+
+		// Add TLS config to secondListener.
+		tlsConfig, err := gabs.ParseJSON([]byte(tls))
+		if err != nil {
+			sklog.Fatal(err)
+		}
+		_, err = secondListener.Set(tlsConfig, "filter_chains", "transport_socket")
+		if err != nil {
+			sklog.Fatal(err)
+		}
+
+		// Add secondListener back into the file.
+		err = file1.ArrayAppend(secondListener, "static_resources", "listeners")
+		if err != nil {
 			sklog.Fatal(err)
 		}
 	}
