8ac75c2707884029f5caf8e75a51e1265ba53dbc
skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
2023-04-26 04:01:32 +0000

    Roll Skia Infra CIPD packages from 8f39f2978ef8 to dec3646b8299 (14 revisions)
    https://skia.googlesource.com/buildbot.git/+log/8f39f2978ef8..dec3646b8299
    
    2023-04-25 jcgregorio@google.com Create helloworld application.
    2023-04-25 bsheedy@chromium.org Define chrome-gpu-gtest corpus
    2023-04-25 borenet@google.com [docker] Add SetTag function to Client
    2023-04-25 borenet@google.com [autoroll] Add Docker child
    2023-04-25 borenet@google.com [docker] Expand go/docker package, add mocks
    2023-04-25 lovisolo@google.com [gold] Add support for custom "triaging is disallowed" messages.
    2023-04-25 lovisolo@google.com [gold] /json/v2/details RPC: Take public params into account.
    2023-04-25 lovisolo@google.com [gold] Add gold_launcher target for chrome-public-gold.skia.org.
    2023-04-25 lovisolo@google.com [gold] Insert window.GoldSettings into page templates as a Go-generated JS object literal.
    2023-04-25 jcgregorio@google.com Revert "Add luci.app support to //go/login."
    2023-04-25 jcgregorio@google.com Revert "Add token cache for valid Bearer Tokens."
    2023-04-25 jcgregorio@google.com Add token cache for valid Bearer Tokens.
    2023-04-25 jcgregorio@google.com Add luci.app support to //go/login.
    2023-04-25 skia-autoroll@skia-public.iam.gserviceaccount.com Roll Skia Infra CIPD packages from 83a6fdf2e118 to 8f39f2978ef8 (7 revisions)
    
    If this roll has caused a breakage, revert this CL and stop the roller
    using the controls here:
    https://autoroll.skia.org/r/skia-infra-cipd-infra
    Please CC borenet@google.com,cmumford@google.com on the revert to ensure that a human
    is aware of the problem.
    
    To file a bug in Skia Infra CIPD packages: https://bugs.chromium.org/p/skia/issues/entry
    To file a bug in Skia Infra: https://bugs.chromium.org/p/skia/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://bugs.chromium.org/p/skia/issues/entry?template=Autoroller+Bug
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Tbr: cmumford@google.com
    Change-Id: Iecf781d6cbd922c633aac2de0cff52f664b11dde
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/681957
    Bot-Commit: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
    Commit-Queue: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>

--- go/cipd/asset_versions_gen.go
+++ go/cipd/asset_versions_gen.go
@@ -196,17 +196,17 @@ var PACKAGES = map[string]*Package{
 	"skia/tools/bazel_build_all/${platform}": {
 		Path:    "task_drivers",
 		Name:    "skia/tools/bazel_build_all/${platform}",
-		Version: "git_revision:8f39f2978ef8a5fe2a3745b6709f717aaada2ce4",
+		Version: "git_revision:dec3646b8299d65ccc07975eafcc3f9b5c78fab1",
 	},
 	"skia/tools/bazel_test_all/${platform}": {
 		Path:    "task_drivers",
 		Name:    "skia/tools/bazel_test_all/${platform}",
-		Version: "git_revision:8f39f2978ef8a5fe2a3745b6709f717aaada2ce4",
+		Version: "git_revision:dec3646b8299d65ccc07975eafcc3f9b5c78fab1",
 	},
 	"skia/tools/command_wrapper/${platform}": {
 		Path:    "task_drivers",
 		Name:    "skia/tools/command_wrapper/${platform}",
-		Version: "git_revision:8f39f2978ef8a5fe2a3745b6709f717aaada2ce4",
+		Version: "git_revision:dec3646b8299d65ccc07975eafcc3f9b5c78fab1",
 	},
 	"skia/tools/goldctl/${platform}": {
 		Path:    "cipd_bin_packages",
@@ -216,6 +216,6 @@ var PACKAGES = map[string]*Package{
 	"skia/tools/presubmit/${platform}": {
 		Path:    "task_drivers",
 		Name:    "skia/tools/presubmit/${platform}",
-		Version: "git_revision:8f39f2978ef8a5fe2a3745b6709f717aaada2ce4",
+		Version: "git_revision:dec3646b8299d65ccc07975eafcc3f9b5c78fab1",
 	},
 }
