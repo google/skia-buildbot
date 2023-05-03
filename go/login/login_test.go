23d269a0c00ab7877103d2783397fd3ebf8b8f3b
Joe Gregorio <jcgregorio@google.com>
2023-05-03 18:09:33 +0000

    Revert "Apply opts before anything else in Init."
    This reverts commit 406f216c98450ab4d6943bd03f08cf566532660a.
    
    Reason for revert: autoroll and status are failing to log in
    
    Original change's description:
    > Apply opts before anything else in Init.
    >
    > Also remove loginInit as it's no longer needed now that we have the
    > SkipLoadingSecrets option.
    >
    > Change-Id: Ibde8a4a65d5461167d20ea831e6dc64178bc85c3
    > Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/689356
    > Reviewed-by: Ravi Mistry <rmistry@google.com>
    > Commit-Queue: Ravi Mistry <rmistry@google.com>
    > Auto-Submit: Joe Gregorio <jcgregorio@google.com>
    
    Change-Id: I354fc1258b70d50c11b4a5e59b6abf9fa2d6b0c8
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/691716
    Auto-Submit: Joe Gregorio <jcgregorio@google.com>
    Commit-Queue: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>

--- go/login/login_test.go
+++ go/login/login_test.go
@@ -41,7 +41,7 @@ var (
 
 func initLoginForTests(t *testing.T) {
 	ctx := context.Background()
-	err := Init(ctx, "http://localhost", defaultAllowedDomains, "", SkiaOrg, SkipLoadingSecrets{})
+	err := initLogin(ctx, "id", "secret", "http://localhost", saltForTesting, defaultAllowedDomains, SkiaOrg)
 	require.NoError(t, err)
 }
 
@@ -175,7 +175,7 @@ func TestDomainFromHost(t *testing.T) {
 }
 
 func TestDomainFromHost_LuciApp(t *testing.T) {
-	err := Init(context.Background(), "", defaultAllowedDomains, "", LuciApp, SkipLoadingSecrets{})
+	err := initLogin(context.Background(), "id", "secret", "", saltForTesting, defaultAllowedDomains, LuciApp)
 	require.NoError(t, err)
 	assert.Equal(t, "localhost", domainFromHost("localhost:10110"))
 	assert.Equal(t, "localhost", domainFromHost("localhost"))
@@ -186,7 +186,6 @@ func TestDomainFromHost_LuciApp(t *testing.T) {
 	assert.Equal(t, "https://luci.app/oauth2callback/", DefaultRedirectURL)
 	assert.Equal(t, "https://luci.app/oauth2callback/",
 		oauthConfig.(*oauth2.Config).RedirectURL)
-	assert.Equal(t, "luci-app-login-oauth2-secrets", loginSecretName)
 }
 
 func TestIsAuthorized(t *testing.T) {
