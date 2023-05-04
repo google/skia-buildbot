83ce434fbf7c374037efcd7670be14485e9e0a02
Joe Gregorio <jcgregorio@google.com>
2023-05-04 11:39:19 -0400

    Add port-forward script for CDB in skia-infra-public.
    Change-Id: I4316e56e88f2feeb08bb8bae601833d0c1fe16fc
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/693117
    Auto-Submit: Joe Gregorio <jcgregorio@google.com>
    Reviewed-by: Alex Sun <sunpeng@google.com>
    Commit-Queue: Alex Sun <sunpeng@google.com>

---
+++ perf/cockroachdb/skia-infra-public-port-forward.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Starts a port-forward of the CockroachDB connection.
+
+set -e
+
+printf "After this command launches run the following in another terminal:\n\n\tcockroach sql --insecure --host=127.0.0.1:26257\n\n"
+
+set -x
+
+../../kube/attach.sh skia-infra-public kubectl port-forward -n perf perf-cockroachdb-0 25000:26257
