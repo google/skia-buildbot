e5ce570c0d1b3fe75b2e858af7fdab89af946cd5
skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
2023-05-11 04:01:04 +0000

    Roll Skia Infra CIPD packages from c4d18f21ffde to 5a8b571f3a48 (12 revisions)
    https://skia.googlesource.com/buildbot.git/+log/c4d18f21ffde..5a8b571f3a48
    
    2023-05-10 jcgregorio@google.com Add zone record for chrome-perf.skia.org.
    2023-05-10 jcgregorio@google.com Fix port in skia-infra-public-port-forward.sh script.
    2023-05-10 lovisolo@google.com [tmm] Add support for the "docker_installed" dimension.
    2023-05-10 seanmccullough@google.com [cabe] add role checks to grpc server
    2023-05-10 jcgregorio@google.com Fix scrapexchange, add missing healthz.
    2023-05-10 borenet@google.com [autoroll] Update README.md with bug-filing links near the top
    2023-05-10 hernantorrisi@gmail.com support resizable players
    2023-05-10 sunpeng@google.com DB optimization.
    2023-05-10 lovisolo@google.com [tmm] Fix "cpu" dimension reported on GCE machines.
    2023-05-10 lovisolo@google.com [tmm] crossplatform.CPUs(): Replace spaces with underscores in returned "cpu" dimensions.
    2023-05-10 lovisolo@google.com [tmm] Improve tests for crossplatform.CPUs().
    2023-05-10 skia-autoroll@skia-public.iam.gserviceaccount.com Roll Skia Infra CIPD packages from 0d5474fe31ef to c4d18f21ffde (7 revisions)
    
    If this roll has caused a breakage, revert this CL and stop the roller
    using the controls here:
    https://autoroll.skia.org/r/skia-infra-cipd-infra
    Please CC borenet@google.com,kjlubick@google.com on the revert to ensure that a human
    is aware of the problem.
    
    To file a bug in Skia Infra CIPD packages: https://bugs.chromium.org/p/skia/issues/entry
    To file a bug in Skia Infra: https://bugs.chromium.org/p/skia/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://bugs.chromium.org/p/skia/issues/entry?template=Autoroller+Bug
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Tbr: kjlubick@google.com
    Change-Id: I92631b3568a693f416ec767f4e5ad8dfb2ca6c87
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/696121
    Commit-Queue: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
    Bot-Commit: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>

--- go/cipd/asset_versions_gen.go
+++ go/cipd/asset_versions_gen.go
@@ -196,17 +196,17 @@ var PACKAGES = map[string]*Package{
 	"skia/tools/bazel_build_all/${platform}": {
 		Path:    "task_drivers",
 		Name:    "skia/tools/bazel_build_all/${platform}",
-		Version: "git_revision:c4d18f21ffde213e24a8f5fc3429642c732cc129",
+		Version: "git_revision:5a8b571f3a48f6203fba065d6726d66e8ff88671",
 	},
 	"skia/tools/bazel_test_all/${platform}": {
 		Path:    "task_drivers",
 		Name:    "skia/tools/bazel_test_all/${platform}",
-		Version: "git_revision:c4d18f21ffde213e24a8f5fc3429642c732cc129",
+		Version: "git_revision:5a8b571f3a48f6203fba065d6726d66e8ff88671",
 	},
 	"skia/tools/command_wrapper/${platform}": {
 		Path:    "task_drivers",
 		Name:    "skia/tools/command_wrapper/${platform}",
-		Version: "git_revision:c4d18f21ffde213e24a8f5fc3429642c732cc129",
+		Version: "git_revision:5a8b571f3a48f6203fba065d6726d66e8ff88671",
 	},
 	"skia/tools/goldctl/${platform}": {
 		Path:    "cipd_bin_packages",
@@ -216,6 +216,6 @@ var PACKAGES = map[string]*Package{
 	"skia/tools/presubmit/${platform}": {
 		Path:    "task_drivers",
 		Name:    "skia/tools/presubmit/${platform}",
-		Version: "git_revision:c4d18f21ffde213e24a8f5fc3429642c732cc129",
+		Version: "git_revision:5a8b571f3a48f6203fba065d6726d66e8ff88671",
 	},
 }
