ee89bbd2db698a102f310adf1df57a489bfa2afa
skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
2023-05-17 04:01:17 +0000

    Roll Skia Infra CIPD packages from cbe6e1312c42 to 8e2c784328a9 (22 revisions)
    https://skia.googlesource.com/buildbot.git/+log/cbe6e1312c42..8e2c784328a9
    
    2023-05-16 seanmccullough@google.com [cabe] use the new direct swarming API connection instead of bigquery
    2023-05-16 seanmccullough@google.com [cabe] add direct swarming API connection to backends/
    2023-05-16 sunxiaodi@google.com [CABE] port replay backend from google3 to skia/buildbot repo
    2023-05-16 funing@google.com Add fetch_chrome_perf_anomalies field and bisect button
    2023-05-16 sunpeng@google.com Fetch chrome perf anomalies based on the configuration
    2023-05-16 katelovett@google.com Add authorized user for flutter and flutter-engine
    2023-05-16 seanmccullough@google.com [cabe] add *Spec proto inference support
    2023-05-16 borenet@google.com Revert "[status] Wrap the Twirp server with gzip middleware"
    2023-05-16 borenet@google.com [status] Wrap the Twirp server with gzip middleware
    2023-05-16 borenet@google.com [stage] Fix double "sha256:" prefix in build-images
    2023-05-16 jcgregorio@google.com Fix SkCQ login control.
    2023-05-15 jcgregorio@google.com Fix leasing login URL.
    2023-05-15 seanmccullough@google.com [cabe] migrate the Checker type to skia
    2023-05-15 borenet@google.com [build-images] Use StageManager to update image versions where enabled
    2023-05-15 jcgregorio@google.com [perf] Fix sidebar width.
    2023-05-15 jcgregorio@google.com [leasing] Use alogin-sk for login control.
    2023-05-15 jcgregorio@google.com Update the theme to be closer to Material Design
    2023-05-15 borenet@google.com [k8s-config presubmit] Add check for pipeline/generate.sh
    2023-05-15 borenet@google.com Add go/term package with tools for tabularizing data for terminals
    2023-05-15 borenet@google.com [cd] Add stagemanager CLI tool
    2023-05-15 jcgregorio@google.com Fix leasing login status.
    2023-05-15 skia-autoroll@skia-public.iam.gserviceaccount.com Roll Skia Infra CIPD packages from 0d899fb75655 to cbe6e1312c42 (11 revisions)
    
    If this roll has caused a breakage, revert this CL and stop the roller
    using the controls here:
    https://autoroll.skia.org/r/skia-infra-cipd-infra
    Please CC borenet@google.com,skiabot@google.com on the revert to ensure that a human
    is aware of the problem.
    
    To file a bug in Skia Infra CIPD packages: https://bugs.chromium.org/p/skia/issues/entry
    To file a bug in Skia Infra: https://bugs.chromium.org/p/skia/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://bugs.chromium.org/p/skia/issues/entry?template=Autoroller+Bug
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Tbr: borenet@google.com
    Change-Id: I60358233d0079b4f7ba0183fd09a10d2ff024bc0
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/699337
    Bot-Commit: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
    Commit-Queue: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>

--- go/cipd/asset_versions_gen.go
+++ go/cipd/asset_versions_gen.go
@@ -196,17 +196,17 @@ var PACKAGES = map[string]*Package{
 	"skia/tools/bazel_build_all/${platform}": {
 		Path:    "task_drivers",
 		Name:    "skia/tools/bazel_build_all/${platform}",
-		Version: "git_revision:cbe6e1312c42394444a2659e929eb736c1ee96e1",
+		Version: "git_revision:8e2c784328a9e645b6325b3faf40393627c15ba3",
 	},
 	"skia/tools/bazel_test_all/${platform}": {
 		Path:    "task_drivers",
 		Name:    "skia/tools/bazel_test_all/${platform}",
-		Version: "git_revision:cbe6e1312c42394444a2659e929eb736c1ee96e1",
+		Version: "git_revision:8e2c784328a9e645b6325b3faf40393627c15ba3",
 	},
 	"skia/tools/command_wrapper/${platform}": {
 		Path:    "task_drivers",
 		Name:    "skia/tools/command_wrapper/${platform}",
-		Version: "git_revision:cbe6e1312c42394444a2659e929eb736c1ee96e1",
+		Version: "git_revision:8e2c784328a9e645b6325b3faf40393627c15ba3",
 	},
 	"skia/tools/goldctl/${platform}": {
 		Path:    "cipd_bin_packages",
@@ -216,6 +216,6 @@ var PACKAGES = map[string]*Package{
 	"skia/tools/presubmit/${platform}": {
 		Path:    "task_drivers",
 		Name:    "skia/tools/presubmit/${platform}",
-		Version: "git_revision:cbe6e1312c42394444a2659e929eb736c1ee96e1",
+		Version: "git_revision:8e2c784328a9e645b6325b3faf40393627c15ba3",
 	},
 }
