0daeddfa6f2430b0ee4a4f2e5d6512ac9b990ad0
skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
2023-06-06 04:01:12 +0000

    Roll Skia Infra CIPD packages from ac4876bcdcc3 to b8f3bbf465f8 (9 revisions)
    https://skia.googlesource.com/buildbot.git/+log/ac4876bcdcc3..b8f3bbf465f8
    
    2023-06-06 funing@google.com Add bisect button and auto-fill in pinpoint bisect params
    2023-06-05 yuanhuang@google.com [cabe] add value check for log transform input values
    2023-06-05 seanmccullough@google.com [cabe] fix NaN issue that was breaking grpc logging in prod
    2023-06-05 cmumford@google.com [ansible] update RadeonVega6 GPU drivers
    2023-06-05 jcgregorio@google.com Remove login code from skottie.
    2023-06-05 jcgregorio@google.com Remove GetViewer checks from autoroll and task-scheduler rpcs.
    2023-06-05 cmumford@google.com [ansible] minor README improvements to download_win_package_src
    2023-06-05 hernantorrisi@gmail.com Add list of sample texts in multiple languages
    2023-06-05 skia-autoroll@skia-public.iam.gserviceaccount.com Roll Skia Infra CIPD packages from 4dbecc7c97a2 to ac4876bcdcc3 (10 revisions)
    
    If this roll has caused a breakage, revert this CL and stop the roller
    using the controls here:
    https://autoroll.skia.org/r/skia-infra-cipd-infra
    Please CC lovisolo@google.com,skiabot@google.com on the revert to ensure that a human
    is aware of the problem.
    
    To file a bug in Skia Infra CIPD packages: https://bugs.chromium.org/p/skia/issues/entry
    To file a bug in Skia Infra: https://bugs.chromium.org/p/skia/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://bugs.chromium.org/p/skia/issues/entry?template=Autoroller+Bug
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Tbr: lovisolo@google.com
    Change-Id: I53c80fc1d4e44dfe151e7b6980f4334453e55c0d
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/707877
    Commit-Queue: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
    Bot-Commit: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>

--- go/cipd/asset_versions_gen.go
+++ go/cipd/asset_versions_gen.go
@@ -196,17 +196,17 @@ var PACKAGES = map[string]*Package{
 	"skia/tools/bazel_build_all/${platform}": {
 		Path:    "task_drivers",
 		Name:    "skia/tools/bazel_build_all/${platform}",
-		Version: "git_revision:ac4876bcdcc3079bbdedd415573db22f6bc4a0f2",
+		Version: "git_revision:b8f3bbf465f8636501287c417c67eaa36e3aabba",
 	},
 	"skia/tools/bazel_test_all/${platform}": {
 		Path:    "task_drivers",
 		Name:    "skia/tools/bazel_test_all/${platform}",
-		Version: "git_revision:ac4876bcdcc3079bbdedd415573db22f6bc4a0f2",
+		Version: "git_revision:b8f3bbf465f8636501287c417c67eaa36e3aabba",
 	},
 	"skia/tools/command_wrapper/${platform}": {
 		Path:    "task_drivers",
 		Name:    "skia/tools/command_wrapper/${platform}",
-		Version: "git_revision:ac4876bcdcc3079bbdedd415573db22f6bc4a0f2",
+		Version: "git_revision:b8f3bbf465f8636501287c417c67eaa36e3aabba",
 	},
 	"skia/tools/goldctl/${platform}": {
 		Path:    "cipd_bin_packages",
@@ -216,6 +216,6 @@ var PACKAGES = map[string]*Package{
 	"skia/tools/presubmit/${platform}": {
 		Path:    "task_drivers",
 		Name:    "skia/tools/presubmit/${platform}",
-		Version: "git_revision:ac4876bcdcc3079bbdedd415573db22f6bc4a0f2",
+		Version: "git_revision:b8f3bbf465f8636501287c417c67eaa36e3aabba",
 	},
 }
