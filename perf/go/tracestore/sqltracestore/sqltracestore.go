f24ac179ced6d785c87ddceec1164c7fbee28e97
Joe Gregorio <jcgregorio@google.com>
2023-06-08 12:24:03 +0000

    [perf] Increase chunk size for writing values.
    The previous increase to postings chunk size did improve performance.
    
    Change-Id: Idea0862890c0985c882d740caee2217929863f33
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/709256
    Reviewed-by: Kevin Lubick <kjlubick@google.com>
    Commit-Queue: Joe Gregorio <jcgregorio@google.com>

--- perf/go/tracestore/sqltracestore/sqltracestore.go
+++ perf/go/tracestore/sqltracestore/sqltracestore.go
@@ -185,7 +185,7 @@ const (
 	// data. Note that values over 200 caused the insert rate to drop precipitously,
 	// going from 20,000 qps with a batch size of 100 down to 400 qps with a batch
 	// size of 200.
-	writeTracesValuesChunkSize    = 100
+	writeTracesValuesChunkSize    = 1000
 	writeTracesPostingsChunkSize  = 1000
 	writeTracesParamSetsChunkSize = 100
 
