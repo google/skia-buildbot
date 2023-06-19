15fb7caa7aea68961e4b685be0e28d3b220238bc
skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
2023-06-19 04:01:41 +0000

    Roll Skia Infra CIPD packages from d044984a01de to b32ce543bca1 (29 revisions)
    https://skia.googlesource.com/buildbot.git/+log/d044984a01de..b32ce543bca1
    
    2023-06-17 sunpeng@google.com Print metric for bisect api failure.
    2023-06-17 lovisolo@google.com Remove gorilla/mux Go module and all remaining references.
    2023-06-17 lovisolo@google.com [gold] Migrate from gorilla/mux to go-chi/chi.
    2023-06-17 lovisolo@google.com [test-service] Migrate from gorilla/mux to go-chi/chi.
    2023-06-17 lovisolo@google.com [task_scheduler] Migrate from gorilla/mux to go-chi/chi.
    2023-06-17 lovisolo@google.com [status] [task_driver] Migrate from gorilla/mux to go-chi/chi.
    2023-06-17 lovisolo@google.com [skottie] Migrate from gorilla/mux to go-chi/chi.
    2023-06-17 lovisolo@google.com [skolo] Migrate from gorilla/mux to go-chi/chi.
    2023-06-17 lovisolo@google.com [machines] Migrate from gorilla/mux to go-chi/chi.
    2023-06-17 lovisolo@google.com [jsfiddle] Migrate from gorilla/mux to go-chi/chi.
    2023-06-17 lovisolo@google.com [jsdoc] Migrate from gorilla/mux to go-chi/chi.
    2023-06-17 lovisolo@google.com [sser] Migrate from gorilla/mux to go-chi/chi.
    2023-06-17 lovisolo@google.com [metrics2] Migrate from gorilla/mux to go-chi/chi.
    2023-06-17 lovisolo@google.com [alogin] Migrate from gorilla/mux to go-chi/chi.
    2023-06-16 lovisolo@google.com [fiddlek] Migrate from gorilla/mux to  go-chi/chi.
    2023-06-16 lovisolo@google.com [emailservice] Migrate from gorilla/mux to go-chi/chi.
    2023-06-16 lovisolo@google.com [docsyserver] Migrate from gorilla/mux to go-chi/chi.
    2023-06-16 lovisolo@google.com [demos] Migrate from gorilla/mux to go-chi/chi.
    2023-06-16 jcgregorio@google.com Fix domain name in config for Chrome Perf internal instance.
    2023-06-16 lovisolo@google.com [debugger] Migrate from gorilla/mux to go-chi/chi.
    2023-06-16 lovisolo@google.com [ct] Migrate from gorilla/mux to go-chi/chi.
    2023-06-16 lovisolo@google.com [cabe] Migrate from gorilla/mux to go-chi/chi.
    2023-06-16 borenet@google.com [autoroll] Fix PYTHONPATH for Skia -> Android gn_to_bp
    2023-06-16 jcgregorio@google.com [gold] Fix Gold auth when using --passive.
    2023-06-16 lovisolo@google.com [autoroll] Migrate from gorilla/mux to go-chi/chi.
    2023-06-16 lovisolo@google.com [api] Migrate from gorilla/mux to go-chi/chi.
    2023-06-16 lovisolo@google.com [android_ingest] Migrate from gorilla/mux to go-chi/chi.
    2023-06-16 jcgregorio@google.com [perf] Tolerate an empty status.roles in cluster-summary-sk.
    2023-06-16 skia-autoroll@skia-public.iam.gserviceaccount.com Roll Skia Infra CIPD packages from 894306cf4c91 to d044984a01de (5 revisions)
    
    If this roll has caused a breakage, revert this CL and stop the roller
    using the controls here:
    https://autoroll.skia.org/r/skia-infra-cipd-infra
    Please CC kjlubick@google.com,skiabot@google.com on the revert to ensure that a human
    is aware of the problem.
    
    To file a bug in Skia Infra CIPD packages: https://bugs.chromium.org/p/skia/issues/entry
    To file a bug in Skia Infra: https://bugs.chromium.org/p/skia/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://bugs.chromium.org/p/skia/issues/entry?template=Autoroller+Bug
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Tbr: kjlubick@google.com
    Change-Id: Icaae64f96c21170ada54ba1d19033f6b62ef7db8
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/713539
    Commit-Queue: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
    Bot-Commit: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>

--- go/cipd/asset_versions_gen.go
+++ go/cipd/asset_versions_gen.go
@@ -196,17 +196,17 @@ var PACKAGES = map[string]*Package{
 	"skia/tools/bazel_build_all/${platform}": {
 		Path:    "task_drivers",
 		Name:    "skia/tools/bazel_build_all/${platform}",
-		Version: "git_revision:d044984a01de20dbe4c385945ef45e2495f9682d",
+		Version: "git_revision:b32ce543bca1db55a6707af1fbf9a71ffaf49af7",
 	},
 	"skia/tools/bazel_test_all/${platform}": {
 		Path:    "task_drivers",
 		Name:    "skia/tools/bazel_test_all/${platform}",
-		Version: "git_revision:d044984a01de20dbe4c385945ef45e2495f9682d",
+		Version: "git_revision:b32ce543bca1db55a6707af1fbf9a71ffaf49af7",
 	},
 	"skia/tools/command_wrapper/${platform}": {
 		Path:    "task_drivers",
 		Name:    "skia/tools/command_wrapper/${platform}",
-		Version: "git_revision:d044984a01de20dbe4c385945ef45e2495f9682d",
+		Version: "git_revision:b32ce543bca1db55a6707af1fbf9a71ffaf49af7",
 	},
 	"skia/tools/goldctl/${platform}": {
 		Path:    "cipd_bin_packages",
@@ -216,6 +216,6 @@ var PACKAGES = map[string]*Package{
 	"skia/tools/presubmit/${platform}": {
 		Path:    "task_drivers",
 		Name:    "skia/tools/presubmit/${platform}",
-		Version: "git_revision:d044984a01de20dbe4c385945ef45e2495f9682d",
+		Version: "git_revision:b32ce543bca1db55a6707af1fbf9a71ffaf49af7",
 	},
 }
