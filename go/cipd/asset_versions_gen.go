27d9a5f2be36d5f7755d7360921f2578205b6eea
skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
2023-08-09 04:00:58 +0000

    Roll Skia Infra CIPD packages from 333a87d1ef8a to 6449131d4768 (17 revisions)
    https://skia.googlesource.com/buildbot.git/+log/333a87d1ef8a..6449131d4768
    
    2023-08-08 jcgregorio@google.com [perf] Docs for querying_wedged alert.
    2023-08-08 hernantorrisi@gmail.com move text sampler component to text box to support one per text layer
    2023-08-08 lovisolo@google.com sk_page: Update some stale comments.
    2023-08-08 lovisolo@google.com sk_page: Optimize development CSS bundles.
    2023-08-08 rmistry@google.com Revert "Testing whitespace change with GitWatcher"
    2023-08-08 cmumford@google.com [scrap] Add deployment docs in README and tweak make target
    2023-08-08 rmistry@google.com Testing whitespace change with GitWatcher
    2023-08-08 jcgregorio@google.com [perf] Use markdown in issue tracker comments.
    2023-08-08 rmistry@google.com Revert "Reland "Testing whitespace change with GitWatcher""
    2023-08-08 rmistry@google.com Reland "Testing whitespace change with GitWatcher"
    2023-08-08 rmistry@google.com Revert "Reland "Reland "Reland "Reland "Reland "Testing whitespace change with GitWatcher""""""
    2023-08-08 jcgregorio@google.com Remove auditlog.Log.
    2023-08-08 jcgregorio@google.com [perf] Update to latest verion of the issue tracker API.
    2023-08-08 rmistry@google.com Reland "Reland "Reland "Reland "Reland "Testing whitespace change with GitWatcher"""""
    2023-08-08 rmistry@google.com Revert "Reland "Reland "Reland "Reland "Testing whitespace change with GitWatcher"""""
    2023-08-08 hernantorrisi@gmail.com move font selector to support one per text box
    2023-08-08 skia-autoroll@skia-public.iam.gserviceaccount.com Roll Skia Infra CIPD packages from 0e52994bf1b6 to 333a87d1ef8a (6 revisions)
    
    If this roll has caused a breakage, revert this CL and stop the roller
    using the controls here:
    https://autoroll.skia.org/r/skia-infra-cipd-infra
    Please CC cmumford@google.com,skiabot@google.com on the revert to ensure that a human
    is aware of the problem.
    
    To file a bug in Skia Infra CIPD packages: https://bugs.chromium.org/p/skia/issues/entry
    To file a bug in Skia Infra: https://bugs.chromium.org/p/skia/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://bugs.chromium.org/p/skia/issues/entry?template=Autoroller+Bug
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Tbr: cmumford@google.com
    Change-Id: Iea8ae519fa08cbaa066b2ff1188c30787279dc56
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/738223
    Commit-Queue: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
    Bot-Commit: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>

--- go/cipd/asset_versions_gen.go
+++ go/cipd/asset_versions_gen.go
@@ -196,17 +196,17 @@ var PACKAGES = map[string]*Package{
 	"skia/tools/bazel_build_all/${platform}": {
 		Path:    "task_drivers",
 		Name:    "skia/tools/bazel_build_all/${platform}",
-		Version: "git_revision:333a87d1ef8ae6440d475a82695ed44d02803673",
+		Version: "git_revision:6449131d47686996539ad65c0847898b7ed88e5d",
 	},
 	"skia/tools/bazel_test_all/${platform}": {
 		Path:    "task_drivers",
 		Name:    "skia/tools/bazel_test_all/${platform}",
-		Version: "git_revision:333a87d1ef8ae6440d475a82695ed44d02803673",
+		Version: "git_revision:6449131d47686996539ad65c0847898b7ed88e5d",
 	},
 	"skia/tools/command_wrapper/${platform}": {
 		Path:    "task_drivers",
 		Name:    "skia/tools/command_wrapper/${platform}",
-		Version: "git_revision:333a87d1ef8ae6440d475a82695ed44d02803673",
+		Version: "git_revision:6449131d47686996539ad65c0847898b7ed88e5d",
 	},
 	"skia/tools/goldctl/${platform}": {
 		Path:    "cipd_bin_packages",
@@ -216,6 +216,6 @@ var PACKAGES = map[string]*Package{
 	"skia/tools/presubmit/${platform}": {
 		Path:    "task_drivers",
 		Name:    "skia/tools/presubmit/${platform}",
-		Version: "git_revision:333a87d1ef8ae6440d475a82695ed44d02803673",
+		Version: "git_revision:6449131d47686996539ad65c0847898b7ed88e5d",
 	},
 }
