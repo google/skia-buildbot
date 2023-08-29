9f1fad0fdc31563f6b43ec469b0550f2eeece804
skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
2023-08-29 04:01:01 +0000

    Roll Skia Infra CIPD packages from 6587ca5b6e44 to d779093a7a96 (3 revisions)
    https://skia.googlesource.com/buildbot.git/+log/6587ca5b6e44..d779093a7a96
    
    2023-08-28 kjlubick@google.com [golden] Add a PodDisruptionBudget for baseline servers
    2023-08-28 cmumford@google.com Use skia-infra-public's debugger/jsfiddle/shaders/scrapexchange
    2023-08-28 skia-autoroll@skia-public.iam.gserviceaccount.com Roll Skia Infra CIPD packages from 7a3cf15a1fc1 to 6587ca5b6e44 (8 revisions)
    
    If this roll has caused a breakage, revert this CL and stop the roller
    using the controls here:
    https://autoroll.skia.org/r/skia-infra-cipd-infra
    Please CC lovisolo@google.com,skiabot@google.com on the revert to ensure that a human
    is aware of the problem.
    
    To file a bug in Skia Infra CIPD packages: https://bugs.chromium.org/p/skia/issues/entry
    To file a bug in Skia Infra: https://bugs.chromium.org/p/skia/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://bugs.chromium.org/p/skia/issues/entry?template=Autoroller+Bug
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Tbr: lovisolo@google.com
    Change-Id: Ia0eed376522e6984f47290baceddab137242f73a
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/748541
    Commit-Queue: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
    Bot-Commit: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>

--- go/cipd/asset_versions_gen.go
+++ go/cipd/asset_versions_gen.go
@@ -196,17 +196,17 @@ var PACKAGES = map[string]*Package{
 	"skia/tools/bazel_build_all/${platform}": {
 		Path:    "task_drivers",
 		Name:    "skia/tools/bazel_build_all/${platform}",
-		Version: "git_revision:6587ca5b6e442cb6d711105834da58caa33167d7",
+		Version: "git_revision:d779093a7a967634efae99c914e7e4d9559fd7ea",
 	},
 	"skia/tools/bazel_test_all/${platform}": {
 		Path:    "task_drivers",
 		Name:    "skia/tools/bazel_test_all/${platform}",
-		Version: "git_revision:6587ca5b6e442cb6d711105834da58caa33167d7",
+		Version: "git_revision:d779093a7a967634efae99c914e7e4d9559fd7ea",
 	},
 	"skia/tools/command_wrapper/${platform}": {
 		Path:    "task_drivers",
 		Name:    "skia/tools/command_wrapper/${platform}",
-		Version: "git_revision:6587ca5b6e442cb6d711105834da58caa33167d7",
+		Version: "git_revision:d779093a7a967634efae99c914e7e4d9559fd7ea",
 	},
 	"skia/tools/goldctl/${platform}": {
 		Path:    "cipd_bin_packages",
@@ -216,6 +216,6 @@ var PACKAGES = map[string]*Package{
 	"skia/tools/presubmit/${platform}": {
 		Path:    "task_drivers",
 		Name:    "skia/tools/presubmit/${platform}",
-		Version: "git_revision:6587ca5b6e442cb6d711105834da58caa33167d7",
+		Version: "git_revision:d779093a7a967634efae99c914e7e4d9559fd7ea",
 	},
 }
