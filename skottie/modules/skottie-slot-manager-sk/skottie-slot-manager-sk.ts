bf82784581afd5faf5342e8c2b75144ea4b9d58c
Jorge Betancourt <jmbetancourt@google.com>
2023-11-01 11:18:24 -0400

    add more slot support skottie web
    scalar and vec2 slot support for the skottie web player
    
    bug: b/307509241
    Change-Id: I5d4f2c19f5718ccc0d14eb943152ac38ea9fddd1
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/771179
    Reviewed-by: Kevin Lubick <kjlubick@google.com>
    Commit-Queue: Jorge Betancourt <jmbetancourt@google.com>

--- skottie/modules/skottie-slot-manager-sk/skottie-slot-manager-sk.ts
+++ skottie/modules/skottie-slot-manager-sk/skottie-slot-manager-sk.ts
@@ -13,18 +13,25 @@ import { ElementSk } from '../../../infra-sk/modules/ElementSk';
 import { html } from 'lit-html';
 import { SkottieColorEventDetail } from '../skottie-color-input-sk/skottie-color-input-sk';
 import { colorToHex, hexToColor } from '../helpers/color';
-import { ColorSlot } from './slot-info';
+import { ColorSlot, ScalarSlot, Vec2Slot } from './slot-info';
 import { SkottiePlayerSk } from '../skottie-player-sk/skottie-player-sk';
 import { define } from '../../../elements-sk/modules/define';
+import { SkottieVec2EventDetail } from './skottie-vec2-input-sk';
+// without this import, the vec2 input div doesn't populate and vec 2 slots don't render
+import './skottie-vec2-input-sk';
 
 export class SkottieSlotManagerSk extends ElementSk {
   private _player: SkottiePlayerSk | null = null;
   private colorSlots: ColorSlot[] = [];
+  private scalarSlots: ScalarSlot[] = [];
+  private vec2Slots: Vec2Slot[] = [];
 
   private static template = (ele: SkottieSlotManagerSk) => html`
-    <div>
+    <div class="wrapper">
       <ul class="slots-container">
         ${ele.colorSlots.map((item: ColorSlot) => ele.colorSlot(item))}
+        ${ele.scalarSlots.map((item: ScalarSlot) => ele.scalarSlot(item))}
+        ${ele.vec2Slots.map((item: Vec2Slot) => ele.vec2Slot(item))}
       </ul>
     </div>
   `;
@@ -40,6 +47,32 @@ export class SkottieSlotManagerSk extends ElementSk {
     </div>
   `;
 
+  private scalarSlot = (ss: ScalarSlot) => html`
+    <div class="slot--scalar">
+      <span class="slotID">${ss.id}</span>
+      <div class="text-box">
+        <input
+          type="number"
+          class="text-box--input"
+          @change=${(e: Event) => this.onScalarSlotChange(e, ss.id)}
+          value=${ss.scalar}
+          required />
+      </div>
+    </div>
+  `;
+
+  private vec2Slot = (vs: Vec2Slot) => html`
+    <div class="slot--vec2">
+      <skottie-vec2-input-sk
+        .label=${vs.id}
+        .x=${vs.x}
+        .y=${vs.y}
+        @vec2-change=${(e: CustomEvent<SkottieVec2EventDetail>) =>
+          this.onVec2SlotChange(e)}>
+      </skottie-vec2-input-sk>
+    </div>
+  `;
+
   private onColorSlotChange(
     e: CustomEvent<SkottieColorEventDetail>,
     sid: string
@@ -52,10 +85,30 @@ export class SkottieSlotManagerSk extends ElementSk {
     this._render();
   }
 
+  private onScalarSlotChange(e: Event, sid: string): void {
+    if (!this._player) {
+      return;
+    }
+    const target = e.target as HTMLInputElement;
+    this._player.managedAnimation()?.setScalarSlot(sid, Number(target.value));
+    this._render();
+  }
+
+  private onVec2SlotChange(e: CustomEvent<SkottieVec2EventDetail>): void {
+    if (!this._player) {
+      return;
+    }
+    this._player
+      .managedAnimation()
+      ?.setVec2Slot(e.detail.label, [e.detail.x, e.detail.y]);
+  }
+
   set player(value: SkottiePlayerSk) {
     this._player = value;
 
     this.colorSlots = [];
+    this.scalarSlots = [];
+    this.vec2Slots = [];
     const managedAnimation = this._player?.managedAnimation();
     if (managedAnimation) {
       const slotInfo = managedAnimation.getSlotInfo();
@@ -64,6 +117,18 @@ export class SkottieSlotManagerSk extends ElementSk {
         if (color) {
           const colorHex = colorToHex(Array.from(color));
           this.colorSlots.push({ id: sid, colorHex: colorHex });
+        }
+      }
+      for (const sid of slotInfo.scalarSlotIDs) {
+        const scalar = managedAnimation.getScalarSlot(sid);
+        if (scalar) {
+          this.scalarSlots.push({ id: sid, scalar: scalar });
+        }
+      }
+      for (const sid of slotInfo.vec2SlotIDs) {
+        const vec2 = managedAnimation.getVec2Slot(sid);
+        if (vec2) {
+          this.vec2Slots.push({ id: sid, x: vec2[0], y: vec2[1] });
         }
       }
     }
--- skottie/modules/skottie-slot-manager-sk/slot-info.ts
+++ skottie/modules/skottie-slot-manager-sk/slot-info.ts
@@ -2,3 +2,14 @@ export interface ColorSlot {
   id: string;
   colorHex: string;
 }
+
+export interface ScalarSlot {
+  id: string;
+  scalar: number;
+}
+
+export interface Vec2Slot {
+  id: string;
+  x: number;
+  y: number;
+}
