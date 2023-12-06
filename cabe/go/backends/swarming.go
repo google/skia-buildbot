bebb07797be75c784f76823c434ca4a33b520378
seanmccullough <seanmccullough@google.com>
2023-12-05 16:59:16 -0800

    [cabe] add trace spans for swarming API calls
    Uses the ochttp helper package since swarming's API client library uses
    net/http for transport.
    
    This should report spans that tend to take up about the first half of
    total request time (anecdata; based only on some manual spot checks I
    did in the gcp console trace viewer).
    
    Change-Id: I05ae0c0646a02ed678c73d9422904853d9db7259
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/786687
    Reviewed-by: Hao Wu <haowoo@google.com>
    Reviewed-by: Jeff Yoon <jeffyoon@google.com>
    Commit-Queue: Sean McCullough <seanmccullough@google.com>

--- cabe/go/backends/swarming.go
+++ cabe/go/backends/swarming.go
@@ -2,7 +2,9 @@ package backends
 
 import (
 	"context"
+	"net/http"
 
+	"go.opencensus.io/plugin/ochttp"
 	"go.skia.org/infra/go/auth"
 	"go.skia.org/infra/go/httputils"
 	"go.skia.org/infra/go/sklog"
@@ -20,9 +22,13 @@ func DialSwarming(ctx context.Context) (swarming.ApiClient, error) {
 	if err != nil {
 		sklog.Fatalf("Problem setting up default token source: %s", err)
 	}
-	client := httputils.DefaultClientConfig().WithTokenSource(httpClientTokenSource).With2xxOnly().Client()
 
-	ret, err := swarming.NewApiClient(client, swarmingServiceAddress)
+	client := httputils.DefaultClientConfig().WithTokenSource(httpClientTokenSource).With2xxOnly().Client()
+	tracedClient := &http.Client{Transport: &ochttp.Transport{
+		Base: client.Transport,
+	}}
+
+	ret, err := swarming.NewApiClient(tracedClient, swarmingServiceAddress)
 
 	return ret, err
 }
