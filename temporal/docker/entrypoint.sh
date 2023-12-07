fd238f3fc588a98bedd2e2921f53310c61b5eafb
Hao Wu <haowoo@google.com>
2023-12-07 06:06:26 +0000

    Add cli and db tools to temporal image.
    This CL adds two tools:
    1. temporal: the command line tool to manage temporal service
    2. temporal-sql-tool: the command line tool to manage database
    
    This CL also adds dockerize to templatize config.yaml used for temporal-server.
    
    Bug: b/311455474
    Change-Id: Ia88519427c68e415d88426f8118a7ea56e2fdbd8
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/787105
    Commit-Queue: Hao Wu <haowoo@google.com>
    Reviewed-by: Leandro Lovisolo <lovisolo@google.com>

---
+++ temporal/docker/entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#
+# Modified from:
+# https://github.com/temporalio/docker-builds/blob/9f2a59c6d2dd979b2afb8a83b221769b33557cd8/docker/entrypoint.sh#L1
+
+set -eu -o pipefail
+
+: "${BIND_ON_IP:=$(getent hosts "$(hostname)" | awk '{print $1;}')}"
+export BIND_ON_IP
+
+# check TEMPORAL_ADDRESS is not empty
+if [[ -z "${TEMPORAL_ADDRESS:-}" ]]; then
+    echo "TEMPORAL_ADDRESS is not set, setting it to ${BIND_ON_IP}:7233"
+
+    if echo "${BIND_ON_IP}" | grep -Eq ":"; then
+        # ipv6
+        export TEMPORAL_ADDRESS="[${BIND_ON_IP}]:7233"
+    else
+        # ipv4
+        export TEMPORAL_ADDRESS="${BIND_ON_IP}:7233"
+    fi
+fi
+
+/temporal/dockerize -template /temporal/config/config_template.yaml:/tmp/docker.yaml
+
+exec /temporal/temporal-server --config /tmp -env docker start
