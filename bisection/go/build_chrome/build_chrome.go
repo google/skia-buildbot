1039ea31a5f8b7f207690259b5446168eb4ca7bc
Leina Sun <sunxiaodi@google.com>
2024-01-08 17:13:32 +0000

    [bisection] build chrome
    This CL omits support for gerrit patches and non-chromium commits.
    Those features will be added in subsequent CLs.
    
    Bug: b/315215756
    Change-Id: I6fba043de5bfcd2a0a82252a249d153326cbce3e
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/792216
    Reviewed-by: Jeff Yoon <jeffyoon@google.com>
    Reviewed-by: Sean McCullough <seanmccullough@google.com>
    Commit-Queue: Leina Sun <sunxiaodi@google.com>

--- bisection/go/build_chrome/build_chrome.go
+++ bisection/go/build_chrome/build_chrome.go
@@ -8,16 +8,45 @@ package build_chrome
 import (
 	"context"
 	"fmt"
+	"strconv"
+	"strings"
+	"time"
 
+	"github.com/google/uuid"
 	"go.chromium.org/luci/grpc/prpc"
+	"go.skia.org/infra/bisection/go/bot_configs"
 	"go.skia.org/infra/go/auth"
 	"go.skia.org/infra/go/buildbucket"
 	"go.skia.org/infra/go/httputils"
+	"go.skia.org/infra/go/skerr"
 	"go.skia.org/infra/go/sklog"
 	"golang.org/x/oauth2/google"
+	"google.golang.org/protobuf/types/known/fieldmaskpb"
+	"google.golang.org/protobuf/types/known/structpb"
 
 	buildbucketpb "go.chromium.org/luci/buildbucket/proto"
+	swarmingV1 "go.chromium.org/luci/common/api/swarming/swarming/v1"
 )
+
+// BuildChrome stores all of the parameters
+// used by the build Chrome workflow
+type BuildChrome struct {
+	// Client is the buildbucket client.
+	Client buildbucketpb.BuildsClient
+	// Commit is the chromium commit hash.
+	Commit string
+	// Device is the name of the device, e.g. "linux-perf".
+	Device string
+	// The corresponding Chrome builder associated with
+	// the device. Can get the builder from package
+	// bot_configs.
+	Builder string
+	// Target is name of the build isolate target
+	// e.g. "performance_test_suite".
+	Target string
+	// Patch is the Gerrit patch included in the build.
+	Patch []*buildbucketpb.GerritChange
+}
 
 // pinpointWaterfall maps builder names from Pinpoint to Waterfall
 // Builds from waterfall can also be recycled for bisection
@@ -53,6 +82,39 @@ var pinpointWaterfall = map[string]string{
 	"Win x64 Builder Perf PGO":                   "win64-builder-perf-pgo",
 }
 
+// name of the bucket used by Pinpoint builders in LUCI
+const pinpointBucket = "try"
+
+// chromium gitiles links
+const (
+	chromiumGitilesUrl     = "https://chromium.googlesource.com/chromium/src"
+	chromiumGitilesHost    = "chromium.googlesource.com"
+	chromiumGitilesProject = "chromium/src"
+	chromiumGitilesRef     = "refs/heads/main"
+)
+
+// The swarming instance the build CAS isolate belongs to
+// TODO(b/315215756): Support other swarming instances. There are three known
+// swarming instances Pinpoint supports. The majority of Pinpoint builds are
+// this defaultInstance. Buildbucket API does not report the swarming instance
+// so our options are to:
+// - include the expected instance in the build tags
+// - try all 3 known swarming instances and brute force it
+const defaultInstance string = "projects/chrome-swarming/instances/default_instance"
+
+// RBE CAS isolates expire after 32 days. We use 30 out of caution.
+const casExpiration int = 30
+
+// These constants define default fields used in a buildbucket
+// ScheduleBuildRequest for Pinpoint Chrome builds
+const (
+	ScheduleReqClobber string = "clobber"
+	ScheduleReqDeps    string = "deps_revision_overrides"
+	ScheduleReqGit     string = "git_repo"
+	ScheduleReqRev     string = "revision"
+	ScheduleReqStage   string = "staging"
+)
+
 // DialBuildClient returns an authenticated LUCI Buildbucket client instance.
 //
 // Although skia has their own buildbucket wrapper type, it cannot build Chrome
@@ -73,13 +135,12 @@ func DialBuildClient(ctx context.Context) (buildbucketpb.BuildsClient, error) {
 	}), nil
 }
 
-func createSearchBuildRequest(bucket string, builder string, commit string,
-	patch []*buildbucketpb.GerritChange) *buildbucketpb.SearchBuildsRequest {
+func (b *BuildChrome) createSearchBuildRequest(bucket string, builder string) *buildbucketpb.SearchBuildsRequest {
 	tags := []*buildbucketpb.StringPair{
 		{
 			// buildset is how commit information is tracked in waterfall and Pinpoint.
 			Key:   "buildset",
-			Value: fmt.Sprintf("commit/gitiles/chromium.googlesource.com/chromium/src/+/%s", commit),
+			Value: fmt.Sprintf("commit/gitiles/chromium.googlesource.com/chromium/src/+/%s", b.Commit),
 		},
 	}
 	req := &buildbucketpb.SearchBuildsRequest{
@@ -93,31 +154,45 @@ func createSearchBuildRequest(bucket string, builder string, commit string,
 		},
 	}
 	// patch is always nil for waterfall builds
-	if patch != nil {
-		req.Predicate.GerritChanges = patch
+	if b.Patch != nil {
+		req.Predicate.GerritChanges = b.Patch
 	}
 	return req
 }
 
-func search(ctx context.Context, client buildbucketpb.BuildsClient,
-	req *buildbucketpb.SearchBuildsRequest) (int64, buildbucketpb.Status, error) {
-	resp, err := client.SearchBuilds(ctx, req)
+func (b *BuildChrome) search(ctx context.Context, req *buildbucketpb.SearchBuildsRequest) (int64, error) {
+	resp, err := b.Client.SearchBuilds(ctx, req)
 	if err != nil {
-		return 0, 0, fmt.Errorf("error searching buildbucket builds with request %v\n and error: %s", req, err)
+		return 0, fmt.Errorf("error searching buildbucket builds with request %v\n and error: %s", req, err)
 	}
-	// builds are returned from newest to oldest
+	if resp == nil {
+		return 0, nil
+	}
+
 	for _, build := range resp.Builds {
+		// builds are returned from newest to oldest
+		// if a completed build is past the expiration point,
+		// then all remaining builds are too old
+		// incomplete builds have default endtime of 1970-01-01 00:00 UTC
+		if build.Status.Number() > buildbucketpb.Status_ENDED_MASK.Number() &&
+			time.Now().Sub(build.EndTime.AsTime()).Hours()/24 > float64(casExpiration) {
+			return 0, nil
+		}
+		// verify successful or ongoing build with correct gerrit changes
 		// client.SearchBuilds will find all builds that include the GerritChanges
 		// rather than matching builds that have the exact same GerritChanges
 		// so we need to verify there are an equal number of gerrit patches
-		if len(req.Predicate.GerritChanges) == len(build.Input.GerritChanges) {
-			return build.Id, build.Status, nil
+		if (build.Status == buildbucketpb.Status_SUCCESS ||
+			build.Status == buildbucketpb.Status_STARTED ||
+			build.Status == buildbucketpb.Status_SCHEDULED) &&
+			len(req.Predicate.GerritChanges) == len(build.Input.GerritChanges) {
+			return build.Id, nil
 		}
 	}
-	return 0, 0, nil
+	return 0, nil
 }
 
-// SearchBuild looks for an existing buildbucket build using the
+// searchBuild looks for an existing buildbucket build using the
 // builder and the commit and returns the build ID and status of the build
 // TODO(b/315215756): add support for non-chromium commits. A non-chromium build, such
 // as this one: https://ci.chromium.org/ui/p/chrome/builders/try/Android%20arm64%20Compile%20Perf/117084/overview,
@@ -126,19 +201,19 @@ func search(ctx context.Context, client buildbucketpb.BuildsClient,
 // be to fetch all the builds under the chromium buildset and hash, and then iterate
 // through each build for the correct deps_revision_overrides. A better solution
 // would be to add the non-chromium commit info to the tags and query the tags.
-func SearchBuild(ctx context.Context, client buildbucketpb.BuildsClient, builder string,
-	commit string, patch []*buildbucketpb.GerritChange) (int64, buildbucketpb.Status, error) {
+func (b *BuildChrome) searchBuild(ctx context.Context, builder string) (int64, error) {
 	req := &buildbucketpb.SearchBuildsRequest{}
 
 	// search Pinpoint for build
-	req = createSearchBuildRequest("try", builder, commit, patch)
-	buildID, status, err := search(ctx, client, req)
+	req = b.createSearchBuildRequest(pinpointBucket, builder)
+	sklog.Debugf("SearchBuild: request %v", req)
+	buildId, err := b.search(ctx, req)
 	if err != nil {
-		return 0, 0, fmt.Errorf("error searching buildbucket builds with request %v\n and error: %s", req, err)
+		return 0, fmt.Errorf("error searching buildbucket builds with request %v\n and error: %s", req, err)
 	}
-	if buildID != 0 {
-		sklog.Debugf("build %d found in Pinpoint builder with status %v\n", buildID, status)
-		return buildID, status, nil
+	if buildId != 0 {
+		sklog.Debugf("SearchBuild: build %d found in Pinpoint builder\n", buildId)
+		return buildId, nil
 	}
 
 	// Search waterfall for build if there is an appropriate waterfall
@@ -146,19 +221,170 @@ func SearchBuild(ctx context.Context, client buildbucketpb.BuildsClient, builder
 	// because waterfall builders lag behind main. A user could try to
 	// request a build via Pinpoint before waterfall has the chance to
 	// build the same commit.
-	if pinpointWaterfall[builder] != "" && len(patch) == 0 {
-		sklog.Debugf("search waterfall builder %s for build", pinpointWaterfall[builder])
-		req = createSearchBuildRequest("ci", pinpointWaterfall[builder], commit, nil)
-		buildID, status, err := search(ctx, client, req)
+	if pinpointWaterfall[builder] != "" && len(b.Patch) == 0 {
+		sklog.Debugf("SearchBuild: search waterfall builder %s for build", pinpointWaterfall[builder])
+		req = b.createSearchBuildRequest("ci", pinpointWaterfall[builder])
+		buildId, err := b.search(ctx, req)
 		if err != nil {
-			return 0, 0, fmt.Errorf("error searching buildbucket builds with request %v\n and error: %s", req, err)
+			return 0, skerr.Fmt("error searching buildbucket builds with request %v\n and error: %s", req, err)
 		}
-		if buildID != 0 {
-			sklog.Debugf("build %d found in Waterfall builder with status %v", buildID, status)
-			return buildID, status, nil
+		if buildId != 0 {
+			sklog.Debugf("SearchBuild: build %d found in Waterfall builder", buildId)
+			return buildId, nil
 		}
 	}
 
-	sklog.Debug("build could not be found")
-	return 0, 0, nil
+	sklog.Debug("SearchBuild: build could not be found")
+	return 0, nil
+}
+
+// TODO(b/315215756): Add support for non-chromium commits and gerrit patches
+func (b *BuildChrome) createScheduleRequest(builder string) (
+	*buildbucketpb.ScheduleBuildRequest, error) {
+	req := &buildbucketpb.ScheduleBuildRequest{
+		RequestId: uuid.New().String(),
+		Builder: &buildbucketpb.BuilderID{
+			Project: "chrome",
+			Bucket:  pinpointBucket,
+			Builder: builder,
+		},
+		Properties: &structpb.Struct{
+			Fields: map[string]*structpb.Value{
+				ScheduleReqClobber: {
+					Kind: &structpb.Value_BoolValue{
+						BoolValue: false,
+					},
+				},
+				ScheduleReqGit: {
+					Kind: &structpb.Value_StringValue{
+						StringValue: chromiumGitilesUrl,
+					},
+				},
+				ScheduleReqRev: {
+					Kind: &structpb.Value_StringValue{
+						StringValue: b.Commit,
+					},
+				},
+				ScheduleReqStage: {
+					Kind: &structpb.Value_BoolValue{
+						BoolValue: false,
+					},
+				},
+			},
+		},
+		GitilesCommit: &buildbucketpb.GitilesCommit{
+			Host:    chromiumGitilesHost,
+			Project: chromiumGitilesProject,
+			Id:      b.Commit,
+			Ref:     chromiumGitilesRef,
+		},
+		GerritChanges: b.Patch,
+		// TODO(b/315215756): Implement createTags function to
+		// generalize across different job types
+		Tags: []*buildbucketpb.StringPair{
+			{
+				Key:   "pinpoint_job_id",
+				Value: "1",
+			},
+			{
+				Key:   "skia_pinpoint_bisect",
+				Value: "true",
+			},
+			{
+				Key:   "buildset",
+				Value: fmt.Sprintf("commit/gitiles/chromium.googlesource.com/chromium/src/+/%s", b.Commit),
+			},
+		},
+	}
+
+	return req, nil
+}
+
+// TODO(b/315215756): implement cancel build
+
+// Run searches buildbucket for an old Chrome build
+// and will schedule a new Chrome build if it cannot
+// find one.
+// Run returns the build ID of an existing, ongoing,
+// or new build.
+func (b *BuildChrome) Run(ctx context.Context) (
+	int64, error) {
+
+	builder, err := bot_configs.GetBotConfig(b.Device, false)
+	if err != nil {
+		return 0, err
+	}
+
+	buildId, err := b.searchBuild(ctx, builder.Builder)
+	if err != nil {
+		return 0, err
+	}
+	// found a build
+	if buildId != 0 {
+		return buildId, nil
+	}
+
+	// if the ongoing build failed or the build was not found, start new build
+	newBuildReq, err := b.createScheduleRequest(builder.Builder)
+	if err != nil {
+		return 0, err
+	}
+
+	build, err := b.Client.ScheduleBuild(ctx, newBuildReq)
+	if err != nil {
+		return 0, err
+	}
+	return build.Id, nil
+}
+
+// CheckBuildStatus returns the builds status given a buildId
+// TODO(b/315215756): switch from polling to pub sub
+func (b *BuildChrome) CheckBuildStatus(ctx context.Context, buildId int64) (buildbucketpb.Status, error) {
+	statusReq := &buildbucketpb.GetBuildStatusRequest{
+		Id: buildId,
+	}
+	build, err := b.Client.GetBuildStatus(ctx, statusReq)
+	if err != nil {
+		return buildbucketpb.Status_STATUS_UNSPECIFIED, err
+	}
+
+	return build.Status, nil
+}
+
+// RetrieveCAS returns the CAS location of the build given buildId
+func (b *BuildChrome) RetrieveCas(ctx context.Context, buildId int64) (
+	*swarmingV1.SwarmingRpcsCASReference, error) {
+	req := buildbucketpb.GetBuildRequest{
+		Id: buildId,
+		Mask: &buildbucketpb.BuildMask{
+			Fields: &fieldmaskpb.FieldMask{
+				Paths: []string{"output.properties"},
+			},
+		},
+	}
+	build, err := b.Client.GetBuild(ctx, &req)
+	if err != nil {
+		return nil, err
+	}
+	if build.Status != buildbucketpb.Status_SUCCESS {
+		return nil, skerr.Fmt("Cannot retrieve CAS from build %d with status %v",
+			buildId, build.Status)
+	}
+	for k, v := range build.Output.Properties.Fields {
+		if strings.Contains(k, "swarm_hashes_refs") {
+			cas := fmt.Sprintf("%v", v.GetStructValue().AsMap()[b.Target])
+			bytes, err := strconv.ParseInt(strings.Split(cas, "/")[1], 10, 64)
+			if err != nil {
+				return nil, err
+			}
+			return &swarmingV1.SwarmingRpcsCASReference{
+				CasInstance: defaultInstance,
+				Digest: &swarmingV1.SwarmingRpcsDigest{
+					Hash:      strings.Split(cas, "/")[0],
+					SizeBytes: bytes,
+				},
+			}, nil
+		}
+	}
+	return nil, skerr.Fmt("Could not find the CAS outputs to build %d", buildId)
 }
--- bisection/go/build_chrome/build_chrome_test.go
+++ bisection/go/build_chrome/build_chrome_test.go
@@ -8,53 +8,107 @@ import (
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
 	buildbucketpb "go.chromium.org/luci/buildbucket/proto"
+	swarmingV1 "go.chromium.org/luci/common/api/swarming/swarming/v1"
+	"google.golang.org/protobuf/types/known/structpb"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 type mockBuildsClient struct {
 	shouldErr bool
 }
 
+// casOutput is the default the build output
+// properties mocked return. The structure is
+// rather complex so it's defined here to avoid
+// obscuring what the tests do
+var casOutput = buildbucketpb.Build_Output{
+	Properties: &structpb.Struct{
+		Fields: map[string]*structpb.Value{
+			"fake_field": {},
+			"swarm_hashes_refs": {
+				Kind: &structpb.Value_StructValue{
+					StructValue: &structpb.Struct{
+						Fields: map[string]*structpb.Value{
+							"target": {
+								Kind: &structpb.Value_StringValue{
+									StringValue: "hash/123",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	},
+}
+
 func TestSearchBuild(t *testing.T) {
 	for i, test := range []struct {
 		name          string
+		bc            *BuildChrome
 		builder       string
-		commit        string
-		patch         []*buildbucketpb.GerritChange
-		expected      *buildbucketpb.SearchBuildsResponse
+		mockResp      *buildbucketpb.SearchBuildsResponse
+		expected      int64
 		expectedError bool
 	}{
 		{
-			name:          "buildbucket search error",
+			name: "buildbucket search error",
+			bc: &BuildChrome{
+				Commit: "commit",
+			},
 			builder:       "builder",
-			commit:        "commit",
-			patch:         nil,
-			expected:      &buildbucketpb.SearchBuildsResponse{},
+			expected:      0,
 			expectedError: true,
 		},
 		{
 			name:    "build found",
 			builder: "builder",
-			commit:  "commit",
-			patch:   nil,
-			expected: &buildbucketpb.SearchBuildsResponse{
+			bc: &BuildChrome{
+				Commit: "commit",
+			},
+			mockResp: &buildbucketpb.SearchBuildsResponse{
 				Builds: []*buildbucketpb.Build{
 					{
-						Id:     1,
-						Status: buildbucketpb.Status_SUCCESS,
+						Id:      1,
+						Status:  buildbucketpb.Status_SUCCESS,
+						EndTime: timestamppb.Now(),
 						Input: &buildbucketpb.Build_Input{
 							GerritChanges: []*buildbucketpb.GerritChange{},
 						},
 					},
 				},
 			},
+			expected:      1,
 			expectedError: false,
 		},
 		{
-			name:          "build not found",
-			builder:       "builder",
-			commit:        "commit",
-			patch:         nil,
-			expected:      &buildbucketpb.SearchBuildsResponse{},
+			name:    "build found but failure",
+			builder: "builder",
+			bc: &BuildChrome{
+				Commit: "commit",
+			},
+			mockResp: &buildbucketpb.SearchBuildsResponse{
+				Builds: []*buildbucketpb.Build{
+					{
+						Id:      1,
+						Status:  buildbucketpb.Status_FAILURE,
+						EndTime: timestamppb.Now(),
+						Input: &buildbucketpb.Build_Input{
+							GerritChanges: []*buildbucketpb.GerritChange{},
+						},
+					},
+				},
+			},
+			expected:      0,
+			expectedError: false,
+		},
+		{
+			name:    "build not found",
+			builder: "builder",
+			bc: &BuildChrome{
+				Commit: "commit",
+			},
+			expected:      0,
 			expectedError: false,
 		},
 	} {
@@ -66,24 +120,251 @@ func TestSearchBuild(t *testing.T) {
 
 			if test.expectedError {
 				client.EXPECT().SearchBuilds(ctx, gomock.Any()).Return(
-					test.expected, fmt.Errorf("some error"))
+					nil, fmt.Errorf("some error"))
 			} else {
 				client.EXPECT().SearchBuilds(ctx, gomock.Any()).Return(
-					test.expected, nil)
+					test.mockResp, nil)
 			}
 
-			id, status, err := SearchBuild(ctx, client, test.builder, test.commit, test.patch)
+			test.bc.Client = client
+
+			id, err := test.bc.searchBuild(ctx, test.builder)
 			if test.expectedError {
 				assert.Error(t, err)
 			} else {
 				assert.NoError(t, err)
-				if len(test.expected.Builds) > 0 {
-					assert.Equal(t, test.expected.Builds[0].Id, id)
-					assert.Equal(t, test.expected.Builds[0].Status, status)
-				} else {
-					assert.Zero(t, id)
-					assert.Zero(t, status)
-				}
+				assert.Equal(t, test.expected, id)
+			}
+		})
+	}
+}
+
+func TestCheckBuildStatus(t *testing.T) {
+	for i, test := range []struct {
+		name          string
+		mockResp      *buildbucketpb.Build
+		expected      buildbucketpb.Status
+		expectedError bool
+	}{
+		{
+			name: "build success",
+			mockResp: &buildbucketpb.Build{
+				Status: buildbucketpb.Status_SUCCESS,
+			},
+			expected:      buildbucketpb.Status_SUCCESS,
+			expectedError: false,
+		},
+		{
+			name: "build failed",
+			mockResp: &buildbucketpb.Build{
+				Status: buildbucketpb.Status_FAILURE,
+			},
+			expected:      buildbucketpb.Status_FAILURE,
+			expectedError: false,
+		},
+		{
+			name:          "GetBuildStatus error",
+			expected:      buildbucketpb.Status_STATUS_UNSPECIFIED,
+			expectedError: true,
+		},
+	} {
+		t.Run(fmt.Sprintf("[%d] %s", i, test.name), func(t *testing.T) {
+			ctx := context.Background()
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+			client := buildbucketpb.NewMockBuildsClient(ctrl)
+
+			if test.expectedError {
+				client.EXPECT().GetBuildStatus(ctx, gomock.Any()).Return(
+					test.mockResp, fmt.Errorf("some error"))
+			} else {
+				client.EXPECT().GetBuildStatus(ctx, gomock.Any()).Return(
+					test.mockResp, nil)
+			}
+
+			bc := &BuildChrome{
+				Client: client,
+			}
+
+			status, err := bc.CheckBuildStatus(ctx, 0)
+			if test.expectedError {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+			assert.Equal(t, test.expected, status)
+		})
+	}
+}
+
+func TestBuildNonExistentDevice(t *testing.T) {
+	ctx := context.Background()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	client := buildbucketpb.NewMockBuildsClient(ctrl)
+
+	bc := BuildChrome{
+		Client: client,
+		Device: "non-existent device",
+		Target: "target",
+		Commit: "commit",
+	}
+
+	id, err := bc.Run(ctx)
+	assert.Error(t, err)
+	assert.Zero(t, id)
+}
+
+func TestBuildFound(t *testing.T) {
+	mockResp := &buildbucketpb.SearchBuildsResponse{
+		Builds: []*buildbucketpb.Build{
+			{
+				Id:      1,
+				Status:  buildbucketpb.Status_SUCCESS,
+				EndTime: timestamppb.Now(),
+				Input: &buildbucketpb.Build_Input{
+					GerritChanges: []*buildbucketpb.GerritChange{},
+				},
+			},
+		},
+	}
+	expected := int64(1)
+
+	ctx := context.Background()
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	client := buildbucketpb.NewMockBuildsClient(ctrl)
+
+	client.EXPECT().SearchBuilds(gomock.Any(), gomock.Any()).Return(
+		mockResp, nil)
+
+	bc := BuildChrome{
+		Client: client,
+		Device: "linux-perf",
+		Target: "target",
+		Commit: "commit",
+	}
+
+	id, err := bc.Run(ctx)
+	assert.NoError(t, err)
+	assert.Equal(t, expected, id)
+}
+
+func TestNewBuild(t *testing.T) {
+	for i, test := range []struct {
+		name          string
+		mockResp      *buildbucketpb.Build
+		expected      int64
+		expectedError bool
+	}{
+		{
+			name: "build success",
+			mockResp: &buildbucketpb.Build{
+				Id: 1,
+			},
+			expected:      1,
+			expectedError: false,
+		},
+		{
+			name:          "build failed",
+			expected:      0,
+			expectedError: true,
+		},
+	} {
+		t.Run(fmt.Sprintf("[%d] %s", i, test.name), func(t *testing.T) {
+			ctx := context.Background()
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+			client := buildbucketpb.NewMockBuildsClient(ctrl)
+
+			client.EXPECT().SearchBuilds(ctx, gomock.Any()).Return(
+				nil, nil)
+			client.EXPECT().SearchBuilds(gomock.Any(), gomock.Any()).Return(
+				nil, nil)
+			if test.expectedError {
+				client.EXPECT().ScheduleBuild(ctx, gomock.Any()).Return(
+					nil, fmt.Errorf("some error"))
+			} else {
+				client.EXPECT().ScheduleBuild(ctx, gomock.Any()).Return(
+					test.mockResp, nil)
+			}
+
+			bc := BuildChrome{
+				Client: client,
+				Device: "linux-perf",
+				Target: "target",
+				Commit: "commit",
+			}
+
+			id, err := bc.Run(ctx)
+			if test.expectedError {
+				assert.Error(t, err)
+			} else {
+				assert.NoError(t, err)
+			}
+			assert.Equal(t, test.expected, id)
+		})
+	}
+}
+
+func TestRetrieveCas(t *testing.T) {
+	for i, test := range []struct {
+		name          string
+		mockResp      *buildbucketpb.Build
+		expected      *swarmingV1.SwarmingRpcsDigest
+		expectedError bool
+	}{
+		{
+			name: "build failed",
+			mockResp: &buildbucketpb.Build{
+				Status: buildbucketpb.Status_FAILURE,
+			},
+			expected:      nil,
+			expectedError: true,
+		},
+		{
+			name: "build ongoing",
+			mockResp: &buildbucketpb.Build{
+				Status: buildbucketpb.Status_STARTED,
+			},
+			expected:      nil,
+			expectedError: true,
+		},
+		{
+			name: "retrieve cas success",
+			mockResp: &buildbucketpb.Build{
+				Status: buildbucketpb.Status_SUCCESS,
+				Output: &casOutput,
+			},
+			expected: &swarmingV1.SwarmingRpcsDigest{
+				Hash:      "hash",
+				SizeBytes: 123,
+			},
+			expectedError: false,
+		},
+	} {
+		t.Run(fmt.Sprintf("[%d] %s", i, test.name), func(t *testing.T) {
+			ctx := context.Background()
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+			client := buildbucketpb.NewMockBuildsClient(ctrl)
+
+			client.EXPECT().GetBuild(ctx, gomock.Any()).Return(
+				test.mockResp, nil)
+
+			bc := BuildChrome{
+				Client: client,
+				Target: "target",
+			}
+
+			cas, err := bc.RetrieveCas(ctx, 0)
+			if test.expectedError {
+				assert.Error(t, err)
+				assert.Nil(t, cas)
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, test.expected.Hash, cas.Digest.Hash)
+				assert.Equal(t, test.expected.SizeBytes, cas.Digest.SizeBytes)
 			}
 		})
 	}
