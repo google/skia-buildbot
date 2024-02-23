4a5a5836c0682310cd9c5652a9b66b2164dd3a95
skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
2024-02-23 04:00:50 +0000

    Roll Skia Infra CIPD packages from c615566d8899 to 88ef1b147e5e (14 revisions)
    https://skia.googlesource.com/buildbot.git/+log/c615566d8899..88ef1b147e5e
    
    2024-02-23 sunxiaodi@google.com [pinpoint] implement run benchmark temporal workflow
    2024-02-23 haowoo@google.com Add FindMidCommit and remove CombinedCommit and CommitRange.
    2024-02-23 haowoo@google.com Use CombinedCommit to build Chrome.
    2024-02-23 sunxiaodi@google.com [pinpoint] add status check helper functions to run_benchmark
    2024-02-23 haowoo@google.com Add FindDepsCommit.
    2024-02-22 borenet@google.com [bbv2] Set finished timestamp for jobs we failed to start
    2024-02-22 ashwinpv@google.com Create an initial version of the backend service.
    2024-02-22 borenet@google.com [bbv2] Call StartBuild regardless of whether syncing failed
    2024-02-22 haowoo@google.com Add the Build and TestRun struct.
    2024-02-22 borenet@google.com [bbv2] Add --buildbucket_project flag to task-scheduler-jc
    2024-02-22 borenet@google.com [bbv2] Add scheduler URL flag to task-scheduler-bbfe
    2024-02-22 haowoo@google.com Make CombinedCommit public.
    2024-02-22 borenet@google.com [bbv2] Fail RunTask and StartBuild when Buildbucket tokens are missing
    2024-02-22 skia-autoroll@skia-public.iam.gserviceaccount.com Roll Skia Infra CIPD packages from 0c57895d60d7 to c615566d8899 (6 revisions)
    
    If this roll has caused a breakage, revert this CL and stop the roller
    using the controls here:
    https://autoroll.skia.org/r/skia-infra-cipd-infra
    Please CC jcgregorio@google.com,skiabot@google.com on the revert to ensure that a human
    is aware of the problem.
    
    To file a bug in Skia Infra CIPD packages: https://bugs.chromium.org/p/skia/issues/entry
    To file a bug in Skia Infra: https://bugs.chromium.org/p/skia/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://issues.skia.org/issues/new?component=1389291&template=1850622
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Tbr: jcgregorio@google.com
    Change-Id: I3084125a8c1c3b3814fd9991516d9e1802f3d46e
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/817540
    Bot-Commit: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
    Commit-Queue: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>

--- go/cipd/asset_versions_gen.go
+++ go/cipd/asset_versions_gen.go
@@ -186,17 +186,17 @@ var PACKAGES = map[string]*Package{
 	"skia/tools/bazel_build_all/${platform}": {
 		Path:    "task_drivers",
 		Name:    "skia/tools/bazel_build_all/${platform}",
-		Version: "git_revision:c615566d88994355d8b93a9040177489484e03a7",
+		Version: "git_revision:88ef1b147e5ee862de12069ed560f89a7d09b2af",
 	},
 	"skia/tools/bazel_test_all/${platform}": {
 		Path:    "task_drivers",
 		Name:    "skia/tools/bazel_test_all/${platform}",
-		Version: "git_revision:c615566d88994355d8b93a9040177489484e03a7",
+		Version: "git_revision:88ef1b147e5ee862de12069ed560f89a7d09b2af",
 	},
 	"skia/tools/command_wrapper/${platform}": {
 		Path:    "task_drivers",
 		Name:    "skia/tools/command_wrapper/${platform}",
-		Version: "git_revision:c615566d88994355d8b93a9040177489484e03a7",
+		Version: "git_revision:88ef1b147e5ee862de12069ed560f89a7d09b2af",
 	},
 	"skia/tools/goldctl/${platform}": {
 		Path:    "cipd_bin_packages",
@@ -206,6 +206,6 @@ var PACKAGES = map[string]*Package{
 	"skia/tools/presubmit/${platform}": {
 		Path:    "task_drivers",
 		Name:    "skia/tools/presubmit/${platform}",
-		Version: "git_revision:c615566d88994355d8b93a9040177489484e03a7",
+		Version: "git_revision:88ef1b147e5ee862de12069ed560f89a7d09b2af",
 	},
 }
