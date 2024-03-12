8a28b882960763ce6cc1d66d8718e6ae90c546f6
Vidit Chitkara <viditchitkara@google.com>
2024-03-12 19:24:45 +0000

    [pinpoint] Refactor tests for mann whitney
    This CL refactors tests in pinpoint/go/compare/mann_whitney_u_test
    to the test nomenclature followed by skia
    
    Bug: b/327664735
    Change-Id: Ib0e8d8aecc8ce85ef418773081496b4f9b561baa
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/821958
    Reviewed-by: Kevin Lubick <kjlubick@google.com>
    Commit-Queue: Vidit Chitkara <viditchitkara@google.com>

--- pinpoint/go/compare/mann_whitney_u_test.go
+++ pinpoint/go/compare/mann_whitney_u_test.go
@@ -1,70 +1,47 @@
 package compare
 
 import (
-	"fmt"
-	"math"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestMannWhitneyU(t *testing.T) {
-	for i, test := range []struct {
-		name     string
-		xdata    []float64
-		ydata    []float64
-		expected float64
-		// if True, use almostEquals criteria, which is numerical difference
-		// rounded to the 7th difference
-		// https://docs.python.org/3/library/unittest.html#unittest.TestCase.assertAlmostEqual
-		// else, use exact equals
-		almostEquals bool
-	}{
-		{
-			name:         "small samples",
-			xdata:        []float64{0},
-			ydata:        []float64{1},
-			expected:     1.0,
-			almostEquals: false,
-		},
-		{
-			name:         "basic test 1",
-			xdata:        []float64{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
-			ydata:        []float64{20, 21, 22, 23, 24, 25, 26, 27, 28, 29},
-			expected:     0.00018267179110955002,
-			almostEquals: true,
-		},
-		{
-			name:         "basic test 2",
-			xdata:        []float64{0, 1, 2, 3, 4},
-			ydata:        []float64{0, 1, 2, 3, 4, 5, 6, 7, 8, 9},
-			expected:     0.13986357686781267,
-			almostEquals: true,
-		},
-		{
-			name:         "duplicate values",
-			xdata:        []float64{0, 0, 0, 0, 0},
-			ydata:        []float64{1, 1, 1, 1, 1},
-			expected:     0.0039767517097886512,
-			almostEquals: true,
-		},
-		{
-			name:         "identical values",
-			xdata:        []float64{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-			ydata:        []float64{0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-			expected:     1.0,
-			almostEquals: false,
-		},
-	} {
-		t.Run(fmt.Sprintf("[%d] %s", i, test.name), func(t *testing.T) {
-			result := MannWhitneyU(test.xdata, test.ydata)
-			if test.almostEquals {
-				if math.Abs(result-test.expected) > 1e-7 {
-					t.Errorf("Expected %v, but got %v", test.expected, result)
-				}
+func TestMannWhitneyU_Samples_ReturnsExpectedOutput(t *testing.T) {
+	test := func(name string, xData, yData []float64, expected float64, exact bool) {
+		t.Run(name, func(t *testing.T) {
+			result := MannWhitneyU(xData, yData)
+			if exact {
+				assert.Equal(t, result, expected)
 			} else {
-				if result != test.expected {
-					t.Errorf("Expected %v, but got %v", test.expected, result)
-				}
+				// almostExpected criteria is numerical difference
+				// rounded to the 7th difference
+				assert.InDelta(t, expected, result, 1e-7)
 			}
 		})
 	}
+
+	x := []float64{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	y := []float64{20, 21, 22, 23, 24, 25, 26, 27, 28, 29}
+	// The expected values are calculated as explained here:
+	// https://en.wikipedia.org/wiki/Mann%E2%80%93Whitney_U_test
+	test("basic test 1", x, y, 0.0001827, false)
+
+	x = []float64{0, 1, 2, 3, 4}
+	y = []float64{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	test("basic test 2", x, y, 0.1398636, false)
+
+	x = []float64{0, 0, 0, 0, 0}
+	y = []float64{1, 1, 1, 1, 1}
+	test("duplicate values", x, y, 0.0039768, false)
+
+	// For the following cases, it is expected that the outputs should exactly
+	// match the expected values.
+	// See https://source.corp.google.com/h/skia/buildbot/+/main:pinpoint/go/compare/mann_whitney_u.go;drc=fffea2a3556ac9ab9d2bf1ee62526c81aa328f62;l=50
+	x = []float64{0}
+	y = []float64{1}
+	test("small samples", x, y, 1.0, true)
+
+	x = []float64{0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+	y = []float64{0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+	test("identical values", x, y, 1.0, true)
 }
