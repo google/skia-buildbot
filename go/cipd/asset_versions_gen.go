f847b3c18c278417cfddff4c6e769b57360de205
skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
2024-03-26 04:01:38 +0000

    Roll Skia Infra CIPD packages from 3a33a92e0392 to c18c48f0aba5 (18 revisions)
    https://skia.googlesource.com/buildbot.git/+log/3a33a92e0392..c18c48f0aba5
    
    2024-03-25 wenbinzhang@google.com [chromeperf] update anomaly group schema to keep anomaly ids
    2024-03-25 haowoo@google.com Refactor pinpoint backend to use the interface.
    2024-03-25 ashwinpv@google.com Backend Service: Remove use of local credentials
    2024-03-25 rmistry@google.com [bugs-central] File.name() seems to be returning full path
    2024-03-25 borenet@google.com Fix panic running git_common.IsFromCIPD locally
    2024-03-25 borenet@google.com [deps-parser] Add support for GCS dependencies
    2024-03-25 ashwinpv@google.com Reland "Add services to BE and utility for generating clients"
    2024-03-25 sunxiaodi@google.com [pinpoint] bisect workflow continues for completed benchmark failures
    2024-03-25 rmistry@google.com [bugs-central] Read monorail SA key using secrets API for move to skia-infra-public
    2024-03-25 lovisolo@google.com Rename Infra-PerCommit-{Build,Test}-Bazel-RBE to Infra-PerCommit-{Build,Test}.
    2024-03-25 haowoo@google.com Use workflow.Channel and workflow.Selector to process bisections.
    2024-03-25 lovisolo@google.com Fix typo.
    2024-03-25 borenet@google.com Update skia.org.zone for task-scheduler-staging
    2024-03-25 ashwinpv@google.com Add the configs directory to the BE image
    2024-03-25 viditchitkara@google.com [pinpoint: pairwise] Add ability to fetch available bots
    2024-03-25 rmistry@google.com [ct] Fix email for skia-infra-public
    2024-03-25 rmistry@google.com Update DNS to move ct.skia.org to skia-infra-public
    2024-03-25 skia-autoroll@skia-public.iam.gserviceaccount.com Roll Skia Infra CIPD packages from 245a4567847f to 3a33a92e0392 (1 revision)
    
    If this roll has caused a breakage, revert this CL and stop the roller
    using the controls here:
    https://autoroll.skia.org/r/skia-infra-cipd-infra
    Please CC kjlubick@google.com,skiabot@google.com on the revert to ensure that a human
    is aware of the problem.
    
    To file a bug in Skia Infra CIPD packages: https://bugs.chromium.org/p/skia/issues/entry
    To file a bug in Skia Infra: https://bugs.chromium.org/p/skia/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://issues.skia.org/issues/new?component=1389291&template=1850622
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Tbr: kjlubick@google.com
    Change-Id: Icddd184381a6acd97e400a85ed0e1ffd3701d8f5
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/832159
    Commit-Queue: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
    Bot-Commit: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>

--- go/cipd/asset_versions_gen.go
+++ go/cipd/asset_versions_gen.go
@@ -186,17 +186,17 @@ var PACKAGES = map[string]*Package{
 	"skia/tools/bazel_build_all/${platform}": {
 		Path:    "task_drivers",
 		Name:    "skia/tools/bazel_build_all/${platform}",
-		Version: "git_revision:3a33a92e039280cac63b9f20c977eabd061d0aa3",
+		Version: "git_revision:c18c48f0aba52ef4542ef9866780d2355d12633d",
 	},
 	"skia/tools/bazel_test_all/${platform}": {
 		Path:    "task_drivers",
 		Name:    "skia/tools/bazel_test_all/${platform}",
-		Version: "git_revision:3a33a92e039280cac63b9f20c977eabd061d0aa3",
+		Version: "git_revision:c18c48f0aba52ef4542ef9866780d2355d12633d",
 	},
 	"skia/tools/command_wrapper/${platform}": {
 		Path:    "task_drivers",
 		Name:    "skia/tools/command_wrapper/${platform}",
-		Version: "git_revision:3a33a92e039280cac63b9f20c977eabd061d0aa3",
+		Version: "git_revision:c18c48f0aba52ef4542ef9866780d2355d12633d",
 	},
 	"skia/tools/goldctl/${platform}": {
 		Path:    "cipd_bin_packages",
@@ -206,6 +206,6 @@ var PACKAGES = map[string]*Package{
 	"skia/tools/presubmit/${platform}": {
 		Path:    "task_drivers",
 		Name:    "skia/tools/presubmit/${platform}",
-		Version: "git_revision:3a33a92e039280cac63b9f20c977eabd061d0aa3",
+		Version: "git_revision:c18c48f0aba52ef4542ef9866780d2355d12633d",
 	},
 }
