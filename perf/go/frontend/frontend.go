3acfcc233c13ec91bd097fd519905f7f8c446cda
Joe Gregorio <jcgregorio@google.com>
2024-04-10 19:36:06 +0000

    [perf] Fix the /help page.
    Needed GoogleAnalyticsMeasurementID added to the template context.
    
    Change-Id: Ifd68956a74eb630a13d68db06a8b10ef0201d90c
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/838899
    Reviewed-by: Ravi Mistry <rmistry@google.com>
    Commit-Queue: Joe Gregorio <jcgregorio@google.com>
    Auto-Submit: Joe Gregorio <jcgregorio@google.com>
    Commit-Queue: Ravi Mistry <rmistry@google.com>

--- perf/go/frontend/frontend.go
+++ perf/go/frontend/frontend.go
@@ -547,13 +547,15 @@ func (f *Frontend) helpHandler(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/html")
 		calcContext := calc.NewContext(nil, nil)
 		templateContext := struct {
-			Nonce   string
-			Funcs   map[string]calc.Func
-			Context template.JS
+			Nonce                        string
+			Funcs                        map[string]calc.Func
+			GoogleAnalyticsMeasurementID string
+			Context                      template.JS
 		}{
-			Nonce:   secure.CSPNonce(r.Context()),
-			Funcs:   calcContext.Funcs,
-			Context: context,
+			Nonce:                        secure.CSPNonce(r.Context()),
+			Funcs:                        calcContext.Funcs,
+			GoogleAnalyticsMeasurementID: config.Config.GoogleAnalyticsMeasurementID,
+			Context:                      context,
 		}
 		if err := f.templates.ExecuteTemplate(w, "help.html", templateContext); err != nil {
 			sklog.Error("Failed to expand template:", err)
