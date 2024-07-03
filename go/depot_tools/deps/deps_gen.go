d35ababad9bfdc98393e1028aa53ea8596ee525f
skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
2024-07-03 04:01:13 +0000

    Roll Skia Infra CIPD packages from a06878f25d87 to 20c36c70094f (5 revisions)
    https://skia.googlesource.com/buildbot.git/+log/a06878f25d87..20c36c70094f
    
    2024-07-02 ashwinpv@google.com Misc tweaks and fixes for the perf UI
    2024-07-02 jeffyoon@google.com Delete unused package "bisection"
    2024-07-02 borenet@google.com [autoroll] Downgrade find-and-replace error to warning
    2024-07-02 skia-autoroll@skia-public.iam.gserviceaccount.com Roll Skia Infra CIPD packages from ca190d337809 to a06878f25d87 (6 revisions)
    2024-07-02 skia-autoroll@skia-public.iam.gserviceaccount.com Roll Depot Tools from 6a00e594c9c2 to 5ab4dcc7bf08 (2 revisions)
    
    If this roll has caused a breakage, revert this CL and stop the roller
    using the controls here:
    https://autoroll.skia.org/r/skia-infra-cipd-infra
    Please CC rmistry@google.com,skiabot@google.com on the revert to ensure that a human
    is aware of the problem.
    
    To file a bug in Skia Infra CIPD packages: https://bugs.chromium.org/p/skia/issues/entry
    To file a bug in Skia Infra: https://bugs.chromium.org/p/skia/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://issues.skia.org/issues/new?component=1389291&template=1850622
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Tbr: rmistry@google.com
    Change-Id: I51e5ceb091276defe0df6c2dc0d7dfd99f0f1bd2
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/872996
    Commit-Queue: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
    Bot-Commit: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>

--- go/depot_tools/deps/deps_gen.go
+++ go/depot_tools/deps/deps_gen.go
@@ -154,17 +154,17 @@ var deps = deps_parser.DepsEntries{
 	},
 	"skia/tools/bazel_build_all": {
 		Id:      "skia/tools/bazel_build_all",
-		Version: "git_revision:a06878f25d8757c07423f536a74ee5409a207916",
+		Version: "git_revision:20c36c70094fd23fedaf307107231d15358bd63d",
 		Path:    "task_drivers",
 	},
 	"skia/tools/bazel_test_all": {
 		Id:      "skia/tools/bazel_test_all",
-		Version: "git_revision:a06878f25d8757c07423f536a74ee5409a207916",
+		Version: "git_revision:20c36c70094fd23fedaf307107231d15358bd63d",
 		Path:    "task_drivers",
 	},
 	"skia/tools/command_wrapper": {
 		Id:      "skia/tools/command_wrapper",
-		Version: "git_revision:a06878f25d8757c07423f536a74ee5409a207916",
+		Version: "git_revision:20c36c70094fd23fedaf307107231d15358bd63d",
 		Path:    "task_drivers",
 	},
 	"skia/tools/goldctl": {
@@ -174,7 +174,7 @@ var deps = deps_parser.DepsEntries{
 	},
 	"skia/tools/presubmit": {
 		Id:      "skia/tools/presubmit",
-		Version: "git_revision:a06878f25d8757c07423f536a74ee5409a207916",
+		Version: "git_revision:20c36c70094fd23fedaf307107231d15358bd63d",
 		Path:    "task_drivers",
 	},
 }
