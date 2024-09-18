e6c1f4c09172d3f763d861b0364758b59e44d565
Vidit Chitkara <viditchitkara@google.com>
2024-09-18 18:21:24 +0000

    [perf] Make the dry-run table less jumpy.
    Move action buttons on a separate column instead of expandable rows.
    This would make sure the user doesn't lose visual context of the table.
    
    The commit message is truncated since the relevant message can be viewed from the "commit" link in the action buttons.
    
    Bug: b/361511374
    Change-Id: Ib3b23e8f8485b7b496eb6c4b08e40219eea43f37
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/901196
    Reviewed-by: Eduardo Yap <eduardoyap@google.com>
    Commit-Queue: Vidit Chitkara <viditchitkara@google.com>

--- perf/modules/commit-detail-sk/commit-detail-sk.ts
+++ perf/modules/commit-detail-sk/commit-detail-sk.ts
@@ -7,7 +7,6 @@
  */
 import { html } from 'lit/html.js';
 import { define } from '../../../elements-sk/modules/define';
-import { $$ } from '../../../infra-sk/modules/dom';
 import { upgradeProperty } from '../../../elements-sk/modules/upgradeProperty';
 import { diffDate } from '../../../infra-sk/modules/human';
 import { ElementSk } from '../../../infra-sk/modules/ElementSk';
@@ -30,17 +29,18 @@ export class CommitDetailSk extends ElementSk {
   }
 
   private static template = (ele: CommitDetailSk) => html`
-    <div @click=${() => ele._click()} class="linkish">
+    <div class="linkish">
       <pre>
-${ele.cid.hash.slice(0, 8)} - ${diffDate(ele.cid.ts * 1000)} - ${ele.cid
-          .message} - ${ele.cid.author}</pre
+${ele.cid.hash.slice(0, 8)} - ${ele.cid.author} - ${diffDate(
+          ele.cid.ts * 1000
+        )} - ${ele.cid.message}</pre
       >
-    </div>
-    <div class="tip hidden">
-      <a href="/g/e/${ele.cid.hash}">Explore</a>
-      <a href="/g/c/${ele.cid.hash}">Cluster</a>
-      <a href="/g/t/${ele.cid.hash}">Triage</a>
-      <a href="${ele.cid.url}">Commit</a>
+      <div class="tip">
+        <a href="/g/e/${ele.cid.hash}">Explore</a>
+        <a href="/g/c/${ele.cid.hash}">Cluster</a>
+        <a href="/g/t/${ele.cid.hash}">Triage</a>
+        <a href="${ele.cid.url}">Commit</a>
+      </div>
     </div>
   `;
 
@@ -48,10 +48,6 @@ ${ele.cid.hash.slice(0, 8)} - ${diffDate(ele.cid.ts * 1000)} - ${ele.cid
     super.connectedCallback();
     upgradeProperty(this, 'cid');
     this._render();
-  }
-
-  private _click() {
-    $$('.tip', this)!.classList.toggle('hidden');
   }
 
   /** The details about a commit. */
