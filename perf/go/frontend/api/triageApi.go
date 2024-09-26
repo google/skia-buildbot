3ba9cd40c151d775af4749739523ee1a9324cb0d
Wenbin Zhang <wenbinzhang@google.com>
2024-09-26 10:32:06 -0700

    [perf] Add EditAnomalies to triageApi
    The function will send request to the edit_anomalies_skia endpoint on Chromeperf.
    
    Bug: b/365008534
    Change-Id: I02d8801455d46138dcb7d594abc8ecbf2849b608
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/902780
    Commit-Queue: Wenbin Zhang <wenbinzhang@google.com>
    Reviewed-by: Eduardo Yap <eduardoyap@google.com>

--- perf/go/frontend/api/triageApi.go
+++ perf/go/frontend/api/triageApi.go
@@ -17,7 +17,8 @@ import (
 )
 
 const (
-	defaultFileBugTimeout = time.Second * 30
+	defaultFileBugTimeout     = time.Second * 30
+	defaultEditAnomalyTimeout = time.Second * 5
 )
 
 type triageApi struct {
@@ -51,8 +52,26 @@ type ChromeperfFileBugResponse struct {
 	Error string `json:"error"`
 }
 
+// Request object for the request from the following triage actions:
+//   - Ignore
+//   - X button (untriage the anomaly)
+//   - Nudge (move the anomaly position to adjacent datapoints)
+type EditAnomaliesRequest struct {
+	Keys          []int    `json:"keys"`
+	BugId         int      `json:"bug_id,omitempty"`
+	StartRevision int      `json:"start_revision,omitempty"`
+	EndRevision   int      `json:"end_revision,omitempty"`
+	TraceNames    []string `json:"trace_names"`
+}
+
+// Response object from the chromeperf edit anomaly request.
+type EditAnomaliesResponse struct {
+	Error string `json:"error"`
+}
+
 func (api triageApi) RegisterHandlers(router *chi.Mux) {
 	router.Post("/_/triage/file_bug", api.FileNewBug)
+	router.Post("/_/triage/edit_anomalies", api.EditAnomalies)
 }
 
 func NewTriageApi(loginProvider alogin.Login, chromeperfClient chromeperf.ChromePerfClient, anomalyStore anomalies.Store) triageApi {
@@ -74,7 +93,7 @@ func (api triageApi) FileNewBug(w http.ResponseWriter, r *http.Request) {
 		httputils.ReportError(w, err, "Failed to decode JSON on new bug request.", http.StatusInternalServerError)
 		return
 	}
-	sklog.Debugf("[SkiaTriage] File new bug request received from frondend: %s", fileBugRequest)
+	sklog.Debugf("[SkiaTriage] File new bug request received from frontend: %s", fileBugRequest)
 
 	w.Header().Set("Content-Type", "application/json")
 
@@ -101,10 +120,54 @@ func (api triageApi) FileNewBug(w http.ResponseWriter, r *http.Request) {
 
 	sklog.Debugf("[SkiaTriage] b/%s is created.", chromeperfResponse.BugId)
 
-	for _, traceName := range fileBugRequest.TraceNames {
-		sklog.Debugf("[SkiaTriage] Invalidated the following traceName in cache: %s", traceName)
-		api.anomalyStore.InvalidateTestsCacheForTraceName(ctx, traceName)
-	}
+	api.markTracesForCacheInvalidation(ctx, fileBugRequest.TraceNames)
 
 	return
+}
+
+func (api triageApi) EditAnomalies(w http.ResponseWriter, r *http.Request) {
+	if api.loginProvider.LoggedInAs(r) == "" {
+		httputils.ReportError(w, errors.New("Not logged in"), fmt.Sprintf("You must be logged in to complete this action."), http.StatusUnauthorized)
+		return
+	}
+
+	var editAnomaliesRequest EditAnomaliesRequest
+	if err := json.NewDecoder(r.Body).Decode(&editAnomaliesRequest); err != nil {
+		httputils.ReportError(w, err, "Failed to decode JSON on edit anomalies request.", http.StatusInternalServerError)
+		return
+	}
+	sklog.Debugf("[SkiaTriage] Edit anomalies request received from frontend: %s", editAnomaliesRequest)
+
+	w.Header().Set("Content-Type", "application/json")
+
+	ctx, cancel := context.WithTimeout(r.Context(), defaultEditAnomalyTimeout)
+	defer cancel()
+
+	editAnomalyResponse := &EditAnomaliesResponse{}
+
+	err := api.chromeperfClient.SendPostRequest(ctx, "edit_anomalies_skia", "", editAnomaliesRequest, editAnomalyResponse, []int{200, 400, 401, 500})
+	if err != nil {
+		httputils.ReportError(w, err, "Failed to finish edit anomalies request.", http.StatusInternalServerError)
+		return
+	}
+
+	if editAnomalyResponse.Error != "" {
+		// TODO(wenbinzhang): Should update all end-user facing messages to be more informative and actionable. b/369622563.
+		httputils.ReportError(w, errors.New(editAnomalyResponse.Error), "Edit anomalies request returned error message.", http.StatusInternalServerError)
+		return
+	}
+
+	sklog.Debugf("[SkiaTriage] Anomalies (%d) are updated with: bug_id: %d, start_revision: %d, end_revision: %d", editAnomaliesRequest.Keys, editAnomaliesRequest.BugId, editAnomaliesRequest.StartRevision, editAnomaliesRequest.EndRevision)
+
+	api.markTracesForCacheInvalidation(ctx, editAnomaliesRequest.TraceNames)
+
+	return
+}
+
+// For each trace name, mark it as invalidated in the anomalystore's tests cache.
+func (api triageApi) markTracesForCacheInvalidation(ctx context.Context, traceNames []string) {
+	for _, traceName := range traceNames {
+		api.anomalyStore.InvalidateTestsCacheForTraceName(ctx, traceName)
+	}
+	sklog.Debugf("[SkiaTriage] The following traces in cache are marked invalidated: %s", traceNames)
 }
