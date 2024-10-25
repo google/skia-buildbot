e50eebfa691731f66f0bf7103407d9a9bd78a31b
Wenbin Zhang <wenbinzhang@google.com>
2024-10-25 14:56:17 -0700

    [perf] add endpoint to load anomalies from chromeperf
    Add endpoint /anomaly_list for UI to load a list of anomalies.
    The request will be forwarded to Chromeperf.
    
    The changes in Chromeperf:
    https://chromium-review.googlesource.com/c/catapult/+/5967576
    
    Change-Id: I2dcc22e619003fa26678d542489f896de68ade63
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/912874
    Reviewed-by: Maggie Dong <jiaxindong@google.com>
    Commit-Queue: Wenbin Zhang <wenbinzhang@google.com>

--- perf/go/frontend/api/anomaliesApi.go
+++ perf/go/frontend/api/anomaliesApi.go
@@ -25,14 +25,33 @@ type anomaliesApi struct {
 	loginProvider    alogin.Login
 }
 
-// Request object for the request from new bug UI.
+// Response object for the request from sheriff list UI.
 type GetSheriffListResponse struct {
 	SheriffList []string `json:"sheriff_list"`
 	Error       string   `json:"error"`
 }
 
+// Request object for the request from the anomaly table UI.
+type GetAnomaliesRequest struct {
+	Sheriff             string `json:"sheriff"`
+	IncludeTriaged      bool   `json:"triaged"`
+	IncludeImprovements bool   `json:"improvements"`
+	QueryCursor         string `json:"anomaly_cursor"`
+}
+
+// Request object for the request from the anomaly table UI.
+type GetAnomaliesResponse struct {
+	// The list of anomalies.
+	Anomalies []chromeperf.Anomaly `json:"anomaly_list"`
+	// The cursor of the current query. It will be used to 'Load More' for the next query.
+	QueryCursor string `json:"anomaly_cursor"`
+	// Error message if any.
+	Error string `json:"error"`
+}
+
 func (api anomaliesApi) RegisterHandlers(router *chi.Mux) {
 	router.Get("/_/anomalies/sheriff_list", api.GetSheriffList)
+	router.Get("/_/anomalies/anomaly_list", api.GetAnomalyList)
 }
 
 func NewAnomaliesApi(loginProvider alogin.Login, chromeperfClient chromeperf.ChromePerfClient) anomaliesApi {
@@ -72,6 +91,45 @@ func (api anomaliesApi) GetSheriffList(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	sklog.Debugf("[SkiaTriage] sheriff config list is loaded: %s", getSheriffListResponse.SheriffList)
+
+	return
+}
+
+func (api anomaliesApi) GetAnomalyList(w http.ResponseWriter, r *http.Request) {
+	if api.loginProvider.LoggedInAs(r) == "" {
+		httputils.ReportError(w, errors.New("Not logged in"), fmt.Sprintf("You must be logged in to complete this action."), http.StatusUnauthorized)
+		return
+	}
+
+	var getAnoamliesRequest GetAnomaliesRequest
+	if err := json.NewDecoder(r.Body).Decode(&getAnoamliesRequest); err != nil {
+		httputils.ReportError(w, err, "Failed to decode JSON on get anomalies request.", http.StatusInternalServerError)
+		return
+	}
+	sklog.Debugf("[SkiaTriage] Get anomalies request received from frontend: %s", getAnoamliesRequest)
+
+	w.Header().Set("Content-Type", "application/json")
+
+	ctx, cancel := context.WithTimeout(r.Context(), defaultAnomaliesRequestTimeout)
+	defer cancel()
+	getAnoamliesResponse := &GetAnomaliesResponse{}
+
+	err := api.chromeperfClient.SendPostRequest(ctx, "alerts_skia", "", getAnoamliesRequest, getAnoamliesResponse, []int{200, 400})
+	if err != nil {
+		httputils.ReportError(w, err, "Get anomalies request failed due to an internal server error. Please try again.", http.StatusInternalServerError)
+		return
+	}
+
+	if getAnoamliesResponse.Error != "" {
+		httputils.ReportError(w, errors.New(getAnoamliesResponse.Error), fmt.Sprintf("Error when getting the anomaly list. Please double check each request parameter, and try again: %v", getAnoamliesResponse.Error), http.StatusBadRequest)
+		return
+	}
+
+	if err := json.NewEncoder(w).Encode(getAnoamliesResponse); err != nil {
+		httputils.ReportError(w, err, "Failed to write get anoamlies response.", http.StatusInternalServerError)
+		return
+	}
+	sklog.Debugf("[SkiaTriage] %d anomalies are received.", len(getAnoamliesResponse.Anomalies))
 
 	return
 }
