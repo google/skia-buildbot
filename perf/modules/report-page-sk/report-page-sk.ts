594fe3cae8c3dc2912aa71fb4f909a164ecc49f7
Eduardo Yap <eduardoyap@google.com>
2024-11-27 00:11:37 +0000

    [Triage Workflow] Parse URL Params in Report Page and make back-end call.
    Change-Id: I11c4c64536a55a864d3b702ea819576c96e84ad3
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/923627
    Reviewed-by: Maggie Dong <jiaxindong@google.com>
    Commit-Queue: Eduardo Yap <eduardoyap@google.com>

--- perf/modules/report-page-sk/report-page-sk.ts
+++ perf/modules/report-page-sk/report-page-sk.ts
@@ -6,18 +6,88 @@
 import { html } from 'lit/html.js';
 import { define } from '../../../elements-sk/modules/define';
 import { ElementSk } from '../../../infra-sk/modules/ElementSk';
+import { errorMessage } from '../errorMessage';
+import { jsonOrThrow } from '../../../infra-sk/modules/jsonOrThrow';
+import { SpinnerSk } from '../../../elements-sk/modules/spinner-sk/spinner-sk';
+
+import '../../../elements-sk/modules/spinner-sk';
+
+class ReportPageParams {
+  // A revision number.
+  rev: string = '';
+
+  // Comma-separated list of urlsafe Anomaly keys (optional).
+  anomalyIDs: string = '';
+
+  // A Buganizer bug number ID.
+  bugID: string = '';
+
+  // An Anomaly Group ID
+  anomalyGroupID: string = '';
+
+  // A hash of a group of anomaly keys.
+  sid: string = '';
+}
 
 export class ReportPageSk extends ElementSk {
+  private params: ReportPageParams = new ReportPageParams();
+
+  private _spinner: SpinnerSk | null = null;
+
   constructor() {
     super(ReportPageSk.template);
   }
 
-  connectedCallback(): void {
+  async connectedCallback() {
     super.connectedCallback();
     this._render();
+
+    this._spinner = this.querySelector('#loading-spinner');
+
+    // Parse the URL Params.
+    const params = new URLSearchParams(window.location.search);
+    this.params.rev = params.get('rev') || '';
+    this.params.anomalyIDs = params.get('anomalyIDs') || '';
+    this.params.bugID = params.get('bugID') || '';
+    this.params.anomalyGroupID = params.get('anomalyGroupID') || '';
+    this.params.sid = params.get('sid') || '';
+
+    await this.fetchAnomalies();
   }
 
-  private static template = () => html``;
+  private static template = () => html`
+    <div>
+      <spinner-sk id="loading-spinner"></spinner-sk>
+    </div>
+  `;
+
+  private async fetchAnomalies() {
+    this._spinner!.active = true;
+    this._render();
+
+    await fetch('/_/group_report', {
+      method: 'POST',
+      body: JSON.stringify(this.params),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(jsonOrThrow)
+      .then((_) => {
+        this.initializePage();
+        this._spinner!.active = false;
+        this._render();
+      })
+      .catch((msg: any) => {
+        errorMessage(msg);
+        this._spinner!.active = false;
+        this._render();
+      });
+  }
+
+  private initializePage() {
+    // TODO(eduardoyap): Initialize table and graphs.
+  }
 }
 
 define('report-page-sk', ReportPageSk);
