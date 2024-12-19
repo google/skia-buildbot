d850389155a4efb2ba9a0e810a925660b5bbd8f7
skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
2024-12-18 20:01:50 -0800

    Roll Depot Tools from 138f15432a69 to 3c68cd01f472 (3 revisions)
    https://skia.googlesource.com/buildbot.git/+log/138f15432a69..3c68cd01f472
    
    2024-12-19 rop@google.com Restructuring 'ALLOWED_SPDX_LICENSES'
    2024-12-18 recipe-mega-autoroller@chops-service-accounts.iam.gserviceaccount.com Roll recipe dependencies (trivial).
    2024-12-18 recipe-mega-autoroller@chops-service-accounts.iam.gserviceaccount.com Roll recipe dependencies (trivial).
    
    If this roll has caused a breakage, revert this CL and stop the roller
    using the controls here:
    https://autoroll.skia.org/r/depot-tools-skia-infra
    Please CC kjlubick@google.com,skiabot@google.com on the revert to ensure that a human
    is aware of the problem.
    
    To file a bug in Depot Tools: https://bugs.chromium.org/p/chromium/issues/entry
    To file a bug in Skia Infra: https://bugs.chromium.org/p/skia/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://issues.skia.org/issues/new?component=1389291&template=1850622
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Tbr: kjlubick@google.com
    Change-Id: I7161feaf5ce7df5908b803c008cb14c4e3238a26
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/931637
    Commit-Queue: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
    Bot-Commit: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>

--- go/depot_tools/deps/deps_gen.go
+++ go/depot_tools/deps/deps_gen.go
@@ -9,7 +9,7 @@ import (
 var deps = deps_parser.DepsEntries{
 	"chromium.googlesource.com/chromium/tools/depot_tools": {
 		Id:      "chromium.googlesource.com/chromium/tools/depot_tools",
-		Version: "138f15432a69ba7081c895cd109abf0175c9926b",
+		Version: "3c68cd01f4728df4f696d396857e4f85cc091ecc",
 		Path:    "depot_tools",
 	},
 	"infra/3pp/tools/cpython/linux-amd64": {
