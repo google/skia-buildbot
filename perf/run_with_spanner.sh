85746a58cca5a61cd56bf44837324d04b58026f9
Ashwin Verleker <ashwinpv@google.com>
2025-01-21 21:50:11 +0000

    Remove invalid param from run_with_spanner.sh
    Change-Id: Iaa73837d184bfab22eb7c90c67881296e604c7dd
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/940606
    Reviewed-by: Maggie Dong <jiaxindong@google.com>
    Auto-Submit: Ashwin Verleker <ashwinpv@google.com>
    Reviewed-by: Farid (Mojtaba) Faridzad <faridzad@google.com>
    Commit-Queue: Ashwin Verleker <ashwinpv@google.com>

--- perf/run_with_spanner.sh
+++ perf/run_with_spanner.sh
@@ -17,7 +17,7 @@ done
 echo "Using the following params: -p=$p -i=$i -d=$d -config=$config"
 
 # Now let's run pgadapter connected to the supplied spanner database.
-docker run -d -p 127.0.0.1:5432:5432 \
+sudo docker run -d -p 127.0.0.1:5432:5432 \
 	-v $HOME/.config/gcloud/application_default_credentials.json:/acct_credentials.json \
 	gcr.io/cloud-spanner-pg-adapter/pgadapter:latest \
 	-p $p -i $i -d $d -c /acct_credentials.json -x
@@ -32,7 +32,6 @@ bazelisk build --config=mayberemote -c dbg //perf/...
 		--prom_port=:20001 \
 		--config_filename=$config \
 		--display_group_by=false \
-		--disable_git_update=true \
         --disable_metrics_update=true \
 		--resources_dir=../_bazel_bin/perf/pages/development/ \
 		--connection_string=postgresql://root@127.0.0.1:5432/${d}?sslmode=disable
