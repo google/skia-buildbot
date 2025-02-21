8b74f12660b85fb6f181b64d202b94edb1afe783
skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
2025-02-20 21:08:03 -0800

    Roll Skia Infra CIPD packages from 9cbf2dfdb4ef to 9fb204fb2e8f (10 revisions)
    https://skia.googlesource.com/buildbot.git/+log/9cbf2dfdb4ef..9fb204fb2e8f
    
    2025-02-21 skia-autoroll@skia-public.iam.gserviceaccount.com Roll LUCI CIPD packages from 9ade7a409056 to dfca49005c63 (7 revisions)
    2025-02-21 skia-autoroll@skia-public.iam.gserviceaccount.com Roll Depot Tools from ab387d1ddc53 to 2259aa176628 (1 revision)
    2025-02-20 ashwinpv@google.com Set optimize_sqltracestore to true for all spanner instances
    2025-02-20 jeffyoon@google.com [perf] add null check to subscription
    2025-02-20 ashwinpv@google.com Turn off regression notifications in Flutter CDB instances and enable on spanner ones
    2025-02-20 ashwinpv@google.com Remove all usage of materialized views from Gold
    2025-02-20 wenbinzhang@google.com [perf] remove lacros logics
    2025-02-20 bwils@google.com Add no merge paths to low power profile
    2025-02-20 borenet@google.com [task scheduler] Tolerate small change in Swarming task creation time
    2025-02-20 skia-autoroll@skia-public.iam.gserviceaccount.com Roll Skia Infra CIPD packages from 42d30b3302bb to dd753d8252ac (9 revisions)
    
    If this roll has caused a breakage, revert this CL and stop the roller
    using the controls here:
    https://autoroll.skia.org/r/skia-infra-cipd-infra
    Please CC borenet@google.com,skiabot@google.com on the revert to ensure that a human
    is aware of the problem.
    
    To file a bug in Skia Infra CIPD packages: https://bugs.chromium.org/p/skia/issues/entry
    To file a bug in Skia Infra: https://bugs.chromium.org/p/skia/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://issues.skia.org/issues/new?component=1389291&template=1850622
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Tbr: borenet@google.com
    Change-Id: If59c7c8bedf19ec58dda39acd83c4c19bbc6b7b2
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/953713
    Bot-Commit: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
    Commit-Queue: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>

--- go/depot_tools/deps/deps_gen.go
+++ go/depot_tools/deps/deps_gen.go
@@ -159,17 +159,17 @@ var deps = deps_parser.DepsEntries{
 	},
 	"skia/tools/bazel_build_all": {
 		Id:      "skia/tools/bazel_build_all",
-		Version: "git_revision:dd753d8252ac993ebf2fa23bfecc2c1f0064446a",
+		Version: "git_revision:9fb204fb2e8fc339bf36ada13b2128c96f0abd67",
 		Path:    "task_drivers",
 	},
 	"skia/tools/bazel_test_all": {
 		Id:      "skia/tools/bazel_test_all",
-		Version: "git_revision:dd753d8252ac993ebf2fa23bfecc2c1f0064446a",
+		Version: "git_revision:9fb204fb2e8fc339bf36ada13b2128c96f0abd67",
 		Path:    "task_drivers",
 	},
 	"skia/tools/command_wrapper": {
 		Id:      "skia/tools/command_wrapper",
-		Version: "git_revision:dd753d8252ac993ebf2fa23bfecc2c1f0064446a",
+		Version: "git_revision:9fb204fb2e8fc339bf36ada13b2128c96f0abd67",
 		Path:    "task_drivers",
 	},
 	"skia/tools/goldctl": {
@@ -179,7 +179,7 @@ var deps = deps_parser.DepsEntries{
 	},
 	"skia/tools/presubmit": {
 		Id:      "skia/tools/presubmit",
-		Version: "git_revision:dd753d8252ac993ebf2fa23bfecc2c1f0064446a",
+		Version: "git_revision:9fb204fb2e8fc339bf36ada13b2128c96f0abd67",
 		Path:    "task_drivers",
 	},
 }
