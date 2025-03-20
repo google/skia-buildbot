907f24fc1195a02557b5561fa800c8be06b93169
Wenbin Zhang <wenbinzhang@google.com>
2025-03-20 12:43:17 -0700

    [perf] anomaly group start using real subscription info
    Anomaly group will have the correct subscription namd and revision.
    
    It will not start sending bugs to end users
    
    Bug: b/318739230
    Change-Id: I12319f43364dcd9b6ca0f6ec58e196b6e3642636
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/967794
    Auto-Submit: Wenbin Zhang <wenbinzhang@google.com>
    Commit-Queue: Wenbin Zhang <wenbinzhang@google.com>
    Reviewed-by: Eduardo Yap <eduardoyap@google.com>

--- perf/go/anomalygroup/utils/anomalygrouputils.go
+++ perf/go/anomalygroup/utils/anomalygrouputils.go
@@ -76,10 +76,8 @@ func ProcessRegression(
 	resp, err := ag_client.FindExistingGroups(
 		ctx,
 		&ag.FindExistingGroupsRequest{
-			// Subscription info will be loaded from alerts.Alert in the future.
-			// Using hard coded values for now. Subscription name will diff from day to day.
-			SubscriptionName:     "Test-Sub-Name", // hardcoded name before alert.SubscriptionName is ready,
-			SubscriptionRevision: "Test-Sub-Rev",  // // hardcoded name before alert.SubscriptionRevision is ready,
+			SubscriptionName:     alert.SubscriptionName,
+			SubscriptionRevision: alert.SubscriptionRevision,
 			Action:               ag.GroupActionType(ag.GroupActionType_value[groupAction]),
 			StartCommit:          startCommit,
 			EndCommit:            endCommit,
@@ -94,8 +92,8 @@ func ProcessRegression(
 		newGroupID, err := ag_client.CreateNewAnomalyGroup(
 			ctx,
 			&ag.CreateNewAnomalyGroupRequest{
-				SubscriptionName:     "Test-Sub-Name", // hardcoded name before alert.SubscriptionName is ready,
-				SubscriptionRevision: "Test-Sub-Rev",  // // hardcoded name before alert.SubscriptionRevision is ready,
+				SubscriptionName:     alert.SubscriptionName,
+				SubscriptionRevision: alert.SubscriptionRevision,
 				Domain:               paramSet["master"],
 				Benchmark:            paramSet["benchmark"],
 				StartCommit:          startCommit,
