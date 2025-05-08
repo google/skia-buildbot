73b6a4e89a4398b47c8e178fb49f97a248221082
skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
2025-05-07 21:01:13 -0700

    Roll Depot Tools from 8c9aa837476d to fab0a4296b88 (2 revisions)
    https://skia.googlesource.com/buildbot.git/+log/8c9aa837476d..fab0a4296b88
    
    2025-05-07 mlcui@google.com subcommand: Add type annotations
    2025-05-07 ukai@google.com DEPOT_TOOLS_WIN_TOOLCHAIN=0 does not work with remote execution
    
    If this roll has caused a breakage, revert this CL and stop the roller
    using the controls here:
    https://autoroll.skia.org/r/depot-tools-skia-infra
    Please CC kjlubick@google.com,skiabot@google.com on the revert to ensure that a human
    is aware of the problem.
    
    To file a bug in Depot Tools: https://bugs.chromium.org/p/chromium/issues/entry
    To file a bug in Skia Infra: https://bugs.chromium.org/p/skia/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://issues.skia.org/issues/new?component=1389291&template=1850622
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Tbr: kjlubick@google.com
    Change-Id: Ib5e4bc57e454f8f363083e8c259c7c107e9ad2b4
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/990927
    Commit-Queue: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
    Bot-Commit: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>

--- go/depot_tools/deps/deps_gen.go
+++ go/depot_tools/deps/deps_gen.go
@@ -9,7 +9,7 @@ import (
 var deps = deps_parser.DepsEntries{
 	"chromium.googlesource.com/chromium/tools/depot_tools": {
 		Id:      "chromium.googlesource.com/chromium/tools/depot_tools",
-		Version: "8c9aa837476d8b95915ea500f75320698a8d7cea",
+		Version: "fab0a4296b8830c569a4dc82285bfb58a5c2fca8",
 		Path:    "depot_tools",
 	},
 	"infra/3pp/tools/cpython/linux-amd64": {
