2a1143240ba35f5854fca772fba26763daf9043b
Farid <faridzad@google.com>
2025-06-10 13:20:32 -0700

    Filter detected Chrome builds by comparing them with the JSON files.
    After detecting the Chrome builds, filter by supported platforms and retain builds that are either unprocessed or newer versions.
    
    Bug: 388894957
    Change-Id: I51d8aea1f63691aba110c06b8ddce02be9b725ac
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/1005736
    Commit-Queue: Farid (Mojtaba) Faridzad <faridzad@google.com>
    Reviewed-by: John Chen <zhanliang@google.com>

--- pinpoint/go/workflows/internal/chrome_releases_info.go
+++ pinpoint/go/workflows/internal/chrome_releases_info.go
@@ -3,13 +3,18 @@ package internal
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 
 	"go.skia.org/infra/go/httputils"
+	"go.skia.org/infra/go/skerr"
 	"go.skia.org/infra/go/sklog"
 )
 
 // BuildInfo fromt the ChromiumDash response.
 type BuildInfo struct {
+	// Browser contains 'Chrome', 'Edge', 'Safari'
+	Browser string `json:"browser"`
+
 	// Channel contains 'Canary', 'Dev', 'Beta', 'Stable'
 	Channel string `json:"channel"`
 
@@ -23,6 +28,10 @@ type BuildInfo struct {
 const (
 	// chromiumDashUrl response contains the latest Chrome build versions.
 	chromiumDashUrl = "https://chromiumdash.appspot.com/fetch_releases?num=1"
+	// chromeInternalBucket is the bucket to save the build info JSON files.
+	chromeInternalBucket = "chrome-perf-experiment-non-public"
+	// cbbRefInfoPath is the root of the build info files in the bucket.
+	cbbRefInfoPath = "cbb_ref_info/chrome/%s/%s.json"
 )
 
 // getChromiumDashInfo provides a list of latest Chrome build versions.
@@ -37,5 +46,58 @@ func GetChromeReleasesInfoActivity(ctx context.Context) ([]BuildInfo, error) {
 	if err := json.NewDecoder(resp.Body).Decode(&builds); err != nil {
 		sklog.Fatalf("Invalid ChromiumDash response:%s, err: %s", resp.Body, err)
 	}
-	return builds, nil
+	return filterBuilds(ctx, builds)
+}
+
+func filterBuilds(ctx context.Context, builds []BuildInfo) ([]BuildInfo, error) {
+	// Keys match the ChromiumDash and Values match the subfolders in the GCS.
+	var cbbChannels = map[string]string{
+		"Dev":    "dev",
+		"Stable": "stable",
+	}
+	var cbbPlatforms = map[string]string{
+		"Android": "Android",
+		"Mac":     "macOS",
+		"Windows": "Windows",
+	}
+	var store, err = NewStore(ctx, chromeInternalBucket, true)
+	if err != nil {
+		return nil, skerr.Wrap(err)
+	}
+	var targetBuilds []BuildInfo
+	for _, build := range builds {
+		if _, found := cbbChannels[build.Channel]; !found {
+			continue
+		}
+		if _, found := cbbPlatforms[build.Platform]; !found {
+			continue
+		}
+		filePath := fmt.Sprintf(cbbRefInfoPath, cbbChannels[build.Channel], cbbPlatforms[build.Platform])
+		if store.Exists(filePath) {
+			var content, err = store.GetFileContent(filePath)
+			if err != nil {
+				return nil, skerr.Wrap(err)
+			}
+			var gcsBuild BuildInfo
+			if err := json.Unmarshal(content, &gcsBuild); err != nil {
+				return nil, skerr.Wrap(err)
+			}
+			if build.Version == gcsBuild.Version {
+				continue
+			}
+		}
+		build.Browser = "Chrome"
+
+		// TODO(b/388894957): We may need to update the GCS after committing.
+		jsonData, err := json.Marshal(build)
+		if err != nil {
+			return nil, skerr.Wrap(err)
+		}
+		if err := store.WriteFile(filePath, string(jsonData)); err != nil {
+			return nil, skerr.Wrap(err)
+		}
+
+		targetBuilds = append(targetBuilds, build)
+	}
+	return targetBuilds, nil
 }
---
+++ pinpoint/go/workflows/internal/gcs_store.go
@@ -0,0 +1,75 @@
+package internal
+
+import (
+	"context"
+	"io"
+
+	"cloud.google.com/go/storage"
+	"go.skia.org/infra/go/auth"
+	"go.skia.org/infra/go/httputils"
+	"go.skia.org/infra/go/skerr"
+	"go.skia.org/infra/go/util"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/option"
+)
+
+// GCS store info.
+type store struct {
+	ctx    context.Context
+	bucket *storage.BucketHandle
+}
+
+func NewStore(ctx context.Context, bucketName string, readOnly bool) (*store, error) {
+	var storeOption option.ClientOption
+	if readOnly {
+		storeOption = option.WithScopes(storage.ScopeReadOnly)
+	} else {
+		ts, err := google.DefaultTokenSource(ctx, auth.ScopeFullControl)
+		if err != nil {
+			return nil, skerr.Wrap(err)
+		}
+
+		client := httputils.DefaultClientConfig().WithTokenSource(ts).With2xxOnly().Client()
+		storeOption = option.WithHTTPClient(client)
+
+	}
+	storageClient, err := storage.NewClient(context.Background(), storeOption)
+	if err != nil {
+		return nil, skerr.Wrap(err)
+	}
+
+	return &store{ctx: ctx, bucket: storageClient.Bucket(bucketName)}, nil
+}
+
+// Exists returns true if file found.
+func (s *store) Exists(storeFilePath string) bool {
+	o := s.bucket.Object(storeFilePath)
+	_, err := o.Attrs(s.ctx)
+	return err == nil
+}
+
+// GetFileContent returns the file content in GCS.
+func (s *store) GetFileContent(storeFilePath string) ([]byte, error) {
+	response, err := s.bucket.Object(storeFilePath).NewReader(s.ctx)
+	if err != nil {
+		return nil, skerr.Wrap(err)
+	}
+	defer util.Close(response)
+	return io.ReadAll(response)
+}
+
+// WriteFile creates or updates a file in GCS.
+func (s *store) WriteFile(storeFilePath string, content string) error {
+	w := s.bucket.Object(storeFilePath).NewWriter(s.ctx)
+	w.ObjectAttrs.ContentEncoding = "text/plain"
+	_, err := w.Write([]byte(content))
+	if err != nil {
+		_ = w.Close()
+		return skerr.Wrap(err)
+	}
+	err = w.Close()
+	if err != nil {
+		return skerr.Wrap(err)
+	}
+	return nil
+}
