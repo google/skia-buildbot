8b0f61a32ec25749cb6396eed0963e6281d15caf
skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
2025-07-01 21:01:25 -0700

    Roll Skia Infra CIPD packages from ef6b3564f12e to c45c84ffa29e (6 revisions)
    https://skia.googlesource.com/buildbot.git/+log/ef6b3564f12e..c45c84ffa29e
    
    2025-07-01 natnaelal@google.com [Pinpoint] Building Spanner database schema for Pairwise Jobs
    2025-07-01 ashwinpv@google.com Create MCP tool to retrieve perf data
    2025-07-01 borenet@google.com [autoroll] Use stable sort for GCS child
    2025-07-01 borenet@google.com Reland "[logging] Use Cloud Logging package for structured logging"
    2025-07-01 skia-autoroll@skia-public.iam.gserviceaccount.com Roll Depot Tools from 5cc29c7a2178 to 179c4a53fc20 (1 revision)
    2025-07-01 skia-autoroll@skia-public.iam.gserviceaccount.com Roll Skia Infra CIPD packages from 686cc37642f5 to ef6b3564f12e (9 revisions)
    
    If this roll has caused a breakage, revert this CL and stop the roller
    using the controls here:
    https://autoroll.skia.org/r/skia-infra-cipd-infra
    Please CC kjlubick@google.com,skiabot@google.com on the revert to ensure that a human
    is aware of the problem.
    
    To file a bug in Skia Infra CIPD packages: https://bugs.chromium.org/p/skia/issues/entry
    To file a bug in Skia Infra: https://bugs.chromium.org/p/skia/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://issues.skia.org/issues/new?component=1389291&template=1850622
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Tbr: kjlubick@google.com
    Change-Id: I8c3d095a0bcf5557039124c731370a141b720ad7
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/1014514
    Bot-Commit: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
    Commit-Queue: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>

--- go/cipd/asset_versions_gen.go
+++ go/cipd/asset_versions_gen.go
@@ -196,17 +196,17 @@ var PACKAGES = map[string]*Package{
 	"skia/tools/bazel_build_all/${platform}": {
 		Path:    "task_drivers",
 		Name:    "skia/tools/bazel_build_all/${platform}",
-		Version: "git_revision:ef6b3564f12e322ce7da19967d06f2d4b8938ffa",
+		Version: "git_revision:c45c84ffa29e50bb1c868a57a07a6a1ff9833b58",
 	},
 	"skia/tools/bazel_test_all/${platform}": {
 		Path:    "task_drivers",
 		Name:    "skia/tools/bazel_test_all/${platform}",
-		Version: "git_revision:ef6b3564f12e322ce7da19967d06f2d4b8938ffa",
+		Version: "git_revision:c45c84ffa29e50bb1c868a57a07a6a1ff9833b58",
 	},
 	"skia/tools/command_wrapper/${platform}": {
 		Path:    "task_drivers",
 		Name:    "skia/tools/command_wrapper/${platform}",
-		Version: "git_revision:ef6b3564f12e322ce7da19967d06f2d4b8938ffa",
+		Version: "git_revision:c45c84ffa29e50bb1c868a57a07a6a1ff9833b58",
 	},
 	"skia/tools/goldctl/${platform}": {
 		Path:    "cipd_bin_packages",
@@ -216,6 +216,6 @@ var PACKAGES = map[string]*Package{
 	"skia/tools/presubmit/${platform}": {
 		Path:    "task_drivers",
 		Name:    "skia/tools/presubmit/${platform}",
-		Version: "git_revision:ef6b3564f12e322ce7da19967d06f2d4b8938ffa",
+		Version: "git_revision:c45c84ffa29e50bb1c868a57a07a6a1ff9833b58",
 	},
 }
--- go/depot_tools/deps/deps_gen.go
+++ go/depot_tools/deps/deps_gen.go
@@ -164,17 +164,17 @@ var deps = deps_parser.DepsEntries{
 	},
 	"skia/tools/bazel_build_all": {
 		Id:      "skia/tools/bazel_build_all",
-		Version: "git_revision:ef6b3564f12e322ce7da19967d06f2d4b8938ffa",
+		Version: "git_revision:c45c84ffa29e50bb1c868a57a07a6a1ff9833b58",
 		Path:    "task_drivers",
 	},
 	"skia/tools/bazel_test_all": {
 		Id:      "skia/tools/bazel_test_all",
-		Version: "git_revision:ef6b3564f12e322ce7da19967d06f2d4b8938ffa",
+		Version: "git_revision:c45c84ffa29e50bb1c868a57a07a6a1ff9833b58",
 		Path:    "task_drivers",
 	},
 	"skia/tools/command_wrapper": {
 		Id:      "skia/tools/command_wrapper",
-		Version: "git_revision:ef6b3564f12e322ce7da19967d06f2d4b8938ffa",
+		Version: "git_revision:c45c84ffa29e50bb1c868a57a07a6a1ff9833b58",
 		Path:    "task_drivers",
 	},
 	"skia/tools/goldctl": {
@@ -184,7 +184,7 @@ var deps = deps_parser.DepsEntries{
 	},
 	"skia/tools/presubmit": {
 		Id:      "skia/tools/presubmit",
-		Version: "git_revision:ef6b3564f12e322ce7da19967d06f2d4b8938ffa",
+		Version: "git_revision:c45c84ffa29e50bb1c868a57a07a6a1ff9833b58",
 		Path:    "task_drivers",
 	},
 }
