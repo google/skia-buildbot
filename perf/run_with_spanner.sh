e1a3067d58b2cfe84c0ea687ce081a17733fc85c
Anri Sidorov <ansid@google.com>
2025-07-28 08:16:57 -0700

    [perf][dev] Exit early if build failed in run_with_spanner.sh
    I spent some time debugging until I noticed the build error. Prevent it from happening by checking bazelisk return code.
    
    Change-Id: Ie5a8e4b3fcb670c05226c9745c5878866f4cf0e4
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/1028096
    Commit-Queue: Anri Sidorov <ansid@google.com>
    Reviewed-by: Ashwin Verleker <ashwinpv@google.com>
    Auto-Submit: Anri Sidorov <ansid@google.com>
    Commit-Queue: Ashwin Verleker <ashwinpv@google.com>

--- perf/run_with_spanner.sh
+++ perf/run_with_spanner.sh
@@ -30,21 +30,24 @@ fi
 echo "Using the following params: -p=$p -i=$i -d=$d -config=$config -domain=$domain -repo=$repo"
 # Now let's run pgadapter connected to the supplied spanner database.
 sudo docker run -d -p 127.0.0.1:5432:5432 \
-	-v $HOME/.config/gcloud/application_default_credentials.json:/acct_credentials.json \
-	gcr.io/cloud-spanner-pg-adapter/pgadapter:latest \
-	-p $p -i $i -d $d -c /acct_credentials.json -x
+  -v $HOME/.config/gcloud/application_default_credentials.json:/acct_credentials.json \
+  gcr.io/cloud-spanner-pg-adapter/pgadapter:latest \
+  -p $p -i $i -d $d -c /acct_credentials.json -x
 # Now that pgadapter is connected to the spanner instance, let's run the local frontend
-# pointed to pgadapter using the supplied config file.
-bazelisk build --config=mayberemote -c dbg //perf/...
+# pointed to pgadapter using the supplied config file. First, build the perfserver.
+bazelisk build --config=mayberemote -c dbg //perf/... || {
+  echo "Build failed, exiting (not trying to run outdated perfserver)."
+  exit 1
+}
 ../_bazel_bin/perf/go/perfserver/perfserver_/perfserver frontend \
-		--dev_mode \
-		--localToProd \
-		--do_clustering=false \
-		--port=:8002 \
-		--prom_port=:20001 \
-		--config_filename=$config \
-		--display_group_by=false \
-		--disable_metrics_update=true \
-		--resources_dir=../_bazel_bin/perf/pages/development/ \
-		--connection_string=postgresql://root@127.0.0.1:5432/${d}?sslmode=disable \
-		--commit_range_url=https://${domain}/${repo}/+log/{begin}..{end}
+  --dev_mode \
+  --localToProd \
+  --do_clustering=false \
+  --port=:8002 \
+  --prom_port=:20001 \
+  --config_filename=$config \
+  --display_group_by=false \
+  --disable_metrics_update=true \
+  --resources_dir=../_bazel_bin/perf/pages/development/ \
+  --connection_string=postgresql://root@127.0.0.1:5432/${d}?sslmode=disable \
+  --commit_range_url=https://${domain}/${repo}/+log/{begin}..{end}
