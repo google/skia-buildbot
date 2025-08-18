c86dbe4ffdbc1820a60530b047f5c55967fb8ffc
Eric Boren <borenet@google.com>
2025-08-18 12:26:48 +0000

    [autoroll] Refactor Placeholders from GenericPreUploadStep
    This is needed in a future CL.
    
    Change-Id: I055f715d51e90f35697747462d599aa96aede135
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/1039038
    Commit-Queue: Kaylee Lubick <kjlubick@google.com>
    Auto-Submit: Eric Boren <borenet@google.com>
    Reviewed-by: Kaylee Lubick <kjlubick@google.com>

--- autoroll/go/repo_manager/parent/pre_upload_steps.go
+++ autoroll/go/repo_manager/parent/pre_upload_steps.go
@@ -467,6 +467,109 @@ func GetGenericPreUploadStep(cfg *config.PreUploadConfig) PreUploadStep {
 	}
 }
 
+// Placeholders is a struct containing placeholder variables which may be
+// substituted into various parts of commands.
+type Placeholders struct {
+	CIPDRoot      string
+	ParentRepoDir string
+	PathVar       string
+	RollingFromID string
+	RollingToID   string
+}
+
+// String substitutes the Placeholders into the given string.
+func (p Placeholders) String(s string) string {
+	s = strings.ReplaceAll(s, "${cipd_root}", p.CIPDRoot)
+	s = strings.ReplaceAll(s, "${parent_dir}", p.ParentRepoDir)
+	s = strings.ReplaceAll(s, "${PATH}", p.PathVar)
+	s = strings.ReplaceAll(s, "${rolling_from}", p.RollingFromID)
+	s = strings.ReplaceAll(s, "${rolling_to}", p.RollingToID)
+	return s
+}
+
+// Strings substitutes the Placeholders into the given string slice.
+func (p Placeholders) Strings(strs []string) []string {
+	cpy := make([]string, 0, len(strs))
+	for _, s := range strs {
+		cpy = append(cpy, p.String(s))
+	}
+	return cpy
+}
+
+// CIPDPackage substitutes the Placeholders into the given CIPDPackage package
+// config.
+func (p Placeholders) CIPDPackage(pkg *config.PreUploadCIPDPackageConfig) (*cipd.Package, error) {
+	pkgName := p.String(pkg.Name)
+	pkgVersion := p.String(pkg.Version)
+	if pkgVersion == "${use_pinned_version}" {
+		builtin, err := cipd.GetPackage(pkgName)
+		if err != nil {
+			return nil, skerr.Wrap(err)
+		}
+		pkgVersion = builtin.Version
+	}
+	return &cipd.Package{
+		Name:    pkgName,
+		Path:    p.String(pkg.Path),
+		Version: pkgVersion,
+	}, nil
+}
+
+// CIPDPackages substitutes the Placeholders into the given CIPDPackage configs.
+func (p Placeholders) CIPDPackages(pkgs []*config.PreUploadCIPDPackageConfig) ([]*cipd.Package, error) {
+	rv := []*cipd.Package{}
+	for _, pkgCfg := range pkgs {
+		pkg, err := p.CIPDPackage(pkgCfg)
+		if err != nil {
+			return nil, skerr.Wrap(err)
+		}
+		rv = append(rv, pkg)
+	}
+	return rv, nil
+}
+
+// Command substitutes the Placeholders into the given Command.
+func (p Placeholders) Command(ctx context.Context, cmd []string, cwd string, env []string) (*exec.Command, error) {
+	env = p.Strings(env)
+	pathVar := os.Getenv("PATH")
+	for _, envVar := range env {
+		split := strings.SplitN(envVar, "=", 2)
+		if len(split) == 2 && split[0] == "PATH" {
+			pathVar = split[1]
+		}
+	}
+	cmd = p.Strings(cmd)
+	executable, err := exec.LookPath(ctx, cmd[0], pathVar)
+	if err != nil {
+		return nil, skerr.Wrap(err)
+	}
+
+	return &exec.Command{
+		Name: executable,
+		Args: cmd[1:],
+		Dir:  p.String(cwd),
+		Env:  env,
+	}, nil
+}
+
+// PreUploadConfig substitutes the Placeholders into the PreUploadConfig.
+func (p Placeholders) PreUploadConfig(ctx context.Context, env []string, cfg *config.PreUploadConfig) ([]*cipd.Package, []*exec.Command, error) {
+	cipdPkgs, err := p.CIPDPackages(cfg.CipdPackage)
+	if err != nil {
+		return nil, nil, skerr.Wrap(err)
+	}
+	cmds := make([]*exec.Command, 0, len(cfg.Command))
+	for _, cmdCfg := range cfg.Command {
+		cmdEnv := exec.MergeEnv(env, cmdCfg.Env)
+		cmd, err := p.Command(ctx, strings.Fields(cmdCfg.Command), cmdCfg.Cwd, cmdEnv)
+		if err != nil {
+			return nil, nil, skerr.Wrap(err)
+		}
+		cmds = append(cmds, cmd)
+	}
+	return cipdPkgs, cmds, nil
+}
+
 // GenericPreUploadStep runs a pre-upload step as specified by the given config.
 func GenericPreUploadStep(ctx context.Context, cfg *config.PreUploadConfig, env []string, client *http.Client, parentRepoDir string, from *revision.Revision, to *revision.Revision) error {
 	defer metrics2.FuncTimer().Stop()
@@ -484,70 +587,27 @@ func GenericPreUploadStep(ctx context.Context, cfg *config.PreUploadConfig, env 
 			path = split[1]
 		}
 	}
-	replaceMagicVars := func(s string) string {
-		s = strings.ReplaceAll(s, "${cipd_root}", cipdRoot)
-		s = strings.ReplaceAll(s, "${parent_dir}", parentRepoDir)
-		s = strings.ReplaceAll(s, "${PATH}", path)
-		s = strings.ReplaceAll(s, "${rolling_from}", from.Id)
-		s = strings.ReplaceAll(s, "${rolling_to}", to.Id)
-		return s
+	p := Placeholders{
+		CIPDRoot:      cipdRoot,
+		ParentRepoDir: parentRepoDir,
+		PathVar:       path,
+		RollingFromID: from.Id,
+		RollingToID:   to.Id,
 	}
-
-	if len(cfg.CipdPackage) > 0 {
-		cipdPkgs := []*cipd.Package{}
-		for _, pkg := range cfg.CipdPackage {
-			pkgName := replaceMagicVars(pkg.Name)
-			pkgVersion := replaceMagicVars(pkg.Version)
-			if pkgVersion == "${use_pinned_version}" {
-				builtin, err := cipd.GetPackage(pkgName)
-				if err != nil {
-					return skerr.Wrap(err)
-				}
-				pkgVersion = builtin.Version
-			}
-			cipdPkgs = append(cipdPkgs, &cipd.Package{
-				Name:    pkgName,
-				Path:    replaceMagicVars(pkg.Path),
-				Version: pkgVersion,
-			})
-		}
+	cipdPkgs, cmds, err := p.PreUploadConfig(ctx, env, cfg)
+	if err != nil {
+		return skerr.Wrap(err)
+	}
+	if len(cipdPkgs) > 0 {
 		sklog.Info("Installing CIPD packages...")
 		if err := cipd.Ensure(ctx, client, cipdRoot, true, cipdPkgs...); err != nil {
 			return skerr.Wrap(err)
 		}
 	}
-	for _, cmd := range cfg.Command {
-		cmdEnv := make([]string, len(env)+len(cmd.Env))
-		for _, envVar := range env {
-			cmdEnv = append(cmdEnv, replaceMagicVars(envVar))
-		}
-		for _, envVar := range cmd.Env {
-			cmdEnv = append(cmdEnv, replaceMagicVars(envVar))
-		}
-		pathVar := os.Getenv("PATH")
-		for _, envVar := range cmdEnv {
-			split := strings.SplitN(envVar, "=", 2)
-			if len(split) == 2 && split[0] == "PATH" {
-				pathVar = split[1]
-			}
-		}
-		split := strings.Split(cmd.Command, " ")
-		for idx := range split {
-			split[idx] = replaceMagicVars(split[idx])
-		}
-		executable, err := exec.LookPath(ctx, split[0], pathVar)
-		if err != nil {
-			return skerr.Wrap(err)
-		}
-
-		sklog.Infof("Running command: %s", strings.Join(split, " "))
-		if _, err := exec.RunCommand(ctx, &exec.Command{
-			Name: executable,
-			Args: split[1:],
-			Dir:  replaceMagicVars(cmd.Cwd),
-			Env:  cmdEnv,
-		}); err != nil {
-			if cmd.IgnoreFailure {
+	for idx, cmd := range cmds {
+		sklog.Infof("Running command: %s %s", cmd.Name, strings.Join(cmd.Args, " "))
+		if _, err := exec.RunCommand(ctx, cmd); err != nil {
+			if cfg.Command[idx].IgnoreFailure {
 				sklog.Errorf("%s", err)
 			} else {
 				return skerr.Wrap(err)
--- autoroll/go/repo_manager/parent/pre_upload_steps_test.go
+++ autoroll/go/repo_manager/parent/pre_upload_steps_test.go
@@ -11,6 +11,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.skia.org/infra/autoroll/go/config"
+	"go.skia.org/infra/go/cipd"
 	"go.skia.org/infra/go/exec"
 	"go.skia.org/infra/go/testutils"
 	"go.skia.org/infra/go/testutils/unittest"
@@ -86,4 +87,150 @@ func TestFlutterLicenseScripts(t *testing.T) {
 	mainDartErr = error(nil)
 	err = FlutterLicenseScripts(ctx, nil, nil, wd, nil, nil)
 	assert.NoError(t, err)
+}
+
+func TestPlaceholders_String(t *testing.T) {
+	p := Placeholders{
+		ParentRepoDir: "/path/to/repo",
+		RollingFromID: "abc",
+		RollingToID:   "def",
+	}
+	require.Equal(t, "/path/to/repo/foo", p.String("${parent_dir}/foo"))
+	require.Equal(t, "rolling from abc to def", p.String("rolling from ${rolling_from} to ${rolling_to}"))
+	require.Equal(t, "no placeholders", p.String("no placeholders"))
+	require.Equal(t, "${unknown}", p.String("${unknown}"))
+}
+
+func TestPlaceholders_Strings(t *testing.T) {
+	p := Placeholders{
+		ParentRepoDir: "/path/to/repo",
+		RollingFromID: "abc",
+		RollingToID:   "def",
+	}
+	input := []string{
+		"${parent_dir}/foo",
+		"rolling from ${rolling_from} to ${rolling_to}",
+		"no placeholders",
+	}
+	expected := []string{
+		"/path/to/repo/foo",
+		"rolling from abc to def",
+		"no placeholders",
+	}
+	require.Equal(t, expected, p.Strings(input))
+}
+
+func TestPlaceholders_CIPDPackage(t *testing.T) {
+	p := Placeholders{
+		ParentRepoDir: "/path/to/repo",
+		RollingFromID: "abc",
+		RollingToID:   "def",
+	}
+
+	// Test with placeholder substitution.
+	pkgCfg := &config.PreUploadCIPDPackageConfig{
+		Name:    "some/package/${rolling_to}",
+		Path:    "${parent_dir}/cipd_pkgs",
+		Version: "version:${rolling_from}",
+	}
+	expected := &cipd.Package{
+		Name:    "some/package/def",
+		Path:    "/path/to/repo/cipd_pkgs",
+		Version: "version:abc",
+	}
+	actual, err := p.CIPDPackage(pkgCfg)
+	require.NoError(t, err)
+	require.Equal(t, expected, actual)
+
+	// Test with use_pinned_version.
+	pkgCfg = &config.PreUploadCIPDPackageConfig{
+		Name:    "skia/bots/go",
+		Path:    ".",
+		Version: "${use_pinned_version}",
+	}
+	_, err = p.CIPDPackage(pkgCfg)
+	require.NoError(t, err)
+
+	// Test with use_pinned_version for a package that doesn't exist.
+	pkgCfg = &config.PreUploadCIPDPackageConfig{
+		Name:    "does/not/exist",
+		Path:    ".",
+		Version: "${use_pinned_version}",
+	}
+	_, err = p.CIPDPackage(pkgCfg)
+	require.Error(t, err)
+}
+
+func TestPlaceholders_CIPDPackages(t *testing.T) {
+	p := Placeholders{
+		ParentRepoDir: "/path/to/repo",
+		RollingFromID: "abc",
+		RollingToID:   "def",
+	}
+	pkgCfg1 := &config.PreUploadCIPDPackageConfig{
+		Name:    "some/package/${rolling_to}",
+		Path:    "${parent_dir}/cipd_pkgs",
+		Version: "version:${rolling_from}",
+	}
+	pkgCfg2 := &config.PreUploadCIPDPackageConfig{
+		Name:    "another/package",
+		Path:    ".",
+		Version: "stable",
+	}
+	pkgs, err := p.CIPDPackages([]*config.PreUploadCIPDPackageConfig{pkgCfg1, pkgCfg2})
+	require.NoError(t, err)
+	require.Len(t, pkgs, 2)
+	require.Equal(t, "some/package/def", pkgs[0].Name)
+	require.Equal(t, "/path/to/repo/cipd_pkgs", pkgs[0].Path)
+	require.Equal(t, "version:abc", pkgs[0].Version)
+	require.Equal(t, "another/package", pkgs[1].Name)
+	require.Equal(t, ".", pkgs[1].Path)
+	require.Equal(t, "stable", pkgs[1].Version)
+}
+
+func TestPlaceholders_Command(t *testing.T) {
+	p := Placeholders{
+		ParentRepoDir: "/path/to/repo",
+		RollingFromID: "abc",
+		RollingToID:   "def",
+	}
+	cmd, err := p.Command(t.Context(), []string{"echo", "hello", "${rolling_to}"}, "${parent_dir}", []string{"VAR=${rolling_from}"})
+	require.NoError(t, err)
+	require.True(t, strings.HasSuffix(cmd.Name, "echo"))
+	require.Equal(t, []string{"hello", "def"}, cmd.Args)
+	require.Equal(t, "/path/to/repo", cmd.Dir)
+	require.Equal(t, []string{"VAR=abc"}, cmd.Env)
+}
+
+func TestPlaceholders_PreUploadConfig(t *testing.T) {
+	p := Placeholders{
+		ParentRepoDir: "/path/to/repo",
+		RollingFromID: "abc",
+		RollingToID:   "def",
+	}
+	cfg := &config.PreUploadConfig{
+		CipdPackage: []*config.PreUploadCIPDPackageConfig{
+			{
+				Name:    "some/package/${rolling_to}",
+				Path:    "${parent_dir}/cipd_pkgs",
+				Version: "version:${rolling_from}",
+			},
+		},
+		Command: []*config.PreUploadCommandConfig{
+			{
+				Command: "echo hello ${rolling_to}",
+				Cwd:     "${parent_dir}",
+				Env:     []string{"VAR=${rolling_from}"},
+			},
+		},
+	}
+	cipdPkgs, cmds, err := p.PreUploadConfig(t.Context(), []string{}, cfg)
+	require.NoError(t, err)
+	require.Len(t, cipdPkgs, 1)
+	require.Equal(t, "some/package/def", cipdPkgs[0].Name)
+	require.Len(t, cmds, 1)
+	require.True(t, strings.HasSuffix(cmds[0].Name, "echo"))
+	require.Equal(t, []string{"hello", "def"}, cmds[0].Args)
+	require.Equal(t, "/path/to/repo", cmds[0].Dir)
+	require.Equal(t, []string{"VAR=abc"}, cmds[0].Env)
 }
