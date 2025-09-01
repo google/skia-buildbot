da279fb6dd34022b1b94d2c52cdabdbad203b486
Joe Gregorio <joe@bitworking.org>
2025-09-01 14:08:09 -0400

    bazel test //perf/go/... now passes with all needed emulators started by run_emulators.

--- bazel/external/cipd/git/git.go
+++ bazel/external/cipd/git/git.go
@@ -3,12 +3,8 @@ package git
 import (
 	"context"
 	"os/exec"
-	"path/filepath"
-	"runtime"
 
-	"go.skia.org/infra/bazel/go/bazel"
 	"go.skia.org/infra/go/git/git_common"
-	"go.skia.org/infra/go/skerr"
 )
 
 // FindGit returns the path to the Git binary found in the corresponding CIPD package.
@@ -16,15 +12,15 @@ import (
 // Calling this function from any Go package will automatically establish a Bazel dependency on the
 // corresponding CIPD package, which Bazel will download as needed.
 func FindGit() (string, error) {
-	if !bazel.InBazel() {
-		return exec.LookPath("git")
-	}
-	if runtime.GOOS == "windows" {
-		return filepath.Join(bazel.RunfilesDir(), "external", "git_amd64_windows", "bin", "git.exe"), nil
-	} else if runtime.GOOS == "linux" {
-		return filepath.Join(bazel.RunfilesDir(), "external", "git_amd64_linux", "bin", "git"), nil
-	}
-	return "", skerr.Fmt("unsupported runtime.GOOS: %q", runtime.GOOS)
+	//	if !bazel.InBazel() {
+	return exec.LookPath("git")
+	//}
+	//	if runtime.GOOS == "windows" {
+	//		return filepath.Join(bazel.RunfilesDir(), "external", "git_amd64_windows", "bin", "git.exe"), nil
+	//	} else if runtime.GOOS == "linux" {
+	//		return filepath.Join(bazel.RunfilesDir(), "external", "git_amd64_linux", "bin", "git"), nil
+	//	}
+	//	return "", skerr.Fmt("unsupported runtime.GOOS: %q", runtime.GOOS)
 }
 
 func UseGitFinder(ctx context.Context) context.Context {
