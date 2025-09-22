1e1e631c654ea8524d538f0d5f745c06c7a35d50
Tony Seaward <seawardt@google.com>
2025-09-22 10:25:29 -0700

    Revert "Fix concurrent map write in TraceSourceInfo"
    This reverts commit 04794e62d8f9c7afe07b0a0d51553ff843990777.
    
    Reason for revert: Perf is crashing
    
    Original change's description:
    > Fix concurrent map write in TraceSourceInfo
    >
    > - add a RWMutex to protect the map operations.
    >
    > Change-Id: Ic908ff9f2e18f7bea1a18b644142f1399cf69c90
    > Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/1059600
    > Reviewed-by: Tony Seaward <seawardt@google.com>
    > Commit-Queue: Ashwin Verleker <ashwinpv@google.com>
    
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Change-Id: I72e20286571aebd0eb20ca388a77649af5b81a88
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/1059616
    Commit-Queue: Tony Seaward <seawardt@google.com>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>

--- perf/go/types/types.go
+++ perf/go/types/types.go
@@ -2,7 +2,6 @@ package types
 
 import (
 	"fmt"
-	"sync"
 	"time"
 
 	"go.skia.org/infra/go/vec32"
@@ -257,23 +256,10 @@ var (
 type TraceSourceInfo struct {
 	// Map where the key is a commit number and the value is a source file id.
 	sourceMap map[CommitNumber]int64
-
-	// Mutex to synchronize operations on the map.
-	mutex sync.RWMutex
-}
-
-// NewTraceSourceInfo returns a new TraceSourceInfo object.
-func NewTraceSourceInfo() *TraceSourceInfo {
-	return &TraceSourceInfo{
-		sourceMap: map[CommitNumber]int64{},
-		mutex:     sync.RWMutex{},
-	}
 }
 
 // Add adds a new source file id for a commit number to the info.
 func (ts *TraceSourceInfo) Add(commitNumber CommitNumber, sourceFileID int64) {
-	ts.mutex.Lock()
-	defer ts.mutex.Unlock()
 	if ts.sourceMap == nil {
 		ts.sourceMap = map[CommitNumber]int64{}
 	}
@@ -283,8 +269,6 @@ func (ts *TraceSourceInfo) Add(commitNumber CommitNumber, sourceFileID int64) {
 // Get retrieves the source file id for a commit number.
 // Returns a false value when commit number is not present.
 func (ts *TraceSourceInfo) Get(commitNumber CommitNumber) (int64, bool) {
-	ts.mutex.RLock()
-	defer ts.mutex.RUnlock()
 	if ts.sourceMap == nil {
 		ts.sourceMap = map[CommitNumber]int64{}
 	}
@@ -294,15 +278,10 @@ func (ts *TraceSourceInfo) Get(commitNumber CommitNumber) (int64, bool) {
 
 // CopyFrom copies the data from the provided TraceSourceInfo object
 // into the current one.
-func (ts *TraceSourceInfo) CopyFrom(other *TraceSourceInfo) {
-	ts.mutex.Lock()
-	defer ts.mutex.Unlock()
+func (ts *TraceSourceInfo) CopyFrom(other TraceSourceInfo) {
 	if ts.sourceMap == nil {
 		ts.sourceMap = map[CommitNumber]int64{}
 	}
-	// We need to lock the 'other' object as well to safely read its map.
-	other.mutex.RLock()
-	defer other.mutex.RUnlock()
 	for commitNumber, sourceFileID := range other.sourceMap {
 		ts.sourceMap[commitNumber] = sourceFileID
 	}
@@ -310,8 +289,6 @@ func (ts *TraceSourceInfo) CopyFrom(other *TraceSourceInfo) {
 
 // GetAllSourceFileIds returns all the sourceFileIds added to the object.
 func (ts *TraceSourceInfo) GetAllSourceFileIds() []int64 {
-	ts.mutex.RLock()
-	defer ts.mutex.RUnlock()
 	ret := []int64{}
 	if ts.sourceMap != nil {
 		for _, sourceFileID := range ts.sourceMap {
@@ -323,8 +300,6 @@ func (ts *TraceSourceInfo) GetAllSourceFileIds() []int64 {
 
 // GetAllCommitNumbers returns all the commit numbers added to the object.
 func (ts *TraceSourceInfo) GetAllCommitNumbers() []CommitNumber {
-	ts.mutex.RLock()
-	defer ts.mutex.RUnlock()
 	ret := []CommitNumber{}
 	if ts.sourceMap != nil {
 		for commitNumber := range ts.sourceMap {
