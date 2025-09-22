e83761c754a12b56983c7b5fcfde2cf31c3dfaf8
Marcin Mordecki <mordeckimarcin@google.com>
2025-09-22 16:31:46 +0000

    [Perf] fix google-chart legend
    Bug: b/445926691 and b/445593547
    
    Remove CachedDataView and CachedDataTable, since they have been the source of problems.
    
    R=seawardt@google.com
    
    Change-Id: I67013f9f4e19235f31133b2bf5fc7d11b03374dc
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/1059556
    Reviewed-by: Tony Seaward <seawardt@google.com>
    Commit-Queue: Tony Seaward <seawardt@google.com>

--- perf/modules/plot-google-chart-sk/plot-google-chart-sk.ts
+++ perf/modules/plot-google-chart-sk/plot-google-chart-sk.ts
@@ -246,11 +246,6 @@ export class PlotGoogleChartSk extends LitElement {
   // cache the googleChart object within the module
   private chart: google.visualization.CoreChartBase | null = null;
 
-  // cache the DataView to avoid re-creating it on every update.
-  private _cachedDataView: google.visualization.DataView | null = null;
-
-  private _cachedDataTable: DataTable = null;
-
   // cache the labels which were removed, so that they can be easily re-added
   private removedLabelsCache: string[] = [];
 
@@ -384,11 +379,7 @@ export class PlotGoogleChartSk extends LitElement {
       return;
     }
 
-    if (this._cachedDataTable !== dt) {
-      this._cachedDataTable = dt;
-      this._cachedDataView = new google.visualization.DataView(dt!);
-    }
-    const view = this._cachedDataView!;
+    const view = new google.visualization.DataView(dt!);
     const ncols = view.getNumberOfColumns();
 
     // The first two columns are the commit position and the date.
