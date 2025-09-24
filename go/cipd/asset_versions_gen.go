d4a9810bb62e2ad50d06725fb86712d431b31e77
skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
2025-09-23 23:12:19 -0700

    Roll Skia Infra CIPD packages from 568da2c9bd8c to 068b075f5ef2 (2 revisions)
    https://skia.googlesource.com/buildbot.git/+log/568da2c9bd8c..068b075f5ef2
    
    2025-09-24 skia-autoroll@skia-public.iam.gserviceaccount.com Roll Skia Infra CIPD packages from 00b2af652822 to 568da2c9bd8c (10 revisions)
    2025-09-24 skia-autoroll@skia-public.iam.gserviceaccount.com Roll Depot Tools from 9b361d9c0954 to 838e4c6aa7b6 (4 revisions)
    
    If this roll has caused a breakage, revert this CL and stop the roller
    using the controls here:
    https://autoroll.skia.org/r/skia-infra-cipd-infra
    Please CC kjlubick@google.com,skiabot@google.com on the revert to ensure that a human
    is aware of the problem.
    
    To file a bug in Skia Infra CIPD packages: https://bugs.chromium.org/p/skia/issues/entry
    To file a bug in Skia Infra: https://bugs.chromium.org/p/skia/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://issues.skia.org/issues/new?component=1389291&template=1850622
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Tbr: kjlubick@google.com
    Change-Id: Ife56fd65213e2a25e8490409e6833ee1678e46ab
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/1060916
    Commit-Queue: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
    Bot-Commit: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>

--- go/cipd/asset_versions_gen.go
+++ go/cipd/asset_versions_gen.go
@@ -196,17 +196,17 @@ var PACKAGES = map[string]*Package{
 	"skia/tools/bazel_build_all/${platform}": {
 		Path:    "task_drivers",
 		Name:    "skia/tools/bazel_build_all/${platform}",
-		Version: "git_revision:568da2c9bd8c3d603760a409d54385604aef2bc2",
+		Version: "git_revision:068b075f5ef24a88053a06b25c7af0df9694ba46",
 	},
 	"skia/tools/bazel_test_all/${platform}": {
 		Path:    "task_drivers",
 		Name:    "skia/tools/bazel_test_all/${platform}",
-		Version: "git_revision:568da2c9bd8c3d603760a409d54385604aef2bc2",
+		Version: "git_revision:068b075f5ef24a88053a06b25c7af0df9694ba46",
 	},
 	"skia/tools/command_wrapper/${platform}": {
 		Path:    "task_drivers",
 		Name:    "skia/tools/command_wrapper/${platform}",
-		Version: "git_revision:568da2c9bd8c3d603760a409d54385604aef2bc2",
+		Version: "git_revision:068b075f5ef24a88053a06b25c7af0df9694ba46",
 	},
 	"skia/tools/goldctl/${platform}": {
 		Path:    "cipd_bin_packages",
@@ -216,6 +216,6 @@ var PACKAGES = map[string]*Package{
 	"skia/tools/presubmit/${platform}": {
 		Path:    "task_drivers",
 		Name:    "skia/tools/presubmit/${platform}",
-		Version: "git_revision:568da2c9bd8c3d603760a409d54385604aef2bc2",
+		Version: "git_revision:068b075f5ef24a88053a06b25c7af0df9694ba46",
 	},
 }
--- go/depot_tools/deps/deps_gen.go
+++ go/depot_tools/deps/deps_gen.go
@@ -164,17 +164,17 @@ var deps = deps_parser.DepsEntries{
 	},
 	"skia/tools/bazel_build_all": {
 		Id:      "skia/tools/bazel_build_all",
-		Version: "git_revision:568da2c9bd8c3d603760a409d54385604aef2bc2",
+		Version: "git_revision:068b075f5ef24a88053a06b25c7af0df9694ba46",
 		Path:    "task_drivers",
 	},
 	"skia/tools/bazel_test_all": {
 		Id:      "skia/tools/bazel_test_all",
-		Version: "git_revision:568da2c9bd8c3d603760a409d54385604aef2bc2",
+		Version: "git_revision:068b075f5ef24a88053a06b25c7af0df9694ba46",
 		Path:    "task_drivers",
 	},
 	"skia/tools/command_wrapper": {
 		Id:      "skia/tools/command_wrapper",
-		Version: "git_revision:568da2c9bd8c3d603760a409d54385604aef2bc2",
+		Version: "git_revision:068b075f5ef24a88053a06b25c7af0df9694ba46",
 		Path:    "task_drivers",
 	},
 	"skia/tools/goldctl": {
@@ -184,7 +184,7 @@ var deps = deps_parser.DepsEntries{
 	},
 	"skia/tools/presubmit": {
 		Id:      "skia/tools/presubmit",
-		Version: "git_revision:568da2c9bd8c3d603760a409d54385604aef2bc2",
+		Version: "git_revision:068b075f5ef24a88053a06b25c7af0df9694ba46",
 		Path:    "task_drivers",
 	},
 }
