674357119b91590b2682f1290366480398f4133a
skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
2025-10-15 21:01:56 -0700

    Roll Skia Infra CIPD packages from fb1930c766c4 to 97b75fbdfdf0 (6 revisions)
    https://skia.googlesource.com/buildbot.git/+log/fb1930c766c4..97b75fbdfdf0
    
    2025-10-15 ashwinpv@google.com Remove the write operations to the postings table
    2025-10-15 ansid@google.com [perfserver] Set issue id in anomalygroups table on REPORT
    2025-10-15 mordeckimarcin@google.com GetAnomalyBy...: Move filtering to sql
    2025-10-15 mordeckimarcin@google.com Implement new group report by anomaly group id.
    2025-10-15 skia-autoroll@skia-public.iam.gserviceaccount.com Roll Skia Infra CIPD packages from 1b83298e5499 to fb1930c766c4 (7 revisions)
    2025-10-15 skia-autoroll@skia-public.iam.gserviceaccount.com Roll Depot Tools from f39ee3c6735f to 13964577cd89 (4 revisions)
    
    If this roll has caused a breakage, revert this CL and stop the roller
    using the controls here:
    https://autoroll.skia.org/r/skia-infra-cipd-infra
    Please CC borenet@google.com,skiabot@google.com on the revert to ensure that a human
    is aware of the problem.
    
    To file a bug in Skia Infra CIPD packages: https://bugs.chromium.org/p/skia/issues/entry
    To file a bug in Skia Infra: https://bugs.chromium.org/p/skia/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://issues.skia.org/issues/new?component=1389291&template=1850622
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Tbr: borenet@google.com
    Change-Id: I8eff5499cc94f1300706c5a60b6b94ceb0736b22
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/1076159
    Commit-Queue: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
    Bot-Commit: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>

--- DEPS
+++ DEPS
@@ -4,7 +4,7 @@ vars = {
   'cpython_version':    'version:2@2.7.18.chromium.47',
   'cpython3_version':   'version:3@3.11.9.chromium.36',
   'luci_tools_version': 'git_revision:7537fa4503847dabbf52ec69c429f0e5952330aa',
-  'skia_tools_version': 'git_revision:fb1930c766c49b33fb8bded842923eab53a87cf8',
+  'skia_tools_version': 'git_revision:97b75fbdfdf0eb0f3ba020b57fb3421c2b7c1442',
   'tools_git_version':  'version:3@2.51.0.chromium.11',
 }
 
--- go/cipd/asset_versions_gen.go
+++ go/cipd/asset_versions_gen.go
@@ -196,17 +196,17 @@ var PACKAGES = map[string]*Package{
 	"skia/tools/bazel_build_all/${platform}": {
 		Path:    "task_drivers",
 		Name:    "skia/tools/bazel_build_all/${platform}",
-		Version: "git_revision:fb1930c766c49b33fb8bded842923eab53a87cf8",
+		Version: "git_revision:97b75fbdfdf0eb0f3ba020b57fb3421c2b7c1442",
 	},
 	"skia/tools/bazel_test_all/${platform}": {
 		Path:    "task_drivers",
 		Name:    "skia/tools/bazel_test_all/${platform}",
-		Version: "git_revision:fb1930c766c49b33fb8bded842923eab53a87cf8",
+		Version: "git_revision:97b75fbdfdf0eb0f3ba020b57fb3421c2b7c1442",
 	},
 	"skia/tools/command_wrapper/${platform}": {
 		Path:    "task_drivers",
 		Name:    "skia/tools/command_wrapper/${platform}",
-		Version: "git_revision:fb1930c766c49b33fb8bded842923eab53a87cf8",
+		Version: "git_revision:97b75fbdfdf0eb0f3ba020b57fb3421c2b7c1442",
 	},
 	"skia/tools/goldctl/${platform}": {
 		Path:    "cipd_bin_packages",
@@ -216,6 +216,6 @@ var PACKAGES = map[string]*Package{
 	"skia/tools/presubmit/${platform}": {
 		Path:    "task_drivers",
 		Name:    "skia/tools/presubmit/${platform}",
-		Version: "git_revision:fb1930c766c49b33fb8bded842923eab53a87cf8",
+		Version: "git_revision:97b75fbdfdf0eb0f3ba020b57fb3421c2b7c1442",
 	},
 }
