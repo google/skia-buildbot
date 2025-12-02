1073a9e32be16dc79a50dca20f9a0f3706afd5b9
skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
2025-12-01 20:01:42 -0800

    Roll Skia Infra CIPD packages from 419c2b30a277 to 6c10ef924124 (12 revisions)
    https://skia.googlesource.com/buildbot.git/+log/419c2b30a277..6c10ef924124
    
    2025-12-01 ashwinpv@google.com Gold updates to support new instances
    2025-12-01 seawardt@google.com Perf: Add Gemini side panel
    2025-12-01 borenet@google.com [bazel] Remove run_commands_* from skia_app_container
    2025-12-01 borenet@google.com [bazel] Make npm-audit-mirror not use run_commands_root
    2025-12-01 jiaxindong@google.com Add more test in the regressions page.
    2025-12-01 eduardoyap@google.com feat: Update fuchsia-exp-internal notification to anomalygroup
    2025-12-01 ansid@google.com Fix: update Redis instance names in chrome-internal-ng and v8-internal-ng perf configs
    2025-12-01 skia-autoroll@skia-public.iam.gserviceaccount.com Roll Depot Tools from 8efa575d754b to c81b50d23457 (1 revision)
    2025-12-01 skia-autoroll@skia-public.iam.gserviceaccount.com Roll LUCI CIPD packages from f1d8bd5336c2 to 808a00437f24 (1 revision)
    2025-12-01 skia-autoroll@skia-public.iam.gserviceaccount.com Roll Skia Infra CIPD packages from 60a43373eff9 to 419c2b30a277 (5 revisions)
    2025-12-01 skia-autoroll@skia-public.iam.gserviceaccount.com Roll jsfiddle-final from 843c9396daa6 to 92efb0ddb9b8
    2025-12-01 skia-autoroll@skia-public.iam.gserviceaccount.com Roll debugger-app-final from 012767186853 to 6e0291dcb56a
    
    If this roll has caused a breakage, revert this CL and stop the roller
    using the controls here:
    https://autoroll.skia.org/r/skia-infra-cipd-infra
    Please CC kjlubick@google.com,skiabot@google.com on the revert to ensure that a human
    is aware of the problem.
    
    To file a bug in Skia Infra CIPD packages: https://bugs.chromium.org/p/skia/issues/entry
    To file a bug in Skia Infra: https://bugs.chromium.org/p/skia/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://issues.skia.org/issues/new?component=1389291&template=1850622
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Tbr: kjlubick@google.com
    Change-Id: I1218f4524aadf66dba9f08272f140398ea154687
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/1113942
    Bot-Commit: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
    Commit-Queue: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>

--- DEPS
+++ DEPS
@@ -4,7 +4,7 @@ vars = {
   'cpython_version':    'version:2@2.7.18.chromium.47',
   'cpython3_version':   'version:3@3.11.9.chromium.36',
   'luci_tools_version': 'git_revision:808a00437f24bb404c09608ad8bf3847a78de369',
-  'skia_tools_version': 'git_revision:419c2b30a277872a5c961e8b55864b853eda38a2',
+  'skia_tools_version': 'git_revision:6c10ef92412464d7c7eb37966311cd1786b9b915',
   'tools_git_version':  'version:3@2.51.2.chromium.11',
 }
 
--- go/depot_tools/deps/deps_gen.go
+++ go/depot_tools/deps/deps_gen.go
@@ -84,17 +84,17 @@ var deps = deps_parser.DepsEntries{
 	},
 	"skia/tools/bazel_build_all": {
 		Id:      "skia/tools/bazel_build_all",
-		Version: "git_revision:419c2b30a277872a5c961e8b55864b853eda38a2",
+		Version: "git_revision:6c10ef92412464d7c7eb37966311cd1786b9b915",
 		Path:    "task_drivers",
 	},
 	"skia/tools/bazel_test_all": {
 		Id:      "skia/tools/bazel_test_all",
-		Version: "git_revision:419c2b30a277872a5c961e8b55864b853eda38a2",
+		Version: "git_revision:6c10ef92412464d7c7eb37966311cd1786b9b915",
 		Path:    "task_drivers",
 	},
 	"skia/tools/command_wrapper": {
 		Id:      "skia/tools/command_wrapper",
-		Version: "git_revision:419c2b30a277872a5c961e8b55864b853eda38a2",
+		Version: "git_revision:6c10ef92412464d7c7eb37966311cd1786b9b915",
 		Path:    "task_drivers",
 	},
 	"skia/tools/goldctl": {
@@ -104,7 +104,7 @@ var deps = deps_parser.DepsEntries{
 	},
 	"skia/tools/presubmit": {
 		Id:      "skia/tools/presubmit",
-		Version: "git_revision:419c2b30a277872a5c961e8b55864b853eda38a2",
+		Version: "git_revision:6c10ef92412464d7c7eb37966311cd1786b9b915",
 		Path:    "task_drivers",
 	},
 }
