1881e6c9200e7618766ae2eff961a6412cc2fef8
Maxim Sheshukov <maximsheshukov@google.com>
2026-02-13 14:43:58 +0000

    Display raw list of jobs instead of hello world
    This CL checks connectivity to the legacy Pinpoint.
    
    Change-Id: I98e10bbaf812b4f749e3a644b5c3c1cf028f835f
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/1165416
    Reviewed-by: Anri Sidorov <ansid@google.com>
    Commit-Queue: Maxim Sheshukov <maximsheshukov@google.com>

--- pinpoint/go/webui/main.go
+++ pinpoint/go/webui/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"net/http"
 
 	"go.skia.org/infra/go/common"
+	"go.skia.org/infra/go/httputils"
 	"go.skia.org/infra/go/sklog"
 )
 
@@ -15,9 +17,18 @@ var (
 )
 
 func serveHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain")
-	if _, err := fmt.Fprintln(w, "Hello Pinpoint"); err != nil {
-		sklog.Errorf("Failed writing response: %s", err)
+	client := httputils.NewTimeoutClient()
+	resp, err := client.Get("https://pinpoint-dot-chromeperf.appspot.com/api/jobs")
+	if err != nil {
+		http.Error(w, "Failed to fetch jobs", http.StatusInternalServerError)
+		sklog.Errorf("Failed fetching jobs: %s", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	w.Header().Set("Content-Type", resp.Header.Get("Content-Type"))
+	if _, err := io.Copy(w, resp.Body); err != nil {
+		http.Error(w, fmt.Sprintf("Failed writing response: %s", err), http.StatusInternalServerError)
 	}
 }
 
