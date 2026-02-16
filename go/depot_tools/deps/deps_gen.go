68204d778857fe8f21933caffab384d8af44a43b
skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
2026-02-15 20:01:09 -0800

    Roll Skia Infra CIPD packages from 8ff522b74b01 to db23e5d58c86 (8 revisions)
    https://skia.googlesource.com/buildbot.git/+log/8ff522b74b01..db23e5d58c86
    
    2026-02-13 seawardt@google.com Perf: Update run_perfserver.sh for robust local development
    2026-02-13 ashwinpv@google.com Add the evals folder to history rag ingester image
    2026-02-13 ashwinpv@google.com Enable historyrag eval pipeline for the staging instance
    2026-02-13 zhanliang@google.com [CBB] Show results with alternative Finch variations
    2026-02-13 jiaxindong@google.com Add puppeteer test for triage-menu-sk.
    2026-02-13 borenet@google.com [codereview-watcher] Close PR before commenting
    2026-02-13 skia-autoroll@skia-public.iam.gserviceaccount.com Roll Depot Tools from 425882d8c0ac to f1715a77db07 (4 revisions)
    2026-02-13 skia-autoroll@skia-public.iam.gserviceaccount.com Roll Skia Infra CIPD packages from 63fc10e8f242 to 8ff522b74b01 (7 revisions)
    
    If this roll has caused a breakage, revert this CL and stop the roller
    using the controls here:
    https://autoroll.skia.org/r/skia-infra-cipd-infra
    Please CC borenet@google.com,skiabot@google.com on the revert to ensure that a human
    is aware of the problem.
    
    To file a bug in Skia Infra CIPD packages: https://bugs.chromium.org/p/skia/issues/entry
    To file a bug in Skia Infra: https://bugs.chromium.org/p/skia/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://issues.skia.org/issues/new?component=1389291&template=1850622
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Tbr: borenet@google.com
    Change-Id: I45ca697cbf4b0273b36b4db3ad059e2896ca5949
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/1166416
    Commit-Queue: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>
    Bot-Commit: skia-autoroll <skia-autoroll@skia-public.iam.gserviceaccount.com>

--- go/depot_tools/deps/deps_gen.go
+++ go/depot_tools/deps/deps_gen.go
@@ -84,17 +84,17 @@ var deps = deps_parser.DepsEntries{
 	},
 	"skia/tools/bazel_build_all": {
 		Id:      "skia/tools/bazel_build_all",
-		Version: "git_revision:8ff522b74b01b88620f2823d2a936eb065cab990",
+		Version: "git_revision:db23e5d58c862ba324c46b34c9ea0a787ccd139a",
 		Path:    "task_drivers",
 	},
 	"skia/tools/bazel_test_all": {
 		Id:      "skia/tools/bazel_test_all",
-		Version: "git_revision:8ff522b74b01b88620f2823d2a936eb065cab990",
+		Version: "git_revision:db23e5d58c862ba324c46b34c9ea0a787ccd139a",
 		Path:    "task_drivers",
 	},
 	"skia/tools/command_wrapper": {
 		Id:      "skia/tools/command_wrapper",
-		Version: "git_revision:8ff522b74b01b88620f2823d2a936eb065cab990",
+		Version: "git_revision:db23e5d58c862ba324c46b34c9ea0a787ccd139a",
 		Path:    "task_drivers",
 	},
 	"skia/tools/goldctl": {
@@ -104,7 +104,7 @@ var deps = deps_parser.DepsEntries{
 	},
 	"skia/tools/presubmit": {
 		Id:      "skia/tools/presubmit",
-		Version: "git_revision:8ff522b74b01b88620f2823d2a936eb065cab990",
+		Version: "git_revision:db23e5d58c862ba324c46b34c9ea0a787ccd139a",
 		Path:    "task_drivers",
 	},
 }
