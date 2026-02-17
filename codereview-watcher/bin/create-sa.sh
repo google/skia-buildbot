3e04721eb9f84ccf19309e702475d3900f942952
Eric Boren <borenet@google.com>
2026-02-17 17:00:09 +0000

    Remove codereview-watcher
    Bug: b/484302116
    Change-Id: I898b17dcbd06b1603f4545acb34958acd396b8e8
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/1167322
    Reviewed-by: Kaylee Lubick <kjlubick@google.com>
    Auto-Submit: Eric Boren <borenet@google.com>
    Commit-Queue: Kaylee Lubick <kjlubick@google.com>

--- codereview-watcher/bin/create-sa.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-
-# Creates the service account for codereview-watcher.
-
-../../kube/secrets/add-service-account.sh \
-  skia-public \
-  skia-public \
-  skia-codereview-watcher \
-  "Service account for Codereview Watcher."
