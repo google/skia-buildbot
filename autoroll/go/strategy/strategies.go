795321cd9bb92d0cdddb3cfc3e6628bc4abbc003
Eric Boren <borenet@google.com>
2026-02-20 20:27:14 +0000

    [autoroll] Change n_batch size to 10
    Change-Id: Icde89e7cf69c49c68b31accd9473cea2b10df9fb
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/1171381
    Reviewed-by: Noelle Scobie <nscobie@google.com>
    Auto-Submit: Eric Boren <borenet@google.com>
    Commit-Queue: Noelle Scobie <nscobie@google.com>

--- autoroll/go/strategy/strategies.go
+++ autoroll/go/strategy/strategies.go
@@ -13,7 +13,7 @@ const (
 	ROLL_STRATEGY_SINGLE  = "single"
 
 	// The number of Revisions to use in ROLL_STRATEGY_N_BATCH.
-	N_REVISIONS = 20
+	N_REVISIONS = 10
 )
 
 // NextRollStrategy is an interface for modules which determine what the next
