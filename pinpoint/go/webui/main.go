772370308dd8122c77dbdcaa9896dc5c161f3493
Maxim Sheshukov <maximsheshukov@google.com>
2026-03-06 11:14:11 +0000

    Attempt to connect pinpoint web UI to the legacy backend
    This CL uses `httputils` for creating a pinpoint webui client and sending get requests to the legacy pinpoint API.
    
    This is experimental code and will be completely rewritten later.
    
    Change-Id: I5bf5c5ce3818137b938766f85563a21b28c0368a
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/1180637
    Commit-Queue: Maxim Sheshukov <maximsheshukov@google.com>
    Reviewed-by: Marcin Mordecki <mordeckimarcin@google.com>

--- pinpoint/go/webui/main.go
+++ pinpoint/go/webui/main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"context"
 	"flag"
-	"fmt"
 	"io"
 	"net/http"
+	"time"
 
+	"go.skia.org/infra/go/auth"
 	"go.skia.org/infra/go/common"
 	"go.skia.org/infra/go/httputils"
 	"go.skia.org/infra/go/sklog"
+	"golang.org/x/oauth2/google"
 )
 
 var (
@@ -16,28 +19,36 @@ var (
 	promPort = flag.String("prom_port", ":20000", "Metrics service address (e.g., ':10110')")
 )
 
-func serveHTTP(w http.ResponseWriter, r *http.Request) {
-	client := httputils.NewTimeoutClient()
-	resp, err := client.Get("https://pinpoint-dot-chromeperf.appspot.com/api/jobs")
-	if err != nil {
-		http.Error(w, "Failed to fetch jobs", http.StatusInternalServerError)
-		sklog.Errorf("Failed fetching jobs: %s", err)
-		return
-	}
-	defer resp.Body.Close()
-
-	w.Header().Set("Content-Type", resp.Header.Get("Content-Type"))
-	if _, err := io.Copy(w, resp.Body); err != nil {
-		http.Error(w, fmt.Sprintf("Failed writing response: %s", err), http.StatusInternalServerError)
-	}
-}
-
 func main() {
 	common.InitWithMust(
 		"pinpoint-webui",
 		common.PrometheusOpt(promPort),
 	)
 
-	http.Handle("/", http.HandlerFunc(serveHTTP))
+	ctx := context.Background()
+	tokenSource, err := google.DefaultTokenSource(ctx, auth.ScopeUserinfoEmail)
+	if err != nil {
+		sklog.Fatalf("Failed to create token source: %s", err)
+	}
+
+	client := httputils.DefaultClientConfig().WithTokenSource(tokenSource).Client()
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		ctx, cancel := context.WithTimeout(r.Context(), time.Minute)
+		defer cancel()
+		resp, err := httputils.GetWithContext(ctx, client, "https://pinpoint-dot-chromeperf.appspot.com/api/jobs")
+		if err != nil {
+			sklog.Errorf("Failed to fetch jobs: %s", err)
+			http.Error(w, "Failed to fetch jobs", http.StatusInternalServerError)
+			return
+		}
+		defer resp.Body.Close()
+
+		w.Header().Set("Content-Type", resp.Header.Get("Content-Type"))
+		if _, err := io.Copy(w, resp.Body); err != nil {
+			sklog.Errorf("Failed to write response: %s", err)
+			http.Error(w, "Failed to write response", http.StatusInternalServerError)
+		}
+	})
 	sklog.Fatal(http.ListenAndServe(*port, nil))
 }
