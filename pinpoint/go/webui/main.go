fc36dad50dc65db5d384121fded94d6e165de976
Maxim Sheshukov <maximsheshukov@google.com>
2026-03-06 16:28:21 +0000

    Add a button for creating a test pinpoint job
    This code test integration with the legacy Pinpoint and will not be a part of the production code.
    
    This CL tests the create job API. This functionality works locally
    because of the access to the ADC. It is expected that this functionality
    will not work on the server without user authentication.
    
    Change-Id: If130d89e5a26d79c77d00a67dc6592a4473fae4e
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/1181502
    Reviewed-by: Marcin Mordecki <mordeckimarcin@google.com>
    Commit-Queue: Maxim Sheshukov <maximsheshukov@google.com>

--- pinpoint/go/webui/main.go
+++ pinpoint/go/webui/main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"net/url"
 	"time"
 
 	"go.skia.org/infra/go/auth"
@@ -19,6 +20,36 @@ var (
 	port     = flag.String("port", ":8000", "HTTP service address (e.g., ':8000')")
 	promPort = flag.String("prom_port", ":20000", "Metrics service address (e.g., ':10110')")
 )
+
+const indexHTML = `
+<!DOCTYPE html>
+<html>
+<body>
+    <button id="test-job-btn">Test Job</button>
+    <div id="jobs-container">Loading jobs...</div>
+    <script>
+        document.getElementById('test-job-btn').addEventListener('click', async () => {
+            try {
+                const response = await fetch('/api/testjob', { method: 'POST' });
+                alert(await response.text());
+            } catch (error) {
+                alert('Error: ' + error);
+            }
+        });
+
+        var jobsContainer = document.getElementById('jobs-container');
+        fetch('/api/jobs')
+            .then(response => response.text())
+            .then(text => {
+                jobsContainer.textContent = text;
+            })
+            .catch(err => {
+                jobsContainer.textContent = 'Failed to load jobs: ' + err;
+            });
+    </script>
+</body>
+</html>
+`
 
 func main() {
 	common.InitWithMust(
@@ -36,13 +67,23 @@ func main() {
 	}
 
 	http.HandleFunc("/healthz", httputils.ReadyHandleFunc)
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html")
+		if _, err := w.Write([]byte(indexHTML)); err != nil {
+			http.Error(w, fmt.Sprintf("Failed to write response: %s", err), http.StatusInternalServerError)
+		}
+	})
+
+	http.HandleFunc("/api/jobs", func(w http.ResponseWriter, r *http.Request) {
 		if client == nil {
 			http.Error(w, fmt.Sprintf("Service not fully initialized: %s", tokenSourceErr), http.StatusInternalServerError)
 			return
 		}
+
 		ctx, cancel := context.WithTimeout(r.Context(), time.Minute)
 		defer cancel()
+
 		resp, err := httputils.GetWithContext(ctx, client, "https://pinpoint-dot-chromeperf.appspot.com/api/jobs")
 		if err != nil {
 			http.Error(w, "Failed to fetch jobs", http.StatusInternalServerError)
@@ -52,8 +93,45 @@ func main() {
 
 		w.Header().Set("Content-Type", resp.Header.Get("Content-Type"))
 		if _, err := io.Copy(w, resp.Body); err != nil {
-			http.Error(w, "Failed to write response", http.StatusInternalServerError)
+			http.Error(w, fmt.Sprintf("Failed to write response: %s", err), http.StatusInternalServerError)
 		}
 	})
+
+	http.HandleFunc("/api/testjob", func(w http.ResponseWriter, r *http.Request) {
+		if client == nil {
+			http.Error(w, fmt.Sprintf("Service not fully initialized: %s", tokenSourceErr), http.StatusInternalServerError)
+			return
+		}
+		if r.Method != http.MethodPost {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+
+		ctx, cancel := context.WithTimeout(r.Context(), time.Minute)
+		defer cancel()
+
+		params := url.Values{}
+		params.Set("comparison_mode", "try")
+		params.Set("benchmark", "speedometer3.1.crossbench")
+		params.Set("configuration", "win-11-perf")
+		params.Set("story", "default")
+		params.Set("base_git_hash", "-HEAD")
+		params.Set("end_git_hash", "-HEAD")
+		testJobUrl := fmt.Sprintf("%s?%s", "https://pinpoint-dot-chromeperf.appspot.com/api/new", params.Encode())
+
+		resp, err := httputils.PostWithContext(ctx, client, testJobUrl, "application/json", nil)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusMethodNotAllowed)
+			return
+		}
+		defer resp.Body.Close()
+
+		w.Header().Set("Content-Type", resp.Header.Get("Content-Type"))
+		w.WriteHeader(resp.StatusCode)
+		if _, err := io.Copy(w, resp.Body); err != nil {
+			http.Error(w, fmt.Sprintf("Failed to write response: %s", err), http.StatusInternalServerError)
+		}
+	})
+
 	sklog.Fatal(http.ListenAndServe(*port, nil))
 }
