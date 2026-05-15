316788e581bf4a51b4a4058f955c7a5d9c0548ce
Eric Boren <borenet@google.com>
2026-05-15 15:55:33 +0000

    [autogardener] Add download-debug-info tool
    This just downloads the debug info to a temp directory for inspection.
    
    Bug: b/491418947
    Change-Id: Iae00fd8a5231ccb22352b286917f79b806822fff
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/1236477
    Commit-Queue: Kaylee Lubick <kjlubick@google.com>
    Reviewed-by: Kaylee Lubick <kjlubick@google.com>
    Auto-Submit: Eric Boren <borenet@google.com>

---
+++ autogardener/go/download-debug-info/main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+	"sort"
+	"strings"
+
+	"cloud.google.com/go/storage"
+	"go.skia.org/infra/autogardener/go/gemini"
+	"go.skia.org/infra/go/auth"
+	"go.skia.org/infra/go/sklog"
+	"go.skia.org/infra/go/util"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/option"
+)
+
+var (
+	gcsBucketDebug = flag.String("gcs-bucket-debug", "", "Optional, GCS bucket name to upload debug information.")
+	object         = flag.String("object", "", "GCS object path")
+)
+
+func main() {
+	ctx := context.Background()
+	flag.Parse()
+
+	ts, err := google.DefaultTokenSource(ctx, auth.ScopeReadWrite)
+	if err != nil {
+		sklog.Fatal(err)
+	}
+	gcs, err := storage.NewClient(ctx, option.WithTokenSource(ts))
+	if err != nil {
+		sklog.Fatal(err)
+	}
+	fmt.Printf("%v\n", os.Args)
+	r, err := gcs.Bucket(*gcsBucketDebug).Object(*object).NewReader(ctx)
+	if err != nil {
+		sklog.Fatal(err)
+	}
+	var debug gemini.DebugInfo
+	if err := json.NewDecoder(r).Decode(&debug); err != nil {
+		sklog.Fatal(err)
+	}
+	tmp, err := os.MkdirTemp("", "autogardener-debug-")
+	if err != nil {
+		sklog.Fatal(err)
+	}
+	if err := os.WriteFile(filepath.Join(tmp, "prompt.txt"), []byte(debug.Prompt), os.ModePerm); err != nil {
+		util.RemoveAll(tmp)
+		sklog.Fatal(err)
+	}
+	if err := os.WriteFile(filepath.Join(tmp, "result.txt"), []byte(debug.Result), os.ModePerm); err != nil {
+		util.RemoveAll(tmp)
+		sklog.Fatal(err)
+	}
+	for idx, toolCall := range debug.ToolCalls {
+		argNames := make([]string, 0, len(toolCall.Args))
+		for arg := range toolCall.Args {
+			argNames = append(argNames, arg)
+		}
+		sort.Strings(argNames)
+		var sb strings.Builder
+		_, _ = fmt.Fprintf(&sb, "Tool: %s\nArgs:\n", toolCall.Tool)
+		for _, argName := range argNames {
+			argVal := toolCall.Args[argName]
+			_, _ = fmt.Fprintf(&sb, "- %s = %v\n", argName, argVal)
+		}
+		_, _ = fmt.Fprintf(&sb, "\nResult:\n\n%s\n", toolCall.Result)
+
+		if err := os.WriteFile(filepath.Join(tmp, fmt.Sprintf("tool_call_%d.txt", idx)), []byte(sb.String()), os.ModePerm); err != nil {
+			util.RemoveAll(tmp)
+			sklog.Fatal(err)
+		}
+	}
+	fmt.Printf("Wrote debug info to %s\n", tmp)
+}
