e8cdb991bdd4f0de441f44c41b9980f4b4f4cb65
Maxim Sheshukov <maximsheshukov@google.com>
2026-05-19 09:24:12 +0000

    [pinpoint][webui] Add a home page button
    This CL adds a home page button close to the Pinpoint title. Clicking on the button opens the job list page.
    
    Screenshot:
    https://screenshot.googleplex.com/A3oHXMucxpnRMsX
    
    Bug: b/514546658
    Change-Id: Ie072b0031d0fd359c1d234d4bd66ba07e4df9ff5
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/1237918
    Reviewed-by: Ruslan Schalkenbajew <sruslan@google.com>

--- pinpoint/webui/app/header/header.component.ts
+++ pinpoint/webui/app/header/header.component.ts
@@ -3,12 +3,13 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import { RouterLink } from '@angular/router';
 import { GatewayService } from '../gateway/gateway.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule],
+  imports: [MatToolbarModule, MatButtonModule, MatIconModule, MatMenuModule, RouterLink],
   templateUrl: './header.component.html',
 })
 export class HeaderComponent implements OnInit {
