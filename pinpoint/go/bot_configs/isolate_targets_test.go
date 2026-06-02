b727e25c9c89694f5f5fcccbc9c6a65b0ced90ee
Eduardo Yap <eduardoyap@google.com>
2026-06-02 00:05:11 +0000

    pinpoint: Add android-pixel10-perf bot configuration
    Bug: 517859627
    
    Change-Id: Icaba35766a5af6d5780d5e6ce5b03b14e4e2ee00
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/1251737
    Commit-Queue: Eduardo Yap <eduardoyap@google.com>
    Reviewed-by: Maxim Sheshukov <maximsheshukov@google.com>

--- pinpoint/go/bot_configs/isolate_targets_test.go
+++ pinpoint/go/bot_configs/isolate_targets_test.go
@@ -9,31 +9,37 @@ import (
 
 func TestGetIsolateTarget_WithConfigDefinedBot_ReturnsTarget(t *testing.T) {
 	target, err := GetIsolateTarget("android-pixel4-perf", "benchmark")
-	assert.Equal(t, target, "performance_test_suite_android_trichrome_chrome_google_64_32_bundle")
+	assert.Equal(t, "performance_test_suite_android_trichrome_chrome_google_64_32_bundle", target)
+	assert.NoError(t, err)
+}
+
+func TestGetIsolateTarget_WithPixel10Perf_ReturnsTarget(t *testing.T) {
+	target, err := GetIsolateTarget("android-pixel10-perf", "benchmark")
+	assert.Equal(t, "performance_test_suite_android_trichrome_chrome_google_64_32_bundle", target)
 	assert.NoError(t, err)
 }
 
 func TestGetIsolateTarget_WithPixel10CbbBot_ReturnsTarget(t *testing.T) {
 	target, err := GetIsolateTarget("android-pixel10-perf-cbb", "benchmark")
-	assert.Equal(t, target, "performance_test_suite_android_trichrome_chrome_google_64_32_bundle")
+	assert.Equal(t, "performance_test_suite_android_trichrome_chrome_google_64_32_bundle", target)
 	assert.NoError(t, err)
 }
 
 func TestGetIsolateTarget_WithRegexMatching_ReturnsTarget(t *testing.T) {
 	target, err := GetIsolateTarget("android-pixel4_webview-perf", "benchmark")
-	assert.Equal(t, target, "performance_webview_test_suite")
+	assert.Equal(t, "performance_webview_test_suite", target)
 	assert.NoError(t, err)
 }
 
 func TestGetIsolateTarget_WithConfigUnlistedBot_ReturnsTarget(t *testing.T) {
 	target, err := GetIsolateTarget("linux-perf", "benchmark")
-	assert.Equal(t, target, "performance_test_suite")
+	assert.Equal(t, "performance_test_suite", target)
 	assert.NoError(t, err)
 }
 
 func TestGetIsolateTarget_WithWebRTCBenchmark_ReturnsTarget(t *testing.T) {
 	target, err := GetIsolateTarget("linux-perf", "webrtc_perf_tests")
-	assert.Equal(t, target, "webrtc_perf_tests")
+	assert.Equal(t, "webrtc_perf_tests", target)
 	assert.NoError(t, err)
 }
 
