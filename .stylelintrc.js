653550cd55601322717dd04f88984061e61c20b8
Anri Sidorov <ansid@google.com>
2026-06-11 15:49:46 +0200

    Apply stylelint --fix
    All changes expect for .stylelintrc.js are generated with stylelint
    --fix.
    
    Change-Id: I4a6b31aa8d99d86658469261c7c3d04da38f4321
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/1262276
    Reviewed-by: Maxim Sheshukov <maximsheshukov@google.com>
    SLSA-Policy-Verified: SLSA Policy Verification Service <devtools-gerritcodereview-exitgate@google.com>
    Commit-Queue: Anri Sidorov <ansid@google.com>

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -16,27 +16,8 @@ module.exports = {
     // -- Temporary overrides --
     // TODO(ansid): enable these rules.
 
-    // Autofixable with `stylelint --fix`
-    'rule-empty-line-before': null,
-    'declaration-empty-line-before': null,
-    'at-rule-empty-line-before': null,
-    'scss/double-slash-comment-empty-line-before': null,
-    'comment-whitespace-inside': null,
-    'length-zero-no-unit': null,
-    'color-function-alias-notation': null,
-    'color-function-notation': null,
-    'alpha-value-notation': null,
-    'shorthand-property-no-redundant-values': null,
-    'declaration-block-no-redundant-longhand-properties': null,
-    'media-feature-range-notation': null,
-    'font-family-name-quotes': null,
-    'import-notation': null,
-    'block-no-empty': null,
-
     // Needs manual fixes
-    'scss/load-partial-extension': null,
+    'block-no-empty': null,
     'font-family-no-missing-generic-family-keyword': null,
-    'declaration-block-no-duplicate-properties': null,
-    'property-no-deprecated': null,
   },
 };
