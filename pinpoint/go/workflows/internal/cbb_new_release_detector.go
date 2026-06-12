4b396bbd4f57a5202eaab83f2f8387194d8cd8bf
John Chen <zhanliang@google.com>
2026-06-11 17:48:23 -0700

    [CBB] Temporarly disable STP download
    Safari Technology Preview (STP) for MacOS 15 is no longer available.
    Temporarily disable STP download to avoid errors. Will re-eanble once we
    have the chance to upgrade CBB Mac devices to newer OS.
    
    Bug: b/522979184
    Change-Id: I4162e8635594a2d8fd14046e5be1d3e214ff9a34
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/1262474
    Auto-Submit: John Chen <zhanliang@google.com>
    Commit-Queue: Farid (Mojtaba) Faridzad <faridzad@google.com>
    Reviewed-by: Farid (Mojtaba) Faridzad <faridzad@google.com>

--- pinpoint/go/workflows/internal/cbb_new_release_detector.go
+++ pinpoint/go/workflows/internal/cbb_new_release_detector.go
@@ -213,13 +213,18 @@ func CbbNewReleaseDetectorWorkflow(ctx workflow.Context) (*ChromeReleaseInfo, er
 		wg.Wait(ctx)
 	}
 
-	// Check for new Safari Technology Preview and download it. This is
-	// intentionally done at the end of the workflow, to avoid new Safari TP
-	// being installed on test devices while we're running CBB benchmarks.
-	var stpVersion string
-	if err := workflow.ExecuteActivity(ctx, DownloadSafariTPActivity, isDev).Get(ctx, &stpVersion); err != nil {
-		return nil, skerr.Wrap(err)
-	}
+	// TODO(b/522979184): We require Safari Technology Preview for MacOS 15,
+	// which apparently is no longer available. Temporarily skipping
+	// DownloadSafariTPActivity while we figure out the solution.
+	/*
+		// Check for new Safari Technology Preview and download it. This is
+		// intentionally done at the end of the workflow, to avoid new Safari TP
+		// being installed on test devices while we're running CBB benchmarks.
+		var stpVersion string
+		if err := workflow.ExecuteActivity(ctx, DownloadSafariTPActivity, isDev).Get(ctx, &stpVersion); err != nil {
+			return nil, skerr.Wrap(err)
+		}
+	*/
 
 	return &commitInfo, nil
 }
