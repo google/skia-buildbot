002763e2a59354e452e4960338ed41cf527bcb77
Eric Boren <borenet@google.com>
2026-06-23 15:53:08 +0000

    Add a tool to check tasks.json
    This tool matches tasks in tasks.json to machines in Swarming and
    highlights cases where there are no machines to run sets of tasks or no
    tasks to run on sets of machines.
    
    Bug: b/492480059
    Change-Id: I4e54dbb780d38c065c4546f197a04a44b709754c
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/1269282
    Commit-Queue: Eric Boren <borenet@google.com>
    Reviewed-by: Kaylee Lubick <kjlubick@google.com>

---
+++ task_scheduler/go/check-tasks-json/main.go
@@ -0,0 +1,288 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"sort"
+	"strings"
+
+	apipb "go.chromium.org/luci/swarming/proto/api_v2"
+	"go.skia.org/infra/go/auth"
+	"go.skia.org/infra/go/httputils"
+	"go.skia.org/infra/go/skerr"
+	"go.skia.org/infra/go/sklog"
+	swarmingv2 "go.skia.org/infra/go/swarming/v2"
+	"go.skia.org/infra/go/util"
+	"go.skia.org/infra/task_scheduler/go/specs"
+	"golang.org/x/oauth2/google"
+)
+
+var (
+	swarmingServer = flag.String("swarming", "chromium-swarm.appspot.com", "Swarming server to use.")
+	tasksJSONFile  = flag.String("tasks-json", "", "tasks.json file to check. If not provided, attempts to find it in the current working directory.")
+	showAll        = flag.Bool("show-all", false, "If set, show all task+machine groups as opposed to only those with no matches.")
+)
+
+func main() {
+	ctx := context.Background()
+	flag.Parse()
+	ts, err := google.DefaultTokenSource(ctx, auth.ScopeUserinfoEmail)
+	if err != nil {
+		sklog.Fatal(err)
+	}
+	c := httputils.DefaultClientConfig().WithTokenSource(ts).Client()
+	swarm := swarmingv2.NewDefaultClient(c, *swarmingServer)
+
+	var tasksCfg *specs.TasksCfg
+	if *tasksJSONFile != "" {
+		contents, err := os.ReadFile(*tasksJSONFile)
+		if err != nil {
+			sklog.Fatal(err)
+		}
+		tasksCfg, err = specs.ParseTasksCfg(string(contents))
+	} else {
+		checkoutRoot, err := specs.GetCheckoutRoot()
+		if err != nil {
+			sklog.Fatal(err)
+		}
+		tasksCfg, err = specs.ReadTasksCfg(checkoutRoot)
+	}
+	if err != nil {
+		sklog.Fatal(err)
+	}
+
+	report, err := checkTasksJSON(ctx, tasksCfg, swarm)
+	if err != nil {
+		sklog.Fatal(err)
+	}
+	if len(report.NoMatchingMachines) > 0 {
+		fmt.Println("## The following tasks have no matching machines:")
+		for _, g := range report.NoMatchingMachines {
+			fmt.Println(g.String())
+		}
+	}
+	if len(report.NoMatchingTasks) > 0 {
+		fmt.Println("## The following machines have no matching tasks:")
+		for _, g := range report.NoMatchingTasks {
+			fmt.Println(g.String())
+		}
+	}
+	if *showAll {
+		for _, g := range report.Okay {
+			fmt.Println(g.String())
+		}
+	}
+}
+
+type Report struct {
+	NoMatchingMachines []*Group
+	NoMatchingTasks    []*Group
+	Okay               []*Group
+}
+
+type Group struct {
+	Tasks      []string
+	Machines   []string
+	Dimensions []string
+	LastTaskID string
+}
+
+func printList(w io.Writer, name string, items []string) {
+	_, _ = fmt.Fprintf(w, "%s:\n", name)
+	for _, item := range items {
+		_, _ = fmt.Fprintf(w, "  - %s\n", item)
+	}
+}
+
+func (g *Group) String() string {
+	var sb strings.Builder
+	_, _ = fmt.Fprintln(&sb, "==================================")
+	printList(&sb, "Dimensions", g.Dimensions)
+	if g.LastTaskID != "" {
+		_, _ = fmt.Fprintf(&sb, "Last task: https://%s/task?id=%s", *swarmingServer, g.LastTaskID)
+	}
+	printList(&sb, "Tasks", g.Tasks)
+	_, _ = fmt.Fprintf(&sb, "Swarming search: https://%s/machinelist?f=%s\n", *swarmingServer, strings.Join(g.Dimensions, "&f="))
+	printList(&sb, "Machines", g.Machines)
+	_, _ = fmt.Fprintln(&sb, "==================================")
+	return sb.String()
+}
+
+func checkTasksJSON(ctx context.Context, tasksCfg *specs.TasksCfg, swarm swarmingv2.SwarmingV2Client) (*Report, error) {
+	// Extract the list of Swarming pools to query and the list of dimension
+	// keys used by all tasks.
+	pools := util.StringSet{}
+	allowedDimKeys := util.StringSet{}
+	for taskName, taskSpec := range tasksCfg.Tasks {
+		for _, dim := range taskSpec.Dimensions {
+			split := strings.SplitN(dim, ":", 2)
+			if len(split) != 2 {
+				return nil, skerr.Fmt("Task %s has invalid dimension %s; expected `key:value`", taskName, dim)
+			}
+			allowedDimKeys[split[0]] = true
+			if split[0] == "pool" {
+				pools[split[1]] = true
+			}
+		}
+	}
+	machines, err := loadAllMachines(ctx, swarm, pools.Keys())
+	if err != nil {
+		return nil, skerr.Wrap(err)
+	}
+
+	// Match machines to tasks.
+
+	// Create a machines-by-swarming-dimension mapping.
+	machinesByDim := map[string]util.StringSet{}
+	for _, b := range machines {
+		for _, dim := range b.Dimensions {
+			if _, ok := machinesByDim[dim]; !ok {
+				machinesByDim[dim] = util.StringSet{}
+			}
+			machinesByDim[dim][b.ID] = true
+		}
+	}
+
+	groups := map[string]*Group{}
+	matchedMachine := make(map[string]bool, len(machines))
+	for name, taskSpec := range tasksCfg.Tasks {
+		sort.Strings(taskSpec.Dimensions)
+		key := strings.Join(taskSpec.Dimensions, ";")
+		group, ok := groups[key]
+		if !ok {
+			group = &Group{}
+			groups[key] = group
+		}
+		group.Tasks = append(group.Tasks, name)
+		if ok {
+			// We already have the dimensions and matching machines for this
+			// dimension set.
+			continue
+		}
+
+		group.Dimensions = taskSpec.Dimensions
+
+		matches := util.StringSet{}
+		for i, d := range taskSpec.Dimensions {
+			if i == 0 {
+				matches = matches.Union(machinesByDim[d])
+			} else {
+				matches = matches.Intersect(machinesByDim[d])
+			}
+		}
+		group.Machines = matches.Keys()
+		sort.Strings(group.Machines)
+		for _, machine := range group.Machines {
+			matchedMachine[machine] = true
+		}
+	}
+
+	// Separate the groups which had no matching machines.
+	noMatchingMachines := map[string]*Group{}
+	for key, group := range groups {
+		if len(group.Machines) == 0 {
+			noMatchingMachines[key] = group
+			delete(groups, key)
+			lastTask, err := findLastTask(ctx, swarm, group.Tasks)
+			if err != nil {
+				return nil, skerr.Wrap(err)
+			}
+			if lastTask != nil {
+				group.LastTaskID = lastTask.TaskId
+			}
+		}
+	}
+
+	// Find unused machines.
+	unusedMachines := map[string][]string{}
+	unusedDims := map[string][]string{}
+	for _, machine := range machines {
+		if matchedMachine[machine.ID] {
+			continue
+		}
+		dimensions := make([]string, 0, len(allowedDimKeys))
+		for _, dim := range machine.Dimensions {
+			split := strings.SplitN(dim, ":", 2)
+			if len(split) != 2 {
+				return nil, skerr.Fmt("machine %s has invalid dimension %q; expected `key:value`", machine.ID, dim)
+			}
+			if !allowedDimKeys[split[0]] {
+				continue
+			}
+			dimensions = append(dimensions, dim)
+		}
+		sort.Strings(dimensions)
+		key := strings.Join(dimensions, ";")
+		unusedMachines[key] = append(unusedMachines[key], machine.ID)
+		if _, ok := unusedDims[key]; !ok {
+			unusedDims[key] = dimensions
+		}
+	}
+	noMatchingTasks := map[string]*Group{}
+	for key, machines := range unusedMachines {
+		noMatchingTasks[key] = &Group{
+			Machines:   machines,
+			Dimensions: unusedDims[key],
+		}
+	}
+
+	report := &Report{
+		NoMatchingMachines: make([]*Group, 0, len(noMatchingMachines)),
+		NoMatchingTasks:    make([]*Group, 0, len(noMatchingTasks)),
+		Okay:               make([]*Group, 0, len(groups)),
+	}
+	for _, group := range groups {
+		report.Okay = append(report.Okay, group)
+	}
+	for _, group := range noMatchingMachines {
+		report.NoMatchingMachines = append(report.NoMatchingMachines, group)
+	}
+	for _, group := range noMatchingTasks {
+		report.NoMatchingTasks = append(report.NoMatchingTasks, group)
+	}
+	return report, nil
+}
+
+type Machine struct {
+	ID         string
+	Dimensions []string
+}
+
+func loadAllMachines(ctx context.Context, swarm swarmingv2.SwarmingV2Client, pools []string) ([]*Machine, error) {
+	var rv []*Machine
+	for _, pool := range pools {
+		machines, err := swarmingv2.ListBotsForPool(ctx, swarm, pool)
+		if err != nil {
+			return nil, skerr.Wrap(err)
+		}
+		for _, item := range machines {
+			rv = append(rv, &Machine{
+				ID:         item.BotId,
+				Dimensions: swarmingv2.BotDimensionsToStringSlice(item.Dimensions),
+			})
+		}
+	}
+	return rv, nil
+}
+
+func findLastTask(ctx context.Context, swarm swarmingv2.SwarmingV2Client, taskNames []string) (*apipb.TaskResultResponse, error) {
+	var lastTask *apipb.TaskResultResponse
+	for _, taskName := range taskNames {
+		resp, err := swarm.ListTasks(ctx, &apipb.TasksWithPerfRequest{
+			Limit: 1,
+			Tags:  []string{fmt.Sprintf("name:%s", taskName)},
+		})
+		if err != nil {
+			return nil, skerr.Wrap(err)
+		}
+		for _, task := range resp.Items {
+			if task.CreatedTs.AsTime().After(lastTask.CreatedTs.AsTime()) {
+				lastTask = task
+			}
+		}
+	}
+	return lastTask, nil
+}
