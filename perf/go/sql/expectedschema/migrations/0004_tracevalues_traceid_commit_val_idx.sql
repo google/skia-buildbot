2d09a931106a665262cfd48248566a484931d6c9
Marcin Mordecki <mordeckimarcin@google.com>
2026-07-10 12:23:36 +0000

    Fix 004.sql
    Add idempotence to the statement
    
    Change-Id: I08123a1f5ed5745feae94de8559f6a4e8eea481d
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/1290356
    Commit-Queue: Marcin Mordecki <mordeckimarcin@google.com>
    Commit-Queue: Sergei Rudenkov <sergeirudenkov@google.com>
    Reviewed-by: Sergei Rudenkov <sergeirudenkov@google.com>

--- perf/go/sql/expectedschema/migrations/0004_tracevalues_traceid_commit_val_idx.sql
+++ perf/go/sql/expectedschema/migrations/0004_tracevalues_traceid_commit_val_idx.sql
@@ -1,4 +1,4 @@
 -- Rollback SQL (For manual reference):
--- DROP INDEX by_traceid_commit_number_desc_include_val;
+-- DROP INDEX IF EXISTS by_traceid_commit_number_desc_include_val;
 
-CREATE INDEX by_traceid_commit_number_desc_include_val ON TraceValues (trace_id, commit_number DESC) INCLUDE (val);
+CREATE INDEX IF NOT EXISTS by_traceid_commit_number_desc_include_val ON TraceValues (trace_id, commit_number DESC) INCLUDE (val);
