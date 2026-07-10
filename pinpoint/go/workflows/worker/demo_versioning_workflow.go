a866d203d1e0096292ec610c32ae86e69f129cb9
Sergei Rudenkov <sergeirudenkov@google.com>
2026-07-10 12:51:22 +0000

    [perf] Introduce V2 change to DemoVersioningWorkflow
    This CL simulates a "V2" release for the Temporal versioning demo.
    It introduces a deliberate breaking change by adding a new required
    `version int` argument to the `DemoVersioningWorkflow` signature.
    
    Without Temporal Build-ID versioning, this change would cause
    in-flight V1 workflows to fail with a NonDeterministicWorkflowError
    during replay. With versioning enabled, this CL proves that new
    workflows can be routed to V2 while existing sleeping V1 workflows
    safely finish on sunsetting V1 pods.
    
    Change-Id: I7379c5b046f5b81d2a13083b379237ce341e3514
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/1290277
    Reviewed-by: Ruslan Schalkenbajew <sruslan@google.com>

--- pinpoint/go/workflows/worker/demo_versioning_workflow.go
+++ pinpoint/go/workflows/worker/demo_versioning_workflow.go
@@ -8,13 +8,17 @@ import (
 
 // DemoVersioningWorkflow is a dummy workflow used to demonstrate Build-ID versioning.
 // Version 1 expects a single string argument and sleeps for 15 sec.
-func DemoVersioningWorkflow(ctx workflow.Context, name string) error {
+// V1 signature (left as a comment for the demo):
+// func DemoVersioningWorkflow(ctx workflow.Context, name string) error {
+
+// V2 breaking change: added 'version' argument and new log messages
+func DemoVersioningWorkflow(ctx workflow.Context, name string, version int) error {
 	logger := workflow.GetLogger(ctx)
-	logger.Info("V1 Started", "Name", name)
+	logger.Info("V2 Started! Breaking change deployed successfully.", "Name", name, "Version", version)
 
 	// Sleep for 15 seconds
 	_ = workflow.Sleep(ctx, 15*time.Second)
 
-	logger.Info("V1 Finished successfully!")
+	logger.Info("V2 Finished successfully!")
 	return nil
 }
