7d9bea4955ebbfdc3631b9e5a16270efd21e54d5
Sergei Rudenkov <sergeirudenkov@google.com>
2026-07-13 13:33:48 +0000

    DemoVersioningWorkflow back to v1
    Change-Id: Ie9075ff0852959f723d1931f4ff018ee5b767dc2
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/1292056
    Commit-Queue: Sergei Rudenkov <sergeirudenkov@google.com>
    Reviewed-by: Ruslan Schalkenbajew <sruslan@google.com>

--- pinpoint/go/workflows/worker/demo_versioning_workflow.go
+++ pinpoint/go/workflows/worker/demo_versioning_workflow.go
@@ -8,17 +8,13 @@ import (
 
 // DemoVersioningWorkflow is a dummy workflow used to demonstrate Build-ID versioning.
 // Version 1 expects a single string argument and sleeps for 15 sec.
-// V1 signature (left as a comment for the demo):
-// func DemoVersioningWorkflow(ctx workflow.Context, name string) error {
-
-// V2 breaking change: added 'version' argument and new log messages
-func DemoVersioningWorkflow(ctx workflow.Context, name string, version int) error {
+func DemoVersioningWorkflow(ctx workflow.Context, name string) error {
 	logger := workflow.GetLogger(ctx)
-	logger.Info("V2 Started! Breaking change deployed successfully.", "Name", name, "Version", version)
+	logger.Info("V1 Started! Breaking change deployed successfully.", "Name", name)
 
 	// Sleep for 15 seconds
 	_ = workflow.Sleep(ctx, 15*time.Second)
 
-	logger.Info("V2 Finished successfully!")
+	logger.Info("V1 Finished successfully!")
 	return nil
 }
