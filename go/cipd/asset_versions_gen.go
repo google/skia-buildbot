3a8800d3d80a0bb53325d39b76d03653a2c30994
skia-autoroll@skia-public.iam.gserviceaccount.com <skia-autoroll@skia-public.iam.gserviceaccount.com>
2026-07-22 21:01:19 -0700

    Roll command_wrapper from 220a44d0a9312b3da... to efbe3b5be0bcc47c8... (1 revision)
    2026-07-22 user:louhi-prod-1-6316342352543744@louhi-prod-1.iam.gserviceaccount.com skia/tools/command_wrapper/linux-amd64:EwwYFotaiEFXODFxiBbJyL5uEuFEzHHUF3zjR0NhKAAC
    
    If this roll has caused a breakage, revert this CL and stop the roller
    using the controls here:
    https://autoroll.skia.org/r/command-wrapper-skia-infra
    Please CC borenet@google.com,skiabot@google.com on the revert to ensure that a human
    is aware of the problem.
    
    To file a bug in command_wrapper: https://bugs.chromium.org/p/skia/issues/entry
    To file a bug in Skia Infra: https://bugs.chromium.org/p/skia/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://issues.skia.org/issues/new?component=1389291&template=1850622
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Tbr: borenet@google.com
    Change-Id: Ic42df8a7005166bd8b36fd2eebf0694b93f1df8f
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/1301517
    Bot-Commit: skia-autoroll@skia-public.iam.gserviceaccount.com <skia-autoroll@skia-public.iam.gserviceaccount.com>
    Commit-Queue: skia-autoroll@skia-public.iam.gserviceaccount.com <skia-autoroll@skia-public.iam.gserviceaccount.com>

--- go/cipd/asset_versions_gen.go
+++ go/cipd/asset_versions_gen.go
@@ -126,7 +126,7 @@ var PACKAGES = map[string]*Package{
 	"skia/tools/command_wrapper/${platform}": {
 		Path:    ".",
 		Name:    "skia/tools/command_wrapper/${platform}",
-		Version: "git_revision:220a44d0a9312b3da2769c80e2fdcf8aba071c64",
+		Version: "git_revision:efbe3b5be0bcc47c8511a675ac9925f662b97c9a",
 	},
 	"skia/tools/goldctl/${platform}": {
 		Path:    ".",
