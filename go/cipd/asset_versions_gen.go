6d0e4722a1b8cc2f33162a551c5668adf2b3d5db
skia-autoroll@skia-public.iam.gserviceaccount.com <skia-autoroll@skia-public.iam.gserviceaccount.com>
2026-07-28 07:44:56 -0700

    Manual roll command_wrapper from 3a8800d3d80a0bb53... to 42f146e4e61d7f641... (1 revision)
    Manual roll requested by borenet@google.com
    
    2026-07-28 user:louhi-prod-1-6316342352543744@louhi-prod-1.iam.gserviceaccount.com skia/tools/command_wrapper/linux-amd64:DzAeUIqIxsZ-qut93RTLCIp0OUu9hIVT8TSOjVx4NrEC
    
    If this roll has caused a breakage, revert this CL and stop the roller
    using the controls here:
    https://autoroll.skia.org/r/command-wrapper-skia-infra
    Please CC borenet@google.com,skiabot@google.com on the revert to ensure that a human
    is aware of the problem.
    
    To file a bug in command_wrapper: https://bugs.chromium.org/p/skia/issues/entry
    To file a bug in Skia Infra: https://bugs.chromium.org/p/skia/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://issues.skia.org/issues/new?component=1389291&template=1850622
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Tbr: borenet@google.com
    Change-Id: I4cab54cd4c3e37f7a4be3821760a00e6ded9b500
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/1306717
    Bot-Commit: skia-autoroll@skia-public.iam.gserviceaccount.com <skia-autoroll@skia-public.iam.gserviceaccount.com>
    Commit-Queue: skia-autoroll@skia-public.iam.gserviceaccount.com <skia-autoroll@skia-public.iam.gserviceaccount.com>

--- go/cipd/asset_versions_gen.go
+++ go/cipd/asset_versions_gen.go
@@ -126,7 +126,7 @@ var PACKAGES = map[string]*Package{
 	"skia/tools/command_wrapper/${platform}": {
 		Path:    ".",
 		Name:    "skia/tools/command_wrapper/${platform}",
-		Version: "git_revision:3a8800d3d80a0bb53325d39b76d03653a2c30994",
+		Version: "git_revision:42f146e4e61d7f6416df333db35a4e0dd3a01f43",
 	},
 	"skia/tools/goldctl/${platform}": {
 		Path:    ".",
