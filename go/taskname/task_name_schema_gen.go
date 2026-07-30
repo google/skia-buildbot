e5a5eb98eb1ce9d46f637a7cc77fb221157ea978
Eric Boren <borenet@google.com>
2026-07-30 13:35:50 +0000

    Regenerate task name schema
    Change-Id: I7937bbb16a705ffec81266f8625daa3784c33b82
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/1309856
    Auto-Submit: Eric Boren <borenet@google.com>
    Reviewed-by: Kaylee Lubick <kjlubick@google.com>
    Commit-Queue: Kaylee Lubick <kjlubick@google.com>

--- go/taskname/task_name_schema_gen.go
+++ go/taskname/task_name_schema_gen.go
@@ -6,14 +6,13 @@ var SCHEMA_FROM_GIT = map[string]*Schema{
 	"BazelBuild":  {Keys: []string{"label", "config", "host"}, OptionalKeys: []string(nil), RecurseRoles: []string(nil)},
 	"BazelTest":   {Keys: []string{"task_driver", "label", "build_config", "host"}, OptionalKeys: []string{"test_config"}, RecurseRoles: []string(nil)},
 	"Build":       {Keys: []string{"os", "compiler", "target_arch", "configuration"}, OptionalKeys: []string{"extra_config"}, RecurseRoles: []string(nil)},
-	"BuildStats":  {Keys: []string{"os", "compiler", "target_arch", "configuration"}, OptionalKeys: []string{"extra_config"}, RecurseRoles: []string(nil)},
 	"Canary":      {Keys: []string{"project"}, OptionalKeys: []string(nil), RecurseRoles: []string(nil)},
 	"CodeSize":    {Keys: []string{"binary_name", "os", "compiler", "target_arch", "configuration"}, OptionalKeys: []string{"extra_config"}, RecurseRoles: []string(nil)},
 	"Housekeeper": {Keys: []string{"frequency"}, OptionalKeys: []string{"extra_config"}, RecurseRoles: []string(nil)},
 	"Infra":       {Keys: []string{"frequency"}, OptionalKeys: []string{"extra_config"}, RecurseRoles: []string(nil)},
 	"Perf":        {Keys: []string{"os", "compiler", "model", "cpu_or_gpu", "cpu_or_gpu_value", "arch", "configuration", "test_filter"}, OptionalKeys: []string{"extra_config"}, RecurseRoles: []string(nil)},
 	"Test":        {Keys: []string{"os", "compiler", "model", "cpu_or_gpu", "cpu_or_gpu_value", "arch", "configuration", "test_filter"}, OptionalKeys: []string{"extra_config"}, RecurseRoles: []string(nil)},
-	"Upload":      {Keys: []string(nil), OptionalKeys: []string(nil), RecurseRoles: []string{"Build", "BuildStats", "Perf", "Test"}},
+	"Upload":      {Keys: []string(nil), OptionalKeys: []string(nil), RecurseRoles: []string{"Build", "Perf", "Test"}},
 }
 
 var SEPARATOR_FROM_GIT = "-"
