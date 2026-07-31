d814dbe46b77b0827e6897559a249fb2c342e9a8
skia-autoroll@skia-public.iam.gserviceaccount.com <skia-autoroll@skia-public.iam.gserviceaccount.com>
2026-07-30 21:01:06 -0700

    Roll presubmit from 1ec13b32113053c4c... to 5ded320b8a68a8eac... (1 revision)
    2026-07-29 user:louhi-prod-1-6316342352543744@louhi-prod-1.iam.gserviceaccount.com skia/tools/presubmit/linux-amd64:Itd5RN3EPHohGAP4lpRzPHQvB-zTiQ1RQ9jvtAShvWoC
    
    If this roll has caused a breakage, revert this CL and stop the roller
    using the controls here:
    https://autoroll.skia.org/r/presubmit-skia-infra
    Please CC borenet@google.com,skiabot@google.com on the revert to ensure that a human
    is aware of the problem.
    
    To file a bug in presubmit: https://bugs.chromium.org/p/skia/issues/entry
    To file a bug in Skia Infra: https://bugs.chromium.org/p/skia/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://issues.skia.org/issues/new?component=1389291&template=1850622
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Tbr: borenet@google.com
    Change-Id: I0cca3c23ee947341b6fb179f00db0ffbb31de7a5
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/1310576
    Commit-Queue: skia-autoroll@skia-public.iam.gserviceaccount.com <skia-autoroll@skia-public.iam.gserviceaccount.com>
    Bot-Commit: skia-autoroll@skia-public.iam.gserviceaccount.com <skia-autoroll@skia-public.iam.gserviceaccount.com>

--- go/cipd/asset_versions_gen.go
+++ go/cipd/asset_versions_gen.go
@@ -136,6 +136,6 @@ var PACKAGES = map[string]*Package{
 	"skia/tools/presubmit/${platform}": {
 		Path:    ".",
 		Name:    "skia/tools/presubmit/${platform}",
-		Version: "git_revision:1ec13b32113053c4cf0ba090a87919ec37ee70a2",
+		Version: "git_revision:5ded320b8a68a8eac43728c9ce6c125d6f4069e0",
 	},
 }
