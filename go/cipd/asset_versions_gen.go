dac8e08b121ac33b3f986fe3ebbccba358832110
skia-autoroll@skia-public.iam.gserviceaccount.com <skia-autoroll@skia-public.iam.gserviceaccount.com>
2026-08-02 21:01:26 -0700

    Roll lucicfg from 184210f295937da77... to de24faf21b953ffe4... (1 revision)
    2026-08-03 user:infra-packager@chops-service-accounts.iam.gserviceaccount.com infra/tools/luci/lucicfg/linux-amd64:c49Ad4VgA1JkgCv5N7vDTJ2BmdlGmQeIiaNeOULTQp8C
    
    If this roll has caused a breakage, revert this CL and stop the roller
    using the controls here:
    https://autoroll.skia.org/r/lucicfg-skia-infra
    Please CC borenet@google.com,skiabot@google.com on the revert to ensure that a human
    is aware of the problem.
    
    To file a bug in lucicfg: https://bugs.chromium.org/p/skia/issues/entry
    To file a bug in Skia Infra: https://bugs.chromium.org/p/skia/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://issues.skia.org/issues/new?component=1389291&template=1850622
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Tbr: borenet@google.com
    Change-Id: I340603526172658d5425dfb62b6ab0cdeea38f6f
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/1312257
    Bot-Commit: skia-autoroll@skia-public.iam.gserviceaccount.com <skia-autoroll@skia-public.iam.gserviceaccount.com>
    Commit-Queue: skia-autoroll@skia-public.iam.gserviceaccount.com <skia-autoroll@skia-public.iam.gserviceaccount.com>

--- go/cipd/asset_versions_gen.go
+++ go/cipd/asset_versions_gen.go
@@ -51,7 +51,7 @@ var PACKAGES = map[string]*Package{
 	"infra/tools/luci/lucicfg/${platform}": {
 		Path:    ".",
 		Name:    "infra/tools/luci/lucicfg/${platform}",
-		Version: "git_revision:184210f295937da7708d5787bdc611ca990bb629",
+		Version: "git_revision:de24faf21b953ffe46dd29ef36b8935d6940a513",
 	},
 	"infra/tools/luci/swarming/${platform}": {
 		Path:    ".",
