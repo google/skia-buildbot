d4056032b67fccb4c04979c3f4958f7b6bc0e889
skia-autoroll@skia-public.iam.gserviceaccount.com <skia-autoroll@skia-public.iam.gserviceaccount.com>
2026-08-18 22:23:15 -0700

    Roll git-credential-luci from 2de3d5d5b1e9c5e75... to 0e4cb6152e3aee32e... (1 revision)
    2026-08-19 user:infra-packager@chops-service-accounts.iam.gserviceaccount.com infra/tools/luci/git-credential-luci/linux-amd64:6H2qj_DhYvbyoSdtXij7_ZtNDqQ4CrWjtWkPzeuBU8kC
    
    If this roll has caused a breakage, revert this CL and stop the roller
    using the controls here:
    https://autoroll.skia.org/r/git-credential-luci-skia-infra
    Please CC borenet@google.com,skiabot@google.com on the revert to ensure that a human
    is aware of the problem.
    
    To file a bug in git-credential-luci: https://bugs.chromium.org/p/skia/issues/entry
    To file a bug in Skia Infra: https://bugs.chromium.org/p/skia/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://issues.skia.org/issues/new?component=1389291&template=1850622
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Tbr: borenet@google.com
    Change-Id: I6279cbdbd39db279477a877e3b30e28d8fd38bf6
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/1330496
    Bot-Commit: skia-autoroll@skia-public.iam.gserviceaccount.com <skia-autoroll@skia-public.iam.gserviceaccount.com>
    Commit-Queue: skia-autoroll@skia-public.iam.gserviceaccount.com <skia-autoroll@skia-public.iam.gserviceaccount.com>

--- go/cipd/asset_versions_gen.go
+++ go/cipd/asset_versions_gen.go
@@ -36,7 +36,7 @@ var PACKAGES = map[string]*Package{
 	"infra/tools/luci/git-credential-luci/${platform}": {
 		Path:    ".",
 		Name:    "infra/tools/luci/git-credential-luci/${platform}",
-		Version: "git_revision:2de3d5d5b1e9c5e75f930c363904413000823425",
+		Version: "git_revision:0e4cb6152e3aee32eca411edf436f6017c39068a",
 	},
 	"infra/tools/luci/isolate/${platform}": {
 		Path:    ".",
