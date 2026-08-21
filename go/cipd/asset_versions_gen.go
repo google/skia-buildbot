6587f439dcec62391ffbfb530dfe16851cab822d
skia-autoroll@skia-public.iam.gserviceaccount.com <skia-autoroll@skia-public.iam.gserviceaccount.com>
2026-08-20 21:01:04 -0700

    Roll kitchen from 4110875244eb5702c... to 95618d958fcca225b... (1 revision)
    2026-08-20 user:infra-packager@chops-service-accounts.iam.gserviceaccount.com infra/tools/luci/kitchen/linux-amd64:ml9849xTgEwJn3j-jna_CSKKN6zX53JcqJTlm2_97zIC
    
    If this roll has caused a breakage, revert this CL and stop the roller
    using the controls here:
    https://autoroll.skia.org/r/kitchen-skia-infra
    Please CC borenet@google.com,skiabot@google.com on the revert to ensure that a human
    is aware of the problem.
    
    To file a bug in kitchen: https://bugs.chromium.org/p/skia/issues/entry
    To file a bug in Skia Infra: https://bugs.chromium.org/p/skia/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://issues.skia.org/issues/new?component=1389291&template=1850622
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Tbr: borenet@google.com
    Change-Id: I65125e71471a1911ecbc48fcc7bc2d627a388691
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/1333276
    Bot-Commit: skia-autoroll@skia-public.iam.gserviceaccount.com <skia-autoroll@skia-public.iam.gserviceaccount.com>
    Commit-Queue: skia-autoroll@skia-public.iam.gserviceaccount.com <skia-autoroll@skia-public.iam.gserviceaccount.com>

--- go/cipd/asset_versions_gen.go
+++ go/cipd/asset_versions_gen.go
@@ -46,7 +46,7 @@ var PACKAGES = map[string]*Package{
 	"infra/tools/luci/kitchen/${platform}": {
 		Path:    ".",
 		Name:    "infra/tools/luci/kitchen/${platform}",
-		Version: "git_revision:4110875244eb5702cc7e0b2f815ccd2283ba67ef",
+		Version: "git_revision:95618d958fcca225b4fd9fc69cb7c67b647c7b69",
 	},
 	"infra/tools/luci/lucicfg/${platform}": {
 		Path:    ".",
