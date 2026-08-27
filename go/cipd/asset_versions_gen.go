9e29f25733b2d279ad4a529dad6163be42889642
skia-autoroll@skia-public.iam.gserviceaccount.com <skia-autoroll@skia-public.iam.gserviceaccount.com>
2026-08-26 23:13:08 -0700

    Roll luci-auth from b13ab40e69d4f6190... to 3d1174859c0a5bb00... (1 revision)
    2026-08-27 user:infra-packager@chops-service-accounts.iam.gserviceaccount.com infra/tools/luci-auth/linux-amd64:DMalp74sEUSRJdOgunvhRMVh6_tZhPPqjA5vO5NtaXkC
    
    If this roll has caused a breakage, revert this CL and stop the roller
    using the controls here:
    https://autoroll.skia.org/r/luci-auth-skia-infra
    Please CC borenet@google.com,skiabot@google.com on the revert to ensure that a human
    is aware of the problem.
    
    To file a bug in luci-auth: https://bugs.chromium.org/p/skia/issues/entry
    To file a bug in Skia Infra: https://bugs.chromium.org/p/skia/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://issues.skia.org/issues/new?component=1389291&template=1850622
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Tbr: borenet@google.com
    Change-Id: Ifa5237f57a3dbfccf083c5a5bab31dff3241cdae
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/1336644
    Commit-Queue: skia-autoroll@skia-public.iam.gserviceaccount.com <skia-autoroll@skia-public.iam.gserviceaccount.com>
    Bot-Commit: skia-autoroll@skia-public.iam.gserviceaccount.com <skia-autoroll@skia-public.iam.gserviceaccount.com>

--- go/cipd/asset_versions_gen.go
+++ go/cipd/asset_versions_gen.go
@@ -26,7 +26,7 @@ var PACKAGES = map[string]*Package{
 	"infra/tools/luci-auth/${platform}": {
 		Path:    ".",
 		Name:    "infra/tools/luci-auth/${platform}",
-		Version: "git_revision:b13ab40e69d4f619043106ee53eb9aaf82e71055",
+		Version: "git_revision:3d1174859c0a5bb00d87c78506f8c031afad16aa",
 	},
 	"infra/tools/luci/docker-credential-luci/${platform}": {
 		Path:    ".",
