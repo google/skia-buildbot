2aa7c2d98e4c385d79ac485a0246f05947039a9b
skia-autoroll@skia-public.iam.gserviceaccount.com <skia-autoroll@skia-public.iam.gserviceaccount.com>
2026-09-10 22:41:53 -0700

    Roll cipd from 6389b3fb52460c9c4... to e8ff1a9251fd84ffa... (1 revision)
    2026-09-11 user:infra-packager@chops-service-accounts.iam.gserviceaccount.com infra/tools/cipd/linux-amd64:ALIN5hrmEv4ajtwtO_88fY7bxcrYcb1WAKvIqD7ltJIC
    
    If this roll has caused a breakage, revert this CL and set the roller
    to dry run mode using the controls here:
    https://autoroll.skia.org/r/cipd-skia-infra
    Please CC borenet@google.com,skiabot@google.com on the revert to ensure that a human
    is aware of the problem.
    
    To file a bug in cipd: https://bugs.chromium.org/p/skia/issues/entry
    To file a bug in Skia Infra: https://bugs.chromium.org/p/skia/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://issues.skia.org/issues/new?component=1389291&template=1850622
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Tbr: borenet@google.com
    Change-Id: I7c8783c0e2d2d122960fca77f34bb8e9eba8abae
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/1357976
    Bot-Commit: skia-autoroll@skia-public.iam.gserviceaccount.com <skia-autoroll@skia-public.iam.gserviceaccount.com>
    Commit-Queue: skia-autoroll@skia-public.iam.gserviceaccount.com <skia-autoroll@skia-public.iam.gserviceaccount.com>

--- go/cipd/asset_versions_gen.go
+++ go/cipd/asset_versions_gen.go
@@ -16,7 +16,7 @@ var PACKAGES = map[string]*Package{
 	"infra/tools/cipd/${platform}": {
 		Path:    ".",
 		Name:    "infra/tools/cipd/${platform}",
-		Version: "git_revision:6389b3fb52460c9c4dc9f849860f6ab3606b4b42",
+		Version: "git_revision:e8ff1a9251fd84ffa2645964347cf41281903c09",
 	},
 	"infra/tools/git/${platform}": {
 		Path:    ".",
