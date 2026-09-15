bad6e60fad7b432418be86c5daa8711435e8e43b
skia-autoroll@skia-public.iam.gserviceaccount.com <skia-autoroll@skia-public.iam.gserviceaccount.com>
2026-09-15 00:59:15 -0700

    Roll goldctl from 3d1174859c0a5bb00... to e065d6c92631cf730... (1 revision)
    2026-09-15 user:infra-packager@chops-service-accounts.iam.gserviceaccount.com skia/tools/goldctl/linux-amd64:mzsfuIHZD64HZruM-czaYtjnG2sfC98pAR7fJ2VcSCcC
    
    If this roll has caused a breakage, revert this CL and set the roller
    to dry run mode using the controls here:
    https://autoroll.skia.org/r/goldctl-skia-infra
    Please CC borenet@google.com,skiabot@google.com on the revert to ensure that a human
    is aware of the problem.
    
    To file a bug in goldctl: https://bugs.chromium.org/p/skia/issues/entry
    To file a bug in Skia Infra: https://bugs.chromium.org/p/skia/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://issues.skia.org/issues/new?component=1389291&template=1850622
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Tbr: borenet@google.com
    Change-Id: I70ca3c8e9de32d09ee079eca55e6b20c09a844e8
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/1361456
    Commit-Queue: skia-autoroll@skia-public.iam.gserviceaccount.com <skia-autoroll@skia-public.iam.gserviceaccount.com>
    Bot-Commit: skia-autoroll@skia-public.iam.gserviceaccount.com <skia-autoroll@skia-public.iam.gserviceaccount.com>

--- go/cipd/asset_versions_gen.go
+++ go/cipd/asset_versions_gen.go
@@ -131,7 +131,7 @@ var PACKAGES = map[string]*Package{
 	"skia/tools/goldctl/${platform}": {
 		Path:    ".",
 		Name:    "skia/tools/goldctl/${platform}",
-		Version: "git_revision:3d1174859c0a5bb00d87c78506f8c031afad16aa",
+		Version: "git_revision:e065d6c92631cf7300d87433b416e958539dddd3",
 	},
 	"skia/tools/presubmit/${platform}": {
 		Path:    ".",
