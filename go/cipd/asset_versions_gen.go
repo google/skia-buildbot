9e3fa86a6e0cbb4a04bb75d1f31cce7a82b1c69e
skia-autoroll@skia-public.iam.gserviceaccount.com <skia-autoroll@skia-public.iam.gserviceaccount.com>
2026-09-15 21:01:32 -0700

    Roll bazel_test_all from 7f4ff8fcd686d7589... to bad6e60fad7b43241... (1 revision)
    2026-09-15 user:louhi-prod-1-6316342352543744@louhi-prod-1.iam.gserviceaccount.com skia/tools/bazel_test_all/linux-amd64:WFTr0krn4QkC3awJMNEJT2mfLf4_eHouRmXuOg31loQC
    
    If this roll has caused a breakage, revert this CL and set the roller
    to dry run mode using the controls here:
    https://autoroll.skia.org/r/bazel-test-all-skia-infra
    Please CC borenet@google.com,skiabot@google.com on the revert to ensure that a human
    is aware of the problem.
    
    To file a bug in bazel_test_all: https://bugs.chromium.org/p/skia/issues/entry
    To file a bug in Skia Infra: https://bugs.chromium.org/p/skia/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://issues.skia.org/issues/new?component=1389291&template=1850622
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Tbr: borenet@google.com
    Change-Id: I769589637ff2d4325f47875638807e0c269a9aa9
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/1362757
    Commit-Queue: skia-autoroll@skia-public.iam.gserviceaccount.com <skia-autoroll@skia-public.iam.gserviceaccount.com>
    Bot-Commit: skia-autoroll@skia-public.iam.gserviceaccount.com <skia-autoroll@skia-public.iam.gserviceaccount.com>

--- go/cipd/asset_versions_gen.go
+++ go/cipd/asset_versions_gen.go
@@ -121,7 +121,7 @@ var PACKAGES = map[string]*Package{
 	"skia/tools/bazel_test_all/${platform}": {
 		Path:    ".",
 		Name:    "skia/tools/bazel_test_all/${platform}",
-		Version: "git_revision:7f4ff8fcd686d7589239089966acdab8eaf1a828",
+		Version: "git_revision:bad6e60fad7b432418be86c5daa8711435e8e43b",
 	},
 	"skia/tools/command_wrapper/${platform}": {
 		Path:    ".",
