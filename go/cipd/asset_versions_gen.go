0ab1d49e88f09c7b9a87983214f96e86060947b1
skia-autoroll@skia-public.iam.gserviceaccount.com <skia-autoroll@skia-public.iam.gserviceaccount.com>
2026-09-22 21:01:37 -0700

    Roll presubmit from 8980bebfac32472d2... to 31928f60c2ca96ce3... (1 revision)
    2026-09-22 user:louhi-prod-1-6316342352543744@louhi-prod-1.iam.gserviceaccount.com skia/tools/presubmit/linux-amd64:tPlBNCHoBqIcyfJWzY8MJjajfb3D_xEfO8dmX5jIQXEC
    
    If this roll has caused a breakage, revert this CL and set the roller
    to dry run mode using the controls here:
    https://autoroll.skia.org/r/presubmit-skia-infra
    Please CC borenet@google.com,skiabot@google.com on the revert to ensure that a human
    is aware of the problem.
    
    To file a bug in presubmit: https://bugs.chromium.org/p/skia/issues/entry
    To file a bug in Skia Infra: https://bugs.chromium.org/p/skia/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://issues.skia.org/issues/new?component=1389291&template=1850622
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Tbr: borenet@google.com
    Change-Id: I0f64377c99b2b8c7da8ab730c3f06f3f95ba697e
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/1369519
    Commit-Queue: skia-autoroll@skia-public.iam.gserviceaccount.com <skia-autoroll@skia-public.iam.gserviceaccount.com>
    Bot-Commit: skia-autoroll@skia-public.iam.gserviceaccount.com <skia-autoroll@skia-public.iam.gserviceaccount.com>

--- go/cipd/asset_versions_gen.go
+++ go/cipd/asset_versions_gen.go
@@ -136,6 +136,6 @@ var PACKAGES = map[string]*Package{
 	"skia/tools/presubmit/${platform}": {
 		Path:    ".",
 		Name:    "skia/tools/presubmit/${platform}",
-		Version: "git_revision:8980bebfac32472d2b3b36e3ea13b707da1a2664",
+		Version: "git_revision:31928f60c2ca96ce3cb713014f4cd87bcd5d4b44",
 	},
 }
