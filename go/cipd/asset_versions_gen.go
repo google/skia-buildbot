24c01f9c901b6d17ad3789f25b276c3be6ceea0a
skia-autoroll@skia-public.iam.gserviceaccount.com <skia-autoroll@skia-public.iam.gserviceaccount.com>
2026-09-22 21:01:33 -0700

    Roll isolate from e8ff1a9251fd84ffa... to ed2f1af28c621f1d1... (1 revision)
    2026-09-22 user:infra-packager@chops-service-accounts.iam.gserviceaccount.com infra/tools/luci/isolate/linux-amd64:pAo2gxcv6JrcJqJobeg8BGiPVKYh5MQleLKah5uUqh0C
    
    If this roll has caused a breakage, revert this CL and set the roller
    to dry run mode using the controls here:
    https://autoroll.skia.org/r/isolate-skia-infra
    Please CC borenet@google.com,skiabot@google.com on the revert to ensure that a human
    is aware of the problem.
    
    To file a bug in isolate: https://bugs.chromium.org/p/skia/issues/entry
    To file a bug in Skia Infra: https://bugs.chromium.org/p/skia/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://issues.skia.org/issues/new?component=1389291&template=1850622
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Tbr: borenet@google.com
    Change-Id: Iec14a48cda5964735a86d5abea62bb08b062962b
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/1369559
    Bot-Commit: skia-autoroll@skia-public.iam.gserviceaccount.com <skia-autoroll@skia-public.iam.gserviceaccount.com>
    Commit-Queue: skia-autoroll@skia-public.iam.gserviceaccount.com <skia-autoroll@skia-public.iam.gserviceaccount.com>

--- go/cipd/asset_versions_gen.go
+++ go/cipd/asset_versions_gen.go
@@ -41,7 +41,7 @@ var PACKAGES = map[string]*Package{
 	"infra/tools/luci/isolate/${platform}": {
 		Path:    ".",
 		Name:    "infra/tools/luci/isolate/${platform}",
-		Version: "git_revision:e8ff1a9251fd84ffa2645964347cf41281903c09",
+		Version: "git_revision:ed2f1af28c621f1d15a461cb8aa3c9ea8c8ce2a5",
 	},
 	"infra/tools/luci/kitchen/${platform}": {
 		Path:    ".",
