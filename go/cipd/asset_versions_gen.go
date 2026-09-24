20a34927b2bff72c36d730cb772e4355831b58db
skia-autoroll@skia-public.iam.gserviceaccount.com <skia-autoroll@skia-public.iam.gserviceaccount.com>
2026-09-23 21:58:06 -0700

    Roll vpython3 from 54ed0bd3e4d32804e... to 9b8bbe639b96d3322... (1 revision)
    2026-09-24 user:infra-packager@chops-service-accounts.iam.gserviceaccount.com infra/tools/luci/vpython3/linux-amd64:Fh5kfGUG9dmUYI3_D8WmoYrBrgD0g4FOpewOSXfgP7MC
    
    If this roll has caused a breakage, revert this CL and set the roller
    to dry run mode using the controls here:
    https://autoroll.skia.org/r/vpython3-skia-infra
    Please CC borenet@google.com,skiabot@google.com on the revert to ensure that a human
    is aware of the problem.
    
    To file a bug in vpython3: https://bugs.chromium.org/p/skia/issues/entry
    To file a bug in Skia Infra: https://bugs.chromium.org/p/skia/issues/entry
    
    To report a problem with the AutoRoller itself, please file a bug:
    https://issues.skia.org/issues/new?component=1389291&template=1850622
    
    Documentation for the AutoRoller is here:
    https://skia.googlesource.com/buildbot/+doc/main/autoroll/README.md
    
    Tbr: borenet@google.com
    Change-Id: Ifa3224c7e611aefd9c1d1bb90d9f82cf4d3ca65b
    Reviewed-on: https://skia-review.googlesource.com/c/buildbot/+/1371216
    Commit-Queue: skia-autoroll@skia-public.iam.gserviceaccount.com <skia-autoroll@skia-public.iam.gserviceaccount.com>
    Bot-Commit: skia-autoroll@skia-public.iam.gserviceaccount.com <skia-autoroll@skia-public.iam.gserviceaccount.com>

--- go/cipd/asset_versions_gen.go
+++ go/cipd/asset_versions_gen.go
@@ -61,7 +61,7 @@ var PACKAGES = map[string]*Package{
 	"infra/tools/luci/vpython3/${platform}": {
 		Path:    ".",
 		Name:    "infra/tools/luci/vpython3/${platform}",
-		Version: "git_revision:54ed0bd3e4d32804e63447a506f7ae5c71de16ca",
+		Version: "git_revision:9b8bbe639b96d3322fcd4a695520148bf63c997a",
 	},
 	"skia/bots/bazel": {
 		Path:    "bazel",
